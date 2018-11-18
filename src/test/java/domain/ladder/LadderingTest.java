cd75957b7d4a97b27648fce5fc19f49aea022929
lalwr <lalwrj@gmail.com>
2018-11-19 01:59:39 +0900

    refactor : Test 메소드 수정

--- src/test/java/domain/ladder/LadderingTest.java
+++ src/test/java/domain/ladder/LadderingTest.java
@@ -12,6 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LadderingTest {
 
     Laddering laddering;
+    LadderResult ladderResult;
 
     @Before
     public void init() {
@@ -31,59 +32,25 @@ public class LadderingTest {
         Lines lines = new Lines(line);
 
         laddering = new Laddering(players, lines);
-    }
-
-    /*@Test
-    public void 사다리결과() {
 
         List<String> result = Arrays.asList("꽝","5000","꽝","3000");
-        LadderResult ladderResult = new LadderResult(result);
-        assertThat("" + laddering.showResult("pobi", ladderResult).get(0)+"").isEqualTo("pobi : 꽝");
-        assertThat("" + laddering.showResult("honux", ladderResult).get(0)+"").isEqualTo("honux : 3000");
-        assertThat("" + laddering.showResult("crong", ladderResult).get(0)+"").isEqualTo("crong : 꽝");
-        assertThat("" + laddering.showResult("jk", ladderResult).get(0)+"").isEqualTo("jk : 5000");
+        ladderResult = new LadderResult(result);
 
+        LadderManage ladderManage = new LadderManage(player, line);
+        ladderManage.play("ALL");
+    }
+
+    @Test
+    public void 사다리결과() {
+        assertThat("" + laddering.playerResult("pobi", ladderResult).get(0)+"").isEqualTo("pobi : 꽝");
     }
 
     @Test
     public void 사다리결과_ALL() {
-
-        List<String> result = Arrays.asList("꽝","5000","꽝","3000");
-        LadderResult ladderResult = new LadderResult(result);
-        assertThat("" + laddering.showResult("ALL", ladderResult).get(0)+"").isEqualTo("pobi : 꽝");
-        assertThat("" + laddering.showResult("ALL", ladderResult).get(1)+"").isEqualTo("honux : 3000");
-        assertThat("" + laddering.showResult("ALL", ladderResult).get(2)+"").isEqualTo("crong : 꽝");
-        assertThat("" + laddering.showResult("ALL", ladderResult).get(3)+"").isEqualTo("jk : 5000");
-
-    }*/
-
-    @Test
-    public void 사다리결과_ALL2() {
-
-        List<Player> player = new ArrayList<>();
-        player.add(new Player("pobi"));
-        player.add(new Player("honux"));
-        player.add(new Player("crong"));
-        player.add(new Player("jk"));
-
-        List<Line> line = new ArrayList<>();
-        line.add(new Line(Arrays.asList(true, false, true)));
-        line.add(new Line(Arrays.asList(false, true, false)));
-        line.add(new Line(Arrays.asList(true, false, false)));
-        line.add(new Line(Arrays.asList(false, true, false)));
-        line.add(new Line(Arrays.asList(true, false, true)));
-
-        LadderManage ladderManage = new LadderManage(player, line);
-        ladderManage.play("ALL");
-
-        List<String> result = Arrays.asList("꽝","5000","꽝","3000");
-        LadderResult ladderResult = new LadderResult(result);
-
         assertThat("" + laddering.playerResult("pobi", ladderResult).get(0)+"").isEqualTo("pobi : 꽝");
         assertThat("" + laddering.playerResult("honux", ladderResult).get(1)+"").isEqualTo("honux : 3000");
         assertThat("" + laddering.playerResult("crong", ladderResult).get(2)+"").isEqualTo("crong : 꽝");
         assertThat("" + laddering.playerResult("jk", ladderResult).get(3)+"").isEqualTo("jk : 5000");
-
     }
 
 }
