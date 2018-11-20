c4b4482a67ca129a461f79c6acd553f46c585082
hwanseok-Joo <rokmctkd6@gmail.com>
2018-11-20 17:40:40 +0900

    ladder step1 commit (#20)
    * ladder step1 commit
    
    * step1 피드백 반영 했습니다.

---
+++ src/main/java/Line.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Line {
+    private static final int BEFORE_COUNT = 1;
+    private static final Boolean LINEAR = true;
+    private static final Boolean BLANK = false;
+    private List<Point> points = new ArrayList<>();
+    private int count;
+
+    public Line(int countOfPerson) {
+        this.count = countOfPerson;
+
+    }
+    public void initLine(){
+        for (int i = 0; i < count - BEFORE_COUNT; i++) {
+            points.add(calculatePoint(i));
+        }
+    }
+
+    public Point calculatePoint(int count) {
+        if (count == 0) {
+            return new PointGenerator().getRandomBoolean();
+        }
+        //이전이 직선인지 아닌지 확인!
+        if (validationCheck(points)){
+            return Point.of(BLANK);
+        }
+        return new PointGenerator().getRandomBoolean();
+
+    }
+
+    public Boolean validationCheck(List<Point> points) {
+        return points.get(points.size() - BEFORE_COUNT).equals(Point.of(LINEAR));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder lineString = new StringBuilder();
+        lineString.append("  |  ");
+        points.stream().forEach(point -> lineString.append(point.toString()));
+        return lineString.toString();
+    }
+}
+
+
+
---
+++ src/test/java/LadderTest.java
@@ -0,0 +1,40 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class LadderTest {
+
+    @Test
+    public void 사람수_구하기() throws Exception {
+        String persons="heel,good,fufu,hhh";
+        Person personObject = new Person(persons);
+        int result = personObject.personCounts();
+        assertThat(result).isEqualTo(4);
+    }
+
+    @Test(expected=Exception.class)
+    public void 사람이름_길이체크() throws Exception {
+        String persons = "hwanseok,good,hhlleoe";
+        Person personObject = new Person(persons);
+    }
+    @Test
+    public void 첫번째_포인트_체크() {
+
+        Line line = new Line(4);
+    }
+
+    @Test
+    public void 앞좌표상태_체크() {
+        Line line = new Line(3);
+        List<Point> points = new ArrayList<>();
+        points.add(Point.of(true));
+        boolean result = line.validationCheck(points);
+        assertThat(result).isEqualTo(true);
+
+
+    }
+
+}
