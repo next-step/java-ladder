0dd996e73cde428805502c8fcf1727b8c947982f
Sejong Park <devsejong@gmail.com>
2018-12-03 23:12:08 +0900

    기존 사다리 코드 제거

--- src/main/java/net/chandol/ladder/v1/domain/bridge/BridgesSupplier.java
+++
@@ -1,7 +0,0 @@
-package net.chandol.ladder.v1.domain.bridge;
-
-import net.chandol.ladder.v1.value.Positive;
-
-public interface BridgesSupplier {
-    Bridges createBridges(Positive lineSize, Positive height);
-}
