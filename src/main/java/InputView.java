150f0d94684203d6a495f35a70b6ab444e14eb66
HyeongJu Yoo <zerofunc@naver.com>
2018-12-05 07:05:08 +0900

    add InputView

---
+++ src/main/java/InputView.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class InputView {
+    public List<Participant> getParticipant() {
+
+    }
+
+}
---
+++ src/main/java/LadderGame.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LadderGame {
+    private List<LadderLine> ladderLineList = new ArrayList<LadderLine>();
+
+
+}
