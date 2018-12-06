524c19624e3308496d667994b798fda146cab807
dicorndl <dicorndl@gmail.com>
2018-12-06 23:49:43 +0900

    else 안 쓰기

--- src/main/java/domain/LadderGeneratorFactory.java
+++ src/main/java/domain/LadderGeneratorFactory.java
@@ -12,22 +12,18 @@ public class LadderGeneratorFactory {
     private static final String INPUT_HARD = "상";
 
     public static LadderGenerator create(String level) {
-        HeightLevel heightLevel;
-        PointLevel pointLevel;
-
         if (INPUT_EASY.equals(level)) {
-            heightLevel = HeightLevel.EASY;
-            pointLevel = PointLevel.EASY;
-        } else if (INPUT_NORMAL.equals(level)) {
-            heightLevel = HeightLevel.NORMAL;
-            pointLevel = PointLevel.NORMAL;
-        } else if (INPUT_HARD.equals(level)) {
-            heightLevel = HeightLevel.HARD;
-            pointLevel = PointLevel.HARD;
-        } else {
-            throw new IllegalArgumentException("상, 중, 하 중 입력해야한다.");
+            return new LevelLadderGenerator(HeightLevel.EASY, PointLevel.EASY);
         }
 
-        return new LevelLadderGenerator(heightLevel, pointLevel);
+        if (INPUT_NORMAL.equals(level)) {
+            return new LevelLadderGenerator(HeightLevel.NORMAL, PointLevel.NORMAL);
+        }
+
+        if (INPUT_HARD.equals(level)) {
+            return new LevelLadderGenerator(HeightLevel.HARD, PointLevel.HARD);
+        }
+
+        throw new IllegalArgumentException("상, 중, 하 중 입력해야한다.");
     }
 }
