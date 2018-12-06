bd5c91b080ba96c93a001c4c3e8e8b589246293d
ninezero90hy <ninezero90hy@gmail.com>
2018-12-07 01:43:26 +0900

    사다리 2단계 피드백 적용

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -14,29 +14,6 @@ public abstract class ResultView {
         System.out.println(players.toString());
     }
 
-//    public static void printLadder(final Ladder ladder) {
-//        final String[] results = new String[ladder.getCountOfPlayer()];
-//        initializeResultArray(ladder, results);
-//        calculatorLadderPoint(ladder, results);
-//        printResult(results);
-//    }
-//
-//    private static void calculatorLadderPoint(final Ladder ladder, final String[] results) {
-//        IntStream.range(0, ladder.getLines().size()).forEach(i -> loopLinePoint(results, ladder.getLines().get(i)));
-//    }
-//
-//    private static void loopLinePoint(final String[] results, final Line line) {
-//        IntStream.range(0, line.getPoints().size()).forEach(j -> results[j] += line.isExistence(j) ? "|-----" : "|     ");
-//    }
-//
-//    private static void printResult(final String[] results) {
-//        Arrays.stream(results).forEach(System.out::println);
-//    }
-//
-//    private static void initializeResultArray(final Ladder ladder, final String[] results) {
-//        IntStream.range(0, ladder.getCountOfPlayer()).forEach(i -> results[i] = "");
-//    }
-
     private static void newLine() {
         System.out.println();
     }
@@ -48,4 +25,5 @@ public abstract class ResultView {
     public static void printLadder(final Ladder ladder) {
         System.out.println(ladder.toString());
     }
+
 }
