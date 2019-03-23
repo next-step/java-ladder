2f965601c6d373562939a6d1db02878103f4202b
Park Dongwook <wanderhoward@gmail.com>
2019-03-23 18:23:05 +0900

    Step1. 사다리 생성 시에 참가자들을 입력하여 라인의 Points를 생성하도록 변경

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -6,15 +6,21 @@ import java.util.stream.IntStream;
 
 public class Ladder {
 
+  private List<Participant> participants;
   private List<Line> lines;
 
-  public Ladder(int countOfPerson, int height) {
+  public Ladder(List<Participant> participants, int height) {
 
     if (height <= 0) {
       throw new IllegalArgumentException();
     }
+
+    if (participants == null || participants.isEmpty() || participants.size() == 1) {
+      throw new IllegalArgumentException();
+    }
+
     this.lines = IntStream.range(0, height)
-        .mapToObj(index -> new Line(countOfPerson))
+        .mapToObj(index -> new Line(participants.size()))
         .collect(Collectors.toList());
   }
 
--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -2,6 +2,10 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import org.junit.Test;
 
 public class LadderTest {
@@ -10,11 +14,21 @@ public class LadderTest {
   public void test_ladder_constructor() {
 
     // Given
-    int countOfPerson = 4;
+    String pobi = "pobi";
+    String honux = "honux";
+    String crong = "crong";
+    String jk = "jk";
     int height = 5;
 
+    List<Participant> participants = Arrays.asList(
+        new Participant(pobi),
+        new Participant(honux),
+        new Participant(crong),
+        new Participant(jk)
+    );
+
     // When
-    Ladder ladder = new Ladder(countOfPerson, height);
+    Ladder ladder = new Ladder(participants, height);
 
     // Then
     assertThat(ladder).isNotNull();
@@ -25,21 +39,77 @@ public class LadderTest {
   public void test_ladder_heightIsZero_exception() {
 
     // Given
-    int countOfPerson = 4;
+    String pobi = "pobi";
+    String honux = "honux";
+    String crong = "crong";
+    String jk = "jk";
     int height = 0;
 
+    List<Participant> participants = Arrays.asList(
+        new Participant(pobi),
+        new Participant(honux),
+        new Participant(crong),
+        new Participant(jk)
+    );
+
     // When
-    new Ladder(countOfPerson, height);
+    new Ladder(participants, height);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void test_ladder_heightIsNegative_exception() {
 
     // Given
-    int countOfPerson = 4;
+    String pobi = "pobi";
+    String honux = "honux";
+    String crong = "crong";
+    String jk = "jk";
     int height = -5;
 
+    List<Participant> participants = Arrays.asList(
+        new Participant(pobi),
+        new Participant(honux),
+        new Participant(crong),
+        new Participant(jk)
+    );
+
     // When
-    new Ladder(countOfPerson, height);
+    new Ladder(participants, height);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void test_ladder_participantsIsNull_exception() {
+
+    // Given
+    int height = 5;
+
+    // When
+    new Ladder(null, height);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void test_ladder_participantsIsEmpty_exception() {
+
+    // Given
+    int height = 5;
+    List<Participant> participants = new ArrayList<>();
+
+    // When
+    new Ladder(participants, height);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void test_ladder_participantsIsSingle_exception() {
+
+    // Given
+    String pobi = "pobi";
+    int height = 5;
+
+    List<Participant> participants = Collections.singletonList(
+        new Participant(pobi)
+    );
+
+    // When
+    new Ladder(participants, height);
   }
 }
