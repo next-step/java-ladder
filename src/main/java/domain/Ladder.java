23b20b228f848812dacba7a3a15a5ec6ff97ef78
Park Dongwook <wanderhoward@gmail.com>
2019-03-23 18:15:43 +0900

    Step1. 사다리 생성 시에 입력한 height 만큼 Line 생성하도록 기능 변경

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,18 +1,24 @@
 package domain;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Ladder {
 
-  private int height;
+  private List<Line> lines;
 
-  public Ladder(int height) {
+  public Ladder(int countOfPerson, int height) {
 
     if (height <= 0) {
       throw new IllegalArgumentException();
     }
-    this.height = height;
+    this.lines = IntStream.range(0, height)
+        .mapToObj(index -> new Line(countOfPerson))
+        .collect(Collectors.toList());
   }
 
-  public int getHeight() {
-    return height;
+  public List<Line> getLines() {
+    return lines;
   }
 }
--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -7,36 +7,39 @@ import org.junit.Test;
 public class LadderTest {
 
   @Test
-  public void test_ladder_contructor() {
+  public void test_ladder_constructor() {
 
     // Given
+    int countOfPerson = 4;
     int height = 5;
 
     // When
-    Ladder ladder = new Ladder(height);
+    Ladder ladder = new Ladder(countOfPerson, height);
 
     // Then
     assertThat(ladder).isNotNull();
-    assertThat(ladder.getHeight()).isEqualTo(height);
+    assertThat(ladder.getLines().size()).isEqualTo(height);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void test_ladder_heightIsZero_exception() {
 
     // Given
+    int countOfPerson = 4;
     int height = 0;
 
     // When
-    new Ladder(height);
+    new Ladder(countOfPerson, height);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void test_ladder_heightIsNegative_exception() {
 
     // Given
+    int countOfPerson = 4;
     int height = -5;
 
     // When
-    new Ladder(height);
+    new Ladder(countOfPerson, height);
   }
 }
