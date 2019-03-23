91bbe4950f0eecc0a27762ebff684e66f2e671ca
Namjun Kim <namjunemy@gmail.com>
2019-03-24 02:35:08 +0900

    Person 객체 테스트, 코드 작성

---
+++ src/main/java/ladder/domain/Person.java
@@ -0,0 +1,18 @@
+package ladder.domain;
+
+public class Person {
+    private static final int MAX = 5;
+    private final String name;
+
+    public Person(String name) {
+        if (name.length() > MAX) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
