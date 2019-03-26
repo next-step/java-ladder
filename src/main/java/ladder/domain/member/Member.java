1360d0b068d0f4690081a130b354901d34c4c00a
devson <sonyc5720@gmail.com>
2019-03-27 08:15:51 +0900

    사다리게임에 참여하는 Member 생성

---
+++ src/main/java/ladder/domain/member/Member.java
@@ -0,0 +1,25 @@
+package ladder.domain.member;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Member {
+    private static final int MAX_NAME_LENGTH = 5;
+
+    private final String name;
+
+    public Member(String name) {
+        if (!isValidName(name)) {
+            throw new IllegalArgumentException("It's not valid name");
+        }
+
+        this.name = name;
+    }
+
+    private boolean isValidName(String name) {
+        return StringUtils.isNotBlank(name) && (name.length() <= MAX_NAME_LENGTH);
+    }
+
+    String getName() {
+        return name;
+    }
+}
