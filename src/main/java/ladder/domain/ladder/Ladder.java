fcffbf457d9a22e2c93ed2e87bd3d57943ca6a90
devson <sonyc5720@gmail.com>
2019-03-27 09:27:37 +0900

    사다리의 한줄들(List<Line>)을 멤버로 같는 Ladder class 생성

---
+++ src/main/java/ladder/domain/ladder/Ladder.java
@@ -0,0 +1,11 @@
+package ladder.domain.ladder;
+
+import java.util.List;
+
+public class Ladder {
+    private final List<Line> lines;
+
+    public Ladder(List<Line> lines) {
+        this.lines = lines;
+    }
+}
