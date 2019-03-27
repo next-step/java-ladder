cf8923c8541e75caeb5ca6f19cc86823613a2667
devson <sonyc5720@gmail.com>
2019-03-27 09:28:29 +0900

    게임의 참여하는 참가자들(List<Member>)을 멤버로 갖는 MemberGroup class 생성

---
+++ src/main/java/ladder/domain/member/MemberGroup.java
@@ -0,0 +1,11 @@
+package ladder.domain.member;
+
+import java.util.List;
+
+public class MemberGroup {
+    private final List<Member> members;
+
+    public MemberGroup(List<Member> members) {
+        this.members = members;
+    }
+}
