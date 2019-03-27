38f5b789bbbcd7f97652ea027aa0efbd4595aae4
devson <sonyc5720@gmail.com>
2019-03-27 09:22:38 +0900

    Line validation 메서드 변경

--- src/main/java/ladder/domain/ladder/Line.java
+++ src/main/java/ladder/domain/ladder/Line.java
@@ -8,23 +8,27 @@ public class Line {
     private final List<Point> points;
 
     public Line(List<Point> points) {
-        validateLine(points);
+        if (!isValidLine(points)) {
+            throw new IllegalArgumentException("Horizontal cross lines are duplicated");
+        }
 
         this.points = points;
     }
 
-    private void validateLine(List<Point> points) {
+    private boolean isValidLine(List<Point> points) {
         for (int i = SECOND_INDEX; i < points.size(); i++) {
             Point previous = points.get(i - 1);
             Point current = points.get(i);
 
-            checkCrossDuplicated(previous, current);
+            if (checkCrossDuplicated(previous, current)) {
+                return false;
+            }
         }
+
+        return true;
     }
 
-    private void checkCrossDuplicated(Point previous, Point current) {
-        if (previous.isCross() && current.isCross()) {
-            throw new IllegalArgumentException("Horizontal cross line is duplicated.");
-        }
+    private boolean checkCrossDuplicated(Point previous, Point current) {
+        return previous.isCross() && current.isCross();
     }
 }
