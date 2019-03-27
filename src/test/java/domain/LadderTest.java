670aa5b820ea36f36a1182b38d7a9c07d57eaed4
jcheolho <fnel159@naver.com>
2019-03-28 01:39:29 +0900

    step1: feat: 사다리(수평으로 그려지는 라인 정보를 가짐)

---
+++ src/test/java/domain/LadderTest.java
@@ -0,0 +1,41 @@
+package domain;
+
+import generator.RandomBooleanGenerator;
+import org.junit.*;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LadderTest {
+
+    @Test
+    public void test_겹치는_라인을_제외하고_랜덤으로_라인을_생성() {
+        int length = 8;
+        for (int i = 0; i < 100; i++) {
+            Ladder ladder = new Ladder(length, new RandomBooleanGenerator());
+            assertThat(ladder.getLines())
+                    .hasSize(length)
+                    .doesNotContainSequence(true, true);
+        }
+    }
+
+    @Test
+    public void test_겹치는_라인을_제외하고_모든_라인을_생성() {
+        int length = 8;
+        Ladder ladder = new Ladder(length, () -> true);
+
+        assertThat(ladder.getLines())
+                .hasSize(length)
+                .isEqualTo(Arrays.asList(false, true, false, true, false, true, false, true));
+    }
+
+    @Test
+    public void test_라인을_생성하지_않음() {
+        int length = 8;
+        Ladder ladder = new Ladder(length, () -> false);
+
+        assertThat(ladder.getLines())
+                .isEqualTo(Arrays.asList(false, false, false, false, false, false, false, false));
+    }
+}
