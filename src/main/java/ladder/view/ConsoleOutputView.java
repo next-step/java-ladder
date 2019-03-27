e9bfad96dedc81a8a7f2367e2d68d7d08490fa9c
devson <sonyc5720@gmail.com>
2019-03-27 19:05:28 +0900

    기존 toString의 역할을 domain객체로 옮겼기 때문에 해당 로직 제거

--- src/main/java/ladder/view/ConsoleOutputView.java
+++ src/main/java/ladder/view/ConsoleOutputView.java
@@ -1,46 +1,23 @@
 package ladder.view;
 
-import ladder.domain.member.Member;
+import ladder.domain.LadderGame;
+import ladder.domain.ladder.Ladder;
 import ladder.domain.member.MemberGroup;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class ConsoleOutputView {
     private ConsoleOutputView() {
     }
 
-    public static void printMemberGroup(MemberGroup memberGroup) {
-        System.out.println(getMemberGroupString(memberGroup));
+    public static void printLadderGame(LadderGame ladderGame) {
+        printMemberGroup(ladderGame.getMemberGroup());
+        printLadder(ladderGame.getLadder());
     }
 
-    static String getMemberGroupString(MemberGroup memberGroup) {
-        List<Member> members = memberGroup.getMembers();
-        String whiteSpace = getMaxWhiteSpace(members);
-
-        return members.stream()
-                .map(Member::toString)
-                .collect(Collectors.joining(whiteSpace));
+    private static void printMemberGroup(MemberGroup memberGroup) {
+        System.out.println(memberGroup);
     }
 
-    private static String getMaxWhiteSpace(List<Member> members) {
-        StringBuilder whiteSpace = new StringBuilder();
-        int maxNameLength = getMaxNameLengthOfMembers(members);
-
-        for (int i = 0; i < maxNameLength; i++) {
-            whiteSpace.append(" ");
-        }
-
-        return whiteSpace.toString();
-    }
-
-    private static int getMaxNameLengthOfMembers(List<Member> members) {
-        return members.stream()
-                .mapToInt(member -> {
-                    String name = member.toString();
-                    return name.length();
-                })
-                .max()
-                .getAsInt();
+    private static void printLadder(Ladder ladder) {
+        System.out.println(ladder);
     }
 }
