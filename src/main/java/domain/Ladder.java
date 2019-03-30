b8df99075da2034cde10ac049c4345443ff82737
hyoj <chohyojoy@gmail.com>
2019-03-31 00:16:21 +0900

    Step1 Ladder 접근 제어자 변경
    Ladder 객체 생성은 같은 패키지 내에서만 가능하도록 default (package-private) 으로 변경
    Ladder 객체가 불변함을 보장하도록 멤버 변수 final 적용

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -4,9 +4,9 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class Ladder {
-    private List<Line> lines;
+    final private List<Line> lines;
 
-    protected Ladder(List<Line> lines) {
+    Ladder(List<Line> lines) {
         this.lines = lines;
     }
 
