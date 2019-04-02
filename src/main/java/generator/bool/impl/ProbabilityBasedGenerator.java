7b4c02a06a00974cf4bdb7ede4e84a9be15e4746
jcheolho <fnel159@naver.com>
2019-04-02 22:51:45 +0900

    step4: refactor: 랜덤불린생성기 -> 확률기반불린생성기로 변경

---
+++ src/main/java/generator/bool/impl/ProbabilityBasedGenerator.java
@@ -0,0 +1,29 @@
+package generator.bool.impl;
+
+import java.util.Random;
+import java.util.function.BooleanSupplier;
+
+public class ProbabilityBasedGenerator implements BooleanSupplier {
+
+    private static final int UPPER_BOUND_INCLUSIVE = 100;
+
+    private static final int UNDER_BOUND_INCLUSIVE = 1;
+
+    private Random random;
+
+    private final int percentage;
+
+    public ProbabilityBasedGenerator(int percentage) {
+        this.random = new Random();
+        this.percentage = percentage;
+    }
+
+    @Override
+    public boolean getAsBoolean() {
+        int randomNumber = random.nextInt(UPPER_BOUND_INCLUSIVE) + UNDER_BOUND_INCLUSIVE;
+        if (randomNumber <= percentage) {
+            return true;
+        }
+        return false;
+    }
+}
