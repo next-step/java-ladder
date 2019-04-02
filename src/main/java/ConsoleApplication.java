398756d7e790e95c3a3f11655a16d8f75e1365c4
jcheolho <fnel159@naver.com>
2019-04-02 22:56:19 +0900

    step4: refactor: 리팩토링에 따른 콘솔애플리케이션 변경

--- src/main/java/ConsoleApplication.java
+++ src/main/java/ConsoleApplication.java
@@ -1,7 +1,11 @@
 import domain.game.LadderGameBoard;
 import domain.game.LaddersAndPrizes;
+import domain.ladder.Ladders;
 import domain.player.Players;
 import domain.prize.Prizes;
+import generator.bool.impl.ProbabilityBasedGenerator;
+import generator.ladder.LaddersGenerator;
+import generator.ladder.impl.RandomLineLaddersGenerator;
 import view.InputView;
 import view.ResultView;
 
@@ -10,12 +14,17 @@ import java.io.IOException;
 public class ConsoleApplication {
 
     public static void main(String[] args) throws IOException {
-        Players players = Players.generate(InputView.inputPlayerNames());
-        Prizes prizes = Prizes.generate(InputView.inputRunResults());
-        LaddersAndPrizes laddersAndPrizes = new LaddersAndPrizes(InputView.inputHeight(), prizes);
-        LadderGameBoard gameBoard = new LadderGameBoard(players, laddersAndPrizes);
+        final LaddersGenerator laddersGenerator = new RandomLineLaddersGenerator(new ProbabilityBasedGenerator(50));
+
+        final Players players = Players.generate(InputView.inputPlayerNames());
+        final Prizes prizes = Prizes.generate(InputView.inputRunResults());
+        final Ladders ladders = laddersGenerator.generate(InputView.inputHeight(), prizes.size());
+
+        final LaddersAndPrizes laddersAndPrizes = new LaddersAndPrizes(ladders, prizes);
+        final LadderGameBoard gameBoard = new LadderGameBoard(players, laddersAndPrizes);
 
         ResultView.viewLadders(players, laddersAndPrizes);
+
         String wantSeeResult;
         do {
             wantSeeResult = InputView.inputWantSeeResult();
