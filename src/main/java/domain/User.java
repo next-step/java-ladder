c4cea1b1642a80c608fd5eb5ddcc6b4d5eb6a3b1
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-04-07 21:14:47 +0900

    #step1 - 게임 참여자 이름 저장 객체 추가
    #ADD
    User - 사용자명 lenght check 및 저장
    UserTest - 사용자명 저장 test

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,16 @@
+package domain;
+
+import java.util.Optional;
+
+public class User {
+    private static final int MIN_SIZE = 1;
+    private static final int MAX_SIZE = 5;
+
+    private String name;
+
+    public User(String name) {
+        this.name = Optional.of(name)
+                .filter(s -> s.length() >= MIN_SIZE && s.length() <= MAX_SIZE)
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}
