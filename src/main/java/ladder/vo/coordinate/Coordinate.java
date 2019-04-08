d92eefb76f1e38b292304ac81951460e1e3f928d
devson <sonyc5720@gmail.com>
2019-04-08 16:07:05 +0900

    사다리의 Point의 위치를 가리키는 좌표를 의미하는 Coordinate class 와 좌표값에 해당하는 CoordinateValue class 생성

---
+++ src/main/java/ladder/vo/coordinate/Coordinate.java
@@ -0,0 +1,47 @@
+package ladder.vo.coordinate;
+
+import java.util.Objects;
+
+public class Coordinate {
+    private final CoordinateValue x;
+    private final CoordinateValue y;
+
+    public Coordinate(CoordinateValue x, CoordinateValue y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getXValue() {
+        return x.getValue();
+    }
+
+    public int getYValue() {
+        return y.getValue();
+    }
+
+    public Coordinate left() {
+        return new Coordinate(this.x.decrement(), this.y);
+    }
+
+    public Coordinate right() {
+        return new Coordinate(this.x.increment(), this.y);
+    }
+
+    public Coordinate down() {
+        return new Coordinate(this.x, this.y.decrement());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinate that = (Coordinate) o;
+        return Objects.equals(x, that.x) &&
+                Objects.equals(y, that.y);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
