1ed8a55c68bd7be3a05fdc8ef17f27b6d0b6e31d
hyoj <chohyojoy@gmail.com>
2019-04-09 00:31:50 +0900

    refactor: 라인 객체를 반환하는 메서드는 라인 생성기 인터페이스의 default method 로 변경
    라인 객체를 구성하는 포인트 목록의 생성 로직은 인터페이스 구현체에서 재정의

--- src/main/java/domain/DefaultLineGenerator.java
+++ src/main/java/domain/DefaultLineGenerator.java
@@ -8,19 +8,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class DefaultLineGenerator implements LineGenerator {
-
-    @Override
-    public Line generate(int lineSize) {
-        List<Point> points = new ArrayList<>(lineSize);
-
-        while (points.stream().filter(p -> p.hasDirectionType(DirectionType.NONE)).count() == points.size()) {
-            points = this.createPoints(lineSize);
-        }
-
-        return new Line(points);
-    }
-
-    private List<Point> createPoints(int lineSize) {
+    public List<Point> createPoints(int lineSize) {
         Supplier<Movable> simpleMovableSupplier = () -> Math.round(Math.random()) == 1 ?  Movable.TRUE : Movable.FALSE;
         Function<Boolean, Movable> nextMovableSupplier = currentMovable -> {
             if (currentMovable) {
--- src/main/java/domain/LineGenerator.java
+++ src/main/java/domain/LineGenerator.java
@@ -1,5 +1,18 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface LineGenerator {
-    Line generate(final int lineSize);
+    List<Point> createPoints(int lineSize);
+
+    default Line generate(int lineSize) {
+        List<Point> points = new ArrayList<>(lineSize);
+
+        while (points.stream().filter(p -> p.hasDirectionType(DirectionType.NONE)).count() == points.size()) {
+            points = this.createPoints(lineSize);
+        }
+
+        return new Line(points);
+    }
 }
