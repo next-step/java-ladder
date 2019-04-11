f312eca8d80017441888106e36aedad40c93e9d3
isisrin <isisrin@nate.com>
2019-04-11 09:22:39 +0900

    step2 구현 - @Setter 없앰, for문 -> 스트림으로 바꿈

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -12,14 +12,18 @@ import java.util.stream.IntStream;
 @Getter
 @ToString
 @EqualsAndHashCode
-@Setter // 테스트 코드 하려고 넣었습니다..ㅠㅠ
 public class Ladder {
     public static final int MIN_FLOOR = 2;
-    private List<Line> lines = new ArrayList<>();
 
     public Ladder(int countOfPerson, int height) {
         validateHeight(height);
         initLine(countOfPerson, height);
+    }
+
+    private List<Line> lines = new ArrayList<>();
+
+    protected void setLines(List<Line> lines) {
+        this.lines = lines;
     }
 
     private void validateHeight(int height) {
@@ -32,14 +36,9 @@ public class Ladder {
         IntStream.range(0, height).forEach(count -> lines.add(new Line(countOfPerson)));
     }
 
-    // TODO :  여기는 람다를 어떻게 써야할 지 모르겠습니다..
-    // 람다를 쓰면 pointIndex가 final이지 않으면 안된다고 하고... 묘수 없을런지요??ㅠㅠ
     public int move(int index) {
-        int pointIndex = index;
-//        IntStream.range(0, lines.size()).forEach(lineIndex -> pointIndex = lines.get(lineIndex).movePoint(index));
-        for (int lineIndex = 0; lineIndex < lines.size(); lineIndex++) {
-            pointIndex = lines.get(lineIndex).movePoint(pointIndex);
-        }
-        return pointIndex;
+        final int[] pointIndex = {index};
+        IntStream.range(0, lines.size()).forEach(lineIndex -> pointIndex[0] = lines.get(lineIndex).movePoint(pointIndex[0]));
+        return pointIndex[0];
     }
 }
