6e247b3e1e2443b1d182a25b66b9bbd57210f413
devson <sonyc5720@gmail.com>
2019-04-11 22:18:08 +0900

    특정 지점(Point)에서 좌/우로 이동 가능을 알려주는 Direction class 생성

---
+++ src/test/java/ladder/domain/ladder/DirectionTest.java
@@ -0,0 +1,68 @@
+package ladder.domain.ladder;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+
+public class DirectionTest {
+    @Test
+    public void 양쪽_모두_true일_경우_IllegalStateException() {
+        // when
+        // then
+        assertThatIllegalStateException().isThrownBy(() -> new Direction(true, true));
+    }
+
+    @Test
+    public void 첫번째_Direction은_왼쪽이_false() {
+        // given
+        Direction firstDirection = Direction.first(true);
+
+        // when
+        boolean isLeft = firstDirection.isLeft();
+
+        // then
+        assertThat(isLeft).isFalse();
+    }
+
+    @Test
+    public void 다음_Direction의_left는_이전_Direction의_right를_따라감() {
+        // given
+        boolean rightOfPreviousDirection = true;
+        Direction direction = new Direction(false, rightOfPreviousDirection);
+
+        // when
+        Direction nextDirection = direction.next(false);
+
+        // then
+        assertThat(nextDirection.isLeft()).isEqualTo(rightOfPreviousDirection);
+    }
+
+    // TODO Point를 만드는 쪽에서 테스트
+//    @Test
+//    public void 이전_Direction의_right가_true면_다음_Direction의_right는_false() {
+//        // given
+//        boolean rightOfPreviousDirection = true;
+//        Direction direction = new Direction(false, rightOfPreviousDirection);
+//
+//        // when
+//        Direction nextDirection = direction.next(false);
+//
+//        // then
+//        assertThat(nextDirection.isLeft()).isEqualTo(rightOfPreviousDirection);
+//    }
+
+    @Test
+    public void 마지막_Direction() {
+        // given
+        boolean rightOfPreviousDirection = false;
+        Direction direction = new Direction(false, rightOfPreviousDirection);
+
+        // when
+        Direction lastDirection = direction.last();
+
+        // then
+        assertThat(lastDirection.isLeft()).isEqualTo(rightOfPreviousDirection);
+        assertThat(lastDirection.isRight()).isFalse();
+    }
+}
