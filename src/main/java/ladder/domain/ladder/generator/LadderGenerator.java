93fdbf97d99bbbd3b3e7a251d10403d1e511d9c8
devson <sonyc5720@gmail.com>
2019-04-12 12:01:26 +0900

    LadderGenerator 변경된 설계에 맞춰 변경

--- src/main/java/ladder/domain/ladder/generator/LadderGenerator.java
+++ src/main/java/ladder/domain/ladder/generator/LadderGenerator.java
@@ -1,62 +1,30 @@
 package ladder.domain.ladder.generator;
 
-import ladder.domain.ladder.Ladder;
-import ladder.domain.ladder.Line;
-import ladder.domain.ladder.Lines;
-import ladder.domain.ladder.Point;
+import ladder.domain.ladder.*;
 import ladder.domain.member.MemberGroup;
 import ladder.vo.Length;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LadderGenerator {
-    private static final int FIRST_INDEX = 0;
+    private final DirectionGenerator directionGenerator;
 
-    private final PointGenerator pointGenerator;
-
-    public LadderGenerator(PointGenerator pointGenerator) {
-        this.pointGenerator = pointGenerator;
+    public LadderGenerator(DirectionGenerator directionGenerator) {
+        this.directionGenerator= directionGenerator;
     }
 
 
-    public Ladder generateLadder(MemberGroup memberGroup, Length height) {
-        return new Ladder(generateLines(memberGroup, height));
+    public Ladder2 generateLadder(MemberGroup memberGroup, Length height) {
+        return new Ladder2(generateLines(memberGroup, height));
     }
 
-    Lines generateLines(MemberGroup memberGroup, Length height) {
-        List<Line> lines = IntStream.range(FIRST_INDEX, height.getValue())
-                .mapToObj(i -> generateLine(memberGroup))
+    Lines2 generateLines(MemberGroup memberGroup, Length height) {
+        List<Line2> lines = IntStream.range(0, height.getValue())
+                .mapToObj(i -> Line2.init(memberGroup.getNumberOfMembers(), directionGenerator))
                 .collect(Collectors.toList());
 
-        return new Lines(lines);
-    }
-
-    Line generateLine(MemberGroup memberGroup) {
-        int numberOfMembers = memberGroup.getNumberOfMembers();
-        int lastIndex = numberOfMembers - 1;
-        List<Point> points = new ArrayList<>(numberOfMembers);
-
-        for (int i = 0; i < numberOfMembers; i++) {
-            points.add(getPoint(i, lastIndex, points));
-        }
-
-        return new Line(points);
-    }
-
-    private Point getPoint(int currentIndex, int lastIndex, List<Point> points) {
-        if (FIRST_INDEX == currentIndex) {
-            return this.pointGenerator.generateLeftmost();
-        }
-
-        Point previousPoint = points.get(currentIndex - 1);
-
-        if (lastIndex == currentIndex) {
-            return this.pointGenerator.generateRightmost(previousPoint);
-        }
-
-        return this.pointGenerator.generate(previousPoint);
+        return new Lines2(lines);
     }
 }
