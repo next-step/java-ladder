d93e572994c31f26b89a870bb88b988b3d220107
crystal_rim <remember2889@gmail.com>
2019-04-18 00:07:06 +0900

    [UPDATE] ladder ste4 복잡도 열거형 타입 추가

---
+++ src/main/java/ladder/domain/ComplexityType.java
@@ -0,0 +1,35 @@
+package ladder.domain;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public enum ComplexityType {
+    HIGH("상", 0.9, 20),
+    MIDDLE("중", 0.6, 10),
+    LOW("하", 0.3, 5);
+
+    private final String complexity;
+    private final double probabilityOfLink;
+    private final int height;
+
+    ComplexityType(String complexity, double probabilityOfLink, int height) {
+        this.complexity = complexity;
+        this.probabilityOfLink = probabilityOfLink;
+        this.height = height;
+    }
+
+    public static ComplexityType of(String complexity) {
+        return Arrays.stream(values())
+                .filter(complexityType -> complexityType.complexity.equals(complexity))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("복잡도는 상중하 중에 선택해주세요."));
+    }
+
+    public double getProbabilityOfLink() {
+        return probabilityOfLink;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
