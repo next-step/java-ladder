22c10356fb29aa4d36b40d37164e768a289ae07f
Sunho Hwang <willing1026@gmail.com>
2019-04-26 09:30:35 +0900

    #step3 기능구현 (#134)
    * #결과확인 수정 - 모든결과(all) 보기전까지 반복 확인하도록 수정
    
    * #step3 - direction enum추가, pair next메서드 수정
    
    - Direction Enum추가 및 방향결정 로직 이동 (Pair -> Direciton)
    - 테스트명 수정 (UserTest)
    
    * #Pair 사다리 생성규칙 check 로직 추가, 테스트케이스 추가

--- src/test/java/domain/user/UsersTest.java
+++ src/test/java/domain/user/UsersTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class UsersTest {
 
     @Test
-    public void name() {
+    public void 참여자_생성확인() {
         String names = "pobi,honux,crong,jk";
         Users users = new Users(names);
         assertThat(users.size()).isEqualTo(4);
