ec41124550c286a53c074e7d8e096f712b1ac9f2
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-28 07:35:59 +0900

    [feat] 요구사항 1 - Optional을 활용해 조건에 따른 반환

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,8 @@
 package nextstep.optional;
 
+import java.util.Objects;
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,7 +36,11 @@ public class User {
     }
     
     public static boolean ageIsInRange2(User user) {
-        return false;
+        return Optional.ofNullable(user)
+                .filter(u -> Objects.nonNull(u.age))
+                .filter(u -> u.age >= 30)
+                .filter(u -> u.age <= 45)
+                .isPresent();
     }
 
     @Override
