d610ca47330ed59c7c3b114177feecd4a6b85db5
choebk <qjarbs111@gmail.com>
2019-06-28 02:33:07 +0900

    Step1/java8 (#138)
    * [feature] step1 java 8
    
    * [feature] lambda 적용

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Lambda {
 	public static void printAllOld(List<Integer> numbers) {
@@ -27,30 +28,21 @@ public class Lambda {
 	}
 
 	public static int sumAll(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	        total += number;
-	    }
-	    return total;
+	    return sum(numbers, num -> true);
 	}
 	
 	public static int sumAllEven(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	        if (number % 2 == 0) {
-	            total += number;
-	        }
-	    }
-	    return total;
+	    return sum(numbers, num -> num % 2 == 0);
 	}
 
 	public static int sumAllOverThree(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	    	if (number > 3) {
-	    		total += number;
-	    	}
-	    }
-	    return total;
+		return sum(numbers, num -> num > 3);
+	}
+
+	private static int sum(List<Integer> numbers, Predicate<Integer> predicate) {
+		return numbers.stream()
+			.filter(predicate)
+			.mapToInt(number -> number)
+			.sum();
 	}
 }
