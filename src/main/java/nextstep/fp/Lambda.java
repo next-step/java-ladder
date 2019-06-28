7de59b84f21a038064ad32a8498fbcf961997d55
sunkyung <sunkyungch@gmail.com>
2019-06-28 18:25:44 +0900

    람다 실습 1, 2

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -31,6 +31,16 @@ public class Lambda {
 		return total;
 	}
 
+	public static int sum(List<Integer> numbers, Conditional conditional) {
+		int total = 0;
+		for (int number : numbers) {
+			if(conditional.cal(number)) {
+				total += number;
+			}
+		}
+		return total;
+	}
+
 	public static int sumAll(List<Integer> numbers) {
 	    return sum(numbers, number -> true);
 	}
