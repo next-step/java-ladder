b7f42727deb0517eae2f4b54f5ab6dd27ab4d730
박원영 <wyparks2@gmail.com>
2019-06-28 16:38:41 +0900

    Step1 (#146)
    * test - CarTest 람다 적용
    
    * test - 람다를 활용해 중복 제거
    
    * test - 람다를 활용해 중복 제거2
    
    * test - 람다를 활용해 중복 제거3 - Condition 인터페이스를 통해 코드 중복 제거
    
    * test - map, reduce, filter 실습 1
    
    * test - map, reduce, filter 실습 2
    
    * test - Optional을 활용해 조건에 따른 반환
    
    * test - Optional에서 값을 반환
    
    * test - Optional에서 exception 처리

---
+++ src/main/java/nextstep/fp/Conditional.java
@@ -0,0 +1,10 @@
+package nextstep.fp;
+
+/**
+ * Created by wyparks2@gmail.com on 2019-06-27
+ * Blog : http://wyparks2.github.io
+ * Github : http://github.com/wyparks2
+ */
+public interface Conditional {
+    boolean test (Integer number);
+}
