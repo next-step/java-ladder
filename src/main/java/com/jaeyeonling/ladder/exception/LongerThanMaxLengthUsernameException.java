c75ce2f7184cf14cc9cac7ba86ed47187b66a717
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-28 20:06:45 +0900

    [refactor] LongerThanMaxLengthUsernameException 에러 추가

---
+++ src/main/java/com/jaeyeonling/ladder/exception/LongerThanMaxLengthUsernameException.java
@@ -0,0 +1,12 @@
+package com.jaeyeonling.ladder.exception;
+
+import com.jaeyeonling.ladder.Username;
+
+public class LongerThanMaxLengthUsernameException extends IllegalArgumentException {
+
+    private static final String ERROR_MESSAGE = "유저의 이름의 길이는 %d를 넘을 수 없습니다. (입력 값: %s)";
+
+    public LongerThanMaxLengthUsernameException(final String input) {
+        super(String.format(ERROR_MESSAGE, Username.MAX_LENGTH, input));
+    }
+}
