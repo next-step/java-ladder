3f3ff81e2ce11fdf4f1f698c0e5d85f24842aec2
daioooor36 <pd6016@naver.com>
2019-06-29 00:29:51 +0900

    feat(Users) : getUser 메소드 Optional.ofNullable 적용

--- src/main/java/nextstep/optional/Users.java
+++ src/main/java/nextstep/optional/Users.java
@@ -1,7 +1,7 @@
 package nextstep.optional;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Users {
     static final User DEFAULT_USER = new User("codesquad", 100);
@@ -11,13 +11,11 @@ public class Users {
             new User("pobi", 30),
             new User("jk", 40),
             new User("honux", 45));
-    
+
     User getUser(String name) {
-        for (User user : users) {
-            if (user.matchName(name)) {
-                return user;
-            }
-        }
-        return DEFAULT_USER;
+        return users.stream()
+                .map(user -> Optional.ofNullable(user).orElse(DEFAULT_USER))
+                .filter(user -> user.matchName(name))
+                .findFirst().orElse(DEFAULT_USER);
     }
 }
