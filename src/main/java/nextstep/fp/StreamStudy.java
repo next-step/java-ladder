6aec71d8a31861f9acd0c973523430a7c60df8f9
daioooor36 <pd6016@naver.com>
2019-06-28 23:43:31 +0900

    feat(StreamStudy) : printLongestWordTop100 메소드 기능 구현

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,13 +23,27 @@ public class StreamStudy {
 		}
 		return count;
 	}
-	
+
+	/*
+	단어의 길이가 12자를 초과하는 단어를 추출한다.
+	12자가 넘는 단어 중 길이가 긴 순서로 100개의 단어를 추출한다.
+	단어 중복을 허용하지 않는다. 즉, 서로 다른 단어 100개를 추출해야 한다.
+	추출한 100개의 단어를 출력한다. 모든 단어는 소문자로 출력해야 한다.
+	 */
 	public static void printLongestWordTop100() throws IOException {
 		String contents = new String(Files.readAllBytes(Paths
 				.get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
 		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 		
-		// TODO 이 부분에 구현한다.
+		List<String> wordTop100 = words.stream()
+				.filter(word -> word.length() > 12)
+				.sorted(Comparator.comparing(String::length).reversed())
+				.distinct()
+				.map(word -> word.toLowerCase())
+				.limit(100)
+				.collect(Collectors.toList());
+
+		System.out.println(wordTop100);
 	}
 
 	// List에 담긴 모든 숫자 값을 2배한 결과 List를 생성한다.
