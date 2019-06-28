f070792aa50c11c6925d02b758942155a3a0a8d8
byrage <byrage91@gmail.com>
2019-06-28 09:20:42 +0900

    [step1] refactor: 스트림, 람다, Optional

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -15,30 +15,40 @@ public class StreamStudy {
 				.get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
 		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-		long count = 0;
-		for (String w : words) {
-		  if (w.length() > 12) count++;  
-		}
-		return count;
+		return words.stream()
+				.filter(w -> w.length() > 12)
+				.count();
 	}
 	
 	public static void printLongestWordTop100() throws IOException {
 		String contents = new String(Files.readAllBytes(Paths
 				.get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
 		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
-		
-		// TODO 이 부분에 구현한다.
+
+		words.stream()
+				.filter(s -> s.length() > 12)
+				.sorted((s1, s2)-> s2.length() -  s1.length())
+				.limit(100)
+				.distinct()
+				.map(String::toLowerCase)
+				.forEach(System.out::println);
 	}
 
 	public static List<Integer> doubleNumbers(List<Integer> numbers) {
-		return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
+		return numbers.stream()
+				.map(x -> x * 2)
+				.collect(Collectors.toList());
 	}
 
 	public static long sumAll(List<Integer> numbers) {
-		return numbers.stream().reduce(0, (x, y) -> x + y);
+		return numbers.stream().reduce(0, Integer::sum);
 	}
 
 	public static long sumOverThreeAndDouble(List<Integer> numbers) {
-		return 0;
+
+		return numbers.stream()
+				.filter(number -> number > 3)
+				.mapToInt(number -> number * 2)
+				.sum();
 	}
 }
