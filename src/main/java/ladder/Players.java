84f0431a4c23c2890334cd4a04cdda60d725d40f
changjun89 <leechang0423@naver.com>
2019-06-29 00:57:46 +0900

    [step2] feat(ladder) player는 최소 2명이다.

---
+++ src/main/java/ladder/Players.java
@@ -0,0 +1,43 @@
+package ladder;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Players {
+
+  public static final String PLAYERS_NAME_DELIMITER = ",";
+  List<Player> players;
+
+  public Players(String playersName) {
+    if (isBlank(playersName)) {
+      throw new IllegalArgumentException("players 이름을 입력하세요");
+    }
+    players = initPlayers(playersName);
+
+  }
+
+  private boolean isBlank(String playersName) {
+    return playersName == null || "".equals(playersName.trim());
+  }
+
+  private List<Player> initPlayers(String playersName) {
+    String[] splitNames = splitPlayersName(playersName);
+    checkPlayersCount(splitNames);
+    return Arrays.stream(splitNames)
+        .map(Player::new)
+        .collect(toList());
+  }
+
+  private String[] splitPlayersName(String playersName) {
+    return playersName.split(PLAYERS_NAME_DELIMITER);
+  }
+
+  private void checkPlayersCount(String[] splitNames) {
+    if (splitNames.length < 2) {
+      throw new IllegalArgumentException("player 는 최소 2명입니다.");
+    }
+  }
+
+}
