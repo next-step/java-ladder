a3990ff018f1d12952bfe3e07b908f5e56baeb4a
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-28 19:42:37 +0900

    [test] isNullOrEmpty에 문자열 값을 입력 시 false를 리턴한다. 테스트 추가

--- src/test/java/com/jaeyeonling/ladder/utils/StringUtilsTest.java
+++ src/test/java/com/jaeyeonling/ladder/utils/StringUtilsTest.java
@@ -3,6 +3,7 @@ package com.jaeyeonling.ladder.utils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,6 +18,20 @@ class StringUtilsTest {
 
         // then
         assertThat(isNullOrEmpty).isTrue();
+    }
+
+    @DisplayName("isNullOrEmpty에 문자열 값을 입력 시 false를 리턴한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "asda", "sdfsdf", "asfsdfsd", "e23r32f", "dsvsdfsdsds",
+            "sdvsdvsds", "dvsdwwe", "a", "23gr3eggre23erthge3r4er"
+    })
+    void isNullOrEmpty_notEmptyValue_returnFalse(final String notEmptyValue) {
+        // when
+        final boolean isNullOrEmpty = StringUtils.isNullOrEmpty(notEmptyValue);
+
+        // then
+        assertThat(isNullOrEmpty).isFalse();
     }
 
 }
