5b38fa2d7593e5407c029e300ddf98653eb9b1fb
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-28 19:25:18 +0900

    [test] 유저 이름 생성 테스트 추가

---
+++ src/main/java/com/jaeyeonling/ladder/Username.java
@@ -0,0 +1,14 @@
+package com.jaeyeonling.ladder;
+
+class Username {
+
+    private final String username;
+
+    private Username(final String username) {
+        this.username = username;
+    }
+
+    public static Username of(final String username) {
+        return null;
+    }
+}
---
+++ src/test/java/com/jaeyeonling/ladder/UsernameTest.java
@@ -0,0 +1,20 @@
+package com.jaeyeonling.ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class UsernameTest {
+
+    @DisplayName("유저 이름 생성에 성공한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "김재연",
+            "matt",
+            "kjy",
+            "ggg"
+    })
+    void create(final String rawUsername) {
+        final Username username = Username.of(rawUsername);
+    }
+}
