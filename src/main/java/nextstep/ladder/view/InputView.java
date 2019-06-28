85057933b8c2564f19850088486527a9e4ab7f0f
권병윤 <sksggg123@gmail.com>
2019-06-29 02:19:54 +0900

    [docs] : 요구사항 1차 정리

---
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -0,0 +1,14 @@
+package nextstep.ladder.view;
+
+/**
+ * author       : gwonbyeong-yun <sksggg123>
+ * ------------------------------------------
+ * | email        : sksggg123               |
+ * | github       : github.com/sksggg123    |
+ * | blog         : sksggg123.github.io     |
+ * ------------------------------------------
+ * project      : java-ladder
+ * create date  : 2019-06-29 01:58
+ */
+public interface InputView {
+}
---
+++ src/main/java/nextstep/ladder/view/impl/InputViewImpl.java
@@ -0,0 +1,14 @@
+package nextstep.ladder.view.impl;
+
+/**
+ * author       : gwonbyeong-yun <sksggg123>
+ * ------------------------------------------
+ * | email        : sksggg123               |
+ * | github       : github.com/sksggg123    |
+ * | blog         : sksggg123.github.io     |
+ * ------------------------------------------
+ * project      : java-ladder
+ * create date  : 2019-06-29 02:06
+ */
+public class InputViewImpl {
+}
