ad2586cb9812cd8c2277cdc079e3ecca4a8e1bd1
최경수/Dooray개발실/NE <nova1122@naver.com>
2019-06-30 04:48:07 +0900

    refactor(InputView, LadderLine) : 필요없는 임포트 제거 및, 문자열 수정.

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class InputView {
-    private static final String JOIN_GUIDE_TEXT = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요.";
+    private static final String JOIN_GUIDE_TEXT = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요.)";
     private static final String MAX_LADDER_GUIDE_TEXT = "최대 사다리 높이는 몇 개인가요?";
 
     private static final Scanner SCANNER = new Scanner(System.in);
--- src/main/java/LadderLine.java
+++ src/main/java/LadderLine.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class LadderLine {
     private static final int DO = 0;
