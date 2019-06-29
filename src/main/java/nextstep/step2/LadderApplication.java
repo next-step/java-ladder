75fd5ef836f8219e13561964baeb0e5455f48970
최범균(Beomgyun Choe)/Platform Engineering팀/11ST <choebk@sk.com>
2019-06-30 01:16:35 +0900

    [feature] application, ui, dto 클래스 추가

--- src/main/java/nextstep/step2/LadderApplication.java
+++ src/main/java/nextstep/step2/LadderApplication.java
@@ -1,9 +1,21 @@
-package nextstep.step2.domain;
+package nextstep.step2;
+
+import nextstep.step2.domain.LadderGame;
+import nextstep.step2.domain.Players;
+import nextstep.step2.ui.InputView;
+import nextstep.step2.ui.OutputView;
 
 public class LadderApplication {
 
     public static void main(String[] args) {
+        final var playersDTO = InputView.inputName();
+        final var players = Players.create(playersDTO.getNames());
+        final var height = InputView.inputLadderHeight();
 
+        LadderGame game = new LadderGame(players, height);
+
+        final var result = game.start();
+        OutputView.printLadderModel(players, result);
     }
 
 }
--- src/main/java/nextstep/step2/ui/OutputView.java
+++ src/main/java/nextstep/step2/ui/OutputView.java
@@ -1,4 +1,37 @@
 package nextstep.step2.ui;
 
+import nextstep.step2.domain.Ladder;
+import nextstep.step2.domain.Line;
+import nextstep.step2.domain.Players;
+import nextstep.step2.domain.Point;
+
 public class OutputView {
+    private static final String VERTICAL_LINE = "|";
+    private static final String HORIZONTAL_LINE = "-----";
+    private static final String HORIZONTAL_BLANK = "     ";
+
+    public static void printLadderModel(Players players, Ladder ladder) {
+        System.out.println("\n실행결과\n");
+        players.getPlayers()
+               .stream()
+               .map(player -> String.format("%-5s", player))
+               .forEach(System.out::print);
+        System.out.println();
+
+        for (Line line : ladder.getLines()) {
+            line.getPoints()
+                .stream()
+                .map(OutputView::getLadder)
+                .forEach(System.out::print);
+            System.out.println(VERTICAL_LINE);
+        }
+    }
+
+    private static String getLadder(Point point) {
+        if (point.isPoint()) {
+            return VERTICAL_LINE + HORIZONTAL_LINE;
+        }
+
+        return VERTICAL_LINE + HORIZONTAL_BLANK;
+    }
 }
