b2d9734f41526f1b4c964f3d22877291b2a6ca86
wyparks2 <wyparks2@mrblue.com>
2019-06-29 17:56:21 +0900

    feat - Ladder 구현

---
+++ src/main/java/nextstep/ladder/Ladder.java
@@ -0,0 +1,29 @@
+package nextstep.ladder;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Created by wyparks2@gmail.com on 2019-06-29
+ * Blog : http://wyparks2.github.io
+ * Github : http://github.com/wyparks2
+ */
+public class Ladder {
+    private List<Line> lines;
+
+    public Ladder(int rowSize) {
+        if (rowSize < 1) {
+            throw new IllegalArgumentException("사다리 높이는 1이상이여야 합니다.");
+        }
+
+        this.lines = IntStream.rangeClosed(1, rowSize)
+                .boxed()
+                .map(Line::init)
+                .collect(Collectors.toList());
+    }
+
+    public int size() {
+        return this.lines.size();
+    }
+}
--- src/test/java/nextstep/ladder/LadderTest.java
+++ src/test/java/nextstep/ladder/LadderTest.java
@@ -1,9 +1,27 @@
 package nextstep.ladder;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 /**
  * Created by wyparks2@gmail.com on 2019-06-29
  * Blog : http://wyparks2.github.io
  * Github : http://github.com/wyparks2
  */
-public class LadderTest {
+class LadderTest {
+    @Test
+    void create() {
+        int rowSize = 5;
+        Ladder ladder = new Ladder(5);
+
+        assertThat(ladder.size()).isEqualTo(rowSize);
+    }
+
+    @Test
+    void 사다리_높이는_1이상() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new Ladder(0));
+    }
 }
