fe2d33fd5f1e31ce0a033f06fb8e217208502c79
최범균(Beomgyun Choe)/Platform Engineering팀/11ST <choebk@sk.com>
2019-06-30 01:15:07 +0900

    [feature] Point 객체 및 테스트 추가

--- src/test/java/nextstep/step2/PointTest.java
+++ src/test/java/nextstep/step2/PointTest.java
@@ -1,4 +1,21 @@
 package nextstep.step2;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import nextstep.step2.domain.Point;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 public class PointTest {
+
+    @Test
+    @DisplayName("Point 생성")
+    void create() {
+        assertAll(
+            () -> assertThat(new Point()).isInstanceOf(Point.class),
+            () -> assertThat(Point.create(new Point())).isInstanceOf(Point.class)
+        );
+    }
 }
