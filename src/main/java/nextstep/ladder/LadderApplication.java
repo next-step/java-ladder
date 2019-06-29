bc7575779cd052e35485ec36922d8f6bd7389a98
권병윤 <sksggg123@gmail.com>
2019-06-30 02:48:26 +0900

    [feat] : Controller에서 사다리 입력받는 부분 연결

--- src/main/java/nextstep/ladder/LadderApplication.java
+++ src/main/java/nextstep/ladder/LadderApplication.java
@@ -1,6 +1,7 @@
 package nextstep.ladder;
 
 import nextstep.ladder.domain.Custom;
+import nextstep.ladder.domain.Ladder;
 import nextstep.ladder.util.StringUtil;
 import nextstep.ladder.view.InputView;
 import nextstep.ladder.view.impl.InputViewImpl;
@@ -33,5 +34,9 @@ public class LadderApplication {
         String customNames = inputView.inputCustomNames();
         // 사용자 생성
         Custom custom = Custom.of(StringUtil.split(customNames));
+        // 사다리 높이 입력받기
+        int ladderHeight = inputView.inputLadderHeight();
+        // 사다리 생성 (사용자 수, 사다리 높이)
+        Ladder ladder = new Ladder(custom.count(), ladderHeight);
     }
 }
