8603c842a838b0bb68eb1ff7fd5e90bb6dc45972
wyparks2 <wyparks2@mrblue.com>
2019-06-29 17:39:39 +0900

    feat - Persons 일급콜렉션 구현

---
+++ src/main/java/nextstep/ladder/Persons.java
@@ -0,0 +1,24 @@
+package nextstep.ladder;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Created by wyparks2@gmail.com on 2019-06-29
+ * Blog : http://wyparks2.github.io
+ * Github : http://github.com/wyparks2
+ */
+public class Persons {
+    private Set<Person> persons;
+
+    public Persons(List<String> names) {
+        this.persons = names.stream()
+                .map(Person::new)
+                .collect(Collectors.toSet());
+    }
+
+    public int size() {
+        return persons.size();
+    }
+}
---
+++ src/test/java/nextstep/ladder/PersonsTest.java
@@ -0,0 +1,30 @@
+package nextstep.ladder;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by wyparks2@gmail.com on 2019-06-29
+ * Blog : http://wyparks2.github.io
+ * Github : http://github.com/wyparks2
+ */
+class PersonsTest {
+
+    @Test
+    void create() {
+        List<String> personNames = Arrays.asList("pobi", "honux", "crong", "jk");
+        new Persons(personNames);
+    }
+
+    @Test
+    void 동일이름을_입력한경우_중복제거() {
+        List<String> personNames = Arrays.asList("pobi", "honux", "crong", "jk", "jk");
+        Persons persons = new Persons(personNames);
+
+        assertThat(persons.size()).isEqualTo(personNames.size() - 1);
+    }
+}
