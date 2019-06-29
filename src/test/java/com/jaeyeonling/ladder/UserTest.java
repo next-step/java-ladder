18d21da7f408b463b1ebcf9704342d62fed597a7
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-29 09:32:47 +0900

    [test] 유저 이름에 길이가 ${Username.MAX_LENGTH} 를 넘는 값이 들어가면 실패한다. 테스트 추가

--- src/test/java/com/jaeyeonling/ladder/UserTest.java
+++ src/test/java/com/jaeyeonling/ladder/UserTest.java
@@ -1,7 +1,11 @@
 package com.jaeyeonling.ladder;
 
+import com.jaeyeonling.ladder.exception.LongerThanMaxLengthUsernameException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,5 +22,20 @@ class UserTest {
 
         // then
         assertThat(user).isNotNull();
+    }
+
+    @DisplayName("유저 이름에 길이가 " + Username.MAX_LENGTH + " 를 넘는 값이 들어가면 실패한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "김재연123",
+            "mattsdasda",
+            "kjysgadasadgads",
+            "gggdsa 아ㅏㅏㅏㅏㅏ sadsas "
+    })
+    void Create_LongerThanMaxLengthUsername_ThrowException(final String longerThanMaxLengthUsername) {
+        Assertions.assertThatExceptionOfType(LongerThanMaxLengthUsernameException.class)
+                .isThrownBy(() -> {
+                    User.of(longerThanMaxLengthUsername);
+                });
     }
 }
