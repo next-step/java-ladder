4daf555b9d604cc441702e7ec4f00184439e2197
Iksoo Shin <pd6016@naver.com>
2019-06-29 09:28:24 +0900

    [Step1] - 자바8 스트림, 람다, Optional (#155)
    * docs(README) : 기능목록 정의
    
    * feat(StreamStudy) : sumOverThreeAndDouble 메소드 정의
    
    * feat(StreamStudy) : printLongestWordTop100 메소드 기능 구현
    
    * fix(StreamStudy) : 불필요한 주석 제거
    
    * feat(User) : ageIsInRange2 메소드 Optional기능 적용
    
    * feat(Users) : getUser 메소드 Optional.ofNullable 적용
    
    * feat(Expression) : of메소드 Optional.orElseThrow() 적용
    
    * feat(Lambda,Conditional) : 람다실습 부분 구현내역 추가

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,13 +22,21 @@ public class StreamStudy {
 		}
 		return count;
 	}
-	
+
 	public static void printLongestWordTop100() throws IOException {
 		String contents = new String(Files.readAllBytes(Paths
 				.get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
 		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 		
-		// TODO 이 부분에 구현한다.
+		List<String> wordTop100 = words.stream()
+				.filter(word -> word.length() > 12)
+				.sorted(Comparator.comparing(String::length).reversed())
+				.distinct()
+				.map(word -> word.toLowerCase())
+				.limit(100)
+				.collect(Collectors.toList());
+
+		System.out.println(wordTop100);
 	}
 
 	public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +48,9 @@ public class StreamStudy {
 	}
 
 	public static long sumOverThreeAndDouble(List<Integer> numbers) {
-		return 0;
+		return numbers.stream()
+				.filter(x -> x > 3)
+				.map(x -> x * 2)
+				.reduce(0, (x, y) -> x + y);
 	}
 }
