192236c2298cc5d8169ea5f1ccca6aa990591182
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-29 10:07:25 +0900

    [feat] 유저들에 여러명을 생성에 성공한다. 테스트 통과

--- src/main/java/com/jaeyeonling/ladder/Users.java
+++ src/main/java/com/jaeyeonling/ladder/Users.java
@@ -1,6 +1,9 @@
 package com.jaeyeonling.ladder;
 
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Users {
 
@@ -12,8 +15,12 @@ public class Users {
         this.users = users;
     }
 
-    public static Users of(String rawUsers) {
-        return new Users(List.of(User.of(rawUsers)));
+    public static Users of(final String rawUsers) {
+        final List<User> users = Arrays.stream(rawUsers.split(SEPARATOR))
+                .map(User::of)
+                .collect(Collectors.toList());
+
+        return new Users(users);
     }
 
     int size() {
