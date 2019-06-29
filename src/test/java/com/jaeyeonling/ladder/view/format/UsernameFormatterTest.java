4c3e947e148650ef1de186db57d3ea3f25c30978
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-29 09:48:51 +0900

    [test] 포맷팅 시 길이는 ${Username.MAX_LENGTH} 이어야 한다. 테스트 추가

--- src/test/java/com/jaeyeonling/ladder/view/format/UsernameFormatterTest.java
+++ src/test/java/com/jaeyeonling/ladder/view/format/UsernameFormatterTest.java
@@ -3,6 +3,8 @@ package com.jaeyeonling.ladder.view.format;
 import com.jaeyeonling.ladder.Username;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,5 +18,25 @@ class UsernameFormatterTest {
 
         // then
         assertThat(usernameFormatter).isNotNull();
+    }
+
+    @DisplayName("포맷팅 시 길이는 " + Username.MAX_LENGTH + " 이어야 한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "김재연",
+            "matt",
+            "kjy",
+            "ggg"
+    })
+    void Format_Username_LengthShouldSame(final String rawUsername) {
+        // given
+        final Username username = Username.of(rawUsername);
+        final Formatter<Username> usernameFormatter = new UsernameFormatter();
+
+        // when
+        final String formattedUsername = usernameFormatter.format(username);
+
+        // then
+        assertThat(formattedUsername).hasSize(Username.MAX_LENGTH);
     }
 }
