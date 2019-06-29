8e622b919082c1d1c74eb260e6ef1c057af6afac
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-29 13:40:08 +0900

    [test] 앞 포인트가 True면 다음 포인트는 무조건 False를 리턴한다. 테스트 추가

--- src/test/java/com/jaeyeonling/ladder/StrategyBaseLineGeneratorTest.java
+++ src/test/java/com/jaeyeonling/ladder/StrategyBaseLineGeneratorTest.java
@@ -24,4 +24,23 @@ class StrategyBaseLineGeneratorTest {
 
         assertThat(first).isFalse();
     }
+
+    @DisplayName("앞 포인트가 True면 다음 포인트는 무조건 False를 리턴한다.")
+    @Test
+    void Generate_IfBeforeTrue_Always() {
+        final LineGenerator lineGenerator = StrategyBaseLineGenerator.of(() -> true);
+
+        final Users users = Users.of("1,2,3,4,5,6,7");
+        final CountOfUsers countOfUsers = users.getCountOfUsers();
+
+        final Line line = lineGenerator.generate(countOfUsers);
+
+        final List<Boolean> points = line.getPoints();
+        for (int i = 2; i < points.size(); i++) {
+            final Boolean before = points.get(i - 1);
+            if (before) {
+                assertThat(points.get(i)).isFalse();
+            }
+        }
+    }
 }
