00164fd7dd774cf076f9c2e2bc5a3c46c77a5e3f
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-29 14:43:24 +0900

    [test] 포인트가 True 인 경우 포맷팅에 성공한다. 테스트 통과

--- src/main/java/com/jaeyeonling/ladder/view/format/PointFormatter.java
+++ src/main/java/com/jaeyeonling/ladder/view/format/PointFormatter.java
@@ -1,9 +1,24 @@
 package com.jaeyeonling.ladder.view.format;
 
+import com.jaeyeonling.ladder.Username;
+
 public class PointFormatter implements Formatter<Boolean> {
 
+    private static final String EMPTY = " ";
+    private static final String VERTICAL = "|";
+    private static final String HORIZONTAL = "-";
+
     @Override
-    public String format(Boolean raw) {
-        return null;
+    public String format(final Boolean point) {
+        final String replaceCharacter = pointToReplaceCharacter(point);
+        return replaceCharacter.repeat(Username.MAX_LENGTH) + VERTICAL;
+    }
+
+    private String pointToReplaceCharacter(final boolean point) {
+        if (point) {
+            return HORIZONTAL;
+        }
+
+        return EMPTY;
     }
 }
