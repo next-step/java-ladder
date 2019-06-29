20f715dc416dd10f4a22f08cd0d885a5291e066f
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-29 15:59:46 +0900

    [feat] LinesFormatter 추가

--- src/main/java/com/jaeyeonling/ladder/domain/line/Lines.java
+++ src/main/java/com/jaeyeonling/ladder/domain/line/Lines.java
@@ -1,6 +1,7 @@
 package com.jaeyeonling.ladder.domain.line;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Lines {
 
@@ -8,6 +9,10 @@ public class Lines {
 
     private Lines(final List<Line> lines) {
         this.lines = lines;
+    }
+
+    public Stream<Line> stream() {
+        return lines.stream();
     }
 
     public static Lines of(final List<Line> lines) {
