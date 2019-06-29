e5be6c45a6808beb309ec795111b356d556e7d20
권병윤 <sksggg123@gmail.com>
2019-06-30 02:46:34 +0900

    [refactor] : split 할때 공백제거 추가

--- src/main/java/nextstep/ladder/util/StringUtil.java
+++ src/main/java/nextstep/ladder/util/StringUtil.java
@@ -2,6 +2,7 @@ package nextstep.ladder.util;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * author       : gwonbyeong-yun <sksggg123>
@@ -19,9 +20,12 @@ public class StringUtil {
     private static final String FORMAT_REPLACE_SPACE_SOURCE = " ";
     private static final String FORMAT_REPLACE_SPACE_TARGET = "";
 
-    public static List<String> split(String split) {
+    public static List<String> split(String nameSplit) {
         return Arrays.asList(
-                split.replaceAll(FORMAT_REPLACE_SPACE_SOURCE, FORMAT_REPLACE_SPACE_TARGET)
-                        .split(FORMAT_SPLIT_COMMA));
+                nameSplit.replaceAll(FORMAT_REPLACE_SPACE_SOURCE, FORMAT_REPLACE_SPACE_TARGET)
+                        .split(FORMAT_SPLIT_COMMA)).stream()
+                        .filter(name -> !"".equals(name))
+                        .distinct()
+                        .collect(Collectors.toList());
     }
 }
