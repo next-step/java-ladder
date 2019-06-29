806a29cbafe049c40f2a07d51c6e3acd4b30438a
권병윤 <sksggg123@gmail.com>
2019-06-30 02:48:06 +0900

    [feat] : 사다리 객체 초기 구현

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -11,4 +11,15 @@ package nextstep.ladder.domain;
  * create date  : 2019-06-29 03:27
  */
 public class Ladder {
+    private static final int MIN_LADDER_HEIGHT = 3;
+    private final int customNumber;
+    private final int ladderHeight;
+
+    public Ladder(int customNumber, int ladderHeight) {
+        if(ladderHeight < MIN_LADDER_HEIGHT) {
+            throw new IllegalArgumentException("사다리의 높이는 3이상만 가능합니다.");
+        }
+        this.customNumber = customNumber;
+        this.ladderHeight = ladderHeight;
+    }
 }
