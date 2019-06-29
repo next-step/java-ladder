0b6c0c4c79849ddc190dbed9ffa612eaa6cfc5ee
최범균(Beomgyun Choe)/Platform Engineering팀/11ST <choebk@sk.com>
2019-06-30 01:14:08 +0900

    [feature] Players 객체 및 테스트 추가

--- src/main/java/nextstep/step2/domain/Players.java
+++ src/main/java/nextstep/step2/domain/Players.java
@@ -1,4 +1,28 @@
 package nextstep.step2.domain;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Players {
+    private final List<Player> players;
+
+    public Players(List<Player> players) {
+        this.players = players;
+    }
+
+    public static Players create(List<String> playersName) {
+        return new Players(playersName.stream()
+                                      .map(Player::new)
+                                      .collect(Collectors.toList()));
+    }
+
+    public List<Player> getPlayers() {
+        return players.stream()
+                      .map(Player::new)
+                      .collect(Collectors.toList());
+    }
+
+    public int getPlayerCount() {
+        return this.players.size();
+    }
 }
--- src/test/java/nextstep/step2/PlayersTest.java
+++ src/test/java/nextstep/step2/PlayersTest.java
@@ -1,4 +1,20 @@
 package nextstep.step2;
 
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import nextstep.step2.domain.Players;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PlayersTest {
+
+    @Test
+    @DisplayName("3명의 플레이어 생성")
+    void create_players() {
+        final var players = Players.create(List.of("choebk", "beomgyun", "nextstep"));
+        assertThat(players.getPlayerCount()).isEqualTo(3);
+    }
 }
