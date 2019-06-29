2fc233913652d9fc28a95daab90fe105c23f7380
Minseok <iamminseokpark@gmail.com>
2019-06-29 15:03:08 +0900

    feat(ladder): 이름이 최대 5자인 경우 예외 추가

--- src/main/java/ladder/LadderGame.java
+++ src/main/java/ladder/LadderGame.java
@@ -27,6 +27,18 @@ public class LadderGame {
     }
 
     private List<String> generatePlayer(String names) {
-        return Arrays.asList(names.split(DELIMITER_NAMES));
+        String[] rawNames = names.split(DELIMITER_NAMES);
+        if (isThereTooLong(rawNames)) {
+            throw new IllegalArgumentException("이름은 최대 5자까지 설정할 수 있습니다.");
+        }
+
+        return Arrays.asList(rawNames);
+    }
+
+    private boolean isThereTooLong(String[] rawNames) {
+        return Arrays.stream(rawNames)
+                .filter(name -> name.length() > 5)
+                .findFirst()
+                .isPresent();
     }
 }
--- src/test/java/ladder/LadderGameTest.java
+++ src/test/java/ladder/LadderGameTest.java
@@ -1,17 +1,27 @@
 package ladder;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class LadderGameTest {
+    static final String INPUT_USER_NAMES = "pobi,honux,crong,jk";
+    static final String INPUT_USER_NAMES_INVALID_LENGTH = "toolongname,honux,crong,jk";
+    static final int INPUT_LADDER_HEIGHT = 1;
+
+    private LadderGame ladder;
+
+    @BeforeEach
+    void setUp() {
+        ladder = new LadderGame(INPUT_USER_NAMES, INPUT_LADDER_HEIGHT);
+    }
+
     @Test
     void generate() {
-        String names = "pobi,honux,crong,jk";
-        int height = 1;
-        LadderGame ladder = new LadderGame(names, height);
         LineStrategy strategy = new LineDefaultStrategy.LineEvenStrategy();
 
         Line line = new Line(4, new LineDefaultStrategy.LineEvenStrategy());
@@ -19,5 +29,12 @@ public class LadderGameTest {
         ladderForm.add(line);
 
         assertThat(ladder.generate(strategy)).isEqualTo(ladderForm);
+    }
+
+    @Test
+    void too_long_user_name() {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            new LadderGame(INPUT_USER_NAMES_INVALID_LENGTH, INPUT_LADDER_HEIGHT);
+        });
     }
 }
