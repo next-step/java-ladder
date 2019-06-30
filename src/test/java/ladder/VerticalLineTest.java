f99d4831c67f7f2d48b8f333eb9751dd555ab288
SangHyoun Yun <hainco@gmail.com>
2019-07-01 01:25:23 +0900

    test(ladder): VerticalLineTest 에서 HorizontalLine 을 갖는 테스트 추가

--- src/test/java/ladder/VerticalLineTest.java
+++ src/test/java/ladder/VerticalLineTest.java
@@ -1,16 +1,29 @@
 package ladder;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class VerticalLineTest {
+    int lineLength = 5;
+    VerticalLine verticalLine;
+
+    @BeforeEach
+    void setUp() {
+        verticalLine = new VerticalLine(lineLength);
+    }
+
     @Test
     @DisplayName("주어진 길이로 VerticalLine 인스턴스 생성")
     void constructor() {
-        int lineLength = 5;
-        VerticalLine verticalLine = new VerticalLine(lineLength);
         assertThat(verticalLine.getLength()).isEqualTo(lineLength);
+    }
+
+    @Test
+    @DisplayName("최초 생성된 VerticalLine 은 HorizontalLine 이 없다")
+    void hasDefaultHorizontalLine() {
+        assertThat(verticalLine.hasHorizontalLine()).isFalse();
     }
 }
