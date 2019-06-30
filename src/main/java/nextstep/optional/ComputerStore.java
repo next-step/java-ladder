eff4ecb11828c304d7b90601b1f176c1b885f548
choihyunjin <iamchoihyunjin@gmail.com>
2019-06-30 22:10:16 +0900

    feat(ComputerStore): add code getVersionOptional

--- src/main/java/nextstep/optional/ComputerStore.java
+++ src/main/java/nextstep/optional/ComputerStore.java
@@ -3,24 +3,29 @@ package nextstep.optional;
 import nextstep.optional.Computer.Soundcard;
 import nextstep.optional.Computer.USB;
 
+import java.util.Optional;
+
 public class ComputerStore {
-	public static final String UNKNOWN_VERSION = "UNKNOWN";
-	
-	public static String getVersion(Computer computer) {
-		String version = UNKNOWN_VERSION;
-		if(computer != null){
-		  Soundcard soundcard = computer.getSoundcard();
-		  if(soundcard != null){
-		    USB usb = soundcard.getUsb();
-		    if(usb != null){
-		      version = usb.getVersion();
-		    }
-		  }
-		}
-		return version;
-	}
-	
-	public static String getVersionOptional(Computer computer) {
-		return null;
-	}
+    public static final String UNKNOWN_VERSION = "UNKNOWN";
+
+    public static String getVersion(Computer computer) {
+        String version = UNKNOWN_VERSION;
+        if (computer != null) {
+            Soundcard soundcard = computer.getSoundcard();
+            if (soundcard != null) {
+                USB usb = soundcard.getUsb();
+                if (usb != null) {
+                    version = usb.getVersion();
+                }
+            }
+        }
+        return version;
+    }
+
+    public static String getVersionOptional(Computer computer) {
+        return Optional.ofNullable(computer)
+                .filter(com -> com.getSoundcard() != null && com.getSoundcard().getUsb() != null)
+                .map(e -> e.getSoundcard().getUsb().getVersion())
+                .orElse(UNKNOWN_VERSION);
+    }
 }
