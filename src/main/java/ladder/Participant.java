6995af9ae906ef46f2ae5bf2ca6220baa0c1326a
SangHyoun Yun <hainco@gmail.com>
2019-06-30 22:29:32 +0900

    refactor(ladder): Participant 객체에 equals 및 hashCode 추가

--- src/main/java/ladder/Participant.java
+++ src/main/java/ladder/Participant.java
@@ -1,5 +1,7 @@
 package ladder;
 
+import java.util.Objects;
+
 public class Participant {
     private final Name name;
 
@@ -9,5 +11,18 @@ public class Participant {
 
     public String getName() {
         return name.getName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Participant that = (Participant) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
