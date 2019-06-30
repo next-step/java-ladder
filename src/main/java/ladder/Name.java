a2a39ffe278959a4f7cb337cd5acdb5b99df196e
SangHyoun Yun <hainco@gmail.com>
2019-06-30 22:34:29 +0900

    refactor(ladder): Name 및 Names 에 equals, hashCode 추가

--- src/main/java/ladder/Name.java
+++ src/main/java/ladder/Name.java
@@ -1,5 +1,7 @@
 package ladder;
 
+import java.util.Objects;
+
 public class Name {
     public static final int MAX_NAME_LENGTH = 5;
     private final String name;
@@ -13,5 +15,18 @@ public class Name {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
--- src/main/java/ladder/Names.java
+++ src/main/java/ladder/Names.java
@@ -2,6 +2,7 @@ package ladder;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Names {
@@ -15,5 +16,18 @@ public class Names {
 
     public List<Name> getNames() {
         return Collections.unmodifiableList(names);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Names names1 = (Names) o;
+        return Objects.equals(names, names1.names);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(names);
     }
 }
