5ff9b0e2ddb643d7bc1e9d2a80a5dc2ce0715dfe
SangHyoun Yun <hainco@gmail.com>
2019-06-30 22:22:31 +0900

    feat(ladder): Names 일급 컬렉션 생성

---
+++ src/main/java/ladder/Names.java
@@ -0,0 +1,19 @@
+package ladder;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Names {
+    List<Name> names;
+
+    public Names(List<String> nameStrings) {
+        this.names = nameStrings.stream()
+                .map(Name::new)
+                .collect(Collectors.toList());
+    }
+
+    public List<Name> getNames() {
+        return Collections.unmodifiableList(names);
+    }
+}
