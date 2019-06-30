ff102b514c3821e6b83469047706ef1ad8f8b10f
문재헌 <jaeheon209@gmail.com>
2019-06-30 22:42:26 +0900

    test(step2) : Ladder Line Generator - Green Test

--- src/main/java/ladder/domain/LadderGenerator.java
+++ src/main/java/ladder/domain/LadderGenerator.java
@@ -1,7 +1,17 @@
 package ladder.domain;
 
-public class LadderGenerator {
-    public static Line lineGenerator(int input) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+public class LadderGenerator {
+    static Random random = new Random();
+
+    public static Line lineGenerator(int input) {
+        List<Boolean> points = new ArrayList<>();
+        for (int i = 1; i < input; i++) {
+            points.add(random.nextBoolean());
+        }
+        return new Line(points);
     }
 }
