ddeb22b2bbbda7aef1e29b4f5c64762d1edadf6c
SangHyoun Yun <hainco@gmail.com>
2019-07-01 00:41:37 +0900

    refactor(ladder): Lines -> VerticalLines 으로 변경

--- src/main/java/ladder/VerticalLines.java
+++ src/main/java/ladder/VerticalLines.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Lines {
+public class VerticalLines {
     private final List<VerticalLine> verticalLines;
 
-    public Lines(int lineQuantity, int lineLength) {
+    public VerticalLines(int lineQuantity, int lineLength) {
         this.verticalLines = new ArrayList<>();
         for (int i = 0; i < lineQuantity; i++) {
             verticalLines.add(new VerticalLine(lineLength));
--- src/test/java/ladder/VerticalLinesTest.java
+++ src/test/java/ladder/VerticalLinesTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class LinesTest {
+public class VerticalLinesTest {
     @Test
     @DisplayName("참여자 수와, 사다리 갯수만큼 사다리가 생성된다.")
     void constructor() {
@@ -13,7 +13,7 @@ public class LinesTest {
         Names names = Names.of("1,2");
         Participants participants = Participants.of(names);
 
-        Lines lines = new Lines(participants.size(), lineLength);
-        assertThat(lines.size()).isEqualTo(participants.size());
+        VerticalLines verticalLines = new VerticalLines(participants.size(), lineLength);
+        assertThat(verticalLines.size()).isEqualTo(participants.size());
     }
 }
