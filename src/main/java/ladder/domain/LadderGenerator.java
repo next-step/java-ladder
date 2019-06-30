b3c2ff633d930b6c67cafcd7d265c2bbc395ca64
문재헌 <jaeheon209@gmail.com>
2019-06-30 22:39:39 +0900

    test(step2) : Ladder Line Generator - Red Test(Compile Pass)

---
+++ src/main/java/ladder/domain/LadderGenerator.java
@@ -0,0 +1,7 @@
+package ladder.domain;
+
+public class LadderGenerator {
+    public static Line lineGenerator(int input) {
+
+    }
+}
---
+++ src/main/java/ladder/domain/Line.java
@@ -0,0 +1,17 @@
+package ladder.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Line {
+    private List<Boolean> points = new ArrayList<>();
+
+    public Line(List<Boolean> points) {
+        this.points = points;
+    }
+
+    public int size() {
+        return points.size();
+    }
+
+}
