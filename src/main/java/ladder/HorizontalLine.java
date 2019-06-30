e66927b612c2d4f271efe2337cbefc600ff322c2
SangHyoun Yun <hainco@gmail.com>
2019-07-01 01:11:31 +0900

    feat(ladder): HorizontalLine 생성

---
+++ src/main/java/ladder/HorizontalLine.java
@@ -0,0 +1,13 @@
+package ladder;
+
+public class HorizontalLine {
+    private boolean hasLine;
+
+    public HorizontalLine() {
+        this.hasLine = false;
+    }
+
+    public boolean hasLine() {
+        return hasLine;
+    }
+}
--- src/test/java/ladder/HorizontalLineTest.java
+++ src/test/java/ladder/HorizontalLineTest.java
@@ -3,6 +3,8 @@ package ladder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class HorizontalLineTest {
     @Test
     @DisplayName("최초 생성시에는 hasLine 이 false 이다")
