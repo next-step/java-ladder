d79c8cd771af3a243ea9ae6943338a93d67a3365
daioooor36 <pd6016@naver.com>
2019-06-30 14:46:30 +0900

    test(Ladder) : 실행 결과 출력문 총문자열 길이에 대한 테스트코드 작성

---
+++ src/test/java/nextstep/ladder/LadderTest.java
@@ -0,0 +1,28 @@
+package nextstep.ladder;
+
+import nextstep.ladder.model.Ladder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LadderTest {
+    private final int COUNT_OF_HEIGHT = 2;
+    private final int COUNT_OF_PERSON = 3;
+    private final int LENGTH_OF_POINT = 5;
+    private final int LENGTH_OF_PIPELINE = 1;
+
+    private Ladder ladder;
+
+    @BeforeEach
+    void setUp() {
+        ladder = new Ladder(COUNT_OF_HEIGHT, COUNT_OF_PERSON);
+    }
+
+    @DisplayName("실행결과 Line의 문자열 총 길이 체크")
+    @Test
+    void getFormattedLineTest() {
+        assertThat(ladder.getFormattedLine().length()).isEqualTo((COUNT_OF_PERSON * (LENGTH_OF_POINT + LENGTH_OF_PIPELINE) + LENGTH_OF_PIPELINE) * COUNT_OF_HEIGHT);
+    }
+}
--- src/test/java/nextstep/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -4,13 +4,24 @@ import nextstep.ladder.model.Line;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineTest {
+    private final int COUNT_OF_PERSON = 3;
+
     @DisplayName("(사람 수 - 1) 만큼의 사다리 Line 생성")
     @Test
-    void Line_만들기 () {
-        Line line = new Line(3);
-        assertThat(line.getPoints().size()).isEqualTo(2);
+    void Line_만들기() {
+        Line line = new Line(COUNT_OF_PERSON);
+        assertThat(line.getPoints().size()).isEqualTo(COUNT_OF_PERSON - 1);
+    }
+
+    @DisplayName("List<Boolean> 으로 Line 생성")
+    @Test
+    void Line_만들기2() {
+        Line line = new Line(Arrays.asList(true, false, true, true));
+        assertThat(line.getPoints().size()).isEqualTo(4);
     }
 }
