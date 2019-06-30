91494c23a38534b6cd48987ea054442b3d613805
SangHyoun Yun <hainco@gmail.com>
2019-07-01 01:26:18 +0900

    feat(ladder): VerticalLine 에 HorizontalLine 을 추가

--- src/main/java/ladder/VerticalLine.java
+++ src/main/java/ladder/VerticalLine.java
@@ -2,12 +2,18 @@ package ladder;
 
 public class VerticalLine {
     private final int length;
+    private HorizontalLine horizontalLine;
 
     public VerticalLine(int length) {
         this.length = length;
+        this.horizontalLine = HorizontalLine.LINE_FALSE;
     }
 
     public int getLength() {
         return length;
+    }
+
+    public boolean hasHorizontalLine() {
+        return horizontalLine.hasLine();
     }
 }
