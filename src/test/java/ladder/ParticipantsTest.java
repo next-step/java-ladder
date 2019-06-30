44494e9efe14a3631da5bc1c1ea566a8afe42ab1
SangHyoun Yun <hainco@gmail.com>
2019-06-30 22:29:07 +0900

    test(ladder): ParticipantsTest 에서 Names 를 사용하도록 변경

--- src/test/java/ladder/ParticipantsTest.java
+++ src/test/java/ladder/ParticipantsTest.java
@@ -3,9 +3,9 @@ package ladder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,12 +13,12 @@ public class ParticipantsTest {
     @Test
     @DisplayName("이름 리스트로 인스턴스를 생설 할 수 있다")
     void ofList() {
-        List<String> nameList = Arrays.asList("1,2");
-        List<Participant> participantList = new ArrayList<>();
-        participantList.add(new Participant(new Name("1")));
-        participantList.add(new Participant(new Name("2")));
+        Names names = new Names(Arrays.asList("1,2"));
+        List<Participant> participantList = names.getNames().stream()
+                .map(Participant::new)
+                .collect(Collectors.toList());
 
-        Participants participants = Participants.of(nameList);
-        assertThat(participants).isEqualTo(participantList);
+        Participants participants = Participants.of(names);
+        assertThat(participants.getParticipants()).isEqualTo(participantList);
     }
 }
