6e161386eb40c5f7022f9a6ccc51ee7a38455a69
SangHyoun Yun <hainco@gmail.com>
2019-06-30 22:30:34 +0900

    feat(ladder): Participants 일급 컬렉션 추가

---
+++ src/main/java/ladder/Participants.java
@@ -0,0 +1,38 @@
+package ladder;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Participants {
+    private final List<Participant> participants;
+
+    private Participants(List<Participant> participants) {
+        this.participants = participants;
+    }
+
+    public static Participants of(Names names) {
+        List<Participant> participantList = names.getNames().stream()
+                .map(Participant::new)
+                .collect(Collectors.toList());
+        return new Participants(participantList);
+    }
+
+    public List<Participant> getParticipants() {
+        return Collections.unmodifiableList(participants);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Participants that = (Participants) o;
+        return Objects.equals(participants, that.participants);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(participants);
+    }
+}
