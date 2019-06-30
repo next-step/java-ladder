2175f926eb7bfd0a65d53c9ecac4eb3a376bb5d2
SangHyoun Yun <hainco@gmail.com>
2019-07-01 01:17:37 +0900

    test(ladder): HorizontalLineTest 에서 static 인스턴스를 사용

--- src/test/java/ladder/HorizontalLineTest.java
+++ src/test/java/ladder/HorizontalLineTest.java
@@ -3,13 +3,11 @@ package ladder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 class HorizontalLineTest {
     @Test
-    @DisplayName("최초 생성시에는 hasLine 이 false 이다")
+    @DisplayName("LINE_TRUE 와 LINE_FALSE 는 각각 맞는 값을 갖고 있다.")
     void constructor() {
-        HorizontalLine horizontalLine = new HorizontalLine();
-        assertThat(horizontalLine.hasLine()).isFalse();
+        assertThat(HorizontalLine.LINE_TRUE.hasLine()).isTrue();
+        assertThat(HorizontalLine.LINE_FALSE.hasLine()).isFalse();
     }
 }
