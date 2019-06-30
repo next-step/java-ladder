03e1997be521ae55b7720e8c5bcdc3b26496cab6
SangHyoun Yun <hainco@gmail.com>
2019-06-30 22:55:30 +0900

    feat(ladder): Line 추가

---
+++ src/main/java/ladder/Line.java
@@ -0,0 +1,13 @@
+package ladder;
+
+public class Line {
+    private final int maxLine;
+
+    public Line(int maxLine) {
+        this.maxLine = maxLine;
+    }
+
+    public int getMaxLine() {
+        return maxLine;
+    }
+}
