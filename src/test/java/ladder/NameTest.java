dfa573ec75c876e823c6dd88ddb3a6b53bf68576
SangHyoun Yun <hainco@gmail.com>
2019-06-30 18:17:47 +0900

    test(ladder): NameTest 추가

---
+++ src/test/java/ladder/NameTest.java
@@ -0,0 +1,23 @@
+package ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class NameTest {
+    @Test
+    @DisplayName("5글짜 이하 이름 생성")
+    void constructor() {
+        String nameText = "pobi";
+        Name name = new Name(nameText);
+        assertThat(name.getName()).isEqualTo(nameText);
+    }
+
+    @Test
+    @DisplayName("5글짜 이상 이름은 에러")
+    void constructor_error() {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new Name("pobicrong"));
+    }
+}
