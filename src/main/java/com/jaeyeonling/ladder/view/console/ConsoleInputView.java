ae2dd8a939b2a0881cddf0b59f8cee9e3dfbc61f
Jaeyeonling <jaeyeonling@gmail.com>
2019-07-01 22:25:56 +0900

    [fix] 메시지 수정

--- src/main/java/com/jaeyeonling/ladder/view/console/ConsoleInputView.java
+++ src/main/java/com/jaeyeonling/ladder/view/console/ConsoleInputView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public final class ConsoleInputView {
 
     private static final String READ_USERS_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
-    private static final String READ_LADDER_RESULTS_MESSAGE = "결과를 보고 싶은 사람은?";
+    private static final String READ_LADDER_RESULTS_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String READ_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
 
     private static final Scanner CONSOLE = new Scanner(System.in);
