e8ee7f39747503f2ebe82ff529fc0472618c4b96
Jaeyeonling <jaeyeonling@gmail.com>
2019-07-01 22:13:06 +0900

    [test] 테스트코드 추가

--- src/test/java/com/jaeyeonling/ladder/domain/line/StrategyBaseLineGeneratorTest.java
+++ src/test/java/com/jaeyeonling/ladder/domain/line/StrategyBaseLineGeneratorTest.java
@@ -1,5 +1,6 @@
 package com.jaeyeonling.ladder.domain.line;
 
+import com.jaeyeonling.ladder.domain.point.Direction;
 import com.jaeyeonling.ladder.domain.user.CountOfUsers;
 import com.jaeyeonling.ladder.domain.user.Users;
 import org.junit.jupiter.api.DisplayName;
@@ -11,54 +12,53 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StrategyBaseLineGeneratorTest {
-//
-//    @DisplayName("첫 포인트는 무조건 False를 리턴한다.")
-//    @ParameterizedTest
-//    @ValueSource(strings = {
-//            "김재연,matt,kjy,ggg,다섯글자다",
-//            "김,kjy,글자다"
-//    })
-//    void should_return_false_when_if_firstPoint() {
-//        final LineGenerator lineGenerator = StrategyBaseLineGenerator.withStrategy(() -> true);
-//
-//        final Users users = Users.ofSeparator("1ddd,g23,g3223");
-//        final CountOfUsers countOfUsers = users.getCountOfUsers();
-//
-//        final Line line = lineGenerator.generate(countOfUsers);
-//
-//        final List<Boolean> points = line.getPoints();
-//        final boolean first = points.get(0);
-//
-//        assertThat(first).isFalse();
-//    }
-//
-//    @DisplayName("앞 포인트가 True면 다음 포인트는 무조건 False를 리턴한다.")
-//    @ParameterizedTest
-//    @ValueSource(strings = {
-//            "김재연,matt,kjy,ggg,다섯글자다",
-//            "김,kjy,글자다"
-//    })
-//    void should_return_false_when_if_before_point_true_by_trueStrategy(final String rawUsers) {
-//        should_return_false_when_if_before_point_true(rawUsers, () -> true);
-//        should_return_false_when_if_before_point_true(rawUsers, () -> false);
-//    }
-//
-//    private void should_return_false_when_if_before_point_true(final String rawUsers,
-//                                                               final PointGenerateStrategy pointGenerateStrategy) {
-//        final LineGenerator lineGenerator = StrategyBaseLineGenerator.withStrategy(pointGenerateStrategy);
-//
-//        final Users users = Users.ofSeparator(rawUsers);
-//        final CountOfUsers countOfUsers = users.getCountOfUsers();
-//
-//        final Line line = lineGenerator.generate(countOfUsers);
-//
-//        final List<Boolean> points = line.getPoints();
-//        for (int i = 2; i < points.size(); i++) {
-//            final Boolean before = points.get(i - 1);
-//            if (before) {
-//                assertThat(points.get(i)).isFalse();
-//            }
-//        }
-//    }
-//
+
+    @DisplayName("첫 포인트에 LEFT가 올 수 없다.")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "김재연,matt,kjy,ggg,다섯글자다",
+            "김,kjy,글자다"
+    })
+    void should_return_not_left_if_first_direction() {
+        final LineGenerator lineGenerator = StrategyBaseLineGenerator.withStrategy(() -> true);
+
+        final Users users = Users.ofSeparator("1ddd,g23,g3223");
+        final CountOfUsers countOfUsers = users.getCountOfUsers();
+
+        final Line line = lineGenerator.generate(countOfUsers);
+        final List<Direction> directions = line.getDirections();
+
+        final Direction first = directions.get(0);
+
+        assertThat(first).isNotEqualTo(Direction.LEFT);
+    }
+
+    @DisplayName("앞이 RIGHT면 다음은 무조건 LEFT를 리턴한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "김재연,matt,kjy,ggg,다섯글자다",
+            "김,kjy,글자다"
+    })
+    void should_return_left_when_if_before_point_right_by(final String rawUsers) {
+        should_return_left_when_if_before_point_right_by(rawUsers, () -> true);
+        should_return_left_when_if_before_point_right_by(rawUsers, () -> false);
+    }
+
+    private void should_return_left_when_if_before_point_right_by(final String rawUsers,
+                                                                  final PointGenerateStrategy pointGenerateStrategy) {
+        final LineGenerator lineGenerator = StrategyBaseLineGenerator.withStrategy(pointGenerateStrategy);
+
+        final Users users = Users.ofSeparator(rawUsers);
+        final CountOfUsers countOfUsers = users.getCountOfUsers();
+
+        final Line line = lineGenerator.generate(countOfUsers);
+
+        final List<Direction> directions = line.getDirections();
+        for (int i = 2; i < directions.size(); i++) {
+            final Direction before = directions.get(i - 1);
+            if (before == Direction.RIGHT) {
+                assertThat(directions.get(i)).isEqualTo(Direction.LEFT);
+            }
+        }
+    }
 }
