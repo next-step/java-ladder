2348e15c34353a1df07b47d076ca58e7ea679c72
byrage <byrage91@gmail.com>
2019-07-01 11:36:43 +0900

    [step2] feat: 참여자명 유효성 체크

--- src/main/java/ladder/domain/Participant.java
+++ src/main/java/ladder/domain/Participant.java
@@ -2,15 +2,29 @@ package ladder.domain;
 
 public class Participant {
 
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 5;
     private String name;
 
     public Participant(String name) {
 
+        validateName(name);
         this.name = name;
     }
 
     public String getName() {
 
         return name;
+    }
+
+    private void validateName(String name) {
+
+        if (name == null) {
+            throw new IllegalArgumentException("참여자명이 null 입니다.");
+        }
+
+        if (name.length() < MIN_LENGTH || name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(String.format("참여자명 길이는 %d~%d자 여야 합니다.", MIN_LENGTH, MAX_LENGTH));
+        }
     }
 }
