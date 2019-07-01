b8067915e8b114a47e94c65f4f8c097c557efcaf
HyeonAh Lee <bono.yi@gmail.com>
2019-07-02 00:02:36 +0900

    feature: Input 구현

---
+++ src/main/java/nextstep/ladder/LadderMain.java
@@ -0,0 +1,10 @@
+package nextstep.ladder;
+
+public class LadderMain {
+    public static void main(String[] args) {
+        String s = Input.getUsers();
+        int x = Input.getMaxHeight();
+
+
+    }
+}
