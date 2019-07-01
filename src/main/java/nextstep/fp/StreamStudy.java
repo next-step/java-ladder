bb9a206e62b2ae60e68f27790de50537c9fab659
rabbitfoot <wowyowowyo@gmail.com>
2019-07-01 17:26:34 +0900

    [step1/feat] StreamStudy commit

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,20 +5,25 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
 
 	public static long countWords() throws IOException {
-		String contents = new String(Files.readAllBytes(Paths
-				.get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+		String contents = new String(
+				Files.readAllBytes(Paths.get("src/main/resources/fp/war-and-peace.txt")),
+				StandardCharsets.UTF_8);
 		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
 		long count = 0;
+		/*
 		for (String w : words) {
 		  if (w.length() > 12) count++;  
 		}
+		*/
+		count = words.stream().filter(word -> word.length() > 12).count();
 		return count;
 	}
 	
@@ -26,8 +31,11 @@ public class StreamStudy {
 		String contents = new String(Files.readAllBytes(Paths
 				.get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
 		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
-		
-		// TODO 이 부분에 구현한다.
+		words.stream()
+				.filter(word -> word.length() > 12)
+				.distinct()
+				.sorted(Comparator.comparing(String::length))
+				.forEach(w -> System.out.println(w.toLowerCase()));
 	}
 
 	public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +47,6 @@ public class StreamStudy {
 	}
 
 	public static long sumOverThreeAndDouble(List<Integer> numbers) {
-		return 0;
+		return sumAll(doubleNumbers(numbers.stream().filter(number -> number > 3).collect(Collectors.toList())));
 	}
 }
