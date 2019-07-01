527817e4c2bb39aaaeb7c169cf3d8d49563abb5e
Juyoung <juy5790@outlook.com>
2019-07-01 16:34:26 +0900

    docs #step3 요구사항 정리 및 피드백 적용

--- src/main/java/com/ladder/model/Line.java
+++ src/main/java/com/ladder/model/Line.java
@@ -12,15 +12,40 @@ public class Line {
         this.points = new ArrayList<>(point);
     }
 
-    public static Line of(int numberOfPoints) {
-        PointsGenerator pointsGenerator = new PointsGenerator();
-        return new Line(pointsGenerator.generate(numberOfPoints));
+    public static Line of(int countByPlayers) {
+        PointStrategy pointStrategy = new PointRandomStrategy();
+        List<Boolean> points = ofPoints(countByPlayers, pointStrategy);
+        return new Line(points);
     }
 
-    public static Line of(int numberOfPoints, PointStrategy pointStrategy) {
-        PointsGenerator pointsGenerator = new PointsGenerator(pointStrategy);
-        return new Line(pointsGenerator.generate(numberOfPoints));
+    private static List<Boolean> ofPoints(int countByPlayers, PointStrategy pointStrategy) {
+        List<Boolean> points = new ArrayList<>();
+        points.add(pointStrategy.generate());
+        for (int i = 1; i < countByPlayers - 1; i++) {
+            Boolean beforePoint = points.get(i - 1);
+            generateNextPoint(points, beforePoint, pointStrategy);
+        }
+        generateEndPoint(points);
+        return points;
     }
+
+    public static Line of(int countByPlayers, PointStrategy pointStrategy) {
+        List<Boolean> points = ofPoints(countByPlayers, pointStrategy);
+        return new Line(points);
+    }
+
+    private static void generateNextPoint(List<Boolean> points, boolean before, PointStrategy pointStrategy) {
+        if (before) {
+            points.add(false);
+            return;
+        }
+        points.add(pointStrategy.generate());
+    }
+
+    private static void generateEndPoint(List<Boolean> points) {
+        points.add(false);
+    }
+
 
     public List<Boolean> getPoints() {
         return Collections.unmodifiableList(points);
--- src/test/java/com/ladder/model/LineTest.java
+++ src/test/java/com/ladder/model/LineTest.java
@@ -3,6 +3,8 @@ package com.ladder.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineTest {
@@ -35,4 +37,42 @@ public class LineTest {
         assertThat(points.getPoints()).containsExactly(true, false);
     }
 
+
+    @DisplayName("플레이어 수 만큼 위치를 생성하는데 성공한다")
+    @Test
+    void createPoint_countByPlayer_success() {
+        // given
+        int countByPlayers = 5;
+
+        // when
+        Line line = Line.of(countByPlayers);
+        List<Boolean> points = line.getPoints();
+
+        // then
+        assertThat(points).hasSize(countByPlayers);
+    }
+
+    @DisplayName("전 위치에서 이동가능 한 경우, 위치는 이동이 불가능하다.")
+    @Test
+    void whenBeforePointTrue_Then_currentPoint_isFalse() {
+        // when
+        Line line = Line.of(5);
+        List<Boolean> points = line.getPoints();
+        boolean nextPoint = points.get(1);
+
+        // then
+        assertThat(nextPoint).isFalse();
+    }
+
+    @DisplayName("마지막 위치는 항상 이동이 불가능하다")
+    @Test
+    void lastPoint_isFalse_success() {
+        // when
+        Line line = Line.of(5);
+        List<Boolean> points = line.getPoints();
+        boolean lastPoint = points.get(points.size() - 1);
+
+        // then
+        assertThat(lastPoint).isFalse();
+    }
 }
