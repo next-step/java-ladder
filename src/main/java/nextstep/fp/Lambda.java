045a42e12e2178ec71e801a4e729f715a291bce7
rabbitfoot <wowyowowyo@gmail.com>
2019-07-01 16:34:06 +0900

    [step1/feat] LambdaTest commit

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -53,4 +53,13 @@ public class Lambda {
 	    }
 	    return total;
 	}
+	public static int sumParamConditional(List<Integer> numbers, Conditional conditional) {
+		int total = 0;
+		for (int number : numbers) {
+			if (conditional.test(number)) {
+				total += number;
+			}
+		}
+		return total;
+	}
 }
--- src/test/java/nextstep/fp/LambdaTest.java
+++ src/test/java/nextstep/fp/LambdaTest.java
@@ -34,18 +34,44 @@ public class LambdaTest {
 	@Test
 	public void sumAll() throws Exception {
 		int sum = Lambda.sumAll(numbers);
+		int sum2 = Lambda.sumParamConditional(numbers, new Conditional() {
+			@Override
+			public boolean test(int number) {
+				return true;
+			}
+		});
+		int sum3 = Lambda.sumParamConditional(numbers, number -> true);
 		assertThat(sum).isEqualTo(21);
+		assertThat(sum2).isEqualTo(21);
+		assertThat(sum3).isEqualTo(21);
 	}
 	
 	@Test
 	public void sumAllEven() throws Exception {
 		int sum = Lambda.sumAllEven(numbers);
+		int sum2 = Lambda.sumParamConditional(numbers, new Conditional() {
+			@Override
+			public boolean test(int number) {
+				return number % 2 == 0;
+			}
+		});
+		int sum3 = Lambda.sumParamConditional(numbers, number -> number % 2 == 0);
 		assertThat(sum).isEqualTo(12);
+		assertThat(sum2).isEqualTo(12);
 	}
 	
 	@Test
 	public void sumAllOverThree() throws Exception {
 		int sum = Lambda.sumAllOverThree(numbers);
+		int sum2 = Lambda.sumParamConditional(numbers, new Conditional() {
+			@Override
+			public boolean test(int number) {
+				return 3 < number;
+			}
+		});
+		int sum3 = Lambda.sumParamConditional(numbers, number -> 3 < number);
 		assertThat(sum).isEqualTo(15);
+		assertThat(sum2).isEqualTo(15);
+		assertThat(sum3).isEqualTo(15);
 	}
 }
