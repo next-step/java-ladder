3095e6b2375b347afebedaa2af4a576204b80b50
Bentley <p2c2kbf@gmail.com>
2019-07-02 14:08:36 +0900

    feat: main함수 추가

---
+++ src/main/java/nextstep/controller/LadderGameMain.java
@@ -0,0 +1,15 @@
+package nextstep.controller;
+
+
+import nextstep.domain.LadderGame;
+import nextstep.view.*;
+
+public class LadderGameMain {
+    public static void main(String[] args){
+        String userNames = Inputview.inputUserName();
+        int maxHeight = Inputview.inputLadderMaxHeight();
+
+        LadderGame ladderGame = new LadderGame(userNames,maxHeight);
+
+    }
+}
