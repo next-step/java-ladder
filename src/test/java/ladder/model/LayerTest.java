caa6cb90921716fa3666f6de7db4cea1d71d3500
KIM JUNGHWAN <writer0713@naver.com>
2019-07-02 21:22:54 +0900

    #step2 [test] Layer.of 테스트 추가 (test fail)

--- src/test/java/ladder/model/LayerTest.java
+++ src/test/java/ladder/model/LayerTest.java
@@ -1,13 +1,23 @@
 package ladder.model;
 
+import ladder.util.RandomRungGenerator;
+import ladder.util.RungGenerator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class LayerTest {
 
-    private
-
     @Test
+    @DisplayName("Layer.of")
     void layerTest() {
+        RungGenerator rungGenerator = new RandomRungGenerator();
+        int players = 4;
+        Layer layer = Layer.of(rungGenerator, players);
 
+        assertThat(layer.getRungs().size()).isEqualTo(3);
     }
 }
