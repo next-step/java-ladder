eefdeb4e2d809ec00360195e69fb2ca2a11c80f6
KIM JUNGHWAN <writer0713@naver.com>
2019-07-02 21:30:28 +0900

    #step2 [feature] Ladder.of static 생성자 구현 (test success)

---
+++ src/main/java/ladder/model/Ladder.java
@@ -0,0 +1,27 @@
+package ladder.model;
+
+import ladder.util.RandomRungGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ladder {
+
+    private final List<Layer> layers;
+
+    public Ladder(final List<Layer> layers) {
+        this.layers = layers;
+    }
+
+    public static Ladder of(int countOfPlayers, int height) {
+        List<Layer> layers = new ArrayList<>();
+        for(int i = 0; i < height; i++) {
+            layers.add(Layer.of(new RandomRungGenerator(), countOfPlayers));
+        }
+        return new Ladder(layers);
+    }
+
+    public List<Layer> getLayers() {
+        return layers;
+    }
+}
