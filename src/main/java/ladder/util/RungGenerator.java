a8627d31009647d823b1983517f36882f559ae3d
KIM JUNGHWAN <writer0713@naver.com>
2019-07-02 18:05:50 +0900

    #step2 [feature] RungGenerator Rung 여러개 생성 (test success)

--- src/main/java/ladder/util/RungGenerator.java
+++ src/main/java/ladder/util/RungGenerator.java
@@ -2,8 +2,19 @@ package ladder.util;
 
 import ladder.model.Rung;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RungGenerator {
     public static Rung generate() {
         return new Rung(true);
+    }
+
+    public static List<Rung> generate(int countOfPlayers) {
+        List<Rung> rungs = new ArrayList<>();
+        for(int i = 0; i < countOfPlayers; i++) {
+            rungs.add(generate());
+        }
+        return rungs;
     }
 }
