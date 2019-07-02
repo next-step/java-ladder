0f9e1e8878a3b13d030e54c48c61129e274d9dcc
KIM JUNGHWAN <writer0713@naver.com>
2019-07-02 17:41:24 +0900

    #step2 [feature] RungTest 추가 (test success)
    isRung 메소드 추가

---
+++ src/main/java/ladder/model/Rung.java
@@ -0,0 +1,14 @@
+package ladder.model;
+
+public class Rung {
+
+    private final boolean isRung;
+
+    public Rung(boolean isRung) {
+        this.isRung = isRung;
+    }
+
+    public boolean isRung() {
+        return this.isRung;
+    }
+}
