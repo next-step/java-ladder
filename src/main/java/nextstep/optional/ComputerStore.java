5c6b203916473ba92ce50074685cbba6122cc6d3
Bentleypark <p2c2kbf@gmail.com>
2019-07-02 13:18:52 +0900

    [Step1] 자바8 <스트림, 람다, Optional> 리뷰 요청드립니다. (#179)
    * feat: 람다실습1
    
    * feat: 람다실습2
    
    * feat: 스트림 실습
    
    * feat: Optional 3 단계
    
    * feat:Option 실습 1,2
    
    * feat: lambda 실습
    
    * feat: lambda 실습
    
    * refector: lambda식 표현 함수형 단위로 개행
    
    * refector: filter 조건식 병합
    
    * feact: ComputerStor 실습

--- src/main/java/nextstep/optional/ComputerStore.java
+++ src/main/java/nextstep/optional/ComputerStore.java
@@ -3,24 +3,24 @@ package nextstep.optional;
 import nextstep.optional.Computer.Soundcard;
 import nextstep.optional.Computer.USB;
 
+import java.util.Optional;
+
 public class ComputerStore {
-	public static final String UNKNOWN_VERSION = "UNKNOWN";
-	
-	public static String getVersion(Computer computer) {
-		String version = UNKNOWN_VERSION;
-		if(computer != null){
-		  Soundcard soundcard = computer.getSoundcard();
-		  if(soundcard != null){
-		    USB usb = soundcard.getUsb();
-		    if(usb != null){
-		      version = usb.getVersion();
-		    }
-		  }
-		}
-		return version;
-	}
-	
-	public static String getVersionOptional(Computer computer) {
-		return null;
-	}
+    public static final String UNKNOWN_VERSION = "UNKNOWN";
+
+    public static String getVersion(Computer computer) {
+        Optional<Computer> opt = Optional.ofNullable(computer);
+        return opt.map(Computer::getSoundcard)
+                .map(Soundcard::getUsb)
+                .map(USB::getVersion)
+                .orElse(UNKNOWN_VERSION);
+    }
+
+    public static String getVersionOptional(Computer computer) {
+        Optional<Computer> opt = Optional.ofNullable(computer);
+        return opt.map(Computer::getSoundcard)
+                .map(Soundcard::getUsb)
+                .map(USB::getVersion)
+                .orElse(UNKNOWN_VERSION);
+    }
 }
