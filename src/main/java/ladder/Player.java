c44d5e0b8a352c8777d745ec0c97a70ebf021997
1nteger <rhep0820@gmail.com>
2019-07-02 16:50:31 +0900

    [step2] refactor : 예외처리 메서드 분리 및 상수 사용

--- src/main/java/ladder/Player.java
+++ src/main/java/ladder/Player.java
@@ -2,20 +2,27 @@ package ladder;
 
 public class Player {
 
+    static final int MAXIMUM_NAME_LENGTH = 5;
+    private static final String ALERT_EXCEED_OF_NAME_LENGTH = "이름은 최대 5글자까지 가능합니다.";
+    private static final String ALERT_EMPTY_NAME = "이름은 최소 1글자 이상 입력하세요.";
     private final String name;
 
     private Player(String name) {
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("이름은 최대 5글자까지 가능합니다.");
-        }
-        if ("".equals(name)) {
-            throw new IllegalArgumentException("이름은 최소 1글자 이상 입력하세요.");
-        }
+        validationPlayerName(name);
         this.name = name;
     }
 
     public static Player from(String name) {
         return new Player(name);
+    }
+
+    private void validationPlayerName(String name) {
+        if (name.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException(ALERT_EXCEED_OF_NAME_LENGTH);
+        }
+        if ("".equals(name)) {
+            throw new IllegalArgumentException(ALERT_EMPTY_NAME);
+        }
     }
 
     public String getName() {
