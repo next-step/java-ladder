5d744ccdca12b02d7f85b04fab72c2fbe2b88b18
KIM JUNGHWAN <writer0713@naver.com>
2019-07-02 18:24:30 +0900

    #step2 [test] getPreviousValue 테스트 (test fail)
    현재 만들려고 하는 Rung 이전의 Rung이 어떤 boolean 값을 가지고 있는지 확인

--- src/test/java/ladder/model/RungGeneratorTest.java
+++ src/test/java/ladder/model/RungGeneratorTest.java
@@ -3,6 +3,8 @@ package ladder.model;
 import ladder.util.RungGenerator;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
@@ -22,5 +24,17 @@ public class RungGeneratorTest {
         List<Rung> rungs = RungGenerator.generate(countOfPlayers);
 
         assertThat(rungs.size()).isEqualTo(4);
+    }
+
+    @Test
+    void getPreviousValue() {
+        List<Rung> emptyRungs = new ArrayList<>();
+        assertThat(RungGenerator.getPreviousValue(emptyRungs)).isFalse();
+
+        List<Rung> trueRungList = Arrays.asList(new Rung(true));
+        assertThat(RungGenerator.getPreviousValue(trueRungList)).isTrue();
+
+        List<Rung> falseRungList = Arrays.asList(new Rung(false));
+        assertThat(RungGenerator.getPreviousValue(falseRungList)).isFalse();
     }
 }
