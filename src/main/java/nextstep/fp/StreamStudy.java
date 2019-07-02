43440831dc24367bdd6af203787b2b291b14d004
sy.kim <kauksy9022@gmail.com>
2019-07-02 14:07:34 +0900

    feat(StreamStudy): stream 요구사항2 완료.

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,6 +30,13 @@ public class StreamStudy {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         // TODO 이 부분에 구현한다.
+        List<String> filterWords = words.stream().filter(w -> w.length() > 12).collect(Collectors.toList());
+        List<String> toLowerCaseWords = filterWords.stream().map(String::toLowerCase).collect(Collectors.toList());
+        List<String > distinctWords = toLowerCaseWords.stream().distinct().collect(Collectors.toList());
+        List<String> sortedWords = distinctWords.stream().sorted(Comparator.comparing(String::length)).collect(Collectors.toList());
+
+        sortedWords.stream().limit(100).forEach(System.out::println);
+
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
