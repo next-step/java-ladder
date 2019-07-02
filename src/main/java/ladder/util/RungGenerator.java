bccfd1b958c5ac08e4b83126af1b9c2b18c37255
KIM JUNGHWAN <writer0713@naver.com>
2019-07-02 18:30:52 +0900

    #step2 [feature] oppositeRule 구현 (test success)

--- src/main/java/ladder/util/RungGenerator.java
+++ src/main/java/ladder/util/RungGenerator.java
@@ -6,15 +6,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RungGenerator {
+
     public static Rung generate() {
         return new Rung(true);
     }
 
+    public static Rung generate(boolean isRung) {
+        return new Rung(isRung);
+    }
+
     public static List<Rung> generate(int countOfPlayers) {
         List<Rung> rungs = new ArrayList<>();
+
         for(int i = 0; i < countOfPlayers; i++) {
-            rungs.add(generate());
+            boolean previousValue = getPreviousValue(rungs);
+            rungs.add(generate(oppositeRule(previousValue)));
         }
         return rungs;
     }
+
+    public static boolean getPreviousValue(List<Rung> rungs) {
+        if (rungs.isEmpty()) {
+            return false;
+        }
+        return rungs.get(rungs.size() - 1).isRung();
+    }
+
+    public static boolean oppositeRule(boolean previousValue) {
+        return !previousValue;
+    }
+
+
 }
