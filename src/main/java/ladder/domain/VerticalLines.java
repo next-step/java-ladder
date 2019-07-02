64d248fbb94175ab99613f7f183469092976306b
SangHyoun Yun <hainco@gmail.com>
2019-07-03 02:16:14 +0900

    feat(ladder): HorizontalLines 로 부터 VerticalLines 생성하는 of 메서드 추가

--- src/main/java/ladder/domain/VerticalLines.java
+++ src/main/java/ladder/domain/VerticalLines.java
@@ -3,6 +3,7 @@ package ladder.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class VerticalLines {
     private final List<VerticalLine> verticalLines;
@@ -12,6 +13,19 @@ public class VerticalLines {
         for (int i = 0; i < lineQuantity; i++) {
             verticalLines.add(new VerticalLine(false));
         }
+    }
+
+    private VerticalLines(List<VerticalLine> verticalLines) {
+        this.verticalLines = verticalLines;
+    }
+
+    public static VerticalLines of(HorizontalLines horizontalLines) {
+        return new VerticalLines(
+                horizontalLines.getHorizontalLines().stream()
+                        .map(HorizontalLine::hasLine)
+                        .map(VerticalLine::new)
+                        .collect(Collectors.toList())
+        );
     }
 
     public int size() {
