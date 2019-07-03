49654939b7738b5be9ea1ad051ab184ababa7f8c
SangHyoun Yun <hainco@gmail.com>
2019-07-04 01:22:47 +0900

    refactor(ladder): Main.java 동작하도록 수정

--- src/main/java/ladder/controller/Main.java
+++ src/main/java/ladder/controller/Main.java
@@ -1,6 +1,8 @@
 package ladder.controller;
 
+import ladder.domain.Ladder;
 import ladder.domain.Names;
+import ladder.domain.RandomLineGenerator;
 import ladder.view.InputView;
 import ladder.view.ResultView;
 
@@ -17,12 +19,12 @@ public class Main {
         int ladderHeight = scanner.nextInt();
 
         Names names = Names.of(nameString);
+        RandomLineGenerator randomLineGenerator = new RandomLineGenerator(names.size());
 
-        // TODO: Ladder 생성시 사다리가 인접하지 않게 생성되도록 구현한다.
-        // Ladder ladder = new Ladder(names.size(), ladderHeight);
+        Ladder ladder = Ladder.of(randomLineGenerator, ladderHeight);
 
         System.out.println(ResultView.getDefaultResultText());
         System.out.println(ResultView.getNamesView(names));
-//        System.out.println(ResultView.getLadderView(ladder));
+        System.out.println(ResultView.getLadderView(ladder));
     }
 }
