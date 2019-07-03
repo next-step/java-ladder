3ced03ea1e4c0c8c93d60d3414eb77db2a30ee3a
daioooor36 <pd6016@naver.com>
2019-07-03 09:51:56 +0900

    fix(Direction) : of메소드 위치변경 및 Exception내용 추가

--- src/main/java/nextstep/ladder/model/Direction.java
+++ src/main/java/nextstep/ladder/model/Direction.java
@@ -1,7 +1,5 @@
 package nextstep.ladder.model;
 
-import java.util.Objects;
-
 import static java.lang.Boolean.FALSE;
 import static nextstep.ladder.model.LadderPointGenerator.generatePoint;
 
@@ -11,30 +9,10 @@ public class Direction {
 
     private Direction(boolean left, boolean right) {
         if (left && right) {
-            throw new IllegalStateException();
+            throw new IllegalStateException("양쪽이 true인 Direction은 생성 불가합니다.");
         }
-
         this.left = left;
         this.right = right;
-    }
-
-    public boolean isRight() {
-        return this.right;
-    }
-
-    public boolean isLeft() {
-        return this.left;
-    }
-
-    public Direction next(boolean nextRight) {
-        return of(this.right, nextRight);
-    }
-
-    public Direction next() {
-        if (this.right) {
-            return next(FALSE);
-        }
-        return next(generatePoint());
     }
 
     public static Direction of(boolean first, boolean second) {
@@ -45,29 +23,26 @@ public class Direction {
         return of(FALSE, right);
     }
 
+    public Direction next() {
+        if (this.right) {
+            return next(FALSE);
+        }
+        return next(generatePoint());
+    }
+
+    public Direction next(boolean nextRight) {
+        return of(this.right, nextRight);
+    }
+
     public Direction last() {
         return of(this.right, FALSE);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Direction pair = (Direction) o;
-        return left == pair.left &&
-                right == pair.right;
+    public boolean isRight() {
+        return this.right;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(left, right);
-    }
-
-    @Override
-    public String toString() {
-        return "Direction{" +
-                "left=" + left +
-                ", right=" + right +
-                '}';
+    public boolean isLeft() {
+        return this.left;
     }
 }
