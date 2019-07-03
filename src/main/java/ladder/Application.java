23729f3d0e0eea2e880e166e3021b31c99600a41
1nteger <rhep0820@gmail.com>
2019-07-04 01:14:35 +0900

    [step2] feat : 참여자 이름과 사다리 높이를 입력받는 InputView 메서드 추가

--- src/main/java/ladder/Application.java
+++ src/main/java/ladder/Application.java
@@ -1,8 +1,16 @@
 package ladder;
 
+import ladder.domain.Height;
+import ladder.domain.Ladder;
+import ladder.domain.Players;
+import ladder.view.InputView;
+
 public class Application {
 
     public static void main(String[] args) {
+        Players players = Players.of(InputView.askPlayers());
+        Height height = Height.from(InputView.askHeight());
 
+        Ladder.from(height, players);
     }
 }
