798656f439b96b1dc16d7cb9877da012801c6c59
daioooor36 <pd6016@naver.com>
2019-07-03 11:00:49 +0900

    fix(Point,Direction) : equals,HashCode 메소드추가

--- src/main/java/nextstep/ladder/model/Point.java
+++ src/main/java/nextstep/ladder/model/Point.java
@@ -40,4 +40,32 @@ public class Point {
     public boolean showLeft() {
         return direction.isLeft();
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((direction == null) ? 0 : direction.hashCode());
+		result = prime * result + index;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		if (direction == null) {
+			if (other.direction != null)
+				return false;
+		} else if (!direction.equals(other.direction))
+			return false;
+		if (index != other.index)
+			return false;
+		return true;
+	}    
 }
