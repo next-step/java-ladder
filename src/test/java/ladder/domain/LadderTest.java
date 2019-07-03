5ba0d7d7773f063bc560a86fa5cb0cbf365c964a
seungkil choi <seung-kil.choi@raizegls.com>
2019-07-04 01:37:58 +0900

    [FAIL] 정상 추가 케이스 작성

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -6,12 +6,29 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
+
+
+
+	@Test
+	void addRow() {
+		// Arrange
+		Ladder ladder = new Ladder();
+
+		HorizontalStepList rowOne = new HorizontalStepList(5, () -> new Random().nextBoolean());
+		ladder.addRow(rowOne);
+
+		HorizontalStepList rowTwo = new HorizontalStepList(5, () -> new Random().nextBoolean());
+		ladder.addRow(rowTwo);
+
+		assertThat(ladder.size()).isEqualTo(2);
+	}
 
 	@DisplayName("개수가 다른 가로열 추가 시도")
 	@Test
