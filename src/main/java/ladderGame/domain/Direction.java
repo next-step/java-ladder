9d4d7ff9e3f25ab394b45cf565e657cb396712d2
SunkyungCho <sunkyung@queryjet.com>
2019-07-03 10:06:41 +0900

    feature: 방향을 나타내는 정보 생성

---
+++ src/main/java/ladderGame/domain/Direction.java
@@ -0,0 +1,24 @@
+package ladderGame.domain;
+
+import java.util.EnumSet;
+
+public enum  Direction {
+    RIGHT(false, true),
+    LEFT(true, false),
+    STRAIGHT(false, false);
+
+    private final boolean left;
+    private final boolean right;
+
+    Direction(boolean left, boolean right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public static Direction decide(boolean left, boolean right) {
+        return EnumSet.allOf(Direction.class).stream()
+                .filter(Direction -> Direction.left == left && Direction.right == right)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("연속된 계단은 만들어 질 수 없어"));
+    }
+}
