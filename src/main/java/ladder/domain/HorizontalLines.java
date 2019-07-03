b68d0c0d89b20487f85125773b5e4ee53d453979
SangHyoun Yun <hainco@gmail.com>
2019-07-03 23:42:13 +0900

    refactor(ladder): HorizontalLines 에서 validation 체크를 throw 로 진행

--- src/main/java/ladder/domain/HorizontalLines.java
+++ src/main/java/ladder/domain/HorizontalLines.java
@@ -3,63 +3,43 @@ package ladder.domain;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class HorizontalLines {
     private final List<HorizontalLine> horizontalLines;
 
     private HorizontalLines(List<HorizontalLine> horizontalLines) {
-        /*
-        if (!isValid()) {
-            throw new IllegalArgumentException("유효하지 않은 HorizontalLines 입니다");
-        }
-        */
         this.horizontalLines = horizontalLines;
+        validate();
     }
 
-    /*
-    이쪽의 private 메서드들을 통해서, 주어진 HorizontalLines 가 모두 valid 한지 검사하고 싶습니다.
-    그런데 람다를 사용하려니, index 를 사용할 수 없어서 특정 위치의 HorizontalLine 이 이웃한 라인이 있는지 없는지
-    체크하기가 어려운것 같습니다.
-    indexOf 로 위치를 찾아보려 했으나, 정적 팩토리 메서드 사용으로 인해 true 인 HorizontalLine 은
-    모두 같은 인스턴스이기 때문에 찾을 수 없는것 같습니다.
-    자바에 익숙하지 못하여 제대로 사용하지 못하고 있는것 같은데 뭔가 좋은 방법이 없는지 궁금합니다..
-     */
-    private static boolean hasNeighborLine(List<HorizontalLine> horizontalLines, int index) {
-        if (index < 0 || index > horizontalLines.size()) {
-            throw new IllegalArgumentException("올바르지 않은 index 값입니다.");
-        }
-        int nextIndex = index + 1;
-        int previousIndex = index - 1;
+    private void validate() {
+        checkFirstLineFalse();
+        allHorizontalLineLineHasNoNeighborLine();
+    }
 
-        if (index < 1) {
-            return horizontalLines.get(nextIndex).hasLine();
+    private void checkFirstLineFalse() {
+        if (horizontalLines.get(0).hasLine()) {
+            throw new IllegalArgumentException("첫번째 HorizontalLine 은 Line 을 가질 수 없습니다.");
         }
+    }
 
+    private boolean hasLineAndHasNeighborLine(int index) {
+        if (!horizontalLines.get(index).hasLine() || index == 0) {
+            return false;
+        }
         if (index == horizontalLines.size() - 1) {
-            return horizontalLines.get(previousIndex).hasLine();
+            return horizontalLines.get(index - 1).hasLine();
         }
-
-        return horizontalLines.get(previousIndex).hasLine()
-                && horizontalLines.get(nextIndex).hasLine();
+        return horizontalLines.get(index - 1).hasLine() || horizontalLines.get(index + 1).hasLine();
     }
 
-    private boolean isValid() {
-        return isFirstLineFalse() && allHorizontalLineLineHasNoNeighborLine();
-    }
-
-    private boolean isFirstLineFalse() {
-        return horizontalLines.get(0).hasLine();
-    }
-
-    private boolean allHorizontalLineLineHasNoNeighborLine() {
-        boolean valid = true;
-        for (int i = 0; i < horizontalLines.size(); i++) {
-            boolean inValid = HorizontalLines.hasNeighborLine(horizontalLines, i);
-            if (inValid) {
-                valid = false;
-            }
+    private void allHorizontalLineLineHasNoNeighborLine() {
+        if (IntStream.range(0, horizontalLines.size())
+                .mapToObj(this::hasLineAndHasNeighborLine)
+                .anyMatch(aBoolean -> aBoolean)) {
+            throw new IllegalArgumentException("연속으로 Line 이 있는 HorizontalLines 입니다.");
         }
-        return valid;
     }
 
     public static HorizontalLines of(VerticalLines verticalLines) {
