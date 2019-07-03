573a5ce8e1c898e260e5715d5480d5aeea2d41c0
rabbitfoot <wowyowowyo@gmail.com>
2019-07-03 10:38:50 +0900

    [step2/refactor] GamerTest 리펙토링

--- src/test/java/ladder/domain/GamerTest.java
+++ src/test/java/ladder/domain/GamerTest.java
@@ -1,5 +1,6 @@
 package ladder.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -7,10 +8,15 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class GamerTest {
     private static final String GAMER_NAMES = "pobi,honux,crong,jk";
+    private Gamer gamer;
+
+    @BeforeEach
+    void setUp() {
+        gamer = Gamer.newGamers(GAMER_NAMES);
+    }
 
     @Test
     void 정상참여자_두명구하기() {
-        Gamer gamer = Gamer.newGamers(GAMER_NAMES);
         assertThat(gamer.getNames().size()).isEqualTo(4);
     }
 
@@ -25,7 +31,6 @@ public class GamerTest {
 
     @Test
     void 게이머이름출력() {
-        Gamer gamer = Gamer.newGamers("pobi,honux,crong,jk");
         gamer.getNames().stream().forEach(System.out::println);
     }
 }
