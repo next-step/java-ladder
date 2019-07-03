427337f3605bf43572085966112a55958ff98d92
daioooor36 <pd6016@naver.com>
2019-07-03 11:16:09 +0900

    test(Point,LadderLine) : Point, LadderLine 테스트코드 추가

--- src/test/java/nextstep/ladder/model/LadderLineTest.java
+++ src/test/java/nextstep/ladder/model/LadderLineTest.java
@@ -1,16 +1,17 @@
 package nextstep.ladder.model;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class LadderLineTest {
     private final int FIRST_POSITION = 0;
+    private final int LADDER_LINE_SIZE = 3;
 
     private LadderLine ladderLine;
 
@@ -29,13 +30,19 @@ public class LadderLineTest {
 
     @DisplayName("사람 수 만큼의 LadderLine 생성")
     @Test
-    void Line_만들기() {
+    void lineCreateTest() {
         assertThat(ladderLine.getPoints().size()).isEqualTo(4);
     }
 
-    @DisplayName("한 줄짜리 사다리 테스트")
+    @DisplayName("왼쪽 끝에서 move하면 오른쪽으로 이동")
     @Test
-    void goDownTest() {
+    void moveFromLeftEndTest() {
         assertThat(ladderLine.move(FIRST_POSITION)).isEqualTo(FIRST_POSITION + 1);
+    }
+
+    @DisplayName("오른쪽 끝에서 move하면 왼쪽으로 이동")
+    @Test
+    void moveFromRightEndTest() {
+        assertThat(ladderLine.move(LADDER_LINE_SIZE)).isEqualTo(LADDER_LINE_SIZE - 1);
     }
 }
--- src/test/java/nextstep/ladder/model/PointTest.java
+++ src/test/java/nextstep/ladder/model/PointTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 
 public class PointTest {
     private Point point;
@@ -13,12 +15,34 @@ public class PointTest {
     void setUp() {
         point = Point.first(true);
     }
-
-    @DisplayName("{false,true} 상태에서 true 쪽으로 한 칸 이동")
+    
+    @DisplayName("point의 Direction 왼쪽 값 체크")
     @Test
-    void nextTest() {
+    void directionLeftTest() {
+        assertThat(point.showLeft()).isEqualTo(false);
+    }
+    
+    @DisplayName("point의 Direction 오른쪽 값 체크")
+    @Test
+    void directionRightTest() {
+        assertThat(point.showRight()).isEqualTo(true);
+    }
+
+    @DisplayName("위치 0 = {false,true}의 상태에서 true 쪽으로 한 칸 이동")
+    @Test
+    void moveTest() {
         assertThat(point.move()).isEqualTo(1);
     }
 
+    @DisplayName("{true,false}인 Point 생성")
+    @Test
+    void nextTest() {
+        assertThat(point.next(false)).isEqualTo(new Point(1, Direction.of(true,  false)));
+    }
 
+    @DisplayName("{true,true}인 Point 생성 시 예외발생")
+    @Test
+    void both_true_Exception() {
+    	assertThatIllegalStateException().isThrownBy(() -> point.next(true));
+    }
 }
