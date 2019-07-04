534023d03d26ddb5a6d5976d6691a9eb393702cd
권병윤 <sksggg123@gmail.com>
2019-07-05 02:44:50 +0900

    [feat] : LadderFactory 클래스 추가 - 랜덤상태 채번 및 Ladder 객체 만들기

--- src/main/java/nextstep/step3/ladder/domain/LadderFactory.java
+++ src/main/java/nextstep/step3/ladder/domain/LadderFactory.java
@@ -1,5 +1,9 @@
 package nextstep.step3.ladder.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -11,4 +15,33 @@ package nextstep.step3.ladder.domain;
  * create date  : 2019-07-05 02:03
  */
 public class LadderFactory {
+
+    public static final int NEXT_INDEX = 1;
+    public static final int LAST_INDEX = 1;
+
+    public static Ladder create(int row, int line) {
+        List<LadderLine> lines = new ArrayList<>();
+        for (int i = 0; i < row; i++) {
+            lines.add(createLine(line));
+        }
+        return new Ladder(lines);
+    }
+
+    private static LadderLine createLine(int line) {
+        List<Link> links = new ArrayList<>();
+        links.add(Link.first(() -> random()));
+        for (int i = NEXT_INDEX; i < line - LAST_INDEX; i++) {
+            links.add(nextLink(links.get(i - NEXT_INDEX)));
+        }
+        links.add(Link.last(links.get(links.size() - LAST_INDEX)));
+        return new LadderLine(links);
+    }
+
+    private static Link nextLink(Link leftLink) {
+        return Link.next(leftLink, () -> leftLink.status() ? false : random());
+    }
+
+    private static boolean random() {
+        return new Random().nextBoolean();
+    }
 }
