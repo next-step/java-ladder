2dababe0672d2dd35191eaa1cf5e8f4960b22bbe
seungkil choi <seung-kil.choi@raizegls.com>
2019-07-05 15:27:32 +0900

    게임 실행 메인 메서드 클래스 추가

--- src/main/java/ladder/GameRunner.java
+++ src/main/java/ladder/GameRunner.java
@@ -1,4 +1,18 @@
 package ladder;
 
+import ladder.view.in.ConsoleDialog;
+import ladder.view.in.InputDialog;
+import ladder.view.out.ConsoleRenderer;
+
+import java.util.Random;
+
 public class GameRunner {
+
+	public static void main(String args[]) {
+		GameController controller = new GameController( new ConsoleRenderer());
+
+		InputDialog input = new ConsoleDialog();
+		Random random = new Random();
+		controller.ready(input, random::nextBoolean);
+	}
 }
