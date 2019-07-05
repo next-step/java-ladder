fd4b0d850eedcc167887bf64ece2c3ea5436f7cd
SunkyungCho <sunkyungch@gmail.com>
2019-07-05 11:22:43 +0900

    refactor: 사다리 한 층의 방향 정보들을 갖는 일급 컬렉션으로 수정

--- src/main/java/ladderGame/domain/DirectionLayer.java
+++ src/main/java/ladderGame/domain/DirectionLayer.java
@@ -1,52 +1,66 @@
 package ladderGame.domain;
 
-import ladderGame.util.RungsGenerator;
+import ladderGame.util.DirectionGenerator;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class DirectionLayer {
 
     private final static int ONE_RUNG = 1;
-    private final static Boolean LIMIT_BOUNDARY_RUNG = false;
 
     private List<Direction> directions;
 
-    private DirectionLayer(List<Boolean> rungs) {
-        directions = new ArrayList<>();
-        rungs.stream()
-                .reduce(LIMIT_BOUNDARY_RUNG, this::buildDirection);
-        directions.add(lastDirection(rungs));
+    private DirectionLayer(List<Direction> directions) {
+        verifyOutOfLadderDirection(directions);
+        verifyLadderRule(directions);
+        this.directions = directions;
     }
 
-    public static DirectionLayer ofGenerator(int numberOfPlayer, RungsGenerator rungsGenerator) {
-        List<Boolean> generate = rungsGenerator.generate(numberOfPlayer - ONE_RUNG);
-        return new DirectionLayer(generate);
+    public static DirectionLayer ofGenerator(int numberOfPlayer, DirectionGenerator directionGenerator) {
+        List<Direction> directions = directionGenerator.generate(numberOfPlayer - ONE_RUNG);
+        verifyQuantity(numberOfPlayer, directions);
+
+        return new DirectionLayer(directions);
     }
 
-    public Direction getDirections(int position) {
-        return directions.get(position);
+    private static void verifyQuantity(int numberOfPlayer, List<Direction> directions) {
+        if (numberOfPlayer != directions.size()) {
+            throw new IllegalArgumentException("방향의 수는 입력된 참가자와 동일해야 합니다.");
+        }
     }
 
-    public List<Direction> getDirections() {
+    public List<Direction> getDirectionsByPosition() {
         return directions;
     }
 
-    private Boolean buildDirection(Boolean left, Boolean right) {
-        directions.add(Direction.decide(left, right));
-        return right;
+    public Direction getDirectionsByPosition(Position position) {
+        return IntStream.range(0, directions.size())
+                .filter(position::isEquals)
+                .mapToObj(index -> directions.get(index))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 범위의 Position 입니다." + position.toString()));
     }
 
-    private Direction lastDirection(List<Boolean> rungs) {
-        Boolean lastElement = getLastElement(rungs);
-        if (lastElement) {
-            return Direction.LEFT;
+    private void verifyLadderRule(List<Direction> directions) {
+        directions.stream()
+                .reduce(null, this::verifyRule);
+    }
+
+    private Direction verifyRule(Direction previous, Direction current) {
+        if (previous == Direction.RIGHT && current == Direction.LEFT) {
+            throw new IllegalArgumentException("방향이 서로 충동하고 있습니다.");
         }
-        return Direction.STRAIGHT;
+        return current;
     }
 
-    private Boolean getLastElement(List<Boolean> rungs) {
-        return rungs.get(rungs.size() - 1);
+    private void verifyOutOfLadderDirection(List<Direction> directions) {
+        Direction firstDirection = directions.get(0);
+        Direction lastDirection = directions.get(directions.size() - 1);
+
+        if (firstDirection == Direction.LEFT || lastDirection == Direction.RIGHT) {
+            throw new IllegalArgumentException("사다리 범위를 넘어가는 움직임");
+        }
     }
 
 }
--- src/test/java/ladderGame/domain/DirectionLayerTest.java
+++ src/test/java/ladderGame/domain/DirectionLayerTest.java
@@ -11,17 +11,51 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 class DirectionLayerTest {
 
     @Test
-    @DisplayName("참가자 수로 생성된 층의 개수 확인")
-    public void createByEntry() {
-        DirectionLayer directionLayer = DirectionLayer.ofGenerator(5, count -> Arrays.asList(false, true, false, true));
-        assertThat(directionLayer.getDirections()).containsExactly(Direction.STRAIGHT, Direction.RIGHT, Direction.LEFT, Direction.RIGHT, Direction.LEFT);
+    @DisplayName("현재 Position에 따른 방향 찾기")
+    public void findByPosition() {
+        DirectionLayer directionLayer = DirectionLayer.ofGenerator(2, count -> Arrays.asList(Direction.STRAIGHT, Direction.LEFT));
+        Direction direction = directionLayer.getDirectionsByPosition(new Position(0));
+        assertThat(direction == Direction.STRAIGHT).isTrue();
     }
 
     @Test
-    @DisplayName("true 가 연속됐을 경우 예외처리")
-    public void test() {
+    @DisplayName("존재하지 않는 position")
+    public void findByPositionException() {
+        DirectionLayer directionLayer = DirectionLayer.ofGenerator(2, count -> Arrays.asList(Direction.STRAIGHT, Direction.LEFT));
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            DirectionLayer.ofGenerator(5, count -> Arrays.asList(true, true, true, false));
+            directionLayer.getDirectionsByPosition(new Position(2));
+        });
+    }
+
+    @Test
+    @DisplayName("방향이 충돌되었을 경우 예외처리")
+    public void conflictException() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            DirectionLayer.ofGenerator(4, count -> Arrays.asList(Direction.RIGHT, Direction.RIGHT, Direction.LEFT, Direction.STRAIGHT));
+        });
+    }
+
+    @Test
+    @DisplayName("사다리 범위를 오른쪽 넘어가는 방향 예외처리")
+    public void rightSideOutOfLadderException() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            DirectionLayer.ofGenerator(2, count -> Arrays.asList(Direction.STRAIGHT, Direction.RIGHT));
+        });
+    }
+
+    @Test
+    @DisplayName("사다리 범위를 왼쪽 넘어가는 방향 예외처리")
+    public void leftSideOutOfLadderException() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            DirectionLayer.ofGenerator(2, count -> Arrays.asList(Direction.LEFT, Direction.STRAIGHT));
+        });
+    }
+
+    @Test
+    @DisplayName("참가자 수와 맞지 않는 방향 생성 예외처리")
+    public void smallDirectThenNumberOfPlayersException() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            DirectionLayer.ofGenerator(5, count -> Arrays.asList(Direction.RIGHT, Direction.STRAIGHT));
         });
     }
 }
