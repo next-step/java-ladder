6162bb028018b18c4e08b8526d0878379ab73b77
KIM JUNGHWAN <writer0713@naver.com>
2019-07-06 19:42:49 +0900

    #step3 [refactor] Reward에서 Position 객체 사용하도록 수정

--- src/main/java/ladder/model/Reward.java
+++ src/main/java/ladder/model/Reward.java
@@ -3,16 +3,11 @@ package ladder.model;
 public class Reward {
 
     private final String value;
-
-    private int position;
-
-    public Reward(String value) {
-        this.value = value;
-    }
+    private Position position;
 
     public Reward(String value, int position) {
         this.value = value;
-        this.position = position;
+        this.position = new Position(position);
     }
 
     public String getValue() {
