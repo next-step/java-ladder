ddecc676f41a88529480085ed0aff8edf1e0ddb2
KIM JUNGHWAN <writer0713@naver.com>
2019-07-06 19:44:41 +0900

    #step3 [feature] 게임 결과 구하고 출력 구현

--- src/main/java/ladder/Application.java
+++ src/main/java/ladder/Application.java
@@ -4,21 +4,24 @@ import ladder.model.*;
 import ladder.view.InputView;
 import ladder.view.OutputView;
 
-import java.util.List;
-
 public class Application {
 
     public static void main(String[] args) {
 
         Players players = InputView.askPlayers();
-
         Rewards rewards = InputView.askRewards();
-
         int height = InputView.askHeight();
 
         Ladder ladder = Ladder.of(players.size(), height);
 
+        LadderGame.start(ladder, players);
+        Result result = LadderGame.makeResult(players, rewards);
+
         OutputView.print(players, ladder, rewards);
+
+        String name = InputView.askResult();
+        OutputView.print(name, result);
+
 
     }
 }
