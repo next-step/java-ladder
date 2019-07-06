ef61bbb86ed65f7f39a91723170e86ac01b7159e
KIM JUNGHWAN <writer0713@naver.com>
2019-07-06 18:56:24 +0900

    #step3 [test] LadderGameTest 추가 (test fail)

---
+++ src/test/java/ladder/model/LadderGameTest.java
@@ -0,0 +1,45 @@
+package ladder.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class LadderGameTest {
+
+  @Test
+  void test_startGame() {
+
+    Rewards rewards = makeMockRewards();
+    Players players = makeMockPlayers();
+    Ladder ladder = makeMockLayer();
+
+    LadderGame.start(ladder, players);
+
+    Result result = LadderGame.makeResult(players, rewards);
+
+    assertThat(result.get("kim")).isEqualTo("3000");
+    assertThat(result.get("lee")).isEqualTo("4000");
+  }
+
+  private Ladder makeMockLayer() {
+    Layer layer1 = new Layer(Arrays.asList(false, true, false));
+    Layer layer2 = new Layer(Arrays.asList(false, false, true));
+    Layer layer3 = new Layer(Arrays.asList(false, true, false));
+
+    Ladder ladder = new Ladder(Arrays.asList(layer1, layer2, layer3));
+    return ladder;
+  }
+
+  private Players makeMockPlayers() {
+    List<String> names = Arrays.asList("kim", "lee", "park", "low");
+    return Players.of(names);
+  }
+
+  private Rewards makeMockRewards() {
+    List<String> rewards = Arrays.asList("3000", "꽝", "2000", "4000");
+    return Rewards.of(rewards);
+  }
+}
