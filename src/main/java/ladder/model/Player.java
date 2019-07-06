dcc9c38462ad26f12c3298b219ef548f0e8f5314
KIM JUNGHWAN <writer0713@naver.com>
2019-07-06 16:57:20 +0900

    #step3 [refactor] Player가 position 갖도록 수정
    Constructor 추가

--- src/main/java/ladder/model/Player.java
+++ src/main/java/ladder/model/Player.java
@@ -3,9 +3,15 @@ package ladder.model;
 public class Player {
 
     private final String name;
+    private int position;
 
     public Player(String name) {
         this.name = name;
+    }
+
+    public Player(String name, int position) {
+        this.name = name;
+        this.position = position;
     }
 
     public String getName() {
