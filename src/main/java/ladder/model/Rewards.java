bc3fa5eb8ff1cedde1c97d5c59b6b6322023fc48
KIM JUNGHWAN <writer0713@naver.com>
2019-07-06 17:08:13 +0900

    #step3 [feature] Rewards 구현 (test success)

---
+++ src/main/java/ladder/model/Rewards.java
@@ -0,0 +1,28 @@
+package ladder.model;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Rewards {
+
+  private final List<Reward> rewards;
+
+  public Rewards(List<Reward> rewards) {
+    this.rewards = Collections.unmodifiableList(rewards);
+  }
+
+  public static Rewards of(List<String> values) {
+    int count = values.size();
+    List<Reward> rewards = IntStream.range(0, count)
+            .mapToObj(index -> new Reward(values.get(index), index))
+            .collect(toList());
+    return new Rewards(rewards);
+  }
+
+  public int getSize() {
+    return this.rewards.size();
+  }
+}
