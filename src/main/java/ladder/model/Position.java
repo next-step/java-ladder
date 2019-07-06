e1102205f736a3f9e9cb8031aa9125903875da3d
KIM JUNGHWAN <writer0713@naver.com>
2019-07-06 18:58:00 +0900

    #step3 [refactor] toString 구현

--- src/main/java/ladder/model/Position.java
+++ src/main/java/ladder/model/Position.java
@@ -15,6 +15,13 @@ public class Position {
   }
 
   @Override
+  public String toString() {
+    return "Position{" +
+            "value=" + value +
+            '}';
+  }
+
+  @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
@@ -24,7 +31,6 @@ public class Position {
 
   @Override
   public int hashCode() {
-
     return Objects.hash(value);
   }
 }
