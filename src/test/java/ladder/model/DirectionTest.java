6c2cfc5b70d3a36ffc7a59e4ad99bfbc1b6d23c8
KIM JUNGHWAN <writer0713@naver.com>
2019-07-06 18:02:06 +0900

    #step3 [test] Direction.move test 추가

--- src/test/java/ladder/model/DirectionTest.java
+++ src/test/java/ladder/model/DirectionTest.java
@@ -14,4 +14,12 @@ public class DirectionTest {
     assertThat(Direction.of(true, false)).isEqualTo(Direction.LEFT);
     assertThat(Direction.of(false, false)).isEqualTo(Direction.PASS);
   }
+
+  @Test
+  @DisplayName("move")
+  void test_move() {
+    assertThat(Direction.RIGHT.move(new Position(0))).isEqualTo(new Position(1));
+    assertThat(Direction.PASS.move(new Position(0))).isEqualTo(new Position(0));
+    assertThat(Direction.LEFT.move(new Position(1))).isEqualTo(new Position(0));
+  }
 }
