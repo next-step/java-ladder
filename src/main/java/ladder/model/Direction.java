2ea058a95021b8311cbb4979f8ae0433d45e1ace
KIM JUNGHWAN <writer0713@naver.com>
2019-07-06 17:58:52 +0900

    #step3 [refactor] Player가 Position 객체를 사용하도록 테스트 수정 (test success)

--- src/main/java/ladder/model/Direction.java
+++ src/main/java/ladder/model/Direction.java
@@ -16,6 +16,14 @@ public enum Direction {
         this.right = right;
     }
 
+    Position moveRight(Position position) {
+        return new Position(position.getValue() + 1);
+    }
+
+    Position moveLeft(Position position) {
+        return new Position(position.getValue() - 1);
+    }
+
     public static Direction of(boolean left, boolean right) {
         return EnumSet.allOf(Direction.class).stream()
                 .filter(Direction -> Direction.left == left && Direction.right == right)
@@ -23,15 +31,11 @@ public enum Direction {
                 .orElseThrow(() -> new IllegalArgumentException("연속된 Direction은 만들수 없습니다"));
     }
 
-    public int move(int position) {
-        if (this == Direction.RIGHT) {
-            return position + 1;
-        }
+    public Position move(final Position position) {
+        if (this == Direction.RIGHT) return moveRight(position);
 
-        if (this == Direction.LEFT) {
-            return position - 1;
-        }
+        if (this == Direction.LEFT)  return moveLeft(position);
 
-        return position;
+        return new Position(position.getValue());
     }
 }
--- src/main/java/ladder/model/Player.java
+++ src/main/java/ladder/model/Player.java
@@ -3,7 +3,7 @@ package ladder.model;
 public class Player {
 
     private final String name;
-    private int position;
+    private Position position;
 
     public Player(String name) {
         this.name = name;
@@ -11,18 +11,15 @@ public class Player {
 
     public Player(String name, int position) {
         this.name = name;
-        this.position = position;
+        this.position = new Position(position);
     }
 
     public String getName() {
         return name;
     }
 
-    public int getPosition() {
-        return position;
-    }
-
-    public void ride(Direction direction) {
+    public Position ride(Direction direction) {
         this.position = direction.move(this.position);
+        return this.position;
     }
 }
