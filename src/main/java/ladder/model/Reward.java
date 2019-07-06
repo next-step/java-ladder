b6609d4206078b2e67b6c31027849bf84270d31f
KIM JUNGHWAN <writer0713@naver.com>
2019-07-06 17:07:44 +0900

    #step3 [refactor] Reward가 position을 갖도록 수정

--- src/main/java/ladder/model/Reward.java
+++ src/main/java/ladder/model/Reward.java
@@ -4,8 +4,15 @@ public class Reward {
 
     private final String value;
 
+    private int position;
+
     public Reward(String value) {
         this.value = value;
+    }
+
+    public Reward(String value, int position) {
+        this.value = value;
+        this.position = position;
     }
 
     public String getValue() {
