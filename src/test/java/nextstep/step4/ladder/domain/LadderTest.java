d993bf1a256713631a1b2770770519562f5fb262
권병윤 <sksggg123@gmail.com>
2019-07-07 17:48:29 +0900

    [test] : @NullAndEmptySource로 테스트 코드 변경

--- src/test/java/nextstep/step4/ladder/domain/LadderTest.java
+++ src/test/java/nextstep/step4/ladder/domain/LadderTest.java
@@ -1,7 +1,10 @@
 package nextstep.step4.ladder.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,35 +24,12 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
  * create date  : 2019-06-30 04:18
  */
 public class LadderTest {
-    @DisplayName("사다리 생성시 전달된 인자가 비어있을때")
-    @Test
-    void createLadderException() {
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            new Ladder(Arrays.asList());
-        }).withMessageContaining("사다리가 비어있습니다.");
-    }
 
-    @DisplayName("시작점으로 부터 마지막 도착 index 가지고 오기")
-    @Test
-    void executeLadderGame() {
-        Ladder ladder = new Ladder(
-                Arrays.asList(
-                        new LadderLine(
-                                Arrays.asList(
-                                        new Link(0, new Point(true, false)),
-                                        new Link(1, new Point(false, true)))),
-                        new LadderLine(
-                                Arrays.asList(
-                                        new Link(0, new Point(false, false)),
-                                        new Link(1, new Point(false, false))))));
+    private Ladder ladder;
 
-        assertThat(ladder.execute(0)).isEqualTo(1);
-    }
-
-    @DisplayName("play 결과 반환 Map Collection")
-    @Test
-    void paly() {
-        Ladder ladder = new Ladder(
+    @BeforeEach
+    void setUp() {
+        ladder = new Ladder(
                 Arrays.asList(
                         new LadderLine(
                                 Arrays.asList(
@@ -61,6 +41,26 @@ public class LadderTest {
                                         new Link(0, new Point(false, false)),
                                         new Link(1, new Point(true, false)),
                                         new Link(2, new Point(false, true))))));
+    }
+
+    @DisplayName("사다리 생성시 전달된 인자가 비어있을때")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void createLadderException(List<LadderLine> ladderLines) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Ladder(ladderLines);
+        }).withMessageContaining("사다리가 비어있습니다.");
+    }
+
+    @DisplayName("시작점으로 부터 마지막 도착 index 가지고 오기")
+    @Test
+    void executeLadderGame() {
+        assertThat(ladder.execute(0)).isEqualTo(2);
+    }
+
+    @DisplayName("play 결과 반환 Map Collection")
+    @Test
+    void paly() {
         Name name1 = Name.of("kwon");
         Name name2 = Name.of("be");
         Name name3 = Name.of("why");
