5dc8c170e803ded948434cd8d20a052e74e4856e
SangHyoun Yun <hainco@gmail.com>
2019-07-07 15:18:11 +0900

    feat(ladder): Reward 클래스 추가

---
+++ src/main/java/ladder/domain/Reward.java
@@ -0,0 +1,13 @@
+package ladder.domain;
+
+public class Reward {
+    private final String name;
+
+    public Reward(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
