ac0fad87f0e27f288f0d4cfc56ba55684a00d38c
sogoagain <imyong0@gmail.com>
2019-07-07 18:02:09 +0900

    refactor: 참가자 수 검증 로직 추가

--- src/main/java/ladder/model/Participants.java
+++ src/main/java/ladder/model/Participants.java
@@ -7,9 +7,17 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Participants {
+    private static final int MINIMUM_NUMBER_OF_PARTICIPANTS = 2;
+    private static final String MESSAGE_OF_MINIMUM_PARTICIPANTS_EXCEPTION = "참가자 수는 최소 %d 이상이어야 합니다.";
+
     private final List<Participant> participants;
 
     public Participants(List<String> names) {
+        if (names.size() < MINIMUM_NUMBER_OF_PARTICIPANTS) {
+            throw new IllegalArgumentException(
+                    String.format(MESSAGE_OF_MINIMUM_PARTICIPANTS_EXCEPTION, MINIMUM_NUMBER_OF_PARTICIPANTS));
+        }
+
         participants = names.stream()
                             .map(Participant::new)
                             .collect(Collectors.collectingAndThen(toList(), Collections::unmodifiableList));
