ecbda158afb882b221b9085b10b68244819ef33f
KIM JUNGHWAN <writer0713@naver.com>
2019-07-08 22:08:14 +0900

    #step4 [refactor] 불필요한 객체 생성 관련 test들 삭제

--- src/test/java/ladder/model/LayerTest.java
+++ src/test/java/ladder/model/LayerTest.java
@@ -13,32 +13,6 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 public class LayerTest {
 
     @Test
-    @DisplayName("Layer.of")
-    void layerTest() {
-        RungGenerator rungGenerator = new RandomRungGenerator();
-        int players = 4;
-        Layer layer = Layer.of(rungGenerator, players);
-
-        assertThat(layer.getDirections().size()).isEqualTo(4);
-
-        layer.getDirections().forEach(System.out::println);
-    }
-
-    @Test
-    @DisplayName("Constructor")
-    void constructor() {
-        List<Boolean> rungs = Arrays.asList(true, false, true);
-        Layer layer = new Layer(rungs);
-
-        assertThat(layer.getDirections()).containsExactly(
-                Direction.RIGHT,
-                Direction.LEFT,
-                Direction.RIGHT,
-                Direction.LEFT
-        );
-    }
-
-    @Test
     @DisplayName("lastDirection")
     void lastDirection() {
         assertThat(Layer.lastDirection(Arrays.asList(true))).isEqualTo(Direction.LEFT);
