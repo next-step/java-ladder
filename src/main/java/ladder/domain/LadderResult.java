f73624e00ff3d50fc21928df10758a16dd00fa50
SangHyoun Yun <hainco@gmail.com>
2019-07-09 23:47:03 +0900

    feat(ladder): LadderResult 클래스에 result 메서드 추가

--- src/main/java/ladder/domain/LadderResult.java
+++ src/main/java/ladder/domain/LadderResult.java
@@ -1,9 +1,16 @@
 package ladder.domain;
 
+import java.util.stream.IntStream;
+
 public class LadderResult {
     private final Ladder ladder;
 
     public LadderResult(Ladder ladder) {
         this.ladder = ladder;
+    }
+
+    public int result(int index) {
+        return IntStream.range(0, ladder.height())
+                .reduce(index, (resultIndex, current) -> resultIndex = ladder.linesByLevel(current).move(resultIndex));
     }
 }
