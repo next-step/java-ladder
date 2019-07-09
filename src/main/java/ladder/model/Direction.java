f17f33014445ff614cf35a4bead45f9aca7178d3
sogoagain <imyong0@gmail.com>
2019-07-09 21:17:44 +0900

    refactor: Direction enum 인스턴스 변수 제거

--- src/main/java/ladder/model/Direction.java
+++ src/main/java/ladder/model/Direction.java
@@ -1,17 +1,13 @@
 package ladder.model;
 
 public enum Direction {
-    DOWN(false, false, Constants.DOWN_STEP),
-    RIGHT(false, true, Constants.RIGHT_STEP),
-    LEFT(true, false, Constants.LEFT_STEP);
+    DOWN(Constants.DOWN_STEP),
+    RIGHT(Constants.RIGHT_STEP),
+    LEFT(Constants.LEFT_STEP);
 
-    private final boolean left;
-    private final boolean right;
     private final int step;
 
-    Direction(boolean left, boolean right, int step) {
-        this.left = left;
-        this.right = right;
+    Direction(int step) {
         this.step = step;
     }
 
