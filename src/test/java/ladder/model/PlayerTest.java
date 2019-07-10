4d9b6ae8302cbddbcface8aafb71c432ebca5eb4
KIM JUNGHWAN <writer0713@naver.com>
2019-07-10 18:55:46 +0900

    #step4 [test] takeReward 테스트 추가 (test fail)
    상 수여에 대한 테스트 추가

--- src/test/java/ladder/model/PlayerTest.java
+++ src/test/java/ladder/model/PlayerTest.java
@@ -1,16 +1,33 @@
 package ladder.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class PlayerTest {
 
+  private Player player;
+
+  @BeforeEach
+  void setUp() {
+    String name = "kim";
+    int position = 0;
+    this.player = new Player(name, position);
+  }
+
   @Test
   void ride() {
-    Player player = new Player("kim", 0);
-    
     assertThat(player.ride(Direction.RIGHT)).isEqualTo(new Position(1));
     assertThat(player.ride(Direction.RIGHT)).isEqualTo(new Position(2));
+  }
+
+  @Test
+  void takeReward() {
+    Reward rewardWithSamePosition = new Reward("3000", 0);
+    assertThat(player.takeReward(rewardWithSamePosition)).isEqualTo(true);
+
+    Reward rewardWithDifferentPosition = new Reward("4000", 2);
+    assertThat(player.takeReward(rewardWithDifferentPosition)).isEqualTo(false);
   }
 }
