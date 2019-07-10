4063a7c80aef7bcbe416c297d55bf0092a1e476e
SangHyoun Yun <hainco@gmail.com>
2019-07-11 00:42:51 +0900

    refactor(ladder): PointTest 에서 TRUE 케이스와 FALSE 케이스를 모두 검사

--- src/test/java/ladder/domain/PointTest.java
+++ src/test/java/ladder/domain/PointTest.java
@@ -3,12 +3,15 @@ package ladder.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PointTest {
     @Test
     @DisplayName("Point.first 를 통해 왼쪽으로 갈 수 없는 Point 가 생성된다")
     void first() {
-        assertThat(Point.first(false).move()).isEqualTo(0);
+        assertThat(Point.first(FALSE).move()).isEqualTo(0);
+        assertThat(Point.first(TRUE).move()).isEqualTo(1);
     }
 }
