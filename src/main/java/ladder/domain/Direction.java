15d0a8446bafb21c57be3d3217d5338ca17342fa
SangHyoun Yun <hainco@gmail.com>
2019-07-11 01:11:30 +0900

    feat(ladder): Direction 에 of, isLeft, isRight 와 함께 추가

---
+++ src/main/java/ladder/domain/Direction.java
@@ -0,0 +1,26 @@
+package ladder.domain;
+
+public class Direction {
+    private final boolean left;
+    private final boolean right;
+
+    private Direction(boolean left, boolean right) {
+        if (left && right) {
+            throw new IllegalArgumentException("방향은 왼쪽과 오른쪽이 동시에 존재 할 수 없습니다.");
+        }
+        this.left = left;
+        this.right = right;
+    }
+
+    public static Direction of(boolean left, boolean right) {
+        return new Direction(left, right);
+    }
+
+    public boolean isLeft() {
+        return left;
+    }
+
+    public boolean isRight() {
+        return right;
+    }
+}
