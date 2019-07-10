3d5cb9769a9e3319c4362e69bce7334ea1b696a9
SangHyoun Yun <hainco@gmail.com>
2019-07-11 01:14:10 +0900

    test(ladder): DirectionTest 에 of 에 대한 TC 추가

--- src/test/java/ladder/domain/DirectionTest.java
+++ src/test/java/ladder/domain/DirectionTest.java
@@ -5,11 +5,35 @@ import org.junit.jupiter.api.Test;
 
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class DirectionTest {
     @Test
-    @DisplayName("of 메서드를 통해 좌 우 방향을 가진 Direction 을 생성한다.")
+    @DisplayName("of 메서드를 통해 좌 방향을 가진 Direction 을 생성한다.")
     void of() {
         assertThat(Direction.of(TRUE, FALSE).isLeft()).isTrue();
+        assertThat(Direction.of(TRUE, FALSE).isRight()).isFalse();
+    }
+
+    @Test
+    @DisplayName("of 메서드를 통해 우 방향을 가진 Direction 을 생성한다.")
+    void of2() {
+        assertThat(Direction.of(FALSE, TRUE).isLeft()).isFalse();
+        assertThat(Direction.of(FALSE, TRUE).isRight()).isTrue();
+    }
+
+    @Test
+    @DisplayName("of 메서드를 통해 아무 방향이 없는 Direction 을 생성한다.")
+    void of3() {
+        assertThat(Direction.of(FALSE, FALSE).isLeft()).isFalse();
+        assertThat(Direction.of(FALSE, FALSE).isRight()).isFalse();
+    }
+
+    @Test
+    @DisplayName("of 메서드를 통해 양쪽 방향을 생성하면 Error 발생")
+    void ofError() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> Direction.of(TRUE, TRUE));
     }
 }
