1969cd859d224993a4455c8bfce24c614e74f03d
SangHyoun Yun <hainco@gmail.com>
2019-07-11 00:48:52 +0900

    feat(ladder): Point 에 next() 추가 및 생성자에서 validation 체크하도록 추가

--- src/main/java/ladder/domain/Point.java
+++ src/main/java/ladder/domain/Point.java
@@ -6,6 +6,9 @@ public class Point {
     private final boolean isRight;
 
     private Point(int index, boolean isLeft, boolean isRight) {
+        if (isLeft && isRight) {
+            throw new IllegalArgumentException("사다리는 왼쪽과 오른쪽이 동시에 존재 할 수 없습니다.");
+        }
         this.index = index;
         this.isLeft = isLeft;
         this.isRight = isRight;
@@ -23,5 +26,9 @@ public class Point {
             return index + 1;
         }
         return index;
+    }
+
+    public Point next() {
+        return new Point(index + 1, isRight, false);
     }
 }
