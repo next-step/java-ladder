94e650deb0b2b9afafb5192f0a345622128b09b3
jaeheon209 <jaeheon209@gmail.com>
2019-07-11 14:44:48 +0900

    refactor(step4) :  피드백 반영-resultOfPlayer 메소드 위치 변경

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -17,4 +17,23 @@ public class Ladder {
     public List<Line> getLadder() {
         return Collections.unmodifiableList(ladder);
     }
+
+    public Point_old resultByPlayer (Point_old pointOldOfPlayer){
+        Point_old pointOld = pointOldOfPlayer;
+        for (Line line : ladder) {
+            if (pointOld.isFirst()) {
+                pointOld = pointOld.move(Direction_old.first(line.isLine(pointOld.getIndex())));
+                continue;
+            }
+            if (!pointOld.isFirst() && !pointOld.isLast(line.size())) {
+                pointOld = pointOld.move(Direction_old.of(line.isLine(pointOld.getIndex() - 1), line.isLine(pointOld.getIndex())));
+                continue;
+            }
+            if (pointOld.isLast(line.size())) {
+                pointOld = pointOld.move(Direction_old.last(line.isLine(pointOld.getIndex() - 1)));
+                continue;
+            }
+        }
+        return pointOldOfPlayer;
+    }
 }
--- src/main/java/ladder/domain/LadderGame.java
+++ src/main/java/ladder/domain/LadderGame.java
@@ -19,29 +19,10 @@ public class LadderGame {
 
 		Map<Player, Reward> ladderResult = new HashMap<>();
 		for (Player player : players.getPlayers()) {
-			Point result = resultOfPlayer(players.pointOfPlayer(player), ladder);
+			Point_old result = ladder.resultByPlayer(players.pointOfPlayer(player));
 			ladderResult.put(player, rewards.rewardByResult(result));
 		}
 		return new LadderResult(ladderResult);
-	}
-
-	public static Point resultOfPlayer(Point playerPoint, Ladder ladder) {
-		Point point = playerPoint;
-		for (Line line : ladder.getLadder()) {
-			if (point.isFirst()) {
-				point = point.isMove(Direction.first(line.isLine(point.getIndex())));
-				continue;
-			}
-			if (!point.isFirst() && !point.isLast(line.size())) {
-				point = point.isMove(Direction.of(line.isLine(point.getIndex() - 1), line.isLine(point.getIndex())));
-				continue;
-			}
-			if (point.isLast(line.size())) {
-				point = point.isMove(Direction.last(line.isLine(point.getIndex() - 1)));
-				continue;
-			}
-		}
-		return point;
 	}
 
 	public Players getPlayers() {
