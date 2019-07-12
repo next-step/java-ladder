e816327c0d62dedf73822c5e4dac612927630eef
한정수 <integer@zuminternet.com>
2019-07-12 16:42:18 +0900

    [step4] feat : Point 인스턴스의 생성조건에 따라 연결 방향을 표현하는 Enum 추가

---
+++ src/main/java/ladder/domain/Direction.java
@@ -0,0 +1,16 @@
+package ladder.domain;
+
+public enum Direction {
+
+    LEFT(true, false),
+    RIGHT(false, true),
+    PASS(false, false);
+
+    private boolean left;
+    private boolean right;
+
+    Direction(boolean left, boolean right) {
+        this.left = left;
+        this.right = right;
+    }
+}
