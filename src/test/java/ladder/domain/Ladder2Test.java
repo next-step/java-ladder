e2a64ba1b1e490ced5df9882a965d7ef90d8cdad
SangHyoun Yun <hainco@gmail.com>
2019-07-14 21:36:57 +0900

    test(ladder): Ladder2Test 생성

---
+++ src/test/java/ladder/domain/Ladder2Test.java
@@ -0,0 +1,16 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Ladder2Test {
+    int height = 4;
+    int ladderSize = 4;
+
+    @Test
+    @DisplayName("Ladder 를 주어진 크기와 높이만큼 생성한다")
+    void constructor() {
+        Ladder2 ladder2 = Ladder2.of(height, ladderSize);
+        assertThat(ladder2.height()).isEqualTo(height);
+    }
+}
