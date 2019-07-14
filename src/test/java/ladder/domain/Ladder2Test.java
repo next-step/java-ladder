7e5971fc3d119e61444fe68d7c5001e52824de4f
SangHyoun Yun <hainco@gmail.com>
2019-07-14 21:47:42 +0900

    feat(ladder): Ladder2Test 클래스 생성

--- src/test/java/ladder/domain/Ladder2Test.java
+++ src/test/java/ladder/domain/Ladder2Test.java
@@ -3,6 +3,8 @@ package ladder.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Ladder2Test {
     int height = 4;
     int ladderSize = 4;
