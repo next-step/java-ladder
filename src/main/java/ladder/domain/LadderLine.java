baa99dc060896f170a3ee5d9aac6f31a84c32c75
SangHyoun Yun <hainco@gmail.com>
2019-07-14 20:30:45 +0900

    feat(ladder): LadderLine 클래스 추가

---
+++ src/main/java/ladder/domain/LadderLine.java
@@ -0,0 +1,47 @@
+package ladder.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class LadderLine {
+    public static final int MIN_LADDER_SIZE = 2;
+    private List<Point> points;
+
+    private LadderLine(int ladderSize) {
+        if (ladderSize < MIN_LADDER_SIZE) {
+            throw new IllegalArgumentException("ladderSize 는 " + ladderSize + "보다 커야합니다.");
+        }
+        this.points = new ArrayList<>();
+    }
+
+    public static LadderLine init(int ladderSize) {
+        LadderLine ladderLine = new LadderLine(ladderSize);
+        ladderLine.initFirst();
+        ladderLine.initBody(ladderSize);
+        ladderLine.initLast();
+        return ladderLine;
+    }
+
+    public int size() {
+        return points.size();
+    }
+
+    private void initFirst() {
+        Point first = Point.first(RandomGenerator.randomBoolean());
+        points.add(first);
+    }
+
+    private void initLast() {
+        Point last = points.get(points.size() - 1).last();
+        points.add(last);
+    }
+
+    private void initBody(int ladderSize) {
+        IntStream.range(0, ladderSize - 2)
+                .forEach((index) -> {
+                    Point nextPoint = points.get(index).next();
+                    points.add(nextPoint);
+                });
+    }
+}
