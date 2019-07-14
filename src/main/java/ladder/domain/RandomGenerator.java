794b8cf3130cce6fead0663be217e50a10f0aa77
SangHyoun Yun <hainco@gmail.com>
2019-07-14 20:28:29 +0900

    refactor(ladder): RandomGenerator 에 빠진 random 인스턴스 초기화 추가

--- src/main/java/ladder/domain/RandomGenerator.java
+++ src/main/java/ladder/domain/RandomGenerator.java
@@ -3,7 +3,7 @@ package ladder.domain;
 import java.util.Random;
 
 public class RandomGenerator {
-    private static Random random;
+    private static Random random = new Random();
 
     public static boolean randomBoolean() {
         return random.nextBoolean();
