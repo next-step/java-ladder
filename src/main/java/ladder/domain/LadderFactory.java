81249c6c15395efff7e828110f8a771f9182df80
Bentley <p2c2kbf@gmail.com>
2019-07-15 17:18:07 +0900

    refactor: LadderFactory 변수명 수정

--- src/main/java/ladder/domain/LadderFactory.java
+++ src/main/java/ladder/domain/LadderFactory.java
@@ -3,21 +3,21 @@ package ladder.domain;
 import java.util.*;
 
 public class LadderFactory {
-    private List<Line> ladderList;
+    private List<Line> ladder;
 
     public LadderFactory(int maxHeight, int sizeofusergroup) {
 
-        ladderList = new ArrayList<>();
+        ladder = new ArrayList<>();
         Line line;
 
         for (int i = 0; i < maxHeight; ++i) {
             line = new Line(sizeofusergroup);
-            ladderList.add(line);
+            ladder.add(line);
         }
     }
 
     public List<Line> getLadder() {
-        return ladderList;
+        return ladder;
     }
 
 }
