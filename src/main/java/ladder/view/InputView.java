4ddd1c8f2507029bb1baf577d9b5ee18635116e3
Bentley <p2c2kbf@gmail.com>
2019-07-17 23:00:16 +0900

    refactor: InputView 객체 수정

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -7,31 +7,27 @@ public class InputView {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        String inputUserNames = scanner.nextLine();
-        return inputUserNames;
+        return scanner.nextLine();
     }
 
     public static int inputLadderMaxHeight() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("\n최대 사다리 높이는 몇 개인가요?");
-        int maxHeight = scanner.nextInt();
-        return maxHeight;
+        return scanner.nextInt();
     }
 
     public static String inputUserReward() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("\n실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
-        String inputUserReward = scanner.nextLine();
-        return inputUserReward;
+        return scanner.nextLine();
     }
 
     public static String requestResult() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("\n결과를 보고 싶은 사람은?");
-        String userName = scanner.nextLine();
-        return userName;
+        return scanner.nextLine();
     }
 }
