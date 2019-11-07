13b704b79c2d9e169c59d1e9b2a40be1020a6746
pplenty <newtype26@naver.com>
2019-11-08 01:42:51 +0900

    feat: computer store

--- src/main/java/nextstep/optional/ComputerStore.java
+++ src/main/java/nextstep/optional/ComputerStore.java
@@ -3,24 +3,30 @@ package nextstep.optional;
 import nextstep.optional.Computer.SoundCard;
 import nextstep.optional.Computer.USB;
 
+import java.util.Optional;
+
 public class ComputerStore {
-	public static final String UNKNOWN_VERSION = "UNKNOWN";
-	
-	public static String getVersion(Computer computer) {
-		String version = UNKNOWN_VERSION;
-		if(computer != null){
-		  SoundCard soundCard = computer.getSoundCard();
-		  if(soundCard != null){
-		    USB usb = soundCard.getUsb();
-		    if(usb != null){
-		      version = usb.getVersion();
-		    }
-		  }
-		}
-		return version;
-	}
-	
-	public static String getVersionOptional(Computer computer) {
-		return null;
-	}
+    public static final String UNKNOWN_VERSION = "UNKNOWN";
+
+    public static String getVersion(Computer computer) {
+        String version = UNKNOWN_VERSION;
+        if (computer != null) {
+            SoundCard soundCard = computer.getSoundCard();
+            if (soundCard != null) {
+                USB usb = soundCard.getUsb();
+                if (usb != null) {
+                    version = usb.getVersion();
+                }
+            }
+        }
+        return version;
+    }
+
+    public static String getVersionOptional(Computer computer) {
+        return Optional.ofNullable(computer)
+                .map(Computer::getSoundCard)
+                .map(SoundCard::getUsb)
+                .map(USB::getVersion)
+                .orElse(UNKNOWN_VERSION);
+    }
 }
