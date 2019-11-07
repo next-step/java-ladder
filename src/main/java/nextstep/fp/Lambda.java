f73ab80776849666bcc5f03736964cf9d0570e95
dave915 <htaehyun@gmail.com>
2019-11-07 22:22:03 +0900

    [refector] sumAllOverThree 수정

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -40,12 +40,9 @@ public class Lambda {
 	}
 
 	public static int sumAllOverThree(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	    	if (number > 3) {
-	    		total += number;
-	    	}
-	    }
-	    return total;
+		int start = 0;
+		return numbers.stream()
+				.filter(number -> number > 3)
+				.reduce(start, (sum, b) -> sum += b);
 	}
 }
