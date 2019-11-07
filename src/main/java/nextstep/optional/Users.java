ba55e75ee432f1367d9a58f086ee3aff9972ad4d
Kim YoonHo <kimunoya@gmail.com>
2019-11-08 04:23:03 +0900

    refactor: 불필요 코드 제거

--- src/main/java/nextstep/optional/Users.java
+++ src/main/java/nextstep/optional/Users.java
@@ -14,11 +14,6 @@ public class Users {
             new User("honux", 45));
 
     User getUser(String name) {
-        for (User user : users) {
-            if (user.matchName(name)) {
-                return user;
-            }
-        }
         return users.stream().filter(user -> user.matchName(name)).findFirst().orElse(DEFAULT_USER);
     }
 }
