0e785b557e5b81d0b01016c63de94cffaa295186
Kim YoonHo <kimunoya@gmail.com>
2019-11-08 03:46:15 +0900

    refactor: step2 람다를 활용한 중복제거

---
+++ src/main/java/nextstep/fp/Conditional.java
@@ -0,0 +1,11 @@
+package nextstep.fp;
+
+/**
+ * @author : 김윤호
+ * @version : 1.0
+ * @date : 2019-11-08 03:25
+ */
+@FunctionalInterface
+public interface Conditional {
+    public boolean test(Integer number);
+}
--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,56 +1,51 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Lambda {
-	public static void printAllOld(List<Integer> numbers) {
-		System.out.println("printAllOld");
-		
-		for (int number : numbers) {
-		    System.out.println(number);
-		}
-	}
+    public static void printAllOld(List<Integer> numbers) {
+        System.out.println("printAllOld");
 
-	public static void printAllLambda(List<Integer> numbers) {
-		System.out.println("printAllLambda");
-		
-		numbers.forEach(System.out::println);
-	}
-	
-	public static void runThread() {
-		new Thread(new Runnable() {
-		    @Override
-		    public void run() {
-		        System.out.println("Hello from thread");
-		    }
-		}).start();
-	}
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
 
-	public static int sumAll(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	        total += number;
-	    }
-	    return total;
-	}
-	
-	public static int sumAllEven(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	        if (number % 2 == 0) {
-	            total += number;
-	        }
-	    }
-	    return total;
-	}
+    public static void printAllLambda(List<Integer> numbers) {
+        System.out.println("printAllLambda");
 
-	public static int sumAllOverThree(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	    	if (number > 3) {
-	    		total += number;
-	    	}
-	    }
-	    return total;
-	}
+        numbers.forEach(System.out::println);
+    }
+
+    public static void runThread() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello from thread");
+            }
+        }).start();
+    }
+
+    public static int sum(List<Integer> numbers, Predicate<Integer> predicate) {
+        return numbers.stream().filter(predicate).mapToInt(Integer::intValue).sum();
+    }
+
+    public static int sumAll(List<Integer> numbers) {
+        return sum(numbers, (Predicate<Integer>) (number) -> true);
+    }
+
+    public static int sumAllEven(List<Integer> numbers) {
+        final Predicate<Integer> predicate = (number) -> number % 2 == 0;
+        return sum(numbers, predicate);
+    }
+
+    public static int sumAllOverThree(List<Integer> numbers) {
+        final Predicate<Integer> predicate = (number) -> number > 3;
+        return sum(numbers, predicate);
+    }
+
+    public static int sum(List<Integer> numbers, Conditional conditional) {
+        return numbers.stream().filter(number -> conditional.test(number)).mapToInt(Integer::intValue).sum();
+    }
 }
