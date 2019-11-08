b21ece69715f5a9b28299a5d532244eb3f12031f
dave915 <40193460+dave915@users.noreply.github.com>
2019-11-08 16:58:07 +0900

    [Step1] 자바8 스트림, 람다, Optional (#247)
    * [refector] 익명 클래스를 람다로 전환
    
    * [refector] sumAll reduce 사용
    
    * [refector] sumAllEven 수정
    
    * [refector] sumAllOverThree 수정
    
    * [refector] sumAll  중복 제거
    
    * [test] sumOverThreeAndDouble 테스트
    
    * [refector] printLongestWordTop100 구현
    
    * [refector] ageIsInRange2 Optional 이용해 구현
    
    * [refector] getUser 구현
    
    * [refector] Expression.of orElseThrow 사용

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Lambda {
 	public static void printAllOld(List<Integer> numbers) {
 		System.out.println("printAllOld");
-		
+
 		for (int number : numbers) {
 		    System.out.println(number);
 		}
@@ -13,10 +13,10 @@ public class Lambda {
 
 	public static void printAllLambda(List<Integer> numbers) {
 		System.out.println("printAllLambda");
-		
+
 		numbers.forEach(System.out::println);
 	}
-	
+
 	public static void runThread() {
 		new Thread(new Runnable() {
 		    @Override
@@ -27,30 +27,23 @@ public class Lambda {
 	}
 
 	public static int sumAll(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	        total += number;
-	    }
-	    return total;
+		int start = 0;
+	    return numbers.stream()
+				.reduce(start, (sum, b) -> sum += b);
 	}
-	
+
 	public static int sumAllEven(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	        if (number % 2 == 0) {
-	            total += number;
-	        }
-	    }
-	    return total;
+	    return sumAll(numbers, number -> number % 2 == 0);
 	}
 
 	public static int sumAllOverThree(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	    	if (number > 3) {
-	    		total += number;
-	    	}
-	    }
-	    return total;
+		return sumAll(numbers, number -> number > 3);
+	}
+
+	private static int sumAll(List<Integer> numbers, Conditional conditional) {
+		int start = 0;
+		return numbers.stream()
+				.filter(conditional::test)
+				.reduce(start, (sum, b) -> sum += b);
 	}
 }
