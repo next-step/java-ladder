ac138be042af10abe0a4e23e3b1d35396c2c5b4f
naheenosaur <log.info.527@gmail.com>
2019-11-08 09:55:05 +0900

    map, reduce, filter 실습 2 - sumOverThreeAndDouble

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -21,7 +21,7 @@ public class StreamStudy {
 		}
 		return count;
 	}
-	
+
 	public static void printLongestWordTop100() throws IOException {
 		String contents = new String(Files.readAllBytes(Paths
 				.get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
@@ -38,7 +38,9 @@ public class StreamStudy {
 		return numbers.stream().reduce(0, (x, y) -> x + y);
 	}
 
-	public static long sumOverThreeAndDouble(List<Integer> numbers) {
-		return 0;
+	public static long sumOverThreeAndDouble(List<Integer> numbers) throws IOException {
+		return numbers.stream().filter(number -> number > 3)
+				.map(number -> number * 2)
+				.reduce(0, (o1, o2) -> o1 + o2);
 	}
 }
