68ab768df4e49d3d367f19bf5157b7ef297e8a81
vagabond95 <progjh@gmail.com>
2019-11-10 20:45:31 +0900

    [feat] PointType 에 Text 변수 추가

--- src/main/java/nextstep/ladder/PointType.java
+++ src/main/java/nextstep/ladder/PointType.java
@@ -1,5 +1,17 @@
 package nextstep.ladder;
 
 public enum  PointType {
-    VERTICAL_LINE, HORIZONTAL_LINE, EMPTY
+    VERTICAL_LINE("|"),
+    HORIZONTAL_LINE("-----"),
+    EMPTY("     ");
+
+    private String text;
+
+    PointType(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
 }
