5d8e846f1a941e3fe999abac7d13f83701e9bab5
Kim YoonHo <kimunoya@gmail.com>
2019-11-11 00:28:50 +0900

    style: 가독성을 위한 줄바꿈

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -28,7 +28,11 @@ public class Lambda {
     }
 
     public static int sum(List<Integer> numbers, Predicate<Integer> predicate) {
-        return numbers.stream().filter(predicate).mapToInt(Integer::intValue).sum();
+        return numbers
+                .stream()
+                .filter(predicate)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
     public static int sumAll(List<Integer> numbers) {
