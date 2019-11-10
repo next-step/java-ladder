dc27fe1ca65ae7f1842dc734194ae1eb96c2e4c6
naheenosaur <log.info.527@gmail.com>
2019-11-10 13:18:21 +0900

    refactor : 유효성 검증, 입출력 문구/매직넘버 분리

--- src/main/java/ladder/game/LadderGame.java
+++ src/main/java/ladder/game/LadderGame.java
@@ -3,13 +3,37 @@ package ladder.game;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 public class LadderGame {
+    private static final String DELIMITER = ",";
+    private static final int PARTICIPANT_NAME_MAX_LENGTH = 5;
+    private static final String PARTICIPANT_NAME_MAX_LENGTH_EXCEPTION = "참여자의 이름은 %d글자를 넘을 수 없습니다";
+    private static final int PARTICIPANT_MIN_SIZE = 2;
+    private static final String PARTICIPANTS_MIN_SIZE_EXCEPTION = "참여자는 최소 %d명 입니다.";
+
     private Ladder ladder;
     private List<String> participants;
 
     public LadderGame(String participants, int ladderHeight) {
-        this.participants = Arrays.asList(participants.replace(" ", "").split(","));
+        this.participants = verifyParticipants(participants);
         this.ladder = new Ladder(this.participants.size(), ladderHeight);
+    }
+
+    private List<String> verifyParticipants(String inputParticipants) {
+        List<String> participants
+                = Arrays.stream(inputParticipants.split(DELIMITER))
+                .map(participant -> participant.trim())
+                .collect(toList());
+        if (participants.stream().anyMatch(participant -> participant.length() > 5)) {
+            throw new IllegalArgumentException
+                    (String.format(PARTICIPANT_NAME_MAX_LENGTH_EXCEPTION, PARTICIPANT_NAME_MAX_LENGTH));
+        }
+        if (participants.size() < PARTICIPANT_MIN_SIZE) {
+            throw new IllegalArgumentException
+                    (String.format(PARTICIPANTS_MIN_SIZE_EXCEPTION, PARTICIPANT_MIN_SIZE));
+        }
+        return participants;
     }
 
     public Ladder getLadder() {
--- src/main/java/ladder/view/ResultView.java
+++ src/main/java/ladder/view/ResultView.java
@@ -7,19 +7,28 @@ import java.util.Collections;
 import java.util.List;
 
 public class ResultView {
+    private static final String RESULT = "실행결과";
+    private static final String EMPTY = "";
+    private static final String SINGLE_SPACE = " ";
+    private static final String DOUBLE_SPACE = "  ";
+    private static final String VERTICAL = "|";
+    private static final String HORIZON = "-----";
+    private static final String EMPTY_VERTICAL = "     ";
+    private static final int NAME_SPACE_SIZE = 6;
+
     public static void show(LadderGame ladderGame) {
-        System.out.println("실행결과");
+        System.out.println(RESULT);
         showParticipant(ladderGame.getParticipants());
         drawLadder(ladderGame.getLadder());
     }
 
     private static void showParticipant(List<String> participants) {
         for (String participant : participants) {
-            int left = (6 - participant.length()) / 2;
-            int right = 6 - participant.length() - left;
-            System.out.print(String.join("", Collections.nCopies(left, " ")));
+            int left = (NAME_SPACE_SIZE - participant.length()) / 2;
+            int right = NAME_SPACE_SIZE - participant.length() - left;
+            System.out.print(String.join(EMPTY, Collections.nCopies(left, SINGLE_SPACE)));
             System.out.print(participant);
-            System.out.print(String.join("", Collections.nCopies(right, " ")));
+            System.out.print(String.join(EMPTY, Collections.nCopies(right, SINGLE_SPACE)));
         }
         System.out.println();
     }
@@ -27,17 +36,16 @@ public class ResultView {
     private static void drawLadder(Ladder ladder) {
         boolean[][] ladderTable = ladder.getLadder();
         for (boolean[] line : ladderTable) {
-            System.out.print("  |");
+            System.out.print(DOUBLE_SPACE + VERTICAL);
             for (boolean value : line) {
                 if (value) {
-                    System.out.print("-----|");
+                    System.out.print(HORIZON + VERTICAL);
                 } else {
-                    System.out.print("     |");
+                    System.out.print(EMPTY_VERTICAL + VERTICAL);
                 }
             }
-            System.out.println();
+            System.out.println(EMPTY);
         }
-
-        System.out.println();
+        System.out.println(EMPTY);
     }
 }
