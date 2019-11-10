780b00d7d4036332c6a6954e44f42773b089244a
vagabond95 <progjh@gmail.com>
2019-11-10 20:05:57 +0900

    [feat] point type 추가

--- src/main/java/nextstep/ladder/Point.java
+++ src/main/java/nextstep/ladder/Point.java
@@ -2,4 +2,9 @@ package nextstep.ladder;
 
 public class Point {
 
+    private final PointType pointType;
+
+    public Point(PointType pointType) {
+        this.pointType = pointType;
+    }
 }
---
+++ src/main/java/nextstep/ladder/PointType.java
@@ -0,0 +1,5 @@
+package nextstep.ladder;
+
+public enum  PointType {
+    VERTICAL_LINE, HORIZONTAL_LINE, EMPTY
+}
