27b09cc3db11db80075b585a4d7da7fec19a3550
Kim YoonHo <kimunoya@gmail.com>
2019-11-11 00:21:50 +0900

    conflicts merge

--- src/main/java/nextstep/optional/Users.java
+++ src/main/java/nextstep/optional/Users.java
@@ -12,8 +12,11 @@ public class Users {
             new User("pobi", 30),
             new User("jk", 40),
             new User("honux", 45));
-
+    
     User getUser(String name) {
-        return users.stream().filter(user -> user.matchName(name)).findFirst().orElse(DEFAULT_USER);
+        return users.stream()
+                .filter(user -> user.matchName(name))
+                .findFirst()
+                .orElse(DEFAULT_USER);
     }
 }
