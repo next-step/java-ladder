49741a18a679db3528f0c79c311729db6e4e5901
Kim YoonHo <kimunoya@gmail.com>
2019-11-11 19:42:07 +0900

    feat: ladder 게임 생성

---
+++ src/main/java/nextstep/ladder/LadderGame.java
@@ -0,0 +1,12 @@
+package nextstep.ladder;
+
+/**
+ * @author : 김윤호
+ * @version : 1.0
+ * @date : 2019-11-11 19:40
+ */
+public class LadderGame {
+    public static void main(String[] args) {
+
+    }
+}
