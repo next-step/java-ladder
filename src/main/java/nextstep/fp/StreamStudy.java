1b90bfedd16d62cc6bdb48cdcf8f70f4abb2ae87
WoohyunLee <whlee0525@gmail.com>
2019-11-11 21:17:09 +0900

    feat (practice stream)

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -11,41 +11,44 @@ import java.util.stream.Collectors;
 
 public class StreamStudy {
 
-	public static long countWords() throws IOException {
-		String contents = new String(Files.readAllBytes(Paths
-				.get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+    public static long countWords() throws IOException {
+        String contents = new String(Files.readAllBytes(Paths
+                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-		long count = 0;
-		for (String w : words) {
-		  if (w.length() > 12) count++;  
-		}
-		return count;
-	}
-	
-	public static void printLongestWordTop100() throws IOException {
-		String contents = new String(Files.readAllBytes(Paths
-				.get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+        long count = 0;
+        for (String w : words) {
+            if (w.length() > 12) count++;
+        }
+        return count;
+    }
 
-		words.stream()
-				.filter(word -> word.length() > 12)
-				.sorted(Comparator.comparing(String::length))
-				.map(word -> word.toLowerCase())
-				.distinct()
-				.limit(100)
-				.forEach(System.out::println);
-	}
+    public static void printLongestWordTop100() throws IOException {
+        String contents = new String(Files.readAllBytes(Paths
+                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-	public static List<Integer> doubleNumbers(List<Integer> numbers) {
-		return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
-	}
+        words.stream()
+                .filter(word -> word.length() > 12)
+                .sorted(Comparator.comparing(String::length))
+                .map(word -> word.toLowerCase())
+                .distinct()
+                .limit(100)
+                .forEach(System.out::println);
+    }
 
-	public static long sumAll(List<Integer> numbers) {
-		return numbers.stream().reduce(0, (x, y) -> x + y);
-	}
+    public static List<Integer> doubleNumbers(List<Integer> numbers) {
+        return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
+    }
 
-	public static long sumOverThreeAndDouble(List<Integer> numbers) {
-		return 0;
-	}
+    public static long sumAll(List<Integer> numbers) {
+        return numbers.stream().reduce(0, (x, y) -> x + y);
+    }
+
+    public static long sumOverThreeAndDouble(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(number -> number > 3)
+                .map(number -> number * 2)
+                .reduce(0, (a, b) -> a + b);
+    }
 }
