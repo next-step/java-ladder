188760f3825e52aebbf5f89c27e0f130c9f0d2e4
Jae Eun Na <jae-eun.na@linecorp.com>
2019-11-11 23:55:21 +0900

    test pass

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,7 +13,8 @@ public class StreamStudy {
 
     public static long countWords() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+                                                                .get("src/main/resources/fp/war-and-peace.txt")),
+                                     StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         long count = 0;
@@ -23,11 +25,16 @@ public class StreamStudy {
     }
 
     public static void printLongestWordTop100() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+        String contents = new String(Files.readAllBytes(Paths.get("src/main/resources/fp/war-and-peace.txt")),
+                                     StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
-
-        // TODO 이 부분에 구현한다.
+        words.parallelStream()
+             .filter(word -> word.length() > 12)
+             .distinct()
+             .sorted(Comparator.comparingInt(String::length).reversed())
+             .limit(100)
+             .map(String::toLowerCase)
+             .forEachOrdered(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +46,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                      .filter(integer -> integer > 3)
+                      .map(i -> i * 2)
+                      .reduce(0, Integer::sum);
     }
 }
