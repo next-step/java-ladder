212a5f0e96d251b747dceb0f63bcec9ed8e5d193
naheenosaur <log.info.527@gmail.com>
2019-11-12 22:10:31 +0900

    refactor : connectionStrategy 변수 제거

--- src/main/java/ladder/structure/LineOfLadder.java
+++ src/main/java/ladder/structure/LineOfLadder.java
@@ -7,25 +7,23 @@ import java.util.List;
 
 public class LineOfLadder {
     private List<Boolean> connections;
-    private ConnectionStrategy connectionStrategy;
 
     public LineOfLadder(int ladderWidth, ConnectionStrategy connectionStrategy) {
         this.connections = new ArrayList<>();
-        this.connectionStrategy = connectionStrategy;
-        createLine(ladderWidth);
+        createLine(ladderWidth, connectionStrategy);
     }
 
     public boolean isConnected(int index) {
         return connections.get(index);
     }
 
-    private void createLine(int ladderWidth) {
+    private void createLine(int ladderWidth, ConnectionStrategy connectionStrategy) {
         for (int width = 0; width < ladderWidth; width++) {
-            connections.add(createConnection());
+            connections.add(createConnection(connectionStrategy));
         }
     }
 
-    private boolean createConnection() {
+    private boolean createConnection(ConnectionStrategy connectionStrategy) {
         int connectedCount = connections.size();
         if (connectedCount != 0 && connections.get(connectedCount - 1)) {
             return false;
