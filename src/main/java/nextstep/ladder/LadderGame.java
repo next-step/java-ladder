5deff35f42a229eeccf8bfce3e2e81610efbc320
Kim YoonHo <kimunoya@gmail.com>
2019-11-12 20:36:52 +0900

    feat: 사다리 게임을 실행하는 메인 생성

--- src/main/java/nextstep/ladder/LadderGame.java
+++ src/main/java/nextstep/ladder/LadderGame.java
@@ -1,5 +1,11 @@
 package nextstep.ladder;
 
+import nextstep.ladder.domain.Ladder;
+import nextstep.ladder.domain.LadderBridge;
+import nextstep.ladder.view.LadderInputView;
+
+import java.util.List;
+
 /**
  * @author : 김윤호
  * @version : 1.0
@@ -7,6 +13,9 @@ package nextstep.ladder;
  */
 public class LadderGame {
     public static void main(String[] args) {
-
+        List<String> names = LadderInputView.inputNames();
+        Ladder ladder = new Ladder(names.size(), LadderInputView.inputLadderHeight());
+        LadderInputView.showUsers(names);
+        LadderInputView.showLadder(ladder.showLadder());
     }
 }
