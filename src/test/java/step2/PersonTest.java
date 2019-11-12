f3e1aa11640496dad2950a44e37d88d84366bf87
임완호 <wanho.im@kt.com>
2019-11-12 16:34:49 +0900

    feat: 이름 최대 5글자까지 입력

--- src/test/java/step2/PersonTest.java
+++ src/test/java/step2/PersonTest.java
@@ -1,4 +1,22 @@
 package step2;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
 public class PersonTest {
+    @Test
+    void 이름_5글자() {
+        Person person = new Person("abcd");
+
+        assertThat(person.getLength()).isEqualTo(4);
+    }
+
+    @Test
+    void 이름_5글자_초과에러() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+           new Person("abcdef");
+        });
+    }
 }
