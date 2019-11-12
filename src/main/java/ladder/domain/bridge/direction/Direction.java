776d13c1f92a87302274d3cb9e88d89e055f28d4
chanin <pci2676@gmail.com>
2019-11-12 23:40:13 +0900

    java-ladder step3 - docs 예상 요구사항 정리

--- src/main/java/ladder/domain/bridge/direction/Direction.java
+++ src/main/java/ladder/domain/bridge/direction/Direction.java
@@ -8,7 +8,6 @@ public enum Direction {
     Direction() {
     }
 
-
     public static Direction getDirection(WayGenerator wayGenerator) {
         return wayGenerator.getWay() ? RIGHT : DOWN;
     }
