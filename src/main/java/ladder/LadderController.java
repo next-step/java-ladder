20ef25a9730454a91160be75f9ca802f35a60921
liquidjoo <liquidjoo@gmail.com>
2019-11-13 18:49:26 +0900

    feat : 결과값 그리기

--- src/main/java/ladder/LadderController.java
+++ src/main/java/ladder/LadderController.java
@@ -2,11 +2,13 @@ package ladder;
 
 import ladder.domain.Lines;
 import ladder.view.InputView;
+import ladder.view.ResultView;
 
 public class LadderController {
     public void execute() {
         String person = InputView.inputName();
         int height = InputView.inputLadderHeight();
-//        Lines lines = new Lines()
+        Lines lines = new Lines(height, person);
+        ResultView.print(lines);
     }
 }
--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -1,26 +1,16 @@
 package ladder.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Line {
-    private static final String LADDER_LINE = "|";
     private Points points;
 
     public Line(int countOfPerson) {
         this.points = new Points(countOfPerson, new PointsShuffleGenerator());
     }
 
-
-    public void test() {
-        List<Boolean> ladderPoints = this.points.getPoints();
-        for (Boolean ladderPoint : ladderPoints) {
-            if (ladderPoint) {
-                System.out.print(LADDER_LINE + "-----");
-            } else {
-                System.out.print(LADDER_LINE + "     ");
-            }
-        }
-        System.out.print(LADDER_LINE);
+    public List<Boolean> getPoints() {
+        return new ArrayList<>(points.getPoints());
     }
-
 }
