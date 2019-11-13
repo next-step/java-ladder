176fbf3e68a3981e6d4738f1e64f223c10b63f28
cwpark723 <chiwon7942@naver.com>
2019-11-13 22:10:39 +0900

    (refactor) map, reduce, filter 실습2

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,6 +26,14 @@ public class StreamStudy {
 		String contents = new String(Files.readAllBytes(Paths
 				.get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
 		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+
+		words.stream()
+				.filter(w -> w.length() > 12)
+                .sorted(Comparator.comparing(String::length).reversed())
+				.distinct()
+                .limit(100)
+                .map(String::toLowerCase)
+				.forEach(System.out::println);
 	}
 
 	public static List<Integer> doubleNumbers(List<Integer> numbers) {
