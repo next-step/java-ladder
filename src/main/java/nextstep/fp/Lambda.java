6e363a938a42b41fd2d007d3e855733428bce567
guraband <guraband@hanmail.net>
2019-11-16 21:46:37 +0900

    Step1 (#284)
    * 람다 실습 2 - 람다를 활용해 중복 제거
    
    * 다양한 stream method 실습
    
    * Optional

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -5,52 +5,43 @@ import java.util.List;
 public class Lambda {
 	public static void printAllOld(List<Integer> numbers) {
 		System.out.println("printAllOld");
-		
+
 		for (int number : numbers) {
-		    System.out.println(number);
+			System.out.println(number);
 		}
 	}
 
 	public static void printAllLambda(List<Integer> numbers) {
 		System.out.println("printAllLambda");
-		
+
 		numbers.forEach(System.out::println);
 	}
-	
+
 	public static void runThread() {
 		new Thread(new Runnable() {
-		    @Override
-		    public void run() {
-		        System.out.println("Hello from thread");
-		    }
+			@Override
+			public void run() {
+				System.out.println("Hello from thread");
+			}
 		}).start();
 	}
 
-	public static int sumAll(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	        total += number;
-	    }
-	    return total;
+	public static int sum(List<Integer> numbers, Conditional conditional) {
+		int total = 0;
+		return numbers.stream()
+				.filter(conditional::test)
+				.reduce(total, Integer::sum);
 	}
-	
+
+	public static int sumAll(List<Integer> numbers) {
+		return sum(numbers, number -> true);
+	}
+
 	public static int sumAllEven(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	        if (number % 2 == 0) {
-	            total += number;
-	        }
-	    }
-	    return total;
+		return sum(numbers, number -> number % 2 == 0);
 	}
 
 	public static int sumAllOverThree(List<Integer> numbers) {
-	    int total = 0;
-	    for (int number : numbers) {
-	    	if (number > 3) {
-	    		total += number;
-	    	}
-	    }
-	    return total;
+		return sum(numbers, number -> number > 3);
 	}
 }
