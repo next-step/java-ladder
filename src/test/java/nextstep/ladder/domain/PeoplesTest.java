2a400954a83e0ff9a5fff56d77571d2ea1fddb50
dave915 <htaehyun@gmail.com>
2019-11-17 00:39:10 +0900

    [test] getPeoplesByResultName 에 대한 테스트 보강

--- src/test/java/nextstep/ladder/domain/PeoplesTest.java
+++ src/test/java/nextstep/ladder/domain/PeoplesTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PeoplesTest {
     private Peoples peoples;
@@ -26,5 +29,29 @@ public class PeoplesTest {
     void getPeople() {
         People expectedPeople = new People("pobi");
         assertThat(peoples.peopleByIndex(0)).isEqualTo(expectedPeople);
+    }
+
+    @Test
+    @DisplayName("입력받은 이름에 따른 Peoples를 리턴 하는지 확인")
+    void getPeoplesByResultName() {
+        Peoples allPeoples = peoples.getResultPeoples("all");
+        assertThat(allPeoples.size()).isEqualTo(4);
+        for (int i = 0; i < peoples.size(); i++) {
+            assertThat(peoples.peopleByIndex(i).toString()).isIn("pobi", "honux", "crong", "jk");
+        }
+
+        Peoples pobi = peoples.getResultPeoples("pobi");
+        assertThat(pobi.size()).isEqualTo(1);
+        for (int i = 0; i < pobi.size(); i++) {
+            assertThat(pobi.peopleByIndex(i).toString()).isEqualTo("pobi");
+        }
+    }
+
+    @Test
+    @DisplayName("참여 명단에 없는 이름에 따른 에러를 발생 하는지 확이 ")
+    void getPeoplesByResultNameNoSuch() {
+        assertThatThrownBy(() -> {
+            peoples.getResultPeoples("test");
+        }).isInstanceOf(NoSuchElementException.class);
     }
 }
