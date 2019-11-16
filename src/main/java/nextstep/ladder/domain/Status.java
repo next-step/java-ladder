f0f0fe62be06d84c12cd13e07f12cfa7408f1a2a
WoohyunLee <whlee0525@gmail.com>
2019-11-16 23:44:02 +0900

    feat (create Status)

---
+++ src/main/java/nextstep/ladder/domain/Status.java
@@ -0,0 +1,47 @@
+package nextstep.ladder.domain;
+
+import java.util.Objects;
+
+public class Status {
+
+    private boolean left;
+    private boolean right;
+
+    private Status(boolean left, boolean right) {
+        if (left && right) {
+            throw new IllegalArgumentException();
+        }
+        this.left = left;
+        this.right = right;
+    }
+
+    public static Status ofFisrt(boolean next) {
+        return new Status(false, next);
+    }
+
+    public Status createNext(boolean next) {
+        return right ? new Status(right, false) : new Status(right, next);
+    }
+
+    public Status createLast() {
+        return new Status(right, false);
+    }
+
+    public int move() {
+        return left ? -1 : right ? 1 : 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Status status = (Status) o;
+        return left == status.left &&
+                right == status.right;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
+    }
+}
