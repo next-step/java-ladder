92a70ec3f13918ae9150da34a9ce28618e507d95
dave915 <htaehyun@gmail.com>
2019-11-17 00:26:14 +0900

    [refector] DisplayName 안붙어 있던 부분 추가

--- src/test/java/nextstep/ladder/domain/PointTest.java
+++ src/test/java/nextstep/ladder/domain/PointTest.java
@@ -1,16 +1,19 @@
 package nextstep.ladder.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class PointTest {
     @Test
+    @DisplayName("첫번째 라인에서 생성 할 Point 테스트")
     void firstIndex() {
         assertThat(Point.firstIndex()).isIn(new Point(0, Direction.RIGHT), new Point(0, Direction.BOTTOM));
     }
 
     @Test
+    @DisplayName("Point에서 옆 Line 같은 높이에 대한 Point를 생성한다.")
     void next() {
         Point point = new Point(0, Direction.RIGHT);
         assertThat(point.next(1)).isEqualTo(new Point(1, Direction.LEFT));
