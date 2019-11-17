5682809110dde61b44ad9aeb325c098ef68f96c7
vagabond95 <progjh@gmail.com>
2019-11-17 18:09:27 +0900

    [refactor] 사용되지 않는 코드 삭제

--- src/main/java/nextstep/ladder/domain/Point.java
+++ src/main/java/nextstep/ladder/domain/Point.java
@@ -44,24 +44,4 @@ public class Point {
 
         return index;
     }
-
-    /*public static Point createRandomlyHorizontalLine(RandomBooleanProvider randomBooleanProvider) {
-        return new Point(randomBooleanProvider.get() ? HORIZONTAL_LINE : EMPTY);
-    }
-
-    public static Point createHorizontalLine() {
-        return new Point(HORIZONTAL_LINE);
-    }
-
-    public static Point createEmpty() {
-        return new Point(EMPTY);
-    }
-
-    public static Point createVerticalLine() {
-        return new Point(VERTICAL_LINE);
-    }*/
-
-    /*public boolean hasConnection() {
-        return pointType == HORIZONTAL_LINE;
-    }*/
 }
