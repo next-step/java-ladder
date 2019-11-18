5d45631c5d542319922b37bdb64fe06e0a282fb5
seungeuncho <chose@woowahan.com>
2019-11-19 02:24:20 +0900

    feat Solve task with optional

--- src/main/java/nextstep/optional/ComputerStore.java
+++ src/main/java/nextstep/optional/ComputerStore.java
@@ -1,26 +1,32 @@
 package nextstep.optional;
 
+import java.util.Optional;
 import nextstep.optional.Computer.Soundcard;
 import nextstep.optional.Computer.USB;
 
 public class ComputerStore {
-	public static final String UNKNOWN_VERSION = "UNKNOWN";
-	
-	public static String getVersion(Computer computer) {
-		String version = UNKNOWN_VERSION;
-		if(computer != null){
-		  Soundcard soundcard = computer.getSoundcard();
-		  if(soundcard != null){
-		    USB usb = soundcard.getUsb();
-		    if(usb != null){
-		      version = usb.getVersion();
-		    }
-		  }
-		}
-		return version;
-	}
-	
-	public static String getVersionOptional(Computer computer) {
-		return null;
-	}
+
+    public static final String UNKNOWN_VERSION = "UNKNOWN";
+
+    public static String getVersion(Computer computer) {
+        String version = UNKNOWN_VERSION;
+        if (computer != null) {
+            Soundcard soundcard = computer.getSoundcard();
+            if (soundcard != null) {
+                USB usb = soundcard.getUsb();
+                if (usb != null) {
+                    version = usb.getVersion();
+                }
+            }
+        }
+        return version;
+    }
+
+    public static String getVersionOptional(Computer computer) {
+        return Optional.ofNullable(computer)
+            .map(Computer::getSoundcard)
+            .map(Soundcard::getUsb)
+            .map(USB::getVersion)
+            .orElse(UNKNOWN_VERSION);
+    }
 }
