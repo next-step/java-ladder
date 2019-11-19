f0bfabc7c514c4a62b622c0b3d14429fbe9659d1
dave915 <40193460+dave915@users.noreply.github.com>
2019-11-19 13:40:48 +0900

    [Step4] 사다리(리팩토링) (#288)
    * [refector] DisplayName 안붙어 있던 부분 추가
    
    * [test] GiftTest 이름 길이 체크 테스트 추가
    
    * [test] getPeoplesByResultName 에 대한 테스트 보강

--- src/test/java/nextstep/ladder/domain/LineTest.java
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -1,6 +1,7 @@
 package nextstep.ladder.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -18,16 +19,19 @@ public class LineTest {
     }
 
     @Test
+    @DisplayName("line의 height 리턴값 확인")
     void height() {
         assertThat(line.height()).isEqualTo(list.size());
     }
 
     @Test
+    @DisplayName("height에 맞는 Direction 리턴 하는지 확인")
     void direction() {
         assertThat(line.getDirectionByHeight(1)).isEqualTo(Direction.RIGHT);
     }
 
     @Test
+    @DisplayName("height에 맞는 Point 리턴 하는지 확인")
     void point() {
         assertThat(line.getPointByHeight(1)).isEqualTo(new Point(0, Direction.RIGHT));
     }
--- src/test/java/nextstep/ladder/domain/LinesTest.java
+++ src/test/java/nextstep/ladder/domain/LinesTest.java
@@ -1,6 +1,7 @@
 package nextstep.ladder.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -22,16 +23,19 @@ public class LinesTest {
     }
 
     @Test
+    @DisplayName("index와 height를 넘겼을때 해당 위치의 Direction을 리턴 하는지 확인")
     void getDirection() {
         assertThat(lines.getDirection(0, 0)).isEqualTo(Direction.RIGHT);
     }
 
     @Test
+    @DisplayName("index와 height를 넘겼을때 해당 위치의 Point를 리턴 하는지 확인")
     void getPoint() {
         assertThat(lines.getPoint(0, 0)).isEqualTo(new Point(0, Direction.RIGHT));
     }
 
     @Test
+    @DisplayName("이전 Line 기준으로 알맞는 Point가 생성되는지 확인")
     void createPoint() {
         assertThat(lines.createPoint(0, 0)).isIn(new Point(0, Direction.RIGHT), new Point(0, Direction.BOTTOM));
         assertThat(lines.createPoint(lines.getLastIndex(), 2)).isEqualTo(new Point(lines.getLastIndex(), Direction.BOTTOM));
