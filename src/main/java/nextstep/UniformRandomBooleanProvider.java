cf2a09b932860a106a2e48877eea2fbfdfd5de7b
vagabond95 <progjh@gmail.com>
2019-11-21 23:45:24 +0900

    [refactor] UniformRandomBooleanProvider 클래스 인스턴스를 싱글톤으로 생성하도록 변경

--- src/main/java/nextstep/UniformRandomBooleanProvider.java
+++ src/main/java/nextstep/UniformRandomBooleanProvider.java
@@ -4,6 +4,19 @@ import nextstep.ladder.RandomBooleanProvider;
 
 public class UniformRandomBooleanProvider implements RandomBooleanProvider {
 
+    private static UniformRandomBooleanProvider INSTANCE;
+
+    public static UniformRandomBooleanProvider getInstance() {
+        if (INSTANCE == null) {
+            synchronized (UniformRandomBooleanProvider.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new UniformRandomBooleanProvider();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
     @Override
     public boolean get() {
         return Math.random() < 0.5;
