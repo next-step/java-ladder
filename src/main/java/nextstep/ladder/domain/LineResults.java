9912eae7b58b8663cae35d64ecf048bba07beb68
vagabond95 <progjh@gmail.com>
2019-11-22 01:29:24 +0900

    [feat] LineResults 테스트 코드 추가

--- src/main/java/nextstep/ladder/domain/LineResults.java
+++ src/main/java/nextstep/ladder/domain/LineResults.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class LineResults {
 
+    private static final String INVALID_POSITION = "결과를 가져올 수 없습니다. 위치를 다시 확인해주세요.";
     private final List<String> lineResults;
 
     public LineResults(List<String> results) {
@@ -12,10 +13,17 @@ public class LineResults {
     }
 
     public String match(int position) {
+        assertPositionRange(position);
         return lineResults.get(position);
     }
 
     public List<String> getValue() {
         return Collections.unmodifiableList(lineResults);
+    }
+
+    private void assertPositionRange(int position) {
+        if (position < 0 || position >= lineResults.size()) {
+            throw new IllegalArgumentException(INVALID_POSITION);
+        }
     }
 }
---
+++ src/test/java/nextstep/ladder/LineResultTest.java
@@ -0,0 +1,23 @@
+package nextstep.ladder;
+
+import nextstep.ladder.domain.LineResults;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class LineResultTest {
+
+    @Test
+    void positionRangeTest() {
+        LineResults lineResults = new LineResults(Collections.singletonList("100, 200, 꽝"));
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            lineResults.match(-1);
+        });
+
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            lineResults.match(3);
+        });
+    }
+}
