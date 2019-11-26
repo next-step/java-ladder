5ea47d2c761e6b08e94e0b5a668a831d3099dee3
Kim YoonHo <kimunoya@gmail.com>
2019-11-27 02:52:17 +0900

    feat: 사다리 그리기 기능 추가
    - 비즈니스 로직에서 View에서 처리하도록 수정

--- src/main/java/nextstep/ladder/view/LadderResultView.java
+++ src/main/java/nextstep/ladder/view/LadderResultView.java
@@ -1,7 +1,7 @@
 package nextstep.ladder.view;
 
 import nextstep.ladder.domain.Ladder;
-import nextstep.ladder.domain.LadderBridge;
+import nextstep.ladder.domain.LadderLine;
 import nextstep.ladder.domain.LadderRequireElement;
 import org.apache.commons.lang3.StringUtils;
 
@@ -16,21 +16,14 @@ import java.util.stream.Collectors;
 public class LadderResultView {
 
     private static final int MINIMUM_MAXIMUM_VALUE = 5;
+    private static final int FIRST_LINE_INDEX = 0;
     private static final String BLANK = " ";
-
-    private static void printLadderLine(List<LadderBridge> line) {
-        line.forEach(ladderLine -> System.out.print(ladderLine.getBridge()));
-    }
+    private static final String EMPTY = "    ";
+    private static final String BRIDGE = "|-----";
+    private static final String EMPTY_PIPE = "|     ";
 
     private static String leftPadBlank(String value) {
         return StringUtils.leftPad(value, MINIMUM_MAXIMUM_VALUE, BLANK);
-    }
-
-    private static void showLadder(List<List<LadderBridge>> showLadder) {
-        showLadder.forEach(line -> {
-            printLadderLine(line);
-            System.out.println();
-        });
     }
 
     private static void showList(List<String> names) {
@@ -41,14 +34,38 @@ public class LadderResultView {
         System.out.println(result);
     }
 
+    private static void showLadderLines(List<LadderLine> ladderLines) {
+        ladderLines.forEach(line -> {
+            showLine(line);
+        });
+    }
+
+    private static void showLine(LadderLine line) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int lineNo = 0; lineNo < line.getPointOfLine().size(); lineNo++) {
+            if (lineNo == FIRST_LINE_INDEX) {
+                stringBuilder.append(EMPTY);
+            }
+
+            stringBuilder.append(getBridgeOrEmptyPipe(lineNo, line.getPointOfLine().get(lineNo)));
+        }
+        System.out.println(stringBuilder.toString());
+    }
+
+    private static String getBridgeOrEmptyPipe(int lineNo, Boolean point) {
+        if (point) {
+            return BRIDGE;
+        }
+        return EMPTY_PIPE;
+    }
+
     public static void showLadder(LadderRequireElement ladderRequireElement, Ladder ladder) {
         showList(ladderRequireElement.getParticipant());
-        showLadder(ladder.showLadder());
+        showLadderLines(ladder.getLadderLines());
         showList(ladderRequireElement.getExecutionResult());
     }
 
     public static void showLadderResult(List<String> result) {
         result.forEach(System.out::println);
     }
-
 }
