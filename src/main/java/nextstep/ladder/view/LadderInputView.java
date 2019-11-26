0f695bf21e39a4bd2c48a25650a0eb8bfa96d22e
Kim YoonHo <kimunoya@gmail.com>
2019-11-27 02:59:06 +0900

    fix: 누락된 split 추가

--- src/main/java/nextstep/ladder/view/LadderInputView.java
+++ src/main/java/nextstep/ladder/view/LadderInputView.java
@@ -22,7 +22,7 @@ public class LadderInputView {
 
     public static List<String> inputNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        String inputNames = StringUtils.deleteWhitespace(SCANNER.nextLine());
+        String[] inputNames = StringUtils.deleteWhitespace(SCANNER.nextLine()).split(",");
         return Arrays.asList(inputNames);
     }
 
