a68829eb2b91fbd38c28eb1f2f6fb00b1d1fa87e
WoohyunLee <whlee0525@gmail.com>
2019-11-27 23:12:02 +0900

    refactor (change validCheck order)

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -5,14 +5,28 @@ import java.util.List;
 
 public class Ladder {
 
+    private final int STEP_MIN_NUM = 1;
+
     private List<Line> ladder;
 
     public Ladder(int totalStep, int spotSize) {
+        this.ladder = generateLadder(totalStep, spotSize);
+    }
+
+    private List<Line> generateLadder(int totalStep, int spotSize) {
+        if (!isValidTotalStep(totalStep)) {
+            throw new IllegalArgumentException("사다리 높이는 1층 이상이어야 합니다");
+        }
+
         List<Line> ladder = new ArrayList<>();
         for (int i = 0; i < totalStep; i++) {
             ladder.add(new Line(spotSize));
         }
-        this.ladder = ladder;
+        return ladder;
+    }
+
+    private boolean isValidTotalStep(int totalStep) {
+        return (totalStep >= STEP_MIN_NUM);
     }
 
     public int calculateArrival(int departure) {
