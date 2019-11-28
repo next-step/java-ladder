aa489a3aadfb0e58810ead11d02db99c3bc7fb43
liquidjoo <liquidjoo@gmail.com>
2019-11-28 10:35:26 +0900

    refactor : 사용하지 않는 패키지 정리

--- src/main/java/ladder/domain/LadderGenerator.java
+++
@@ -1,8 +0,0 @@
-package ladder.domain;
-
-import java.util.List;
-
-@FunctionalInterface
-public interface LadderGenerator {
-    List<Line> generate();
-}
--- src/main/java/ladder/domain/LineGenerator.java
+++
@@ -1,6 +0,0 @@
-package ladder.domain;
-
-@FunctionalInterface
-public interface LineGenerator {
-    Line generate(int countOfPerson);
-}
