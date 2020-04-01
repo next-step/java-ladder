fbc6db1ee6e00dfc805c68b21439fd06cb444634
Yeoseon Yun <devyyskr@gmail.com>
2020-04-01 22:25:20 +0900

    refactor(Conditional): 인터페이스 생성

--- src/main/java/nextstep/fp/Conditional.java
+++ src/main/java/nextstep/fp/Conditional.java
@@ -1,11 +1,5 @@
 package nextstep.fp;
 
 public interface Conditional {
-    // 변경되는 부분
-    private static int sum(int total, int number) {
-        if(number > 3) {
-            total += number;
-        }
-        return total;
-    }
+    boolean isSumTarget(Integer number);
 }
