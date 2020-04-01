a454172db24c0e295368c8dd3d73dda5f4d66856
teahyuk <jtaeh57@gmail.com>
2020-04-01 22:28:22 +0900

    optional 실습 1

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,7 +35,11 @@ public class User {
     }
 
     public static boolean ageIsInRange2(User user) {
-        return false;
+        return Optional.ofNullable(user)
+                .map(User::getAge)
+                .filter(old->old>=30)
+                .filter(old->old<=45)
+                .isPresent();
     }
 
     @Override
