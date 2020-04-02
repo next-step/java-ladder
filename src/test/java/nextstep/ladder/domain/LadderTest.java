d8a71bfd93de1046050325be82c3ac2e360000a3
dongchul <sah3122@naver.com>
2020-04-02 20:39:54 +0900

    test(Ladder): add ladder test case

---
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -0,0 +1,29 @@
+package nextstep.ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LadderTest {
+
+    @DisplayName("높이에 맞는 라인 생성")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void createLine(int height) {
+        Line line = Line.of(height);
+
+        assertThat(line.getHeight()).isEqualTo(height);
+    }
+
+    @DisplayName("가로 라인 생성")
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5})
+    void createStep(int position) {
+        Step step = Step.of(position);
+
+        assertThat(step.getPosition()).isEqualTo(position);
+    }
+}
