11e28b6e6b1cd480cb39ffee555bf700b5dc2188
crystal <crystal2840@gmail.com>
2020-04-03 00:09:14 +0900

    StreamStudy 클래스의 printLongestWordTop100() 메서드를 구현

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,7 +30,12 @@ public class StreamStudy {
                                      StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+             .filter(w -> w.length() > 12)
+             .sorted(Comparator.comparing(String::length).reversed())
+             .distinct()
+             .map(String::toLowerCase)
+             .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -41,6 +47,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return numbers.stream().filter(x -> x > 3).map(x -> 2 * x).reduce(0, Integer::sum);
+        return numbers.stream()
+                      .filter(x -> x > 3)
+                      .map(x -> 2 * x)
+                      .reduce(0, Integer::sum);
     }
 }
