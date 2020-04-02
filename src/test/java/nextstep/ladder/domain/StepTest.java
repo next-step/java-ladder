350d6e8736a62ba60d2ec9d23a0165c7db75d726
dongchul <sah3122@naver.com>
2020-04-03 00:14:23 +0900

    test(Step): add step case

---
+++ src/test/java/nextstep/ladder/domain/StepTest.java
@@ -0,0 +1,18 @@
+package nextstep.ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StepTest {
+    @DisplayName("가로 라인 생성")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    void createStep(int position) {
+        Step step = Step.of(position);
+
+        assertThat(step.getPosition()).isEqualTo(position);
+    }
+}
