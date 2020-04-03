5c007c39ce031e775c01ecc106e2f3424a82fe26
LeeSeungHee <palmseung@gmail.com>
2020-04-04 02:34:14 +0900

    Feat (Height) : add feature to validate Height value is positive

--- src/main/java/ladder/Messages.java
+++ src/main/java/ladder/Messages.java
@@ -7,4 +7,5 @@ public class Messages {
     public static final String WARNING_NULL_OR_EMPTY = "공백 또는 빈 문자열은 입력할 수 없습니다.";
     public static final String WARNING_NOT_NUMBER_TYPE = "숫자만 입력 가능합니다.";
     public static final String WARNING_NOT_ALLOWED_NAME_LENGTH = "플레이어의 이름은 다섯 글자를 넘을 수 없습니다.";
+    public static final String WARNING_NOT_ALLOWED_HEIGHT_LENGTH= "사다리의 높이는 0보다 커야 합니다.";
 }
--- src/main/java/ladder/model/Height.java
+++ src/main/java/ladder/model/Height.java
@@ -1,12 +1,20 @@
 package ladder.model;
 
+import static ladder.Messages.WARNING_NOT_ALLOWED_HEIGHT_LENGTH;
+
 public class Height {
+    private static int ZERO = 0;
+
     private int height;
 
-    public Height(int input) {
+    public Height(int height) {
+        validatePositive(height);
+        this.height = height;
     }
 
-    public int getHeight() {
-        return height;
+    private void validatePositive(int input) {
+        if (input <= ZERO) {
+            throw new IllegalArgumentException(WARNING_NOT_ALLOWED_HEIGHT_LENGTH);
+        }
     }
 }
