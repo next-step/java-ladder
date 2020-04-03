27b00bbcc8f11c91e1a9188cdf1068ad4f8ec1a8
dongchul <sah3122@naver.com>
2020-04-04 00:06:01 +0900

    test(Ladder): add ladder test case

--- src/test/java/nextstep/ladder/domain/LadderTest.java
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -5,16 +5,23 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LadderTest {
     @DisplayName("사다리 정상 생성")
     @Test
-    void name() {
+    void createLadder() {
+        int heightOfLine = 10;
+        List<String> personNames = Arrays.asList("dong", "chul");
+        List<Person> people = personNames.stream()
+                .map(Person::new)
+                .collect(Collectors.toList());
+        Ladder ladder = Ladder.of(people, heightOfLine);
 
-        Ladder ladder = Ladder.of(heightOfLine, personNames);
-
-        assertThat(ladder.getLines()).hasSize(personNames.size());
-        assertThat(ladder.getHeightOfLine()).isEqualTo(heightOfLine);
+        assertThat(ladder.getHeightOfLadder()).isEqualTo(heightOfLine);
     }
 }
