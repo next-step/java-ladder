8923ca2a4e15a669cecee023bc28b06da191fa0a
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-03 22:28:37 +0900

    feat (Lines.java) : 일급 컬렉션 생성

---
+++ src/main/java/ladder/domain/Lines.java
@@ -0,0 +1,12 @@
+package ladder.domain;
+
+import java.util.List;
+
+public class Lines {
+
+    private final List<Line> lines;
+
+    public Lines(final List<Line> lines) {
+        this.lines = lines;
+    }
+}
