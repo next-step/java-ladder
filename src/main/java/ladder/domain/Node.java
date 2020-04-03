e43032b5da706d37f69d4616492dafc8611aaff3
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-03 21:04:13 +0900

    feat (Way.java) : Node 마다 이동할 길을 정해주는 Way 추가

--- src/main/java/ladder/domain/Node.java
+++ src/main/java/ladder/domain/Node.java
@@ -3,8 +3,11 @@ package ladder.domain;
 public class Node {
 
     private final int index;
+    private final Way way;
 
-    public Node(int index) {
+
+    public Node(int index, Way way) {
         this.index = index;
+        this.way = way;
     }
 }
