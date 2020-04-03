4cc2aa6be5d2c4c1397263324b146ca351531893
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-03 21:18:30 +0900

    feat (Way.java) : 이동 방향 클래스

--- src/main/java/ladder/domain/Way.java
+++ src/main/java/ladder/domain/Way.java
@@ -1,4 +1,12 @@
 package ladder.domain;
 
 public class Way {
+
+    private final boolean left;
+    private final boolean right;
+
+    public Way(boolean left, boolean right) {
+        this.left = left;
+        this.right = right;
+    }
 }
