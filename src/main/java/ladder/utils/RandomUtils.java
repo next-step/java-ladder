8d78fb6ac22fa95e82abfca9af0bafe820724cdc
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-03 22:45:05 +0900

    feat (RandomUtils.java) : 랜덤값 생성 유틸 제작

---
+++ src/main/java/ladder/utils/RandomUtils.java
@@ -0,0 +1,17 @@
+package ladder.utils;
+
+import java.util.Random;
+
+public final class RandomUtils {
+
+    private static final String NON_CREATE_MSG = "유틸 객체 생성 불가";
+
+    private RandomUtils() {
+        throw new IllegalArgumentException(NON_CREATE_MSG);
+    }
+
+    public boolean getRandomBoolean() {
+        Random random = new Random();
+        return random.nextBoolean();
+    }
+}
