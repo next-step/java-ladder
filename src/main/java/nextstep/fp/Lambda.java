3a49c5eba94bdf39f9f4c9cdf5bae8e1b943fcfa
Jeongwon LEE <jwee0330@gmail.com>
2020-04-03 21:47:48 +0900

    Step1 리뷰 요청드립니다. (#311)
    * test(CarTest): 인터페이스를 람다표현식으로 변경
    
    * test(Lambda): Lambda 연습
    
    * test(Computer): Optional 연습
    
    * test(Expression): Optional 연습
    
    * test(User): Optional 연습
    
    * test(Users): Optional 연습

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -6,9 +6,7 @@ public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
         System.out.println("printAllOld");
 
-        for (int number : numbers) {
-            System.out.println(number);
-        }
+        numbers.forEach(System.out::println);
     }
 
     public static void printAllLambda(List<Integer> numbers) {
@@ -18,39 +16,34 @@ public class Lambda {
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> System.out.println("Hello from thread")).start();
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return numbers.stream()
+                .filter(Lambda::isEvenNumber)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return numbers.stream()
+                .filter(Lambda::isOverThree)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    private static boolean isEvenNumber(int number) {
+        return number % 2 == 0;
+    }
+
+    private static boolean isOverThree(int number) {
+        return number > 3;
     }
 }
