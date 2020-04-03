18d43b5bae219208705ec78fd4d1b8fb8b73b79e
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-03 23:14:12 +0900

    feat (Line.java) : 다음 노드 추가 기능, 테스트

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -1,13 +1,25 @@
 package ladder.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Line {
 
     private final List<Node> nodes;
 
+    public Line() {
+        this(new ArrayList<>());
+    }
+
     public Line(final List<Node> nodes) {
         this.nodes = nodes;
+    }
+
+    public Line addNode(Node node) {
+        List<Node> merge = new ArrayList<>();
+        merge.addAll(this.nodes);
+        merge.add(node);
+        return new Line(merge);
     }
 
     public int size() {
--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class LineTest {
 
     @DisplayName("생성자 테스트")
@@ -16,6 +18,17 @@ class LineTest {
                 Arrays.asList(new Node(0, Way.right()),
                         new Node(1, Way.right())));
         Line line = new Line(nodes);
+    }
 
+    @Test
+    public void addNode() throws Exception {
+        //given
+        Line line = new Line();
+        Node node = new Node(1, Way.left());
+        //when
+        line = line.addNode(node);
+
+        //then
+        assertThat(line.size()).isEqualTo(1);
     }
 }
