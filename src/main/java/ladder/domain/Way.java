d174d569a23bcd6f0cd24cb5a88d1e3afac91532
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-03 21:31:47 +0900

    feat (Way.java) : 생성자 validate 추가, 테스트

--- src/main/java/ladder/domain/Way.java
+++ src/main/java/ladder/domain/Way.java
@@ -1,12 +1,21 @@
 package ladder.domain;
 
+import ladder.exception.LadderException;
+
 public class Way {
 
     private final boolean left;
     private final boolean right;
 
-    public Way(boolean left, boolean right) {
+    public Way(final boolean left, final boolean right) {
+        validate(left, right);
         this.left = left;
         this.right = right;
+    }
+
+    private void validate(final boolean left, final boolean right) {
+        if (left && right) {
+            throw new LadderException("양방향 이동은 불가");
+        }
     }
 }
--- src/test/java/ladder/domain/WayTest.java
+++ src/test/java/ladder/domain/WayTest.java
@@ -1,7 +1,29 @@
 package ladder.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import ladder.exception.LadderException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class WayTest {
 
+    @DisplayName("생성자 정상 테스트")
+    @Test
+    public void constructor_success() throws Exception {
+        //then
+        assertThat(new Way(true, false));
+        assertThat(new Way(false, true));
+        assertThat(new Way(false, false));
+    }
+
+    @DisplayName("양방향 이동은 불가능 하다")
+    @Test
+    public void validate_fail() throws Exception {
+        //then
+        assertThatThrownBy(
+                () -> new Way(true, true)
+        ).isInstanceOf(LadderException.class);
+    }
 }
