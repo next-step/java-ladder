fe799a8e515731b63acd72e9f1b3208b19eb6610
ohtaeg <otk1090@naver.com>
2020-04-05 03:05:41 +0900

    test (Ladder) : 높이당 라인을 가지는지 테스트

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -6,13 +6,26 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LadderTest {
-    @DisplayName("높이를 가지는 사다리 객체 생성")
+
+    @DisplayName("높이를 가지는 사다리 객체를 생성할 수 있다.")
     @Test
     void createLadder() {
-        Height height = Height.of("1");
+        final Height height = Height.of("1");
+        Ladder expect = new Ladder(height);
 
         Ladder actual = new Ladder(height);
 
-        assertThat(actual.height()).isEqualTo(height);
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    @DisplayName("높이당 하나의 라인을 가진다.")
+    @Test
+    void createLadder2() {
+        Height height = Height.of("2");
+        Ladder expect = new Ladder(height);
+
+        Ladder actual = new Ladder(height);
+
+        assertThat(actual.lines()).isEqualTo(expect.lines());
     }
 }
