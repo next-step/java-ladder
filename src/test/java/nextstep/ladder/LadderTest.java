b1c484ff54c929caaad8e605d0cd299de3935f40
Yeoseon Yun <devyyskr@gmail.com>
2020-04-04 21:29:29 +0900

    refactor(PersonTest): 메소드명 변경

---
+++ src/test/java/nextstep/ladder/LadderTest.java
@@ -0,0 +1,9 @@
+package nextstep.ladder;
+
+import org.junit.jupiter.api.Test;
+
+public class LadderTest {
+    @Test
+    void name() {
+    }
+}
--- src/test/java/nextstep/ladder/PersonTest.java
+++ src/test/java/nextstep/ladder/PersonTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 public class PersonTest {
     @Test
     @DisplayName("이름을 입력했을 떄, 5자가 넘으면 Exception을 발생한다.")
-    void personValidate() {
+    void assertPersonNameLength() {
         String name = "yeoseon";
 
         assertThatIllegalArgumentException().isThrownBy(() -> {
