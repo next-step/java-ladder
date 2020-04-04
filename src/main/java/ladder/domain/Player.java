8fbace93432983781d09446d6e397a8d526f3260
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-04 10:18:44 +0900

    feat (PlayerException.java) : 참가자 관련 exception 생성

--- src/main/java/ladder/domain/Player.java
+++ src/main/java/ladder/domain/Player.java
@@ -3,8 +3,14 @@ package ladder.domain;
 public class Player {
     private final String name;
 
-    public Player(String name) {
+    public Player(final String name) {
         this.name = name;
+    }
+
+    private void validateName(final String name) {
+        if (name.length() > 5) {
+
+        }
     }
 
     public String getName() {
