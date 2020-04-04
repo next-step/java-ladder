fc0f8deaa22b59295c8033c0fcfe3477bd42bae6
ohtaeg <otk1090@naver.com>
2020-04-05 03:06:10 +0900

    refactor (Ladder) : 높이당 Line을 가지도록 메서드 추가

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -1,13 +1,36 @@
 package ladder.domain;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Ladder {
     private Height height;
+    private List<Line> lines;
 
     public Ladder(final Height height) {
+        this.lines = createLines(height);
         this.height = height;
     }
 
-    public Height height() {
-        return height;
+    public List<Line> lines() {
+        return lines;
+    }
+
+    private List<Line> createLines(final Height height) {
+        return null;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Ladder ladder = (Ladder) o;
+        return Objects.equals(height, ladder.height) &&
+                Objects.equals(lines, ladder.lines);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(height, lines);
     }
 }
