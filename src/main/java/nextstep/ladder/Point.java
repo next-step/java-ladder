f4896fd1a07030eff64cd0a4fc99b088dd4e03d8
crystal <crystal2840@gmail.com>
2020-04-04 22:37:37 +0900

    포인트 객체 다시작성

--- src/main/java/nextstep/ladder/Point.java
+++ src/main/java/nextstep/ladder/Point.java
@@ -3,19 +3,10 @@ package nextstep.ladder;
 import java.util.Objects;
 
 public class Point {
-    private final int point;
     private boolean nextPoint = false;
-
-    public Point(int point) {
-        this.point = point;
-    }
 
     public void moveNextPoint() {
         nextPoint = true;
-    }
-
-    public int getPoint() {
-        return point;
     }
 
     public boolean isNextPoint() {
@@ -26,13 +17,12 @@ public class Point {
     public boolean equals(Object o) {
         if (this == o) { return true; }
         if (!(o instanceof Point)) { return false; }
-        final Point point1 = (Point) o;
-        return getPoint() == point1.getPoint() &&
-               isNextPoint() == point1.isNextPoint();
+        final Point point = (Point) o;
+        return isNextPoint() == point.isNextPoint();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getPoint(), isNextPoint());
+        return Objects.hash(isNextPoint());
     }
 }
--- src/test/java/nextstep/ladder/PointTest.java
+++ src/test/java/nextstep/ladder/PointTest.java
@@ -8,24 +8,18 @@ import org.junit.jupiter.api.Test;
 
 class PointTest {
 
-    @DisplayName("포인트를 생성해보고 같은객체인지 확인해본다.")
-    @Test
-    void createPoint() {
-        Point point = new Point(0);
-        assertThat(point).isEqualTo(new Point(0));
-    }
 
     @DisplayName("포인트에서 다음 포인트로 이동하지 않으면 false를 반환한다")
     @Test
     void stopPoint() {
-        Point point = new Point(1);
+        Point point = new Point();
         assertThat(point.isNextPoint()).isFalse();
     }
 
     @DisplayName("포인트에서 다음 포인트로 이동할건지(선을 그으면) true를 반환한다.")
     @Test
     void movePoint() {
-        Point point = new Point(1);
+        Point point = new Point();
         point.moveNextPoint();
         assertThat(point.isNextPoint()).isTrue();
     }
