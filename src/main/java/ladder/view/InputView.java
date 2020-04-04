95cea8e594934950e992a4541ead88ce153a872f
ohtaeg <otk1090@naver.com>
2020-04-04 21:53:17 +0900

    refactor (InputView) : 이름을 입력받을시 Player를 반환하도록 변경

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -1,9 +1,13 @@
 package ladder.view;
 
+import ladder.domain.Player;
+
 import java.io.InputStream;
 import java.util.Scanner;
 
 public class InputView {
+    private static final String INPUT_PLAYER_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+
     private Scanner scanner;
 
     public InputView() {
@@ -14,7 +18,8 @@ public class InputView {
         this.scanner = new Scanner(inputStream);
     }
 
-    public String inputPlayerName() {
-        return scanner.nextLine();
+    public Player inputPlayerName() {
+        System.out.println(INPUT_PLAYER_NAMES_MESSAGE);
+        return new Player(scanner.nextLine());
     }
 }
