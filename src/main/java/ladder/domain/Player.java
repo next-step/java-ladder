2c28ad89ef33bcad6fc319df838650de88b679c1
ohtaeg <otk1090@naver.com>
2020-04-04 21:54:15 +0900

    feat (Player) : 이름을 가진 참가자 객체 구현

---
+++ src/main/java/ladder/domain/Player.java
@@ -0,0 +1,24 @@
+package ladder.domain;
+
+import java.util.Objects;
+
+public class Player {
+    private String name;
+
+    public Player(final String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
