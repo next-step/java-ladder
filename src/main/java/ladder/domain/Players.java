e272908373236b9bde836e419104c4be52767d70
ohtaeg <otk1090@naver.com>
2020-04-04 23:18:46 +0900

    feat (Players) : 입력받은 문자열을 람다를 통해 리스트로 변환하도록 구현

---
+++ src/main/java/ladder/domain/Players.java
@@ -0,0 +1,40 @@
+package ladder.domain;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Players {
+    private static final String SEPARATOR = ",";
+
+    private List<Player> players;
+
+    public Players(final List<Player> players) {
+        this.players = Collections.unmodifiableList(players);
+    }
+
+    public Players (final String names) {
+        this.players = Arrays.stream(names.split(SEPARATOR))
+                             .map(Player::new)
+                             .collect(Collectors.toList());
+    }
+
+    public int size() {
+        return players.size();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Players players1 = (Players) o;
+        return Objects.equals(players, players1.players);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(players);
+    }
+}
