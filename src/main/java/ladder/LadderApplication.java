d3700af95db7de82d0a6bcbea6046174ad045f14
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-04 12:51:17 +0900

    feat (LadderApplication.java) : 컨트롤러 동작 추가

--- src/main/java/ladder/LadderApplication.java
+++ src/main/java/ladder/LadderApplication.java
@@ -1,4 +1,18 @@
 package ladder;
 
+import ladder.domain.LadderGame;
+import ladder.service.LadderService;
+import ladder.view.InputView;
+
 public class LadderApplication {
+
+    public static void main(String[] args) {
+        LadderService ladderService = new LadderService();
+
+        String names = InputView.inputNames();
+        String height = InputView.inputHeight();
+
+        LadderGame ladder = ladderService.createLadder(names, height);
+        System.out.println(ladder);
+    }
 }
