34a167d5047b9fbad770b3efb198995262cee552
ohtaeg <otk1090@naver.com>
2020-04-05 03:53:28 +0900

    test (Ladder) : 사다리 객체 생성 테스트 코드 변경

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -3,6 +3,8 @@ package ladder.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LadderTest {
@@ -10,11 +12,11 @@ class LadderTest {
     @DisplayName("참여자 수와 높이를 가지는 사다리 객체를 생성할 수 있다.")
     @Test
     void createLadder() {
-        final int playerCount = 1;
         final Height height = Height.of("1");
+        final int playerCount = 1;
 
-        Ladder actual = new Ladder(playerCount, height);
+        Map<Height, Line> actual = new Ladder(playerCount, height).getLadder();
 
-        assertThat(actual).isNotNull();
+        assertThat(actual.get(height)).isEqualTo(new Line(playerCount));
     }
 }
