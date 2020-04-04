417532c8f255db8caf9678073f821a5e7dd6a401
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-05 00:19:58 +0900

    feat (InputView.java) : 게임 결과 input 기능 추가

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -9,6 +9,8 @@ public class InputView {
     private static final String HEIGHT_INPUT_MSG =
             "최대 사다리 높이는 몇 개인가요?";
     private static final String WHOSE_RESULT = "결과를 보고 싶은 사람은?";
+    private static final String GAME_RESULT =
+            "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
 
     public static String inputNames() {
         Scanner scanner = new Scanner(System.in);
@@ -29,5 +31,12 @@ public class InputView {
         System.out.println(WHOSE_RESULT);
         String height = scanner.nextLine();
         return height;
+    }
+
+    public static String inputGameResult() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(GAME_RESULT);
+        String result = scanner.nextLine();
+        return result;
     }
 }
