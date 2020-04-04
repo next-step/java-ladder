c5c1703eecb25ecfed0048bd9df71a3af4fd7bc4
ohtaeg <otk1090@naver.com>
2020-04-04 23:00:58 +0900

    test (Players) : 여러명의 참여자를 생성하는 테스트 추가

---
+++ src/test/java/ladder/domain/PlayersTest.java
@@ -0,0 +1,25 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PlayersTest {
+
+    @DisplayName("여러명의 참여자를 생성한다.")
+    @Test
+    void create() {
+        List<Player> playerList = Arrays.asList(new Player("pobi"), new Player("honux"));
+        Players expect = new Players(playerList);
+
+        Players actual = new Players(playerList);
+
+        assertThat(actual).isEqualTo(expect);
+        assertThat(actual.size()).isEqualTo(2);
+    }
+
+}
