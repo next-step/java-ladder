7bef6c2148fc4de026a0efbbb070742ee02cfeb6
ohtaeg <otk1090@naver.com>
2020-04-04 21:38:47 +0900

    test (InputViewTest) : 입력을 테스트 할 수 있도록 생성자에 InputStream 의존성 주입하도록 변경

--- src/test/java/ladder/view/InputViewTest.java
+++ src/test/java/ladder/view/InputViewTest.java
@@ -1,8 +1,9 @@
 package ladder.view;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,18 +11,18 @@ class InputViewTest {
 
     private InputView inputView;
 
-    @BeforeEach
-    void setUp() {
-    }
-
     @DisplayName("참여자 한명의 이름을 입력받는다.")
     @Test
     void inputPlayerNames() {
         String expect = "pobi";
-        inputView = new InputView();
+        inputView = new InputView(createInputStream(expect));
 
         String actual = inputView.inputPlayerName();
 
         assertThat(actual).isEqualTo(expect);
+    }
+
+    ByteArrayInputStream createInputStream(String name) {
+        return new ByteArrayInputStream(name.getBytes());
     }
 }
