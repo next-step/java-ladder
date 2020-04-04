3d340a153cea96490b6b5e40cceab488c193f09f
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-05 00:08:39 +0900

    feat (LadderGameResult.java) : 게임 결과 first class collection

---
+++ src/main/java/ladder/domain/LadderGameResult.java
@@ -0,0 +1,12 @@
+package ladder.domain;
+
+import java.util.List;
+
+public class LadderGameResult {
+
+    private final List<String> result;
+
+    public LadderGameResult(List<String> result) {
+        this.result = result;
+    }
+}
