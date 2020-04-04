f6295691ac2e2143319f8031e9371d69e5796143
luvram <sahul0804@gmail.com>
2020-04-04 20:17:51 +0900

    refactor: 2depth 이상의 코드를 분리함

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -1,35 +1,47 @@
 package nextstep.ladder.domain;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Ladder {
-    private List<Line> ladder;
+    // todo 어떻게 인자수를 줄이지?
+    private List<HorizontalLine> ladder;
     private RightDirection rightDirection;
-    public Ladder(int width, int height, RightDirection rightDirection) {
+    private LineSelector lineSelector;
+
+    // todo 받아야 하는 인자가 너무 많다.
+    public Ladder(int width, int height, RightDirection rightDirection,
+                  LineSelector lineSelector) {
         this.rightDirection = rightDirection;
+        this.lineSelector = lineSelector;
         initLadder(width, height);
     }
 
     private void initLadder(int width, int height) {
-        ladder = new ArrayList<>();
-        // todo refactor
-        for(int i = 0; i < height; i++) {
-            ladder.add(new Line(width, rightDirection));
-        }
-        // todo refactor
-        for(int i = 0; i < width; i++ ) {
-            boolean hasRightDirection =
-                    vertical(i).stream().anyMatch(Point::hasRightDirection);
+        ladder = IntStream.range(0, height)
+                .mapToObj(i -> new HorizontalLine(width, rightDirection))
+                .collect(Collectors.toList());
 
-            // todo refactor
-            if(!hasRightDirection) {
-                int adjustIndex = (new Random()).nextInt(height);
-                ladder.get(adjustIndex).reverse(i);
-            }
+        adjustLadder();
+    }
+
+    private void adjustLadder() {
+        int width = ladder.get(0).size();
+        for (int i = 0; i < width; i++) {
+            adjustLadder(i);
         }
+    }
+
+    private void adjustLadder(int index) {
+        boolean hasRightDirection = vertical(index).stream()
+                .anyMatch(Point::hasRightDirection);
+        if (hasRightDirection) {
+            return;
+        }
+        int selectedIndex = lineSelector.select(ladder.size());
+        HorizontalLine horizontalLine = ladder.get(selectedIndex);
+        horizontalLine.reverse(index);
     }
 
     public int height() {
@@ -38,7 +50,7 @@ public class Ladder {
 
     public List<Point> vertical(int index) {
         return ladder.stream()
-                .map(line -> line.getPoint(index))
+                .map(horizontalLine -> horizontalLine.getPoint(index))
                 .collect(Collectors.toList());
     }
 }
--- src/test/java/nextstep/ladder/domain/LadderTest.java
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -8,9 +8,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LadderTest {
     private RightDirection rightDirection;
+    private LineSelector lineSelector;
     @BeforeEach
     void setUp() {
         rightDirection = () -> true;
+        lineSelector = (size) -> 1;
     }
 
     @DisplayName("입력받은 높이만큼의 사다리가 만들어진다.")
@@ -18,7 +20,7 @@ public class LadderTest {
     void height() {
         int height = 5;
         int width = 4;
-        Ladder ladder = new Ladder(width, height, rightDirection);
+        Ladder ladder = new Ladder(width, height, rightDirection, lineSelector);
         assertThat(ladder.height()).isEqualTo(height);
     }
 
@@ -28,7 +30,7 @@ public class LadderTest {
         int height = 5;
         int width = 4;
         RightDirection rightDirection = () -> false;
-        Ladder ladder = new Ladder(width, height, rightDirection);
+        Ladder ladder = new Ladder(width, height, rightDirection, lineSelector);
 
         boolean hasRightDirection = ladder.vertical(0).stream()
                 .anyMatch(Point::hasRightDirection);
