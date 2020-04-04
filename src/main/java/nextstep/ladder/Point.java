848fe746db75647777b10863033b41047c6965f4
Yeoseon Yun <devyyskr@gmail.com>
2020-04-04 22:03:42 +0900

    feat(Point): Point 객체 생성

---
+++ src/main/java/nextstep/ladder/Point.java
@@ -0,0 +1,13 @@
+package nextstep.ladder;
+
+public class Point {
+    private int x;
+    private int y;
+    private boolean hasLine;
+
+    public Point(int x, int y, boolean hasLine) {
+        this.x = x;
+        this.y = y;
+        this.hasLine = hasLine;
+    }
+}
