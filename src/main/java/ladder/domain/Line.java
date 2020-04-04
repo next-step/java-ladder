dcc5d49e9614a182cf5aef4c4df4e83eef6ea30b
ohtaeg <otk1090@naver.com>
2020-04-05 03:57:17 +0900

    refactor (Line) : 참여자 수 -1 만큼 라인 생성 여부 생성하도록 변경

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -1,16 +1,35 @@
 package ladder.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class Line {
     private List<Boolean> line;
 
     public Line(int countOfPerson) {
-        this.line = new ArrayList<>(countOfPerson);
+        List<Boolean> lines = new ArrayList<>(countOfPerson);
+        for (int i = Player.PLAYER_MIN_COUNT; i < countOfPerson; i++) {
+            lines.add(true);
+        }
+        this.line = Collections.unmodifiableList(lines);
     }
 
     public List<Boolean> getLine() {
         return line;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Line line1 = (Line) o;
+        return Objects.equals(line, line1.line);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(line);
     }
 }
