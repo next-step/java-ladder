a4863bdd5c6319fe5f171f16c03ce22b5b7c61c8
feel-coding-republic <62166873+feel-coding-republic@users.noreply.github.com>
2020-04-04 18:56:00 +0900

    Step1 리뷰요청드립니다. (#319)
    * feat:실습1구현
    
    * feat:stream method 실습구현
    
    * feat:구현완료
    
    Co-authored-by: feel-coding-republic <feel.coding.republic@gmail.com>

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,8 +35,11 @@ public class User {
     }
 
     public static boolean ageIsInRange2(User user) {
-        return false;
+        return Optional.ofNullable(user)
+                .filter(userFilter -> user.getAge() != null && user.getAge() >= 30 && user.getAge() <= 45)
+                .isPresent();
     }
+
 
     @Override
     public int hashCode() {
