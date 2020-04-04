2d02496935fb7385440a49b662fcc30b287b45cf
Yeoseon Yun <devyyskr@gmail.com>
2020-04-05 04:08:22 +0900

    test(Ladder): Test가 통과하도록 변경 사항 수정

--- src/test/java/nextstep/ladder/LadderGameTest.java
+++ src/test/java/nextstep/ladder/LadderGameTest.java
@@ -1,28 +1,17 @@
 package nextstep.ladder;
 
-import nextstep.ladder.controller.LadderGame;
 import nextstep.ladder.domain.Ladder;
-import nextstep.ladder.domain.Participant;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LadderGameTest {
     @Test
-    @DisplayName("입력받은 참가자 이름(width)과 사다리 높이(height) 만큼의 사다리가 생성되는지 확인한다.")
+    @DisplayName("입력받은 참가자 이름(width - 1)과 사다리 높이(height) 만큼의 사다리가 생성되는지 확인한다.")
     void createLadder() {
-        int height = 5;
-        List<Participant> participants = new ArrayList<>();
-        participants.add(new Participant("pobi"));
-        participants.add(new Participant("honux"));
-        participants.add(new Participant("crong"));
-        participants.add(new Participant("jk"));
+        Ladder ladder = new Ladder(4, 5);
 
-        Ladder ladder = LadderGame.createLadder(participants, height);
         assertThat(ladder.getWidth()).isEqualTo(3);
         assertThat(ladder.getHeight()).isEqualTo(5);
     }
--- src/test/java/nextstep/ladder/LadderTest.java
+++ src/test/java/nextstep/ladder/LadderTest.java
@@ -17,27 +17,20 @@ public class LadderTest {
     @Test
     @DisplayName("사다리 높이가 1보다 작을 경우 Exception 처리를 한다.")
     void assertLadderHeight() {
-        List<Participant> participants = new ArrayList<>();
-        participants.add(new Participant("pobi"));
-        participants.add(new Participant("honux"));
-        participants.add(new Participant("crong"));
-        participants.add(new Participant("jk"));
         int height = 0;
 
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            LadderGame.createLadder(participants, height);
+            new Ladder(4, height);
         }).withMessage(Ladder.LADDER_HEIGHT_ERROR);
     }
 
     @Test
     @DisplayName("사다리의 Point(참여자 수)는 2명 이상이어야 한다.")
     void assertLadderWidth() {
-        List<Participant> participants = new ArrayList<>();
-        participants.add(new Participant("pobi"));
-        int height = 5;
+        int participantCount = 1;
 
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            LadderGame.createLadder(participants, height);
+            new Ladder(participantCount, 5);
         }).withMessage(Line.POINT_COUNT_ERROR);
     }
 }
