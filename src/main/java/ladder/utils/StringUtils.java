951fa52ac1ea75531ddd0559ba17af4186abb816
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-04 12:44:48 +0900

    feat (StringUtils.java) : 문자열을 int형으로 변환 하는 유틸 추가, 테스트 추가

--- src/main/java/ladder/utils/StringUtils.java
+++ src/main/java/ladder/utils/StringUtils.java
@@ -1,5 +1,7 @@
 package ladder.utils;
 
+import ladder.exception.LadderException;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,6 +9,7 @@ import java.util.stream.Collectors;
 public final class StringUtils {
 
     private static final String NON_CREATE_MSG = "유틸 객체 생성 불가";
+    private static final String NON_NUMERIC_MSG = "숫자로 변환할 수 없습니다.";
 
     private StringUtils() {
         throw new IllegalArgumentException(NON_CREATE_MSG);
@@ -16,5 +19,13 @@ public final class StringUtils {
         return Arrays.stream(input.split(delimiter))
                 .map(s -> s.trim())
                 .collect(Collectors.toList());
+    }
+
+    public static int stringToInt(final String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new LadderException(NON_NUMERIC_MSG);
+        }
     }
 }
--- src/test/java/ladder/utils/StringUtilsTest.java
+++ src/test/java/ladder/utils/StringUtilsTest.java
@@ -1,11 +1,16 @@
 package ladder.utils;
 
+import ladder.exception.LadderException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringUtilsTest {
 
@@ -24,5 +29,26 @@ class StringUtilsTest {
         assertThat(result.get(1)).isEqualTo("honux");
         assertThat(result.get(2)).isEqualTo("crong");
         assertThat(result.get(3)).isEqualTo("jk");
+    }
+
+    @DisplayName("문자열을 숫자로 변환")
+    @ParameterizedTest
+    @CsvSource(value = {"1:1", "2:2", "10:10"}, delimiter = ':')
+    public void stringToInt_success(String input, int expect) throws Exception {
+        //given
+        int num = StringUtils.stringToInt(input);
+
+        //then
+        assertThat(num).isEqualTo(expect);
+    }
+
+    @DisplayName("문자열을 숫자로 변환")
+    @ParameterizedTest
+    @ValueSource(strings = {"1a", "2.2", "a", "?"})
+    public void stringToInt_fail(String input) throws Exception {
+        //then
+        assertThatThrownBy(
+                () -> StringUtils.stringToInt(input)
+        ).isInstanceOf(LadderException.class);
     }
 }
