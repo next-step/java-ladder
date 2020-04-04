637598d67d17b2422ad4e2597b5bb8940e3e250e
Yeoseon Yun <devyyskr@gmail.com>
2020-04-04 21:33:58 +0900

    test(LadderTest): 사다리 높이가 검증 테스트 메소드 추가

--- src/test/java/nextstep/ladder/LadderTest.java
+++ src/test/java/nextstep/ladder/LadderTest.java
@@ -1,9 +1,17 @@
 package nextstep.ladder;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LadderTest {
     @Test
-    void name() {
+    @DisplayName("사다리 높이가 1보다 작을 경우 Exception 처리를 한다.")
+    void assertLadderHeight() {
+        int height;
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Ladder(height);
+        }).withMessage("사다리 높이는 1보다 커야합니다.")
     }
 }
