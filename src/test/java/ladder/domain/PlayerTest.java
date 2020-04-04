2186a9beb7ff1fc747156f442ded0c0a99c96423
ohtaeg <otk1090@naver.com>
2020-04-04 22:33:39 +0900

    test (Player) : 이름이 5글자 넘기면 예외를 발생하는 테스트 추가

--- src/test/java/ladder/domain/PlayerTest.java
+++ src/test/java/ladder/domain/PlayerTest.java
@@ -1,6 +1,9 @@
 package ladder.domain;
 
+import ladder.exception.InvalidNameException;
+import ladder.exception.NameLengthOutOfBoundException;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -13,8 +16,17 @@ class PlayerTest {
     @DisplayName("참여자 이름이 null 이거나 공백일경우 커스텀 예외를 발생한다.")
     @ParameterizedTest
     @MethodSource("invalidNames")
-    void validNameLength(String name) {
-        assertThatExceptionOfType(RuntimeException.class).isThrownBy(
+    void validName(String name) {
+        assertThatExceptionOfType(InvalidNameException.class).isThrownBy(
+                () ->  new Player(name)
+        );
+    }
+
+    @DisplayName("참여자 이름이 5글자를 넘기면 커스텀 예외를 발생한다.")
+    @Test
+    void validNameLength() {
+        String name = "ohtaeg";
+        assertThatExceptionOfType(NameLengthOutOfBoundException.class).isThrownBy(
                 () ->  new Player(name)
         );
     }
