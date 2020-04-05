078b7fb0bcf2571fd572780665d568f68dbf27a1
dongchul <sah3122@naver.com>
2020-04-05 21:25:19 +0900

    refactor(Height): make height to vo

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -5,15 +5,23 @@ import java.util.List;
 
 public class Ladder {
     private final List<Line> lines;
-    private final int heightOfLadder;
+    private final Height heightOfLadder;
 
     public Ladder(List<Line> lines, int heightOfLadder) {
+        validateLine(lines);
+
         this.lines = Collections.unmodifiableList(lines);
-        this.heightOfLadder = heightOfLadder;
+        this.heightOfLadder = new Height(heightOfLadder);
+    }
+
+    private void validateLine(List<Line> lines) {
+        if (lines.size() < 2) {
+            throw new IllegalArgumentException("라인은 두개 이상이여야 합니다.");
+        }
     }
 
     public int getHeightOfLadder() {
-        return heightOfLadder;
+        return heightOfLadder.getHeight();
     }
 
     public List<Line> getLines() {
