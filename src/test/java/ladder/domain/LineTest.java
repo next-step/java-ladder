5fc586fc7f302765babc2bfed92f6e75241cbd0b
ohtaeg <otk1090@naver.com>
2020-04-05 23:45:24 +0900

    test (Line) : 랜덤 생성으로 인해 테스트 코드 제거

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -19,43 +19,4 @@ class LineTest {
 
         assertThat(actual.get(0)).isFalse();
     }
-
-    @DisplayName("참여자가 2명이라면 라인을 생성할 수 있다.")
-    @Test
-    void createLine_TwoPlayer() {
-        int playerCount = 2;
-        Line line = new Line(playerCount);
-
-        List<Boolean> actual = line.getLine();
-
-        assertThat(actual.get(0)).isFalse();
-        assertThat(actual.get(1)).isTrue();
-    }
-
-    @DisplayName("참여자가 3명이라면 연속된 라인이 생성되면 안된다.")
-    @Test
-    void createLine_ThreePlayer() {
-        int playerCount = 3;
-        Line line = new Line(playerCount);
-
-        List<Boolean> actual = line.getLine();
-
-        assertThat(actual.get(0)).isFalse();
-        assertThat(actual.get(1)).isTrue();
-        assertThat(actual.get(2)).isFalse();
-    }
-
-    @DisplayName("참여자가 4명이라면 연속된 라인이 생성되면 안된다.")
-    @Test
-    void createLine_FourPlayer() {
-        int playerCount = 4;
-        Line line = new Line(playerCount);
-
-        List<Boolean> actual = line.getLine();
-
-        assertThat(actual.get(0)).isFalse();
-        assertThat(actual.get(1)).isTrue();
-        assertThat(actual.get(2)).isFalse();
-        assertThat(actual.get(3)).isTrue();
-    }
 }
