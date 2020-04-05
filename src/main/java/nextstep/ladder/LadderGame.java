91951a5e841ad783ed4287893ee1c5101a7485cc
dongchul <sah3122@naver.com>
2020-04-05 20:08:37 +0900

    feat(Ladder): add ladder game

---
+++ src/main/java/nextstep/ladder/LadderGame.java
@@ -0,0 +1,49 @@
+package nextstep.ladder;
+
+import nextstep.ladder.domain.Ladder;
+import nextstep.ladder.domain.Line;
+import nextstep.ladder.domain.Person;
+import nextstep.ladder.domain.step.strategy.MovableBothSide;
+import nextstep.ladder.domain.step.strategy.MovablePrev;
+import nextstep.ladder.dto.LadderRequestDto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LadderGame {
+    private static final String DELIMITER = ",";
+
+    public static Ladder run(LadderRequestDto ladderRequestDto) {
+        int heightOfLadder = ladderRequestDto.getHeight();
+        List<Person> persons = namesToPersons(ladderRequestDto);
+
+        List<Line> lines = new ArrayList<>();
+        Line previousLine = Line.first(persons.get(0), heightOfLadder);
+        lines.add(previousLine);
+        generateLines(persons, lines, previousLine);
+        return new Ladder(lines, heightOfLadder);
+    }
+
+    private static void generateLines(List<Person> persons, List<Line> lines, Line previousLine) {
+        for (int i = 1; i < persons.size(); i++) {
+            previousLine = getLine(persons, previousLine, i);
+            lines.add(previousLine);
+        }
+    }
+
+    private static List<Person> namesToPersons(LadderRequestDto ladderRequestDto) {
+        return Arrays.asList(ladderRequestDto.getNames().split(DELIMITER)).stream()
+                .map(Person::new)
+                .collect(Collectors.toList());
+    }
+
+    private static Line getLine(List<Person> persons, Line previousLine, int i) {
+        if (i == persons.size() - 1) {
+            return Line.of(persons.get(i), previousLine.getSteps(), new MovablePrev());
+        }
+        return Line.of(persons.get(i), previousLine.getSteps(), new MovableBothSide());
+    }
+
+}
