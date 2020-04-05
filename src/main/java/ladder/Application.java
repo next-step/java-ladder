ef04543c9925673350bb500c207d7a1ade603bfd
ohtaeg <otk1090@naver.com>
2020-04-05 20:15:46 +0900

    refactor (Application) : 사다 출력 추가

--- src/main/java/ladder/Application.java
+++ src/main/java/ladder/Application.java
@@ -1,8 +1,10 @@
 package ladder;
 
 import ladder.domain.Height;
+import ladder.domain.Ladder;
 import ladder.domain.Players;
 import ladder.view.InputView;
+import ladder.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
@@ -10,5 +12,10 @@ public class Application {
 
         Players players = inputView.inputPlayerName();
         Height height = inputView.inputHeight();
+
+        Ladder ladder = new Ladder(players.count(), height);
+
+        OutputView.printPlayerName(players);
+        OutputView.printLadder(ladder);
     }
 }
