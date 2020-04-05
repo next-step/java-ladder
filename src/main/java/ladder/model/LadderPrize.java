c01d7417e5d65f2d57874ed3c0b2c5b0101a3069
LeeSeungHee <palmseung@gmail.com>
2020-04-05 20:33:20 +0900

    Feat (LadderPrize) : add feature to validate appropriate input

--- src/main/java/ladder/model/LadderPrize.java
+++ src/main/java/ladder/model/LadderPrize.java
@@ -4,17 +4,24 @@ import static ladder.Messages.WARNING_LADDER_PRIZE_NOT_ALLOWED_STRING;
 
 public class LadderPrize {
     private static final String BLANK = "꽝";
+    private static final String ALL = "all";
 
     private String prizeName;
 
     public LadderPrize(String prizeName) {
         validateString(prizeName);
-        validateNumber(prizeName);
+        if(!isAllowedInputPrize(prizeName)){
+            validateNumber(prizeName);
+        }
         this.prizeName = prizeName;
     }
 
+    private boolean isAllowedInputPrize(String input){
+        return BLANK.equals(input) || ALL.equals(input.toLowerCase());
+    }
+
     private void validateString(String input) {
-        if (!BLANK.equals(input)) {
+        if (!isAllowedInputPrize(input)) {
             throw new IllegalArgumentException(WARNING_LADDER_PRIZE_NOT_ALLOWED_STRING);
         }
     }
