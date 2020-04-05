c6a59505318c1c54cf3bf8786329eff937dcdff4
LeeSeungHee <palmseung@gmail.com>
2020-04-05 20:23:06 +0900

    Test (LadderPrize) : add LadderPrizeTest.createTest

--- src/test/java/ladder/LadderPrizeTest.java
+++ src/test/java/ladder/LadderPrizeTest.java
@@ -5,10 +5,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LadderPrizeTest {
-    @DisplayName("꽝이 아닌 글자를 입력하면 예외 발생")
+    @DisplayName("all, 꽝을 입력하면 정상 수행")
+    @ParameterizedTest
+    @ValueSource(strings = {"all", "All", "ALL", "꽝"})
+    void createTest(String input) {
+        assertThatCode(() -> new LadderPrize(input)).doesNotThrowAnyException();
+    }
+
+    @DisplayName("꽝과 all이 아닌 글자를 입력하면 예외 발생")
     @ParameterizedTest
     @ValueSource(strings = {"글자", "이것은 글자"})
     void throwExceptionWhenItsNameAreNotBlank(String input) {
