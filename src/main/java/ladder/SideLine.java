a1f110b132855ae386f4477360625beb76b83cd5
isntyet <isntyet@gmail.com>
2020-04-05 22:42:46 +0900

    feat(step2): SideLine 정적 팩토리 메소드
    SideLine에 정적 팩토리 메소드 추가

--- src/main/java/ladder/SideLine.java
+++ src/main/java/ladder/SideLine.java
@@ -1,13 +1,29 @@
 package ladder;
 
+import java.util.Objects;
+
 public class SideLine {
 
     private final Point leftPoint;
     private final Point rightPoint;
+    private boolean drawing;
 
-    public SideLine(Point leftPoint, Point rightPoint) {
+    public static SideLine ofDrawing(Point leftPoint, Point rightPoint) {
+        return new SideLine(leftPoint, rightPoint, true);
+    }
+
+    public static SideLine ofNotDrawing(Point leftPoint, Point rightPoint) {
+        return new SideLine(leftPoint, rightPoint, false);
+    }
+
+    private SideLine(Point leftPoint, Point rightPoint, boolean drawing) {
         this.leftPoint = leftPoint;
         this.rightPoint = rightPoint;
+        this.drawing = drawing;
+    }
+
+    public boolean isDrawing() {
+        return this.drawing;
     }
 
     public Point getLeftPoint() {
@@ -16,5 +32,19 @@ public class SideLine {
 
     public Point getRightPoint() {
         return rightPoint;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SideLine sideLine = (SideLine) o;
+        return Objects.equals(leftPoint, sideLine.leftPoint) &&
+                Objects.equals(rightPoint, sideLine.rightPoint);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(leftPoint, rightPoint);
     }
 }
--- src/test/java/ladder/SideLineTest.java
+++ src/test/java/ladder/SideLineTest.java
@@ -8,6 +8,6 @@ public class SideLineTest {
     @Test
     @DisplayName("옆줄 생성 테스트")
     void createSideLineTest() {
-        SideLine sideLine = new SideLine(new Point(0, 0), new Point(1, 0));
+        SideLine sideLine = SideLine.ofDrawing(new Point(0, 0), new Point(1, 0));
     }
 }
