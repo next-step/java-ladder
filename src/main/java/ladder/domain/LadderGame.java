8dc5c53495d761a2fed222505afc34b148b101fb
parkeeseul <eeseul.p@gmail.com>
2020-04-05 23:08:57 +0900

    refactor: User 도메인 분리

--- src/main/java/ladder/domain/LadderGame.java
+++ src/main/java/ladder/domain/LadderGame.java
@@ -1,38 +1,19 @@
 package ladder.domain;
 
-import ladder.Exception.ExceptionType;
-import ladder.Exception.LadderException;
 import ladder.utils.LadderGenerator;
 import lombok.Getter;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class LadderGame {
-    private static final String REGEX = ",";
-    private static final int NAME_LENGTH_LIMIT = 5;
-
     @Getter
     private Ladder ladder;
 
     @Getter
-    private List<String> users;
+    private List<User> users;
 
     public LadderGame(String userNameValues, String heightValue) {
-        String[] userNames = userNameValues.split(REGEX);
-
-        validNameLength(Arrays.asList(userNames));
-
-        this.ladder = LadderGenerator.createLadder(userNames.length, Integer.parseInt(heightValue));
-        this.users = Arrays.asList(userNames);
-    }
-
-    private void validNameLength(List<String> userNames) {
-        boolean invalidName = userNames.stream()
-                .anyMatch(userName -> userName.length() > NAME_LENGTH_LIMIT);
-
-        if (invalidName) {
-            throw new LadderException(ExceptionType.INVALID_NAME_SIZE);
-        }
+        this.users = User.listOf(userNameValues);
+        this.ladder = LadderGenerator.createLadder(this.users.size(), Integer.parseInt(heightValue));
     }
 }
---
+++ src/main/java/ladder/domain/User.java
@@ -0,0 +1,40 @@
+package ladder.domain;
+
+import ladder.Exception.ExceptionType;
+import ladder.Exception.LadderException;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class User {
+    private static final int NAME_LENGTH_LIMIT = 5;
+    private static final String REGEX = ",";
+
+    @Getter
+    private String name;
+
+    private User(String name) {
+        this.name = name;
+    }
+
+    public static List<User> listOf(String userNameValues) {
+        String[] userNames = userNameValues.split(REGEX);
+
+        validNameLength(Arrays.asList(userNames));
+
+        return Arrays.stream(userNames)
+                .map(username -> new User(username))
+                .collect(Collectors.toList());
+    }
+
+    private static void validNameLength(List<String> userNames) {
+        boolean invalidName = userNames.stream()
+                .anyMatch(userName -> userName.length() > NAME_LENGTH_LIMIT);
+
+        if (invalidName) {
+            throw new LadderException(ExceptionType.INVALID_NAME_SIZE);
+        }
+    }
+}
