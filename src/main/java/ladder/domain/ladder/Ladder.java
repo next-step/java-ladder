f246a43a89d8424ee2408ec01d8f26ed981c3ec1
teahyuk <jtaeh57@gmail.com>
2020-04-05 12:34:39 +0900

    feat(Ladder) : Ladder 추가.

---
+++ src/main/java/ladder/domain/ladder/Ladder.java
@@ -0,0 +1,33 @@
+package ladder.domain.ladder;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Ladder {
+    private final List<Line> lines;
+
+    private Ladder(List<Line> lines) {
+        this.lines = lines;
+    }
+
+    public static Ladder of(int height, int size) {
+        return Optional.of(height)
+                .filter(h -> h > 0)
+                .map(h -> makeLadder(h,size))
+                .orElseThrow(() -> new IllegalArgumentException(
+                        String.format("Create Ladder failed. height must be over 0: height=%d", height)));
+    }
+
+    private static Ladder makeLadder(int height, int size) {
+        return new Ladder(Stream.iterate(
+                Line.ofCount(size), line -> Line.ofCount(size))
+                .limit(height)
+                .collect(Collectors.toList()));
+    }
+
+    public List<Line> getLines() {
+        return lines;
+    }
+}
