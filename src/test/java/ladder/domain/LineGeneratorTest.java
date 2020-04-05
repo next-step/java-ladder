d946ed177fceb1ef794944d6cdff8d606dd6137f
ohtaeg <otk1090@naver.com>
2020-04-06 02:53:35 +0900

    test (LineGenerator) : 시작 위치에는 라인을 생성할 수 없다.

--- src/test/java/ladder/domain/LineGeneratorTest.java
+++ src/test/java/ladder/domain/LineGeneratorTest.java
@@ -1,6 +1,7 @@
 package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -15,10 +16,19 @@ class LineGeneratorTest {
     @DisplayName("라인을 생성할 수 있다.")
     @ParameterizedTest
     @ValueSource(ints = {2, 3, 4, 5})
-    void generateLine(int playerCount) {
+    void generate(int playerCount) {
         lineGenerator = new LineGenerator(playerCount);
         List<Boolean> lines = lineGenerator.generate();
 
         assertThat(lines.size()).isEqualTo(playerCount);
+    }
+
+    @DisplayName("처음 위치엔 라인을 생성할 수 없다.")
+    @Test
+    void generateStartLine() {
+        lineGenerator = new LineGenerator(1);
+        List<Boolean> lines = lineGenerator.generate();
+
+        assertThat(lines.get(0)).isFalse();
     }
 }
