c01a006af877d9d1bdf9d5586cd6d4d790cdfcf7
ohtaeg <otk1090@naver.com>
2020-04-05 19:35:39 +0900

    refactor (Ladder) : 높이에 따른 라인을 가진 Map을 Line 리스트를 가지도록 변경

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -1,25 +1,25 @@
 package ladder.domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 public class Ladder {
-    private final Map<Height, Line> ladder;
+    private final List<Line> lines;
 
     public Ladder(final int playerCount, final Height height) {
-        this.ladder = createLines(playerCount, height);
+        this.lines = createLines(playerCount, height);
     }
 
-    public Map<Height, Line> getLadder() {
-        return ladder;
+    public List<Line> getLines() {
+        return lines;
     }
 
-    private Map<Height, Line> createLines(final int playerCount, final Height height) {
-        Map<Height, Line> ladder = new HashMap<>();
+    private List<Line> createLines(final int playerCount, final Height height) {
+        List<Line> lines = new ArrayList<>();
         for (int i = Height.MIN_HEIGHT; i <= height.value(); i++) {
-            ladder.put(Height.of(i), new Line(playerCount));
+            lines.add(new Line(playerCount));
         }
-        return Collections.unmodifiableMap(ladder);
+        return Collections.unmodifiableList(lines);
     }
 }
