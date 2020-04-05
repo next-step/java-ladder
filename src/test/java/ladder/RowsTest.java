aeeaa5db23951339ca6eb8ec99da38bb24461c59
LeeSeungHee <palmseung@gmail.com>
2020-04-05 14:29:07 +0900

    Fix (Rows) : add test fixtures

--- src/test/java/ladder/RowsTest.java
+++ src/test/java/ladder/RowsTest.java
@@ -1,9 +1,14 @@
 package ladder;
 
 import ladder.model.Height;
+import ladder.model.Player;
+import ladder.model.Players;
 import ladder.model.Rows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,8 +16,15 @@ public class RowsTest {
     @DisplayName("Rows 객체 생성")
     @Test
     void createTest() {
+        //given
+        List<Player> players = Arrays.asList(
+                new Player("Mark"),
+                new Player("Palm"),
+                new Player("Jyung")
+        );
+
         //when
-        Rows rows = Rows.create(4, new Height(5));
+        Rows rows = Rows.create(Players.create(players), new Height(5));
 
         //then
         assertThat(rows.getRows()).hasSize(5);
