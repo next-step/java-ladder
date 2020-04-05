f46c30e8770adffcc525023a2953ebf5ec62431f
dongchul <sah3122@naver.com>
2020-04-05 21:28:40 +0900

    test(Ladder): refactor ladder test

--- src/test/java/nextstep/ladder/domain/LadderTest.java
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -5,23 +5,47 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LadderTest {
     @DisplayName("사다리 정상 생성")
-    @Test
-    void createLadder() {
-        int heightOfLine = 10;
-        List<String> personNames = Arrays.asList("dong", "chul", "lee");
-        List<Person> people = personNames.stream()
-                .map(Person::new)
-                .collect(Collectors.toList());
-        Ladder ladder = Ladder.of(people, heightOfLine);
+    @ParameterizedTest
+    @ValueSource(ints = 5)
+    void createLadder(int heightOfLadder) {
+        List<Line> lines = Arrays.asList(
+                Line.first(new Person("dong"), heightOfLadder),
+                Line.first(new Person("chul"), heightOfLadder)
+        );
 
-        assertThat(ladder.getHeightOfLadder()).isEqualTo(heightOfLine);
+        Ladder ladder = new Ladder(lines, heightOfLadder);
+        assertThat(ladder.getLines()).containsAll(lines);
+    }
+
+    @DisplayName("라인이 1개 이하 일 경우 생성 실패")
+    @ParameterizedTest
+    @ValueSource(ints = 5)
+    void createFailByLineCount(int heightOfLadder) {
+        List<Line> lines = Arrays.asList(
+                Line.first(new Person("dong"), heightOfLadder)
+        );
+
+        assertThatIllegalArgumentException().isThrownBy(() ->  new Ladder(lines, heightOfLadder));
+    }
+
+    @DisplayName("높이가 1보다 작을 경우 생성 실패")
+    @ParameterizedTest
+    @ValueSource(ints = {-5, -3, 0, 1})
+    void createFailByHeight(int heightOfLadder) {
+        List<Line> lines = Arrays.asList(
+                Line.first(new Person("dong"), heightOfLadder)
+        );
+
+        assertThatIllegalArgumentException().isThrownBy(() ->  new Ladder(lines, heightOfLadder));
     }
 }
