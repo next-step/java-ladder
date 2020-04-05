c31669d64334b97a4d3a051478acc667e9c0b585
ohtaeg <otk1090@naver.com>
2020-04-05 20:13:51 +0900

    feat (OutputView) : 사다리 출력 기능 추가

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -1,21 +1,42 @@
 package ladder.view;
 
-import ladder.domain.Player;
+import ladder.domain.Ladder;
+import ladder.domain.Line;
 import ladder.domain.Players;
+
+import java.util.stream.Collectors;
 
 public class OutputView {
 
     private OutputView() {}
 
     public static void printPlayerName(Players players) {
-        for (Player player : players.getPlayers()) {
-            System.out.print(" ");
-            System.out.print(formatName(player.getName()));
-        }
+        System.out.println("실행결과");
+        System.out.println();
+        players.getPlayers()
+               .stream()
+               .map(player -> formatName(player.getName()))
+               .forEach(System.out::print);
+        System.out.println();
+    }
+
+    public static void printLadder(final Ladder ladder) {
+        ladder.getLines()
+              .stream()
+              .map(OutputView::printLine)
+              .forEach(System.out::println);
+    }
+
+    private static String printLine(final Line line) {
+        return line.getLine()
+                   .stream()
+                   .map(isLine -> isLine ? "-----" : "     ")
+                   .collect(Collectors.joining("|", "", "|"));
     }
 
     // TODO : 테스트를 위한 public 접근 제한자, private로 수정할 것.
     public static String formatName(String name) {
-        return String.format("%5s", name);
+        return String.format("%5s", name) + " ";
     }
+
 }
