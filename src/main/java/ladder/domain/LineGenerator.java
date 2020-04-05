2c3b2a1f8e9329148b5bf75963f7eb05d702778b
ohtaeg <otk1090@naver.com>
2020-04-06 02:54:31 +0900

    refactor (LineGenerator) : 첫 위치는 라인을 생성할 수 없기에 초기화 메서드 구현

--- src/main/java/ladder/domain/LineGenerator.java
+++ src/main/java/ladder/domain/LineGenerator.java
@@ -11,10 +11,16 @@ public class LineGenerator {
     }
 
     public List<Boolean> generate() {
-        final List<Boolean> lines = new ArrayList<>();
-        for (int i = 0; i < playerCount; i++) {
+        final List<Boolean> lines = init();
+        for (int i = lines.size(); i < playerCount; i++) {
             lines.add(false);
         }
+        return lines;
+    }
+
+    private List<Boolean> init() {
+        List<Boolean> lines = new ArrayList<>();
+        lines.add(Boolean.FALSE);
         return lines;
     }
 }
