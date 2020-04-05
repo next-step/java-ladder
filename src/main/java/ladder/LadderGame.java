c0ded7d75b72c6fd9a3b27d06bd6fb7457ad6b67
teahyuk <jtaeh57@gmail.com>
2020-04-05 13:27:28 +0900

    feat(UI) : UI 및 컨트롤러 개발
    - InputView
    - OutputView
    - LadderGame

---
+++ src/main/java/ladder/LadderGame.java
@@ -0,0 +1,23 @@
+package ladder;
+
+import ladder.domain.Gamers;
+import ladder.domain.ladder.Ladder;
+import ladder.ui.InputView;
+import ladder.ui.OutputView;
+
+public class LadderGame {
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+
+    private void start() {
+        Gamers gamers = Gamers.ofComma(inputView.getGamers());
+        Ladder ladder = Ladder.of(inputView.getheight(), gamers.getGamerList().size()-1);
+
+        outputView.printGamers(gamers);
+        outputView.printLadder(ladder);
+    }
+
+    public static void main(String[] args) {
+        new LadderGame().start();
+    }
+}
---
+++ src/main/java/ladder/ui/InputView.java
@@ -0,0 +1,26 @@
+package ladder.ui;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+    private static final String GET_GAMER_MSG = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String GET_HEIGHT_MSG = "최대 사다리 높이는 몇 개 인가요?";
+
+    private final Scanner scanner;
+
+    public InputView() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String getGamers() {
+        System.out.println(GET_GAMER_MSG);
+        return scanner.nextLine();
+    }
+
+    public int getheight() {
+        System.out.println(GET_HEIGHT_MSG);
+        return Integer.parseInt(scanner.nextLine());
+    }
+}
