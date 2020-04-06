de9fdea5f2585351ed884dc47a10e152446a2f29
Yeoseon Yun <devyyskr@gmail.com>
2020-04-07 01:52:07 +0900

    test(LadderTest): moveTest 통과 처리

--- src/test/java/nextstep/ladder/LadderTest.java
+++ src/test/java/nextstep/ladder/LadderTest.java
@@ -51,20 +51,20 @@ public class LadderTest {
     @DisplayName("출발점으로부터 사다리를 타고 알맞은 Position에 도착해야 한다.")
     void moveTest1() {
         List<Line> lines = new ArrayList<>();
-        List<Point> points = new ArrayList<>();
-        points.add(new Point(0, true));
-        points.add(new Point(1, false));
-        Line line1 = new Line(points);
+        List<Point> points1 = new ArrayList<>();
+        points1.add(new Point(0, true));
+        points1.add(new Point(1, false));
+        Line line1 = new Line(points1);
 
-        points.clear();
-        points.add(new Point(0, false));
-        points.add(new Point(1, true));
-        Line line2 = new Line(points);
+        List<Point> points2 = new ArrayList<>();
+        points2.add(new Point(0, false));
+        points2.add(new Point(1, true));
+        Line line2 = new Line(points2);
 
-        points.clear();
-        points.add(new Point(0, false));
-        points.add(new Point(1, true));
-        Line line3 = new Line(points);
+        List<Point> points3 = new ArrayList<>();
+        points3.add(new Point(0, false));
+        points3.add(new Point(1, true));
+        Line line3 = new Line(points3);
 
         lines.add(line1);
         lines.add(line2);
