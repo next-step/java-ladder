fb035a0fb37d7c3a3adcf2fcad102a9e31288a2f
ohtaeg <otk1090@naver.com>
2020-04-06 23:34:57 +0900

    refactor (Ladder) : 높이와 높이당 라인을 가지도록 변경

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -6,18 +6,20 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Ladder {
+    private final Height height;
     private final List<Line> lines;
 
     public Ladder(final int playerCount, final Height height) {
-        this.lines = createLines(playerCount, height);
+        this.height = height;
+        this.lines = createLines(playerCount, height.value());
     }
 
     public List<Line> getLines() {
         return lines;
     }
 
-    private List<Line> createLines(final int playerCount, final Height height) {
-        final List<Line> lines = IntStream.rangeClosed(Height.MIN_HEIGHT, height.value())
+    private List<Line> createLines(final int playerCount, final int height) {
+        final List<Line> lines = IntStream.rangeClosed(Height.MIN_HEIGHT, height)
                                           .mapToObj(count -> Line.of(playerCount))
                                           .collect(Collectors.toList());
         return Collections.unmodifiableList(lines);
