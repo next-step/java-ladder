c1f35df6fe71637cbc2ad81aac4ccf5b69a97f1d
parkeeseul <eeseul.p@gmail.com>
2020-04-06 22:55:38 +0900

    test: LineTest 수정

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -14,4 +14,13 @@ public class LineTest {
         assertThat(lines).hasSize(5);
         assertThat(lines.get(0).getActions()).hasSize(3);
     }
+
+    @Test
+    void getMovePoint() {
+        Line line = Line.listOf(3, 5).get(0);
+
+        int actual = line.getMovePoint(0);
+
+        assertThat(actual).isBetween(-1, 1);
+    }
 }
