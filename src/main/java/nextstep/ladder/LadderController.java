a645ea464fc1d5152c4b236b032e546fb116f148
crystal <crystal2840@gmail.com>
2020-04-06 23:10:21 +0900

    컨트롤러 위치변경

--- src/main/java/nextstep/ladder/LadderController.java
+++ src/main/java/nextstep/ladder/LadderController.java
@@ -1,16 +1,28 @@
 package nextstep.ladder;
 
+import nextstep.ladder.domain.Ladder;
+import nextstep.ladder.domain.PlayLadderGame;
+import nextstep.ladder.domain.Results;
+import nextstep.ladder.domain.Users;
+import nextstep.ladder.view.InputView;
+import nextstep.ladder.view.ResultView;
+
 public class LadderController {
     private static InputView inputView = InputView.getInputView();
     private static ResultView resultView = ResultView.getResultView();
 
     public static void main(String[] args) {
         Users users = inputView.enterUserNames();
+        Results results = inputView.enterResults();
         int ladderHeight = inputView.enterLadderHeight();
+
         int countOfPerson = users.getUsers().size();
         Ladder ladder = new Ladder(countOfPerson, ladderHeight);
 
-        resultView.printLadder(users, ladder);
+        resultView.printLadder(users, ladder, results);
+
+        String userName = inputView.enterResultUser(users);
+        PlayLadderGame playLadderGame = new PlayLadderGame();
 
     }
 }
