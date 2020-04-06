1cec3cd1676446ab53f12ff51186b21926d800be
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-06 20:18:00 +0900

    refactor (LadderService.java) : 메서드 이름 변경 사항 적용

--- src/main/java/ladder/service/LadderService.java
+++ src/main/java/ladder/service/LadderService.java
@@ -11,8 +11,8 @@ public class LadderService {
     private static final String NAME_DELIMITER = ",";
 
     public LadderGame createLadder(final String inputName, final String inputHeight) {
-        List<String> names = StringUtils.splitStringToList(inputName, NAME_DELIMITER);
-        int height = StringUtils.stringToInt(inputHeight);
+        List<String> names = StringUtils.split(inputName, NAME_DELIMITER);
+        int height = StringUtils.parse(inputHeight);
         Players players = Players.of(names);
 
         LadderGame game = LadderGame.createLadder(players, height);
