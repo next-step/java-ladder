dfc76a9e0a6e6d10943231df7714757a1e1e3979
ohtaeg <otk1090@naver.com>
2020-04-06 23:26:00 +0900

    refactor (Player) : 이름과 시작위치를 갖도록 변경

--- src/main/java/ladder/domain/Player.java
+++ src/main/java/ladder/domain/Player.java
@@ -9,11 +9,13 @@ public class Player {
     private static final int NAME_MAX_LENGTH = 5;
 
     private final String name;
+    private final int position;
 
-    public Player(final String name) {
+    public Player(final String name, final int startPosition) {
         validName(name);
         validNameLength(name);
         this.name = name;
+        this.position = startPosition;
     }
 
     public String getName() {
@@ -37,11 +39,12 @@ public class Player {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         final Player player = (Player) o;
-        return Objects.equals(name, player.name);
+        return position == player.position &&
+                Objects.equals(name, player.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(name, position);
     }
 }
