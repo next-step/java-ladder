83ac604e25731b9790513443bebb0e9725bf753b
Yeoseon Yun <devyyskr@gmail.com>
2020-04-06 20:27:56 +0900

    refactor(Participant): 에러메시지 명시화

--- src/main/java/nextstep/ladder/domain/Participant.java
+++ src/main/java/nextstep/ladder/domain/Participant.java
@@ -1,7 +1,7 @@
 package nextstep.ladder.domain;
 
 public class Participant {
-    public static final String PARTICIPANT_NAME_LENGTH_ERROR = "이름은 5자 이하로 입력해주세요.";
+    public static final String PARTICIPANT_NAME_LENGTH_ERROR = "이름은 5자 이하로 입력해주세요. [이름: %s]";
     public static final int MAX_PARTICIPANT_NAME_LENGTH = 5;
     private String name;
 
@@ -12,7 +12,7 @@ public class Participant {
 
     private void assertParticipantName(String name) {
         if(name.length() > MAX_PARTICIPANT_NAME_LENGTH) {
-            throw new IllegalArgumentException(PARTICIPANT_NAME_LENGTH_ERROR);
+            throw new IllegalArgumentException(String.format(PARTICIPANT_NAME_LENGTH_ERROR, name));
         }
     }
 
--- src/test/java/nextstep/ladder/ParticipantTest.java
+++ src/test/java/nextstep/ladder/ParticipantTest.java
@@ -14,6 +14,6 @@ public class ParticipantTest {
 
         assertThatIllegalArgumentException().isThrownBy(() -> {
             new Participant(name);
-        }).withMessage(Participant.PARTICIPANT_NAME_LENGTH_ERROR);
+        }).withMessage(String.format(Participant.PARTICIPANT_NAME_LENGTH_ERROR, name));
     }
 }
