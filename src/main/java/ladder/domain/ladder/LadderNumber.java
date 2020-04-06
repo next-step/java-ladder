7423c7eb4d007543d267b15b41ef3ae689e40faf
teahyuk <jtaeh57@gmail.com>
2020-04-07 01:06:02 +0900

    refactor(LadderNumber) : 저번주 배운 캐시 기능 추가.

--- src/main/java/ladder/domain/ladder/LadderNumber.java
+++ src/main/java/ladder/domain/ladder/LadderNumber.java
@@ -1,12 +1,24 @@
 package ladder.domain.ladder;
 
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LadderNumber {
     private static final String INSTANTIATE_ERROR_FORMAT = "Create LadderNo failed. number must be at least %d : number=%d";
     private static final int MINIMUM_NUMBER = 0;
     private int ladderNo;
+
+    private static final Map<Integer, LadderNumber> numberMap;
+
+    static { //10개 캐싱으로 넣어둠.
+        numberMap = IntStream.range(MINIMUM_NUMBER, 10)
+                .boxed()
+                .collect(Collectors.toMap(Function.identity(), LadderNumber::new));
+    }
 
     private LadderNumber(int ladderNo) {
         this.ladderNo = ladderNo;
@@ -15,15 +27,19 @@ public class LadderNumber {
     public static LadderNumber of(int number) {
         return Optional.of(number)
                 .filter(v -> v >= MINIMUM_NUMBER)
-                .map(LadderNumber::new)
-                .orElseThrow(() -> throwIllegalArgumentException(number));
+                .map(numberMap::get)
+                .orElseGet(() -> Optional.of(number)
+                        .filter(v -> v >= MINIMUM_NUMBER)
+                        .map(LadderNumber::new)
+                        .map(instance -> numberMap.putIfAbsent(number, instance)) //캐시에 없으면 캐시에 추가하는 부분
+                        .orElseThrow(() -> throwIllegalArgumentException(number)));
     }
 
     private static IllegalArgumentException throwIllegalArgumentException(int number) {
         return new IllegalArgumentException(String.format(INSTANTIATE_ERROR_FORMAT, MINIMUM_NUMBER, number));
     }
 
-    public int getLadderNo() {
+    int getLadderNo() {
         return ladderNo;
     }
 
