465fba7e8f5c756cedc764e4d1945a27d4f86f6b
ohtaeg <otk1090@naver.com>
2020-04-06 23:25:13 +0900

    test (Player) : 이름과 시작위치를 갖도록 변경

--- src/test/java/ladder/domain/PlayerTest.java
+++ src/test/java/ladder/domain/PlayerTest.java
@@ -9,16 +9,32 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class PlayerTest {
+
+    @DisplayName("참여자는 이름과 시작 위치를 갖고 있다.")
+    @Test
+    void create() {
+        String name = "ohtae";
+        int position = 0;
+
+        Player expect = new Player(name, position);
+
+        Player actual = new Player(name, position);
+
+        assertThat(actual).isEqualTo(expect);
+    }
 
     @DisplayName("참여자 이름이 null 이거나 공백일경우 커스텀 예외를 발생한다.")
     @ParameterizedTest
     @MethodSource("invalidNames")
     void validName(String name) {
+        int position = 0;
+
         assertThatExceptionOfType(InvalidNameException.class).isThrownBy(
-                () ->  new Player(name)
+                () ->  new Player(name, position)
         );
     }
 
@@ -26,8 +42,9 @@ class PlayerTest {
     @Test
     void validNameLength() {
         String name = "ohtaeg";
+        int position = 0;
         assertThatExceptionOfType(NameLengthOutOfBoundException.class).isThrownBy(
-                () ->  new Player(name)
+                () ->  new Player(name, position)
         );
     }
 
