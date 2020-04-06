8c9443ba3c35e50f07a6d79f90ce8981c3e68985
ohtaeg <otk1090@naver.com>
2020-04-07 08:04:14 +0900

    refactor (Prizes) : 상품 찾는 메서드 구현

--- src/main/java/ladder/domain/Prizes.java
+++ src/main/java/ladder/domain/Prizes.java
@@ -1,11 +1,13 @@
 package ladder.domain;
 
 import ladder.exception.PrizeCountNotMatchException;
+import ladder.exception.PrizeNotFoundException;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class Prizes {
@@ -16,19 +18,31 @@ public class Prizes {
     public Prizes(final String prizes, final int playerCount) {
         String[] splitPrizes = prizes.split(SEPARATOR);
         checkAvailablePrizes(splitPrizes.length, playerCount);
-        this.prizes = Arrays.stream(splitPrizes)
-                            .map(Prize::new)
-                            .collect(Collectors.toList());
+        this.prizes = createPrize(splitPrizes);
+    }
+
+    public Prize find(Position position) {
+        return prizes.stream()
+                     .filter(prize -> prize.match(position))
+                     .findFirst()
+                     .orElseThrow(PrizeNotFoundException::new);
+    }
+
+    public List<Prize> getPrizes() {
+        return Collections.unmodifiableList(prizes);
+    }
+
+    private List<Prize> createPrize(final String[] splitPrizes) {
+        AtomicInteger index = new AtomicInteger();
+        return Arrays.stream(splitPrizes)
+                     .map(prizeName -> new Prize(index.getAndIncrement(), prizeName))
+                     .collect(Collectors.toList());
     }
 
     private void checkAvailablePrizes(final int prizeCount, final int playerCount) {
         if (prizeCount != playerCount) {
             throw new PrizeCountNotMatchException(prizeCount);
         }
-    }
-
-    public List<Prize> getPrizes() {
-        return Collections.unmodifiableList(prizes);
     }
 
     @Override
