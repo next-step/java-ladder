35ccf1bce67d7072aa7a79ad04a19cca45cb6f3f
ohtaeg <otk1090@naver.com>
2020-04-07 02:55:50 +0900

    test (Line) : 참여자 2명, 라인이 존재하지 않을경우 위치값 그대로 반환하도록 테스트 추가 - 현재 Line값이 랜덤으로 생성되어 테스트가 불가능 하므로, LineGenerator 인터페이스를 추출하여 가짜 객체가 LineGenerator를 구현하여 테스트 값을 만들도록 의존성 변경

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -1,7 +1,10 @@
 package ladder.domain;
 
+import ladder.domain.fake.FakeLineGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -30,5 +33,17 @@ class LineTest {
         int moveResult = line.move(playerStartPosition);
 
         assertThat(moveResult).isZero();
+    }
+
+    @DisplayName("참여자 2명, 라인이 존재하지 않으면 참여자 시작위치를 그대로 반환한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1})
+    void moveTwoByZero(int playerStartPosition) {
+        int playerCount = 2;
+        Line line = Line.of(playerCount, new FakeLineGenerator());
+
+        int moveResult = line.move(playerStartPosition);
+
+        assertThat(moveResult).isEqualTo(playerStartPosition);
     }
 }
