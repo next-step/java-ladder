7bdefb1b666060bbbaafe0a3edae94570aeaec2b
LeeSeungHee <palmseung@gmail.com>
2020-04-06 17:04:08 +0900

    Feat (PlayerName) : add feature to create PlayerName
    * Add PlayerName.validateNameLength

--- src/main/java/ladder/model/player/PlayerName.java
+++ src/main/java/ladder/model/player/PlayerName.java
@@ -1,4 +1,20 @@
 package ladder.model.player;
 
+import static ladder.Messages.WARNING_NOT_ALLOWED_NAME_LENGTH;
+
 public class PlayerName {
+    private static final int MAXIMUM_NAME_LENGTH = 5;
+
+    private String name;
+
+    public PlayerName(String name) {
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException(WARNING_NOT_ALLOWED_NAME_LENGTH);
+        }
+    }
 }
