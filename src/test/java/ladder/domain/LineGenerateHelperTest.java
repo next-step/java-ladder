e1084d9fe9bf810105f439804dcc287779a7bb0c
ohtaeg <otk1090@naver.com>
2020-04-06 19:45:24 +0900

    test (LineGeneratorHelper) : generator를 helper로 변경 테스트

--- src/test/java/ladder/domain/LineGenerateHelperTest.java
+++ src/test/java/ladder/domain/LineGenerateHelperTest.java
@@ -9,16 +9,12 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class LineGeneratorTest {
-
-    private LineGenerator lineGenerator;
-
+class LineGenerateHelperTest {
     @DisplayName("라인을 생성할 수 있다.")
     @ParameterizedTest
     @ValueSource(ints = {2, 3, 4, 5})
     void generate(int playerCount) {
-        lineGenerator = new LineGenerator(playerCount);
-        List<Boolean> lines = lineGenerator.generate();
+        List<Boolean> lines = LineGenerateHelper.generate(playerCount);
 
         assertThat(lines.size()).isEqualTo(playerCount);
     }
@@ -26,8 +22,9 @@ class LineGeneratorTest {
     @DisplayName("처음 위치엔 라인을 생성할 수 없다.")
     @Test
     void generateStartLine() {
-        lineGenerator = new LineGenerator(1);
-        List<Boolean> lines = lineGenerator.generate();
+        int playerCount = 1;
+
+        List<Boolean> lines = LineGenerateHelper.generate(playerCount);
 
         assertThat(lines.get(0)).isFalse();
     }
