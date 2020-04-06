6d026acc67f31807eb7114c1f46f1a04fb2bc390
ohtaeg <otk1090@naver.com>
2020-04-06 21:10:26 +0900

    refactor (InputView) : 뽑기 결과를 입력는 메서드 추가

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -2,12 +2,14 @@ package ladder.view;
 
 import ladder.domain.Height;
 import ladder.domain.Players;
+import ladder.domain.Prize;
 
 import java.io.InputStream;
 import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_PLAYER_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String INPUT_EXPECT_VALUES_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String INPUT_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
 
     private Scanner scanner;
@@ -23,6 +25,11 @@ public class InputView {
     public Players inputPlayerName() {
         System.out.println(INPUT_PLAYER_NAMES_MESSAGE);
         return new Players(scanner.nextLine());
+    }
+
+    public Prize inputPrizes() {
+        System.out.println(INPUT_EXPECT_VALUES_MESSAGE);
+        return new Prize(scanner.nextLine());
     }
 
     public Height inputHeight() {
