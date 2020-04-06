62293cbb8e4df1d38735e39fc458173446df405d
ohtaeg <otk1090@naver.com>
2020-04-07 08:03:22 +0900

    refactor (Prize) : 상품위치 추가

--- src/main/java/ladder/domain/Prize.java
+++ src/main/java/ladder/domain/Prize.java
@@ -3,10 +3,20 @@ package ladder.domain;
 import java.util.Objects;
 
 public class Prize {
+    private final Position prizePosition;
     private final String prize;
 
-    public Prize(final String prize) {
+    public Prize(final int position, final String prize) {
+        this.prizePosition = new Position(position);
         this.prize = prize;
+    }
+
+    public boolean match(final Position position) {
+        return prizePosition.equals(position);
+    }
+
+    public String getPrize() {
+        return prize;
     }
 
     @Override
@@ -14,11 +24,13 @@ public class Prize {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         final Prize prize1 = (Prize) o;
-        return Objects.equals(prize, prize1.prize);
+        return Objects.equals(prizePosition, prize1.prizePosition) &&
+                Objects.equals(prize, prize1.prize);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(prize);
+        return Objects.hash(prizePosition, prize);
     }
+
 }
