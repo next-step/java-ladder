1d500e9042c4dd933467992308c0b25f459c35be
kwanguuuu <kwangyu326@gmail.com>
2020-04-07 14:17:25 +0900

    new : view 에서 Ladder input, output 예상하여 클래스 추가

---
+++ src/main/java/ladder/view/LadderOutput.java
@@ -0,0 +1,9 @@
+package ladder.view;
+
+public class LadderOutput {
+    private static final String LADDER_DRAW_RESULT ="실행결과";
+
+    public void ladderDrawResult(){
+        System.out.println(LADDER_DRAW_RESULT);
+    }
+}
