e14653bd67f393bfae85c6797b1e013fb3ffb6d8
dongchul <sah3122@naver.com>
2020-04-07 15:25:43 +0900

    refactor(Line): change Naming

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -1,25 +1,32 @@
 package nextstep.ladder.domain;
 
 import nextstep.ladder.domain.step.Steps;
-import nextstep.ladder.domain.step.strategy.StepGenerator;
+import nextstep.ladder.domain.step.strategy.MovablePrevAndNext;
+import nextstep.ladder.domain.step.strategy.MovablePrev;
+
+import java.util.Objects;
 
 public class Line {
     private static final int START_POSITION = 0;
 
-    private final Person person;
     private final Steps steps;
+    private final Person person;
 
     private Line(Steps steps, Person person) {
         this.steps = steps;
         this.person = person;
     }
 
-    public static Line first(Person person, int height) {
+    public static Line firstLine(Person person, int height) {
         return new Line(Steps.movableNext(height, START_POSITION), person);
     }
 
-    public static Line of(Person person, Steps previousLineSteps, StepGenerator stepGenerator) {
-        return new Line(Steps.movableByPreviousCondition(previousLineSteps, stepGenerator), person);
+    public static Line middleLine(Person person, Steps previousLineSteps) {
+        return new Line(Steps.movableByPreviousCondition(previousLineSteps, new MovablePrevAndNext()), person);
+    }
+
+    public static Line lastLine(Person person, Steps previousLineSteps) {
+        return new Line(Steps.movableByPreviousCondition(previousLineSteps, new MovablePrev()), person);
     }
 
     public Steps getSteps() {
@@ -28,5 +35,19 @@ public class Line {
 
     public Person getPerson() {
         return person;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Line line = (Line) o;
+        return Objects.equals(steps, line.steps) &&
+                Objects.equals(person, line.person);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(steps, person);
     }
 }
--- src/test/java/nextstep/ladder/domain/LineTest.java
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
@@ -15,16 +13,16 @@ public class LineTest {
     @ParameterizedTest
     @ValueSource(ints = {2, 3})
     void createLine(int height) {
-        assertThatCode(() -> Line.first(new Person("dong"), height)).doesNotThrowAnyException();
+        assertThatCode(() -> Line.firstLine(new Person("dong"), height)).doesNotThrowAnyException();
     }
 
     @DisplayName("이전 라인의 상태에 따라 가로 라인을 만드는 라인 생성")
     @ParameterizedTest
     @ValueSource(ints = {2, 3})
     void createByPreviousLine(int height) {
-        Line first = Line.first(new Person("dong"), height);
+        Line first = Line.firstLine(new Person("dong"), height);
         assertThatCode(
-                () -> Line.of(new Person("chul"), first.getSteps(), (previouSteps, bridge) -> Step.of(bridge, () -> true))
+                () -> Line.middleLine(new Person("chul"), first.getSteps())
         ).doesNotThrowAnyException();
     }
 }
