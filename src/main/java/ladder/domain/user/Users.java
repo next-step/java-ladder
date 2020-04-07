08adccd745829ae49abcebf286a9b7638a0b5eb9
kwanguuuu <kwangyu326@gmail.com>
2020-04-07 15:41:02 +0900

    refactor : Users.java - name 중 띄어쓰기 변경 (map.trim())

--- src/main/java/ladder/domain/user/Users.java
+++ src/main/java/ladder/domain/user/Users.java
@@ -14,6 +14,7 @@ public class Users implements Line {
 
     public Users(String names) {
         this.users = Stream.of(names.split(NAME_DELIMITER))
+                .map(name -> name.trim())
                 .filter(name -> lengthValidator(name.length()))
                 .collect(Collectors.toList());
     }
