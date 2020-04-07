ea8f82c3e5f9263017370185fe6fc67b934103f3
dongchul <sah3122@naver.com>
2020-04-07 14:17:27 +0900

    refactor(Ladder): move create line to ladder

--- src/main/java/nextstep/ladder/LadderGame.java
+++ src/main/java/nextstep/ladder/LadderGame.java
@@ -1,38 +1,20 @@
 package nextstep.ladder;
 
 import nextstep.ladder.domain.Ladder;
-import nextstep.ladder.domain.Line;
 import nextstep.ladder.domain.Person;
-import nextstep.ladder.domain.step.strategy.MovableBothSide;
-import nextstep.ladder.domain.step.strategy.MovablePrev;
 import nextstep.ladder.dto.LadderRequestDto;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LadderGame {
-    private static final int ONE = 1;
-    private static final int FIRST_PERSON = 0;
     private static final String DELIMITER = ",";
 
     public static Ladder run(LadderRequestDto ladderRequestDto) {
         int heightOfLadder = ladderRequestDto.getHeight();
         List<Person> persons = namesToPersons(ladderRequestDto);
-
-        List<Line> lines = new ArrayList<>();
-        Line previousLine = Line.first(persons.get(FIRST_PERSON), heightOfLadder);
-        lines.add(previousLine);
-        generateLines(persons, lines, previousLine);
-        return new Ladder(lines, heightOfLadder);
-    }
-
-    private static void generateLines(List<Person> persons, List<Line> lines, Line previousLine) {
-        for (int i = 1; i < persons.size(); i++) {
-            previousLine = getLine(persons, previousLine, i);
-            lines.add(previousLine);
-        }
+        return Ladder.of(persons, heightOfLadder);
     }
 
     private static List<Person> namesToPersons(LadderRequestDto ladderRequestDto) {
@@ -40,16 +22,4 @@ public class LadderGame {
                 .map(Person::new)
                 .collect(Collectors.toList());
     }
-
-    private static Line getLine(List<Person> persons, Line previousLine, int i) {
-        if (i == lastPersonsIndex(persons)) {
-            return Line.of(persons.get(i), previousLine.getSteps(), new MovablePrev());
-        }
-        return Line.of(persons.get(i), previousLine.getSteps(), new MovableBothSide());
-    }
-
-    private static int lastPersonsIndex(List<Person> persons) {
-        return persons.size() - ONE;
-    }
-
 }
--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -1,21 +1,54 @@
 package nextstep.ladder.domain;
 
+import nextstep.ladder.vo.Height;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Ladder {
+    private static final int ONE = 1;
+    private static final int FIRST_PERSON = 0;
+    private static final int MINIMUM_LINE_SIZE = 2;
+
     private final List<Line> lines;
     private final Height heightOfLadder;
 
-    public Ladder(List<Line> lines, int heightOfLadder) {
+    private Ladder(List<Line> lines, int heightOfLadder) {
         validateLine(lines);
 
         this.lines = Collections.unmodifiableList(lines);
         this.heightOfLadder = new Height(heightOfLadder);
     }
 
+    public static Ladder of(List<Person> persons, int heightOfLadder) {
+        List<Line> lines = new ArrayList<>();
+        Line previousLine = Line.firstLine(persons.get(FIRST_PERSON), heightOfLadder);
+        lines.add(previousLine);
+        for (int lineIndex = 1; lineIndex < persons.size(); lineIndex++) {
+            previousLine = getLine(persons, previousLine, lineIndex);
+            lines.add(previousLine);
+        }
+        return new Ladder(lines, heightOfLadder);
+    }
+
+    private static Line getLine(List<Person> persons, Line previousLine, int lineIndex) {
+        if (isLastLine(persons, lineIndex)) {
+            return Line.lastLine(persons.get(lineIndex), previousLine.getSteps());
+        }
+        return Line.middleLine(persons.get(lineIndex), previousLine.getSteps());
+    }
+
+    private static boolean isLastLine(List<Person> persons, int lineIndex) {
+        return lineIndex == lastPersonsIndex(persons);
+    }
+
+    private static int lastPersonsIndex(List<Person> persons) {
+        return persons.size() - ONE;
+    }
+
     private void validateLine(List<Line> lines) {
-        if (lines.size() < 2) {
+        if (lines.size() < MINIMUM_LINE_SIZE) {
             throw new IllegalArgumentException("라인은 두개 이상이여야 합니다.");
         }
     }
