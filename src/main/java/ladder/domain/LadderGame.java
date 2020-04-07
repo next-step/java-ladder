04162341826cbacb11a852fcc385476061e09dde
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-08 08:55:25 +0900

    refactor (LadderGame.java) : Ladder를 멤버로 추가

--- src/main/java/ladder/domain/LadderGame.java
+++ src/main/java/ladder/domain/LadderGame.java
@@ -1,53 +1,30 @@
 package ladder.domain;
 
-import ladder.exception.LadderException;
-
 public class LadderGame {
 
-    private static final String USER_AND_RESULT_COUNT_MATCH_ERR_MST =
-            "참가자와 게임 결과의 수가 일치해야 합니다.";
-
     private final Players players;
-    private final Lines lines;
-    private final LadderPrize ladderPrize;
+    private final Ladder ladder;
 
-    public LadderGame(final Players players,
-                      final Lines lines,
-                      final LadderPrize ladderPrize) {
-        validateUserAndResult(players, ladderPrize);
+    public LadderGame(Players players, Ladder ladder) {
         this.players = players;
-        this.lines = lines;
-        this.ladderPrize = ladderPrize;
+        this.ladder = ladder;
     }
 
-    public static LadderGame of(final Players players,
-                                final int height,
-                                final LadderPrize ladderPrize) {
-        Lines lines = Lines.of(players.size(), height);
-
-        return new LadderGame(players, lines, ladderPrize);
-    }
-
-    private void validateUserAndResult(final Players players, final LadderPrize ladderPrize) {
-        if (players.size() != ladderPrize.size()) {
-            throw new LadderException(USER_AND_RESULT_COUNT_MATCH_ERR_MST);
-        }
-    }
 
     public Node findPlayerResult(final String playerName) {
         int playerIndex = players.findPlayer(playerName);
-        return lines.move(playerIndex);
+        return ladder.findLastNode(playerIndex);
     }
 
     public String findPrize(final int prizeIndex) {
-        return ladderPrize.getPrize().get(prizeIndex);
-    }
-
-    public Lines getLines() {
-        return lines;
+        return ladder.getLadderPrize().getPrize().get(prizeIndex);
     }
 
     public Players getPlayers() {
         return players;
+    }
+
+    public Ladder getLadder() {
+        return ladder;
     }
 }
--- src/test/java/ladder/domain/LadderGameTest.java
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -1,13 +1,11 @@
 package ladder.domain;
 
-import ladder.exception.LadderException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LadderGameTest {
 
@@ -19,68 +17,16 @@ class LadderGameTest {
         //given
         Players players =
                 new Players(Arrays.asList(new Player("a"), new Player("b")));
-        Lines lines = new Lines(Arrays.asList(new Line(), new Line(), new Line()));
+        Lines lines = new Lines(Arrays.asList(new Line(), new Line()));
+        Ladder ladder = new Ladder(lines, new LadderPrize(Arrays.asList("1", "2")));
 
         //when
-        LadderGame game = new LadderGame(players, lines, gameResult);
-    }
-
-    @DisplayName("참가자와 게임 결과의 수가 일치해야 한다.")
-    @Test
-    public void constructor_fail() throws Exception {
-        //given
-        Players players = Players.of(Arrays.asList("a", "b", "c"));
-        Lines lines = new Lines();
-        LadderPrize gameResult = new LadderPrize(Arrays.asList("5000", "꽝"));
-
-        //then
-        assertThatThrownBy(
-                () -> new LadderGame(players, lines, gameResult)
-        ).isInstanceOf(LadderException.class);
-    }
-
-    @DisplayName("사다리 게임 생성")
-    @Test
-    public void createLadder_success() throws Exception {
-        //given
-        Players players =
-                new Players(Arrays.asList(new Player("a"), new Player("b")));
-
-        //when
-        LadderGame game = LadderGame.of(players, 3, gameResult);
-
-        //then
-        assertThat(game.getLines().getLines().size()).isEqualTo(3);
-    }
-
-    @DisplayName("참가자의 이름을 전달 받고 사다리 진행 결과 Node 반환")
-    @Test
-    public void getPlayerResult_success() throws Exception {
-        //given
-        final String playerName = "a";
-        final Players players = Players.of(Arrays.asList("a", "b"));
-        final Lines lines = new Lines(
-                Arrays.asList(
-                        new Line(Arrays.asList(
-                                new Node(0, new Way(false, true)),
-                                new Node(1, new Way(true, false)))
-                        ),
-                        new Line(Arrays.asList(
-                                new Node(0, new Way(false, true)),
-                                new Node(1, new Way(true, false)))
-                        )));
-        LadderGame game = new LadderGame(players, lines, gameResult);
-
-        //when
-        Node result = game.findPlayerResult(playerName);
-
-        //then
-        assertThat(result.getIndex()).isEqualTo(0);
+        LadderGame game = new LadderGame(players, ladder);
     }
 
     @DisplayName("상품 이름을 찾고 반환한다.")
     @Test
-    public void findPrice_success() throws Exception {
+    public void findPrize_success() throws Exception {
         //given
         final Players players = Players.of(Arrays.asList("a", "b"));
         final Lines lines = new Lines(
@@ -93,12 +39,15 @@ class LadderGameTest {
                                 new Node(0, new Way(false, true)),
                                 new Node(1, new Way(true, false)))
                         )));
-        LadderGame game = new LadderGame(players, lines, gameResult);
+        final LadderPrize ladderPrize = new LadderPrize(Arrays.asList("100", "200"));
+        Ladder ladder = new Ladder(lines, ladderPrize);
+
+        LadderGame game = new LadderGame(players, ladder);
 
         //when
         String prize = game.findPrize(0);
 
         //then
-        assertThat(prize).isEqualTo("꽝");
+        assertThat(prize).isEqualTo("100");
     }
 }
