db75128e02df1f857c3c6ba7aa93767f9889bf7c
parkeeseul <eeseul.p@gmail.com>
2020-04-07 20:35:08 +0900

    refactor(Line): Line 리팩토링

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -4,33 +4,21 @@ import ladder.domain.type.ActionType;
 import lombok.Getter;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Line {
-    private static final List<ActionType> ACTION_TYPES = Arrays.asList(ActionType.RIGHT, ActionType.DOWN);
-    private static final int POINT_INDEX = 0;
-
     @Getter
     private List<ActionType> actions;
 
-    private Line(int userCount) {
-        Collections.shuffle(ACTION_TYPES);
-
-        actions = new ArrayList<>();
-        actions.add(ACTION_TYPES.get(POINT_INDEX));
-
-        for (int i = 1; i < userCount; i++) {
-            actions.add(getPoint(userCount, i));
-        }
+    public Line(List<ActionType> actions) {
+        this.actions = actions;
     }
 
     public static List<Line> listOf(int userCount, int lineHeight) {
         List<Line> lines = new ArrayList<>();
 
         for (int i = 0; i < lineHeight; i++) {
-            lines.add(new Line(userCount));
+            lines.add(new Line(ActionType.listOf(userCount)));
         }
 
         return lines;
@@ -38,19 +26,5 @@ public class Line {
 
     public int getMovePoint(int index) {
         return this.actions.get(index).getMove();
-    }
-
-    private ActionType getPoint(int userCount, int index) {
-        if (this.actions.get(index - 1) == ActionType.RIGHT) {
-            return ActionType.LEFT;
-        }
-
-        if (index == userCount - 1) {
-            return ActionType.DOWN;
-        }
-
-        Collections.shuffle(ACTION_TYPES);
-
-        return ACTION_TYPES.get(POINT_INDEX);
     }
 }
--- src/main/java/ladder/domain/type/ActionType.java
+++ src/main/java/ladder/domain/type/ActionType.java
@@ -2,15 +2,50 @@ package ladder.domain.type;
 
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public enum ActionType {
     RIGHT(1),
     LEFT(-1),
     DOWN(0);
+
+    private static final List<ActionType> ACTION_TYPES = Arrays.asList(ActionType.RIGHT, ActionType.DOWN);
+    private static final int POINT_INDEX = 0;
 
     @Getter
     private int move;
 
     ActionType(int move) {
         this.move = move;
+    }
+
+    public static List<ActionType> listOf(int userCount) {
+        Collections.shuffle(ACTION_TYPES);
+
+        List<ActionType> actions = new ArrayList<>();
+        actions.add(ACTION_TYPES.get(POINT_INDEX));
+
+        for (int i = 1; i < userCount; i++) {
+            actions.add(getPoint(userCount, i, actions));
+        }
+
+        return actions;
+    }
+
+    private static ActionType getPoint(int userCount, int index, List<ActionType> actions) {
+        if (actions.get(index - 1) == ActionType.RIGHT) {
+            return ActionType.LEFT;
+        }
+
+        if (index == userCount - 1) {
+            return ActionType.DOWN;
+        }
+
+        Collections.shuffle(ACTION_TYPES);
+
+        return ACTION_TYPES.get(POINT_INDEX);
     }
 }
