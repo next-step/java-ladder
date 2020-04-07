eb2d0a7da66b37002b33b0f4aa7b28f08868e170
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-08 00:26:01 +0900

    feat (LadderResultDto.java) : view단으로 전달할 dto 생성

---
+++ src/main/java/ladder/view/LadderResultDto.java
@@ -0,0 +1,18 @@
+package ladder.view;
+
+import java.util.Map;
+
+public class LadderResultDto {
+    private Map<String, String> result;
+
+    public LadderResultDto() {
+    }
+
+    public Map<String, String> getResult() {
+        return result;
+    }
+
+    public void setResult(Map<String, String> result) {
+        this.result = result;
+    }
+}
