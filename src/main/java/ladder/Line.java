a876d7872394b8f5d28faf6485e592aa2ee3ed9c
wholemann <wholeman.dev@gmail.com>
2020-04-07 19:13:05 +0900

    feat(line): 랜덤한 라인을 생성한다
    만약 라인의 모든 블럭이 비어있다면 다시 재귀적으로 생성한다.

--- src/main/java/ladder/Line.java
+++ src/main/java/ladder/Line.java
@@ -2,12 +2,14 @@ package ladder;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
 public class Line {
     private final List<Boolean> points;
+    private boolean lineAppearLeft = false;
 
     public Line(int countOfPerson) {
         points = generateLine(countOfPerson);
@@ -22,9 +24,31 @@ public class Line {
     }
 
     private List<Boolean> generateLine(int countOfPerson) {
-        return Stream
-                .generate(() -> true)
+        List<Boolean> line = Stream
+                .generate(() -> randomGenerateBlock())
                 .limit(countOfPerson)
                 .collect(toList());
+
+        if (isEmptyLine(line)) {
+            line = generateLine(countOfPerson);
+        }
+
+        return line;
+    }
+
+    private boolean isEmptyLine(List<Boolean> line) {
+        return line.stream()
+                .limit(line.size() - 1)
+                .allMatch(point -> !point);
+    }
+
+    private boolean randomGenerateBlock() {
+        Random random = new Random();
+        boolean line = lineAppearLeft ? false : random.nextBoolean();
+        lineAppearLeft = false;
+        if (line) {
+            lineAppearLeft = true;
+        }
+        return line;
     }
 }
