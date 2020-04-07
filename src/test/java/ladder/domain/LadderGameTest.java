5ba4d280ff42fa3e26eab435199fcac6267aaf09
parkeeseul <eeseul.p@gmail.com>
2020-04-08 01:41:23 +0900

    test(LadderGameTest): 네이밍 수정

--- src/test/java/ladder/domain/LadderGameTest.java
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -20,14 +20,14 @@ public class LadderGameTest {
     }
 
     @Test
-    void ladderGame_invalid_name_size_Exception() {
+    void ladderGame_invalidNameSizeException() {
         assertThatThrownBy(() -> new LadderGame("pobi,honuxx,crong,jk", "5", "꽝,5000,꽝,3000"))
                 .isInstanceOf(LadderException.class)
                 .hasMessageContaining(ExceptionType.INVALID_NAME_SIZE.getErrorMessage());
     }
 
     @Test
-    void ladderGame_invalid_result_size_Exception() {
+    void ladderGame_invalidResultSizeException() {
         assertThatThrownBy(() -> new LadderGame("pobi,honux,crong,jk", "5", "꽝,5000,꽝"))
                 .isInstanceOf(LadderException.class)
                 .hasMessageContaining(ExceptionType.INVALID_RESULT_SIZE.getErrorMessage());
