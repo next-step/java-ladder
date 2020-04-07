22c5bd1a50102072b46b1ae14a893adcec9173c8
crystal <crystal2840@gmail.com>
2020-04-07 21:42:00 +0900

    사다리 게임 결과를 만드는 클래스 작성 및 기능구현

---
+++ src/main/java/nextstep/ladder/domain/PlayLadderGame.java
@@ -0,0 +1,99 @@
+package nextstep.ladder.domain;
+
+import java.util.List;
+
+public class PlayLadderGame {
+
+    public String playResultByUser(Users paramUsers, String userName) {
+        String result = paramUsers.getUsers()
+                                  .stream()
+                                  .filter(user -> user.getName().equals(userName))
+                                  .findFirst()
+                                  .get().getResult();
+        return result;
+    }
+
+    public Users generateResultsForAllPlayers(Users paramUsers, Ladder paramLadder, Results paramResults) {
+        List<User> users = paramUsers.getUsers();
+        List<Line> ladder = paramLadder.getLadder();
+        List<Result> results = paramResults.getResults();
+
+        int userCount = users.size();
+        int last = userCount - 1;
+        for (int i = 0; i < users.size(); i++) {
+            int index = initIndex(userCount, i);
+            index = repeatLineForFindIndex(ladder, last, index);
+            users.get(i).setResult(results.get(index).getResult());
+        }
+
+        return new Users(users);
+    }
+
+    public int plusIndex(int index, boolean isLine) {
+        if (isLine) {
+            ++index;
+        }
+        return index;
+    }
+
+    public int minusIndex(int index, boolean isLine) {
+        if (isLine) {
+            --index;
+        }
+        return index;
+    }
+
+    public int judgeIndex(int index, boolean isLeftLine, boolean isRightLine) {
+        if (isLeftLine) {
+            --index;
+            return index;
+        }
+
+        if (isRightLine) {
+            ++index;
+            return index;
+        }
+
+        return index;
+    }
+
+    private int initIndex(int userCount, int i) {
+        int index = i;
+        if (i >= userCount) {
+            index = userCount - 1;
+        }
+        return index;
+    }
+
+    private int repeatLineForFindIndex(List<Line> ladder, int last, int index) {
+        for (int j = 0; j < ladder.size(); j++) {
+            List<Point> points = ladder.get(j).getPoints();
+            Point point = getPoint(points, last, index);
+            index = getIndex(last, index, points, point);
+        }
+        return index;
+    }
+
+    private int getIndex(int last, int index, List<Point> points, Point point) {
+        if (index == last) {
+            index = minusIndex(index, point.isNextPoint());
+            return index;
+        }
+        if (index == 0) {
+            index = plusIndex(index, point.isNextPoint());
+            return index;
+        }
+        if (index > 0 && index < last) {
+            Point prePoint = points.get(index - 1);
+            index = judgeIndex(index, prePoint.isNextPoint(), point.isNextPoint());
+        }
+        return index;
+    }
+
+    private Point getPoint(List<Point> points, int last, int index) {
+        if (index < last) {
+            return points.get(index);
+        }
+        return points.get(index - 1);
+    }
+}
---
+++ src/test/java/nextstep/ladder/domain/PlayLadderGameTest.java
@@ -0,0 +1,152 @@
+package nextstep.ladder.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import nextstep.ladder.view.ResultView;
+
+class PlayLadderGameTest {
+    private static ResultView resultView = ResultView.getResultView();
+    PlayLadderGame playLadderGame = new PlayLadderGame();
+    private Users users;
+    private Line line;
+    private List<Line> lineList;
+    private Ladder ladder;
+    private Results results;
+    private Users resultUsers;
+
+    @BeforeEach
+    void setUp() {
+
+        List<Point> points = Stream.of(new Point(true),
+                                       new Point(false),
+                                       new Point(true))
+                                   .collect(Collectors.toList());
+
+        line = new Line(points);
+
+        List<Point> points2 = Stream.of(new Point(false),
+                                        new Point(true),
+                                        new Point(false))
+                                    .collect(Collectors.toList());
+
+        Line line2 = new Line(points2);
+
+        List<Point> points3 = Stream.of(new Point(true),
+                                        new Point(false),
+                                        new Point(false))
+                                    .collect(Collectors.toList());
+
+        Line line3 = new Line(points3);
+
+        List<Point> points4 = Stream.of(new Point(false),
+                                        new Point(true),
+                                        new Point(false))
+                                    .collect(Collectors.toList());
+
+        Line line4 = new Line(points4);
+
+        List<Point> points5 = Stream.of(new Point(true),
+                                        new Point(false),
+                                        new Point(true))
+                                    .collect(Collectors.toList());
+
+        Line line5 = new Line(points5);
+
+        lineList = Stream.of(line, line2, line3, line4, line5)
+                         .collect(Collectors.toList());
+
+        ladder = new Ladder(lineList);
+
+        users = new Users(new ArrayList<>(
+                Stream.of(new User("pobi"),
+                          new User("honux"),
+                          new User("crong"),
+                          new User("jk")
+                )
+                      .collect(Collectors.toList())));
+
+        results = new Results(new ArrayList<>(
+                Stream.of(new Result("꽝"),
+                          new Result("5000"),
+                          new Result("꽝"),
+                          new Result("3000")
+                )
+                      .collect(Collectors.toList())));
+
+        resultUsers = playLadderGame.generateResultsForAllPlayers(users, ladder, results);
+
+    }
+
+    @DisplayName("점의 위치가 첫번째 이고 선이 있을 경우 무조건 index 값이 +1 증가함(이동)")
+    @Test
+    void plusIndexForFirstPosition() {
+        int index = 0;
+        boolean isLine = true;
+        int plusIndex = playLadderGame.plusIndex(index, isLine);
+        assertThat(plusIndex).isEqualTo(index + 1);
+    }
+
+    @DisplayName("점의 위치가 마지막이고 선이 있을 경우 무조건 index 값이 -1 증가함(이동)")
+    @Test
+    void minusIndexForLastPosition() {
+        int index = 2;
+        boolean isLine = true;
+        int minusIndex = playLadderGame.minusIndex(index, isLine);
+        assertThat(minusIndex).isEqualTo(index - 1);
+    }
+
+    @DisplayName("점의 위치가 처음과 마지막이 아닐 경우는"
+                 + "양옆에 왼쪽 또 오른쪽에 선이 있는는 경우에 따라 다르게 이동한다."
+                 + "왼쪽으로 이동하는 경우 -1")
+    @Test
+    void judgeIndexForMiddlePositionAndMovingToLeft() {
+        int index = 1;
+        boolean isLeftLine = true;
+        boolean isRightLine = false;
+        int judgeIndex = playLadderGame.judgeIndex(index, isLeftLine, isRightLine);
+        assertThat(judgeIndex).isEqualTo(index - 1);
+
+    }
+
+    @DisplayName("점의 위치가 처음과 마지막이 아닐 경우는"
+                 + "양옆에 왼쪽 또 오른쪽에 선이 있는는 경우에 따라 다르게 이동한다."
+                 + "오른으로 이동하는 경우 +1")
+    @Test
+    void judgeIndexForMiddlePositionAndMovingToRight() {
+        int index = 1;
+        boolean isLeftLine = false;
+        boolean isRightLine = true;
+        int judgeIndex = playLadderGame.judgeIndex(index, isLeftLine, isRightLine);
+        assertThat(judgeIndex).isEqualTo(index + 1);
+
+    }
+
+    @DisplayName("사다리 게임에 대한 전체 결과 값을 만든다.")
+    @Test
+    void GenerateResultsForAllPlayers() {
+        resultView.printLadder(users, ladder, results);
+        resultUsers = playLadderGame.generateResultsForAllPlayers(users, ladder, results);
+
+        List<User> userList = resultUsers.getUsers();
+        assertThat(userList.get(0).getResult()).isEqualTo("꽝");
+        assertThat(userList.get(1).getResult()).isEqualTo("3000");
+        assertThat(userList.get(2).getResult()).isEqualTo("꽝");
+        assertThat(userList.get(3).getResult()).isEqualTo("5000");
+    }
+
+    @DisplayName("특정 유저의 실행결과 값을 가져온다.")
+    @Test
+    void playResultByUser() {
+        String resultByUser = playLadderGame.playResultByUser(resultUsers, "honux");
+        assertThat(resultByUser).isEqualTo("3000");
+    }
+}
