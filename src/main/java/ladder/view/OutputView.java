d8ecfd4d460003f9ebcddc2948e6679d482941d3
ohtaeg <otk1090@naver.com>
2020-04-07 09:19:10 +0900

    refactor (OutputView) : 결과 출력 메서드 구현

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -1,25 +1,29 @@
 package ladder.view;
 
-import ladder.domain.Ladder;
-import ladder.domain.Line;
-import ladder.domain.Player;
-import ladder.domain.Players;
+import ladder.domain.*;
 
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class OutputView {
-    private static final String RESULT_MESSAGE = "실행결과\n";
     private static final String NAME_FORMAT_EXPRESSION = "%5s";
+    private static final String PRIZE_FORMAT_EXPRESSION = "%-5s";
+
     private static final String LADDER_STRAIGHT_LINE = "|";
     private static final String LADDER_LINE = "-----";
     private static final String LADDER_EMPTY_LINE = "     ";
     private static final String BLANK_STRING = " ";
     private static final String EMPTY_STRING = "";
 
+    private static final String LADDER_RESULT_MESSAGE = "사다리 결과\n";
+    private static final String RESULT_MESSAGE = "실행결과";
+    private static final String WHO_WANT_TO_SEE_RESULT = "결과를 보고 싶은 사람은?";
+    private static final String ALL = "all";
+
     private OutputView() {}
 
     public static void printPlayerName(Players players) {
-        System.out.println(RESULT_MESSAGE);
+        System.out.println(LADDER_RESULT_MESSAGE);
         players.getPlayers()
                .stream()
                .map(Player::name)
@@ -35,6 +39,23 @@ public class OutputView {
               .forEach(System.out::println);
     }
 
+    public static void printPrize(final Prizes prizes) {
+        prizes.getPrizes()
+              .stream()
+              .map(Prize::getPrize)
+              .map(OutputView::formatPrize)
+              .forEach(System.out::print);
+        System.out.println();
+    }
+
+    static String formatName(String name) {
+        return String.format(NAME_FORMAT_EXPRESSION, name) + BLANK_STRING;
+    }
+
+    static String formatPrize(String name) {
+        return String.format(PRIZE_FORMAT_EXPRESSION, name) + BLANK_STRING;
+    }
+
     private static String printLine(final Line line) {
         return line.getLine()
                    .stream()
@@ -42,7 +63,21 @@ public class OutputView {
                    .collect(Collectors.joining(LADDER_STRAIGHT_LINE, EMPTY_STRING, LADDER_STRAIGHT_LINE));
     }
 
-    static String formatName(String name) {
-        return String.format(NAME_FORMAT_EXPRESSION, name) + BLANK_STRING;
+    public static void printResult(final Result result) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println(WHO_WANT_TO_SEE_RESULT);
+            String name = scanner.nextLine();
+            if (ALL.equals(name)) {
+                break;
+            }
+            System.out.println(result.findOne(name));
+            System.out.println();
+        }
+
+        System.out.println(RESULT_MESSAGE);
+        result.getResult()
+              .keySet()
+              .forEach(player -> System.out.println(player.name().value() + " : " + result.get(player).getPrize()));
     }
 }
