0981d7d6904e42e17caf119d535b9d14c5756482
feel-coding-republic <feel.coding.republic@gmail.com>
2020-04-09 00:17:14 +0900

    feat:라인테스트코드작성후프로덕션코드작성

--- src/main/java/JavaLadder/domain/Line.java
+++ src/main/java/JavaLadder/domain/Line.java
@@ -2,16 +2,68 @@ package JavaLadder.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Line {
     private List<Boolean> line = new ArrayList<>();
 
-    public Line(int countOfPersn) {
+    public Line(int countOfPerson) {
+        List line = generate(countOfPerson);
+        this.line = line;
+    }
 
+    public Line(List<Boolean> list) {
+        this.line = list;
+    }
 
+    private List generate(int countOfPerson) {
+        for (int i = 0; i < countOfPerson - 1; i++) {
+            checkSuccessTrue(line, i);
+        }
+        return line;
+    }
+
+    private void checkSuccessTrue(List<Boolean> line, int index) {
+        if (index > 0 && line.get(index - 1)) {
+            line.add(false);
+            return;
+        }
+        line.add(new Random().nextBoolean());
+        return;
     }
 
     public int size() {
         return line.size();
+    }
+
+    public boolean isTrue(int index) {
+        return this.line.get(index);
+    }
+
+    public int numberOfTrue() {
+        return (int) line.stream().filter(n -> n.equals(true)).count();
+    }
+
+    public void isMove(Point point) {
+        if (point.getPoint() == 0){
+            if(line.get(point.getPoint())){
+                point.moveRight();
+            }
+            return;
+        }
+
+        int originalPoint = point.getPoint();
+        if (line.get(point.getPoint() - 1)) {
+            point.moveLeft();
+            return;
+        }
+        if (originalPoint == point.getPoint()){
+            if (line.get(point.getPoint())){
+                point.moveRight();
+                return;
+            }
+        }
+
+
     }
 }
--- src/test/java/JavaLadder/domain/LineTest.java
+++ src/test/java/JavaLadder/domain/LineTest.java
@@ -1,16 +1,66 @@
 package JavaLadder.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LineTest {
 
     @Test
+    @DisplayName("라인생성테스트")
     public void lineConstructorTest() {
         Line line = new Line(5);
-        assertThat(line.size()).isEqualTo(5);
+        assertThat(line.size()).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("값확인")
+    public void checkTrueTest() {
+        List<Boolean> list = Arrays.asList(true,false,true);
+        Line line = new Line(list);
+        assertThat(line.isTrue(0)).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("한라인에 true갯수확인")
+    public void numberOfLineTest() {
+        List<Boolean> list = Arrays.asList(true,false,true);
+        Line line = new Line(list);
+        assertThat(line.numberOfTrue()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("가장왼쪽포인트이동")
+    public void moveTheMostLeftTest() {
+        List<Boolean> list = Arrays.asList(true,false,true);
+        Line line = new Line(list);
+        Point point = new Point(0);
+        line.isMove(point);
+        assertThat(point.getPoint()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("라인상의 중간위치 포인트이동")
+    public void moveLeftAndRightTest() {
+        List<Boolean> list = Arrays.asList(false,true,false);
+        Line line = new Line(list);
+        Point point = new Point(1);
+        line.isMove(point);
+        assertThat(point.getPoint()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("라인상의 중간위치 포인트이동")
+    public void moveTheMostIrhgtTest() {
+        List<Boolean> list = Arrays.asList(false,false,true);
+        Line line = new Line(list);
+        Point point = new Point(3);
+        line.isMove(point);
+        assertThat(point.getPoint()).isEqualTo(2);
     }
 }
