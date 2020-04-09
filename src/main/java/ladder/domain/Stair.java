3029bc0a390feba5792f0cf03989f68f75df42df
ohtaeg <otk1090@naver.com>
2020-04-10 00:55:02 +0900

    feat (Stair) : 한 라인의 각 계단을 객체로 구현

---
+++ src/main/java/ladder/domain/Stair.java
@@ -0,0 +1,21 @@
+package ladder.domain;
+
+public class Stair {
+    private final boolean stair;
+
+    public Stair() {
+        this(false);
+    }
+
+    public Stair(final boolean stair) {
+        this.stair = stair;
+    }
+
+    public Stair first() {
+        return new Stair(false);
+    }
+
+    public boolean getStair() {
+        return stair;
+    }
+}
