0addf55b2e393062c1529ba50335d62c9857dab0
gale34 <gales34@gmail.com>
2020-04-09 16:52:04 +0900

    refactor: edit ladder game process logic

--- src/main/java/ladder/model/LadderLine.java
+++ src/main/java/ladder/model/LadderLine.java
@@ -2,7 +2,6 @@ package ladder.model;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class LadderLine {
@@ -18,7 +17,6 @@ public class LadderLine {
         return new LadderLine(Arrays.asList(bridges));
     }
 
-    // TODO: 2020-04-06 겹치는 구간이 없는지 validation 추가
     private void validate(final List<LadderBridge> bridges) {
         if (Objects.isNull(bridges) || bridges.isEmpty()) {
             throw new IllegalArgumentException("Ladder Bridge must be greater than zero.");
@@ -39,42 +37,12 @@ public class LadderLine {
         return new LadderLine(bridges);
     }
 
-    public LadderPoles getInitLadderPoles() {
-        List<LadderPole> poles = IntStream.rangeClosed(0, bridges.size())
-                .mapToObj(LadderPole::of)
-                .collect(Collectors.toList());
-        return LadderPoles.newInstance(poles);
+    public LadderPoles proceedLine(final LadderPoles ladderPoles) {
+        return ladderPoles.nextLadderPoles(bridges);
     }
 
-    public LadderPoles proceed(LadderPoles ladderPoles) {
-        List<LadderPole> nextLadderPoles = ladderPoles.getLadderPoles()
-                .stream()
-                .map(ladderPole -> nextLadderPole(ladderPole))
-                .collect(Collectors.toList());
-
-        return LadderPoles.newInstance(nextLadderPoles);
-    }
-
-    private LadderPole nextLadderPole(final LadderPole ladderPole) {
-        return getNearLadderBridgeIndex(ladderPole.toInt()).stream()
-                .filter(i -> bridges.get(i) == LadderBridge.EXIST)
-                .findAny()
-                .map(ladderPole::move)
-                .orElse(ladderPole);
-    }
-
-    private List<Integer> getNearLadderBridgeIndex(final int polePosition) {
-        List<Integer> bridgeIndexes = new ArrayList<>();
-
-        if (polePosition != 0) {
-            bridgeIndexes.add(polePosition - 1);
-        }
-
-        if (polePosition != bridges.size()) {
-            bridgeIndexes.add(polePosition);
-        }
-
-        return bridgeIndexes;
+    public int getPoleCount() {
+        return bridges.size() + 1;
     }
 
     public List<LadderBridge> getBridges() {
