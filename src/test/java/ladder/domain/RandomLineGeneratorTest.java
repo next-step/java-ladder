6bf6f4742230d78d3db0ac1d07a79bdf126ea6fb
ohtaeg <otk1090@naver.com>
2020-04-10 01:23:10 +0900

    test (LineGenerator) : 인스턴스 타입의 변경으로 임시 복사한 LineGenerator2로 테스트

--- src/test/java/ladder/domain/RandomLineGeneratorTest.java
+++ src/test/java/ladder/domain/RandomLineGeneratorTest.java
@@ -1,6 +1,7 @@
 package ladder.domain;
 
 import ladder.domain.generator.RandomLineGenerator;
+import ladder.domain.generator.RandomLineGenerator2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -15,9 +16,9 @@ class RandomLineGeneratorTest {
     @ParameterizedTest
     @ValueSource(ints = {2, 3, 4, 5})
     void generate(int playerCount) {
-        List<Boolean> lines = new RandomLineGenerator().generate(playerCount);
+        List<Stair> stairs = new RandomLineGenerator2().generate(playerCount);
 
-        assertThat(lines.size()).isEqualTo(playerCount);
+        assertThat(stairs.size()).isEqualTo(playerCount);
     }
 
     @DisplayName("처음 위치엔 라인을 생성할 수 없다.")
@@ -25,8 +26,8 @@ class RandomLineGeneratorTest {
     void generateStartLine() {
         int playerCount = 1;
 
-        List<Boolean> lines = new RandomLineGenerator().generate(playerCount);
+        List<Stair> stairs = new RandomLineGenerator2().generate(playerCount);
 
-        assertThat(lines.get(0)).isFalse();
+        assertThat(stairs.get(0)).isEqualTo(new Stair(false));
     }
 }
