c2333560a9e38ee4e1b3bc36956ac2d9fab91eb1
ohtaeg <otk1090@naver.com>
2020-04-10 02:09:48 +0900

    refactor (Line) : boolean -> Stair로 변경, 오른쪽 이동 가능한지 체크 메서드의 로직을 메서드로 분리

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -3,13 +3,15 @@ package ladder.domain;
 import ladder.domain.generator.LineGenerator;
 import ladder.domain.generator.RandomLineGenerator;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
 
 public class Line {
-    private final List<Boolean> lines;
+    private final List<Stair> stairs;
 
-    private Line(final List<Boolean> lines) {
-        this.lines = lines;
+    private Line(final List<Stair> stairs) {
+        this.stairs = stairs;
     }
 
     public static Line of(final int playerCount) {
@@ -20,43 +22,50 @@ public class Line {
         return new Line(lineGenerator.generate(playerCount));
     }
 
-    public List<Boolean> getLine() {
-        return Collections.unmodifiableList(lines);
+    public List<Stair> getLine() {
+        return Collections.unmodifiableList(stairs);
     }
 
     public Position move(final Position position) {
         int playerPosition = position.value();
         // 좌측 이동이 가능한지
-        if (isMoveLeft(playerPosition)) {
+        if (isMovableLeft(playerPosition)) {
             return position.left();
         }
 
         // 우측 이동이 가능한지
-        if (isMoveRight(playerPosition)) {
+        if (isMovableRight(playerPosition)) {
             return position.right();
         }
         return position;
     }
 
-    private Boolean isMoveLeft(final int playerPosition) {
-        return lines.get(playerPosition);
+    private boolean isMovableLeft(final int playerPosition) {
+        return stairs.get(playerPosition).isExist();
     }
 
-    private boolean isMoveRight(final int playerPosition) {
-        return playerPosition + Position.DEFAULT_MOVEMENT_POSITION < lines.size()
-                && lines.get(playerPosition + Position.DEFAULT_MOVEMENT_POSITION);
+    private boolean isMovableRight(final int playerPosition) {
+        return isLineIn(playerPosition) && nextStair(playerPosition).isExist();
+    }
+
+    private boolean isLineIn(final int playerPosition) {
+        return playerPosition + Position.NEXT < stairs.size();
+    }
+
+    private Stair nextStair(final int playerPosition) {
+        return stairs.get(playerPosition + Position.NEXT);
     }
 
     @Override
     public boolean equals(final Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        final Line line1 = (Line) o;
-        return Objects.equals(lines, line1.lines);
+        final Line line = (Line) o;
+        return Objects.equals(stairs, line.stairs);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(lines);
+        return Objects.hash(stairs);
     }
 }
