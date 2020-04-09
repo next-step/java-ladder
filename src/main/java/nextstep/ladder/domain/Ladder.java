5e653988533f285d5c9098fcd0a240a34bf56fa8
luvram <sahul0804@gmail.com>
2020-04-09 19:12:52 +0900

    refactor(ladder): 사다리 라인을 직접 초기화하는 로직을 라인 클래스로 옮김 (예제 적용)

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -3,81 +3,27 @@ package nextstep.ladder.domain;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class Ladder implements Iterable<HorizontalLine> {
-    private List<HorizontalLine> ladder;
+public class Ladder implements Iterable<LadderLine> {
+    private List<LadderLine> ladder;
 
-    public static Ladder valueOf(LadderSize ladderSize,
-                                 LineSelector lineSelector,
-                                 RightDirection rightDirection) {
+    public static Ladder valueOf(LadderSize ladderSize) {
         Ladder ladder = new Ladder(generateHorizontalLines(ladderSize));
-        ladder.assignOneLineToEachVerticalLines(ladderSize.getWidth(),
-                lineSelector);
-        ladder.assignLinesToEachHorizontalLines(rightDirection);
         return ladder;
     }
 
-    private static List<HorizontalLine> generateHorizontalLines(
+    private static List<LadderLine> generateHorizontalLines(
             LadderSize ladderSize) {
-        List<HorizontalLine> horizontalLines = new ArrayList<>();
+        List<LadderLine> ladderLines = new ArrayList<>();
         for (int i = 0, size = ladderSize.getHeight(); i < size; i++) {
-            horizontalLines.add(new HorizontalLine(ladderSize.getWidth()));
+            ladderLines.add(LadderLine.init(ladderSize.getWidth()));
         }
 
-        return horizontalLines;
+        return ladderLines;
     }
 
-    private Ladder(List<HorizontalLine> horizontalLines) {
-        this.ladder = new ArrayList<>(horizontalLines);
-    }
-
-    private void assignOneLineToEachVerticalLines(int width,
-                                                  LineSelector lineSelector) {
-        int beforeSelectedIndex = -1;
-        for (int i = 0; i < width; i++) {
-            beforeSelectedIndex =
-                    assignOneLineToVerticalLine(i, beforeSelectedIndex,
-                            lineSelector);
-        }
-    }
-
-    private int assignOneLineToVerticalLine(int index,
-                                            int beforeSelectedIndex,
-                                            LineSelector lineSelector) {
-        int height = ladder.size();
-        int selectedIndex = lineSelector.select(height);
-        if (beforeSelectedIndex == selectedIndex) {
-            selectedIndex = (selectedIndex + 1) % height;
-        }
-        HorizontalLine horizontalLine = ladder.get(selectedIndex);
-        horizontalLine.makeDirection(index);
-        return selectedIndex;
-    }
-
-    private void assignLinesToEachHorizontalLines(
-            RightDirection rightDirection) {
-        for (HorizontalLine horizontalLine : ladder) {
-            assignLinesToHorizontalLine(horizontalLine, rightDirection);
-        }
-    }
-
-    private void assignLinesToHorizontalLine(HorizontalLine horizontalLine,
-                                             RightDirection rightDirection) {
-        for (int i = 0; i < horizontalLine.size(); i++) {
-            boolean isAbleToRight = rightDirection.isAbleToRight();
-            horizontalLine.makeDirectionTo(i, isAbleToRight);
-        }
-    }
-
-    public List<Point> getVerticalLine(int index) {
-        return ladder.stream()
-                .map(horizontalLine -> horizontalLine.getPoint(index))
-                .collect(Collectors.toList());
-    }
-
-    public HorizontalLine getHorizontalLine(int index) {
-        return ladder.get(index);
+    private Ladder(List<LadderLine> ladderLines) {
+        this.ladder = new ArrayList<>(ladderLines);
     }
 
     public int height() {
@@ -86,15 +32,15 @@ public class Ladder implements Iterable<HorizontalLine> {
 
     public int result(int index) {
         int nextIndex = index;
-        for (HorizontalLine horizontalLine : ladder) {
-            nextIndex = horizontalLine.getNextIndex(nextIndex);
+        for (LadderLine ladderLine : ladder) {
+            nextIndex = ladderLine.move(nextIndex);
         }
 
         return nextIndex;
     }
 
     @Override
-    public Iterator<HorizontalLine> iterator() {
+    public Iterator<LadderLine> iterator() {
         return ladder.iterator();
     }
 }
