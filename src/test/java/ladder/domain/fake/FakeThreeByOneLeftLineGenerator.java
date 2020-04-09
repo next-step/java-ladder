b17b1bdd61be073b29a338c46a991794e79b1909
ohtaeg <otk1090@naver.com>
2020-04-10 02:10:54 +0900

    test (LineGenerator) : boolean -> Stair로 변경

--- src/test/java/ladder/domain/fake/FakeThreeByOneLeftLineGenerator.java
+++ src/test/java/ladder/domain/fake/FakeThreeByOneLeftLineGenerator.java
@@ -1,5 +1,6 @@
 package ladder.domain.fake;
 
+import ladder.domain.Stair;
 import ladder.domain.generator.LineGenerator;
 
 import java.util.ArrayList;
@@ -9,7 +10,7 @@ import java.util.List;
 public class FakeThreeByOneLeftLineGenerator implements LineGenerator {
 
     @Override
-    public List<Boolean> generate(final int playerCount) {
-        return new ArrayList<>(Arrays.asList(false, true, false));
+    public List<Stair> generate(final int playerCount) {
+        return new ArrayList<>(Arrays.asList(new Stair().first(), new Stair(true), new Stair(false)));
     }
 }
