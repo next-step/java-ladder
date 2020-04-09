c0b317448c248e475fb04906e10dd64ca6dbaa7a
ohtaeg <otk1090@naver.com>
2020-04-10 02:43:41 +0900

    refactor (Stairs) : Line 객체으 move 메서드 로직 이동

--- src/main/java/ladder/domain/Stairs.java
+++ src/main/java/ladder/domain/Stairs.java
@@ -13,4 +13,38 @@ public class Stairs {
     public int count() {
         return stairs.size();
     }
+
+    public List<Stair> getStairs() {
+        return stairs;
+    }
+
+    public Position move(final Position position) {
+        int playerPosition = position.value();
+        // 좌측 이동이 가능한지
+        if (isMovableLeft(playerPosition)) {
+            return position.left();
+        }
+
+        // 우측 이동이 가능한지
+        if (isMovableRight(playerPosition)) {
+            return position.right();
+        }
+        return position;
+    }
+
+    private boolean isMovableLeft(final int playerPosition) {
+        return stairs.get(playerPosition).isExist();
+    }
+
+    private boolean isMovableRight(final int playerPosition) {
+        return isLineIn(playerPosition) && nextStair(playerPosition).isExist();
+    }
+
+    private boolean isLineIn(final int playerPosition) {
+        return playerPosition + Position.NEXT < stairs.size();
+    }
+
+    private Stair nextStair(final int playerPosition) {
+        return stairs.get(playerPosition + Position.NEXT);
+    }
 }
