c865a923e67844bf2db3cd2d4ef33c43b3ffa820
minho park <riderkaman@gmail.com>
2020-04-11 00:15:03 +0900

    1단계 스트림, 람다, 옵셔널 연습 완료

--- src/main/java/nextstep/optional/Users.java
+++ src/main/java/nextstep/optional/Users.java
@@ -2,6 +2,8 @@ package nextstep.optional;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Users {
     static final User DEFAULT_USER = new User("codesquad", 100);
@@ -12,12 +14,12 @@ public class Users {
             new User("jk", 40),
             new User("honux", 45));
 
+    // TODO 깔끔하지 않은 것 같은데 개선방안이 있을지요?
     User getUser(String name) {
-        for (User user : users) {
-            if (user.matchName(name)) {
-                return user;
-            }
-        }
-        return DEFAULT_USER;
+        Optional<User> u = users.stream().filter(
+                (user) -> user.matchName(name) ).findFirst();
+        u = Optional.ofNullable(u).orElse(Optional.of(Users.DEFAULT_USER));
+
+        return u.orElse(Users.DEFAULT_USER);
     }
 }
