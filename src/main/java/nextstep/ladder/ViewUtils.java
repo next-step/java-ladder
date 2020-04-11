59a92f66a8925463ac1938c2b9872a1fc07a010c
Yeoseon Yun <devyyskr@gmail.com>
2020-04-11 21:58:17 +0900

    bug(ViewUtils): static 메소드로 수정함에 따라 생성자 삭제 및 scanner를 멤버변수에서 초기화하도록 처리

--- src/main/java/nextstep/ladder/ViewUtils.java
+++ src/main/java/nextstep/ladder/ViewUtils.java
@@ -5,11 +5,7 @@ import java.util.Scanner;
 public class ViewUtils {
     private static final String INPUT_NULL_EMPTY_EXCEPTION = "내용을 입력해주세요.";
     private static final String NUMBER_FORMAT_EXCEPTION = "입력된 내용이 숫자가 아닙니다.";
-    private static Scanner scanner;
-
-    public ViewUtils() {
-        this.scanner = new Scanner(System.in);
-    }
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void printLine(String line) {
         System.out.println(line);
