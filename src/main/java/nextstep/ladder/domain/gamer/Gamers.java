57f167cc0d205c3ccd453af88a74b255069a96d0
teahyuk <jtaeh57@gmail.com>
2020-04-11 15:52:55 +0900

    feat(Gamers): Gamers class 개발

--- src/main/java/nextstep/ladder/domain/gamer/Gamers.java
+++ src/main/java/nextstep/ladder/domain/gamer/Gamers.java
@@ -1,20 +1,51 @@
 package nextstep.ladder.domain.gamer;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class Gamers {
+    private static final String INSTANTIATE_ERROR_FORMAT = "Create Gamer fail. name must be at least 1 character and 5 characters : name=%s";
+    private static final int MAX_GAMER_STRING_LENGTH = 5;
     private final List<String> gamers;
 
     private Gamers(List<String> gamers) {
         this.gamers = gamers;
     }
 
-    public static Gamers of(List<String> gamers){
-        //TODO validation
+    public static Gamers of(List<String> gamers) {
+        if (gamers.stream()
+                .anyMatch(gamerName -> gamerName.length() > MAX_GAMER_STRING_LENGTH)) {
+            throw new IllegalArgumentException(String.format(INSTANTIATE_ERROR_FORMAT, gamers));
+        }
         return new Gamers(gamers);
     }
 
-    public int getLadderNumber(String gamer){
-        return 0;
+    public int getLadderNumber(String gamer) {
+        return gamers.indexOf(gamer);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gamers gamers1 = (Gamers) o;
+        return Objects.equals(gamers, gamers1.gamers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gamers);
+    }
+
+    @Override
+    public String toString() {
+        return "Gamers{" +
+                "gamers=" + gamers +
+                '}';
+    }
+
+    public int size() {
+        return gamers.size();
     }
 }
--- src/test/java/nextstep/ladder/domain/gamer/GamersTest.java
+++ src/test/java/nextstep/ladder/domain/gamer/GamersTest.java
@@ -1,7 +1,40 @@
 package nextstep.ladder.domain.gamer;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class GamersTest {
+
+    @Test
+    void of(){
+        Gamers gamers = Gamers.of(Arrays.asList("pobi","unit1","test"));
+        assertThat(gamers)
+                .isEqualTo(Gamers.of(Arrays.asList("pobi","unit1","test")));
+    }
+
+    @Test
+    void ofException(){
+        assertThatThrownBy(()->Gamers.of(Collections.singletonList("tester")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void size(){
+        Gamers gamers = Gamers.of(Arrays.asList("pobi","unit1","test"));
+        assertThat(gamers.size())
+                .isEqualTo(3);
+    }
+
+    @Test
+    void getNumber(){
+        Gamers gamers = Gamers.of(Arrays.asList("pobi","unit1","test"));
+        assertThat(gamers.getLadderNumber("test"))
+                .isEqualTo(2);
+    }
 
 }
