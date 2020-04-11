a14afdb6bb7be3a8326c4dc16d7a37c9ea535c07
yohan <yohan.lee@uneedcomms.com>
2020-04-11 23:27:32 +0900

    refactor: Controller 구현

--- src/main/java/ladder/LadderGameController.java
+++ src/main/java/ladder/LadderGameController.java
@@ -1,12 +1,24 @@
 package ladder;
 
 import ladder.domain.LadderGame;
+import ladder.domain.LadderGameResult;
+import ladder.domain.LadderRewards;
 import ladder.view.InputView;
 import ladder.view.ResultView;
 
 public class LadderGameController {
 
     public static void main(String[] args) {
-        ResultView.print(LadderGame.of(InputView.inputNames(), InputView.inputHeight()));
+        String names = InputView.inputNames();
+        LadderRewards ladderRewards = LadderRewards.of(InputView.inputRewards());
+        int height = InputView.inputHeight();
+
+        LadderGame ladderGame = LadderGame.of(names, height);
+
+        ResultView.print(ladderGame, ladderRewards);
+
+        LadderGameResult ladderGameResult = ladderGame.start(ladderRewards);
+        String result = InputView.inputResult();
+        ResultView.printResult(ladderGameResult.getNodeResults(result));
     }
 }
