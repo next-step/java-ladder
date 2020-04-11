bf12e74602e129b9a9df13a12dc0c4a695c8840f
Yeoseon Yun <devyyskr@gmail.com>
2020-04-12 01:57:15 +0900

    test(Line, Point, Direction): 테스트 코드 추가

--- src/test/java/nextstep/ladder/Line2Test.java
+++ src/test/java/nextstep/ladder/Line2Test.java
@@ -1,4 +1,18 @@
 package nextstep.ladder;
 
+import nextstep.ladder.domain.Line2;
+import org.junit.jupiter.api.Test;
+
 public class Line2Test {
+    @Test
+    public void init() {
+        int sizeOfPerson = 5;
+        System.out.println(Line2.init(sizeOfPerson));
+    }
+
+    @Test
+    public void move() {
+        Line2 line = Line2.init(2);
+        System.out.println("ladder result : " + line.move(0));
+    }
 }
--- src/test/java/nextstep/ladder/Point2Test.java
+++ src/test/java/nextstep/ladder/Point2Test.java
@@ -1,4 +1,40 @@
 package nextstep.ladder;
 
+import nextstep.ladder.domain.Point2;
+import org.junit.jupiter.api.Test;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Point2Test {
+    @Test
+    public void first() {
+        assertThat(Point2.first(TRUE).move(), is(1));
+        assertThat(Point2.first(FALSE).move(), is(0));
+    }
+
+    @Test
+    public void next_stay() {
+        Point2 second = Point2.first(FALSE).next(FALSE);
+        assertThat(second.move(), is(1));
+    }
+
+    @Test
+    public void next_left() {
+        Point2 second = Point2.first(TRUE).next(FALSE);
+        assertThat(second.move(), is(0));
+    }
+
+    @Test
+    public void next_right() {
+        Point2 second = Point2.first(FALSE).next(TRUE);
+        assertThat(second.move(), is(2));
+    }
+
+    @Test
+    public void next() {
+        Point2 second = Point2.first(TRUE).next();
+        assertThat(second.move(), is(0));
+    }
 }
