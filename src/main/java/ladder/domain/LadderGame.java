0c03fff28c92c892687c5824a46551e57e73d015
parkeeseul <eeseul.p@gmail.com>
2020-04-12 02:42:09 +0900

    refactor(LadderGame): 람다 리팩토링

--- src/main/java/ladder/domain/LadderGame.java
+++ src/main/java/ladder/domain/LadderGame.java
@@ -4,9 +4,10 @@ import ladder.exception.ExceptionType;
 import ladder.exception.LadderException;
 import lombok.Getter;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LadderGame {
     private static final String REGEX = ",";
@@ -27,10 +28,9 @@ public class LadderGame {
         String[] userNames = userNameValues.split(REGEX);
         List<String> rewards = Arrays.asList(resultValues.split(REGEX));
 
-        List<LadderLine> ladderLines = new ArrayList<>();
-        for (int i = 0, end = Integer.parseInt(heightValue); i < end; i++) {
-            ladderLines.add(LadderLine.of(userNames.length));
-        }
+        List<LadderLine> ladderLines = IntStream.range(0, Integer.parseInt(heightValue))
+                .mapToObj(value -> LadderLine.of(userNames.length))
+                .collect(Collectors.toList());
 
         return new LadderGame(ladderLines, Arrays.asList(userNames), rewards);
     }
