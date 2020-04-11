e8205840f852db2f960163899c2e03fbf34ae807
etff <wish2rich@gmail.com>
2020-04-11 20:45:14 +0900

    style(Ladder): 변수이름 수정

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Ladder {
     private static final int MINIMUM_HEIGHT = 1;
-    private static final int MINNIMUM_USERS = 2;
+    private static final int MINIMUM_USERS = 2;
     private static final String MAXIMUM_HEIGHT_ERROR = "사다리 높이는 1이상 입력해야합니다";
     private static final String MAXIMUM_USER_ERROR = "사용자가 두명 이상이어야합니다.";
 
@@ -22,7 +22,7 @@ public class Ladder {
             throw new IllegalArgumentException(MAXIMUM_HEIGHT_ERROR);
         }
 
-        if (personCount < MINNIMUM_USERS) {
+        if (personCount < MINIMUM_USERS) {
             throw new IllegalArgumentException(MAXIMUM_USER_ERROR);
         }
     }
