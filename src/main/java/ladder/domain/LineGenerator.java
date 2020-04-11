61965a87dc220caea37f724a01a43dc798f70e62
jwee0330 <jwee0330@gmail.com>
2020-04-11 19:55:59 +0900

    feat(LineGenerator): 조건에 따라 Line을 생성해서 Line의 일급컬렉션인 List<Line>을 생성하는 Generator

---
+++ src/main/java/ladder/domain/LineGenerator.java
@@ -0,0 +1,32 @@
+package ladder.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LineGenerator {
+
+    static List<Line> first(int height) {
+        List<Line> firstLadderLine = new ArrayList<>();
+        for (int i = 0; i < height; i++) {
+            firstLadderLine.add(new Line(i, new RandomLinkable().get()));
+        }
+        return firstLadderLine;
+    }
+
+    static List<Line> last(int height) {
+        List<Line> lastLadderLine = new ArrayList<>();
+        for (int i = 0; i < height; i++) {
+            lastLadderLine.add(new Line(i, false));
+        }
+        return lastLadderLine;
+    }
+
+    static List<Line> normal(int height, LadderLine beforeLadderLine) {
+        List<Line> normalLadderLine = new ArrayList<>();
+        for (int i = 0; i < height; i++) {
+            Line beforeLine = beforeLadderLine.get(i);
+            normalLadderLine.add(Line.newLineOnBeforeLine(beforeLine));
+        }
+        return normalLadderLine;
+    }
+}
