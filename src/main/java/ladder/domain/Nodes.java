32a0c85021b022e0a9ac1345f7e302b2cc0f9499
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-12 15:26:06 +0900

    feat (Nodes.java) 일급 컬렉션 추가

---
+++ src/main/java/ladder/domain/Nodes.java
@@ -0,0 +1,11 @@
+package ladder.domain;
+
+import java.util.List;
+
+public class Nodes {
+    private final List<Node> nodes;
+
+    public Nodes(List<Node> nodes) {
+        this.nodes = nodes;
+    }
+}
