198c3c72c3c2b3a02b7a4680ff8afc7d710047ed
lucas <lucas@zoyi.co>
2020-04-12 17:22:44 +0900

    feat: Players class

--- src/main/java/nextstep/ladder/Players.java
+++ src/main/java/nextstep/ladder/Players.java
@@ -1,15 +1,20 @@
 package nextstep.ladder;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Players {
 
   private List<Player> players;
 
   public Players(List<String> names) {
+    players = IntStream.range(0, names.size())
+        .mapToObj(position -> new Player(names.get(position), position))
+        .collect(Collectors.toList());
   }
 
   public List<Player> getPlayers() {
-    return null;
+    return players;
   }
 }
