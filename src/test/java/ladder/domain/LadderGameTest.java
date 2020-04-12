4d4a21c59900c4691bc14da9a09e1ada5966b773
ookhub <damlook@naver.com>
2020-04-12 23:21:15 +0900

    test(LadderGame): add ladderGame claim test

--- src/test/java/ladder/domain/LadderGameTest.java
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -8,4 +8,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LadderGameTest {
 
+    @ParameterizedTest
+    @CsvSource(value = {"pobi,honux,crong,jk pobi:0", "pobi,honux,crong crong:2", "pobi,honux pobi:0"}, delimiter = ':')
+    void userLadderClaimTest(String input, String expected) {
+        String[] inputs = input.split(" ");
+        Users users = Users.of(inputs[0]);
+        LadderMap ladderMap = LadderMap.of(users, 4, () -> true);
+
+        LadderGame ladderGame = LadderGame.of(users, ladderMap);
+        int result = ladderGame.userClaim(inputs[1]);
+
+        assertThat(result).isEqualTo(Integer.parseInt(expected));
+    }
 }
