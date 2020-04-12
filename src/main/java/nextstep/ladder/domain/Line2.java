1443fff156e57ec97546b6943cb402babf3f2cbc
Yeoseon Yun <devyyskr@gmail.com>
2020-04-12 14:24:34 +0900

    feat(Line2): Line2 프로덕션 예제 적용

--- src/main/java/nextstep/ladder/domain/Line2.java
+++ src/main/java/nextstep/ladder/domain/Line2.java
@@ -1,5 +1,52 @@
 package nextstep.ladder.domain;
 
-public class Line2 {
+import nextstep.ladder.RandomBooleanProvider;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Line2 {
+    private final List<Point> points;
+
+    public Line2(List<Point> points) {
+        this.points = points;
+    }
+
+    public int move(int position) {
+        return points.get(position).move();
+    }
+
+    public static Line2 init(int sizeOfPerson) {
+        List<Point> points = new ArrayList<>();
+        Point point = initFirst(points);
+        point = initBody(sizeOfPerson, points, point);
+        initLast(points, point);
+        return new Line2(points);
+    }
+
+    private static Point initBody(int sizeOfPerson, List<Point> points, Point point) {
+        for (int i = 1; i < sizeOfPerson - 1; i++) {
+            point = point.next();
+            points.add(point);
+        }
+        return point;
+    }
+
+    private static void initLast(List<Point> points, Point point) {
+        point = point.last();
+        points.add(point);
+    }
+
+    private static Point initFirst(List<Point> points) {
+        Point point = Point.first(RandomBooleanProvider.getRandomBoolean());
+        points.add(point);
+        return point;
+    }
+
+    @Override
+    public String toString() {
+        return "LadderLine{" +
+                "points=" + points +
+                '}';
+    }
 }
