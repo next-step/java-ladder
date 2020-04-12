064c8e4d650c0b73bb09a4fc6f0f65b8c69537a0
crystal <crystal2840@gmail.com>
2020-04-13 02:49:51 +0900

    컨트롤러 변경된 클래스로 변경작업

--- src/main/java/nextstep/ladder/LadderController.java
+++ src/main/java/nextstep/ladder/LadderController.java
@@ -2,16 +2,19 @@ package nextstep.ladder;
 
 import nextstep.ladder.domain.GameInfo;
 import nextstep.ladder.domain.Ladder;
+import nextstep.ladder.domain.Ladder2;
 import nextstep.ladder.domain.PlayLadderGame;
+import nextstep.ladder.domain.PlayLadderGame2;
 import nextstep.ladder.domain.Results;
 import nextstep.ladder.domain.Users;
 import nextstep.ladder.view.InputView;
 import nextstep.ladder.view.ResultView;
+import nextstep.ladder.view.ResultView2;
 
 public class LadderController {
     private static InputView inputView = InputView.getInputView();
-    private static ResultView resultView = ResultView.getResultView();
-    private static PlayLadderGame playLadderGame = PlayLadderGame.getPlayLadderGame();
+    private static ResultView2 resultView = ResultView2.getResultView();
+    private static PlayLadderGame2 playLadderGame = PlayLadderGame2.getPlayLadderGame();
 
     public static void main(String[] args) {
         Users users = inputView.enterUserNames();
@@ -19,10 +22,9 @@ public class LadderController {
         GameInfo gameInfo = new GameInfo(users, results);
         int ladderHeight = inputView.enterLadderHeight();
 
-        Ladder ladder = new Ladder(users.getCountOfPerson(), ladderHeight);
+        Ladder2 ladder = new Ladder2(users.getCountOfPerson(), ladderHeight);
         resultView.printLadder(gameInfo, ladder);
         Users resultsForAllPlayers = playLadderGame.generateResultsForAllPlayers(gameInfo, ladder);
-
         resultView.repeatPrintPlayResult(resultsForAllPlayers);
     }
 }
