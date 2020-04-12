a0876afb0b7804286fda3e516d7d1dc010783d57
crystal <crystal2840@gmail.com>
2020-04-12 15:05:22 +0900

    변경 불가능한 리스트로 만들어준다.

--- src/main/java/nextstep/ladder/domain/Results.java
+++ src/main/java/nextstep/ladder/domain/Results.java
@@ -1,12 +1,13 @@
 package nextstep.ladder.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Results {
     private List<Result> results;
 
     public Results(List<Result> results) {
-        this.results = results;
+        this.results = Collections.unmodifiableList(results);
     }
 
     public List<Result> getResults() {
