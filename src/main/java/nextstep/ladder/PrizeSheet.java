52d9a76bbc92fb31fead3ad144418990fe95f1cc
lucas <lucas@zoyi.co>
2020-04-13 17:31:01 +0900

    feat: PrizeSheet class

--- src/main/java/nextstep/ladder/PrizeSheet.java
+++ src/main/java/nextstep/ladder/PrizeSheet.java
@@ -1,14 +1,31 @@
 package nextstep.ladder;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import nextstep.ladder.model.Position;
 import nextstep.ladder.model.Prize;
 
 public class PrizeSheet {
+  private Map<Position, Prize> prizeSheet = new HashMap<>();
 
   public PrizeSheet(List<String> prizes) {
+    IntStream.range(0, prizes.size())
+        .forEach(position ->
+            prizeSheet.put(Position.at(position), new Prize(prizes.get(position))));
   }
 
   public Prize checkPrize(Player player) {
-    return null;
+    return prizeSheet.get(player.getPosition());
+  }
+
+  public ResultSheet produceResult(Players players) {
+    List<Prize> prizes = players.getPlayers().stream()
+        .map(this::checkPrize)
+        .collect(Collectors.toList());
+
+    return new ResultSheet(players, prizes);
   }
 }
---
+++ src/main/java/nextstep/ladder/ResultSheet.java
@@ -0,0 +1,25 @@
+package nextstep.ladder;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+import nextstep.ladder.model.Name;
+import nextstep.ladder.model.Prize;
+
+public class ResultSheet {
+  private Map<Name, Prize> resultSheet = new HashMap<>();
+
+  public ResultSheet(Players players, List<Prize> prizes) {
+    List<Player> playerList = players.getPlayers();
+    if (playerList.size() != prizes.size()) {
+      throw new IllegalArgumentException("선수의 수와 상의 개수가 같아야합니다.");
+    }
+
+    IntStream.range(0, prizes.size())
+        .forEach(index -> {
+          Player player = playerList.get(index);
+          resultSheet.put(player.getName(), prizes.get(index));
+        });
+  }
+}
