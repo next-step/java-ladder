5f2bc2c6ec2844de55d2ca6b124c20f3f1e25011
jwee0330 <jwee0330@gmail.com>
2020-04-15 00:23:20 +0900

    refactor(linkStrategy): 피드백을 받아 interface의 장점을 살릴수 있도록 클래스 변수 제거

--- src/main/java/ladder/ladder/infrastructure/RandomLinkStrategy.java
+++ src/main/java/ladder/ladder/infrastructure/RandomLinkStrategy.java
@@ -2,14 +2,14 @@ package ladder.ladder.infrastructure;
 
 import ladder.ladder.domain.LinkStrategy;
 import ladder.ladder.util.RandomUtils;
+import org.graalvm.compiler.hotspot.nodes.profiling.RandomSeedNode;
 
 import java.util.Random;
 
 public class RandomLinkStrategy implements LinkStrategy {
-    private final Random random = RandomUtils.RANDOM;
 
     @Override
     public boolean get() {
-        return random.nextBoolean();
+        return new Random().nextBoolean();
     }
 }
