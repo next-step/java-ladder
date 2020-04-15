f17e6e6584751fb34bb9d6cd67d031fe07dba5ab
jwee0330 <jwee0330@gmail.com>
2020-04-15 22:27:38 +0900

    chore(delete): 패키지 이동으로 발생한 파일 삭제

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -98,10 +98,7 @@ public class User {
         } else if (!age.equals(other.age))
             return false;
         if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        return true;
+            return other.name == null;
+        } else return name.equals(other.name);
     }
 }
