a65d0df44090c9069219a613e438aeefe9871794
crystal <crystal2840@gmail.com>
2020-04-15 13:38:11 +0900

    자기생성자 호출하도록 변경

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -1,26 +1,27 @@
 package nextstep.ladder.domain;
 
+import static java.util.Collections.unmodifiableList;
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Ladder {
     private List<LadderLine> ladder;
 
     public Ladder(int countOfPerson, int height) {
-        this.ladder = createLadder(countOfPerson, height);
+        this(createLadder(countOfPerson, height));
     }
 
     public Ladder(List<LadderLine> ladder) {
-        this.ladder = Collections.unmodifiableList(new ArrayList<>(ladder));
+        this.ladder = unmodifiableList(new ArrayList<>(ladder));
     }
 
-    public List<LadderLine> createLadder(int countOfPerson, int height) {
+    public static List<LadderLine> createLadder(int countOfPerson, int height) {
         List<LadderLine> ladder = new ArrayList<>();
         for (int i = 0; i < height; i++) {
             ladder.add(LadderLine.init(countOfPerson));
         }
-        return Collections.unmodifiableList(new ArrayList<>(ladder));
+        return unmodifiableList(new ArrayList<>(ladder));
     }
 
     public List<LadderLine> getLadder() {
