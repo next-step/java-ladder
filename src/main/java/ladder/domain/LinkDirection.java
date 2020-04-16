d135d5d4af169ca1414661a54b45199cc28a4012
yohan <yohan.lee@uneedcomms.com>
2020-04-16 21:38:29 +0900

    refactor: 재설계로 인한 패키지 삭제

--- src/main/java/ladder/domain/LinkDirection.java
+++
@@ -1,5 +0,0 @@
-package ladder.domain;
-
-public interface LinkDirection {
-    int checkDirection(Position position);
-}
--- src/main/java/ladder/domain/LinkStrategy.java
+++
@@ -1,5 +0,0 @@
-package ladder.domain;
-
-public interface LinkStrategy {
-    boolean isConnectable();
-}
