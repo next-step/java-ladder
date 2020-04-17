94517f55790eb834924dc075b6bc91f42e0a7cc2
jwee0330 <jwee0330@gmail.com>
2020-04-18 05:33:35 +0900

    refactor(view): 뷰와 드라이브 메소드 리팩토링

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -1,141 +1,130 @@
 package ladder.view;
 
-import ladder.game.dto.LadderGameDto;
+import ladder.game.ui.LadderGame;
 import ladder.ladder.domain.Ladder;
 import ladder.ladder.domain.LadderLine;
-import ladder.ladder.domain.Line;
+import ladder.ladder.domain.Point;
 import ladder.player.domain.Player;
-import ladder.player.domain.Players;
 import ladder.prize.domain.Prize;
-import ladder.prize.domain.Prizes;
 
 import java.io.PrintStream;
-import java.util.Collection;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.toList;
+import static java.util.Comparator.comparing;
 
 public class OutputView {
     private static final int ZERO = 0;
-    private static final int NUMBER_ONE = 1;
-    private static final String LADDER_LINE_MARKER = "|";
+    private static final String LADDER_MARKER = "|";
     private static final String LINE_MARKER = "-";
     private static final String BLANK_MARKER = " ";
     private static final String CARRIAGE_RETURN = "\n";
+    private static final String COLON = " : ";
     private static final PrintStream CONSOLE = System.out;
 
     private OutputView() {
     }
 
-    public static void print(LadderGameDto ladderGameDTO) {
+    public static void print(LadderGame ladderGame) {
+        List<String> players = collectPlayerString(ladderGame);
+        List<String> prizes = collectPrizeString(ladderGame);
+        int maxLength = checkMaxLength(players, prizes);
+
         StringBuilder stringBuilder = new StringBuilder();
-        Players players = ladderGameDTO.getPlayers();
+        stringBuilder.append(makeSpacing(players, maxLength));
+        stringBuilder.append(drawLadder(ladderGame.getLadder(), maxLength));
+        stringBuilder.append(makeSpacing(prizes, maxLength));
 
-        int maxNameLength = getMaxNameLength(ladderGameDTO);
-        stringBuilder.append(alignSpacingColumns(players.getPlayerNames(), maxNameLength));
-
-        Ladder ladder = ladderGameDTO.getLadder();
-        int height = ladder.getHeight();
-        for (int i = 0; i < height; i++) {
-            stringBuilder.append(drawOnlyHeight(i, ladderGameDTO))
-                    .append(CARRIAGE_RETURN);
-        }
-
-        Prizes prizes = ladderGameDTO.getPrizes();
-        stringBuilder.append(alignSpacingColumns(prizes.getPrizeNames(), maxNameLength));
         CONSOLE.println(stringBuilder.toString());
     }
 
-    public static void print(Map<Player, Prize> ladderGameResults) {
-        CONSOLE.println("실행 결과");
-        ladderGameResults.entrySet()
-                .forEach(entry -> CONSOLE.println(entry.getKey().getName() + " : " + entry.getValue().getPrize()));
+    public static void print(Map<Player, Prize> allPlayersResult) {
+        allPlayersResult.forEach((key, value) ->
+                CONSOLE.println(key.getName() + COLON + value.getPrize()));
     }
 
-    private static String alignSpacingColumns(List<String> columns, int maxNameLength) {
+    private static List<String> collectPlayerString(LadderGame ladderGame) {
+        return ladderGame.getPlayers()
+                .getPlayers()
+                .stream()
+                .map(Player::getName)
+                .collect(Collectors.toList());
+    }
+
+    private static List<String> collectPrizeString(LadderGame ladderGame) {
+        return ladderGame.getPrizes()
+                .getPrizes()
+                .stream()
+                .map(Prize::getPrize)
+                .collect(Collectors.toList());
+    }
+
+    private static String drawLadder(Ladder ladder, int maxLength) {
         StringBuilder stringBuilder = new StringBuilder();
-        String firstColumn = columns.get(ZERO);
-        stringBuilder.append(String.format("%-" + maxNameLength + "s", firstColumn))
-                .append(BLANK_MARKER);
-
-        int countOfPerson = columns.size();
-        for (int i = NUMBER_ONE; i < countOfPerson; i++) {
-            String name = columns.get(i);
-            stringBuilder.append(String.format("%" + maxNameLength + "s", name))
-                    .append(BLANK_MARKER);
+        for (LadderLine ladderLine : ladder.getLadder()) {
+            stringBuilder.append(drawTillMaxLength(BLANK_MARKER, maxLength));
+            stringBuilder.append(drawLadderLine(ladderLine, maxLength));
         }
-
-        return stringBuilder.append(CARRIAGE_RETURN)
-                .toString();
+        return stringBuilder.append(CARRIAGE_RETURN).toString();
     }
 
-    private static int getMaxNameLength(LadderGameDto ladderGameDTO) {
-        Players players = ladderGameDTO.getPlayers();
-        Prizes prizes = ladderGameDTO.getPrizes();
-
-        int logiestPlayerName = getLogiestLength(players.getPlayerNames());
-        int logiestPrizeName = getLogiestLength(prizes.getPrizeNames());
-
-        if (logiestPlayerName > logiestPrizeName) {
-            return logiestPlayerName;
-        }
-        return logiestPrizeName;
-    }
-
-    private static int getLogiestLength(List<String> columns) {
-        return columns.stream()
-                .max(Comparator.comparing(String::length))
-                .orElseThrow(IllegalArgumentException::new)
-                .length();
-    }
-
-    private static String drawOnlyHeight(int height, LadderGameDto ladderGameDto) {
-        Ladder ladder = ladderGameDto.getLadder();
-        List<Line> linesAtSameHeight = getLinesAtSameHeight(height, ladder.getLadderLines());
+    private static String drawLadderLine(LadderLine ladderLine, int maxLength) {
         StringBuilder stringBuilder = new StringBuilder();
+        for (Point point : ladderLine.getPoints()) {
+            stringBuilder.append(drawPoint(point, maxLength));
+        }
+        return stringBuilder.append(CARRIAGE_RETURN).toString();
+    }
 
-        int maxNameLength = getMaxNameLength(ladderGameDto);
-        stringBuilder.append(spaceAppend(maxNameLength));
-
-        for (Line line : linesAtSameHeight) {
-            stringBuilder.append(LADDER_LINE_MARKER);
-            stringBuilder.append(drawLine(line, maxNameLength));
+    private static String drawPoint(Point point, int maxLength) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(LADDER_MARKER);
+        if (point.getDirection().isRight()) {
+            stringBuilder.append(drawTillMaxLength(LINE_MARKER, maxLength));
+        }
+        if (!point.getDirection().isRight()) {
+            stringBuilder.append(drawTillMaxLength(BLANK_MARKER, maxLength));
         }
         return stringBuilder.toString();
     }
 
-    private static List<Line> getLinesAtSameHeight(int height, List<LadderLine> ladder) {
-        return ladder.stream()
-                .map(LadderLine::getLines)
-                .flatMap(Collection::stream)
-                .filter(line -> line.getHeight() == height)
-                .collect(toList());
-    }
-
-    private static String spaceAppend(int maxNameLength) {
+    private static String drawTillMaxLength(String marker, int maxLength) {
         StringBuilder stringBuilder = new StringBuilder();
-        int spaceLength = Math.subtractExact(maxNameLength, NUMBER_ONE);
-        for (int i = 0; i < spaceLength; i++) {
-            stringBuilder.append(BLANK_MARKER);
-        }
-        return stringBuilder.toString();
-    }
-
-    private static String drawLine(Line line, int maxNameLength) {
-        if (line.isLink()) {
-            return drawTillMaxLength(LINE_MARKER, maxNameLength);
-        }
-        return drawTillMaxLength(BLANK_MARKER, maxNameLength);
-    }
-
-    private static String drawTillMaxLength(String marker, int maxNameLength) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < maxNameLength; i++) {
+        for (int i = 0; i < maxLength; i++) {
             stringBuilder.append(marker);
         }
         return stringBuilder.toString();
+    }
+
+    private static String makeSpacing(List<String> targets, int maxLength) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String leftAlignName = targets.get(ZERO);
+        stringBuilder.append(String.format("%-" + maxLength + "s ", leftAlignName));
+
+        for (int i = 1; i < targets.size(); i++) {
+            String rightAlignName = targets.get(i);
+            stringBuilder.append(String.format("%" + maxLength + "s ", rightAlignName));
+        }
+        return stringBuilder.append(CARRIAGE_RETURN).toString();
+    }
+
+    private static int checkMaxLength(List<String> players, List<String> prizes) {
+        String maxName = getLongestName(players);
+        String maxPrize = getLogiestPrize(prizes);
+        return Math.max(maxName.length(), maxPrize.length());
+    }
+
+    private static String getLongestName(List<String> players) {
+        return players.stream()
+                .max(comparing(String::length))
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private static String getLogiestPrize(List<String> prizes) {
+        return prizes.stream()
+                .max(comparing(String::length))
+                .orElseThrow(IllegalArgumentException::new);
     }
 
 }
