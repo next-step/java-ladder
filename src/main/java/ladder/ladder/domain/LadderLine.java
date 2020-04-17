e1d3a361482212b62344d5aa4761722003f20d73
jwee0330 <jwee0330@gmail.com>
2020-04-18 05:29:26 +0900

    refactor(ladderLine): 기존의 세로줄로 바라보던 관점에서 가로줄로 바라보는 관점으로 리팩토링

--- src/main/java/ladder/ladder/domain/LadderLine.java
+++ src/main/java/ladder/ladder/domain/LadderLine.java
@@ -1,4 +1,74 @@
 package ladder.ladder.domain;
 
+import ladder.ladder.infrastructure.RandomLinkStrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class LadderLine {
+    private static final int ONE = 1;
+
+    private final List<Point> points;
+
+    private LadderLine(List<Point> points) {
+        this.points = points;
+    }
+
+    public static LadderLine of(List<Point> points) {
+        return new LadderLine(points);
+    }
+
+    public static LadderLine init(int sizeOfPlayer) {
+        List<Point> initPoints = new ArrayList<>();
+        Point firstPoint = initFirst();
+        initPoints.add(firstPoint);
+
+        final int bodySize = sizeOfPlayer - ONE;
+        initPoints.addAll(initBody(bodySize, firstPoint));
+
+        Point lastPoint = initLast(initPoints.get(bodySize - ONE));
+        initPoints.add(lastPoint);
+
+        return new LadderLine(initPoints);
+    }
+
+    private static Point initFirst() {
+        return Point.generateFirst(new RandomLinkStrategy().get());
+    }
+
+    private static List<Point> initBody(int bodySize, Point point) {
+        List<Point> bodyPoints = new ArrayList<>();
+        for (int i = 1; i < bodySize; i++) {
+            point = point.generateNext(new RandomLinkStrategy());
+            bodyPoints.add(point);
+        }
+        return bodyPoints;
+    }
+
+    private static Point initLast(Point point) {
+        return point.generateLast();
+    }
+
+    public Index move(Index position) {
+        int currentIndex = position.getIndex();
+        return points.get(currentIndex)
+                .move();
+    }
+
+    public List<Point> getPoints() {
+        return new ArrayList<>(points);
+    }
+
+    public int size() {
+        return points.size();
+    }
+
+    @Override
+    public String toString() {
+        return "LadderLine{" +
+                "points=" + points +
+                '}';
+    }
+
 }
--- src/test/java/ladder/ladder/domain/LadderLineTest.java
+++ src/test/java/ladder/ladder/domain/LadderLineTest.java
@@ -1,5 +1,33 @@
-import static org.junit.jupiter.api.Assertions.*;
+package ladder.ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LadderLineTest {
+
+    @DisplayName("참여자 수 만큼 사다리 한줄이 생긴다 (가로줄)")
+    @Test
+    public void init() {
+        int sizeOfPlayer = 5;
+        LadderLine ladderLine = LadderLine.init(sizeOfPlayer);
+
+        assertThat(ladderLine.size()).isEqualTo(sizeOfPlayer);
+    }
+
+    @DisplayName("사다리줄에서 현재 포지션을을 입력 후 움직이면 방향에 따라 포지션이 변경된다.")
+    @Test
+    public void move() {
+        Point first = Point.of(0, Direction.of(FALSE, TRUE));
+        Point second = Point.of(1, Direction.of(FALSE, FALSE));
+        LadderLine ladderLine = LadderLine.of(Arrays.asList(first, second));
+
+        assertThat(ladderLine.move(Index.of(0))).isEqualTo(Index.of(1));
+    }
 
 }
