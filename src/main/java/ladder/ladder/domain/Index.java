20050790d1dea3b153134791fba3ea5a0053bf1e
jwee0330 <jwee0330@gmail.com>
2020-04-18 05:25:45 +0900

    refactor(index): 라인의 위치값을 나타내기 위한 index 추가

--- src/main/java/ladder/ladder/domain/Index.java
+++ src/main/java/ladder/ladder/domain/Index.java
@@ -1,4 +1,61 @@
 package ladder.ladder.domain;
 
+import java.util.Objects;
+
 public class Index {
+    private static final int ZERO = 0;
+    private static final int INDEX_DEFAULT_STEP = 1;
+
+    private final int index;
+
+    private Index(int index) {
+        validateNegative(index);
+        this.index = index;
+    }
+
+    public static Index init() {
+        return new Index(ZERO);
+    }
+
+    public static Index of(int index) {
+        return new Index(index);
+    }
+
+    private void validateNegative(int index) {
+        if (index < ZERO) {
+            throw new NegativeIndexException();
+        }
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public Index increase() {
+        return Index.of(index + INDEX_DEFAULT_STEP);
+    }
+
+    public Index decrease() {
+        return Index.of(index - INDEX_DEFAULT_STEP);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Index)) return false;
+        Index index1 = (Index) o;
+        return getIndex() == index1.getIndex();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getIndex());
+    }
+
+    @Override
+    public String toString() {
+        return "Index{" +
+                "index=" + index +
+                '}';
+    }
 }
--- src/test/java/ladder/ladder/domain/IndexTest.java
+++ src/test/java/ladder/ladder/domain/IndexTest.java
@@ -1,5 +1,44 @@
-import static org.junit.jupiter.api.Assertions.*;
+package ladder.ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class IndexTest {
+
+    @DisplayName("인덱스를 초기화 할 수 있다")
+    @Test
+    void init() {
+        assertThat(Index.init()).isEqualTo(Index.of(0));
+    }
+
+    @DisplayName("인덱스는 음수를 가질 수 없다")
+    @Test
+    public void initNegative() {
+        assertThatThrownBy(() -> Index.of(-1))
+                .isInstanceOf(NegativeIndexException.class)
+                .hasMessageContaining("인덱스는 음수가 될 수 없습니다.");
+    }
+
+    @DisplayName("인덱스를 증가 시킬때마다 1 만큼 증가한다")
+    @Test
+    public void increase() {
+        assertThat(Index.of(0)).satisfies(index -> {
+            assertThat(index.increase()).isEqualTo(Index.of(1));
+            assertThat(index.increase().increase()).isEqualTo(Index.of(2));
+        });
+    }
+
+    @DisplayName("인덱스를 감소 시킬때마다 1 만큼 증가한다")
+    @Test
+    public void decrease() {
+        assertThat(Index.of(2)).satisfies(index -> {
+            assertThat(index.decrease()).isEqualTo(Index.of(1));
+            assertThat(index.decrease().decrease()).isEqualTo(Index.of(0));
+        });
+    }
+
 
 }
