b5ca5cd480457ecd1d509270e400928f7e78ef39
yohan <yohan.lee@uneedcomms.com>
2020-04-19 02:27:39 +0900

    refactor(LadderResults): getResultPosition 메서드 구현

--- src/main/java/ladder/LadderResults.java
+++ src/main/java/ladder/LadderResults.java
@@ -10,6 +10,10 @@ public class LadderResults {
         this.results = results;
     }
 
+    public Position getResultPosition(int position) {
+        return results.get(new Position(position));
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
