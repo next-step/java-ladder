e75ae034a385bdfbe815baa48bb5ae528dcdadcc
kiyeon <kyeoneee@gmail.com>
2020-05-03 21:02:45 +0900

    feat : step5 - Direction, Point 테스트코드 추가

---
+++ src/main/java/ladder/domain/Direction.java
@@ -0,0 +1,26 @@
+package ladder.domain;
+
+public class Direction {
+    public static Direction of(boolean b, boolean b1) {
+    }
+
+    public static Direction first(Boolean aTrue) {
+        return null;
+    }
+
+    public Direction next() {
+        return null;
+    }
+
+    public Direction next(Boolean nextRight) {
+        return null;
+    }
+
+    public boolean isLeft() {
+        return false;
+    }
+
+    public Direction last() {
+        return null;
+    }
+}
---
+++ src/main/java/ladder/domain/Point.java
@@ -0,0 +1,19 @@
+package ladder.domain;
+
+public class Point {
+    public static Point first(Boolean aTrue) {
+        return null;
+    }
+
+    public int move() {
+        return -1;
+    }
+
+    public Point next(Boolean aFalse) {
+        return null;
+    }
+
+    public Point next() {
+        return null;
+    }
+}
