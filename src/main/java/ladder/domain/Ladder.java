05fd3583a501c9957cbc5b3d3ec42f297c11adf7
kiyeon <kyeoneee@gmail.com>
2020-05-05 01:32:13 +0900

    refactor (ladder) : step4 - Line 객체 변경에 따른 Ladder 객체 리팩토링

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -3,9 +3,7 @@ package ladder.domain;
 import ladder.view.exception.InvalidLadderHeightException;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Ladder {
     private static final int LINE_COUNT_MIN = 1;
@@ -13,29 +11,23 @@ public class Ladder {
     private List<Line> lines;
 
     private Ladder(List<Line> lines) {
+        this.lines = lines;
     }
 
-    public static Ladder getInstance(int height, int width) {
+    public static Ladder getInstance(int height, int countOfPerson) {
         if (height < LINE_COUNT_MIN) {
             throw new InvalidLadderHeightException();
         }
 
-//        DirectionGenerator directionGenerator = new DefaultDirectionGenerator();
-//        List<Line> lines = new ArrayList<>();
-//        for (int i = 0; i < height; i++) {
-//            lines.add(Line.init(width, directionGenerator));
-//        }
+        List<Line> lines = new ArrayList<>();
+        for (int i = 0; i < height; i++) {
+            lines.add(Line.init(countOfPerson));
+        }
 
-        return new Ladder(Arrays.asList(Line.init(width)));
-    }
-
-    public static Ladder getInstance(List<Line> lines) {
         return new Ladder(lines);
     }
 
-
     public boolean isHeightSame(int height) {
-        return height == lines.size();
+        return lines.size() == height;
     }
-
 }
