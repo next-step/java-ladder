1d303c34b192b07002200a632ef65172337bcdbf
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-26 21:10:00 +0900

    refactor(Lambda): sumAll
    - 객체지향 생활체조 원칙을 지키기 위해 if문에서 처리하는 로직을 별도 메서드로 분리

--- src/test/java/nextstep/fp/LambdaTest.java
+++ src/test/java/nextstep/fp/LambdaTest.java
@@ -1,7 +1,10 @@
 package nextstep.fp;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -47,5 +50,13 @@ public class LambdaTest {
     public void sumAllOverThree() throws Exception {
         int sum = Lambda.sumAll(numbers, number -> number > 3);
         assertThat(sum).isEqualTo(15);
+    }
+
+    @DisplayName("조건에 맞는 숫자는 해당 숫자를 반환하고, 조건에 맞지 않는 숫자는 0을 반환한다")
+    @ParameterizedTest
+    @CsvSource(value = {"4:4", "2:0"}, delimiter = ':')
+    public void conditionMatchNumber(int inputNumber, int result) {
+        Integer resultNumber = Lambda.conditionMatchNumber(inputNumber, number -> number > 3);
+        assertThat(resultNumber).isEqualTo(result);
     }
 }
