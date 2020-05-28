c9ca7b54b69b59606d5d8e740d81018489422dda
ka0oll <60064643+ka0oll@users.noreply.github.com>
2020-05-29 00:31:15 +0900

    1단계 - 자바8 스트림, 람다, Optional (#388)
    * feat 람다 실습 1 - 익명 클래스를 람다로 전환
    
    * feat 람다 실습 2 - 람다를 활용해 중복 제거
    
    * feat stream method 실습
    
    * feat optional 실습
    
    Co-authored-by: ka0oll <jjyoung12@naver.com>

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,8 @@
 package nextstep.optional;
 
+import java.util.Optional;
+import java.util.function.Predicate;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,7 +36,10 @@ public class User {
     }
 
     public static boolean ageIsInRange2(User user) {
-        return false;
+        return Optional.ofNullable(user)
+            .map(User::getAge)
+            .filter(integer -> user.getAge() >= 30 && user.getAge() <= 45)
+            .isPresent();
     }
 
     @Override
