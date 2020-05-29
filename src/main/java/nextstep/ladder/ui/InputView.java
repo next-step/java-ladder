ba8f913e9df1716468a01f60c9d6144479eb95a2
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-29 09:24:48 +0900

    feat(InputView): createByUserInput
    - 콘솔로 사용자 입력을 받아서 객체를 생성할 수 있는 정적 팩터리 메서드 구현

--- src/main/java/nextstep/ladder/ui/InputView.java
+++ src/main/java/nextstep/ladder/ui/InputView.java
@@ -2,6 +2,8 @@ package nextstep.ladder.ui;
 
 import nextstep.ladder.domain.PlayerNames;
 
+import java.util.Scanner;
+
 public class InputView {
     private PlayerNames playerNames;
     private int maxLadderHeight;
@@ -9,5 +11,23 @@ public class InputView {
     public InputView(String playerList, int maxLadderHeight) {
         this.playerNames = PlayerNames.create(playerList);
         this.maxLadderHeight = maxLadderHeight;
+    }
+
+    public static InputView createByUserInput(Scanner scanner) {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        String playerList = scanner.nextLine();
+
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        int maxLadderHeight = scanner.nextInt();
+
+        return new InputView(playerList, maxLadderHeight);
+    }
+
+    public PlayerNames getPlayerNames() {
+        return playerNames;
+    }
+
+    public int getMaxLadderHeight() {
+        return maxLadderHeight;
     }
 }
