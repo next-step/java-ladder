c3ddcafdee8d620a821735aa13b0a4e1a233b3a5
junilHwang <tjsdlf4261@naver.com>
2020-06-01 00:59:22 +0900

    test: ParticipantsTest - 참여자 수에 대한 검증 테스트 완료

--- src/test/java/ladder/step2/domain/ParticipantsTest.java
+++ src/test/java/ladder/step2/domain/ParticipantsTest.java
@@ -1,4 +1,24 @@
 package ladder.step2.domain;
 
+import ladder.step2.exception.ParticipantsMinimumSizeException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ParticipantsTest {
+
+  @DisplayName("참여자가 2명 미만일 경우에 ParticipantsMinimumSizeException 발생")
+  @ParameterizedTest
+  @ValueSource(strings = { "aa", "" })
+  void 참여자수_검증_테스트 (String participants) {
+    assertThatExceptionOfType(ParticipantsMinimumSizeException.class)
+      .isThrownBy(() -> Participants.ofString(participants));
+  }
 }
