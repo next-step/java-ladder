caffbfc3c0c42fd892e6fe74c934062013e6c67c
junilHwang <tjsdlf4261@naver.com>
2020-05-31 23:36:53 +0900

    refactor: Strategy를 singleton으로 변경

--- src/main/java/ladder/step2/domain/strategy/LadderLineStrategyImpl.java
+++ src/main/java/ladder/step2/domain/strategy/LadderLineStrategyImpl.java
@@ -3,7 +3,11 @@ package ladder.step2.domain.strategy;
 import java.util.Random;
 
 public class LadderLineStrategyImpl implements LadderLineStrategy {
+  private static final LadderLineStrategy instance = new LadderLineStrategyImpl();
   private static final Random random = new Random();
+
+  private LadderLineStrategyImpl () {}
+
   @Override
   public boolean createLine(boolean prev) {
     if (prev) {
@@ -11,5 +15,9 @@ public class LadderLineStrategyImpl implements LadderLineStrategy {
     }
 
     return random.nextBoolean();
+  }
+
+  public static LadderLineStrategy getInstance () {
+    return instance;
   }
 }
