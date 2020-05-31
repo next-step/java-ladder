00dd3e4efeeac5ae44be41c297226395b9ba1b17
ka0oll <jjyoung12@naver.com>
2020-05-31 11:33:39 +0900

    refactor domain 패키지로 이동

--- src/test/java/nextstep/ladder/domain/LineTest.java
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -1,10 +1,12 @@
-package nextstep.ladder;
+package nextstep.ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.List;
+import nextstep.ladder.domain.Line;
+import nextstep.ladder.domain.Point;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
