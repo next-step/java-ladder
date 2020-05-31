72af5bee0cec04f95bd6d2e7db5fd7a987c17187
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-01 08:56:12 +0900

    refactor(LineFactory): makeLineWhenMoreThanThwoPeople
    - 너무 구체적인 메서드명을 피하기 위해 makeLine으로 메서드명 변경
    - 변경된 이름에 맞게 책임을 수행하기 위해 게임 참여자가 1명인 경우도 해당 메서드에서 처리

--- src/main/java/nextstep/ladder/domain/LineFactory.java
+++ src/main/java/nextstep/ladder/domain/LineFactory.java
@@ -3,7 +3,6 @@ package nextstep.ladder.domain;
 import nextstep.ladder.domain.exceptions.PointsNeedMoreThanOnePersonException;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class LineFactory {
@@ -11,15 +10,17 @@ public class LineFactory {
 
     public static Line create(int countOfPerson, PointAddStrategy pointAddStrategy) {
         validateCountOfPerson(countOfPerson);
-        if (countOfPerson == MIN_COUNT_OF_PERSON) {
-            return new Line(Collections.singletonList(false));
-        }
-        return new Line(new ArrayList<>(makeLineWhenMoreThanTwoPeople(countOfPerson, pointAddStrategy)));
+        return new Line(new ArrayList<>(makeLine(countOfPerson, pointAddStrategy)));
     }
 
-    private static List<Boolean> makeLineWhenMoreThanTwoPeople(int countOfPerson, PointAddStrategy pointAddStrategy) {
+    private static List<Boolean> makeLine(int countOfPerson, PointAddStrategy pointAddStrategy) {
         List<Boolean> values = new ArrayList<>();
         values.add(false);
+
+        if (countOfPerson == MIN_COUNT_OF_PERSON) {
+            return values;
+        }
+
         for (int i = 1; i < countOfPerson; i++) {
             values.add(pointAddStrategy.confirmPointLocation(!values.get(values.size() - 1)));
         }
