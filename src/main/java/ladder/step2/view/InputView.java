8b424b8a1afd32cd0348c75cbf239433b6008433
junilHwang <tjsdlf4261@naver.com>
2020-06-01 22:43:21 +0900

    refactor: 상수를 대문자로 (2)

--- src/main/java/ladder/step2/view/InputView.java
+++ src/main/java/ladder/step2/view/InputView.java
@@ -6,23 +6,23 @@ import java.util.Scanner;
 
 public class InputView {
 
-  private static final InputView instance = new InputView();
-  private static final Scanner scanner = new Scanner(System.in);
+  private static final InputView INSTANCE = new InputView();
+  private static final Scanner SCANNER = new Scanner(System.in);
 
   private InputView () { }
 
   public Participants inputParticipants() {
     System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-    return Participants.ofString(scanner.nextLine());
+    return Participants.ofString(SCANNER.nextLine());
   }
 
   public int inputHeight () {
     System.out.println("\n최대 사다리 높이는 몇 개인가요?");
-    return Integer.parseInt(scanner.nextLine());
+    return Integer.parseInt(SCANNER.nextLine());
   }
 
   public static InputView getInstance() {
-    return instance;
+    return INSTANCE;
   }
 
 
