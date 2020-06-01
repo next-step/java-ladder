a12a00acd473c2893f3f82a4950f7ce97e89c5ca
junilHwang <tjsdlf4261@naver.com>
2020-06-01 23:27:48 +0900

    test: LadderHeightTest - 사다리 높이의 입력값이 문자열일 때 형 변환 테스트 완료

--- src/test/java/ladder/step2/domain/LadderHeightTest.java
+++ src/test/java/ladder/step2/domain/LadderHeightTest.java
@@ -4,16 +4,18 @@ import ladder.step2.exception.LadderHeightMinimumSizeException;
 import ladder.step2.exception.LadderHeightNonNumberException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LadderHeightTest {
 
   @DisplayName("사다리 높이가 1 미만일 경우 LadderHeightMinimumSizeException 발생")
   @ParameterizedTest
   @ValueSource(ints = {-1, 0})
-  void 사다리_높이_검증_테스트 (int height) {
+  void 최소값_검증_테스트 (int height) {
     assertThatExceptionOfType(LadderHeightMinimumSizeException.class)
       .isThrownBy(() -> LadderHeight.valueOf(height));
   }
@@ -21,8 +23,15 @@ public class LadderHeightTest {
   @DisplayName("사다리 높이에 대한 입력값이 숫자가 아닐경우 LadderHeightNonNumberException 발생")
   @ParameterizedTest
   @ValueSource(strings = {"a", "!@#", "높이"})
-  void 사다리_높이_검증_테스트 (String height) {
+  void 숫자_검증_테스트 (String height) {
     assertThatExceptionOfType(LadderHeightNonNumberException.class)
       .isThrownBy(() -> LadderHeight.valueOf(height));
+  }
+
+  @DisplayName("사다리 높이에 대한 입력값이 문자열 형태의 숫자일 때 잘 생성 되는지 확인")
+  @ParameterizedTest
+  @ValueSource(strings = {"1","2","3"})
+  void 형변환_테스트 (String height) {
+    assertEquals(Integer.parseInt(height), LadderHeight.valueOf(height).getValue());
   }
 }
