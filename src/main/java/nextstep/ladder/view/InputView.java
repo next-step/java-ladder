cb7fd68975d0877f3b84131a96ceb3fb5046a683
iljun <sumusb@naver.com>
2020-06-02 01:23:50 +0900

    [feature] LadderApplication & view

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -1,19 +1,25 @@
 package nextstep.ladder.view;
 
+import nextstep.ladder.dto.LadderRequestDto;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private final Scanner SCANNER = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
-    public List<String> inputNames() {
+    public static LadderRequestDto inputLadderRequestDto() {
+        return new LadderRequestDto(inputNames(), inputLadderHeight());
+    }
+
+    private static List<String> inputNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         String names = SCANNER.next();
         return Arrays.asList(names.split(","));
     }
 
-    public int inputLadderHeight() {
+    private static int inputLadderHeight() {
         System.out.println("최대 사다리 높이는 몇 개 인가요?");
         return SCANNER.nextInt();
     }
