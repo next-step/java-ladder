3e592b47c60e448b7a35688f7dff963e453f0207
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-02 08:55:48 +0900

    feat(MoveStrategy): static factory method
    - 정적 팩터리 메서드를 통해 상황에 맞는 정책 객체 생성 가능하도록 기능 구현

--- src/main/java/nextstep/ladder/domain/MoveStrategy.java
+++ src/main/java/nextstep/ladder/domain/MoveStrategy.java
@@ -1,5 +1,35 @@
 package nextstep.ladder.domain;
 
+import nextstep.ladder.domain.exceptions.NotExistMoveStrategyException;
+
+import java.util.Arrays;
+
 public enum MoveStrategy {
-    MOVE_LEFT, MOVE_RIGHT, STAY
+    MOVE_LEFT(true, false),
+    MOVE_RIGHT(false, true),
+    STAY(false, false);
+
+    private boolean currentPointStatus;
+    private boolean nextPointStatus;
+
+    MoveStrategy(boolean currentPointStatus, boolean nextPointStatus) {
+        this.currentPointStatus = currentPointStatus;
+        this.nextPointStatus = nextPointStatus;
+    }
+
+    public static MoveStrategy create(boolean currentPointStatus, boolean nextPointStatus) {
+        return Arrays.stream(MoveStrategy.values())
+                .filter(moveStrategy -> moveStrategy.getCurrentPointStatus() == currentPointStatus &&
+                        moveStrategy.getNextPointStatus() == nextPointStatus)
+                .findFirst()
+                .orElseThrow(() -> new NotExistMoveStrategyException("Such MoveStrategy not exists"));
+    }
+
+    private boolean getCurrentPointStatus() {
+        return currentPointStatus;
+    }
+
+    private boolean getNextPointStatus() {
+        return nextPointStatus;
+    }
 }
---
+++ src/main/java/nextstep/ladder/domain/exceptions/NotExistMoveStrategyException.java
@@ -0,0 +1,7 @@
+package nextstep.ladder.domain.exceptions;
+
+public class NotExistMoveStrategyException extends RuntimeException {
+    public NotExistMoveStrategyException(String message) {
+        super(message);
+    }
+}
