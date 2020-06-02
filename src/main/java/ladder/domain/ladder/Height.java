a8cf7528c0b2c6e02b776ad4dacf50daf63f5b35
kwonheejeong <gmlwjd9405@naver.com>
2020-06-02 20:27:40 +0900

    feat: 사다리 높이 클래스 구현

---
+++ src/main/java/ladder/domain/ladder/Height.java
@@ -0,0 +1,23 @@
+package ladder.domain.ladder;
+
+public class Height {
+
+    static final int MIN_HEIGHT = 1;
+
+    private final int value;
+
+    private Height(final int value) {
+        verifyHeight(value);
+        this.value = value;
+    }
+
+    public static Height of(final int value) {
+        return new Height(value);
+    }
+
+    private static void verifyHeight(final int height) {
+        if (height < MIN_HEIGHT) {
+            throw new IllegalArgumentException("사다리의 높이는 최소 1 이상이여야 합니다.");
+        }
+    }
+}
