d258bf7d38fb4e95fbeaded2007e4d57f6be8007
JuWanHan <baekseongsa@naver.com>
2020-06-03 01:38:34 +0900

    feat(Line): 라인 객체 구현

---
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -0,0 +1,37 @@
+package nextstep.ladder.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+public class Line {
+
+    private final List<Point> points;
+
+    private Line(List<Point> points) {
+        validate(points);
+        this.points = points;
+    }
+
+    public static Line newInstance(List<Point> points) {
+        return new Line(points);
+    }
+
+    private void validate(List<Point> points) {
+        if (points == null) {
+            throw new IllegalArgumentException("라인이 존재하지 않습니다.");
+        }
+
+        if (points.stream().anyMatch(Objects::isNull)) {
+            throw new IllegalArgumentException("존재하지 않은 라인이 포함되어 있습니다.");
+        }
+
+        IntStream.range(1, points.size())
+                .forEach(index -> points.get(index).validate(points.get(index - 1)));
+    }
+
+    public List<Point> toList() {
+        return Collections.unmodifiableList(this.points);
+    }
+}
