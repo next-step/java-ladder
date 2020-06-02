0cce6ba48b1ff249c65ce05a6e2b5fec4e817916
JuWanHan <baekseongsa@naver.com>
2020-06-03 01:35:43 +0900

    feat(Height): 높이 객체 구현

---
+++ src/main/java/nextstep/ladder/domain/Height.java
@@ -0,0 +1,42 @@
+package nextstep.ladder.domain;
+
+import nextstep.ladder.exception.InputValueException;
+
+import java.util.Objects;
+
+public class Height {
+
+    private final int height;
+
+    private Height(int height) {
+        validate(height);
+        this.height = height;
+    }
+
+    public static Height newInstance(int height) {
+        return new Height(height);
+    }
+
+    private void validate(int height) {
+        if (height < 1) {
+            throw new InputValueException("사다리 높이는 0 보다 커야 합니다.");
+        }
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Height height1 = (Height) o;
+        return height == height1.height;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(height);
+    }
+}
