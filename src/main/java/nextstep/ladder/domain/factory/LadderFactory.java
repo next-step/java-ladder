612815fe3897984c8a5d64897a562339cff9c9db
JuWanHan <baekseongsa@naver.com>
2020-06-03 01:42:03 +0900

    feat(LadderFactory): 사다리 팩토리 객체 생성

---
+++ src/main/java/nextstep/ladder/domain/factory/LadderFactory.java
@@ -0,0 +1,37 @@
+package nextstep.ladder.domain.factory;
+
+import nextstep.ladder.domain.*;
+import nextstep.ladder.domain.strategy.RandomPointStrategy;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public final class LadderFactory {
+
+    private LadderFactory() {
+    }
+
+    public static Ladder create(Players players, Height height) {
+        validatePlayers(players);
+        validateHeight(height);
+
+        List<Line> lines = Stream.generate(() -> LineFactory.create(players.getCount(), new RandomPointStrategy()))
+                .limit(height.getHeight())
+                .collect(Collectors.toList());
+
+        return Ladder.newInstance(players, Lines.newInstance(lines));
+    }
+
+    private static void validatePlayers(Players players) {
+        if (players == null) {
+            throw new IllegalArgumentException("참여자 정보가 존재하지 않습니다.");
+        }
+    }
+
+    private static void validateHeight(Height height) {
+        if (height == null) {
+            throw new IllegalArgumentException("사다리 높이 정보가 존재하지 않습니다.");
+        }
+    }
+}
