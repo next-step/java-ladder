5e057c36a3beddce56c11e24e72009bc9f8f428a
JuWanHan <baekseongsa@naver.com>
2020-06-03 01:35:00 +0900

    feat(StringUtils): 문자열 유틸 클래스 구현

---
+++ src/main/java/nextstep/ladder/util/StringUtils.java
@@ -0,0 +1,15 @@
+package nextstep.ladder.util;
+
+public final class StringUtils {
+
+    private StringUtils() {
+    }
+
+    public static boolean isBlank(String value) {
+        return value == null || value.trim().isEmpty();
+    }
+
+    public static int toInt(String value) {
+        return Integer.parseInt(value);
+    }
+}
