73f579ea6cf7c1caf830e8dec6ac09d7b7c0a690
junilHwang <tjsdlf4261@naver.com>
2020-06-03 00:37:25 +0900

    refactor: [step3] LadderGame에서 로직 분리 (2)

--- src/main/java/ladder/step3/domain/LadderGame.java
+++ src/main/java/ladder/step3/domain/LadderGame.java
@@ -1,44 +1,16 @@
 package ladder.step3.domain;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
+import java.util.Map;
 
 public class LadderGame {
 
-  private final HashMap<Participant, LadderResult> resultMap;
+  private final Map<Participant, LadderResult> resultMap;
 
-  private LadderGame (HashMap<Participant, LadderResult> resultMap) {
+  private LadderGame (Map<Participant, LadderResult> resultMap) {
     this.resultMap = resultMap;
   }
 
-  public static LadderGame of (Ladder ladder, LadderResults results, Participants participants) {
-    HashMap<Participant, LadderResult> resultMap = new HashMap<>();
-    List<LadderResult> resultList = results.stream().collect(toList());
-    List<List<Boolean>> shape = ladder.shape();
-    int[] offset = { 0, 0 };
-    int width = participants.size() - 1;
-    int height = shape.size();
-    participants.stream()
-                .forEach(participant -> {
-                  int x = offset[0];
-                  int y = offset[1];
-                  while (y < height) {
-                    List<Boolean> nowLadder = shape.get(y);
-                    y += 1;
-                    if (x > 0 && nowLadder.get(x - 1)) {
-                      x -= 1;
-                      continue;
-                    }
-                    if (x < width && nowLadder.get(x)) {
-                      x += 1;
-                    }
-                  }
-                  offset[0] += 1;
-                  resultMap.put(participant, resultList.get(x));
-                });
+  public static LadderGame of (Map<Participant, LadderResult> resultMap) {
     return new LadderGame(resultMap);
   }
 
