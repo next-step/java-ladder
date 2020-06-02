2e18e8f87ce213a82d6f8d3ab92487100542e3ba
JuWanHan <baekseongsa@naver.com>
2020-06-03 01:34:35 +0900

    feat(InputValueException): 사용자 정의 예외 구현

---
+++ src/main/java/nextstep/ladder/exception/InputValueException.java
@@ -0,0 +1,7 @@
+package nextstep.ladder.exception;
+
+public class InputValueException extends RuntimeException {
+    public InputValueException(String message) {
+        super(message);
+    }
+}
