5252bf641e0ea8f6bbf7fa2344b45b1bd18af4c9
JuWanHan <baekseongsa@naver.com>
2020-06-03 01:40:29 +0900

    feat(Lines): 라인 일급 컬렉션 구현

---
+++ src/main/java/nextstep/ladder/domain/Lines.java
@@ -0,0 +1,33 @@
+package nextstep.ladder.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class Lines {
+
+    private final List<Line> lines;
+
+    private Lines(List<Line> lines) {
+        validate(lines);
+        this.lines = lines;
+    }
+
+    public static Lines newInstance(List<Line> lines) {
+        return new Lines(lines);
+    }
+
+    private void validate(List<Line> lines) {
+        if (lines == null || lines.size() == 0) {
+            throw new IllegalArgumentException("라인 목록이 존재하지 않습니다.");
+        }
+
+        if (lines.stream().anyMatch(Objects::isNull)) {
+            throw new IllegalArgumentException("존재하지 않는 라인이 포함되어 있습니다.");
+        }
+    }
+
+    public List<Line> toList() {
+        return Collections.unmodifiableList(lines);
+    }
+}
