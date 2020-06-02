819b92aa448556bb88e6bdaae8098b1a5fdfe1d9
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-02 11:14:54 +0900

    feat(Reward): validation
    - 빈 값이거나 null인 상품명으로 객체를 생성할 수 없도록 유효성 검증

--- src/main/java/nextstep/ladder/domain/Reward.java
+++ src/main/java/nextstep/ladder/domain/Reward.java
@@ -1,11 +1,23 @@
 package nextstep.ladder.domain;
 
+import nextstep.ladder.domain.exceptions.InvalidRewardArgumentException;
+
 public class Reward {
     private String name;
     private Player player;
 
     public Reward(String rewardName, Player player) {
+        validateRewardName(rewardName);
         this.name = rewardName;
         this.player = player;
+    }
+
+    private void validateRewardName(String rewardName) {
+        if (rewardName == null) {
+            throw new InvalidRewardArgumentException("Reward name argument must not be null");
+        }
+        if (rewardName.trim().isEmpty()) {
+            throw new InvalidRewardArgumentException("Reward name argument must not be empty");
+        }
     }
 }
--- src/test/java/nextstep/ladder/domain/RewardTests.java
+++ src/test/java/nextstep/ladder/domain/RewardTests.java
@@ -1,16 +1,36 @@
 package nextstep.ladder.domain;
 
+import nextstep.ladder.domain.exceptions.InvalidRewardArgumentException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RewardTests {
+    private Player testPlayer;
+
+    @BeforeEach
+    public void setup() {
+        testPlayer = new Player("poppo", new HorizontalLocation(0, 3));
+    }
+
     @DisplayName("상품명과 상품 당첨 Player를 입력받아서 객체를 생성할 수 있다.")
     @Test
     void createTest() {
-        assertThat(new Reward("100",
-                new Player("poppo", new HorizontalLocation(0, 3))))
-                .isNotNull();
+        assertThat(new Reward("100", testPlayer)).isNotNull();
+    }
+
+    @DisplayName("상품명이 null이거나 빈 값일 경우 객체를 생성 할 수 없다.")
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"  ", ""})
+    void createEmptyValidationTest(String invalidRewardName) {
+        assertThatThrownBy(() -> new Reward(invalidRewardName, testPlayer))
+                .isInstanceOf(InvalidRewardArgumentException.class);
     }
 }
