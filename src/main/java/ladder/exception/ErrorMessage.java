08493c59449c36516fcb529b93f0702dc2462d2c
kwonheejeong <gmlwjd9405@naver.com>
2020-06-02 20:37:53 +0900

    feat: StringUtil 클래스 구현

---
+++ src/main/java/ladder/exception/ErrorMessage.java
@@ -0,0 +1,10 @@
+package ladder.exception;
+
+public class ErrorMessage {
+
+    public static final String IS_NULL_OR_EMPTY = "입력 값이 null 이거나 빈 공백 문자입니다.";
+    public static final String NULL_VALUE = "입력값이 null 입니다.";
+
+    private ErrorMessage() {
+    }
+}
