3fbd168d0883dd82324c922e77d46dbe855bfc56
kwonheejeong <gmlwjd9405@naver.com>
2020-06-03 21:58:36 +0900

    feat: 계단의 상태에 대한 클래스 구현

---
+++ src/main/java/ladder/domain/ladder/StairState.java
@@ -0,0 +1,24 @@
+package ladder.domain.ladder;
+
+import ladder.domain.ladder.strategy.StairGenerationStrategy;
+
+public class StairState {
+
+    private final boolean existLine;
+
+    private StairState(final boolean existLine) {
+        this.existLine = existLine;
+    }
+
+    public static StairState ofFirstPillar(final StairGenerationStrategy strategy) {
+        return new StairState(strategy.isGeneratable());
+    }
+
+    public StairState ofMiddlePillar(final StairGenerationStrategy strategy) {
+        return new StairState(strategy.isGeneratable());
+    }
+
+    public StairState ofMiddlePillarWithNoLine() {
+        return new StairState(false);
+    }
+}
