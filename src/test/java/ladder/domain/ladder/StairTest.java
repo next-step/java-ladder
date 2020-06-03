318af78999606768c543a73ace01b669f654342b
kwonheejeong <gmlwjd9405@naver.com>
2020-06-03 22:34:01 +0900

    test: Stair 위치와 상태값에 대한 유효성 검사

--- src/test/java/ladder/domain/ladder/StairTest.java
+++ src/test/java/ladder/domain/ladder/StairTest.java
@@ -1,11 +1,29 @@
 package ladder.domain.ladder;
 
+import ladder.domain.ladder.strategy.RandomStairGenerationStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StairTest {
+
+    @DisplayName("계단의 위치가 0보다 작으면 예외 반환")
+    @Test
+    void createFailure() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> Stair.of(
+                        Stair.FIRST_PILLAR_POSITION - 1,
+                        StairState.ofFirstPillar(new RandomStairGenerationStrategy())));
+    }
+
+    @DisplayName("계단의 상태값이 null 이면 예외 반환")
+    @Test
+    void createFailureByStairStateIsNull() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> Stair.of(Stair.FIRST_PILLAR_POSITION, null));
+    }
 
     @DisplayName("첫 번째 기둥의 계단은 랜던 전략으로 생성")
     @Test
