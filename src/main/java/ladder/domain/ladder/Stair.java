3b047ae2b77206ae1204d1638a53d2f328d4d338
kwonheejeong <gmlwjd9405@naver.com>
2020-06-04 19:22:30 +0900

    refactor: 사용하지 않는 위치 정보를 Stair에서 제거

--- src/main/java/ladder/domain/ladder/Stair.java
+++ src/main/java/ladder/domain/ladder/Stair.java
@@ -2,47 +2,38 @@ package ladder.domain.ladder;
 
 import ladder.domain.ladder.strategy.RandomStairGenerationStrategy;
 import ladder.exception.ErrorMessage;
-import ladder.exception.ValueOutOfBoundsException;
 
 import java.util.Objects;
 
 public class Stair {
 
-    static final int FIRST_PILLAR_POSITION = 0;
-    private static final int POSITION_GAP = 1;
-
-    private final int position;
     private final StairState state;
 
-    private Stair(final int position, final StairState state) {
-        validate(position, state);
-        this.position = position;
+    private Stair(final StairState state) {
+        validate(state);
         this.state = state;
     }
 
-    public static Stair of(final int position, final StairState state) {
-        return new Stair(position, state);
+    public static Stair of(final StairState state) {
+        return new Stair(state);
     }
 
-    private void validate(final int position, final StairState state) {
-        if (position < FIRST_PILLAR_POSITION) {
-            throw new ValueOutOfBoundsException(ErrorMessage.REQUIRED_MIN_STAIR_POSITION);
-        }
+    private void validate(final StairState state) {
         if (Objects.isNull(state)) {
             throw new IllegalArgumentException(ErrorMessage.NULL_VALUE);
         }
     }
 
     public static Stair createOfFirstPillar() {
-        return new Stair(FIRST_PILLAR_POSITION, StairState.ofFirstPillar(RandomStairGenerationStrategy.getInstance()));
+        return new Stair(StairState.ofFirstPillar(RandomStairGenerationStrategy.getInstance()));
     }
 
     public Stair createOfNextPillar() {
-        return new Stair(position + POSITION_GAP, state.ofNextPillar(RandomStairGenerationStrategy.getInstance()));
+        return new Stair(state.ofNextPillar(RandomStairGenerationStrategy.getInstance()));
     }
 
     public Stair createOfLastPillar() {
-        return new Stair(position + POSITION_GAP, state.ofNextPillarWithNoLine());
+        return new Stair(state.ofNextPillarWithNoLine());
     }
 
     public boolean isExistLine() {
