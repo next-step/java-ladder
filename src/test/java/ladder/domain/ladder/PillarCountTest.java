3b87ae9f37fcdf09c3932b5a1db6d7aa07b41806
kwonheejeong <gmlwjd9405@naver.com>
2020-06-04 21:52:05 +0900

    test: 한 행에 있는 사다리 기둥의 개수가 최소 개수인 1과 같으면 return true 반환

--- src/test/java/ladder/domain/ladder/PillarCountTest.java
+++ src/test/java/ladder/domain/ladder/PillarCountTest.java
@@ -3,7 +3,11 @@ package ladder.domain.ladder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -22,6 +26,21 @@ public class PillarCountTest {
     void create(final int count) {
         assertThatCode(() -> PillarCount.of(count))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("한 행에 있는 사다리 기둥의 개수가 최소 개수인 1과 같으면 return true 반환")
+    @ParameterizedTest
+    @MethodSource
+    void isMinCount(final PillarCount pillarCount, final boolean expected) {
+        assertThat(pillarCount.isMinCount())
+                .isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> isMinCount() {
+        return Stream.of(
+                Arguments.of(PillarCount.of(PillarCount.MIN_COUNT), true),
+                Arguments.of(PillarCount.of(PillarCount.MIN_COUNT + 1), false)
+        );
     }
 
     @DisplayName("논리적 동치성 비교")
