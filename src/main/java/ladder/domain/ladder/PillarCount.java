cac031fa1df09bda719504fdf415edf15fcbbf55
kwonheejeong <gmlwjd9405@naver.com>
2020-06-04 21:52:37 +0900

    feat: 기둥의 개수가 최소 개수인지 확인하는 메서드 추가

--- src/main/java/ladder/domain/ladder/PillarCount.java
+++ src/main/java/ladder/domain/ladder/PillarCount.java
@@ -30,6 +30,10 @@ public class PillarCount {
         return value;
     }
 
+    public boolean isMinCount() {
+        return value == MIN_COUNT;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
