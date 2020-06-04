d98d48c540984847e2759918e31e6354c42bd73d
kwonheejeong <gmlwjd9405@naver.com>
2020-06-04 18:32:39 +0900

    feat: 게임 참여자와 사다리 높이에 대한 유효성 검사 추가

--- src/main/java/ladder/domain/LadderGame.java
+++ src/main/java/ladder/domain/LadderGame.java
@@ -5,6 +5,9 @@ import ladder.domain.dto.StairDto;
 import ladder.domain.ladder.Height;
 import ladder.domain.ladder.Ladder;
 import ladder.domain.player.Players;
+import ladder.exception.ErrorMessage;
+
+import java.util.Objects;
 
 public class LadderGame {
 
@@ -12,12 +15,19 @@ public class LadderGame {
     private final Ladder ladder;
 
     private LadderGame(final Players players, final Height height) {
+        validate(players, height);
         this.players = players;
         this.ladder = Ladder.of(height, players.count());
     }
 
     public static LadderGame of(final Players players, final Height height) {
         return new LadderGame(players, height);
+    }
+
+    private void validate(final Players players, final Height height) {
+        if (Objects.isNull(players) || Objects.isNull(height)) {
+            throw new IllegalArgumentException(ErrorMessage.NULL_VALUE);
+        }
     }
 
     public LadderShapeResult play() {
