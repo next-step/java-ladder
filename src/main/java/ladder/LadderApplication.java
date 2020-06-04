6201e567c9005e71d3b257739111a914407075d1
kwonheejeong <gmlwjd9405@naver.com>
2020-06-05 01:42:01 +0900

    refactor: LadderShapeInfo 로 LadderGame 을 생성하도록 리팩토링

--- src/main/java/ladder/LadderApplication.java
+++ src/main/java/ladder/LadderApplication.java
@@ -3,6 +3,7 @@ package ladder;
 import ladder.domain.LadderGame;
 import ladder.domain.dto.LadderShapeResult;
 import ladder.domain.ladder.Height;
+import ladder.domain.ladder.shape.LadderShapeInfo;
 import ladder.domain.player.Players;
 import ladder.view.InputView;
 import ladder.view.ResultView;
@@ -12,8 +13,10 @@ public class LadderApplication {
     public static void main(String[] args) {
         Players players = Players.of(InputView.inputPlayers());
         Height height = Height.of(InputView.inputHeight());
+        LadderShapeInfo ladderShapeInfo = LadderShapeInfo.valueOf(players, height);
 
-        LadderShapeResult result = LadderGame.of(players, height).play();
+//        LadderShapeResult result = LadderGame.of(players, height).play();
+        LadderShapeResult result = LadderGame.of(ladderShapeInfo).play();
 
         ResultView.printResult(result);
     }
