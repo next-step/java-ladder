c15b45992afe656850be6f7f0c5a83e82ee7e80f
kwonheejeong <gmlwjd9405@naver.com>
2020-06-05 01:50:27 +0900

    test: LadderGame setUp 데이터 리팩토링

--- src/test/java/ladder/domain/LadderGameTest.java
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -16,23 +16,31 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 public class LadderGameTest {
 
-    private List<String> names;
-    private List<String> singlePlayerName;
+    private Players players;
+    private Players singlePlayer;
+
+    private Height minHeight;
+    private Height height;
 
     @BeforeEach
     void setUp() {
-        this.names = new ArrayList<>();
+        List<String> names = new ArrayList<>();
         names.add("pobi");
         names.add("honux");
+        this.players = Players.of(names);
 
-        this.singlePlayerName = new ArrayList<>();
+        List<String> singlePlayerName = new ArrayList<>();
         singlePlayerName.add("heee");
+        this.singlePlayer = Players.of(singlePlayerName);
+
+        this.minHeight = Height.of(Height.MIN_HEIGHT);
+        this.height = Height.of(Height.MIN_HEIGHT * 5);
     }
 
     @DisplayName("LadderGame 생성")
     @Test
     void create() {
-        assertThatCode(() -> LadderGame.of(Players.of(names), Height.of(Height.MIN_HEIGHT * 5)))
+        assertThatCode(() -> LadderGame.of(players, height))
                 .doesNotThrowAnyException();
     }
 
@@ -41,7 +49,7 @@ public class LadderGameTest {
     @ParameterizedTest
     void playerIsNull(final Players players) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> LadderGame.of(players, Height.of(Height.MIN_HEIGHT)));
+                .isThrownBy(() -> LadderGame.of(players, minHeight));
     }
 
     @DisplayName("LadderGame 생성 실패 : 사다리 높이가 null")
@@ -49,20 +57,20 @@ public class LadderGameTest {
     @ParameterizedTest
     void heightIsNull(final Height height) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> LadderGame.of(Players.of(names), height));
+                .isThrownBy(() -> LadderGame.of(players, height));
     }
 
     @DisplayName("게임 참여자 이름과 사다리 판의 정보를 가진 LadderShapeResult 를 반환")
     @Test
     void play() {
-        assertThatCode(() -> LadderGame.of(Players.of(names), Height.of(Height.MIN_HEIGHT * 5)).play())
+        assertThatCode(() -> LadderGame.of(players, height).play())
                 .doesNotThrowAnyException();
     }
 
     @DisplayName("게임 참여자가 1명 일 때, LadderGame 실행 가능")
     @Test
     void createWithSinglePlayer() {
-        assertThatCode(() -> LadderGame.of(Players.of(singlePlayerName), Height.of(Height.MIN_HEIGHT * 5)).play())
+        assertThatCode(() -> LadderGame.of(singlePlayer, height).play())
                 .doesNotThrowAnyException();
     }
 }
