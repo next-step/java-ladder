9e67ef9de838bbfea65b468d647ddec1083ef116
kwonheejeong <gmlwjd9405@naver.com>
2020-06-05 00:47:30 +0900

    test: 1명의 참여자로 게임을 실행하면 모든 행 기둥의 계단 수는 0개

--- src/test/java/ladder/domain/dto/StairDtoTest.java
+++ src/test/java/ladder/domain/dto/StairDtoTest.java
@@ -2,13 +2,17 @@ package ladder.domain.dto;
 
 import ladder.domain.ladder.Height;
 import ladder.domain.ladder.Ladder;
+import ladder.domain.ladder.PillarCount;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class StairDtoTest {
@@ -29,5 +33,24 @@ public class StairDtoTest {
     void create() {
         assertThatCode(() -> StairDto.from(ladder))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("인자로 지정한 높이 만큼 StairDto 리스트가 생성")
+    @ParameterizedTest
+    @ValueSource(ints = { Height.MIN_HEIGHT, Height.MIN_HEIGHT * 5 })
+    void getLines(final int height) {
+        Ladder ladder = Ladder.of(Height.of(height), PillarCount.MIN_COUNT);
+
+        assertThat(StairDto.from(ladder).size())
+                .isEqualTo(height);
+    }
+
+    @DisplayName("한 명의 참여자로 게임을 실행하면 모든 높이의 기둥에 존재하는 계단 수는 0개")
+    @Test
+    void isSinglePillar() {
+        Ladder ladder = Ladder.of(Height.of(Height.MIN_HEIGHT * 5), PillarCount.MIN_COUNT);
+
+        StairDto.from(ladder)
+                .forEach(stairDto -> assertThat(stairDto.isSinglePillar()).isEqualTo(true));
     }
 }
