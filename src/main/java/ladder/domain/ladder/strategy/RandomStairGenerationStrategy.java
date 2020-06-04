397c60662cda5292979b13141fff0cb047b3cfe4
kwonheejeong <gmlwjd9405@naver.com>
2020-06-04 13:14:47 +0900

    refactor: RandomStairGenerationStrategy 클래스를 Singleton으로 변경

--- src/main/java/ladder/domain/ladder/strategy/RandomStairGenerationStrategy.java
+++ src/main/java/ladder/domain/ladder/strategy/RandomStairGenerationStrategy.java
@@ -6,8 +6,20 @@ public class RandomStairGenerationStrategy implements StairGenerationStrategy {
 
     private static final Random random = new Random();
 
+    private RandomStairGenerationStrategy() {
+    }
+
+    public static RandomStairGenerationStrategy getInstance() {
+        return InnerInstanceClass.instance;
+    }
+
     @Override
     public boolean generate() {
         return random.nextBoolean();
     }
+
+    private static class InnerInstanceClass {
+        private static final RandomStairGenerationStrategy instance = new RandomStairGenerationStrategy();
+    }
+
 }
