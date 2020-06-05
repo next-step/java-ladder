727f106e1689523e331169f8450a1b321e0961de
junilhwang <tjsdlf4261@naver.com>
2020-06-05 16:54:54 +0900

    feat: [step4] LadderPoint 작업 중

---
+++ src/main/java/ladder/step3/domain/LadderPoint.java
@@ -0,0 +1,28 @@
+/* Copyright (c) 2020 ZUM Internet, Inc.
+ * All right reserved.
+ * http://www.zum.com
+ * This software is the confidential and proprietary information of ZUM
+ * , Inc. You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with ZUM.
+ *
+ * Revision History
+ * Author                    Date                     Description
+ * ------------------       --------------            ------------------
+ *   junil                2020-06-05
+ */
+package ladder.step3.domain;
+
+public class LadderPoint {
+    private final int point;
+    private final Direction direction;
+
+    private LadderPoint (int point, Direction direction) {
+        this.point = point;
+        this.direction = direction;
+    }
+
+    public static LadderPoint of (int point, Direction direction) {
+        return new LadderPoint(point, direction);
+    }
+}
