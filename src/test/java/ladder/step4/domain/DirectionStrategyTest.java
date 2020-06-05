d21001f715be9950e70ec58123d77a22e3ec9eb3
junilHwang <tjsdlf4261@naver.com>
2020-06-05 22:05:24 +0900

    test: [step4] TailDirectionStrategyTest 완료

--- src/test/java/ladder/step4/domain/DirectionStrategyTest.java
+++ src/test/java/ladder/step4/domain/DirectionStrategyTest.java
@@ -11,21 +11,37 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class BodyDirectionStrategyTest {
+public class DirectionStrategyTest {
 
-    private static final DirectionStrategy strategy = BodyDirectionStrategy.getInstance();
+    private static final DirectionStrategy bodyStrategy = BodyDirectionStrategy.getInstance();
+    private static final DirectionStrategy tailStrategy = TailDirectionStrategy.getInstance();
 
     @DisplayName("사다리에 줄이 생기지 말아야할 경우에 대한 테스트")
     @ParameterizedTest
     @MethodSource("provideDirection")
     void 사다리_줄_생성_테스트(Direction prev) {
-        assertEquals(false, strategy.create(prev) == prev);
+        assertEquals(false, bodyStrategy.create(prev) == prev);
     }
 
     private static Stream<Arguments> provideDirection () {
         return Stream.of(
           Arguments.of(Direction.LEFT),
           Arguments.of(Direction.RIGHT)
+        );
+    }
+
+    @DisplayName("마지막 줄에 대한 생성 테스트")
+    @ParameterizedTest
+    @MethodSource("provideDirectionAndTailDirection")
+    void 마지막_줄_생성_테스트(Direction prev, Direction expected) {
+        assertEquals(expected, tailStrategy.create(prev));
+    }
+
+    private static Stream<Arguments> provideDirectionAndTailDirection () {
+        return Stream.of(
+            Arguments.of(Direction.LEFT, Direction.EMPTY),
+            Arguments.of(Direction.RIGHT, Direction.LEFT),
+            Arguments.of(Direction.EMPTY, Direction.EMPTY)
         );
     }
 
