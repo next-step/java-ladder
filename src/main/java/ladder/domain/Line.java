789f432469c9873df7c4b1c8d430d49a0542799d
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-06-06 14:48:20 +0900

    refactor (Line) : 예외처리 메소드 분리

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -5,10 +5,15 @@ import java.util.List;
 
 public class Line {
 
+    private static final int MINIMUM_PLAYER_COUNTS = 1;
     private final List<Boolean> points = new ArrayList<>();
 
     public Line(int playerCounts) {
-        if (playerCounts < 1) {
+        validatePlayerCounts(playerCounts);
+    }
+
+    private void validatePlayerCounts(int playerCounts) {
+        if (playerCounts < MINIMUM_PLAYER_COUNTS) {
             throw new IllegalArgumentException(ErrorMessages.INVALID_PLAYER_COUNTS);
         }
     }
