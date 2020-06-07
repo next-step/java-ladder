7818f69db459eb7bb4a26bcef42287dda5a6d14a
chulman <chlcjfals0122@gmail.com>
2020-06-07 17:19:53 +0900

    refactor (User): User class move to user package

--- src/test/java/nextstep/ladder/domain/user/UserTest.java
+++ src/test/java/nextstep/ladder/domain/user/UserTest.java
@@ -1,5 +1,6 @@
-package nextstep.ladder.domain;
+package nextstep.ladder.domain.user;
 
+import nextstep.ladder.domain.user.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
