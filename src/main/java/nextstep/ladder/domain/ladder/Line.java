eee601feaec3798936e298fc9316ce92dec536c2
chulman <chlcjfals0122@gmail.com>
2020-06-07 17:58:55 +0900

    refactor (Line): 라인 존재 유무 element 제거

--- src/main/java/nextstep/ladder/domain/ladder/Line.java
+++ src/main/java/nextstep/ladder/domain/ladder/Line.java
@@ -2,14 +2,9 @@ package nextstep.ladder.domain.ladder;
 
 public class Line {
 
-    private final boolean isExist;
     private final Direction direction;
 
-    public Line(boolean isExist, Direction direction) {
-        this.isExist = isExist;
-        if (!isExist) {
-            direction = null;
-        }
+    public Line(Direction direction) {
         this.direction = direction;
     }
 }
