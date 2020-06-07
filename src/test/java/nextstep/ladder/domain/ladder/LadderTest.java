1291b34a17353cff4ee35d03e690311b1ac5d324
chulman <chlcjfals0122@gmail.com>
2020-06-08 02:21:11 +0900

    refactor (LadderTest): modify ladder create test

--- src/test/java/nextstep/ladder/domain/ladder/LadderTest.java
+++ src/test/java/nextstep/ladder/domain/ladder/LadderTest.java
@@ -3,24 +3,22 @@ package nextstep.ladder.domain.ladder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.*;
 
 public class LadderTest {
 
     @Test
     @DisplayName("사다리 생성 테스트")
     void create() {
-        assertThatCode(() -> new Ladder(Height.of(5), 3, new DirectionRandomPredicate())).doesNotThrowAnyException();
+        Ladder ladder = new Ladder(Height.of(5), 3, new DirectionRandomPredicate());
+        assertThat(ladder.getLines()).hasSize(5);
+        assertThat(ladder.getLines().get(0).getPositions()).hasSize(3);
+        assertThat(ladder.getHeight()).isEqualTo(5);
     }
 
     @Test
     @DisplayName("사다리 생성 에외 테스트 - 위치가 2보다 작으면 예외가 발생한다.")
     void exception() {
-        List<Position> positions = Arrays.asList(new Position(0, Direction.LEFT));
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new Ladder(Height.of(5), 1, new DirectionRandomPredicate()));
     }
