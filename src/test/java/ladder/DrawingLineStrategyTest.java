802a180e479e3d3ffcb29d7a3cde9a67677c5db7
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-06-07 14:54:42 +0900

    test (DrawingLineStrategy) : 전략 테스트 추가

--- src/test/java/ladder/DrawingLineStrategyTest.java
+++ src/test/java/ladder/DrawingLineStrategyTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,5 +24,33 @@ public class DrawingLineStrategyTest {
         List<Point> points = drawingLineStrategy.drawLine(playerCounts);
 
         assertThat(points.size()).isEqualTo(playerCounts - 1);
+    }
+
+    @DisplayName("다른 전략으로 Line 생성 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 5, 10})
+    public void useOtherStrategy(int countsOfPerson) {
+        DrawingLineStrategy alwaysDrawingStrategy = (playerCounts) -> {
+            return Stream.generate(() -> new Point(true))
+                    .limit(playerCounts)
+                    .collect(Collectors.toList());
+        };
+
+        DrawingLineStrategy alwaysNotDrawingStrategy = (playerCounts) -> {
+            return Stream.generate(() -> new Point(false))
+                    .limit(playerCounts)
+                    .collect(Collectors.toList());
+        };
+
+        boolean isAlwaysDrawingLineValid = alwaysDrawingStrategy.drawLine(countsOfPerson)
+                .stream()
+                .allMatch(point -> point.getIsOnPosition() == true);
+
+        boolean isAlwaysNotDrawingLineValid = alwaysNotDrawingStrategy.drawLine(countsOfPerson)
+                .stream()
+                .allMatch(point -> point.getIsOnPosition() == false);
+
+        assertThat(isAlwaysDrawingLineValid).isEqualTo(true);
+        assertThat(isAlwaysNotDrawingLineValid).isEqualTo(true);
     }
 }
