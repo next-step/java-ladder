0203963844b07b9630c33cf44672a7f4215ea7f4
최홍희 <vvshinevv@gmail.com>
2020-06-07 16:23:12 +0900

    [test] line 테스트

--- src/test/java/nextstep/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -1,4 +1,13 @@
 package nextstep.ladder;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class LineTest {
+
+    @DisplayName("라인이 제대로 만들어지는지 확인")
+    @Test
+    void lineTest() {
+
+    }
 }
