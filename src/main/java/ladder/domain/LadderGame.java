4173d945534e7c9f0d607193f7e859c9de7f4e73
kwonheejeong <gmlwjd9405@naver.com>
2020-06-09 07:07:42 +0900

    feat: play 메서드 네이밍 수정 및 Prizes 데이터 추가

--- src/main/java/ladder/domain/LadderGame.java
+++ src/main/java/ladder/domain/LadderGame.java
@@ -4,6 +4,7 @@ import ladder.domain.dto.LadderShapeResult;
 import ladder.domain.ladder.Ladder;
 import ladder.domain.ladder.shape.LadderShapeInfo;
 import ladder.domain.player.Players;
+import ladder.domain.prize.Prizes;
 import ladder.exception.ErrorMessage;
 
 import java.util.Objects;
@@ -11,11 +12,13 @@ import java.util.Objects;
 public class LadderGame {
 
     private final Players players;
+    private final Prizes prizes;
     private final Ladder ladder;
 
     private LadderGame(final LadderShapeInfo ladderShapeInfo) {
         validateLadderShapeInfo(ladderShapeInfo);
         this.players = ladderShapeInfo.getPlayers();
+        this.prizes = ladderShapeInfo.getPrizes();
         this.ladder = Ladder.of(ladderShapeInfo);
     }
 
@@ -29,7 +32,7 @@ public class LadderGame {
         }
     }
 
-    public LadderShapeResult play() {
-        return LadderShapeResult.of(players.getNames(), ladder);
+    public LadderShapeResult ready() {
+        return LadderShapeResult.of(players.getNames(), prizes.getNames(), ladder);
     }
 }
