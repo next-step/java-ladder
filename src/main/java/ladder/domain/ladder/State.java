159502ec2adf87a092bd93a119d653177c9bf786
kwonheejeong <gmlwjd9405@naver.com>
2020-06-09 08:23:03 +0900

    feat: 계단 상테에 대한 enum 클래스 기본 기능 구현

---
+++ src/main/java/ladder/domain/ladder/State.java
@@ -0,0 +1,47 @@
+package ladder.domain.ladder;
+
+import ladder.domain.ladder.strategy.StairGenerationStrategy;
+
+import java.util.function.Function;
+
+public enum State {
+
+    EMPTY(position -> position),
+    RIGHT(position -> position + 1),
+    LEFT(position -> position - 1);
+
+    private Function<Integer, Integer> movePosition;
+
+    State(final Function<Integer, Integer> movePosition) {
+        this.movePosition = movePosition;
+    }
+
+    public static State ofFirstPillar(final StairGenerationStrategy strategy) {
+        return createStair(strategy);
+    }
+
+    public State ofNextPillar(final StairGenerationStrategy strategy) {
+        if (isExistLine()) {
+            return LEFT;
+        }
+        return createStair(strategy);
+    }
+
+    public State ofLastPillar() {
+        if (isExistLine()) {
+            return LEFT;
+        }
+        return EMPTY;
+    }
+
+    public boolean isExistLine() {
+        return this == RIGHT;
+    }
+
+    private static State createStair(final StairGenerationStrategy strategy) {
+        if (strategy.generate()) {
+            return RIGHT;
+        }
+        return EMPTY;
+    }
+}
