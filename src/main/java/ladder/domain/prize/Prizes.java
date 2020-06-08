4983db6c587d5821d7f051c36ea8e702ce5b6039
kwonheejeong <gmlwjd9405@naver.com>
2020-06-09 06:29:59 +0900

    feat: Prizes 기본 기능 구현

---
+++ src/main/java/ladder/domain/prize/Prizes.java
@@ -0,0 +1,39 @@
+package ladder.domain.prize;
+
+import ladder.exception.ErrorMessage;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Prizes {
+
+    private final List<Prize> prizes;
+
+    private Prizes(final List<String> names) {
+        validateNames(names);
+        this.prizes = names.stream()
+                .map(Prize::of)
+                .collect(Collectors.toList());
+    }
+
+    public static Prizes of(List<String> names) {
+        return new Prizes(names);
+    }
+
+    private void validateNames(final List<String> names) {
+        if (Objects.isNull(names) || names.size() <= 0) {
+            throw new IllegalArgumentException(ErrorMessage.IS_NULL_OR_EMPTY);
+        }
+    }
+
+    public List<String> getNames() {
+        return prizes.stream()
+                .map(Prize::getName)
+                .collect(Collectors.toList());
+    }
+
+    public Prize indexOf(final int index) {
+        return prizes.get(index);
+    }
+}
