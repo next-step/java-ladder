f3b7b26c1818fc67fe0658313c13b8e740d6c70a
kwonheejeong <gmlwjd9405@naver.com>
2020-06-09 07:00:34 +0900

    feat: LadderShapeResult에 Prizes 이름 정보 추가 구현

--- src/main/java/ladder/domain/dto/LadderShapeResult.java
+++ src/main/java/ladder/domain/dto/LadderShapeResult.java
@@ -7,19 +7,25 @@ import java.util.List;
 public class LadderShapeResult {
 
     private final List<String> playerNames;
+    private final List<String> prizeNames;
     private final List<StairDto> stairDtos;
 
-    private LadderShapeResult(final List<String> playerNames, final List<StairDto> stairDtos) {
+    private LadderShapeResult(final List<String> playerNames, final List<String> prizeNames, final List<StairDto> stairDtos) {
         this.playerNames = playerNames;
+        this.prizeNames = prizeNames;
         this.stairDtos = stairDtos;
     }
 
-    public static LadderShapeResult of(final List<String> playerNames, final Ladder ladder) {
-        return new LadderShapeResult(playerNames, StairDto.from(ladder));
+    public static LadderShapeResult of(final List<String> playerNames, final List<String> prizeNames, final Ladder ladder) {
+        return new LadderShapeResult(playerNames, prizeNames, StairDto.from(ladder));
     }
 
     public List<String> getPlayerNames() {
         return playerNames;
+    }
+
+    public List<String> getPrizeNames() {
+        return prizeNames;
     }
 
     public List<StairDto> getStairDtos() {
