eef97e69e0f726acef8d0b25635ab3d195ab30c9
kwonheejeong <gmlwjd9405@naver.com>
2020-06-09 06:46:57 +0900

    test: LadderShapeInfo 에 Prizes를 추가함에 따른 테스트 코드 수정

--- src/test/java/ladder/domain/ladder/shape/LadderShapeInfoTest.java
+++ src/test/java/ladder/domain/ladder/shape/LadderShapeInfoTest.java
@@ -2,6 +2,7 @@ package ladder.domain.ladder.shape;
 
 import ladder.domain.ladder.Height;
 import ladder.domain.player.Players;
+import ladder.domain.prize.Prizes;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,7 @@ import static org.assertj.core.api.Assertions.*;
 public class LadderShapeInfoTest {
 
     private Players players;
+    private Prizes prizes;
     private Height height;
 
     @BeforeEach
@@ -24,14 +26,19 @@ public class LadderShapeInfoTest {
         names.add("pobi");
         names.add("honux");
 
+        List<String> prizeNames = new ArrayList<>();
+        prizeNames.add("3000");
+        prizeNames.add("꽝");
+
         this.players = Players.of(names);
+        this.prizes = Prizes.of(prizeNames);
         this.height = Height.of(5);
     }
 
     @DisplayName("LadderShapeInfo 생성")
     @Test
     void create() {
-        assertThatCode(() -> LadderShapeInfo.valueOf(players, height))
+        assertThatCode(() -> LadderShapeInfo.valueOf(players, prizes, height))
                 .doesNotThrowAnyException();
     }
 
@@ -40,7 +47,15 @@ public class LadderShapeInfoTest {
     @ParameterizedTest
     void playerIsNull(final Players players) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> LadderShapeInfo.valueOf(players, height));
+                .isThrownBy(() -> LadderShapeInfo.valueOf(players, prizes, height));
+    }
+
+    @DisplayName("LadderShapeInfo 생성 실패 : 게임 실행 결과가 null")
+    @NullSource
+    @ParameterizedTest
+    void heightIsNull(final Prizes prizes) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LadderShapeInfo.valueOf(players, prizes, height));
     }
 
     @DisplayName("LadderShapeInfo 생성 실패 : 사다리 높이가 null")
@@ -48,27 +63,34 @@ public class LadderShapeInfoTest {
     @ParameterizedTest
     void heightIsNull(final Height height) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> LadderShapeInfo.valueOf(players, height));
+                .isThrownBy(() -> LadderShapeInfo.valueOf(players, prizes, height));
     }
 
     @DisplayName("입력한 게임 참여자를 반환")
     @Test
     void getPlayers() {
-        assertThat(LadderShapeInfo.valueOf(players, height).getPlayers().count())
+        assertThat(LadderShapeInfo.valueOf(players, prizes, height).getPlayers().count())
                 .isEqualTo(players.count());
+    }
+
+    @DisplayName("입력한 게임 결를 반환")
+    @Test
+    void getPrizes() {
+        assertThat(LadderShapeInfo.valueOf(players, prizes, height).getPrizes())
+                .isEqualTo(prizes);
     }
 
     @DisplayName("입력한 사다리의 높이를 반환")
     @Test
     void getHeight() {
-        assertThat(LadderShapeInfo.valueOf(players, height).getHeight())
+        assertThat(LadderShapeInfo.valueOf(players, prizes, height).getHeight())
                 .isEqualTo(height.getValue());
     }
 
     @DisplayName("사다리의 한 행에 있는 기둥의 수를 반환")
     @Test
     void getWidth() {
-        assertThat(LadderShapeInfo.valueOf(players, height).getWidth().getValue())
+        assertThat(LadderShapeInfo.valueOf(players, prizes, height).getWidth().getValue())
                 .isEqualTo(players.count());
     }
 }
