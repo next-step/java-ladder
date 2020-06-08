60c9cdb50758fae48172f75ffd49514656263013
kwonheejeong <gmlwjd9405@naver.com>
2020-06-09 07:25:17 +0900

    test: StairDtoTest 에서 Prizes 데이터 추가

--- src/test/java/ladder/domain/dto/StairDtoTest.java
+++ src/test/java/ladder/domain/dto/StairDtoTest.java
@@ -4,6 +4,7 @@ import ladder.domain.ladder.Height;
 import ladder.domain.ladder.Ladder;
 import ladder.domain.ladder.shape.LadderShapeInfo;
 import ladder.domain.player.Players;
+import ladder.domain.prize.Prizes;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,8 @@ public class StairDtoTest {
     private Players players;
     private Players singlePlayer;
 
+    private Prizes prizes;
+
     private Height height;
 
     @BeforeEach
@@ -31,6 +34,11 @@ public class StairDtoTest {
         names.add("pobi");
         names.add("honux");
         this.players = Players.of(names);
+
+        List<String> prizeNames = new ArrayList<>();
+        prizeNames.add("3000");
+        prizeNames.add("꽝");
+        this.prizes = Prizes.of(prizeNames);
 
         List<String> singlePlayerName = new ArrayList<>();
         singlePlayerName.add("heee");
@@ -42,7 +50,7 @@ public class StairDtoTest {
     @DisplayName("사다리 정보로 각 행의 좌->우로의 계단 라인 정보 리스트 반환")
     @Test
     void create() {
-        assertThatCode(() -> StairDto.from(Ladder.of(LadderShapeInfo.valueOf(players, height))))
+        assertThatCode(() -> StairDto.from(Ladder.of(LadderShapeInfo.valueOf(players, prizes, height))))
                 .doesNotThrowAnyException();
     }
 
@@ -50,7 +58,7 @@ public class StairDtoTest {
     @ParameterizedTest
     @MethodSource
     void from(final Height height, final int heightValue) {
-        Ladder ladder = Ladder.of(LadderShapeInfo.valueOf(players, height));
+        Ladder ladder = Ladder.of(LadderShapeInfo.valueOf(players, prizes, height));
 
         assertThat(StairDto.from(ladder).size())
                 .isEqualTo(heightValue);
@@ -66,7 +74,7 @@ public class StairDtoTest {
     @DisplayName("한 명의 참여자로 게임을 실행하면 모든 높이의 기둥에 존재하는 계단 수는 0개")
     @Test
     void isSinglePillar() {
-        Ladder ladder = Ladder.of(LadderShapeInfo.valueOf(singlePlayer, height));
+        Ladder ladder = Ladder.of(LadderShapeInfo.valueOf(singlePlayer, prizes, height));
 
         StairDto.from(ladder)
                 .forEach(stairDto -> assertThat(stairDto.isSinglePillar()).isEqualTo(true));
