7cc180e50505d9a00774f1e696b2c39e89b2281b
kwonheejeong <gmlwjd9405@naver.com>
2020-06-09 06:38:04 +0900

    feat: 실행 결과 입력받기 구현

--- src/main/java/ladder/LadderApplication.java
+++ src/main/java/ladder/LadderApplication.java
@@ -5,6 +5,7 @@ import ladder.domain.dto.LadderShapeResult;
 import ladder.domain.ladder.Height;
 import ladder.domain.ladder.shape.LadderShapeInfo;
 import ladder.domain.player.Players;
+import ladder.domain.prize.Prizes;
 import ladder.view.InputView;
 import ladder.view.ResultView;
 
@@ -12,6 +13,7 @@ public class LadderApplication {
 
     public static void main(String[] args) {
         Players players = Players.of(InputView.inputPlayers());
+        Prizes prizes = Prizes.of(InputView.inputPrizes());
         Height height = Height.of(InputView.inputHeight());
         LadderShapeInfo ladderShapeInfo = LadderShapeInfo.valueOf(players, height);
 
