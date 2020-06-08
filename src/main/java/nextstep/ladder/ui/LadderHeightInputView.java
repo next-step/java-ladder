895ddaef31890047415906219aabd142035fedf5
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-08 09:27:50 +0900

    rename(LadderHeightInputView)
    - 애매한 상수명 명확하게 변경
    - 의미단위로 언더바 구분

--- src/main/java/nextstep/ladder/ui/LadderHeightInputView.java
+++ src/main/java/nextstep/ladder/ui/LadderHeightInputView.java
@@ -3,7 +3,7 @@ package nextstep.ladder.ui;
 import java.util.Scanner;
 
 public class LadderHeightInputView {
-    private static final int MIN_MAXLADDERHEIGHT = 1;
+    private static final int MIN_LADDER_HEIGHT = 1;
     private static final String INPUT_LADDER_HEIGHT_COMMENT = System.lineSeparator() + "최대 사다리 높이는 몇 개인가요?";
 
     private int maxLadderHeight;
@@ -23,7 +23,7 @@ public class LadderHeightInputView {
     }
 
     private void validateLadderHeight(int maxLadderHeight) {
-        if (maxLadderHeight < MIN_MAXLADDERHEIGHT) {
+        if (maxLadderHeight < MIN_LADDER_HEIGHT) {
             throw new IllegalArgumentException("Max Ladder Height must bigger than 0");
         }
     }
