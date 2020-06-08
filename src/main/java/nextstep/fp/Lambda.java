e1f23faa5cb106cc22832faee1040e96733ed566
Gobukgol <zxczc159@gmail.com>
2020-06-08 14:46:29 +0900

    refactor(Lambda): 람다 실습 2 - Lambda sum 메소드 중복 제거 feat(Conditional): 덧셈 조건을 확인하는 인터페이스 구현

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -27,30 +27,25 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumWithCondition(numbers, (number) -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumWithCondition(numbers, (number) -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
+        return sumWithCondition(numbers, (number) -> number > 3);
+    }
+
+    private static int sumWithCondition(List<Integer> numbers, Conditional conditional) {
         int total = 0;
         for (int number : numbers) {
-            if (number > 3) {
+            if (conditional.test(number)) {
                 total += number;
             }
         }
+
         return total;
     }
 }
