1d518d31147088258fb63460b7ee68b6235c6bf6
kwonheejeong <gmlwjd9405@naver.com>
2020-06-09 07:44:01 +0900

    test: 참여자의 이름에 해당하는 참여자의 게임 결과값을 반환 테스트

--- src/test/java/ladder/domain/dto/LadderMatchResultTest.java
+++ src/test/java/ladder/domain/dto/LadderMatchResultTest.java
@@ -2,17 +2,19 @@ package ladder.domain.dto;
 
 import ladder.domain.player.Player;
 import ladder.domain.prize.Prize;
-import ladder.domain.prize.Prizes;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullSource;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -45,5 +47,20 @@ public class LadderMatchResultTest {
 
         assertThat(LadderMatchResult.of(matchResult).getPlayers())
                 .isEqualTo(players);
+    }
+
+    @DisplayName("참여자의 이름에 해당하는 참여자의 게임 결과값을 반환")
+    @ParameterizedTest
+    @MethodSource
+    void match(final String playerName, final Prize expected) {
+        assertThat(LadderMatchResult.of(matchResult).match(playerName))
+                .isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> match() {
+        return Stream.of(
+                Arguments.of("pobi", Prize.of("3000")),
+                Arguments.of("honux", Prize.of("꽝"))
+        );
     }
 }
