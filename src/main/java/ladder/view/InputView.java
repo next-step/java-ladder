2981da03d4e2c61c36e4f2660624388f53e7dc38
rlapwl <khji1210@naver.com>
2020-06-09 00:04:18 +0900

    feat: Question 실행결과 추가

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -6,9 +6,15 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String QUESTION_PEOPLE_NAME = "참여할 사람 이름을 입력하세요.";
     private static final String QUESTION_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
+    private static final String QUESTION_RESULT = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
 
     public static String[] askingPeopleName() {
         System.out.println(QUESTION_PEOPLE_NAME);
+        return scanner.nextLine().split(",");
+    }
+
+    public static String[] askingResult() {
+        System.out.println(QUESTION_RESULT);
         return scanner.nextLine().split(",");
     }
 
