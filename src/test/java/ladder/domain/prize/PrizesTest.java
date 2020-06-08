a5ad0f80e01711f5e2e5c53b6843c813762078fe
kwonheejeong <gmlwjd9405@naver.com>
2020-06-09 06:28:25 +0900

    teat: Prizes 기본 기능 테스트

---
+++ src/test/java/ladder/domain/prize/PrizesTest.java
@@ -0,0 +1,60 @@
+package ladder.domain.prize;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class PrizesTest {
+
+    private List<String> names;
+
+    @BeforeEach
+    void setUp() {
+        names = new ArrayList<>();
+        names.add("3000");
+        names.add("꽝");
+
+    }
+
+    @DisplayName("null 이거나 0개의 이름이 입력되면 예외를 반환")
+    @NullAndEmptySource
+    @ParameterizedTest
+    void createFailure(final List<String> names) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Prizes.of(names));
+    }
+
+    @DisplayName("Prizes 의 모든 이름을 반환")
+    @Test
+    void getNames() {
+        assertThat(Prizes.of(names).getNames())
+                .isEqualTo(names);
+    }
+
+    @DisplayName("배열 순서에 해당하는 Prize 를 반환")
+    @ParameterizedTest
+    @MethodSource
+    void indexOf(final int index, final Prize expected) {
+        assertThat(Prizes.of(names).indexOf(index))
+                .isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> indexOf() {
+        return Stream.of(
+                Arguments.of(0, Prize.of("3000")),
+                Arguments.of(1, Prize.of("꽝"))
+        );
+    }
+
+}
