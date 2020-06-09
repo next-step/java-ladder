53bc5094fedef306f6cbe406fa8b9aca2b574be4
eunji <jeeell.922@gmail.com>
2020-06-10 00:11:08 +0900

    feat runThread 익명클래스를 람다로 변경

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -18,15 +18,10 @@ public class Lambda {
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> System.out.println("Hello from thread")).start();
     }
 
-    public static int sumAll(List<Integer> numbers, Conditional c){
+    public static int sumAll(List<Integer> numbers, Conditional c) {
         int total = 0;
         for (int number : numbers) {
             if (c.test(number)) {
