b13ac20bd827816ce064c15ac27948d3db65e63f
kwonheejeong <gmlwjd9405@naver.com>
2020-06-09 13:02:29 +0900

    test: 사다리를 타고 난 후 매칭된 prize index list 반환 테스트

--- src/test/java/ladder/domain/ladder/LadderTest.java
+++ src/test/java/ladder/domain/ladder/LadderTest.java
@@ -10,21 +10,34 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.*;
 
 public class LadderTest {
 
     private Players players;
     private Prizes prizes;
 
+    private static LadderShapeInfo shapeInfoOfSinglePlayer;
+    private static LadderShapeInfo shapeInfoOfMultiplePlayers;
+
     @BeforeEach
     void setUp() {
         List<String> names = new ArrayList<>();
         names.add("pobi");
-
         this.players = Players.of(names);
-        this.prizes = Prizes.of(names);
+
+        List<String> prizeNames = new ArrayList<>();
+        prizeNames.add("3000");
+        this.prizes = Prizes.of(prizeNames);
+
+        shapeInfoOfSinglePlayer = LadderShapeInfo.valueOf(players, prizes, Height.of(Height.MIN_HEIGHT));
+
+        names.add("honux");
+        prizeNames.add("꽝");
+        shapeInfoOfMultiplePlayers = LadderShapeInfo.valueOf(
+                Players.of(names),
+                Prizes.of(prizeNames),
+                Height.of(Height.MIN_HEIGHT * 5));
     }
 
     @DisplayName("사다리 생성 실패: 사다리의 높이가 1보다 작은 경우")
@@ -47,8 +60,25 @@ public class LadderTest {
     @DisplayName("Ladder 생성")
     @Test
     void create() {
-        assertThatCode(() -> Ladder.of(
-                LadderShapeInfo.valueOf(players, prizes, Height.of(Height.MIN_HEIGHT))))
+        assertThatCode(() -> Ladder.of(shapeInfoOfSinglePlayer))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("한 명의 사용자가 사다리를 타면 index 0 의 prize 리스트를 반환")
+    @Test
+    void rideWithSinglePlayer() {
+        Ladder ladder = Ladder.of(shapeInfoOfSinglePlayer);
+
+        assertThat(ladder.ride().size())
+                .isEqualTo(shapeInfoOfSinglePlayer.getPrizes().getNames().size());
+    }
+
+    @DisplayName("모든 사용자가 사다리를 타고 난 결과에 대한 prize index 리스트를 반환")
+    @Test
+    void rideWithMultiplePlayers() {
+        Ladder ladder = Ladder.of(shapeInfoOfMultiplePlayers);
+
+        assertThat(ladder.ride().size())
+                .isEqualTo(shapeInfoOfMultiplePlayers.getPrizes().getNames().size());
     }
 }
