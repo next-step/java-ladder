27266d203507e2b7237ea791fbbb280a5f0d6444
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-09 19:31:51 +0900

    tests(HorizontalMoveStrategyTests)
    - 정책을 위반하는 Direction 생성 자체가 불가능해졌기 때문에 필요없는 테스트케이스 삭제

--- src/test/java/nextstep/ladder/domain/ladder/HorizontalMoveStrategyTests.java
+++ src/test/java/nextstep/ladder/domain/ladder/HorizontalMoveStrategyTests.java
@@ -1,9 +1,6 @@
 package nextstep.ladder.domain.ladder;
 
-import nextstep.ladder.domain.exceptions.NotExistMoveStrategyException;
-import nextstep.ladder.domain.ladder.HorizontalMoveStrategy;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,7 +8,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class HorizontalMoveStrategyTests {
     @DisplayName("정적 팩토리 메서드에 전달된 인자에 맞는 정책 객체를 반환한다.")
@@ -26,12 +22,5 @@ class HorizontalMoveStrategyTests {
                 Arguments.of(false, true, HorizontalMoveStrategy.MOVE_RIGHT),
                 Arguments.of(false, false, HorizontalMoveStrategy.STAY)
         );
-    }
-
-    @DisplayName("존재하지 않는 정책을 요구할 경우 NotExistMoveStrategyException 발생")
-    @Test
-    void validationTest() {
-        assertThatThrownBy(() -> HorizontalMoveStrategy.find(true, true))
-                .isInstanceOf(NotExistMoveStrategyException.class);
     }
 }
