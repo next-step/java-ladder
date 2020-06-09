4d76a9e77e6d05e094c273b9c8d43e8b20463702
kwonheejeong <gmlwjd9405@naver.com>
2020-06-09 13:03:00 +0900

    feat: 모든 참여자가 사다리를 타고 난 후 매칭된 결과 prize index 리스트 반환 구현

--- src/main/java/ladder/domain/ladder/Ladder.java
+++ src/main/java/ladder/domain/ladder/Ladder.java
@@ -3,12 +3,16 @@ package ladder.domain.ladder;
 import ladder.domain.ladder.shape.LadderShapeInfo;
 import ladder.exception.ErrorMessage;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Ladder {
+
+    private static final int FIRST_INDEX = 0;
 
     private final List<RowPillars> rowPillars;
 
@@ -35,5 +39,34 @@ public class Ladder {
 
     public List<RowPillars> getRowPillars() {
         return rowPillars;
+    }
+
+    public List<Integer> ride() {
+        if (isSinglePlayer()) {
+            return Collections.singletonList(FIRST_INDEX);
+        }
+
+        return IntStream.range(FIRST_INDEX, getLadderWidth())
+                .map(this::rideOnePlayer)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    private int rideOnePlayer(final int playerPosition) {
+        int position = playerPosition;
+
+        for (RowPillars pillar : rowPillars) {
+            position = pillar.move(position);
+        }
+
+        return position;
+    }
+
+    private boolean isSinglePlayer() {
+        return getLadderWidth() == 0;
+    }
+
+    private int getLadderWidth() {
+        return rowPillars.get(FIRST_INDEX).getWidth();
     }
 }
