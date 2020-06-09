1a22c880984e159c06e15b651929967587b7ce04
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-06-10 04:38:35 +0900

    feat : 메인작성

---
+++ src/main/java/laddergame/LadderGame.java
@@ -0,0 +1,21 @@
+package laddergame;
+
+import java.util.Scanner;
+import laddergame.model.Ladder;
+import laddergame.view.LadderGameView;
+
+public class LadderGame {
+
+  private static Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    LadderGameView.printInputNameMsg();
+    String[] nameStrArr = scanner.nextLine().split(",");
+
+    LadderGameView.printInputLadderHeightMsg();
+    int ladderHeight = scanner.nextInt();
+    scanner.nextLine();
+
+    LadderGameView.printResult(Ladder.createByHeightAndNamesStrArr(ladderHeight, nameStrArr));
+  }
+}
