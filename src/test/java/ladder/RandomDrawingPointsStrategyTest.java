6d1497ffcb6bddfd7004a8360fb6aadb39779496
정지연 <zeeyon.c@gmail.com>
2020-06-09 15:51:04 +0900

    test: 마지막 좌표값 테스트

--- src/test/java/ladder/RandomDrawingPointsStrategyTest.java
+++ src/test/java/ladder/RandomDrawingPointsStrategyTest.java
@@ -17,4 +17,15 @@ class RandomDrawingPointsStrategyTest {
 
 		assertThat(points.size()).isEqualTo(count);
 	}
+
+	@DisplayName("마지막 좌표는 다음 좌표와 연결되어 있지 않다")
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2})
+	void lastPointIsNotConnectedToNext(int count) {
+		RandomDrawingPointsStrategy drawingStrategy = new RandomDrawingPointsStrategy();
+		LadderLinePoints points = drawingStrategy.draw(count);
+		LadderLinePoint point = points.getContent().get(0);
+
+		assertThat(point.isConnectedToNextPoint()).isFalse();
+	}
 }
