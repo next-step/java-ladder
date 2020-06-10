b2f0ff540ddd42de4cb0724beda3db442f32eff6
neojjc <32336530+neojjc@users.noreply.github.com>
2020-06-10 22:05:48 +0900

    [Step1] 자바8 스트림, 람다, Optional (#421)
    * [doc] LADDER 1단계 요구사항 정리
    
    * [feat] 람다 실습 1 - 익명 클래스를 람다로 전환
    
    * [feat] 람다 실습 2 - 람다를 활용해 중복 제거
    
    * [feat] map, reduce, filter 실습 1
    
    * [feat] map, reduce, filter 실습 2
    
    * [feat] 요구사항1 - Optional을 활용해 조건에 따른 반환
    
    * [feat] 요구사항2 - Optional에서 값을 반환
    
    * [feat] 요구사항3 - Optional에서 Exception 처리

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -18,39 +18,29 @@ public class Lambda {
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
+        new Thread(() -> System.out.println("Hello from thread"))
+                .start();
+    }
+
+    public static int sumAll(List<Integer> numbers, Conditional condition) {
+        int total = 0;
+        for (int number : numbers) {
+            if(condition.meetCondition(number)) {
+                total += number;
             }
-        }).start();
+        }
+        return total;
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAll(numbers, number -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, number -> (number % 2 == 0));
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, number -> (number > 3));
     }
 }
