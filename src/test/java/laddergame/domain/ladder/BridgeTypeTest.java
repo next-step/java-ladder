e3330b1f45ff9b37e18d816efe78c7aa8676beb7
Gobukgol <zxczc159@gmail.com>
2020-06-10 17:45:48 +0900

    refactor(BridgeType): 연결 여부의 Boolean 값이 아닌 연결 방향의 열거 타입으로 변경

--- src/test/java/laddergame/domain/ladder/BridgeTypeTest.java
+++ src/test/java/laddergame/domain/ladder/BridgeTypeTest.java
@@ -4,16 +4,27 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BridgeTypeTest {
 
-    @DisplayName("boolean 값으로 맞는 BridgeType을 가져온다.")
+    @DisplayName("int 값으로 맞는 BridgeType을 가져온다.")
     @ParameterizedTest
-    @CsvSource({"true, CONNECTED", "false, DISCONNECTED"})
-    void findType(boolean isConnected, BridgeType bridgeType) {
-        assertThat(BridgeType.findType(isConnected)).isEqualTo(bridgeType);
+    @CsvSource({"-1, LEFT", "1, RIGHT", "0, NONE"})
+    void findType(int moveValue, BridgeType bridgeType) {
+        assertThat(BridgeType.findByMoveValue(moveValue)).isEqualTo(bridgeType);
+    }
+
+    @DisplayName("없는 값으로 BridgeType을 찾으면 IllegalArgumentException Throw")
+    @ParameterizedTest
+    @ValueSource(ints = {2, 3})
+    void findTypeThrowException(int moveValue) {
+        assertThatThrownBy(() -> BridgeType.findByMoveValue(moveValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 값을 입력하였습니다. - " + moveValue);
     }
 }
