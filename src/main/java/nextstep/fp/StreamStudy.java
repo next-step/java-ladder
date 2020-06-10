dfb295e22d3042468f84a980d71d0a5fb509c7a7
eunji <43842782+eunji12@users.noreply.github.com>
2020-06-10 23:34:20 +0900

    Step1 자바8 스트림, 람다, Optional (#433)
    * feat 람다 실습 1 - 익명 클래스를 람다로 전환
    
    * feat 람다 실습 2 - 람다를 활용해 중복 제거
    
    * feat runThread 익명클래스를 람다로 변경
    
    * feat map, reduce, filter 실습 1
    
    * feat map, reduce, filter 실습 2
    
    * feat 요구사항 1 - Optional을 활용해 조건에 따른 반환
    
    * feat 요구사항 2 - Optional에서 값을 반환
    
    * feat 요구사항 3 - Optional에서 exception 처리

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -27,7 +27,19 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
+
         // TODO 이 부분에 구현한다.
+        words.stream().filter(x -> x.length() > 12).sorted((x1, x2) -> customCompare(x1, x2))
+                .limit(100).distinct().forEach(x -> System.out.print(x.toLowerCase() + " "))
+        ;
+    }
+
+    private static int customCompare(String x1, String x2) {
+        if (x1.length() > x2.length())
+            return -1;
+        if (x1.length() < x2.length())
+            return 1;
+        return 0;
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +51,7 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream().filter(x -> x > 3)
+                .map(x -> x * 2).reduce(0, (x, y) -> x + y);
     }
 }
