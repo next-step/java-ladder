4871beb3e8ae41e798ecf22a2b69ad00723a87bb
Sungjun <sungjunpizz@gmail.com>
2020-06-11 23:08:57 +0900

    feat: Player 클래스 추가

---
+++ src/main/java/ladder/domain/Player.java
@@ -0,0 +1,13 @@
+package ladder.domain;
+
+public class Player {
+    private final String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
