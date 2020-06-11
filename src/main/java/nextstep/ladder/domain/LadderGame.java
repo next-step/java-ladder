58c0f6223b58a09998929b5cd7eafb81b4895bb1
하재권 <hazxz@naver.com>
2020-06-11 23:47:30 +0900

    feat(UI): 최대 사다리 높이를 입력 받는 기능 추가

--- src/main/java/nextstep/ladder/domain/LadderGame.java
+++ src/main/java/nextstep/ladder/domain/LadderGame.java
@@ -1,7 +1,9 @@
 package nextstep.ladder.domain;
 
 import nextstep.ladder.ui.InputView;
+import nextstep.ladder.ui.ResultView;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 
 public class LadderGame {
@@ -9,5 +11,7 @@ public class LadderGame {
         InputView inputView = new InputView();
 
         List<User> userList = inputView.insertUserLine();
+
+        int height = inputView.insertHeight();
     }
 }
--- src/main/java/nextstep/ladder/ui/InputView.java
+++ src/main/java/nextstep/ladder/ui/InputView.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private static final String INPUT_USER_LINE_COMMENT = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String INPUT_HEIGHT_COMMENT = "최대 사다리 높이는 몇 개인가요?";
     private static final String USER_DELIMITER = ",";
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -19,5 +20,10 @@ public class InputView {
         return Arrays.stream(userLineString.split(USER_DELIMITER))
                 .map(User::new)
                 .collect(Collectors.toList());
+    }
+
+    public int insertHeight() {
+        System.out.println(INPUT_HEIGHT_COMMENT);
+        return Integer.parseInt(scanner.nextLine());
     }
 }
