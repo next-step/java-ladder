62c6dde58bbf24774243d8665a2daf7f5bb55da1
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-11 10:54:16 +0900

    feat: change anonymous class to lambda

--- src/test/java/nextstep/fp/CarTest.java
+++ src/test/java/nextstep/fp/CarTest.java
@@ -7,25 +7,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CarTest {
     @Test
     public void 이동() {
-        Car car = new Car("pobi", 0);
-        Car actual = car.move(new MoveStrategy() {
-            @Override
-            public boolean isMovable() {
-                return true;
-            }
-        });
-        assertThat(actual).isEqualTo(new Car("pobi", 1));
+        Car car = new Car("Sigrid", 0);
+        Car actual = car.move(() -> true);
+        assertThat(actual).isEqualTo(new Car("Sigrid", 1));
     }
 
     @Test
     public void 정지() {
-        Car car = new Car("pobi", 0);
-        Car actual = car.move(new MoveStrategy() {
-            @Override
-            public boolean isMovable() {
-                return false;
-            }
-        });
-        assertThat(actual).isEqualTo(new Car("pobi", 0));
+        Car car = new Car("Jin", 0);
+        Car actual = car.move(() -> false);
+        assertThat(actual).isEqualTo(new Car("Jin", 0));
     }
 }
