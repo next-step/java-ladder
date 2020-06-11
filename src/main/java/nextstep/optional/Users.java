71b52e7fbf63a5be585731ac805d922a8a2807f6
eunji <jeeell.922@gmail.com>
2020-06-11 21:37:51 +0900

    style 줄바꿈 변경

--- src/main/java/nextstep/optional/Users.java
+++ src/main/java/nextstep/optional/Users.java
@@ -2,6 +2,7 @@ package nextstep.optional;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class Users {
     static final User DEFAULT_USER = new User("codesquad", 100);
@@ -13,7 +14,9 @@ public class Users {
             new User("honux", 45));
 
     User getUser(String name) {
-        return users.stream().filter(user -> user.matchName(name))
-                .findFirst().orElse(DEFAULT_USER);
+        return users.stream()
+                .filter(user -> user.matchName(name))
+                .findFirst()
+                .orElse(DEFAULT_USER);
     }
 }
