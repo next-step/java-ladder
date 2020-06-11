b30447a0e402afe08e984e5fa3a56c90f2a496fb
Sungjun <sungjunpizz@gmail.com>
2020-06-12 08:08:34 +0900

    feat: Height 클래스 생성 추가

---
+++ src/main/java/ladder/domain/Height.java
@@ -0,0 +1,28 @@
+package ladder.domain;
+
+import java.util.Objects;
+
+public class Height {
+    private final int height;
+
+    public Height(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Height)) return false;
+        Height height1 = (Height) o;
+        return height == height1.height;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(height);
+    }
+}
