dbdaef4e2f546c36d4b96d01221053c6fcea97b3
Sungjun <sungjunpizz@gmail.com>
2020-06-11 23:20:25 +0900

    test: Player validateNameLength TC 추가

--- src/main/java/ladder/domain/Player.java
+++ src/main/java/ladder/domain/Player.java
@@ -4,7 +4,16 @@ public class Player {
     private final String name;
 
     public Player(String name) {
+        validateNameLength(name);
         this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        int NAME_MAX_LENGTH = 5;
+
+        if (name.length() > NAME_MAX_LENGTH) {
+            throw new PlayerNameLengthException();
+        }
     }
 
     public String getName() {
