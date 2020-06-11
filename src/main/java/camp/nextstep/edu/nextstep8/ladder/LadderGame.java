e7db0547d2120092a7876066ab1febc6035b8095
neojjc <neo.jjc@gmail.com>
2020-06-11 16:56:56 +0900

    [feat] 사다리 게임 추가

---
+++ src/main/java/camp/nextstep/edu/nextstep8/ladder/LadderGame.java
@@ -0,0 +1,8 @@
+package camp.nextstep.edu.nextstep8.ladder;
+
+public class LadderGame {
+    public static void main(String[] args) {
+        LadderInput.getJoinMembers();
+        LadderInput.getLadderHeight();
+    }
+}
