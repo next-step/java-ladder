ea2483c77befba6ea5da1d1bcce584906d5bb29d
하재권 <hazxz@naver.com>
2020-06-11 21:48:34 +0900

    feat(user): User class 테스트를 성공시키는 로직 개발
    * 이름은 5자 이하여야 한다
    * 이름은 5자 보다 크면 예외 발생

--- src/main/java/nextstep/ladder/domain/User.java
+++ src/main/java/nextstep/ladder/domain/User.java
@@ -2,9 +2,13 @@ package nextstep.ladder.domain;
 
 public class User {
 
+    private static final int MAX_LENGTH = 5;
     private final String name;
 
     public User(String name) {
+        if (name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("`name` length is must under " + MAX_LENGTH);
+        }
         this.name = name;
     }
 
