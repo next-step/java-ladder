52c67016b51bd380e90b9de92482678ab8d57ef5
Sungjun <sungjunpizz@gmail.com>
2020-06-12 08:05:06 +0900

    test: Players name 검증 TC 추가

--- src/test/java/ladder/domain/PlayersTest.java
+++ src/test/java/ladder/domain/PlayersTest.java
@@ -2,8 +2,15 @@ package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @DisplayName("Players 클래스 테스트")
 class PlayersTest {
@@ -13,8 +20,17 @@ class PlayersTest {
     void create_player() {
         String name = "pobi";
         String names = name + ",honux,crong,jk";
-        Players players = new Players(names);
+        Players players = new Players(Arrays.stream(names.split(",")).collect(Collectors.toList()));
 
         assertThat(players.getPlayers().get(0)).isEqualTo(new Player(name));
+    }
+
+    @DisplayName("null 또는 0개의 이름이 입력되면 예외를 반환한다.")
+    @NullAndEmptySource
+    @ParameterizedTest
+    void validate_names(final List<String> names) {
+        assertThatExceptionOfType(PlayersNamesException.class)
+                .isThrownBy(() -> new Players(names))
+                .withMessage(PlayersNamesException.MESSAGE);
     }
 }
