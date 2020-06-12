92dfd127a846efd6f248fae7a499d7c79ec638d4
Gobukgol <zxczc159@gmail.com>
2020-06-12 17:18:07 +0900

    feat(Ladder): 사다리를 전체를 만드는 Ladder 구현

---
+++ src/main/java/laddergame/domain/ladder/Ladder.java
@@ -0,0 +1,29 @@
+package laddergame.domain.ladder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ladder {
+    private static final int MIN_LADDER_HEIGHT = 1;
+    private final List<Line> lines;
+
+    public Ladder(int ladderHeight, int numberOfPlayer, BridgeGenerator bridgeGenerator) {
+        validateLadderHeight(ladderHeight);
+        this.lines = createLines(ladderHeight, numberOfPlayer, bridgeGenerator);
+    }
+
+    private List<Line> createLines(int ladderHeight, int numberOfPlayer, BridgeGenerator bridgeGenerator) {
+        List<Line> lines = new ArrayList<>();
+        for (int i = 0; i < ladderHeight; i++) {
+            lines.add(new Line(numberOfPlayer, bridgeGenerator));
+        }
+
+        return lines;
+    }
+
+    public void validateLadderHeight(int ladderHeight) {
+        if (ladderHeight < MIN_LADDER_HEIGHT) {
+            throw new IllegalArgumentException("사다리 높이가 너무 작습니다. - " + ladderHeight);
+        }
+    }
+}
