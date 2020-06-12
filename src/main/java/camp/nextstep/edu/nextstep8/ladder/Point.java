7f3ada9413b73452f37dcd9749e440a5bcbd437e
neojjc <neo.jjc@gmail.com>
2020-06-12 13:49:21 +0900

    [feat] 사다리 초기 버전

---
+++ src/main/java/camp/nextstep/edu/nextstep8/ladder/Point.java
@@ -0,0 +1,27 @@
+package camp.nextstep.edu.nextstep8.ladder;
+
+public class Point {
+    private boolean left;
+    private boolean right;
+
+    public Point() {
+        this.left = false;
+        this.right = false;
+    }
+
+    public boolean hasLeftLine() {
+        return left;
+    }
+
+    public boolean hasRightLine() {
+        return right;
+    }
+
+    public void markStart() {
+        this.right = true;
+    }
+
+    public void markEnd() {
+        this.left = true;
+    }
+}
