5b59a05a0e0e1a58f1ac0f9335b97859c4a8b0d5
kwonheejeong <gmlwjd9405@naver.com>
2020-06-12 09:40:52 +0900

    feat: Positions 기본 기능 구현

---
+++ src/main/java/ladder/domain/ladder/Positions.java
@@ -0,0 +1,30 @@
+package ladder.domain.ladder;
+
+import ladder.exception.ErrorMessage;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Positions {
+
+    private final List<Position> positions;
+
+    private Positions(final List<Position> positions) {
+        validatePositions(positions);
+        this.positions = positions;
+    }
+
+    public static Positions of(List<Position> positions) {
+        return new Positions(positions);
+    }
+
+    private void validatePositions(final List<Position> positions) {
+        if (Objects.isNull(positions)) {
+            throw new IllegalArgumentException(ErrorMessage.NULL_VALUE);
+        }
+    }
+
+    public Position indexOf(final int index) {
+        return positions.get(index);
+    }
+}
