407e8cff8ef40ca8144fe102dd08c25049e634e3
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2020-06-12 17:10:10 +0900

    Refactor: remove ternary operator

--- src/main/java/ladder/view/ResultView.java
+++ src/main/java/ladder/view/ResultView.java
@@ -1,6 +1,8 @@
 package ladder.view;
 
 import ladder.domain.ladder.Ladder;
+import ladder.domain.ladder.Line;
+import ladder.domain.ladder.Point;
 import ladder.domain.player.Player;
 import ladder.domain.player.Players;
 
@@ -14,10 +16,19 @@ public class ResultView {
         System.out.println("\n실행결과\n");
         players.stream().map(Player::getName).forEach(name -> System.out.printf("%6s", name));
         System.out.println();
-        ladder.stream().forEach(line -> {
-            System.out.print(BLANK_LINE);
-            line.stream().forEach(point -> System.out.print(COLUMN + (point.hasLine() ? LINE : BLANK_LINE)));
-            System.out.println();
-        });
+        ladder.stream().forEach(this::printLine);
+    }
+
+    private void printLine(Line line) {
+        System.out.print(BLANK_LINE);
+        line.stream().forEach(point -> System.out.print(COLUMN + getLine(point)));
+        System.out.println();
+    }
+
+    private String getLine(Point point) {
+        if (point.hasLine()) {
+            return LINE;
+        }
+        return BLANK_LINE;
     }
 }
