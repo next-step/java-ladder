4f465ac461f3e8823bd506a6b27e6a2eb5cdb826
DongHyukki <kderr2791@gmail.com>
2020-06-14 01:25:48 +0900

    feat : Participant, Generator Added : Participant, ParticipantGenerator - Participant ( participant name ) - get name - Generate Participant List by InputScanner

---
+++ src/main/java/ladder/StringInputScanner.java
@@ -0,0 +1,35 @@
+package ladder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class StringInputScanner {
+
+    private String[] namesString;
+
+    private void getInputStream() {
+        Scanner scanner = new Scanner(System.in);
+        this.namesString  = scanner.nextLine().replace(" ", "").split(",");
+    }
+
+    public List<String> getNames() {
+        getInputStream();
+
+        return Arrays.stream(namesString)
+                .map(wrapper(s -> String.valueOf(s)))
+                .collect(Collectors.toList());
+    }
+
+    private <T, R, E extends Exception> Function<T, R> wrapper(FunctionWithException<T, R, E> fe) {
+        return arg -> {
+            try {
+                return fe.apply(arg);
+            } catch (Exception e) {
+                throw new IllegalArgumentException("입력이 잘못 되었습니다.");
+            }
+        };
+    }
+}
