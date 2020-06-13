8a5b88b7b53a6c87e15b0948edccc4d1a8e7b350
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-06-13 17:24:17 +0900

    refactor (GameResult) : 잘못된 Key값에 대한 예외 처리 추가

--- src/main/java/ladder/domain/GameResult.java
+++ src/main/java/ladder/domain/GameResult.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class GameResult {
+    private static final String INVALID_PLAYER_NAME_MESSAGE = "해당 이름의 참가자는 존재하지 않습니다.";
 
     private final Map<String, String> gameResult;
 
@@ -11,11 +12,11 @@ public class GameResult {
         this.gameResult = gameResult;
     }
 
-    public String getGamePrizeNameByPlayerName(String playerName) {
-        return gameResult.get(playerName);
-    }
-
     public Set<String> getKeySet() {
         return gameResult.keySet();
+    }
+
+    public String getGamePrizeNameByPlayerName(String playerName) {
+        return gameResult.getOrDefault(playerName, INVALID_PLAYER_NAME_MESSAGE);
     }
 }
--- src/test/java/ladder/GameResultTest.java
+++ src/test/java/ladder/GameResultTest.java
@@ -1,6 +1,7 @@
 package ladder;
 
 import ladder.domain.GameResult;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,17 +11,26 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GameResultTest {
+    private GameResult gameResult;
+
+    @BeforeEach
+    public void setupGameResult() {
+        Map<String, String> gameResultMap = new HashMap<>();
+        gameResultMap.put("test1", "꽝");
+        gameResultMap.put("test2", "승리");
+        gameResult = new GameResult(gameResultMap);
+    }
 
     @DisplayName("GameResult 객체에서 Player 이름 문자열을 받으면 당첨 상품을 알려줌")
     @Test
     public void getGamePrizeByPlayerName() {
-        Map<String, String> gameResultMap = new HashMap<>();
-        gameResultMap.put("test1", "꽝");
-        gameResultMap.put("test2", "승리");
-
-        GameResult gameResult = new GameResult(gameResultMap);
-
         assertThat(gameResult.getGamePrizeNameByPlayerName("test1")).isEqualTo("꽝");
         assertThat(gameResult.getGamePrizeNameByPlayerName("test2")).isEqualTo("승리");
+    }
+
+    @DisplayName("GameResult 객체에 저장되지 않은 Key 이름으로 접근시 예외 처리 String 문구 반환")
+    @Test
+    public void throwExceptionWhenInvalidKeyName() {
+        assertThat(gameResult.getGamePrizeNameByPlayerName("null")).isEqualTo("해당 이름의 참가자는 존재하지 않습니다.");
     }
 }
