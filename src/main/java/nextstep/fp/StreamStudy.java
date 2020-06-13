4694d3d0254adde9bab3ecb078107ddebaf4f603
김다혜 <dahye432111@gmail.com>
2020-06-13 17:34:59 +0900

    refactor: filter를 사용해 for문 변경

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -15,10 +15,7 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
+        long count = words.stream().filter(w -> w.length() > 12).count();
         return count;
     }
 
