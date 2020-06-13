1115872ee3b2d26e8359ae282184caff04272b38
neojjc <neo.jjc@gmail.com>
2020-06-13 17:12:34 +0900

    [feat] Refactoring 완료

--- src/main/java/camp/nextstep/edu/nextstep8/ladder/entity/Ladder.java
+++ src/main/java/camp/nextstep/edu/nextstep8/ladder/entity/Ladder.java
@@ -3,9 +3,14 @@ package camp.nextstep.edu.nextstep8.ladder.entity;
 import camp.nextstep.edu.nextstep8.ladder.JoinMemberConvertor;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Ladder {
+    private static final String PIPE = "|";
+    private static final String LINE = "-----";
+    private static final String SPACE = "     ";
     private static Random RANDOM = new Random();
+
     private final int cols;
     private final int height;
     private final List<Row> rows;
@@ -35,22 +40,6 @@ public class Ladder {
         return new Row(points);
     }
 
-    public List<Row> getRows() {
-        return Collections.unmodifiableList(rows);
-    }
-
-    public List<String> getJoiners() {
-        return Collections.unmodifiableList(joiners);
-    }
-
-    public int getCols() {
-        return cols;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
     public void make() {
         int randomRows, randomCols;
         for(int i = 0; i < cols * height; i++) {
@@ -58,5 +47,43 @@ public class Ladder {
             randomCols = RANDOM.nextInt(cols - 1);
             rows.get(randomRows).draw(randomRows, randomCols);
         }
+    }
+
+    public String getHeaderPrintString() {
+        return joiners.stream().collect(Collectors.joining(SPACE));
+    }
+
+    public String getLadderPrintString() {
+        StringBuilder output = new StringBuilder();
+        int x = 0;
+        for(Row row : rows) {
+            output.append(getRowPrintString(row, x));
+            output.append("\n");
+            x++;
+        }
+        return output.toString();
+    }
+
+    private String getRowPrintString(Row row, int rowIndex) {
+        StringBuilder output = new StringBuilder();
+        for(int y = 0; y < cols; y++) {
+            output.append(PIPE);
+            output.append(resolveDrawLine(row.pick(rowIndex, y), row.pick(rowIndex, y + 1)));
+        }
+        return output.toString();
+    }
+
+    private boolean hasDrawLine(Point cur, Point next) {
+        if(cur.meetLast(cols)) {
+            return false;
+        }
+        return cur.isStart() && next.isEnd();
+    }
+
+    private String resolveDrawLine(Point cur, Point next) {
+        if(hasDrawLine(cur, next)) {
+            return LINE;
+        }
+        return SPACE;
     }
 }
