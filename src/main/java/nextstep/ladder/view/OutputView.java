480b70d07e499916adb4c1cea4b5e0639c163b60
csbsjy <seojaeyeon@lotte.net>
2020-06-14 18:06:27 +0900

    refactor : OutputView indent, 매직넘버

--- src/main/java/nextstep/ladder/view/OutputView.java
+++ src/main/java/nextstep/ladder/view/OutputView.java
@@ -4,6 +4,9 @@ import nextstep.ladder.domain.Ladder;
 import nextstep.ladder.domain.Point;
 
 public class OutputView {
+    private static final String VERTICAL_LADDER_LINE = "|";
+    private static final String LADDER_CONNECTION_LINE = "-----";
+    private static final String NAME_INTERVAL = "     ";
 
     private OutputView() {
     }
@@ -15,17 +18,29 @@ public class OutputView {
         System.out.println();
 
         for (int currentPosition = 0; currentPosition < ladder.getMaxHeight(); currentPosition++) {
-            StringBuilder stringBuilder = new StringBuilder("     ");
-            for (int i = 0; i < ladder.size(); i++) {
-                stringBuilder.append("|");
-                if (ladder.getConnectionPoints(i).has(Point.of(currentPosition))) {
-                    stringBuilder.append("-----");
-                } else {
-                    stringBuilder.append("     ");
-                }
-            }
-            System.out.println(stringBuilder.toString());
+            StringBuilder outputResult = new StringBuilder(NAME_INTERVAL);
+            drawLadderLine(ladder, currentPosition, outputResult);
+            System.out.println(outputResult.toString());
         }
 
+    }
+
+    private static void drawLadderLine(final Ladder ladder, final int currentPosition, final StringBuilder outputResult) {
+        for (int i = 0, count = ladder.getNumberOfBaseLine(); i < count; i++) {
+            outputResult.append(VERTICAL_LADDER_LINE);
+            drawConnectionLine(ladder, currentPosition, outputResult, i);
+        }
+    }
+
+    private static void drawConnectionLine(final Ladder ladder, final int currentPosition, final StringBuilder outputResult, final int i) {
+        if (isConnected(ladder, currentPosition, i)) {
+            outputResult.append(LADDER_CONNECTION_LINE);
+        } else {
+            outputResult.append(NAME_INTERVAL);
+        }
+    }
+
+    private static boolean isConnected(final Ladder ladder, final int currentPosition, final int i) {
+        return ladder.getConnectionPoints(i).has(Point.of(currentPosition));
     }
 }
