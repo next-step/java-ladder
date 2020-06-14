43418365d1d6da9a3bcc8898f12d78159bc5560b
chulman <chlcjfals0122@gmail.com>
2020-06-14 12:01:13 +0900

    test (UserConverter) : input에 따라 값 반환 메소드 테스트 추가 및 구현

--- src/main/java/nextstep/ladder/utils/UserConverter.java
+++ src/main/java/nextstep/ladder/utils/UserConverter.java
@@ -6,8 +6,9 @@ import nextstep.ladder.domain.user.User;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 public class UserConverter {
+
+    private static final String ALL = "all";
 
     private UserConverter() {
     }
@@ -15,6 +16,16 @@ public class UserConverter {
     public static List<User> convertToList(String[] users) {
         return IntStream.range(0, users.length)
                 .mapToObj(operand -> User.newInstance(users[operand], Order.from(operand)))
+                .collect(Collectors.toList());
+    }
+
+    public static List<User> convertToListFromInput(String input, List<User> users) {
+        if (input.equalsIgnoreCase(ALL)) {
+            return users;
+        }
+        return IntStream.range(0, users.size())
+                .mapToObj(users::get)
+                .filter(user -> user.getName().equals(input))
                 .collect(Collectors.toList());
     }
 }
