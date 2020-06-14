bc304af1d67cd92b5ea9773c3a219ceacc1e2586
Jaehun Lee <ljhokgo@gmail.com>
2020-06-15 01:42:16 +0900

    feat: Lines 클래스 추가

---
+++ src/main/java/nextstep/ladder/domain/line/Lines.java
@@ -0,0 +1,20 @@
+package nextstep.ladder.domain.line;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lines {
+    private List<Line> lines = new ArrayList<>();
+
+    public Lines(int playerCount, int ladderHeight) {
+        System.out.println(playerCount);
+
+        for(int i = 0; i < ladderHeight; i++) {
+            lines.add(new Line(playerCount));
+        }
+    }
+
+    public List<Line> getLines() {
+        return lines;
+    }
+}
