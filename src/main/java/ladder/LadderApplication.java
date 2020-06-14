ccee0e81e9d1de5f7389dd3b47e320c9c2f57ff7
eunji <jeeell.922@gmail.com>
2020-06-14 23:50:46 +0900

    feat 사다리게임 메인 추가

--- src/main/java/ladder/LadderApplication.java
+++ src/main/java/ladder/LadderApplication.java
@@ -1,4 +1,23 @@
 package ladder;
 
+import ladder.domain.Ladder;
+import ladder.domain.Participants;
+import ladder.view.InputView;
+
 public class LadderApplication {
+
+    public static void main(String[] args) {
+        //참가자 생성
+        String nameOfPerson = InputView.enterNameOfPerson();
+        Participants participants = Participants.create(nameOfPerson);
+
+        //사다리 생성
+        int ladderHeight = InputView.enterLadderHeight();
+        int countOfPerson = participants.tellCountOfPerson();
+        Ladder ladder = Ladder.create(ladderHeight, countOfPerson);
+
+        //결과 출력
+        ResultView.printLadder(participants, ladder);
+
+    }
 }
