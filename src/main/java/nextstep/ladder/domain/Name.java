21c69915b9688766a146a75cc1d81983272ff706
Jaehun Lee <ljhokgo@gmail.com>
2020-06-14 22:44:44 +0900

    feat(Name): 이름 생성 기능 추가

---
+++ src/main/java/nextstep/ladder/domain/Name.java
@@ -0,0 +1,22 @@
+package nextstep.ladder.domain;
+
+public class Name {
+    private static final int NAME_UPPER_BOUND = 5;
+
+    private final String name;
+
+    public Name(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if(name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("이름에 빈 문자가 들어가면 안됩니다.");
+        }
+
+        if(name.length() > NAME_UPPER_BOUND) {
+            throw new IllegalArgumentException(String.format("이름은 %d 글자를 넘으면 안됩니다.", NAME_UPPER_BOUND));
+        }
+    }
+}
