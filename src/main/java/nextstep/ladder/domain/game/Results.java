55a9b0de7be24e7e09cc1348a77468473f941f19
chulman <chlcjfals0122@gmail.com>
2020-06-14 11:07:48 +0900

    feat (Results) : feat get method

--- src/main/java/nextstep/ladder/domain/game/Results.java
+++ src/main/java/nextstep/ladder/domain/game/Results.java
@@ -1,16 +1,24 @@
 package nextstep.ladder.domain.game;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Results {
 
     private final List<Result> results;
 
     public Results(List<Result> results) {
+        if (Objects.isNull(results)) {
+            throw new IllegalArgumentException("inject result list is null from results class");
+        }
         this.results = results;
     }
 
-    public int size(){
+    public int size() {
         return results.size();
+    }
+
+    public Result get(int index) {
+        return results.get(index);
     }
 }
--- src/test/java/nextstep/ladder/domain/game/ResultsTest.java
+++ src/test/java/nextstep/ladder/domain/game/ResultsTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class ResultsTest {
 
@@ -21,10 +23,24 @@ public class ResultsTest {
         assertThat(results.size()).isEqualTo(resultList.size());
     }
 
+    @NullSource
+    @ParameterizedTest
+    @DisplayName("results 생성 테스트")
+    void exception(List<Result> resultList) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Results(resultList));
+    }
+
+    @MethodSource("결과셋_생성")
+    @ParameterizedTest
+    @DisplayName("get 검증 테스트")
+    void get(List<Result> resultList) {
+        Results results = new Results(resultList);
+        assertThat(results.get(0)).isEqualTo(resultList.get(0));
+    }
+
     private static Stream<Arguments> 결과셋_생성() {
-
         List<Result> resultList = Arrays.asList(Result.from("꽝"), Result.from("50000"), Result.from("꽝"), Result.from("10000"));
-
         return Stream.of(Arguments.of(resultList));
     }
 }
