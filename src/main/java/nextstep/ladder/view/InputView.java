9afefca5c183998466c8c193a0bd59a25d75cada
Jaehun Lee <ljhokgo@gmail.com>
2020-06-14 23:01:51 +0900

    feat(ui): 사다리 높이 입력 기능 추가

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -1,5 +1,6 @@
 package nextstep.ladder.view;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
@@ -13,5 +14,23 @@ public class InputView {
         String names = scanner.nextLine();
 
         return names.split(PLAYER_NAME_DELIMITER);
+    }
+
+    public static int scanLadderHeight() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+
+        int input;
+
+        try {
+            input = scanner.nextInt();
+            scanner.nextLine();     // 버퍼에서 개행문자 제거
+        } catch(InputMismatchException e) {
+            scanner.nextLine();     // 버퍼에서 개행문자 제거
+            System.out.println("숫자만 입력 가능합니다. 다시 입력해주세요.");
+
+            return scanLadderHeight();
+        }
+
+        return input;
     }
 }
