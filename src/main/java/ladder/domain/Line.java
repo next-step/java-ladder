75ad1b2256c1ca9d177a3d1e604f5cd030b16f86
eunji <jeeell.922@gmail.com>
2020-06-14 22:39:48 +0900

    feat Line 클래스/테스트코드 추가

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -2,11 +2,36 @@ package ladder.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class Line {
-    private List<Boolean> points = new ArrayList<>();
+    private static final Random RANDOM = new Random();
+    private List<Boolean> points;
 
     public Line(int countOfPerson) {
-        // 라인의 좌표 값에 선이 있는지 유무를 판단하는 로직 추가
+        this.points = createLine(countOfPerson);
+    }
+
+    public static Line create(int countOfPerson) {
+        return new Line(countOfPerson);
+    }
+
+    private List<Boolean> createLine(int countOfPerson) {
+        List<Boolean> points = new ArrayList<>();
+        IntStream.rangeClosed(0, countOfPerson)
+                .mapToObj(i -> false)
+                .reduce((point, first) -> {
+                    points.add(point);
+                    return checkLineOverlap(point);
+                });
+        return points;
+    }
+
+    boolean checkLineOverlap(Boolean point) {
+        if (point) {
+            return false;
+        }
+        return RANDOM.nextBoolean();
     }
 }
--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -1,5 +1,27 @@
 package ladder.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
 class LineTest {
+    int countOfPerson = 3;
+
+    @Test
+    @DisplayName("생성 테스트")
+    void create() {
+        assertThatCode(() -> Line.create(countOfPerson)).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("이 전 값이 true이면 false를 반환한다")
+    void checkLineOverlap() {
+        Line line = Line.create(countOfPerson);
+        boolean point = line.checkLineOverlap(true);
+
+        assertThat(point).isFalse();
+    }
 
 }
