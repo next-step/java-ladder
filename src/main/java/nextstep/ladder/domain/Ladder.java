bbb2d7a9b631cade8693db0b00601cf31c0f63f4
하재권 <hazxz@naver.com>
2020-06-14 11:39:02 +0900

    refactor: 변수명에 타입을 포함시키지 않도록 수정

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -4,15 +4,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Ladder {
-    private final List<Line> lineList;
+    private final List<Line> lines;
 
     public Ladder(LadderGenerator ladderGenerator) {
-        this.lineList = ladderGenerator.generate().stream()
+        this.lines = ladderGenerator.generate().stream()
                 .map(Line::new)
                 .collect(Collectors.toList());
     }
 
-    public List<Line> getLineList() {
-        return lineList;
+    public List<Line> getLines() {
+        return lines;
     }
 }
--- src/main/java/nextstep/ladder/ui/ResultView.java
+++ src/main/java/nextstep/ladder/ui/ResultView.java
@@ -13,22 +13,22 @@ public class ResultView {
     private static final String RESULT_LADDER_OPEN = "-----|";
     private static final String RESULT_LADDER_CLOSE = "     |";
 
-    public static void printResult(List<User> userList, Ladder ladder) {
+    public static void printResult(List<User> users, Ladder ladder) {
         System.out.println(RESULT_NOTICE_COMMENT);
 
-        userList.stream()
+        users.stream()
                 .forEach(user -> System.out.print(String.format(RESULT_USER_NAME_FORMAT, user.getName())));
 
         System.out.print("\n");
 
-        ladder.getLineList().stream()
+        ladder.getLines().stream()
                 .forEach(line -> System.out.println(getLineString(line)));
     }
 
     private static String getLineString(Line line) {
-        List<String> lineStringList = line.getPoints().stream()
+        List<String> lineShapes = line.getPoints().stream()
                 .map(b -> b ? RESULT_LADDER_OPEN : RESULT_LADDER_CLOSE)
                 .collect(Collectors.toList());
-        return String.join("", lineStringList);
+        return String.join("", lineShapes);
     }
 }
