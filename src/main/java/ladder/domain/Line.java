8be3793ecf0624e5d3dec724ec22f0401a2e8d9a
DongHyukki <kderr2791@gmail.com>
2020-06-14 23:18:53 +0900

    refact : Line
    Changes :
    - Line ( PRE_CHECKOUT_POINT value 1 -> 2 )

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -2,42 +2,51 @@ package ladder.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.IntStream;
 
 public class Line {
 
-    List<Linetype> lineTypeList = new ArrayList<>();
     private static final int PER_PERSON_LINE = 2;
     private static final int CHUNKER_VALUE = 2;
     private static final int MIN_CHECKOUT_POINT = 1;
-    private static final int PRE_CHECKOUT_POINT = 1;
+    private static final int PRE_CHECKOUT_POINT = 2;
+    private List<Linetype> lineTypes = new ArrayList<>();
+    private PossibilityStrategy possibilityStrategy;
 
     public Line() {
-
     }
 
-    public Line(int participantCount) {
+    public Line(List<Linetype> lineTypes) {
+        this.lineTypes = lineTypes;
+    }
+
+    public Line(int participantCount, PossibilityStrategy possibilityStrategy) {
+        this.possibilityStrategy = possibilityStrategy;
+
         IntStream.rangeClosed(0, (participantCount - 1) * PER_PERSON_LINE).forEach(point -> {
-            lineTypeList.add(makeType(point));
+            lineTypes.add(makeType(point));
         });
     }
 
-    public Linetype makeType(int point) {
+    public List<Linetype> getLineTypes() {
+        return lineTypes;
+    }
+
+    private Linetype makeType(int point) {
         if (point % CHUNKER_VALUE == 0) {
             return Linetype.VERTICAL;
         }
         return getRandomType(point);
     }
 
-    public Linetype getRandomType(int point) {
+    private Linetype getRandomType(int point) {
         if (checkPrePoint(point)) {
             return Linetype.BLANK;
         }
-        return getHorizontalType(new HalfPossibilityStrategy());
+        return getHorizontalType(possibilityStrategy);
     }
 
-    public Linetype getHorizontalType(PossibilityStrategy possibilityStrategy) {
+    private Linetype getHorizontalType(PossibilityStrategy possibilityStrategy) {
         if (possibilityStrategy.check()) {
             return Linetype.HORIZONTAL;
         }
@@ -48,14 +57,6 @@ public class Line {
         if (point <= MIN_CHECKOUT_POINT) {
             return false;
         }
-        return lineTypeList.get(point - PRE_CHECKOUT_POINT).equals(Linetype.HORIZONTAL);
-    }
-
-    public Line(List<Linetype> lineTypeList) {
-        this.lineTypeList = lineTypeList;
-    }
-
-    public List<Linetype> getLineTypeList() {
-        return lineTypeList;
+        return lineTypes.get(point - PRE_CHECKOUT_POINT).equals(Linetype.HORIZONTAL);
     }
 }
