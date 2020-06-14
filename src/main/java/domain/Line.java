9bf0cca5034565f93d131776b07c9809d89dff6f
Seoyeong-Kim <seoyeong2352@gmail.com>
2020-06-14 15:51:03 +0900

    step1(feat) line 구현, random하게 연결되는 generator구현

---
+++ src/main/java/domain/Line.java
@@ -0,0 +1,33 @@
+package domain;
+
+import generator.PointGenerator;
+import generator.RandomPointGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Line {
+    private final List<Boolean> points;
+
+    public Line(int countOfPerson) {
+        this(countOfPerson, new RandomPointGenerator());
+    }
+
+    public Line(int countOfPerson, PointGenerator pointGenerator) {
+        this.points = createPoints(countOfPerson, pointGenerator);
+    }
+
+    private List<Boolean> createPoints(int length, PointGenerator pointGenerator) {
+        List<Boolean> points = new ArrayList<>();
+        points.add(pointGenerator.isConnect(false));
+        for(int i=1; i<length; i++) {
+            points.add(pointGenerator.isConnect(points.get(i-1)));
+        }
+
+        return points;
+    }
+
+    public int getPersonSize() {
+        return points.size();
+    }
+}
