87960bf3f117a5392cba146f8eee255a3e5f19e8
jwooss <wodn4131@gmail.com>
2020-06-15 23:46:39 +0900

    feat: printLongestWordTop100 메서드 구현

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -29,7 +30,12 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(word -> word.length() > 12)
+                .distinct()
+                .sorted(Comparator.comparing(String::length).reversed())
+                .limit(100)
+                .forEach(word -> System.out.println(word.toLowerCase()));
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -41,12 +47,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        Predicate<Integer> isOverThree = isGraterThan(3);
-        Function<Integer, Integer> calculateDouble = multiply(2);
-
         return numbers.stream()
-                .filter(isOverThree)
-                .map(calculateDouble)
+                .filter(isGraterThan(3))
+                .map(multiply(2))
                 .reduce(Integer::sum)
                 .orElse(0);
     }
