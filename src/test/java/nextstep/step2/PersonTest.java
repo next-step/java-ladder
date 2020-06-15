3ca26903e5e10174fc440b10f1e03bae7aa8ef73
bbbnam <bbbnam@naver.com>
2020-06-15 22:39:32 +0900

    test : Person 사람 입력 구현 테스트

---
+++ src/test/java/nextstep/step2/PersonTest.java
@@ -0,0 +1,19 @@
+package nextstep.step2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import step2.domain.Person;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PersonTest {
+
+    @ParameterizedTest
+    @DisplayName("사람 이름 유효성 체크 - 최대 5자")
+    @ValueSource(strings = {"pobi123", "honux1", "crongi", "jklmnop"})
+    void InvalidPerson(String name){
+        assertThatThrownBy(() -> Person.of(name))
+        .isInstanceOf(IllegalArgumentException.class);
+    }
+}
