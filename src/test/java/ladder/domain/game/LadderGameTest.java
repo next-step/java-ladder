f3a8f8aeefee0b489afbc7755c954a88c221507e
Lim Geun <shing100@naver.com>
2020-06-16 00:06:47 +0900

    Test(사다리 게임 관련 테스트 추가)

--- src/test/java/ladder/domain/game/LadderGameTest.java
+++ src/test/java/ladder/domain/game/LadderGameTest.java
@@ -1,7 +1,48 @@
 package ladder.domain.game;
 
-import static org.junit.jupiter.api.Assertions.*;
+import ladder.domain.ladder.Line;
+import ladder.domain.ladder.Lines;
+import ladder.domain.ladder.ladderInfo.LadderHeight;
+import ladder.domain.player.LadderPlayer;
+import ladder.domain.player.LadderPlayers;
+import ladder.ui.InputView;
+import ladder.ui.ResultView;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LadderGameTest {
+
+    @Test
+    void LadderGameCreateLadderTest() {
+        Lines ladder = LadderGame.createLadder(5, LadderHeight.inputHeight(5));
+
+        List<Line> lineList = ladder.getLineList();
+        assertThat(lineList.size()).isEqualTo(5);
+    }
+
+    @Test
+    void LadderGameStartTest() {
+        String[] playersName = {"a", "b", "c", "d"};
+        String[] rewards = {"100", "꽝", "1000", "꽝"};
+
+        LadderPlayers ladderPlayers = LadderPlayers.participate(playersName);
+        LadderRewards ladderRewards = LadderRewards.inputRewards(rewards);
+        LadderHeight ladderHeight = LadderHeight.inputHeight(5);
+        Lines ladder = LadderGame.createLadder(ladderPlayers.getPlayerCount(), ladderHeight);
+        LadderGameResults results = LadderGame.start(ladder, ladderPlayers, ladderRewards);
+
+        Map<LadderPlayer, LadderReward> results1 = results.getResults();
+        LadderPlayer ladderPlayerA = LadderPlayer.create("a");
+        LadderPlayer ladderPlayerB = LadderPlayer.create("b");
+        LadderPlayer ladderPlayerE = LadderPlayer.create("e");
+
+        assertThat(results1.containsKey(ladderPlayerA)).isTrue();
+        assertThat(results1.containsKey(ladderPlayerB)).isTrue();
+        assertThat(results1.containsKey(ladderPlayerE)).isFalse();
+    }
 
 }
--- src/test/java/ladder/domain/game/LadderRewardsTest.java
+++ src/test/java/ladder/domain/game/LadderRewardsTest.java
@@ -1,7 +1,28 @@
 package ladder.domain.game;
 
-import static org.junit.jupiter.api.Assertions.*;
+import ladder.domain.ladder.Position;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LadderRewardsTest {
+
+    @Test
+    void LadderRewardsCreateTest() {
+        String[] rewards = {"1000", "10", "10000", "1"};
+        LadderRewards ladderRewards = LadderRewards.inputRewards(rewards);
+        LadderReward ladderReward = ladderRewards.getLadderReward(new Position(1));
+        String reward = ladderReward.getReward();
+
+        assertThat(reward).isEqualTo("10");
+    }
+
+    @Test
+    void LadderRewardsCreate_Fail_Test() {
+        String[] rewards = null;
+        assertThatThrownBy(() ->LadderRewards.inputRewards(rewards))
+                .hasMessageMatching("실행 결과를 입력하세요");
+    }
 
 }
