c2a430e833d1900f6b61d59fc3e1d76012b13c08
Gobukgol <zxczc159@gmail.com>
2020-06-15 15:58:40 +0900

    refactor(Position): Column에 대한 유효성 검사 추가

--- src/main/java/laddergame/domain/player/Position.java
+++ src/main/java/laddergame/domain/player/Position.java
@@ -2,10 +2,12 @@ package laddergame.domain.player;
 
 public class Position {
     private static final int START_HEIGHT = 1;
+    private static final int MIN_COLUMN = 1;
     private final int height;
     private final int column;
 
     public Position(int column) {
+        validateColumn(column);
         this.height = START_HEIGHT;
         this.column = column;
     }
@@ -16,5 +18,11 @@ public class Position {
 
     public int getColumn() {
         return this.column;
+    }
+
+    private void validateColumn(int column) {
+        if (column < MIN_COLUMN) {
+            throw new IllegalArgumentException("Column 번호는 1 이상이어야 합니다. - " + column);
+        }
     }
 }
--- src/test/java/laddergame/domain/player/PositionTest.java
+++ src/test/java/laddergame/domain/player/PositionTest.java
@@ -1,10 +1,12 @@
 package laddergame.domain.player;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PositionTest {
     @DisplayName("처음 시작 위치는 자신의 열번호와 높이 1로 구성된다.")
@@ -15,5 +17,14 @@ class PositionTest {
 
         assertThat(position.getHeight()).isEqualTo(1);
         assertThat(position.getColumn()).isEqualTo(column);
+    }
+
+    @DisplayName("열번호가 1보다 작으면 IllegalArgumentExcetpion Throw")
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -2})
+    void validateColumn(int column) {
+        assertThatThrownBy(() -> new Position(column))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Column 번호는 1 이상이어야 합니다. - " + column);
     }
 }
