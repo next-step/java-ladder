b00ee56fa6c0a1021a47cd296368b4a93b5366b8
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-06-16 06:12:03 +0900

    feat : 메인 작성

--- src/main/java/laddergame/LadderGame.java
+++ src/main/java/laddergame/LadderGame.java
@@ -1,7 +1,10 @@
 package laddergame;
 
+import java.util.Map;
 import java.util.Scanner;
 import laddergame.model.Ladder;
+import laddergame.model.LadderResultPositions;
+import laddergame.model.Name;
 import laddergame.model.Names;
 import laddergame.model.PositiveNumber;
 import laddergame.view.LadderGameView;
@@ -11,14 +14,31 @@ public class LadderGame {
   private static final Scanner scanner = new Scanner(System.in);
 
   public static void main(String[] args) {
-    LadderGameView.printInputNameMsg();
-    Names names = Names.createByNameStrArr(scanner.nextLine().split(","));
+    LadderGameView.printInputPlayerNamesMsg();
+    Names players = Names.createByNameStrArr(scanner.nextLine().split(","));
+
+    LadderGameView.printInputResultNamesMsg();
+    Names results = Names.createByNameStrArrWithLength(
+        scanner.nextLine().split(","), players.getCountOfNames());
 
     LadderGameView.printInputLadderHeightMsg();
     PositiveNumber ladderHeight = new PositiveNumber(scanner.nextInt());
     scanner.nextLine();
 
-    LadderGameView
-        .printResult(names, Ladder.createByHeightAndCountOfPerson(ladderHeight, names.getCountOfNames()));
+    Ladder ladder = Ladder.createByHeightAndCountOfPerson(ladderHeight, players.getCountOfNames());
+
+    LadderGameView.printLadder(players, ladder, results);
+
+    LadderResultPositions resultPositions = LadderResultPositions
+        .createByLadderWithCountOfPerson(ladder, players.getCountOfNames());
+
+    Map<Name, Name> resultMap = resultPositions.getMapOf(players, results);
+
+    while (true) {
+      LadderGameView.printInputResultOfPlayerMsg();
+      LadderGameView.printResult(resultMap, new Name(scanner.nextLine()));
+    }
+
+
   }
 }
