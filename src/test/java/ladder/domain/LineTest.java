cb15d025b73e2bff9a240656ec7e1d42029e63ec
Sungjun <sungjunpizz@gmail.com>
2020-06-16 23:36:54 +0900

    test: Line 클래스 필요없는 TC 제거

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -16,30 +16,5 @@ class LineTest {
 
         assertThat(line.getPoints()).hasSize(countOfPlayers);
     }
-
-    @DisplayName("첫 번째는 기둥 사이를 잇는 다리를 놓을 수 없다.")
-    @Test
-    void first_is_false() {
-        Line line = new Line(countOfPlayers, new RandomPointGenerationStrategy());
-
-        assertThat(line.getPoints().get(0).isPoint()).isFalse();
-    }
-
-
-    @DisplayName("기둥 사이를 잇는 다리를 연속해서 생성할 수 없다.")
-    @Test
-    void cannot_be_created_continuously() {
-        Line line = new Line(countOfPlayers, new AlwaysTruePointGenerationStrategy());
-        assertThat(line.getPoints()).hasSize(countOfPlayers);
-        assertThat(line.getPoints().get(0).isPoint()).isFalse();
-        assertThat(line.getPoints().get(1).isPoint()).isTrue();
-        assertThat(line.getPoints().get(2).isPoint()).isFalse();
-    }
-
-    private static class AlwaysTruePointGenerationStrategy implements PointGenerationStrategy {
-        @Override
-        public boolean generate() {
-            return true;
-        }
-    }
 }
+
