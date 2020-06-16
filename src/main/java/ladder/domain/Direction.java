5d703ed83661a42e1bec59c3e524b5a3ad435237
Sungjun <sungjunpizz@gmail.com>
2020-06-16 22:52:47 +0900

    test: Direction 클래스 next 메소드 추가
    - equals, hashCode 추가

--- src/main/java/ladder/domain/Direction.java
+++ src/main/java/ladder/domain/Direction.java
@@ -1,5 +1,7 @@
 package ladder.domain;
 
+import java.util.Objects;
+
 import static java.lang.Boolean.FALSE;
 
 public class Direction {
@@ -19,11 +21,36 @@ public class Direction {
         return new Direction(this.right, FALSE);
     }
 
+    public Direction next(boolean nextRight) {
+        return new Direction(this.right, nextRight);
+    }
+
+    public Direction next(PointGenerationStrategy strategy) {
+        if (this.right) {
+            return next(FALSE);
+        }
+        return next(strategy.generate());
+    }
+
     public boolean isLeft() {
         return left;
     }
 
     public boolean isRight() {
         return right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Direction)) return false;
+        Direction direction = (Direction) o;
+        return left == direction.left &&
+                right == direction.right;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
     }
 }
