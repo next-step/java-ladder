e7ad1e78b3a1600dc93694c4eeed9b7d7bb6c7fe
Sungjun <sungjunpizz@gmail.com>
2020-06-16 22:16:59 +0900

    feat: Direction 클래스 추가

---
+++ src/main/java/ladder/domain/Direction.java
@@ -0,0 +1,11 @@
+package ladder.domain;
+
+public class Direction {
+    private final boolean left;
+    private final boolean right;
+
+    public Direction(boolean left, boolean right) {
+        this.left = left;
+        this.right = right;
+    }
+}
