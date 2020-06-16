ffa1dfda79fd700dad05c35271fb007c3ba859ec
Sungjun <sungjunpizz@gmail.com>
2020-06-16 22:21:20 +0900

    refactor: Direction first 메소드 추가

--- src/main/java/ladder/domain/Direction.java
+++ src/main/java/ladder/domain/Direction.java
@@ -1,5 +1,7 @@
 package ladder.domain;
 
+import static java.lang.Boolean.FALSE;
+
 public class Direction {
     private final boolean left;
     private final boolean right;
@@ -7,5 +9,17 @@ public class Direction {
     public Direction(boolean left, boolean right) {
         this.left = left;
         this.right = right;
+    }
+
+    public static Direction first(boolean right) {
+        return new Direction(FALSE, right);
+    }
+
+    public boolean isLeft() {
+        return left;
+    }
+
+    public boolean isRight() {
+        return right;
     }
 }
