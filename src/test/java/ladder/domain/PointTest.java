bd4b90e50dffaa2aa0b5b16ee7540210deb3cc4d
Sungjun <sungjunpizz@gmail.com>
2020-06-16 23:28:10 +0900

    test: Point next 메소드 TC 추가

--- src/test/java/ladder/domain/PointTest.java
+++ src/test/java/ladder/domain/PointTest.java
@@ -19,4 +19,12 @@ class PointTest {
         Point last = Point.first(new AlwaysTruePointGenerationStrategy()).last();
         assertThat(last.move()).isEqualTo(0);
     }
+
+    @Test
+    void next() {
+        PointGenerationStrategy strategy = new AlwaysTruePointGenerationStrategy();
+        Point next = Point.first(strategy).next(strategy);
+
+        assertThat(next.move()).isEqualTo(0);
+    }
 }
