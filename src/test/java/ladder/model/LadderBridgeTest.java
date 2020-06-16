f7650021084925782d357f13f11387b27715114a
Kim-yongjin <yongjin@sixshop.com>
2020-06-17 07:54:30 +0900

    <test> [Step 1] Random Generator 생성 테스트, randomBridge 동작 테스트 추가

--- src/test/java/ladder/model/LadderBridgeTest.java
+++ src/test/java/ladder/model/LadderBridgeTest.java
@@ -1,11 +1,10 @@
 package ladder.model;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 @DisplayName("사다리 가로 라인 테스트")
 public class LadderBridgeTest {
@@ -16,5 +15,11 @@ public class LadderBridgeTest {
         assertThat(LadderBridge.makeLadderBridgeByPreBridge(LadderBridge.EXIST)).isNotEqualTo(LadderBridge.EXIST);
         assertThat(LadderBridge.makeLadderBridgeByPreBridge(null)).isIn(Arrays.asList(LadderBridge.values()));
         assertThat(LadderBridge.makeLadderBridgeByPreBridge(LadderBridge.NOT_EXIST)).isIn(Arrays.asList(LadderBridge.values()));
+    }
+
+    @DisplayName("랜덤 가로 라인 테스트")
+    @Test
+    public void randomBridgeTest() {
+        assertThat(LadderBridge.randomBridge()).isIn(Arrays.asList(LadderBridge.values()));
     }
 }
