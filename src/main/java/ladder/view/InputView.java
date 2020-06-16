5874a805a311c25ca73a0fc6292a5c7270b3e09e
Sungjun <sungjunpizz@gmail.com>
2020-06-17 01:31:33 +0900

    feat: InputView 결과 출력 추가

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -9,7 +9,8 @@ public class InputView {
     private static final String DELIMITER = ",";
     private static final String PRINT_PLAYER_NAMES = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String PRINT_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
-    private static final String PRINT_RESULT= "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private static final String PRINT_REWARDS= "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private static final String PRINT_RESULT= "결과를 보고 싶은 사람은?";
 
     public static List<String> printPlayers() {
         Scanner scanner = new Scanner(System.in);
@@ -29,7 +30,7 @@ public class InputView {
 
     public static List<String> printRewards() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(PRINT_RESULT);
+        System.out.println(PRINT_REWARDS);
 
         String rewards = scanner.nextLine();
 
@@ -39,5 +40,12 @@ public class InputView {
     private static List<String> getList(String names) {
         return Arrays.stream(names.split(DELIMITER))
                 .collect(Collectors.toList());
+    }
+
+    public static String printResult() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(PRINT_RESULT);
+
+        return scanner.nextLine();
     }
 }
