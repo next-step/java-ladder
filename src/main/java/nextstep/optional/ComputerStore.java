d0f8aafa706a3796fe51034d0773fc4d91207d5a
kimdahyeee <dahye432111@gmail.com>
2020-06-16 09:35:55 +0900

    1단계 - 자바8 스트림, 람다, Optional (#450)
    * refactor: 익명 클래스를 람다로 전환
    
    * refactor: 람다를 활용해 중복 제거
    
    * refactor: filter를 사용해 for문 변경
    
    * feat: sumOverThreeAndDouble() 메서드 구현
    
    * feat: stream 사용하여 printLongestWordTop100() 메서드 구현
    
    * feat: Optional을 사용하여 ageIsInRange2() 메서드 구현
    
    * refactor: orElse() 메소드 활용하여 getUser() 메소드 변경
    
    * refactor: Expression의 of 메소드를 Optional을 통해 exception 처리하도록 변경
    
    * refactor: filter 조건 병합
    
    * refactor: stream() 구문 병합 및 라인 개행
    
    * refactor: for문 stream() 사용하여 변경
    
    * refactor: stream() 개행
    
    * refactor: lambda 축약형으로 변경
    
    * refactor: getVersionOptional() 메소드 Optional 사용하여 변경

--- src/main/java/nextstep/optional/ComputerStore.java
+++ src/main/java/nextstep/optional/ComputerStore.java
@@ -3,11 +3,14 @@ package nextstep.optional;
 import nextstep.optional.Computer.Soundcard;
 import nextstep.optional.Computer.USB;
 
+import java.util.Optional;
+
 public class ComputerStore {
     public static final String UNKNOWN_VERSION = "UNKNOWN";
 
     public static String getVersion(Computer computer) {
         String version = UNKNOWN_VERSION;
+
         if (computer != null) {
             Soundcard soundcard = computer.getSoundcard();
             if (soundcard != null) {
@@ -21,6 +24,10 @@ public class ComputerStore {
     }
 
     public static String getVersionOptional(Computer computer) {
-        return null;
+        return Optional.ofNullable(computer)
+                .map(Computer::getSoundcard)
+                .map(Soundcard::getUsb)
+                .map(USB::getVersion)
+                .orElse(UNKNOWN_VERSION);
     }
 }
