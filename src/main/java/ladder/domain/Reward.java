bf67803b2dd55d6f65f39707c8fd9f2da4a8b5e0
Sungjun <sungjunpizz@gmail.com>
2020-06-16 21:57:20 +0900

    feat: Reward 클래스 추가

---
+++ src/main/java/ladder/domain/Reward.java
@@ -0,0 +1,24 @@
+package ladder.domain;
+
+import java.util.Objects;
+
+public class Reward {
+    private final String reward;
+
+    public Reward(String reward) {
+        this.reward = reward;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Reward)) return false;
+        Reward reward1 = (Reward) o;
+        return Objects.equals(reward, reward1.reward);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(reward);
+    }
+}
