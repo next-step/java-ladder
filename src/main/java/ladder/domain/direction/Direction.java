36efca69b17104f67f124f38ca80dce1b6938ef3
Zeeyon <zeeyon.c@gmail.com>
2020-06-18 01:02:45 +0900

    refactor: Direction enum으로 변경

--- src/main/java/ladder/domain/direction/Direction.java
+++ src/main/java/ladder/domain/direction/Direction.java
@@ -1,15 +1,15 @@
 package ladder.domain.direction;
 
-public class Direction {
+public enum Direction {
 
-    public static final Direction RIGHT = new Direction(false,  true);
-    public static final Direction LEFT  = new Direction(true,   false);
-    public static final Direction STAY  = new Direction(false,  false);
+    RIGHT (false, true),
+    LEFT  (true,  false),
+    STAY  (false, false);
 
     private final boolean left;
     private final boolean right;
 
-    private Direction(boolean isLeft, boolean isRight) {
+    Direction(boolean isLeft, boolean isRight) {
         this.left = isLeft;
         this.right = isRight;
     }
