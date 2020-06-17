77cd0abc5c28ea1f68235457df5e5bb2b3b3a6f8
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2020-06-17 22:49:47 +0900

    Refactor: remove static keyword

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private final static Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
 
     public String inputPlayers() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
