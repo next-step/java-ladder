68969d74c4d2d8e030c1c581924f5f4bb9e3a3c6
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-17 10:22:22 +0900

    test: verify point does not connect continuously and last point is false

--- src/test/java/nextstep/ladder/painter/RandomPointPainterTest.java
+++ src/test/java/nextstep/ladder/painter/RandomPointPainterTest.java
@@ -2,11 +2,15 @@ package nextstep.ladder.painter;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import nextstep.ladder.point.Point;
 import nextstep.ladder.point.Points;
 
 public class RandomPointPainterTest {
@@ -24,5 +28,28 @@ public class RandomPointPainterTest {
 	void 주어진_수만큼_좌표를_그린다(int count) {
 		Points points = painter.drawPoints(count);
 		assertThat(points.getSize()).isEqualTo(count);
+	}
+
+	@DisplayName("좌표는 연속으로 연결되지 않는다.")
+	@Test
+	void 좌표는_연속으로_연결되지_않는다() {
+		Points points = painter.drawPoints(100);
+		AtomicBoolean isContinuouslyConnected = new AtomicBoolean(false);
+		points.getPoints().stream()
+			.reduce((previous, next) -> {
+				if (previous.isConnectedToNextPoint() && next.isConnectedToNextPoint()) {
+					isContinuouslyConnected.set(true);
+				}
+				return next;
+			});
+		assertThat(isContinuouslyConnected.get()).isEqualTo(false);
+	}
+
+	@DisplayName("마지막 좌표는 다음 좌표와 연결되지 않는다.")
+	@Test
+	void 마지막_좌표는_다음_좌표와_연결되지_않는다() {
+		Points points = painter.drawPoints(100);
+		Point point = points.getPoints().get(points.getSize() - 1);
+		assertThat(point.isConnectedToNextPoint()).isEqualTo(false);
 	}
 }
