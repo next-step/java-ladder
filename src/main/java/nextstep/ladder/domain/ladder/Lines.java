27508079af38ac8836dc983ab8ea8ef2cf026ad9
chulman <chlcjfals0122@gmail.com>
2020-06-18 02:08:28 +0900

    feat (lines) : feat lines class

---
+++ src/main/java/nextstep/ladder/domain/ladder/Lines.java
@@ -0,0 +1,25 @@
+package nextstep.ladder.domain.ladder;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Lines {
+
+    private final List<Line> lines;
+
+    public Lines(Height height, int sizeOfPerson, DirectionPredicate predicate) {
+        this.lines = createLines(height, sizeOfPerson, predicate);
+    }
+
+    private List<Line> createLines(Height height, int sizeOfPerson, DirectionPredicate predicate) {
+        return IntStream.range(0, height.getHeight())
+                .unordered()
+                .mapToObj(integer -> Line.init(sizeOfPerson, predicate))
+                .collect(Collectors.toList());
+    }
+
+    public List<Line> getLines() {
+        return lines;
+    }
+}
