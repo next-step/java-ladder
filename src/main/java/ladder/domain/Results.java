2d2864147215143dce9eae190c71f5448bcd7eba
eunji <jeeell.922@gmail.com>
2020-06-18 11:30:24 +0900

    feat Results 객체 생성

---
+++ src/main/java/ladder/domain/Results.java
@@ -0,0 +1,30 @@
+package ladder.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Results {
+    public static final String SEPARATOR = ",";
+
+    private final List<Result> results;
+
+    private Results(List<Result> results) {
+        this.results = results;
+    }
+
+    public static Results valueOf(String enteredResults, int countOfPerson) {
+        List<Result> results = Arrays.stream(enteredResults.split(SEPARATOR))
+                .map(Result::newInstance)
+                .collect(Collectors.toList());
+
+        checkResultSize(results, countOfPerson);
+        return new Results(results);
+    }
+
+    private static void checkResultSize(List<Result> results, int countOfPerson) {
+        if (results.size() != countOfPerson) {
+            throw new IllegalArgumentException("게임 결과 수와 참가자 수가 일치하지 않습니다.");
+        }
+    }
+}
