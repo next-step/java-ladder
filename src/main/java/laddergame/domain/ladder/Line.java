b07c80ec473715529bc6c793114bb62336522a0e
Gobukgol <zxczc159@gmail.com>
2020-06-18 14:36:17 +0900

    feat(Line): isSameHeight, movePosition 메소드 구현
    isSameHeight은 인자로 받은 Position의 높이와 자신의 Line 높이와 같은지 여부 반환,
    movePosition은 인자로 받은 Position의 열 번호를 보고 자신의 Bridge 중 맞는 것을 찾아서 사다리를 이동 시키고, 높이를 한단계 내려간다.

--- src/main/java/laddergame/domain/ladder/Line.java
+++ src/main/java/laddergame/domain/ladder/Line.java
@@ -1,5 +1,6 @@
 package laddergame.domain.ladder;
 
+import laddergame.domain.player.Position;
 import laddergame.domain.vo.Height;
 
 import java.util.Collections;
@@ -23,5 +24,18 @@ public class Line {
         return bridges.stream()
                 .map(Bridge::isConnected)
                 .collect(Collectors.toList());
+    }
+
+    public Position movePosition(Position position) {
+        Bridge findBridge = bridges.stream()
+                .filter(bridge -> bridge.isBridgeColumn(position.column()))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+
+        return findBridge.movePositionColumn(position).moveDown();
+    }
+
+    public boolean isSameHeight(Position position) {
+        return lineHeight.equals(position.height());
     }
 }
--- src/test/java/laddergame/domain/ladder/LineTest.java
+++ src/test/java/laddergame/domain/ladder/LineTest.java
@@ -1,10 +1,13 @@
 package laddergame.domain.ladder;
 
+import laddergame.domain.player.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,13 +36,19 @@ class LineTest {
 
     }
 
+    private static Stream<Arguments> provideBeforeAndAfterPosition() {
+        return Stream.of(Arguments.of(new Position(1, 1), new Position(2, 2)),
+                Arguments.of(new Position(2, 1), new Position(1, 2)),
+                Arguments.of(new Position(3, 1), new Position(3, 2)));
+    }
+
     @DisplayName("인원수에 맞게 다리를 생성한다.")
     @ParameterizedTest
     @MethodSource("provideNumberOfPlayerAndBridges")
     void createLine(int numberOfPlayer, List<Boolean> expectedConnection, List<Bridge> expectedResult) {
         FixedConnectGenerator fixedConnectGenerator = new FixedConnectGenerator(expectedConnection);
 
-        Line line = new Line(numberOfPlayer, fixedConnectGenerator);
+        Line line = new Line(1, numberOfPlayer, fixedConnectGenerator);
 
         assertThat(line.getBridges()).isEqualTo(expectedResult);
     }
@@ -47,7 +56,28 @@ class LineTest {
     @DisplayName("한 라인의 연결 상태를 boolean 값으로 반환한다.")
     @Test
     void getLineStates() {
-        Line line = new Line(4, () -> true);
+        Line line = new Line(1, 4, () -> true);
         assertThat(line.getLineStates()).isEqualTo(Arrays.asList(true, false, true));
+    }
+
+    @DisplayName("Position의 높이와 Line의 높이가 같은지 확인")
+    @ParameterizedTest
+    @CsvSource({"1, true", "2, false"})
+    void isSameHeight(int height, boolean result) {
+        Line line = new Line(1, 4, () -> true);
+        Position position = new Position(1, height);
+        assertThat(line.isSameHeight(position)).isEqualTo(result);
+    }
+
+    @DisplayName("인자로 받은 Position 의 높이와 열 번호를 보고 이동시킨다.")
+    @ParameterizedTest
+    @MethodSource("provideBeforeAndAfterPosition")
+    void movePosition(Position before, Position after) {
+        BridgeConnectGenerator bridgeConnectGenerator = new FixedConnectGenerator(Arrays.asList(true, false, false));
+        Line line = new Line(1, 4, bridgeConnectGenerator);
+
+        Position actualResult = line.movePosition(before);
+
+        assertThat(actualResult).isEqualTo(after);
     }
 }
