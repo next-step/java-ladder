170082798112e55a6c1e38e7acbefd5ae851d5e1
Sungjun <sungjunpizz@gmail.com>
2020-06-18 23:50:50 +0900

    refactor: Line 클래스 createLine 메서드 리팩토링

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -7,27 +7,37 @@ public class Line {
     private final List<Point> points;
 
     public Line(int countOfPlayers, PointGenerationStrategy strategy) {
-        this.points = createLine(countOfPlayers, strategy);
+        this.points = init(countOfPlayers, strategy);
     }
 
     public int move(int position) {
         return points.get(position).move();
     }
 
-    private List<Point> createLine(int countOfPlayers, PointGenerationStrategy strategy) {
+    private List<Point> init(int countOfPlayers, PointGenerationStrategy strategy) {
         List<Point> points = new ArrayList<>();
+        Point point = initFirst(points, strategy);
+        point = initBody(countOfPlayers, points, point, strategy);
+        initLast(points, point);
+        return points;
+    }
 
+    private Point initFirst(List<Point> points, PointGenerationStrategy strategy) {
         Point point = Point.first(strategy);
         points.add(point);
+        return point;
+    }
 
+    private static Point initBody(int countOfPlayers, List<Point> points, Point point, PointGenerationStrategy strategy) {
         for (int i = 1; i < countOfPlayers - 1; i++) {
             point = point.next(strategy);
             points.add(point);
         }
+        return point;
+    }
 
+    private void initLast(List<Point> points, Point point) {
         points.add(point.last());
-
-        return points;
     }
 
     public List<Point> getPoints() {
