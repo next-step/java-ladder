421f0c9d6f751163dfa8129cc64ac6d0764919a9
jhseo <jhseo1@iteyes.co.kr>
2020-06-19 08:57:32 +0900

    fix(ALL) 패키지 정리

--- src/main/java/nextstep/step2/Main.java
+++ src/main/java/nextstep/step2/Main.java
@@ -1,5 +1,9 @@
 package nextstep.step2;
 
+import nextstep.step2.domain.Ladder;
+import nextstep.step2.view.InputView;
+import nextstep.step2.view.ResultView;
+
 public class Main {
     public static void main(String[] args) {
         InputView.printMessage("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
