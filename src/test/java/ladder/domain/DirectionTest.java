db1b10d7051978d3f477f8356e07bed68d16b5b7
Sungjun <sungjunpizz@gmail.com>
2020-06-18 23:11:57 +0900

    test: Direction 예외 케이스 TC 추가

--- src/test/java/ladder/domain/DirectionTest.java
+++ src/test/java/ladder/domain/DirectionTest.java
@@ -3,8 +3,7 @@ package ladder.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("Direction 클래스 테스트")
 public class DirectionTest {
@@ -14,6 +13,13 @@ public class DirectionTest {
         assertThatCode(() -> new Direction(true, false))
                 .doesNotThrowAnyException();
     }
+
+    @Test()
+    public void create_invalid() {
+        assertThatExceptionOfType(IllegalStateException.class)
+                .isThrownBy(() -> new Direction(true, true));
+    }
+
 
     @Test
     void first() {
