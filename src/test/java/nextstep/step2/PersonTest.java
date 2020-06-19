c30196eb1cf785f1d8665ecdf4f182983fe6dbf7
bbbnam <bbbnam@naver.com>
2020-06-19 23:26:19 +0900

    test : Person객체 5자 이하로 입력시 정상적으로 생성 여부 추가

--- src/test/java/nextstep/step2/PersonTest.java
+++ src/test/java/nextstep/step2/PersonTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import step2.domain.Person;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PersonTest {
@@ -16,5 +17,13 @@ public class PersonTest {
         assertThatThrownBy(() -> Person.of(name))
         .isInstanceOf(IllegalArgumentException.class)
         .hasMessage(Person.NAME_LIMIT_EXCEPTION);
+    }
+
+    @ParameterizedTest
+    @DisplayName("사람이름 5자 이하 입력시 정상여부 체크")
+    @ValueSource(strings = {"pobi", "honux", "crong", "jk"})
+    void validPerson(String name){
+        assertThat(Person.of(name).getName())
+                .isEqualTo(name);
     }
 }
