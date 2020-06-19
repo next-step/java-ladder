727df76132aff3cd6a3dbdc7e01101e307f61840
kimdahyeee <dahye432111@gmail.com>
2020-06-19 17:43:23 +0900

    feat: 사다리 높이 입력받는 메서드 추가

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -9,4 +9,9 @@ public class InputView {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         return scanner.nextLine();
     }
+
+    public static int doInputLadderHeight() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        return Integer.parseInt(scanner.nextLine());
+    }
 }
