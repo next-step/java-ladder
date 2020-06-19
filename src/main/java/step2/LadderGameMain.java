dd25b53cbd0f887d061d071240562c4eeabb7534
bbbnam <bbbnam@naver.com>
2020-06-19 23:12:32 +0900

    refactor : 컬렉션 래핑 클래스 생성으로 리팩토링

--- src/main/java/step2/LadderGameMain.java
+++ src/main/java/step2/LadderGameMain.java
@@ -4,8 +4,6 @@ import step2.domain.*;
 import step2.view.InputView;
 import step2.view.ResultView;
 
-import java.util.List;
-
 public class LadderGameMain {
     public static void main(String[] args) {
         String personsString = InputView.inputPersons();
@@ -13,9 +11,8 @@ public class LadderGameMain {
 
         Persons persons = Persons.of(personsString);
         Height height = Height.of(ladderHeight);
-        Lines lines = Lines.createVertical(height, persons.count());
-        List<Point> drewLines = lines.createHorizontal();
+        Ladder ladder = Ladder.create(height, persons.count());
 
-        ResultView.printLadder(persons, drewLines);
+        ResultView.print(persons, ladder);
     }
 }
