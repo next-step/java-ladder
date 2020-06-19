78cd2216cb59a93fc93be529deeb52769521feef
bbbnam <bbbnam@naver.com>
2020-06-19 23:04:04 +0900

    refactor : Line -> Layer , Lines -> Ladder 명칭 변경, 테스트 코드 수정

---
+++ src/main/java/step2/domain/Layer.java
@@ -0,0 +1,72 @@
+package step2.domain;
+
+import step2.util.RandomGenerator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Layer {
+    private static final int INDEX_ONE = 1;
+    private static final int ONE = 1;
+    private static final int MINIMUN_POINTS = 2;
+    private static final String INIT_LINE_SPACE = "      ";
+    public static final String MINIMUN_POINTS_EXCEPTION = "포인트가 2개보다 적으면 게임이 성립되지 않습니다.";
+    public static final String CONNECTED_LINE_EXCEPTION = "가로라인은 겹치면 안됩니다.";
+
+    private List<Point> points;
+
+    public Layer(List<Point> points) {
+        validateLayer(points);
+        this.points = points;
+    }
+
+    private void validateLayer(List<Point> points) {
+        if (points.size() < MINIMUN_POINTS){
+            throw new IllegalArgumentException(MINIMUN_POINTS_EXCEPTION);
+        }
+
+        if (points.stream().anyMatch(point -> point.left() && point.right())){
+            throw new IllegalArgumentException(CONNECTED_LINE_EXCEPTION);
+        }
+    }
+
+    private static Layer createLine(int countOfperson) {
+        List<Point> points = new ArrayList<>();
+        Point point = Point.first(RandomGenerator.generate());
+        points.add(point);
+
+        int lastPointIndex = countOfperson - ONE;
+        for (int i = INDEX_ONE; i < lastPointIndex; i++){
+            point = point.mid();
+            points.add(point);
+        }
+
+        Point lastPoint = point.last();
+        points.add(lastPoint);
+
+        return new Layer(points);
+    }
+
+    public static Layer of(int countOfPerson) {
+        return createLine(countOfPerson);
+    }
+
+    public static Layer of(List<Point> points) {
+        return new Layer(points);
+    }
+
+    public List<Point> getPoints() {
+        return Collections.unmodifiableList(points);
+    }
+
+    @Override
+    public String toString() {
+        System.out.print(INIT_LINE_SPACE);
+        return points.stream()
+                     .limit(points.size())
+                     .map(Point::toString)
+                     .collect(Collectors.joining());
+    }
+}
---
+++ src/test/java/nextstep/step2/LayerTest.java
@@ -0,0 +1,47 @@
+package nextstep.step2;
+
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import step2.domain.Layer;
+import step2.domain.Point;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LayerTest {
+
+    @ParameterizedTest
+    @DisplayName("사람수에 따른 라인(|)수 정상 리턴하는지 테스트")
+    @ValueSource(ints = {3,6,7,10})
+    void makeLineTest(int countOfPerson){
+        Layer layer = Layer.of(countOfPerson);
+        assertThat(layer.getPoints().size()).isEqualTo(countOfPerson);
+    }
+
+    @Test
+    @DisplayName("사다리 한개 높이 예외처리 테스트 - 포인트 1개뿐이면 layer 형성 예외처리")
+    void validateLayerTest(){
+        Point first = Point.of(0,false,true);
+        List<Point> points = Lists.list(first);
+        assertThatThrownBy(() -> Layer.of(points))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(Layer.MINIMUN_POINTS_EXCEPTION);
+    }
+
+    @Test
+    @DisplayName("사다리 한개 높이 예외처리 테스트2 - 가로라인 겹치면 예외처리")
+    void validateLayerTest2(){
+        Point first = Point.of(0,false,true);
+        Point next = Point.of(1,true,true);
+        Point last = Point.of(2,true,false);
+        List<Point> points = Lists.list(first, next, last);
+        assertThatThrownBy(() -> Layer.of(points))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Layer.CONNECTED_LINE_EXCEPTION);
+    }
+}
