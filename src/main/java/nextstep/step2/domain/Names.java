1aec9644f1fa3434c16f49bed1b9b59d5e5237d9
jhseo <jhseo1@iteyes.co.kr>
2020-06-19 12:24:34 +0900

    fix(Names) comma 상수화

--- src/main/java/nextstep/step2/domain/Names.java
+++ src/main/java/nextstep/step2/domain/Names.java
@@ -5,10 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Names {
+    private static final String NAME_SEPARATOR = ",";
     private List<Name> names = new ArrayList<>();
 
     public Names(String names) {
-        String[] nameArray = names.split(",");
+        String[] nameArray = names.split(NAME_SEPARATOR);
         List<String> nameList = Arrays.asList(nameArray);
         nameList.forEach(name -> this.names.add(new Name(name)));
     }
