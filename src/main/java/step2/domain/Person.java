337470e69494c10f95c3ef62603eef1eef70c98f
bbbnam <bbbnam@naver.com>
2020-06-19 23:07:30 +0900

    feat : 이름 빈 값 체크 기능 추가

--- src/main/java/step2/domain/Person.java
+++ src/main/java/step2/domain/Person.java
@@ -1,17 +1,27 @@
 package step2.domain;
 
+import java.util.Objects;
+
 public class Person {
-    private static final String NAME_LIMIT_EXCEPTION = "이름은 최대 5글자까지만 입력 가능합니다.";
-    public static final int NAME_LIMIT = 5;
+    private static final int NAME_LIMIT = 5;
+    private static final String NAME_NULL_EXCEPTION = "이름은 빈값이 올 수 없습니다.";
+    public static final String NAME_LIMIT_EXCEPTION = "이름은 최대 5글자까지만 입력 가능합니다.";
 
     private final String name;
 
     private Person(String name) {
-        validateName(name);
+        validateNameNull(name);
+        validateNameLimit(name);
         this.name = name;
     }
 
-    private void validateName(String name) {
+    private void validateNameNull(String name) {
+        if (Objects.isNull(name) || name.isEmpty()){
+            throw new IllegalArgumentException(NAME_NULL_EXCEPTION);
+        }
+    }
+
+    private void validateNameLimit(String name) {
         if (name.length() > NAME_LIMIT){
             throw new IllegalArgumentException(NAME_LIMIT_EXCEPTION);
         }
