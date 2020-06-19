ac5b129c3b32c7b0b5b5d49f250dbb120ac3c7c3
bbbnam <bbbnam@naver.com>
2020-06-19 23:08:58 +0900

    refactor : 입력값 빈값 유효성 검사, 컬렉션 객체 리턴시 수정불가 객체 리턴

--- src/main/java/step2/domain/Persons.java
+++ src/main/java/step2/domain/Persons.java
@@ -3,29 +3,40 @@ package step2.domain;
 import step2.util.StringUtil;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toList;
 
 public class Persons {
     private static final int LADDER_GAME_MINIMUM_PLAYER = 2;
-    private static final String LADDER_GAME_MINIMUM_EXCEPTION = "사다리 게임은 최소 2인 이상이 진행 가능합니다.";
+    private static final String PERSONS_NULL_EXCEPTION = "참여자를 빈값으로 입력할 수 없습니다.";
+    public static final String LADDER_GAME_MINIMUM_EXCEPTION = "사다리 게임은 최소 2인 이상이 진행 가능합니다.";
+
     private final List<Person> persons;
 
     private Persons(List<Person> persons) {
+        checkCountPerson(persons.size());
         this.persons = persons;
     }
 
     public static Persons of(String persons){
+        validatePersonsNull(persons);
         return Arrays.asList(StringUtil.split(persons))
                      .stream()
                      .map(Person::of)
                      .collect(collectingAndThen(toList(), Persons::convertObject));
     }
 
+    private static void validatePersonsNull(String Persons) {
+        if (Objects.isNull(Persons) || Persons.isEmpty()){
+            throw new IllegalArgumentException(PERSONS_NULL_EXCEPTION);
+        }
+    }
+
     private static Persons convertObject(List<Person> persons){
-        checkCountPerson(persons.size());
         return new Persons(persons);
     }
 
@@ -40,6 +51,6 @@ public class Persons {
     }
 
     public List<Person> getPersons() {
-        return persons;
+        return Collections.unmodifiableList(persons);
     }
 }
