c99102983ebc442adb6236d34b5ad8715cefe602
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-20 12:28:42 +0900

    refactor: initialize position when creating each of player

--- src/main/java/nextstep/ladder/player/Player.java
+++ src/main/java/nextstep/ladder/player/Player.java
@@ -6,15 +6,17 @@ public class Player {
 
 	private static final int MAXIMUM_NAME_LENGTH = 5;
 	private String name;
+	private int position;
 
-	private Player(String name) {
+	private Player(String name, int position) {
 		this.name = name;
+		this.position = position;
 	}
 
-	public static Player ofName(String name) {
+	public static Player ofNameAndPosition(String name, int position) {
 		validateNotNull(name);
 		validateNameLength(name);
-		return new Player(name);
+		return new Player(name, position);
 	}
 
 	private static void validateNotNull(String name) {
@@ -29,7 +31,16 @@ public class Player {
 		}
 	}
 
+	public int updatePosition(int position) {
+		this.position = position;
+		return this.position;
+	}
+
 	public String getName() {
 		return name;
+	}
+
+	public int getPosition() {
+		return position;
 	}
 }
