7d0e6183546719e03726ce0df5edb4b2708741bd
eunji <jeeell.922@gmail.com>
2020-06-21 23:38:27 +0900

    refactor LadderLine 객체 리팩토링
    - 라인별 기능 구현(first, body, last)

---
+++ src/main/java/ladder/domain/LadderLine.java
@@ -0,0 +1,50 @@
+package ladder.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LadderLine {
+    private final List<Point> points;
+
+    private LadderLine(List<Point> points) {
+        this.points = points;
+    }
+
+    public static LadderLine init(int sizeOfPerson) {
+        List<Point> points = new ArrayList<>();
+        Point point = initFirst(points, new LadderPointGenerator());
+        point = initBody(sizeOfPerson, points, point);
+        initLast(points, point);
+        return new LadderLine(points);
+    }
+
+    private static Point initFirst(List<Point> points, PointGenerator generator) {
+        Point point = Point.first(generator.generate());
+        points.add(point);
+        return point;
+    }
+
+    private static Point initBody(int sizeOfPerson, List<Point> points, Point point) {
+        for (int i = 1; i < sizeOfPerson - 1; i++) {
+            point = point.next();
+            points.add(point);
+        }
+        return point;
+    }
+
+    private static void initLast(List<Point> points, Point point) {
+        point = point.last();
+        points.add(point);
+    }
+
+    public int move(int position) {
+        return points.get(position).move();
+    }
+
+    @Override
+    public String toString() {
+        return "LadderLine{" +
+                "points=" + points +
+                '}';
+    }
+}
