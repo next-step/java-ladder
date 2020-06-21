d937e2ba5358311e47a105ed8d7f2523374c8cfc
김다혜 <dahye432111@gmail.com>
2020-06-21 23:55:05 +0900

    feat: 실행 결과 입력 메소드 추가

--- src/main/java/ladder/LadderGame.java
+++ src/main/java/ladder/LadderGame.java
@@ -1,22 +1,19 @@
 package ladder;
 
 import ladder.domain.ladder.Game;
-import ladder.domain.ladder.Ladder;
 import ladder.domain.play.Items;
 import ladder.domain.play.Playing;
 import ladder.util.ConvertUtils;
 import ladder.view.InputView;
 import ladder.view.ResultView;
 
-import java.util.List;
-
 public class LadderGame {
     public static void main(String[] args) {
-        List<String> names = ConvertUtils.split(InputView.doInputParticipants());
+        Items players = Items.of(ConvertUtils.split(InputView.doInputParticipants()));
+        Items results = Items.of(ConvertUtils.split(InputView.doInputResults()));
         int height = InputView.doInputLadderHeight();
 
-        Items players = Items.of(names);
-        Game game = Game.play(Playing.of(players, players), height);
+        Game game = Game.play(Playing.of(players, results), height);
         ResultView.printResult(game);
     }
 }
--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -10,6 +10,11 @@ public class InputView {
         return scanner.nextLine();
     }
 
+    public static String doInputResults() {
+        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        return scanner.nextLine();
+    }
+
     public static int doInputLadderHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         return Integer.parseInt(scanner.nextLine());
