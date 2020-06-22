6c75d81e1c52de6ea83f113afc6f306c982f5b4a
bbbnam <bbbnam@naver.com>
2020-06-22 21:58:52 +0900

    refactor : 뷰가 필요한 값만 반환하도록 리팩토링

--- src/main/java/ladder/view/ResultView.java
+++ src/main/java/ladder/view/ResultView.java
@@ -1,7 +1,9 @@
 package ladder.view;
 
 import ladder.domain.*;
-import ladder.domain.LadderGameInfo;
+import ladder.domain.LadderGameResult;
+
+import java.util.List;
 
 public class ResultView {
 
@@ -9,16 +11,15 @@ public class ResultView {
     private final static String NEW_LINE = System.lineSeparator();
     private static final String INIT_LINE_SPACE = "      ";
 
-    public static void printLadder(LadderGameInfo ladderGameInfo) {
-        arrangePersons(ladderGameInfo.getPersons());
+    public static void printLadder(LadderGameResult ladderGameResult) {
+        arrangePersons(ladderGameResult.getPersons());
         System.out.print(NEW_LINE);
-        arrangeLadder(ladderGameInfo.getLadder());
-        arrangeResults(ladderGameInfo.getPrizes());
+        arrangeLadder(ladderGameResult.getLadder());
+        arrangeResults(ladderGameResult.getPrizes());
     }
 
-    private static void arrangePersons(Persons persons){
-       persons.getPersons()
-              .stream()
+    private static void arrangePersons(List<Person> persons){
+       persons.stream()
               .map(person -> formatWord(person.getName()))
               .forEach(System.out::print);
     }
@@ -35,10 +36,9 @@ public class ResultView {
               });
     }
 
-    private static void arrangeResults(Prizes prizes) {
-        prizes.getPrizes()
-                     .stream()
-                     .map(result -> formatWord(result))
-                     .forEach(result -> System.out.print(result));
+    private static void arrangeResults(List<String> prizes) {
+        prizes.stream()
+              .map(result -> formatWord(result))
+              .forEach(result -> System.out.print(result));
     }
 }
