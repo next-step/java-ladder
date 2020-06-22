952b02185d2fa114bab3f2377fad8340f93798d8
junpil park <iamfeel2@gmail.com>
2020-06-23 02:51:37 +0900

    feat (StringUtils) : StringUtils 클래스 추가 (Split 메소드 추가)

---
+++ src/main/java/ladder/util/StringUtils.java
@@ -0,0 +1,18 @@
+package ladder.util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StringUtils {
+    public static List<String> split(String text, String regex) {
+        validateNullOrEmpty(text);
+        return Arrays.stream(text.split(regex)).map(String::trim).collect(Collectors.toList());
+    }
+
+    private static void validateNullOrEmpty(String text) {
+        if(text == null || text.isEmpty()) {
+            throw new IllegalArgumentException("입력된 텍스트가 비어있거나 NULL 입니다.");
+        }
+    }
+}
