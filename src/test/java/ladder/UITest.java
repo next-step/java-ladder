5e0bdbee706ae07db91ed5b871de4e9977f794ec
gwangjunchoi <namsookill@gmail.com>
2020-06-22 22:24:38 +0900

    refactor ladder UI 수정

--- src/test/java/ladder/UITest.java
+++ src/test/java/ladder/UITest.java
@@ -1,14 +1,14 @@
 package ladder;
 
-import ladder.domain.Draw;
 import ladder.domain.UI;
+import ladder.view.ResultView;
 import org.junit.jupiter.api.Test;
 
 public class UITest {
     @Test
     void displayUI() {
-        System.out.println(UI.find(new Draw(true)).getString() +
-                UI.find().getString() +
-                UI.find(new Draw(false)).getString());
+        System.out.println(UI.find(ResultView.LADDER_HORIZONTAL).getString() +
+                UI.find(ResultView.LADDER_EMPTY).getString() +
+                UI.find(ResultView.LADDER_VERTICAL).getString());
     }
 }
