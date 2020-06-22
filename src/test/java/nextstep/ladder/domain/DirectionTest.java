e7a9b8a06fef21632eec1d1931f4b0dfb7912b86
Jaehun Lee <ljhokgo@gmail.com>
2020-06-22 22:57:51 +0900

    refactor: step4 힌트 반영

---
+++ src/test/java/nextstep/ladder/domain/DirectionTest.java
@@ -0,0 +1,67 @@
+package nextstep.ladder.domain;
+
+import nextstep.ladder.domain.line.Direction;
+import org.junit.jupiter.api.Test;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class DirectionTest {
+
+    @Test
+    public void init() {
+        assertThat(Direction.of(true, false))
+                .isEqualTo(Direction.of(true, false));
+    }
+
+    @Test
+    public void init_invalid() {
+        assertThatThrownBy(() -> Direction.of(TRUE, TRUE))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    public void next_random_true() {
+        Direction next = Direction.first(TRUE).next();
+
+        assertThat(next)
+                .isEqualTo(Direction.of(TRUE, FALSE));
+    }
+
+    @Test
+    public void next_random_false() {
+        for (int i = 0; i < 100; i++) {
+            Direction.first(FALSE).next();
+        }
+    }
+
+    @Test
+    public void next_true() {
+        Direction next = Direction.of(TRUE, FALSE).next(TRUE);
+
+        assertThat(next).isEqualTo(Direction.of(FALSE, TRUE));
+    }
+
+    @Test
+    public void next_false() {
+        Direction next = Direction.of(FALSE, TRUE).next(FALSE);
+
+        assertThat(next).isEqualTo(Direction.of(TRUE, FALSE));
+    }
+
+    @Test
+    public void first() {
+        Direction first = Direction.first(TRUE);
+
+        assertThat(first.isLeft()).isEqualTo(FALSE);
+    }
+
+    @Test
+    public void last() {
+        Direction last = Direction.first(TRUE).last();
+
+        assertThat(last).isEqualTo(Direction.of(TRUE, FALSE));
+    }
+}
