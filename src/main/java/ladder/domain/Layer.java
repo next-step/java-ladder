397262f525d5f7178e20092aecc7603d3a68e466
bbbnam <bbbnam@naver.com>
2020-06-23 23:06:38 +0900

    refactor : 사다리 생성부분 리팩토링

--- src/main/java/ladder/domain/Layer.java
+++ src/main/java/ladder/domain/Layer.java
@@ -11,13 +11,13 @@ public class Layer {
 
     public static final String MINIMUN_POINTS_EXCEPTION = "포인트가 2개보다 적으면 게임이 성립되지 않습니다.";
 
-    private static final int INDEX_ONE = 1;
-    private static final int ONE = 1;
     private static final int MINIMUN_POINTS = 2;
+    private static final int ONE = 1;
+    private static final int INDEX_ONE = 1;
 
     private List<Point> points;
 
-    public Layer(List<Point> points) {
+    private Layer(List<Point> points) {
         validateLayer(points);
         this.points = points;
     }
@@ -28,13 +28,16 @@ public class Layer {
         }
     }
 
+    public static Layer of(int countOfPerson) {
+        return createLine(countOfPerson);
+    }
+
     private static Layer createLine(int countOfperson) {
         List<Point> points = new ArrayList<>();
         Point point = Point.first(new RandomGenerator());
         points.add(point);
 
-        List<Point> midPoints = makeMidPoints(countOfperson, point);
-        points.addAll(midPoints);
+        point = addMidPoints(countOfperson, point, points);
 
         Point lastPoint = point.last();
         points.add(lastPoint);
@@ -42,22 +45,21 @@ public class Layer {
         return new Layer(points);
     }
 
-    private static List<Point> makeMidPoints(int countOfperson, Point point) {
-        List<Point> midPoints = new ArrayList<>();
+    private static Point addMidPoints(int countOfperson, Point point, List<Point> points) {
         int lastPointIndex = countOfperson - ONE;
         for (int i = INDEX_ONE; i < lastPointIndex; i++){
             point = point.mid();
-            midPoints.add(point);
+            points.add(point);
         }
-        return midPoints;
-    }
-
-    public static Layer of(int countOfPerson) {
-        return createLine(countOfPerson);
+        return point;
     }
 
     public static Layer of(List<Point> points) {
         return new Layer(points);
+    }
+
+    public int move(int position){
+        return points.get(position).move();
     }
 
     public List<Point> getPoints() {
