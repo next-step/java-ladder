c40921bcfe1e9cfb1b82871ca14bd736302a4462
kys9261 <nser789@gmail.com>
2020-06-24 00:10:29 +0900

    Ladder - step3 - refactor (사다리게임 실행 결과를 사용자와 매핑)

--- src/main/java/nextstep/step3/domain/Ladder.java
+++ src/main/java/nextstep/step3/domain/Ladder.java
@@ -1,27 +1,35 @@
 package nextstep.step3.domain;
 
 import nextstep.step3.domain.strategy.Position;
+import nextstep.step3.domain.strategy.RandomDrawLineStrategy;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Ladder {
     private List<Line> lines;
 
-    public Ladder(List<Line> lines) {
-        this.lines = lines;
+    public Ladder(int userCount, int ladderHeight) {
+        this.lines = Stream.generate(() -> new Line(userCount, new RandomDrawLineStrategy()))
+                .limit(ladderHeight)
+                .collect(Collectors.toList());
     }
 
     public List<Line> getLines() {
         return lines;
     }
 
-    public List<User> getLadderGameResult(List<String> userNames) {
-        List<User> users = new ArrayList<>();
+    public Map<String, User> getLadderGameResult(List<String> userNames, List<String> playResult) {
+        Map<String, User> resultMap = new HashMap<>();
         for(int i=0; i<userNames.size(); i++){
-            users.add(new User(userNames.get(i), play(i)));
+            String userName = userNames.get(i);
+            String result = playResult.get(play(i));
+            resultMap.put(userName, new User(userName, result));
         }
-        return users;
+        return resultMap;
     }
 
     private int play(int startBreathPosition) {
