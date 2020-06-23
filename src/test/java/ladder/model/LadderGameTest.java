415f261fc95de571edf695f2ec392194b4a0692f
Kim-yongjin <yongjin@sixshop.com>
2020-06-24 03:01:35 +0900

    <refactor> 필요 없는 변수 삭제

--- src/test/java/ladder/model/LadderGameTest.java
+++ src/test/java/ladder/model/LadderGameTest.java
@@ -2,6 +2,7 @@ package ladder.model;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
+import ladder.model.dto.LadderGameInfo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,14 +11,12 @@ import org.junit.jupiter.api.Test;
 public class LadderGameTest {
 
     private Ladder ladder;
-    private LadderGameRewords ladderGameRewords;
     private LadderGameInfo ladderGameInfo;
 
     @BeforeEach
     public void init() {
         ladder = Ladder.create(5, 5);
-        ladderGameRewords = LadderGameRewords.create("5000,꽝,꽝,1000");
-        ladderGameInfo = LadderGameInfo.create(Members.create("pobi,yong,crong,taeg"), ladderGameRewords);
+        ladderGameInfo = LadderGameInfo.create(Members.create("pobi,yong,crong,taeg"), LadderGameRewords.create("5000,꽝,꽝,1000"));
     }
 
     @DisplayName("사다리 게임 생성 테스트")
