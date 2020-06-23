06274b055f702e991b4c92671799e7f1790466a3
junpil park <iamfeel2@gmail.com>
2020-06-24 00:43:05 +0900

    refactor (Ladder) : Ladder의 positionCount 이름을 statPostionCount로 변경

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -9,17 +9,17 @@ public class Ladder {
 
     private final List<HorizonLine> horizonLines = new ArrayList<>();
 
-    public Ladder(LadderHeight height, int positionCount) {
-       this(height, positionCount, () -> new Random().nextBoolean());
+    public Ladder(LadderHeight height, int startPositionCount) {
+       this(height, startPositionCount, () -> new Random().nextBoolean());
     }
 
-    public Ladder(LadderHeight height, int positionCount, ShortLineEnableJudge shortLineEnableJudge) {
-        validateMinPositionCount(positionCount);
-        createHorizonLine(height, positionCount, shortLineEnableJudge);
+    public Ladder(LadderHeight height, int startPositionCount, ShortLineEnableJudge shortLineEnableJudge) {
+        validateMinPositionCount(startPositionCount);
+        createHorizonLine(height, startPositionCount, shortLineEnableJudge);
     }
 
-    public void validateMinPositionCount(int positionCount) {
-        if (positionCount < MIN_START_POSITION_COUNT) {
+    public void validateMinPositionCount(int startPositionCount) {
+        if (startPositionCount < MIN_START_POSITION_COUNT) {
             throw new IllegalArgumentException("사다리를 시작하기 위해서는 2명 이상의 사용자가 필요합니다.");
         }
     }
@@ -34,12 +34,12 @@ public class Ladder {
         }
     }
 
-    public boolean isEnabledShortLineOfRight(int heightIndex, int position) {
-        return horizonLines.get(heightIndex).isEnabledShortLineOfRight(position);
+    public boolean isEnabledShortLineOfRight(int heightIndex, int  startPosition) {
+        return horizonLines.get(heightIndex).isEnabledShortLineOfRight(startPosition);
     }
 
-    public boolean isEnabledShortLineOfLeft(int heightIndex, int position) {
-        return horizonLines.get(heightIndex).isEnabledShortLineOfLeft(position);
+    public boolean isEnabledShortLineOfLeft(int heightIndex, int startPosition) {
+        return horizonLines.get(heightIndex).isEnabledShortLineOfLeft(startPosition);
     }
 
     public int getShortLineCountInHorizonLine(int heightIndex) {
