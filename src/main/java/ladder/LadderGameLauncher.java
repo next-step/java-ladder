53a21c54390795e18e2cc47fea6ee6869703d5fc
bbbnam <bbbnam@naver.com>
2020-06-23 23:07:55 +0900

    feat : 게임 기능추가(결과값 확인)

--- src/main/java/ladder/LadderGameLauncher.java
+++ src/main/java/ladder/LadderGameLauncher.java
@@ -1,15 +1,19 @@
 package ladder;
 
-import ladder.domain.LadderSetting;
-import ladder.domain.LadderGameResult;
+import ladder.domain.LadderGameInfo;
+import ladder.domain.LadderResult;
+import ladder.domain.ResultPrize;
 import ladder.view.InputView;
 import ladder.view.ResultView;
 
 public class LadderGameLauncher {
     public static void main(String[] args) {
-        LadderSetting ladderSetting = InputView.inputParameters();
-        LadderGameResult ladderGameResult = LadderGame.draw(ladderSetting);
+        LadderGameInfo ladderGameInfo = InputView.inputParameters();
+        LadderResult ladderGameResult = LadderGame.draw(ladderGameInfo);
 
         ResultView.printLadder(ladderGameResult);
+
+        ResultPrize resultPrize = LadderGame.play(ladderGameResult);
+        ResultView.printResultofPerson(resultPrize);
     }
 }
