d49f7feb38e27455707cbcb85ae1fc333ee9be0a
kys9261 <nser789@gmail.com>
2020-06-23 23:34:11 +0900

    Ladder - step3 - refactor (사다리의 결과를 알고있는 Position 객체를 추가)

---
+++ src/main/java/nextstep/step3/domain/strategy/Position.java
@@ -0,0 +1,26 @@
+package nextstep.step3.domain.strategy;
+
+import nextstep.step3.domain.Line;
+
+public class Position {
+    private int breath;
+
+    public Position(int breath) {
+        this.breath = breath;
+    }
+
+    public void movePosition(Line line) {
+        if (breath < line.getPoints().size() && line.getPoints().get(breath)) {
+            ++breath;
+            return;
+        }
+
+        if (breath > 0 && line.getPoints().get(breath - 1)) {
+            --breath;
+        }
+    }
+
+    public int getBreath() {
+        return breath;
+    }
+}
