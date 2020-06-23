0988cce91816562d238efc522eb53cd6de8dbfb4
bbbnam <bbbnam@naver.com>
2020-06-23 23:01:39 +0900

    refactor : 메소드명 변경

--- src/main/java/ladder/domain/moveStrategy/RandomGenerator.java
+++ src/main/java/ladder/domain/moveStrategy/RandomGenerator.java
@@ -6,10 +6,6 @@ public class RandomGenerator implements Shift{
 
     private static final Random random = new Random();
 
-    public static boolean generate(){
-        return random.nextBoolean();
-    }
-
     @Override
     public boolean next() {
         return random.nextBoolean();
