a7e7dfdb1be18ae7f430f1dc6b12c4cecfaf329b
Kim-yongjin <yongjin@sixshop.com>
2020-06-24 01:26:23 +0900

    <refactor> LadderGame Create method 시그니쳐 변경되어 깨지는 테스트 수정

--- src/test/java/ladder/model/LadderGameTest.java
+++ src/test/java/ladder/model/LadderGameTest.java
@@ -1,17 +1,29 @@
 package ladder.model;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThatCode;
 
 @DisplayName("사다리 게임 테스트")
 public class LadderGameTest {
 
+    private Ladder ladder;
+    private LadderGameRewords ladderGameRewords;
+    private LadderGameInfo ladderGameInfo;
+
+    @BeforeEach
+    public void init() {
+        ladder = Ladder.create(5, 5);
+        ladderGameRewords = LadderGameRewords.create("5000,꽝,꽝,1000");
+        ladderGameInfo = LadderGameInfo.create(Members.create("pobi,yong,crong,taeg"), ladderGameRewords);
+    }
+
     @DisplayName("사다리 게임 생성 테스트")
     @Test
     public void createLadderTest() {
-        assertThatCode(() -> LadderGame.create(Members.create("pobi,krong,yong"), LadderHeight.create(4)))
+        assertThatCode(() -> LadderGame.create(ladderGameInfo, ladder))
             .doesNotThrowAnyException();
     }
 }
