f09620fbd915670497673c2734b6e1a8e7b62fd6
김다혜 <dahye432111@gmail.com>
2020-06-24 01:53:09 +0900

    feat: 결과 입력 값을 출력하는 메소드 구현

--- src/main/java/ladder/view/ResultView.java
+++ src/main/java/ladder/view/ResultView.java
@@ -1,6 +1,10 @@
 package ladder.view;
 
 import ladder.domain.ladder.Game;
+import ladder.domain.play.Item;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ResultView {
     public static void printResult(Game game) {
@@ -10,5 +14,19 @@ public class ResultView {
 
     private static void printLadder(Game game) {
         System.out.println(game.toString());
+    }
+
+    public static void printResults(Map<Item, Item> results) {
+        int size = results.size();
+        if (size == 1) {
+            System.out.println(results.entrySet().stream()
+                    .map(itemItemEntry -> itemItemEntry.getValue().getInput())
+                    .collect(Collectors.joining()));
+            return;
+        }
+
+        System.out.println(results.entrySet().stream()
+                .map(itemItemEntry -> itemItemEntry.getKey().getInput() + " : " + itemItemEntry.getValue().getInput())
+                .collect(Collectors.joining("\n")));
     }
 }
