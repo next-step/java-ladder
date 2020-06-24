a8f88eaf0204f531140ccb36fcf82a6fed2fd03b
kys9261 <nser789@gmail.com>
2020-06-25 00:27:40 +0900

    Ladder - step4 - test (step3 테스트 코드 복사)

---
+++ src/test/java/nextstep/step4/domain/LadderTest.java
@@ -0,0 +1,48 @@
+package nextstep.step4.domain;
+
+import nextstep.step3.domain.Ladder;
+import nextstep.step3.domain.Line;
+import nextstep.step3.domain.User;
+import nextstep.step4.strategy.TestDrawLineStrategy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Ladder 테스트")
+class LadderTest {
+
+    private int userCount = 4;
+    private int ladderHeight = 3;
+    private List<Boolean> points1 = Arrays.asList(true, false, true);
+    private Ladder ladder = new Ladder(userCount, ladderHeight, new TestDrawLineStrategy(points1));
+
+    @DisplayName("Ladder 생성자 체크")
+    @Test
+    void Ladder() {
+        assertThat(ladder.getLines().size()).isEqualTo(ladderHeight);
+        assertThat(ladder.getLines().get(0)).isInstanceOf(Line.class);
+    }
+
+    @DisplayName("사다리 게임 결과 확인")
+    @Test
+    void getLadderGameResult() {
+        List<String> userNames = Arrays.asList("name1", "name2", "name3", "name4");
+        List<String> playResult = Arrays.asList("1등","꽝","2등","3등");
+
+        Map<String, User> expectResult = new HashMap<>();
+        expectResult.put("name1", new User("name1", "꽝"));
+        expectResult.put("name2", new User("name2", "1등"));
+        expectResult.put("name3", new User("name3", "3등"));
+        expectResult.put("name4", new User("name4", "2등"));
+
+        Map<String, User> actual = ladder.getLadderGameResult(userNames, playResult);
+
+        assertThat(actual).isEqualTo(expectResult);
+    }
+}
---
+++ src/test/java/nextstep/step4/domain/PositionTest.java
@@ -0,0 +1,51 @@
+package nextstep.step4.domain;
+
+import nextstep.step3.domain.Line;
+import nextstep.step3.domain.Position;
+import nextstep.step4.strategy.TestDrawLineStrategy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("Position 테스트")
+class PositionTest {
+
+    private int userCount = 4;
+    private int breath = 0;
+    private List<Boolean> points = Arrays.asList(true, false, true);
+    private Line line = new Line(userCount, new TestDrawLineStrategy(points));
+    private Position position = new Position(breath);
+
+    @DisplayName("Position 이동")
+    @ParameterizedTest
+    @CsvSource(delimiter = ':', value = {"1:1", "2:0", "3:1"})
+    void movePosition(int moveCount, int currentBreath) {
+        for (int i = 0; i < moveCount; i++) {
+            position.movePosition(line);
+        }
+        assertThat(position.getBreath()).isEqualTo(currentBreath);
+    }
+
+    @DisplayName("사다리 범위가 넘는 Position 이동")
+    @Test
+    void movePositionException() {
+        List<Line> lines = new ArrayList<>();
+        lines.add(line);
+        lines.add(line);
+        lines.add(line);
+
+        assertThatThrownBy(() -> {
+            for (int i = 0; i < lines.size() + 1; i++) {
+                position.movePosition(lines.get(i));
+            }
+        }).isInstanceOf(IndexOutOfBoundsException.class);
+    }
+}
