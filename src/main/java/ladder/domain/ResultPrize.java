00511b189a4496f30f717f98f5b5c5fc6f5e2d5f
bbbnam <bbbnam@naver.com>
2020-06-25 11:00:05 +0900

    refactor : 정적팩토리 메소드 패턴으로 변경

--- src/main/java/ladder/domain/ResultPrize.java
+++ src/main/java/ladder/domain/ResultPrize.java
@@ -6,8 +6,12 @@ public class ResultPrize {
 
     private final Map<String, String> result;
 
-    public ResultPrize(Map<String, String> result) {
+    private ResultPrize(Map<String, String> result) {
         this.result = result;
+    }
+
+    public static ResultPrize of(LadderResult ladderGameResult) {
+        return new ResultPrize(ladderGameResult.findResult());
     }
 
     public Map<String, String> getResult() {
