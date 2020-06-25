cc830306893fe9005a6ea76a2ae9ee400f1119d7
yjs2952 <yjs2952@naver.com>
2020-06-25 21:20:07 +0900

    feat: User 생성 로직 및 테스트

---
+++ src/test/java/nextstep/ladder/domain/UserTest.java
@@ -0,0 +1,25 @@
+package nextstep.ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class UserTest {
+
+    @DisplayName("이름이 5자를 넘을 시 예외발생")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void nameNullOrEmptyTest(String name){
+        assertThatThrownBy(() -> new User(name)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("이름이 5자를 넘을 시 예외발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"ironman", "batman", "superman"})
+    void nameLengthTest(String name){
+        assertThatThrownBy(() -> new User(name)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
