0fdbed364592523bbb6705cefe20f0b07ab7dd72
jun-pil park <iamfeel2@gmail.com>
2020-06-26 08:54:40 +0900

    feat (Participants) : Participants 클래스 추가
    사디리 게임에 참여하는 사용자와 사다리의 StartPosition을 연결하는 역할을 함

---
+++ src/main/java/ladder/domain/Participants.java
@@ -0,0 +1,38 @@
+package ladder.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Participants {
+
+    private final List<User> participants;
+
+    private Participants(List<User> users, Ladder ladder) {
+        if(!ladder.equalStartPositionCount(users.size())) {
+            throw new IllegalArgumentException("사용자의 수와 사다리의 StatPosition 수가 다릅니다.");
+        }
+        participants = Collections.unmodifiableList(users);
+    }
+
+    public static Participants of(List<User> users, Ladder ladder) {
+        return new Participants(users, ladder);
+    }
+
+    public int getStartPosition(User user) {
+        int startPosition = 0;
+        for (User participant : participants) {
+            if(participant.equals(user)) {
+                return startPosition;
+            }
+            startPosition++;
+        }
+        throw new IllegalArgumentException("입력하신 사용자는 사다리 게임 참가자가 아닙니다.");
+    }
+
+    public User getUser(int startPosition) {
+        if(startPosition < 0 || startPosition > participants.size()) {
+            throw new IllegalArgumentException("입력된 startPosition은 사디리 올바른 statPosition이 아닙니다.");
+        }
+        return participants.get(startPosition);
+    }
+}
---
+++ src/test/java/ladder/domain/ParticipantsTest.java
@@ -0,0 +1,70 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ParticipantsTest {
+
+    @DisplayName("User들의 statPosition을 얻을 수 있다.")
+    @Test
+    public void getStartPositionTest() {
+        List<User> users = User.of(List.of("pobi", "honux", "crong", "jk"));
+        Ladder ladder = new Ladder(LadderHeight.of(3), users.size());
+        Participants participants = Participants.of(users, ladder);
+
+        int startPosition = participants.getStartPosition(User.of("pobi"));
+
+        assertThat(startPosition).isEqualTo(0);
+    }
+
+    @DisplayName("참가자가 아닌 User 일 경우 IllegalArgumet Exception을 발생한다.")
+    @Test
+    public void getStartPositionNoParticipant() {
+        List<User> users = User.of(List.of("pobi", "honux", "crong", "jk"));
+        Ladder ladder = new Ladder(LadderHeight.of(3), users.size());
+        Participants participants = Participants.of(users, ladder);
+
+        assertThatThrownBy(() -> {
+            participants.getStartPosition(User.of("pobi2"));
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력된 User가 ladder의 크기가 다르면 IllegalArgumentException 을 발생한다.")
+    @Test
+    public void differntUserCountAndLadderStartPositionCount() {
+        List<User> users = User.of(List.of("pobi", "honux", "crong", "jk"));
+        Ladder ladder = new Ladder(LadderHeight.of(3), 3);
+
+        assertThatThrownBy(() -> {
+            Participants.of(users, ladder);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("사디리 startPosition에 있는 User를 가져 올 수 있다.")
+    @Test
+    public void getUserTest() {
+        List<User> users = User.of(List.of("pobi", "honux", "crong", "jk"));
+        Ladder ladder = new Ladder(LadderHeight.of(3), 4);
+        Participants participants = Participants.of(users, ladder);
+
+        User user = participants.getUser(0);
+
+        assertThat(user).isEqualTo(users.get(0));
+    }
+
+    @DisplayName("사다리 범위를 벗어나는 startPosition 을 입력하면 IllegalArgument 예외가 발생한다.")
+    public void getUserOutRange() {
+        List<User> users = User.of(List.of("pobi", "honux", "crong", "jk"));
+        Ladder ladder = new Ladder(LadderHeight.of(3), 3);
+        Participants participants = Participants.of(users, ladder);
+
+        assertThatThrownBy(() -> {
+            participants.getUser(6);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
