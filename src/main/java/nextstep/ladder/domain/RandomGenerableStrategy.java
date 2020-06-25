14dd5be73b046e46f0a686594d468c44ff618d8c
wangyongchoi <choiwangyong@gmail.com>
2020-06-25 18:52:56 +0900

    feat: make ladder
    - 사다리 위치 생성기능 (랜덤으로 생성)

--- src/main/java/nextstep/ladder/domain/RandomGenerableStrategy.java
+++ src/main/java/nextstep/ladder/domain/RandomGenerableStrategy.java
@@ -1,7 +1,5 @@
 package nextstep.ladder.domain;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class RandomGenerableStrategy implements GenerableStrategy {
