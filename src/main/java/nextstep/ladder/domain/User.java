62235a99a5d34edbf148e35f0caa7c9912bdc742
wangyongchoi <choiwangyong@gmail.com>
2020-06-26 22:29:53 +0900

    refactor: add exception for name with whitespace
    https://github.com/next-step/java-ladder/pull/515#discussion_r446150575

--- src/main/java/nextstep/ladder/domain/User.java
+++ src/main/java/nextstep/ladder/domain/User.java
@@ -3,6 +3,7 @@ package nextstep.ladder.domain;
 import java.util.Objects;
 
 public class User {
+
     private static final String INVALID_USER_NAME_LENGTH = "사다리 게임에 참여하는 사람에 이름을 최대 5글자까지 부여할 수 있습니다.";
 
     private String name;
@@ -13,7 +14,11 @@ public class User {
     }
 
     private void validateName(String name) {
-        if(name == null || name.length() > 5) {
+        if (name == null) {
+            throw new IllegalArgumentException(INVALID_USER_NAME_LENGTH);
+        }
+        name = name.trim();
+        if (name.length() < 5) {
             throw new IllegalArgumentException(INVALID_USER_NAME_LENGTH);
         }
     }
