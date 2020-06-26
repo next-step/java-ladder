efc2caeadbb7f7a0e4296679a021f4f813592181
wangyongchoi <choiwangyong@gmail.com>
2020-06-27 00:11:48 +0900

    refactor: modify Line Class
    Line class의 불필요한 상속을 없애고, 전략패턴만 사용하여, 생성할수 있도록 수정하였습니다.
    https://github.com/next-step/java-ladder/pull/515#discussion_r446139088

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -3,15 +3,14 @@ package nextstep.ladder.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Line extends LineGenerating {
+public class Line {
 
     private static final String INVALID_LINE_LENGTH = "Line 길이는 2이상으로 생성 할 수 있습니다.";
     private List<Boolean> points;
 
     public Line(int length, GenerableStrategy generableStrategy) {
-        super(generableStrategy);
         validateLength(length);
-        points = generate(length);
+        points = generableStrategy.generate(length);
     }
 
     private void validateLength(int length) {
@@ -19,7 +18,6 @@ public class Line extends LineGenerating {
             throw new IllegalArgumentException(INVALID_LINE_LENGTH);
         }
     }
-
 
     public int getLadderLength() {
         return points.size();
