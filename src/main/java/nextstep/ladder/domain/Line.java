2ed790c124857b9772db22c495868f1de854f47e
wangyongchoi <choiwangyong@gmail.com>
2020-06-26 21:44:39 +0900

    refactor: modify Class Name (Generating -> LineGenerating)
    https://github.com/next-step/java-ladder/pull/515#discussion_r446130506

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -3,7 +3,7 @@ package nextstep.ladder.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Line extends Generating{
+public class Line extends LineGenerating {
 
     private static final String INVALID_LINE_LENGTH = "Line 길이는 2이상으로 생성 할 수 있습니다.";
     private List<Boolean> ladders;
