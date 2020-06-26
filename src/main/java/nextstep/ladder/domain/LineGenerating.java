bda2f4da5cf8863701938724be4718258bcb9e52
wangyongchoi <choiwangyong@gmail.com>
2020-06-26 21:46:14 +0900

    refactor: move constructor position
    https://github.com/next-step/java-ladder/pull/515#discussion_r446131239

--- src/main/java/nextstep/ladder/domain/LineGenerating.java
+++ src/main/java/nextstep/ladder/domain/LineGenerating.java
@@ -5,17 +5,16 @@ import java.util.List;
 public class LineGenerating {
 
     public static final String INVALID_PARAMETER_GENERABLE_STRATEGY = "generableStrategy cannot be null";
-
     private GenerableStrategy generableStrategy;
-
-    public List<Boolean> generate(int length) {
-        return generableStrategy.generate(length);
-    }
 
     public LineGenerating(GenerableStrategy generableStrategy) {
         if (generableStrategy == null) {
             throw new IllegalArgumentException(INVALID_PARAMETER_GENERABLE_STRATEGY);
         }
         this.generableStrategy = generableStrategy;
+    }
+
+    public List<Boolean> generate(int length) {
+        return generableStrategy.generate(length);
     }
 }
