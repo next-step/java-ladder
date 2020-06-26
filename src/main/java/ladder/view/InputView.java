9501daf8770b065803863d240b60eb392c4e86f7
jun-pil park <iamfeel2@gmail.com>
2020-06-26 20:41:29 +0900

    feat (InputView) : 실행결과를 입력 받는 기능, 결과를 보고 싶은 사람을 입력 받는 기능 추가

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -8,7 +8,9 @@ import java.util.Scanner;
 public class InputView {
     private static final String INPUT_USER_QUESTION = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String INPUT_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
-
+    private static final String INPUT_LADDER_RESULTS = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private static final String INPUT_SHOW_LADDER_RESULT = "결과를 보고 싶은 사람은?";
+    
     private static final Scanner scanner = new Scanner(System.in);
 
     public static List<String> inputUser() {
@@ -23,5 +25,17 @@ public class InputView {
         int height = scanner.nextInt();
         scanner.nextLine();
         return height;
+    }
+
+    public static List<String> inputLadderResult() {
+        System.out.println(INPUT_LADDER_RESULTS);
+        String text = scanner.nextLine();
+
+        return StringUtils.split(text, ",");
+    }
+
+    public static String inputUserNameToShowLadderResult() {
+        System.out.println(INPUT_SHOW_LADDER_RESULT);
+        return scanner.nextLine();
     }
 }
