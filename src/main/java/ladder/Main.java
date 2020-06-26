f0a77a58cba69f3bd3cb7c79c36353ca16687776
jun-pil park <iamfeel2@gmail.com>
2020-06-26 20:43:56 +0900

    feat (Main) : 실행결과를 입력받고, 개암을 실행 결과를 출력하는 기능 적용

--- src/main/java/ladder/Main.java
+++ src/main/java/ladder/Main.java
@@ -1,8 +1,6 @@
 package ladder;
 
-import ladder.domain.Ladder;
-import ladder.domain.LadderHeight;
-import ladder.domain.User;
+import ladder.domain.*;
 import ladder.view.InputView;
 import ladder.view.OutputView;
 
@@ -11,11 +9,19 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<User> users = User.of(InputView.inputUser());
+        List<LadderPrize> ladderPrizes = LadderPrize.of(InputView.inputLadderResult());
         int height = InputView.inputHeight();
-        Ladder ladder = new Ladder(LadderHeight.of(height), users.size());
 
-        OutputView.printTitle();
-        OutputView.printUsers(users);
-        OutputView.printLadder(ladder);
+        Ladder ladder = new Ladder(LadderHeight.of(height), users.size());
+        Participants participants = Participants.of(users, ladder);
+        OutputView.printLadder(participants, ladder, ladderPrizes);
+
+        LadderGame ladderGame = new LadderGame(participants, ladder);
+
+        String name = InputView.inputUserNameToShowLadderResult();
+
+        List<Integer> ladderPrizesIndex = ladderGame.play(name);
+        OutputView.printLadderResult(ladderPrizesIndex, ladderPrizes, participants);
+
     }
 }
