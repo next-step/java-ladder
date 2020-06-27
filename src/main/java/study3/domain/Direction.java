caec6e29bc5ee4b1182a1378e94f1119abc3b958
hyangkeunchoi <gidrms99@naver.com>
2020-06-27 17:10:32 +0900

    add Point 생성

--- src/main/java/study3/domain/Direction.java
+++ src/main/java/study3/domain/Direction.java
@@ -1,12 +1,57 @@
 package study3.domain;
 
+import static java.lang.Boolean.FALSE;
+
 public class Direction {
 	
 	private final boolean left;
     private final boolean right;
     
     public Direction(boolean left, boolean right) {
+    	if (left && right) {
+            throw new IllegalStateException();
+        }
+    	
     	this.left = left;
     	this.right = right;
+    }
+    
+    public boolean isRight() {
+        return this.right;
+    }
+
+    public boolean isLeft() {
+        return this.left;
+    }
+
+    public Direction next(boolean nextRight) {
+        return of(this.right, nextRight);
+    }
+
+    public Direction next() {
+        if (this.right) {
+            return next(FALSE);
+        }
+        return next(randomPoint());
+    }
+
+    public static Direction of(boolean first, boolean second) {
+        return new Direction(first, second);
+    }
+
+    public static Direction first(boolean right) {
+        return of(FALSE, right);
+    }
+		
+    public Direction last() {
+        return of(this.right, FALSE);
+    }
+    
+    @Override
+    public String toString() {
+        return "Direction{" +
+                "left=" + left +
+                ", right=" + right +
+                '}';
     }
 }
