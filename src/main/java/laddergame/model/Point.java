57096eda48335a344c63923215198e97f1b83b92
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-06-27 17:51:54 +0900

    feat : Point에 position, linkedPoint 추가

--- src/main/java/laddergame/model/Point.java
+++ src/main/java/laddergame/model/Point.java
@@ -1,5 +1,6 @@
 package laddergame.model;
 
+import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Point {
@@ -7,10 +8,35 @@ public class Point {
   private static final String RUNG_EXIST = "-----|";
   private static final String RUNG_NOT_EXIST = "     |";
 
-  private final boolean rungCreated;
+  private boolean rungCreated;
+
+  private int position;
+  private Point linkedPoint;
+
 
   public Point(boolean rungCreated) {
     this.rungCreated = rungCreated;
+  }
+
+  public Point(int position) {
+    this.position = position;
+  }
+
+  public Point(int position, Point linkedPoint) {
+    this.position = position;
+    this.linkedPoint = linkedPoint;
+  }
+
+  public int getPosition() {
+    return position;
+  }
+
+  public int getNextPosition() {
+    if (linkedPoint != null) {
+      return linkedPoint.position;
+    }
+
+    return position;
   }
 
   public static Point createNonDuplicatedRungWith(Point target) {
@@ -25,7 +51,53 @@ public class Point {
   }
 
   @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Point point = (Point) o;
+    return rungCreated == point.rungCreated &&
+        position == point.position &&
+        linkedPoint.equals(point.linkedPoint);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(rungCreated, position, linkedPoint);
+  }
+
+  @Override
   public String toString() {
     return rungCreated ? RUNG_EXIST : RUNG_NOT_EXIST;
+  }
+
+  public boolean hasLinkedPoint() {
+    if (linkedPoint != null) {
+      return true;
+    }
+
+    return false;
+  }
+
+  public Point createNext() {
+    if (hasLinkedPoint()) {
+      return new Point(position + 1);
+    }
+
+    return createNextWithLinkedBy(ThreadLocalRandom.current().nextBoolean());
+  }
+
+  public Point createNextWithLinkedBy(boolean isLinked) {
+    Point next = new Point(position + 1);
+
+    if (isLinked) {
+      this.linkedPoint = next;
+      next.linkedPoint = this;
+    }
+
+    return next;
   }
 }
--- src/test/java/laddergame/model/PointTest.java
+++ src/test/java/laddergame/model/PointTest.java
@@ -1,10 +1,14 @@
 package laddergame.model;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class PointTest {
 
@@ -26,7 +30,65 @@ class PointTest {
 
     for (int i = 0; i < 1000; i++) {
       assertThat(Point.createNonDuplicatedRungWith(pointHasRung).isRungCreated()).isFalse();
-      System.out.print(Point.createNonDuplicatedRungWith(pointNotHasRung).isRungCreated()+"||");
+      System.out.print(Point.createNonDuplicatedRungWith(pointNotHasRung).isRungCreated() + "||");
     }
+  }
+
+  @ParameterizedTest
+  @MethodSource("providePoint")
+  void hasNextPoint(Point point, boolean expected) {
+    assertThat(point.hasLinkedPoint()).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> providePoint() {
+    return Stream.of(
+        arguments(
+            new Point(0),
+            false
+        ),
+        arguments(
+            new Point(0, new Point(1)),
+            true
+        )
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("createWithNext")
+  void createNextWithLinkedBy(Point point) {
+    boolean isLinked = true;
+
+    Point next = point.createNextWithLinkedBy(isLinked);
+
+    assertThat(next.hasLinkedPoint()).isEqualTo(isLinked);
+    assertThat(next.getPosition()).isEqualTo(point.getNextPosition());
+  }
+
+  static Stream<Arguments> createWithNext() {
+    return Stream.of(
+        arguments(
+            new Point(0)
+        )
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("createNextWithLinkedBy_실패할경우")
+  void createNextWithLinkedBy_실패할경우(Point point) {
+    boolean isLinked = false;
+
+    Point next = point.createNextWithLinkedBy(isLinked);
+
+    assertThat(next.hasLinkedPoint()).isEqualTo(isLinked);
+    assertThat(point.getPosition()).isEqualTo(point.getNextPosition());
+
+  }
+
+  static Stream<Arguments> createNextWithLinkedBy_실패할경우() {
+    return Stream.of(
+        arguments(
+            new Point(0)
+        )
+    );
   }
 }
