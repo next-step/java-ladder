cbb5defe7e5786e23a591681fb729a4f9e78f31b
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-28 18:30:41 +0900

    refactor: move view string creation logic from view to players class

--- src/main/java/nextstep/ladder/application/view/OutputView.java
+++ src/main/java/nextstep/ladder/application/view/OutputView.java
@@ -1,14 +1,9 @@
 package nextstep.ladder.application.view;
 
-import java.util.Iterator;
-import java.util.Map;
-
-import nextstep.ladder.application.prize.Prize;
 import nextstep.ladder.application.prize.Prizes;
 import nextstep.ladder.application.result.GameResult;
 import nextstep.ladder.domain.ladder.Ladder;
 import nextstep.ladder.domain.line.Lines;
-import nextstep.ladder.domain.player.Player;
 import nextstep.ladder.domain.player.Players;
 import nextstep.ladder.domain.point.Points;
 
@@ -51,12 +46,6 @@ public class OutputView {
 	}
 
 	public static void printResults(GameResult result) {
-		StringBuilder stringBuilder = new StringBuilder("실행 결과\n");
-		Iterator<Map.Entry<Player, Prize>> playerPrize = result.getPlayerPrizes().entrySet().stream().iterator();
-		while (playerPrize.hasNext()) {
-			Map.Entry<Player, Prize> entry = playerPrize.next();
-			stringBuilder.append(entry.getKey().getName()).append(":").append(entry.getValue().getName()).append("\n");
-		}
-		System.out.println(stringBuilder.toString());
+		System.out.println(result.printResults());
 	}
 }
