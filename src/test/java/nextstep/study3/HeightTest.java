b3630b6b85586d17360162be898fda6325d3f0b4
hyangkeunchoi <gidrms99@naver.com>
2020-06-28 19:11:36 +0900

    add

---
+++ src/test/java/nextstep/study3/HeightTest.java
@@ -0,0 +1,36 @@
+package nextstep.study3;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import study2.domain.Height;
+
+class HeightTest {
+	
+	private Height height;
+	
+	@Test
+	@DisplayName("사다리의 높이 검증하는 테스트 메소드")
+	void 사다리의_높이는_1보다_커야된다() {
+		
+		int 사다리의_최소_높이 = 0;
+		
+		assertThatThrownBy(() -> height = new Height(사다리의_최소_높이))
+		.isInstanceOf(IllegalArgumentException.class);
+		
+	}
+	
+	
+	@Test
+	@DisplayName("사다리의 높이가 같은지 검증")
+	void 사다리의_높이가_같은지_검증() {
+		Height height = new Height(50);
+		Height height1 = new Height(50);
+		
+		assertThat(height).isEqualTo(height1);
+		assertThat(height.getHeight()).isEqualTo(height1.getHeight());
+	}
+}
---
+++ src/test/java/nextstep/study3/PlayerTest.java
@@ -0,0 +1,42 @@
+package nextstep.study3;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import study2.domain.Player;
+import study2.domain.Players;
+
+class PlayerTest {
+
+	Player player;
+	Players players;
+
+	@Test
+	@DisplayName("이름 길이 테스트")
+	void 이름의_길이가_5자를넘어가면_안된다() {
+		assertThatThrownBy(() -> player = new Player("choihyang"))
+									.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@ParameterizedTest
+	@DisplayName("플레이어들은 최소 1명이상입니다.")
+	@MethodSource("blankStrings")
+	void 플레이어_체크_테스트(String testString) {
+		// 이부분에서 nullPointerException과
+		// IllegalArgumentExcpetion에 대한 분기처리를 어떻게 할 수 있나요?
+		assertThatThrownBy(() -> player = new Player(testString))
+									.isInstanceOf(IllegalArgumentException.class);
+	}
+	
+	static Stream<String> blankStrings() {
+		return Stream.of("", " ", null);
+	}
+}
