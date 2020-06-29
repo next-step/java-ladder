33b6da7ad24ab0d7b939d052b69762f5b30683a9
Junpil Park <iamfeel2@gmail.com>
2020-06-30 00:21:10 +0900

    Update src/main/java/ladder/domain/Participants.java
    refactor (Participants) : if 후 띄워쓰기 convention 수정
    
    Co-authored-by: Taegyeoung Oh <otk1090@naver.com>

--- src/main/java/ladder/domain/Participants.java
+++ src/main/java/ladder/domain/Participants.java
@@ -30,7 +30,7 @@ public class Participants {
     }
 
     public User get(int startPosition) {
-        if(startPosition < 0 || startPosition > participants.size()) {
+        if (startPosition < 0 || startPosition > participants.size()) {
             throw new IllegalArgumentException("입력된 startPosition은 사디리 올바른 statPosition이 아닙니다.");
         }
         return participants.get(startPosition);
