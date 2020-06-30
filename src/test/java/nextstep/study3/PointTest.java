c955fe83ef60c60f5189ed6f3bab5735ba9838c7
hyangkeunchoi <gidrms99@naver.com>
2020-06-30 22:35:54 +0900

    PointTest 추가

--- src/test/java/nextstep/study3/PointTest.java
+++ src/test/java/nextstep/study3/PointTest.java
@@ -2,6 +2,7 @@ package nextstep.study3;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -18,26 +19,30 @@ class PointTest {
     }
 	
 	@Test
+	@DisplayName("두번째 move가 제자리인 경우 인덱스 검증")
     public void next_stay() {
         Point second = new Point(0, new Direction(false, false)).next(FALSE);
         assertThat(second.move()).isEqualTo(1);
     }
 
-	/*@Test
+	@Test
+	@DisplayName("왼쪽으로 움직이면 인덱스가 하나떨어진다")
 	public void next_left() {
-	    Point second = Point.first(TRUE).next(FALSE);
-	    assertThat(second.move(), is(0));
+	    Point second = new Point(0, new Direction(false, true)).next(FALSE);
+	    assertThat(second.move()).isEqualTo(0);
 	}
 	
 	@Test
+	@DisplayName("오른쪽으로 움직였을떄 2가된다")
 	public void next_right() {
-	    Point second = Point.first(FALSE).next(TRUE);
-	    assertThat(second.move(), is(2));
+	    Point second = new Point(0, new Direction(false, false)).next(TRUE);
+	    assertThat(second.move()).isEqualTo(2);
 	}
 	
 	@Test
+	@DisplayName("right가 true이면 next는 false / 한라인에 두번이동불가")
 	public void next() {
-	    Point second = Point.first(TRUE).next();
-	    assertThat(second.move(), is(0));
-	}*/
+	    Point second = new Point(0, new Direction(false, true)).next();
+	    assertThat(second.move()).isEqualTo(0);
+	}
 }
