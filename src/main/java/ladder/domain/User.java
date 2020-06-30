99cd42ed2b8e3ea74b6ca787de7868b468f84b6a
Ho <doitluke@naver.com>
2020-07-01 03:15:13 +0900

    refactor:user관련 기능 보완(검증기능)

--- src/main/java/ladder/domain/User.java
+++ src/main/java/ladder/domain/User.java
@@ -4,13 +4,14 @@ import ladder.utils.InputUtils;
 
 public class User {
 
+    private final int MAX_NAME_LENGTH = 5;
     private String name;
 
     public User(String inputName) {
         InputUtils.isEmptyOrNull(inputName);
-        if(inputName.length() > 5){
+        if (inputName.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("이름은 5글자를 초과할 수 없습니다.");
         }
-        name = inputName;
+        this.name = inputName;
     }
 }
