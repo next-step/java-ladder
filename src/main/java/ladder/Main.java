7f5ac50cd55c66b0beb3cdec703e71dbac5c1fc8
junpil park <iamfeel2@gmail.com>
2020-06-30 16:32:51 +0900

    fix (Main) : all 명령이 들어 올 때까지 실행결과를 볼 수 있도록 수정

--- src/main/java/ladder/Main.java
+++ src/main/java/ladder/Main.java
@@ -8,21 +8,18 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> users = User.of(InputView.inputUser());
-        List<LadderPrize> ladderPrizes = LadderPrize.of(InputView.inputLadderResult());
+        Participants participants = Participants.of(InputView.inputUser());
+        LadderPrizes ladderPrizes = LadderPrizes.of(InputView.inputLadderResult());
         int height = InputView.inputHeight();
-
-        Ladder ladder = new Ladder(LadderHeight.of(height), users.size());
-        LadderPrizes ladderResult = LadderPrizes.of(ladderPrizes, ladder);
-        Participants participants = Participants.of(users, ladder);
-
-        OutputView.printLadder(participants, ladder, ladderResult);
+        Ladder ladder = new Ladder(LadderHeight.of(height), participants, ladderPrizes);
+        OutputView.printLadder(participants, ladder, ladderPrizes);
 
         LadderGame ladderGame = new LadderGame(participants, ladder);
-
-        String name = InputView.inputUserNameToShowLadderResult();
-        List<Integer> ladderPrizesIndex = ladderGame.play(name);
-        OutputView.printLadderResult(participants, LadderPrizes.convert(ladderPrizesIndex, ladderResult));
-
+        String name;
+        do {
+            name = InputView.inputUserNameToShowLadderResult();
+            List<Integer> ladderPrizesIndex = ladderGame.play(name);
+            OutputView.printLadderResult(participants, LadderPrizes.convert(ladderPrizesIndex, ladderPrizes));
+        } while (!ladderGame.isFinishGame(name));
     }
 }
