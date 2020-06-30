5450c4f0006f8d9a77b7eb17fe6e9a8b9df4ed4e
hyangkeunchoi <gidrms99@naver.com>
2020-06-30 22:17:39 +0900

    LadderLine 및 PointTest메소드 추가

--- src/test/java/nextstep/study3/LadderLineTest.java
+++ src/test/java/nextstep/study3/LadderLineTest.java
@@ -3,20 +3,25 @@ package nextstep.study3;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import study3.domain.LadderLine;
+import study3.domain.Line;
 
 class LadderLineTest {
-
+	
+	
+	
 	@Test
-	@DisplayName("")
+	@DisplayName("라인메소드 initLine 검증")
     public void init() {
         int sizeOfPerson = 5;
-        System.out.println(LadderLine.init(sizeOfPerson));
+        Line line = new Line(sizeOfPerson, false);
+        System.out.println(line.getLine());
     }
 
     @Test
+    @DisplayName("move 메소드 검증")
     public void move() {
-        LadderLine line = LadderLine.init(2);
+        Line line = new Line(2, false);
+        
         System.out.println("ladder result : " + line.move(0));
     }
 
--- src/test/java/nextstep/study3/PointTest.java
+++ src/test/java/nextstep/study3/PointTest.java
@@ -1,6 +1,7 @@
 package nextstep.study3;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static java.lang.Boolean.FALSE;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -14,5 +15,29 @@ class PointTest {
 	@DisplayName("인덱스 검증 메소드")
     public void 우측으로_한칸움직이면_move의_인덱스가_하나늘어난다() {
         assertThat(new Point(0, new Direction(false, true)).move()).isEqualTo(1);
-    }	
+    }
+	
+	@Test
+    public void next_stay() {
+        Point second = new Point(0, new Direction(false, false)).next(FALSE);
+        assertThat(second.move()).isEqualTo(1);
+    }
+
+	/*@Test
+	public void next_left() {
+	    Point second = Point.first(TRUE).next(FALSE);
+	    assertThat(second.move(), is(0));
+	}
+	
+	@Test
+	public void next_right() {
+	    Point second = Point.first(FALSE).next(TRUE);
+	    assertThat(second.move(), is(2));
+	}
+	
+	@Test
+	public void next() {
+	    Point second = Point.first(TRUE).next();
+	    assertThat(second.move(), is(0));
+	}*/
 }
