a3d45078eff2bc1c71f526df7f478a78acbef890
하재권 <hazxz@naver.com>
2020-06-30 13:46:41 +0900

    refactor: User 클래스는 기존과 크게 다르지 않지만 새로 개발된 클래스들과 명명법이나 코딩 스타일을 맞춤

--- src/main/java/nextstep/ladder/domain/User.java
+++ src/main/java/nextstep/ladder/domain/User.java
@@ -1,15 +1,18 @@
 package nextstep.ladder.domain;
 
 public class User {
-
     private static final int MAX_LENGTH = 5;
     private final String name;
 
     public User(String name) {
-        if (name.length() > MAX_LENGTH) {
-            throw new IllegalArgumentException("`name` length is must under " + MAX_LENGTH);
-        }
+        validateName(name);
         this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("name length is more than " + MAX_LENGTH);
+        }
     }
 
     public String getName() {
