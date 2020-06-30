4c425872959ae441d5d786c3a9ffaad488f89edb
Ho <doitluke@naver.com>
2020-07-01 03:17:13 +0900

    ãfeat: 출력 클래스 , 인터í페이스 테스트클래스 작성

--- src/main/java/ladder/ui/OutputView.java
+++ src/main/java/ladder/ui/OutputView.java
@@ -1,4 +1,17 @@
 package ladder.ui;
 
+import ladder.domain.Line;
+import ladder.domain.User;
+
+import java.util.List;
+
 public class OutputView {
+
+    private OutputView(){
+    }
+
+    public static void drawLadder(List<String> ladder, List<User> users){
+        System.out.print(users);
+        System.out.println(ladder);
+    }
 }
