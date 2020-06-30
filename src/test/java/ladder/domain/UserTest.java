1e06943587d6e7e6eb02b19a3b131ffb32cdb7c5
HSR <tradlinx@DESKTOP-MVDAAFK>
2020-06-30 15:55:53 +0900

    feat: User클래스 생성 및 Doc 클래스이름 수정 & inputUtil클래스 생성

---
+++ src/test/java/ladder/domain/UserTest.java
@@ -0,0 +1,41 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class UserTest {
+
+    private User user;
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @DisplayName("생성자를 통한 user클래스 생성")
+    @Test
+    public void createUser() {
+        user = new User("pobi");
+    }
+
+    @DisplayName("5글자 초과한 이름에 대해 에러 발생")
+    @Test
+    public void createUserWithError() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> user = new User("pobierror"));
+    }
+
+    @DisplayName("공백, null에 대해 에러 발생")
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void createUserWithEmptyNull(String inputText) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> user = new User(inputText));
+    }
+}
