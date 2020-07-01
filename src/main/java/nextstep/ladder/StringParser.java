bfe91f89d32aceb477595686849ca38eb71c81e4
yjs2952 <yjs2952@naver.com>
2020-07-01 23:27:10 +0900

    feat: 문자열 정수로 변환 메소드 추가

--- src/main/java/nextstep/ladder/StringParser.java
+++ src/main/java/nextstep/ladder/StringParser.java
@@ -6,8 +6,21 @@ import java.util.stream.Collectors;
 
 public class StringParser {
 
+    private static final String NUMBER_FORMAT_EXCEPTION_MESSAGE = "숫자로 변환할 수 없는 문자입니다.";
+
     public static List<String> splitNames(String input){
         return Arrays.stream(input.split(","))
                 .collect(Collectors.toList());
+    }
+
+    public static int toInteger(String input) {
+
+        int number = 0;
+        try {
+            number = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException(NUMBER_FORMAT_EXCEPTION_MESSAGE);
+        }
+        return number;
     }
 }
