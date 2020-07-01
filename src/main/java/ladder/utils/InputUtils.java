11c6fcfea16aab109f9132fd13fa3b3e0525e732
Ho <doitluke@naver.com>
2020-07-01 23:41:57 +0900

    ãrefactor: 리턴타입 변경

--- src/main/java/ladder/utils/InputUtils.java
+++ src/main/java/ladder/utils/InputUtils.java
@@ -2,9 +2,11 @@ package ladder.utils;
 
 public class InputUtils {
 
-    public static void isEmptyOrNull(String text) {
+    public static String isEmptyOrNull(String text) {
         if (text == null || text.isEmpty()) {
             throw new IllegalArgumentException("공백이거나 비어있을수 없습니다.");
         }
+
+        return text;
     }
 }
