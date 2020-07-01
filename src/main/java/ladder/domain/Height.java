30de96e8d140ce124fc1f9c1fcc56dc91ab97509
Ho <doitluke@naver.com>
2020-07-01 23:40:49 +0900

    refactor: ãUsers toㄴãString수정 및 getUserCount메서드 추가 & 유저 입력ê´숫자 포장할 Height클래스 생성

---
+++ src/main/java/ladder/domain/Height.java
@@ -0,0 +1,20 @@
+package ladder.domain;
+
+public class Height {
+
+    private final static int MINIMUN_HEIGHT = 0;
+    private int height;
+
+    public Height(int inputHeight) {
+
+        if (inputHeight < MINIMUN_HEIGHT) {
+            throw new IllegalArgumentException("0보다 작을 수 없습니다.");
+        }
+
+        this.height = inputHeight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
--- src/main/java/ladder/domain/Users.java
+++ src/main/java/ladder/domain/Users.java
@@ -6,21 +6,33 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 public class Users {
 
+    private final static String BLANK = " ";
     private static final String COMMA = ",";
     private List<User> users;
 
-    public Users(String usersName){
-        this(Arrays.stream(usersName.split(COMMA))
+    public Users(String usersName) {
+        this(Arrays.stream(InputUtils.isEmptyOrNull(usersName).split(COMMA))
                 .map(String::trim)
                 .filter(text -> !text.isEmpty())
                 .map(User::new)
                 .collect(Collectors.toList()));
     }
 
-    public Users(List<User> users){
+    public Users(List<User> users) {
         this.users = users;
     }
 
+    public int getUserCount() {
+        return this.users.size();
+    }
+
+    @Override
+    public String toString() {
+        return this.users.stream()
+                .map(User::toString)
+                .collect(Collectors.joining(BLANK));
+    }
 }
