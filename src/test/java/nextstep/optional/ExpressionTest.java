5cf57c8a6d6f9bbe6502c91a78ddbde851aff9c1
neojjc2 <neo.jjc+2@gmail.com>
2020-08-11 12:42:33 +0900

    [fix][Step1] Optional 요구사항 구현 완료

--- src/test/java/nextstep/optional/ExpressionTest.java
+++ src/test/java/nextstep/optional/ExpressionTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-
 public class ExpressionTest {
     @DisplayName("연산기호에 맞는 값이 제대로 반환 되는지 확인")
     @CsvSource(value = {
@@ -19,12 +18,14 @@ public class ExpressionTest {
     }, delimiter = ':')
     @ParameterizedTest
     public void of(String symbol, Expression expected) {
+        // when & then
         assertThat(Expression.of(symbol) == expected).isTrue();
     }
 
     @DisplayName("지원하지 않는 연산기호 일 경우 예외")
     @Test
     public void notValidExpression() {
+        // when & then
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> Expression.of("&"))
                 .withMessage("&는 사칙연산에 해당하지 않는 표현식입니다.");
