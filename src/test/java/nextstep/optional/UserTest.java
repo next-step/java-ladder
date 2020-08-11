bef807fec06dd7fa726b0b383e59048d2a24e4be
neojjc2 <neo.jjc+2@gmail.com>
2020-08-11 12:37:19 +0900

    [fix][Step1] 테스트 코드 정리

--- src/test/java/nextstep/optional/UserTest.java
+++ src/test/java/nextstep/optional/UserTest.java
@@ -6,25 +6,30 @@ import org.junit.jupiter.api.Test;
 import static nextstep.optional.User.ageIsInRange1;
 import static nextstep.optional.User.ageIsInRange2;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class UserTest {
     @DisplayName("입력한 사용자의 연령이 30세 이상 45세 이하 인지 확인")
     @Test
     public void whenFiltersWithoutOptional_thenCorrect() {
-        assertThat(ageIsInRange1(new User("crong", 35))).isTrue();
-        assertThat(ageIsInRange1(new User("crong", 48))).isFalse();
-        assertThat(ageIsInRange1(new User("crong", null))).isFalse();
-        assertThat(ageIsInRange1(new User("crong", 29))).isFalse();
-        assertThat(ageIsInRange1(null)).isFalse();
+        assertAll(
+                () -> assertThat(ageIsInRange1(new User("crong", 35))).isTrue(),
+                () -> assertThat(ageIsInRange1(new User("crong", 48))).isFalse(),
+                () -> assertThat(ageIsInRange1(new User("crong", null))).isFalse(),
+                () -> assertThat(ageIsInRange1(new User("crong", 29))).isFalse(),
+                () -> assertThat(ageIsInRange1(null)).isFalse());
+
     }
 
     @DisplayName("입력한 사용자의 연령이 30세 이상 45세 이하 인지 확인 (Optional 적용)")
     @Test
     public void whenFiltersWithOptional_thenCorrect() {
-        assertThat(ageIsInRange2(new User("crong", 35))).isTrue();
-        assertThat(ageIsInRange2(new User("crong", 48))).isFalse();
-        assertThat(ageIsInRange2(new User("crong", null))).isFalse();
-        assertThat(ageIsInRange2(new User("crong", 29))).isFalse();
-        assertThat(ageIsInRange2(null)).isFalse();
+        assertAll(
+                () -> assertThat(ageIsInRange2(new User("crong", 35))).isTrue(),
+                () -> assertThat(ageIsInRange2(new User("crong", 48))).isFalse(),
+                () -> assertThat(ageIsInRange2(new User("crong", null))).isFalse(),
+                () -> assertThat(ageIsInRange2(new User("crong", 29))).isFalse(),
+                () -> assertThat(ageIsInRange2(null)).isFalse()
+        );
     }
 }
--- src/test/java/nextstep/optional/UsersTest.java
+++ src/test/java/nextstep/optional/UsersTest.java
@@ -1,11 +1,13 @@
 package nextstep.optional;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UsersTest {
 
+    @DisplayName("해당 유저가 있을 경우 잘 반환 되는지 확인")
     @Test
     public void getUser() {
         Users users = new Users();
@@ -13,6 +15,7 @@ public class UsersTest {
     }
 
 
+    @DisplayName("해당 유저가 없을 경우 기본 유저 정보가 잘 반환되는지 확인")
     @Test
     public void getDefaultUser() {
         Users users = new Users();
