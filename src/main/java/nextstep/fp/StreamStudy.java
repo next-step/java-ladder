87c41703f7ee4d192b620589640850e590a1b40b
newton <antksk@gmail.com>
2020-08-15 00:28:41 +0900

    [fix][사다리-step1] 다양한 stream method 실습

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -8,6 +8,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.Comparator.comparingInt;
+import static java.util.stream.Collectors.toList;
+
 public class StreamStudy {
 
     public static long countWords() throws IOException {
@@ -15,11 +18,10 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream()
+                    .filter(w -> 12 < w.length())
+                    .count()
+            ;
     }
 
     public static void printLongestWordTop100() throws IOException {
@@ -27,18 +29,28 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        List<String> wordTop100 = words.stream()
+                                       .sorted(comparingInt(String::length).reversed())
+                                       .limit(100)
+                                       .collect(toList())
+            ;
+        System.out.println(wordTop100);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
+        return numbers.stream().map(x -> 2 * x).collect(toList());
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers.stream().reduce(0, Integer::sum);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                      .filter(n -> 3 < n)
+                      .map(n -> n * 2)
+                      .mapToInt(Integer::intValue)
+                      .sum()
+            ;
     }
 }
