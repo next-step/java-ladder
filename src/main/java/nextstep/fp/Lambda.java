c22c0cade124d24966ec7bc9f30910828934b544
박찬석 <pcspapa@gmail.com>
2020-08-21 23:59:28 +0900

    feat: 람다 실습 2 - 람다를 활용 & 중복 제거
    Conditional 을 꼭 사용해야 하나요?

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,47 +1,43 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
-public class Lambda {
+public class Lambda  {
     public static void printAllOld(List<Integer> numbers) {
-        System.out.println("printAllOld");
-
-        numbers.stream()
-            .forEach(System.out::println);
+        printNumbers("printAllOld", numbers, System.out::println);
     }
 
     public static void printAllLambda(List<Integer> numbers) {
-        System.out.println("printAllLambda");
+        printNumbers("printAllLambda", numbers, System.out::println);
+    }
 
+    private static void printNumbers(String message, List<Integer> numbers, Consumer<Integer> write) {
+        System.out.println(message);
         numbers.stream()
-            .forEach(System.out::println);
+            .forEach(write);
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> System.out.println("Hello from thread")).start();
     }
 
     public static int sumAll(List<Integer> numbers) {
-        return numbers.stream()
-            .mapToInt(Integer::intValue)
-            .sum();
+        return filteringSum(numbers, number -> Boolean.TRUE);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        return numbers.stream()
-            .filter(number -> number % 2 == 0)
-            .mapToInt(Integer::intValue)
-            .sum();
+        return filteringSum(numbers, number -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
+        return filteringSum(numbers, number -> number > 3);
+    }
+
+    private static int filteringSum(List<Integer> numbers, Predicate<Integer> predicate) {
         return numbers.stream()
-            .filter(number-> number > 3)
+            .filter(predicate)
             .mapToInt(Integer::intValue)
             .sum();
     }
