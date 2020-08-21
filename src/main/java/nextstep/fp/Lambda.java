b41dd1f2c3da2eaea37c7794c3a5178d8ec923f7
박찬석 <pcspapa@gmail.com>
2020-08-21 23:38:59 +0900

    feat: 람다 실습 2 - 람다를 활용

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -6,15 +6,15 @@ public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
         System.out.println("printAllOld");
 
-        for (int number : numbers) {
-            System.out.println(number);
-        }
+        numbers.stream()
+            .forEach(System.out::println);
     }
 
     public static void printAllLambda(List<Integer> numbers) {
         System.out.println("printAllLambda");
 
-        numbers.forEach(System.out::println);
+        numbers.stream()
+            .forEach(System.out::println);
     }
 
     public static void runThread() {
@@ -27,30 +27,22 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return numbers.stream()
+            .mapToInt(Integer::intValue)
+            .sum();
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return numbers.stream()
+            .filter(number -> number % 2 == 0)
+            .mapToInt(Integer::intValue)
+            .sum();
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return numbers.stream()
+            .filter(number-> number > 3)
+            .mapToInt(Integer::intValue)
+            .sum();
     }
 }
