4eee4d7963abc616eef9bde682cfdd136a8233c9
Sang Uk Ahn <suahnn@gmail.com>
2020-08-22 00:10:01 +0900

    refactor: draw 확률 상수 처리

--- src/main/java/ladder/domain/RandomDrawStrategy.java
+++ src/main/java/ladder/domain/RandomDrawStrategy.java
@@ -4,11 +4,13 @@ import java.util.function.Consumer;
 
 public class RandomDrawStrategy implements DrawStrategy {
 
+    private static final double DRAW_PROBABILITY = 0.5;
+
     private boolean beforeDraw;
 
     @Override
     public void draw(int index, Consumer<Integer> indexConsumer) {
-        if (!beforeDraw && Math.random() > 0.5) {
+        if (!beforeDraw && Math.random() > DRAW_PROBABILITY) {
             indexConsumer.accept(index);
             beforeDraw = true;
             return;
