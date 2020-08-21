a09a1fdf493759ba6bce7f23b30db36153c58e16
박찬석 <pcspapa@gmail.com>
2020-08-22 00:12:48 +0900

    feat: map, reduce, filter 실습 1

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -10,35 +10,40 @@ import java.util.stream.Collectors;
 
 public class StreamStudy {
 
-    public static long countWords() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+  public static long countWords() throws IOException {
+    String contents = new String(Files.readAllBytes(Paths
+        .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+    List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+    long count = 0;
+    for (String w : words) {
+      if (w.length() > 12) {
+        count++;
+      }
     }
+    return count;
+  }
 
-    public static void printLongestWordTop100() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+  public static void printLongestWordTop100() throws IOException {
+    String contents = new String(Files.readAllBytes(Paths
+        .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+    List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
-    }
+    // TODO 이 부분에 구현한다.
+    System.out.println(words.stream().filter(w -> w.length() > 12).count());
+  }
 
-    public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
-    }
+  public static List<Integer> doubleNumbers(List<Integer> numbers) {
+    return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
+  }
 
-    public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
-    }
+  public static long sumAll(List<Integer> numbers) {
+    return numbers.stream().reduce(0, (x, y) -> x + y);
+  }
 
-    public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
-    }
+  public static long sumOverThreeAndDouble(List<Integer> numbers) {
+    return numbers.stream()
+        .filter(number -> number > 3)
+        .reduce(0, (a, b) -> a + b * 2);
+  }
 }
