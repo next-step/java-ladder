a8b4a8735a65b3562dea421669bd178f77f88a7b
Sang Uk Ahn <suahnn@gmail.com>
2020-08-23 00:48:03 +0900

    text: move 테스트케이스 추가

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -2,6 +2,8 @@ package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -42,5 +44,27 @@ public class LineTest {
         line.drawsFrom(drawPosition);
         // then
         assertThat(line.startsWith(drawPosition)).isTrue();
+    }
+
+    /**
+     * 선 이동 테스트
+     * |-----| 1 -> 0, 0 -> 1
+     * @param countOfPoint 포인트 개수
+     * @param drawPosition draw 시작 포인트
+     * @param startPosition 시작 위치
+     * @param moveToPosition 이동 위치
+     */
+    @ParameterizedTest
+    @CsvSource(value = {
+        "2,0,1,0", "2,0,0,1"
+    })
+    @DisplayName("선 이동")
+    void testMove(int countOfPoint, int drawPosition, int startPosition, int moveToPosition) {
+        // given
+        Line line = new Line(countOfPoint);
+        // when
+        line.drawsFrom(drawPosition);
+        // then
+        assertThat(line.move(startPosition)).isEqualTo(moveToPosition);
     }
 }
