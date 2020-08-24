2723c0c3f040f71509fa3a03d0046075c3e8fd25
박찬석 <pcspapa@gmail.com>
2020-08-24 22:13:24 +0900

    refactor: 메시지를 상수로 추출

--- src/main/java/nextstep/ladder/ViewInput.java
+++ src/main/java/nextstep/ladder/ViewInput.java
@@ -7,6 +7,9 @@ import java.util.stream.Collectors;
 
 public class ViewInput {
 
+  public static final String SCRIPT_PLAYER_NAMES = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+  public static final String SCRIPT_LADDER_HEIGHT = "\n최대 사다리 높이는 몇 개인가요?";
+
   private Scanner scanner;
 
   public ViewInput(Scanner scanner) {
@@ -26,12 +29,12 @@ public class ViewInput {
   }
 
   private String playerNames() {
-    System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+    System.out.println(SCRIPT_PLAYER_NAMES);
     return scanner.next();
   }
 
   private int ladderHeight() {
-    System.out.println("\n최대 사다리 높이는 몇 개인가요?");
+    System.out.println(SCRIPT_LADDER_HEIGHT);
     return scanner.nextInt();
   }
 
