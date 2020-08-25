2712f1c31b0c88da0d4ccd8eb91a2885c92e9c03
yongdae <7942yongdae@gmail.com>
2020-08-25 18:59:46 +0900

    refactor: https://github.com/next-step/java-ladder/pull/553#discussion_r475571480 수정

--- src/main/java/nextstep/mission/Ladder.java
+++ src/main/java/nextstep/mission/Ladder.java
@@ -8,13 +8,11 @@ import java.util.stream.Stream;
 public class Ladder {
 
     private List<Participant> participants;
-    private int height;
     private List<Line> lines;
     private int cursor;
 
     private Ladder(List<Participant> participants, int height) {
         this.participants = participants;
-        this.height = height;
         this.lines = Stream.generate(() -> new Line(participants.size()))
                 .limit(height)
                 .collect(Collectors.toList());
@@ -26,13 +24,12 @@ public class Ladder {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Ladder ladder = (Ladder) o;
-        return height == ladder.height &&
-                Objects.equals(participants, ladder.participants);
+        return Objects.equals(participants, ladder.participants);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(participants, height);
+        return Objects.hash(participants);
     }
 
     public static Ladder make(List<Participant> participants, int height) {
