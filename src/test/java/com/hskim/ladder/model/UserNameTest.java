e749bd1a527a1e196323e9afa45897f4ba4b30b5
samkko <wkvk1234@gmail.com>
2020-08-26 01:34:28 +0900

    [step02][test] User, UserName의 util성 메소드 TC 추가

--- src/test/java/com/hskim/ladder/model/UserNameTest.java
+++ src/test/java/com/hskim/ladder/model/UserNameTest.java
@@ -2,6 +2,13 @@ package com.hskim.ladder.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -25,5 +32,27 @@ public class UserNameTest {
         assertThatIllegalArgumentException().isThrownBy(() -> {
             new UserName("exceed");
         });
+    }
+
+    @DisplayName("유저 이름 리스트를 통해 UserName 객체 리스트를 만드는 로직 테스트")
+    @ParameterizedTest
+    @MethodSource("provideNamesAndResult")
+    void makeUserNamesFromNames(List<String> names, List<UserName> expected) {
+        // when
+        List<UserName> result = UserName.makeUserNamesFromNames(names);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideNamesAndResult() {
+        return Stream.of(
+                Arguments.of(Arrays.asList("kim", "hs", "test"),
+                        Arrays.asList(new UserName("kim"), new UserName("hs"), new UserName("test"))),
+                Arguments.of(Arrays.asList("java", "lad", "der"),
+                        Arrays.asList(new UserName("java"), new UserName("lad"), new UserName("der"))),
+                Arguments.of(Arrays.asList("name1", "name2", "name3"),
+                        Arrays.asList(new UserName("name1"), new UserName("name2"), new UserName("name3")))
+        );
     }
 }
--- src/test/java/com/hskim/ladder/model/UserTest.java
+++ src/test/java/com/hskim/ladder/model/UserTest.java
@@ -2,6 +2,13 @@ package com.hskim.ladder.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,5 +22,30 @@ public class UserTest {
 
         // when & then
         assertThat(user).isEqualTo(new User(new UserName("test")));
+    }
+
+    @DisplayName("UserName 객체 리스트를 통해 User 객체 리스트를 만드는 로직 테스트")
+    @ParameterizedTest
+    @MethodSource("provideUserNamesAndResult")
+    void makeUsersFromNames(List<UserName> userNames, List<User> expected) {
+        // when
+        List<User> result = User.makeUsersFromNames(userNames);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideUserNamesAndResult() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(new UserName("kim"), new UserName("hs"), new UserName("test")),
+                        Arrays.asList(new User(new UserName("kim")), new User(new UserName("hs")),
+                                new User(new UserName("test")))),
+                Arguments.of(Arrays.asList(new UserName("name1"), new UserName("name2"), new UserName("name3")),
+                        Arrays.asList(new User(new UserName("name1")), new User(new UserName("name2")),
+                                new User(new UserName("name3")))),
+                Arguments.of(Arrays.asList(new UserName("hard"), new UserName("to"), new UserName("test")),
+                        Arrays.asList(new User(new UserName("hard")), new User(new UserName("to")),
+                                new User(new UserName("test"))))
+        );
     }
 }
