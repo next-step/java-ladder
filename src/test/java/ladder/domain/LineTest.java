17b186294fcdd3778912e83a2da92c07b401b4ef
sefide <sefide@naver.com>
2020-08-27 00:03:29 +0900

    test: LineTest - 테스트데이터 추가

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -1,6 +1,7 @@
 package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -15,17 +16,23 @@ public class LineTest {
 
     private static Stream<Arguments> makeLineCreateData() {
         return Stream.of(
-                Arguments.of(Arrays.asList(Point.getFirst(true)), 1),
-                Arguments.of(Arrays.asList(Point.getFirst(true), Point.of(true, false), Point.getLast(false)), 3),
-                Arguments.of(Arrays.asList(Point.getFirst(true), Point.of(true, false), Point.of(false, false)
-                        , Point.of(false, true), Point.getLast(true)), 5)
+                Arguments.of(Arrays.asList(Point.getFirst(true))
+                        , 1, false),
+                Arguments.of(Arrays.asList(Point.getFirst(true), Point.of(true, false), Point.getLast(false))
+                        , 3, false),
+                Arguments.of(Arrays.asList(Point.getFirst(true), Point.of(true, false)
+                        , Point.of(false, false), Point.of(false, true), Point.getLast(true))
+                        , 5, false),
+                Arguments.of(Arrays.asList(Point.getFirst(true), Point.of(true, true)
+                        , Point.of(true, false), Point.getLast(false))
+                        , 4, true)
         );
     }
 
     @DisplayName("Line 생성 테스트")
     @ParameterizedTest
     @MethodSource("makeLineCreateData")
-    void create(List<Point> points, int participantNumber) {
+    void create(List<Point> points, int expectedParticipantNumber, boolean expectedResult) {
         Line line = Line.of(points);
 
         boolean actual = line.getPoints()
@@ -33,8 +40,8 @@ public class LineTest {
                 .anyMatch(point -> point.isRight() && point.isLeft());
 
         // 참가자 수 만큼 생성되었는지 확인
-        assertThat(line.getSize()).isEqualTo(participantNumber);
+        assertThat(line.getSize()).isEqualTo(expectedParticipantNumber);
         // 연속되지 않게 생성되었는지 확인
-        assertThat(actual).isFalse();
+        assertThat(actual).isEqualTo(expectedResult);
     }
 }
