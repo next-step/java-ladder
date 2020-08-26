4669a2bfeed8c89664ea7fb16c0f90d3504bc8a6
ted.kang <ted.kang@kakaocorp.com>
2020-08-26 22:48:35 +0900

    feature 2. 람다 실습 2 - 람다를 활용해 중복 제거

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
@@ -27,30 +28,20 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAllByPredicate(numbers, number -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAllByPredicate(numbers, number -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAllByPredicate(numbers, number -> number > 3);
+    }
+
+    private static int sumAllByPredicate(List<Integer> numbers, Predicate<Integer> predicate) {
+        return numbers.stream()
+                .filter(predicate)
+                .reduce(0, Integer::sum);
     }
 }
