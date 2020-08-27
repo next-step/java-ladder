f9870fc6a19ad13386c3f8847c88cb706908a55f
김다희 <sefide36@tmon.co.kr>
2020-08-27 23:16:45 +0900

    refactor: 불필요한 개행 제거

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class InputView {
     private static final String PARTICIPANTS_NAME_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String LADDER_HEIGHT_MESSAGE = "\n최대 사다리 높이는 몇 개인가요?";
-
     private static final Scanner scanner = new Scanner(System.in);
 
     public static String scanParticipantNames() {
