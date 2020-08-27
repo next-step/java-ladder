2eecadfd7af5211df69b3f27e9bf0a4a2504f559
Tommy <50355263+LimHanGyeol@users.noreply.github.com>
2020-08-27 13:49:23 +0900

    Step1 - 자바8 스트림, 람다, Optional (#564)
    * feat : lambda 실습1 - CarTest
    
    * feat : lambda 실습2 - Sum
    
    * feat : stream 실습1 - sumOverThreeAndDouble
    
    * feat : stream 실습2 - printLongestWordTop100
    
    * feat : Optional 실습1 - User.ageIsInRange2
    
    * feat : Optional 실습2 - Users.getUser
    
    * feat : Optional 실습3 - Expression

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,40 +5,58 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
+
+    private static final int NUMBER_TWELVE = 12;
+    private static final int NUMBER_TWO = 2;
+    private static final int NUMBER_ZERO = 0;
+    private static final int NUMBER_THREE = 3;
+    private static final int NUMBER_HUNDRED = 100;
 
     public static long countWords() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream()
+                .filter(word -> word.length() > NUMBER_TWELVE)
+                .count();
     }
 
-    public static void printLongestWordTop100() throws IOException {
+    public static List<String> printLongestWordTop100() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        return words.stream()
+                .filter(word -> word.length() > NUMBER_TWELVE)
+                .sorted(Comparator.comparing(String::length).reversed())
+                .distinct()
+                .limit(NUMBER_HUNDRED)
+                .map(word -> word.toLowerCase())
+                .collect(Collectors.toList());
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
+        return numbers.stream()
+                .map(x -> NUMBER_TWO * x)
+                .collect(Collectors.toList());
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers.stream()
+                .reduce(NUMBER_ZERO, (x, y) -> x + y);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(number -> number > NUMBER_THREE)
+                .map(number -> number * NUMBER_TWO )
+                .reduce(NUMBER_ZERO, (x, y) -> x + y);
     }
+
 }
