2d31cf6aef2eac2977080c8c867df7b21cf5145e
enemfk777 <55019030+enemfk777@users.noreply.github.com>
2020-08-27 21:23:50 +0900

    1단계 - 자바8 스트림, 람다, Optional (#565)
    * feature 1. 람다 실습 1 - 익명 클래스를 람다로 전환
    
    * feature 2. 람다 실습 2 - 람다를 활용해 중복 제거
    
    * feature 3. 다양한 stream method 실습
                1. List에 담긴 모든 숫자 중 3보다 큰 숫자를 2배 한 후 모든 값의 합을 구한다. 지금까지 학습한 map, reduce, filter를 활용해 구현해야 한다.
                2. nextstep.fp.StreamStudy 클래스의 printLongestWordTop100() 메서드를 구현한다. 요구사항은 다음과 같다.
    
    * feature 4. 요구사항 1 - Optional을 활용해 조건에 따른 반환
    
    * feature 5. 요구사항 2 - Optional에서 값을 반환
    
    * feature 6. 요구사항 3 - Optional에서 exception 처리
    
    * 파일 마지막에 new line 추가
    
    Co-authored-by: ted.kang <ted.kang@kakaocorp.com>

---
+++ src/main/java/nextstep/README.md
@@ -0,0 +1,9 @@
+#Feature List
+1. 람다 실습 1 - 익명 클래스를 람다로 전환
+2. 람다 실습 2 - 람다를 활용해 중복 제거
+3. 다양한 stream method 실습
+    1. List에 담긴 모든 숫자 중 3보다 큰 숫자를 2배 한 후 모든 값의 합을 구한다. 지금까지 학습한 map, reduce, filter를 활용해 구현해야 한다.
+    2. nextstep.fp.StreamStudy 클래스의 printLongestWordTop100() 메서드를 구현한다. 요구사항은 다음과 같다.
+4. 요구사항 1 - Optional을 활용해 조건에 따른 반환
+5. 요구사항 2 - Optional에서 값을 반환
+6. 요구사항 3 - Optional에서 exception 처리
--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,7 +35,11 @@ public class User {
     }
 
     public static boolean ageIsInRange2(User user) {
-        return false;
+        return Optional.ofNullable(user)
+                .filter(user1 -> user1.getAge() != null)
+                .filter(user1 -> user1.getAge() >= 30)
+                .filter(user1 -> user1.getAge() <= 45)
+                .isPresent();
     }
 
     @Override
