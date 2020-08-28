f3ab80c04f4d721f170beea00bd05e7ce2258826
김다희 <sefide36@tmon.co.kr>
2020-08-28 11:35:14 +0900

    test: LadderResult 테스트코드 작성

--- src/main/java/ladder/domain/LadderResult.java
+++ src/main/java/ladder/domain/LadderResult.java
@@ -1,2 +1,33 @@
-package ladder.domain;public class LadderResult {
+package ladder.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class LadderResult {
+    private List<String> results;
+
+    public LadderResult(String ...inputs) {
+
+    }
+
+    public static LadderResult from(String input) {
+        return null;
+    }
+
+    public String getResultBy(int index) {
+        return "";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LadderResult that = (LadderResult) o;
+        return results.equals(that.results);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(results);
+    }
 }
--- src/test/java/ladder/domain/LadderResultTest.java
+++ src/test/java/ladder/domain/LadderResultTest.java
@@ -1,16 +1,50 @@
 package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-public class ResultTest {
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LadderResultTest {
 
     @DisplayName("실행 결과 입력값에 따른 생성 테스트")
     @ParameterizedTest
-    @ValueSource(strings = {""})
-    void from(String input) {
-        LadderResult
+    @MethodSource("makeLadderResultData")
+    void from(String input, String ...inputs) {
+        LadderResult ladderResult = LadderResult.from(input);
+
+        assertThat(ladderResult).isEqualTo(new LadderResult(inputs));
     }
+
+    private static Stream<Arguments> makeLadderResultData() {
+        return Stream.of(
+                Arguments.of("삼겹살,김밥,아이스크림", "삼겹살", "김밥", "아이스크림"),
+                Arguments.of("광, 꼴찌,꽝,1등, 2등", "광", "꼴찌", "꽝", "1등", "2등"),
+                Arguments.of("1000, 2000", "1000", "2000")
+        );
+    }
+
+    @DisplayName("인덱스 별 결과 반환 테스트")
+    @ParameterizedTest
+    @MethodSource("makeGetResultByData")
+    void getResultBy(String results, int index, String expectedResult) {
+        LadderResult ladderResult = LadderResult.from(results);
+
+        String actual = ladderResult.getResultBy(index);
+
+        assertThat(actual).isEqualTo(expectedResult);
+    }
+
+    private static Stream<Arguments> makeGetResultByData() {
+        return Stream.of(
+                Arguments.of("츄러스,마카롱,콘치즈", 1, "마카롱"),
+                Arguments.of("1,2,3,4,5,6", 7, null),
+                Arguments.of("프레첼,호떡,붕어빵,국화빵", 0, "프레첼")
+        );
+    }
+
 }
