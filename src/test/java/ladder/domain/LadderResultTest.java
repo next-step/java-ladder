4dcf7b80442a535806932b18cc5c947defa2ee0a
sefide <sefide@naver.com>
2020-08-28 12:12:18 +0900

    test: 실행결과 참가자 수에 맞지 않게 입력 시, 테스트코드 추가

--- src/test/java/ladder/domain/LadderResultTest.java
+++ src/test/java/ladder/domain/LadderResultTest.java
@@ -1,5 +1,6 @@
 package ladder.domain;
 
+import ladder.exception.LadderExceptionMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -8,31 +9,54 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LadderResultTest {
 
     @DisplayName("실행 결과 입력값에 따른 생성 테스트")
     @ParameterizedTest
     @MethodSource("makeLadderResultData")
-    void from(String input, String ...inputs) {
-        LadderResult ladderResult = LadderResult.from(input);
+    void from(String names, String input, String... inputs) {
+        Participants participants = Participants.of(names);
+        LadderResult ladderResult = LadderResult.of(participants, input);
 
         assertThat(ladderResult).isEqualTo(new LadderResult(inputs));
     }
 
     private static Stream<Arguments> makeLadderResultData() {
         return Stream.of(
-                Arguments.of("삼겹살,김밥,아이스크림", "삼겹살", "김밥", "아이스크림"),
-                Arguments.of("광, 꼴찌,꽝,1등, 2등", "광", "꼴찌", "꽝", "1등", "2등"),
-                Arguments.of("1000, 2000", "1000", "2000")
+                Arguments.of("p1,p2,p3", "삼겹살,김밥,아이스크림", "삼겹살", "김밥", "아이스크림"),
+                Arguments.of("p1,p2,p3,p4,p5", "광, 꼴찌,꽝,1등, 2등", "광", "꼴찌", "꽝", "1등", "2등"),
+                Arguments.of("p1,p2", "1000, 2000", "1000", "2000")
+        );
+    }
+
+    @DisplayName("실행 결과를 참가자 수와 다르게 입력한 경우 테스트")
+    @ParameterizedTest
+    @MethodSource("makeLadderResultInvalidData")
+    void from_shortage_ladder_result(String names, String input) {
+        Participants participants = Participants.of(names);
+        String exceptionMessage = LadderExceptionMessage.INVALID_LADDER_RESULT_COUNT;
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LadderResult.of(participants, input))
+                .withMessage(exceptionMessage);
+    }
+
+    private static Stream<Arguments> makeLadderResultInvalidData() {
+        return Stream.of(
+                Arguments.of("p1,p2", "1등,2등,3등,4등,5등"),
+                Arguments.of("p1,p2,p3,p4,p5,p6", "1등,2등,3등,4등"),
+                Arguments.of("p1", "")
         );
     }
 
     @DisplayName("인덱스 별 결과 반환 테스트")
     @ParameterizedTest
     @MethodSource("makeGetResultByData")
-    void getResultBy(String results, int index, String expectedResult) {
-        LadderResult ladderResult = LadderResult.from(results);
+    void getResultBy(String names, String results, int index, String expectedResult) {
+        Participants participants = Participants.of(names);
+        LadderResult ladderResult = LadderResult.of(participants, results);
 
         String actual = ladderResult.getResultBy(index);
 
@@ -41,9 +65,9 @@ public class LadderResultTest {
 
     private static Stream<Arguments> makeGetResultByData() {
         return Stream.of(
-                Arguments.of("츄러스,마카롱,콘치즈", 1, "마카롱"),
-                Arguments.of("1,2,3,4,5,6", 7, null),
-                Arguments.of("프레첼,호떡,붕어빵,국화빵", 0, "프레첼")
+                Arguments.of("p1,p2,p3", "츄러스,마카롱,콘치즈", 1, "마카롱"),
+                Arguments.of("p1,p2,p3,p4,p5,p6", "1,2,3,4,5,6", 7, null),
+                Arguments.of("p1,p2,p3,p4", "프레첼,호떡,붕어빵,국화빵", 0, "프레첼")
         );
     }
 
