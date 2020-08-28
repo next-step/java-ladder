c044ee329172ad8aa1f7d4ea4090eb49cf26a875
sefide <sefide@naver.com>
2020-08-28 15:10:31 +0900

    feat: 실행 결과 보고싶은 참가자 입력

--- src/main/java/ladder/LadderGameController.java
+++ src/main/java/ladder/LadderGameController.java
@@ -11,17 +11,20 @@ public class LadderGameController {
 
     public static void main(String[] args) {
         String names = InputView.scanParticipantNames();
-
         Participants participants = Participants.of(names);
 
         LadderResult ladderResult = LadderResult.of(participants, InputView.scanLadderResult());
 
         int ladderHeight = InputView.scanLadderHeight();
-
         Ladder ladder = Ladder.of(ladderHeight, participants, new LineRandomGenerator());
 
         OutputView.printLadder(participants, ladder, ladderResult);
 
         participants.calculateResult(ladder);
+
+        for (int i = 0; i < 3; i++) {
+            String wishParticipantName = InputView.scanWishParticipantName();
+
+        }
     }
 }
--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -6,6 +6,7 @@ public final class InputView {
     private static final String PARTICIPANTS_NAME_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String LADDER_HEIGHT_MESSAGE = "\n최대 사다리 높이는 몇 개인가요?";
     private static final String LADDER_RESULT_MESSAGE = "\n실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private static final String WISH_PARTICIPANT_RESULT_MESSAGE = "\n결과를 보고 싶은 사람은?";
     private static final Scanner scanner = new Scanner(System.in);
 
     private InputView() {
@@ -25,6 +26,12 @@ public final class InputView {
 
     public static String scanLadderResult() {
         System.out.println(LADDER_RESULT_MESSAGE);
+
+        return scanner.nextLine();
+    }
+
+    public static String scanWishParticipantName() {
+        System.out.println(WISH_PARTICIPANT_RESULT_MESSAGE);
 
         return scanner.nextLine();
     }
