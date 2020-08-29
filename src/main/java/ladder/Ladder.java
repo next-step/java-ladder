53382b8438c9c7c4f19dc456853616e13e456ceb
misudev <jungmisu801@gmail.com>
2020-08-29 16:13:50 +0900

    feat: 높이와 인원수에 맞게 사다리 구현하기

---
+++ src/main/java/ladder/Ladder.java
@@ -0,0 +1,56 @@
+package ladder;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class Ladder {
+    private static final int MIN_WIDTH = 1;
+    private static final int MIN_COUNT_OF_POINTS = 2;
+    private final List<Line> lines;
+
+    public Ladder(List<Line> lines) {
+        this.lines = lines;
+    }
+
+    public static Ladder of(int width, int countOfPoints, LadderGenerateStrategy ladderGenerateStrategy) {
+        validateLadder(width, countOfPoints);
+        List<Line> lines = new ArrayList<>();
+        int countOfStep = countOfPoints - 1;
+
+        for (int i = 0; i < width; i++) {
+            lines.add(new Line(countOfStep, ladderGenerateStrategy));
+        }
+
+        return new Ladder(lines);
+    }
+
+    public List<Line> getLines() {
+        return Collections.unmodifiableList(lines);
+    }
+
+    private static void validateLadder(int width, int countOfPoints) {
+        if (width < MIN_WIDTH) {
+            throw new IllegalArgumentException("사다리 높이는 " + MIN_WIDTH + "이상입니다.");
+        }
+
+        if (countOfPoints < MIN_COUNT_OF_POINTS) {
+            throw new IllegalArgumentException("사다리 게임 최소 유저수 " + MIN_COUNT_OF_POINTS + "이상입니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ladder ladder = (Ladder) o;
+        return Objects.equals(lines, ladder.lines);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lines);
+    }
+
+}
--- src/main/java/ladder/Line.java
+++ src/main/java/ladder/Line.java
@@ -2,6 +2,7 @@ package ladder;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.IntStream;
@@ -10,22 +11,22 @@ public class Line {
     private static final int MIN_INDEX = 0;
     private static final int STEP_INTERVAL = 1;
     private final int size;
-
-    private List<Boolean> steps = new ArrayList<>();
-
-    public Line(int size, MakingStepCondition condition) {
-        this.size = size;
-
-        for (int i = MIN_INDEX; i < size; i++) {
-            Boolean point = !hasBeforeStep(i) && condition.makeStep();
-            steps.add(point);
-        }
-    }
+    private final List<Boolean> steps;
 
     public Line(List<Boolean> steps) {
         this.steps = steps;
-        this.size = steps.size();
+        size = steps.size();
         validateLine();
+    }
+
+    public Line(int countOfStep, LadderGenerateStrategy generateStrategy) {
+        steps = new ArrayList<>();
+        size = countOfStep;
+
+        for (int i = MIN_INDEX; i < size; i++) {
+            Boolean step = !hasBeforeStep(i) && generateStrategy.generate();
+            steps.add(step);
+        }
     }
 
     private boolean hasBeforeStep(int pointIndex) {
@@ -34,19 +35,23 @@ public class Line {
 
     private void validateLine() {
         boolean hasInvalidStep = IntStream.range(MIN_INDEX, size)
-                .anyMatch(i -> steps.get(i) && hasBeforeStep(i));
+                                        .anyMatch(i -> steps.get(i) && hasBeforeStep(i));
 
         if (hasInvalidStep) {
             throw new IllegalArgumentException("인접한 사다리 다리가 있습니다.");
         }
     }
 
-    public boolean canSwapLocation(int left, int right) {
+    public boolean hasStepBetween(int left, int right) {
         if (right - left != STEP_INTERVAL) {
             return false;
         }
 
         return steps.get(left);
+    }
+
+    public List<Boolean> getSteps() {
+        return Collections.unmodifiableList(steps);
     }
 
     @Override
