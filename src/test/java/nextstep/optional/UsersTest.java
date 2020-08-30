8d2082b626a1370372a1d3b25269ce01588ee231
slamdunk7575 <slamdunk7575@gmail.com>
2020-08-31 03:46:59 +0900

    (refactor) 실습 2 - Optional에서 값을 반환

--- src/test/java/nextstep/optional/UsersTest.java
+++ src/test/java/nextstep/optional/UsersTest.java
@@ -1,5 +1,6 @@
 package nextstep.optional;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class UsersTest {
 
     @Test
+    @DisplayName("유저가 있는 경우 비교 테스트")
     public void getUser() {
         Users users = new Users();
         assertThat(users.getUser("crong")).isEqualTo(new User("crong", 35));
@@ -14,6 +16,7 @@ public class UsersTest {
 
 
     @Test
+    @DisplayName("유저가 없을 경우 기본 유저 정보 반환 테스트")
     public void getDefaultUser() {
         Users users = new Users();
         assertThat(users.getUser("codesquard")).isEqualTo(Users.DEFAULT_USER);
