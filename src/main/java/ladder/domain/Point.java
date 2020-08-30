0ef867d3dae6bacb45c9f77957993e8744dce04d
dhmin5693 <dhmin5693@naver.com>
2020-08-30 20:46:46 +0900

    Add: Line에서 point 사이를 움직이는 책임을 맡을 Direction 클래스 추가

--- src/main/java/ladder/domain/Point.java
+++ src/main/java/ladder/domain/Point.java
@@ -6,19 +6,27 @@ public enum Point {
     FALSE("     |");
 
     private final String line;
+    private final Direction direction;
 
     Point(String line) {
         this.line = line;
+        direction = Direction.of(this);
     }
 
     public static Point of(boolean pointType) {
+
         if (pointType) {
             return Point.TRUE;
         }
+
         return Point.FALSE;
     }
 
     public String lineString() {
         return line;
+    }
+
+    public int move() {
+        return direction.move();
     }
 }
