adacd9bedaf25b3ddc52d20017315b6b518042ab
slamdunk7575 <slamdunk7575@gmail.com>
2020-08-31 02:55:35 +0900

    (refactor) 실습 2 - 람다를 활용해 중복 제거

--- src/test/java/nextstep/fp/LambdaTest.java
+++ src/test/java/nextstep/fp/LambdaTest.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -17,33 +18,39 @@ public class LambdaTest {
     }
 
     @Test
+    @DisplayName("For문 이용한 전체 출력 ")
     public void printAllOld() throws Exception {
         Lambda.printAllOld(numbers);
     }
 
     @Test
+    @DisplayName("Lambda를 이용한 전체 출력 ")
     public void printAllLambda() throws Exception {
         Lambda.printAllLambda(numbers);
     }
 
     @Test
+    @DisplayName("Thread 이용한 출력")
     public void runThread() throws Exception {
         Lambda.runThread();
     }
 
     @Test
+    @DisplayName("전체 총 합을 구하는 테스트")
     public void sumAll() throws Exception {
         int sum = Lambda.sumAll(numbers);
         assertThat(sum).isEqualTo(21);
     }
 
     @Test
+    @DisplayName("짝수 총 합을 구하는 테스트")
     public void sumAllEven() throws Exception {
         int sum = Lambda.sumAllEven(numbers);
         assertThat(sum).isEqualTo(12);
     }
 
     @Test
+    @DisplayName("3이상 숫자 총 합을 구하는 테스트")
     public void sumAllOverThree() throws Exception {
         int sum = Lambda.sumAllOverThree(numbers);
         assertThat(sum).isEqualTo(15);
