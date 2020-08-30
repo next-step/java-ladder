cd3420578f49a2b78da5457b626fedd5d00f80cb
John <bass0223@naver.com>
2020-08-30 15:52:09 +0900

    말의 움직임을 위한 계산을 해주는 getDirectionFrom 메서드 추가

--- src/main/java/ladder/domain/line/Line.java
+++ src/main/java/ladder/domain/line/Line.java
@@ -3,18 +3,23 @@ package ladder.domain.line;
 import java.util.ArrayList;
 import java.util.List;
 
+import ladder.domain.playing.Direction;
 import ladder.domain.util.RandomUtils;
 
 public class Line {
 	private final List<Boolean> points;
 	private int currentIndex = -1;
 
-	private Line(int width) {
-		this.points = makePoints(width);
+	private Line(List<Boolean> points) {
+		this.points = points;
 	}
 
 	public static Line of(int countOfPerson) {
-		return new Line(countOfPerson);
+		return new Line(makePoints(countOfPerson));
+	}
+
+	public static Line of(List<Boolean> points) {
+		return new Line(points);
 	}
 
 	public int getWidth() {
@@ -29,7 +34,18 @@ public class Line {
 		return points.get(++currentIndex);
 	}
 
-	private List<Boolean> makePoints(int countOfPerson) {
+	public Direction getDirectionFrom(int width) {
+		if (width != points.size() && points.get(width)) {
+			return Direction.RIGHT;
+		}
+		if (width > 0 && points.get(width - 1)) {
+			return Direction.LEFT;
+		}
+		return Direction.STRAIGHT;
+	}
+
+
+	private static List<Boolean> makePoints(int countOfPerson) {
 		List<Boolean> result = initPoints(countOfPerson);
 
 		for (int i = 1; i < countOfPerson - 1; i++) {
@@ -40,18 +56,18 @@ public class Line {
 		return result;
 	}
 
-	private List<Boolean> initPoints(int count) {
+	private static List<Boolean> initPoints(int count) {
 		List<Boolean> points = new ArrayList<>(count);
 		points.add(0, RandomUtils.booleanValue());
 
 		return points;
 	}
 
-	private boolean makePoint(Boolean previousPoint) {
+	private static boolean makePoint(Boolean previousPoint) {
 		return previousPoint.equals(Boolean.FALSE) ? RandomUtils.booleanValue() : Boolean.FALSE;
 	}
 
-	private void addTrueIfItsAllFalse(List<Boolean> points) {
+	private static void addTrueIfItsAllFalse(List<Boolean> points) {
 		boolean allFalse = points.stream()
 								 .noneMatch(point -> point);
 		if (allFalse) {
