96b10840bc907f06f54114d30c1116c6091ab516
John <bass0223@naver.com>
2020-08-30 15:54:44 +0900

    게임을 컨트롤하는 LadderApplication 내용 변경

--- src/main/java/ladder/LadderApplication.java
+++ src/main/java/ladder/LadderApplication.java
@@ -1,11 +1,19 @@
 package ladder;
 
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+
 import ladder.domain.ladder.Ladder;
 import ladder.domain.ladder.LadderArgument;
 import ladder.domain.ladder.LadderFactory;
+import ladder.domain.ladder.Name;
 import ladder.domain.ladder.Names;
-import ladder.view.draw.DrawArgument;
+import ladder.domain.playing.LadderGame;
+import ladder.domain.playing.Winnings;
 import ladder.view.draw.DrawingExecutor;
+import ladder.view.draw.LadderDrawingArgument;
+import ladder.view.draw.ResultDrawingArgument;
 import ladder.view.input.InputView;
 
 public class LadderApplication {
@@ -14,10 +22,17 @@ public class LadderApplication {
 		Names names = Names.of(InputView.inputNames());
 		LadderArgument argument = LadderArgument.of(names, InputView.inputLadderHeight());
 		Ladder ladder = LadderFactory.create(argument);
+		Winnings winnings = Winnings.of(InputView.inputWinnings(), names.getSize());
 
-		DrawArgument drawArgument = DrawArgument.of(names, ladder);
-		DrawingExecutor.drawResult(drawArgument);
+		LadderDrawingArgument ladderDrawingArgument = LadderDrawingArgument.of(names, ladder, winnings);
+		DrawingExecutor.drawResult(ladderDrawingArgument);
 
-
+		List<Integer> resultNameIndexes = names.getIndexesOf(Name.of(InputView.inputResult()));
+		ResultDrawingArgument resultDrawingArgument =
+				ResultDrawingArgument.of(Winnings.of(resultNameIndexes.stream()
+																	  .map(i -> winnings.getWinningOf(LadderGame.play(i, ladder)))
+																	  .collect(toList())),
+										 names);
+		DrawingExecutor.drawWinningResult(resultDrawingArgument);
 	}
 }
