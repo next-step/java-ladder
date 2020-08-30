ee95d197ae1286f72dc634566f6c3fb8dc5ef65d
John <bass0223@naver.com>
2020-08-30 15:54:08 +0900

    사용자로부터 받는 입력값들 추가

--- src/main/java/ladder/view/input/InputView.java
+++ src/main/java/ladder/view/input/InputView.java
@@ -16,6 +16,18 @@ public class InputView {
 
 	public static int inputLadderHeight() {
 		System.out.println("최대 사다리 높이는 몇 개인가요?");
-		return SCANNER.nextInt();
+		int height = SCANNER.nextInt();
+		SCANNER.nextLine();
+		return height;
+	}
+
+	public static String inputWinnings() {
+		System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+		return SCANNER.nextLine();
+	}
+
+	public static String inputResult() {
+		System.out.println("결과를 보고 싶은 사람은?");
+		return SCANNER.nextLine();
 	}
 }
