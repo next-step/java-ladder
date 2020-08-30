99c1774f34bba80ebf3819ad761d68032843ce72
slamdunk7575 <slamdunk7575@gmail.com>
2020-08-31 03:38:04 +0900

    (refactor) 실습 1 - Optional을 활용해 조건에 따른 반환

--- src/test/java/nextstep/optional/UserTest.java
+++ src/test/java/nextstep/optional/UserTest.java
@@ -1,5 +1,6 @@
 package nextstep.optional;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static nextstep.optional.User.ageIsInRange1;
@@ -7,7 +8,9 @@ import static nextstep.optional.User.ageIsInRange2;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserTest {
+
     @Test
+    @DisplayName("User 나이가 30세 이상 45세 이하 인지 확인")
     public void whenFiltersWithoutOptional_thenCorrect() {
         assertThat(ageIsInRange1(new User("crong", 35))).isTrue();
         assertThat(ageIsInRange1(new User("crong", 48))).isFalse();
@@ -17,6 +20,7 @@ public class UserTest {
     }
 
     @Test
+    @DisplayName("User 나이가 30세 이상 45세 이하 인지 확인 (Optional 적용)")
     public void whenFiltersWithOptional_thenCorrect() {
         assertThat(ageIsInRange2(new User("crong", 35))).isTrue();
         assertThat(ageIsInRange2(new User("crong", 48))).isFalse();
