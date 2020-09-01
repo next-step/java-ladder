49142acbd85b9936528ad24ec2a7fce4d20cbe9f
great0417 <great0417@hanmail.net>
2020-09-01 23:43:10 +0900

    feat - stream을 사용한 3이상 의 합의 2배, 12자 이상의 긴 글자 내림차순으로 프린트 구현

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
+
+    private static final int WORD_TOP_LIMIT = 100;
+    private static final int LONG_WORD_CONDITION = 12;
 
     public static long countWords() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
@@ -26,8 +28,14 @@ public class StreamStudy {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
-
         // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(word -> word.length() > LONG_WORD_CONDITION)
+                .distinct()
+                .sorted((word1, word2) -> word2.length() - (word1.length()))
+                .limit(WORD_TOP_LIMIT)
+                .map(word -> word.toLowerCase())
+                .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +47,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(number -> number > 3)
+                .mapToLong(number -> number * 2)
+                .reduce(0, (x, y) -> x + y);
     }
 }
