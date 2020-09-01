e5f9251618a6bd863bdf28bd906e3dcff28fcb3e
jhPark <qkrwhdgh1007@gmail.com>
2020-09-02 01:20:59 +0900

    [test] LadderTest 작성

---
+++ src/test/java/ladder/domain/line/LadderTest.java
@@ -0,0 +1,23 @@
+package ladder.domain.line;
+
+import ladder.domain.player.Players;
+import ladder.domain.strategy.PointStrategy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LadderTest {
+
+    @DisplayName("Ladder 객체 생성 테스트")
+    @Test
+    void createLadderTest() {
+        Players players = Players.of("pobi,honux,crong,jk");
+        Height height = Height.of(5);
+        PointStrategy pointStrategy = () -> false;
+
+        Ladder ladder = Ladder.of(players, height, pointStrategy);
+
+        assertThat(ladder).isEqualTo(Ladder.of(players, height, pointStrategy));
+    }
+}
