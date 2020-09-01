1c96e06a907bc64b8f4ed503552d2ca38addd1d7
great0417 <great0417@hanmail.net>
2020-09-02 00:57:00 +0900

    refactor - MoveStrategy 타입변환 제거

--- src/test/java/nextstep/fp/CarTest.java
+++ src/test/java/nextstep/fp/CarTest.java
@@ -8,14 +8,14 @@ public class CarTest {
     @Test
     public void 이동() {
         Car car = new Car("pobi", 0);
-        Car actual = car.move((MoveStrategy)() -> true);
+        Car actual = car.move(() -> true);
         assertThat(actual).isEqualTo(new Car("pobi", 1));
     }
 
     @Test
     public void 정지() {
         Car car = new Car("pobi", 0);
-        Car actual = car.move((MoveStrategy)() -> false);
+        Car actual = car.move(() -> false);
         assertThat(actual).isEqualTo(new Car("pobi", 0));
     }
 }
