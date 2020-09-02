02e204ff266a42662c67b4b528528bfa1ddce228
ap-jylee <e200uk@gmail.com>
2020-09-03 04:42:11 +0900

    refactor: 리팩토링
    1. 임포트 제거
    2. 컨벤션

--- src/main/java/nextstep/domain/Person.java
+++ src/main/java/nextstep/domain/Person.java
@@ -5,7 +5,7 @@ public class Person {
     private final String name;
 
     public Person(String name) {
-        if(!validateName(name)) {
+        if (!validateName(name)) {
             throw new IllegalArgumentException("유효하지 않은 이름입니다.");
         }
         this.name = name;
