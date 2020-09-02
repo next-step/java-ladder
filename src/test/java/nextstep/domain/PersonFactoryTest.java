5cbfe8f94f062a3df82c0306d16f3b87a12a8594
ap-jylee <e200uk@gmail.com>
2020-09-03 05:05:21 +0900

    refactor: 중복 제거

--- src/test/java/nextstep/domain/PersonFactoryTest.java
+++ src/test/java/nextstep/domain/PersonFactoryTest.java
@@ -2,6 +2,8 @@ package nextstep.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
@@ -9,11 +11,10 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 public class PersonFactoryTest {
 
     @DisplayName("Person 생성")
-    @Test
-    public void createPerson() {
-        assertThatIllegalArgumentException().isThrownBy(() -> PersonFactory.createPerson(""));
-        assertThatIllegalArgumentException().isThrownBy(() -> PersonFactory.createPerson("  "));
-        assertThatIllegalArgumentException().isThrownBy(() -> PersonFactory.createPerson("이주영12345"));
+    @ParameterizedTest
+    @ValueSource(strings = { "", " ", "이주영12345" })
+    public void createPerson(String name) {
+        assertThatIllegalArgumentException().isThrownBy(() -> PersonFactory.createPerson(name));
     }
 
     @DisplayName("Persons 생성")
