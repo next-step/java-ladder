ee9a5ba08588d63a7d3a200eb985bfe72072e5ef
ap-jylee <e200uk@gmail.com>
2020-09-02 23:27:08 +0900

    refactor: Person 밸리데이션 리팩토링
    1. enum 생성하여 일괄 벨리데이션

--- src/main/java/nextstep/domain/Person.java
+++ src/main/java/nextstep/domain/Person.java
@@ -3,13 +3,7 @@ package nextstep.domain;
 public class Person {
 
     public static boolean validateName(String name) {
-        if(StringUtils.isNullOrEmpty(name)) {
-            return false;
-        }
-        if(name.length() > 5) {
-            return false;
-        }
-        return true;
+        return PersonValidator.validateAll(name);
     }
 
 }
