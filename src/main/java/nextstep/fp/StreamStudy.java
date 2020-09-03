fe07c1d912c6713fd4a39f4b273bab0f5f052ed2
Lee Hyeon Ah <47413225+hyeoni90@users.noreply.github.com>
2020-09-03 21:58:05 +0900

    Step1 - 자바8 스트림, 람다, Optional (#591)
    * refactor: stream 활용한 Lambda 리팩토링
    
    * refactor: Car 익명클래스에서 람다 적용
    
    * feat: 람다 filter를 위한 Conditional 함수형 인터페이스 추가
    
    * refactor: Conditional 인터페이스 적용 및 람다를 활용한 중복 제거
    
    * feat: Stream 실습
    
    * feat: Optional 활용을 활용해 조건에 따른 반환
    
    * refactor(Users): Optional에서 값을 반환
    
    * refactor(Expression): Optional에서 exception 처리
    
    * style: 코드 포맷팅 수정
    
    * feat: ComputerStore getVersionOptional

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,21 +5,22 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
+    private static final Long LONG_ONE = 0L;
 
     public static long countWords() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream()
+                .filter(w -> w.length() > 12)
+                .mapToLong(String::length)
+                .reduce(LONG_ONE, Long::sum);
     }
 
     public static void printLongestWordTop100() throws IOException {
@@ -27,18 +28,30 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(w -> w.length() > 12)
+                .sorted(Comparator.comparingLong(String::length).reversed())
+                .distinct()
+                .limit(100)
+                .map(String::toLowerCase)
+                .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
+        return numbers.stream()
+                .map(x -> 2 * x)
+                .collect(Collectors.toList());
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers.stream()
+                .reduce(0, Integer::sum);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(num -> num > 3)
+                .mapToLong(num -> num * 2)
+                .reduce(LONG_ONE, Long::sum);
     }
 }
