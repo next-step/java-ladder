7ea7e3a739c896285284226c0132e955af0f20d3
rftengineer <nightpw1@naver.com>
2020-09-05 01:29:46 +0900

    Refactor(step2) : PersonTest에 대해 DisplayName을 넣어 설명 추가

--- src/test/java/step2/model/PersonTest.java
+++ src/test/java/step2/model/PersonTest.java
@@ -1,5 +1,6 @@
 package step2.model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -12,6 +13,7 @@ public class PersonTest {
 
     @ParameterizedTest
     @CsvSource(value = {"Craig", "Zig", "Steve"})
+    @DisplayName("Person 객체 테스트 : 이름 입력은 정상적으로 되는가?")
     void create_Person_Object_With_Construct(String value) {
         person = new Person(value);
         assertThat(person.printName()).isEqualTo(value);
@@ -19,6 +21,7 @@ public class PersonTest {
 
     @ParameterizedTest
     @CsvSource(value = {"Kotlin", "Dmitri", "Collation"})
+    @DisplayName("Person 객체 예외 테스트 : 5자리 이상의 이름이 입력될 시, Exception 발생하는가?")
     void create_Person_With_Exception_Over_5_Characters(String value) {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> new Person(value)
