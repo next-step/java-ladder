f20982ea84e2b178804f6e66446cb5378f20d0e9
rftengineer <nightpw1@naver.com>
2020-09-05 01:47:35 +0900

    Refactor(step2) : RandomDrawStrategy Magic Number Refactoring

--- src/main/java/step2/strategy/RandomDrawStrategy.java
+++ src/main/java/step2/strategy/RandomDrawStrategy.java
@@ -4,9 +4,12 @@ import java.util.Random;
 
 public class RandomDrawStrategy implements DrawStrategy{
     private static final Random rand = new Random();
+    public static final int MAXIMUM_BOUND = 100;
+    public static final int RANDOM_CONDITION = 50;
+
     @Override
     public boolean draw() {
-        if(rand.nextInt(100) > 50) {
+        if(rand.nextInt(MAXIMUM_BOUND) > RANDOM_CONDITION) {
             return true;
         }
         return false;
