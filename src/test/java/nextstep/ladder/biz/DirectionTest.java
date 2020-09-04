ad16dd6dc6eb7bee5e1bf14fbff9c58000729a27
박찬석 <pcspapa@gmail.com>
2020-09-04 20:09:46 +0900

    feat: DirectionTest
    Test Class 복사 후 Product Class 만들어감.
    
    Closes #41

---
+++ src/test/java/nextstep/ladder/biz/DirectionTest.java
@@ -0,0 +1,58 @@
+package nextstep.ladder.biz;
+
+import org.junit.jupiter.api.Test;
+
+import static java.lang.Boolean.TRUE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class DirectionTest {
+  @Test
+  public void init() {
+    assertThat(Direction.of(true, false))
+            .isEqualTo(Direction.of(true, false));
+  }
+
+  @Test
+  public void init_invalid() {
+    assertThatThrownBy(() -> Direction.of(true, true))
+    .isInstanceOf(IllegalStateException.class);
+  }
+
+  @Test
+  public void next_random_true() {
+    Direction next = Direction.first(true).next();
+    assertThat(next).isEqualTo(Direction.of(true, false));
+  }
+
+  @Test
+  public void next_random_false() {
+    for (int i = 0; i < 100; i++) {
+      Direction.first(false).next();
+    }
+  }
+
+  @Test
+  public void next_true() {
+    Direction next = Direction.of(true, false).next(true);
+    assertThat(next).isEqualTo(Direction.of(false, true));
+  }
+
+  @Test
+  public void next_false() {
+    Direction next = Direction.of(false, true).next(false);
+    assertThat(next).isEqualTo(Direction.of(true, false));
+  }
+
+  @Test
+  public void first() {
+    Direction first = Direction.first(TRUE);
+    assertThat(first.isLeft()).isEqualTo(false);
+  }
+
+  @Test
+  public void last() {
+    Direction last = Direction.first(true).last();
+    assertThat(last).isEqualTo(Direction.of(true, false));
+  }
+}
