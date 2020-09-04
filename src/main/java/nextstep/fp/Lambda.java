a73130a30fbb804f81c3ea8cd53a1505ee788579
Hee Su Lee <hesue615@gmail.com>
2020-09-04 11:49:46 +0900

    Step1 스트림, 람다, Optional 학습 (#595)

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -27,30 +27,20 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAll(numbers, number -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, number -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, number -> number > 3);
+    }
+
+    private static int sumAll(List<Integer> numbers, Conditional condition) {
+        return numbers.stream()
+                .filter(condition::isValid)
+                .reduce(0, Integer::sum);
     }
 }
