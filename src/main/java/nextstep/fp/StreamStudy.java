89be0f86d43f50f328cbd0cb8db7e6ab9a9fc9fe
hJJANG <dacapolife87@gmail.com>
2020-09-05 02:04:55 +0900

    refactor : 1단계 피드백 내용 수정
    1단계 피드백 주신 내용 수정하여 반영하였습니다.

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -23,19 +23,29 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        words.stream().filter(word -> word.length() > 12).sorted().distinct().limit(100).forEach(word -> System.out.println(word.toLowerCase()));
+        words.stream()
+                .filter(word -> word.length() > 12)
+                .sorted()
+                .distinct()
+                .limit(100)
+                .forEach(word -> System.out.println(word.toLowerCase()));
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
+        return numbers.stream()
+                .map(x -> 2 * x)
+                .collect(Collectors.toList());
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers.stream()
+                .reduce(0, (x, y) -> x + y);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
 
-        return numbers.stream().filter(number -> number > 3).reduce(0, (x, y) -> x += 2 * y);
+        return numbers.stream()
+                .filter(number -> number > 3)
+                .reduce(0, (x, y) -> x += 2 * y);
     }
 }
