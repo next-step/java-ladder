953d99f57feb6a7108165283ea5d38ae1316a0f7
great0417 <great0417@hanmail.net>
2020-09-06 00:05:07 +0900

    feat - 사다리 출력 및 이름을 출력하는 객체 구현

---
+++ src/main/java/ladder/view/PrintResult.java
@@ -0,0 +1,51 @@
+package ladder.view;
+
+import ladder.domain.GamePerson;
+import ladder.domain.Line;
+import ladder.domain.Point;
+
+import java.util.List;
+
+public class PrintResult {
+
+    private static final int SEPERATE_DEFAULT = 5;
+
+    public static void makeLadder(List<Line> lines) {
+        lines.stream()
+                .forEach(line -> makeLine(line));
+    }
+
+    public static void makeLine(Line line) {
+        line.getPoints().stream()
+                .map(Point::isPrevPoint)
+                .map(PrintResult::getHorizontalOrNot)
+                .forEach(System.out::print);
+
+        System.out.println();
+    }
+
+    private static String getHorizontalOrNot(boolean check) {
+        if(check) {
+            return "-----|";
+        }
+        return "     |";
+    }
+
+    public static void printNames(List<GamePerson> names) {
+        names.stream()
+                .map(GamePerson::getName)
+                .map(PrintResult::printName)
+                .forEach(System.out::print);
+        System.out.println();
+    }
+
+    private static String printName(String name) {
+        StringBuilder printValue = new StringBuilder(" ");
+        for (int i = 0; i < SEPERATE_DEFAULT - name.length(); i++) {
+            printValue.append(" ");
+        }
+        printValue.append(name);
+
+        return printValue.toString();
+    }
+}
