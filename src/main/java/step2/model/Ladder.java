e31c01b4b0aa0cce5aa8d43dee9705384895613b
rftengineer <nightpw1@naver.com>
2020-09-05 15:26:57 +0900

    fix(step2) : Line의 일급 콜렉션인 Ladder 선언

---
+++ src/main/java/step2/model/Ladder.java
@@ -0,0 +1,22 @@
+package step2.model;
+
+import step2.strategy.RandomDrawStrategy;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Ladder {
+    private List<Line> ladder;
+
+    public Ladder(int height, int countOfPeople) {
+        ladder = Stream.generate(Line::new)
+                .limit(height)
+        .collect(Collectors.toList());
+        ladder.forEach(it -> it.drawNewLine(countOfPeople, new RandomDrawStrategy()));
+    }
+
+    public Line getLineOfHeight(int height) {
+        return ladder.get(height);
+    }
+}
---
+++ src/test/java/step2/model/LadderTest.java
@@ -0,0 +1,15 @@
+package step2.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LadderTest {
+    private Ladder ladder;
+
+    @Test
+    void create_Ladder_Collection() {
+        ladder = new Ladder(4,4);
+        assertThat(ladder.getLineOfHeight(0)).isNotNull();
+    }
+}
