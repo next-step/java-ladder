ae8784226b4abf8de51e7cd0aec3c1d2d0fab99a
great0417 <great0417@hanmail.net>
2020-09-06 00:00:46 +0900

    feat - step1 날린 내용 임시 추가

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -10,6 +10,9 @@ import java.util.stream.Collectors;
 
 public class StreamStudy {
 
+    private static final int WORD_TOP_LIMIT = 100;
+    private static final int LONG_WORD_CONDITION = 12;
+
     public static long countWords() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
@@ -26,12 +29,19 @@ public class StreamStudy {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
-
-        // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(word -> word.length() > LONG_WORD_CONDITION)
+                .distinct()
+                .sorted((word1, word2) -> word2.length() - (word1.length()))
+                .limit(WORD_TOP_LIMIT)
+                .map(String::toLowerCase)
+                .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
+        System.out.println();
         return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
+
     }
 
     public static long sumAll(List<Integer> numbers) {
