2d43eaa67e86ac00d27e76d388d6b8324473a19f
ap-jylee <e200uk@gmail.com>
2020-09-05 21:17:11 +0900

    test: Person 생성 테스트 추가

--- src/main/java/nextstep/domain/Person.java
+++ src/main/java/nextstep/domain/Person.java
@@ -1,5 +1,7 @@
 package nextstep.domain;
 
+import java.util.Objects;
+
 public class Person {
 
     private final String name;
@@ -19,4 +21,16 @@ public class Person {
         return PersonValidator.validateAll(name);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
--- src/test/java/nextstep/domain/PersonTest.java
+++ src/test/java/nextstep/domain/PersonTest.java
@@ -1,6 +1,7 @@
 package nextstep.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -8,6 +9,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("사람")
 public class PersonTest {
+
+    @DisplayName("생성")
+    @Test
+    public void create() {
+        Person expect = new Person("test");
+        Person actual = new Person("test");
+        assertThat(actual).isEqualTo(expect);
+    }
 
     @DisplayName("이름 밸리데이션 - 최대5글자")
     @ParameterizedTest
