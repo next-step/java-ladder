5232e474190c3ebc0854100b0f39bdd7bc1f4cbd
hJJANG <dacapolife87@gmail.com>
2020-09-07 00:11:46 +0900

    feat : 실행시 화면에 필요한 문구를 상수로 추가
    화면에 필요한 문구를 상수로 추가하였습니다.

--- src/main/java/nextstep/ladder/constant/PrintMessage.java
+++ src/main/java/nextstep/ladder/constant/PrintMessage.java
@@ -3,7 +3,10 @@ package nextstep.ladder.constant;
 public class PrintMessage {
 
     public static final String QUESTION_TEXT_JOIN_USERS = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    public static final String QUESTION_TEXT_RESULT_REWARD = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    public static final String QUESTION_TEXT_RESULT_TARGET = "결과를 보고 싶은 사람은?";
     public static final String QUESTION_TEXT_LADDER_HEIGHT = "최대 사다리 높이는 몇개 인가요?";
-    public static final String PRINT_TEXT_RESULT = "실행결과";
+    public static final String PRINT_TEXT_LADDER_RESULT = "사다리결과";
+    public static final String PRINT_TEXT_REWARD_RESULT = "실행결과";
 
 }
