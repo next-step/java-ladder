e09f1c66add2433b50fa923ed8016f566f9eca47
hJJANG <dacapolife87@gmail.com>
2020-09-07 00:24:49 +0900

    feat : 경품당첨조회 결과 화면추가
    경품당첨조회시 조회내용을 출력하는 기능을 추가하였습니다.

--- src/main/java/nextstep/ladder/view/ResultView.java
+++ src/main/java/nextstep/ladder/view/ResultView.java
@@ -1,14 +1,12 @@
 package nextstep.ladder.view;
 
-import nextstep.ladder.domain.Ladder;
-import nextstep.ladder.domain.Line;
-import nextstep.ladder.domain.User;
-import nextstep.ladder.domain.Users;
+import nextstep.ladder.domain.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static nextstep.ladder.constant.PrintMessage.PRINT_TEXT_RESULT;
+import static nextstep.ladder.constant.PrintMessage.PRINT_TEXT_LADDER_RESULT;
+import static nextstep.ladder.constant.PrintMessage.PRINT_TEXT_REWARD_RESULT;
 
 public class ResultView {
 
@@ -17,17 +15,32 @@ public class ResultView {
     private final String UNIT_EMPTY_BRIDGE_CHARACTOR = "     ";
     private final String NAME_FIXED_SIX_SPACE = "      ";
 
-    public void showLadderGameResult(Users users, Ladder ladder) {
-        System.out.println(PRINT_TEXT_RESULT);
+    public void showLadderResult(Users users, Ladder ladder, Rewards rewards) {
+        System.out.println(PRINT_TEXT_LADDER_RESULT);
         printUsers(users);
         printLadder(ladder);
+        printReward(rewards);
+    }
+
+    public void showLadderGameResult(String gameResult) {
+        System.out.println(PRINT_TEXT_REWARD_RESULT);
+        System.out.println(gameResult);
+    }
+
+    private void printReward(Rewards rewards) {
+        List<Reward> rewardList = rewards.getRewards();
+
+        String rewardNamesField = rewardList.stream()
+                .map(reward -> getFixedLengthName(reward.getName()))
+                .reduce("", (rewwardNames, rewardName) -> rewwardNames + rewardName);
+        System.out.println(rewardNamesField);
     }
 
     private void printUsers(Users users) {
         List<User> userList = users.getUsers();
 
         String userNamesField = userList.stream()
-                .map(user -> getFixedLengthUserName(user.getUserName()))
+                .map(user -> getFixedLengthName(user.getUserName()))
                 .reduce("", (userNames, userName) -> userNames + userName);
         System.out.println(userNamesField);
     }
@@ -46,11 +59,11 @@ public class ResultView {
         return isBridge ? UNIT_BRIDGE_CHARACTOR : UNIT_EMPTY_BRIDGE_CHARACTOR;
     }
 
-    private String getFixedLengthUserName(String userName) {
-        StringBuilder fixedLengthUserName = new StringBuilder();
-        String newPrefix = NAME_FIXED_SIX_SPACE.substring(userName.length());
-        fixedLengthUserName.append(userName).append(newPrefix);
+    private String getFixedLengthName(String name) {
+        StringBuilder fixedLengthName = new StringBuilder();
+        String newPrefix = NAME_FIXED_SIX_SPACE.substring(name.length());
+        fixedLengthName.append(name).append(newPrefix);
 
-        return fixedLengthUserName.toString();
+        return fixedLengthName.toString();
     }
 }
