fc6e641b41afb5797439de0d869b9f5ff7290f9e
misudev <jungmisu801@gmail.com>
2020-09-06 19:34:00 +0900

    feat: 실행결과 입력받기 구현

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -6,8 +6,10 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_NAMES_PHRASE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String INPUT_PRIZES_PHRASE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String LADDER_WIDTH_PHRASE = "최대 사다리 높이는 몇 개인가요?";
-    private static final String NAME_DELIMITER = ",";
+    private static final String RESULT_QUERY_PHRASE = "결과를 보고 싶은 사람은?";
+    private static final String DELIMITER_STRING = ",";
     private static final String BLANK_STRING = " ";
     private static final String EMPTY_STRING = "";
 
@@ -15,10 +17,7 @@ public class InputView {
 
     public List<String> getUserNames() {
         System.out.println(INPUT_NAMES_PHRASE);
-        String userNames = scanner.nextLine();
-
-        return Arrays.asList(userNames.replaceAll(BLANK_STRING, EMPTY_STRING)
-                                      .split(NAME_DELIMITER));
+        return getElements();
     }
 
     public int getWidth() {
@@ -26,6 +25,23 @@ public class InputView {
         String width = scanner.nextLine();
 
         return Integer.parseInt(width);
+    }
+
+    public List<String> getPrizeNames() {
+        System.out.println(INPUT_PRIZES_PHRASE);
+        return getElements();
+    }
+
+    public String getResultUserName() {
+        System.out.println();
+        System.out.println(RESULT_QUERY_PHRASE);
+        return scanner.nextLine();
+    }
+
+    private List<String> getElements() {
+        String elements = scanner.nextLine();
+        return Arrays.asList(elements.replaceAll(BLANK_STRING, EMPTY_STRING)
+                .split(DELIMITER_STRING));
     }
 
 }
