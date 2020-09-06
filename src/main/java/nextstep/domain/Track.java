b3b8cc2cf88a82eaa753d48def645193c05dbdd3
ap-jylee <e200uk@gmail.com>
2020-09-06 23:59:45 +0900

    feact: track 객체 생성
    1. 기존의 Line 객체는 횡단선으로 사용하고 이 객체를 트랙으로 사용한다.

---
+++ src/main/java/nextstep/domain/Track.java
@@ -0,0 +1,26 @@
+package nextstep.domain;
+
+import java.util.Objects;
+
+public class Track {
+
+    private final int trackNumber;
+
+    public Track(int trackNumber) {
+        this.trackNumber = trackNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Track track = (Track) o;
+        return trackNumber == track.trackNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(trackNumber);
+    }
+
+}
