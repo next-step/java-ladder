230c77c9bacf85f7a1852b5a7325e5638a078011
misudev <jungmisu801@gmail.com>
2020-09-06 19:34:58 +0900

    feat: 실행결과 출력하기 구현

--- src/main/java/ladder/LadderApplication.java
+++ src/main/java/ladder/LadderApplication.java
@@ -1,6 +1,8 @@
 package ladder;
 
 import ladder.model.LadderGame;
+import ladder.model.LadderResult;
+import ladder.model.Prizes;
 import ladder.model.RandomGenerateStrategy;
 import ladder.view.InputView;
 import ladder.view.ResultView;
@@ -8,20 +10,35 @@ import ladder.view.ResultView;
 import java.util.List;
 
 public class LadderApplication {
+    private static final String ALL_USER_QUERY_STRING = "all";
 
     public static void main(String[] args) {
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
 
         List<String> userNames = inputView.getUserNames();
+        List<String> prizeNames = inputView.getPrizeNames();
         int width = inputView.getWidth();
 
         LadderGame ladderGame = LadderGame.of(userNames, width, new RandomGenerateStrategy());
+        Prizes prizes = Prizes.of(prizeNames);
 
-        resultView.printResultPhrase();
-        resultView.printUserNames(ladderGame.getUsersName());
+        resultView.printLadderResultPhrase();
+        resultView.printLadderElements(ladderGame.getUsersName());
         resultView.printLadder(ladderGame.getLines());
 
+        ladderGame.play();
+        LadderResult ladderResult = ladderGame.makeLadderResult(prizes);
+
+        resultView.printLadderElements(prizes.getPrizeNames());
+        String resultUserName = inputView.getResultUserName();
+
+        if (ALL_USER_QUERY_STRING.equals(resultUserName)) {
+            resultView.printAllResults(userNames, ladderResult);
+            return;
+        }
+
+        resultView.printResult(resultUserName, ladderResult);
     }
 
 }
--- src/main/java/ladder/view/ResultView.java
+++ src/main/java/ladder/view/ResultView.java
@@ -1,28 +1,36 @@
 package ladder.view;
 
+import ladder.model.LadderResult;
 import ladder.model.Line;
+import ladder.model.User;
 
 import java.util.List;
 
 public class ResultView {
+    private static final String LADDER_PHRASE = "사다리 결과";
     private static final String RESUlT_PHRASE = "실행결과";
     private static final String NAME_FORMAT = "%6s";
     private static final String LADDER_FRONT_STRING = "   ";
     private static final String EMPTY_STEP_STRING = "     ";
     private static final String STEP_STRING = "-----";
     private static final String POINT_STRING = "|";
+    private static final String RESULT_QUERY_FORMAT = "%s : %s";
 
+
+    public void printLadderResultPhrase() {
+        System.out.println(LADDER_PHRASE);
+    }
 
     public void printResultPhrase() {
         System.out.println(RESUlT_PHRASE);
     }
 
-    public void printUserNames(List<String> userNames) {
-        userNames.forEach(this::printUserName);
+    public void printLadderElements(List<String> elements) {
+        elements.forEach(this::printElement);
         System.out.println();
     }
 
-    private void printUserName(String name) {
+    private void printElement(String name) {
         System.out.printf(NAME_FORMAT, name);
     }
 
@@ -45,6 +53,15 @@ public class ResultView {
 
     private String getStepString(boolean hasStep) {
         return hasStep ? STEP_STRING : EMPTY_STEP_STRING;
+    }
+
+    public void printResult(String target, LadderResult ladderResult) {
+        String prize = ladderResult.getPrizeByUser(new User(target)).getPrize();
+        System.out.println(String.format(RESULT_QUERY_FORMAT, target, prize));
+    }
+
+    public void printAllResults(List<String> userNames, LadderResult ladderResult) {
+        userNames.forEach(userName -> printResult(userName, ladderResult));
     }
 
 }
