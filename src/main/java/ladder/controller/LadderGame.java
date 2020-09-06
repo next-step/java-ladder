d018f8c70c6c4a7ed82c5208baf3f0d431017d93
jhPark <qkrwhdgh1007@gmail.com>
2020-09-06 23:34:48 +0900

    [feat] view, LadderGame 수정

--- src/main/java/ladder/controller/LadderGame.java
+++ src/main/java/ladder/controller/LadderGame.java
@@ -3,6 +3,8 @@ package ladder.controller;
 import ladder.domain.line.Height;
 import ladder.domain.line.Ladder;
 import ladder.domain.player.Players;
+import ladder.domain.result.LadderResults;
+import ladder.domain.reward.Rewards;
 import ladder.domain.strategy.RandomPointStrategy;
 
 import static ladder.view.InputView.*;
@@ -11,9 +13,16 @@ import static ladder.view.ResultView.*;
 public class LadderGame {
     public void start() {
         Players players = inputPlayers();
+        Rewards rewards = inputReward(players.size());
         Height height = inputHeight();
 
         Ladder ladder = Ladder.of(players, height, new RandomPointStrategy());
         printLadderResult(players, ladder);
+        printRewards(rewards);
+
+        LadderResults ladderResults = LadderResults.of(players, rewards, ladder);
+
+        String playerName = inputResultPlayerName();
+        printResults(playerName, ladderResults);
     }
 }
--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -2,12 +2,15 @@ package ladder.view;
 
 import ladder.domain.line.Height;
 import ladder.domain.player.Players;
+import ladder.domain.reward.Rewards;
 
 import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_PLAYERS_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String INPUT_RESULT_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요";
     private static final String INPUT_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    private static final String INPUT_RESULT_PLAYER_MESSAGE = "결과를 보고 싶은 사람은?";
     private static final Scanner scanner = new Scanner(System.in);
 
     private InputView() {}
@@ -20,5 +23,15 @@ public class InputView {
     public static Height inputHeight() {
         System.out.println(INPUT_HEIGHT_MESSAGE);
         return Height.of(scanner.nextInt());
+    }
+
+    public static Rewards inputReward(int size) {
+        System.out.println(INPUT_RESULT_MESSAGE);
+        return Rewards.ofPrizes(scanner.nextLine(), size);
+    }
+
+    public static String inputResultPlayerName() {
+        System.out.println(INPUT_RESULT_PLAYER_MESSAGE);
+        return scanner.next();
     }
 }
