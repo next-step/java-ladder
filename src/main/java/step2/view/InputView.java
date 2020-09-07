b2b9e1b97a9ea8ef08484b8e5c64f95ecc936c12
rftengineer <nightpw1@naver.com>
2020-09-07 20:59:11 +0900

    feat(step3) : InputView에 실행결과를 묻는 static Method 추가

--- src/main/java/step2/view/InputView.java
+++ src/main/java/step2/view/InputView.java
@@ -10,6 +10,11 @@ public class InputView {
         return scanner.nextLine().split(",");
     }
 
+    public static String[] askResultForLadderGame() {
+        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        return scanner.nextLine().split(",");
+    }
+
     public static int askHeightForLadderGame() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         return scanner.nextInt();
