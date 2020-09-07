200ae963448d1a7e652d32a6e2afe5d5e8924f5f
great0417 <great0417@hanmail.net>
2020-09-08 04:34:08 +0900

    refactor - 생성자는 생성자의 기능만 하게 변경

--- src/main/java/ladder/controller/Main.java
+++ src/main/java/ladder/controller/Main.java
@@ -1,16 +1,14 @@
 package ladder.controller;
 
 import ladder.domain.LadderGame;
-import ladder.domain.Line;
 import ladder.util.InputUtil;
-import ladder.view.PrintResult;
 
 public class Main {
     public static void main(String[] args) {
 
         String names = InputUtil.returnString("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         int ladderHeight = InputUtil.returnInteger("최대 사다리 높이는 몇 개인가요?");
-        LadderGame ladderGame = new LadderGame(names, ladderHeight);
+        LadderGame ladderGame = LadderGame.of(names, ladderHeight);
         ladderGame.game();
     }
 }
