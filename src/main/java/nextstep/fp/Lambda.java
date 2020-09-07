098df4415804246076a21a040b03ce5c734ab2f0
kimdaeyong <dev.kimdaeyong@gmail.com>
2020-09-07 10:37:12 +0900

    refactor: pass condition to sumAll

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
@@ -26,31 +27,21 @@ public class Lambda {
         }).start();
     }
 
+    public static int sumAll(List<Integer> numbers, Predicate<Integer> condition) {
+        return numbers.stream()
+            .filter(condition)
+            .reduce(0, (acc, cur) -> acc += cur);
+    }
+
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAll(numbers, v -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, v -> v % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, v -> v > 3);
     }
 }
