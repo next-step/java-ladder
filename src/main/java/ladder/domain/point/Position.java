3daa3bce348f0f2ba102527f20d62bfac0caeaad
jhPark <qkrwhdgh1007@gmail.com>
2020-09-08 19:33:06 +0900

    [feat] Position class, Direction class 추가

---
+++ src/main/java/ladder/domain/point/Position.java
@@ -0,0 +1,47 @@
+package ladder.domain.point;
+
+import java.util.Objects;
+
+public class Position {
+    private static final int START_POSITION = 0;
+    private static final int MOVE_SIZE = 1;
+
+    private final int movement;
+
+    private Position(int movement) {
+        this.movement = movement;
+    }
+
+    public static Position of(int movement) {
+        return new Position(movement);
+    }
+
+    public static Position init() {
+        return new Position(START_POSITION);
+    }
+
+    public int location() {
+        return movement;
+    }
+
+    Position left() {
+        return new Position(movement - MOVE_SIZE);
+    }
+
+    Position right() {
+        return new Position(movement + MOVE_SIZE);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position that = (Position) o;
+        return movement == that.movement;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(movement);
+    }
+}
