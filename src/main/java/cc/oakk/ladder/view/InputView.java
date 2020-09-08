702235d4a7e65134e653d20e8457f5e15a6cce7a
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-09-08 21:37:23 +0900

    refactor: use Printer<String> in Input, ResultView

--- src/main/java/cc/oakk/ladder/view/InputView.java
+++ src/main/java/cc/oakk/ladder/view/InputView.java
@@ -1,28 +1,36 @@
 package cc.oakk.ladder.view;
 
+import cc.oakk.ladder.view.printer.Printer;
+import cc.oakk.ladder.view.printer.impl.StandardPrinter;
+
 import java.util.Scanner;
 
 public class InputView {
     public static final String PERSON_DELIMITER = ",";
+    public static final String LINE_BREAK = System.lineSeparator();
+
     private final Scanner scanner;
+    private final Printer<String> stringPrinter;
 
     public InputView() {
+        this.stringPrinter = StandardPrinter.getInstance();
         this.scanner = new Scanner(System.in);
     }
 
-    public InputView(Scanner scanner) {
+    public InputView(Scanner scanner, Printer<String> stringPrinter) {
         this.scanner = scanner;
+        this.stringPrinter = stringPrinter;
     }
 
     public String[] getPersonNames() {
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        stringPrinter.print("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)" + LINE_BREAK);
         String line = scanner.nextLine();
         return line.split(PERSON_DELIMITER);
     }
 
     public int getLadderHeight() {
-        System.out.println();
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        stringPrinter.print(LINE_BREAK);
+        stringPrinter.print("최대 사다리 높이는 몇 개인가요?" + LINE_BREAK);
 
         int height = scanner.nextInt();
         scanner.nextLine();
@@ -30,6 +38,6 @@ public class InputView {
     }
 
     public void printError(Throwable throwable) {
-        System.err.println("오류발생: " + throwable.getMessage() + "\n");
+        stringPrinter.print("오류발생: " + throwable.getMessage() + LINE_BREAK);
     }
 }
--- src/main/java/cc/oakk/ladder/view/ResultView.java
+++ src/main/java/cc/oakk/ladder/view/ResultView.java
@@ -4,23 +4,34 @@ import cc.oakk.ladder.model.Ladder;
 import cc.oakk.ladder.model.Person;
 import cc.oakk.ladder.model.line.Line;
 import cc.oakk.ladder.view.printer.Printer;
+import cc.oakk.ladder.view.printer.impl.StandardPrinter;
 
 import java.util.List;
 
 public class ResultView {
+    public static final String LINE_BREAK = System.lineSeparator();
+
     private final Printer<Person> personPrinter;
     private final Printer<Line> linePrinter;
+    private final Printer<String> stringPrinter;
 
     public ResultView(Printer<Person> personPrinter, Printer<Line> linePrinter) {
         this.personPrinter = personPrinter;
         this.linePrinter = linePrinter;
+        this.stringPrinter = StandardPrinter.getInstance();
+    }
+
+    public ResultView(Printer<Person> personPrinter, Printer<Line> linePrinter, Printer<String> stringPrinter) {
+        this.personPrinter = personPrinter;
+        this.linePrinter = linePrinter;
+        this.stringPrinter = stringPrinter;
     }
 
     public void printLadder(List<Person> persons, Ladder ladder) {
-        System.out.println("실행결과");
-        System.out.println();
+        stringPrinter.print("실행결과" + LINE_BREAK);
+        stringPrinter.print(LINE_BREAK);
         persons.forEach(personPrinter::print);
-        System.out.println();
+        stringPrinter.print(LINE_BREAK);
         ladder.getLines().forEach(linePrinter::print);
     }
 }
