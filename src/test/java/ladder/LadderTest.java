2c66ab9385b643b6c08050fe1d432ea4446cd59f
dhmin5693 <dhmin5693@naver.com>
2020-09-09 21:18:02 +0900

    [Add] DirectionTest 추가 및 LadderTest 내용 변경

--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -1,11 +1,30 @@
 package ladder;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import ladder.domain.Ladder;
+import ladder.domain.LadderLine;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class LadderTest {
 
-    @Test
-    public void ladderTest() {
+    @DisplayName("Ladder 생성 테스트")
+    @ParameterizedTest
+    @CsvSource(value = "5,5", delimiter = ',')
+    public void ladderTest(int playerCount, int height) {
 
+        Ladder ladder = new Ladder(playerCount, height);
+
+        assertNotNull(ladder.getLadderLine());
+
+        assertThat(ladder.getLadderLine())
+            .isInstanceOf(LadderLine.class);
+
+        assertEquals(ladder.getLadderLinesString().size(), height);
     }
 }
