bf7555f7a768f0774bc6500df4c3a6dfd06f7612
taehee9 <dhstoalfh9509@gmail.com>
2020-09-10 01:23:35 +0900

    feat: 출력 구현

---
+++ src/main/java/ladder/view/ResultView.java
@@ -0,0 +1,45 @@
+package ladder.view;
+
+import ladder.domain.Ladder;
+import ladder.domain.Line;
+import ladder.domain.User;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ResultView {
+    private final static String NAME_FORMAT = "%5s";
+    private final static String LADDER_HEIGHT = "|";
+    private final static String LADDER_LINE = "-----";
+    private final static String LADDER_EMPTY = "     ";
+
+    public static void resultLadder(List<User> users, Ladder ladder) {
+        System.out.println("실행결과");
+        System.out.println();
+
+        printUserName(users);
+        System.out.println();
+        printLadder(ladder);
+    }
+
+    private static void printUserName(List<User> users) {
+        users.stream()
+                .map(user -> user.getUserName())
+                .forEach(name ->
+                System.out.print(String.format(NAME_FORMAT, name) + " "));
+    }
+
+    private static void printLadder(Ladder ladder) {
+        ladder.getLadder()
+                .stream()
+                .map(ResultView::printLine)
+                .forEach(System.out::println);
+    }
+
+    private static String printLine(Line line){
+        return line.getLine()
+                .stream()
+                .map(isLine -> isLine ? LADDER_LINE : LADDER_EMPTY)
+                .collect(Collectors.joining(LADDER_HEIGHT, "", LADDER_HEIGHT));
+    }
+}
