717e806a70c666041374fea953ce1a3e53dc7413
taehee9 <dhstoalfh9509@gmail.com>
2020-09-10 01:25:18 +0900

    feat: 사다리 구현

---
+++ src/main/java/ladder/domain/Ladder.java
@@ -0,0 +1,25 @@
+package ladder.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Ladder {
+    private List<Line> lines;
+
+    public Ladder(int userCount, Height height) {
+        this.lines = generateLines(userCount, height);
+    }
+
+    private List<Line> generateLines(int userCount, Height height) {
+        List<Line> lineList =  IntStream.rangeClosed(Height.MIN_HEIGHT, height.getHeight())
+                                    .mapToObj(count -> new Line(userCount))
+                                    .collect(Collectors.toList());
+        return Collections.unmodifiableList(lineList);
+    }
+
+    public List<Line> getLadder(){
+        return lines;
+    }
+}
