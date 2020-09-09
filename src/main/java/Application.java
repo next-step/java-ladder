301c0348dc920ed885c0f17f6ed27aceb073c61e
Zorba's Company <kulkulzaza@gmail.com>
2020-09-09 18:19:46 +0900

    implement : init program -User Inputs

---
+++ src/main/java/Application.java
@@ -0,0 +1,11 @@
+import util.UserInput;
+
+public class Application {
+    public static void main(String[] args) {
+        String names[] = UserInput.userNameInput();
+        int height = UserInput.inputHeight();
+        UserInput.showResult();
+
+
+    }
+}
