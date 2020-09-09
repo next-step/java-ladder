b1e9f5f2834ce07c6b4d3fe2caac250ee0888295
LimHanGyeol <dlagksruf19@naver.com>
2020-09-10 02:51:29 +0900

    refactor : LadderLines.position 지역변수로 매핑

--- src/main/java/nextstep/ladder/domain/line/LadderLines.java
+++ src/main/java/nextstep/ladder/domain/line/LadderLines.java
@@ -16,23 +16,24 @@ public class LadderLines {
     public Map<User, Integer> goDownALadder(Map<User, Integer> ladderResult) {
         for (User user : ladderResult.keySet()) {
             int position = ladderResult.get(user);
-            position = caculatePosition(ladders, position);
+            position = caculatePosition(position);
             ladderResult.put(user, position);
         }
         return ladderResult;
     }
 
-    private int caculatePosition(List<LadderLine> ladders, int position) {
-        for (int i = 0; i < ladders.size(); i++) {
-            LadderLine ladderLine = ladders.get(i);
-            position = ladderLine.ladderPlayCondition(position);
+    private int caculatePosition(int position) {
+        int finalPosition = position;
+        for (LadderLine ladderLine : ladders) {
+            finalPosition = ladderLine.ladderPlayCondition(finalPosition);
         }
-        return position;
+        return finalPosition;
     }
 
     public LadderLine getLine(int index) {
         return ladders.get(index);
     }
+
 
     public int size() {
         return ladders.size();
