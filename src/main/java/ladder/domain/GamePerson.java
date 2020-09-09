c20ff10e669b8fea1f7132fe370ec51bdbd6cf8c
great0417 <great0417@hanmail.net>
2020-09-10 00:53:32 +0900

    ignore - rebase가 안되서 다시 생성

---
+++ src/main/java/ladder/domain/GamePerson.java
@@ -0,0 +1,38 @@
+package ladder.domain;
+
+import java.util.Objects;
+
+public class GamePerson {
+
+    public static final int NAME_LENGTH = 5;
+
+    private String name;
+
+    public GamePerson(String name) {
+        validationName(name);
+        this.name = name;
+    }
+
+    private void validationName(String name) {
+        if (name.length() > NAME_LENGTH) {
+            throw new IllegalArgumentException("사람 이름은 최대 5자까지 등록 가능합니다.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GamePerson name1 = (GamePerson) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
