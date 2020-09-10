f569ba02ed20b893519a8979fae347a2c4193768
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-09-11 01:21:59 +0900

    feat(controller): modify controller.

--- src/main/java/cc/oakk/ladder/LadderApplication.java
+++ src/main/java/cc/oakk/ladder/LadderApplication.java
@@ -3,20 +3,28 @@ package cc.oakk.ladder;
 import cc.oakk.ladder.controller.LadderGame;
 import cc.oakk.ladder.model.person.Person;
 import cc.oakk.ladder.model.line.dto.LineDto;
+import cc.oakk.ladder.model.person.Persons;
+import cc.oakk.ladder.model.result.Result;
+import cc.oakk.ladder.model.result.Results;
 import cc.oakk.ladder.view.InputView;
+import cc.oakk.ladder.view.QueryView;
 import cc.oakk.ladder.view.ResultView;
 import cc.oakk.ladder.view.printer.Printer;
-import cc.oakk.ladder.view.printer.impl.LinePrinter;
-import cc.oakk.ladder.view.printer.impl.PersonPrinter;
+import cc.oakk.ladder.view.printer.impl.*;
 
 public class LadderApplication {
     public static void main(String[] args) {
         Printer<LineDto> linePrinter = new LinePrinter();
+        Printer<Persons> personsPrinter = new PersonsPrinter();
+        Printer<Results> resultsPrinter = new ResultsPrinter();
+
         Printer<Person> personPrinter = new PersonPrinter();
-        
+        Printer<Result> resultPrinter = new ResultPrinter();
+
         InputView inputView = new InputView();
-        ResultView resultView = new ResultView(personPrinter, linePrinter);
-        LadderGame game = new LadderGame(inputView, resultView);
+        ResultView resultView = new ResultView(personsPrinter, linePrinter, resultsPrinter);
+        QueryView queryView = new QueryView(personPrinter, resultPrinter);
+        LadderGame game = new LadderGame(inputView, resultView, queryView);
         game.start();
     }
 }
--- src/main/java/cc/oakk/ladder/controller/LadderGame.java
+++ src/main/java/cc/oakk/ladder/controller/LadderGame.java
@@ -3,30 +3,38 @@ package cc.oakk.ladder.controller;
 import cc.oakk.ladder.model.ladder.Ladder;
 import cc.oakk.ladder.model.person.Person;
 import cc.oakk.ladder.model.person.Persons;
+import cc.oakk.ladder.model.result.Result;
+import cc.oakk.ladder.model.result.Results;
 import cc.oakk.ladder.model.size.LadderHeight;
+import cc.oakk.ladder.model.trace.Trace;
+import cc.oakk.ladder.model.trace.dto.TraceResultsDto;
 import cc.oakk.ladder.util.RandomUtils;
-import cc.oakk.ladder.util.ValidationUtils;
 import cc.oakk.ladder.view.InputView;
+import cc.oakk.ladder.view.QueryView;
 import cc.oakk.ladder.view.ResultView;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static cc.oakk.ladder.util.ValidationUtils.tryUntilSuccess;
 
 public class LadderGame {
+    public static final Person ALL = new Person("all");
+
     private final InputView inputView;
     private final ResultView resultView;
+    private final QueryView queryView;
 
-    public LadderGame(InputView inputView, ResultView resultView) {
+    public LadderGame(InputView inputView, ResultView resultView, QueryView queryView) {
         this.inputView = inputView;
         this.resultView = resultView;
+        this.queryView = queryView;
     }
 
     public void start() {
         Persons persons = getPersons();
+        Results results = getResults(persons);
         LadderHeight height = getHeight();
 
         Ladder ladder = new Ladder(persons.size(), height.get());
@@ -35,11 +43,38 @@ public class LadderGame {
         resultView.printHeader();
         resultView.printPersons(persons);
         resultView.printLadder(ladder.getDto());
+        resultView.printResults(results);
+
+        query(ladder.traceAll().getDto(), persons, results);
+    }
+
+    private void query(TraceResultsDto traceResults, Persons persons, Results results) {
+        Person wantedPerson = tryUntilSuccess(() -> new Person(queryView.getWantedPerson()), queryView::printError);
+        if (ALL.equals(wantedPerson)) {
+            queryView.printTraceResults(traceResults, persons, results);
+            return;
+        }
+
+        int indexOfPerson = persons.find(wantedPerson);
+        if (indexOfPerson == Persons.NOT_FOUND) {
+            queryView.printError(new IllegalArgumentException("존재하지 않는 사람입니다."));
+            query(traceResults, persons, results);
+            return;
+        }
+        int indexOfResults = traceResults.getTraceResult(Trace.of(indexOfPerson)).get();
+        queryView.printResult(results.get(indexOfResults));
+        query(traceResults, persons, results);
     }
 
     private LadderHeight getHeight() {
         return tryUntilSuccess(() -> new LadderHeight(inputView.getLadderHeight()),
                 inputView::printError);
+    }
+
+    private Results getResults(Persons persons) {
+        return tryUntilSuccess(() -> new Results(Arrays.stream(inputView.getResultNames(persons.size()))
+                .map(Result::new)
+                .collect(Collectors.toList())), inputView::printError);
     }
 
     private Persons getPersons() {
@@ -49,11 +84,9 @@ public class LadderGame {
     }
 
     private void initLadder(Ladder ladder) {
-        ladder.initLines(width -> {
-            int even = RandomUtils.nextInt(2);
-            return IntStream.range(0, width - 1)
-                    .filter(i -> i % 2 == even && RandomUtils.nextBoolean())
-                    .toArray();
-        });
+        ladder.initLines(width -> IntStream.iterate(RandomUtils.nextInt(2), i -> i + 2)
+                .limit(width / 2)
+                .filter(i -> RandomUtils.nextBoolean())
+                .toArray());
     }
 }
