7e6e7f7403db3486408e05dc40f23be3aa961289
jhPark <qkrwhdgh1007@gmail.com>
2020-09-11 12:34:39 +0900

    [refactor] Direction enum 으로 변경

--- src/main/java/ladder/domain/point/Direction.java
+++ src/main/java/ladder/domain/point/Direction.java
@@ -1,44 +1,28 @@
 package ladder.domain.point;
 
-import ladder.domain.strategy.PointStrategy;
-
-import java.util.Objects;
-
-import static java.lang.Boolean.FALSE;
-
-public class Direction {
-    private static final String NOT_CORRECT_DIRECTION_MESSAGE = "방향이 올바르지 않습니다.";
+public enum Direction {
+    LEFT(true, false),
+    RIGHT(false, true),
+    DOWN(false, false);
 
     private final boolean left;
     private final boolean right;
 
-    private Direction(boolean left, boolean right) {
-        validateDirection(left, right);
+    Direction(boolean left, boolean right) {
         this.left = left;
         this.right = right;
     }
 
-    public static Direction of(boolean left, boolean right) {
-        return new Direction(left, right);
+    static Direction first(boolean isRight) {
+        return isRight ? RIGHT : DOWN;
     }
 
-    public static Direction first(boolean right) {
-        return of(FALSE, right);
-    }
-
-    public Direction last() {
-        return of(right, FALSE);
-    }
-
-    public Direction next(boolean strategy) {
-        return of(right, strategy);
-    }
-
-    public Direction next(PointStrategy pointStrategy) {
+    Direction next(boolean isNext) {
         if (this.right) {
-            return next(FALSE);
+            return LEFT;
         }
-        return next(pointStrategy.next());
+
+        return isNext ? RIGHT : DOWN;
     }
 
     public boolean isLeft() {
@@ -47,25 +31,5 @@ public class Direction {
 
     public boolean isRight() {
         return right;
-    }
-
-    private void validateDirection(boolean left, boolean right) {
-        if (left && right) {
-            throw new IllegalStateException(NOT_CORRECT_DIRECTION_MESSAGE);
-        }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Direction that = (Direction) o;
-        return left == that.left &&
-                right == that.right;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(left, right);
     }
 }
--- src/test/java/ladder/domain/point/DirectionTest.java
+++ src/test/java/ladder/domain/point/DirectionTest.java
@@ -2,8 +2,12 @@ package ladder.domain.point;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DirectionTest {
 
@@ -13,21 +17,31 @@ class DirectionTest {
         assertThat(Direction.first(Boolean.TRUE).isLeft()).isFalse();
     }
 
-    @DisplayName("last 테스트")
+    @DisplayName("left 테스트")
     @Test
-    void lastTest() {
-        assertThat(Direction.of(false, true).last().isRight()).isFalse();
+    void leftTest() {
+        assertThat(Direction.LEFT.isLeft()).isTrue();
+        assertThat(Direction.LEFT.isRight()).isFalse();
+    }
+
+    @DisplayName("right 테스트")
+    @Test
+    void rightTest() {
+        assertThat(Direction.RIGHT.isLeft()).isFalse();
+        assertThat(Direction.RIGHT.isRight()).isTrue();
     }
 
     @DisplayName("next 테스트")
-    @Test
-    void nextTest() {
-        assertThat(Direction.of(false, true).next(() -> true).isLeft()).isTrue();
-    }
+    @ParameterizedTest
+    @EnumSource(value = Direction.class, names = {"LEFT", "RIGHT", "DOWN"})
+    void nextTest(Direction direction) {
+        if (direction.isRight()) {
+            assertThat(direction.next(FALSE)).isEqualTo(Direction.LEFT);
+        }
 
-    @DisplayName("예외처리 테스트")
-    @Test
-    void validateDirection() {
-        assertThatIllegalStateException().isThrownBy(() -> Direction.of(true, true));
+        if (direction.isLeft()) {
+            assertThat(direction.next(FALSE)).isEqualTo(Direction.DOWN);
+            assertThat(direction.next(TRUE)).isEqualTo(Direction.RIGHT);
+        }
     }
 }
