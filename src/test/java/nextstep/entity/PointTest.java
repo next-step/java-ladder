acda67bda9609ff266bbcb24cc0b51ab3673c2db
skid901 <tlrudejr1993@naver.com>
2020-09-12 22:21:04 +0900

    test: test Point class methods
    1. test Point.next() method
    2. test Point.getStartPoint() method
    3. test Point.getLastPoint() method
    4. test Point.getSinglePoint() method
    5. test Point.of() method

--- src/test/java/nextstep/entity/PointTest.java
+++ src/test/java/nextstep/entity/PointTest.java
@@ -1,9 +1,11 @@
 package nextstep.entity;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.BDDAssertions.then;
 
 class PointTest {
@@ -31,5 +33,45 @@ class PointTest {
     void play(boolean leftPedal, boolean rightPedal, int index, int expected) {
         Point point = Point.of(leftPedal, rightPedal);
         then(point.play(index)).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("인스턴스 생성 메소드 검증")
+    void next() {
+        Point point = Point.of(false, true);
+        then(point.next(true).hasLeftPedal()).isTrue();
+        then(point.next(true).hasRightPedal()).isFalse();
+    }
+
+    @Test
+    @DisplayName("시작 페달 생성 메소드 검증")
+    void getStartPoint() {
+        Point point = Point.getStartPoint(true);
+        then(point.hasLeftPedal()).isFalse();
+        then(point.hasRightPedal()).isTrue();
+    }
+
+    @Test
+    @DisplayName("마지막 페달 생성 메소드 검증")
+    void getLastPoint() {
+        Point point = Point.of(false, true).getLastPoint();
+        then(point.hasLeftPedal()).isTrue();
+        then(point.hasRightPedal()).isFalse();
+    }
+
+    @Test
+    @DisplayName("단일 페달 생성 메소드 검증")
+    void getSinglePoint() {
+        Point point = Point.getSinglePoint();
+        then(point.hasLeftPedal()).isFalse();
+        then(point.hasRightPedal()).isFalse();
+    }
+
+    @Test
+    @DisplayName("인스턴스 생성 메소드 예외처리 검증")
+    void of() {
+        assertThatThrownBy(() -> Point.of(true, true))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("좌측과 우측 모두 페달을 생성할 수 없습니다.");
     }
 }
