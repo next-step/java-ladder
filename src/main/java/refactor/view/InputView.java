070e207d5e638d534115ee2c378a97728d421e3f
great0417 <great0417@hanmail.net>
2020-09-15 00:26:13 +0900

    feat - InputView 객체 생성

---
+++ src/main/java/refactor/view/InputView.java
@@ -0,0 +1,14 @@
+package refactor.view;
+
+import ladder.util.InputUtil;
+import refactor.domain.LadderGame;
+
+public class InputView {
+    public void input() {
+        String names = InputUtil.returnString("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        String resultValue = InputUtil.returnString("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        int ladderHeight = InputUtil.returnInteger("최대 사다리 높이는 몇 개인가요?");
+        LadderGame ladderGame = LadderGame.of(names,ladderHeight);
+        ladderGame.game(resultValue);
+    }
+}
