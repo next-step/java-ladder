05bb502728faa88149ac368b75923b57ea59d43d
Heesu Lee <hesue615@gmail.com>
2020-09-15 01:01:23 +0900

    LadderResult 변경

---
+++ src/main/java/ladder/legacy/domain/LadderResult.java
@@ -0,0 +1,31 @@
+package ladder.legacy.domain;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class LadderResult {
+
+    private final Map<Name, Integer> results;
+
+    public LadderResult(Map<Name, Integer> results) {
+        this.results = results;
+    }
+
+    public static LadderResult of() {
+        Map<Name, Integer> result = new LinkedHashMap<>();
+        return new LadderResult(result);
+    }
+
+    public void addResult(Name name, int position) {
+        results.put(name, position);
+    }
+
+    public int findResultByName(Name name) {
+        return results.get(name);
+    }
+
+    public Set<Name> getAllNames() {
+        return results.keySet();
+    }
+}
---
+++ src/main/java/ladder/refactoring/domain/LadderResult.java
@@ -0,0 +1,27 @@
+package ladder.refactoring.domain;
+
+import ladder.legacy.domain.Name;
+
+import java.util.Map;
+import java.util.Set;
+
+public class LadderResult {
+
+    private final Map<Participant, Integer> result;
+
+    public LadderResult(Map<Participant, Integer> result) {
+        this.result = result;
+    }
+
+    public static LadderResult of(Map<Participant, Integer> result) {
+        return new LadderResult(result);
+    }
+
+    public int findResultByParticipant(Participant name) {
+        return result.get(name);
+    }
+
+    public Set<Participant> getAllParticipants() {
+        return result.keySet();
+    }
+}
