b9620d114952a5c50d64599119f64311c5bf6149
great0417 <great0417@hanmail.net>
2020-09-15 00:20:00 +0900

    refactor - InputView에 입력받는 부분 위임

--- src/main/java/refactor/controller/RefactorMain.java
+++ src/main/java/refactor/controller/RefactorMain.java
@@ -2,14 +2,11 @@ package refactor.controller;
 
 import ladder.util.InputUtil;
 import refactor.domain.LadderGame;
+import refactor.view.InputView;
 
 public class RefactorMain {
     public static void main(String[] args) {
-
-        String names = InputUtil.returnString("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        String resultValue = InputUtil.returnString("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
-        int ladderHeight = InputUtil.returnInteger("최대 사다리 높이는 몇 개인가요?");
-        LadderGame ladderGame = LadderGame.of(names, ladderHeight);
-        ladderGame.game(resultValue);
+        InputView inputView = new InputView();
+        inputView.input();
     }
 }
