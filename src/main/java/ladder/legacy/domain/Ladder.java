a4a0f3566dd3a0dc82548dad3441becda4491522
Heesu Lee <hesue615@gmail.com>
2020-09-15 00:57:02 +0900

    Ladder 리펙토링

---
+++ src/main/java/ladder/legacy/domain/Ladder.java
@@ -0,0 +1,32 @@
+package ladder.legacy.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Ladder {
+
+    private final List<Line> lines;
+
+    public Ladder(List<Line> lines) {
+        this.lines = lines;
+    }
+
+    public static Ladder of(Participants participants, Height height) {
+        List<Line> lines = Stream.generate(() -> Line.of(participants))
+                .limit(height.getValue())
+                .collect(Collectors.toList());
+        return new Ladder(lines);
+    }
+
+    public List<Line> getLines() {
+        return lines;
+    }
+
+    public int move(int postion) {
+        for (Line line : lines) {
+            postion = line.move(postion);
+        }
+        return postion;
+    }
+}
