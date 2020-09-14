3dcdedf798ab5edb3f0715b5448668ccf7df950f
fist-kim <kimjungkwon101@gmail.com>
2020-09-14 12:52:09 +0900

    feat : step1 옵셔널

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,7 +35,8 @@ public class User {
     }
 
     public static boolean ageIsInRange2(User user) {
-        return false;
+        Optional<User> targetUser = Optional.ofNullable(user);
+        return targetUser.map(User::getAge).filter(age -> age >= 30).filter(age -> age <= 45).isPresent();
     }
 
     @Override
