bede3247c7f95104206333e83224159838a4e565
hanogo <trullo393@gmail.com>
2020-09-14 21:35:23 +0900

    [feat] Ladder 클래스 구현 및 테스트

---
+++ src/main/java/ladder/domain/Ladder.java
@@ -0,0 +1,20 @@
+package ladder.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Ladder {
+    List<Line> lines;
+
+    public Ladder(int countOfPlayer, int ladderSize, RightPointRandom rightPointRandom) {
+        this.lines = IntStream.range(0, ladderSize)
+                .mapToObj(i -> new Line(countOfPlayer, rightPointRandom))
+                .collect(Collectors.toList());
+    }
+
+    public List<Line> getLines() {
+        return Collections.unmodifiableList(lines);
+    }
+}
---
+++ src/test/java/ladder/domain/LadderTest.java
@@ -0,0 +1,13 @@
+package ladder.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LadderTest {
+
+    @Test
+    void getSize() {
+        Ladder ladder = new Ladder(5, 3, new RightPointRandom());
+        Assertions.assertThat(ladder.getLines().size()).isEqualTo(3);
+    }
+}
