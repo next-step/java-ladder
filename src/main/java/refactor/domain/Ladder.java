b398965cbe8e41b5b16f93a26800fc2df40a3ca1
great0417 <great0417@hanmail.net>
2020-09-15 00:21:25 +0900

    refactor - of 메소드 생성. 생성자 private로 변경, for문 stream으로 변경

--- src/main/java/refactor/domain/Ladder.java
+++ src/main/java/refactor/domain/Ladder.java
@@ -6,22 +6,27 @@ import refactor.domain.pointGenerate.RandomPoint;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Ladder {
     private List<LadderLine> ladderLines;
 
-    public Ladder(List<LadderLine> ladderLines) {
+    private Ladder(List<LadderLine> ladderLines) {
         this.ladderLines = ladderLines;
     }
 
     public static Ladder init(int ladderHeight, int sizeOfPerson) {
         PointStrategy pointStrategy = new RandomPoint();
         List<LadderLine> ladderLines = new ArrayList<>();
-        for (int i = 0; i < ladderHeight; i++) {
-            ladderLines.add(LadderLine.init(sizeOfPerson, pointStrategy));
-        }
+        IntStream.rangeClosed(0, ladderHeight)
+                .forEach(i -> ladderLines.add(LadderLine.init(sizeOfPerson,pointStrategy)));
+        return Ladder.of(ladderLines);
+    }
+
+    public static Ladder of(List<LadderLine> ladderLines) {
         return new Ladder(ladderLines);
     }
+
 
     public List<LadderLine> getLadderLines() {
         return Collections.unmodifiableList(ladderLines);
