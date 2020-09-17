050eea3448b62b4832f72e7c8b7c80ce667b0649
Jay <beginin15@gmail.com>
2020-09-17 13:18:35 +0900

    refactor: Point를 불변 클래스로 변경, 재사용성 테스트

--- src/main/java/nextstep/ladder/Point.java
+++ src/main/java/nextstep/ladder/Point.java
@@ -1,10 +1,23 @@
 package nextstep.ladder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Point {
 
-    private boolean connection;
+    private final boolean connection;
+    private static final Map<Boolean, Point> POINTS = new HashMap<>();
 
-    public Point(boolean connection) {
+    static {
+        POINTS.put(true, new Point(true));
+        POINTS.put(false, new Point(false));
+    }
+
+    private Point(boolean connection) {
         this.connection = connection;
+    }
+
+    public static Point of(boolean connection) {
+        return POINTS.get(connection);
     }
 }
--- src/test/java/nextstep/ladder/PointTest.java
+++ src/test/java/nextstep/ladder/PointTest.java
@@ -1,10 +1,12 @@
 package nextstep.ladder;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PointTest {
 
@@ -12,6 +14,13 @@ public class PointTest {
     @ValueSource(booleans = {true, false})
     @DisplayName("생성자 정상 작동")
     void create(boolean connection) {
-        assertNotNull(new Point(connection));
+        assertNotNull(Point.of(connection));
+    }
+
+    @Test
+    @DisplayName("객체 재사용 확인")
+    void reusable() {
+        assertTrue(Point.of(true) == Point.of(true));
+        assertTrue(Point.of(false) == Point.of(false));
     }
 }
