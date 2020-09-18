5bcc40b46d46d4e5f97e74e0ed201332c96eb6b9
Taehee9 <dhstoalfh9509@naver.com>
2020-09-19 01:08:04 +0900

    refactor: 오타수정

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -7,7 +7,7 @@ public class InputView {
     private Scanner scanner = new Scanner(System.in);
 
     public String joinUser() {
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼포(,)로 구분하세요)");
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         return scanner.next();
     }
 
