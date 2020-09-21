f6f06fae84ee5c0f00cdcf666eb668b8b55d560e
Jay <beginin15@gmail.com>
2020-09-21 21:06:58 +0900

    feat: 결과 출력을 처리하는 ResultView 클래스 정의

---
+++ src/main/java/nextstep/ladder/ResultView.java
@@ -0,0 +1,35 @@
+package nextstep.ladder;
+
+import java.util.List;
+
+public class ResultView {
+
+    private static final String PRINT_INTERVAL = "%6s";
+    private static final String HORIZONTAL = "-----";
+    private static final String VERTICAL = "|";
+
+    public static void printLadder(Ladder ladder, List<Player> players) {
+        System.out.println("실행 결과");
+        printNames(players);
+        printLines(ladder);
+    }
+
+    private static void printNames(List<Player> players) {
+        players.stream().forEach(name -> System.out.printf(PRINT_INTERVAL, name));
+        System.out.println();
+    }
+
+    private static void printLines(Ladder ladder) {
+        for (Line line : ladder.getLines()) {
+            System.out.printf(PRINT_INTERVAL, "|");
+            printPoints(line.getPoints());
+            System.out.println();
+        }
+    }
+
+    private static void printPoints(List<Point> points) {
+        points.stream()
+                .map(point -> point.isConnection() ? HORIZONTAL + VERTICAL : VERTICAL)
+                .forEach(point -> System.out.printf(PRINT_INTERVAL, point));
+    }
+}
