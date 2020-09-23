0494809751440a7b2f185a8e5dc1d8578d0da9cb
Jay <beginin15@gmail.com>
2020-09-23 23:52:24 +0900

    test: Lines.convertToPipes 메소드에 대한 테스트 코드 추가

--- src/test/java/nextstep/ladder/domain/LinesTest.java
+++ src/test/java/nextstep/ladder/domain/LinesTest.java
@@ -1,29 +1,39 @@
 package nextstep.ladder.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toList;
+import static nextstep.ladder.TestUtil.asPointList;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class LinesTest {
 
     private final int countOfPerson = 4;
+    private List<Line> lineList;
 
-    @Test
-    @DisplayName("팩토리 메소드 정상 작동")
-    void of() {
-        Lines lines = Lines.of(countOfPerson,
+    @BeforeEach
+    private void setUp() {
+        lineList = Arrays.asList(
                 asLine(true, false, true),
                 asLine(false, true, false),
                 asLine(true, false, false),
                 asLine(false, true, false),
                 asLine(true, false, true)
         );
+    }
+
+    @Test
+    @DisplayName("팩토리 메소드 정상 작동")
+    void of() {
+        Lines lines = Lines.of(countOfPerson, lineList);
         assertNotNull(lines);
     }
 
@@ -31,13 +41,7 @@ public class LinesTest {
     @DisplayName("팩토리 메소드 예외 발생 - countOfPerson")
     void of_validateSize() {
         assertThatIllegalArgumentException().isThrownBy(() ->
-                Lines.of(countOfPerson + 1,
-                        asLine(true, false, true),
-                        asLine(false, true, false),
-                        asLine(true, false, false),
-                        asLine(false, true, false),
-                        asLine(true, false, true)
-                )
+                Lines.of(countOfPerson + 1, lineList)
         );
     }
 
@@ -45,5 +49,31 @@ public class LinesTest {
         return Arrays.stream(points)
                 .map(Point::of)
                 .collect(collectingAndThen(toList(), Line::of));
+    }
+
+    @Test
+    @DisplayName("DTO 리스트로 변환")
+    void convertToPipes() {
+        // given
+        Lines lines = Lines.of(countOfPerson, lineList);
+
+        // when
+        List<Pipes> actual = lines.convertToPipes();
+
+        // then
+        List<Pipes> expected = Arrays.asList(
+                asPipes(true, false, true),
+                asPipes(false, true, false),
+                asPipes(true, false, false),
+                asPipes(false, true, false),
+                asPipes(true, false, true)
+        );
+        assertEquals(expected, actual);
+    }
+
+    private static Pipes asPipes(Boolean... points) {
+        return asPointList(points).stream()
+                .map(Pipe::of)
+                .collect(collectingAndThen(toList(), Pipes::new));
     }
 }
