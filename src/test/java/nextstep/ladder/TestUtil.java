5c78de4ae7edb532e57a952fc27348ca7feb2c71
Jay <beginin15@gmail.com>
2020-09-23 20:43:01 +0900

    refactor: 테스트 코드 리팩토링

--- src/test/java/nextstep/ladder/TestUtil.java
+++ src/test/java/nextstep/ladder/TestUtil.java
@@ -1,10 +1,14 @@
 package nextstep.ladder;
 
+import nextstep.ladder.domain.Line;
 import nextstep.ladder.domain.Point;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
 
 public class TestUtil {
 
@@ -12,5 +16,11 @@ public class TestUtil {
         return Arrays.stream(points)
                 .map(Point::of)
                 .collect(Collectors.toList());
+    }
+
+    public static Line asLine(Boolean... points) {
+        return Arrays.stream(points)
+                .map(Point::of)
+                .collect(collectingAndThen(toList(), Line::new));
     }
 }
--- src/test/java/nextstep/ladder/factory/LadderFactoryTest.java
+++ src/test/java/nextstep/ladder/factory/LadderFactoryTest.java
@@ -7,40 +7,40 @@ import nextstep.ladder.pattern.RandomPointGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static nextstep.ladder.TestUtil.asPointList;
+import java.util.Arrays;
+import java.util.List;
+
+import static nextstep.ladder.TestUtil.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class LadderFactoryTest {
 
+    private final int height = 5;
+    private final int countOfPerson = 4;
+
     @Test
     @DisplayName("랜덤 Ladder 생성")
     void generate() {
-        // given
-        int height = 5;
-        int countOfPerson = 4;
-
-        // when
         Ladder actual = LadderFactory.generate(height, countOfPerson, new RandomPointGenerator());
-
-        // then
         assertNotNull(actual);
     }
 
     @Test
     @DisplayName("연결되지 않은 Ladder 생성")
     void generate_noConnection() {
-        int height = 5;
-        int countOfPerson = 4;
-
+        // when
         Ladder actual = LadderFactory.generate(height, countOfPerson, () -> Point.of(false));
-        Ladder expected = Ladder.of(height, countOfPerson,
-                Line.of(asPointList(false, false, false)),
-                Line.of(asPointList(false, false, false)),
-                Line.of(asPointList(false, false, false)),
-                Line.of(asPointList(false, false, false)),
-                Line.of(asPointList(false, false, false))
+
+        // then
+        List<Line> lines = Arrays.asList(
+                asLine(false, false, false),
+                asLine(false, false, false),
+                asLine(false, false, false),
+                asLine(false, false, false),
+                asLine(false, false, false)
         );
+        Ladder expected = Ladder.of(height, countOfPerson, lines);
 
         assertEquals(expected, actual);
     }
