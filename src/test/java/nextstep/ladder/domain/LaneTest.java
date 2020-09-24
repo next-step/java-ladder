6e714360775914da83a27896b8516dc6453c3e31
Jay <beginin15@gmail.com>
2020-09-25 00:48:28 +0900

    test: Lane.change 메소드에 대한 테스트 케이스 추가

--- src/test/java/nextstep/ladder/domain/LaneTest.java
+++ src/test/java/nextstep/ladder/domain/LaneTest.java
@@ -2,6 +2,11 @@ package nextstep.ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,12 +21,20 @@ public class LaneTest {
         );
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("provideDirectionAndLane")
     @DisplayName("Direction에 따른 인덱스 변경")
-    void change() {
-        Lane actual = Lane.of(0).change(Direction.RIGHT);
-        Lane expected = Lane.of(1);
+    void change(int beforeIndex, Direction direction, int afterIndex) {
+        Lane actual = Lane.of(beforeIndex).change(direction);
+        Lane expected = Lane.of(afterIndex);
         assertEquals(expected, actual);
     }
 
+    private static Stream<Arguments> provideDirectionAndLane() {
+        return Stream.of(
+                Arguments.of(0, Direction.RIGHT, 1),
+                Arguments.of(1, Direction.RIGHT, 2),
+                Arguments.of(1, Direction.LEFT, 0)
+        );
+    }
 }
