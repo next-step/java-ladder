5b470dd4b3805337432f8f883c3812d314207ced
Jay <beginin15@gmail.com>
2020-09-27 23:10:03 +0900

    feat: 입력받은 실행 결과를 저장하는 LadderResults 클래스 정의

---
+++ src/main/java/nextstep/ladder/domain/LadderResults.java
@@ -0,0 +1,28 @@
+package nextstep.ladder.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LadderResults {
+
+    private List<String> results;
+
+    private LadderResults(List<String> results) {
+        this.results = results;
+    }
+
+    public static LadderResults of(int countOfPersons, List<String> results) {
+        validateCount(countOfPersons, results);
+        return new LadderResults(results);
+    }
+
+    private static void validateCount(int countOfPersons, List<String> results) {
+        if (countOfPersons != results.size()) {
+            throw new IllegalArgumentException("결과 수가 사람 수와 일치하지 않습니다.");
+        }
+    }
+
+    public List<String> getResults() {
+        return Collections.unmodifiableList(results);
+    }
+}
