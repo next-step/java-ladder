2b3ecbd347884c552cf2a47e94c21ec458245581
siyoon <siyoon210@gmail.com>
2020-11-19 21:46:29 +0900

    test: 메스드명 rename

--- src/test/java/nextstep/ladder/PointsTest.java
+++ src/test/java/nextstep/ladder/PointsTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 public class PointsTest {
     @Test
     @DisplayName("Points의 사이즈는 참가인원(CountOfPerson)보다 1 적어야 한다.")
-    void createTest() {
+    void pointsSizeTest() {
         CountOfPerson countOfPerson = CountOfPerson.of(4);
         Points points = new Points(countOfPerson);
         assertThat(points.size()).isEqualTo(countOfPerson.value - 1);
@@ -17,7 +17,7 @@ public class PointsTest {
 
     @Test
     @DisplayName("Points값은 연속된 true가 나오면 안된다.")
-    void linePointerTest() {
+    void pointsValueTest() {
         CountOfPerson countOfPerson = CountOfPerson.of(10_000);
         assertThatCode(() -> new Points(countOfPerson))
                 .doesNotThrowAnyException();
