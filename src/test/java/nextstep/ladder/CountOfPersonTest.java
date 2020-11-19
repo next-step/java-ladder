0564182294c1f8a670d4c8f39ec5bf7cde218a82
siyoon <siyoon210@gmail.com>
2020-11-19 21:07:03 +0900

    test: CountOfPerson 생성 테스트 케이스 추가

--- src/test/java/nextstep/ladder/CountOfPersonTest.java
+++ src/test/java/nextstep/ladder/CountOfPersonTest.java
@@ -1,15 +1,27 @@
 package nextstep.ladder;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class CountOfPersonTest {
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {2, 100})
+    @DisplayName("CountOfPerson 생성시에 value값은 2이상의 정수여야 한다.")
+    void createTest(int inputValue) {
+        assertThatCode(() -> new CountOfPerson(inputValue))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 1})
     @DisplayName("CountOfPerson 생성시에 value값이 2보다 작은 숫자면 throw Exception")
-    void createTest() {
+    void createTestInvalidInputValue(int inputValue) {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new CountOfPerson(1));
+                .isThrownBy(() -> new CountOfPerson(inputValue))
+                .withMessage(CountOfPerson.INPUT_VALUE_RANGE_ERR_MSG);
     }
 }
