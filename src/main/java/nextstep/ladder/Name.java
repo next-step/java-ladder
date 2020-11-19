4304c394a94e15ae84cf1b25a460fed6a851ee13
siyoon <siyoon210@gmail.com>
2020-11-19 22:30:44 +0900

    feat: Name 클래스 생성

---
+++ src/main/java/nextstep/ladder/Name.java
@@ -0,0 +1,21 @@
+package nextstep.ladder;
+
+public class Name {
+    public static final String INVALID_NAME_LENGTH_ERR_MSG = "이름은 1글자 이상 5글자 이하만 허용합니다.";
+    private final String value;
+
+    private Name(String value) {
+        validateLength(value.length());
+        this.value = value;
+    }
+
+    public static Name of(String value) {
+        return new Name(value);
+    }
+
+    private void validateLength(int length) {
+        if (length < 1 || length > 5) {
+            throw new IllegalArgumentException(INVALID_NAME_LENGTH_ERR_MSG);
+        }
+    }
+}
