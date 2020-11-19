c4f94ca1feaa912da5dbd8ae24674be4bf4f49cb
siyoon <siyoon210@gmail.com>
2020-11-19 21:29:32 +0900

    refactor: Line이 List<Boolean>이 아닌 Points 일급컬렉션을 갖도록 변경

--- src/main/java/nextstep/ladder/Line.java
+++ src/main/java/nextstep/ladder/Line.java
@@ -1,34 +1,9 @@
 package nextstep.ladder;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 public class Line {
-    private final List<Boolean> points;
+    private final Points points;
 
     public Line(CountOfPerson countOfPerson) {
-        points = new ArrayList<>(countOfPerson.value - 1);
-        setPoints(countOfPerson);
-    }
-
-    private void setPoints(CountOfPerson countOfPerson) {
-        Random random = new Random();
-        points.add(random.nextBoolean());
-        for (int j = 1; j < countOfPerson.value - 1; j++) {
-            if (points.get(j - 1)) {
-                points.add(false);
-                continue;
-            }
-            points.add(random.nextBoolean());
-        }
-    }
-
-    public int size() {
-        return points.size();
-    }
-
-    public List<Boolean> getPoints() {
-        return points;
+        points = new Points(countOfPerson);
     }
 }
--- src/test/java/nextstep/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -1,31 +1,4 @@
 package nextstep.ladder;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class LineTest {
-    @Test
-    @DisplayName("Line의 사이즈는 참가인원보다 1 적어야 한다.")
-    void lineCreateTest() {
-        CountOfPerson countOfPerson = new CountOfPerson(4);
-        Line line = new Line(countOfPerson);
-        assertThat(line.size()).isEqualTo(countOfPerson.value - 1);
-    }
-
-    @Test
-    @DisplayName("Line의 point값은 연속으로 true가 나오면 안된다.")
-    void linePointerTest() {
-        CountOfPerson countOfPerson = new CountOfPerson(10_000);
-        Line line = new Line(countOfPerson);
-        List<Boolean> points = line.getPoints();
-        for (int i = 0; i < points.size() - 1; i++) {
-            if (points.get(i)) {
-                assertThat(points.get(i + 1)).isFalse();
-            }
-        }
-    }
 }
