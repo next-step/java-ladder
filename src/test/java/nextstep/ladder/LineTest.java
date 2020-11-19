e94d328f9548b0092e70a08da2c9fc386d52c300
siyoon <siyoon210@gmail.com>
2020-11-19 20:35:31 +0900

    test: 테스트시에 사용하는 숫자 변수 선언

--- src/test/java/nextstep/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -9,7 +9,8 @@ public class LineTest {
     @Test
     @DisplayName("Line의 사이즈는 참가인원보다 1 적어야 한다.")
     void lineCreateTest() {
-        Line line = new Line(4);
-        assertThat(line.size()).isEqualTo(3);
+        int countOfPerson = 4;
+        Line line = new Line(countOfPerson);
+        assertThat(line.size()).isEqualTo(countOfPerson - 1);
     }
 }
