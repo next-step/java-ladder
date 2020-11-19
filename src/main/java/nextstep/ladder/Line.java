cc008372499c54b36e96331826ad88002a4c4cc9
siyoon <siyoon210@gmail.com>
2020-11-19 20:34:39 +0900

    refactor: 생성자 인수명 변경

--- src/main/java/nextstep/ladder/Line.java
+++ src/main/java/nextstep/ladder/Line.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class Line {
     private final List<Boolean> points;
 
-    public Line(int i) {
-        points = new ArrayList<>(i - 1);
-        for (int j = 0; j < i - 1; j++) {
+    public Line(int countOfPerson) {
+        points = new ArrayList<>(countOfPerson - 1);
+        for (int j = 0; j < countOfPerson - 1; j++) {
             points.add(false);
         }
     }
