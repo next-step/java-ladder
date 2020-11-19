ae33fce233cbfab3ee918df0c47f0c2de7b6f6aa
siyoon <siyoon210@gmail.com>
2020-11-19 20:54:42 +0900

    test: Line의 point값은 연속으로 true가 나오면 안된다.

--- src/test/java/nextstep/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -3,6 +3,8 @@ package nextstep.ladder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineTest {
@@ -12,5 +14,18 @@ public class LineTest {
         int countOfPerson = 4;
         Line line = new Line(countOfPerson);
         assertThat(line.size()).isEqualTo(countOfPerson - 1);
+    }
+
+    @Test
+    @DisplayName("Line의 point값은 연속으로 true가 나오면 안된다.")
+    void linePointerTest() {
+        int countOfPerson = 100;
+        Line line = new Line(countOfPerson);
+        List<Boolean> points = line.getPoints();
+        for (int i = 0; i < points.size() - 1; i++) {
+            if (points.get(i)) {
+                assertThat(points.get(i + 1)).isFalse();
+            }
+        }
     }
 }
