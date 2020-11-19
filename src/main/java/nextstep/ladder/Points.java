9a8ad86235837250a6a27b2ec0efb437fa5aaf4b
siyoon <siyoon210@gmail.com>
2020-11-19 21:27:16 +0900

    feat: Points값이 연속으로 true가 나오면 throw Exception

--- src/main/java/nextstep/ladder/Points.java
+++ src/main/java/nextstep/ladder/Points.java
@@ -5,12 +5,14 @@ import java.util.List;
 import java.util.Random;
 
 public class Points {
+    public static final String POINTS_INVALID_VALUE_ERR_MSG = "Points의 값이 연속으로 true일 수 없습니다.";
     private final List<Boolean> values;
     private final Random random = new Random();
 
     public Points(CountOfPerson countOfPerson) {
         this.values = new ArrayList<>(countOfPerson.value - 1);
         setPoints(countOfPerson);
+        validatePoints();
     }
 
     private void setPoints(CountOfPerson countOfPerson) {
@@ -21,6 +23,14 @@ public class Points {
                 continue;
             }
             values.add(random.nextBoolean());
+        }
+    }
+
+    private void validatePoints() {
+        for (int i = 0; i < values.size() - 1; i++) {
+            if (values.get(i) && values.get(i + 1)) {
+                throw new IllegalStateException(POINTS_INVALID_VALUE_ERR_MSG);
+            }
         }
     }
 
