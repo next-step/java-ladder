843d8d61d7b269a258de8bbcbd9f4430cabb2422
siyoon <siyoon210@gmail.com>
2020-11-19 22:25:34 +0900

    test: 이름의 유효한 길이는 1글자 이상 5글자 이하

---
+++ src/test/java/nextstep/ladder/NameTest.java
@@ -0,0 +1,15 @@
+package nextstep.ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
+public class NameTest {
+    @Test
+    @DisplayName("이름의 유효한 길이는 1글자 이상 5글자 이하")
+    void invalidNameTest() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> Name.of("123456"));
+    }
+}
