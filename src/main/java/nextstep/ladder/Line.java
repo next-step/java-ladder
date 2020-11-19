097b595c6a29d1d4a947f1643620022b7b38fba1
siyoon <siyoon210@gmail.com>
2020-11-19 20:44:25 +0900

    refactor: points값 설정시 랜덤하게 값 할당

--- src/main/java/nextstep/ladder/Line.java
+++ src/main/java/nextstep/ladder/Line.java
@@ -2,14 +2,24 @@ package nextstep.ladder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Line {
     private final List<Boolean> points;
 
     public Line(int countOfPerson) {
         points = new ArrayList<>(countOfPerson - 1);
-        for (int j = 0; j < countOfPerson - 1; j++) {
-            points.add(false);
+        setPoints(countOfPerson);
+    }
+
+    private void setPoints(int countOfPerson) {
+        Random random = new Random();
+        points.add(random.nextBoolean());
+        for (int j = 1; j < countOfPerson - 1; j++) {
+            if (points.get(j - 1)) {
+                continue;
+            }
+            points.add(random.nextBoolean());
         }
     }
 
