db73cab5b780281a188704916d78ae754c734bda
siyoon <siyoon210@gmail.com>
2020-11-19 21:15:02 +0900

    refactor: Line이 래핑된 CountOfPerson을 사용하도록 변경

--- src/main/java/nextstep/ladder/Line.java
+++ src/main/java/nextstep/ladder/Line.java
@@ -7,15 +7,15 @@ import java.util.Random;
 public class Line {
     private final List<Boolean> points;
 
-    public Line(int countOfPerson) {
-        points = new ArrayList<>(countOfPerson - 1);
+    public Line(CountOfPerson countOfPerson) {
+        points = new ArrayList<>(countOfPerson.value - 1);
         setPoints(countOfPerson);
     }
 
-    private void setPoints(int countOfPerson) {
+    private void setPoints(CountOfPerson countOfPerson) {
         Random random = new Random();
         points.add(random.nextBoolean());
-        for (int j = 1; j < countOfPerson - 1; j++) {
+        for (int j = 1; j < countOfPerson.value - 1; j++) {
             if (points.get(j - 1)) {
                 points.add(false);
                 continue;
--- src/test/java/nextstep/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -11,15 +11,15 @@ public class LineTest {
     @Test
     @DisplayName("Line의 사이즈는 참가인원보다 1 적어야 한다.")
     void lineCreateTest() {
-        int countOfPerson = 4;
+        CountOfPerson countOfPerson = new CountOfPerson(4);
         Line line = new Line(countOfPerson);
-        assertThat(line.size()).isEqualTo(countOfPerson - 1);
+        assertThat(line.size()).isEqualTo(countOfPerson.value - 1);
     }
 
     @Test
     @DisplayName("Line의 point값은 연속으로 true가 나오면 안된다.")
     void linePointerTest() {
-        int countOfPerson = 100;
+        CountOfPerson countOfPerson = new CountOfPerson(10_000);
         Line line = new Line(countOfPerson);
         List<Boolean> points = line.getPoints();
         for (int i = 0; i < points.size() - 1; i++) {
