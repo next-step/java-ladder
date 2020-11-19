e3458408eb878e60347884029ac38640cacdf25b
catsbi <hansolsolsol@mosition.co.kr>
2020-11-19 13:23:11 +0900

    [refacotr] LadderBuildDTO 유효성 검증 로직 추가

--- src/main/java/step2/domain/ladder/dto/LadderBuildDTO.java
+++ src/main/java/step2/domain/ladder/dto/LadderBuildDTO.java
@@ -2,13 +2,33 @@ package step2.domain.ladder.dto;
 
 import step2.domain.ladder.LadderPlayers;
 
+import java.util.Objects;
+
 public class LadderBuildDTO {
+    public static final String ERROR_INVALID_LADDER_HEIGHT = "사다리 높이는 0보다 커야 합니다.";
     private final LadderPlayers players;
     private final int lineHeight;
 
     public LadderBuildDTO(LadderPlayers players, int lineHeight) {
+        isValidElements(players, lineHeight);
+
         this.players = players;
         this.lineHeight = lineHeight;
+    }
+
+    private void isValidElements(LadderPlayers players, int lineHeight) {
+        isValidPlayer(players);
+        isValidLineHeight(lineHeight);
+    }
+
+    private void isValidPlayer(LadderPlayers players) {
+        Objects.requireNonNull(players);
+    }
+
+    private void isValidLineHeight(int lineHeight) {
+        if (lineHeight <= 0) {
+            throw new IllegalArgumentException(ERROR_INVALID_LADDER_HEIGHT);
+        }
     }
 
     public LadderPlayers getPlayers() {
--- src/test/java/step2/makeLadderTest.java
+++ src/test/java/step2/makeLadderTest.java
@@ -6,10 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import step2.domain.ladder.LadderGame;
-import step2.domain.ladder.LadderPlayers;
-import step2.domain.ladder.Line;
-import step2.domain.ladder.Point;
+import org.junit.jupiter.params.provider.ValueSource;
+import step2.domain.ladder.*;
+import step2.domain.ladder.dto.LadderBuildDTO;
 import step2.strategy.MakeLineStrategy;
 import step2.strategy.TestLineMakeStrategy;
 
@@ -19,6 +18,7 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class makeLadderTest {
     private MakeLineStrategy allTrueStrategy;
@@ -26,40 +26,38 @@ public class makeLadderTest {
 
     @BeforeEach
     void setup() {
-        allTrueStrategy = new TestLineMakeStrategy(){
+        allTrueStrategy = new TestLineMakeStrategy() {
             @Override
             public List<Boolean> create(int countOfPerson) {
                 return IntStream.range(0, countOfPerson - 1)
-                        .mapToObj((index)->true)
+                        .mapToObj((index) -> true)
                         .collect(Collectors.toList());
             }
         };
-        allFalseStrategy = new TestLineMakeStrategy(){
+        allFalseStrategy = new TestLineMakeStrategy() {
             @Override
             public List<Boolean> create(int countOfPerson) {
                 return IntStream.range(0, countOfPerson - 1)
-                        .mapToObj((index)->false)
+                        .mapToObj((index) -> false)
                         .collect(Collectors.toList());
             }
         };
     }
 
 
-
-
     @DisplayName("라인 생성 전략")
     @Test
     void executeLineStrategy() {
-        Line allMarkedLine = new Line(4, (count) -> IntStream.range(0, count-1).mapToObj(i -> true).collect(Collectors.toList()));
-        Line allUnMarkedLine = new Line(4, (count) -> IntStream.range(0, count-1).mapToObj(i -> false).collect(Collectors.toList()));
+        Line allMarkedLine = new Line(4, (count) -> IntStream.range(0, count - 1).mapToObj(i -> true).collect(Collectors.toList()));
+        Line allUnMarkedLine = new Line(4, (count) -> IntStream.range(0, count - 1).mapToObj(i -> false).collect(Collectors.toList()));
 
-        assertThat(allMarkedLine.isExistsPoint(new Point(1,1))).isTrue();
-        assertThat(allMarkedLine.isExistsPoint(new Point(2,1))).isTrue();
-        assertThat(allMarkedLine.isExistsPoint(new Point(3,1))).isTrue();
+        assertThat(allMarkedLine.isExistsPoint(new Point(1, 1))).isTrue();
+        assertThat(allMarkedLine.isExistsPoint(new Point(2, 1))).isTrue();
+        assertThat(allMarkedLine.isExistsPoint(new Point(3, 1))).isTrue();
 
-        assertThat(allUnMarkedLine.isExistsPoint(new Point(1,1))).isFalse();
-        assertThat(allUnMarkedLine.isExistsPoint(new Point(2,2))).isFalse();
-        assertThat(allUnMarkedLine.isExistsPoint(new Point(3,3))).isFalse();
+        assertThat(allUnMarkedLine.isExistsPoint(new Point(1, 1))).isFalse();
+        assertThat(allUnMarkedLine.isExistsPoint(new Point(2, 2))).isFalse();
+        assertThat(allUnMarkedLine.isExistsPoint(new Point(3, 3))).isFalse();
 
     }
 
@@ -67,20 +65,30 @@ public class makeLadderTest {
     @ParameterizedTest
     @MethodSource("provideLadderPlayersAndLadderHeight")
     void makeLadder(LadderPlayers players, int height) {
-//        Ladder ladder = LadderGame.makeLadder(new LadderBuildDTO(players, height), allTrueStrategy);
+        Ladder ladder = LadderGame.makeLadder(new LadderBuildDTO(players, height), allTrueStrategy);
         System.out.println(players.toString());
 
-//        for (int i = 0; i < height; i++) {
-//            for (int j = 0; j < players.count()-1; j++) {
-//                assertThat(ladder.isExistsPoint(new LinePoint(j,i))).isTrue();
-//            }
-//        }
+        assertThat(ladder.isExistsPoint(new Point(0,0))).isTrue();
+        assertThat(ladder.isExistsPoint(new Point(0,1))).isTrue();
+        assertThat(ladder.isExistsPoint(new Point(0,2))).isTrue();
+        assertThat(ladder.isExistsPoint(new Point(0,3))).isTrue();
 
     }
 
     private static Stream<Arguments> provideLadderPlayersAndLadderHeight() {
         return Stream.of(
-                Arguments.of(LadderGame.join("pobi,crong,haha,bibi"), 5)
+                Arguments.of(LadderGame.join("pobi,crong,haha,bibi"), 3)
         );
+    }
+
+    @DisplayName("사다리 생성 높이 음수 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi,crong,haha,bibi"})
+    void makeLadderFromMinusHeight(String input) {
+        LadderPlayers players = LadderGame.join(input);
+        int ladderHeight = -1;
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LadderGame.makeLadder(new LadderBuildDTO(players, ladderHeight), allTrueStrategy))
+                .withMessage(LadderBuildDTO.ERROR_INVALID_LADDER_HEIGHT);
     }
 }
