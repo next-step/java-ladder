be8bf440914455c6d4a8d4c05473ef57d4bbd3aa
siyoon <siyoon210@gmail.com>
2020-11-20 23:32:28 +0900

    test: Line 테스트 케이스 추가, 테스트 메시지 수정

--- src/test/java/nextstep/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -5,12 +5,15 @@ import nextstep.ladder.domain.NumberOfParticipants;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class LineTest {
     @Test
-    @DisplayName("Line의 사이즈는 참가인원보다 1 적어야 한다.")
+    @DisplayName("Line의 size()는 참가인원보다 1 적어야 한다.")
     void lineSizeTest() {
         int numberOfParticipantsValue = 4;
         NumberOfParticipants numberOfParticipants = NumberOfParticipants.valueOf(numberOfParticipantsValue);
@@ -19,10 +22,19 @@ public class LineTest {
     }
 
     @Test
-    @DisplayName("Line의 Points값은 연속된 true가 나오면 안된다.")
-    void lineValueTest() {
+    @DisplayName("Line의 points값은 연속된 true가 나오면 안된다.")
+    void lineValueTestSuccessCase() {
         NumberOfParticipants numberOfParticipants = NumberOfParticipants.valueOf(10_000);
         assertThatCode(() -> Line.from(numberOfParticipants))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("Line의 points값중에 연속된 true가 존재하면 throw Exception")
+    void lineValueTestFailureCase() {
+        NumberOfParticipants numberOfParticipants = NumberOfParticipants.valueOf(3);
+        assertThatExceptionOfType(IllegalStateException.class)
+                .isThrownBy(() -> Line.of(numberOfParticipants, num -> Arrays.asList(true, true, true)))
+                .withMessage(Line.POINTS_INVALID_VALUE_ERR_MSG);
     }
 }
