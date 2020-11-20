6202e76d1d21eb2fdf4e295ac7aae7494abf820b
siyoon <siyoon210@gmail.com>
2020-11-20 22:42:08 +0900

    test: NameTest 테스트케이스 추가

--- src/test/java/nextstep/ladder/NameTest.java
+++ src/test/java/nextstep/ladder/NameTest.java
@@ -2,15 +2,26 @@ package nextstep.ladder;
 
 import nextstep.ladder.domain.Name;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class NameTest {
-    @Test
-    @DisplayName("이름의 유효한 길이는 1글자 이상 5글자 이하")
-    void invalidNameTest() {
+    @ParameterizedTest
+    @ValueSource(strings = {"", "123456"})
+    @DisplayName("이름이 1글자 미만 5글자 초과면 throw Exception")
+    void nameTestFailureCase(String invalidName) {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> Name.valueOf("123456"));
+                .isThrownBy(() -> Name.valueOf(invalidName));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "12345"})
+    @DisplayName("이름의 유효한 길이는 1글자 이상 5글자 이하다.")
+    void nameTestSuccessCase(String validName) {
+        assertThatCode(() -> Name.valueOf(validName))
+                .doesNotThrowAnyException();
     }
 }
