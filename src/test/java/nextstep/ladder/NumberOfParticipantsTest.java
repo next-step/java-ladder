3af3f282927765ed9913d0cea13aab13a14c16a4
siyoon <siyoon210@gmail.com>
2020-11-20 22:45:28 +0900

    test: NumberOfParticipantsTest 메시지 수정, 메서드 rename

--- src/test/java/nextstep/ladder/NumberOfParticipantsTest.java
+++ src/test/java/nextstep/ladder/NumberOfParticipantsTest.java
@@ -10,19 +10,19 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class NumberOfParticipantsTest {
     @ParameterizedTest
-    @ValueSource(ints = {2, 100})
-    @DisplayName("CountOfPerson 생성시에 value값은 2이상의 정수여야 한다.")
-    void createTest(int inputValue) {
-        assertThatCode(() -> NumberOfParticipants.valueOf(inputValue))
-                .doesNotThrowAnyException();
-    }
-
-    @ParameterizedTest
     @ValueSource(ints = {-1, 0, 1})
-    @DisplayName("CountOfPerson 생성시에 value값이 2보다 작은 숫자면 throw Exception")
-    void createTestInvalidInputValue(int inputValue) {
+    @DisplayName("NumberOfParticipants 생성시에 value값이 2보다 작은 숫자면 throw Exception")
+    void numberOfParticipantsTestFailureCase(int inputValue) {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> NumberOfParticipants.valueOf(inputValue))
                 .withMessage(NumberOfParticipants.INPUT_VALUE_RANGE_ERR_MSG);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 100})
+    @DisplayName("NumberOfParticipants 생성시에 value값은 2이상의 정수여야 한다.")
+    void numberOfParticipantsTestSuccessCase(int inputValue) {
+        assertThatCode(() -> NumberOfParticipants.valueOf(inputValue))
+                .doesNotThrowAnyException();
     }
 }
