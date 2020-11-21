14973052899a36046d405ed2253118d879aa30e9
catsbi <killfine198@gmail.com>
2020-11-21 20:15:44 +0900

    [feat] Point 이동 메소드 구현(좌,우,아래)

---
+++ src/main/java/step3/type/DirectionType.java
@@ -0,0 +1,21 @@
+package step3.type;
+
+import step3.domain.ladder.Point;
+
+import java.util.function.UnaryOperator;
+
+public enum DirectionType {
+    DOWN(Point::downMove),
+    LEFT(Point::leftMove),
+    RIGHT(Point::rightMove);
+
+    private final UnaryOperator<Point> move;
+
+    DirectionType(UnaryOperator<Point> move) {
+        this.move = move;
+    }
+
+    public static Point execute(DirectionType type, Point currentPoint) {
+        return type.move.apply(currentPoint);
+    }
+}
