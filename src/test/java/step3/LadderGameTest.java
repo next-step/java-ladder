c796a97cba336d77f9790031c07f9bb0d61acf08
catsbi <killfine198@gmail.com>
2020-11-21 20:13:40 +0900

    [feat] 사다리게임 테스트 케이스 작성

--- src/test/java/step3/LadderGameTest.java
+++ src/test/java/step3/LadderGameTest.java
@@ -6,27 +6,44 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import step3.domain.ladder.*;
 import step3.domain.ladder.dto.LadderBuildDTO;
+import step3.domain.ladder.dto.LadderResultDTO;
 import step3.strategy.MakeLadderLineStrategy;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LadderGameTest {
 
     @DisplayName("사다리게임 진행 테스트")
     @ParameterizedTest
     @MethodSource("provideLadderGameInfo")
-    void playLadderGame(LadderPlayers players, int lineHeight, LadderResults ladderResults) {
-        Ladder ladder = LadderGame.makeLadder(new LadderBuildDTO(players, lineHeight), new MakeLadderLineStrategy());
-        Player player = players.pick("crong");
+    void playLadderGame(Ladder ladder, LadderPlayers players, LadderResults ladderResults) {
+        Player player = players.pick("a");
         Point resultPoint = LadderGame.play(player, ladder);
+
+        LadderResultDTO ladderResultDTO = ladderResults.resultByPoint(resultPoint.getX());
+
+        assertThat(ladderResultDTO.getResult()).isEqualTo("5000");
+
     }
 
     private static Stream<Arguments> provideLadderGameInfo() {
-        LadderPlayers players = LadderGame.join("crong,pobi,kiki,bobi");
-        LadderResults ladderResults = LadderResults.of("꽝,5000,꽝,3000");
+        LadderPlayers players = LadderGame.join("a,b,c,d,e,");
+        LadderResults ladderResults = LadderResults.of("꽝,5000,꽝,3000,10000");
+
+        List<Line> lines = Arrays.asList(
+                Line.of(Arrays.asList(true, false, true, false)),
+                Line.of(Arrays.asList(false, false, false, false)),
+                Line.of(Arrays.asList(true, false, false, true)),
+                Line.of(Arrays.asList(true, false, false, true))
+        );
+        Ladder ladder = new Ladder(lines);
 
         return Stream.of(
-                Arguments.of(players, 5, ladderResults)
+                Arguments.of(ladder, players, ladderResults)
         );
     }
 
