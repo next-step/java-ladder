e90296a36c5f8e85e47e57ab3bd2048fc4e963bf
siyoon <siyoon210@gmail.com>
2020-11-22 15:38:50 +0900

    refactor: Participants names 만드는 부분 메소드추출, Name의 컬렉션 타입 Set으로 변경

--- src/main/java/nextstep/ladder/domain/Participants.java
+++ src/main/java/nextstep/ladder/domain/Participants.java
@@ -2,20 +2,25 @@ package nextstep.ladder.domain;
 
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class Participants {
     public static final String DUPLICATE_NAME_EXIST_ERR_MSG = "중복된 이름은 사용 할 수 없습니다.";
-    private final LinkedHashSet<Name> names;
+    private final Set<Name> names;
     private final NumberOfParticipants numberOfParticipants;
 
     private Participants(List<String> inputNames) {
-        this.names = inputNames.stream()
-                .map(Name::valueOf)
-                .collect(Collectors.toCollection(LinkedHashSet::new));
+        names = getNames(inputNames);
         numberOfParticipants = NumberOfParticipants.valueOf(inputNames.size());
         validateDuplication(inputNames);
+    }
+
+    private Set<Name> getNames(List<String> inputNames) {
+        return inputNames.stream()
+                .map(Name::valueOf)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 
     private void validateDuplication(List<String> inputNames) {
