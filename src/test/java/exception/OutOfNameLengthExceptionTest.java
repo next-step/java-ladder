c1e931b24cf965e1cecb1a774120cd9d9169871f
eyabc <bey4314@naver.com>
2020-11-24 00:40:04 +0900

    feat: OutOfNameLengthException

--- src/test/java/exception/OutOfNameLengthExceptionTest.java
+++ src/test/java/exception/OutOfNameLengthExceptionTest.java
@@ -1,4 +1,15 @@
 package exception;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 public class OutOfNameLengthExceptionTest {
+    @Test
+    void test() {
+        assertThatExceptionOfType(OutOfNameLengthException.class)
+                .isThrownBy(() -> {
+                    throw new OutOfNameLengthException();
+                });
+    }
 }
