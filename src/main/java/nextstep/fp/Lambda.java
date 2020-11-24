3e46333bb015d9e777a6ab98f9155881f2bda2bf
kaeum <oror1024@gmail.com>
2020-11-24 17:21:25 +0900

    feat: 람다를 활용해 중복 제거

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -26,31 +26,7 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
-    }
-
-    public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
-    }
-
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+    public static int sum(List<Integer> numbers, SumStrategy sumStrategy) {
+        return numbers.stream().filter(sumStrategy::isSuitable).reduce(0, (x, y) -> x + y);
     }
 }
