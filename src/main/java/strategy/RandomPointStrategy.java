1ea440a3a5f15e09250763ef8286394e8cd268e7
eyabc <bey4314@naver.com>
2020-11-24 20:05:28 +0900

    refactor: RandomPointStrategy Singleton

--- src/main/java/strategy/RandomPointStrategy.java
+++ src/main/java/strategy/RandomPointStrategy.java
@@ -4,6 +4,17 @@ import java.util.Random;
 
 public class RandomPointStrategy implements PointStrategy {
     private final static Random RANDOM_GENERATOR = new java.util.Random();
+    private final static RandomPointStrategy randomPointStrategy;
+
+    static {
+        randomPointStrategy = new RandomPointStrategy();
+    }
+
+    private RandomPointStrategy() {}
+
+    public static RandomPointStrategy of() {
+        return randomPointStrategy;
+    }
 
     @Override
     public boolean hasPoint() {
