3e4137176c168ab96267172787173f7cf21d2608
devyoon91 <kimbyungyoun91@gmail.com>
2020-11-24 22:22:20 +0900

    [refactor] : 요구사항3 findAny -> findFirst 변경

--- src/main/java/nextstep/optional/Expression.java
+++ src/main/java/nextstep/optional/Expression.java
@@ -18,7 +18,7 @@ enum Expression {
     static Expression of(String expression) {
         return Arrays.stream(values())
                 .filter(v -> matchExpression(v,expression))
-                .findAny()
+                .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(
                         String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression)
                 ));
