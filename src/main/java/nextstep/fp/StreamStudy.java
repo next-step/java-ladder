c79c05f01cfb947b37217f4f54c0513014b8e940
devyoon91 <kimbyungyoun91@gmail.com>
2020-11-24 21:40:58 +0900

    [refactor] : map, reduce, filter 실습 1

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -28,6 +28,7 @@ public class StreamStudy {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         // TODO 이 부분에 구현한다.
+
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +40,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(number -> number > 3)
+                .map(e -> e * 2)
+                .reduce(0, Integer::sum);
     }
 }
