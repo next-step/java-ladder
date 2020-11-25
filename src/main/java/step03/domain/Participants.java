e5ee993cacaab3683f17b4e3b78d625a5df6afa3
eyabc <bey4314@naver.com>
2020-11-25 23:26:01 +0900

    refactor: Participants 를 Names 의 조합으로 변경

--- src/main/java/step03/domain/Participants.java
+++ src/main/java/step03/domain/Participants.java
@@ -1,79 +1,25 @@
 package step03.domain;
 
-import exception.DuplicatedParticipantsNameException;
-import exception.InvalidCountOfParticipantsException;
-import step03.utils.Tokenizer;
-
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class Participants {
-    private static final Integer MIN_COUNT_OF_PARTICIPANTS = 2;
-    private final List<Name> participants;
+    private final Names names;
 
-
-    private Participants(List<Name> participants) {
-        this.participants = participants;
+    private Participants(Names names) {
+        names.validateUnique();
+        this.names = names;
     }
 
     public static Participants of(String names) {
-        return new Participants(parseNames(names));
-    }
-
-    private static List<String> tokenizeNames(String names) {
-        return Tokenizer.tokenizeWithComma(names);
-    }
-
-    private static void validate(List<String> nameTokens) {
-        if (isDuplicated(nameTokens)) {
-            throw new DuplicatedParticipantsNameException();
-        }
-
-        if (isInValidCountOfParticipant(nameTokens)) {
-            throw new InvalidCountOfParticipantsException();
-        }
-    }
-
-    private static boolean isDuplicated(List<String> nameTokens) {
-        return nameTokens.stream().distinct().count() != nameTokens.size();
-    }
-
-    private static boolean isInValidCountOfParticipant(List<String> nameTokens) {
-        return nameTokens.size() < MIN_COUNT_OF_PARTICIPANTS;
-    }
-
-    private static List<Name> parseNames(String names) {
-        List<String> nameTokens = tokenizeNames(names);
-        validate(nameTokens);
-
-        return nameTokens.stream()
-                .map(Name::of)
-                .collect(Collectors.toList());
-    }
-
-    public Integer size() {
-        return participants.size();
+        return new Participants(Names.of(names));
     }
 
     public List<String> getNames() {
-        return participants.stream()
-                .map(Name::toString)
-                .collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
+        return names.getNames();
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Participants that = (Participants) o;
-        return Objects.equals(participants, that.participants);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(participants);
+    public Integer size() {
+        return names.size();
     }
 
 }
