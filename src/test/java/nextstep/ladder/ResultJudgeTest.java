b117f654810ee2ca2449ebac1856e36f6aef236f
siyoon <siyoon210@gmail.com>
2020-11-26 08:54:44 +0900

    test: MockInputView 삭제

--- src/test/java/nextstep/ladder/ResultJudgeTest.java
+++ src/test/java/nextstep/ladder/ResultJudgeTest.java
@@ -4,11 +4,7 @@ import nextstep.ladder.domain.ExecutionsResults;
 import nextstep.ladder.domain.Height;
 import nextstep.ladder.domain.Ladder;
 import nextstep.ladder.domain.Participants;
-import nextstep.ladder.mock.MockInputView;
 import nextstep.ladder.mock.MockPointsGenerator;
-import nextstep.ladder.util.pointsgenerator.PointsGenerator;
-import nextstep.ladder.view.InputView;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -17,21 +13,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ResultJudgeTest {
-    private InputView mockInputView;
-    private PointsGenerator mockPointsGenerator;
-
-    @BeforeEach
-    void init() {
-        mockInputView = new MockInputView();
-        mockPointsGenerator = new MockPointsGenerator();
-    }
-
     @Test
     void resultJudgeTest() {
         //given
-        Participants participants = Participants.from(mockInputView.getParticipantNames());
-        Height height = Height.valueOf(mockInputView.getHeight());
-        Ladder ladder = Ladder.of(participants, height, mockPointsGenerator);
+        Participants participants = Participants.of("pobi", "honux", "crong", "jk");
+        Height height = Height.valueOf(5);
+        Ladder ladder = Ladder.of(participants, height, new MockPointsGenerator());
         ExecutionsResults executionResults = ExecutionsResults.of("꽝", "5000", "꽝", "3000");
 
         //when
--- src/test/java/nextstep/ladder/mock/MockPointsGenerator.java
+++ src/test/java/nextstep/ladder/mock/MockPointsGenerator.java
@@ -3,14 +3,11 @@ package nextstep.ladder.mock;
 import nextstep.ladder.util.pointsgenerator.PointsGenerator;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 public class MockPointsGenerator implements PointsGenerator {
     private final List<List<Boolean>> mockPoints;
     private int index = 0;
-    private final Iterator<List<Boolean>> lineIterator;
-    private Iterator<Boolean> booleanIterator;
 
     public MockPointsGenerator() {
         mockPoints = Arrays.asList(
@@ -20,19 +17,10 @@ public class MockPointsGenerator implements PointsGenerator {
                 Arrays.asList(false, true, false),
                 Arrays.asList(true, false, true)
         );
-
-        lineIterator = mockPoints.iterator();
     }
 
     @Override
     public List<Boolean> getPoints(int pointsSize) {
         return mockPoints.get(index++);
-    }
-
-    public boolean getMockPoint() {
-        if (booleanIterator == null || !booleanIterator.hasNext()) {
-            booleanIterator = lineIterator.next().iterator();
-        }
-        return booleanIterator.next();
     }
 }
