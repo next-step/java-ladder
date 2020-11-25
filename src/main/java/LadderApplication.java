9220d66f4b424a9ad755df603286f85f7befdaed
kaeum <oror1024@gmail.com>
2020-11-25 16:34:41 +0900

    feat: 사다리 높이를 입력받는 기능 구현

--- src/main/java/LadderApplication.java
+++ src/main/java/LadderApplication.java
@@ -3,5 +3,6 @@ import ui.InputView;
 public class LadderApplication {
     public static void main(String[] args) {
         String names = InputView.askNames();
+        int ladderHeight = InputView.askLadderHeight();
     }
 }
--- src/main/java/ui/InputView.java
+++ src/main/java/ui/InputView.java
@@ -5,9 +5,15 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String INSERT_NAMES = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String INSERT_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
 
     public static String askNames() {
         System.out.println(INSERT_NAMES);
         return scanner.nextLine();
+    }
+    
+    public static int askLadderHeight() {
+        System.out.println(INSERT_LADDER_HEIGHT);
+        return scanner.nextInt();
     }
 }
