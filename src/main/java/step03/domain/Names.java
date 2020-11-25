6b763b0d903ca5d65330d7101510d13d12f9ff5a
eyabc <bey4314@naver.com>
2020-11-25 23:24:49 +0900

    feat: Names

---
+++ src/main/java/step03/domain/Names.java
@@ -0,0 +1,75 @@
+package step03.domain;
+
+import exception.DuplicatedNameException;
+import exception.InvalidCountOfNamesException;
+import step03.utils.Tokenizer;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Names {
+    private static final Integer MIN_COUNT_OF_NAMES = 2;
+    private final List<Name> names;
+
+    private Names(List<Name> names) {
+        this.names = names;
+    }
+
+    public static Names of(String names) {
+        return new Names(parseNames(names));
+    }
+
+    private static List<String> tokenizeNames(String names) {
+        return Tokenizer.tokenizeWithComma(names);
+    }
+
+    private static void validate(List<String> nameTokens) {
+        if (isInValidCount(nameTokens)) {
+            throw new InvalidCountOfNamesException();
+        }
+    }
+
+    public void validateUnique() {
+        if (names.stream().distinct().count() != names.size()) {
+            throw new DuplicatedNameException();
+        }
+    }
+
+    private static boolean isInValidCount(List<String> nameTokens) {
+        return nameTokens.size() < MIN_COUNT_OF_NAMES;
+    }
+
+    private static List<Name> parseNames(String names) {
+        List<String> nameTokens = tokenizeNames(names);
+        validate(nameTokens);
+
+        return nameTokens.stream()
+                .map(Name::of)
+                .collect(Collectors.toList());
+    }
+
+    public Integer size() {
+        return names.size();
+    }
+
+    public List<String> getNames() {
+        return names.stream()
+                .map(Name::toString)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Names names1 = (Names) o;
+        return Objects.equals(names, names1.names);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(names);
+    }
+}
