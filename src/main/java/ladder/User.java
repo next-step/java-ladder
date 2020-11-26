ffca4420bf1e59b9adc43ea5c358fc7096ebf179
홍성민 <honggasm@gmail.com>
2020-11-27 02:33:45 +0900

    feat: user 구현

---
+++ src/main/java/ladder/User.java
@@ -0,0 +1,22 @@
+package ladder;
+
+import util.StringUtils;
+
+import static util.Preconditions.checkArgument;
+
+public class User {
+    public static final String USER_NAME_MUST_NOT_BE_BLANK = "user name must not be blank";
+    public static final String USER_NAME_LENGTH_MUST_NOT_MORE_THEN_FIVE = "user name length must not more then five";
+
+    private final String name;
+
+    private User(final String name) {
+        this.name = name;
+    }
+
+    public static User of(final String userName) {
+        checkArgument(StringUtils.isNotBlank(userName), USER_NAME_MUST_NOT_BE_BLANK);
+        checkArgument(userName.length() <= 5, USER_NAME_LENGTH_MUST_NOT_MORE_THEN_FIVE);
+        return new User(userName);
+    }
+}
