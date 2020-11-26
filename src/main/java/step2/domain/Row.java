68f893b35e9fca9c92b731ac24a069043bc76569
oen142 <oen142@naver.com>
2020-11-26 23:26:59 +0900

    feat : 사람이 한명 미만일때 익셉션이 터진다.

--- src/main/java/step2/domain/Row.java
+++ src/main/java/step2/domain/Row.java
@@ -1,26 +1,47 @@
 package step2.domain;
 
+import step2.exception.PlayerCountException;
+
 public class Row {
 
-    private int[] players;
+    private final int[] players;
 
     public Row(int playersCount) {
+        validPlayerCount(playersCount);
         players = new int[playersCount];
+    }
+    private void validPlayerCount(int playersCount) {
+        if(playersCount < 1){
+            throw new PlayerCountException();
+        }
     }
 
     public void drawLine(int startPosition) {
-        players[startPosition] = 1;
-        players[startPosition + 1] = 1;
+        players[startPosition] = initMoveCondition(MoveCondition.LEFT);
+        players[startPosition + 1] = initMoveCondition(MoveCondition.RIGHT);
+    }
+
+    private int initMoveCondition(MoveCondition condition) {
+        return condition.getMoveCondition();
     }
 
     public int move(int personPosition) {
-        if (players[personPosition] == 0) {
+        if (moveNot(personPosition)) {
             return personPosition;
         }
-        if (personPosition - 1 >= 0 && players[personPosition - 1] == 1) {
-            return personPosition - 1;
+        if (moveLeft(personPosition)) {
+            return personPosition + 1;
         }
-        return personPosition + 1;
+        return personPosition - 1;
     }
+
+    private boolean moveNot(int personPosition) {
+        return players[personPosition] == initMoveCondition(MoveCondition.CENTER);
+    }
+
+    private boolean moveLeft(int personPosition) {
+        return players[personPosition] == initMoveCondition(MoveCondition.LEFT);
+    }
+
 
 }
