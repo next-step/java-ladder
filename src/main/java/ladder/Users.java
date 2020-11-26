15dbd0ea773cc5dc8d1d09d0b3272160dc0a3eec
홍성민 <honggasm@gmail.com>
2020-11-27 02:34:13 +0900

    feat: users 추가

---
+++ src/main/java/ladder/Users.java
@@ -0,0 +1,22 @@
+package ladder;
+
+import util.CollectionUtil;
+
+import java.util.List;
+
+import static util.Preconditions.checkArgument;
+
+public class Users {
+    public static final String NUMBER_OF_USER_MUST_BE_MORE_THEN_TWO = "number of user must be more then two";
+    private final List<User> users;
+
+    private Users(final List<User> users) {
+        this.users = users;
+    }
+
+    public static Users of(final List<User> userList) {
+        checkArgument(CollectionUtil.isNotEmpty(userList), NUMBER_OF_USER_MUST_BE_MORE_THEN_TWO);
+        checkArgument(userList.size() >= 2, NUMBER_OF_USER_MUST_BE_MORE_THEN_TWO);
+        return new Users(userList);
+    }
+}
--- src/main/java/util/CollectionUtil.java
+++ src/main/java/util/CollectionUtil.java
@@ -3,7 +3,11 @@ package util;
 import java.util.Collection;
 
 public class CollectionUtil {
-    public static boolean isEmpty(Collection<?> collection) {
+    public static boolean isNotEmpty(final Collection<?> collection) {
+        return !isEmpty(collection);
+    }
+
+    public static boolean isEmpty(final Collection<?> collection) {
         return collection == null || collection.isEmpty();
     }
 }
