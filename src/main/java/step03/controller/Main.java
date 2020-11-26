d915a479700b64c9a4c331e48b1a324322bfe969
eyabc <bey4314@naver.com>
2020-11-26 17:28:57 +0900

    feat: controller

--- src/main/java/step03/controller/Main.java
+++ src/main/java/step03/controller/Main.java
@@ -1,9 +1,9 @@
 package step03.controller;
 
-import step03.domain.Participant;
+import step03.domain.Ladder;
+import step03.domain.Matcher;
+import step03.domain.NamesOfResult;
 import step03.domain.Participants;
-import step03.domain.Steps;
-import step03.domain.StepGenerator;
 import step03.view.InputView;
 import step03.view.ResultView;
 import strategy.RandomPointStrategy;
@@ -12,17 +12,37 @@ public class Main {
     public static void main(String[] args) {
 
         Participants participants = readParticipants();
+        NamesOfResult namesOfResult = readNamesOfResults();
 
+        Matcher.Builder matcherBuilder = Matcher.Builder.of(participants)
+                .namesOfResult(namesOfResult);
 
-        Integer heightOfLadder = InputView.readCountOfSteps();
-        StepGenerator stepGenerator = StepGenerator.of(participants, RandomPointStrategy.of());
-        Steps steps = Steps.of(heightOfLadder, stepGenerator);
-        ResultView.print(participants, steps);
+        Integer countOfSteps = InputView.readCountOfSteps();
 
+        Ladder ladder = Ladder.of(countOfSteps, participants.size(), RandomPointStrategy.of());
+        ResultView.printLadderWithNames(participants, namesOfResult, ladder.getLadder());
+
+        Matcher matcher = matcherBuilder.matchedTable(ladder.getResultTable()).build();
+
+        printResult(matcher);
     }
 
     public static Participants readParticipants() {
         String namesOfParticipants = InputView.readNamesOfParticipants();
         return Participants.of(namesOfParticipants);
+    }
+
+    public static NamesOfResult readNamesOfResults() {
+        String resultsOfLADDER = InputView.readResultsOfLadder();
+        return NamesOfResult.of(resultsOfLADDER);
+    }
+
+    public static void printResult(Matcher matcher) {
+        String target = InputView.readNamesOfParticipant();
+        while(!target.equals("all")) {
+            ResultView.printResult(matcher.getResultByParticipant(target));
+            target = InputView.readNamesOfParticipant();
+        }
+        ResultView.printResults(matcher);
     }
 }
--- src/main/java/step03/view/ResultView.java
+++ src/main/java/step03/view/ResultView.java
@@ -1,18 +1,20 @@
 package step03.view;
 
-import step03.domain.Participants;
-import step03.domain.Steps;
+import step03.domain.*;
 
 import java.util.List;
+import java.util.Map;
 
 public class ResultView {
-    private static final String RESULT = "실행결과";
+    private static final String EXECUTE_RESULT = "실행결과";
+    private static final String LADDER_RESULT = "사다리결과";
     private static final String STEP = "|";
     private static final String LINE = "-----";
     private static final String EMPTY = "";
     private static final String NAME_SPACE = padLeft(EMPTY, 1);
     private static final String FIRST_BLANK = padLeft(EMPTY, 4);
     private static final String BLANK = padLeft(EMPTY, 5);
+    private static final String ALL_RESULTS = "%s : %s";
 
     private ResultView() {}
 
@@ -20,20 +22,19 @@ public class ResultView {
         return String.format("%" + repeat + "s", string);
     }
 
-    public static void print(Participants participants, Steps steps) {
-        System.out.println(RESULT);
-        printNamesOfParticipants(participants);
-        printLadder(steps);
+    public static void printLadderWithNames(Participants participants, NamesOfResult namesOfResult, List<List<Boolean>> ladder) {
+        System.out.println(LADDER_RESULT);
+        printNames(participants.getNames());
+        printLadder(ladder);
+        printNames(namesOfResult.getNames());
     }
-    private static void printNamesOfParticipants(Participants participants) {
-        participants.getNames()
-                .forEach(name -> System.out.print(padLeft(name, 5) + NAME_SPACE));
+    private static void printNames(List<String> names) {
+        names.forEach(name -> System.out.print(padLeft(name, 5) + NAME_SPACE));
         System.out.println();
     }
 
-    private static void printLadder(Steps steps) {
-        steps.getSteps()
-                .forEach(step -> {
+    private static void printLadder(List<List<Boolean>> ladder) {
+        ladder.forEach(step -> {
                     System.out.print(FIRST_BLANK + STEP);
                     printLine(step);
                     System.out.println();
@@ -44,6 +45,21 @@ public class ResultView {
         step.forEach(point -> {
             String drawing = point ? LINE : BLANK;
             System.out.print(drawing + STEP);
+        });
+    }
+
+    public static void printResult(String nameOfResult) {
+        System.out.println(EXECUTE_RESULT);
+        System.out.println(nameOfResult);
+    }
+
+    public static void printResults(Matcher matcher) {
+        System.out.println(EXECUTE_RESULT);
+
+        Map<String, String> map = matcher.getResults();
+        map.forEach((key, value) -> {
+            System.out.printf(ALL_RESULTS, key, value);
+            System.out.println();
         });
     }
 
