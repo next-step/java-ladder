5e6cf857ab7685bf789238e8f972c13b6b6947c2
siyoon <siyoon210@gmail.com>
2020-11-27 08:58:28 +0900

    test: Result 입력 길이 유효성 검사 추가

--- src/main/java/nextstep/ladder/domain/Result.java
+++ src/main/java/nextstep/ladder/domain/Result.java
@@ -3,14 +3,25 @@ package nextstep.ladder.domain;
 import java.util.function.Consumer;
 
 public class Result {
+    public static final String INVALID_RESULT_LENGTH_ERR_MSG = "실행결과는 1글자 이상 5글자 이하만 허용합니다.";
+    private static final int MIN_AVAILABLE_LENGTH = 1;
+    private static final int MAX_AVAILABLE_LENGTH = 5;
     private final String value;
 
     private Result(String value) {
         this.value = value;
+        validateLength();
     }
 
     public static Result valueOf(String value) {
         return new Result(value);
+    }
+
+    private void validateLength() {
+        int length = value.length();
+        if (length < MIN_AVAILABLE_LENGTH || length > MAX_AVAILABLE_LENGTH) {
+            throw new IllegalArgumentException(INVALID_RESULT_LENGTH_ERR_MSG);
+        }
     }
 
     public String getValue() {
---
+++ src/test/java/nextstep/ladder/ResultTest.java
@@ -0,0 +1,27 @@
+package nextstep.ladder;
+
+import nextstep.ladder.domain.Result;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
+public class ResultTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"", "123456"})
+    @DisplayName("실행결과 입력값이 1글자 미만 5글자 초과면 throw Exception")
+    void nameTestFailureCase(String invalidResult) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> Result.valueOf(invalidResult));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "12345"})
+    @DisplayName("실행결과 입력값의 유효한 길이는 1글자 이상 5글자 이하다.")
+    void nameTestSuccessCase(String validResult) {
+        assertThatCode(() -> Result.valueOf(validResult))
+                .doesNotThrowAnyException();
+    }
+}
