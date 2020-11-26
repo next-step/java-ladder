7c654dbfad9c97f9681e9e6098d13a05e5e0ccbf
oen142 <oen142@naver.com>
2020-11-27 00:54:49 +0900

    feat : 익셉션 정의

--- src/main/java/step2/exception/NameLengthException.java
+++ src/main/java/step2/exception/NameLengthException.java
@@ -1,7 +1,10 @@
 package step2.exception;
 
 public class NameLengthException extends IllegalArgumentException{
+
+    public static final String NAME_LENGTH_EXCEPTION = "이름은 5글자까지 입니다.";
+
     public NameLengthException() {
-        super("이름은 5글자까지 입니다.");
+        super(NAME_LENGTH_EXCEPTION);
     }
 }
