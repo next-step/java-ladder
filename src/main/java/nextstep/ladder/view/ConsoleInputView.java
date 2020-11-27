c76fb2dc1a53f0cdb75cfd838a3304d844bafa6c
siyoon <siyoon210@gmail.com>
2020-11-27 10:27:23 +0900

    refactor: 메시지 상수화

--- src/main/java/nextstep/ladder/view/ConsoleInputView.java
+++ src/main/java/nextstep/ladder/view/ConsoleInputView.java
@@ -9,6 +9,7 @@ public class ConsoleInputView implements InputView {
     private static final String PARTICIPANT_NAMES_INPUT_MSG = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String HEIGHT_INPUT_MSG = "최대 사다리 높이는 몇 개인가요?";
     private static final String RESULTS_INPUT_MSG = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private static final String NAME_OF_WANT_TO_CHECK_INPUT_MSG = "결과를 보고 싶은 사람은?";
     private static final String ERROR_MSG_PREFIX = "[ERROR] ";
     private static final String MULTIPLE_INPUT_DELIMITER = ",";
     private final Scanner scanner;
@@ -41,7 +42,7 @@ public class ConsoleInputView implements InputView {
 
     @Override
     public String getNameOfWantToCheck() {
-        System.out.println("결과를 보고 싶은 사람은?");
+        System.out.println(NAME_OF_WANT_TO_CHECK_INPUT_MSG);
         return scanner.nextLine();
     }
 
