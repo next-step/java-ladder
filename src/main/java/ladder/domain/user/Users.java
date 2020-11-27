de249f469d43527b51d64c30fde0623f2bc363d8
SeungwanJo <wh7923@naver.com>
2020-11-27 16:26:35 +0900

    feature (참가자관련 클래스 작성)

---
+++ src/main/java/ladder/domain/user/Users.java
@@ -0,0 +1,67 @@
+package ladder.domain.user;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import ladder.exception.LadderGameException;
+
+public class Users {
+
+    private static final int MIN_USER_COUNT = 2;
+    private static final int DUPLICATE_USER_CHECK_COUNT = 1;
+    private static final String USER_SHOULD_EQUAL_OR_OVER_N = "참가자 수는 %d명 이상이어야 합니다.";
+    private static final String PLEASE_INPUT_NOT_DUPLICATE_USER = "중복되지 않은 참가자를 입력해주세요.";
+
+    private final List<User> users;
+
+    public Users(List<User> users) {
+        checkUserCount(users);
+        checkUserName(users);
+        this.users = users;
+    }
+
+    public int getUsersCount() {
+        return users.size();
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    private void checkUserName(List<User> users) {
+        long duplicateUserNameCount = users.stream()
+            .map(User::getName)
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+            .entrySet().stream()
+            .filter(m -> m.getValue() > 1)
+            .count();
+
+        if (duplicateUserNameCount >= DUPLICATE_USER_CHECK_COUNT) {
+            throw new LadderGameException(PLEASE_INPUT_NOT_DUPLICATE_USER);
+        }
+    }
+
+    private void checkUserCount(List<User> users) {
+        if (users.size() < MIN_USER_COUNT) {
+            throw new LadderGameException(String.format(USER_SHOULD_EQUAL_OR_OVER_N, MIN_USER_COUNT));
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Users users1 = (Users) o;
+        return Objects.equals(users, users1.users);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(users);
+    }
+}
---
+++ src/test/java/ladder/domain/user/UsersTest.java
@@ -0,0 +1,18 @@
+package ladder.domain.user;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Arrays;
+import ladder.domain.Position;
+import ladder.exception.LadderGameException;
+import org.junit.jupiter.api.Test;
+
+class UsersTest {
+
+    @Test
+    void 객체생성_실패_테스트() {
+        User user1 = new User("test", new Position(0));
+        User user2 = new User("test", new Position(1));
+        assertThrows(LadderGameException.class, () -> new Users(Arrays.asList(user1, user2)));
+    }
+}
