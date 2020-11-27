b4044367482293abdafceee0585ba88aaa8f92d0
oen142 <oen142@naver.com>
2020-11-27 21:48:23 +0900

    feat : 이름을 가지는 player 구현

---
+++ src/main/java/step2/domain/Player.java
@@ -0,0 +1,38 @@
+package step2.domain;
+
+import step2.exception.NameLengthException;
+
+import java.util.Objects;
+
+public class Player {
+
+    private final String name;
+
+    public Player(final String name) {
+        validationName(name);
+        this.name = name;
+    }
+
+    private void validationName(final String name) {
+        if (name.length() > 5) {
+            throw new NameLengthException();
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
