5f598ad73b148668c26497a9466865ea12a2906b
oen142 <oen142@naver.com>
2020-11-29 01:09:23 +0900

    feat : 플레이어 생성

--- src/main/java/step2/domain/RandomLineStrategy.java
+++ src/main/java/step2/domain/RandomLineStrategy.java
@@ -7,22 +7,36 @@ import java.util.stream.IntStream;
 
 public class RandomLineStrategy implements LineStrategy {
     private final Random random = new Random();
-    private boolean previousLadder = false;
+    private Direction previousLadder;
 
     @Override
-    public List<Boolean> createLine(int personCount) {
-        return IntStream.range(1, personCount)
-                .mapToObj(this::randomLine)
+    public List<LadderPoint> createLine(int personCount) {
+        previousLadder = Direction.DOWN;
+        return IntStream.range(0, personCount)
+                .mapToObj(position ->randomLine( position , personCount))
                 .collect(Collectors.toList());
     }
 
-    private Boolean randomLine(int position) {
-        if (previousLadder) {
-            previousLadder = false;
-            return false;
+    private LadderPoint randomLine(int position, int personCount) {
+
+        if (previousLadder == Direction.RIGHT) {
+            previousLadder = Direction.LEFT;
+            return new LadderPoint(position, previousLadder);
+        }
+        if (previousLadder == Direction.LEFT) {
+            previousLadder = Direction.DOWN;
+            return new LadderPoint(position, previousLadder);
         }
 
-        previousLadder = random.nextBoolean();
-        return previousLadder;
+        if (random.nextBoolean() && personCount-1 != position) {
+            previousLadder = Direction.RIGHT;
+            return new LadderPoint(position, previousLadder);
+
+        }
+
+        previousLadder = Direction.DOWN;
+        return new LadderPoint(position, previousLadder);
+
     }
+
 }
