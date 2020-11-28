a0c24a6a28cb063bb609021ef4bcf7afb90c8eea
kit(김광수) <kit.t@kakaocorp.com>
2020-11-28 18:35:12 +0900

    test: 입력받은 이름을 분리한다

---
+++ src/test/java/nextstep/ladder/InputViewTest.java
@@ -0,0 +1,12 @@
+package nextstep.ladder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InputViewTest {
+    @Test
+    void splitNames() {
+        assertThat(InputView.splitName("white,blue,green,red")).containsExactly("white", "blue", "green", "red");
+    }
+}
