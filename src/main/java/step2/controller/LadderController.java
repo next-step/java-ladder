37bffe12bed7bf2c9229b9ab72b0e8bd7f8782ee
oen142 <oen142@naver.com>
2020-11-28 22:17:24 +0900

    fix : DTO로 컨트롤러에서 도메인 넘겨주기

--- src/main/java/step2/controller/LadderController.java
+++ src/main/java/step2/controller/LadderController.java
@@ -1,9 +1,8 @@
 package step2.controller;
 
-import step2.domain.Ladder;
-import step2.domain.NameSplitter;
-import step2.domain.Players;
-import step2.domain.RandomLineStrategy;
+import step2.domain.*;
+import step2.dto.LadderDto;
+import step2.dto.PlayersRewardsDto;
 import step2.view.InputView;
 import step2.view.OutputView;
 
@@ -14,12 +13,15 @@ public class LadderController {
 
     public static void runLadder() {
         Players players = Players.of(NameSplitter.splitParticipationNames(InputView.putParticipation()));
+        Rewards rewards = Rewards.of(NameSplitter.splitParticipationNames(InputView.putReward()));
         int ladderHeight = InputView.putLadderHeight();
 
         OutputView.printResult();
-        Ladder ladder = Ladder.of(players, ladderHeight, new RandomLineStrategy());
+        Ladder ladder = Ladder.of(new PlayersRewardsDto(players, rewards), new LadderDto(ladderHeight, new RandomLineStrategy()));
 
         OutputView.printPlayers(players);
         OutputView.printLadder(ladder);
+        OutputView.printRewards(rewards);
+
     }
 }
