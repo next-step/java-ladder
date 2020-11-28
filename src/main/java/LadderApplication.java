b0a9744b0c00cdfab8cccb573ff04faa8435db7d
Kaeum <oror1024@gmail.com>
2020-11-28 11:16:19 +0900

    refactor: 하위 수준의 Exception을 던지도록 수정

--- src/main/java/LadderApplication.java
+++ src/main/java/LadderApplication.java
@@ -1,11 +1,12 @@
 import domain.LadderGame;
 import domain.Length;
 import domain.PlayerNames;
+import exception.NegativeLengthException;
 import ui.InputView;
 import ui.ResultView;
 
 public class LadderApplication {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws NegativeLengthException {
         String names = InputView.askNames();
         int ladderHeight = InputView.askLadderHeight();
 
