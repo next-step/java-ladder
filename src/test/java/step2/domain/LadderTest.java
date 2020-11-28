95ce6d691d6f06b00beb78d6b6bc7e7916dba2f8
oen142 <oen142@naver.com>
2020-11-28 22:17:41 +0900

    test fix : DTO로 변경하면서 나는 컴파일 에러 변경

--- src/test/java/step2/domain/LadderTest.java
+++ src/test/java/step2/domain/LadderTest.java
@@ -2,7 +2,10 @@ package step2.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step2.dto.LadderDto;
+import step2.dto.PlayersRewardsDto;
 import step2.exception.LadderHeightException;
+import step2.exception.NotMatchPlayerRewardsCountException;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -13,19 +16,32 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class LadderTest {
 
     @Test
-    @DisplayName("네임이 5글자를 초과하는경우 익셉션 발생")
+    @DisplayName("사다리 높이가 0일경우 익셉션 발생")
     void throwLadderHeightException() {
-        assertThatThrownBy(() -> Ladder.of(Players.of(Collections.emptyList()), 0, new MustLineStrategy()))
+        assertThatThrownBy(() -> Ladder.of(
+                new PlayersRewardsDto(Players.of(Collections.emptyList()), Rewards.of(Collections.emptyList())),
+                new LadderDto(0, new MustLineStrategy())))
                 .isInstanceOf(LadderHeightException.class);
+    }
+
+    @Test
+    @DisplayName("리워드의 숫자와 플레이어의 숫자가 일치하지 않을경우 익셉션 발생")
+    void throwPlayerRewardsCountException() {
+        assertThatThrownBy(() -> Ladder.of(
+                new PlayersRewardsDto(Players.of(Arrays.asList("name")), Rewards.of(Collections.emptyList())),
+                new LadderDto(1, new MustLineStrategy())))
+                .isInstanceOf(NotMatchPlayerRewardsCountException.class);
     }
 
     @Test
     @DisplayName("Ladder가 잘 생성되는지 확인한다.")
     void create() {
-        Ladder ladder = Ladder.of(Players.of(Arrays.asList("a","b")), 2, new MustLineStrategy());
+        Ladder ladder = Ladder.of(new PlayersRewardsDto(Players.of(Arrays.asList("a", "b")), Rewards.of(Arrays.asList("a", "b")))
+                , new LadderDto(2, new MustLineStrategy()));
 
         assertThat(ladder)
-                .isEqualTo(Ladder.of(Players.of(Arrays.asList("a","b")), 2, new MustLineStrategy()));
+                .isEqualTo(Ladder.of(new PlayersRewardsDto(Players.of(Arrays.asList("a", "b")), Rewards.of(Arrays.asList("a", "b")))
+                        , new LadderDto(2, new MustLineStrategy())));
     }
 
 }
