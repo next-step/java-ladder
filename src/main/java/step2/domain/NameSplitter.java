5a3bd2b23de9aa70f767f048e79ce5af5c0a6555
oen142 <oen142@naver.com>
2020-11-29 01:09:05 +0900

    feat : 이름이 빈공백일경우 trim , 아예 빈값일 경우 Exception 발생

--- src/main/java/step2/domain/NameSplitter.java
+++ src/main/java/step2/domain/NameSplitter.java
@@ -1,16 +1,27 @@
 package step2.domain;
 
+import step2.exception.EmptyNameException;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class NameSplitter {
 
-    private NameSplitter() { }
+    private NameSplitter() {
+    }
 
     public static List<String> splitParticipationNames(String participationNames) {
+        validEmpty(participationNames);
         return Arrays.stream(splitName(participationNames))
+                .map(String::trim)
                 .collect(Collectors.toList());
+    }
+
+    private static void validEmpty(String participationNames) {
+        if(participationNames.isEmpty()){
+            throw new EmptyNameException();
+        }
     }
 
     private static String[] splitName(String participationNames) {
