e90a9329c6e032b2bf8945e81e2587de33cc4a6a
siyoon <siyoon210@gmail.com>
2020-11-30 07:35:48 +0900

    test: Direction의 left와 right가 모두 true인 경우 throw Exception

--- src/test/java/nextstep/ladder/step4/DirectionTest.java
+++ src/test/java/nextstep/ladder/step4/DirectionTest.java
@@ -1,12 +1,21 @@
 package nextstep.ladder.step4;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class DirectionTest {
     @Test
     void init() {
         assertThat(Direction.of(true, false)).isEqualTo(Direction.of(true, false));
+    }
+
+    @Test
+    @DisplayName("Direction의 left와 right가 모두 true인 경우 throw Exception")
+    void invalidStateTest() {
+        assertThatExceptionOfType(IllegalStateException.class)
+                .isThrownBy(() -> Direction.of(true, true));
     }
 }
