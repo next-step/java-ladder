d651350ca5dff528428699718a50313e6d7b2e70
changgunyee <minyoung403@naver.com>
2020-11-29 18:04:05 +0900

    feat: check name size

--- src/main/java/nextstep/ladder/Player.java
+++ src/main/java/nextstep/ladder/Player.java
@@ -6,6 +6,9 @@ public class Player {
     private String name;
 
     public Player(String name) {
+        if (name.length() > 5)
+            throw new IllegalArgumentException("name must be over 5");
+
         this.name = name;
     }
 
