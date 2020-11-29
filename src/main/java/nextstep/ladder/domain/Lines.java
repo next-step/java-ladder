28d6c59b51fb6df9f80a1ac5f46021f99ed8a765
changgunyee <minyoung403@naver.com>
2020-11-29 23:10:40 +0900

    refactor: add length and remove strategy from Lines

--- src/main/java/nextstep/ladder/domain/Lines.java
+++ src/main/java/nextstep/ladder/domain/Lines.java
@@ -6,16 +6,18 @@ import java.util.stream.IntStream;
 
 public class Lines {
     private final List<Line> lines;
-    private final ConnectionCreationStrategy connectionCreationStrategy;
 
-    public Lines(List<Line> lines, ConnectionCreationStrategy connectionCreationStrategy) {
+    public Lines(List<Line> lines) {
         this.lines = lines;
-        this.connectionCreationStrategy = connectionCreationStrategy;
     }
 
-    public static Lines fromHeight(int height, ConnectionCreationStrategy connectionCreationStrategy) {
+    public static Lines fromHeight(int length, int height, ConnectionCreationStrategy connectionCreationStrategy) {
         return new Lines(IntStream.range(0, height)
-                .mapToObj(idx -> new Line(idx, connectionCreationStrategy))
-                .collect(Collectors.toList()), connectionCreationStrategy);
+                .mapToObj(idx -> new Line(length, connectionCreationStrategy))
+                .collect(Collectors.toList()));
+    }
+
+    public List<Line> getLines() {
+        return lines;
     }
 }
