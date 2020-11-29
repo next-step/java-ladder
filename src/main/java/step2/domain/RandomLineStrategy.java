db117e9c71b81c9eb92f7b04a990afb55e2305e8
oen142 <oen142@naver.com>
2020-11-30 00:35:11 +0900

    feat : 게임 라인긋는 전략 변경

--- src/main/java/step2/domain/RandomLineStrategy.java
+++ src/main/java/step2/domain/RandomLineStrategy.java
@@ -10,33 +10,29 @@ public class RandomLineStrategy implements LineStrategy {
     private Direction previousLadder;
 
     @Override
-    public List<LadderPoint> createLine(int personCount) {
+    public List<Direction> createLine(int personCount) {
         previousLadder = Direction.DOWN;
         return IntStream.range(0, personCount)
-                .mapToObj(position ->randomLine( position , personCount))
+                .mapToObj(position -> randomLine(position, personCount))
                 .collect(Collectors.toList());
     }
 
-    private LadderPoint randomLine(int position, int personCount) {
+    private Direction randomLine(int position, int personCount) {
 
         if (previousLadder == Direction.RIGHT) {
             previousLadder = Direction.LEFT;
-            return new LadderPoint(position, previousLadder);
+            return previousLadder;
         }
         if (previousLadder == Direction.LEFT) {
             previousLadder = Direction.DOWN;
-            return new LadderPoint(position, previousLadder);
+            return previousLadder;
         }
-
-        if (random.nextBoolean() && personCount-1 != position) {
+        if (random.nextBoolean() && personCount - 1 != position) {
             previousLadder = Direction.RIGHT;
-            return new LadderPoint(position, previousLadder);
-
+            return previousLadder;
         }
-
         previousLadder = Direction.DOWN;
-        return new LadderPoint(position, previousLadder);
-
+        return previousLadder;
     }
 
 }
