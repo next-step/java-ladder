c60e797ad9847551b44bbf46485bd94336d0d5e6
changgunyee <minyoung403@naver.com>
2020-11-29 23:23:18 +0900

    refactor: clean main function

--- src/main/java/nextstep/ladder/Main.java
+++ src/main/java/nextstep/ladder/Main.java
@@ -8,10 +8,9 @@ import nextstep.ladder.view.OutputView;
 
 public class Main {
     public static void main(String[] args) {
-        String nameString = InputView.getNames();
-        Players players = Players.from(nameString);
-
+        Players players = Players.from(InputView.getNames());
         int height = InputView.getHeight();
+        
         Lines lines = Lines.fromHeight(players.getSize() - 1, height, new ConnectionCreationStrategyImpl());
         
         OutputView.showPlayers(players);
