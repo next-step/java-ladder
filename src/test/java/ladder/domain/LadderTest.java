b46e759975cac7188b3346af25b410a0bbe73f92
devyoon91 <kimbyungyoun91@gmail.com>
2020-11-29 14:25:00 +0900

    [feat] : Ladder 테스트 케이스 추가

---
+++ src/test/java/ladder/domain/LadderTest.java
@@ -0,0 +1,35 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LadderTest {
+
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "5:true,false,true,false,false",
+            "4:true,false,true,false",
+            "3:true,false,false",
+            "2:true,false"
+    }, delimiter = ':')
+    @DisplayName("이동가능 여부를 참으로 할 경우 결과값 확인")
+    void ladder_createLines(int countOfPerson, String points) {
+        Lines lines = Ladder.createLines(countOfPerson,1,() -> true);
+        Line actual = lines.getLines().get(0);
+
+        List<Boolean> pointList = Arrays.stream(points.split(","))
+                .map(Boolean::parseBoolean)
+                .collect(Collectors.toList());
+        Line expected = new Line(pointList);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
