d0189641bb8a3db2583b0366cd484438b7de58f2
devyoon91 <kimbyungyoun91@gmail.com>
2020-11-29 14:21:00 +0900

    [feat] : Line 객체 추가, Line 일급컬렉션 추가

---
+++ src/main/java/ladder/domain/Lines.java
@@ -0,0 +1,16 @@
+package ladder.domain;
+
+import java.util.List;
+
+public class Lines {
+
+    private final List<Line> lines;
+
+    public Lines(List<Line> lines) {
+        this.lines = lines;
+    }
+
+    public List<Line> getLines() {
+        return lines;
+    }
+}
