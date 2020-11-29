db304bf4c6e8b477f2a8cf97424ee2ad8d399e19
siyoon <siyoon210@gmail.com>
2020-11-30 08:48:51 +0900

    test: first point의 right가 true이면 다음 인덱스는 1, false 이면 다음 인덱스가 0이다.

---
+++ src/test/java/nextstep/ladder/step4/PointTest.java
@@ -0,0 +1,17 @@
+package nextstep.ladder.step4;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class PointTest {
+    @Test
+    @DisplayName("first point의 right가 true이면 다음 인덱스는 1, false 이면 다음 인덱스가 0이다.")
+    void firstPointTest() {
+        assertAll(
+                () ->  assertThat(Point.first(true).move()).isEqualTo(1),
+                () ->  assertThat(Point.first(false).move()).isEqualTo(0)
+        );
+    }
+}
