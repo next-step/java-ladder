bd74fceaee6f38519e1a2c0b7e0b75ddb1c1314d
changgunyee <minyoung403@naver.com>
2020-11-29 19:59:38 +0900

    feat: create Lines

---
+++ src/main/java/nextstep/ladder/domain/Lines.java
@@ -0,0 +1,19 @@
+package nextstep.ladder.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Lines {
+    private final List<Line> lines;
+
+    public Lines(List<Line> lines) {
+        this.lines = lines;
+    }
+
+    public static Lines fromHeight(int height) {
+        return new Lines(IntStream.range(0, height)
+                .mapToObj(Line::new)
+                .collect(Collectors.toList()));
+    }
+}
---
+++ src/test/java/nextstep/ladder/domain/LinesTest.java
@@ -0,0 +1,12 @@
+package nextstep.ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LinesTest {
+    @Test
+    @DisplayName("높이만큼 라인 생성")
+    void createLines() {
+        Lines lines = Lines.fromHeight(3);
+    }
+}
