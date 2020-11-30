95d0481d54dd9870f379359e3671d012411347b8
changgunyee <minyoung403@naver.com>
2020-11-30 23:34:18 +0900

    refactor: remove unused property length

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -5,15 +5,13 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Line {
-    private final int length;
     private final Connections connections;
 
     public Line(int length, ConnectionCreationStrategy connectionCreationStrategy) {
-        this.length = length;
-        this.connections = createConnections(connectionCreationStrategy);
+        this.connections = createConnections(length, connectionCreationStrategy);
     }
 
-    private Connections createConnections(ConnectionCreationStrategy connectionCreationStrategy) {
+    private Connections createConnections(int length, ConnectionCreationStrategy connectionCreationStrategy) {
         ConnectionCreator connectionCreator = new ConnectionCreator();
 
         return new Connections(IntStream.range(0, length)
