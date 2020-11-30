6776b08b145a121121fabcedf3e10ad8fa428ea3
kyeongmin <kyeongmin.jake.lee@gmail.com>
2020-11-30 20:37:38 +0900

    feat - 참여자 목록에서 참여자 분리 기능

--- src/main/java/nextstep/ladder/controller/LadderController.java
+++ src/main/java/nextstep/ladder/controller/LadderController.java
@@ -1,13 +1,28 @@
 package nextstep.ladder.controller;
 
+import nextstep.ladder.domain.InputUsers;
+import nextstep.ladder.domain.User;
 import nextstep.ladder.domain.Users;
 import nextstep.ladder.view.InputView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LadderController {
 
     public void start() {
         InputView inputView = new InputView();
 
-        Users users = new Users(inputView.inputParticipants());
+        InputUsers inputUsers = new InputUsers(inputView.inputParticipants());
+        Users users = initUsers(inputUsers);
+    }
+
+    public Users initUsers(InputUsers inputUsers) {
+        List<User> userList = Arrays.stream(inputUsers.splitUsers())
+                .map(User::new)
+                .collect(Collectors.toList());
+
+        return new Users(userList);
     }
 }
--- src/main/java/nextstep/ladder/domain/User.java
+++ src/main/java/nextstep/ladder/domain/User.java
@@ -1,26 +1,24 @@
 package nextstep.ladder.domain;
 
-import java.util.List;
 import java.util.Objects;
 
-public class Users {
-    private List<User> users;
+public class User {
+    private String name;
 
-    public Users(List<User> users) {
-        this.users = users;
+    public User(String name) {
+        this.name = name;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Users users1 = (Users) o;
-        return Objects.equals(users, users1.users);
+        User user = (User) o;
+        return Objects.equals(name, user.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(users);
+        return Objects.hash(name);
     }
-
 }
