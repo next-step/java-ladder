ef4e54d9b8447da742deb5620935d01692f4f84b
siyoon <siyoon210@gmail.com>
2020-12-01 08:33:03 +0900

    feat: LadderLine sizeOfPerson으로 초기화하기

--- src/main/java/nextstep/ladder/step4/LadderLine.java
+++ src/main/java/nextstep/ladder/step4/LadderLine.java
@@ -1,16 +1,42 @@
 package nextstep.ladder.step4;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static nextstep.ladder.step4.LadderPointGenerator.generatePoint;
 
 public class LadderLine {
     private final List<Point> points;
 
-    public LadderLine(List<Point> points) {
+    private LadderLine(List<Point> points) {
         this.points = points;
     }
 
     public static LadderLine init(int sizeOfPerson) {
-        return null;
+        List<Point> points = new ArrayList<>();
+        Point point = initFirst(points);
+        point = initBody(sizeOfPerson, points, point);
+        initLast(points, point);
+        return new LadderLine(points);
+    }
+
+    private static Point initFirst(List<Point> points) {
+        Point point = Point.first(generatePoint());
+        points.add(point);
+        return point;
+    }
+
+    private static Point initBody(int sizeOfPerson, List<Point> points, Point point) {
+        for (int i = 1; i < sizeOfPerson - 1; i++) {
+            point = point.next();
+            points.add(point);
+        }
+        return point;
+    }
+
+    private static void initLast(List<Point> points, Point point) {
+        point = point.last();
+        points.add(point);
     }
 
     @Override
