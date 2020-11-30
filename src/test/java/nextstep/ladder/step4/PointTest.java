5ed5209ce587d1640a26a0a1cec7a376a56ba925
siyoon <siyoon210@gmail.com>
2020-11-30 09:06:00 +0900

    test: left Direction, right Direction의 다음(아래) Point index 테스트 추가

--- src/test/java/nextstep/ladder/step4/PointTest.java
+++ src/test/java/nextstep/ladder/step4/PointTest.java
@@ -22,4 +22,18 @@ public class PointTest {
         Point point = Point.first(false).next(false);
         assertThat(point.move()).isEqualTo(1);
     }
+
+    @Test
+    @DisplayName("left Direction인 경우 다음(아래) Point 인덱스는 현재 Point 인덱스 보다 1 작다.")
+    void leftTest() {
+        Point point = Point.first(true).next();
+        assertThat(point.move()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("right Direction인 경우 다음(아래) Point 인덱스는 현재 Point 인덱스 보다 1 크다.")
+    void rightTest() {
+        Point point = Point.first(false).next(true);
+        assertThat(point.move()).isEqualTo(2);
+    }
 }
