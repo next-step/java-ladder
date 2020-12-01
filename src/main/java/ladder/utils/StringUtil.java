095f4cb1ac93432f076687b36c6ab902fe3a1af1
dbeod2 <darung.0211@gmail.com>
2020-12-01 23:48:59 +0900

    feat : StringUtil padRight 메서드 추가

--- src/main/java/ladder/utils/StringUtil.java
+++ src/main/java/ladder/utils/StringUtil.java
@@ -2,9 +2,14 @@ package ladder.utils;
 
 public class StringUtil {
 
-    private StringUtil() {}
+    private StringUtil() {
+    }
 
     public static boolean isEmpty(String value) {
         return null == value || value.trim().isEmpty();
+    }
+
+    public static String padRight(String value, int repeat) {
+        return String.format("%-" + repeat + "s", value);
     }
 }
