2828d75d4a266116af4774e7b870d1f12432bdfd
이병무 <am003507@gmail.com>
2020-12-01 22:45:34 +0900

    feat - 사용자 생성

---
+++ src/main/java/nextstep/ladder/ErrorMessage.java
@@ -0,0 +1,8 @@
+package nextstep.ladder;
+
+public class ErrorMessage {
+    private ErrorMessage() {
+    }
+
+    public static final String INVALID_USER_NAME = "Invalid User Name";
+}
---
+++ src/main/java/nextstep/ladder/domain/User.java
@@ -0,0 +1,19 @@
+package nextstep.ladder.domain;
+
+import nextstep.ladder.ErrorMessage;
+
+public class User {
+
+    private final String name;
+
+    public User(String name) {
+        throwIfInvalidName(name);
+        this.name = name;
+    }
+
+    private void throwIfInvalidName(String name) {
+        if (name == null || name.length() == 0 || name.length() > 5) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_USER_NAME);
+        }
+    }
+}
