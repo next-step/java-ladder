a72e40aad8af37c8bb7864fd75913402154b9603
sehan <sehan.choi@gmail.com>
2020-12-01 15:58:15 +0900

    refactor: 실습2 람다를 사용하여 중복제거

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
@@ -18,39 +19,33 @@ public class Lambda {
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> System.out.println("Hello from thread")).start();
+    }
+
+    public static int sumIf(List<Integer> numbers, Predicate<Integer> matched){
+        return numbers.stream()
+                .filter(matched)
+                .reduce(Integer::sum)
+                .orElse(0);
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumIf(numbers, it -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumIf(numbers, Lambda::isEven);
+    }
+
+    private static boolean isEven(Integer number) {
+        return number % 2 == 0;
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumIf(numbers, Lambda::isGreaterThenThree);
+    }
+
+    private static boolean isGreaterThenThree(Integer number) {
+        return number > 3;
     }
 }
