0deb8ca737c650a07624b91eca36ed72e3bd0801
changgunyee <minyoung403@naver.com>
2020-12-02 00:35:16 +0900

    refactor: rename to LineResult

--- src/main/java/nextstep/ladder/domain/LineResult.java
+++ src/main/java/nextstep/ladder/domain/LineResult.java
@@ -1,26 +1,24 @@
 package nextstep.ladder.domain;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
-public class Results {
+public class LineResult {
 
     public static final String SPLITTER = ",";
     private final List<Result> results;
 
-    public Results(List<Result> results) {
-        this.results = results;
+    public LineResult(List<Result> results) {
+        this.results = new ArrayList<>(results);
     }
 
-    public static Results from(int length, String results) {
+    public static LineResult from(int length, String results) {
         List<Result> resultList = Arrays.stream(results.split(SPLITTER))
                 .map(Result::new)
                 .collect(Collectors.toList());
         checkSize(length, resultList);
 
-        return new Results(resultList);
+        return new LineResult(resultList);
     }
 
     private static void checkSize(int length, List<Result> resultList) {
@@ -33,8 +31,8 @@ public class Results {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Results results1 = (Results) o;
-        return Objects.equals(results, results1.results);
+        LineResult lineResult1 = (LineResult) o;
+        return Objects.equals(results, lineResult1.results);
     }
 
     @Override
--- src/test/java/nextstep/ladder/domain/LineResultTest.java
+++ src/test/java/nextstep/ladder/domain/LineResultTest.java
@@ -8,21 +8,21 @@ import java.util.Arrays;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class ResultsTest {
+public class LineResultTest {
     @Test
     @DisplayName("results 생성")
     void createResults() {
-        Results results = Results.from(3, "꽝,5000,3000");
+        LineResult lineResult = LineResult.from(3, "꽝,5000,3000");
 
-        Results resultsExpected = new Results(Arrays.asList(new Result("꽝"), new Result("5000"), new Result("3000")));
-        assertThat(results).isEqualTo(resultsExpected);
+        LineResult lineResultExpected = new LineResult(Arrays.asList(new Result("꽝"), new Result("5000"), new Result("3000")));
+        assertThat(lineResult).isEqualTo(lineResultExpected);
     }
 
     @Test
     @DisplayName("results 생성 시 사이즈 다르면 에러")
     void createResults_notMatchSize() {
         assertThatThrownBy(() -> {
-            Results.from(4, "꽝,5000,3000");
+            LineResult.from(4, "꽝,5000,3000");
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("length and results size is different");
     }
