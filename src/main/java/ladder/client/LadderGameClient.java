991c255cbeaaaaf9d5fcbbed010bc7029e399313
rlawls1991 <rlawls1991@gmail.com>
2020-12-02 22:37:06 +0900

    style

--- src/main/java/ladder/client/LadderGameClient.java
+++ src/main/java/ladder/client/LadderGameClient.java
@@ -2,7 +2,7 @@ package ladder.client;
 
 import ladder.controller.PlayLadderGame;
 
-public class ladderGameClient {
+public class LadderGameClient {
     public static void main(String[] args) {
         PlayLadderGame ladderGame = new PlayLadderGame();
         ladderGame.playGame();
