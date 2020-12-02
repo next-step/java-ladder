92b407a71bc71322df7dfeee88187939a7120e34
rlawls1991 <rlawls1991@gmail.com>
2020-12-02 22:22:08 +0900

    feat 사다리 게임 관리객체
    기능
    1. 사다리 게임 line init
    2. 사다리 모습 그려주는 기능

---
+++ src/main/java/ladder/domain/LadderGame.java
@@ -0,0 +1,40 @@
+package ladder.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LadderGame {
+
+    private InputValueStatus inputValueStatus;
+    private List<Line> lineList = new ArrayList<>();
+    private Names names;
+
+    public LadderGame(final String names, final int ladderHeight) {
+        initLadderGame(names, ladderHeight);
+    }
+
+    private void initLadderGame(final String names, final int ladderHeight) {
+        this.inputValueStatus = new InputValueStatus(names, ladderHeight);
+        this.names = new Names(names);
+        addLine(ladderHeight);
+    }
+
+    private void addLine(final int ladderHeight) {
+        Line line;
+
+        for (int i = 0; i < ladderHeight; i++) {
+            line = new Line(this.names.getNames().size());
+            this.lineList.add(line);
+        }
+    }
+
+    public void printLadder() {
+        PrintLadder printLadder = new PrintLadder();
+        printLadder.printNames(names.getNames());
+
+        for (Line line : lineList) {
+            printLadder.printLadder(line.getPoints(), names.getNames().size());
+        }
+
+    }
+}
