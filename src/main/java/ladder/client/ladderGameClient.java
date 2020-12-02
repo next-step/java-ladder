76357e6e83333d3b7c96240839f3f6b07abfb784
rlawls1991 <rlawls1991@gmail.com>
2020-12-02 22:28:28 +0900

    feat client 객체 생성

---
+++ src/main/java/ladder/client/ladderGameClient.java
@@ -0,0 +1,10 @@
+package ladder.client;
+
+import ladder.controller.PlayLadderGame;
+
+public class ladderGameClient {
+    public static void main(String[] args) {
+        PlayLadderGame ladderGame = new PlayLadderGame();
+        ladderGame.playGame();
+    }
+}
