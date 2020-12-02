384d5b581ce3aef29e496f28eb88fe4b29aee13c
oen142 <oen142@naver.com>
2020-12-02 23:10:51 +0900

    feat : 이름잘르는 클래스 리팩토링

--- src/test/java/step2/domain/NameSplitterTest.java
+++ src/test/java/step2/domain/NameSplitterTest.java
@@ -2,13 +2,9 @@ package step2.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import step2.dto.LadderDto;
-import step2.dto.PlayersRewardsDto;
 import step2.exception.EmptyNameException;
-import step2.exception.LadderHeightException;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
