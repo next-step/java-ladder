f715a51324a79eba6297a89e8b157981f8cf5577
SungHyeob Baek <baek9652@naver.com>
2020-12-02 19:04:25 +0900

    [feat] InputView 에 사다리 높이 입력 받는 기능 추가

--- src/main/java/ladder/Client.java
+++ src/main/java/ladder/Client.java
@@ -7,14 +7,20 @@ import java.util.List;
 public class Client {
 
     private static Users users;
+    private static int ladderHeight;
 
     public static void main(String[] args) {
         userSetUpPhase();
+        ladderHeightSetUpPhase();
     }
 
     private static void userSetUpPhase() {
         String rawInput = InputView.askNames();
         List<String> parsedInput = InputParser.parseRawInput(rawInput);
         users = new Users(parsedInput);
+    }
+
+    private static void ladderHeightSetUpPhase() {
+        ladderHeight = InputView.askLadderHeight();
     }
 }
--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String WHEN_ASK_USER_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String WHEN_ASK_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static String askNames() {
         System.out.println(WHEN_ASK_USER_NAMES_MESSAGE);
         return scanner.nextLine();
+    }
+
+    public static int askLadderHeight() {
+        System.out.println(WHEN_ASK_LADDER_HEIGHT_MESSAGE);
+        return scanner.nextInt();
     }
 }
