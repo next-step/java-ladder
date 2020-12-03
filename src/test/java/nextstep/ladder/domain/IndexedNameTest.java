b6ab0ef76fc635cbeb25f3e7f01052020bc3463d
kit(김광수) <kit.t@kakaocorp.com>
2020-12-03 10:31:54 +0900

    test: 인덱스 검증 추가

--- src/test/java/nextstep/ladder/domain/IndexedNameTest.java
+++ src/test/java/nextstep/ladder/domain/IndexedNameTest.java
@@ -1,19 +1,24 @@
 package nextstep.ladder.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class IndexedNameTest {
     @Test
-    void notNull() {
-        assertThatThrownBy(() -> new IndexedName(0, null))
-                .isInstanceOf(IllegalArgumentException.class);
+    void notNullAndNoNegativeIndex() {
+        Assertions.assertAll(
+                () -> assertThatThrownBy(() -> new IndexedName(0, null))
+                        .isInstanceOf(IllegalArgumentException.class),
+                () -> assertThatThrownBy(() -> new IndexedName(-1, "name"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     private static class IndexedName {
         public IndexedName(int index, String name) {
-            throw new IllegalArgumentException("이름이 반드시 제공되어야 한다");
+            throw new IllegalArgumentException("이름/인덱스가 올바르지 않습니다");
         }
     }
 }
