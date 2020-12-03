74edf8e2bcf47906aeab7560ae41978ba7273bd9
kit(김광수) <kit.t@kakaocorp.com>
2020-12-03 19:58:10 +0900

    feat: 왼쪽 이동 가능여부 재공

--- src/test/java/nextstep/ladder2/DirectionTest.java
+++ src/test/java/nextstep/ladder2/DirectionTest.java
@@ -47,9 +47,12 @@ public class DirectionTest {
         private static final Direction LEFT = new Direction(true, false);
         private static final Direction RIGHT = new Direction(false, true);
         private static final Direction NEUTRAL = new Direction(false, true);
+        private final boolean left;
+        private final boolean right;
 
         public Direction(boolean left, boolean right) {
-
+            this.left = left;
+            this.right = right;
         }
 
         public static Direction of(boolean left, boolean right) {
@@ -63,6 +66,17 @@ public class DirectionTest {
                 return RIGHT;
             }
             return NEUTRAL;
+        }
+
+        public static Direction first(boolean right) {
+            if (right) {
+                return RIGHT;
+            }
+            return NEUTRAL;
+        }
+
+        public boolean isLeft() {
+            return left;
         }
     }
 }
