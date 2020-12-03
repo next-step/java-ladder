9bb5ff8a7bca6d0296c0d404b6525ed4873905b6
kit(김광수) <kit.t@kakaocorp.com>
2020-12-03 20:23:08 +0900

    docs: 요구사항 구현완료로 주석 제거

--- src/test/java/nextstep/ladder2/DirectionTest.java
+++ src/test/java/nextstep/ladder2/DirectionTest.java
@@ -9,15 +9,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-/**
- * Direction 요구사항
- * <p>
- * * 특정 지점의 오른쪽/왼쪽 이동가능한지를 나타낸다
- * * 좌측 끝지점은 항상 왼쪽은 이동이 불가능하다
- * * 우측 끝지점은 항상 오른쪽 이동이 불가능하다
- * * 중간지점은 왼쪽 또는 오른쪽으로 이동가능하나 둘다 이동가능할 순 없다. 단 둘다 이동불가능함은 가능하다.
- * * 다음 오른쪽 지점을 나타내는 객체를 생성하는 함수를 제공한다.
- */
 public class DirectionTest {
     @DisplayName("왼쪽/오른쪽 이동 가능한지를 표현한다")
     @Test
