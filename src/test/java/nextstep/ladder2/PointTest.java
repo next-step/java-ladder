ca237958e662fe1e5b3480a1b6c5b7eab2b923a0
kit(김광수) <kit.t@kakaocorp.com>
2020-12-03 21:11:00 +0900

    test: 첫번째 지점은 오른쪽 이동가능여부를 입력받는다

--- src/test/java/nextstep/ladder2/PointTest.java
+++ src/test/java/nextstep/ladder2/PointTest.java
@@ -1,10 +1,13 @@
 package nextstep.ladder2;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  * Point 요구사항
- *
+ * <p>
  * * 포인트는 위치와 이동가능여부(Direction) 을 가진다.
  * * 이동 함수를 제공하며 위치값을 반환한다
  */
@@ -12,6 +15,14 @@ public class PointTest {
     @Test
     void create() {
         new Point(0, Direction.of(false, true));
+    }
+
+    @DisplayName("첫번째 지점은 오른쪽 이동가능여부를 입력받는다")
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    void first(boolean right) {
+        assertThat(Point.first(right)).isEqualTo(new Point(0, Direction.of(false, right)));
+        assertThat(Point.first(right)).isEqualTo(new Point(0, Direction.of(false, right)));
     }
 
     private static class Point {
