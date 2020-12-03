13d7a6f95e717fef7c6c80aba2fd25b30ef6fe46
kit(김광수) <kit.t@kakaocorp.com>
2020-12-03 10:25:51 +0900

    feat: 이름이 없다면 예외발생

--- src/test/java/nextstep/ladder/domain/PlayerTest.java
+++ src/test/java/nextstep/ladder/domain/PlayerTest.java
@@ -1,12 +1,19 @@
 package nextstep.ladder.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PlayerTest {
     @Test
     void notNull() {
-        Assertions.assertThatThrownBy(() -> new Player(null))
+        assertThatThrownBy(() -> new Player(null))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static class Player {
+        public Player(String name) {
+            throw new IllegalArgumentException("이름이 반드시 제공되어야 한다");
+        }
     }
 }
