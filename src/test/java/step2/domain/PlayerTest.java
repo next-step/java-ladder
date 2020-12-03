e0b13bb9d0e13a684975edb8f142b1788314f82f
oen142 <oen142@naver.com>
2020-12-03 23:41:23 +0900

    test fix : 주신 리팩토링 힌트로 바꿈

--- src/test/java/step2/domain/PlayerTest.java
+++ src/test/java/step2/domain/PlayerTest.java
@@ -2,6 +2,8 @@ package step2.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import step2.exception.NameLengthException;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -28,5 +30,19 @@ class PlayerTest {
     void validName() {
         Player player = Player.of("a", 0);
         assertThat(player.getPlayerGameName()).isEqualTo("a");
+    }
+
+    @ParameterizedTest
+    @DisplayName("입력 값을 바탕으로 플레이어 들이 잘 생성 되는지 확인")
+    @CsvSource(value = {
+            "player1,player2,player3,player4=4",
+            "player1,player2,player3=3",
+            "player1=1"
+    }, delimiter = '=')
+    void generate_player_test(String input, int result) {
+        // given
+        Players players = Players.of(NameSplitter.splitParticipationNames(input));
+        // when & then
+        assertThat(players.getPlayersCount()).isEqualTo(result);
     }
 }
