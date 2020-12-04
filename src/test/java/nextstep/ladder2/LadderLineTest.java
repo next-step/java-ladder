d19cbf5d64632b405b2ccd6e4fb016e703172e88
kit(김광수) <kit.t@kakaocorp.com>
2020-12-04 13:32:54 +0900

    refactor: Point 생성하는 부분을 분리

--- src/test/java/nextstep/ladder2/LadderLineTest.java
+++ src/test/java/nextstep/ladder2/LadderLineTest.java
@@ -5,11 +5,11 @@ import nextstep.ladder.domain.TestingBooleanGenerator;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
+import java.util.function.Supplier;
 import java.util.stream.Collector;
 
-import static nextstep.ladder.domain.BooleanGenerator.*;
+import static nextstep.ladder.domain.BooleanGenerator.generateBooleans;
 import static nextstep.ladder.utils.BinaryOperators.nope;
-import static nextstep.ladder2.LadderLineTest.LadderLine.buildPoints;
 import static nextstep.ladder2.LadderLineTest.Lists.last;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -32,11 +32,15 @@ public class LadderLineTest {
                 .isEqualTo(new LadderLine(buildPoints(true, false)));
     }
 
+    private List<Point> buildPoints(Boolean... booleans) {
+        return new PointListBuilder(booleans).build();
+    }
+
     static class LadderLine {
         private static final Random RANDOM = new Random();
         private final List<Point> points;
 
-        public LadderLine(List<Point> points) {
+        LadderLine(List<Point> points) {
             this.points = points;
         }
 
@@ -44,42 +48,13 @@ public class LadderLineTest {
             return of(size, RANDOM::nextBoolean);
         }
 
-        static LadderLine of(int size, BooleanGenerator booleanGenerator) {
-            return new LadderLine(buildPoints(generateBooleans(size - 1, booleanGenerator)));
+        static LadderLine of(int size, BooleanGenerator generator) {
+            return new LadderLine(new PointListBuilder(
+                    generateBooleans(excludeFirstPoint(size), generator)).build());
         }
 
-        static List<Point> buildPoints(Boolean... rightMovables) {
-            List<Point> result = Arrays.stream(rightMovables)
-                    .collect(collectPoints());
-            return addLast(result);
-        }
-
-        private static Collector<Boolean, List<Point>, List<Point>> collectPoints() {
-            return Collector.of(ArrayList::new,
-                                LadderLine::addPoint,
-                                nope()
-            );
-        }
-
-        private static void addPoint(List<Point> list, Boolean right) {
-            if (list.isEmpty()) {
-                addFirst(list, right);
-                return;
-            }
-            addNext(list, right);
-        }
-
-        private static void addFirst(List<Point> list, Boolean right) {
-            list.add(Point.first(right));
-        }
-
-        private static void addNext(List<Point> list, Boolean right) {
-            list.add(last(list).next(right));
-        }
-
-        private static List<Point> addLast(List<Point> result) {
-            result.add(last(result).last());
-            return result;
+        private static int excludeFirstPoint(int size) {
+            return size - 1;
         }
 
         int sizeOfPoints() {
@@ -105,6 +80,48 @@ public class LadderLineTest {
     static class Lists {
         public static <T> T last(List<T> list) {
             return list.get(list.size() - 1);
+        }
+    }
+
+    static class PointListBuilder {
+        private final Boolean[] rightMovables;
+
+        public PointListBuilder(Boolean[] rightMovables) {
+            this.rightMovables = rightMovables;
+        }
+
+        private List<Point> build() {
+            List<Point> result = Arrays.stream(rightMovables)
+                    .collect(collectPointsTo(ArrayList::new));
+            addLast(result);
+            return result;
+        }
+
+        private Collector<Boolean, List<Point>, List<Point>> collectPointsTo(Supplier<List<Point>> supplier) {
+            return Collector.of(supplier,
+                                this::addPoint,
+                                nope()
+            );
+        }
+
+        private void addPoint(List<Point> list, Boolean right) {
+            if (list.isEmpty()) {
+                addFirst(list, right);
+                return;
+            }
+            addNext(list, right);
+        }
+
+        private void addFirst(List<Point> list, Boolean right) {
+            list.add(Point.first(right));
+        }
+
+        private static void addNext(List<Point> list, Boolean right) {
+            list.add(last(list).next(right));
+        }
+
+        private void addLast(List<Point> result) {
+            result.add(last(result).last());
         }
     }
 }
