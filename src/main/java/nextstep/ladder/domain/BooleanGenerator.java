4540261522c78e85aa8817373d4efdb738cc6b66
kit(김광수) <kit.t@kakaocorp.com>
2020-12-04 12:46:19 +0900

    feat: make it pass

--- src/main/java/nextstep/ladder/domain/BooleanGenerator.java
+++ src/main/java/nextstep/ladder/domain/BooleanGenerator.java
@@ -1,6 +1,15 @@
 package nextstep.ladder.domain;
 
+import java.util.stream.IntStream;
+
 @FunctionalInterface
 public interface BooleanGenerator {
     Boolean nextBoolean();
+
+    static Boolean[] generateBooleans(int size, BooleanGenerator booleanGenerator) {
+        return IntStream
+                .range(0, size)
+                .mapToObj(index -> booleanGenerator.nextBoolean())
+                .toArray(Boolean[]::new);
+    }
 }
--- src/test/java/nextstep/ladder2/LadderLineTest.java
+++ src/test/java/nextstep/ladder2/LadderLineTest.java
@@ -1,12 +1,21 @@
 package nextstep.ladder2;
 
+import nextstep.ladder.domain.BooleanGenerator;
+import nextstep.ladder.domain.TestingBooleanGenerator;
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+import java.util.stream.Collector;
+
+import static nextstep.ladder.domain.BooleanGenerator.*;
+import static nextstep.ladder.utils.BinaryOperators.nope;
+import static nextstep.ladder2.LadderLineTest.LadderLine.buildPoints;
+import static nextstep.ladder2.LadderLineTest.Lists.last;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * 요구사항
- *
+ * <p>
  * * 넓이(플레이어 수)를 입력받는다.
  * * 플레이어 수 만큼 Point 들을 생성한다
  * * Point 간 이동을 대리하는 move 메시지를 제공한다.
@@ -23,19 +32,79 @@ public class LadderLineTest {
                 .isEqualTo(new LadderLine(buildPoints(true, false)));
     }
 
-    private static class LadderLine {
-        private int size;
+    static class LadderLine {
+        private static final Random RANDOM = new Random();
+        private final List<Point> points;
 
-        public LadderLine(int size) {
-            this.size = size;
+        public LadderLine(List<Point> points) {
+            this.points = points;
         }
 
         public static LadderLine of(int size) {
-            return new LadderLine(3);
+            return of(size, RANDOM::nextBoolean);
         }
 
-        public int sizeOfPoints() {
-            return size;
+        static LadderLine of(int size, BooleanGenerator booleanGenerator) {
+            return new LadderLine(buildPoints(generateBooleans(size - 1, booleanGenerator)));
+        }
+
+        static List<Point> buildPoints(Boolean... rightMovables) {
+            List<Point> result = Arrays.stream(rightMovables)
+                    .collect(collectPoints());
+            return addLast(result);
+        }
+
+        private static Collector<Boolean, List<Point>, List<Point>> collectPoints() {
+            return Collector.of(ArrayList::new,
+                                LadderLine::addPoint,
+                                nope()
+            );
+        }
+
+        private static void addPoint(List<Point> list, Boolean right) {
+            if (list.isEmpty()) {
+                addFirst(list, right);
+                return;
+            }
+            addNext(list, right);
+        }
+
+        private static void addFirst(List<Point> list, Boolean right) {
+            list.add(Point.first(right));
+        }
+
+        private static void addNext(List<Point> list, Boolean right) {
+            list.add(last(list).next(right));
+        }
+
+        private static List<Point> addLast(List<Point> result) {
+            result.add(last(result).last());
+            return result;
+        }
+
+        int sizeOfPoints() {
+            return points.size();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+            LadderLine that = (LadderLine) o;
+            return points.equals(that.points);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(points);
+        }
+    }
+
+    static class Lists {
+        public static <T> T last(List<T> list) {
+            return list.get(list.size() - 1);
         }
     }
 }
