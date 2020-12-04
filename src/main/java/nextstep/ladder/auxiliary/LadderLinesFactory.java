b4bda62eab4e3d347904bc0556630f3b18392c36
kit(김광수) <kit.t@kakaocorp.com>
2020-12-04 21:06:23 +0900

    refactor: Type Bound

--- src/main/java/nextstep/ladder/auxiliary/LadderLinesFactory.java
+++ src/main/java/nextstep/ladder/auxiliary/LadderLinesFactory.java
@@ -7,7 +7,7 @@ import nextstep.ladder.domain.alternative.NextStepLadderLine;
 import java.util.function.BiFunction;
 
 public class LadderLinesFactory {
-    public static <T> BiFunction<Integer, Integer, LadderLines> getConstructorFunction(Class<T> clazz) {
+    public static <T extends LadderLines> BiFunction<Integer, Integer, LadderLines> getConstructorFunction(Class<T> clazz) {
         if (clazz.isAssignableFrom(NextStepLadderLine.class)) {
             return NextStepLadderLine::new;
         }
