ef2fd53808bdcbb936eca9dd9b2099932ec630bf
rlawls1991 <rlawls1991@gmail.com>
2020-12-05 00:04:34 +0900

    refactor 1. 일급컬랙션 사용으로 인한 코드 수정 2. 사다리 그려주는 기능 이관

--- src/main/java/ladder/domain/LadderGame.java
+++ src/main/java/ladder/domain/LadderGame.java
@@ -1,41 +1,29 @@
 package ladder.domain;
 
-import java.util.ArrayList;
+
+import ladder.dto.LadderLineDTO;
+
 import java.util.List;
 
 public class LadderGame {
-
-    private InputValueStatus inputValueStatus;
-    private List<Line> lineList = new ArrayList<>();
-    private Names names;
+    private Persons person;
+    private LadderLineDTO ladderLineDTO = new LadderLineDTO();
 
     public LadderGame(final String names, final int ladderHeight) {
         initLadderGame(names, ladderHeight);
     }
 
     private void initLadderGame(final String names, final int ladderHeight) {
-        this.inputValueStatus = new InputValueStatus(names, ladderHeight);
-        this.names = new Names(names);
-
-        addLine(ladderHeight);
+        this.person = new Persons(names);
+        LadderLine ladderLine = new LadderLine(ladderHeight, person.getNames().size());
+        this.ladderLineDTO = ladderLine.getLadderLineDTO();
     }
 
-    private void addLine(final int ladderHeight) {
-        Line line;
-
-        for (int i = 0; i < ladderHeight; i++) {
-            line = new Line(this.names.getNames().size());
-            this.lineList.add(line);
-        }
+    public LadderLineDTO getLadderLineDTO() {
+        return ladderLineDTO;
     }
 
-    public void printLadder() {
-        PrintLadder printLadder = new PrintLadder();
-        printLadder.printNames(names.getNames());
-
-        for (Line line : lineList) {
-            printLadder.printLadder(line.getPoints(), names.getNames().size());
-        }
-
+    public List<String> getNames(){
+        return person.getNames();
     }
 }
