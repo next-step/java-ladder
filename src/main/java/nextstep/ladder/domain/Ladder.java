a0924fa32204af5b03bfa5a1d70703c598780f23
kit(김광수) <kit.t@kakaocorp.com>
2020-12-05 19:57:25 +0900

    feat: 결과 목록 정렬

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
+import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toMap;
+import static nextstep.ladder.domain.IndexedName.unwrap;
+import static nextstep.ladder.utils.ImmutableMaps.ordered;
 
 public class Ladder {
     private final List<IndexedName> goals;
@@ -21,7 +24,8 @@ public class Ladder {
 
     public Map<String, String> moveForAll(List<IndexedName> players) {
         return players.stream()
-                .collect(toMap(IndexedName::toString, this::moveFor));
+                .collect(collectingAndThen(toMap(IndexedName::toString, this::moveFor),
+                                           result -> ordered(result, unwrap(players))));
     }
 
     public Position moveOn(Position from) {
