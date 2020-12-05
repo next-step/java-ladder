c8ae43aaabb159487f7505e41151d1adc797b2f8
dbeod2 <darung.0211@gmail.com>
2020-12-05 23:38:36 +0900

    refactor : 참여자 유효성 체크 추가

--- src/main/java/ladder/domain/Participants.java
+++ src/main/java/ladder/domain/Participants.java
@@ -5,15 +5,17 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Participants {
-
     private static final String NAME_SEPARATOR = ",";
-    private List<Name> names;
+    private static final Integer MIN_LENGTH = 2;
+
+    private final List<Name> names;
 
     private Participants(List<Name> names) {
         this.names = names;
     }
 
     public static Participants of(String names) {
+        validate(names);
         return new Participants(create(names));
     }
 
@@ -29,4 +31,14 @@ public class Participants {
         return this.names;
     }
 
+    private static void validate(String name) {
+        countOfPerson(name);
+    }
+
+    private static void countOfPerson(String name) {
+        String[] names = name.split(NAME_SEPARATOR);
+        if (names.length < MIN_LENGTH) {
+            throw new IllegalArgumentException("참여하는 사람은 최소 2명입니다.");
+        }
+    }
 }
