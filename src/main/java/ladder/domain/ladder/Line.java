48958f8c21ab64d268d5d217fd95e6621cb46a61
DongHwan Kim <bkuekany@gmail.com>
2020-12-07 21:49:12 +0900

    feat : Add 이동경로를 표시하는 Line 클래스 추가

---
+++ src/main/java/ladder/domain/ladder/Line.java
@@ -0,0 +1,34 @@
+package ladder.domain.ladder;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Line {
+
+    private final List<Boolean> points;
+
+    public Line(List<Boolean> points) {
+        this.points = points;
+    }
+
+    public boolean isAvailableMove(int countOfPerson) {
+        return points.get(countOfPerson);
+    }
+
+    public int sizeWidth() {
+        return points.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Line line = (Line) o;
+        return Objects.equals(points, line.points);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(points);
+    }
+}
