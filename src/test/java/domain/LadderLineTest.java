75d1fbb5ed8974e9914c1aabe4727856e2e17b29
rlawls1991 <rlawls1991@gmail.com>
2020-12-10 02:32:28 +0900

    test 사다리 라인 테스트

--- src/test/java/domain/LadderLineTest.java
+++ src/test/java/domain/LadderLineTest.java
@@ -1,6 +1,6 @@
 package domain;
 
-import ladder.domain.LadderLines;
+import ladder.domain.Ladder;
 import ladder.util.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,14 +10,14 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class LadderLineTest {
 
-    private static LadderLines LadderLinesTest;
+    private static Ladder LadderLineTest;
 
     @ParameterizedTest
     @DisplayName("입력받은 사다리 높이가 0 또는 음수일 때 예외를 던진다")
     @ValueSource(ints = {0, -100, -200, -3})
     void checkLadderHeight(int ladderHeight){
         assertThatThrownBy(() ->
-                LadderLinesTest = new LadderLines(ladderHeight, 10)
+                LadderLineTest = new Ladder(ladderHeight, 10)
         ).isInstanceOf(RuntimeException.class)
                 .withFailMessage(ErrorMessage.getCheckInputLadderHeight());
     }
