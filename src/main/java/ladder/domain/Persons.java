54e7e4cf767e2b2b998a0e1bbbeda20eb4d8859d
rlawls1991 <rlawls1991@gmail.com>
2020-12-10 02:29:56 +0900

    feat - 사다리게임 최종 position 추가

--- src/main/java/ladder/domain/Persons.java
+++ src/main/java/ladder/domain/Persons.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class Persons {
     private static int MAX_NAME_LENGTH = 5;
+    private static int MIN_COUNT_OF_PERSON = 2;
 
     private List<Person> persons = new ArrayList<>();
 
@@ -16,18 +17,37 @@ public class Persons {
         initNames(names);
     }
 
+    public Persons(Persons persons, List<Integer> positionList) {
+        checkListSize(persons.getPersons().size(), positionList.size());
+
+        Person person = null;
+        this.persons = new ArrayList<>();
+
+        for (int i = 0; i < persons.getPersons().size(); i++) {
+            person = new Person(persons.getPersons().get(i).getName(), positionList.get(i));
+            this.persons.add(person);
+        }
+    }
+
+    private void checkListSize(int first, int second) {
+        if (first != second) {
+            throw new RuntimeException(ErrorMessage.getCheckPersonsInit());
+        }
+    }
+
     private void initNames(final String names) {
         List<String> nameList = Arrays.asList(names.replaceAll(" ", "").split(","));
 
         checkMakeList(nameList);
+        checkCountOfPerson(nameList.size());
         addPerson(nameList);
     }
 
     private void addPerson(final List<String> names) {
         Person person;
 
-        for (String name : names) {
-            person = new Person(name);
+        for (int i = 0; i < names.size(); i++) {
+            person = new Person(names.get(i));
             persons.add(person);
         }
     }
@@ -52,6 +72,12 @@ public class Persons {
     private void checkNull(final String names) {
         if (names.isEmpty()) {
             throw new RuntimeException(ErrorMessage.getCheckInputNames());
+        }
+    }
+
+    private void checkCountOfPerson(int countOfPerson) {
+        if (countOfPerson < MIN_COUNT_OF_PERSON) {
+            throw new RuntimeException(ErrorMessage.getCheckCountOfPerson());
         }
     }
 
