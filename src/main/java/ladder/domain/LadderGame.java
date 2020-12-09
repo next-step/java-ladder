1783f4156c727f4ef0bf1a40466feff87a615501
hanogo <trullo393@gmail.com>
2020-12-09 14:10:21 +0900

    [Feat] Main 및 View패키지 구현

---
+++ src/main/java/ladder/domain/LadderGame.java
@@ -0,0 +1,17 @@
+package ladder.domain;
+
+import ladder.domain.ladder.Ladder;
+import ladder.domain.player.Players;
+import ladder.domain.util.RightPointRandom;
+import ladder.view.InputView;
+import ladder.view.ResultView;
+
+public class LadderGame {
+    public void run() {
+        Players players = new Players(InputView.inputGamePlayers());
+        Ladder ladder = new Ladder(players.getPlayerCount(), InputView.inputLadderSize(),
+                new RightPointRandom());
+
+        ResultView.printLadderResult(players, ladder.getLines());
+    }
+}
