7cd0900525175f397ba7962ab10909080c78d45f
rlawls1991 <rlawls1991@gmail.com>
2020-12-10 02:26:32 +0900

    feat 1. 이동전 index번호 초기화

--- src/main/java/ladder/domain/LadderLine.java
+++ src/main/java/ladder/domain/LadderLine.java
@@ -4,22 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LadderLine {
-    private static final int SUBTRACT_NUMBER = 1;
     private List<LadderPoint> points = new ArrayList<>();
+    private List<Integer> positionList = new ArrayList<>();
 
     public LadderLine(final int countOfPerson) {
-        initLineList(countOfPerson);
+        initLine(countOfPerson);
+        initPosition(countOfPerson);
     }
 
-    private void initLineList(final int countOfPerson) {
-        points.add(new LadderPoint(false));
+    public LadderLine(List<LadderPoint> points, List<Integer> positionList) {
+        this.points = points;
+        this.positionList = positionList;
+    }
 
-        for (int i = 1; i < countOfPerson - SUBTRACT_NUMBER; i++) {
-            points.add(new LadderPoint(points.get(points.size() - 1).isLadderPoint()));
+    private void initLine(final int countOfPerson) {
+        LadderPoint point = new LadderPoint(false);
+        this.points.add(point);
+
+        for (int i = 1; i < countOfPerson - 1; i++) {
+            point = new LadderPoint(points.get(points.size() - 1).isLadderPoint());
+            points.add(point);
+        }
+    }
+
+    private void initPosition(final int countOfPerson) {
+        for (int i = 0; i < countOfPerson; i++) {
+            this.positionList.add(i);
         }
     }
 
     public List<LadderPoint> getPoints() {
         return points;
+    }
+
+    public List<Integer> getPositionList() {
+        return positionList;
     }
 }
