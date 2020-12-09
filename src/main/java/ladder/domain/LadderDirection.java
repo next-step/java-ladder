a5503b7a8385073a462f82dca220401f110d99d7
rlawls1991 <rlawls1991@gmail.com>
2020-12-10 02:24:53 +0900

    feat - 사다리 이동 방향 관리 객체

---
+++ src/main/java/ladder/domain/LadderDirection.java
@@ -0,0 +1,36 @@
+package ladder.domain;
+
+public class LadderDirection {
+
+    private boolean left;
+    private boolean right;
+
+    public LadderDirection(boolean left, boolean right) {
+        checkDirection(left, right);
+
+        this.left = left;
+        this.right = right;
+    }
+
+    public boolean isLeft() {
+        return left;
+    }
+
+    public boolean isRight() {
+        return right;
+    }
+
+    private void checkDirection(boolean left, boolean right) {
+        if (left == true && right == true) {
+            throw new RuntimeException("방향을 제대로 선택해주세요");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "LadderDirection{" +
+                "left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
