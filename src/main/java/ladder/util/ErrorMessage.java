5ef35a813b2adc1e76f65bae4a37ad47a6f689ed
rlawls1991 <rlawls1991@gmail.com>
2020-12-10 02:23:57 +0900

    feat - 새로운 에러 메세지 생성

--- src/main/java/ladder/util/ErrorMessage.java
+++ src/main/java/ladder/util/ErrorMessage.java
@@ -5,8 +5,15 @@ public class ErrorMessage {
     private static final String CHECK_INPUT_NAMES = "참여할 사람의 이름을 확인해주세요";
     private static final String CHECK_NAME_LENGTH = "이름의 길이는 5를 넘지 못합니다.";
     private static final String CHECK_INPUT_LADDER_HEIGHT = "최소 사다리 갯수는 1개입니다.";
-
+    private static final String CHECK_COUNT_OF_PERSON = "최소 참가인원은 2명입니다.";
+    private static final String CHECK_PERSONS_INIT = "psersons 객체 초기화 시 list사이즈가 같아야 합니다.";
+    private static final String CHECK_WINNNG_SIZE = "당첨결과 크기와 참여 명단의 크기가 같아야 합니다.";
+    private static final String CHECK_INPUT_WINNING = "당첨 결결과 입력을 확인해주세요";
     private ErrorMessage(){
+    }
+
+    public static String getCheckCountOfPerson() {
+        return CHECK_COUNT_OF_PERSON;
     }
 
     public static String getCheckNameLength() {
@@ -15,6 +22,18 @@ public class ErrorMessage {
 
     public static String getCheckInputNames() {
         return CHECK_INPUT_NAMES;
+    }
+
+    public static String getCheckPersonsInit() {
+        return CHECK_PERSONS_INIT;
+    }
+
+    public static String getCheckWinnngSize() {
+        return CHECK_WINNNG_SIZE;
+    }
+
+    public static String getCheckInputWinning() {
+        return CHECK_INPUT_WINNING;
     }
 
     public static String getCheckInputLadderHeight() {
