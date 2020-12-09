a382b37cfbb29c49ab3fc0fb21621681112935ac
rlawls1991 <rlawls1991@gmail.com>
2020-12-10 02:25:24 +0900

    feat 1. 사다리 이동기능 추가 2. 결과 데이터 가져오는 기능 추가

--- src/main/java/ladder/domain/LadderGame.java
+++ src/main/java/ladder/domain/LadderGame.java
@@ -1,24 +1,46 @@
 package ladder.domain;
 
 
-public class LadderGame {
-    private Persons persons;
-    private LadderLines ladderLine;
+import ladder.dto.LadderWinningDTO;
 
-    public LadderGame(final String names, final int ladderHeight) {
-        initLadderGame(names, ladderHeight);
+import java.util.List;
+
+public class LadderGame {
+
+    private Persons persons;
+    private Ladder ladder;
+    private LadderWinnings ladderWinning;
+
+    public LadderGame(final String names, final int ladderHeight, final String winnings) {
+        initLadderGame(names, ladderHeight, winnings);
     }
 
-    private void initLadderGame(final String names, final int ladderHeight) {
+    private void initLadderGame(final String names, final int ladderHeight, final String winnings) {
         this.persons = new Persons(names);
-        this.ladderLine = new LadderLines(ladderHeight, persons.getPersons().size());
+        this.ladder = new Ladder(ladderHeight, persons.getPersons().size());
+        this.persons = new Persons(persons, ladder.getLadderLine().get(ladder.getLadderLine().size() - 1).getPositionList());
+        this.ladderWinning = new LadderWinnings(winnings, this.persons);
+    }
+
+    public List<LadderWinningDTO> getWinningResult(String name){
+
+        if(name.equals("all")){
+            return ladderWinning.getAllWinningResult();
+        }
+
+        return ladderWinning.getOneWinningResult(name);
+    }
+
+    public List<LadderWinning> getWinnings(){
+        return ladderWinning.getWinnings();
     }
 
     public Persons getPerson() {
         return persons;
     }
 
-    public LadderLines getLadderLine() {
-        return ladderLine;
+    public Ladder getLadder() {
+        return ladder;
     }
+
 }
