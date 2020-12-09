a6cf33044627b26c8c7c343594c1cf8dd96d3674
이병무 <am003507@gmail.com>
2020-12-09 22:05:31 +0900

    refactor - Direction을 생성하는 코드를 LadderPoint로 이동

--- src/main/java/nextstep/ladder/domain/Direction.java
+++ src/main/java/nextstep/ladder/domain/Direction.java
@@ -3,7 +3,6 @@ package nextstep.ladder.domain;
 import nextstep.ladder.ErrorMessage;
 
 import java.util.Arrays;
-import java.util.Random;
 
 public enum Direction {
 
@@ -11,7 +10,6 @@ public enum Direction {
     NONE(false, false),
     RIGHT(false, true);
 
-    private static Random random = new Random();
     private final boolean left;
     private final boolean right;
 
@@ -19,6 +17,7 @@ public enum Direction {
         this.left = left;
         this.right = right;
     }
+
 
     public static Direction of(boolean left, boolean right) {
         return Arrays.stream(values())
@@ -28,19 +27,12 @@ public enum Direction {
                 .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_DIRECTION));
     }
 
-    public static Direction generateFirst() {
-        return Direction.of(false, random.nextBoolean());
+    public static Direction of(Direction leftDirection, boolean right) {
+        return Direction.of(leftDirection.right, right);
     }
 
-    public static Direction generateLast(Direction before) {
-        return Direction.of(before.right, false);
-    }
-
-    public static Direction next(Direction before) {
-        if (before.right) {
-            return Direction.of(true, false);
-        }
-        return Direction.of(false, random.nextBoolean());
+    public boolean isRight() {
+        return right;
     }
 
 }
--- src/test/java/nextstep/ladder/domain/DirectionTest.java
+++ src/test/java/nextstep/ladder/domain/DirectionTest.java
@@ -1,12 +1,9 @@
 package nextstep.ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 class DirectionTest {
 
@@ -16,53 +13,6 @@ class DirectionTest {
         assertThatThrownBy(() -> {
             Direction.of(true, true);
         }).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("첫번째로 나올수있는 방향 테스트")
-    @RepeatedTest(10)
-    public void generateFirstDirectionTest() {
-        Direction direction = Direction.generateFirst();
-        assertThat(direction).isNotEqualTo(Direction.LEFT);
-    }
-
-    @DisplayName("마지막으로 올수있는 방향 테스트 - 이전 방향이 왼쪽")
-    @RepeatedTest(10)
-    public void generateLastDirectionTestBeforeLeft() {
-        Direction direction = Direction.generateLast(Direction.LEFT);
-        assertAll(
-                () -> assertThat(direction).isEqualTo(Direction.NONE),
-                () -> assertThat(direction).isNotEqualTo(Direction.LEFT)
-        );
-    }
-
-    @DisplayName("마지막으로 올수있는 방향 테스트 - 이전 방향이 오른쪽")
-    @RepeatedTest(10)
-    public void generateLastDirectionTestBeforeRight() {
-        Direction direction = Direction.generateLast(Direction.RIGHT);
-        assertAll(
-                () -> assertThat(direction).isEqualTo(Direction.LEFT),
-                () -> assertThat(direction).isNotEqualTo(Direction.NONE),
-                () -> assertThat(direction).isNotEqualTo(Direction.RIGHT)
-        );
-    }
-
-    @DisplayName("이전 방향이 왼쪽")
-    @Test
-    @RepeatedTest(10)
-    public void nextDirectionTestBeforeLeft(){
-        Direction direction = Direction.generateLast(Direction.LEFT);
-        assertThat(direction).isNotEqualTo(Direction.LEFT);
-    }
-
-    @DisplayName("이전 방향이 오른쪽")
-    @Test
-    public void nextDirectionTestBeforeRight(){
-        Direction direction = Direction.next(Direction.RIGHT);
-        assertAll(
-                () -> assertThat(direction).isEqualTo(Direction.LEFT),
-                () -> assertThat(direction).isNotEqualTo(Direction.NONE),
-                () -> assertThat(direction).isNotEqualTo(Direction.RIGHT)
-        );
     }
 
 }
