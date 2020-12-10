703a6233c8646ec4f7a2e8c376f409fdeacd7b5d
SungHyeob Baek <baek9652@naver.com>
2020-12-10 14:01:09 +0900

    [test] Point 에 다음 인덱스를 반환하는 기능 테스트 추가

--- src/test/java/ladder/domain/PointTest.java
+++ src/test/java/ladder/domain/PointTest.java
@@ -3,6 +3,11 @@ package ladder.domain;
 import ladder.ladderexceptions.InvalidLayoutException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -17,5 +22,24 @@ public class PointTest {
 
         assertThatThrownBy(() -> new Point(true, true))
                 .isInstanceOf(InvalidLayoutException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("allStatus")
+    @DisplayName("아랫 방향  움직임 테스트")
+    void testNextIndex(Point point, int expectedIndex) {
+        int startingIndex = 1;
+
+        assertThat(point.nextIndex(startingIndex)).isEqual(expectedIndex);
+
+    }
+
+    @MethodSource
+    private static Stream<Arguments> allStatus() {
+        return Stream.of(
+                Arguments.of(new Point(false, false), 1),
+                Arguments.of(new Point(false, true), 2),
+                Arguments.of(new Point(true, false), 0)
+        );
     }
 }
