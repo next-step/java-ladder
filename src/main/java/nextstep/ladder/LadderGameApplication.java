f9cd9976a8eed75e7fe27974b15701ae77483073
Jaehee Park <jaeheepark0145@gmail.com>
2020-12-11 00:18:52 +0900

    feat : ResultView 추가

--- src/main/java/nextstep/ladder/LadderGameApplication.java
+++ src/main/java/nextstep/ladder/LadderGameApplication.java
@@ -1,6 +1,9 @@
 package nextstep.ladder;
 
+import nextstep.ladder.domain.LadderGame;
+import nextstep.ladder.domain.LadderGenerateRule;
 import nextstep.ladder.view.InputView;
+import nextstep.ladder.view.ResultView;
 
 import java.util.List;
 
@@ -8,8 +11,13 @@ public class LadderGameApplication {
     public static void main(String[] args) {
 
         InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
         List<String> names = inputView.inputNames();
         int height = inputView.inputHeight();
 
+        LadderGame ladderGame = LadderGame.of(names, height, new LadderGenerateRule());
+
+        resultView.printNames(ladderGame.getUserNames());
+        resultView.printLadder(ladderGame.getLines());
     }
 }
