95c58db71a5c5d41015dc5edb9fea1fb7c00bff3
dbeod2 <darung.0211@gmail.com>
2020-12-12 02:42:04 +0900

    refactor : 게임 실행 결과 코드 추가

--- src/main/java/step3/Controller/LadderGameController.java
+++ src/main/java/step3/Controller/LadderGameController.java
@@ -1,25 +1,29 @@
 package step3.Controller;
 
-import step3.domain.Ladder;
-import step3.domain.Participants;
-import step3.domain.Rewards;
+import step3.domain.*;
 import step3.utils.SpliterUtil;
 import step3.view.InputUi;
 import step3.view.OutputUi;
 
+import java.util.List;
+
 public class LadderGameController {
 
     public static void start() {
+
         Participants participants = Participants.of(SpliterUtil.splitNames(InputUi.nameOfParticipate()));
         Rewards rewards = Rewards.of(SpliterUtil.splitNames(InputUi.inputExecutionResult()));
-
         int maxHeight = InputUi.maximumHeight();
-
-        Ladder ladder = Ladder.of(participants.getParticipants().size(), maxHeight);
+        Ladder ladder = Ladder.of(participants, maxHeight);
 
         OutputUi.printLadder(participants, rewards, ladder);
 
-//        InputUi.wantToSeeResult()
+        String name = InputUi.wantToSeeResult();
+        LadderGame ladderGame = new LadderGame(ladder.getLines(), participants, maxHeight);
+        List<LadderGameResult> results = ladderGame.results(name, rewards);
+        for(LadderGameResult ladderGameResult : results) {
+            System.out.println(ladderGameResult.getName() +":reward ," + ladderGameResult.getReward().getName());
+        }
         InputUi.close();
     }
 
