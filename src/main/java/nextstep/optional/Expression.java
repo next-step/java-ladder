e9bbb7a961d0ef91420129aaf7942c7264fcc325
sjh <sjh@revhat.com>
2020-12-12 23:58:46 +0900

    Optinal 요구사항 3

--- src/main/java/nextstep/optional/Expression.java
+++ src/main/java/nextstep/optional/Expression.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Arrays;
+
 enum Expression {
     PLUS("+"), MINUS("-"), TIMES("*"), DIVIDE("/");
 
@@ -19,7 +21,11 @@ enum Expression {
                 return v;
             }
         }
-
-        throw new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression));
+        return Arrays
+                .stream(Expression.values())
+                .filter(v -> matchExpression(v, expression))
+                .findFirst()
+                .orElseThrow(() ->
+                        new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression)));
     }
 }
