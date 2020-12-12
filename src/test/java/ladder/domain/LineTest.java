29c3d9ff466182c79e17a67ca9cfcd3e7eb03075
SungHyeob Baek <baek9652@naver.com>
2020-12-13 00:27:48 +0900

    [test] Line 수동 생성 테스트 추가

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,11 +18,11 @@ class LineTest {
         return Stream.of(
                 Arguments.of((ConnectionMode) () -> false, 1),
 
-                // false, true, false, true ....
+                // false, true, false, true ...
                 Arguments.of(new SampleShufflePattern(), 2),
 
                 // PointStatus 의 방어로직에 의해 연속되어 생성되지 않는다.
-                // true, false, true, false...
+                // true, false, true, false ...
                 Arguments.of((ConnectionMode) () -> true, 0)
         );
     }
@@ -36,4 +38,29 @@ class LineTest {
         assertThat(result).isEqualTo(expected);
     }
 
+
+    private static Stream<Arguments> makeExpected() {
+        return Stream.of(
+                Arguments.of(0, 1),
+                Arguments.of(1, 0),
+                Arguments.of(2, 2),
+                Arguments.of(3, 4),
+                Arguments.of(4, 3)
+        );
+    }
+
+    @MethodSource("makeExpected")
+    @ParameterizedTest
+    @DisplayName("배열 수동 생성에 따른 값 테스트")
+    void testManualLine(int input, int expected) {
+        Line sampleLine = Line.ofPoints(new ArrayList<>(Arrays.asList(
+                Point.custom1(0, PointStatus.custom(false, true)),
+                Point.custom1(1, PointStatus.custom(true, false)),
+                Point.custom1(2, PointStatus.custom(false, false)),
+                Point.custom1(3, PointStatus.custom(false, true)),
+                Point.custom1(4, PointStatus.custom(true, false))
+        )));
+
+        assertThat(sampleLine.getNextIndex(input)).isEqualTo(expected);
+    }
 }
