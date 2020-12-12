0fddb768b6fea09538303459036fc5c1fb5f3b1e
dbeod2 <darung.0211@gmail.com>
2020-12-12 09:43:38 +0900

    test : participants 테스트 코드 추가

--- src/test/java/ladder/step3/domain/ParticipantsTest.java
+++ src/test/java/ladder/step3/domain/ParticipantsTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.params.provider.ValueSource;
 import step3.domain.Participants;
 import step3.utils.SpliterUtil;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,5 +27,23 @@ public class ParticipantsTest {
         assertThatThrownBy(() ->
                 Participants.of(SpliterUtil.splitNames(name)))
                 .hasMessage("참여하는 사람은 최소 2명입니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("게임결과 확인 : 미참가자 입력시 예외처리")
+    @ValueSource(strings = "yu,hh")
+    public void 미참가자_예외(String name) {
+        assertThatThrownBy(() ->
+                Participants.of(SpliterUtil.splitNames(name)).getPoint("hi"))
+                .hasMessage("입력하신 참여자를 찾을 수 없습니다. (입력한 참여자 이름: hi)");
+    }
+
+    @ParameterizedTest
+    @DisplayName("게임결과 확인 : 참가자 처음 위치 확인")
+    @ValueSource(strings = "yu,hh")
+    public void 참가자_확인(String name) {
+        List<String> participants = SpliterUtil.splitNames(name);
+        assertThat(Participants.of(participants).getParticipants().get(0).getPosition().getX()).isEqualTo(0);
+        assertThat(Participants.of(participants).getParticipants().get(1).getPosition().getX()).isEqualTo(2);
     }
 }
