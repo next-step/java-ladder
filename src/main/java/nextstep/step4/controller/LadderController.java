db4afb66517747dfcff4b320774fc29e35677645
kyeongmin <kyeongmin.jake.lee@gmail.com>
2020-12-13 19:08:56 +0900

    refactor - InputView 메소드의 리턴타입 변경

--- src/main/java/nextstep/step4/controller/LadderController.java
+++ src/main/java/nextstep/step4/controller/LadderController.java
@@ -1,10 +1,17 @@
 package nextstep.step4.controller;
 
+import nextstep.step4.domain.Results;
+import nextstep.step4.domain.Users;
 import nextstep.step4.view.InputView;
 
 public class LadderController {
 
     public void start() {
         InputView inputView = new InputView();
+
+        Users users = inputView.getUsers();
+        Results results = inputView.getResults();
+        int height = inputView.getHeight();
+
     }
 }
--- src/main/java/nextstep/step4/view/InputView.java
+++ src/main/java/nextstep/step4/view/InputView.java
@@ -1,27 +1,41 @@
 package nextstep.step4.view;
 
+import nextstep.step4.domain.Results;
+import nextstep.step4.domain.Users;
+
 import java.util.Scanner;
 
 public class InputView {
 
+
     private static Scanner scanner = new Scanner(System.in);
 
     private static final String INPUT_USER = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
-    public static final String INPUT_RESULT = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
-    public static final String INPUT_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
+    private static final String INPUT_RESULT = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private static final String INPUT_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
 
-    public String getUsers() {
+    private static final int HEIGHT_LIMIT = 2;
+
+    public Users getUsers() {
         System.out.println(INPUT_USER);
-        return scanner.nextLine();
+        return Users.initUsers(scanner.nextLine());
     }
 
-    public String getResults() {
+    public Results getResults() {
         System.out.println(INPUT_RESULT);
-        return scanner.nextLine();
+        return Results.initResults(scanner.nextLine());
     }
 
-    public String getHeight() {
+    public int getHeight() {
         System.out.println(INPUT_HEIGHT);
-        return scanner.nextLine();
+        int height = Integer.parseInt(scanner.nextLine());
+        validateHeight(height);
+        return height;
+    }
+
+    private void validateHeight(int height) {
+        if(height < HEIGHT_LIMIT) {
+            throw new IllegalArgumentException("높이는 2 이상이어야 합니다.");
+        }
     }
 }
