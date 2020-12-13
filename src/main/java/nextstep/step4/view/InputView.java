d27655383f749786761e0f8fdce9d1b1b418b95a
kyeongmin <kyeongmin.jake.lee@gmail.com>
2020-12-13 18:25:40 +0900

    feat - 사다리 높이 입력 기능

--- src/main/java/nextstep/step4/view/InputView.java
+++ src/main/java/nextstep/step4/view/InputView.java
@@ -8,6 +8,7 @@ public class InputView {
 
     private static final String INPUT_USER = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     public static final String INPUT_RESULT = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    public static final String INPUT_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
 
     public String getUsers() {
         System.out.println(INPUT_USER);
@@ -16,6 +17,11 @@ public class InputView {
 
     public String getResults() {
         System.out.println(INPUT_RESULT);
+        return scanner.nextLine();
+    }
+
+    public String getHeight() {
+        System.out.println(INPUT_HEIGHT);
         return scanner.nextLine();
     }
 }
