9630bf098670c199aacc7374f736865307ca3a63
kyeongmin <kyeongmin.jake.lee@gmail.com>
2020-12-13 18:24:06 +0900

    feat - 결과 입력 기능

--- src/main/java/nextstep/step4/view/InputView.java
+++ src/main/java/nextstep/step4/view/InputView.java
@@ -7,9 +7,15 @@ public class InputView {
     private static Scanner scanner = new Scanner(System.in);
 
     private static final String INPUT_USER = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    public static final String INPUT_RESULT = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
 
     public String getUsers() {
         System.out.println(INPUT_USER);
+        return scanner.nextLine();
+    }
+
+    public String getResults() {
+        System.out.println(INPUT_RESULT);
         return scanner.nextLine();
     }
 }
