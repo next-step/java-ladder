863a57de376dcfb7aea105e1bc4f7c02c0a7c54d
jhoon.bak <wecube.1@gmail.com>
2020-12-13 15:21:15 +0900

    refactor(ResultView): 사다리 생성 화면 요구사항에 맞게 수정

--- src/main/java/ladder/view/ResultView.java
+++ src/main/java/ladder/view/ResultView.java
@@ -9,46 +9,60 @@ import java.util.stream.IntStream;
 public class ResultView {
     public static final String RESULT = "실행결과";
     private static final String BAR = "|";
+    private static final String SPACE = " ";
     private static final String BLANK = "     ";
     private static final String LADDER = "-----";
     private static final int MAX_NAME_SPACE = 6;
 
     protected static void printResult(Ladder ladder, List<String> participants) {
         printResultHeader();
-        printParticipants(participants);
-        printLadders(ladder);
+        printLadderGameResult(ladder, participants);
     }
 
     private static void printResultHeader() {
-        System.out.println();
+        printOpeningLetter();
         System.out.println(RESULT);
+        printOpeningLetter();
+    }
+
+    private static void printLadderGameResult(Ladder ladder, List<String> participants) {
+        printName(participants.get(0), 0);
+
+        IntStream.range(1, participants.size())
+                .forEach(index -> printName(participants.get(index), MAX_NAME_SPACE - participants.get(index).length()));
+        printOpeningLetter();
+
+        int firstNameSpace = participants.get(0).length() - 1;
+        printLadders(ladder, firstNameSpace);
+    }
+
+    private static void printName(String name, int space) {
+        printSpace(space);
+        System.out.print(name);
+    }
+
+    private static void printSpace(int length) {
+        IntStream.range(0, length)
+                .forEach(index -> System.out.print(SPACE));
+    }
+
+    private static void printOpeningLetter() {
         System.out.println();
     }
 
-    private static void printLadders(Ladder ladder) {
-        for (LadderLine ladderLine : ladder.getLadderLines()) {
-            printLadderLine(ladderLine);
-        }
-    }
-
-    private static void printParticipants(List<String> participants) {
-        participants.forEach(name -> {
-            System.out.print(name);
-            printBlank(name);
-        });
-        System.out.println();
-    }
-
-    private static void printBlank(String name) {
-        IntStream.range(0, MAX_NAME_SPACE - name.length())
-                .forEach(index -> System.out.print(" "));
+    private static void printLadders(Ladder ladder, int space) {
+        ladder.getLadderLines()
+                .forEach(ladderLine -> {
+                    printSpace(space);
+                    printLadderLine(ladderLine);
+                });
     }
 
     private static void printLadderLine(LadderLine ladderLine) {
         IntStream.range(0, ladderLine.getPoints().size())
                 .mapToObj(index -> getLadder(ladderLine, index))
                 .forEach(System.out::print);
-        System.out.println();
+        printOpeningLetter();
     }
 
     private static String getLadder(LadderLine ladderLine, int index) {
