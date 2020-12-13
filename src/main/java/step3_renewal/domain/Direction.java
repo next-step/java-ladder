4801863d29ae8648075831a604a7a46dfb0ccbe8
dbeod2 <darung.0211@gmail.com>
2020-12-14 00:37:16 +0900

    refactor : 정적 팩토리 메서드 패턴사용으로 인한 접근 제어자 변경

--- src/main/java/step3_renewal/domain/Direction.java
+++ src/main/java/step3_renewal/domain/Direction.java
@@ -7,7 +7,7 @@ public class Direction {
     private final boolean left;
     private final boolean right;
 
-    public Direction(boolean left, boolean right) {
+    private Direction(boolean left, boolean right) {
         if (left && right) {
             throw new IllegalStateException();
         }
