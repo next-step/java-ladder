62fc01224fb6253b0dfb8dcc74ccfa18430b5884
dbeod2 <darung.0211@gmail.com>
2020-12-14 01:09:21 +0900

    refactor : gameStart메서드 GameResults로 역할 위임

--- src/main/java/step3_renewal/controller/LadderGameController.java
+++ src/main/java/step3_renewal/controller/LadderGameController.java
@@ -5,34 +5,19 @@ import step3_renewal.utils.StringUtil;
 import step3_renewal.view.InputUi;
 import step3_renewal.view.OutputUi;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import static step3_renewal.domain.GameResults.results;
 
 public class LadderGameController {
 
     public static void run() {
         Participants participants = Participants.of(StringUtil.splitToList(InputUi.nameOfParticipate()));
         Rewards rewards =
-                Rewards.of(StringUtil.splitToList(InputUi.inputExecutionResult()), participants.getParticipants().size());
+                Rewards.of(StringUtil.splitToList(InputUi.inputExecutionResult()), participants.participants().size());
 
         Ladder ladder = Ladder.of(InputUi.maximumHeight(), participants);
         OutputUi.printAll(participants, rewards, ladder);
-        OutputUi.printLadderGameResult(InputUi.wantToSeeResult(), gameStart(participants, rewards, ladder));
+        OutputUi.printLadderGameResult(InputUi.wantToSeeResult(), results(participants, rewards, ladder));
 
         InputUi.close();
-    }
-
-    public static GameResults gameStart(Participants participants, Rewards rewards, Ladder ladder) {
-        List<GameResult> list =
-                participants.getParticipants()
-                        .stream()
-                        .map(participant -> toGameResult(participant, rewards, ladder))
-                        .collect(Collectors.toList());
-        return GameResults.of(list);
-    }
-
-    private static GameResult toGameResult(Participant participant, Rewards rewards, Ladder ladder) {
-        return GameResult.of(participant.getParticipantName()
-                , rewards.getRewardName(ladder.move(participant.getPosition())));
     }
 }
--- src/main/java/step3_renewal/domain/GameResults.java
+++ src/main/java/step3_renewal/domain/GameResults.java
@@ -2,6 +2,7 @@ package step3_renewal.domain;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GameResults {
 
@@ -17,5 +18,19 @@ public class GameResults {
 
     public List<GameResult> getGameResults() {
         return Collections.unmodifiableList(gameResults);
+    }
+
+    public static GameResults results(Participants participants, Rewards rewards, Ladder ladder) {
+        List<GameResult> list =
+                participants.participants()
+                        .stream()
+                        .map(participant -> gameResult(participant, rewards, ladder))
+                        .collect(Collectors.toList());
+        return new GameResults(list);
+    }
+
+    private static GameResult gameResult(Participant participant, Rewards rewards, Ladder ladder) {
+        return GameResult.of(participant.name()
+                , rewards.getRewardName(ladder.move(participant.position())));
     }
 }
