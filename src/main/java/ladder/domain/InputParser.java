57668939c22c4950fe3b70f3bfa385af451547b5
SungHyeob Baek <baek9652@naver.com>
2020-12-14 12:03:23 +0900

    [feat] inputParser 스플릿 기능 구현

--- src/main/java/ladder/domain/InputParser.java
+++ src/main/java/ladder/domain/InputParser.java
@@ -6,11 +6,13 @@ import java.util.stream.Collectors;
 
 public class InputParser {
 
+    private static final String splitPattern = "[,][ ]?";
+
     private InputParser() {
     }
 
     public static List<String> parseRawInput(String input) {
-        return Arrays.stream(input.split(","))
+        return Arrays.stream(input.split(splitPattern))
                 .collect(Collectors.toList());
     }
 }
