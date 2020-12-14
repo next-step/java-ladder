5f580a76b92e2782ff4bca164ba0af809c2a0c9a
devyoon91 <kimbyungyoun91@gmail.com>
2020-12-14 20:42:22 +0900

    [feat] : 참/거짓 값을 리턴하는 클래스 생성

---
+++ src/main/java/ladder/domain/PointGenerator.java
@@ -0,0 +1,26 @@
+package ladder.domain;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
+public class PointGenerator {
+
+    private static final int FIRST_INDEX = 0;
+    private static final List<Boolean> NEXT_BOOLEAN = new LinkedList<>();
+
+    static {
+        NEXT_BOOLEAN.add(TRUE);
+        NEXT_BOOLEAN.add(FALSE);
+    }
+
+    private PointGenerator() {}
+
+    public static boolean hasMovable() {
+        Collections.shuffle(NEXT_BOOLEAN);
+        return NEXT_BOOLEAN.get(FIRST_INDEX);
+    }
+}
