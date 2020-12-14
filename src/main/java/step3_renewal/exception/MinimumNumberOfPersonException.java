d6a98cee67ed8ed824904b449982748239843d0c
dbeod2 <darung.0211@gmail.com>
2020-12-14 23:36:46 +0900

    refactor : 예외처리 메시지 수정

--- src/main/java/step3_renewal/exception/MinimumNumberOfPersonException.java
+++ src/main/java/step3_renewal/exception/MinimumNumberOfPersonException.java
@@ -2,6 +2,6 @@ package step3_renewal.exception;
 
 public class MinimumNumberOfPersonException extends IllegalArgumentException {
     public MinimumNumberOfPersonException(int minNumber) {
-        super("참여하는 사람은 최소" + minNumber + "명입니다.");
+        super("참여하는 사람은 최소 " + minNumber + "명입니다.");
     }
 }
