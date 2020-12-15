ee3bea175593869b2b62d9427cbcec9d51b20ba6
rlawls1991 <rlawls1991@gmail.com>
2020-12-15 22:12:59 +0900

    refactor 1. 이동여부 확인 2. 이동한 idnex 가지고옴

--- src/main/java/ladder/domain/LadderPoint.java
+++ src/main/java/ladder/domain/LadderPoint.java
@@ -1,25 +1,53 @@
 package ladder.domain;
 
-import java.util.Random;
+import ladder.util.ErrorMessage;
 
 public class LadderPoint {
-    private static Random random = new Random();
 
-    private boolean ladderPoint;
+    private int index;
+    private LadderDirection direction;
 
-    public LadderPoint(final boolean ladderPoint) {
-        this.ladderPoint = initPoint(ladderPoint);
+    public LadderPoint(final int index, final LadderDirection direction) {
+        checkValue(index, direction);
+        initPoint(index, direction);
     }
 
-    private boolean initPoint(boolean point) {
-        if (point) {
-            return false;
+    private void initPoint(final int index, final LadderDirection direction) {
+        this.index = index;
+        this.direction = direction;
+    }
+
+    public void move(){
+        this.index = index+ direction.move();
+    }
+
+    public int getMoveIndex(){
+        return direction.move();
+    }
+
+    private void checkValue(final int index, final LadderDirection direction) {
+        checkIndex(index);
+        checkNull(direction);
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0) {
+            throw new RuntimeException(ErrorMessage.getCheckPointIndex());
         }
-
-        return new Boolean(random.nextBoolean());
     }
 
-    public boolean isLadderPoint() {
-        return this.ladderPoint;
+    private void checkNull(final LadderDirection direction) {
+        if (direction == null) {
+            throw new RuntimeException(ErrorMessage.getCheckNullDirection());
+        }
     }
+
+    public LadderDirection getDirection() {
+        return direction;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
 }
