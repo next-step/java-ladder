f136bcfb0e064dcec4a71722220de3dcc0036036
rlawls1991 <rlawls1991@gmail.com>
2020-12-15 22:12:36 +0900

    refactor 1. 라인 생성 2. 객체를 더 작게 쪼겠기 때문에 기능 수정   - LadderDirection 에 이동추가

--- src/main/java/ladder/domain/LadderLine.java
+++ src/main/java/ladder/domain/LadderLine.java
@@ -2,42 +2,105 @@ package ladder.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class LadderLine {
+
     private List<LadderPoint> points = new ArrayList<>();
-    private List<Integer> positionList = new ArrayList<>();
+    private static Random random = new Random();
 
     public LadderLine(final int countOfPerson) {
-        initLine(countOfPerson);
-        initPosition(countOfPerson);
+        initLadderLine(countOfPerson);
     }
 
-    public LadderLine(List<LadderPoint> points, List<Integer> positionList) {
-        this.points = points;
-        this.positionList = positionList;
+    public LadderLine(final LadderLine ladderLine) {
+        initLadderLine(ladderLine);
     }
 
-    private void initLine(final int countOfPerson) {
-        LadderPoint point = new LadderPoint(false);
+    public void initLadderLine(final int countOfPerson) {
+        initStartOrLast(0, new LadderDirection(false, random.nextBoolean()));
+        initFirstBody(countOfPerson);
+
+        if (countOfPerson > 2) {
+            initStartOrLast(points.size(),
+                    new LadderDirection(this.points.get(points.size() - 1).getDirection().isRight(), false));
+        }
+    }
+
+    public void initLadderLine(final LadderLine ladderLine) {
+        firstAdd(ladderLine);
+        bodyAdd(ladderLine);
+        lastAdd(ladderLine);
+    }
+
+    private void firstAdd(final LadderLine ladderLine) {
+        LadderDirection direction = new LadderDirection(false, random.nextBoolean());
+        int index = 0;
+
+        if (direction.isRight()) {
+            index = 1;
+        }
+        LadderPoint ladderPoint = new LadderPoint(ladderLine.getPoints().get(index).getIndex(), direction);
+
+        points.add(ladderPoint);
+    }
+
+
+    private void lastAdd(final LadderLine ladderLine) {
+        LadderDirection direction = new LadderDirection(points.get(points.size() - 1).getDirection().isRight(), false);
+
+        int index = ladderLine.getPoints().size() - 1;
+
+        if (direction.isLeft()) {
+            index = index - 1;
+        }
+
+        LadderPoint ladderPoint = new LadderPoint(ladderLine.getPoints().get(index).getIndex(), direction);
+        points.add(ladderPoint);
+    }
+
+    public void bodyAdd(final LadderLine ladderLine) {
+        for (int i = 1; i < ladderLine.getPoints().size() - 1; i++) {
+            LadderDirection direction = checkDirection(points.get(i - 1).getDirection());
+            LadderPoint ladderPoint = new LadderPoint(ladderLine.getPoints().get(i).getIndex(), direction);
+            chageAdd(ladderPoint, i, ladderLine);
+        }
+    }
+
+    private void chageAdd(LadderPoint ladderPoint, int paramIndex, LadderLine ladderLine) {
+        int index = paramIndex + ladderPoint.getMoveIndex();
+        LadderPoint point = new LadderPoint(ladderLine.getPoints().get(index).getIndex(), ladderPoint.getDirection());
         this.points.add(point);
+    }
+
+    private void initStartOrLast(final int index, final LadderDirection direction) {
+        LadderPoint point = new LadderPoint(index, direction);
+        point.move();
+        this.points.add(point);
+    }
+
+    private void initFirstBody(final int countOfPerson) {
+        LadderPoint point;
 
         for (int i = 1; i < countOfPerson - 1; i++) {
-            point = new LadderPoint(points.get(points.size() - 1).isLadderPoint());
-            points.add(point);
+            point = new LadderPoint(i, checkDirection(points.get(i - 1).getDirection()));
+            point.move();
+            this.points.add(point);
         }
     }
 
-    private void initPosition(final int countOfPerson) {
-        for (int i = 0; i < countOfPerson; i++) {
-            this.positionList.add(i);
+    private LadderDirection checkDirection(LadderDirection beforePoint) {
+        boolean left;
+        boolean right;
+
+        if (beforePoint.isRight()) {
+            return new LadderDirection(true, false);
         }
+
+        return new LadderDirection(false, random.nextBoolean());
     }
 
     public List<LadderPoint> getPoints() {
         return points;
-    }
-
-    public List<Integer> getPositionList() {
-        return positionList;
     }
 }
