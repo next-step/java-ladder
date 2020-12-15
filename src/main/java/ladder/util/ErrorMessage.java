3b4a2229c6d985907e6be29b8bd662cb5ecf07f0
rlawls1991 <rlawls1991@gmail.com>
2020-12-15 22:09:30 +0900

    refactor - 에러 메세지 추가

--- src/main/java/ladder/util/ErrorMessage.java
+++ src/main/java/ladder/util/ErrorMessage.java
@@ -6,27 +6,37 @@ public class ErrorMessage {
     private static final String CHECK_NAME_LENGTH = "이름의 길이는 5를 넘지 못합니다.";
     private static final String CHECK_INPUT_LADDER_HEIGHT = "최소 사다리 갯수는 1개입니다.";
     private static final String CHECK_COUNT_OF_PERSON = "최소 참가인원은 2명입니다.";
-    private static final String CHECK_PERSONS_INIT = "psersons 객체 초기화 시 list사이즈가 같아야 합니다.";
     private static final String CHECK_WINNNG_SIZE = "당첨결과 크기와 참여 명단의 크기가 같아야 합니다.";
     private static final String CHECK_INPUT_WINNING = "당첨 결결과 입력을 확인해주세요";
+    private static final String CHECK_DIRECTION = "사다리 방향이 양쪽 다 true일 수가 없습니다.";
+    private static final String CHECK_POINT_INDEX = "Point의 idnex은 음수가 될 수 없습니다.";
+    private static final String CHECK_NULL_DIRECTION = "Point의 direction은 null일수가 없습니다.";
+    private static final String CHECK_MOVE_NEGATIVE = "index가 0일때 왼쪽으로 이동하면 에러를 던진다.";
 
-    private ErrorMessage(){
-    }
+    private static final String CHECK_SEARCH_NAME = "결과를 보고 싶은 사람이 이름이 존재하지 않습니다.";
 
-    public static String getCheckCountOfPerson() {
-        return CHECK_COUNT_OF_PERSON;
-    }
-
-    public static String getCheckNameLength() {
-        return CHECK_NAME_LENGTH;
+    private ErrorMessage() {
     }
 
     public static String getCheckInputNames() {
         return CHECK_INPUT_NAMES;
     }
 
-    public static String getCheckPersonsInit() {
-        return CHECK_PERSONS_INIT;
+    public static String getCheckNameLength() {
+        return CHECK_NAME_LENGTH;
+    }
+
+    public static String getCheckInputLadderHeight() {
+        return CHECK_INPUT_LADDER_HEIGHT;
+    }
+
+    public static String getCheckCountOfPerson() {
+        return CHECK_COUNT_OF_PERSON;
+    }
+
+
+    public static String getCheckNullDirection() {
+        return CHECK_NULL_DIRECTION;
     }
 
     public static String getCheckWinnngSize() {
@@ -37,7 +47,19 @@ public class ErrorMessage {
         return CHECK_INPUT_WINNING;
     }
 
-    public static String getCheckInputLadderHeight() {
-        return CHECK_INPUT_LADDER_HEIGHT;
+    public static String getCheckDirection() {
+        return CHECK_DIRECTION;
+    }
+
+    public static String getCheckPointIndex() {
+        return CHECK_POINT_INDEX;
+    }
+
+    public static String getCheckMoveNegative() {
+        return CHECK_MOVE_NEGATIVE;
+    }
+
+    public static String getCheckSearchName() {
+        return CHECK_SEARCH_NAME;
     }
 }
