9c33b5c5f7a43e0fc2c5cc914b6a1c0053fabeae
dbeod2 <darung.0211@gmail.com>
2020-12-15 23:04:59 +0900

    refactor : exception param final 선언

--- src/main/java/step3_renewal/exception/MinHeightException.java
+++ src/main/java/step3_renewal/exception/MinHeightException.java
@@ -1,7 +1,7 @@
 package step3_renewal.exception;
 
 public class MinHeightException extends IllegalArgumentException {
-    public MinHeightException(int minHeight) {
+    public MinHeightException(final int minHeight) {
         super("사다리 다리는 최소 " + minHeight + "부터 입니다.");
     }
 }
