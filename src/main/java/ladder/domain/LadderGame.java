6e4d54558d7cd02c7913be44d0f52d0ed6da4946
DongHwan Kim <bkuekany@gmail.com>
2020-12-15 20:50:36 +0900

    feat : Add 사다리 게임 실행 기능 추가

--- src/main/java/ladder/domain/LadderGame.java
+++ src/main/java/ladder/domain/LadderGame.java
@@ -1,9 +1,12 @@
 package ladder.domain;
 
 
+import ladder.domain.dto.GameResult;
+import ladder.domain.dto.Reward;
 import ladder.domain.ladder.Ladder;
 import ladder.domain.ladder.Line;
 import ladder.domain.ladder.LineGenerator;
+import ladder.domain.participant.Participants;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,9 +14,11 @@ import java.util.List;
 public class LadderGame {
 
     private LineGenerator lineGenerator;
+    private Reward reward;
 
-    public LadderGame(LineGenerator lineGenerator) {
+    public LadderGame(LineGenerator lineGenerator, Reward reward) {
         this.lineGenerator = lineGenerator;
+        this.reward = reward;
     }
 
     public Ladder makeLadder(int width, int height) {
@@ -23,5 +28,14 @@ public class LadderGame {
             lines.add(line);
         }
         return new Ladder(lines);
+    }
+
+    public GameResult play(Participants participants, Ladder ladder) {
+
+        for (int i = 0; i < ladder.sizeHeight(); i++) {
+             ladder.movePosition(i, participants);
+        }
+
+        return new GameResult(participants.getParticipants(), reward);
     }
 }
--- src/test/java/ladder/domain/LadderGameTest.java
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -1,9 +1,19 @@
 package ladder.domain;
 
+import ladder.domain.dto.GameResult;
+import ladder.domain.dto.Reward;
 import ladder.domain.ladder.Ladder;
+import ladder.domain.ladder.Line;
 import ladder.domain.ladder.RandomLineGenerator;
+import ladder.domain.participant.Participants;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,7 +23,7 @@ public class LadderGameTest {
     @CsvSource(value = {"3:5", "2:6", "5:3", "6:12"}, delimiter = ':')
     public void makeLadderTest(int width, int height) {
         //Given
-        LadderGame game = new LadderGame(new RandomLineGenerator());
+        LadderGame game = new LadderGame(new RandomLineGenerator(), new Reward(Arrays.asList("꽝", "3000", "50000")));
 
         //When
         Ladder ladder = game.makeLadder(width, height);
@@ -21,5 +31,30 @@ public class LadderGameTest {
         //Then
         assertThat(ladder.sizeWidth()).isEqualTo(width);
         assertThat(ladder.sizeHeight()).isEqualTo(height);
+    }
+
+    @ParameterizedTest
+    @MethodSource("createLines")
+    public void gamePlayTest(List<Line> lines) {
+        //Given
+        Participants participants = Participants.of(Arrays.asList("user1", "user2", "user3", "user4", "user5"));
+        Ladder ladder = new Ladder(lines);
+        LadderGame game = new LadderGame(new RandomLineGenerator(), new Reward(Arrays.asList("꽝", "3000", "50000", "2000", "1000")));
+
+        //When
+        GameResult gameResult = game.play(participants, ladder);
+
+        //Then
+        assertThat(gameResult).isNotNull();
+
+
+    }
+    private static Stream<Arguments> createLines() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(new Line(Arrays.asList(true, false, false, true)),
+                        new Line(Arrays.asList(false, true, false, true)),
+                        new Line(Arrays.asList(false, true, false, true)))
+                )
+        );
     }
 }
