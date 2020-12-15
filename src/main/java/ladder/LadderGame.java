4ef5b3dcd0c02b0b38439dc8c5ebb852abaa1c27
kriery <jongchan.lee@lmep.kr>
2020-12-15 23:26:21 +0900

    maintain: attendees와 prizes 길이 검증

--- src/main/java/ladder/LadderGame.java
+++ src/main/java/ladder/LadderGame.java
@@ -14,6 +14,9 @@ public class LadderGame {
     public static void main(String[] args) {
         List<String> attendees = InputView.getAttendees();
         List<String> prizes = InputView.getPrizes();
+        if(attendees.size() != prizes.size()) {
+            throw new IllegalArgumentException("count of attendees and prize not equals");
+        }
         int height = InputView.getHeight();
 
         Ladder ladder = Ladder.of(attendees.size(), height, new RandomLinkStrategy());
