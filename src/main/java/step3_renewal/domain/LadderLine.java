e83bb960b48348cff4c042112dc5129de8498e3e
dbeod2 <darung.0211@gmail.com>
2020-12-16 22:01:13 +0900

    refactor : 메소드 위치 변경, 3개 이상 param변경

--- src/main/java/step3_renewal/domain/LadderLine.java
+++ src/main/java/step3_renewal/domain/LadderLine.java
@@ -12,31 +12,30 @@ public class LadderLine {
         this.points = points;
     }
 
-    public static LadderLine init(int sizeOfPerson, LadderRandomGenerator ladderRandomGenerator) {
-        List<Point> points = new ArrayList<>();
-        Point point = initFirst(points);
-        initBody(sizeOfPerson, points, point, ladderRandomGenerator);
-        return new LadderLine(points);
-    }
-
-    public int move(int position) {
-        return points.get(position).move();
-    }
-
     private static Point initFirst(List<Point> points) {
         Point point = Point.first(new LadderRandomGenerator().nextLine());
         points.add(point);
         return point;
     }
 
-    private static Point initBody(int sizeOfPerson, List<Point> points, Point point, LadderRandomGenerator ladderRandomGenerator) {
+    private static Point initBody(Point point, LadderRandomGenerator ladderRandomGenerator) {
+        return point.next(ladderRandomGenerator.nextLine());
+    }
+
+    public static LadderLine init(int sizeOfPerson, LadderRandomGenerator ladderRandomGenerator) {
+        List<Point> points = new ArrayList<>();
+        Point point = initFirst(points);
         for (int i = 1; i < sizeOfPerson - 1; i++) {
-            point = point.next(ladderRandomGenerator.nextLine());
+            point = initBody(point, ladderRandomGenerator);
             points.add(point);
         }
         points.add(point.last());
 
-        return point;
+        return new LadderLine(points);
+    }
+
+    public int move(int position) {
+        return points.get(position).move();
     }
 
     public List<Point> getPoints() {
