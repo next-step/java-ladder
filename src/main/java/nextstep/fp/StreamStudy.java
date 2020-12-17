893f71800cf437c9f23b2350cb89ab43c014ba2a
naru <hsik0225@gmail.com>
2020-12-17 09:14:14 +0900

    feat(stream-study): printLongestWordTop100() 메소드 구현

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -26,6 +26,12 @@ public class StreamStudy {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(word -> word.length() > 12)
+                .sorted((a, b) -> b.length() - a.length())
+                .distinct()
+                .limit(100)
+                .forEach(word -> System.out.println(word.toLowerCase()));
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
