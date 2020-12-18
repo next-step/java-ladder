a20e83cb4d321dc91bb29ed41eca4993e42a8daa
MaroKim <dkgell6@gmail.com>
2020-12-19 03:50:37 +0900

    테스트 코드 추가

--- src/test/java/nextstep/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -4,6 +4,10 @@ import nextstep.ladder.model.Line;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LineTest {
 
 
@@ -15,4 +19,20 @@ public class LineTest {
         System.out.println(line.getPoints().toString());
 
     }
+
+    @Test
+    @DisplayName("PreLine 테스트")
+    public void preLineTest(){
+
+        Line line = new Line(5);
+        List<Boolean> points = line.getPoints();
+        int index = points.indexOf(true);
+
+        if(index !=-1){
+            assertThat(line.isPreLine(index+1)).isEqualTo(false);
+        }
+
+
+    }
+
 }
