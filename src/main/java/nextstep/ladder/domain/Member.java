10ed3aa20f7d6336156e0fd8d5bf065dec3e62f6
joshua(박근희) <joshua.park321@kakaocorp.com>
2020-12-19 23:51:36 +0900

    feat: 이름 5자 제한

--- src/main/java/nextstep/ladder/domain/Member.java
+++ src/main/java/nextstep/ladder/domain/Member.java
@@ -4,10 +4,18 @@ public class Member {
     private final String name;
 
     public Member(String name) {
+        validateName(name);
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+
+    private void validateName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(name + " : 이름은 5자를 초과할수 없습니다");
+        }
     }
 }
