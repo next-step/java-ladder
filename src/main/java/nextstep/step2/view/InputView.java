a3060fa601e4f3654a532c650ef4e73cc5602d74
sjh <sjh@revhat.com>
2020-12-20 18:32:25 +0900

    참가자, 사다리 높이 유효성 검사 구현

--- src/main/java/nextstep/step2/view/InputView.java
+++ src/main/java/nextstep/step2/view/InputView.java
@@ -3,6 +3,7 @@ package nextstep.step2.view;
 import nextstep.step2.VO.Position;
 import nextstep.step2.VO.User;
 import nextstep.step2.VO.Users;
+import org.omg.PortableInterceptor.USER_EXCEPTION;
 
 import java.util.List;
 import java.util.Scanner;
@@ -11,6 +12,8 @@ import java.util.stream.IntStream;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String USER_EXCEPTION_COMMENT = "최소 2인 이상 입력해주셔야 합니다.";
+    private static final String HEIGHT_EXCEPTION_COMMENT = "최소 1이상의 사다리 높이를 입력해주세요.";
     private static final String PLEASE_INPUT_USERS_NAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String PLEASE_INPUT_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
 
@@ -24,14 +27,28 @@ public class InputView {
 
     public static int inputHeight() {
         System.out.println(PLEASE_INPUT_LADDER_HEIGHT);
-        return getIntValue();
+        return validateHeight(getIntValue());
     }
 
     public static List<User> getUserList(String[] names) {
+        validateUserList(names);
         return IntStream.range(0, names.length)
                 .mapToObj(index -> new User(names[index], new Position(index)))
                 .collect(Collectors.toList());
 
+    }
+
+    private static int validateHeight(int height) {
+        if (height < 1) {
+            throw new IllegalArgumentException(HEIGHT_EXCEPTION_COMMENT);
+        }
+        return height;
+    }
+
+    private static void validateUserList(String[] names) {
+        if (names.length <= 1) {
+            throw new IllegalArgumentException(USER_EXCEPTION_COMMENT);
+        }
     }
 
     private static String[] getStringArray() {
