b88feafc7283c06047ff4295fc403a2b811e3844
joshua(박근희) <joshua.park321@kakaocorp.com>
2020-12-21 00:40:13 +0900

    feat: Height 유효성 검사

--- src/main/java/nextstep/ladder/domain/Height.java
+++ src/main/java/nextstep/ladder/domain/Height.java
@@ -4,10 +4,17 @@ public class Height {
     private final int height;
 
     public Height(int height) {
+        validateHeight(height);
         this.height = height;
     }
 
     public int getHeight() {
         return height;
+    }
+
+    private void validateHeight(int height) {
+        if (height < 1) {
+            throw new IllegalArgumentException("사다리 높이는 1은 넘어야죠");
+        }
     }
 }
