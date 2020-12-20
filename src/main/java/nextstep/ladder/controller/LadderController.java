5c22a76d5f01ca7f8765d305548102b53f829f5b
MaroKim <dkgell6@gmail.com>
2020-12-20 12:22:08 +0900

    refactor : people 객체 생성에 따른 코드 변경

--- src/main/java/nextstep/ladder/controller/LadderController.java
+++ src/main/java/nextstep/ladder/controller/LadderController.java
@@ -1,6 +1,7 @@
 package nextstep.ladder.controller;
 
 import nextstep.ladder.model.Ladder;
+import nextstep.ladder.model.People;
 import nextstep.ladder.view.InputView;
 import nextstep.ladder.view.ResultView;
 
@@ -9,12 +10,13 @@ public class LadderController {
     public static void main(String[] args) {
         InputView inputView = new InputView();
 
-        String people = inputView.inputPeople();
+        String peopleStr = inputView.inputPeople();
+        People people = new People(peopleStr);
         int ladderHeight = inputView.inputLadderHeight();
 
-        Ladder ladder = new Ladder(people, ladderHeight);
+        Ladder ladder = new Ladder(people.getPeople().length, ladderHeight);
 
         ResultView resultView = new ResultView();
-        resultView.printLadder(ladder.getPeople(), ladderHeight, ladder.getLines().getLines());
+        resultView.printLadder(people.getPeople(), ladderHeight, ladder.getLines());
     }
 }
