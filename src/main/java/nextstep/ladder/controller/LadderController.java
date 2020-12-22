a0b8a321e03b103c67731842769dee3e58c106d6
MaroKim <dkgell6@gmail.com>
2020-12-23 02:47:21 +0900

    feat : 새로운 input에 따른 코드 변경

--- src/main/java/nextstep/ladder/controller/LadderController.java
+++ src/main/java/nextstep/ladder/controller/LadderController.java
@@ -2,6 +2,7 @@ package nextstep.ladder.controller;
 
 import nextstep.ladder.model.Ladder;
 import nextstep.ladder.model.People;
+import nextstep.ladder.model.Results;
 import nextstep.ladder.view.InputView;
 import nextstep.ladder.view.ResultView;
 
@@ -12,11 +13,22 @@ public class LadderController {
 
         String peopleStr = inputView.inputPeople();
         People people = new People(peopleStr);
+
+        String resultStr = inputView.inputResults();
+        Results results = new Results(resultStr);
+
+
         int ladderHeight = inputView.inputLadderHeight();
 
-        Ladder ladder = new Ladder(people.getPeople().length, ladderHeight);
+        Ladder ladder = new Ladder(people.getPeople().size(), ladderHeight);
 
         ResultView resultView = new ResultView();
-        resultView.printLadder(people.getPeople(), ladderHeight, ladder.getLines());
+        resultView.printLadder(people.getPeople(), results.getResults(), ladder.getLines());
+
+        ladder.startMove(people);
+
+        String name = inputView.inputResultOfPerson();
+        resultView.printResult(name, people, results.getResults());
+
     }
 }
--- src/main/java/nextstep/ladder/view/ResultView.java
+++ src/main/java/nextstep/ladder/view/ResultView.java
@@ -1,20 +1,22 @@
 package nextstep.ladder.view;
 
 import nextstep.ladder.model.Line;
+import nextstep.ladder.model.People;
+import nextstep.ladder.model.Person;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class ResultView {
+    private String PRINT_PEOPLE_RESULTS = "all";
 
-    public void printLadder(String[] people, int height, List<Line> lines) {
+    public void printLadder(List<Person> people, String[] results, List<Line> lines) {
 
-        System.out.println("실행결과");
+        System.out.println("사다리 결과");
         System.out.println();
 
-        for (int i = 0; i < people.length; i++) {
-            System.out.printf("%6s", people[i]);
+        for(Person person : people){
+            System.out.printf("%6s", person.getName());
         }
         System.out.println();
 
@@ -23,6 +25,12 @@ public class ResultView {
             printLine(line);
             System.out.println();
         }
+
+        for(String result : results){
+            System.out.printf("%6s", result);
+
+        }
+        System.out.println();
     }
 
     public void printLine(Line line) {
@@ -35,4 +43,27 @@ public class ResultView {
             System.out.print(strLine);
         }
     }
+    public void printResult(String name, People people, String[] results){
+        System.out.println("실행 결과");
+        if(name.equals(PRINT_PEOPLE_RESULTS)){
+            printPeopleResults(people.getPeople(), results);
+        }
+
+        if(!name.equals(PRINT_PEOPLE_RESULTS)){
+            printPersonResults(people.oneOfPerson(name), results);
+        }
+
+
+    }
+
+    public void printPeopleResults(List<Person> people, String[] results){
+        for(Person person : people){
+            System.out.println(person.getName()+" : "+results[person.getPosition()]);
+        }
+    }
+
+    public void printPersonResults(Person person, String[] results){
+        System.out.println(results[person.getPosition()]);
+    }
+
 }
