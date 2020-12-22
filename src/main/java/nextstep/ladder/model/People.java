d1a3fe6ed1b75661fc889633019ae2928930c65f
MaroKim <dkgell6@gmail.com>
2020-12-23 02:41:59 +0900

    refactor : People -> person 객체로 세분화

--- src/main/java/nextstep/ladder/model/People.java
+++ src/main/java/nextstep/ladder/model/People.java
@@ -1,18 +1,36 @@
 package nextstep.ladder.model;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class People {
-    private String[] people;
 
-    public People(String people){
-        this.people = Arrays.stream(people.split(","))
-                .map(String::trim)
-                .toArray(String[]::new);
+    private List<Person> people = new ArrayList<>();
+
+    public People(String peopleStr){
+
+       String[] peopleArr = Arrays.stream(peopleStr.split(","))
+                                    .map(String::trim).toArray(String[]::new);
+
+       for(int i=0;i<peopleArr.length;i++){
+           people.add(new Person(peopleArr[i], i));
+       }
     }
 
-    public String[] getPeople(){
-        return people;
+    public List<Person> getPeople(){
+        return Collections.unmodifiableList(people);
+    }
+    public Person oneOfPerson(String name){
+        return Optional.of(people.stream()
+                                .filter(person -> person.getName().equals(name))
+                                .findAny()
+                                .get())
+                                .orElseThrow(NoSuchElementException::new);
+
     }
 
+
+    public void movePeoplePosition(List<Line> lines){
+        people.stream()
+                .forEach(person -> person.getPosition(lines));
+    }
 }
