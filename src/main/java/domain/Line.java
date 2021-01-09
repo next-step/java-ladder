1fd46651509e1aa00dec42775f7f92529a93efb8
최종원 <90lastone@gmail.com>
2021-01-09 22:48:14 +0900

    feat: 라인 생성시 겹치지않도록 처리

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -10,10 +10,19 @@ public class Line {
     private List<Boolean> points = new ArrayList<>();
 
     public Line(int countOfPerson) {
+        boolean prevLine = false;
         for (int i = 0; i < countOfPerson - 1; i++) {
             // 사다리 생성 참, 거짓
             Collections.shuffle(flagList);
-            points.add(flagList.get(0));
+            // 이전 라인이 있으면 false
+            boolean line;
+            if (prevLine) {
+                line = false;
+            } else {
+                line = flagList.get(0);
+            }
+            points.add(line);
+            prevLine = line;
         }
     }
 
