80b9718a426e508d310339a2afc432e6066a8645
최종원 <90lastone@gmail.com>
2021-01-17 22:27:34 +0900

    feat(Person): 이름 5자 초과 입력 에러

--- src/main/java/domain/Person.java
+++ src/main/java/domain/Person.java
@@ -4,6 +4,9 @@ public class Person {
     private String name;
 
     public Person(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 최대 5글자까지만 입력 가능합니다.");
+        }
         this.name = name;
     }
 
