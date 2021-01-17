e939bc5d7569bf9f47b2a69dce4196c2db4a887f
최종원 <90lastone@gmail.com>
2021-01-17 16:28:54 +0900

    fix(Line): 생성 에러 수정

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -9,10 +9,8 @@ public class Line {
     private static List<Boolean> flagList = new ArrayList<>(Arrays.asList(true, false));
     private List<Boolean> points = new ArrayList<>();
 
-    // TODO 테스트 고민 레이싱 강의 참조
     private boolean createLine(int i) {
-        if (i > 0 && points.get(i)) {
-            points.add(false);
+        if (i > 0 && points.get(i - 1)) {
             return false;
         }
         Collections.shuffle(flagList);
@@ -21,7 +19,7 @@ public class Line {
 
     public Line(int countOfPerson) {
         for (int i = 0; i < countOfPerson - 1; i++) {
-            createLine(i);
+            points.add(createLine(i));
         }
     }
 
