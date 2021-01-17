8f40cf66f6d1c0128c1d667f13d5474cc898eb38
최종원 <90lastone@gmail.com>
2021-01-17 18:09:03 +0900

    refactor(ResultView): 사다기 그리기 이중 for 문 제거

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -1,33 +1,39 @@
 package view;
 
 import domain.Ladder;
+import domain.Line;
 import domain.PersonList;
 
 public class ResultView {
-    public static void ladder(PersonList personList, Ladder ladder) {
-        System.out.println("실행결과");
-        System.out.println();
-
+    private static void printNames(PersonList personList) {
         personList.getList()
                 .stream()
                 .map(person -> person.getName())
                 .forEach(System.out::print);
+    }
+
+    private static void printPoint(boolean point) {
+        String printMessage = point ? "-----" : "     ";
+        System.out.print(printMessage);
+        System.out.print("|");
+    }
+
+    private static void printLine(Line line) {
+        System.out.print("|");
+        line.getLine()
+                .forEach(point -> printPoint(point));
+        System.out.println();
+    }
+
+    public static void ladder(PersonList personList, Ladder ladder) {
+        System.out.println("실행결과");
+        System.out.println();
+
+        printNames(personList);
 
         System.out.println();
 
-        for (int i = 0; i < ladder.getLadder().size(); i++) {
-            ladder.getLadder().get(i).getLine();
-            System.out.print("|");
-            for (int j = 0; j < ladder.getLadder().get(i).getLine().size(); j++) {
-                boolean line = ladder.getLadder().get(i).getLine().get(j);
-                if (line) {
-                    System.out.print("-----");
-                } else {
-                    System.out.print("     ");
-                }
-                System.out.print("|");
-            }
-            System.out.println();
-        }
+        ladder.getLadder()
+                .forEach(line -> printLine(line));
     }
 }
