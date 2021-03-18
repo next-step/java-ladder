e5592896c484d3fc1c75505aaf31d355081ed6e2
JuJinPark <wnwls1216@gmail.com>
2021-03-18 22:28:53 +0900

    [refactor] refactored line test

--- src/test/java/ladder/LineTest.java
+++ src/test/java/ladder/LineTest.java
@@ -4,39 +4,26 @@ import ladder.domain.Line;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineTest {
 
     @Test
-    @DisplayName("라인 생성 테스트 좌표 갯수, 세로줄 생성여부, 가로줄 겹치는 여부 테스트")
+    @DisplayName("라인 생성 테스트 줄 겹치는 여부 테스트")
     void create() {
         int countOfPerson = 5;
         Line line = new Line(5);
 
-        assertThat(line.getPointList()
-                .size()).isEqualTo(countOfPerson * 2 - 1);
-        assertThat(isVerticalLineExist(line)).isTrue();
+        assertThat(line.pointListSize()).isEqualTo(countOfPerson);
         assertThat(isOverlapping(line)).isFalse();
     }
 
-    private boolean isVerticalLineExist(Line line) {
-        List<Boolean> points = line.getPointList();
-        for (int i = 0; i < points.size(); i += 2) {
-            if(!points.get(i)) return false;
-        }
-        return true;
-    }
-
     private boolean isOverlapping(Line line) {
-        List<Boolean> points = line.getPointList();
-        boolean lineIsConnectedBefore = false;
-        for (int i = 1; i < points.size(); i += 2) {
-            Boolean currentPointIsConnected = points.get(i);
-            if (lineIsConnectedBefore && currentPointIsConnected) return true;
-            lineIsConnectedBefore = currentPointIsConnected;
+        for (int i = 1; i < line.pointListSize(); i++) {
+            boolean previousPointIsFilled = line.isFilledAt(i-1);
+            if (previousPointIsFilled &&  line.isFilledAt(i)) {
+                return true;
+            }
         }
         return false;
     }
