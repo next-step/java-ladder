a987d6328d14643f2095e001d1a60180e3d4a1f4
JuJinPark <wnwls1216@gmail.com>
2021-03-19 00:14:23 +0900

    [conflict] fix conflict

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -25,17 +25,16 @@ public class Lambda {
             }
         }).start();
     }
-
-    public static int sumAll(List<Integer> numbers) {
-        return sumByCondition(numbers, (number) -> true);
+    public static int sumAll(List<Integer> numbers, Conditional conditional) {
+        return sumByCondition(numbers, conditional);
     }
 
-    public static int sumAllEven(List<Integer> numbers) {
-        return sumByCondition(numbers, (number) -> number % 2 == 0);
+    public static int sumAllEven(List<Integer> numbers, Conditional conditional) {
+        return sumByCondition(numbers, conditional);
     }
 
-    public static int sumAllOverThree(List<Integer> numbers) {
-        return sumByCondition(numbers, (number) -> number > 3);
+    public static int sumAllOverThree(List<Integer> numbers, Conditional conditional) {
+        return sumByCondition(numbers, conditional);
     }
 
     private static int sumByCondition(List<Integer> numbers, Conditional conditional) {
