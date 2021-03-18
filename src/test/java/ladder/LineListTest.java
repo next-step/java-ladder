e3b7220298d05bce408d88075150bc02ae7ebd38
JuJinPark <wnwls1216@gmail.com>
2021-03-18 22:27:45 +0900

    [test] add line list test

---
+++ src/test/java/ladder/LineListTest.java
@@ -0,0 +1,21 @@
+package ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class LineListTest {
+
+    @Test
+    @DisplayName("line list 생성 테스트")
+    void name() {
+        int height = 5;
+        int countOfPerson = 6;
+        LineList lineList = new LineList();
+        lineList.addBulk(height, countOfPerson);
+
+        assertThat(lineList.size()).isEqualTo(height);
+    }
+
+}
