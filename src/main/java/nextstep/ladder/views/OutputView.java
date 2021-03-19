7e1377c9ba1b408b7aa667d27a556ab6d6959eb7
jeonyeonbin <qtrgor@gmail.com>
2021-03-19 21:54:51 +0900

    feat: 기능 개발 추가

--- src/main/java/nextstep/ladder/views/OutputView.java
+++ src/main/java/nextstep/ladder/views/OutputView.java
@@ -1,10 +1,17 @@
 package nextstep.ladder.views;
 
 import nextstep.ladder.entity.Ladder;
+import nextstep.ladder.entity.PrizeResult;
 
 public class OutputView extends View {
-	public void showResult(Ladder ladder) {
+	public void showResult(Ladder ladder, PrizeResult prizeResult) {
 		out.println("실행 결과");
 		out.println(ladder);
+		out.println(prizeResult);
+	}
+
+	public void showResultParticipant(String result) {
+		out.println("실행 결과");
+		out.println(result);
 	}
 }
