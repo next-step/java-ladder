8766e21f801585cba2e4b06afca66fd862345021
jeonyeonbin <qtrgor@gmail.com>
2021-03-19 21:53:56 +0900

    feat: util 및 exception message 추가

--- src/main/java/nextstep/ladder/exception/UserExceptionMesssage.java
+++ src/main/java/nextstep/ladder/exception/UserExceptionMesssage.java
@@ -3,7 +3,9 @@ package nextstep.ladder.exception;
 public enum UserExceptionMesssage {
 	NAME_SIZE_CHECK_FIVE("이름은 5자가 넘어야됩니다."),
 	DELIMETER_WITHIN_COMMA(", 로 구분자가 되어야됩니다."),
-	NEGATIVE_NOT_AGRRESIVE("음수는 사다리높이로 설정 할 수 없습니다.");
+	NEGATIVE_NOT_AGRRESIVE("음수는 사다리높이로 설정 할 수 없습니다."),
+	NOT_NUMERIC_OR_FALSY("숫자나 꽝만 입력 가능합니다."),
+	NO_MATCH_PRIZE("상과 참가자 숫자가 맞지않습니다.");
 
 	private final String message;
 
