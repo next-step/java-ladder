0450c02689d6a27bdda08af337fd079612494742
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-03-21 02:56:17 +0900

    test(Players): 이름 분할 메서드 테스트 코드 리팩토링

--- src/test/java/laddarGame/domain/PlayersTest.java
+++ src/test/java/laddarGame/domain/PlayersTest.java
@@ -1,18 +1,18 @@
 package laddarGame.domain;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class PlayersTest {
 
-    @DisplayName("이름을 ,기준으로 분할한다.")
+    @DisplayName("입력 받은 순서대로 ,로 구분하여 분할하여 플레이어를 만든다.")
     @Test
-    void parse() {
-        Players players = new Players();
-        String[] names = players.nameParser("LG,SKT");
+    void Player() {
+        Players players = new Players("LG,SKT");
 
-        Assertions.assertEquals(names[1], "LG");
-        Assertions.assertEquals(names[2], "SKT");
+        assertEquals(players.toList().get(0), new Player("LG", 0));
+        assertEquals(players.toList().get(1), new Player("SKT", 1));
     }
 }
