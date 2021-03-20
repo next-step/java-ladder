66a7f8defd73df48c219324184263d23dbb59da2
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-03-21 01:19:38 +0900

    test(Player):이름 길이 테스트 코드 추가

---
+++ src/test/java/laddarGame/domain/PlayerTest.java
@@ -0,0 +1,18 @@
+package laddarGame.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+class PlayerTest {
+
+    @DisplayName("이름의 길이는 최대 5글자 이다.")
+    @Test
+    void position() {
+        assertThatThrownBy(
+                () -> new Player("SISTER", 0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
