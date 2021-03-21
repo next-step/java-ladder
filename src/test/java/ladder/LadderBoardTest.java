7ff52b5b8ae84366a0f3b9929db852a7bfbcd2ae
JuJinPark <wnwls1216@gmail.com>
2021-03-21 10:32:28 +0900

    [test] ladderBoard prize test

---
+++ src/test/java/ladder/LadderBoardTest.java
@@ -0,0 +1,28 @@
+package ladder;
+
+import ladder.domain.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class LadderBoardTest {
+
+    @Test
+    @DisplayName("prize를 통해 결과값 테스트")
+    void prize() {
+        LineList lineList = new LineList();
+        lineList.add(Arrays.asList(new Point(false), new Point(false), new Point(true), new Point(false)));
+        lineList.add(Arrays.asList(new Point(false), new Point(true), new Point(false), new Point(true)));
+        lineList.add(Arrays.asList(new Point(false), new Point(true), new Point(false), new Point(false)));
+        LadderList ladderList = new LadderList(Arrays.asList(new Ladder(0, "꽝"), new Ladder(1, "100"), new Ladder(2, "200"), new Ladder(3, "300")));
+        Player player = new Player("pobi", 1);
+        Prize expectedPrize = new Prize("300");
+
+        LadderBoard ladderBoard = new LadderBoard(lineList, ladderList);
+        Prize prize = ladderBoard.prize(player);
+        assertThat(prize).isEqualTo(expectedPrize);
+    }
+}
