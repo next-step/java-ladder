0dc199131d7a1b0cd501200791b728ea244d19f4
JuJinPark <wnwls1216@gmail.com>
2021-03-21 17:40:59 +0900

    [test] add missing test case and removed unnecessary test

--- src/test/java/ladder/LadderBoardTest.java
+++ src/test/java/ladder/LadderBoardTest.java
@@ -1,6 +1,7 @@
 package ladder;
 
 import ladder.domain.*;
+import ladder.dto.LadderGameRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,20 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class LadderBoardTest {
 
     @Test
+    @DisplayName("LadderGameRequest를 통해 생성 테스트")
+    void create() {
+        String[] nameArray = new String[]{"pobi", "honux", "tax"};
+        String[] prizeArray = new String[]{"0", "100", "10"};
+        int height = 6;
+        LadderGameRequest ladderGameRequest = new LadderGameRequest(nameArray, prizeArray, height);
+
+        LadderBoard ladderBoard = LadderBoard.of(ladderGameRequest);
+
+        assertThat(ladderBoard.ladderList().size()).isEqualTo(prizeArray.length);
+        assertThat(ladderBoard.lineList().size()).isEqualTo(height);
+    }
+
+    @Test
     @DisplayName("prize를 통해 결과값 테스트")
     void prize() {
         LineList lineList = new LineList();
@@ -18,15 +33,14 @@ public class LadderBoardTest {
         lineList.add(Arrays.asList(new Point(false), new Point(true), new Point(false), new Point(true)));
         lineList.add(Arrays.asList(new Point(false), new Point(true), new Point(false), new Point(false)));
         LadderList ladderList = new LadderList(Arrays.asList(new Ladder(0, "꽝"), new Ladder(1, "100"), new Ladder(2, "200"), new Ladder(3, "300")));
-        Player player = new Player("pobi", 1);
-        Player player2 = new Player("tax", 0);
-        Prize expectedPrize = new Prize("300");
+        Player pobi = new Player("pobi", 1);
+        Player tax = new Player("tax", 0);
+        Prize pobiPrize = new Prize("300");
+        Prize taxPrize = new Prize("꽝");
 
-        Prize expectedPrize2 = new Prize("꽝");
         LadderBoard ladderBoard = new LadderBoard(lineList, ladderList);
-        Prize prize = ladderBoard.prize(player);
-        assertThat(prize).isEqualTo(expectedPrize);
-        Prize prize2 = ladderBoard.prize(player2);
-        assertThat(prize2).isEqualTo(expectedPrize2);
+
+        assertThat(ladderBoard.prize(pobi)).isEqualTo(pobiPrize);
+        assertThat(ladderBoard.prize(tax)).isEqualTo(taxPrize);
     }
 }
