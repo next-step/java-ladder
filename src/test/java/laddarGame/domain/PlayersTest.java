7ddc8ae73c7033ef9b19e8d673237e10f622a437
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-03-21 12:16:28 +0900

    test(Players): 같은 위치에는 두명이 있을 수없다 테스트 코드 추가

--- src/test/java/laddarGame/domain/PlayersTest.java
+++ src/test/java/laddarGame/domain/PlayersTest.java
@@ -3,16 +3,27 @@ package laddarGame.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PlayersTest {
 
-    @DisplayName("입력 받은 순서대로 ,로 구분하여 분할하여 플레이어를 만든다.")
+    @DisplayName("입력 받은 이름 만큼 플레이어를 만든다.")
     @Test
     void Player() {
         Players players = new Players("LG,SKT");
 
-        assertEquals(players.toList().get(0), new Player("LG", 0));
-        assertEquals(players.toList().get(1), new Player("SKT", 1));
+        assertEquals(players.playerCount(), 2);
+    }
+
+    @DisplayName("같은 포지션에 두명은 있을 수 없다..")
+    @Test
+    void duplicate() {
+        Player Player1 = new Player("LG", 0);
+        Player Player2 = new Player("LG", 0);
+
+        assertThrows(IllegalArgumentException.class, () -> new Players(Arrays.asList(Player1, Player2)));
     }
 }
