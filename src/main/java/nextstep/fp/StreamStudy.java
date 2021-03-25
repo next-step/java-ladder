a01c8bf168982a1d1693730e8afe2857658e0992
gummybearr <ehrudghl123@naver.com>
2021-03-25 21:36:43 +0900

    feat(step1): pass stream exercise 1

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -15,11 +15,9 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream()
+                .filter(string->string.length()>12)
+                .count();
     }
 
     public static void printLongestWordTop100() throws IOException {
@@ -35,10 +33,13 @@ public class StreamStudy {
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers.stream().reduce(0, Integer::sum);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(number->number>3)
+                .map(number->number*2)
+                .reduce(0, Integer::sum);
     }
 }
