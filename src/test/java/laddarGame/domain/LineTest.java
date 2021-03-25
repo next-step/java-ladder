a7b2edf853223ab0ccec4235d796caaa437a450b
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-03-26 01:19:42 +0900

    test(Line): 사다리 이동 테스트 코드 추가

--- src/test/java/laddarGame/domain/LineTest.java
+++ src/test/java/laddarGame/domain/LineTest.java
@@ -27,4 +27,31 @@ class LineTest {
 
         assertEquals(new LineDto(List.of(Point.of(true), Point.of(false), Point.of(false))), lineDto);
     }
+
+    @DisplayName("사다리 라인에 따라 플레이어가 이동한다.")
+    @Test
+    void move() {
+        Line line = new Line(List.of(true, false, false, true));
+        Players players = createPlayers();
+
+        line.move(players);
+
+        assertEquals(players.getPlayer(0), new Player("SKT", 0));
+        assertEquals(players.getPlayer(1), new Player("LG", 1));
+        assertEquals(players.getPlayer(2), new Player("KT", 2));
+        assertEquals(players.getPlayer(3), new Player("SKT", 3));
+
+    }
+
+    Players createPlayers() {
+        return new Players(
+                List.of(new Player("LG", 0)
+                        , new Player("SKT", 1)
+                        , new Player("KT", 2)
+                        , new Player("TS", 3)));
+    }
+
+    Line createLine(boolean first, boolean second, boolean third, boolean fourth) {
+        return new Line(List.of(first, second, third, fourth));
+    }
 }
