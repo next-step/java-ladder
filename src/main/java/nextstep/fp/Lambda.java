feece2ba5dabd349df9ad0ea6bb75f28b2e8e007
gummybearr <ehrudghl123@naver.com>
2021-03-25 21:30:43 +0900

    feat(step1): complete lambda exercise 2

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
         System.out.println("printAllOld");
-
         for (int number : numbers) {
             System.out.println(number);
         }
@@ -13,44 +12,27 @@ public class Lambda {
 
     public static void printAllLambda(List<Integer> numbers) {
         System.out.println("printAllLambda");
-
         numbers.forEach(System.out::println);
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> System.out.println("Hello from thread")).start();
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return numbers.stream()
+                .reduce(0, Integer::sum);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return numbers.stream()
+                .filter(number->number%2==0)
+                .reduce(0, Integer::sum);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return numbers.stream()
+                .filter(number->number>3)
+                .reduce(0, Integer::sum);
     }
 }
