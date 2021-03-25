1fc14d0fe4af322c4dc49d53bec319637e4a71aa
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-03-26 02:09:12 +0900

    test(Lines):사다리타기 실행 테스트 코드 추가

--- src/test/java/laddarGame/domain/LinesTest.java
+++ src/test/java/laddarGame/domain/LinesTest.java
@@ -4,7 +4,6 @@ import laddarGame.dto.LineDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -29,11 +28,36 @@ class LinesTest {
         assertEquals(new LineDto(true, false, true, false), lineDtos.get(0));
     }
 
-    List<Line> createLines() {
-        return Arrays.asList(createLine(true, false, true, false)
-                , createLine(false, true, false, true)
-                , createLine(true, false, true, false));
+    @DisplayName("사다리 게임에 따라 플레이어가 이동한다..")
+    @Test
+    void move() {
+        Players players = createPlayers();
+        Lines lines = new Lines(createLines());
+
+        Players resultPlayers = lines.play(players);
+
+        assertEquals(new Player("SKT", 0), resultPlayers.getPlayer(0));
+        assertEquals(new Player("TS", 1), resultPlayers.getPlayer(1));
+        assertEquals(new Player("KT", 2), resultPlayers.getPlayer(2));
+        assertEquals(new Player("LG", 3), resultPlayers.getPlayer(3));
+
+
     }
+
+    Players createPlayers() {
+        return new Players(
+                List.of(new Player("LG", 0)
+                        , new Player("SKT", 1)
+                        , new Player("KT", 2)
+                        , new Player("TS", 3)));
+    }
+
+    List<Line> createLines() {  //LG              SKT          KT           TS
+        return List.of(createLine(true, false, true, false)
+                , createLine(false, true, false, true)
+                , createLine(true, false, true, false)
+                , createLine(true, false, true, false));
+    }                              // SKT           TS        KT               LG
 
     Line createLine(boolean first, boolean second, boolean third, boolean fourth) {
         return new Line(List.of(first, second, third, fourth));
