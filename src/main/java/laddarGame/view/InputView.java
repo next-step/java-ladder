a153f9dbebe29937810eb933ca25eab858da0a7d
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-03-26 02:53:27 +0900

    feat(InputView): 결과를 보고싶은 사람 입력 기능 추가

--- src/main/java/laddarGame/view/InputView.java
+++ src/main/java/laddarGame/view/InputView.java
@@ -18,8 +18,13 @@ public class InputView {
         return height;
     }
 
-    public static String InputPrize() {
+    public static String prize() {
         System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        return scanner.nextLine();
+    }
+
+    public static String prizeOfPlayer() {
+        System.out.println("결과를 보고 싶은 사람은?");
         return scanner.nextLine();
     }
 }
