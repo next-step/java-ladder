e5f603c9bb9b2852523b409aa0c9c53bf7dee74f
Jake <cocahack@naver.com>
2021-03-26 22:14:10 +0900

    refactor: 스트림 실습 완료

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -10,24 +10,29 @@ import java.util.stream.Collectors;
 
 public class StreamStudy {
 
+    private StreamStudy() {
+    }
+
     public static long countWords() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+            .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream()
+                    .filter(w -> w.length() > 12)
+                    .count();
     }
 
     public static void printLongestWordTop100() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+            .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+             .filter(w -> w.length() > 12)
+             .sorted((s1, s2) -> s2.length() - s1.length())
+             .distinct()
+             .forEach(s -> System.out.println(s.toLowerCase()));
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -35,10 +40,15 @@ public class StreamStudy {
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers.stream()
+                      .reduce(0, Integer::sum);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                      .filter(n -> n > 3)
+                      .map(n -> n * 2)
+                      .reduce(Integer::sum)
+                      .orElse(0);
     }
 }
