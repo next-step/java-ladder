f85bf6782b2c19b8e8ec068fba983e9a7554eab8
jhLim97 <wnsgur2749@naver.com>
2021-03-27 21:21:31 +0900

    feat: 입력 클래스 생성

---
+++ src/main/java/ladder/InputView.java
@@ -0,0 +1,26 @@
+package ladder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final String SEPARATOR = ",";
+
+    public List<String> inputNames() {
+        System.out.println("참여할 사람 이름을 입력하세요.");
+
+        return Arrays.stream(scanner.nextLine()
+                .split(SEPARATOR))
+                .collect(Collectors.toList());
+    }
+
+    public int inputHeight() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+
+        return scanner.nextInt();
+    }
+}
