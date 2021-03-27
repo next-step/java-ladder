223cfb99b831477d7f882ab8e0b194319aa7b84b
apdkeb <apdkeb@naver.com>
2021-03-27 16:22:51 +0900

    feat(Line) : 포인트 생성 로직 추가
    2021. 03. 27.
    참여자 만큼 포인트 생성
    
    Resolvs : loop-study
    See also : ohtaeg

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -1,20 +1,31 @@
 package ladder.domain;
 
+import ladder.strategy.Point;
+import ladder.strategy.PointStrategy;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Line {
     private static final String MINIMUM_PERSON_ERRRO = "2명 이상 참여해야합니다.";
     private static final int MINIMUM_PERSON = 2;
+    private static final int MINUS_INDEX_SIZE = 1;
     private static final int POINT_START_INDEX = 0;
 
+    private final PointStrategy pointStrategy;
     private final List<Boolean> points;
 
-    public Line (int countOfPerson) {
+    public Line(int countOfPerson) {
+        this(countOfPerson, Point.instance());
+    }
+
+    public Line(int countOfPerson, PointStrategy pointStrategy) {
         isPersonUnderTwo(countOfPerson);
+        this.pointStrategy = pointStrategy;
         this.points = createPoints(countOfPerson);
+        points.stream()
+                .forEach(System.out::println);
     }
 
     private void isPersonUnderTwo(int countOfPerson) {
@@ -24,13 +35,35 @@ public class Line {
     }
 
     private List<Boolean> createPoints(int countOfPerson) {
-        return IntStream.range(POINT_START_INDEX, countOfPerson)
-                .mapToObj(this::createPoint)
-                .collect(Collectors.toList());
+        return recursionPoint((countOfPerson-MINUS_INDEX_SIZE), new ArrayList<>());
     }
 
-    public boolean createPoint(int pointIndex) {
-        return false;
+    private List<Boolean> recursionPoint(int maxSize, List<Boolean> points) {
+        if (points.isEmpty()) {
+            recursionPoint(maxSize, addPoint(points, pointStrategy.isPoint()));
+        }
+
+        if (maxSize == points.size()) {
+            return addPoint(points, Boolean.FALSE);
+        }
+
+        if (!isMaxSize(maxSize, points) && !points.get(points.size()-MINUS_INDEX_SIZE)) {
+            recursionPoint(maxSize, addPoint(points, pointStrategy.isPoint()));
+        }
+
+        if (!isMaxSize(maxSize, points) && points.get(points.size()-MINUS_INDEX_SIZE)) {
+            recursionPoint(maxSize, addPoint(points, Boolean.FALSE));
+        }
+        return points;
+    }
+
+    private boolean isMaxSize(int maxSize, List<Boolean> points) {
+        return maxSize <= points.size();
+    }
+
+    private List<Boolean> addPoint(List<Boolean> points, boolean isPoint) {
+        points.add(isPoint);
+        return points;
     }
 
     public List<Boolean> readOnlyPoints() {
