9e130cef0f17434e73f6eba93c8206cfea8399be
apdkeb <apdkeb@naver.com>
2021-03-27 18:10:23 +0900

    feat : 입력, 출력 추가
    2021. 03. 27.
    InputView, ResultView, LineEnum 추가
    
    Resolvs : loop-study
    See also : ohtaeg

--- src/main/java/ladder/controller/LadderController.java
+++ src/main/java/ladder/controller/LadderController.java
@@ -1,7 +1,21 @@
 package ladder.controller;
 
+import ladder.domain.Ladder;
+import ladder.view.InputView;
+import ladder.view.ResultView;
+
+import java.util.List;
+
 public class LadderController {
     public void start() {
+        InputView inputView = new InputView();
+        List<String> person = inputView.person();
+        int ladderSize = inputView.ladderSize();
 
+        Ladder ladder = new Ladder(ladderSize, person.size());
+
+        ResultView resultView = new ResultView();
+        resultView.printPerson(person);
+        resultView.printLadder(ladder);
     }
 }
