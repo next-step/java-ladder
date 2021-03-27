fdba5673239c4c47414650817dfc2889011dca8f
apdkeb <apdkeb@naver.com>
2021-03-27 13:53:08 +0900

    test(Line) : 2명 미만 에러 확인
    2021. 03. 27.
    2명 미만 에러 테스트 추가
    
    Resolves : loop-study
    See also : ohtaeg

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -6,12 +6,13 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LineTest {
 
     @Test
-    @DisplayName("참여자 만큼 라인 생성")
-    void tdd() {
+    @DisplayName("참여자 만큼 라인 포인트 생성")
+    public void createLinePointTest() throws Exception {
         //given
         int countOfPerson = 3;
 
@@ -21,5 +22,20 @@ public class LineTest {
 
         //then
         assertThat(size).isEqualTo(countOfPerson);
+    }
+
+    @Test
+    @DisplayName("참여자 2명 미만 일시 에러")
+    public void personTwoUnderException() throws Exception {
+        //given
+        int countOfPersion = 2;
+
+        //when
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Line(countOfPersion);
+        });
+
+        //then
+
     }
 }
