e381c19bf7b582d109f68e75404974ed242de968
apdkeb <apdkeb@naver.com>
2021-03-27 16:32:21 +0900

    test(Ladder) : 테스트 추가
    2021. 03. 27.
    LadderTest 추가
    
    Resolvs : loop-study
    See also : ohtaeg

---
+++ src/test/java/ladder/domain/LadderTest.java
@@ -0,0 +1,25 @@
+package ladder.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LadderTest {
+
+    @Test
+    public void createLadderTest() throws Exception {
+        //given
+        int countOfPerson = 5;
+        int ladderSize = 5;
+
+        //when
+        Ladder ladder = new Ladder(countOfPerson, ladderSize);
+        List<Line> lines = ladder.readOnlyLines();
+
+        //then
+        assertThat(lines.size()).isEqual(ladderSize) ;
+    }
+}
