60a793722ea873c009f8dc8f7467163d0bb226dd
apdkeb <apdkeb@naver.com>
2021-03-27 16:38:45 +0900

    test(Ladder) : 테스트 수정
    2021. 03. 27.
    배열 형식으로 변경
    
    Resolvs : loop-study
    See also : ohtaeg

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -1,7 +1,10 @@
 package ladder.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -9,14 +12,15 @@ import static org.assertj.core.api.Assertions.*;
 
 public class LadderTest {
 
-    @Test
-    public void createLadderTest() throws Exception {
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("사다리 길이 생성 확인")
+    public void createLadderTest(int ladderSize) throws Exception {
         //given
         int countOfPerson = 5;
-        int ladderSize = 5;
 
         //when
-        Ladder ladder = new Ladder(countOfPerson, ladderSize);
+        Ladder ladder = new Ladder(ladderSize, countOfPerson);
         List<Line> lines = ladder.readOnlyLines();
 
         //then
