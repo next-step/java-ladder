dfa11ea82d42a86301ac673b101d487dd7f3e0bc
Dayeon myeong <meme91322367@gmail.com>
2021-03-29 22:11:52 +0900

    [사다리타기] 1단계 (#825)
    * feat : 람다실습1
    
    * feat : 람다실습2
    
    * feat : stream 실습
    
    * feat : Optional 실습1
    
    * feat : Optional 실습2
    
    * feat : Optional 실습3

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,40 +5,64 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
 
-    public static long countWords() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+  public static long countWords() throws IOException {
+    String contents = new String(Files.readAllBytes(Paths
+        .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+    List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+    long count = 0;
+    for (String w : words) {
+      if (w.length() > 12) {
+        count++;
+      }
     }
+    return count;
+  }
 
-    public static void printLongestWordTop100() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+  public static void printLongestWordTop100() throws IOException {
+    String contents = new String(Files.readAllBytes(Paths
+        .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+    List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
-    }
+    words.stream()
+        .filter(StreamStudy::checkWordLength)
+        .sorted(Comparator.comparing(String::length))
+        .distinct()
+        .limit(100)
+        .map(String::toLowerCase)
+        .forEach(System.out::println);
+  }
 
-    public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
-    }
+  private static boolean checkWordLength(String word) {
+    return word.length() > 12;
+  }
 
-    public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
-    }
+  public static List<Integer> doubleNumbers(List<Integer> numbers) {
+    return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
+  }
 
-    public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
-    }
+  public static long sumAll(List<Integer> numbers) {
+    return numbers.stream().reduce(0, (x, y) -> x + y);
+  }
+
+  public static long sumOverThreeAndDouble(List<Integer> numbers) {
+    return numbers.stream()
+        .filter(StreamStudy::checkOverThree)
+        .map(StreamStudy::makeDouble)
+        .reduce(0, (x, y) -> x + y);
+  }
+
+  private static boolean checkOverThree(int number) {
+    return number > 3;
+  }
+
+  private static int makeDouble(int number) {
+    return number * 2;
+  }
 }
--- src/main/java/nextstep/optional/Expression.java
+++ src/main/java/nextstep/optional/Expression.java
@@ -1,25 +1,28 @@
 package nextstep.optional;
 
+import java.util.Arrays;
+
 enum Expression {
-    PLUS("+"), MINUS("-"), TIMES("*"), DIVIDE("/");
+  PLUS("+"), MINUS("-"), TIMES("*"), DIVIDE("/");
 
-    private String expression;
+  private String expression;
+  private static final String INVALID_EXPRESSION = "%s는 사칙연산에 해당하지 않는 표현식입니다.";
 
-    Expression(String expression) {
-        this.expression = expression;
-    }
+  Expression(String expression) {
+    this.expression = expression;
+  }
 
-    private static boolean matchExpression(Expression e, String expression) {
-        return expression.equals(e.expression);
-    }
+  private static boolean matchExpression(Expression e, String expression) {
+    return expression.equals(e.expression);
+  }
 
-    static Expression of(String expression) {
-        for (Expression v : values()) {
-            if (matchExpression(v, expression)) {
-                return v;
-            }
-        }
+  static Expression of(String expression) {
 
-        throw new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression));
-    }
+    return Arrays.stream(values())
+        .filter(v -> matchExpression(v, expression))
+        .findFirst()
+        .orElseThrow(() -> new IllegalArgumentException(
+            String.format(INVALID_EXPRESSION, expression)
+        ));
+  }
 }
