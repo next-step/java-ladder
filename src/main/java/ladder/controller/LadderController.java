7239c0a6edd222fff66069cdd44c9a86ba0549bd
jhLim97 <wnsgur2749@naver.com>
2021-03-30 04:09:29 +0900

    feat: 입출력 요구사항 구현

--- src/main/java/ladder/controller/LadderController.java
+++ src/main/java/ladder/controller/LadderController.java
@@ -5,19 +5,26 @@ import ladder.domain.Winning;
 import ladder.dto.InputManagement;
 import ladder.domain.Ladder;
 import ladder.dto.Name;
+import ladder.dto.Results;
 import ladder.view.InputView;
 import ladder.view.ResultView;
+
+import java.util.List;
 
 public class LadderController {
 
     private final InputView inputView;
+    private final ResultView resultView;
 
-    public LadderController(InputView inputView) {
+    public LadderController(InputView inputView, ResultView resultView) {
         this.inputView = inputView;
+        this.resultView = resultView;
     }
 
     public void run() {
-        InputManagement inputManagement = new InputManagement(inputView.names(), inputView.height());
+        List<String> names = inputView.names();
+        Results results = new Results(inputView.results());
+        InputManagement inputManagement = new InputManagement(names, inputView.height());
 
         Ladder ladder = new Ladder(inputManagement.getNames().size(), inputManagement.getHeight());
         ladder.drawLine(new RandomDraw());
@@ -25,10 +32,18 @@ public class LadderController {
         Winning winning = new Winning();
         int startIndex = 0;
         for (Name name : inputManagement.getNames()) {
-            winning.record(name, ladder.result(startIndex++));
+            int resultIndex = ladder.result(startIndex++);
+            winning.record(name, results.getResults().get(resultIndex));
         }
 
-        ResultView resultView = new ResultView();
         resultView.print(inputManagement.getNames(), ladder.getLadder());
+        resultView.printResultList(results);
+
+        int isRemainTurn = inputManagement.getNames().size();
+        String person = "";
+        do {
+            person = inputView.person();
+            resultView.printResult(winning, person);
+        } while (--isRemainTurn > 0 && !person.equals("all"));
     }
 }
--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -12,15 +12,27 @@ public class InputView {
 
     public List<String> names() {
         System.out.println("참여할 사람 이름을 입력하세요.");
+        return inputString();
+    }
 
-        return Arrays.stream(scanner.nextLine()
-                .split(SEPARATOR))
-                .collect(Collectors.toList());
+    public List<String> results() {
+        System.out.println("실행 결과를 입력하세요.");
+        return inputString();
     }
 
     public int height() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
+        return Integer.parseInt(scanner.nextLine());
+    }
 
-        return scanner.nextInt();
+    public String person() {
+        System.out.println("결과를 보고 싶은 사람은?");
+        return scanner.nextLine();
+    }
+
+    private List<String> inputString() {
+        return Arrays.stream(scanner.nextLine()
+                .split(SEPARATOR))
+                .collect(Collectors.toList());
     }
 }
