d244ce96792b8c56e291ed4d007acf41164e0e03
seokrae <kslbsh@gmail.com>
2021-03-30 20:31:31 +0900

    refactor: 요구사항 3 - 예외처리 테스트

--- README.md
+++ README.md
@@ -63,7 +63,7 @@
 
   - 힌트
     - [Guide To Java 8 Optional](https://www.baeldung.com/java-optional) 문서를 참고해 Optional 사용 방법을 익힌다.
-    - 자바의 enum 전체 값은 values() 메소드를 통해 배열로 접근 가능하다.
-    - Arrays.stream()을 이용해 배열을 stream으로 생성할 수 있다.
-    - 일치하는 값 하나를 추출할 때 findFirst() 메소드를 활용 가능하다.
-    - Optional의 orElseThrow() 메소드 활용해 구현한다.
+    - 자바의 `enum` 전체 값은 `values()` 메소드를 통해 배열로 접근 가능하다.
+    - `Arrays.stream()`을 이용해 배열을 stream으로 생성할 수 있다.
+    - 일치하는 값 하나를 추출할 때 `findFirst()` 메소드를 활용 가능하다.
+    - Optional의 `orElseThrow()` 메소드 활용해 구현한다.
--- src/test/java/nextstep/optional/ExpressionTest.java
+++ src/test/java/nextstep/optional/ExpressionTest.java
@@ -1,5 +1,6 @@
 package nextstep.optional;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,11 +8,14 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 
 public class ExpressionTest {
+
+    @DisplayName(value = "Expression의 정의된 연산을 조회 테스트")
     @Test
     public void of() {
         assertThat(Expression.PLUS == Expression.of("+")).isTrue();
     }
 
+    @DisplayName(value = "Expression의 정의된 연산이 아닌 경우 예외 테스트")
     @Test
     public void notValidExpression() {
         assertThatIllegalArgumentException()
