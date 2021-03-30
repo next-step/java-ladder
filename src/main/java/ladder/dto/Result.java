908e1d9379bebd21f1bdf5bcb567c305e752eecf
jhLim97 <wnsgur2749@naver.com>
2021-03-30 19:39:37 +0900

    refactor: 결과를 뜻하는 문자열 포장

---
+++ src/main/java/ladder/dto/Result.java
@@ -0,0 +1,15 @@
+package ladder.dto;
+
+public class Result {
+
+    private final String result;
+
+
+    public Result(String result) {
+        this.result = result;
+    }
+
+    public String getResult() {
+        return result;
+    }
+}
