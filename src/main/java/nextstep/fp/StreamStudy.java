151d76267c0a4985f1df5904985bb90cc0a083d0
hwangjeonghyeon <deert101@gmail.com>
2021-03-30 23:30:02 +0900

    [feat] stream method 실습 1,2

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
@@ -22,12 +22,28 @@ public class StreamStudy {
         return count;
     }
 
-    public static void printLongestWordTop100() throws IOException {
+//    단어의 길이가 12자를 초과하는 단어를 추출한다.
+//    12자가 넘는 단어 중 길이가 긴 순서로 100개의 단어를 추출한다.
+//    단어 중복을 허용하지 않는다. 즉, 서로 다른 단어 100개를 추출해야 한다.
+//    추출한 100개의 단어를 출력한다. 모든 단어는 소문자로 출력해야 한다.
+    public static List<String> printLongestWordTop100() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        return words.stream()
+                    .filter(word -> word.length() > 12)
+                    .sorted(Comparator.comparing(String::length).reversed())
+                    .distinct()
+                    .limit(100)
+                    .map(String::toLowerCase)
+                    .collect(Collectors.toList());
+    }
+
+    public static boolean numberCheck(String text) {
+        boolean flag = text.matches("^[0-9]*$");
+
+        return flag;
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +55,10 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+
+        return numbers.stream()
+                .filter(num -> num > 3)
+                .map(num -> num * 2)
+                .reduce(0, (a, b) -> a + b);
     }
 }
