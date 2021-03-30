2f8fab17483805bb9081ca00bf77cb244874af2e
623nana <6230nana@gmail.com>
2021-03-30 20:56:49 +0900

    style: fomatter 적용

--- src/main/java/nextstep/optional/Computer.java
+++ src/main/java/nextstep/optional/Computer.java
@@ -1,39 +1,42 @@
 package nextstep.optional;
 
 public class Computer {
-    private Soundcard soundcard;
 
-    public Computer(Soundcard soundcard) {
-        this.soundcard = soundcard;
+  private Soundcard soundcard;
+
+  public Computer(Soundcard soundcard) {
+    this.soundcard = soundcard;
+  }
+
+  public Soundcard getSoundcard() {
+    return soundcard;
+  }
+
+  public static class Soundcard {
+
+    private USB usb;
+
+    public Soundcard(USB usb) {
+      super();
+      this.usb = usb;
     }
 
-    public Soundcard getSoundcard() {
-        return soundcard;
+    public USB getUsb() {
+      return usb;
+    }
+  }
+
+  public static class USB {
+
+    private String version;
+
+    public USB(String version) {
+      super();
+      this.version = version;
     }
 
-    public static class Soundcard {
-        private USB usb;
-
-        public Soundcard(USB usb) {
-            super();
-            this.usb = usb;
-        }
-
-        public USB getUsb() {
-            return usb;
-        }
+    public String getVersion() {
+      return this.version;
     }
-
-    public static class USB {
-        private String version;
-
-        public USB(String version) {
-            super();
-            this.version = version;
-        }
-
-        public String getVersion() {
-            return this.version;
-        }
-    }
+  }
 }
--- src/main/java/nextstep/optional/ComputerStore.java
+++ src/main/java/nextstep/optional/ComputerStore.java
@@ -4,23 +4,24 @@ import nextstep.optional.Computer.Soundcard;
 import nextstep.optional.Computer.USB;
 
 public class ComputerStore {
-    public static final String UNKNOWN_VERSION = "UNKNOWN";
 
-    public static String getVersion(Computer computer) {
-        String version = UNKNOWN_VERSION;
-        if (computer != null) {
-            Soundcard soundcard = computer.getSoundcard();
-            if (soundcard != null) {
-                USB usb = soundcard.getUsb();
-                if (usb != null) {
-                    version = usb.getVersion();
-                }
-            }
+  public static final String UNKNOWN_VERSION = "UNKNOWN";
+
+  public static String getVersion(Computer computer) {
+    String version = UNKNOWN_VERSION;
+    if (computer != null) {
+      Soundcard soundcard = computer.getSoundcard();
+      if (soundcard != null) {
+        USB usb = soundcard.getUsb();
+        if (usb != null) {
+          version = usb.getVersion();
         }
-        return version;
+      }
     }
+    return version;
+  }
 
-    public static String getVersionOptional(Computer computer) {
-        return null;
-    }
+  public static String getVersionOptional(Computer computer) {
+    return null;
+  }
 }
