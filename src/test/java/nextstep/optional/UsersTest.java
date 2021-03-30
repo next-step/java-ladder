f604c788012e0369743f90353b89a4a889810603
seokrae <kslbsh@gmail.com>
2021-03-30 20:23:05 +0900

    refactor: 요구사항 2 - 조건 값 또는 기본 값을 반환 리펙토링

--- src/test/java/nextstep/optional/UsersTest.java
+++ src/test/java/nextstep/optional/UsersTest.java
@@ -1,18 +1,20 @@
 package nextstep.optional;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UsersTest {
 
+    @DisplayName(value = "getUser() filtering 테스트")
     @Test
     public void getUser() {
         Users users = new Users();
         assertThat(users.getUser("crong")).isEqualTo(new User("crong", 35));
     }
 
-
+    @DisplayName(value = "getUser() filtering default 값 확인 테스트")
     @Test
     public void getDefaultUser() {
         Users users = new Users();
