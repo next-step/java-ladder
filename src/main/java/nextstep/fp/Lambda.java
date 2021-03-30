1b542c6a7e3f10f6ae26b614f5b67399108c0649
seokrae <kslbsh@gmail.com>
2021-03-30 18:53:17 +0900

    refactor: 실습2 - 함수형 인터페이스 추가로 조건 추가 테스트

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -18,20 +18,13 @@ public class Lambda {
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> System.out.println("Hello from thread")).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+    public static int sumAll(List<Integer> numbers, Conditional conditional) {
+        return numbers.stream()
+                .filter(conditional::test)
+                .reduce(0, Integer::sum);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
--- src/test/java/nextstep/fp/LambdaTest.java
+++ src/test/java/nextstep/fp/LambdaTest.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -16,24 +17,28 @@ public class LambdaTest {
         numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
     }
 
+    @DisplayName(value = "forEach로 반복되는 코드 테스트")
     @Test
-    public void printAllOld() throws Exception {
+    public void printAllOld() {
         Lambda.printAllOld(numbers);
     }
 
+    @DisplayName(value = "메서드 레퍼런스를 통한 반복 코드 테스트")
     @Test
-    public void printAllLambda() throws Exception {
+    public void printAllLambda() {
         Lambda.printAllLambda(numbers);
     }
 
+    @DisplayName(value = "Runnable도 FunctionalInterface이므로 람다를 통한 스레드 테스트가 가능")
     @Test
-    public void runThread() throws Exception {
+    public void runThread() {
         Lambda.runThread();
     }
 
+    @DisplayName(value = "sumAll 전체 숫자를 합산하도록 람다식 작성 테스트")
     @Test
-    public void sumAll() throws Exception {
-        int sum = Lambda.sumAll(numbers);
+    public void sumAll() {
+        int sum = Lambda.sumAll(numbers, (number) -> true);
         assertThat(sum).isEqualTo(21);
     }
 
