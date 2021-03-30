6f05eb6a4ddeff6354da99f5fb534a9dfdf71dd4
seokrae <kslbsh@gmail.com>
2021-03-30 18:48:38 +0900

    refactor: 실습1 - 익명 클래스를 람다로 전환

--- src/test/java/nextstep/fp/CarTest.java
+++ src/test/java/nextstep/fp/CarTest.java
@@ -1,10 +1,12 @@
 package nextstep.fp;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
+    @DisplayName(value = "익명 클래스를 람다식으로 변경하여 이동하는 테스트 코드를 완성")
     @Test
     public void 이동() {
         Car car = new Car("pobi", 0);
@@ -12,6 +14,7 @@ public class CarTest {
         assertThat(actual).isEqualTo(new Car("pobi", 1));
     }
 
+    @DisplayName(value = "익명 클래스를 람다식으로 변경하여 정지하는 테스트 코드를 완성")
     @Test
     public void 정지() {
         Car car = new Car("pobi", 0);
