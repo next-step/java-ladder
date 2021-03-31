cdb85d3a8ebd1e2f61b1aee4595e9d4526d4a48d
623nana <6230nana@gmail.com>
2021-03-31 22:26:15 +0900

    feat: 참가자 이름 리스트 일급 컬렉션

---
+++ src/main/java/ladder/domain/Players.java
@@ -0,0 +1,17 @@
+package ladder.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Players {
+
+  private final List<Name> players;
+
+  public Players(List<Name> players) {
+    this.players = players;
+  }
+
+  public List<Name> names() {
+    return Collections.unmodifiableList(players);
+  }
+}
