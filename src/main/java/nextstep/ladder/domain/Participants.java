f00a3a09433a292bb2c2c0b374d5038a575ffa41
seokrae <kslbsh@gmail.com>
2021-03-31 23:58:48 +0900

    refactor: Participants 필드 타입 변경 List<String> -> List<Name>

--- src/main/java/nextstep/ladder/domain/Participants.java
+++ src/main/java/nextstep/ladder/domain/Participants.java
@@ -8,24 +8,25 @@ import java.util.stream.Collectors;
 public class Participants {
 
     private static final String SPLIT_DELIMITER = ",";
-    private final List<String> users;
+    private final List<Name> users;
 
-    private Participants(String users) {
+    private Participants(final String users) {
         this.users = parseUsers(users);
     }
 
-    private List<String> parseUsers(String users) {
+    private List<Name> parseUsers(final String users) {
         return Arrays.stream(users.split(SPLIT_DELIMITER))
                 .map(String::trim)
+                .map(Name::of)
                 .collect(Collectors.toList());
     }
 
-    public static Participants valueOf(String users) {
+    public static Participants valueOf(final String users) {
         return new Participants(users);
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(final Object o) {
         if (this == o) return true;
         if (!(o instanceof Participants)) return false;
         final Participants that = (Participants) o;
