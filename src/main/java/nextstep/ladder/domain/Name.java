3bc7113d0d4b5292db6e735f388697fcb990e024
seokrae <kslbsh@gmail.com>
2021-03-31 23:57:17 +0900

    refactor: 참여자 이름 원시 값 wrapper클래스 생성 및 테스트

---
+++ src/main/java/nextstep/ladder/domain/Name.java
@@ -0,0 +1,36 @@
+package nextstep.ladder.domain;
+
+import java.util.Objects;
+
+public class Name {
+    private final String name;
+
+    public Name(final String name) {
+        if(name.length() > 5) {
+            throw new IllegalArgumentException("이름은 최대 5글자까지 부여할 수 있습니다.");
+        }
+        this.name = name;
+    }
+
+    public static Name of(final String name) {
+        return new Name(name);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Name)) return false;
+        final Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(name);
+    }
+}
