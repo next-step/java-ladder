dbeb245c9335610ed9933cf96f3cdda79bd60d00
seokrae <kslbsh@gmail.com>
2021-04-01 00:20:32 +0900

    refactor: 이름 1 ~ 5자 예외 처리 조건 추가

--- src/main/java/nextstep/ladder/domain/Name.java
+++ src/main/java/nextstep/ladder/domain/Name.java
@@ -5,10 +5,11 @@ import java.util.Objects;
 public class Name {
 
     private static final int NAME_MAX_SIZE = 5;
+    private static final int NAME_MIN_SIZE = 1;
     private final String name;
 
     public Name(final String name) {
-        if(name.length() > NAME_MAX_SIZE) {
+        if(name.length() > NAME_MAX_SIZE || name.length() < NAME_MIN_SIZE) {
             throw new IllegalArgumentException("이름은 최대 5글자까지 부여할 수 있습니다.");
         }
         this.name = name;
