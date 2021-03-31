75969bc671ff5b545f823139eb727501c2daead9
seokrae <kslbsh@gmail.com>
2021-03-31 23:50:25 +0900

    refactor: 참여자 생성 로직 구현 및 테스트

--- src/main/java/nextstep/ladder/domain/Participants.java
+++ src/main/java/nextstep/ladder/domain/Participants.java
@@ -1,5 +1,44 @@
 package nextstep.ladder.domain;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class Participants {
 
+    private static final String SPLIT_DELIMITER = ",";
+    private final List<String> users;
+
+    private Participants(String users) {
+        this.users = parseUsers(users);
+    }
+
+    private List<String> parseUsers(String users) {
+        return Arrays.stream(users.split(SPLIT_DELIMITER))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
+    public static Participants valueOf(String users) {
+        return new Participants(users);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Participants)) return false;
+        final Participants that = (Participants) o;
+        return Objects.equals(users, that.users);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(users);
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(users);
+    }
 }
---
+++ src/test/java/nextstep/ladder/domain/ParticipantsTest.java
@@ -0,0 +1,20 @@
+package nextstep.ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ParticipantsTest {
+
+    @DisplayName("참여자 생성 테스트")
+    @Test
+    void create_참여자() {
+        // given
+        Participants participants = Participants.valueOf("user1,user2,user3");
+        // when
+        Participants expected = Participants.valueOf("user1, user2, user3");
+        // then
+        assertThat(participants).isEqualTo(expected);
+    }
+}
