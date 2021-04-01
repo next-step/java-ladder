3de37ae5fd1cc2ec0fa79b97415125ffacda3f12
Donghui-Lee <ehdgml3206@gmail.com>
2021-04-01 22:15:16 +0900

    [사다리타기 - Step1] 리뷰 요청드립니다. (#836)
    * docs: 요구사항 정리
    
    * feat: 익명 클래스를 람다로 전환
    
    * feat: 람다를 활용해 중복 제거
    
    * feat: Stream Method 실습 1
    
    * feat: Stream Method 실습 2
    
    * feat: Optional 실습 1
    
    * feat: Optional 실습 2
    
    * feat: Optional 실습 3

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,6 +29,12 @@ public class StreamStudy {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         // TODO 이 부분에 구현한다.
+        words.stream()
+            .filter(w -> w.length() > 12)
+            .distinct()
+            .limit(100)
+            .sorted(Comparator.comparing(String::length).reversed())
+            .forEach(s -> System.out.println(s.toLowerCase()));
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +46,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+            .filter(n -> n > 3)
+            .map(n -> n * 2)
+            .reduce(0, (x, y) -> x + y);
     }
 }
