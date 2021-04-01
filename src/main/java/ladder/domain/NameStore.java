fe2e688a3603bc552660fe68840f152e61bb17bd
623nana <6230nana@gmail.com>
2021-04-01 18:04:14 +0900

    feat: 입력 값 검증 로직 추가

--- src/main/java/ladder/domain/NameStore.java
+++ src/main/java/ladder/domain/NameStore.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import ladder.constant.Constant;
+import ladder.util.InputValidator;
 
 public class NameStore {
 
   public static List<Name> createPlayers(String inputNames) {
+    validateInputNames(inputNames);
     return stringToList(inputNames).stream()
         .map(Name::new)
         .collect(Collectors.toList());
@@ -19,5 +21,11 @@ public class NameStore {
 
   private static String[] splitString(String inputNames) {
     return inputNames.split(Constant.REGEX);
+  }
+
+  public static void validateInputNames(String inputNames) {
+    if (InputValidator.isEmptyOrBlank(inputNames)) {
+      throw new IllegalArgumentException("유효하지 않은 입력값입니다.");
+    }
   }
 }
