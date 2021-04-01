849a0fae67e23c1de6dafc24a3d1c6bc7ac2fde8
seokrae <kslbsh@gmail.com>
2021-04-01 21:46:47 +0900

    refactor: height 클래스 예외처리 테스트

--- src/test/java/nextstep/ladder/domain/HeightTest.java
+++ src/test/java/nextstep/ladder/domain/HeightTest.java
@@ -2,6 +2,8 @@ package nextstep.ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
@@ -20,9 +22,10 @@ class HeightTest {
     }
 
     @DisplayName("사다리 높이 생성 예외처리")
-    @Test
-    void create_Exception() {
+    @ParameterizedTest(name = "{0} 입력 시 예외 처리")
+    @ValueSource(ints = {-1, 0})
+    void create_Exception(int given) {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> Height.valueOf(-1));
+                .isThrownBy(() -> Height.valueOf(given));
     }
 }
