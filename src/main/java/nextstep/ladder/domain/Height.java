2f9371a09c7e208b2d34267fef77b6c507ac5440
seokrae <kslbsh@gmail.com>
2021-04-01 20:49:25 +0900

    refactor: 상수처리

--- src/main/java/nextstep/ladder/domain/Height.java
+++ src/main/java/nextstep/ladder/domain/Height.java
@@ -3,6 +3,7 @@ package nextstep.ladder.domain;
 import java.util.Objects;
 
 public class Height {
+    public static final int MIN_HEIGHT = 0;
     private final int value;
 
     public Height(int value) {
@@ -10,8 +11,8 @@ public class Height {
     }
 
     public static Height valueOf(int value) {
-        if(value < 0) {
-            throw new IllegalArgumentException("사다리 높이가 0보다 작을 수는 없습니다.");
+        if(value < MIN_HEIGHT) {
+            throw new IllegalArgumentException("높이가 0보다 작을 수는 없습니다.");
         }
         return new Height(value);
     }
