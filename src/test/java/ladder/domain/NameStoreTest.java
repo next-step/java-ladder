a373ee0aba40f157e967b2dbb1ef4db690fc9a36
623nana <6230nana@gmail.com>
2021-04-01 18:04:49 +0900

    test: 입력 값 검증 테스트

--- src/test/java/ladder/domain/NameStoreTest.java
+++ src/test/java/ladder/domain/NameStoreTest.java
@@ -1,11 +1,13 @@
 package ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class NameStoreTest {
+
   @Test
   @DisplayName("[NameStore] 입력 받은 텍스트 Name 리스트로 반환하는 기능 테스트")
   void inputNamesToListTest() {
@@ -13,5 +15,14 @@ public class NameStoreTest {
     int size = NameStore.createPlayers(inputNames).size();
 
     assertThat(size).isEqualTo(3);
+  }
+
+  @Test
+  @DisplayName("[NameStore] 입력 값 검증 테스트")
+  void validateInputNamesTest() {
+    String inputNames = null;
+
+    assertThatThrownBy(() -> NameStore.validateInputNames(inputNames))
+        .isInstanceOf(IllegalArgumentException.class);
   }
 }
