14ac319babf7fc3795be1decd58aab7e606d226d
seokrae <kslbsh@gmail.com>
2021-04-01 23:48:07 +0900

    refactor: 출력 로직 추가

--- src/main/java/nextstep/ladder/view/ResultView.java
+++ src/main/java/nextstep/ladder/view/ResultView.java
@@ -1,7 +1,55 @@
 package nextstep.ladder.view;
 
-public class ResultView {
-    public void printResult(Object of) {
+import nextstep.ladder.domain.Line;
+import nextstep.ladder.domain.Name;
+import nextstep.ladder.domain.Participants;
+import nextstep.ladder.service.Ladder;
 
+import java.util.List;
+
+public class ResultView {
+
+    public static final String STRING_NAME_FORMAT = "%6s";
+
+    public void printResult(Ladder ladder, Participants participants) {
+        StringBuilder printBuilder = new StringBuilder();
+
+        printHeader(printBuilder);
+        printNames(participants, printBuilder);
+        printLadder(ladder, printBuilder);
+
+        System.out.println(printBuilder.toString());
+    }
+
+    private void printHeader(StringBuilder printBuilder) {
+        printBuilder.append("실행 결과").append(System.lineSeparator());
+    }
+
+    private void printNames(Participants participants, StringBuilder builder) {
+        participants.getUsers()
+                .forEach(name -> builder.append(parseName(name)));
+        builder.append(System.lineSeparator());
+    }
+
+    private String parseName(Name name) {
+        return String.format(STRING_NAME_FORMAT, name);
+    }
+
+    private void printLadder(Ladder ladder, StringBuilder builder) {
+        ladder.lines()
+                .forEach(line -> createLadder(builder, line));
+    }
+
+    private void createLadder(StringBuilder builder, Line line) {
+        List<Boolean> points = line.points();
+        points.forEach(aBoolean -> builder.append(extracted(aBoolean)));
+        builder.append(System.lineSeparator());
+    }
+
+    private String extracted(Boolean pointFlag) {
+        if(pointFlag) {
+            return String.format(STRING_NAME_FORMAT, "-----|");
+        }
+        return String.format(STRING_NAME_FORMAT, "     |");
     }
 }
