c3fa4411ce8c433823147d67387ba3ba5dddd0ef
seokrae <kslbsh@gmail.com>
2021-04-02 15:09:23 +0900

    fix: 내부에서만 사용하는 함수 접근제한자 수정

--- src/main/java/nextstep/ladder/domain/Height.java
+++ src/main/java/nextstep/ladder/domain/Height.java
@@ -3,7 +3,7 @@ package nextstep.ladder.domain;
 import java.util.Objects;
 
 public class Height {
-    public static final int MIN_HEIGHT = 1;
+    private static final int MIN_HEIGHT = 1;
     private final int value;
 
     public Height(int value) {
