7061bd50cb6780029a5ef44deda13e4d7eb457d9
younghyunjo <younghyunjo99@gmail.com>
2021-04-03 07:25:49 +0900

    test(step2):LinkerTest DisplayName 수정

--- src/test/java/ladder/LinkerTest.java
+++ src/test/java/ladder/LinkerTest.java
@@ -12,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LinkerTest {
     @ParameterizedTest
-    @DisplayName("LinkSelector에 따라 링크를 잘 만드는지 테스트")
+    @DisplayName("LinkSelector에서 링크 설정 테스트")
     @MethodSource("provideLinkSelectorAndExpected")
     void Given_LinkSelector_When_Link_Then_ExpectedLink(LinkSelector linkSelector, Link expected) {
         Linker linker = new Linker(linkSelector);
@@ -28,7 +28,7 @@ public class LinkerTest {
     }
 
     @Test
-    @DisplayName("이전 링크가 닫혔으면, 항상 열린 링크를 반환 테스트")
+    @DisplayName("이전 링크가 닫혔으면, 항상 열린 링크 생성 테스트")
     void Given_ClosedPreviousLink_When_Link_Then_AlwaysOpen() {
         //given
         Link previousLink = Link.CLOSE;
@@ -39,7 +39,7 @@ public class LinkerTest {
     }
 
     @Test
-    @DisplayName("이전 링크가 열렸으면, LinkSelector를 사용하는지 테스트")
+    @DisplayName("이전 링크가 열렸으면, LinkSelector에서 Link 설정 테스트")
     void Given_OpenedPreviousLink_When_Link_Then_UseLinkSelector() {
         //given
         Link previousLink = Link.OPEN;
