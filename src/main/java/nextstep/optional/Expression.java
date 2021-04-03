25ba618610451f8f143046e2b142d01cd4e1c540
Dion <ksundong@gmail.com>
2021-04-04 01:43:42 +0900

    style: 내 스타일로 변경

--- src/main/java/nextstep/optional/Expression.java
+++ src/main/java/nextstep/optional/Expression.java
@@ -1,25 +1,29 @@
 package nextstep.optional;
 
 enum Expression {
-    PLUS("+"), MINUS("-"), TIMES("*"), DIVIDE("/");
 
-    private String expression;
+  PLUS("+"),
+  MINUS("-"),
+  TIMES("*"),
+  DIVIDE("/");
 
-    Expression(String expression) {
-        this.expression = expression;
+  private final String expression;
+
+  Expression(String expression) {
+    this.expression = expression;
+  }
+
+  private static boolean matchExpression(Expression e, String expression) {
+    return expression.equals(e.expression);
+  }
+
+  static Expression of(String expression) {
+    for (Expression v : values()) {
+      if (matchExpression(v, expression)) {
+        return v;
+      }
     }
 
-    private static boolean matchExpression(Expression e, String expression) {
-        return expression.equals(e.expression);
-    }
-
-    static Expression of(String expression) {
-        for (Expression v : values()) {
-            if (matchExpression(v, expression)) {
-                return v;
-            }
-        }
-
-        throw new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression));
-    }
+    throw new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression));
+  }
 }
