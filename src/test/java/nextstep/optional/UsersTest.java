c61061bae8a47fe9f1c7b706abdc48e1e2b9a436
Dion <ksundong@gmail.com>
2021-04-04 01:39:47 +0900

    style: 내 스타일로 변경

--- src/test/java/nextstep/optional/UsersTest.java
+++ src/test/java/nextstep/optional/UsersTest.java
@@ -1,21 +1,20 @@
 package nextstep.optional;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class UsersTest {
+import org.junit.jupiter.api.Test;
 
-    @Test
-    public void getUser() {
-        Users users = new Users();
-        assertThat(users.getUser("crong")).isEqualTo(new User("crong", 35));
-    }
+class UsersTest {
 
+  @Test
+  void getUser() {
+    Users users = new Users();
+    assertThat(users.getUser("crong")).isEqualTo(new User("crong", 35));
+  }
 
-    @Test
-    public void getDefaultUser() {
-        Users users = new Users();
-        assertThat(users.getUser("codesquard")).isEqualTo(Users.DEFAULT_USER);
-    }
+  @Test
+  void getDefaultUser() {
+    Users users = new Users();
+    assertThat(users.getUser("codesquard")).isEqualTo(Users.DEFAULT_USER);
+  }
 }
