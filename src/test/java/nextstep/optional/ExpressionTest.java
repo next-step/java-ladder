d3ef8295cd07b9f23ad8b6664128abee88a300fc
Dion <ksundong@gmail.com>
2021-04-04 01:42:19 +0900

    style: 내 스타일로 변경

--- src/test/java/nextstep/optional/ExpressionTest.java
+++ src/test/java/nextstep/optional/ExpressionTest.java
@@ -1,22 +1,22 @@
 package nextstep.optional;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
+import org.junit.jupiter.api.Test;
 
-public class ExpressionTest {
-    @Test
-    public void of() {
-        assertThat(Expression.PLUS == Expression.of("+")).isTrue();
-    }
+class ExpressionTest {
 
-    @Test
-    public void notValidExpression() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> {
-                    Expression.of("&");
-                });
-    }
+  @Test
+  void of() {
+    assertThat(Expression.PLUS == Expression.of("+")).isTrue();
+  }
+
+  @Test
+  void notValidExpression() {
+    assertThatIllegalArgumentException()
+        .isThrownBy(() -> {
+          Expression.of("&");
+        });
+  }
 }
