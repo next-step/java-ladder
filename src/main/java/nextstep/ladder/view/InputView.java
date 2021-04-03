be6bbd7f8932ce7b131ad38e02c7e27d4126460b
seokrae <kslbsh@gmail.com>
2021-04-03 21:06:49 +0900

    fix: [피드백: 문자열 상수 처리](https://github.com/next-step/java-ladder/pull/841#discussion_r606647457)

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 public class InputView {
 
+    public static final String GUIDE_LADDER_INPUT_PARTICIPANTS = "참여할 사람 이름을 입력하세요. (이름은 쉼표( , )로 구분하세요.";
+    public static final String GUIDE_LADDER_INPUT_LADDER_SIZE = "최대 사다리 높이는 몇 개인가요?";
     private final Scanner in = new Scanner(System.in);
 
     public String inputParticipants() {
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표( , )로 구분하세요.");
+        System.out.println(GUIDE_LADDER_INPUT_PARTICIPANTS);
         return in.nextLine();
     }
 
     public int inputLadderHeight() {
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        System.out.println(GUIDE_LADDER_INPUT_LADDER_SIZE);
         return in.nextInt();
     }
 }
