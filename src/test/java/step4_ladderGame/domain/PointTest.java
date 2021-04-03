d688ac30365a60e433f07b242810e1c105498af6
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-04-03 20:12:57 +0900

    test : Point의 다음값과 마지막 값에 대한 테스트 코드 추가

--- src/test/java/step4_ladderGame/domain/PointTest.java
+++ src/test/java/step4_ladderGame/domain/PointTest.java
@@ -3,6 +3,7 @@ package step4_ladderGame.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PointTest {
@@ -15,5 +16,31 @@ class PointTest {
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("첫번째 사다리 Point의 prevPoint는 무조건 false여야 한다.")
+    @Test
+    void testCase2() {
+        Point first = Point.first(true);
 
+        assertThat(first).isEqualTo(Point.of(false, true));
+    }
+
+    @DisplayName("사다리의 다음 값은 curPoint가 prevPoint로 들어가고 새로운 nextPoint값이 culPoint로 만들어 져야 한다.")
+    @Test
+    void testCase3() {
+        Point first = Point.first(true);
+
+        Point next = first.next(false);
+
+        assertThat(next).isEqualTo(Point.of(true, false));
+    }
+
+    @DisplayName("마지막 사다리 Point의 curPoint는 무조건 false 여야 한다.")
+    @Test
+    void testCase4() {
+        Point point = Point.of(false, true);
+
+        Point last = point.last();
+
+        assertThat(last).isEqualTo(Point.of(true, false));
+    }
 }
