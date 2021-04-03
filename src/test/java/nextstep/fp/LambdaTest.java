52b87e3684c7469e5be53e3b38b8577badd7dfa2
Dion <ksundong@gmail.com>
2021-04-04 01:13:34 +0900

    refactor: sumAll 람다를 사용한 버전으로 변경

--- src/test/java/nextstep/fp/LambdaTest.java
+++ src/test/java/nextstep/fp/LambdaTest.java
@@ -1,50 +1,50 @@
 package nextstep.fp;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+class LambdaTest {
 
-public class LambdaTest {
     private List<Integer> numbers;
 
     @BeforeEach
-    public void setup() {
+    void setup() {
         numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
     }
 
     @Test
-    public void printAllOld() throws Exception {
+    void printAllOld() {
         Lambda.printAllOld(numbers);
     }
 
     @Test
-    public void printAllLambda() throws Exception {
+    void printAllLambda() {
         Lambda.printAllLambda(numbers);
     }
 
     @Test
-    public void runThread() throws Exception {
+    void runThread() {
         Lambda.runThread();
     }
 
     @Test
-    public void sumAll() throws Exception {
-        int sum = Lambda.sumAll(numbers);
+    void sumAll() {
+        int sum = Lambda.sumAllMatchingCondition(numbers, x -> true);
         assertThat(sum).isEqualTo(21);
     }
 
     @Test
-    public void sumAllEven() throws Exception {
+    void sumAllEven() {
         int sum = Lambda.sumAllEven(numbers);
         assertThat(sum).isEqualTo(12);
     }
 
     @Test
-    public void sumAllOverThree() throws Exception {
+    void sumAllOverThree() {
         int sum = Lambda.sumAllOverThree(numbers);
         assertThat(sum).isEqualTo(15);
     }
