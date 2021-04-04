f19f5c2d1ab9922682e95ae361f56655af91be24
seokrae <kslbsh@gmail.com>
2021-04-04 19:17:12 +0900

    refactor: 상수 값 및 생성자 접근 제한자 변경

--- src/main/java/nextstep/ladder/domain/Height.java
+++ src/main/java/nextstep/ladder/domain/Height.java
@@ -3,16 +3,19 @@ package nextstep.ladder.domain;
 import java.util.Objects;
 
 public class Height {
+
+    private static final String GUIDE_ERR_HEIGHT_LESS_THAN = "높이가 1보다 작을 수는 없습니다.";
     private static final int MIN_HEIGHT = 1;
+
     private final int value;
 
-    public Height(int value) {
+    private Height(int value) {
         this.value = value;
     }
 
     public static Height valueOf(int value) {
         if(value < MIN_HEIGHT) {
-            throw new IllegalArgumentException("높이가 1보다 작을 수는 없습니다.");
+            throw new IllegalArgumentException(GUIDE_ERR_HEIGHT_LESS_THAN);
         }
         return new Height(value);
     }
