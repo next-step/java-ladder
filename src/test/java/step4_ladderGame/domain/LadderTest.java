d9950f618e80149ec2ec78bc03e1ea93b16f1293
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-04-04 22:09:59 +0900

    test : Ladder 플레이어 이동 테스트 코드 추가

--- src/test/java/step4_ladderGame/domain/LadderTest.java
+++ src/test/java/step4_ladderGame/domain/LadderTest.java
@@ -1,8 +1,11 @@
 package step4_ladderGame.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LadderTest {
 
@@ -11,6 +14,45 @@ class LadderTest {
     void testCase1() {
         Ladder ladder = Ladder.of(5, 5, RandomPointStrategy.getInstance());
 
-        Assertions.assertThat(ladder.size()).isEqualTo(5);
+        assertThat(ladder.size()).isEqualTo(5);
     }
+
+    @DisplayName("사다리에 따라 플레이어를 이동시킨다.")
+    @Test
+    void testCase2() {
+        Player player = Player.of("LG", 0);
+        Ladder ladder = createLadder();
+
+        Player movePlayer = ladder.move(player);
+
+        assertThat(movePlayer.getPosition()).isEqualTo(Position.of(1));
+    }
+
+    Ladder createLadder() {
+        List<LadderLine> ladderLines =
+                List.of(createLadderLine(Direction.RIGHT, Direction.LEFT, Direction.STOP, Direction.RIGHT)
+                        , createLadderLine(Direction.STOP, Direction.LEFT, Direction.RIGHT, Direction.LEFT)
+                        , createLadderLine(Direction.RIGHT, Direction.LEFT, Direction.STOP, Direction.RIGHT));
+        return Ladder.of(ladderLines);
+    }
+
+    LadderLine createLadderLine(Direction first, Direction second, Direction third, Direction fourth) {
+        List<Stair> stairList = List.of(Stair.of(0, createPoint(first)),
+                Stair.of(1, createPoint(second)),
+                Stair.of(2, createPoint(third)),
+                Stair.of(3, createPoint(fourth)));
+        return LadderLine.of(stairList);
+    }
+
+    Point createPoint(Direction direction) {
+        if (direction == Direction.LEFT) {
+            return Point.of(true, false);
+        }
+        if (direction == Direction.RIGHT) {
+            return Point.of(false, true);
+        }
+        return Point.of(false, false);
+    }
+
+
 }
