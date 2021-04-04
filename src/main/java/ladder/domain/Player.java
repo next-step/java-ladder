f25215ea378fedb81e0f5b45693f5ff1c1000d0b
Donghui-Lee <ehdgml3206@naver.com>
2021-04-04 23:53:00 +0900

    feat: Player Class 추가

---
+++ src/main/java/ladder/domain/Player.java
@@ -0,0 +1,17 @@
+package ladder.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private static final String SEPARATOR = ",";
+    private final List<Name> players;
+
+    public Player(String names) {
+        players = new ArrayList<>();
+    }
+
+    public List<Name> getNames() {
+        return null;
+    }
+}
