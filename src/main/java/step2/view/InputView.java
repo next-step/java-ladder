b71b517e72fe312f84aad0c30859deec1fcc90bc
hwangjeonghyeon <deert101@gmail.com>
2021-04-05 01:39:55 +0900

    [feat] 입력 & 결과 VIEW 개발

--- src/main/java/step2/view/InputView.java
+++ src/main/java/step2/view/InputView.java
@@ -1,2 +1,20 @@
-package step2.view;public class InputView {
+package step2.view;
+
+import step2.Persons;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String inputParticipant() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        return scanner.next();
+    }
+
+    public int inputHeight() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        return scanner.nextInt();
+    }
 }
--- src/main/java/step2/view/ResultView.java
+++ src/main/java/step2/view/ResultView.java
@@ -1,2 +1,55 @@
-package step2.view;public class ResultView {
+package step2.view;
+
+import step2.*;
+
+import java.util.List;
+
+public class ResultView {
+
+    private static final String LINE = "-----|";
+    private static final String BLANK = " ";
+    private static final String NO_LINE = "     |";
+    private static final int MAX_LENGTH = 6;
+
+
+    public void printPerson(Persons persons) {
+        persons.personList()
+                .stream()
+                .map(Person::of)
+                .forEach(name -> {
+                    int blank = MAX_LENGTH - name.length();
+                    printBlank(blank);
+                    System.out.print(name);
+                });
+        System.out.println(BLANK);
+    }
+
+    public void printBlank(int blank) {
+        for (int i = 0; i < blank; i++) {
+            System.out.print(BLANK);
+        }
+    }
+
+    public void printLadder(Lines lines) {
+
+        lines.lines().stream()
+                .map(Line::getPoints)
+                .forEach(ResultView::printLine);
+    }
+
+    private static void printLine(List<Point> points) {
+        points.forEach(
+                point -> System.out.print(getLine(point.currentPoint()))
+        );
+        System.out.println(BLANK);
+    }
+
+    private static String getLine(boolean currentPoint) {
+        if (currentPoint) {
+            return LINE;
+        }
+        return NO_LINE;
+    }
+
+
 }
