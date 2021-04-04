31f7f0c85a1208622a147373fa9e240a5cd3319e
Donghui-Lee <ehdgml3206@naver.com>
2021-04-04 19:17:27 +0900

    refactor: Point Class 리펙토링

--- src/main/java/ladder/domain/Point.java
+++ src/main/java/ladder/domain/Point.java
@@ -1,5 +1,7 @@
 package ladder.domain;
 
+import java.util.Objects;
+
 public class Point {
     private final boolean point;
 
@@ -9,5 +11,20 @@ public class Point {
 
     public Point(boolean point) {
         this.point = point;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Point point1 = (Point)o;
+        return point == point1.point;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(point);
     }
 }
