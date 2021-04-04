77830dc40ae76c175930d9529b38f25c8bccf435
Donghui-Lee <ehdgml3206@naver.com>
2021-04-04 23:46:31 +0900

    refactor: Name Class Refactor

--- src/main/java/ladder/domain/Name.java
+++ src/main/java/ladder/domain/Name.java
@@ -1,10 +1,34 @@
 package ladder.domain;
 
+import java.util.Objects;
+
 public class Name {
     private static final int MAX_NAME_LENGTH = 5;
     private final String name;
 
     public Name(String name) {
+        valid(name);
         this.name = name;
+    }
+
+    private void valid(String name) {
+        if (name == null || name == "" || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("참여자의 이름은 최대 5글자 이하입니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Name name1 = (Name)o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
