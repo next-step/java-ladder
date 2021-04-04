b7b51e941f37bc791f56e8f61b79914c92d81fcf
Donghui-Lee <ehdgml3206@naver.com>
2021-04-05 00:15:30 +0900

    refactor: Name Class toString Override

--- src/main/java/ladder/domain/Name.java
+++ src/main/java/ladder/domain/Name.java
@@ -18,6 +18,11 @@ public class Name {
     }
 
     @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o)
             return true;
