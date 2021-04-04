9f46328b3e6dd4a8a8c570883d5d3bde8202a205
Jake <cocahack@naver.com>
2021-04-04 20:28:28 +0900

    test: 보상 클래스의 테스트 케이스 추가

---
+++ src/test/java/nextstep/ladder/domain/RewardTest.java
@@ -0,0 +1,33 @@
+package nextstep.ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class RewardTest {
+
+    @Test
+    @DisplayName("네 자릿 수를 초과하면 예외 처리한다.")
+    void throwExceptionIfExceedFourDigit() {
+        assertThatThrownBy(() -> new Reward("50000")).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("음수는 허용하지 않는다.")
+    void throwExceptionIfNegativeNumber() {
+        assertThatThrownBy(() -> new Reward("-90")).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("꽝 또는 정수가 아니면 예외 처리한다.")
+    void throwExceptionIfUnsupportedString() {
+        assertAll(
+            () -> assertThatCode(() -> new Reward("꽝")).doesNotThrowAnyException(),
+            () -> assertThatThrownBy(() -> new Reward("오만원")).isInstanceOf(RuntimeException.class)
+        );
+    }
+
+}
