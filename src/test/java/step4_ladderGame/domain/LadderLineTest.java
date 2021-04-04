7395219ed0d2e8395687f8536885877c518a9bc6
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-04-04 21:49:04 +0900

    feat : LadderLine 플레이어 이동 테스트 코드 추가

--- src/test/java/step4_ladderGame/domain/LadderLineTest.java
+++ src/test/java/step4_ladderGame/domain/LadderLineTest.java
@@ -1,8 +1,11 @@
 package step4_ladderGame.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LadderLineTest {
 
@@ -13,8 +16,47 @@ class LadderLineTest {
 
         LadderLine line = LadderLine.of(playerCount, RandomPointStrategy.getInstance());
 
-        Assertions.assertThat(line.size()).isEqualTo(5);
+        assertThat(line.size()).isEqualTo(5);
     }
 
+    @DisplayName("stiar의 Point의 Direction에 따라 플레이어를 오른쪽으로 이동시킨다.")
+    @Test
+    void testCase2() {
+        Player player = Player.of("LG", 0);
+        LadderLine ladderLine = createLadderLine();
 
+        Player movePlayer = ladderLine.move(player);
+
+        assertThat(movePlayer.getPosition()).isEqualTo(Position.of(1));
+    }
+
+    @DisplayName("stiar의 Point의 Direction에 따라 플레이어를 왼쪽으로 이동시킨다.")
+    @Test
+    void testCase3() {
+        Player player = Player.of("LG", 1);
+        LadderLine ladderLine = createLadderLine();
+
+        Player movePlayer = ladderLine.move(player);
+
+        assertThat(movePlayer.getPosition()).isEqualTo(Position.of(0));
+    }
+
+    @DisplayName("stiar의 Point의 Direction에 따라 플레이어를 정지 시킨다.")
+    @Test
+    void testCase4() {
+        Player player = Player.of("LG", 2);
+        LadderLine ladderLine = createLadderLine();
+
+        Player movePlayer = ladderLine.move(player);
+
+        assertThat(movePlayer.getPosition()).isEqualTo(Position.of(2));
+    }
+
+    LadderLine createLadderLine() {
+        List<Stair> stairList = List.of(Stair.of(0, Point.of(false, true)),
+                Stair.of(1, Point.of(true, false)),
+                Stair.of(2, Point.of(false, false)),
+                Stair.of(3, Point.of(false, true)));
+        return LadderLine.of(stairList);
+    }
 }
