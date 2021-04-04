699006538d2675c853cffa82c55fd94b76551815
seokrae <kslbsh@gmail.com>
2021-04-04 18:49:28 +0900

    test: 사다리 생성 테스트

--- src/test/java/nextstep/ladder/domain/LineTest.java
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LineTest {
 
@@ -31,7 +32,20 @@ class LineTest {
         Line line = Line.valueOf(2, lineGenerator);
         List<Point> points = line.points();
         // then
-        assertThat(points.get(0).isExist()).isFalse();
-        assertThat(points.get(1).isExist()).isTrue();
+        assertAll(
+                () -> assertThat(points.get(0).isExist()).isFalse(),
+                () -> assertThat(points.get(1).isExist()).isTrue()
+        );
+    }
+
+    @DisplayName("Line의 Point 확인 테스트 참여자 2인: |-----|")
+    @Test
+    void check_point_값_확인_테스트() {
+        // given
+        Line line = Line.valueOf(2, countOfPerson -> Arrays.asList(Point.init(), Point.of(true), Point.init()));
+        // when
+        Point[] expected = {Point.init(), Point.of(true), Point.init()};
+        // then
+        assertThat(line.points()).containsExactly(expected);
     }
 }
