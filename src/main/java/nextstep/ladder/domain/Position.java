27e83e059c812107e35f316e05c42068afed94af
seokrae <kslbsh@gmail.com>
2021-04-04 19:40:36 +0900

    refactor: position 이동 테스트

---
+++ src/main/java/nextstep/ladder/domain/Position.java
@@ -0,0 +1,46 @@
+package nextstep.ladder.domain;
+
+import java.util.Objects;
+
+public class Position {
+
+    public static final int MOVABLE_COUNT = 1;
+    public static final String GUIDE_ERR_POSITION_BOUND = "왼쪽으로 이동할 수 없습니다.";
+    private final int value;
+
+    private Position(final int value) {
+        this.value = value;
+    }
+
+    public static Position valueOf(final int position) {
+        return new Position(position);
+    }
+
+    public Position left() {
+        if(isValidPositionBound()) {
+            throw new IllegalArgumentException(GUIDE_ERR_POSITION_BOUND);
+        }
+        return new Position(value - MOVABLE_COUNT);
+    }
+
+    private boolean isValidPositionBound() {
+        return value < 1;
+    }
+
+    public Position right() {
+        return new Position(value + MOVABLE_COUNT);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Position)) return false;
+        final Position position1 = (Position) o;
+        return value == position1.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
