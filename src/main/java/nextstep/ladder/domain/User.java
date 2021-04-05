083c7c4f5b0be69405f9e0fb452c207a27d8eb80
seokrae <kslbsh@gmail.com>
2021-04-05 18:25:07 +0900

    refactor: User 생성자 오버로딩으로 Position 파라미터 추가 테스트

--- src/main/java/nextstep/ladder/domain/User.java
+++ src/main/java/nextstep/ladder/domain/User.java
@@ -13,33 +13,42 @@ public class User {
         this(name, Position.valueOf(0));
     }
 
-    private User(String name, Position position) {
+    private User(final String name, final int position) {
+        this(name, Position.valueOf(position));
+    }
+
+    private User(final String name, final Position position) {
+        if(name.length() > NAME_MAX_SIZE || name.length() < NAME_MIN_SIZE) {
+            throw new IllegalArgumentException("이름은 한 글자 이상 최대 5글자까지 부여할 수 있습니다.");
+        }
         this.name = name;
         this.position = position;
     }
 
     public static User valueOf(final String name) {
-        if(name.length() > NAME_MAX_SIZE || name.length() < NAME_MIN_SIZE) {
-            throw new IllegalArgumentException("이름은 한 글자 이상 최대 5글자까지 부여할 수 있습니다.");
-        }
         return new User(name);
+    }
+
+    public static User valueOf(final String name, final int position) {
+        return new User(name, position);
     }
 
     @Override
     public boolean equals(final Object o) {
         if (this == o) return true;
         if (!(o instanceof User)) return false;
-        final User user1 = (User) o;
-        return Objects.equals(name, user1.name);
+        final User user = (User) o;
+        return Objects.equals(name, user.name)
+                && Objects.equals(position, user.position);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(name, position);
     }
 
     @Override
     public String toString() {
-        return String.valueOf(name);
+        return String.format("%s, %s", name, position);
     }
 }
--- src/test/java/nextstep/ladder/domain/UserTest.java
+++ src/test/java/nextstep/ladder/domain/UserTest.java
@@ -24,4 +24,15 @@ class UserTest {
         assertThatExceptionOfType(IllegalArgumentException.class)
         .isThrownBy(() -> User.valueOf("123456"));
     }
+
+    @DisplayName("사용자 명 및 위치 정보 입력 테스트")
+    @Test
+    void create_사용자명_기본_위치정보() {
+        // given
+        User user = User.valueOf("seok", 1);
+        // when
+        User expected = User.valueOf("seok", 1);
+        // then
+        assertThat(user).isEqualTo(expected);
+    }
 }
