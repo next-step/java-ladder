50b3c9aa331845c506efd3116fb15db0c0324273
Dion <ksundong@gmail.com>
2021-04-06 08:23:08 +0900

    feat: 이름 생성 성공하도록 변경

--- src/main/java/nextstep/ladder/Name.java
+++ src/main/java/nextstep/ladder/Name.java
@@ -1,8 +1,29 @@
 package nextstep.ladder;
 
+import java.util.Objects;
+
 public final class Name {
 
-  public Name(final String nameSource) {
+  private final String name;
 
+  public Name(final String name) {
+    this.name = name;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Name)) {
+      return false;
+    }
+    Name name1 = (Name) o;
+    return Objects.equals(name, name1.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
   }
 }
