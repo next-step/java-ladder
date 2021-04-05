9a3456678d918cc711c553760d4f26337c821afc
seokrae <kslbsh@gmail.com>
2021-04-06 03:47:16 +0900

    refactor: inputView nextInt -> nextLine 수정

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class InputView {
 
     public static final String GUIDE_LADDER_INPUT_PARTICIPANTS = "참여할 사람 이름을 입력하세요. (이름은 쉼표( , )로 구분하세요.";
-    public static final String GUIDE_LADDER_INPUT_LADDER_SIZE = "최대 사다리 높이는 몇 개인가요?";
-    public static final String GUIDE_LADDER_INPUT_RESULT = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
-    public static final String GUIDE_LADDER_INPUT_USER = "결과를 보고 싶은 사람은?";
+    public static final String GUIDE_LADDER_INPUT_LADDER_SIZE = "\n최대 사다리 높이는 몇 개인가요?";
+    public static final String GUIDE_LADDER_INPUT_RESULT = "\n실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    public static final String GUIDE_LADDER_INPUT_USER = "\n결과를 보고 싶은 사람은?";
     private final Scanner in = new Scanner(System.in);
 
     public String inputParticipants() {
@@ -20,9 +20,9 @@ public class InputView {
         return in.nextLine();
     }
 
-    public int inputLadderHeight() {
+    public String inputLadderHeight() {
         System.out.println(GUIDE_LADDER_INPUT_LADDER_SIZE);
-        return in.nextInt();
+        return in.nextLine();
     }
 
     public String inputUserResult() {
