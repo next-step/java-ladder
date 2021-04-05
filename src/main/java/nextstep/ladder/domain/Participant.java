1b467c5c7be5969cd2b4e21e727eade3122aea65
hwangyunho <hwang9yh@gmail.com>
2021-04-05 13:27:37 +0900

    feat: LadderException 추가

--- src/main/java/nextstep/ladder/domain/Participant.java
+++ src/main/java/nextstep/ladder/domain/Participant.java
@@ -1,14 +1,30 @@
 package nextstep.ladder.domain;
 
+import nextstep.ladder.exception.LadderException;
+
 public class Participant {
+
+    private static final String NAME_EMPTY_EXCEPTION = "이름으로 빈값이 올 수 없습니다.";
+    private static final String NAME_LENGTH_MAX_FIVE = "이름의 길이는 5글자까지 허용됩니다.";
 
     private String name;
 
     private Participant(final String name) {
+        validation(name);
         this.name = name;
     }
 
     public static Participant of(final String name) {
         return new Participant(name);
+    }
+
+    private void validation(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new LadderException(NAME_EMPTY_EXCEPTION);
+        }
+
+        if (name.length() > 5) {
+            throw new LadderException(NAME_LENGTH_MAX_FIVE);
+        }
     }
 }
