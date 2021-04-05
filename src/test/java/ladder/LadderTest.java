5521c1e1670ef5706f235178c2cbc38789d9709c
Sooncheol <ryxhdrnrmf@gmail.com>
2021-04-05 21:12:43 +0900

    test: LadderTest 작성

--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -1,15 +1,16 @@
 package ladder;
 
+import ladder.domain.Ladder;
 import ladder.domain.Line;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class LineTest {
+public class LadderTest {
     @Test
-    public void Line_참여자수_Test(){
-        assertThatThrownBy(() -> new Line(1))
+    public void Ladder_높이_Test(){
+        assertThatThrownBy(() -> new Ladder(3,0))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("참여자는 2명 이상이어야 합니다.");
+                .hasMessage("사다리 높이는 1이상이어야 합니다.");
     }
 }
