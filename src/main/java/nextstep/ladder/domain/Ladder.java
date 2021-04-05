f5cb62c003f341049a577931c517f442dcb69f77
seokrae <kslbsh@gmail.com>
2021-04-06 01:21:16 +0900

    refactor: LineGenerator도 테스트할 수 있도록 생성자 추가 및 테스트

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -1,27 +1,41 @@
 package nextstep.ladder.domain;
 
+import nextstep.ladder.generator.LineGenerator;
+
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Ladder {
 
     private final List<Line> lines;
 
-    private Ladder(Participants participants, Height height) {
-        this.lines = createLadder(participants, height);
+    private Ladder(Line... lines) {
+        this.lines = Arrays.asList(lines);
     }
 
     public static Ladder valueOf(Participants participants, Height height) {
-        return new Ladder(participants, height);
+        Line[] ladder = createLadder(participants, height);
+        return new Ladder(ladder);
     }
 
-    private List<Line> createLadder(Participants participants, Height height) {
+    private static Line[] createLadder(Participants participants, Height height) {
         return Stream.generate(() -> Line.valueOf(participants))
                 .limit(height.size())
-                .collect(Collectors.toList());
+                .toArray(Line[]::new);
+    }
+
+    public static Ladder valueOf(Participants participants, Height height, LineGenerator generator) {
+        Line[] ladder = createLadder(participants, height, generator);
+        return new Ladder(ladder);
+    }
+
+    private static Line[] createLadder(Participants participants, Height height, LineGenerator generator) {
+        return Stream.generate(() -> Line.valueOf(participants, generator))
+                .limit(height.size())
+                .toArray(Line[]::new);
     }
 
     public List<Line> lines() {
--- src/test/java/nextstep/ladder/service/LadderTest.java
+++ src/test/java/nextstep/ladder/service/LadderTest.java
@@ -1,15 +1,14 @@
 package nextstep.ladder.service;
 
-import nextstep.ladder.domain.Height;
-import nextstep.ladder.domain.Ladder;
-import nextstep.ladder.domain.Line;
-import nextstep.ladder.domain.Participants;
+import nextstep.ladder.domain.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LadderTest {
 
@@ -24,5 +23,26 @@ class LadderTest {
         int expected = Height.valueOf(2).size();
         // then
         assertThat(ladder.size()).isEqualTo(expected);
+    }
+
+    @DisplayName("Ladder 생성 시 line 설정까지 추가")
+    @Test
+    void create_전략_생성_테스트() {
+        // given
+        Participants participants = Participants.valueOf("user1", "user2");
+        Height height = Height.valueOf(2);
+        // when
+        Ladder ladder = Ladder.valueOf(
+                participants,
+                height,
+                countOfPerson -> Arrays.asList(Point.init(), Point.valueOf(true), Point.valueOf(false)));
+        Line line = ladder.lines().get(0);
+        List<Point> points = line.points();
+        // then
+        assertAll(
+                () -> assertThat(points.get(0)).isEqualTo(Point.init()),
+                () -> assertThat(points.get(1)).isEqualTo(Point.valueOf(true)),
+                () -> assertThat(points.get(2)).isEqualTo(Point.valueOf(false))
+        );
     }
 }
