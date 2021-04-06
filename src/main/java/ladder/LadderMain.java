5a8d6c808b4f27e69c1a60c7fa17518a90ebd9c1
Younghyun <younghyunjo99@gmail.com>
2021-04-06 22:22:16 +0900

    style(step2):불필요한 코드 삭제

--- src/main/java/ladder/LadderMain.java
+++ src/main/java/ladder/LadderMain.java
@@ -1,10 +1,5 @@
 package ladder;
 
-import ladder.domain.*;
-import ladder.view.*;
-
-import java.util.List;
-
 public class LadderMain {
     public static void main(String[] args) {
         LadderControl ladderControl = new LadderControl();
