38145e8c8931270c7222455f31b18fdc74361702
623nana <6230nana@gmail.com>
2021-04-06 15:03:33 +0900

    refactor: Store 클래스 패키지 분리

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -3,6 +3,7 @@ package ladder.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import ladder.domain.store.NameStore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
--- src/test/java/ladder/domain/PlayersTest.java
+++ src/test/java/ladder/domain/PlayersTest.java
@@ -2,6 +2,7 @@ package ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import ladder.domain.store.NameStore;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
