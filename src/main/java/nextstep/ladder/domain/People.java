13f675b8bd364f4931f8bcd57c84511dbba69614
Dion <ksundong@gmail.com>
2021-04-07 00:48:09 +0900

    feat: People 생성 기능 구현

--- src/main/java/nextstep/ladder/domain/People.java
+++ src/main/java/nextstep/ladder/domain/People.java
@@ -1,9 +1,30 @@
 package nextstep.ladder.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public final class People {
 
+  private final List<Person> people;
+
   public People(final List<Person> people) {
+    this.people = people;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof People)) {
+      return false;
+    }
+    People people1 = (People) o;
+    return Objects.equals(people, people1.people);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(people);
   }
 }
