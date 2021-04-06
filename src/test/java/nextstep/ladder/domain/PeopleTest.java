7da1d104b8e06f33bead55b4772fa436c69e1b3c
Dion <ksundong@gmail.com>
2021-04-07 01:27:59 +0900

    test: 이름이 긴 항목이 있는 경우 People 생성에 실패하는 테스트 작성

--- src/test/java/nextstep/ladder/domain/PeopleTest.java
+++ src/test/java/nextstep/ladder/domain/PeopleTest.java
@@ -1,8 +1,10 @@
 package nextstep.ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import nextstep.ladder.exception.OverNameLengthLimitException;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -38,5 +40,18 @@ class PeopleTest {
 
     //then
     assertThat(people).isEqualTo(new People(names));
+  }
+
+  @Test
+  @DisplayName("이름의 길이가 긴 항목이 있는경우 생성에 실패한다.")
+  void createFailWhenOverNameLengthLimit() {
+    //given
+    final String[] names = new String[]{"pobi", "crong", "honux", "jk", "0123456"};
+
+    //when
+    //then
+    assertThatThrownBy(() -> new People(names))
+        .isInstanceOf(OverNameLengthLimitException.class)
+        .hasMessage(OverNameLengthLimitException.OVER_NAME_LENGTH_LIMIT);
   }
 }
