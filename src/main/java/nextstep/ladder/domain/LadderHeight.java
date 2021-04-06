e0711a5830cd45e4b517c2487cc11ef14f9b1ccf
Dion <ksundong@gmail.com>
2021-04-07 00:05:55 +0900

    feat: 사다리 생성 테스트가 통과하도록 기능 구현

--- src/main/java/nextstep/ladder/domain/LadderHeight.java
+++ src/main/java/nextstep/ladder/domain/LadderHeight.java
@@ -1,10 +1,29 @@
 package nextstep.ladder.domain;
 
+import java.util.Objects;
+
 public final class LadderHeight {
 
   private final int ladderHeight;
 
   public LadderHeight(int ladderHeight) {
     this.ladderHeight = ladderHeight;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof LadderHeight)) {
+      return false;
+    }
+    LadderHeight that = (LadderHeight) o;
+    return ladderHeight == that.ladderHeight;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(ladderHeight);
   }
 }
