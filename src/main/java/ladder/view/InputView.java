45fc9ad888ef37dcd3eb5aa0f0c5c89cf1650b3d
623nana <6230nana@gmail.com>
2021-04-06 14:37:24 +0900

    feat: 실행 결과와 결과를 조회할 사람을 입력 받는 기능

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -13,4 +13,14 @@ public class InputView {
     System.out.println(System.lineSeparator() + "최대 사다리 높이는 몇 개인가요?");
     return new Scanner(System.in).nextInt();
   }
+
+  public static String inputResult() {
+    System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+    return new Scanner(System.in).nextLine();
+  }
+
+  public static String inputResultOfPerson() {
+    System.out.println("결과를 보고 싶은 사람은?");
+    return new Scanner(System.in).nextLine();
+  }
 }
