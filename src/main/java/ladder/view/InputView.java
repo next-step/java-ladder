2ae405591b589bda0e8136d2ad4e7e2e22173281
623nana <6230nana@gmail.com>
2021-04-07 20:54:11 +0900

    refactor: 출력에 줄바꿈

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -15,12 +15,12 @@ public class InputView {
   }
 
   public static String inputResult() {
-    System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+    System.out.println(System.lineSeparator() + "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
     return new Scanner(System.in).nextLine();
   }
 
   public static String inputResultOfPerson() {
-    System.out.println("결과를 보고 싶은 사람은?");
+    System.out.println(System.lineSeparator() + "결과를 보고 싶은 사람은?");
     return new Scanner(System.in).nextLine();
   }
 }
