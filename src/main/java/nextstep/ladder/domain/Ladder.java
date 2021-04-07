4b4315ace554a20453771045bc825f1eebb9cee9
Dion <ksundong@gmail.com>
2021-04-08 08:20:25 +0900

    feat: 사다리를 생성하는 기능 구현
    lines를 생성하는 전략이 조금 어려운 듯 싶다.

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -1,7 +1,40 @@
 package nextstep.ladder.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public final class Ladder {
 
+  private final LadderHeight ladderHeight;
+  private final People people;
+  private final List<Line> lines;
+
   public Ladder(final int height, final String[] names) {
+    this(new LadderHeight(height), new People(names), new ArrayList<>());
+  }
+
+  public Ladder(LadderHeight ladderHeight, People people, List<Line> lines) {
+    this.ladderHeight = ladderHeight;
+    this.people = people;
+    this.lines = lines;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Ladder)) {
+      return false;
+    }
+    Ladder ladder = (Ladder) o;
+    return Objects.equals(ladderHeight, ladder.ladderHeight) && Objects.equals(people, ladder.people)
+        && Objects.equals(lines, ladder.lines);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(ladderHeight, people, lines);
   }
 }
---
+++ src/main/java/nextstep/ladder/domain/RandomLineCreationStrategy.java
@@ -0,0 +1,11 @@
+package nextstep.ladder.domain;
+
+import java.util.List;
+
+public final class RandomLineCreationStrategy implements LineCreationStrategy {
+
+  @Override
+  public List<Point> create() {
+    return null;
+  }
+}
