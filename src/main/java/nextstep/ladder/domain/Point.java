04800335f60df05be73387e5f61ee8b1062f9555
Dion <ksundong@gmail.com>
2021-04-07 21:33:59 +0900

    feat: point 생성 기능 추가

--- src/main/java/nextstep/ladder/domain/Point.java
+++ src/main/java/nextstep/ladder/domain/Point.java
@@ -1,8 +1,29 @@
 package nextstep.ladder.domain;
 
+import java.util.Objects;
+
 public final class Point {
 
-  public Point(boolean pointSource) {
+  private final boolean point;
 
+  public Point(boolean point) {
+    this.point = point;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Point)) {
+      return false;
+    }
+    Point point1 = (Point) o;
+    return point == point1.point;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(point);
   }
 }
