d1c26bdf20143ab5994fcb8a87f757292fd1942f
Jake <cocahack@naver.com>
2021-04-07 20:47:37 +0900

    test: 참가자가 사다리를 타는 행동의 테스트

--- src/test/java/nextstep/ladder/domain/player/PlayerTest.java
+++ src/test/java/nextstep/ladder/domain/player/PlayerTest.java
@@ -1,8 +1,11 @@
 package nextstep.ladder.domain.player;
 
+import nextstep.ladder.domain.ladder.FakeLadder;
+import nextstep.ladder.domain.ladder.Ladder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayerTest {
@@ -10,7 +13,16 @@ class PlayerTest {
     @Test
     @DisplayName("다섯 글자보다 긴 이름은 예외 처리한다.")
     void throwExceptionIfLengthOfNameExceedFive() {
-        assertThatThrownBy(() -> new Player("banana")).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> new Player("banana", 0)).isInstanceOf(RuntimeException.class);
     }
 
+    @Test
+    @DisplayName("참가자가 사다리를 탄다.")
+    void passThroughLadder() {
+        Ladder ladder = FakeLadder.ladder();
+
+        Player player = new Player("tdd", 0);
+
+        assertThat(player.passThrough(ladder)).isZero();
+    }
 }
