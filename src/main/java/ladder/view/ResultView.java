b64fe5033c692db483a59ad73b2f5e5c8961a930
623nana <6230nana@gmail.com>
2021-04-07 20:53:37 +0900

    feat: 참가자의 결과를 출력하는 기능

--- src/main/java/ladder/view/ResultView.java
+++ src/main/java/ladder/view/ResultView.java
@@ -3,23 +3,42 @@ package ladder.view;
 import ladder.domain.Ladder;
 import ladder.domain.Players;
 import ladder.domain.Point;
+import ladder.domain.Results;
 
 public class ResultView {
 
   public static void showPlayers(Players players) {
-    System.out.println(System.lineSeparator() + "실행 결과" + System.lineSeparator());
+    System.out.println(System.lineSeparator() + "사다리 결과" + System.lineSeparator());
     players.names().stream()
         .map(name -> String.format("%6s", name.getName()))
         .forEach(System.out::print);
     System.out.println();
   }
 
-  public static void showLadder(Ladder ladder) {
+  public static void showLadder(Ladder ladder, Results results) {
     ladder.lines().forEach(line -> {
       line.points().stream()
           .map(Point::pedal)
           .forEach(System.out::print);
       System.out.println();
     });
+
+    results.result().stream()
+        .map(result -> String.format("%6s", result.getResult()))
+        .forEach(System.out::print);
+    System.out.println();
+  }
+
+  public static void showResultOfPerson(Results results, String name) {
+    System.out.println(System.lineSeparator() + "실행 결과");
+    System.out.println(results.findOne(name));
+  }
+
+  public static void showResultAll(Results results) {
+    System.out.println(System.lineSeparator() + "실행 결과");
+    results.gameResult().entrySet()
+        .stream()
+        .map(e -> String.format("%s : %s", e.getKey().getName(), e.getValue().getResult()))
+        .forEach(System.out::println);
   }
 }
