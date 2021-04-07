5dc2ebaeba5e703195833ed6e7d2df58a6555ba7
seokrae <kslbsh@gmail.com>
2021-04-07 23:24:09 +0900

    refactor: 피드백: [인터페이스를 좀 더 위쪽 Layer로 이동](https://github.com/next-step/java-ladder/pull/859#discussion_r607845484), 근데 아이디어가 잘 안떠오름..

--- src/main/java/nextstep/ladder/controller/LadderGameController.java
+++ src/main/java/nextstep/ladder/controller/LadderGameController.java
@@ -1,13 +1,12 @@
 package nextstep.ladder.controller;
 
-import nextstep.ladder.service.LadderService;
+import nextstep.ladder.generator.DefaultLineGenerator;
+import nextstep.ladder.generator.LineGenerator;
+import nextstep.ladder.service.LadderFactory;
 import nextstep.ladder.domain.*;
 import nextstep.ladder.view.InputView;
 import nextstep.ladder.view.ResultView;
-import nextstep.ladder.wrapper.Ladder;
-import nextstep.ladder.wrapper.LadderResult;
-import nextstep.ladder.wrapper.LadderRewards;
-import nextstep.ladder.wrapper.Participants;
+import nextstep.ladder.wrapper.*;
 
 import static nextstep.ladder.controller.LadderParameterHelper.parseArgumentResolver;
 
@@ -16,25 +15,27 @@ public class LadderGameController {
     public static final String GUIDE_LADDER_END_SIGNATURE = "all";
     private final LadderParameterProcessor processor;
     private final ResultView resultView;
+    private final LineGenerator generator;
 
     public LadderGameController(InputView inputView, ResultView resultView) {
-        this(new LadderParameterProcessor(inputView), resultView);
+        this(new LadderParameterProcessor(inputView), resultView, new DefaultLineGenerator());
     }
 
-    public LadderGameController(LadderParameterProcessor processor, ResultView resultView) {
+    public LadderGameController(LadderParameterProcessor processor, ResultView resultView, LineGenerator generator) {
         this.processor = processor;
         this.resultView = resultView;
+        this.generator = generator;
     }
 
     public void start() {
-
         Participants participants = parseArgumentResolver(processor::processUsers);
         LadderRewards rewards =
                 parseArgumentResolver(() -> processor.processLadderRewards(participants.size()));
         Height height = parseArgumentResolver(processor::processHeight);
-        Ladder ladder = Ladder.valueOf(participants, height);
 
-        LadderResult rewardResult = LadderService.rideLadder(participants, ladder, rewards);
+        Ladder ladder = LadderFactory.valueOf(participants, height, generator);
+        LadderResult rewardResult = LadderFactory.rideLadder(participants, ladder, rewards);
+
         resultView.printResult(ladder, participants, rewards);
 
         String user;
--- src/main/java/nextstep/ladder/service/LadderFactory.java
+++ src/main/java/nextstep/ladder/service/LadderFactory.java
@@ -1,23 +1,32 @@
 package nextstep.ladder.service;
 
+import nextstep.ladder.domain.Height;
 import nextstep.ladder.domain.Position;
 import nextstep.ladder.domain.Reward;
 import nextstep.ladder.domain.User;
-import nextstep.ladder.wrapper.Ladder;
-import nextstep.ladder.wrapper.LadderResult;
-import nextstep.ladder.wrapper.LadderRewards;
-import nextstep.ladder.wrapper.Participants;
+import nextstep.ladder.generator.LineGenerator;
+import nextstep.ladder.wrapper.*;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Stream;
 
-public final class LadderService {
+public final class LadderFactory {
 
-    private LadderService() {}
+    private LadderFactory() { }
+
+    public static Ladder valueOf(
+            final Participants participants, final Height height, final LineGenerator generator) {
+        Line[] lines = Stream.generate(() -> generator.createLine(participants.size()))
+                .limit(height.size())
+                .toArray(Line[]::new);
+        return Ladder.valueOf(lines);
+    }
 
     public static LadderResult rideLadder(
             final Participants participants, final Ladder ladder, final LadderRewards ladderRewards) {
+
         Map<User, Reward> result = new LinkedHashMap<>();
         Set<User> users = participants.getUsers();
 
