6733fc84d86bfa852567b84b9e842c110d8a0164
cptbong <cptbbong@gmail.com>
2021-04-07 18:09:45 +0900

    feat - 클라이언트코드추가.

---
+++ src/main/java/nextstep/Client.java
@@ -0,0 +1,7 @@
+package nextstep;
+
+public class Client {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/nextstep/view/InputView.java
@@ -0,0 +1,21 @@
+package nextstep.view;
+
+import java.util.Scanner;
+
+public class InputView implements ConsoleView {
+    private Scanner scanner;
+
+    public InputView() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    @Override
+    public void print(String str) {
+        System.out.println(str);
+    }
+
+    public String inputQuestion(String q) {
+        this.print(q);
+        return this.scanner.nextLine();
+    }
+}
