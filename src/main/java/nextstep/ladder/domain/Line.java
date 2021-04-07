cf596935b1af7d82e96097cb602a2ba2cc5d9cb5
Dion <ksundong@gmail.com>
2021-04-07 22:14:03 +0900

    feat: Line 생성 기능 추가

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -1,7 +1,35 @@
 package nextstep.ladder.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public final class Line {
 
+  private final List<Point> points;
+
   public Line(final int countOfPerson) {
+    final List<Point> points = new ArrayList<>(countOfPerson);
+    for (int i = 0; i < countOfPerson; i++) {
+      points.add(new Point(false));
+    }
+    this.points = points;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Line)) {
+      return false;
+    }
+    Line line = (Line) o;
+    return Objects.equals(points, line.points);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(points);
   }
 }
