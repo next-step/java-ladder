f8b796469b5515dbf71d014b800634324aa32013
seokrae <kslbsh@gmail.com>
2021-04-07 23:38:05 +0900

    refactor: 생성자 및 팩토리 메서드 수정으로 코드 간결화

--- src/main/java/nextstep/ladder/wrapper/Ladder.java
+++ src/main/java/nextstep/ladder/wrapper/Ladder.java
@@ -1,14 +1,11 @@
 package nextstep.ladder.wrapper;
 
-import nextstep.ladder.domain.Height;
 import nextstep.ladder.domain.Position;
-import nextstep.ladder.generator.LineGenerator;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Stream;
 
 public class Ladder {
 
@@ -18,26 +15,8 @@ public class Ladder {
         this.lines = Arrays.asList(lines);
     }
 
-    public static Ladder valueOf(final Participants participants, final Height height) {
-        Line[] ladder = createLadder(participants, height);
-        return new Ladder(ladder);
-    }
-
-    private static Line[] createLadder(final Participants participants, final Height height) {
-        return Stream.generate(() -> Line.valueOf(participants))
-                .limit(height.size())
-                .toArray(Line[]::new);
-    }
-
-    public static Ladder valueOf(final Participants participants, final Height height, final LineGenerator generator) {
-        Line[] ladder = createLadder(participants, height, generator);
-        return new Ladder(ladder);
-    }
-
-    private static Line[] createLadder(final Participants participants, final Height height, final LineGenerator generator) {
-        return Stream.generate(() -> Line.valueOf(participants, generator))
-                .limit(height.size())
-                .toArray(Line[]::new);
+    public static Ladder valueOf(final Line... lines) {
+        return new Ladder(lines);
     }
 
     public List<Line> lines() {
--- src/test/java/nextstep/ladder/service/LadderTest.java
+++ src/test/java/nextstep/ladder/service/LadderTest.java
@@ -1,28 +1,40 @@
 package nextstep.ladder.service;
 
-import nextstep.ladder.domain.*;
+import nextstep.ladder.domain.Bar;
+import nextstep.ladder.domain.Height;
+import nextstep.ladder.domain.Position;
 import nextstep.ladder.wrapper.Ladder;
 import nextstep.ladder.wrapper.Line;
-import nextstep.ladder.wrapper.Participants;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LadderTest {
 
+    private static Stream<Arguments> ladderRideEntry() {
+        return Stream.of(
+                Arguments.of(Position.valueOf(0), Position.valueOf(1)),
+                Arguments.of(Position.valueOf(1), Position.valueOf(0)),
+                Arguments.of(Position.valueOf(2), Position.valueOf(2))
+        );
+    }
+
     @DisplayName("Ladder 생성 시 Height 일치 테스트")
     @Test
     void create_생성_테스트() {
         // given
-        Participants participants = Participants.valueOf("12345", "1234");
-        Height height = Height.valueOf(2);
+        Line firstLine = Line.valueOf(Bar.init(), Bar.valueOf(false), Bar.valueOf(true));
+        Line secondLine = Line.valueOf(Bar.init(), Bar.valueOf(true), Bar.valueOf(false));
         // when
-        List<Line> ladder = Ladder.valueOf(participants, height).lines();
+        List<Line> ladder = Ladder.valueOf(firstLine, secondLine).lines();
         int expected = Height.valueOf(2).size();
         // then
         assertThat(ladder.size()).isEqualTo(expected);
@@ -32,20 +44,34 @@ class LadderTest {
     @Test
     void create_전략_생성_테스트() {
         // given
-        Participants participants = Participants.valueOf("user1", "user2");
-        Height height = Height.valueOf(2);
+        Ladder ladder = Ladder.valueOf(Line.valueOf(Bar.init(), Bar.valueOf(true), Bar.valueOf(false)));
         // when
-        Ladder ladder = Ladder.valueOf(
-                participants,
-                height,
-                countOfPerson -> Arrays.asList(Bar.init(), Bar.valueOf(true), Bar.valueOf(false)));
         Line line = ladder.lines().get(0);
-        List<Bar> bars = line.points();
+        List<Bar> bars = line.points().bars();
         // then
         assertAll(
                 () -> assertThat(bars.get(0)).isEqualTo(Bar.init()),
                 () -> assertThat(bars.get(1)).isEqualTo(Bar.valueOf(true)),
                 () -> assertThat(bars.get(2)).isEqualTo(Bar.valueOf(false))
         );
+    }
+
+    @DisplayName("사다리 타기 테스트")
+    @ParameterizedTest(name = "사다리 타기 테스트 :: 시작 위치: {0}, 도착 위치: {1} ")
+    @MethodSource(value = "ladderRideEntry")
+    void findPosition_사다리타기테스트(Position source, Position dest) {
+        /**
+         * false |-----|     |
+         * false |     |-----|
+         * false |     |-----|
+         */
+        Line line1 = Line.valueOf(Bar.init(), Bar.valueOf(true), Bar.valueOf(false));
+        Line line2 = Line.valueOf(Bar.init(), Bar.valueOf(false), Bar.valueOf(true));
+        Line line3 = Line.valueOf(Bar.init(), Bar.valueOf(false), Bar.valueOf(true));
+
+        Ladder ladder = Ladder.valueOf(line1, line2, line3);
+        Position endPosition = ladder.findEndPosition(source);
+
+        assertThat(endPosition).isEqualTo(dest);
     }
 }
