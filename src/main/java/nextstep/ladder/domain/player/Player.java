52abbc8deec01b8b31deb5e5bf0f8994a5f50202
Jake <cocahack@naver.com>
2021-04-07 20:47:58 +0900

    feat: 참가자가 사다리를 타는 메소드 추가

--- src/main/java/nextstep/ladder/domain/player/Player.java
+++ src/main/java/nextstep/ladder/domain/player/Player.java
@@ -1,13 +1,16 @@
 package nextstep.ladder.domain.player;
 
+import nextstep.ladder.domain.ladder.Ladder;
 import nextstep.ladder.dto.PlayerDto;
 
 public class Player {
 
     private final Name name;
+    private final int position;
 
-    public Player(String name) {
+    public Player(String name, int position) {
         this.name = new Name(name);
+        this.position = position;
     }
 
     public boolean hasName(Name targetName) {
@@ -17,4 +20,9 @@ public class Player {
     public PlayerDto export() {
         return new PlayerDto(name.export());
     }
+
+    public int passThrough(Ladder ladder) {
+        return ladder.passThrough(position);
+    }
+
 }
