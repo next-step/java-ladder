5ebb33d63ec1e29b1fb040a720e30810fe76ef73
Dion <ksundong@gmail.com>
2021-04-07 22:54:39 +0900

    test: 사람의 수가 적은 경우 생성실패하는 실패하는 테스트 작성

--- src/test/java/nextstep/ladder/domain/LineTest.java
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -1,27 +1,43 @@
 package nextstep.ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import nextstep.ladder.exception.PersonCountTooLowException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LineTest {
+
+  LineCreationStrategy lineCreationStrategy;
+
+  @BeforeEach
+  void setUp() {
+    lineCreationStrategy = () -> Stream.of(new Point(true), new Point(false))
+        .collect(Collectors.toList());
+  }
 
   @Test
   @DisplayName("사람의 수를 입력받아서 Line을 생성한다.")
   void create() {
     //given
     final int countOfPerson = 2;
-    final LineCreationStrategy lineCreationStrategy =
-        () -> Stream.of(new Point(true), new Point(false))
-            .collect(Collectors.toList());
 
     //when
     final Line line = new Line(countOfPerson, lineCreationStrategy);
 
     //then
     assertThat(line).isEqualTo(new Line(countOfPerson, lineCreationStrategy));
+  }
+
+  @Test
+  @DisplayName("사람의 수는 최소 2명이어야 한다.")
+  void createFailWhenUnderTwoPerson() {
+    assertThatThrownBy(() -> new Line(0, lineCreationStrategy))
+        .isInstanceOf(PersonCountTooLowException.class)
+        .hasMessage(PersonCountTooLowException.PERSON_COUNT_TOO_LOW);
   }
 }
