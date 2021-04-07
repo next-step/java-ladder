93692f82391eed8f1e34b55552c7e8222bc92a72
10M <tenmillionwon@gmail.com>
2021-04-07 21:59:08 +0900

    feat: add InputResult class

---
+++ src/main/java/ladder/domain/InputResult.java
@@ -0,0 +1,26 @@
+package ladder.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InputResult {
+	List<String> inputResults;
+
+	public InputResult(String inputResults, int playerSize) {
+		validateInputResult(inputResults, playerSize);
+		this.inputResults = Arrays.stream(inputResults.split(","))
+			.map(inputResult -> inputResult.trim())
+			.collect(Collectors.toList());
+	}
+
+	public List<String> getInputResults() {
+		return inputResults;
+	}
+
+	private void validateInputResult(String inputResults, int playerSize) {
+		if (inputResults.split(",").length != playerSize) {
+			throw new IllegalArgumentException("결과값의 갯수와 플레이어 수가 일치해야 합니다.");
+		}
+	}
+}
