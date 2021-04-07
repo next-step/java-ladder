299e61645a7b5b7169b40d415cfe769d35b60464
seokrae <kslbsh@gmail.com>
2021-04-07 17:55:10 +0900

    refactor: 피드백: [Ladder 서비스에서 `사다리 타기`를 통한 `결과값` 도출해내는 삐즈니스 로직 처리](https://github.com/next-step/java-ladder/pull/859#discussion_r607842860)

--- src/main/java/nextstep/ladder/service/LadderService.java
+++ src/main/java/nextstep/ladder/service/LadderService.java
@@ -1,17 +1,32 @@
 package nextstep.ladder.service;
 
+import nextstep.ladder.domain.Position;
+import nextstep.ladder.domain.Reward;
+import nextstep.ladder.domain.User;
 import nextstep.ladder.wrapper.Ladder;
 import nextstep.ladder.wrapper.LadderResult;
 import nextstep.ladder.wrapper.LadderRewards;
 import nextstep.ladder.wrapper.Participants;
 
-public class LadderService {
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 
-    private LadderService() {
-    }
+public final class LadderService {
+
+    private LadderService() {}
 
     public static LadderResult rideLadder(
             final Participants participants, final Ladder ladder, final LadderRewards ladderRewards) {
-        return participants.findReward(ladder, ladderRewards);
+        Map<User, Reward> result = new LinkedHashMap<>();
+        Set<User> users = participants.getUsers();
+
+        for (User user : users) {
+            Position rewardPosition = ladder.findEndPosition(user.position());
+            Reward reward = ladderRewards.findReward(rewardPosition);
+            result.put(user, reward);
+        }
+
+        return LadderResult.valueOf(result);
     }
 }
