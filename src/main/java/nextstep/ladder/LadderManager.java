cd2001a611a72ceeea28c8403af225caea7eefac
Dion <ksundong@gmail.com>
2021-04-08 23:57:01 +0900

    feat: 빈 줄을 추가하는 기능 구현

--- src/main/java/nextstep/ladder/LadderManager.java
+++ src/main/java/nextstep/ladder/LadderManager.java
@@ -4,12 +4,16 @@ import nextstep.ladder.domain.LadderHeight;
 import nextstep.ladder.domain.People;
 import nextstep.ladder.domain.PersonCount;
 import nextstep.ladder.io.InputView;
+import nextstep.ladder.io.ResultView;
 
 public final class LadderManager {
 
   public static void main(String[] args) {
     People people = new People(InputView.inputNames());
     PersonCount personCount = people.personCount();
+    ResultView.printNewLine();
+
     LadderHeight ladderHeight = new LadderHeight(InputView.inputLadderHeight());
+    ResultView.printNewLine();
   }
 }
--- src/main/java/nextstep/ladder/io/ResultView.java
+++ src/main/java/nextstep/ladder/io/ResultView.java
@@ -3,4 +3,8 @@ package nextstep.ladder.io;
 public final class ResultView {
 
   private ResultView() {}
+
+  public static void printNewLine() {
+    System.out.println();
+  }
 }
