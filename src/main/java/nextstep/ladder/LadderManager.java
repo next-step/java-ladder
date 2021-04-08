7588006587de5d164ce467eeb581abde36ab91fe
Dion <ksundong@gmail.com>
2021-04-09 01:00:16 +0900

    feat: 입력 예외처리 기능 구현

--- src/main/java/nextstep/ladder/LadderManager.java
+++ src/main/java/nextstep/ladder/LadderManager.java
@@ -3,19 +3,38 @@ package nextstep.ladder;
 import nextstep.ladder.domain.Ladder;
 import nextstep.ladder.domain.LadderHeight;
 import nextstep.ladder.domain.People;
+import nextstep.ladder.exception.LadderException;
 import nextstep.ladder.io.InputView;
 import nextstep.ladder.io.ResultView;
 
 public final class LadderManager {
 
   public static void main(String[] args) {
-    People people = new People(InputView.inputNames());
+    People people = getPeople();
     ResultView.printNewLine();
 
-    LadderHeight ladderHeight = new LadderHeight(InputView.inputLadderHeight());
+    LadderHeight ladderHeight = getLadderHeight();
     ResultView.printNewLine();
 
     final Ladder ladder = new Ladder(ladderHeight, people);
     ResultView.printLadder(ladder);
+  }
+
+  private static People getPeople() {
+    try {
+      return new People(InputView.inputNames());
+    } catch (LadderException e) {
+      System.err.println(e.getMessage());
+      return getPeople();
+    }
+  }
+
+  private static LadderHeight getLadderHeight() {
+    try {
+      return new LadderHeight(InputView.inputLadderHeight());
+    } catch (LadderException e) {
+      System.err.println(e.getMessage());
+      return getLadderHeight();
+    }
   }
 }
