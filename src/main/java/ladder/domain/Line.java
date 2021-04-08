bb2f79457f577d6c4a32ecbbfa9cb24c0542dee9
Sooncheol <ryxhdrnrmf@gmail.com>
2021-04-08 09:23:46 +0900

    fix: 참가자 수 예외처리 조기 검증

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -4,21 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Line {
-    private static final int MIN_WIDTH = 2;
-    private static final String MESSAGE_MIN_NUMBER_OF_PLAYERS = "참여자는 2명 이상이어야 합니다.";
     private final int numberOfPlayers;
     private final Points points = new Points();
 
     public Line(int numberOfPlayers) {
-        validate(numberOfPlayers);
         this.numberOfPlayers = numberOfPlayers;
         generateLine();
-    }
-
-    private void validate(int numberOfPlayers) {
-        if (numberOfPlayers < MIN_WIDTH) {
-            throw new IllegalArgumentException(MESSAGE_MIN_NUMBER_OF_PLAYERS);
-        }
     }
 
     private void generateLine(){
--- src/main/java/ladder/domain/Players.java
+++ src/main/java/ladder/domain/Players.java
@@ -6,12 +6,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Players {
+    private static final int MIN_NUMBER_OF_PLAYERS = 2;
+    private static final String MESSAGE_MIN_NUMBER_OF_PLAYERS = "참여자는 2명 이상이어야 합니다.";
     private List<Player> players;
 
     public Players(String[] players){
+        validate(players);
         this.players = Arrays.stream(players)
                             .map(Player::new)
                             .collect(Collectors.toList());
+    }
+
+    private void validate(String[] players) {
+        if (players.length < MIN_NUMBER_OF_PLAYERS) {
+            throw new IllegalArgumentException(MESSAGE_MIN_NUMBER_OF_PLAYERS);
+        }
     }
 
     public int numberOfPlayer(){
