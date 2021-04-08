e57f44e72f9e2f45d3169a86d9d550861af6d4d0
Dion <ksundong@gmail.com>
2021-04-09 00:06:39 +0900

    test: 사다리의 최소 높이를 정하는 실패하는 테스트 작성

--- src/test/java/nextstep/ladder/domain/LadderHeightTest.java
+++ src/test/java/nextstep/ladder/domain/LadderHeightTest.java
@@ -1,8 +1,10 @@
 package nextstep.ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import nextstep.ladder.exception.LadderHeightTooLowException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,5 +24,13 @@ class LadderHeightTest {
         () -> assertThat(ladderHeight).isEqualTo(new LadderHeight(heightSource)),
         () -> assertThat(ladderHeight.toInt()).isEqualTo(heightSource)
     );
+  }
+
+  @Test
+  @DisplayName("사다리의 높이는 최소 1이상이어야 한다.")
+  void createFailWhenLadderHeightUnderOne() {
+    assertThatThrownBy(() -> new LadderHeight(0))
+        .isInstanceOf(LadderHeightTooLowException.class)
+        .hasMessage(LadderHeightTooLowException.LADDER_HEIGHT_TOO_LOW);
   }
 }
