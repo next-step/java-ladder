6015b13edf9ee9d9c655ac990e906c7357ffb897
meme2367 <meme91322367@gmail.com>
2021-04-08 13:57:32 +0900

    feat : 5글자 초과 이름 확인

--- src/main/java/ladder/domain/Name.java
+++ src/main/java/ladder/domain/Name.java
@@ -7,10 +7,18 @@ public class Name {
   private final Pattern pattern = Pattern.compile(INVALID_PATTERN_REGEX);
   private final static String INVALID_PATTERN_REGEX = "^[가-힣a-zA-Z]*$";
   private final static String INVALID_NAME = "영어와 한글만 입력하세요.";
+  private final static String INVALID_NAME_LENGTH = "이름은 5글자이하여야 합니다.";
 
   public Name(String name) {
     this.name = name;
     validateName();
+    validateNameLength();
+  }
+
+  private void validateNameLength() {
+    if (name.length() > 5) {
+      throw new IllegalArgumentException(INVALID_NAME_LENGTH);
+    }
   }
 
   private void validateName() {
--- src/test/java/ladder/NameTest.java
+++ src/test/java/ladder/NameTest.java
@@ -9,11 +9,19 @@ import org.junit.jupiter.api.Test;
 public class NameTest {
 
   @Test
-  @DisplayName("잘못된 형식의 이름인지")
+  @DisplayName("잘못된 형식의 이름 확인")
   public void validateName() {
     assertThatThrownBy(() -> {
       Name name = new Name("asb#*s");
     }).isInstanceOf(IllegalArgumentException.class);
+  }
 
+
+  @Test
+  @DisplayName("5글자 초과 이름 확인")
+  public void validateNameLength() {
+    assertThatThrownBy(() -> {
+      Name name = new Name("abababababa");
+    }).isInstanceOf(IllegalArgumentException.class);
   }
 }
