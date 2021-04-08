78942d454bf0a9fbb185da41226e2144212735af
cptbong <cptbbong@gmail.com>
2021-04-08 19:33:03 +0900

    feat - client뷰 구현.

--- src/main/java/nextstep/LadderGameClient.java
+++ src/main/java/nextstep/LadderGameClient.java
@@ -1,28 +1,32 @@
 package nextstep;
 
 import nextstep.view.InputView;
+import nextstep.view.ResultView;
 
 public class LadderGameClient {
     public static void main(String[] args) {
         InputView inputView = new InputView();
+
         String namesString = inputView.inputQuestion("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         String ladderCountString = inputView.inputQuestion("최대 사다리 높이는 몇 개인가요?");
 
+
         int ladderCount = Integer.parseInt(ladderCountString);
+        ResultView resultView = new ResultView(ladderCount);
         String[] names = namesString.split(",");
         OnlineLadder ladder = new OnlineLadder(ladderCount);
-        for (String name : names) {
-            ladder.add(name);
-        }
-        for (Integer height : ladder.heights()) {
-            System.out.println("height = " + height);
-        }
-        System.out.println("");
-        ladder.move();
-        for (Integer height : ladder.heights()) {
-            System.out.println("height = " + height);
-            System.out.println("");
-        }
+        resultView.printAll(ladder, names);
+//        for (String name : names) {
+//            ladder.add(name);
+//        }
+//        ladder.move();
+//        resultView.print(ladder);
+//
+//        ladder.move();
+//        resultView.print(ladder);
+//
+//        ladder.move();
+//        resultView.print(ladder);
 
     }
 }
--- src/main/java/nextstep/view/ResultView.java
+++ src/main/java/nextstep/view/ResultView.java
@@ -1,8 +1,51 @@
 package nextstep.view;
 
+import nextstep.OnlineLadder;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ResultView implements ConsoleView {
+    private static final String HORIZON = "-----";
+    private static final String VERTICAL = "|";
+    private static final String EMPTY = "     ";
+    private List<Integer> beforeHeight;
+    private final int ladderCount;
+
+    public ResultView(int ladderCount) {
+        this.ladderCount = ladderCount;
+        this.beforeHeight = new ArrayList<>();
+        for (int i = 0; i < ladderCount; i++) {
+            this.beforeHeight.add(0);
+        }
+    }
+
     @Override
     public void print(String str) {
         System.out.println(str);
+    }
+
+    public void print(OnlineLadder ladder) {
+        Integer max = Collections.max(ladder.heights());
+        for(int i = 0; i < ladder.heights().size(); i++) {
+            if (beforeHeight.get(i) != ladder.heights().get(i) && ladder.heights().get(i).equals(max)) {
+                System.out.print(HORIZON + VERTICAL);
+            } else {
+                System.out.print(EMPTY + VERTICAL);
+            }
+        }
+        System.out.println();
+        this.beforeHeight = ladder.heights();
+    }
+
+    public void printAll(OnlineLadder ladder, String[] names) {
+        for (String name : names) {
+            ladder.add(name);
+        }
+        for (int i = 0; i < this.ladderCount; i++) {
+            ladder.move();
+            this.print(ladder);
+        }
     }
 }
