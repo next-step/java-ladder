5e66c6d4b3244928e4591989b4f1f68ef7d151c7
shkwon <oper912@naver.com>
2021-04-08 22:18:27 +0900

    feat: Person 추가

---
+++ src/main/java/ladder/domain/Person.java
@@ -0,0 +1,28 @@
+package ladder.domain;
+
+import java.util.Objects;
+
+public class Person {
+    private final Name name;
+
+    private Person(final Name name) {
+        this.name = name;
+    }
+
+    public static Person from(final String name) {
+        return new Person(Name.from(name));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
