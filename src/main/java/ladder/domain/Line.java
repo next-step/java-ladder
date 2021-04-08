9c6040e1e741d960f0748ef81f170197501c2717
shkwon <oper912@naver.com>
2021-04-09 01:32:05 +0900

    feat: Line 추가

---
+++ src/main/java/ladder/domain/Line.java
@@ -0,0 +1,36 @@
+package ladder.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Line {
+    private final List<Boolean> points;
+
+    private Line(final List<Boolean> points) {
+        this.points = points;
+    }
+
+    public static Line of(final int countOfPerson, Supplier<Boolean> supplier) {
+        List<Boolean> points = new ArrayList<>();
+        Boolean beforePoint = Boolean.FALSE;
+        for (int i = 0; i < countOfPerson; i++) {
+            Boolean point = getPoint(beforePoint, supplier);
+            points.add(point);
+            beforePoint = point;
+        }
+        return new Line(points);
+    }
+
+    private static Boolean getPoint(final Boolean beforePoint, Supplier<Boolean> supplier) {
+        if (beforePoint) {
+            return Boolean.FALSE;
+        }
+        return supplier.get();
+    }
+
+    public List<Boolean> getPoints() {
+        return Collections.unmodifiableList(points);
+    }
+}
