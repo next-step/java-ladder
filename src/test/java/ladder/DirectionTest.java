f2de90b29513dc12f1a17d3a08a01e575ef4bc9c
meme2367 <meme91322367@gmail.com>
2021-04-08 10:51:47 +0900

    fix : Direction, Point Test 코드 작성

---
+++ src/test/java/ladder/DirectionTest.java
@@ -0,0 +1,60 @@
+package ladder;
+
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+
+import org.junit.jupiter.api.Test;
+
+
+public class DirectionTest {
+  @Test
+  public void init() {
+    assertThat(Direction.of(true, false), is(Direction.of(true, false)));
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void init_invalid() {
+    Direction.of(TRUE, TRUE);
+  }
+
+  @Test
+  public void next_random_true() {
+    Direction next = Direction.first(TRUE).next();
+    assertThat(next, is(Direction.of(TRUE, FALSE)));
+  }
+
+  @Test
+  public void next_random_false() {
+    for (int i = 0; i < 100; i++) {
+      Direction.first(FALSE).next();
+    }
+  }
+
+  @Test
+  public void next_true() {
+    Direction next = Direction.of(TRUE, FALSE).next(TRUE);
+    assertThat(next, is(Direction.of(FALSE, TRUE)));
+  }
+
+  @Test
+  public void next_false() {
+    Direction next = Direction.of(FALSE, TRUE).next(FALSE);
+    assertThat(next, is(Direction.of(TRUE, FALSE)));
+  }
+
+  @Test
+  public void first() {
+    Direction first = Direction.first(TRUE);
+    assertThat(first.isLeft(), is(FALSE));
+  }
+
+  @Test
+  public void last() {
+    Direction last = Direction.first(TRUE).last();
+    assertThat(last, is(Direction.of(TRUE, FALSE)));
+  }
+}
--- src/test/java/ladder/PointTest.java
+++ src/test/java/ladder/PointTest.java
@@ -3,17 +3,40 @@ package ladder;
 import ladder.domain.Point;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 
 public class PointTest {
 
   @Test
-  @DisplayName("채워져있는 point인지, 비어있는 point인지 확인")
-  public void isUsed() {
-    Point usedPoint = new Point(true);
-    assertThat(usedPoint.isUsed()).isEqualTo(true);
-    Point notUsedPoint = new Point(false);
-    assertThat(notUsedPoint.isUsed()).isEqualTo(false);
+  public void first() {
+    assertThat(Point.first(TRUE).move(), is(1));
+    assertThat(Point.first(FALSE).move(), is(0));
+  }
+
+  @Test
+  public void next_stay() {
+    Point second = Point.first(FALSE).next(FALSE);
+    assertThat(second.move(), is(1));
+  }
+
+  @Test
+  public void next_left() {
+    Point second = Point.first(TRUE).next(FALSE);
+    assertThat(second.move(), is(0));
+  }
+
+  @Test
+  public void next_right() {
+    Point second = Point.first(FALSE).next(TRUE);
+    assertThat(second.move(), is(2));
+  }
+
+  @Test
+  public void next() {
+    Point second = Point.first(TRUE).next();
+    assertThat(second.move(), is(0));
   }
 
 }
