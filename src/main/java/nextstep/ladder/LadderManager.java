6e004d6ac52d7d14229e9b6c5046df1f443a890a
Dion <ksundong@gmail.com>
2021-04-08 22:34:57 +0900

    feat: 참여자 이름을 입력 받는 기능 구현

--- src/main/java/nextstep/ladder/LadderManager.java
+++ src/main/java/nextstep/ladder/LadderManager.java
@@ -1,8 +1,10 @@
 package nextstep.ladder;
 
+import nextstep.ladder.io.InputView;
+
 public final class LadderManager {
 
   public static void main(String[] args) {
-
+    final String[] names = InputView.inputNames();
   }
 }
