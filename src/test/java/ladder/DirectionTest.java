a235612cd998b38b7b9e66a4c14065cd9526471b
meme2367 <meme91322367@gmail.com>
2021-04-08 11:13:02 +0900

    fix : DirectionTest 코드 수정

--- src/test/java/ladder/DirectionTest.java
+++ src/test/java/ladder/DirectionTest.java
@@ -5,29 +5,38 @@ import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import ladder.domain.Direction;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 public class DirectionTest {
   @Test
+  @DisplayName("Direction 생성 확인")
   public void init() {
-    assertThat(Direction.of(true, false), is(Direction.of(true, false)));
-  }
-
-  @Test(expected = IllegalStateException.class)
-  public void init_invalid() {
-    Direction.of(TRUE, TRUE);
+    assertThat(Direction.of(TRUE, FALSE)).isEqualTo(Direction.of(TRUE, FALSE));
   }
 
   @Test
+  @DisplayName("각 포인트의 좌우가 true이면 안됨")
+  public void init_invalid() {
+    assertThatThrownBy(() -> {
+      Direction.of(TRUE, TRUE);
+    }).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  @DisplayName("첫번째 포인트가 TRUE이면 다음 포인트는 FALSE여야함")
   public void next_random_true() {
     Direction next = Direction.first(TRUE).next();
-    assertThat(next, is(Direction.of(TRUE, FALSE)));
+    assertThat(next).isEqualTo(Direction.of(TRUE, FALSE));
+
   }
 
   @Test
+  @DisplayName("")
   public void next_random_false() {
     for (int i = 0; i < 100; i++) {
       Direction.first(FALSE).next();
@@ -37,24 +46,24 @@ public class DirectionTest {
   @Test
   public void next_true() {
     Direction next = Direction.of(TRUE, FALSE).next(TRUE);
-    assertThat(next, is(Direction.of(FALSE, TRUE)));
+    assertThat(next).isEqualTo(Direction.of(FALSE,TRUE));
   }
 
   @Test
   public void next_false() {
     Direction next = Direction.of(FALSE, TRUE).next(FALSE);
-    assertThat(next, is(Direction.of(TRUE, FALSE)));
+    assertThat(next).isEqualTo(Direction.of(TRUE,FALSE));
   }
 
   @Test
   public void first() {
     Direction first = Direction.first(TRUE);
-    assertThat(first.isLeft(), is(FALSE));
+    assertThat(first.isLeft()).isEqualTo(FALSE);
   }
 
   @Test
   public void last() {
     Direction last = Direction.first(TRUE).last();
-    assertThat(last, is(Direction.of(TRUE, FALSE)));
+    assertThat(last).isEqualTo(Direction.of(TRUE,FALSE));
   }
 }
