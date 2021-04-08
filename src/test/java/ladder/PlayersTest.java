193d635e2d7d21f97fc80a89ca0f0f16d6dd641a
Sooncheol <ryxhdrnrmf@gmail.com>
2021-04-08 22:08:10 +0900

    test: Players Test 작성

---
+++ src/test/java/ladder/PlayersTest.java
@@ -0,0 +1,16 @@
+package ladder;
+
+import ladder.domain.Line;
+import ladder.domain.Players;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PlayersTest {
+    @Test
+    public void Players_참여자수_Test(){
+        assertThatThrownBy(() -> new Players(new String[]{"pobi"}))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("참여자는 2명 이상이어야 합니다.");
+    }
+}
