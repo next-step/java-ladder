7d611b2b185d5b8fcafd49bc2681388a86a0cadd
seokrae <kslbsh@gmail.com>
2021-04-09 22:49:13 +0900

    refactor: HintLadderLine 테스트 수정

--- src/test/java/nextstep/ladder/hint/HintLadderLineTest.java
+++ src/test/java/nextstep/ladder/hint/HintLadderLineTest.java
@@ -3,20 +3,21 @@ package nextstep.ladder.hint;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
+
 class HintLadderLineTest {
 
     @DisplayName("LadderLine 인원 수 값으로 초기화 및 생성 테스트")
     @Test
     void init() {
         int sizeOfPerson = 5;
-        HintLadderLine init = HintLadderLine.init(sizeOfPerson);
-        System.out.println(init);
+        assertThatCode(() -> HintLadderLine.init(sizeOfPerson)).doesNotThrowAnyException();
     }
 
     @DisplayName("LadderLine 2명 참여 시 첫 번째 참여자의 이동 테스트")
     @Test
     void move() {
         HintLadderLine line = HintLadderLine.init(2);
-        System.out.println("ladder result : " + line.move(0));
+        assertThatCode(() -> line.move(0)).doesNotThrowAnyException();
     }
 }
