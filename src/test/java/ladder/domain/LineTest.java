efb9243e17766261d56b10502267eb48ed73236d
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-09 23:53:50 +0900

    test : Line 클래스 테스트 코드 작성

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class LineTest {
@@ -21,21 +22,46 @@ class LineTest {
 
         // given when then
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Line(pointCount, new RandomLineWriteStrategy()))
+                .isThrownBy(() -> new Line(new RandomLineWriteStrategy(), pointCount))
                 .withMessageMatching("라인 생성을 위해서는 최소 두 개의 점이 필요합니다.");
     }
 
     @Test
-    @DisplayName("겹치는 라인 생성 방지 - 항상 라인을 그리는 경우 첫 번째 Point에만 라인이 생긴다.")
+    @DisplayName("라인 생성 - 포인트 수")
+    void lineCreate_pointCount() {
+        // given when
+        int pointCount = 10;
+        Line line = new Line(pointCount);
+
+        // then
+        assertThat(pointCount).isEqualTo(line.getPointList().size());
+    }
+
+    @Test
+    @DisplayName("라인 생성 - 가로줄이 없는 경우")
+    void lineCreate_emptyHorizon() {
+        // given when
+        Line line = new Line(() -> false, 10);
+
+        // then
+        for (Point point : line.getPointList()) {
+            assertThat(point.hasLine()).isFalse();
+        }
+    }
+
+    @Test
+    @DisplayName("겹치는 라인 생성 방지 - 항상 라인을 그리는 전략 활용")
     void lineCreate_alwaysLineWrite() {
         // given when
-        Line line = new Line(5, () -> true);
+        Line line = new Line(() -> true, 10);
 
         // then
         Assertions.assertThat(line.getPointList().get(0).hasLine()).isTrue();
-        for (int i = 1; i < line.getPointList().size(); i++) {
-            Assertions.assertThat(line.getPointList().get(i).hasLine()).isFalse();
+        for (int i = 1; i < line.getPointList().size()-1; i++) {
+            boolean prevHasLine = line.getPointList().get(i-1).hasLine();
+            Assertions.assertThat(line.getPointList().get(i).hasLine()).isNotEqualTo(prevHasLine);
         }
+        Assertions.assertThat(line.getPointList().get(line.getPointList().size()-1).hasLine()).isFalse();
     }
 
 }
