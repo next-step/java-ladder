aa3078fe710274cea740447e16ab04d1c1100c1e
Younghyun <younghyunjo99@gmail.com>
2021-04-09 12:27:13 +0900

    feat(step3):LadderResult 추가

--- src/main/java/ladder/domain/ResultElement.java
+++ src/main/java/ladder/domain/ResultElement.java
@@ -1,5 +1,7 @@
 package ladder.domain;
 
+import java.util.Objects;
+
 public class ResultElement {
     private final String name;
     private final String goal;
@@ -9,8 +11,15 @@ public class ResultElement {
         this.goal = goal;
     }
 
-
     public String goal() {
         return goal;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public boolean nameEquals(String name) {
+        return this.name.equals(name);
     }
 }
