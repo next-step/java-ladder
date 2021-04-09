0405ee2350c24b31577ab7431fbc6cbf00f23b7b
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-09 23:09:17 +0900

    feat : generatePointList 메서드 리팩토링  - 첫 포인트, 중간포인트, 마지막 포인트 순서로 생성구조 정리

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -7,6 +7,8 @@ public class Line {
 
     private static final int MIN_POINT_COUNT = 2;
     private static final boolean EMPTY_LINE = false;
+    private static final int FIRST_POINT_INDEX = 0;
+    private static final int FIRST_MIDDLE_POINT_INDEX = 1;
 
     private final List<Point> pointList;
     private final LineWriteStrategy lineWriteStrategy;
@@ -25,28 +27,42 @@ public class Line {
 
     private List<Point> generatePointList(int pointCount) {
         List<Point> points = new ArrayList<>();
-        Point currentPoint = generatePoint();
-        points.add(currentPoint);
-        for (int i = 1; i < pointCount - 1; i++) {
-            currentPoint = generateMiddlePoint(currentPoint);
-            points.add(currentPoint);
-        }
+        points.add(generateFirstPoint());
+        points.addAll(generateMiddlePoints(points.get(FIRST_POINT_INDEX), pointCount));
         points.add(generateLastPoint());
         return points;
+    }
+
+    private Point generateFirstPoint() {
+        return generatePoint();
+    }
+
+    private List<Point> generateMiddlePoints(Point firstPoint, int pointCount) {
+        List<Point> middlePoints = new ArrayList<>();
+        Point middlePoint = firstPoint;
+        for (int i = FIRST_MIDDLE_POINT_INDEX; i < pointCount - 1; i++) {
+            middlePoint = generateMiddlePoint(middlePoint);
+            middlePoints.add(middlePoint);
+        }
+        return middlePoints;
+    }
+
+    private Point generateMiddlePoint(Point currentPoint) {
+        if (currentPoint.hasLine()) {
+            return generateEmptyPoint();
+        }
+        return generatePoint();
+    }
+
+    private Point generateLastPoint() {
+        return generateEmptyPoint();
     }
 
     private Point generatePoint() {
         return new Point(lineWriteStrategy.write());
     }
 
-    private Point generateMiddlePoint(Point currentPoint) {
-        if(currentPoint.hasLine()) {
-            return new Point(EMPTY_LINE);
-        }
-        return generatePoint();
-    }
-
-    private Point generateLastPoint() {
+    private Point generateEmptyPoint() {
         return new Point(EMPTY_LINE);
     }
 
