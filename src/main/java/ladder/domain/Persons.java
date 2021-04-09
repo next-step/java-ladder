7e3af9afe11d61b8bf65966a02c6931939eb301e
shkwon <oper912@naver.com>
2021-04-10 04:51:13 +0900

    refactor: 참여할 사람 이름 중복 불가능한 자료구조로 변경

--- src/main/java/ladder/domain/Persons.java
+++ src/main/java/ladder/domain/Persons.java
@@ -2,34 +2,33 @@ package ladder.domain;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Persons {
-    private List<Person> persons;
+    private final Set<Person> persons;
 
-    private Persons(final List<Person> persons) {
+    private Persons(final Set<Person> persons) {
         this.persons = persons;
     }
 
     public static Persons from(String[] personNames) {
-        List<Person> persons = Arrays.stream(personNames)
+        Set<Person> persons = Arrays.stream(personNames)
                 .map(name -> Person.from(name))
-                .distinct()
-                .collect(Collectors.toList());
+                .collect(Collectors.toSet());
         validate(personNames, persons);
         return new Persons(persons);
     }
 
-    private static void validate(String[] personNames, List<Person> persons) {
+    private static void validate(String[] personNames, Set<Person> persons) {
         if (personNames.length != persons.size()) {
             throw new RuntimeException("참여할 사람 이름은 중복으로 입력할 수 없습니다.");
         }
     }
 
-    public List<Person> getPersons() {
-        return Collections.unmodifiableList(persons);
+    public Set<Person> getPersons() {
+        return Collections.unmodifiableSet(persons);
     }
 
     @Override
