0831caaf6a126a6a3a1b6eeb2e0656f47e637e81
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-09 18:30:06 +0900

    feat : 사다리 폭 표현을 위한 필드추가

--- src/main/java/ladder/controller/dto/LadderGenerationResponse.java
+++ src/main/java/ladder/controller/dto/LadderGenerationResponse.java
@@ -5,12 +5,18 @@ import java.util.List;
 public class LadderGenerationResponse {
 
     private final List<String> participants;
+    private final int ladderWidth;
 
-    public LadderGenerationResponse(List<String> participants) {
+    public LadderGenerationResponse(List<String> participants, int ladderWidth) {
         this.participants = participants;
+        this.ladderWidth = ladderWidth;
     }
 
     public List<String> getParticipants() {
         return participants;
+    }
+
+    public int getLadderWidth() {
+        return ladderWidth;
     }
 }
