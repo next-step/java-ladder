d6e4cb7afbb1936e6593e754071d9ed1fc0ad8a1
jaystar13 <astarisk2@gmail.com>
2021-04-11 07:44:29 +0900

    feat: Line 클래스 구현

---
+++ src/main/java/ladder/Line.java
@@ -0,0 +1,39 @@
+package ladder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Line {
+    private final List<Point> points;
+
+    public Line(List<Point> points) {
+        this.points = points;
+    }
+
+    public static Line init(Players players) {
+        List<Point> points = new ArrayList<>();
+        Optional.ofNullable(players)
+                .map(Players::allPlayers)
+                .orElseThrow(IllegalArgumentException::new)
+                .stream()
+                .map(player -> new Point(players.playerPosition(player)))
+                .forEach(points::add);
+        return new Line(points);
+    }
+
+    public void makeLine() {
+        Point comparePoint = new Point("DEFAULT");
+        for (Point point : points) {
+            point.setDirection(LadderGenerator.generate(point, comparePoint));
+            comparePoint = point;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Line{" +
+                "points=" + points +
+                '}';
+    }
+}
