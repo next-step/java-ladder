5750c16ba50ef8c243c30c9005c70b1a8756f55a
juniq <juniqlim@gmail.com>
2021-04-10 15:13:50 +0900

    feat: 게임 참여자 이름 입력 - 최대 5글자

---
+++ src/main/java/nextstep/ladder/domain/Member.java
@@ -0,0 +1,16 @@
+package nextstep.ladder.domain;
+
+public class Member {
+    public final String name;
+
+    private Member(String name) {
+        this.name = name;
+    }
+
+    public static Member from(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("참여자 이름은 5글자 이하여야 합니다.");
+        }
+        return new Member(name);
+    }
+}
