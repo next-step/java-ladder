ae71c40f8461cf0d680ee35c44b94b454906edda
김우재 <kwj1270@naver.com>
2021-04-10 16:33:16 +0900

    feat:getUser Optional로 리팩터링

--- src/main/java/nextstep/optional/Users.java
+++ src/main/java/nextstep/optional/Users.java
@@ -3,21 +3,19 @@ package nextstep.optional;
 import java.util.Arrays;
 import java.util.List;
 
-public class Users {
+public final class Users {
     static final User DEFAULT_USER = new User("codesquad", 100);
 
-    List<User> users = Arrays.asList(
+    final List<User> users = Arrays.asList(
             new User("crong", 35),
             new User("pobi", 30),
             new User("jk", 40),
             new User("honux", 45));
 
-    User getUser(String name) {
-        for (User user : users) {
-            if (user.matchName(name)) {
-                return user;
-            }
-        }
-        return DEFAULT_USER;
+    final User getUser(String name) {
+        return users.stream()
+                .filter(user -> user.matchName(name))
+                .findFirst()
+                .orElse(DEFAULT_USER);
     }
 }
