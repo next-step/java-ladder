55a1a9b88a7abe474d9da0a7c44326b568364250
jaystar13 <astarisk2@gmail.com>
2021-04-10 19:20:26 +0900

    feat: 참자가의 위치정보를 반환 메서드 구현

--- src/main/java/ladder/Players.java
+++ src/main/java/ladder/Players.java
@@ -7,10 +7,30 @@ import java.util.stream.Collectors;
 public class Players {
     private final List<Player> players;
 
-    public Players(List<String> players) {
+    private Players(List<String> players) {
         this.players = players.stream()
                 .map(Player::new)
                 .collect(Collectors.toList());
+    }
+
+    public static Players of(List<String> players) {
+        return new Players(players);
+    }
+
+    public String playerPosition(Player position) {
+        Player matchedPlayer = players.stream()
+                .filter(player -> player.equals(position))
+                .findFirst()
+                .get();
+
+        if (matchedPlayer.equals(players.get(0))) {
+            return "FIRST";
+        }
+
+        if (matchedPlayer.equals(players.get(players.size() - 1))) {
+            return "LAST";
+        }
+        return "BODY";
     }
 
     @Override
