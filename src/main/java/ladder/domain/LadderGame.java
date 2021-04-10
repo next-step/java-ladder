6f762f01f04edb4a4e199501f1dcae6ba19be0c8
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-10 14:18:10 +0900

    feat : LadderGenerationService 클래스 신규 구현

--- src/main/java/ladder/domain/LadderGame.java
+++ src/main/java/ladder/domain/LadderGame.java
@@ -6,6 +6,8 @@ import java.util.stream.IntStream;
 
 public class LadderGame {
 
+    private static final int MIN_LADDER_HEIGHT = 1;
+
     private final Participants participants;
     private final List<Line> lineList;
 
@@ -19,9 +21,19 @@ public class LadderGame {
     }
 
     private static List<Line> generateLineList(Participants participants, int ladderHeight) {
+        validate(participants, ladderHeight);
         return IntStream.range(0, ladderHeight)
                 .mapToObj(i -> new Line(participants.getCount()))
                 .collect(Collectors.toList());
+    }
+
+    private static void validate(Participants participants, int ladderHeight) {
+        if (participants == null) {
+            throw new IllegalArgumentException("참가자 정보를 입력해 주세요.");
+        }
+        if (ladderHeight < MIN_LADDER_HEIGHT) {
+            throw new IllegalArgumentException("사다리의 높이는 최소 " + MIN_LADDER_HEIGHT + " 이상이어야 합니다.");
+        }
     }
 
     public Participants getParticipants() {
