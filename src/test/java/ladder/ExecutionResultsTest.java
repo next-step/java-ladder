0174dd09b9930a0194bbf6591e750261cd5b1a95
Sooncheol <ryxhdrnrmf@gmail.com>
2021-04-11 04:10:55 +0900

    test: ExecutionResultsTest 메소드 네이밍 수정

--- src/test/java/ladder/ExecutionResultsTest.java
+++ src/test/java/ladder/ExecutionResultsTest.java
@@ -10,30 +10,30 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ExecutionResultsTest {
-    @ParameterizedTest(name = "execution_Result_number_Of_Players_Test")
-    @MethodSource("execution_Result_number_Of_Players_Provider")
-    public void point_Next_RIGHT_Test(int numberOfPlayers, String[] executionResults) {
+    @ParameterizedTest(name = "실행결과_참여자수_Test")
+    @MethodSource("실행결과_참여자수_Provider")
+    public void 실행결과_참여자수_Test(int numberOfPlayers, String[] executionResults) {
         assertThatThrownBy(() -> new ExecutionResults(numberOfPlayers,executionResults))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("실행 결과는 참여자의 수와 일치해야 합니다.");
     }
 
-    static Stream<Arguments> execution_Result_number_Of_Players_Provider() {
+    static Stream<Arguments> 실행결과_참여자수_Provider() {
         return Stream.of(
                 Arguments.of(3,new String[]{"pobi","crong"}),
                 Arguments.of(4,new String[]{"pobi","crong","dog","poo","apple"})
         );
     }
 
-    @ParameterizedTest(name = "execution_Result_Length_Test")
-    @MethodSource("execution_Result_Length_Provider")
-    public void execution_Result_Length_Test(int numberOfPlayers, String[] executionResults) {
+    @ParameterizedTest(name = "실행결과_참여자_이름길이_Test")
+    @MethodSource("실행결과_참여자_이름길이_Provider")
+    public void 실행결과_참여자_이름길이_Test(int numberOfPlayers, String[] executionResults) {
         assertThatThrownBy(() -> new ExecutionResults(numberOfPlayers,executionResults))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("실행 결과는 최대 5글자입니다.");
     }
 
-    static Stream<Arguments> execution_Result_Length_Provider() {
+    static Stream<Arguments> 실행결과_참여자_이름길이_Provider() {
         return Stream.of(
                 Arguments.of(3,new String[]{"pobi","crong","notebook"}),
                 Arguments.of(4,new String[]{"pobi","crong","dog","crayon"})
