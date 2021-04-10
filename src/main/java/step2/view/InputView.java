54f73eafae6e8841796c22b01dbfd3f4b8098bf8
w-beom <w_bum@naver.com>
2021-04-10 18:38:55 +0900

    feat : inputView 사다리 높이 입력받기

--- src/main/java/step2/view/InputView.java
+++ src/main/java/step2/view/InputView.java
@@ -4,11 +4,17 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_MEMBER_MESSAGE = "참여할 사람 이름을 입력하세요.";
+    private static final String INPUT_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개 인가요?";
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static String inputLadderGameMember(){
         System.out.println(INPUT_MEMBER_MESSAGE);
         return scanner.next();
+    }
+
+    public static int inputLadderHeight(){
+        System.out.println(INPUT_LADDER_HEIGHT_MESSAGE);
+        return scanner.nextInt();
     }
 }
