862463d973069c59276941218c5da7319dbcaeec
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-10 11:36:15 +0900

    refactor : LadderGenerationRequest 필드명 변경  - participants -> participantNames 필드명 변경

--- src/main/java/ladder/controller/dto/LadderGenerationRequest.java
+++ src/main/java/ladder/controller/dto/LadderGenerationRequest.java
@@ -2,16 +2,16 @@ package ladder.controller.dto;
 
 public class LadderGenerationRequest {
 
-    private final String participants;
+    private final String participantNames;
     private final int ladderHeight;
 
-    public LadderGenerationRequest(String participants, int ladderHeight) {
-        this.participants = participants;
+    public LadderGenerationRequest(String participantNames, int ladderHeight) {
+        this.participantNames = participantNames;
         this.ladderHeight = ladderHeight;
     }
 
-    public String getParticipants() {
-        return participants;
+    public String getParticipantNames() {
+        return participantNames;
     }
 
     public int getLadderHeight() {
