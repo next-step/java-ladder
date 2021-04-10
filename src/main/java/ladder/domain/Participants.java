4c8c0bdfd4c0bbe69dcd059960cdf56529fdd94f
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-10 11:32:54 +0900

    feat : Participants 일급컬렉션 클래스 구현  - 이름목록 기반 참가자 생성로직 구현  - 검증로직 구현  - 테스트코드 작성

---
+++ src/main/java/ladder/domain/Participants.java
@@ -0,0 +1,40 @@
+package ladder.domain;
+
+import common.utils.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Participants {
+
+    private static final String NAME_DELIMITER = ",";
+
+    private final List<Participant> participantList;
+
+    public Participants(String participantNames) {
+        this(generateParticipantList(participantNames));
+    }
+
+    private Participants(List<Participant> participantList) {
+        this.participantList = participantList;
+    }
+
+    private static List<Participant> generateParticipantList(String participantNames) {
+        validateParticipantNames(participantNames);
+        return Arrays.stream(participantNames.split(NAME_DELIMITER))
+                .map(Participant::new)
+                .collect(Collectors.toList())
+                ;
+    }
+
+    private static void validateParticipantNames(String participantNames) {
+        if (StringUtils.isBlank(participantNames)) {
+            throw new IllegalArgumentException("참가자 이름을 입력해 주세요.");
+        }
+    }
+
+    public List<Participant> getParticipantList() {
+        return participantList;
+    }
+}
---
+++ src/test/java/ladder/domain/ParticipantsTest.java
@@ -0,0 +1,72 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class ParticipantsTest {
+
+    @Test
+    @DisplayName("참가자명 미입력")
+    void nameIsBlank() {
+        // given when then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Participants(null))
+                .withMessageMatching("참가자 이름을 입력해 주세요.");
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Participants(" "))
+                .withMessageMatching("참가자 이름을 입력해 주세요.");
+    }
+
+    @Test
+    @DisplayName("참가자 이름 목록 중 미입력 건 존재")
+    void namesWithBlank() {
+        // given when then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Participants("pobi,,,jk"))
+                .withMessageMatching("참가자 이름을 입력해 주세요.");
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Participants("pobi,   ,crong,jk"))
+                .withMessageMatching("참가자 이름을 입력해 주세요.");
+    }
+
+    @Test
+    @DisplayName("참가자 이름 목록 중 최대입력길이 초과 건 존재")
+    void nameWithMaxLength() {
+        // given when then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Participants("pobi,limdingdong,jk"))
+                .withMessageMatching("참가자 이름은 최대 5글자까지 입력할 수 있습니다.");
+    }
+
+    @Test
+    @DisplayName("참가자 정상생성 - 참가자 수 확인")
+    void create_participantCount() {
+        // given when
+        Participants participants = new Participants("pobi,honux,crong,jk");
+
+        // then
+        assertThat(4).isEqualTo(participants.getParticipantList().size());
+    }
+
+    @Test
+    @DisplayName("참가자 정상생성 - 참가자 명 확인")
+    void create_participantNames() {
+        // given when
+        String participantNames = "pobi,honux,crong,jk";
+        Participants participants = new Participants(participantNames);
+
+        // then
+        for (Participant participant : participants.getParticipantList()) {
+            boolean present = Arrays.stream(participantNames.split(",")).anyMatch(participantName -> participantName.equals(participant.getName()));
+            assertThat(present).isTrue();
+        }
+    }
+
+}
