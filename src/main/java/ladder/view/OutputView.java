d047eeb682eb7a847b3b73d5b3d17deb04614402
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-10 15:45:31 +0900

    feat : 사다리 정보 UI 출력 구현

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -1,15 +1,21 @@
 package ladder.view;
 
 import ladder.controller.dto.LadderGenerationResponse;
+import ladder.controller.dto.LadderLine;
 
 import java.util.List;
 
 public class OutputView {
 
+    private static final String EMPTY_HORIZON = " ";
+    private static final String HORIZON = "-";
+    private static final String POINT = "|";
+
     public void printLadderGenerationResult(LadderGenerationResponse response) {
-        System.out.println("\n실행결과\n");
+        System.out.println("\n실행결과");
         printParticipants(response.getParticipantNameList(), response.getLadderWidth());
-        // TODO: printLadder
+        System.out.println();
+        printLadder(response.getLadderLineList(), response.getLadderWidth());
     }
 
     private void printParticipants(List<String> participants, int ladderWidth) {
@@ -21,8 +27,37 @@ public class OutputView {
     private String printParticipant(String participant, int ladderWidth) {
         StringBuilder result = new StringBuilder(participant);
         for (int i = 0; i < ladderWidth - participant.length() + 1; i++) {
-            result.insert(0, " ");
+            result.insert(0, EMPTY_HORIZON);
         }
         return result.toString();
+    }
+
+    private void printLadder(List<LadderLine> ladderLineList, int ladderWidth) {
+        for (LadderLine ladderLine : ladderLineList) {
+            System.out.println(writeHorizon(EMPTY_HORIZON, ladderWidth) + writeLine(ladderLine, ladderWidth));
+        }
+    }
+
+    private String writeLine(LadderLine ladderLine, int ladderWidth) {
+        StringBuilder line = new StringBuilder();
+        for (Boolean point : ladderLine.getPointList()) {
+            line.append(POINT).append(writeHorizon(convertToHorizon(point), ladderWidth));
+        }
+        return line.toString();
+    }
+
+    private String convertToHorizon(Boolean point) {
+        if (point) {
+            return HORIZON;
+        }
+        return EMPTY_HORIZON;
+    }
+
+    private String writeHorizon(String horizon, int ladderWidth) {
+        StringBuilder horizons = new StringBuilder();
+        for (int i = 0; i < ladderWidth; i++) {
+            horizons.append(horizon);
+        }
+        return horizons.toString();
     }
 }
