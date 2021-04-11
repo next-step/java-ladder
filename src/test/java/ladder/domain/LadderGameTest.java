93560ba5c3c1a2d669868c343c00ffeab8fe467b
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-11 10:57:58 +0900

    test : LadderGame 클래스 테스트코드 작성

---
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -0,0 +1,96 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class LadderGameTest {
+
+    @Test
+    @DisplayName("사다리 게임 생성 - 참가자정보 미입력")
+    void participantsIsNull() {
+        // given when then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LadderGame(null, 5))
+                .withMessageMatching("참가자 정보를 입력해 주세요.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1})
+    @DisplayName("사다리 게임 생성 - 사다리 높이 검증")
+    void ladderHeight(int ladderHeight) {
+        // given when then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LadderGame(new Participants("pobi,honux,crong,jk"), ladderHeight))
+                .withMessageMatching("사다리의 높이는 최소 1 이상이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("참가자 수 검증")
+    void participantsCount() {
+        // given
+        Participants participants = new Participants("pobi,honux,crong,jk");
+
+        // when
+        LadderGame ladderGame = new LadderGame(participants, 5);
+
+        // then
+        assertThat(4).isEqualTo(ladderGame.getParticipants().getCount());
+    }
+
+    @Test
+    @DisplayName("참가자 명 검증")
+    void participantName() {
+        // given
+        String participantNames = "pobi,honux,crong,jk";
+        Participants participants = new Participants(participantNames);
+
+        // when
+        LadderGame ladderGame = new LadderGame(participants, 5);
+
+        // then
+        for (Participant participant : ladderGame.getParticipants().getParticipantList()) {
+            assertThat(Arrays.asList(participantNames.split(",")).contains(participant.getName())).isTrue();
+        }
+    }
+
+    @Test
+    @DisplayName("사다리 생성 검증")
+    void ladder() {
+        // given
+        Participants participants = new Participants("pobi,honux,crong,jk");
+
+        // when
+        LadderGame ladderGame = new LadderGame(participants, 5);
+        List<Line> lineList = ladderGame.getLineList();
+
+        // then
+        assertAll(
+                () -> assertThat(5).isEqualTo(lineList.size()),
+                () -> lineList.forEach(line -> assertThat(4).isEqualTo(line.getPointList().size()))
+        );
+    }
+
+    @Test
+    @DisplayName("그려진 사다리는 수정할 수 없음")
+    void ladderIsUnmodifiable() {
+        // given
+        Participants participants = new Participants("pobi,honux,crong,jk");
+
+        // when
+        LadderGame ladderGame = new LadderGame(participants, 5);
+        List<Line> lineList = ladderGame.getLineList();
+
+        // then
+        assertThrows(UnsupportedOperationException.class, () -> lineList.add(new Line(5)));
+    }
+}
