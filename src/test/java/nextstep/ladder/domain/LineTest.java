55203d645fc6b4fc2d80157d678391bb0a4098d6
juniq <juniqlim@gmail.com>
2021-04-11 17:22:48 +0900

    test: 사다리 라인 생성 - 랜덤으로 가로 라인 생성

--- src/test/java/nextstep/ladder/domain/LineTest.java
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -1,7 +1,10 @@
 package nextstep.ladder.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class LineTest {
@@ -9,5 +12,16 @@ public class LineTest {
     @Test
     void create() {
         assertThatCode(() -> Line.from(5)).doesNotThrowAnyException();
+    }
+
+    @Test
+    void points() {
+        List<Boolean> points = new ArrayList<>();
+        points.add(true);
+        points.add(false);
+        points.add(true);
+        points.add(false);
+
+        assertThat(Line.from(5).points()).isEqualTo(points);
     }
 }
