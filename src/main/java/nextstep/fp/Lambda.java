3f7a1eda7a444a095ac36d6d4ecf3bad72f0be2b
이승환 <lufful.induk@gmail.com>
2021-04-11 16:38:13 +0900

    <feat> Optional을 활용해 조건에 따른 반환
    - Optional에서 값을 반환
    - Optional에서 exception 처리

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -18,12 +18,7 @@ public class Lambda {
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> System.out.println("Hello from thread")).start();
     }
 
     public static int sumAll(List<Integer> numbers, Conditional Conditional) {
@@ -35,12 +30,6 @@ public class Lambda {
     }
 
     public static int sumAllOverThree(List<Integer> numbers, Conditional conditional) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return conditional.calculate(numbers);
     }
 }
--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,7 +5,9 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
@@ -15,11 +17,10 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words
+            .stream()
+            .filter(wordLengthPredicate())
+            .count();
     }
 
     public static void printLongestWordTop100() throws IOException {
@@ -27,7 +28,13 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words
+            .stream()
+            .filter(wordLengthPredicate())
+            .sorted(Comparator.reverseOrder())
+            .distinct()
+            .limit(100)
+            .forEach(word -> System.out.println(word));
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -40,5 +47,9 @@ public class StreamStudy {
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
         return 0;
+    }
+
+    private static Predicate<String> wordLengthPredicate() {
+        return word -> word.length() > 12;
     }
 }
