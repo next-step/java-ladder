7f06c7c9f2fdc2c0f49e6540fea0cd9855bd8b9b
cptbong <cptbbong@gmail.com>
2021-04-11 20:26:24 +0900

    test - 사람이름유효성검사테스트성공.

--- src/main/java/nextstep/Players.java
+++ src/main/java/nextstep/Players.java
@@ -1,17 +1,22 @@
 package nextstep;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Players {
     private final List<String> names;
 
     public Players(String[] names) {
         this.names = new ArrayList<>();
+        Optional<String> first = Arrays.stream(names)
+                .filter(s -> s.length() > 5)
+                .findFirst();
+        if (first.isPresent()) {
+            throw new IllegalArgumentException("사람의 이름은 최대5글자까지 부여할 수 있습니다 : " + first.get());
+        }
         for (String name : names) {
-            if (name.length() > 5) {
-                throw new IllegalArgumentException("사람의 이름은 최대5글자까지 부여할 수 있습니다 : " + name);
-            }
             this.names.add(name);
         }
     }
