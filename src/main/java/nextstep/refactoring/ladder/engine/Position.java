a265b6ceff501227df856d693186bbed0cfc3687
Jake <cocahack@naver.com>
2021-04-11 20:14:06 +0900

    feat: 포지션 구현

---
+++ src/main/java/nextstep/refactoring/ladder/engine/Position.java
@@ -0,0 +1,44 @@
+package nextstep.refactoring.ladder.engine;
+
+import java.util.Objects;
+
+public class Position {
+
+    private final int index;
+
+    private Position(int index) {
+        this.index = index;
+    }
+
+    public static Position of(int index) {
+        validate(index);
+        return new Position(index);
+    }
+
+    private static void validate(int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException("음수는 사용할 수 없습니다.");
+        }
+    }
+
+    public Position getLeft() {
+        return new Position(index - 1);
+    }
+
+    public Position getRight() {
+        return new Position(index + 1);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Position)) return false;
+        Position position = (Position) o;
+        return index == position.index;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(index);
+    }
+}
