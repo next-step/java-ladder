a5a806632468bfac9829353fa3cd60b60871c9bf
Beenie93 <seongbeen93@naver.com>
2021-04-12 00:23:24 +0900

    feat: implement a method to input height

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -5,7 +5,8 @@ import nextstep.ladder.utils.PreCondition;
 import java.util.Scanner;
 
 public class InputView {
-    private static final String PLAYERS = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String INPUT_PLAYERS = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    public static final String INPUT_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
     private final Scanner scanner;
 
     public InputView(Scanner scanner) {
@@ -13,9 +14,16 @@ public class InputView {
     }
 
     public String players() {
-        System.out.println(PLAYERS);
+        System.out.println(INPUT_PLAYERS);
         String players = scanner.nextLine().trim();
         PreCondition.checkNullOrEmpty(players);
         return players;
+    }
+
+    public String height() {
+        System.out.println(INPUT_HEIGHT);
+        String height = scanner.nextLine().trim();
+        PreCondition.checkNullOrEmpty(height);
+        return height;
     }
 }
