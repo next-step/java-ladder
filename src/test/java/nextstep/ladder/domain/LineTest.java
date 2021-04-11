88ad8a75ae58d8b41804ff2a534eb30f3e75dc78
juniq <juniqlim@gmail.com>
2021-04-11 18:10:56 +0900

    test: 사다리 라인 생성 - 가로 라인은 겹치치 않아야 함

--- src/test/java/nextstep/ladder/domain/LineTest.java
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -5,7 +5,10 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LineTest {
 
@@ -23,5 +26,21 @@ public class LineTest {
         points.add(Point.from(false));
 
         assertThat(Line.from(5, () -> true).points()).usingRecursiveComparison().isEqualTo(points);
+    }
+
+    @DisplayName("가로라인은 겹치지 않아야 한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10, 100})
+    void pointsNotContinuousTrue(int countOfPerson) {
+        List<Point> points = Line.from(countOfPerson, new RandomBooleanStrategy()).points();
+
+        for (int i = 0; i < points.size(); i++) {
+            if (i == 0) {
+                continue;
+            }
+            if (points.get(i - 1).value()) {
+                assertThat(points.get(i)).usingRecursiveComparison().isEqualTo(Point.from(false));
+            }
+        }
     }
 }
