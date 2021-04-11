9b5e344b2fceafb925b224ad9587af67d09d3d14
623nana <6230nana@gmail.com>
2021-04-11 22:38:40 +0900

    refactor: Point 클래스 수정에 따른 전면적인 리팩토링

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -1,54 +1,43 @@
 package ladder.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class Line {
 
-  private final List<Point> points = new ArrayList<>();
+  private final List<Point> points;
 
-  public Line(int countOfPerson) {
-    this(countOfPerson, new RandomConnect());
+  private Line(List<Point> points) {
+    this.points = points;
   }
 
-  public Line(int countOfPerson, ConnectStrategy connectStrategy) {
-    IntStream.range(0, countOfPerson)
-        .forEach(i -> points.add(connectPoint(connectStrategy)));
+  public int move(int position) {
+    return points.get(position).move();
   }
 
-  public Point connectPoint(ConnectStrategy connectStrategy) {
+  public static Line init(int countOfPerson, ConnectStrategy connectStrategy) {
+    List<Point> points = new ArrayList<>();
+    IntStream.range(0, countOfPerson - 1)
+        .forEach(i -> points.add(connect(i, points, connectStrategy)));
+    connectLast(points, points.get(countOfPerson - 2));
+    return new Line(points);
+  }
+
+  private static Point connect(int index, List<Point> points, ConnectStrategy connectStrategy) {
     if (points.isEmpty()) {
-      return Point.DISCONNECT;
+      return Point.first(connectStrategy.connect());
     }
+    return points.get(index - 1).next(connectStrategy);
+  }
 
-    Point prePoint = points.get(points.size() - 1);
-    if (prePoint.isConnected()) {
-      return Point.DISCONNECT;
-    }
-
-    return Point.getPoint(connectStrategy.connect());
+  private static void connectLast(List<Point> points, Point point) {
+    point = point.last();
+    points.add(point);
   }
 
   public List<Point> points() {
-    return points;
-  }
-
-  public int move(int index) {
-    return moveRight(index) + moveLeft(index);
-  }
-
-  private int moveRight(int index) {
-    if (index + 1 != points().size() && points.get(index + 1).isConnected()) {
-      return 1;
-    }
-    return 0;
-  }
-
-  private int moveLeft(int index) {
-    if (points.get(index).isConnected()) {
-      return -1;
-    }
-    return 0;
+    return Collections.unmodifiableList(points);
   }
 }
