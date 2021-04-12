6c23a1b64c7897fc570e13834cacf536c80e9122
Beenie93 <seongbeen93@naver.com>
2021-04-13 02:14:28 +0900

    feat: implement Line Object

---
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -0,0 +1,45 @@
+package nextstep.ladder.domain;
+
+import nextstep.ladder.strategy.ConnectStrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Line {
+    private final List<Point> points;
+    private final ConnectStrategy connectStrategy;
+
+    public Line(int countOfPlayer, ConnectStrategy connectStrategy) {
+        this.connectStrategy = connectStrategy;
+        this.points = create(countOfPlayer);
+    }
+
+    private List<Point> create(int countOfPlayer) {
+        List<Point> points = new ArrayList<>(countOfPlayer);
+        Point point = Point.first(connectStrategy);
+        points.add(point);
+
+        for (int i = 0; i < countOfPlayer - 2; i++) {
+            point = point.next();
+            points.add(point);
+        }
+
+        points.add(point.last());
+
+        return points;
+    }
+
+    public List<Point> points() {
+        return points;
+    }
+
+    public String draw() {
+        String points = this.points.stream()
+                .map(Point::draw)
+                .limit(points().size() - 1)
+                .collect(Collectors.joining("|"));
+
+        return "|" + points + "|";
+    }
+}
