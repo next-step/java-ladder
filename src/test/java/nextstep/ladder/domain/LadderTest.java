672cb1ada8a3ad62c70c89f712bc44fd73a27800
Beenie93 <seongbeen93@naver.com>
2021-04-13 02:38:54 +0900

    test: implement a test code to create Ladder

---
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -0,0 +1,16 @@
+package nextstep.ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LadderTest {
+
+    @Test
+    @DisplayName("입력받은 사다리 높이 수 만큼 라인을 생성한다.")
+    public void create() throws Exception {
+        Ladder ladder = new Ladder(new Height(5), 4);
+        assertThat(ladder.lines()).hasSize(5);
+    }
+}
