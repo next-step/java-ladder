25de10e644da8b5f9dd7c4a8f6921677e49bfa3f
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-12 20:20:43 +0900

    feat : 사다리를 표현하기 위한 Ladder 클래스 정의 및 구현  - 사다리타기 게임 실행로직 구현  - LadderGame 클래스에서 분리예정

---
+++ src/test/java/ladder/domain/LadderTest.java
@@ -0,0 +1,88 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class LadderTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1})
+    @DisplayName("사다리 게임 생성 - 사다리 높이 검증")
+    void ladderHeight(int ladderHeight) {
+        // given when then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Ladder(4, ladderHeight))
+                .withMessageMatching("사다리의 높이는 최소 1 이상이어야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, 1})
+    @DisplayName("사다리 게임 생성 - 사다리 너비 검증")
+    void ladderWidth(int ladderWidth) {
+        // given when then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Ladder(ladderWidth, 5))
+                .withMessageMatching("사다리의 너비는 최소 2 이상이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("사다리 생성 검증")
+    void ladder() {
+        // given
+        Ladder ladder = new Ladder(4, 5);
+
+        // when
+        List<Line> lineList = ladder.getLines();
+
+        // then
+        assertAll(
+                () -> assertThat(5).isEqualTo(lineList.size()),
+                () -> lineList.forEach(line -> assertThat(4).isEqualTo(line.getPoints().size()))
+        );
+    }
+
+    @Test
+    @DisplayName("그려진 사다리는 수정할 수 없음")
+    void ladderIsUnmodifiable() {
+        // given
+        Ladder ladder = new Ladder(4, 5);
+
+        // when
+        List<Line> lineList = ladder.getLines();
+
+        // then
+        assertThrows(UnsupportedOperationException.class, () -> lineList.add(new Line(5)));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:0", "1:3", "2:2", "3:1"}, delimiter = ':')
+    @DisplayName("사다리타기 결과 테스트")
+    void getExitNumber(int entranceNumber, int exitNumber) {
+        // given
+        List<Line> lines = new ArrayList<>();
+        lines.add(new Line(Stream.of(true, false, true, false).map(Point::new).collect(Collectors.toList())));
+        lines.add(new Line(Stream.of(false, true, false, false).map(Point::new).collect(Collectors.toList())));
+        lines.add(new Line(Stream.of(true, false, false, false).map(Point::new).collect(Collectors.toList())));
+        lines.add(new Line(Stream.of(false, true, false, false).map(Point::new).collect(Collectors.toList())));
+        lines.add(new Line(Stream.of(true, false, true, false).map(Point::new).collect(Collectors.toList())));
+
+        // when
+        Ladder ladder = new Ladder(lines);
+
+        // then
+        assertThat(exitNumber).isEqualTo(ladder.getExitNumber(entranceNumber));
+    }
+}
