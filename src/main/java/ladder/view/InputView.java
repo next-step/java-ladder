02c51582457fce23560b0801615bf7247673805e
김우재 <kwj1270@naver.com>
2021-04-12 22:31:56 +0900

    feat:InputView 생성 및 이름 입력 기능 구현

---
+++ src/main/java/ladder/view/InputView.java
@@ -0,0 +1,15 @@
+package ladder.view;
+
+import java.util.Scanner;
+
+public final class InputView {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+    public static final String INPUT_PARTICIPANTS_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+
+    public final String inputParticipantsByClient() {
+        System.out.println(INPUT_PARTICIPANTS_MESSAGE);
+        return SCANNER.nextLine();
+    }
+
+}
