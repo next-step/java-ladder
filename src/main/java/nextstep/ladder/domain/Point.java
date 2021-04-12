96508440a968edc6637762288293d891b734a4ec
Beenie93 <seongbeen93@naver.com>
2021-04-13 02:00:38 +0900

    feat: implement Point Object

---
+++ src/main/java/nextstep/ladder/domain/Point.java
@@ -0,0 +1,63 @@
+package nextstep.ladder.domain;
+
+import nextstep.ladder.strategy.ConnectStrategy;
+
+import java.util.Objects;
+
+public class Point {
+    private static final String CONNECTED = "-----";
+    private static final String UNCONNECTED = "     ";
+    private final int index;
+    private final Direction direction;
+
+    public Point(int index, Direction direction) {
+        this.index = index;
+        this.direction = direction;
+    }
+
+    public static Point first(ConnectStrategy connectStrategy) {
+        return new Point(0, Direction.first(connectStrategy));
+    }
+
+    public Point next() {
+        return new Point(index + 1, direction.next());
+    }
+
+    public Point next(ConnectStrategy connectStrategy) {
+        return new Point(index + 1, direction.next(connectStrategy));
+    }
+
+    public Point last() {
+        return new Point(index + 1, direction.last());
+    }
+
+    public boolean isConnected() {
+        return direction.hasAnyDirection();
+    }
+
+    public int move() {
+        return index + direction.move();
+    }
+
+    public String draw() {
+        if (direction.move() == 1) {
+            return CONNECTED;
+        }
+
+        return UNCONNECTED;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return index == point.index &&
+                Objects.equals(direction, point.direction);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(index, direction);
+    }
+}
