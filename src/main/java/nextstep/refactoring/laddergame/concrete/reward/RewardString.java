1822f15b5439b8bb7e225a2f45c5f4c77c25ad92
Jake <cocahack@naver.com>
2021-04-12 22:33:14 +0900

    feat: 보상 구현

---
+++ src/main/java/nextstep/refactoring/laddergame/concrete/reward/RewardString.java
@@ -0,0 +1,25 @@
+package nextstep.refactoring.laddergame.concrete.reward;
+
+public class RewardString {
+
+    private static final String BLANK_REWARD_STRING = "꽝";
+
+    private final String string;
+
+    private RewardString(String string) {
+        this.string = string;
+    }
+
+    public static RewardString of(String string) {
+        validate(string);
+        return new RewardString(string);
+    }
+
+    private static void validate(String string) {
+        if (!(string.chars().allMatch(Character::isDigit) || string.equals(BLANK_REWARD_STRING))) {
+            throw new IllegalArgumentException("꽝 또는 숫자만 사용할 수 있습니다.");
+        }
+    }
+
+
+}
---
+++ src/test/java/nextstep/refactoring/laddergame/concrete/reward/RewardStringTest.java
@@ -0,0 +1,22 @@
+package nextstep.refactoring.laddergame.concrete.reward;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class RewardStringTest {
+
+    @Test
+    @DisplayName("꽝 또는 숫자가 아니면 예외 처리한다.")
+    void throwExceptionIfStringIsNotNumberOrSpecialBlackString() {
+        assertAll(
+            () -> assertThatThrownBy(() -> RewardString.of("tdd")).isInstanceOf(IllegalArgumentException.class),
+            () -> assertDoesNotThrow(() -> RewardString.of("1000")),
+            () -> assertDoesNotThrow(() -> RewardString.of("꽝"))
+        );
+    }
+    
+}
