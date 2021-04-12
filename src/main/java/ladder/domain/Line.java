9f693bd35a3980c72328cb5a2420dea63fe5a2f8
김우재 <kwj1270@naver.com>
2021-04-12 23:28:08 +0900

    feat:Line 인스턴스 참가자 수로 생성 기능 구현

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -1,6 +1,8 @@
 package ladder.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public final class Line {
 
@@ -8,6 +10,14 @@ public final class Line {
 
     private Line(List<Point> points) {
         this.points = points;
+    }
+
+    public Line(int countPerson) {
+        List<Point> tempPoints = new ArrayList<>();
+        for(int i=0; i < countPerson; i++) {
+            tempPoints.add(Point.of(new Random().nextBoolean()));
+        }
+        this.points = tempPoints;
     }
 
     public static final Line of(List<Point> points) {
