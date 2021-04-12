295f7ab04b1420d507a43b49fd87791e9ebab7a6
Beenie93 <seongbeen93@naver.com>
2021-04-13 01:35:45 +0900

    test: implement a test unit to get integer value of each direction

--- src/test/java/nextstep/ladder/domain/DirectionTest.java
+++ src/test/java/nextstep/ladder/domain/DirectionTest.java
@@ -4,15 +4,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.*;
 
 public class DirectionTest {
+    Direction toLeftDirection;
     Direction toRightDirection;
     Direction noneDirection;
 
     @BeforeEach
     void setUp() {
+        toLeftDirection = new Direction(true, false);
         toRightDirection = Direction.first(() -> true);
         noneDirection = Direction.first(() -> false);
     }
@@ -40,6 +41,8 @@ public class DirectionTest {
     @Test
     @DisplayName("현재 방향 객체 값을 기반으로 다음 방향 객체를 생성한다.")
     public void next() throws Exception {
+        assertThat(toLeftDirection.next(() -> false)).isEqualTo(new Direction(false, false));
+        assertThat(toLeftDirection.next(() -> true)).isEqualTo(new Direction(false, true));
         assertThat(toRightDirection.next()).isEqualTo(new Direction(true, false));
         assertThat(noneDirection.next(() -> true)).isEqualTo(new Direction(false, true));
         assertThat(noneDirection.next(() -> false)).isEqualTo(new Direction(false, false));
@@ -52,5 +55,21 @@ public class DirectionTest {
         Direction leftFalse = noneDirection.last();
         assertThat(leftTrue).isEqualTo(new Direction(true, false));
         assertThat(leftFalse).isEqualTo(new Direction(false, false));
+    }
+
+    @Test
+    @DisplayName("좌,우 둘 중 한 방향이라도 존재한다면 참을 반환한다.")
+    public void hasAnyDirection() throws Exception {
+        assertThat(toLeftDirection.hasAnyDirection()).isTrue();
+        assertThat(toRightDirection.hasAnyDirection()).isTrue();
+        assertThat(noneDirection.hasAnyDirection()).isFalse();
+    }
+
+    @Test
+    @DisplayName("좌, 우 연결 방향에 따른 정수 값을 반환한다.")
+    public void move() throws Exception {
+        assertThat(toLeftDirection.move()).isEqualTo(-1);
+        assertThat(toRightDirection.move()).isEqualTo(1);
+        assertThat(noneDirection.move()).isEqualTo(0);
     }
 }
