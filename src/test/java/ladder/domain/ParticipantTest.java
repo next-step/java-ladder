04a54c9dec90f4d6934f74689cb1efec29db08cc
김우재 <kwj1270@naver.com>
2021-04-12 20:51:36 +0900

    test:Participant 인스턴스의 이름 5자 초과일때 예외발생 테스트작성

--- src/test/java/ladder/domain/ParticipantTest.java
+++ src/test/java/ladder/domain/ParticipantTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParticipantTest {
@@ -19,5 +20,18 @@ class ParticipantTest {
 
         // then
         assertThat(participant).isNotNull();
+    }
+
+    @DisplayName("Participant 인스턴스의 이름 5자 초과일때 예외발생 테스트")
+    @Test
+    void 검증() {
+        // given
+        String name = "woojir";
+
+        // when and then
+        assertThatThrownBy(() -> Participant.of(name))
+                .isInstanceOf(InvalidNameSizeException.class)
+                .hasMessage("이름이 5글자를 초과했습니다.");
+
     }
 }
