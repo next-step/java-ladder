cea0be481b76452bd622a0557859dc4ed72f8ac0
Dion <ksundong@gmail.com>
2021-04-13 00:22:40 +0900

    refactor: 이름 구분자 상수화(리뷰 내용 반영)
    https://github.com/next-step/java-ladder/pull/875#discussion_r611309093

--- src/main/java/nextstep/ladder/io/InputView.java
+++ src/main/java/nextstep/ladder/io/InputView.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public final class InputView {
 
+  public static final String NAME_DELIMITER = ",";
   private static final Scanner SCANNER = new Scanner(System.in);
 
   private InputView() {}
 
   public static String[] inputNames() {
     System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요.)");
-    return SCANNER.nextLine().replaceAll("\\s", "").split(",");
+    return SCANNER.nextLine().replaceAll("\\s", "").split(NAME_DELIMITER);
   }
 
   public static int inputLadderHeight() {
