34ae1114c9dcdaa45052674cc2748186110b4a06
Beenie93 <seongbeen93@naver.com>
2021-04-13 01:11:06 +0900

    feat: implement Direction Object

---
+++ src/main/java/nextstep/ladder/domain/Direction.java
@@ -0,0 +1,66 @@
+package nextstep.ladder.domain;
+
+import nextstep.ladder.strategy.ConnectStrategy;
+import nextstep.ladder.strategy.RandomConnectStrategy;
+
+import java.util.Objects;
+
+public class Direction {
+    public static final String CHECK_CONTIONUOUS = "가로 라인이 겹치는 지 확인해주세요.";
+    private final boolean left;
+    private final boolean right;
+
+    public Direction(boolean left, boolean right) {
+        validateContinuous(left, right);
+        this.left = left;
+        this.right = right;
+    }
+
+    private void validateContinuous(boolean left, boolean right) {
+        if (left & right) {
+            throw new IllegalArgumentException(CHECK_CONTIONUOUS);
+        }
+    }
+
+    public static Direction first(ConnectStrategy connectStrategy) {
+        if (connectStrategy.isConnectable()) {
+            return new Direction(false, true);
+        }
+
+        return new Direction(false, false);
+    }
+
+    public Direction next() {
+        if (right) {
+            return next(() -> false);
+        }
+
+        return next(new RandomConnectStrategy());
+    }
+
+    public Direction next(ConnectStrategy connectStrategy) {
+        if (connectStrategy.isConnectable()) {
+            return new Direction(right, true);
+        }
+
+        return new Direction(right, false);
+    }
+
+    public Direction last() {
+        return new Direction(right, false);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Direction direction = (Direction) o;
+        return left == direction.left &&
+                right == direction.right;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
+    }
+}
