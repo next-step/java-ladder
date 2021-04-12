f66e49896efbfe1ff3878fee1e6db070bf6c8781
김우재 <kwj1270@naver.com>
2021-04-12 20:55:09 +0900

    feat:Participant 인스턴스의 이름 5자 초과일때 예외발생 기능 구현

--- src/main/java/ladder/domain/Participant.java
+++ src/main/java/ladder/domain/Participant.java
@@ -1,11 +1,22 @@
 package ladder.domain;
 
+import ladder.exception.InvalidNameSizeException;
+
 public final class Participant {
+
+    public static final int MAXIMUM_NAME_SIZE = 5;
 
     private final String name;
 
     private Participant(String name) {
+        validateSize(name);
         this.name = name;
+    }
+
+    private final void validateSize(String name) {
+        if(name.length() > MAXIMUM_NAME_SIZE) {
+            throw new InvalidNameSizeException();
+        }
     }
 
     public static final Participant of(String name) {
