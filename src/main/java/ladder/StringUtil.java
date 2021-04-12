f55035a0ad558bd4a832a7e8088726c730b008fb
jaystar13 <astarisk2@gmail.com>
2021-04-12 21:36:24 +0900

    feat: StringUtil 클래스 추가

---
+++ src/main/java/ladder/StringUtil.java
@@ -0,0 +1,15 @@
+package ladder;
+
+public class StringUtil {
+
+    public static String[] split(String inputString, String delimiter) {
+        if (isIllegalArgument(inputString)) {
+            throw new IllegalArgumentException();
+        }
+        return inputString.split(delimiter);
+    }
+
+    private static boolean isIllegalArgument(String inputString) {
+        return inputString == null || "".equals(inputString);
+    }
+}
