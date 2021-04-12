acffc39a4b1e98af695a2eb11634672d80cc3b1f
김우재 <kwj1270@naver.com>
2021-04-12 22:36:15 +0900

    feat:Line 인스턴스 생성 기능 구현

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -1,4 +1,17 @@
 package ladder.domain;
 
-public class Line {
+import java.util.List;
+
+public final class Line {
+
+    private final List<Boolean> points;
+
+    private Line(List<Boolean> points) {
+        this.points = points;
+    }
+
+    public static final Line of(List<Boolean> points) {
+        return new Line(points);
+    }
+
 }
