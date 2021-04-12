eaf64cc154000d00d0baf50eae6f828b8157bee0
lee.jaesung <lee.jaesung@joins.com>
2021-04-12 14:57:21 +0900

    feat: 참가자명 글자수 체크

--- src/main/java/ladder/Player.java
+++ src/main/java/ladder/Player.java
@@ -3,10 +3,19 @@ package ladder;
 import java.util.Objects;
 
 public class Player {
+    public static final int MAX_SIZE = 5;
+
     private final String name;
 
     public Player(String name) {
+        if (name.length() > MAX_SIZE) {
+            throw new IllegalArgumentException(String.format("참가자명은 최대 %d글자까지 입력할 수 있습니다.", MAX_SIZE));
+        }
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 
     @Override
