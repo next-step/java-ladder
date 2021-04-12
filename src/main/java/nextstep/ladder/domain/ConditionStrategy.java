493f8c0f680af9ef157eeb6ee1a272b90ebbfc9f
weleek <weleek@gmail.com>
2021-04-12 22:23:44 +0900

    refactor: 재구현을 위한 기존 코드 삭제

--- src/main/java/nextstep/ladder/domain/ConditionStrategy.java
+++
@@ -1,5 +0,0 @@
-package nextstep.ladder.domain;
-
-public interface ConditionStrategy {
-    boolean randomBoolean();
-}
--- src/main/java/nextstep/ladder/utils/IntUtils.java
+++
@@ -1,9 +0,0 @@
-package nextstep.ladder.utils;
-
-public class IntUtils {
-    private IntUtils(){}
-
-    public static boolean isEvenNumber(int number) {
-        return (number % 2 == 0);
-    }
-}
