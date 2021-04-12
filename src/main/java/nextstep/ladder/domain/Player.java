ff168621f8b96b59d706c5695346ee60c525597a
Dongjoon Park <djoon@users.noreply.github.com>
2021-04-13 00:24:31 +0900

    feat: Player 객체 구현

--- src/main/java/nextstep/ladder/domain/Player.java
+++ src/main/java/nextstep/ladder/domain/Player.java
@@ -4,6 +4,9 @@ public class Player {
     private final String name;
 
     public Player(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("사람 이름은 최대 5글자까지 입력 가능합니다.");
+        }
         this.name = name;
     }
 }
