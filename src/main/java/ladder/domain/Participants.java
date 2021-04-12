05aac3c929d1d087a12bdad6aafc66a2a391852a
김우재 <kwj1270@naver.com>
2021-04-12 22:45:49 +0900

    feat:Participants 인스턴스 생성 기능 구현

--- src/main/java/ladder/domain/Participants.java
+++ src/main/java/ladder/domain/Participants.java
@@ -1,4 +1,30 @@
 package ladder.domain;
 
-public class Participants {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class Participants {
+
+    public static final String REGEX = ",";
+
+    private final List<Participant> Participants;
+
+    private Participants(List<Participant> participants) {
+        Participants = participants;
+    }
+
+    public static final Participants of(String names) {
+        return of(stringToParticipantList(names));
+    }
+
+    public static final Participants of(List<Participant> participants) {
+        return new Participants(participants);
+    }
+
+    private static final List<Participant> stringToParticipantList(String names) {
+        return Arrays.stream(names.split(REGEX))
+                .map(Participant::of)
+                .collect(Collectors.toList());
+    }
 }
