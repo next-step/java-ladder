6c9315d2e3ac3a695daef64f6f8fffdd92d985ea
povia <47127607+povia@users.noreply.github.com>
2021-04-13 02:30:08 +0900

    feat: 입력 InputView 추가

---
+++ src/main/java/step2/App.java
@@ -0,0 +1,6 @@
+package step2;
+
+public class App {
+  public static void main(String... args){
+  }
+}
---
+++ src/main/java/step2/view/InputView.java
@@ -0,0 +1,16 @@
+package step2.view;
+
+import java.util.Scanner;
+
+public class InputView {
+  private static final String INPUT_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  private InputView(){}
+
+  public static String nameInputEvent(){
+    System.out.println(INPUT_MESSAGE);
+    return scanner.nextLine();
+  }
+}
