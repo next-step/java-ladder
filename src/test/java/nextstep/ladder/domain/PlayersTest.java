7fdd513f413089a9c58322a6d88ad7201048bc7d
Beenie93 <seongbeen93@naver.com>
2021-04-12 10:34:58 +0900

    test: implement a test unit to get count of player

--- src/test/java/nextstep/ladder/domain/PlayersTest.java
+++ src/test/java/nextstep/ladder/domain/PlayersTest.java
@@ -8,18 +8,20 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlayersTest {
-    
+
     @Test
     @DisplayName("이름 목록을 입력받아 플레이어 객체들을 생성한다.")
     public void create() throws Exception {
-        //given
         Players players = new Players("pobi", "dobi");
         List<Player> allPlayers = players.allPlayers();
+        assertThat(allPlayers).containsExactly(new Player("pobi"), new Player("dobi"));
+    }
 
-        //when
-        int size = allPlayers.size();
-
-        //then
-        assertThat(size).isEqualTo(2);
+    @Test
+    @DisplayName("플레이어의 수를 반환한다.")
+    public void countOfPlayer() throws Exception {
+        Players players = new Players("pobi", "dobi");
+        int countOfPlayers = players.countOfPlayer();
+        assertThat(countOfPlayers).isEqualTo(2);
     }
 }
