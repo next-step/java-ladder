354d11689cb77eaa77a1307111b0541f5738374e
juniq <juniqlim@gmail.com>
2021-04-13 22:11:41 +0900

    test: 사다리 라인 결과 조회
    선택한 사다리가 라인을 통화하면 몇번째로 변하는지 조회

--- src/test/java/nextstep/ladder/domain/LineTest.java
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -1,7 +1,9 @@
 package nextstep.ladder.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 public class LineTest {
@@ -9,5 +11,11 @@ public class LineTest {
     @Test
     void create() {
         assertThatCode(() -> Line.of(5, new RandomBooleanStrategy())).doesNotThrowAnyException();
+    }
+
+    @Test
+    void nextHorizontalNumber() {
+        Line line = Line.of(Arrays.asList(Point.of(true), Point.of(false)));
+        assertThat(line.nextHorizontalNumber(0)).isEqualTo(1);
     }
 }
