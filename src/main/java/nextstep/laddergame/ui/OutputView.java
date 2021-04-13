c22c8a7ad103e5b4387756e7c624f64853976b26
dailyworker <workingssu@gmail.com>
2021-04-14 00:14:10 +0900

    feat : UI 클래스 추가

---
+++ src/main/java/nextstep/laddergame/ui/OutputView.java
@@ -0,0 +1,11 @@
+package nextstep.laddergame.ui;
+
+import nextstep.laddergame.domain.Ladder;
+
+public class OutputView {
+
+  public void printLadder(Ladder ladder) {
+    System.out.println(ladder.toString());
+  }
+
+}
