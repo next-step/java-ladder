7d1419aa96dcce75eb70d0c7712503f00b7560fb
ad7549 <ad7549@gmail.com>
2021-04-14 06:56:55 +0900

    feat: Add Names

---
+++ src/main/java/ladder/domain/Names.java
@@ -0,0 +1,37 @@
+package ladder.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Names {
+    private static final String DELIMITER = ",";
+
+    private final List<Name> names;
+
+    public Names(String namesInput) {
+        this(
+                Arrays.stream(namesInput.split(DELIMITER))
+                        .map(Name::new)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    public Names(List<Name> names) {
+        this.names = names;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Names names1 = (Names) o;
+        return Objects.equals(names, names1.names);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(names);
+    }
+}
