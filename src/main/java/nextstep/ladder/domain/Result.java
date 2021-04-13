79213cf99624a6c7e15690cecd098189635b44a7
Dion <ksundong@gmail.com>
2021-04-14 07:52:19 +0900

    feat: Result 생성 기능 구현

--- src/main/java/nextstep/ladder/domain/Result.java
+++ src/main/java/nextstep/ladder/domain/Result.java
@@ -1,8 +1,29 @@
 package nextstep.ladder.domain;
 
+import java.util.Objects;
+
 public final class Result {
 
-  public Result(String resultSource) {
+  private final String result;
 
+  public Result(String result) {
+    this.result = result;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Result)) {
+      return false;
+    }
+    Result result1 = (Result) o;
+    return Objects.equals(result, result1.result);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(result);
   }
 }
