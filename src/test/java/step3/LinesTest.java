1d4aa9101fcea7b25c6e97bcd5db43af626375eb
hwangjeonghyeon1 <deert202@naver.com>
2021-04-14 00:54:05 +0900

    [test] 사다라 타기 결과에 필요한 로직 추가 테스트

--- src/test/java/step3/LinesTest.java
+++ src/test/java/step3/LinesTest.java
@@ -1,10 +1,8 @@
-package step2;
+package step3;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import step2.domain.Line;
-import step2.domain.Lines;
-import step2.domain.Point;
+import step3.domain.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,9 +16,8 @@ public class LinesTest {
     @DisplayName("여러 라인을 만드는 테스트")
     void multiLineTest() {
 
-        Lines lines = new Lines(3,3);
+        Lines lines = new Lines(new Height(3),3);
         assertThat(lines.lines().size()).isEqualTo(3);
-
     }
 
     @Test
@@ -32,10 +29,31 @@ public class LinesTest {
         assertThat(lines.eachPositionResult(2)).isEqualTo(1);
     }
 
+    @Test
+    @DisplayName("한 사람의 결과 반환 테스트")
+    void onePersonResultTest() {
+        Lines lines = new Lines(createLine());
+        ExecutionResults results = new ExecutionResults(Arrays.asList(new ExecutionResult("꽝"), new ExecutionResult("5000"), new ExecutionResult("3000")));
+        assertThat(lines.eachPositionResult2(0, results)).isEqualTo(results.oneResult(2));
+        assertThat(lines.eachPositionResult2(1, results)).isEqualTo(results.oneResult(0));
+        assertThat(lines.eachPositionResult2(2, results)).isEqualTo(results.oneResult(1));
+    }
+
+    @Test
+    @DisplayName("모든 사람의 결과 반환 테스트")
+    void allPersonResultTest() {
+        Lines lines = new Lines(createLine());
+        Persons persons = new Persons(Arrays.asList(new Person("aa"), new Person("bb"), new Person("cc")));
+        ExecutionResults results = new ExecutionResults(Arrays.asList(new ExecutionResult("꽝"), new ExecutionResult("5000"), new ExecutionResult("3000")));
+        Result result = lines.result(persons, results);
+        assertThat(result.get(new Person("aa"))).isEqualTo(new ExecutionResult("3000"));
+        assertThat(lines.eachPositionResult2(0, results)).isEqualTo(results.oneResult(2));
+    }
+
     public List<Line> createLine() {
         List<Line> lines = new ArrayList<>();
         lines.add(new Line(Arrays.asList(Point.first(true), Point.first(true).next(false), Point.first(true).next(false).last())));
-        lines.add(new Line(Arrays.asList(Point.first(false), Point.first(false).next(false), Point.first(true).next(false).last())));
+        lines.add(new Line(Arrays.asList(Point.first(false), Point.first(false).next(false), Point.first(false).next(false).last())));
         lines.add(new Line(Arrays.asList(Point.first(false), Point.first(false).next(true), Point.first(false).next(true).last())));
         return lines;
     }
