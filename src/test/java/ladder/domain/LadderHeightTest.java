f99a79906e20dc8056ae4f587beaf3aff1f089e6
김우재 <kwj1270@naver.com>
2021-04-13 20:36:53 +0900

    test:LadderHeight 인스턴스 음수 입력시 예외처리 테스트 작성

--- src/test/java/ladder/domain/LadderHeightTest.java
+++ src/test/java/ladder/domain/LadderHeightTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LadderHeightTest {
@@ -19,5 +20,18 @@ class LadderHeightTest {
 
         // then
         assertThat(ladderHeight).isNotNull();
+    }
+
+    @DisplayName("LadderHeight 인스턴스 음수 입력시 예외처리 테스트")
+    @Test
+    void 검증_음수() {
+        // given
+        int height = -1;
+
+        // when
+        assertThatThrownBy(()->LadderHeight.valueOf(5))
+                .isInstanceOf(InputNegativeNumberException.class)
+                .hasMessage("음수의 값이 입력되었습니다.");
+
     }
 }
