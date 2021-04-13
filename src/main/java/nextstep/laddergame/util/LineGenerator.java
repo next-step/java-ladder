067a3d623406aef0654926a1c1354a049e991be9
dailyworker <workingssu@gmail.com>
2021-04-14 00:10:35 +0900

    fix : 누락된 LineGenerator 추가

---
+++ src/main/java/nextstep/laddergame/util/LineGenerator.java
@@ -0,0 +1,40 @@
+package nextstep.laddergame.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class LineGenerator extends Random {
+
+  private static final Random RANDOM = new Random();
+
+  private LineGenerator() {
+
+  }
+
+  public static List<Boolean> createLine(int countOfPerson, DrawStrategy strategy) {
+    List<Boolean> points = new ArrayList<>();
+
+    IntStream.range(0, countOfPerson)
+        .mapToObj(index -> drawLine(isRowContinuousTrue(points, index), strategy))
+        .forEach(points::add);
+
+    return points;
+  }
+
+  public static List<Boolean> generate(int countOfPerson) {
+    return createLine(countOfPerson, RANDOM::nextBoolean);
+  }
+
+  private static boolean isRowContinuousTrue(List<Boolean> points, int index) {
+    return index != 0 && points.get(index - 1);
+  }
+
+  private static boolean drawLine(Boolean isRowContinuous, DrawStrategy strategy) {
+    if (isRowContinuous) {
+      return false;
+    }
+    return strategy.isLine();
+  }
+}
