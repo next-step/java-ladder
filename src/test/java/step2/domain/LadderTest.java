144bfdd530806900cf442fc4383f925db39b053c
w-beom <w_bum@naver.com>
2021-04-13 20:58:31 +0900

    test : 테스트코드 작성

--- src/test/java/step2/domain/LadderTest.java
+++ src/test/java/step2/domain/LadderTest.java
@@ -12,23 +12,23 @@ class LadderTest {
     @Test
     @DisplayName("사다리 높이 테스트")
     void getLadderRowSizeTest() {
-        Ladder ladder = Ladder.of(3, 5);
+        Ladder ladder = Ladder.of(3, 5, new FixedGenerator(true));
         assertThat(ladder.getLadderRowSize()).isEqualTo(5);
     }
 
     @Test
     @DisplayName("사다리 최소 높이 테스트")
-    void ladderMinHeightTest(){
+    void ladderMinHeightTest() {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> Ladder.of(3,0))
+                .isThrownBy(() -> Ladder.of(3, 0, new FixedGenerator(true)))
                 .withMessage("사다리 높이는 1이상 이여야 합니다.");
     }
 
     @Test
     @DisplayName("최소 인원수 테스트")
-    void memberMinCountTest(){
+    void memberMinCountTest() {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> Ladder.of(1,5))
+                .isThrownBy(() -> Ladder.of(1, 5, new FixedGenerator(true)))
                 .withMessage("두 명 이상의 사용자가 참여해야 합니다.");
     }
 }
