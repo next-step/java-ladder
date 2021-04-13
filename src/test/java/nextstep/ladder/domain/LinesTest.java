7fc10c3fafd2093b15e00b7f509a68a646654077
Beenie93 <seongbeen93@naver.com>
2021-04-13 21:09:11 +0900

    test: implement a test code to create lines

---
+++ src/test/java/nextstep/ladder/domain/LinesTest.java
@@ -0,0 +1,23 @@
+package nextstep.ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class LinesTest {
+
+    @Test
+    @DisplayName("사다리 높이를 인수로 해당 수 만큼의 라인을 생성한다.")
+    public void create() throws Exception {
+        //given
+        Lines lines = Lines.createWith(new CountOfPlayer(4), new Height(5));
+
+        //when
+        List<Line> result = lines.lines();
+
+        then(result).hasSize(5);
+    }
+}
