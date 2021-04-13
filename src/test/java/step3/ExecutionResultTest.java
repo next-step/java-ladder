95da0e7fbdb6b4ec3488d4b03a794faf7bb49688
hwangjeonghyeon1 <deert202@naver.com>
2021-04-14 00:53:28 +0900

    [test] 추가된 실행결과 클래스 테스트

--- src/test/java/step3/ExecutionResultTest.java
+++ src/test/java/step3/ExecutionResultTest.java
@@ -2,16 +2,16 @@ package step3;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import step3.domain.Result;
+import step3.domain.ExecutionResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ResultTest {
+public class ExecutionResultTest {
 
     @Test
     @DisplayName("Result 생성 테스트")
     void resultTest() {
-        Result result = new Result("꽝");
-        assertThat(result).isEqualTo(new Result("꽝"));
+        ExecutionResult result = new ExecutionResult("꽝");
+        assertThat(result).isEqualTo(new ExecutionResult("꽝"));
     }
 }
--- src/test/java/step3/ExecutionResultsTest.java
+++ src/test/java/step3/ExecutionResultsTest.java
@@ -2,34 +2,33 @@ package step3;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import step3.domain.Result;
-import step3.domain.Results;
+import step3.domain.ExecutionResult;
+import step3.domain.ExecutionResults;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ResultsTest {
+public class ExecutionResultsTest {
 
     @Test
     @DisplayName("Results 생성 테스트")
     void resultsTest() {
-        assertThat(new Results()).isEqualTo(new Results());
+        assertThat(new ExecutionResults()).isEqualTo(new ExecutionResults());
+    }
+
+    @Test
+    @DisplayName("Result input 값 리스트 변경 테스트")
+    void stringToList() {
+        ExecutionResults results = new ExecutionResults("꽝,5000");
+        assertThat(results).isEqualTo(new ExecutionResults(Arrays.asList(new ExecutionResult("꽝"), new ExecutionResult("5000"))));
     }
 
     @Test
     @DisplayName("포지션에 따른 결과 값 반환")
     void returnOfOnePosition() {
-        Results results = new Results(Arrays.asList(new Result("꽝"), new Result("5000")));
-        assertThat(results.oneResult(1)).isEqualTo(new Result("5000"));
+        ExecutionResults results = new ExecutionResults(Arrays.asList(new ExecutionResult("꽝"), new ExecutionResult("5000")));
+        assertThat(results.oneResult(1)).isEqualTo(new ExecutionResult("5000"));
     }
-
-    @Test
-    @DisplayName("포지션에 따른 결과 값 반환")
-    void returnOfAllPosition() {
-        Results results = new Results(Arrays.asList(new Result("꽝"), new Result("5000")));
-        assertThat(results.allResult(results)).isEqualTo(new Results(Arrays.asList(new Result("꽝"), new Result("5000"))));
-    }
-
 
 }
