a744783346ef0f6ec70671827d256b0e69acb07a
Beenie93 <seongbeen93@naver.com>
2021-04-15 00:33:48 +0900

    feat: implement Prizes Object

--- src/main/java/nextstep/ladder/controller/LadderController.java
+++ src/main/java/nextstep/ladder/controller/LadderController.java
@@ -18,11 +18,14 @@ public class LadderController {
 
     public void run() {
         Players players = new Players(inputView.players());
+        Prizes prizes = new Prizes(players.countOfPlayer(), inputView.prizes());
         Height height = new Height(inputView.height());
         AllPointsForLines allPointsForLines = new AllPointsForLines(new RandomDirectionStrategy());
         List<Points> allPoints = allPointsForLines.allPoints(players.countOfPlayer(), height);
         Lines lines = Lines.from(allPoints);
         Ladder ladder = new Ladder(lines);
-        resultView.printResult(players.names(), ladder.linesConnection());
+        resultView.printResultPhrase();
+        resultView.printNames(players.names());
+        resultView.printLadder(ladder.linesConnection());
     }
 }
