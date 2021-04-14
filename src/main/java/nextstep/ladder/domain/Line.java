27313fda4943cac6e66c59565f50a335c90fd3de
Beenie93 <seongbeen93@naver.com>
2021-04-15 01:39:15 +0900

    feat: implement methods to get next line's point and check if it is the last one

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -1,13 +1,28 @@
 package nextstep.ladder.domain;
 
 public class Line {
+    private final int index;
     private final Points points;
 
-    public Line(Points points) {
+    public Line(int index, Points points) {
+        this.index = index;
         this.points = points;
     }
 
     public Points points() {
         return points;
+    }
+
+    public int index() {
+        return index;
+    }
+
+    public boolean isNotLast(int totalSize) {
+        return index != totalSize - 1;
+    }
+
+    public int nextPointIndexFrom(int pointIndex) {
+        Point point = points.findByIndex(pointIndex);
+        return point.move();
     }
 }
