e767db74a5f25a602e28b712c85c9289123b5f49
Donghui-Lee <ehdgml3206@naver.com>
2021-04-15 02:18:01 +0900

    test: 유효성 체크 추가

--- src/test/java/step4/domain/LadderLineTest.java
+++ src/test/java/step4/domain/LadderLineTest.java
@@ -2,12 +2,33 @@ package step4.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
 public class LadderLineTest {
+
+    @Test
+    public void validate() {
+        // given
+        int countOfPerson = 1;
+        // when & then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> LadderLine.init(countOfPerson));
+    }
+
+    @Test
+    public void validate_2() {
+        // given
+        Point first = Point.first(true);
+        List<Point> points = Arrays.asList(first);
+        // when & then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new LadderLine(points));
+    }
+
     @Test
     public void move() {
         // given
