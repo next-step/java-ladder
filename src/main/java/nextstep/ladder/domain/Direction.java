e5fb2ae6536ca0f80d4c19766e5e44a7e99a8fe3
PandaHun <gnsgnsek@gmail.com>
2021-04-14 20:28:46 +0900

    feat: Create Direction feature

--- src/main/java/nextstep/ladder/domain/Direction.java
+++ src/main/java/nextstep/ladder/domain/Direction.java
@@ -1,11 +1,21 @@
 package nextstep.ladder.domain;
 
+import java.util.Objects;
+
+import static java.lang.Boolean.FALSE;
+import static nextstep.ladder.domain.PointGenerator.generatePoint;
+
 public class Direction {
+
+    private static final String INVALID_DIRECTION_MESSAGE = "양쪽 모두 연결 될 수 없습니다";
 
     private final boolean left;
     private final boolean right;
 
     private Direction(boolean left, boolean right) {
+        if (left && right) {
+            throw new IllegalStateException(INVALID_DIRECTION_MESSAGE);
+        }
         this.left = left;
         this.right = right;
     }
@@ -14,23 +24,43 @@ public class Direction {
         return new Direction(left, right);
     }
 
-    public static Direction first(boolean left) {
-        return new Direction(left, !left);
+    public static Direction first(boolean right) {
+        return new Direction(FALSE, right);
     }
 
     public Direction next() {
-        return this;
+        if (this.right) {
+            return next(FALSE);
+        }
+        return next(generatePoint());
     }
 
-    public Direction next(boolean b) {
-        return this;
+    public Direction next(boolean nextRight) {
+        return of(this.right, nextRight);
+    }
+
+    public boolean isRight() {
+        return this.right;
     }
 
     public boolean isLeft() {
-        return true;
+        return this.left;
     }
 
     public Direction last() {
-        return this;
+        return of(this.right, FALSE);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Direction direction = (Direction) o;
+        return left == direction.left && right == direction.right;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
     }
 }
--- src/test/java/nextstep/ladder/domain/DirectionTest.java
+++ src/test/java/nextstep/ladder/domain/DirectionTest.java
@@ -3,6 +3,8 @@ package nextstep.ladder.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -11,33 +13,33 @@ public class DirectionTest {
     @DisplayName("방향을 생성한다")
     @Test
     void initTest() {
-        assertThat(Direction.of(true, false)).isEqualTo(Direction.of(true, false));
+        assertThat(Direction.of(TRUE, FALSE)).isEqualTo(Direction.of(TRUE, FALSE));
     }
 
     @DisplayName("양방향일 경우 에러를 반환한다")
     @Test
     public void initInvalidTest() {
-        assertThatThrownBy(() -> Direction.of(true, true)).isInstanceOf(IllegalAccessException.class);
+        assertThatThrownBy(() -> Direction.of(TRUE, TRUE)).isInstanceOf(IllegalStateException.class);
     }
 
     @DisplayName("연결한 방향의 다음을 자동으로 생성한다")
     @Test
     public void nextDirectionTest() {
-        Direction next = Direction.first(true).next();
-        assertThat(next).isEqualTo(Direction.of(true, false));
+        Direction next = Direction.first(TRUE).next();
+        assertThat(next).isEqualTo(Direction.of(TRUE, FALSE));
     }
 
     @DisplayName("첫번째 지점의 방향을 생성한다")
     @Test
     public void firstTest() {
-        Direction first = Direction.first(true);
-        assertThat(first.isLeft()).isEqualTo(false);
+        Direction first = Direction.first(TRUE);
+        assertThat(first.isLeft()).isEqualTo(FALSE);
     }
 
     @DisplayName("마지막 지점의 방향을 생성한다")
     @Test
     public void last() {
-        Direction last = Direction.first(true).last();
-        assertThat(last).isEqualTo(Direction.of(true, false));
+        Direction last = Direction.first(TRUE).last();
+        assertThat(last).isEqualTo(Direction.of(TRUE, FALSE));
     }
 }
