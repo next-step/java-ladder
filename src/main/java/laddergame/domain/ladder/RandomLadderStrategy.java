792b02f57b53b11088b0e753620d38d7b0de8002
HJ-Woo <hjwoou@gmail.com>
2021-04-15 00:06:51 +0900

    refactor: random을 객체생성시에 한번에 생성

--- src/main/java/laddergame/domain/ladder/RandomLadderStrategy.java
+++ src/main/java/laddergame/domain/ladder/RandomLadderStrategy.java
@@ -3,9 +3,9 @@ package laddergame.domain.ladder;
 import java.util.Random;
 
 public class RandomLadderStrategy implements LadderStrategy {
+    private final Random random = new Random();
     @Override
     public boolean test() {
-        Random random = new Random();
         return random.nextBoolean();
     }
 }
