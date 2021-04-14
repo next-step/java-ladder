8a84c9a7d26827dddf1f3c35339d65c52e042c90
PandaHun <gnsgnsek@gmail.com>
2021-04-14 20:35:48 +0900

    test: Modify Point test

--- src/test/java/nextstep/ladder/domain/PointTest.java
+++ src/test/java/nextstep/ladder/domain/PointTest.java
@@ -1,5 +1,6 @@
 package nextstep.ladder.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.Boolean.FALSE;
@@ -7,33 +8,39 @@ import static java.lang.Boolean.TRUE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PointTest {
+
+    @DisplayName("첫번째 포인트를 생성한다")
     @Test
-    public void first() {
+    public void firstInitTest() {
         assertThat(Point.first(TRUE).move()).isEqualTo(0);
         assertThat(Point.first(FALSE).move()).isEqualTo(1);
     }
 
+    @DisplayName("다음칸으로 이동하지 않는 포인트를 생성한다")
     @Test
-    public void next_stay() {
+    public void nextStayTest() {
         Point second = Point.first(FALSE).next(FALSE);
-        assertThat(second.move()).isEqualTo(0);
+        assertThat(second.move()).isEqualTo(1);
     }
 
+    @DisplayName("왼쪽으로 이동하는 포인트를 생성한다")
     @Test
-    public void next_left() {
+    public void nextLeftTest() {
         Point second = Point.first(TRUE).next(FALSE);
         assertThat(second.move()).isEqualTo(0);
     }
 
+    @DisplayName("오른쪽으로 이동하는 포인트를 생성한다")
     @Test
-    public void next_right() {
+    public void nextRightTest() {
         Point second = Point.first(FALSE).next(TRUE);
-        assertThat(second.move()).isEqualTo(1);
+        assertThat(second.move()).isEqualTo(2);
     }
 
+    @DisplayName("왼쪽이 연결되어있다면, 왼쪽으로 연결된다")
     @Test
     public void next() {
         Point second = Point.first(TRUE).next();
-        assertThat(second.move()).isEqualTo(2);
+        assertThat(second.move()).isEqualTo(0);
     }
 }
