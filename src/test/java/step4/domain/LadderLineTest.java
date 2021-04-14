c2c146c708ab6b2ca5bb009883dc4d2801496aa9
Donghui-Lee <ehdgml3206@naver.com>
2021-04-14 09:59:53 +0900

    refactor: LadderLine 테스트 코드 수정

--- src/test/java/step4/domain/LadderLineTest.java
+++ src/test/java/step4/domain/LadderLineTest.java
@@ -1,17 +1,24 @@
 package step4.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 public class LadderLineTest {
     @Test
-    public void init() {
-        int sizeOfPerson = 5;
-        System.out.println(LadderLine.init(sizeOfPerson));
-    }
-
-    @Test
     public void move() {
-        LadderLine line = LadderLine.init(2);
-        System.out.println("ladder result : " + line.move(0));
+        // given
+        Point first = Point.first(true);
+        Point next = first.next(false);
+        Point last = next.next(false);
+        List<Point> points = Arrays.asList(first, next, last);
+        LadderLine ladderLine = new LadderLine(points);
+        // when & then
+        assertThat(ladderLine.move(0)).isEqualTo(1);
+        assertThat(ladderLine.move(1)).isEqualTo(0);
+        assertThat(ladderLine.move(2)).isEqualTo(2);
     }
 }
