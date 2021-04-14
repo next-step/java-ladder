c53a340e74fa8106544a311117c5f36e0ddbf84f
cptbong <cptbbong@gmail.com>
2021-04-14 20:29:47 +0900

    refactor - anyMatch사용.

--- src/main/java/nextstep/model/Players.java
+++ src/main/java/nextstep/model/Players.java
@@ -3,7 +3,6 @@ package nextstep.model;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Players {
@@ -11,11 +10,10 @@ public class Players {
 
     public Players(String[] names) {
         this.names = new ArrayList<>();
-        Optional<String> first = Arrays.stream(names)
-                .filter(s -> s.length() > 5)
-                .findFirst();
-        if (first.isPresent()) {
-            throw new IllegalArgumentException("사람의 이름은 최대5글자까지 부여할 수 있습니다 : " + first.get());
+        boolean match = Arrays.stream(names)
+                .anyMatch(s -> s.length() > 5);
+        if (match) {
+            throw new IllegalArgumentException("사람의 이름은 최대5글자까지 부여할 수 있습니다.");
         }
         for (String name : names) {
             this.names.add(name);
