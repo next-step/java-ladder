75b732167137d1e94ae6a8273ef0eb7f3356cae6
dailyworker <workingssu@gmail.com>
2021-04-15 00:14:15 +0900

    refactor : validate 처리 및 hash() & eqauls() 구현 - Members의 distinct 처리를 위하여 hash() & equals() 구현 - 생성자에 validate처리

--- src/main/java/nextstep/laddergame/domain/Member.java
+++ src/main/java/nextstep/laddergame/domain/Member.java
@@ -1,15 +1,45 @@
 package nextstep.laddergame.domain;
 
+import java.util.Objects;
+
 public class Member {
 
   private final String name;
 
   public Member(String name) {
+    validate(name);
     this.name = name;
+  }
+
+  private static void validate(String memberName) {
+    hasNull(memberName);
+  }
+
+  private static void hasNull(String memberName) {
+    if(memberName == null || memberName.trim().length() == 0) {
+      throw new IllegalArgumentException("ERROR : 이름에는 Null을 포함할 수 없습니다.");
+    }
   }
 
   @Override
   public String toString() {
     return name + "\t";
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Member)) {
+      return false;
+    }
+    Member member = (Member) o;
+    return Objects.equals(name, member.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
   }
 }
