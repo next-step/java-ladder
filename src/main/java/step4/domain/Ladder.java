e929c15f68f7b7e1c53f553e89f16dc1ec098659
Donghui-Lee <ehdgml3206@naver.com>
2021-04-14 16:47:36 +0900

    feat: play 기능 구현

--- src/main/java/step4/domain/Ladder.java
+++ src/main/java/step4/domain/Ladder.java
@@ -4,27 +4,45 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
-import ladder.exception.MinimumLadderHeightException;
+import step4.exception.MinimumLadderHeightException;
 
 public class Ladder {
     private final static int MIN_HEIGHT = 1;
 
-    private final List<LadderLine> lines = new ArrayList<>();
+    private final List<LadderLine> lines;
 
-    public Ladder(int countOfPerson, int height) {
-        validate(height);
-        create(countOfPerson, height);
+    private Ladder(List<LadderLine> lines) {
+        this.lines = lines;
     }
 
-    private void validate(int height) {
+    public static Ladder of(List<LadderLine> lines) {
+        validate(lines.size());
+        return new Ladder(lines);
+    }
+
+    public static Ladder of(int countOfPerson, int height) {
+        validate(height);
+        return new Ladder(create(countOfPerson, height));
+    }
+
+    private static void validate(int height) {
         if (height < MIN_HEIGHT) {
             throw new MinimumLadderHeightException("사디리 높이는 최소 1이상이여야 합니다.");
         }
     }
 
-    private void create(int countOfPerson, int height) {
+    private static List<LadderLine> create(int countOfPerson, int height) {
+        List<LadderLine> lines = new ArrayList<>();
         IntStream.range(0, height)
             .forEach(index -> lines.add(LadderLine.init(countOfPerson)));
+        return lines;
+    }
+
+    public int play(int position) {
+        for (LadderLine line : lines) {
+            position = line.move(position);
+        }
+        return position;
     }
 
     public int ladderHeight() {
--- src/test/java/step4/domain/LadderTest.java
+++ src/test/java/step4/domain/LadderTest.java
@@ -1,6 +1,9 @@
 package step4.domain;
 
+import static java.lang.Boolean.*;
 import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,7 +15,7 @@ public class LadderTest {
         // given
         int height = 5;
         int countOfPerson = 5;
-        Ladder ladder = new Ladder(countOfPerson, height);
+        Ladder ladder = Ladder.of(countOfPerson, height);
         // when & then
         assertThat(ladder.ladderHeight()).isEqualTo(height);
     }
@@ -23,6 +26,35 @@ public class LadderTest {
         int height = 0;
         int countOfPerson = 5;
         // when & then
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new Ladder(countOfPerson, height));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Ladder.of(countOfPerson, height));
+    }
+
+    @Test
+    void play() {
+        // given
+        LadderLine first = new LadderLine(
+            Arrays.asList(left(0), middle(1), right(2)));
+        LadderLine second = new LadderLine(
+            Arrays.asList(left(0), middle(1), right(2)));
+        LadderLine third = new LadderLine(
+            Arrays.asList(left(0), middle(1), right(2)));
+        // when
+        Ladder ladder = Ladder.of(Arrays.asList(first, second, third));
+        // then
+        assertThat(ladder.play(0)).isEqualTo(1);
+        assertThat(ladder.play(1)).isEqualTo(0);
+        assertThat(ladder.play(2)).isEqualTo(2);
+    }
+
+    private Point left(int position) {
+        return new Point(position, Direction.of(FALSE, TRUE));
+    }
+
+    private Point middle(int position) {
+        return new Point(position, Direction.of(TRUE, FALSE));
+    }
+
+    private Point right(int position) {
+        return new Point(position, Direction.of(FALSE, FALSE));
     }
 }
