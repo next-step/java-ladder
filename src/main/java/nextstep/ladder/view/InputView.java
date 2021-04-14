bb17e37f97df56958d8d0fe2e745e54b19348a86
Beenie93 <seongbeen93@naver.com>
2021-04-15 01:38:58 +0900

    test: implement test units to get next line's point and check if it is the last one

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -8,6 +8,7 @@ public class InputView {
     private static final String INPUT_PLAYERS = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String INPUT_PRIZES = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String INPUT_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
+    private static final String INPUT_TARGET_PLAYER = "결과를 보고 싶은 사람은?";
 
     private final Scanner scanner;
 
@@ -36,5 +37,13 @@ public class InputView {
         String prizes = scanner.nextLine().trim();
         PreCondition.checkNullOrEmpty(prizes);
         return prizes;
+    }
+
+    public String targetPlayer() {
+        System.out.println();
+        System.out.println(INPUT_TARGET_PLAYER);
+        String player = scanner.nextLine().trim();
+        PreCondition.checkNullOrEmpty(player);
+        return player;
     }
 }
