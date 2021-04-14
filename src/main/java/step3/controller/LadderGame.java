19999aa70add15fafec2fab5d4a3e2f6c19c163b
hwangjeonghyeon1 <deert202@naver.com>
2021-04-14 22:17:20 +0900

    [refactor] method 명 수정

--- src/main/java/step3/controller/LadderGame.java
+++ src/main/java/step3/controller/LadderGame.java
@@ -1,7 +1,7 @@
 package step3.controller;
 
 
-import step3.Result;
+import step3.domain.Result;
 import step3.domain.*;
 import step3.view.InputView;
 import step3.view.ResultView;
@@ -10,7 +10,7 @@ public class LadderGame {
     InputView inputView = new InputView();
     ResultView resultView = new ResultView();
 
-    public void game() {
+    public void start() {
         String participants = inputView.inputParticipant();
         Persons persons = new Persons(participants);
         ExecutionResults executionResults = new ExecutionResults(inputView.inputExecutionResult());
@@ -19,17 +19,22 @@ public class LadderGame {
         resultView.printPerson(persons);
         Lines lines = new Lines(new Height(height), persons.personList().size());
         resultView.printLadder(lines);
-        Result result = lines.result(persons, executionResults);
-        resultView.executionResultLine(result);
 
+        Result result = new Result(lines, executionResults);
+        resultView.executionResultLine(executionResults);
+        
         while (true) {
             String resultName = inputView.inputPerson();
-            Person person = new Person(resultName);
-            if (resultName.equals("all")) {
-                resultView.printAllExecutionResult(result);
-                System.exit(0);
-            }
-            resultView.printExecutionResult(result, person);
+            int personIndex = persons.getPersonIndex(new Person(resultName));
+            checkInputAll(persons, result, resultName);
+            resultView.printExecutionResult(result, personIndex);
+        }
+    }
+
+    private void checkInputAll(Persons persons, Result result, String resultName) {
+        if (resultName.equals("all")) {
+            resultView.printAllExecutionResult(result, persons);
+            System.exit(0);
         }
     }
 }
