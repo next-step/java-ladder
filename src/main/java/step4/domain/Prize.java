cebb8628d104af17892d229bd9202d753496a482
Donghui-Lee <ehdgml3206@naver.com>
2021-04-14 19:10:23 +0900

    refactor: 테스트 성공을 위한 코드 수정

--- src/main/java/step4/domain/Prize.java
+++ src/main/java/step4/domain/Prize.java
@@ -1,11 +1,37 @@
 package step4.domain;
 
+import java.util.Objects;
+
+import step4.exception.MinimumNameLengthException;
+
 public class Prize {
     private final Position position;
     private final String prize;
 
     public Prize(int position, String prize) {
         this.position = Position.valueOf(position);
-        this.prize = prize;
+        this.prize = validate(prize.trim());
+    }
+
+    private String validate(String prize) {
+        if (prize == null || prize.isEmpty()) {
+            throw new MinimumNameLengthException("유효하지 않은 상품입니다.");
+        }
+        return prize;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Prize prize1 = (Prize)o;
+        return Objects.equals(position, prize1.position) && Objects.equals(prize, prize1.prize);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, prize);
     }
 }
