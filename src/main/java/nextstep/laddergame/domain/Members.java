7a08f23fa266012d86f9c61aa08d51573da838d5
dailyworker <workingssu@gmail.com>
2021-04-15 00:13:15 +0900

    refactor : validate 처리 - 생성자에서 입력받은 이름이 중복검증 처리 - 기존 중복 값 카운트 로직 -> duplicationCount() 메소드로 분할

--- src/main/java/nextstep/laddergame/domain/Members.java
+++ src/main/java/nextstep/laddergame/domain/Members.java
@@ -2,7 +2,6 @@ package nextstep.laddergame.domain;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Members {
@@ -10,43 +9,31 @@ public class Members {
   private final List<Member> members;
 
   public Members(List<Member> members) {
+    hasDuplication(members);
     this.members = members;
   }
 
-  public static Members createMembers(String[] userNames) {
-    validate(userNames);
-    return new Members(Arrays.stream(userNames)
+  public static Members createMembers(String[] memberNames) {
+    return new Members(Arrays.stream(memberNames)
         .map(Member::new)
         .collect(Collectors.toList()));
   }
 
-  private static void validate(String[] userNames) {
-    hasNull(userNames);
-    hasDuplication(userNames);
-  }
-
-  private static void hasNull(String[] userNames) {
-    long count = Arrays.stream(userNames)
-        .filter(String::isEmpty)
-        .count();
-
-    if(count >= 1) {
-      throw new IllegalArgumentException("ERROR : 이름에는 Null을 포함할 수 없습니다.");
-    }
-  }
-
-  private static void hasDuplication(String[] userNames) {
-    long count = Arrays.stream(userNames)
-        .distinct()
-        .count();
-
-    if(count != userNames.length) {
+  private void hasDuplication(List<Member> members) {
+    if(duplicationCount(members) != members.size()) {
       throw new IllegalArgumentException("ERROR : 이름을 중복해서 넣을 수 없습니다.");
     }
   }
 
   public int size() {
     return members.size();
+  }
+
+  private long duplicationCount(List<Member> members) {
+    return members
+        .stream()
+        .distinct()
+        .count();
   }
 
   @Override
