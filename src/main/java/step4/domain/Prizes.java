762114d781b2499e047cfeba768022236aedf722
Donghui-Lee <ehdgml3206@naver.com>
2021-04-14 19:13:33 +0900

    test: Prize 테스트 코드 추가

---
+++ src/main/java/step4/domain/Prizes.java
@@ -0,0 +1,19 @@
+package step4.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prizes {
+    private final List<Prize> prizes = new ArrayList<>();
+
+    public Prizes(String stringPrizes, int countOfPerson) {
+    }
+
+    public List<ladder.domain.Prize> getPrizes() {
+        return null;
+    }
+
+    public Prize matchPosition(Position number) {
+        return null;
+    }
+}
---
+++ src/test/java/step4/domain/PrizesTest.java
@@ -0,0 +1,43 @@
+package step4.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PrizesTest {
+    @Test
+    void 생성_테스트() {
+        // given
+        String test = "꽝,500, 꽝, 3000";
+        Prizes prizes = new Prizes(test, 4);
+        // when & then
+        assertThat(prizes.getPrizes().size()).isEqualTo(4);
+    }
+
+    @Test
+    void 상품수_플레이어수_비교_테스트() {
+        // given
+        String test = "꽝,500, 꽝, 3000";
+        // when & then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Prizes(test, 3));
+    }
+
+    @Test
+    void 결과_매칭_테스트() {
+        // given
+        String test = "꽝,500, 꽝, 3000";
+        Prizes prizes = new Prizes(test, 4);
+        // when & then
+        assertThat(prizes.matchPosition(Position.valueOf(1))).isEqualTo(new Prize(1, "500"));
+    }
+
+    @Test
+    void 존재하지않는_상품_테스트() {
+        // given
+        String test = "꽝,500, 꽝, 3000";
+        Prizes prizes = new Prizes(test, 4);
+        // when & then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> prizes.matchPosition(Position.valueOf(5)));
+    }
+}
