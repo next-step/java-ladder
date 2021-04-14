82e221d7029ae46724af7c4ed8ba7bb10645a3ac
Donghui-Lee <ehdgml3206@naver.com>
2021-04-14 14:22:15 +0900

    refactor: 테스트 성공을 위한 코드 수정

--- src/main/java/step4/domain/Position.java
+++ src/main/java/step4/domain/Position.java
@@ -1,13 +1,39 @@
 package step4.domain;
 
+import java.util.Objects;
+
+import step4.exception.NaturalNumberException;
+
 public class Position {
     private final int position;
 
     private Position(int position) {
+        validate(position);
         this.position = position;
     }
 
-    public static Position valueOf(int number) {
-        return new Position(number);
+    public static Position valueOf(int position) {
+        return new Position(position);
+    }
+
+    private void validate(int position) {
+        if (position < 0) {
+            throw new NaturalNumberException("유효하지 않은 라인 번호 입니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Position position1 = (Position)o;
+        return position == position1.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
     }
 }
--- src/test/java/step4/domain/PositionTest.java
+++ src/test/java/step4/domain/PositionTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import step4.exception.NaturalNumberException;
+
 public class PositionTest {
     @Test
     void 생성_테스트() {
@@ -14,6 +16,6 @@ public class PositionTest {
 
     @Test
     void 유효성_테스트() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> Position.valueOf(-1));
+        Assertions.assertThrows(NaturalNumberException.class, () -> Position.valueOf(-1));
     }
 }
