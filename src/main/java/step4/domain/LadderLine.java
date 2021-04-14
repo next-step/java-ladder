7b1fe01c4dd8c2a82f1318f47fc371d28d2736bb
Donghui-Lee <ehdgml3206@naver.com>
2021-04-14 09:55:23 +0900

    refactor: 테스트 성공을 위한 코드 수정

--- src/main/java/step4/domain/LadderLine.java
+++ src/main/java/step4/domain/LadderLine.java
@@ -1,5 +1,7 @@
 package step4.domain;
 
+import static step4.util.LadderPointGenerator.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,11 +13,40 @@ public class LadderLine {
     }
 
     public int move(int position) {
-        return 0;
+        return points.get(position).move();
     }
 
     public static LadderLine init(int sizeOfPerson) {
         List<Point> points = new ArrayList<>();
+        Point point = initFirst(points);
+        point = initBody(sizeOfPerson, points, point);
+        initLast(points, point);
         return new LadderLine(points);
+    }
+
+    private static Point initBody(int sizeOfPerson, List<Point> points, Point point) {
+        for (int i = 1; i < sizeOfPerson - 1; i++) {
+            point = point.next();
+            points.add(point);
+        }
+        return point;
+    }
+
+    private static void initLast(List<Point> points, Point point) {
+        point = point.last();
+        points.add(point);
+    }
+
+    private static Point initFirst(List<Point> points) {
+        Point point = Point.first(generatePoint());
+        points.add(point);
+        return point;
+    }
+
+    @Override
+    public String toString() {
+        return "LadderLine{" +
+            "points=" + points +
+            '}';
     }
 }
