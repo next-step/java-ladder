98c013ea1ac8303de19271f0dd9c1b2e404af976
Beenie93 <seongbeen93@naver.com>
2021-04-15 03:00:43 +0900

    test: implement a test unit to check if height is greater than or equal to 1

--- src/test/java/nextstep/ladder/domain/LadderHeightTest.java
+++ src/test/java/nextstep/ladder/domain/LadderHeightTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LadderHeightTest {
 
@@ -12,5 +13,11 @@ public class LadderHeightTest {
     public void create() throws Exception {
         LadderHeight ladderHeight = new LadderHeight("5");
         assertThat(ladderHeight).isEqualTo(new LadderHeight(5));
+    }
+
+    @Test
+    @DisplayName("1 미만일 경우 예외가 발생한다.")
+    public void validateHeight() throws Exception {
+        assertThatIllegalArgumentException().isThrownBy(() -> new LadderHeight(0));
     }
 }
