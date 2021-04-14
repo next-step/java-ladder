66cc8e92dd86ff3e40a138ca11f2ca20111b8b22
Beenie93 <seongbeen93@naver.com>
2021-04-15 00:30:03 +0900

    feat: implement Prize Object

---
+++ src/main/java/nextstep/ladder/domain/Prize.java
@@ -0,0 +1,28 @@
+package nextstep.ladder.domain;
+
+import java.util.Objects;
+
+public class Prize {
+    private final String prize;
+
+    public Prize(String prize) {
+        this.prize = prize;
+    }
+
+    public String name() {
+        return prize;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Prize prize1 = (Prize) o;
+        return Objects.equals(prize, prize1.prize);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(prize);
+    }
+}
