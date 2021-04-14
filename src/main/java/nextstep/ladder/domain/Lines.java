951ab0c97d8bb7e4dcb7a60984114b593001fd2e
Beenie93 <seongbeen93@naver.com>
2021-04-15 01:40:17 +0900

    feat: implement methods to get first and next line

--- src/main/java/nextstep/ladder/domain/Lines.java
+++ src/main/java/nextstep/ladder/domain/Lines.java
@@ -3,8 +3,11 @@ package nextstep.ladder.domain;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Lines {
+    private static final int FIRST_LINE_INDEX = 0;
+
     private final List<Line> lines;
 
     public Lines(List<Line> lines) {
@@ -12,8 +15,8 @@ public class Lines {
     }
 
     public static Lines from(List<Points> allPoints) {
-        return new Lines(allPoints.stream()
-                .map(Line::new)
+        return new Lines(IntStream.range(0, allPoints.size())
+                .mapToObj((i) -> new Line(i, allPoints.get(i)))
                 .collect(Collectors.toList()));
     }
 
@@ -30,5 +33,13 @@ public class Lines {
 
     public int size() {
         return lines.size();
+    }
+
+    public Line firstLine() {
+        return lines.get(FIRST_LINE_INDEX);
+    }
+
+    public Line nextLine(int lineIndex) {
+        return lines.get(lineIndex + 1);
     }
 }
