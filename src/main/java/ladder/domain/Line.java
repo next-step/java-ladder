23f40a2d16bd156bb2b2e85a02d95097e022610b
jaystar13 <astarisk2@gmail.com>
2021-04-15 21:55:50 +0900

    refactor: 의미가 불분명한 매직넘버를 메서드로 변경

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Line {
+    private final static int FIRST_POINT_COUNT = 1;
+    private final static int LAST_POINT_COUNT = 1;
     private List<Point> points;
 
     public Line(List<Point> points) {
@@ -19,10 +21,14 @@ public class Line {
 
         PointGenerator pointGenerator = new PointGenerator(pointsNew);
         Point point = pointGenerator.first();
-        Point lastBodyPoint = pointGenerator.body(playersCount - 2, point);
+        Point lastBodyPoint = pointGenerator.body(bodyCount(playersCount), point);
         pointGenerator.last(lastBodyPoint);
 
         return pointsNew;
+    }
+
+    private int bodyCount(int playersCount) {
+        return playersCount - (FIRST_POINT_COUNT + LAST_POINT_COUNT);
     }
 
     public int move(int positionIndex) {
