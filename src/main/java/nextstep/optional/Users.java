28c8cd4609bb0f313d0f339d67a81eaeee4e2abb
ttokey <ttokey.lee@gmail.com>
2021-04-16 00:21:29 +0900

    요구사항 2 - Optional에서 값을 반환

--- src/main/java/nextstep/optional/Users.java
+++ src/main/java/nextstep/optional/Users.java
@@ -12,12 +12,19 @@ public class Users {
             new User("jk", 40),
             new User("honux", 45));
 
+//    User getUser(String name) {
+//        for (User user : users) {
+//            if (user.matchName(name)) {
+//                return user;
+//            }
+//        }
+//        return DEFAULT_USER;
+//    }
+
     User getUser(String name) {
-        for (User user : users) {
-            if (user.matchName(name)) {
-                return user;
-            }
-        }
-        return DEFAULT_USER;
+        return users.stream()
+                .filter(user -> user.matchName(name))
+                .findFirst()
+                .orElse(DEFAULT_USER);
     }
 }
