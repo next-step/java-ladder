d1d564e52711cae9561f22d889a9f87c6885dcfb
jaystar13 <astarisk2@gmail.com>
2021-04-15 21:50:26 +0900

    refactor: View 참조를 Controller로 이동

--- src/main/java/ladder/LadderGameApplication.java
+++ src/main/java/ladder/LadderGameApplication.java
@@ -1,24 +1,9 @@
 package ladder;
 
 import ladder.controller.LadderController;
-import ladder.view.InputView;
-import ladder.view.ResultView;
 
 public class LadderGameApplication {
     public static void main(String[] args) {
-        try {
-            InputView inputView = new InputView();
-            inputView.inputPlayer();
-            inputView.inputLadderHeight();
-
-            LadderController ladderController = new LadderController(inputView);
-            ResultView resultView = ladderController.startLadderGame();
-
-            resultView.printOutPlayers();
-            System.out.println();
-            resultView.printOutLineResult();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        new LadderController().startLadderGame();
     }
 }
--- src/main/java/ladder/controller/LadderController.java
+++ src/main/java/ladder/controller/LadderController.java
@@ -6,18 +6,25 @@ import ladder.view.InputView;
 import ladder.view.ResultView;
 
 public class LadderController {
-    private final InputView inputView;
+    private final InputView inputView = new InputView();
 
-    public LadderController(InputView inputView) {
-        this.inputView = inputView;
+    public LadderController() {
     }
 
-    public ResultView startLadderGame() {
-        LineResults lineResults = new LineResults();
-        for (int i = 0; i < inputView.getLadderHeight(); i++) {
-            Line ladderLine = new Line(inputView.playersCount());
-            lineResults.add(ladderLine);
+    public void startLadderGame() {
+        try {
+            InputView inputView = new InputView();
+            inputView.inputPlayer();
+            inputView.inputLadderHeight();
+
+            LineResults lineResults = new LineResults();
+            for (int i = 0; i < inputView.getLadderHeight(); i++) {
+                Line ladderLine = new Line(inputView.playersCount());
+                lineResults.add(ladderLine);
+            }
+            new ResultView(inputView.getPlayers(), lineResults).showLadderDrawResult();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-        return new ResultView(inputView.getPlayers(), lineResults);
     }
 }
