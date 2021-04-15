140a4b71cded5283ac3d269698c5861d5ab0a373
김우재 <kwj1270@naver.com>
2021-04-15 20:52:19 +0900

    docs:all 예외처리 구현 완료 작성

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -8,6 +8,7 @@ public final class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String INPUT_PARTICIPANTS_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String INPUT_LADDER_RESULTS_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String INPUT_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     private static final String REGEX = ",";
 
@@ -27,9 +28,16 @@ public final class InputView {
         return SCANNER.nextLine().split(REGEX);
     }
 
+    public final String[] inputLadderResultsByClient() {
+        System.out.println(INPUT_LADDER_RESULTS_MESSAGE);
+        return SCANNER.nextLine().split(REGEX);
+    }
+
     public final int inputLadderHeightByClient() {
         System.out.println(INPUT_LADDER_HEIGHT_MESSAGE);
         return valueOf(SCANNER.nextLine());
     }
+
+
 
 }
