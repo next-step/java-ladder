eb49cab687183e4ff06a3801ce037aa21cd54d2f
shkwon <oper912@naver.com>
2021-04-15 12:37:43 +0900

    refactor: Line -> LadderLine 변경에 따른 Ladder 리팩토링

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -4,32 +4,53 @@ import ladder.strategy.PointStrategy;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Ladder {
-    private final List<Line> lines;
+    private final List<LadderLine> ladderLines;
 
-    private Ladder(final List<Line> lines) {
-        this.lines = lines;
+    private Ladder(final List<LadderLine> ladderLines) {
+        this.ladderLines = ladderLines;
     }
 
     public static Ladder of(final int height, final int countOfPerson, PointStrategy pointStrategy) {
         return IntStream.range(0, height)
-                .mapToObj(i -> Line.of(countOfPerson, pointStrategy))
+                .mapToObj(i -> LadderLine.of(countOfPerson, pointStrategy))
                 .collect(Collectors.collectingAndThen(Collectors.toList(),
                         Ladder::new));
     }
 
     public int getResultIndex(int startIndex) {
         int resultIndex = startIndex;
-        for (Line line : lines) {
-            resultIndex = line.move(resultIndex);
+        for (LadderLine ladderLine : ladderLines) {
+            resultIndex = ladderLine.move(resultIndex);
         }
         return resultIndex;
     }
 
-    public List<Line> getLines() {
-        return Collections.unmodifiableList(lines);
+    public List<LadderLine> getLadderLines() {
+        return Collections.unmodifiableList(ladderLines);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ladder ladder = (Ladder) o;
+        return Objects.equals(ladderLines, ladder.ladderLines);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ladderLines);
+    }
+
+    @Override
+    public String toString() {
+        return "Ladder{" +
+                "ladderLines=" + ladderLines +
+                '}';
     }
 }
