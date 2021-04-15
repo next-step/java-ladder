a09358d3573cb43ef14418f933fb811c915e2b32
juniq <juniqlim@gmail.com>
2021-04-15 22:46:46 +0900

    test: 입력한 사람의 가로 인덱스 조회
    참여자가 아니면 예외발생

--- src/test/java/nextstep/ladder/domain/MembersTest.java
+++ src/test/java/nextstep/ladder/domain/MembersTest.java
@@ -1,5 +1,6 @@
 package nextstep.ladder.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 import org.junit.jupiter.api.Test;
@@ -9,5 +10,11 @@ public class MembersTest {
     @Test
     void create() {
         assertThatCode(() -> Members.of("pobi,honux,crong,jk")).doesNotThrowAnyException();
+    }
+
+    @Test
+    void findHorizontalNumber() {
+        Members members = Members.of("pobi,honux,crong,jk");
+        assertThat(members.findHorizontalNumber(Member.of("honux"))).isEqualTo(1);
     }
 }
