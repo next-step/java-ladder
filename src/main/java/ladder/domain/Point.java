c4aaad5e4b54a52c7acac49987ac7b88a197e85e
shkwon <oper912@naver.com>
2021-04-15 12:36:08 +0900

    refactor: Point 리팩토링 (Direction 추가)

--- src/main/java/ladder/domain/Point.java
+++ src/main/java/ladder/domain/Point.java
@@ -1,48 +1,68 @@
 package ladder.domain;
 
+import ladder.strategy.PointStrategy;
+
 import java.util.Objects;
 
 public class Point {
     private final int index;
-    private final boolean point;
+    private final Direction direction;
 
-    private Point(final int index, final boolean point) {
+    private Point(final int index, final Direction direction) {
         this.index = index;
-        this.point = point;
+        this.direction = direction;
     }
 
-    public static Point from(final int index, final boolean point) {
-        return new Point(index, point);
+    public static Point from(final int index, final Direction direction) {
+        return new Point(index, direction);
     }
 
     public int move() {
-        return point ? index + 1 : index;
-    }
-
-    public int move(final boolean beforePoint) {
-        if (point) {
+        if (direction.isRight()) {
             return index + 1;
         }
-        if (beforePoint) {
+
+        if (direction.isLeft()) {
             return index - 1;
         }
-        return index;
+
+        return this.index;
     }
 
-    public boolean getPoint() {
-        return point;
+    public Point next(PointStrategy pointStrategy) {
+        return new Point(index + 1, direction.next(pointStrategy));
+    }
+
+    public Point last() {
+        return new Point(index + 1, direction.last());
+    }
+
+    public static Point first(Boolean right) {
+        return new Point(0, Direction.first(right));
+    }
+
+    public boolean isRight() {
+        return direction.isRight();
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Point point1 = (Point) o;
-        return index == point1.index && point == point1.point;
+        Point point = (Point) o;
+        return index == point.index && Objects.equals(direction, point.direction);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(index, point);
+        return Objects.hash(index, direction);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "index=" + index +
+                ", direction=" + direction +
+                '}';
     }
 }
