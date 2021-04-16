9d13becbe8ce12f451db97788e3a39f9c74c50f8
Beenie93 <seongbeen93@naver.com>
2021-04-16 14:28:48 +0900

    refactor: remove from()

--- src/main/java/nextstep/ladder/domain/Lines.java
+++ src/main/java/nextstep/ladder/domain/Lines.java
@@ -3,19 +3,12 @@ package nextstep.ladder.domain;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Lines {
     private final List<Line> lines;
 
     public Lines(List<Line> lines) {
         this.lines = lines;
-    }
-
-    public static Lines from(List<CrossingPoints> allCrossingPoints) {
-        return new Lines(allCrossingPoints.stream()
-                .map(Line::new)
-                .collect(Collectors.toList()));
     }
 
     public List<Line> lines() {
--- src/test/java/nextstep/ladder/domain/LinesTest.java
+++ src/test/java/nextstep/ladder/domain/LinesTest.java
@@ -4,6 +4,7 @@ import nextstep.ladder.strategy.RandomDirectionStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.BDDAssertions.then;
@@ -11,16 +12,16 @@ import static org.assertj.core.api.BDDAssertions.then;
 public class LinesTest {
 
     @Test
-    @DisplayName("각 라인의 좌표들을 인자로 받아 모든 라인을 생성한다.")
+    @DisplayName("라인들을 인자로 받아 모든 라인을 담는 객체를 생성한다.")
     public void create() throws Exception {
         //given
-        CrossingPointsCreator crossingPointsCreator = new CrossingPointsCreator(new RandomDirectionStrategy());
-        List<CrossingPoints> crossPointsForLines = crossingPointsCreator.create(4, 5);
-        Lines lines = Lines.from(crossPointsForLines);
+        Line firstLine = new Line(4, new RandomDirectionStrategy());
+        Line secondLine = new Line(4, new RandomDirectionStrategy());
+        Lines lines = new Lines(Arrays.asList(firstLine, secondLine));
 
         //when
         List<Line> result = lines.lines();
 
-        then(result).hasSize(5);
+        then(result).hasSize(2);
     }
 }
