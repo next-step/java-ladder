f2f51e5751d302d18c0ef1ae3bef36a1cf5fc448
abcdefg <choiyurim@naver.com>
2021-04-16 16:19:59 +0900

    refactor(View): refactoring view

--- src/main/java/step2/view/ResultView.java
+++ src/main/java/step2/view/ResultView.java
@@ -4,19 +4,29 @@ import step2.domain.Line;
 import step2.domain.Player;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ResultView {
+    public static final String LADDER_MOVE = "|-----";
+    public static final String LADDER_STOP = "|     ";
 
     private ResultView() {
     }
 
-    public static void printLadder(List<Line> lines) {
-        lines.stream()
-                .forEach(line -> {
-                    System.out.print("\n     ");
-                    System.out.print(line.toString());
-                    System.out.print("|");
-                });
+    public static void printLadderStart() {
+        System.out.print("\n     ");
+    }
+
+    public static void printLadder(Line line) {
+        IntStream.range(0, line.getSize())
+                .mapToObj(line::getPoint)
+                .map(point -> point ? LADDER_MOVE : LADDER_STOP)
+                .forEach(System.out::print);
+    }
+
+    public static void printLadderEnd() {
+        System.out.print("|");
     }
 
     public static void printPlayer(List<Player> players) {
