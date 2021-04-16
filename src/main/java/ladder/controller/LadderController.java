c364514b6b6e2baf92983208e454ed11cd7bd22d
jaystar13 <astarisk2@gmail.com>
2021-04-17 06:37:06 +0900

    feat: 실행결과 입출력 로직 추가

--- src/main/java/ladder/controller/LadderController.java
+++ src/main/java/ladder/controller/LadderController.java
@@ -1,23 +1,39 @@
 package ladder.controller;
 
+import ladder.domain.LadderResult;
 import ladder.domain.Line;
 import ladder.domain.LineResults;
+import ladder.domain.Players;
 import ladder.view.InputView;
+import ladder.view.PrizeInputView;
+import ladder.view.PrizeResultView;
 import ladder.view.ResultView;
 
 public class LadderController {
     public void startLadderGame() {
         try {
             InputView inputView = new InputView();
-            inputView.inputPlayer();
-            inputView.inputLadderHeight();
+            inputView.inputLadderCondition();
 
             LineResults lineResults = new LineResults();
             for (int i = 0; i < inputView.getLadderHeight(); i++) {
                 Line ladderLine = new Line(inputView.playersCount());
                 lineResults.add(ladderLine);
             }
-            new ResultView(inputView.getPlayers(), lineResults).showLadderDrawResult();
+
+            Players players = inputView.getPlayers();
+            LadderResult ladderResult = new LadderResult(players, lineResults);
+
+            ResultView resultView = new ResultView(ladderResult);
+            resultView.showLadderDrawResult();
+            resultView.printPrize(inputView.getPrize());
+
+            PrizeInputView prizeInputView = new PrizeInputView();
+            PrizeResultView prizeResultView = new PrizeResultView(ladderResult, inputView.getPrize());
+            while (!prizeInputView.isAll()) {
+                prizeInputView.inputPlayerNameForPrize();
+                prizeResultView.printPrizeResult(prizeInputView);
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
