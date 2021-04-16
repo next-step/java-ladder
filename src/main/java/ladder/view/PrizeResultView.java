a9c397406da4a4279b186bc0731a618c4750f12d
jaystar13 <astarisk2@gmail.com>
2021-04-17 06:35:30 +0900

    feat: 참가자별 실행 결과 출력 클래스

---
+++ src/main/java/ladder/view/PrizeResultView.java
@@ -0,0 +1,35 @@
+package ladder.view;
+
+import ladder.domain.LadderResult;
+import ladder.domain.Player;
+import ladder.domain.Prize;
+
+import java.util.Map;
+
+public class PrizeResultView {
+    private final LadderResult ladderResult;
+    private final Prize prize;
+
+    public PrizeResultView(LadderResult ladderResult, Prize prize) {
+        this.ladderResult = ladderResult;
+        this.prize = prize;
+    }
+
+    public void printPrizeResult(PrizeInputView inputView) {
+        System.out.println("실행 결과");
+        if (inputView.isAll()) {
+            printResultAll();
+            return;
+        }
+        printResult(inputView.getPlayerName());
+    }
+
+    public void printResult(String playerName) {
+        System.out.println(ladderResult.playersPrize(new Player(playerName), prize));
+    }
+
+    public void printResultAll() {
+        Map<String, String> allResult = ladderResult.playersPrizeAll(prize);
+        allResult.forEach((s, s2) -> System.out.println(s + " : " + s2));
+    }
+}
