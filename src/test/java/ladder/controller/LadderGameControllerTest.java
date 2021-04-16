d3dc1555777c863dd01ed829c26b8accf2d9d092
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-16 12:47:28 +0900

    remove : LadderGameRequest 클래스 삭제  - LadderGenerationRequest 클래스로 기능 통합

--- src/test/java/ladder/controller/LadderGameControllerTest.java
+++ src/test/java/ladder/controller/LadderGameControllerTest.java
@@ -1,80 +1,106 @@
 package ladder.controller;
 
-import ladder.controller.dto.LadderGameRequest;
 import ladder.controller.dto.LadderGameResponse;
-import ladder.controller.dto.LadderGameTotalResultResponse;
-import ladder.controller.dto.LadderLine;
+import ladder.controller.dto.LadderGenerationRequest;
+import ladder.controller.dto.LadderGenerationResponse;
+import ladder.domain.Ladder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LadderGameControllerTest {
 
-    @ParameterizedTest
-    @CsvSource(value = {"pobi:꽝", "honux:3000", "crong:꽝", "jk:5000"}, delimiter = ':')
-    @DisplayName("사다리게임 결과 조회 - 단건조회")
-    void inquiryGameResult(String inquiryName, String expectedResult) {
+    @Test
+    @DisplayName("참가자 수와 이름 검증")
+    void participants() {
         // given
+        String participantNames = "pobi,honux,crong,jk";
+        int ladderHeight = 5;
+        LadderGenerationRequest request = new LadderGenerationRequest(participantNames, "꽝,5000,꽝,3000", ladderHeight);
         LadderGameController controller = new LadderGameController();
 
-        List<String> participants = Arrays.asList("pobi", "honux", "crong", "jk");
-        List<LadderLine> ladderLines = new ArrayList<>();
-        ladderLines.add(new LadderLine(Arrays.asList(true, false, true, false)));
-        ladderLines.add(new LadderLine(Arrays.asList(false, true, false, false)));
-        ladderLines.add(new LadderLine(Arrays.asList(true, false, false, false)));
-        ladderLines.add(new LadderLine(Arrays.asList(false, true, false, false)));
-        ladderLines.add(new LadderLine(Arrays.asList(true, false, true, false)));
-        List<String> gameResults = Arrays.asList("꽝", "5000", "꽝", "3000");
-        LadderGameRequest request = new LadderGameRequest(participants, ladderLines, gameResults, inquiryName);
-
         // when
-        LadderGameResponse response = controller.inquiryGameResult(request);
+        LadderGenerationResponse response = controller.generateLadder(request);
 
         // then
-        assertThat(expectedResult).isEqualTo(response.getGameResult());
+        assertThat(4).isEqualTo(response.getParticipantNames().size());
+        for (String name : response.getParticipantNames()) {
+            assertThat(Arrays.asList(participantNames.split(",")).contains(name)).isTrue();
+        }
     }
 
     @Test
-    @DisplayName("사다리게임 결과 리스트 조회")
-    void inquiryGameResults() {
+    @DisplayName("사다리 높이 검증")
+    void ladderHeight() {
         // given
-        List<String> expectedList = Arrays.asList("꽝", "3000", "꽝", "5000");
-
+        String participantNames = "pobi,honux,crong,jk";
+        int ladderHeight = 5;
+        LadderGenerationRequest request = new LadderGenerationRequest(participantNames, "꽝,5000,꽝,3000", ladderHeight);
         LadderGameController controller = new LadderGameController();
 
-        List<String> participants = Arrays.asList("pobi", "honux", "crong", "jk");
-        List<LadderLine> ladderLines = new ArrayList<>();
-        ladderLines.add(new LadderLine(Arrays.asList(true, false, true, false)));
-        ladderLines.add(new LadderLine(Arrays.asList(false, true, false, false)));
-        ladderLines.add(new LadderLine(Arrays.asList(true, false, false, false)));
-        ladderLines.add(new LadderLine(Arrays.asList(false, true, false, false)));
-        ladderLines.add(new LadderLine(Arrays.asList(true, false, true, false)));
-        List<String> gameResults = Arrays.asList("꽝", "5000", "꽝", "3000");
-        LadderGameRequest request = new LadderGameRequest(participants, ladderLines, gameResults, "all");
-
         // when
-        LadderGameTotalResultResponse response = controller.inquiryGameResults(request);
+        LadderGenerationResponse response = controller.generateLadder(request);
 
         // then
-        assertAll(
-                () -> {
-                    for (int i = 0; i < participants.size(); i++) {
-                        assertThat(participants.get(i)).isEqualTo(response.getParticipantNames().get(i));
-                    }
-                },
-                () -> {
-                    for (int i = 0; i < expectedList.size(); i++) {
-                        assertThat(expectedList.get(i)).isEqualTo(response.getGameResults().get(i));
-                    }
-                }
-        );
+        assertThat(ladderHeight).isEqualTo(response.getLadderLines().size());
     }
+
+    @Test
+    @DisplayName("사다리 라인 폭 검증")
+    void ladderHorizonLength() {
+        // given
+        String participantNames = "pobi,honux,crong,jk";
+        int ladderHeight = 5;
+        LadderGenerationRequest request = new LadderGenerationRequest(participantNames, "꽝,5000,꽝,3000", ladderHeight);
+        LadderGameController controller = new LadderGameController();
+
+        // when
+        LadderGenerationResponse response = controller.generateLadder(request);
+
+        // then
+        assertThat(Ladder.LADDER_HORIZON_WIDTH).isEqualTo(response.getLadderWidth());
+    }
+
+    @Test
+    @DisplayName("게임생성 결과 검증")
+    void gameResults() {
+        // given
+        String participantNames = "pobi,honux,crong,jk";
+        int ladderHeight = 5;
+        String gameResults = "꽝,5000,꽝,3000";
+        LadderGenerationRequest request = new LadderGenerationRequest(participantNames, gameResults, ladderHeight);
+        LadderGameController controller = new LadderGameController();
+
+        // when
+        LadderGenerationResponse response = controller.generateLadder(request);
+
+        // then
+        for (String gameResult : response.getGameResults()) {
+            assertThat(Arrays.asList(gameResults.split(",")).contains(gameResult)).isTrue();
+        }
+    }
+
+    @Test
+    @DisplayName("게임실행 결과 검증")
+    void ladderGameResult() {
+        // given
+        String participantNames = "pobi,honux,crong,jk";
+        int ladderHeight = 5;
+        String gameResults = "꽝,5000,꽝,3000";
+        LadderGenerationRequest request = new LadderGenerationRequest(participantNames, gameResults, ladderHeight);
+        LadderGameController controller = new LadderGameController();
+
+        // when
+        LadderGenerationResponse ladderGenerationResponse = controller.generateLadder(request);
+
+        // then
+        for (LadderGameResponse gameResponse : ladderGenerationResponse.getGameResponses()) {
+            assertThat(Arrays.asList(participantNames.split(",")).contains(gameResponse.getParticipantName())).isTrue();
+            assertThat(Arrays.asList(gameResults.split(",")).contains(gameResponse.getGameResult())).isTrue();
+        }
+    }
+
 }
