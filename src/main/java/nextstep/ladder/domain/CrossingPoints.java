2014eed5df311ecd693a4b15f7a52c69660df149
Beenie93 <seongbeen93@naver.com>
2021-04-16 14:30:09 +0900

    refactor: remove constructor with CountOfPlayer and DirectionStrategy

--- src/main/java/nextstep/ladder/domain/CrossingPoints.java
+++ src/main/java/nextstep/ladder/domain/CrossingPoints.java
@@ -1,8 +1,5 @@
 package nextstep.ladder.domain;
 
-import nextstep.ladder.strategy.DirectionStrategy;
-
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,31 +7,8 @@ import java.util.stream.Collectors;
 public class CrossingPoints {
     private final List<CrossingPoint> crossingPoints;
 
-    public CrossingPoints(int countOfPlayer, DirectionStrategy directionStrategy) {
-        this(new CountOfPlayer(countOfPlayer), directionStrategy);
-    }
-
-    public CrossingPoints(CountOfPlayer countOfPlayer, DirectionStrategy directionStrategy) {
-        this(createWith(countOfPlayer, directionStrategy));
-    }
-
     public CrossingPoints(List<CrossingPoint> crossingPoints) {
         this.crossingPoints = crossingPoints;
-    }
-
-    private static List<CrossingPoint> createWith(CountOfPlayer countOfPlayer, DirectionStrategy directionStrategy) {
-        List<CrossingPoint> crossingPoints = new ArrayList<>(countOfPlayer.number());
-        CrossingPoint crossingPoint = CrossingPoint.first(directionStrategy);
-        crossingPoints.add(crossingPoint);
-
-        while (crossingPoint.isNotLast(countOfPlayer.number())) {
-            crossingPoint = crossingPoint.next(directionStrategy);
-            crossingPoints.add(crossingPoint);
-        }
-
-        crossingPoints.add(crossingPoint.last());
-
-        return crossingPoints;
     }
 
     public List<CrossingPoint> crossingPoints() {
--- src/test/java/nextstep/ladder/domain/CrossingPointsTest.java
+++ src/test/java/nextstep/ladder/domain/CrossingPointsTest.java
@@ -1,24 +1,37 @@
 package nextstep.ladder.domain;
 
+import nextstep.ladder.strategy.DirectionStrategy;
 import nextstep.ladder.strategy.RandomDirectionStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
 public class CrossingPointsTest {
 
+    private DirectionStrategy connectableTrueStrategy = new RandomDirectionStrategy() {
+        @Override
+        public boolean isConnectable() {
+            return true;
+        }
+    };
+
     @Test
-    @DisplayName("플레이어 인원 수와 연결 전략을 인수로 받아 플레이어 인원 수 만큼의 현재 좌표를 생성한다.")
-    public void create() throws Exception {
+    @DisplayName("현재 좌표에서 이동할 수 있는 방향에 따른 결과의 좌표를 반환한다.")
+    public void move() throws Exception {
         //given
-        CrossingPoints crossingPoints = new CrossingPoints(4, new RandomDirectionStrategy());
+        CrossingPoint first = CrossingPoint.first(connectableTrueStrategy);
+        CrossingPoint second = first.next(connectableTrueStrategy);
+        CrossingPoints crossingPoints = new CrossingPoints(Arrays.asList(first, second));
 
         //when
-        List<CrossingPoint> result = crossingPoints.crossingPoints();
+        int right = crossingPoints.move(0);
+        int left = crossingPoints.move(1);
 
-        then(result).hasSize(4);
+        then(right).isEqualTo(1);
+        then(left).isEqualTo(0);
     }
 }
