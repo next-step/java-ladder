d607358fddb86089b0d7ecd4e442ebc88f0a747b
Beenie93 <seongbeen93@naver.com>
2021-04-16 11:23:21 +0900

    refactor: refactor Lines

--- src/main/java/nextstep/ladder/domain/Lines.java
+++ src/main/java/nextstep/ladder/domain/Lines.java
@@ -6,17 +6,15 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Lines {
-    private static final int FIRST_LINE_INDEX = 0;
-
     private final List<Line> lines;
 
     public Lines(List<Line> lines) {
         this.lines = lines;
     }
 
-    public static Lines from(List<Points> allPoints) {
-        return new Lines(IntStream.range(0, allPoints.size())
-                .mapToObj((i) -> new Line(i, allPoints.get(i)))
+    public static Lines from(List<CrossingPoints> allCrossingPoints) {
+        return new Lines(allCrossingPoints.stream()
+                .map(Line::new)
                 .collect(Collectors.toList()));
     }
 
@@ -26,20 +24,12 @@ public class Lines {
 
     public List<List<Boolean>> linesConnection() {
         return lines.stream()
-                .map(Line::points)
-                .map(Points::pointsConnection)
+                .map(Line::crossingPoints)
+                .map(CrossingPoints::pointsConnection)
                 .collect(Collectors.toList());
     }
 
     public int size() {
         return lines.size();
-    }
-
-    public Line firstLine() {
-        return lines.get(FIRST_LINE_INDEX);
-    }
-
-    public Line nextLine(int lineIndex) {
-        return lines.get(lineIndex + 1);
     }
 }
--- src/test/java/nextstep/ladder/domain/LinesTest.java
+++ src/test/java/nextstep/ladder/domain/LinesTest.java
@@ -4,7 +4,6 @@ import nextstep.ladder.strategy.RandomDirectionStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.BDDAssertions.then;
@@ -15,41 +14,13 @@ public class LinesTest {
     @DisplayName("각 라인의 좌표들을 인자로 받아 모든 라인을 생성한다.")
     public void create() throws Exception {
         //given
-        AllPointsForLines allPointsForLines = new AllPointsForLines(new RandomDirectionStrategy());
-        List<Points> allPoints = allPointsForLines.allPoints(4, 5);
-        Lines lines = Lines.from(allPoints);
+        AllCrossingPointsForLines allCrossingPointsForLines = new AllCrossingPointsForLines(new RandomDirectionStrategy());
+        List<CrossingPoints> allCrossPoints = allCrossingPointsForLines.allCrossingPoints(4, 5);
+        Lines lines = Lines.from(allCrossPoints);
 
         //when
         List<Line> result = lines.lines();
 
         then(result).hasSize(5);
-    }
-
-    @Test
-    @DisplayName("라인 목록에서 첫 번째 라인을 반환한다.")
-    public void firstLine() throws Exception {
-        //given
-        Line line1 = new Line(0, new Points(4, new RandomDirectionStrategy()));
-        Line line2 = new Line(1, new Points(4, new RandomDirectionStrategy()));
-        Lines lines = new Lines(Arrays.asList(line1, line2));
-
-        //when
-        Line firstLine = lines.firstLine();
-
-        then(firstLine).isEqualTo(line1);
-    }
-
-    @Test
-    @DisplayName("라인 인덱스를 인자로 받아 해당 인덱스의 다음 라인을 반환한다.")
-    public void nextLine() throws Exception {
-        //given
-        Line line1 = new Line(0, new Points(4, new RandomDirectionStrategy()));
-        Line line2 = new Line(1, new Points(4, new RandomDirectionStrategy()));
-        Lines lines = new Lines(Arrays.asList(line1, line2));
-
-        //when
-        Line nextLine = lines.nextLine(0);
-
-        then(nextLine).isEqualTo(line2);
     }
 }
