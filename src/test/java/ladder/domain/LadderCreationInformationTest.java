ac6236bd6239e88ce450c1db555375829a762381
김우재 <kwj1270@naver.com>
2021-04-17 12:40:45 +0900

    test:LadderCreationInformation 인스턴스 null 입력시 예외처리 여부 테스트작성

--- src/test/java/ladder/domain/LadderCreationInformationTest.java
+++ src/test/java/ladder/domain/LadderCreationInformationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LadderCreationInformationTest {
@@ -20,5 +21,18 @@ class LadderCreationInformationTest {
 
         // then
         assertThat(ladderCreationInformation).isNotNull();
+    }
+
+    @DisplayName("LadderCreationInformation 인스턴스 null 입력시 예외처리 여부 테스트")
+    @Test
+    void 검증_null() {
+        // given
+        People people = null;
+        LadderHeight ladderHeight = LadderHeight.valueOf(5);
+
+        // when
+        assertThatThrownBy(()-> LadderCreationInformation.from(people, ladderHeight))
+                .isInstanceOf(PeopleNullPointerException.class)
+                .hasMessage("People 인스턴스가 null 입니다.");
     }
 }
