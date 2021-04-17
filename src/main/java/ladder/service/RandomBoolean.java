3fd252127de43d637853b11226341eb18aaebd51
ttokey <ttokey.lee@gmail.com>
2021-04-17 14:33:34 +0900

    fix : 확률이 100일때 True가 return 될 수 있도록 수정

--- src/main/java/ladder/service/RandomBoolean.java
+++ src/main/java/ladder/service/RandomBoolean.java
@@ -3,5 +3,4 @@ package ladder.service;
 public interface RandomBoolean {
     public boolean randomBoolean();
 
-    public boolean randomBoolean(int trueProbability);
 }
--- src/main/java/ladder/service/RandomBooleanGenerator.java
+++ src/main/java/ladder/service/RandomBooleanGenerator.java
@@ -1,15 +1,17 @@
 package ladder.service;
 
+import java.util.Random;
+
 public class RandomBooleanGenerator implements RandomBoolean {
-    private static final int DEFAULT_PROBABILITY = 50;
+    Random random = new Random();
+    private final int DEFAULT_PROBABILITY = 50;
+    private final int MAX_PROBABILITY = 100;
 
     @Override
     public boolean randomBoolean() {
-        return true;
-    }
-
-    @Override
-    public boolean randomBoolean(int trueProbability) {
-        return true;
+        if (random.nextInt(MAX_PROBABILITY) <= DEFAULT_PROBABILITY) {
+            return true;
+        }
+        return false;
     }
 }
