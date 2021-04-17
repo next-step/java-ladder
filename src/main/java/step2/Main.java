ceb9f7a1f3588e8c49da657b5b07884d46353e21
abcdefg <choiyurim@naver.com>
2021-04-17 11:21:12 +0900

    refactor(Main): move main package

--- src/main/java/step2/Main.java
+++ src/main/java/step2/Main.java
@@ -1,17 +1,19 @@
-package step2.controller;
+package step2;
 
+import step2.controller.LadderController;
 import step2.domain.Line;
 import step2.domain.Lines;
 import step2.domain.Player;
+import step2.domain.Players;
 
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Player> players = LadderController.getPlayers();
+        Players players = LadderController.getPlayers();
         int height = LadderController.getHeight();
-        Lines lines = LadderController.createLines(players.size(), height);
+        Lines lines = LadderController.createLines(players.getSize(), height);
 
         LadderController.showResult(players, lines);
     }
