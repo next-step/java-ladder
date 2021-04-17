e94fcf185ff43c7223f85c3a32dc19eca247feb2
abcdefg <choiyurim@naver.com>
2021-04-17 11:20:23 +0900

    refactor(View): refactoring view

--- src/main/java/step2/view/ResultView.java
+++ src/main/java/step2/view/ResultView.java
@@ -1,10 +1,10 @@
 package step2.view;
 
 import step2.domain.Line;
+import step2.domain.Lines;
 import step2.domain.Player;
+import step2.domain.Players;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class ResultView {
@@ -18,7 +18,15 @@ public class ResultView {
         System.out.print("\n     ");
     }
 
-    public static void printLadder(Line line) {
+    public static void printLadder(Lines lines) {
+        lines.getLines().forEach(line -> {
+            ResultView.printLadderStart();
+            ResultView.printLine(line);
+            ResultView.printLadderEnd();
+        });
+    }
+
+    private static void printLine(Line line) {
         IntStream.range(0, line.getSize())
                 .mapToObj(line::getPoint)
                 .map(point -> point ? LADDER_MOVE : LADDER_STOP)
@@ -29,8 +37,9 @@ public class ResultView {
         System.out.print("|");
     }
 
-    public static void printPlayer(List<Player> players) {
-        players.stream()
+    public static void printPlayer(Players players) {
+        players.getPlayers()
+                .stream()
                 .map(Player::getName)
                 .forEach(System.out::print);
     }
