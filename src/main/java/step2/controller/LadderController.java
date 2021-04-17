fe926ad2e2704def3cad2e2717a931295d0d41b2
abcdefg <choiyurim@naver.com>
2021-04-17 11:20:47 +0900

    refactor(Controller) refactoring controller

--- src/main/java/step2/controller/LadderController.java
+++ src/main/java/step2/controller/LadderController.java
@@ -4,20 +4,13 @@ import step2.domain.*;
 import step2.view.InputView;
 import step2.view.ResultView;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.IntStream;
 
-import static java.util.stream.Collectors.*;
 
 public class LadderController {
 
-    public static final String SPLIT_COMMA = ",";
-
-    public static List<Player> getPlayers() {
-        return Arrays.stream(InputView.getPlayers().split(SPLIT_COMMA))
-                .map(Player::new)
-                .collect(toList());
+    public static Players getPlayers() {
+        return Players.of(InputView.getPlayers());
     }
 
     public static int getHeight() {
@@ -25,30 +18,11 @@ public class LadderController {
     }
 
     public static Lines createLines(int size, int height) {
-        PointsGeneration pointsGeneration = new PointsGeneration(size-1);
-
-        return IntStream.range(0, height)
-                .mapToObj(index -> getLine(pointsGeneration))
-                .collect(collectingAndThen(toList(), Lines::new));
+        return Lines.of(height, size, new PointCondition());
     }
 
-    private static Line getLine(PointsGeneration pointsGeneration) {
-        PointCondition pointCondition = new PointCondition();
-        List<Boolean> points = pointsGeneration.getPoints(pointCondition);
-
-        return new Line(points);
-    }
-
-    public static void showResult(List<Player> players, Lines lines) {
+    public static void showResult(Players players, Lines lines) {
         ResultView.printPlayer(players);
-        printLadder(lines);
-    }
-
-    private static void printLadder(Lines lines) {
-        lines.getLines().forEach(line -> {
-            ResultView.printLadderStart();
-            ResultView.printLadder(line);
-            ResultView.printLadderEnd();
-        });
+        ResultView.printLadder(lines);
     }
 }
