69f4cba349e64b9f5d903a6bae385dfb80080775
김우재 <kwj1270@naver.com>
2021-04-17 12:37:28 +0900

    feat:LadderCreationInformation 인스턴스 생성 기능 구현

--- src/main/java/ladder/domain/LadderCreationInformation.java
+++ src/main/java/ladder/domain/LadderCreationInformation.java
@@ -1,4 +1,16 @@
 package ladder.domain;
 
-public class LadderCreationInformation {
+public final class LadderCreationInformation {
+
+    private final People people;
+    private final LadderHeight ladderHeight;
+
+    private LadderCreationInformation(People people, LadderHeight ladderHeight) {
+        this.people = people;
+        this.ladderHeight = ladderHeight;
+    }
+
+    public static final LadderCreationInformation from(People people, LadderHeight ladderHeight) {
+        return new LadderCreationInformation(people, ladderHeight);
+    }
 }
