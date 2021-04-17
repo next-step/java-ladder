bbbf2e276ad3f057fcb7872d6afae8234eddf444
김우재 <kwj1270@naver.com>
2021-04-17 12:44:41 +0900

    test:LadderCreationInformation 인스턴스 null 인 ladderHeight 인스턴스에 입력시 예외처리 여부 테스트작성

--- src/test/java/ladder/domain/LadderCreationInformationTest.java
+++ src/test/java/ladder/domain/LadderCreationInformationTest.java
@@ -24,9 +24,9 @@ class LadderCreationInformationTest {
         assertThat(ladderCreationInformation).isNotNull();
     }
 
-    @DisplayName("LadderCreationInformation 인스턴스 null 입력시 예외처리 여부 테스트")
+    @DisplayName("LadderCreationInformation 인스턴스 null 인 People 인스턴스에 입력시 예외처리 여부 테스트")
     @Test
-    void 검증_null() {
+    void 검증_People_null() {
         // given
         People people = null;
         LadderHeight ladderHeight = LadderHeight.valueOf(5);
@@ -35,5 +35,18 @@ class LadderCreationInformationTest {
         assertThatThrownBy(()-> LadderCreationInformation.from(people, ladderHeight))
                 .isInstanceOf(PeopleNullPointerException.class)
                 .hasMessage("People 인스턴스가 null 입니다.");
+    }
+
+    @DisplayName("LadderCreationInformation 인스턴스 null 인 ladderHeight 인스턴스에 입력시 예외처리 여부 테스트")
+    @Test
+    void 검증_LadderHeight_null() {
+        // given
+        People people = People.of("abcd,efgh,hijk".split(","));
+        LadderHeight ladderHeight = null;
+
+        // when
+        assertThatThrownBy(()-> LadderCreationInformation.from(people, ladderHeight))
+                .isInstanceOf(LadderHeightNullPointerException.class)
+                .hasMessage("LadderHeight 인스턴스가 null 입니다.");
     }
 }
