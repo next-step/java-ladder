7229895b29d090f555c6332a51bc6d8283f4c938
jaystar13 <astarisk2@gmail.com>
2021-04-17 20:56:39 +0900

    refactor: 인스턴스 변수 삭제

--- src/main/java/ladder/view/PrizeInputView.java
+++ src/main/java/ladder/view/PrizeInputView.java
@@ -5,15 +5,11 @@ import java.util.Scanner;
 public class PrizeInputView {
     private static final String ALL = "ALL";
 
-    private final Scanner scanner;
     private String playerName;
     private boolean isAll = false;
 
-    public PrizeInputView() {
-        scanner = new Scanner(System.in);
-    }
-
     public void inputPlayerNameForPrize() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println();
         System.out.println("결과를 보고 싶은 사람은?");
         playerName = scanner.nextLine();
