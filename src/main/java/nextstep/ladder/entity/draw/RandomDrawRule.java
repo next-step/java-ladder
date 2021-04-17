05cafd5679b1acf24fd01926dca960f5d60f0557
DESKTOP-T6O4081\lee <lsecret@naver.com>
2021-04-17 10:10:27 +0900

    refactor: Random static final 로 변경
    Random 객체 drawrule 호출 마다 생성 하지 않도로 변경

--- src/main/java/nextstep/ladder/entity/draw/RandomDrawRule.java
+++ src/main/java/nextstep/ladder/entity/draw/RandomDrawRule.java
@@ -3,8 +3,10 @@ package nextstep.ladder.entity.draw;
 import java.util.Random;
 
 public class RandomDrawRule implements DrawRule {
+    private static final Random random = new Random();
+
     @Override
     public boolean drawRule() {
-        return new Random().nextBoolean();
+        return random.nextBoolean();
     }
 }
