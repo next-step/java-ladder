fab53a3d9d8b2d717c7a3a281204e9286f4e723f
김우재 <kwj1270@naver.com>
2021-04-18 11:19:44 +0900

    test:LineCreator 인스턴스가 Line 만든 값을 검증하는 테스트작성

--- src/test/java/ladder/domain/creator/LineCreatorTest.java
+++ src/test/java/ladder/domain/creator/LineCreatorTest.java
@@ -1,9 +1,14 @@
 package ladder.domain.creator;
 
 import ladder.domain.ladder.Line;
+import ladder.domain.ladder.Point;
 import ladder.strategy.LineGenerateStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -24,7 +29,7 @@ class LineCreatorTest {
     @Test
     void 반환() {
         // given
-        int countOfPerson = 5;
+        int countOfPerson = 3;
         LineGenerateStrategy strategy = () -> true;
 
         // when
@@ -33,6 +38,22 @@ class LineCreatorTest {
 
         // then
         assertThat(line).isNotNull();
+    }
+
+    @DisplayName("LineCreator 인스턴스가 Line 만든 값을 검증하는 테스트")
+    @Test
+    void 검증_반환값() {
+        // given
+        Line expected = Line.of(Arrays.asList(Point.of(false), Point.of(true), Point.of(false)));
+        int countOfPerson = 3;
+        LineGenerateStrategy strategy = () -> true;
+
+        // when
+        LineCreator lineCreator = LineCreator.getInstance();
+        Line actual = lineCreator.create(countOfPerson, strategy);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
     }
 
 
