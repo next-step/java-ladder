69bd3536f9d4ba9ab74ade12c4623f91b25376b3
keke0123 <keke0123@naver.com>
2021-04-18 15:31:07 +0900

    (feat) Floor class 생성

---
+++ src/main/java/nextstep/ladder/src/Floor.java
@@ -0,0 +1,22 @@
+package nextstep.ladder.src;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Floor {
+  private final List<Bridge> floor;
+
+  public Floor(List<Bridge> floor) {
+    this.floor = floor;
+  }
+
+  public static Floor makeByWidht(int width) {
+    return new Floor(
+      IntStream.range(0,  width)
+      .boxed()
+      .map(i -> new Bridge(new MakeBridgeByRandom()))
+      .collect(Collectors.toList())
+    );
+  }
+}
