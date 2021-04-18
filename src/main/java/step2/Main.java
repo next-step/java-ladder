33869f15e1cdb479935084fa054467f19b7b36f5
abcdefg <choiyurim@naver.com>
2021-04-18 18:43:11 +0900

    refactor(Main): refactor Main

--- src/main/java/step2/Main.java
+++ src/main/java/step2/Main.java
@@ -1,20 +1,25 @@
 package step2;
 
 import step2.controller.LadderController;
-import step2.domain.Line;
-import step2.domain.Lines;
-import step2.domain.Player;
-import step2.domain.Players;
+import step2.domain.*;
 
-import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
         Players players = LadderController.getPlayers();
+        Prizes prizes = LadderController.getPrizes(players.getSize());
         int height = LadderController.getHeight();
         Lines lines = LadderController.createLines(players.getSize(), height);
 
-        LadderController.showResult(players, lines);
+        LadderController.showResult(players, lines, prizes);
+
+        String playerName = LadderController.getPlayerName();
+        Players playersByname = LadderController.getPlayersByname(players, playerName);
+        Players EndPlayers = LadderController.getLadderResults(playersByname, lines);
+        Map<Player, String> award = LadderController.getAward(EndPlayers, prizes);
+
+        LadderController.showAward(award);
     }
 }
