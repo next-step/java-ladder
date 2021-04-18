8d38cfe2bd5d2cbb714a0a15db815d7e751f9119
김우재 <kwj1270@naver.com>
2021-04-18 10:28:51 +0900

    feat:LadderCreateModule 인스턴스 생성 기능 구현

--- src/main/java/ladder/domain/LadderCreateModule.java
+++ src/main/java/ladder/domain/LadderCreateModule.java
@@ -1,4 +1,15 @@
 package ladder.domain;
 
-public class LadderCreateModule {
+public final class LadderCreateModule {
+
+    private static final class LadderCreateModuleHolder {
+        private static final LadderCreateModule ladderCreateModule = new LadderCreateModule();
+    }
+
+    public LadderCreateModule() {
+    }
+
+    public static final LadderCreateModule getInstance() {
+        return LadderCreateModuleHolder.ladderCreateModule;
+    }
 }
