094fd948ed2ddf8338f5ce5d5f046012e6a0c258
keke0123 <keke0123@naver.com>
2021-04-18 18:00:46 +0900

    (feat) names string 으로 변환 기능 추가

--- src/main/java/nextstep/ladder/src/Participants.java
+++ src/main/java/nextstep/ladder/src/Participants.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Participants {
+  private static final int SPACE_SIZE = 6;
+  private static final String SPACE = "";
+
   private final List<User> participants;
 
   public Participants(List<User> participants) {
@@ -29,5 +32,12 @@ public class Participants {
 
   public int numberOfParticipants() {
     return participants.size();
+  }
+
+  public String names() {
+    String str = participants.stream()
+      .map(user -> String.format("%" + SPACE_SIZE + "s", user.name()))
+      .collect(Collectors.joining(""));
+    return SPACE + str;
   }
 }
