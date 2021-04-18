847fa11c95622a3600a80a2aa0bc6910a82c2609
김우재 <kwj1270@naver.com>
2021-04-18 10:38:45 +0900

    test:LadderCreateModule 인스턴스가 사다리를 생성하는지 테스트작성

--- src/test/java/ladder/domain/LadderCreateModuleTest.java
+++ src/test/java/ladder/domain/LadderCreateModuleTest.java
@@ -1,7 +1,11 @@
 package ladder.domain;
 
+import ladder.strategy.LineGenerateStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,4 +21,23 @@ class LadderCreateModuleTest {
         // then
         assertThat(ladderCreateModule).isNotNull();
     }
+
+    @DisplayName("LadderCreateModule 인스턴스가 사다리를 생성하는지 테스트")
+    @Test
+    void 반환_사다리() {
+        // given
+        People people = People.of("a,b,c".split(","));
+        LadderHeight ladderHeight = LadderHeight.valueOf(5);
+        LadderCreationInformation creationInformation = LadderCreationInformation.from(people, ladderHeight);
+        LineGenerateStrategy strategy = () -> true;
+
+        // when
+        LadderCreateModule ladderCreateModule = LadderCreateModule.getInstance();
+        Ladder ladder = ladderCreateModule.create(creationInformation, strategy);
+
+
+        // then
+        assertThat(ladder).isNotNull();
+    }
+
 }
