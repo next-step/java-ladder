2a72614be59fbccd87c806108517573973678873
김우재 <kwj1270@naver.com>
2021-04-18 12:16:22 +0900

    test:LadderResults 인스턴스 null 입력시 예외처리 여부 테스트작성

--- src/main/java/ladder/LadderGame.java
+++ src/main/java/ladder/LadderGame.java
@@ -4,6 +4,7 @@ import ladder.domain.creator.LadderCreator;
 import ladder.domain.ladder.*;
 import ladder.domain.participant.People;
 import ladder.domain.participant.Person;
+import ladder.exception.PeopleAndResultsSizeMissMatchException;
 import ladder.strategy.RandomLineGenerateStrategy;
 import ladder.view.InputView;
 import ladder.view.ResultView;
@@ -19,9 +20,9 @@ public class LadderGame {
     }
 
     public static void main(String[] args) {
-        People people = People.of(INPUT_VIEW.inputParticipantsByClient());
-        LadderResults results = LadderResults.of(INPUT_VIEW.inputLadderResultsByClient());
-        LadderHeight height = LadderHeight.valueOf(INPUT_VIEW.inputLadderHeightByClient());
+        People people = getInputParticipants();
+        LadderResults results = getInputLadderResults(people);
+        LadderHeight height = getInputLadderHeight();
 
         LadderCreationInformation creationInformation = LadderCreationInformation.from(people, height);
 
@@ -34,7 +35,43 @@ public class LadderGame {
         extracted(people, ladderResultBoard);
     }
 
-    private static void extracted(People people, LadderResultBoard ladderResultBoard) {
+
+    private static final LadderResults getInputLadderResults(People people) {
+        try {
+            String[] ladderResults = INPUT_VIEW.inputLadderResultsByClient();
+            validateSize(people, ladderResults);
+            return LadderResults.of(ladderResults);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return getInputLadderResults(people);
+        }
+    }
+
+    private static final void validateSize(People people, String[] ladderResults) {
+        if (ladderResults.length != people.countOfPerson()) {
+            throw new PeopleAndResultsSizeMissMatchException();
+        }
+    }
+
+    private static final People getInputParticipants() {
+        try {
+            return People.of(INPUT_VIEW.inputParticipantsByClient());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return getInputParticipants();
+        }
+    }
+
+    private static final LadderHeight getInputLadderHeight() {
+        try {
+            return LadderHeight.valueOf(INPUT_VIEW.inputLadderHeightByClient());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return getInputLadderHeight();
+        }
+    }
+
+    private static final void extracted(People people, LadderResultBoard ladderResultBoard) {
         String command = INPUT_VIEW.inputResultPersonByClient();
         if (!command.equals("all")) {
             RESULT_VIEW.printResult(Person.of(command), ladderResultBoard);
--- src/test/java/ladder/domain/ladder/LadderResultsTest.java
+++ src/test/java/ladder/domain/ladder/LadderResultsTest.java
@@ -1,9 +1,13 @@
 package ladder.domain.ladder;
 
+import ladder.exception.StringListNullPointerException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LadderResultsTest {
 
@@ -18,5 +22,18 @@ class LadderResultsTest {
 
         // then
         assertThat(ladderResults).isNotNull();
+    }
+
+    @DisplayName("LadderResults 인스턴스 null 입력시 예외처리 여부 테스트")
+    @Test
+    void 검증_null() {
+        // given
+        List<String> results = null
+
+        // when
+        assertThatThrownBy(()-> LadderResults.of(results))
+                .isInstanceOf(StringListNullPointerException.class)
+                .hasMessage("List<String>이 null 입니다.");
+
     }
 }
