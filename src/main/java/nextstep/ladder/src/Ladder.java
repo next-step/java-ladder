782ede02286b9a30ee4810f19d1da2ba6eedc74d
keke0123 <keke0123@naver.com>
2021-04-18 18:16:27 +0900

    (feat) ladder 에 size check 추가 및 테스트

--- src/main/java/nextstep/ladder/src/Ladder.java
+++ src/main/java/nextstep/ladder/src/Ladder.java
@@ -6,6 +6,8 @@ import java.util.stream.IntStream;
 
 public class Ladder {
   private static final String SPACE = "     ";
+  private static final int MIN_HEIGHT = 1;
+  private static final int MIN_WIDTH = 1;
   
   private final List<Floor> ladder;
 
@@ -14,6 +16,7 @@ public class Ladder {
   }
 
   public static Ladder makeLadderBySize(int height, int width) {
+    checkSize(height, width);
     return new Ladder(
       IntStream.range(0,  height)
       .boxed()
@@ -22,9 +25,18 @@ public class Ladder {
     );
   }
 
+  public static void checkSize(int height, int width) {
+    if(height < MIN_HEIGHT) {
+      throw new IllegalArgumentException("height 은 1보다 작을수 없습니다.");
+    }
+    if(width < MIN_WIDTH) {
+      throw new IllegalArgumentException("width 는 1보다 작을수 없습니다.");
+    }
+  }
+
   public String ui() {
     return ladder.stream()
       .map(floor -> SPACE + floor.ui())
       .collect(Collectors.joining("\n"));
-  } 
+  }
 }
