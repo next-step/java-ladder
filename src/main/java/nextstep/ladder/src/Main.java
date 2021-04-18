b41c93f0e618b85311996001c0c1849cdd2ca3c0
keke0123 <keke0123@naver.com>
2021-04-18 18:01:30 +0900

    (feat) main 로직 추가

---
+++ src/main/java/nextstep/ladder/src/Main.java
@@ -0,0 +1,16 @@
+package nextstep.ladder.src;
+
+import nextstep.ladder.src.common.InputView;
+import nextstep.ladder.src.common.ResultView;
+
+public class Main {
+  public static void main(String[] args) {
+    Participants participants = Participants.makeByString(InputView.participants());
+    Ladder ladder = Ladder.makeLadderBySize(InputView.height(), participants.numberOfParticipants() - 1);
+
+    System.out.println("실행결과");
+
+    ResultView.users(participants);
+    ResultView.ladder(ladder);
+  }
+}
