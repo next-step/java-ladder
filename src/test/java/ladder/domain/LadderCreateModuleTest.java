e8ef7cf1c81cdd55e1485f50991ffa17528e2aaf
김우재 <kwj1270@naver.com>
2021-04-18 10:27:02 +0900

    test:LadderCreateModule 인스턴스 생성 여부 테스트 작성

---
+++ src/test/java/ladder/domain/LadderCreateModuleTest.java
@@ -0,0 +1,20 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LadderCreateModuleTest {
+
+    @DisplayName("LadderCreateModule 인스턴스 생성 여부 테스트")
+    @Test
+    void 생성() {
+        // when
+        LadderCreateModule ladderCreateModule = LadderCreateModule.getInstance();
+
+        // then
+        assertThat(ladderCreateModule).isNotNull();
+    }
+}
