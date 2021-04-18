f68c8e35304ce9d16c3487a183310d52d7a97eb0
w-beom <w_bum@naver.com>
2021-04-19 01:20:09 +0900

    test : 테스트코드 작성

--- src/test/java/step2/domain/PointTest.java
+++ src/test/java/step2/domain/PointTest.java
@@ -10,21 +10,15 @@ class PointTest {
     @Test
     @DisplayName("첫번째 포인트 생성 테스트")
     void firstTest() {
-        assertThat(Point.first(new FixedGenerator(false))).isEqualTo(new Point(false));
-        assertThat(Point.first(new FixedGenerator(true))).isEqualTo(new Point(true));
     }
 
     @Test
     @DisplayName("마지막 포인트 생성 테스트")
     void lastTest() {
-        assertThat(Point.last()).isEqualTo(new Point(false));
-        assertThat(Point.last()).isNotEqualTo(new Point(true));
     }
 
     @Test
     @DisplayName("포인트의 true, false 테스트")
     void hasLineTest(){
-        assertThat(new Point(false).hasLine()).isFalse();
-        assertThat(new Point(true).hasLine()).isTrue();
     }
 }
