6d1336191136c2c16be8bf04b74f9ae50d4d1d0e
김우재 <kwj1270@naver.com>
2021-04-18 11:40:18 +0900

    test:LadderCreator 인스턴스가 사다리를 생성하는지 테스트작성

--- src/test/java/ladder/domain/creator/LadderCreatorTest.java
+++ src/test/java/ladder/domain/creator/LadderCreatorTest.java
@@ -1,12 +1,14 @@
 package ladder.domain.creator;
 
-import ladder.domain.ladder.Ladder;
-import ladder.domain.ladder.LadderCreationInformation;
-import ladder.domain.ladder.LadderHeight;
+import ladder.domain.ladder.*;
 import ladder.domain.participant.People;
 import ladder.strategy.LineGenerateStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,8 +28,8 @@ class LadderCreatorTest {
     @Test
     void 반환_사다리() {
         // given
-        People people = People.of("a,b,c".split(","));
-        LadderHeight ladderHeight = LadderHeight.valueOf(5);
+        People people = People.of("a,b".split(","));
+        LadderHeight ladderHeight = LadderHeight.valueOf(2);
         LadderCreationInformation creationInformation = LadderCreationInformation.from(people, ladderHeight);
         LineGenerateStrategy strategy = () -> true;
 
@@ -37,6 +39,28 @@ class LadderCreatorTest {
 
         // then
         assertThat(ladder).isNotNull();
+    }
+
+    @DisplayName("LadderCreator 인스턴스가 사다리를 생성하는지 테스트")
+    @Test
+    void 검증_사다리() {
+        // given
+        People people = People.of("a,b".split(","));
+        LadderHeight ladderHeight = LadderHeight.valueOf(2);
+        LadderCreationInformation creationInformation = LadderCreationInformation.from(people, ladderHeight);
+        LineGenerateStrategy strategy = () -> true;
+
+        List<Line> lines = new ArrayList<>();
+        lines.add(Line.of(Arrays.asList(Point.of(false), Point.of(true))));
+        lines.add(Line.of(Arrays.asList(Point.of(false), Point.of(true))));
+        Ladder expected = Ladder.from(lines);
+
+        // when
+        LadderCreator ladderCreator = LadderCreator.getInstance();
+        Ladder actual = ladderCreator.create(creationInformation, strategy);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
     }
 
 }
