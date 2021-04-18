bdb8ccffa31ad15e2191c523e168b83cad49e721
김우재 <kwj1270@naver.com>
2021-04-18 10:49:49 +0900

    test:LineCreator 인스턴스 생성 여부 테스트작성

---
+++ src/test/java/ladder/domain/LineCreatorTest.java
@@ -0,0 +1,21 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LineCreatorTest {
+
+    @DisplayName("LineCreator 인스턴스 생성 여부 테스트")
+    @Test
+    void 생성() {
+
+        // when
+        LineCreator lineCreator = LineCreator.getInstance();
+
+        // then
+        assertThat(lineCreator).isNotNull();
+    }
+}
