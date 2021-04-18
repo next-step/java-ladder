afc4bece691ff5af18b35f6cf8467125ed05d228
abcdefg <choiyurim@naver.com>
2021-04-18 18:44:46 +0900

    refactor(view): refactoring View

--- src/main/java/step2/view/ResultView.java
+++ src/main/java/step2/view/ResultView.java
@@ -1,10 +1,8 @@
 package step2.view;
 
-import step2.domain.Line;
-import step2.domain.Lines;
-import step2.domain.Player;
-import step2.domain.Players;
+import step2.domain.*;
 
+import java.util.Map;
 import java.util.stream.IntStream;
 
 public class ResultView {
@@ -40,7 +38,28 @@ public class ResultView {
     public static void printPlayer(Players players) {
         players.getPlayers()
                 .stream()
-                .map(Player::getName)
+                .map(Player::getNameWithBlank)
                 .forEach(System.out::print);
+    }
+
+    public static void printPrizes(Prizes prizes) {
+        System.out.println();
+        prizes.getPrizes()
+                .stream()
+                .map(prize -> prize + "    ")
+                .forEach(System.out::print);
+    }
+
+    public static void printLadderAward(Map<Player, String> award) {
+        System.out.println("\n실행 결과");
+        award.entrySet()
+                .stream()
+                .map(awardResult -> {
+                    if (award.size() == 1) {
+                        return awardResult.getValue();
+                    }
+                    return awardResult.getKey().getName() +" : "
+                            + awardResult.getValue();
+                }).forEach(System.out::println);
     }
 }
