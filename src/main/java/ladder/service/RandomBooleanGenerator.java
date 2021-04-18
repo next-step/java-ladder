9405d250fad2527fa3d303ccf062e79dee8395b2
ttokey <ttokey.lee@gmail.com>
2021-04-18 11:57:20 +0900

    refactor : nextInt 형태로 받아오던 randomBoolean을 nextBoolean으로 변경

--- src/main/java/ladder/service/RandomBooleanGenerator.java
+++ src/main/java/ladder/service/RandomBooleanGenerator.java
@@ -3,15 +3,8 @@ package ladder.service;
 import java.util.Random;
 
 public class RandomBooleanGenerator implements RandomBoolean {
-    Random random = new Random();
-    private final int DEFAULT_PROBABILITY = 50;
-    private final int MAX_PROBABILITY = 100;
-
     @Override
     public boolean randomBoolean() {
-        if (random.nextInt(MAX_PROBABILITY) <= DEFAULT_PROBABILITY) {
-            return true;
-        }
-        return false;
+        return new Random().nextBoolean();
     }
 }
