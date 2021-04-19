867e7ce1fd25572f378a8ea8a6d582259504193c
keke0123 <keke0123@naver.com>
2021-04-19 22:38:19 +0900

    (feat) 5글자 넘어가는 이름 error 출력

--- src/main/java/nextstep/ladder/src/User.java
+++ src/main/java/nextstep/ladder/src/User.java
@@ -5,12 +5,19 @@ public class User {
 
   public User (String name) {
     assureNotEmpty(name);
+    checkMaxLength(name);
     this.name = name;
   }
 
-  private static void assureNotEmpty(String str) {
-    if(str == null || str.equals("")) {
+  private static void assureNotEmpty(String name) {
+    if(name == null || name.equals("")) {
       throw new IllegalArgumentException("null or 빈값이 들어올 수 없습니다.");
+    }
+  }
+
+  private static void checkMaxLength(String name) {
+    if(name.length() > 5) {
+      throw new IllegalArgumentException("name 은 5 글자를 넘어갈 수 없습니다.");
     }
   }
 
--- src/test/java/nextstep/ladder/src/UserTest.java
+++ src/test/java/nextstep/ladder/src/UserTest.java
@@ -3,6 +3,7 @@ package nextstep.ladder.src;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
@@ -14,6 +15,14 @@ public class UserTest {
   public void assureNotEmpty(String name) {
     assertThatThrownBy(() -> {
       new User(name);
+    }).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  @DisplayName("check max length")
+  public void checkMaxLength() {
+    assertThatThrownBy(() -> {
+      new User("maxlen");
     }).isInstanceOf(IllegalArgumentException.class);
   }
 }
