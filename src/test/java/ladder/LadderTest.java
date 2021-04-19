f66826a9a7863ea76217b6856dd76048e96ce22f
lee.jaesung <lee.jaesung@joins.com>
2021-04-19 14:29:22 +0900

    test: 사다리 생성 테스트 추가

--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -8,15 +8,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LadderTest {
-    private final Ladder ladder = new Ladder();
+    private Ladder ladder;
 
     @BeforeEach
     void ladder() {
+        List<Line> lines = new ArrayList<>();
+
         int index = 0;
 
         Point point1 = new Point(index++, new Direction(false, true));
@@ -32,8 +36,10 @@ public class LadderTest {
 
         Line ladderLine2 = new Line(Arrays.asList(point4, point5, point6));
 
-        ladder.add(ladderLine1);
-        ladder.add(ladderLine2);
+        lines.add(ladderLine1);
+        lines.add(ladderLine2);
+
+        ladder = new Ladder(lines);
     }
 
     @DisplayName("포인트를 입력 받아 사다리 라인을 그리고 이동결과를 반환한다.")
@@ -42,5 +48,16 @@ public class LadderTest {
         assertThat(ladder.arrivalPoint(0)).isEqualTo(2);
         assertThat(ladder.arrivalPoint(1)).isEqualTo(0);
         assertThat(ladder.arrivalPoint(2)).isEqualTo(1);
+    }
+
+    @DisplayName("2층까지의 사다리 생성 후 첫번째 포인트는 0 또는 1을 반환한다.")
+    @Test
+    void generate() {
+        int pointCount = 4;
+        int lineCount = 2;
+
+        Ladder ladder = Ladder.makeLadder(pointCount, lineCount);
+
+        assertThat(ladder.arrivalPoint(0) == 0 || ladder.arrivalPoint(0) == 1).isTrue();
     }
 }
