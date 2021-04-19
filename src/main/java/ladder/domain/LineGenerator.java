304d3f53bd695abd61704a4ea85c0c9db4f0adbf
jaystar13 <astarisk2@gmail.com>
2021-04-19 22:04:02 +0900

    refactor: private 기본 생성자 추가

--- src/main/java/ladder/domain/LineGenerator.java
+++ src/main/java/ladder/domain/LineGenerator.java
@@ -2,6 +2,9 @@ package ladder.domain;
 
 public class LineGenerator {
 
+    private LineGenerator() {
+    }
+
     public static Line generate(int pointCount) {
         return new Line(PointGenerator.makePoints(pointCount));
     }
