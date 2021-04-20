ddaf7680f30db3c09be5b83dee967d9f50e566e4
HeeSeok Noh <nhs0912@nate.com>
2021-04-20 22:55:29 +0900

    test: CarTest @Display 적용 branch TEST용

--- src/test/java/nextstep/fp/CarTest.java
+++ src/test/java/nextstep/fp/CarTest.java
@@ -1,11 +1,13 @@
 package nextstep.fp;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
     @Test
+    @DisplayName("차가 이동하는 테스트")
     public void 이동() {
         Car car = new Car("pobi", 0);
         Car actual = car.move(new MoveStrategy() {
