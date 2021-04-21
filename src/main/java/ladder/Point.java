9018df5358d5c755d6c6388654f7512f18f7b878
김우재 <kwj1270@naver.com>
2021-04-21 22:32:55 +0900

    feat:Point 인스턴스에 첫번째 지점 생성 테스트작성

--- src/main/java/ladder/Point.java
+++ src/main/java/ladder/Point.java
@@ -6,8 +6,15 @@ public final class Point {
     private final boolean current;
 
     public Point(boolean left, boolean current) {
+        if(left && current) {
+            throw new IllegalArgumentException("상태 값이 유효하지 않습니다.");
+        }
         this.left = left;
         this.current = current;
+    }
+
+    public static final Point first(boolean current) {
+        return new Point(false, current);
     }
 
     public final Direction move() {
--- src/test/java/ladder/PointTest.java
+++ src/test/java/ladder/PointTest.java
@@ -4,9 +4,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PointTest {
+
+    @DisplayName("Point 인스턴스에 첫번째 지점 생성 테스트")
+    @Test
+    void first() {
+        Point point = Point.first(true);
+        assertThat(point.move()).isEqualTo(Direction.RIGHT);
+    }
+
+
+    @DisplayName("Point 인스턴스에 부적절한 값 입력시 예외처리 여부 테스트")
+    @Test
+    void invalid() {
+        assertThatThrownBy(()-> new Point(true, true))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("상태 값이 유효하지 않습니다.");
+    }
 
     @DisplayName("Point 인스턴스의 다음 이동이 Direction.LEFT 인지 확인")
     @Test
