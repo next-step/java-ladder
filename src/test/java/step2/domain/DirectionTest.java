bd73cb9294ab4632a904fd98ef744ef0ce01cae2
Hong WooBeom <hob0961@naonsoft.com>
2021-04-21 20:40:58 +0900

    test : 테스트코드 작성

--- src/test/java/step2/domain/DirectionTest.java
+++ src/test/java/step2/domain/DirectionTest.java
@@ -10,7 +10,7 @@ class DirectionTest {
     @Test
     @DisplayName("첫번째 Direction은 왼쪽은 false여야 한다.")
     void first() {
-        Direction direction = Direction.first(new FixedGenerator(true));
+        Direction direction = Direction.first(true);
         assertThat(direction.isRight()).isTrue();
         assertThat(direction.isLeft()).isFalse();
     }
@@ -18,28 +18,33 @@ class DirectionTest {
     @Test
     @DisplayName("중간 Direction이 왼쪽이 true일경우 오른쪽은 false로 고정생성")
     void middleDirectionTest1() {
-        Direction middleDirection = Direction.middle(true, new FixedGenerator(false));
+        Direction firstDirection = Direction.first(true);
+        Direction middleDirection = firstDirection.middle(false);
         assertThat(middleDirection.isLeft()).isTrue();
         assertThat(middleDirection.isRight()).isFalse();
     }
 
     @Test
-    @DisplayName("마지막 Direction은 오른쪽은 무조건 false로 생성")
+    @DisplayName("마지막 Direction의 오른쪽은 무조건 false로 생성")
     void lastDirectionTest() {
-        Direction lastDirection = Direction.last(true);
+        Direction firstDirection = Direction.first(true);
+        Direction lastDirection = firstDirection.last();
         assertThat(lastDirection.isLeft()).isTrue();
         assertThat(lastDirection.isRight()).isFalse();
     }
 
     @Test
-    @DisplayName("left가 true이면 true")
+    @DisplayName("left가 ture이면 true")
     void isLeftTest() {
-        assertThat(Direction.middle(true, new FixedGenerator(false)).isLeft()).isTrue();
+        Direction firstDirection = Direction.first(true);
+        Direction middleDirection = firstDirection.middle(false);
+        assertThat(middleDirection.isLeft()).isTrue();
     }
 
     @Test
     @DisplayName("right가 true이면 true")
     void isRightTest() {
-        assertThat(Direction.middle(false, new FixedGenerator(true)).isRight()).isTrue();
+        Direction firstDirection = Direction.first(true);
+        assertThat(firstDirection.isRight()).isTrue();
     }
 }
