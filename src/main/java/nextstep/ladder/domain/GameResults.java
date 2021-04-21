5d88ecf38f42b9d77121599102ede0289f1772a8
Dion <ksundong@gmail.com>
2021-04-21 21:41:21 +0900

    test: GameResults 생성 테스트 추가

--- src/main/java/nextstep/ladder/domain/GameResults.java
+++ src/main/java/nextstep/ladder/domain/GameResults.java
@@ -1,7 +1,13 @@
 package nextstep.ladder.domain;
 
+import java.util.Map;
+
 public final class GameResults {
 
   public GameResults() {
+  }
+
+  public GameResults(Map<Person, Result> personResultHashMap) {
+
   }
 }
--- src/test/java/nextstep/ladder/domain/GameResultsTest.java
+++ src/test/java/nextstep/ladder/domain/GameResultsTest.java
@@ -2,6 +2,7 @@ package nextstep.ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashMap;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +12,11 @@ class GameResultsTest {
   @DisplayName("GameResult가 생성된다.")
   void create() {
     assertThat(new GameResults()).isEqualTo(new GameResults());
+  }
+
+  @Test
+  @DisplayName("GameResult는 빈 Map으로 생성된 것과 동일하다.")
+  void createWithEmptyMap() {
+    assertThat(new GameResults(new HashMap<Person, Result>())).isEqualTo(new GameResults());
   }
 }
