d93f034b2d05fd329678787fee40f782a3e65d33
Dion <ksundong@gmail.com>
2021-04-21 20:34:27 +0900

    feat: 게임 결과를 저장하는 클래스 생성 기능 구현

--- src/main/java/nextstep/ladder/domain/GameResult.java
+++ src/main/java/nextstep/ladder/domain/GameResult.java
@@ -1,12 +1,35 @@
 package nextstep.ladder.domain;
 
+import java.util.Objects;
+
 public final class GameResult {
 
-  private GameResult(Person person, Result result) {
+  private final Person person;
+  private final Result result;
 
+  private GameResult(Person person, Result result) {
+    this.person = person;
+    this.result = result;
   }
 
   public static GameResult of(Person person, Result result) {
     return new GameResult(person, result);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    GameResult that = (GameResult) o;
+    return Objects.equals(person, that.person) && Objects.equals(result, that.result);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(person, result);
   }
 }
