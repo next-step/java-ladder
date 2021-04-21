d81b1288710c9ffd3bb5023a2b565b0c711a52fd
Dion <ksundong@gmail.com>
2021-04-21 21:43:28 +0900

    feat: GameResults 생성 기능 구현

--- src/main/java/nextstep/ladder/domain/GameResults.java
+++ src/main/java/nextstep/ladder/domain/GameResults.java
@@ -1,13 +1,35 @@
 package nextstep.ladder.domain;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public final class GameResults {
 
+  private final Map<Person, Result> gameResults;
+
   public GameResults() {
+    this(new HashMap<>());
   }
 
-  public GameResults(Map<Person, Result> personResultHashMap) {
+  public GameResults(Map<Person, Result> gameResults) {
+    this.gameResults = gameResults;
+  }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    GameResults that = (GameResults) o;
+    return Objects.equals(gameResults, that.gameResults);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(gameResults);
   }
 }
