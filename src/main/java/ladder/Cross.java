07741a64b02413bf083ca6e39d456944592c222f
김우재 <kwj1270@naver.com>
2021-04-21 22:47:41 +0900

    test:Cross 인스턴스가 오른쪽으로 이동하는지 테스트작성

---
+++ src/main/java/ladder/Cross.java
@@ -0,0 +1,4 @@
+package ladder;
+
+public class Cross {
+}
---
+++ src/test/java/ladder/CrossTest.java
@@ -0,0 +1,18 @@
+package ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CrossTest {
+
+    @DisplayName("Cross 인스턴스가 오른쪽으로 이동하는지 테스트")
+    @Test
+    void right() {
+        Point right = Point.first(true);
+        Cross cross = new Cross(1, right);
+        assertThat(cross.move()).isEqualTo(2);
+    }
+}
