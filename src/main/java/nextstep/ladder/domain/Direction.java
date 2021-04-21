54603db62585d647e5f470bff9ff7167a81798c5
Dion <ksundong@gmail.com>
2021-04-21 09:18:53 +0900

    feat: Direction이 이동하는 방향을 결정하는 기능 구현

--- src/main/java/nextstep/ladder/domain/Direction.java
+++ src/main/java/nextstep/ladder/domain/Direction.java
@@ -1,7 +1,20 @@
 package nextstep.ladder.domain;
 
+import java.util.function.UnaryOperator;
+
 public enum Direction {
-  NONE,
-  LEFT,
-  RIGHT
+
+  NONE(index -> index),
+  LEFT(index -> index - 1),
+  RIGHT(index -> index + 1);
+
+  private final UnaryOperator<Integer> expression;
+
+  Direction(UnaryOperator<Integer> expression) {
+    this.expression = expression;
+  }
+
+  public int move(final int index) {
+    return expression.apply(index);
+  }
 }
