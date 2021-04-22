2be94b0729789a5fd0b6de87b955570ac29a1fa2
김우재 <kwj1270@naver.com>
2021-04-23 00:15:38 +0900

    feat:Participants 인스턴스 생성 테스트작성

--- src/main/java/ladder/domain/participant/Participants.java
+++ src/main/java/ladder/domain/participant/Participants.java
@@ -1,4 +1,30 @@
 package ladder.domain.participant;
 
-public class Participants {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class Participants {
+
+    private final List<Participant> participants;
+
+    public static final Participants of(String[] names) {
+        return of(mapToParticipantList(names));
+    }
+
+    private static final List<Participant> mapToParticipantList(String[] names) {
+        return Arrays.stream(names)
+                .map(Participant::of)
+                .collect(Collectors.toList());
+    }
+
+    public static final Participants of(List<Participant> participants) {
+        return new Participants(participants);
+    }
+
+    private Participants(List<Participant> participants) {
+        this.participants = participants;
+    }
+
+
 }
