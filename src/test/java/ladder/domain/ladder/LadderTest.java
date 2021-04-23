0bd93f630e1c2fd8c7401d05c8d2583c2359f38d
김우재 <kwj1270@naver.com>
2021-04-23 22:29:08 +0900

    test:Ladder 인스턴스 생성 여부 테스트작성

--- src/test/java/ladder/domain/ladder/LadderTest.java
+++ src/test/java/ladder/domain/ladder/LadderTest.java
@@ -1,7 +1,27 @@
 package ladder.domain.ladder;
 
+import ladder.domain.participant.Participants;
+import ladder.domain.strategy.LineGenerateStrategy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LadderTest {
+
+    @DisplayName("Ladder 인스턴스 생성 여부 테스트")
+    @Test
+    void 생성() {
+        // given
+        Participants participants = Participants.of("a,b,c".split(","));
+        LadderHeight ladderHeight = LadderHeight.valueOf(5);
+        LineGenerateStrategy strategy = () -> true;
+
+        // when
+        Ladder ladder = Ladder.from(participants, ladderHeight, strategy);
+
+        // then
+        assertThat(ladder).isNotNull();
+    }
 
 }
