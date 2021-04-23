166c228f43921ce7a7ec5723642f2e6f68108450
김우재 <kwj1270@naver.com>
2021-04-23 22:32:11 +0900

    feat:Ladder 인스턴스 생성 여부 기능 구현

--- src/main/java/ladder/domain/ladder/Ladder.java
+++ src/main/java/ladder/domain/ladder/Ladder.java
@@ -1,4 +1,29 @@
 package ladder.domain.ladder;
 
-public class Ladder {
+import ladder.domain.participant.Participants;
+import ladder.domain.strategy.LineGenerateStrategy;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public final class Ladder {
+
+    private static final int START_INCLUSIVE = 0;
+    private final List<Line> lines;
+
+    public static final Ladder from(final Participants participants, final LadderHeight ladderHeight, final LineGenerateStrategy strategy) {
+        return from(IntStream.range(START_INCLUSIVE, ladderHeight.height())
+                .mapToObj(i -> Line.of(participants.countOfParticipants(), strategy))
+                .collect(Collectors.toList())
+        );
+    }
+
+    public static final Ladder from(final List<Line> lines) {
+        return new Ladder(lines);
+    }
+
+    private Ladder(final List<Line> lines) {
+        this.lines = lines;
+    }
 }
