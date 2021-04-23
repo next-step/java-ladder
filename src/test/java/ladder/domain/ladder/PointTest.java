deadc39624d2ff78ea3af0385799391c84fe8d99
김우재 <kwj1270@naver.com>
2021-04-23 14:33:16 +0900

    test:Point 인스턴스가 생성 여부 테스트작성

--- src/test/java/ladder/domain/ladder/PointTest.java
+++ src/test/java/ladder/domain/ladder/PointTest.java
@@ -7,9 +7,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class PointTest {
 
+    @DisplayName("Point 인스턴스가 생성 여부 테스트")
+    @Test
+    void 생성() {
+        // given
+        int position = 1;
+        Direction pass = Direction.first(false);
+
+        // when
+        Point point = Point.from(position, pass);
+
+        // then
+        assertThat(point).isNotNull();
+    }
+
+
     @DisplayName("Point 인스턴스가 오른쪽으로 이동하는지 테스트")
     @Test
-    void right() {
+    void 기능_오른쪽_이동() {
         // given
         int position = 1;
         int executed = 2;
@@ -24,7 +39,7 @@ class PointTest {
 
     @DisplayName("Point 인스턴스가 왼쪽으로 이동하는지 테스트")
     @Test
-    void left() {
+    void 기능_왼쪽_이동() {
         // given
         int position = 1;
         int executed = 0;
@@ -40,7 +55,7 @@ class PointTest {
 
     @DisplayName("Point 인스턴스가 그대로 아래로 이동하는지 테스트")
     @Test
-    void pass() {
+    void 기능_아래로_이동() {
         // given
         int position = 1;
         int executed = 1;
