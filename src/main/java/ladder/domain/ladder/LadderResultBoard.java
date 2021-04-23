dd1de04f3dd56e01e1f31950ff889e31519c7cac
김우재 <kwj1270@naver.com>
2021-04-23 23:18:19 +0900

    feat:LadderResultBoard 인스턴스 생성 기능 구현

--- src/main/java/ladder/domain/ladder/LadderResultBoard.java
+++ src/main/java/ladder/domain/ladder/LadderResultBoard.java
@@ -1,4 +1,18 @@
 package ladder.domain.ladder;
 
-public class LadderResultBoard {
+import ladder.domain.participant.Participant;
+
+import java.util.Map;
+
+public final class LadderResultBoard {
+
+    private final Map<Participant, String> resultBoard;
+
+    private LadderResultBoard(final Map<Participant, String> resultBoard) {
+        this.resultBoard = resultBoard;
+    }
+
+    public static final LadderResultBoard of(final Map<Participant, String> resultBoard) {
+        return new LadderResultBoard(resultBoard);
+    }
 }
