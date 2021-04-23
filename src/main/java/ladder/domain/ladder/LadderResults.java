11766a7783c0e0602f1fe62134b81e4cfd5ac5b7
김우재 <kwj1270@naver.com>
2021-04-23 23:07:57 +0900

    feat:LadderResults 인스턴스 생성 여부 기능 구현

--- src/main/java/ladder/domain/ladder/LadderResults.java
+++ src/main/java/ladder/domain/ladder/LadderResults.java
@@ -1,4 +1,27 @@
 package ladder.domain.ladder;
 
-public class LadderResults {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class LadderResults {
+
+    private final List<String> results;
+
+    public static final LadderResults of(final String[] results) {
+        return of(toList(results));
+    }
+
+    private static final List<String> toList(final String[] results) {
+        return Arrays.stream(results).collect(Collectors.toList());
+    }
+
+    public static final LadderResults of(final List<String> results) {
+        return new LadderResults(results);
+    }
+
+    private LadderResults(final List<String> results) {
+        this.results = results;
+    }
+
 }
