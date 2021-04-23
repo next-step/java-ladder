c2c942bbaf79dcee3e1fdcc4254deb60d6e79682
wkdehdlr <wkdehdlr@gmail.com>
2021-04-23 15:27:01 +0900

    [feat]Ladder 구현

--- src/main/java/step2/domain/Ladder.java
+++ src/main/java/step2/domain/Ladder.java
@@ -3,6 +3,9 @@ package step2.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Ladder {
@@ -13,8 +16,25 @@ public class Ladder {
         createLadder(height, countOfPerson, lineStrategy);
     }
 
+    public Result play(Users users) {
+        Map<Integer, Integer> result = IntStream.range(0, users.size())
+            .boxed()
+            .collect(Collectors.toMap(Function.identity(), this::getPrize));
+        return new Result(result);
+    }
+
     public List<Line> value() {
         return this.ladder;
+    }
+
+    private Integer getPrize(Integer col) {
+        int height = ladder.size();
+        int row = 0;
+        while (row != height) {
+            Line line = ladder.get(row++);
+            col = line.next(col);
+        }
+        return col;
     }
 
     private void createLadder(int height, int countOfPerson, LineStrategy lineStrategy) {
