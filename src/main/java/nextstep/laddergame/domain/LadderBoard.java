bbd85ce9a721ced63f6dcb216dcaa8a4ef2f6318
dailyworker <workingssu@gmail.com>
2021-04-23 18:57:45 +0900

    feat : 게임의 참여 결과를 제어하는 LadderBoard 구현

---
+++ src/main/java/nextstep/laddergame/domain/LadderBoard.java
@@ -0,0 +1,42 @@
+package nextstep.laddergame.domain;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LadderBoard {
+  private final Ladder ladder;
+  private final Award award;
+
+  public LadderBoard(Ladder ladder, Award award) {
+    this.ladder = ladder;
+    this.award = award;
+  }
+
+  public Ladder ladder() {
+    return this.ladder;
+  }
+
+  public Award award() {
+    return this.award;
+  }
+
+  public String findOne(Name member) {
+    return award.prize(ladder()
+        .match(member))
+        .name();
+  }
+
+  public Map<Name, Name> findAll() {
+    return IntStream.range(0, ladder.members().size())
+        .boxed()
+        .collect(Collectors.toMap(
+            index -> ladder.members()
+                .names()
+                .get(index),
+            index -> award.prizes(ladder.matches(
+                ladder().members()))
+                .prize(index)
+        ));
+  }
+}
