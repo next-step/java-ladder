5dedcdce3e7cbd934f164d241679b55de0862664
hwangjeonghyeon1 <deert202@naver.com>
2021-04-23 13:04:54 +0900

    [refactor] Ladder에서 ExcutionResult 참조하지 않도록 변경

--- src/main/java/finalRefactor/domain/Ladder.java
+++ src/main/java/finalRefactor/domain/Ladder.java
@@ -6,15 +6,15 @@ public class Ladder {
 
     private final List<LadderLine> lines;
 
-    public Ladder(Height height, int countOfPerson) {
-        this.lines = createLines(height, countOfPerson);
-    }
-
     public Ladder(List<LadderLine> lines) {
         this.lines = lines;
     }
 
-    private List<LadderLine> createLines(Height height, int countOfPerson) {
+    public Ladder(Height height, int countOfPerson) {
+        this(createLines(height, countOfPerson));
+    }
+
+    private static List<LadderLine> createLines(Height height, int countOfPerson) {
         List<LadderLine> lines = new ArrayList<>();
         for (int i = 0; i < height.getValue(); i++) {
             lines.add(new LadderLine(countOfPerson));
@@ -24,30 +24,19 @@ public class Ladder {
     }
 
     public int eachPositionResult(int position) {
+
         for (LadderLine line : lines) {
             position = line.move(position);
         }
         return position;
     }
 
-    public ExecutionResult eachPositionExecutionResult(int position, ExecutionResults results) {
-        for (LadderLine line : lines) {
-            position = line.move(position);
-        }
-
-        return results.getOneResult(position);
-    }
-
-    public ExecutionResults getExecutionResult(ExecutionResults executionResults) {
-        List<ExecutionResult> executionResultsList = new ArrayList<>();
-        for (int i = 0; i < executionResults.size(); i++) {
-            executionResultsList.add(eachPositionExecutionResult(i, executionResults));
-        }
-        return new ExecutionResults(executionResultsList);
-    }
-
     public List<LadderLine> lines() {
         return Collections.unmodifiableList(lines);
+    }
+
+    public int size() {
+        return lines.size();
     }
 
 }
