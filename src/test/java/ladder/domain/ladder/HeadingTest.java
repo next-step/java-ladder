dbd406831fdc3b5032da6ae079babdcedb67c1fc
김우재 <kwj1270@naver.com>
2021-04-23 13:26:11 +0900

    test:Heading enum 부적절한 값 입력시 예외처리 여부 테스트작성

--- src/test/java/ladder/domain/ladder/HeadingTest.java
+++ src/test/java/ladder/domain/ladder/HeadingTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HeadingTest {
@@ -23,12 +24,19 @@ class HeadingTest {
     @DisplayName("Heading enum 적절한 값 입력시 알맞은 열거형 생성 여부 테스트")
     @Test
     void 생성_valueOf() {
-
         assertAll(
                 () -> assertThat(Heading.valueOf(false, true)).isEqualTo(Heading.RIGHT),
                 () -> assertThat(Heading.valueOf(true, false)).isEqualTo(Heading.LEFT),
                 () -> assertThat(Heading.valueOf(false, false)).isEqualTo(Heading.PASS)
         );
+    }
+
+    @DisplayName("Heading enum 부적절한 값 입력시 예외처리 여부 테스트")
+    @Test
+    void 생성_부적절한_값() {
+        assertThatThrownBy(()-> Heading.valueOf(true, true))
+                .isInstanceOf(IllegalBooleanArgumentsException.class)
+                .hasMessage("(true, true)는 알맞은 boolean 타입의 매개변수가 아닙니다.");
     }
 
 
