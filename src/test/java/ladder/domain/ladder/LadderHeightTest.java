bfb68311c367a88879e40a86b2d4b4b3859f260d
김우재 <kwj1270@naver.com>
2021-04-23 21:51:34 +0900

    test:LadderHeight 인스턴스에 음수 값 입력시 예외처리 여부 테스트작성

--- src/test/java/ladder/domain/ladder/LadderHeightTest.java
+++ src/test/java/ladder/domain/ladder/LadderHeightTest.java
@@ -1,9 +1,11 @@
 package ladder.domain.ladder;
 
+import ladder.exception.InputNegativeNumberException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LadderHeightTest {
@@ -19,6 +21,18 @@ class LadderHeightTest {
 
         // then
         assertThat(ladderHeight).isNotNull();
+    }
+
+    @DisplayName("LadderHeight 인스턴스에 음수 값 입력시 예외처리 여부 테스트")
+    @Test
+    void 검증_음수() {
+        // given
+        int height = -1;
+
+        // when and then
+        assertThatThrownBy(() -> LadderHeight.valueOf(height))
+                .isInstanceOf(InputNegativeNumberException.class)
+                .hasMessage("(%-1)이라는 음수의 값이 입력되었습니다.");
     }
 
 }
