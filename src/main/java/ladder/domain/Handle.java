4a3bfb41797f744565856f49f9f2e84471ef8beb
ttokey <ttokey.lee@gmail.com>
2021-04-24 19:42:36 +0900

    feat : Handle Enum valueOf 메소드 구현

--- src/main/java/ladder/domain/Handle.java
+++ src/main/java/ladder/domain/Handle.java
@@ -13,7 +13,18 @@ public enum Handle {
     }
 
     public static Handle valueOf(Handle previousHandle, RandomBoolean randomBoolean) {
+        if (randomBoolean.randomBoolean() && !previousHandle.isExist()) {
+            return Handle.EXIST;
+        }
         return Handle.NON_EXIST;
+    }
+
+    public boolean isExist() {
+        if (this.exposure.equals(EXIST.exposure)) {
+            return true;
+        }
+        return false;
+
     }
 
     public String printHandle() {
