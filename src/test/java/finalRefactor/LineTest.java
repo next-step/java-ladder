37d0177726d5fcc3e09ff8000c199c73a01179e0
hwangjeonghyeon1 <deert202@naver.com>
2021-04-24 10:44:37 +0900

    [refactor] LadderLine 테스트케이스 추가

--- src/test/java/finalRefactor/LineTest.java
+++ src/test/java/finalRefactor/LineTest.java
@@ -1,5 +1,9 @@
 package finalRefactor;
 
+import finalRefactor.domain.Direction;
+import finalRefactor.domain.LadderLine;
+import finalRefactor.domain.Point;
+import finalRefactor.domain.PositionGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import step2.domain.Line;
@@ -14,5 +18,17 @@ public class LineTest {
         Line line = new Line(3);
         System.out.println(line);
         assertThat(line.row().size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("첫번째 라인에 대한 포인트")
+    void firstPoint() {
+        LadderLine ladderLine = new LadderLine();
+        assertThat(ladderLine.first(new PositionGenerator() {
+            @Override
+            public boolean generate() {
+                return true;
+            }
+        })).isEqualTo(new Point(0, Direction.of(false, true)));
     }
 }
