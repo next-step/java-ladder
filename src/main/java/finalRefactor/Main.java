bb6b34e0298d047bb4c4799f4125cac119416452
hwangjeonghyeon1 <deert202@naver.com>
2021-04-24 10:41:17 +0900

    [refactor] 메인 클래스명 변경

--- src/main/java/finalRefactor/Main.java
+++ src/main/java/finalRefactor/Main.java
@@ -2,7 +2,7 @@ package finalRefactor;
 
 import finalRefactor.controller.LadderGame;
 
-public class main {
+public class Main {
 
     public static void main(String[] args) {
         LadderGame game = new LadderGame();
