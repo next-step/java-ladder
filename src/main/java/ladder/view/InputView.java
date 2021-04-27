a91f09f9970994061fb6059c9461233c62d2bf25
gmoon <gmoon@rsupport.com>
2021-04-27 20:23:29 +0900

    refactor Scanner 오버 헤드 고려하여 스태틱 변수로 선언

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -5,22 +5,25 @@ import ladder.domain.Players;
 import java.util.Scanner;
 
 public final class InputView {
+  private static final Scanner SCANNER;
   private static final String PLAYERS_INPUT_QUESTION = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
   private static final String HEIGHT_INPUT_QUESTION = "최대 사다리 높이는 몇 개인가요?";
+
+  static {
+    SCANNER = new Scanner(System.in);
+  }
 
   private InputView() {
   }
 
   public static Players playerNames() {
     System.out.println(PLAYERS_INPUT_QUESTION);
-    Scanner sc = new Scanner(System.in);
-    String inputString = sc.nextLine();
-    return Players.create(inputString);
+    return Players.create(SCANNER.nextLine());
   }
 
   public static int height() {
     System.out.println(HEIGHT_INPUT_QUESTION);
-    return new Scanner(System.in).nextInt();
+    return SCANNER.nextInt();
   }
 
 
