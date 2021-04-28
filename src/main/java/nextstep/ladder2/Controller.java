dc7923a7ee3d3dd85e5cdd85c221d3d027b2a692
Dongjoon Park <djoon@users.noreply.github.com>
2021-04-28 17:21:36 +0900

    refactor: 기존 Controller와 View 수정

---
+++ src/main/java/nextstep/ladder2/Controller.java
@@ -0,0 +1,30 @@
+package nextstep.ladder2;
+
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+import nextstep.ladder2.domain.NxLine;
+
+public class Controller {
+
+    public List<String> convertToLadderViewDto(List<NxLine> lines) {
+        return lines.stream()
+                .map(NxLine::getRawValue)
+                .map(this::convertToLineViewDto)
+                .collect(Collectors.toList());
+    }
+
+    private String convertToLineViewDto(List<Boolean> line) {
+        StringJoiner stringJoiner = new StringJoiner(",");
+        for(int i = 0; i < line.size(); i++) {
+            convertToStringIfTrue(line.get(i), i, stringJoiner);
+        }
+        return stringJoiner.toString();
+    }
+
+    private void convertToStringIfTrue(boolean bool, int index, StringJoiner stringJoiner) {
+        if (bool) {
+            stringJoiner.add(String.valueOf(index));
+        }
+    }
+}
---
+++ src/main/java/nextstep/ladder2/view/ResultView.java
@@ -0,0 +1,59 @@
+package nextstep.ladder2.view;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ResultView {
+    public static void printLadderResult(List<String> result, int countOfPerson) {
+        result.stream()
+                .map(line -> drawLadderByLine(line, countOfPerson))
+                .forEach(System.out::println);
+    }
+
+    public static String drawLadderByLine(String mapLine, int countOfPerson) {
+        List<String> tokens = Arrays.asList(mapLine.split(","));
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("     ");
+        for (int i = 0; i < countOfPerson - 1; i++) {
+            stringBuilder.append("|");
+            stringBuilder.append(drawBridge(tokens, i));
+        }
+        stringBuilder.append("|");
+
+        return stringBuilder.toString();
+    }
+
+    private static String drawBridge(List<String> tokens, int index) {
+        if (tokens.contains(String.valueOf(index))) {
+            return "-----";
+        }
+        return "     ";
+    }
+
+    public static void printResultList(List<String> result) {
+        System.out.println("실행 결과");
+        result.stream()
+                .forEach(System.out::println);
+    }
+
+    public static void printObjectsName(List<String> names) {
+        names.stream()
+                .map(ResultView::paddingName)
+                .forEach(System.out::print);
+        System.out.printf("%n");
+    }
+
+    private static String paddingName(String name) {
+        if (name.length() == 5) {
+            return " " + name;
+        }
+        int paddingCount = 5 - name.length();
+        String padding = " ";
+
+        for (int i = 0; i < paddingCount; i++) {
+            padding += " ";
+        }
+        return padding + name;
+    }
+}
