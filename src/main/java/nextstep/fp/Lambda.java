848e5d3523c0a596440fdcdf3ba1ac9845bfd193
hongboram <06boram20@naver.com>
2021-04-29 23:14:21 +0900

    feat Lambda 실습

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -5,52 +5,42 @@ import java.util.List;
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
         System.out.println("printAllOld");
-
-        for (int number : numbers) {
-            System.out.println(number);
-        }
+        numbers.forEach(number -> System.out.println(number));
     }
 
     public static void printAllLambda(List<Integer> numbers) {
         System.out.println("printAllLambda");
-
         numbers.forEach(System.out::println);
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> System.out.println("Hello from thread")).start();
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+
+        return sum(numbers, number -> true);
+        /*return numbers.stream()
+                .filter()
+                .reduce(0, Integer::sum);*/
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sum(numbers, number -> number % 2 == 0);
+        /*8return numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .reduce(0, Integer::sum);*/
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+
+        return sum(numbers, number -> number > 3);
+        /*return numbers.stream()
+                .filter(number -> number > 3)
+                .reduce(0, Integer::sum);*/
+    }
+
+    private static int sum(List<Integer> numbers, Conditional conditional) {
+        return numbers.stream().filter(conditional::isAllowAdd).reduce(0, Integer::sum);
     }
 }
