40ff1dfa801d78806b3b4dd524eab962b4a65781
hongboram <06boram20@naver.com>
2021-04-29 23:18:00 +0900

    fix Lambda 실습

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -5,12 +5,12 @@ import java.util.List;
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
         System.out.println("printAllOld");
-        numbers.forEach(number -> System.out.println(number));
+        numbers.forEach(Lambda::accept);
     }
 
     public static void printAllLambda(List<Integer> numbers) {
         System.out.println("printAllLambda");
-        numbers.forEach(System.out::println);
+        numbers.forEach(Lambda::accept);
     }
 
     public static void runThread() {
@@ -18,29 +18,24 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-
         return sum(numbers, number -> true);
-        /*return numbers.stream()
-                .filter()
-                .reduce(0, Integer::sum);*/
     }
 
     public static int sumAllEven(List<Integer> numbers) {
         return sum(numbers, number -> number % 2 == 0);
-        /*8return numbers.stream()
-                .filter(number -> number % 2 == 0)
-                .reduce(0, Integer::sum);*/
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-
         return sum(numbers, number -> number > 3);
-        /*return numbers.stream()
-                .filter(number -> number > 3)
-                .reduce(0, Integer::sum);*/
     }
 
     private static int sum(List<Integer> numbers, Conditional conditional) {
-        return numbers.stream().filter(conditional::isAllowAdd).reduce(0, Integer::sum);
+        return numbers.stream()
+                .filter(conditional::isAllowAdd)
+                .reduce(0, Integer::sum);
+    }
+
+    private static void accept(Integer number) {
+        System.out.println(number);
     }
 }
