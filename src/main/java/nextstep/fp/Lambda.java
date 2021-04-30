6524900ed53ff2041486f1ad2861de028c75a8d4
whdudtlr9042 <ys9042@gmail.com>
2021-05-01 02:19:13 +0900

    [사다리 - 1단계] 리뷰 요청합니다.

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -26,31 +26,23 @@ public class Lambda {
         }).start();
     }
 
+    public static int lambdaConditional(List<Integer> numbers, Conditional c) {
+        return numbers.stream()
+                .filter(c::test)
+                .mapToInt(number -> number)
+                .sum();
+
+    }
+
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return lambdaConditional(numbers, number -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return lambdaConditional(numbers, number -> (number%2 == 0));
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return lambdaConditional(numbers, number -> number >3);
     }
 }
--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,8 @@
 package nextstep.optional;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,7 +36,10 @@ public class User {
     }
 
     public static boolean ageIsInRange2(User user) {
-        return false;
+        return Optional.ofNullable(user)
+                .map(User::getAge)
+                .filter(age -> age >= 30 && age <= 45)
+                .isPresent();
     }
 
     @Override
