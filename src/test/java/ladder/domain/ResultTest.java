e840d297dd6bd62568dd077ba19b6a0a7789a4ed
gmoon <gmoon@rsupport.com>
2021-05-02 13:46:06 +0900

    test Result get 특정 사용자의 사다리 결과를 반환 테스트 코드 추가

--- src/test/java/ladder/domain/ResultTest.java
+++ src/test/java/ladder/domain/ResultTest.java
@@ -1,9 +1,11 @@
 package ladder.domain;
 
+import ladder.rule.LineRule;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class ResultTest {
 
@@ -23,12 +25,17 @@ class ResultTest {
   }
 
   @Test
-  @DisplayName("")
+  @DisplayName("특정 사용자의 사다리 결과 문자열을 반환한다.")
   void get() {
     // given
-    Result result = Result.generate("꽝,5000,꽝,3000");
+    Players players = Players.create("gmoon,test");
+    Record record = Record.generate(players, Ladder.generate(1, players.totalSize() - 1, LineRule.drawing()));
 
     // then
-//    Record record = Record.generate();
+    Result actual = Result.generate("꽝,5000");
+
+    // then
+    assertAll(() -> assertThat(actual.get(record, Player.create("gmoon"))).isEqualTo("5000"),
+            () -> assertThat(actual.get(record, Player.create("test"))).isEqualTo("꽝"));
   }
 }
