24ac89037bfa4d43219441779b8d3132a525ca6a
dailyworker <workingssu@gmail.com>
2021-05-03 23:49:26 +0900

    refactor : 필요 없는 부분 제거

--- src/main/java/nextstep/laddergame/LineGenerator.java
+++ src/main/java/nextstep/laddergame/LineGenerator.java
@@ -1,6 +1,5 @@
 package nextstep.laddergame;
 
-import java.util.List;
 import java.util.stream.Collector;
 import java.util.stream.Stream;
 import nextstep.laddergame.domain.Line;
@@ -20,16 +19,5 @@ public class LineGenerator {
                               ProtoLine::add,
                               ProtoLine::merge,
                               ProtoLine::toLine));
-  }
-
-  private static boolean isRowContinuousTrue(List<Boolean> points, int index) {
-    return index != 0 && points.get(index - 1);
-  }
-
-  private static boolean drawLine(Boolean isRowContinuous, DrawStrategy strategy) {
-    if (isRowContinuous) {
-      return false;
-    }
-    return strategy.isLine();
   }
 }
