3d87b757ec09fc2925a535a8b663494a87507339
0hun <whdudgns2654@gmail.com>
2021-05-31 00:42:10 +0900

    feat : ageIsInRange2() 구현
    ageIsInRange2() 구현
    - Optional을 활용하여 조건에 맞는 객체가 존재하는지 확인하는 메소드 구현

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,69 +1,83 @@
 package nextstep.optional;
 
+import java.util.Optional;
+
 public class User {
-    private String name;
-    private Integer age;
 
-    public User(String name, Integer age) {
-        this.name = name;
-        this.age = age;
+  private final String name;
+  private final Integer age;
+
+  public User(String name, Integer age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public boolean matchName(String name) {
+    return this.name.equals(name);
+  }
+
+  public static boolean ageIsInRange1(User user) {
+    boolean isInRange = false;
+
+    if (user != null && user.getAge() != null
+        && (user.getAge() >= 30
+        && user.getAge() <= 45)) {
+      isInRange = true;
     }
+    return isInRange;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public static boolean ageIsInRange2(User user) {
+    return Optional.ofNullable(user)
+        .filter(user1 -> user1.getAge() != null)
+        .filter(user1 -> user1.getAge() >= 30)
+        .filter(user1 -> user1.getAge() <= 45)
+        .isPresent();
+  }
 
-    public Integer getAge() {
-        return age;
-    }
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((age == null) ? 0 : age.hashCode());
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    return result;
+  }
 
-    public boolean matchName(String name) {
-        return this.name.equals(name);
-    }
-
-    public static boolean ageIsInRange1(User user) {
-        boolean isInRange = false;
-
-        if (user != null && user.getAge() != null
-                && (user.getAge() >= 30
-                && user.getAge() <= 45)) {
-            isInRange = true;
+  @Override
+  public boolean equals(Object obj) {
+      if (this == obj) {
+          return true;
+      }
+      if (obj == null) {
+          return false;
+      }
+      if (getClass() != obj.getClass()) {
+          return false;
+      }
+    User other = (User) obj;
+    if (age == null) {
+        if (other.age != null) {
+            return false;
         }
-        return isInRange;
-    }
-
-    public static boolean ageIsInRange2(User user) {
+    } else if (!age.equals(other.age)) {
         return false;
     }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((age == null) ? 0 : age.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
+    if (name == null) {
+        if (other.name != null) {
+            return false;
+        }
+    } else if (!name.equals(other.name)) {
+        return false;
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        User other = (User) obj;
-        if (age == null) {
-            if (other.age != null)
-                return false;
-        } else if (!age.equals(other.age))
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        return true;
-    }
+    return true;
+  }
 }
