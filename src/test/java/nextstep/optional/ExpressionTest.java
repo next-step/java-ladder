f898b7082197ffec1661a1b8fa0f6903e7cf4527
0hun <whdudgns2654@gmail.com>
2021-05-31 00:52:51 +0900

    refactor : Expression of() 리펙토링
    Expression of() 리펙토링
    - stream과 Optional을 사용하도록 리팩토링
    - 값이 없을 경우 예외를 던지도록 리펙토링

--- src/test/java/nextstep/optional/ExpressionTest.java
+++ src/test/java/nextstep/optional/ExpressionTest.java
@@ -7,16 +7,17 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 
 public class ExpressionTest {
-    @Test
-    public void of() {
-        assertThat(Expression.PLUS == Expression.of("+")).isTrue();
-    }
 
-    @Test
-    public void notValidExpression() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> {
-                    Expression.of("&");
-                });
-    }
+  @Test
+  public void of() {
+    assertThat(Expression.PLUS == Expression.of("+")).isTrue();
+  }
+
+  @Test
+  public void notValidExpression() {
+    assertThatIllegalArgumentException()
+        .isThrownBy(() -> {
+          Expression.of("&");
+        });
+  }
 }
