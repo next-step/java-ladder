0e367925685fb658cf1b26a679266217bb7db548
0hun <whdudgns2654@gmail.com>
2021-05-30 23:41:41 +0900

    feat(test) : sumOverThreeAndDouble() 구현
    sumOverThreeAndDouble() 구현
    - List에 담긴 모든 숫자 중 3보다 큰 숫자를 2배 한 후 모든 값의 합을 구하도록 구현

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -10,35 +10,44 @@ import java.util.stream.Collectors;
 
 public class StreamStudy {
 
-    public static long countWords() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+  public static long countWords() throws IOException {
+    String contents = new String(
+        Files.readAllBytes(Paths.get("src/main/resources/fp/war-and-peace.txt")),
+        StandardCharsets.UTF_8);
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+    List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+
+    long count = 0;
+
+    for (String w : words) {
+      if (w.length() > 12) {
+        count++;
+      }
     }
 
-    public static void printLongestWordTop100() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+    return count;
+  }
 
-        // TODO 이 부분에 구현한다.
-    }
+  public static void printLongestWordTop100() throws IOException {
+    String contents = new String(Files.readAllBytes(Paths
+        .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
 
-    public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
-    }
+    List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-    public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
-    }
+    // TODO 이 부분에 구현한다.
+  }
 
-    public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
-    }
+  public static List<Integer> doubleNumbers(List<Integer> numbers) {
+    return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
+  }
+
+  public static long sumAll(List<Integer> numbers) {
+    return numbers.stream().reduce(0, (x, y) -> x + y);
+  }
+
+  public static long sumOverThreeAndDouble(List<Integer> numbers) {
+    return numbers.stream().filter(number -> number > 3)
+        .mapToInt(number -> number * 2)
+        .sum();
+  }
 }
