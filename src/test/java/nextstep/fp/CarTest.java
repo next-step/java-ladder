eb3a88717aed56602d74f2a079fcbc3728c87056
0hun <whdudgns2654@gmail.com>
2021-05-31 12:11:29 +0900

    Step1 리뷰 부탁드립니다. (#969)
    * refactor : 익명 클래스를 람다로 전환
    
    익명 클래스를 람다로 전환
    - 익명 클래스를 람다로 전환 코드를 줄여서 표현
    
    * refactor : 람다를 활용하여 중복 제거
    
    람다를 활용하여 중복 제거
    - 인터페이스를 활용하여 람다 적용 결과 중복 제거
    
    * feat(test) : sumOverThreeAndDouble() 구현
    
    sumOverThreeAndDouble() 구현
    - List에 담긴 모든 숫자 중 3보다 큰 숫자를 2배 한 후 모든 값의 합을 구하도록 구현
    
    * feat : printLongestWordTop100() 구현
    
    printLongestWordTop100() 구현
    - 람다와 스트림을 활용하여 메소드 구현
    
    * feat : ageIsInRange2() 구현
    
    ageIsInRange2() 구현
    - Optional을 활용하여 조건에 맞는 객체가 존재하는지 확인하는 메소드 구현
    
    * refactor : getUser() 리펙토링
    
    getUser() 리펙토링
    - stream과 Optional을 사용하도록 리팩토링
    
    * refactor : Expression of() 리펙토링
    
    Expression of() 리펙토링
    - stream과 Optional을 사용하도록 리팩토링
    - 값이 없을 경우 예외를 던지도록 리펙토링

--- src/test/java/nextstep/fp/CarTest.java
+++ src/test/java/nextstep/fp/CarTest.java
@@ -5,27 +5,28 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
-    @Test
-    public void 이동() {
-        Car car = new Car("pobi", 0);
-        Car actual = car.move(new MoveStrategy() {
-            @Override
-            public boolean isMovable() {
-                return true;
-            }
-        });
-        assertThat(actual).isEqualTo(new Car("pobi", 1));
-    }
 
-    @Test
-    public void 정지() {
-        Car car = new Car("pobi", 0);
-        Car actual = car.move(new MoveStrategy() {
-            @Override
-            public boolean isMovable() {
-                return false;
-            }
-        });
-        assertThat(actual).isEqualTo(new Car("pobi", 0));
-    }
+  @Test
+  public void 이동() {
+    //given
+    Car car = new Car("pobi", 0);
+
+    //when
+    Car actual = car.move(() -> true);
+
+    //ten
+    assertThat(actual).isEqualTo(new Car("pobi", 1));
+  }
+
+  @Test
+  public void 정지() {
+    //given
+    Car car = new Car("pobi", 0);
+
+    //when
+    Car actual = car.move(() -> false);
+
+    //ten
+    assertThat(actual).isEqualTo(new Car("pobi", 0));
+  }
 }
