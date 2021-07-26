75f0d8439e07a0a2eb2794ab6cb35ed31f0bbf8a
nokchax <nokchax@gmail.com>
2021-07-26 20:19:15 +0900

    refactor: change anonymous class to lambda

--- src/test/java/nextstep/fp/CarTest.java
+++ src/test/java/nextstep/fp/CarTest.java
@@ -5,17 +5,20 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
+    private static final MoveStrategy ALWAYS_MOVE_STRATEGY = () -> true;
+    private static final MoveStrategy NEVER_MOVE_STRATEGY = () -> false;
+
     @Test
     public void 이동() {
         Car car = new Car("pobi", 0);
-        Car actual = car.move(() -> true);
+        Car actual = car.move(ALWAYS_MOVE_STRATEGY);
         assertThat(actual).isEqualTo(new Car("pobi", 1));
     }
 
     @Test
     public void 정지() {
         Car car = new Car("pobi", 0);
-        Car actual = car.move(() -> false);
+        Car actual = car.move(NEVER_MOVE_STRATEGY);
         assertThat(actual).isEqualTo(new Car("pobi", 0));
     }
 }
