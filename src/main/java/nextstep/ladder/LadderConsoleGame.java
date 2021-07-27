ad4f8840f9105c256a158af6bfb693e922435ad3
nokchax <nokchax@gmail.com>
2021-07-27 22:44:38 +0900

    [G] feat: complete imple console game structure

--- src/main/java/nextstep/ladder/LadderConsoleGame.java
+++ src/main/java/nextstep/ladder/LadderConsoleGame.java
@@ -1,8 +1,23 @@
 package nextstep.ladder;
 
+import nextstep.ladder.domain.init.LadderGameInitInfo;
+import nextstep.ladder.domain.init.LadderInitInfo;
+import nextstep.ladder.domain.init.LadderSize;
+import nextstep.ladder.domain.player.Players;
+import nextstep.ladder.view.InputView;
+import nextstep.ladder.view.ResultView;
+
 public class LadderConsoleGame {
 
     public static void main(String[] args) {
+        Players players = InputView.inputParticipants();
+        int ladderHeight = InputView.inputLadderHeight();
+        LadderSize ladderSize = LadderSize.of(players.getNames().size(), ladderHeight);
+        LadderInitInfo ladderInitInfo = LadderInitInfo.init(ladderSize, () -> true);
+        LadderGameInitInfo ladderGameInitInfo = LadderGameInitInfo.of(ladderInitInfo, players);
 
+        LadderGame ladderGame = LadderGame.init(ladderGameInitInfo);
+
+        ResultView.printResult(ladderGame.result());
     }
 }
--- src/main/java/nextstep/ladder/view/ResultView.java
+++ src/main/java/nextstep/ladder/view/ResultView.java
@@ -1,9 +1,52 @@
 package nextstep.ladder.view;
 
+import nextstep.ladder.dto.LadderResult;
+import nextstep.ladder.dto.RowDto;
+
+import java.util.List;
+
 public class ResultView {
     private static final String RESULT_STATEMENT = "실행결과";
+    private static final String NAME_FORMAT = "%6s";
+    private static final String COLUMN_WITH_STEP = "|-----";
+    private static final String COLUMN_WITHOUT_STEP = "|     ";
 
     private ResultView() {}
 
+    public static void printResult(LadderResult result) {
+        printStatementAndNewLine(RESULT_STATEMENT);
 
+        printPlayers(result.getNames());
+        printLadder(result.getRows());
+    }
+
+    private static void printLadder(List<RowDto> rowDtos) {
+        rowDtos.forEach(ResultView::printSteps);
+    }
+
+    private static void printSteps(RowDto rowDto) {
+        rowDto.getSteps()
+                .forEach(step -> printStatement(step ? COLUMN_WITH_STEP : COLUMN_WITHOUT_STEP));
+        newLine();
+    }
+
+    private static void printPlayers(List<String> names) {
+        names.stream()
+                .map(name -> String.format(NAME_FORMAT, name))
+                .forEach(ResultView::printStatement);
+        newLine();
+    }
+
+    private static void newLine() {
+        System.out.println();
+    }
+
+    private static void printStatement(String statement) {
+        System.out.print(statement);
+    }
+
+    private static void printStatementAndNewLine(String statement) {
+        printStatement(statement);
+        newLine();
+    }
 }
