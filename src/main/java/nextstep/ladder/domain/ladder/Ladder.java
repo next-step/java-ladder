af338a056739c29790cc90458a6194dff91606d8
nokchax <nokchax@gmail.com>
2021-07-27 21:13:03 +0900

    [G] feat: impl init ladder

--- src/main/java/nextstep/ladder/domain/ladder/Ladder.java
+++ src/main/java/nextstep/ladder/domain/ladder/Ladder.java
@@ -2,9 +2,21 @@ package nextstep.ladder.domain.ladder;
 
 import nextstep.ladder.domain.init.LadderInitInfo;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Ladder {
 
-    public static Object init(LadderInitInfo ladderInitInfo2X2) {
-        return null;
+    private final List<Row> rows;
+
+    private Ladder(LadderInitInfo ladderInitInfo) {
+        rows = Stream.generate(() -> Row.init(ladderInitInfo))
+                .limit(ladderInitInfo.getLadderHeight())
+                .collect(Collectors.toList());
+    }
+
+    public static Object init(LadderInitInfo ladderInitInfo) {
+        return new Ladder(ladderInitInfo);
     }
 }
