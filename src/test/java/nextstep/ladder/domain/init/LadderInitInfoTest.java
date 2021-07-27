3bab94e0b5d24df20cdea5ebee674e42737dff82
nokchax <nokchax@gmail.com>
2021-07-27 20:29:43 +0900

    [R] test: test init exception when input value is null

--- src/test/java/nextstep/ladder/domain/init/LadderInitInfoTest.java
+++ src/test/java/nextstep/ladder/domain/init/LadderInitInfoTest.java
@@ -1,11 +1,18 @@
 package nextstep.ladder.domain.init;
 
+import nextstep.ladder.domain.strategy.StepGenerateStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static nextstep.ladder.domain.Fixture.ALWAYS_GENERATE_STRATEGY;
 import static nextstep.ladder.domain.Fixture.LADDER_SIZE_2_X_2;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("사다리 초기화 정보를 담은 클래스 테스트")
 class LadderInitInfoTest {
@@ -14,5 +21,20 @@ class LadderInitInfoTest {
     @Test
     void init() {
         assertThat(LadderInitInfo.init(LADDER_SIZE_2_X_2, ALWAYS_GENERATE_STRATEGY)).isInstanceOf(LadderInitInfo.class);
+    }
+
+    @DisplayName("사다리 초기화 정보 중 null 값이 있다면 예외를 발생 시킨다")
+    @MethodSource
+    @ParameterizedTest
+    void initException(LadderSize ladderSize, StepGenerateStrategy stepGenerateStrategy) {
+        assertThatThrownBy(() -> LadderInitInfo.init(ladderSize, stepGenerateStrategy)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> initException() {
+        return Stream.of(
+                Arguments.of(null, ALWAYS_GENERATE_STRATEGY),
+                Arguments.of(LADDER_SIZE_2_X_2, null),
+                Arguments.of(null, null)
+        );
     }
 }
