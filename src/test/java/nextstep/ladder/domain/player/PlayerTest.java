43a2549f1b24f1c89850d81c6b0e210a320ab8e4
nokchax <nokchax@gmail.com>
2021-07-27 21:35:25 +0900

    [R] test: test throw exception when name over 5

--- src/test/java/nextstep/ladder/domain/player/PlayerTest.java
+++ src/test/java/nextstep/ladder/domain/player/PlayerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("게임 참가자 를 표현하는 플레이어 클래스 테스트")
 class PlayerTest {
@@ -14,4 +15,9 @@ class PlayerTest {
         assertThat(Player.init("name")).isInstanceOf(Player.class);
     }
 
+    @DisplayName("플레이어 이름이 5글자를 초과하는 경우 예외를 발생 시킨다")
+    @Test
+    void initException() {
+        assertThatThrownBy(() -> Player.init("nokchax")).isInstanceOf(IllegalArgumentException.class);
+    }
 }
