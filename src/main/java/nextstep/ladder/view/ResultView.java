5e96e8573154668e4b4f026a582f448da0017471
nokchax <nokchax@gmail.com>
2021-07-27 23:22:29 +0900

    [B] refactor: result view more flexible

--- src/main/java/nextstep/ladder/view/ResultView.java
+++ src/main/java/nextstep/ladder/view/ResultView.java
@@ -4,16 +4,25 @@ import nextstep.ladder.dto.LadderResult;
 import nextstep.ladder.dto.RowDto;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static nextstep.ladder.domain.player.Player.MAXIMUM_LENGTH_OF_NAME;
 
 public class ResultView {
+    private static final int PADDING = 1;
+    private static final String STEP_MARK = "-";
+    private static final String BLANK_MARK = " ";
+    private static final String COLUMN_MARK = "|";
     private static final String RESULT_STATEMENT = "실행결과";
-    private static final String NAME_FORMAT = "%6s";
-    private static final String COLUMN_WITH_STEP = "|-----";
-    private static final String COLUMN_WITHOUT_STEP = "|     ";
+    private static final String NAME_FORMAT = "%" + MAXIMUM_LENGTH_OF_NAME + PADDING + "s";
+    private static final String COLUMN_WITH_STEP = COLUMN_MARK + drawStep(STEP_MARK);
+    private static final String COLUMN_WITHOUT_STEP = COLUMN_MARK + drawStep(BLANK_MARK);
 
     private ResultView() {}
 
     public static void printResult(LadderResult result) {
+        Stream.generate(() -> STEP_MARK).limit(MAXIMUM_LENGTH_OF_NAME).collect(Collectors.joining());
         printStatementAndNewLine(RESULT_STATEMENT);
 
         printPlayers(result.getNames());
@@ -48,5 +57,11 @@ public class ResultView {
     private static void printStatementAndNewLine(String statement) {
         printStatement(statement);
         newLine();
+    }
+
+    private static String drawStep(String mark) {
+        return Stream.generate(() -> mark)
+                .limit(MAXIMUM_LENGTH_OF_NAME)
+                .collect(Collectors.joining());
     }
 }
