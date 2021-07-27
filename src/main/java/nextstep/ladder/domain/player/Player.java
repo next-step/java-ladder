2ac02e652eeb918521ac12b2be2a86f18bae9345
nokchax <nokchax@gmail.com>
2021-07-27 21:45:00 +0900

    [B] refactor: use string utils to check string is empty

--- src/main/java/nextstep/ladder/domain/player/Player.java
+++ src/main/java/nextstep/ladder/domain/player/Player.java
@@ -1,8 +1,9 @@
 package nextstep.ladder.domain.player;
 
-import java.util.Objects;
+import nextstep.util.StringUtils;
 
 public class Player {
+    public static final int MAXIMUM_LENGTH_OF_NAME = 5;
     private final String name;
 
     private Player(String name) {
@@ -12,16 +13,20 @@ public class Player {
     }
 
     private void validate(String name) {
-        if (Objects.isNull(name) || name.isEmpty()) {
+        if (StringUtils.isEmpty(name)) {
             throw new IllegalArgumentException("Name can't be null or empty");
         }
 
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("Name should less than or equal to 5");
+        if (name.length() > MAXIMUM_LENGTH_OF_NAME) {
+            throw new IllegalArgumentException("Name should less than or equal to " + MAXIMUM_LENGTH_OF_NAME);
         }
     }
 
     public static Player init(String name) {
         return new Player(name);
+    }
+
+    public String getName() {
+        return name;
     }
 }
