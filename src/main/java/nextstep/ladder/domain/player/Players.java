ec49dad6ac2be27cf74bdfe2fd93d6e1edc89bed
nokchax <nokchax@gmail.com>
2021-07-27 21:50:18 +0900

    [G] feat: impl players init

--- src/main/java/nextstep/ladder/domain/player/Players.java
+++ src/main/java/nextstep/ladder/domain/player/Players.java
@@ -1,9 +1,19 @@
 package nextstep.ladder.domain.player;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Players {
-    public static Object init(List<String> asList) {
-        return null;
+    private List<Player> players;
+
+    private Players(List<String> names) {
+        players = names.stream()
+                .map(Player::from)
+                .collect(Collectors.toList());
     }
+
+    public static Players init(List<String> names) {
+        return new Players(names);
+    }
+
 }
