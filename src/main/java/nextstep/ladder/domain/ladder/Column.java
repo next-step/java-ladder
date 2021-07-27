346e3eef54b83a267f12ff5b89638d20d0053756
nokchax <nokchax@gmail.com>
2021-07-27 18:53:00 +0900

    [R]test: test init column

--- src/main/java/nextstep/ladder/domain/ladder/Column.java
+++ src/main/java/nextstep/ladder/domain/ladder/Column.java
@@ -1,4 +1,9 @@
 package nextstep.ladder.domain.ladder;
 
+import nextstep.ladder.domain.strategy.StepGenerateStrategy;
+
 public class Column {
+    public static Object init(StepGenerateStrategy alwaysGenerateStrategy) {
+        return null;
+    }
 }
--- src/test/java/nextstep/ladder/domain/ladder/ColumnTest.java
+++ src/test/java/nextstep/ladder/domain/ladder/ColumnTest.java
@@ -2,12 +2,15 @@ package nextstep.ladder.domain.ladder;
 
 import org.junit.jupiter.api.DisplayName;
 
+import static nextstep.ladder.domain.Fixture.ALWAYS_GENERATE_STRATEGY;
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DisplayName("사다리 한 행에서 기둥을 담당하는 컬럼 클래스 테스트")
 class ColumnTest {
 
     @DisplayName("기본적으로 컬럼은 발판 생성 전략을 가지고 초기화를 한다.")
     void init() {
-
+        assertThat(Column.init(ALWAYS_GENERATE_STRATEGY)).isInstanceOf(Column.class);
     }
 
     @DisplayName("첫번 째 컬럼은 하나의 스텝을 가진다.")
