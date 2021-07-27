fdf9a2dd8dda53ab1c3f7c52f0fcee21314fc09e
nokchax <nokchax@gmail.com>
2021-07-27 22:05:29 +0900

    [G] feat: impl init ladder game init info

--- src/main/java/nextstep/ladder/domain/init/LadderGameInitInfo.java
+++ src/main/java/nextstep/ladder/domain/init/LadderGameInitInfo.java
@@ -3,8 +3,23 @@ package nextstep.ladder.domain.init;
 import nextstep.ladder.domain.player.Players;
 
 public class LadderGameInitInfo {
+    private final LadderInitInfo ladderInitInfo;
+    private final Players players;
 
-    public static Object of(LadderInitInfo ladderInitInfo2X2, Players twoPlayers) {
-        return null;
+    private LadderGameInitInfo(LadderInitInfo ladderInitInfo, Players players) {
+        this.ladderInitInfo = ladderInitInfo;
+        this.players = players;
+    }
+
+    public static Object of(LadderInitInfo ladderInitInfo, Players players) {
+        return new LadderGameInitInfo(ladderInitInfo, players);
+    }
+
+    public LadderInitInfo getLadderInitInfo() {
+        return ladderInitInfo;
+    }
+
+    public Players getPlayers() {
+        return players;
     }
 }
