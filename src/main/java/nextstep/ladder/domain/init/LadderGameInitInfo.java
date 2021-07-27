61c7e05c817be1e764a6b26c5dac9126eaabfbfc
nokchax <nokchax@gmail.com>
2021-07-27 22:08:52 +0900

    [R] feat: impl throw exception when param is null

--- src/main/java/nextstep/ladder/domain/init/LadderGameInitInfo.java
+++ src/main/java/nextstep/ladder/domain/init/LadderGameInitInfo.java
@@ -2,13 +2,27 @@ package nextstep.ladder.domain.init;
 
 import nextstep.ladder.domain.player.Players;
 
+import java.util.Objects;
+
 public class LadderGameInitInfo {
     private final LadderInitInfo ladderInitInfo;
     private final Players players;
 
     private LadderGameInitInfo(LadderInitInfo ladderInitInfo, Players players) {
+        validate(ladderInitInfo, players);
+
         this.ladderInitInfo = ladderInitInfo;
         this.players = players;
+    }
+
+    private void validate(LadderInitInfo ladderInitInfo, Players players) {
+        if (Objects.isNull(ladderInitInfo)) {
+            throw new IllegalArgumentException("LadderInitInfo can't be null");
+        }
+
+        if (Objects.isNull(players)) {
+            throw new IllegalArgumentException("Players can't be null");
+        }
     }
 
     public static Object of(LadderInitInfo ladderInitInfo, Players players) {
