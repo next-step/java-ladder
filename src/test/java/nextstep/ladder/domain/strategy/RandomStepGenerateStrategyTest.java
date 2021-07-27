71446db12886bb4582ac48cef8822a1695a658b9
nokchax <nokchax@gmail.com>
2021-07-27 22:57:15 +0900

    [R] test: test init exception when param is null

--- src/test/java/nextstep/ladder/domain/strategy/RandomStepGenerateStrategyTest.java
+++ src/test/java/nextstep/ladder/domain/strategy/RandomStepGenerateStrategyTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("랜덤 발판 생성 전략 클래스 테스트")
 class RandomStepGenerateStrategyTest {
@@ -16,4 +17,9 @@ class RandomStepGenerateStrategyTest {
         assertThat(RandomStepGenerateStrategy.init(new Random())).isInstanceOf(RandomStepGenerateStrategy.class);
     }
 
+    @DisplayName("랜덤 객체가 null 일 경우 예외를 발생 시킨다")
+    @Test
+    void initException() {
+        assertThatThrownBy(() -> RandomStepGenerateStrategy.init(null)).isInstanceOf(IllegalArgumentException.class);
+    }
 }
