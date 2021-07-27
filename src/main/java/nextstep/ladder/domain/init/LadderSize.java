d6085833f6c1d6c7ea4c6f596e3a2e4bc4d6b49a
nokchax <nokchax@gmail.com>
2021-07-27 20:10:04 +0900

    [G] feat: impl init ladder size

--- src/main/java/nextstep/ladder/domain/init/LadderSize.java
+++ src/main/java/nextstep/ladder/domain/init/LadderSize.java
@@ -1,7 +1,16 @@
 package nextstep.ladder.domain.init;
 
 public class LadderSize {
-    public static Object of(int i, int i1) {
-        return null;
+
+    private final int width;
+    private final int height;
+
+    public LadderSize(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public static LadderSize of(int width, int height) {
+        return new LadderSize(width, height);
     }
 }
