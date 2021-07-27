1ebe5f6b8b7f727782ae8b4f7e6b504e34209e1b
nokchax <nokchax@gmail.com>
2021-07-27 21:52:20 +0900

    [G] feat: impl throw exception when param is null

--- src/main/java/nextstep/ladder/domain/player/Players.java
+++ src/main/java/nextstep/ladder/domain/player/Players.java
@@ -1,15 +1,24 @@
 package nextstep.ladder.domain.player;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Players {
     private List<Player> players;
 
     private Players(List<String> names) {
+        validate(names);
+
         players = names.stream()
                 .map(Player::from)
                 .collect(Collectors.toList());
+    }
+
+    private void validate(List<String> names) {
+        if (Objects.isNull(names)) {
+            throw new IllegalArgumentException("Names can't be null");
+        }
     }
 
     public static Players init(List<String> names) {
