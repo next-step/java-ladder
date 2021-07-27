fb81e28c5ecf577f69d54c3231e29daeac7774ab
nokchax <nokchax@gmail.com>
2021-07-27 20:46:36 +0900

    [G] feat: init ladder row using ladder init info class

--- src/main/java/nextstep/ladder/domain/ladder/Row.java
+++ src/main/java/nextstep/ladder/domain/ladder/Row.java
@@ -3,17 +3,51 @@ package nextstep.ladder.domain.ladder;
 import nextstep.ladder.domain.init.LadderInitInfo;
 import nextstep.ladder.domain.strategy.StepGenerateStrategy;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Row {
+    private static final int ONE_OFFSET = 1;
+    private static final int FIRST_AND_END_COUNT = 2;
 
-    private List<Column> columns;
+    private final List<Column> columns;
 
-    public Row(int ladderWidth, StepGenerateStrategy stepGenerateStrategy) {
+    private Row(LadderInitInfo ladderInitInfo) {
+        columns = new ArrayList<>(ladderInitInfo.getLadderWidth());
 
+        createFirst(ladderInitInfo);
+        createMiddle(ladderInitInfo);
+        createLast();
     }
 
-    public static boolean init(LadderInitInfo ladderInitInfo) {
-        return false;
+    private void createFirst(LadderInitInfo ladderInitInfo) {
+        columns.add(Column.initFirst(ladderInitInfo.getStepGenerateStrategy()));
+    }
+
+    private void createMiddle(LadderInitInfo ladderInitInfo) {
+        Stream.generate(() -> createNextColumn(ladderInitInfo.getStepGenerateStrategy()))
+                .limit(ladderInitInfo.getLadderWidth() - FIRST_AND_END_COUNT)
+                .forEach(columns::add);
+    }
+
+    private void createLast() {
+        columns.add(createLastColumn());
+    }
+
+    public static Row init(LadderInitInfo ladderInitInfo) {
+        return new Row(ladderInitInfo);
+    }
+
+    private Column createNextColumn(StepGenerateStrategy stepGenerateStrategy) {
+        return getLastColumn().initNext(stepGenerateStrategy);
+    }
+
+    private Column createLastColumn() {
+        return getLastColumn().initLast();
+    }
+
+    private Column getLastColumn() {
+        return columns.get(columns.size() - ONE_OFFSET);
     }
 }
