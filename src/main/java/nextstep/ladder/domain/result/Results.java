34ea1ed00ecd51b985f7d5071582802ceb030d04
nokchax <nokchax@gmail.com>
2021-07-28 18:26:38 +0900

    [G] feat: imple init and throw exception of results class

--- src/main/java/nextstep/ladder/domain/result/Results.java
+++ src/main/java/nextstep/ladder/domain/result/Results.java
@@ -1,9 +1,35 @@
 package nextstep.ladder.domain.result;
 
+import nextstep.ladder.exception.NullArgumentException;
+
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Results {
-    public static boolean init(List<String> 꽝) {
-        return false;
+    private final List<Result> results;
+
+    private Results(List<String> results) {
+        validate(results);
+
+        this.results = results.stream()
+                .map(Result::from)
+                .collect(Collectors.toList());
+    }
+
+    private void validate(List<String> results) {
+        if (Objects.isNull(results)) {
+            throw new NullArgumentException("Results");
+        }
+    }
+
+    public static Results init(List<String> results) {
+        return new Results(results);
+    }
+
+    public List<String> getResults() {
+        return results.stream()
+                .map(Result::getResult)
+                .collect(Collectors.toList());
     }
 }
