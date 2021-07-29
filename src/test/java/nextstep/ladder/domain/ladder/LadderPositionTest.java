62ce25822499700e58b8f7dc95ab8f5b52b1ca34
nokchax <nokchax@gmail.com>
2021-07-29 10:44:48 +0900

    [R] test: test init exception when param is negative number

--- src/test/java/nextstep/ladder/domain/ladder/LadderPositionTest.java
+++ src/test/java/nextstep/ladder/domain/ladder/LadderPositionTest.java
@@ -1,9 +1,11 @@
 package nextstep.ladder.domain.ladder;
 
+import nextstep.ladder.exception.OutOfRangeArgumentException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("사다리 타기시 위치를 표현하기 위한 클래스")
 class LadderPositionTest {
@@ -14,4 +16,10 @@ class LadderPositionTest {
         assertThat(LadderPosition.from(5)).isInstanceOf(LadderPosition.class);
     }
 
+
+    @DisplayName("사다리 위치는 음수가 될 수 없으며, 음수값이 들어올 경우 예외를 던진다")
+    @Test
+    void initException() {
+        assertThatThrownBy(() -> LadderPosition.from(-1)).isInstanceOf(OutOfRangeArgumentException.class);
+    }
 }
