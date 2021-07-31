afb585653a98b67ab51d10cd65317fbd116cf0ec
shpark <bingbingpa@gmail.com>
2021-08-01 00:20:24 +0900

    feat: 스트림 실습

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,8 +5,12 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.Comparator.*;
 
 public class StreamStudy {
 
@@ -15,11 +19,9 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream()
+                .filter(word -> word.length() > 12)
+                .count();
     }
 
     public static void printLongestWordTop100() throws IOException {
@@ -27,7 +29,13 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(word -> word.length() > 12)
+                .distinct()
+                .sorted(comparing(String::length).reversed())
+                .map(String::toLowerCase)
+                .limit(100)
+                .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -35,10 +43,13 @@ public class StreamStudy {
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers.stream().reduce(0, Integer::sum);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(number -> number > 3)
+                .map(n -> n *2)
+                .reduce(0, Integer::sum);
     }
 }
--- src/test/java/nextstep/fp/StreamStudyTest.java
+++ src/test/java/nextstep/fp/StreamStudyTest.java
@@ -8,7 +8,8 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class StreamStudyTest {
+class StreamStudyTest {
+
     private List<Integer> numbers;
 
     @BeforeEach
@@ -17,30 +18,30 @@ public class StreamStudyTest {
     }
 
     @Test
-    public void countWords() throws Exception {
+    void countWords() throws Exception {
         long result = StreamStudy.countWords();
         System.out.println("result : " + result);
     }
 
     @Test
-    public void printLongestWordTop100() throws Exception {
+    void printLongestWordTop100() throws Exception {
         StreamStudy.printLongestWordTop100();
     }
 
     @Test
-    public void map() throws Exception {
+    void map() {
         List<Integer> doubleNumbers = StreamStudy.doubleNumbers(numbers);
         doubleNumbers.forEach(System.out::println);
     }
 
     @Test
-    public void sumAll() throws Exception {
+    void sumAll() {
         long sum = StreamStudy.sumAll(numbers);
         assertThat(sum).isEqualTo(21);
     }
 
     @Test
-    public void sumOverThreeAndDouble() throws Exception {
+    void sumOverThreeAndDouble() {
         numbers = Arrays.asList(3, 1, 6, 2, 4, 8);
         long sum = StreamStudy.sumOverThreeAndDouble(numbers);
         assertThat(sum).isEqualTo(36);
