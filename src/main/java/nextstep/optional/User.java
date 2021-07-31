88417808feed8de30d00f0a3cbf6da7d705e77e4
Poogle <58318786+suhyunsim@users.noreply.github.com>
2021-07-31 12:29:47 +0900

    [사다리 - 1단계] 리뷰 요청드립니다. (#975)
    * refactor: CarTest의 익명 클래스를 람다로 적용
    
    * feat: Lamda 클래스에서 변경되는 부분을 인터페이스로 추출
    
    * feat: Lamda 클래스에서 중복되는 부분을 람다로 구현해 제거
    
    * refactor: stream을 활용해서 구현
    
    * feat: Optional로 ageIsInRange2 메소드 구현
    
    * refactor: Optional로 getUser 메소드 리팩토링
    
    * refactor: Optional과 Stream으로 of() 메소드를 리팩토링

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,6 +35,10 @@ public class User {
     }
 
     public static boolean ageIsInRange2(User user) {
+        Optional<User> optionalUser = Optional.ofNullable(user);
+        if (optionalUser.isPresent()) {
+            return optionalUser.filter(u -> u.getAge() != null && u.getAge() >= 30 && u.getAge() <= 45).isPresent();
+        }
         return false;
     }
 
