ece016012a2da3fcb7e011155bdb03e52e23f174
jayce6190 <jayce6190@gmail.com>
2021-08-03 00:10:45 +0900

    feature: LadderSize 구현
    (cherry picked from commit fa23fe1c1d70c38023afede24fc083ad08da0a5c)

--- src/main/java/nextstep/ladder/domain/info/LadderSize.java
+++ src/main/java/nextstep/ladder/domain/info/LadderSize.java
@@ -1,2 +1,33 @@
-package nextstep.ladder.domain.info;public class LadderSize {
+package nextstep.ladder.domain.info;
+
+public class LadderSize {
+    protected static final int MIN = 1;
+
+    private final int width;
+    private final int height;
+
+    public LadderSize(int width, int height) {
+        validate(width, height);
+
+        this.width = width;
+        this.height = height;
+    }
+
+    private void validate(int width, int height) {
+        if (width < MIN || height < MIN) {
+            throw new IllegalArgumentException("Ladder Size는 최소값 " + MIN + " 보다 작을수 없습니다");
+        }
+    }
+
+    public static LadderSize of(int width, int height) {
+        return new LadderSize(width, height);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
--- src/test/java/nextstep/ladder/domain/info/LadderSizeTest.java
+++ src/test/java/nextstep/ladder/domain/info/LadderSizeTest.java
@@ -1,4 +1,27 @@
-import static org.junit.jupiter.api.Assertions.*;
+package nextstep.ladder.domain.info;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
 class LadderSizeTest {
-  
+
+    @DisplayName("가로 세로 정보로 LadderSize를 만든다")
+    @Test
+    void should_make_with_width_height() {
+        //arrange, act, assert
+        assertThat(LadderSize.of(1, 1)).isInstanceOf(LadderSize.class);
+    }
+
+    @DisplayName("with, height가 최소값인 1 이하이면 IllegalArgumentException을 던진다")
+    @CsvSource(value = {"1,0", "0,1"}, delimiter = ',')
+    @ParameterizedTest
+    void throw_exception_when_below_min_width_or_height(int width, int height) {
+        assertThatIllegalArgumentException().isThrownBy(() -> LadderSize.of(width, height));
+    }
+
 }
