603af7dc33443cf24c2b2ca65e9754ed0888e582
jayce6190 <jayce6190@gmail.com>
2021-08-02 23:48:02 +0900

    feature: player의 name을 가지는 player클래스와 players클래스 구현
    (cherry picked from commit b233b416d1e79841a203fedf76d92e3868a4ad6a)

---
+++ src/main/java/nextstep/ladder/domain/player/Player.java
@@ -0,0 +1,33 @@
+package nextstep.ladder.domain.player;
+
+import nextstep.ladder.utils.StringUtils;
+
+public class Player {
+    public static final int MAXIMUM_LENGTH_OF_NAME = 5;
+
+    private final String name;
+
+    private Player(String name) {
+        validate(name);
+
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if (StringUtils.isEmpty(name)) {
+            throw new IllegalArgumentException("이름은 null이거나 비어있을수 없습니다.");
+        }
+
+        if (name.length() > MAXIMUM_LENGTH_OF_NAME) {
+            throw new IllegalArgumentException("이름은 최대 " + MAXIMUM_LENGTH_OF_NAME + "자 입니다.");
+        }
+    }
+
+    public static Player of(String name) {
+        return new Player(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/nextstep/ladder/domain/player/PlayersTest.java
@@ -0,0 +1,43 @@
+package nextstep.ladder.domain.player;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class PlayersTest {
+
+    @DisplayName("플레이어즈 그룹은 이름 리스트를 가지고 객체를 생성한다")
+    @Test
+    void should_make_players() {
+        //arrange, act, assert
+        assertThat(Players.of(Arrays.asList("a", "bb", "ccc"))).isInstanceOf(Players.class);
+    }
+
+    @DisplayName("name 리스트가 null이거나 빈값일경우 경우 IllegalArgumentException을 발생시킨다")
+    @NullAndEmptySource
+    @ParameterizedTest
+    void should_throw_exception(List<String> name) {
+        //arrange, act, assert
+        assertThatIllegalArgumentException().isThrownBy(() -> Players.of(name));
+    }
+
+    @DisplayName("플레이어 이름을 반환한다")
+    @Test
+    void should_return_players_name() {
+        //arrange
+        Players players = Players.of(Arrays.asList("a", "bb", "ccc"));
+
+        //act
+        List<String> names = players.getNames();
+
+        //assert
+        assertThat(names).containsExactly("a", "bb", "ccc");
+    }
+}
