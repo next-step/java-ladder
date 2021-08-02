6a8e0a11a820184cf7b0ce11d20a091002c09e74
jayce6190 <jayce6190@gmail.com>
2021-08-02 23:45:45 +0900

    feature: 컨트롤러와 InputView 구현
    (cherry picked from commit efe9999b177e027ce722ce3211a8dfdca054ab50)

---
+++ src/main/java/nextstep/ladder/LadderController.java
@@ -0,0 +1,2 @@
+package nextstep.ladder;public class LadderController {
+}
---
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -0,0 +1,47 @@
+package nextstep.ladder.view;
+
+import nextstep.ladder.domain.player.Players;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+    private static final String INPUT_PARTICIPANTS_STATEMENT = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String INPUT_LADDER_HEIGHT_STATEMENT = "최대 사다리 높이는 몇 개인가요?";
+    private static final String NAMES_DELIMITER = ",";
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private InputView() {}
+
+    public static Players inputParticipants() {
+        printStatement(INPUT_PARTICIPANTS_STATEMENT);
+
+        return Arrays.stream(readNames())
+                .map(String::trim)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), Players::of));
+    }
+
+    public static int inputLadderHeight() {
+        printStatement(INPUT_LADDER_HEIGHT_STATEMENT);
+
+        return getInt();
+    }
+
+    private static void printStatement(String statement) {
+        System.out.println(statement);
+    }
+
+    private static int getInt() {
+        return Integer.parseInt(
+                SCANNER.nextLine()
+                        .trim()
+        );
+    }
+
+    private static String[] readNames() {
+        return SCANNER.nextLine()
+                .split(NAMES_DELIMITER);
+    }
+}
