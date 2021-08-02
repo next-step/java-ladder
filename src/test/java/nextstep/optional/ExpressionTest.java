4c98d20d458bcfbf612ce7c58c5d6c7a98d1f79a
choibyunghyeon <choi8608@gmail.com>
2021-08-02 22:40:21 +0900

    refactor: 요구사항 3 - Optional에서 exception 처리

--- src/test/java/nextstep/optional/ExpressionTest.java
+++ src/test/java/nextstep/optional/ExpressionTest.java
@@ -1,12 +1,13 @@
 package nextstep.optional;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
+import org.junit.jupiter.api.Test;
+
 
 public class ExpressionTest {
+
     @Test
     public void of() {
         assertThat(Expression.PLUS == Expression.of("+")).isTrue();
@@ -15,8 +16,8 @@ public class ExpressionTest {
     @Test
     public void notValidExpression() {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> {
-                    Expression.of("&");
-                });
+            .isThrownBy(() -> {
+                Expression.of("&");
+            });
     }
 }
