0baca9e3a783c684b46ec4659cc190c91d77d845
shpark <bingbingpa@gmail.com>
2021-08-04 04:42:43 +0900

    feat: Player 객체에 position 추가

--- src/main/java/nextstep/ladder/player/Players.java
+++ src/main/java/nextstep/ladder/player/Players.java
@@ -2,10 +2,10 @@ package nextstep.ladder.player;
 
 import nextstep.ladder.util.StringUtil;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Players {
 
@@ -19,19 +19,19 @@ public class Players {
         return new Players(toList(names));
     }
 
+    private static List<Player> toList(String names) {
+        String[] split = StringUtil.split(names);
+
+        return IntStream.range(0, split.length)
+                .mapToObj(i -> Player.of(split[i], i))
+                .collect(Collectors.toList());
+    }
+
     public List<Player> getPlayers() {
         return Collections.unmodifiableList(players);
     }
 
     public int count() {
         return players.size();
-    }
-
-    private static List<Player> toList(String names) {
-        String[] split = StringUtil.split(names);
-
-        return Arrays.stream(split)
-                .map(Player::of)
-                .collect(Collectors.toList());
     }
 }
