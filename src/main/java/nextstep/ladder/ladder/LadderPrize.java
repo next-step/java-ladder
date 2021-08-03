745a39ccb7621d866817e1a6ef28f6746662d6c6
shpark <bingbingpa@gmail.com>
2021-08-03 22:33:26 +0900

    feat: 실행 결과를 입력받는 Prize 객체

---
+++ src/main/java/nextstep/ladder/ladder/LadderPrize.java
@@ -0,0 +1,28 @@
+package nextstep.ladder.ladder;
+
+import nextstep.ladder.util.StringUtil;
+
+public class LadderPrize {
+
+    private final String[] prizes;
+
+    private LadderPrize(String[] prizes) {
+        this.prizes = prizes;
+    }
+
+    public static LadderPrize of(String prizes) {
+        return new LadderPrize(StringUtil.split(prizes));
+    }
+
+    public String findPrize(int index) {
+        indexValidate(index);
+        return prizes[index];
+    }
+
+    private void indexValidate(int index) {
+        int length = prizes.length - 1;
+        if (index > length || index < 0) {
+            throw new IndexOutOfBoundsException("상품 정보는 0부터 " + length + "까지 조회 가능합니다.");
+        }
+    }
+}
