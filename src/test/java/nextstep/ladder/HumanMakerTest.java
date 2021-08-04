5939d5bcc52d9cd13ed6efb6c552e17baa26365e
lsj8367 <lsj8367@naver.com>
2021-08-04 12:42:02 +0900

    test: 테스트 이름 변경

--- src/test/java/nextstep/ladder/HumanMakerTest.java
+++ src/test/java/nextstep/ladder/HumanMakerTest.java
@@ -5,17 +5,17 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class LadderMakerTest {
+public class HumanMakerTest {
     @Test
     @DisplayName("사다리 게임 사람 추가하기")
     void ladderCreateTest() {
-        LadderMaker.of("pobi,honux,crong,jk");
+        HumanMaker.of("pobi,honux,crong,jk");
     }
 
     @Test
-    @DisplayName("사다리 게임 5글자 예외처리")
+    @DisplayName("이름 5글자 예외처리")
     void ladderExceptionTest() {
-        assertThatThrownBy(() -> LadderMaker.of("pobiaaaaa,honux,crong,jk"))
+        assertThatThrownBy(() -> HumanMaker.of("pobiaaaaa,honux,crong,jk"))
                                             .hasMessageContaining("5글자")
                                             .isInstanceOf(IllegalArgumentException.class);
 
