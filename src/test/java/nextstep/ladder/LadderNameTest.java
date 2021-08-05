4d4c5deedf7b28fe1f7f5f895507142d428fda88
lsj8367 <lsj8367@naver.com>
2021-08-05 12:54:55 +0900

    test: 테스트 수정

--- src/test/java/nextstep/ladder/LadderNameTest.java
+++ src/test/java/nextstep/ladder/LadderNameTest.java
@@ -5,21 +5,29 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LadderNameTest {
     @Test
     @DisplayName("인원 생성 테스트")
     void create() {
-        LadderName ladderName = new LadderName("pobi,honux,crong,jk");
-        assertThat(ladderName).isEqualTo(new LadderName("pobi,honux,crong,jk"));
+        LadderName ladderName = LadderName.make("pobi,honux,crong,jk");
+        assertThat(ladderName).isEqualTo(LadderName.make("pobi,honux,crong,jk"));
     }
 
     @Test
     @DisplayName("이름 5글자 예외처리")
     void ladderExceptionTest() {
-        assertThatThrownBy(() -> new LadderName("pobiaaaaa,honux,crong,jk"))
+        assertThatThrownBy(() -> LadderName.make("pobiaaaaa,honux,crong,jk"))
                 .hasMessageContaining("5글자")
                 .isInstanceOf(StringLengthException.class);
+    }
+
+    @Test
+    @DisplayName("이름 공백 예외처리")
+    void inputStrValidationTest() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> LadderName.make(""));
+        assertThat(exception.getMessage()).isEqualTo("이름을 공백으로 할 수 없습니다.");
     }
 
 }
--- src/test/java/nextstep/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -3,21 +3,17 @@ package nextstep.ladder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.Random;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineTest {
-    private static final Random random = new Random();
     @Test
     @DisplayName("사다리 줄 생성")
     void makeLineTest() {
         int height = 5;
-        List<String> list = HumanMaker.of("pobi,honux,crong,jk");
-        String line;
 
         for(int i = 0; i < height; i++) {
-            line = Line.drawLine(list.size());
-            System.out.println(line);
+            Line line = new Line(4, () -> 3);
+            assertThat(line).isEqualTo(new Line(4, () -> 3));
         }
     }
 
