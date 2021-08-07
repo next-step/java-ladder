0e3e755796cf951e7ee4e49167d6f9852871e8b4
shirohoo <shirohoo4102@gmail.com>
2021-08-08 02:33:18 +0900

    feat: map, reduce, filter 실습 2

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -47,6 +47,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                      .filter(number -> number > 3)
+                      .map(number -> number * 2)
+                      .reduce(0, Integer::sum);
     }
 }
