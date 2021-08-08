d2feca49a725e300365f416f1984473b0a79a4c4
shpark <bingbingpa@gmail.com>
2021-08-09 00:53:22 +0900

    feat: 실행 결과 객체 추가

---
+++ src/main/java/nextstep/ladder/ladder/Prizes.java
@@ -0,0 +1,36 @@
+package nextstep.ladder.ladder;
+
+import nextstep.ladder.util.StringUtil;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Prizes {
+
+    private final List<String> prizeNames;
+
+    private Prizes(List<String> prizeNames) {
+        this.prizeNames = prizeNames;
+    }
+
+    public static Prizes of(String prizes) {
+        String[] split = StringUtil.split(prizes);
+        List<String> collect = Arrays.stream(split)
+                .collect(Collectors.toList());
+        return new Prizes(collect);
+    }
+
+    public String findPrize(int index) {
+        indexValidate(index);
+        return prizeNames.get(index);
+    }
+
+    private void indexValidate(int index) {
+        int length = prizeNames.size() - 1;
+        if (index > length || index < 0) {
+            throw new IndexOutOfBoundsException("상품 정보는 0부터 " + length + "까지 조회 가능합니다.");
+        }
+    }
+}
+
