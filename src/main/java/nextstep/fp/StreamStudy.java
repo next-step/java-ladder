6f1311824dfc3da85fb76a5c39f2e35b44c67afa
Hae-gun <newchk610@gmail.com>
2021-08-09 23:33:59 +0900

    feat: Add 스트림 활용하기

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -15,10 +15,7 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
+        long count = words.stream().filter(w -> w.length() > 12).count();
         return count;
     }
 
@@ -27,7 +24,11 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream().filter(word -> word.length() > 12)
+                .sorted().distinct()
+                .map(String::toLowerCase)
+                .collect(Collectors.toList()).subList(0, 100)
+                .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +40,6 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream().filter(number -> number > 3).map(x -> 2 * x).reduce(0, (x, y) -> x + y);
     }
 }
