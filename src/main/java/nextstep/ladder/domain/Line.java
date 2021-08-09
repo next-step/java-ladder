4d687faefe7f25bf0f527503a00dc481cdaaaaee
choibyunghyeon <choi8608@gmail.com>
2021-08-10 00:10:08 +0900

    feat: 테스트를 위한 Line 생성자 추가

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -3,10 +3,17 @@ package nextstep.ladder.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.BiConsumer;
+import nextstep.ladder.domain.exception.InvalidCreateLineException;
 
 public class Line {
 
     private final List<Boolean> exists;
+
+    private Line(final List<Boolean> exists) {
+        createValidation(exists);
+        this.exists = exists;
+    }
 
     private Line(final LineExistsGenerator generator, final int playerCount) {
         final List<Boolean> exists = new ArrayList<>();
@@ -14,7 +21,25 @@ public class Line {
         for (int i = 1; i < playerCount; i++) {
             exists.add(generator.generate(exists.get(i - 1)));
         }
+
+        createValidation(exists);
         this.exists = exists;
+    }
+
+    private void createValidation(final List<Boolean> exists) {
+        final BiConsumer<Boolean, Boolean> validation = (current, next) -> {
+            if (!((current) ? !next : true)) {
+                throw new InvalidCreateLineException();
+            }
+        };
+
+        for (int i = 0; i < exists.size() - 1; i++) {
+            validation.accept(exists.get(i), exists.get(i + 1));
+        }
+    }
+
+    public static Line of(final List<Boolean> exists) {
+        return new Line(exists);
     }
 
     public static Line of(final int playerCount) {
--- src/test/java/nextstep/ladder/domain/LineTest.java
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -1,24 +1,72 @@
 package nextstep.ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import nextstep.ladder.domain.exception.InvalidCreateLineException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @DisplayName("Line (가로줄)")
 class LineTest {
+
+    public static Stream<List<Boolean>> validExists() {
+        return Stream.of(
+            Arrays.asList(true, false, true, false, true),
+            Arrays.asList(false, false, true, false, true),
+            Arrays.asList(false, false, false, false, false),
+            Arrays.asList(false, false, false, false, true)
+        );
+    }
+
+    @DisplayName("[성공] 생성")
+    @ParameterizedTest
+    @MethodSource("validExists")
+    public void create(final List<Boolean> exists) {
+        // given
+
+        // when
+        final Line line = Line.of(exists);
+
+        // then
+        assertThat(line).isNotNull();
+    }
+
+    public static Stream<List<Boolean>> notValidExists() {
+        return Stream.of(
+            Arrays.asList(true, true, true, false, true),
+            Arrays.asList(false, false, true, true, true),
+            Arrays.asList(false, false, false, true, true)
+        );
+    }
+
+    @DisplayName("[성공] 생성 - 유효하지 않은 초기 값")
+    @ParameterizedTest
+    @MethodSource("notValidExists")
+    public void create_notValidExists(final List<Boolean> exists) {
+        // given
+
+        // when
+        assertThrows(InvalidCreateLineException.class, () -> Line.of(exists));
+
+        // then
+    }
 
     @DisplayName("[성공] 생성 - with참여자수")
     @ParameterizedTest
     @CsvSource(value = {
         "10"
     })
-    public void create_withHeight(int playerCount) {
+    public void create_withHeight(final int playerCount) {
         // given
 
         // when
-        Line line = Line.of(playerCount);
+        final Line line = Line.of(playerCount);
 
         // then
         assertThat(line.size()).isEqualTo(playerCount);
