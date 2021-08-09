31fe5fb2d60e2190dd670100cbb652362816bcac
shirohoo <shirohoo4102@gmail.com>
2021-08-10 00:12:21 +0900

    feat: LadderGameConfig를 작성했다
    사다리 게임의 설정을 표현한다.

---
+++ src/test/java/nextstep/ladder/domain/laddar/LadderGameConfigTest.java
@@ -0,0 +1,57 @@
+package nextstep.ladder.domain.laddar;
+
+import nextstep.ladder.domain.strategy.DirectionRandomStrategy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static nextstep.ladder.domain.laddar.LadderGameConfig.EXCLUDE_FIRST_AND_LAST_COUNT;
+import static nextstep.ladder.domain.laddar.LadderGameConfig.of;
+import static nextstep.ladder.fixture.LadderFixture.HEIGHT_SIZE_FIXTURE;
+import static nextstep.ladder.fixture.LadderFixture.WIDTH_SIZE_FIXTURE;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+/**
+ * LadderGameConfig는 사다리 게임 설정에 대한 정보를 갖는다.
+ */
+class LadderGameConfigTest {
+    private final LadderGameConfig ladderGameConfig = of(
+            LadderSize.of(5, 5),
+            DirectionRandomStrategy.getInstance());
+
+    @Test
+    @DisplayName("객체 초기화시 생성자 매개변수가 단 하나라도 null이면 즉시 NPE를 발생시킨다.")
+    void npe() {
+        assertAll(
+                () -> Assertions.assertThatThrownBy(() -> {
+                    of(null, DirectionRandomStrategy.getInstance());
+                }).isInstanceOf(NullPointerException.class),
+
+                () -> Assertions.assertThatThrownBy(() -> {
+                    of(LadderSize.of(WIDTH_SIZE_FIXTURE, HEIGHT_SIZE_FIXTURE), null);
+                }).isInstanceOf(NullPointerException.class)
+        );
+    }
+
+    @Test
+    void getWidth() {
+        Assertions.assertThat(ladderGameConfig.getWidth()).isEqualTo(WIDTH_SIZE_FIXTURE);
+    }
+
+    @Test
+    void getHeight() {
+        Assertions.assertThat(ladderGameConfig.getHeight()).isEqualTo(HEIGHT_SIZE_FIXTURE);
+    }
+
+    @Test
+    @DisplayName("처음 열과 마지막 열을 제외한 중간 열의 사이즈를 구한다. 전체 너비에서 처음 열과 마지막 열을 뺀 값(-2)이 중간 열의 사이즈이다.")
+    void getMiddleSize() {
+        Assertions.assertThat(ladderGameConfig.getMiddleSize()).isEqualTo(WIDTH_SIZE_FIXTURE - EXCLUDE_FIRST_AND_LAST_COUNT);
+    }
+
+    @Test
+    void getDirectionStrategy() {
+        Assertions.assertThat(ladderGameConfig.getDirectionStrategy())
+                  .isInstanceOf(DirectionRandomStrategy.class);
+    }
+}
