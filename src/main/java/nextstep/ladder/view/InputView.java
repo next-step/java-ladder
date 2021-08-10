e6fc76f1dc58c3f1e80fd67ace51f4ce53938304
SeungJae Lee <lsj8367@naver.com>
2021-08-10 16:30:23 +0900

    feat: 실행결과, 당첨 인원 input

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -5,13 +5,24 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner sc = new Scanner(System.in);
 
-    public String initPerson() {
+    public static String initPerson() {
         System.out.println("참여할 사람 이름을 입력하세요.");
         return sc.nextLine();
     }
 
-    public int maxLadderHeight() {
+    public static String excutionResult() {
+        System.out.println("실행 결과를 입력하세요.");
+        return sc.nextLine();
+    }
+
+    public static int maxLadderHeight() {
         System.out.println("최대 사다리 높이는 몇 개 인가요?");
         return sc.nextInt();
     }
+
+    public static String resultName() {
+        System.out.println("결과를 보고 싶은 사람은?");
+        return sc.next();
+    }
+
 }
