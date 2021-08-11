2b02a50161d64ef8707b60636a3897564741f32e
SeungJae Lee <lsj8367@naver.com>
2021-08-11 15:13:34 +0900

    feat: 사다리 위치 클래스 추가

--- src/main/java/nextstep/ladder/domain/Position.java
+++ src/main/java/nextstep/ladder/domain/Position.java
@@ -1,47 +1,25 @@
 package nextstep.ladder.domain;
 
-import java.util.Objects;
-
 public class Position {
-    private static Position position;
-    private int x;
-    private int y;
+    private int moveIndex;
 
-    private Position(int x, int y) {
-        this.x = x;
-        this.y = y;
+    private Position(int moveIndex) {
+        this.moveIndex = moveIndex;
     }
 
-    public static Position getInstance(int x, int y) {
-        if(Objects.isNull(position)) {
-            position = Position.of(x, y);
-        }
-        return position;
-    }
-
-    private static Position of(int x, int y) {
-        return new Position(x, y);
-    }
-
-    public void leftMove() {
-        this.x++;
-        this.y--;
+    public static Position of(int moveIndex) {
+        return new Position(moveIndex);
     }
 
     public void rightMove() {
-        this.x++;
-        this.y++;
+        this.moveIndex += 2;
     }
 
-    public void down() {
-        this.x++;
+    public void leftMove() {
+        this.moveIndex -= 2;
     }
 
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
+    public int ceil() {
+        return (int) Math.ceil(moveIndex / 2);
     }
 }
