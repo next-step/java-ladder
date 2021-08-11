f44362f8e233d86d5b3fa07fa8ec7cf807a1de6f
shirohoo <shirohoo4102@gmail.com>
2021-08-11 22:05:49 +0900

    feat: 사다리 타기 알고리즘 구현 작업 5

--- src/main/java/nextstep/ladder/domain/laddar/Ladder.java
+++ src/main/java/nextstep/ladder/domain/laddar/Ladder.java
@@ -1,17 +1,21 @@
 package nextstep.ladder.domain.laddar;
 
 import nextstep.ladder.domain.dto.RowDto;
+import nextstep.ladder.domain.player.Player;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Ladder {
+    private final LadderGameContext context;
+
     private final List<LadderRow> ladderRows;
 
     private Ladder(final LadderGameContext ladderGameContext) {
+        this.context = ladderGameContext;
         ladderRows = Stream.generate(() -> LadderRow.from(ladderGameContext))
-                           .limit(ladderGameContext.getHeight())
+                           .limit(ladderGameContext.getLadderHeight())
                            .collect(Collectors.toUnmodifiableList());
     }
 
@@ -21,8 +25,16 @@ public class Ladder {
 
     public List<RowDto> convert() {
         return ladderRows.stream()
-                         .map(LadderRow::toDirections)
+                         .map(LadderRow::getLadderMap)
                          .map(RowDto::from)
                          .collect(Collectors.toUnmodifiableList());
+    }
+
+    public int height() {
+        return context.getLadderHeight();
+    }
+
+    public void ride(final Player player, final int stage) {
+        ladderRows.get(stage).ride(player);
     }
 }
