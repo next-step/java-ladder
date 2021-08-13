379aed3c423efd3c1f8e9b8c7c9410aff900b437
SeungJae Lee <lsj8367@naver.com>
2021-08-13 14:09:11 +0900

    feat: 높이 객체 구현

---
+++ src/main/java/nextstep/ladder/domain/Height.java
@@ -0,0 +1,26 @@
+package nextstep.ladder.domain;
+
+public class Height {
+    private static final int ZERO = 0;
+    private final int height;
+
+    private Height(int height) {
+        this.height = height;
+    }
+
+    public static Height of(int height) {
+        heightValidate(height);
+        return new Height(height);
+    }
+
+    private static void heightValidate(int height) {
+        if(height <= ZERO) {
+            throw new IllegalArgumentException("사다리 갯수는 최소 1개여야 합니다.");
+        }
+    }
+
+
+    public int getHeight() {
+        return height;
+    }
+}
