0e08d2010badedbfb2100622330b6cd000b89cee
sam.717 <sam.717@kakaocorp.com>
2021-08-13 18:31:23 +0900

    Optional 실습 진행

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,7 +31,7 @@ public class StreamStudy {
         words.stream()
                 .filter(word -> word.length() > 12)
                 .distinct()
-                .sorted()
+                .sorted(Comparator.comparingInt(String::length))
                 .limit(100)
                 .map(String::toLowerCase)
                 .forEach(System.out::println);
--- src/main/java/nextstep/optional/Users.java
+++ src/main/java/nextstep/optional/Users.java
@@ -2,6 +2,7 @@ package nextstep.optional;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Users {
     static final User DEFAULT_USER = new User("codesquad", 100);
@@ -12,6 +13,7 @@ public class Users {
             new User("jk", 40),
             new User("honux", 45));
 
+    /* Before
     User getUser(String name) {
         for (User user : users) {
             if (user.matchName(name)) {
@@ -19,5 +21,14 @@ public class Users {
             }
         }
         return DEFAULT_USER;
+    }
+    */
+
+    // After
+    User getUser(String name) {
+        return users.stream()
+                .filter(u -> u.matchName(name))
+                .findFirst()
+                .orElse(DEFAULT_USER);
     }
 }
