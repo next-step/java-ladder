99bd862e8a622046b51809db87b5df771d905cfb
syd9538 <syd9538@gmail.com>
2021-08-13 22:55:32 +0900

    feat : 람다 실습 2 구현

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,8 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.stream.Collectors;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
@@ -18,39 +20,25 @@ public class Lambda {
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> System.out.println("Hello from thread")).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+    public static int sumAll(List<Integer> numbers, Conditional conditional) {
+        return numbers.stream()
+                .filter(number -> conditional.test(number))
+                .mapToInt(number -> number)
+                .sum();
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        Conditional conditional = number -> number % 2 == 0;
+
+        return sumAll(numbers, conditional);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        Conditional conditional = number -> number > 3;
+
+        return sumAll(numbers, conditional);
     }
 }
