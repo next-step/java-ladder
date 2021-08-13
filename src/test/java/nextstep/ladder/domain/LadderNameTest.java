2f02acb3cf51ba1526d53d4197688587ecb992d1
SeungJae Lee <lsj8367@naver.com>
2021-08-13 10:30:43 +0900

    test: LadderNames, LadderName 테스트

--- src/test/java/nextstep/ladder/domain/LadderNameTest.java
+++ src/test/java/nextstep/ladder/domain/LadderNameTest.java
@@ -3,37 +3,19 @@ package nextstep.ladder.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import nextstep.ladder.exception.LetterLimitException;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LadderNameTest {
     @Test
-    @DisplayName("LadderName 생성 테스트")
     void create() {
-        LadderName ladderName = LadderName.of("aaaaa");
-        assertThat(ladderName).isEqualTo(LadderName.of("aaaaa"));
-    }
-
-    @Test
-    @DisplayName("5글자 이상 예외")
-    void letterLimitException() {
-        assertThatThrownBy(() -> LadderName.of("asdasd"))
-            .hasMessageContaining("5글자")
-            .isInstanceOf(LetterLimitException.class);
-    }
-
-    @Test
-    void findByName() {
         LadderName ladderName = LadderName.of("a");
-        assertThat(ladderName.findName("a")).isTrue();
+        assertThat(ladderName).isEqualTo(LadderName.of("a"));
     }
 
     @Test
-    void findByNameFalse() {
-        LadderName ladderName = LadderName.of("a");
-        assertThat(ladderName.findName("b")).isFalse();
+    void exception() {
+        assertThatThrownBy(() -> {
+            LadderName.of("abbbbb");
+        }).isInstanceOf(LengthLimitException.class);
     }
-
-
 }
--- src/test/java/nextstep/ladder/domain/LadderNamesTest.java
+++ src/test/java/nextstep/ladder/domain/LadderNamesTest.java
@@ -1,59 +1,14 @@
 package nextstep.ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class LadderNamesTest {
-    LadderNames ladderNames;
-
-    @BeforeEach
-    void setUp() {
-        ladderNames = LadderNames.of("pobi,honux,crong,jk");
-    }
-
+public class LadderNamesTest {
     @Test
-    @DisplayName("인원 생성 테스트")
     void create() {
-        assertThat(ladderNames).isEqualTo(LadderNames.of("pobi,honux,crong,jk"));
-    }
-
-    @Test
-    @DisplayName("이름 공백 예외처리")
-    void inputStrValidationTest() {
-        Throwable exception = assertThrows(IllegalArgumentException.class, () -> LadderNames.of(""));
-        assertThat(exception.getMessage()).isEqualTo("이름을 공백으로 할 수 없습니다.");
-    }
-
-    @Test
-    @DisplayName("이름 찾기")
-    void findByName() {
-        assertThat(ladderNames.findIndex("pobi")).isEqualTo(0);
-    }
-
-    @Test
-    @DisplayName("이름 예외")
-    void findByNameException() {
-        assertThatThrownBy(() -> {
-            ladderNames.findIndex("abc");
-        }).hasMessageContaining("해당하는 사람이 없습니다.");
-    }
-
-    @Test
-    @DisplayName("사이즈 테스트")
-    void sizeTest() {
-        assertThat(ladderNames.size()).isEqualTo(4);
-    }
-
-    @Test
-    @DisplayName("이름 하나 테스트")
-    void nameTest() {
-        assertThat(ladderNames.getLadderName(0)).isEqualTo("pobi");
-
+        LadderNames ladderNames = LadderNames.of("a,b,c,d");
+        assertThat(ladderNames).isEqualTo(LadderNames.of("a,b,c,d"));
     }
 
 }
