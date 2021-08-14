eef185e5af520a064540b914c128712e8cb6f5c1
kdh85 <modudamba@naver.com>
2021-08-14 21:31:48 +0900

    refactor: 사다리게임 step1
    구현목록
    * 실습2에 대한 내용을 구현하기 위해 인터페이스를 선언하고 익명클래스를 활용하도록 구성한 뒤 람다로 변환하여 sumAll 메소드를 리팩토링 한다.
    * sumAll에서 조건에 해당하는 값이 참인 기준으로 조건을 주입하여 각각 sumAllEven, sumAllOverThree에서 호출하여 사용하게 리팩토링한다.

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -26,31 +26,18 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+    public static int sumAll(List<Integer> numbers, Conditional c) {
+        return numbers.stream()
+            .mapToInt(number -> number)
+            .filter(number -> (c.test(number)))
+            .sum();
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers,number -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers,number -> number > 3);
     }
 }
