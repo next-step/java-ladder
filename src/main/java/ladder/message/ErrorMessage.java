6ea7b6f8799c2624538bdda7be3a23fcaa4f13f5
PIH6585 <pih6585@naver.com>
2021-08-15 10:36:10 +0900

    feat : error message 추가

---
+++ src/main/java/ladder/message/ErrorMessage.java
@@ -0,0 +1,7 @@
+package ladder.message;
+
+public class ErrorMessage {
+
+	public static final String CHECK_EMPTY_NAME = "이름이 빈값 입니다.";
+	public static final String CHECK_NAME_LIMIT_LENGTH = "이름은 최대 5글자 입니다.";
+}
