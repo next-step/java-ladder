b118e208b5d59058e0429ce35c274885dbd36b7c
PIH6585 <pih6585@naver.com>
2021-08-15 14:51:46 +0900

    feat : 라인모음 컬렉션 추가

---
+++ src/test/java/ladder/model/LinesTest.java
@@ -0,0 +1,36 @@
+package ladder.model;
+
+import static org.assertj.core.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LinesTest {
+
+	@Test
+	@DisplayName("라인들을 입력하면 여러라인을 생성한다.")
+	public void createLines(){
+		Lines lines = new Lines(getLine());
+
+		assertThat(lines).isEqualTo(new Lines(getLine()));
+	}
+
+	private List<Line> getLine() {
+		List<Line> lines = new ArrayList<>();
+		lines.add(new Line(getPoints(false,false,true)));
+		lines.add(new Line(getPoints(false,true,false)));
+		lines.add(new Line(getPoints(true,false,false)));
+		return lines;
+	}
+
+	private List<Boolean> getPoints(boolean value1,boolean value2,boolean value3) {
+		List<Boolean> points = new ArrayList<>();
+		points.add(value1);
+		points.add(value2);
+		points.add(value3);
+		return points;
+	}
+
+}
