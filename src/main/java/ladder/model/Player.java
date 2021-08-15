e3461df2954c34ff010868235ae483657a549a0a
PIH6585 <pih6585@naver.com>
2021-08-15 10:38:27 +0900

    feat : player 추가

---
+++ src/main/java/ladder/model/Player.java
@@ -0,0 +1,27 @@
+package ladder.model;
+
+import java.util.Objects;
+
+public class Player {
+
+	private final Name name;
+
+	public Player(Name name) {
+		this.name = name;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Player player = (Player)o;
+		return Objects.equals(name, player.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+}
