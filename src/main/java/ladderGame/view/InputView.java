3b4d9419cb1e4301ccde395018dffea59dcb3634
kdh85 <modudamba@naver.com>
2021-08-17 00:34:20 +0900

    refactor : 사다리게임 step2 InputView 클래스 객체생성 관련 리팩토링
    * Scanner 클래스를 매번 새로 생성하지 않도록 변경함.

--- src/main/java/ladderGame/view/InputView.java
+++ src/main/java/ladderGame/view/InputView.java
@@ -4,17 +4,15 @@ import java.util.Scanner;
 
 public class InputView {
 
+  private static Scanner scanner = new Scanner(System.in);
+
   public static String inputUserNamesWithMessage(final String message) {
     System.out.println(message);
-
-    Scanner scanner = new Scanner(System.in);
     return scanner.nextLine();
   }
 
   public static int inputLadderHeightWithMessage(final String message) {
     System.out.println(message);
-
-    Scanner scanner = new Scanner(System.in);
     return scanner.nextInt();
   }
 }
