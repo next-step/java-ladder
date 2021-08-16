d11048ecc665664db43be1aeea6641f1c19e213a
PIH6585 <pih6585@naver.com>
2021-08-16 16:31:25 +0900

    feat : 실형 결과값 추가

--- src/main/java/ladder/controller/LadderController.java
+++ src/main/java/ladder/controller/LadderController.java
@@ -3,6 +3,7 @@ package ladder.controller;
 import ladder.model.Height;
 import ladder.model.Lines;
 import ladder.model.Players;
+import ladder.model.Result;
 import ladder.service.LadderCreate;
 import ladder.view.LadderInputView;
 import ladder.view.LadderOutputView;
@@ -11,7 +12,7 @@ public class LadderController {
 
 	public static void main(String[] args) {
 		Players players = new Players(LadderInputView.inputParticipationPlayer());
-
+		Result result = new Result(LadderInputView.inputGameResult(),players);
 		Height height = new Height(LadderInputView.inputLadderHeight());
 
 		Lines ladderLines = LadderCreate.createLadder(players, height);
--- src/main/java/ladder/view/LadderInputView.java
+++ src/main/java/ladder/view/LadderInputView.java
@@ -8,11 +8,17 @@ import ladder.message.ErrorMessage;
 public class LadderInputView {
 
 	private static final String PARTICIPATION_NAME_MESSAGE = "참여할 사람 이름을 입력하세요.(이름은 쉼표(,)로 구분하세요)";
+	private static final String GAME_RESULT_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요.)";
 	private static final String LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
 	private static final Scanner scanner = new Scanner(System.in);
 
 	public static String inputParticipationPlayer() {
 		System.out.println(PARTICIPATION_NAME_MESSAGE);
+		return scanner.nextLine();
+	}
+
+	public static String inputGameResult() {
+		System.out.println(GAME_RESULT_MESSAGE);
 		return scanner.nextLine();
 	}
 
