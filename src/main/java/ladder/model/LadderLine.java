3d5a0521f9d2af368c3df4fc4936563cfef472ce
YooInKeun <keun0390@naver.com>
2021-08-16 21:51:41 +0900

    refactor: LadderLine 생성자에서 유효성 검사하도록 수정

--- src/main/java/ladder/model/LadderLine.java
+++ src/main/java/ladder/model/LadderLine.java
@@ -4,22 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LadderLine {
-    private static final int MIN_POINT_COUNT = 1;
-
     private final List<Boolean> points;
 
     LadderLine(List<Boolean> points) {
+        validateNotEmpty(points);
         this.points = points;
     }
 
     static LadderLine of(int pointCount) {
-        validateMinPointCount(pointCount);
         return new LadderLine(new ArrayList<>());
     }
 
-    private static void validateMinPointCount(int pointCount) {
-        if (pointCount < MIN_POINT_COUNT) {
-            throw new IllegalArgumentException(String.format("포인트 개수는 최소 %d개 이어야 합니다.", MIN_POINT_COUNT));
+    private void validateNotEmpty(List<Boolean> points) {
+        if (points == null || points.isEmpty()) {
+            throw new IllegalArgumentException("포인트 목록이 비었습니다.");
         }
     }
 }
--- src/test/java/ladder/model/LadderLineTest.java
+++ src/test/java/ladder/model/LadderLineTest.java
@@ -3,17 +3,23 @@ package ladder.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 @DisplayName("사다리 라인 테스트")
 public class LadderLineTest {
 
-    @DisplayName("사다리 라인의 포인트 개수가 1개 미만이면 예외가 발생한다.")
+    @DisplayName("사다리 라인의 포인트 목록이 비었으면 예외가 발생한다")
     @Test
-    void pointCountLessThanMinExceptionTest() {
+    void emptyLadderLineExceptionTest() {
         // given, when, then
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> LadderLine.of(0))
-                .withMessage("포인트 개수는 최소 1개 이어야 합니다.");
+                .isThrownBy(() -> new LadderLine(null))
+                .withMessage("포인트 목록이 비었습니다.");
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LadderLine(new ArrayList<>()))
+                .withMessage("포인트 목록이 비었습니다.");
     }
 }
