038c652b25411e24c6be13f4b2f9a772884e5d81
YooInKeun <keun0390@naver.com>
2021-08-16 22:13:23 +0900

    feat(chore): 겹치는 라인이 존재 여부, 유효성 검사 기능 추가

--- src/main/java/ladder/model/LadderLine.java
+++ src/main/java/ladder/model/LadderLine.java
@@ -2,13 +2,26 @@ package ladder.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.lang.Boolean.TRUE;
 
 public class LadderLine {
     private final List<Boolean> points;
 
     LadderLine(List<Boolean> points) {
         validateNotEmpty(points);
+        validateNoOverlappingLine(points);
         this.points = points;
+    }
+
+    private void validateNoOverlappingLine(List<Boolean> points) {
+        IntStream.range(0, points.size() - 1)
+                .filter(index -> points.get(index) == TRUE && points.get(index + 1) == TRUE)
+                .findAny()
+                .ifPresent(point -> {
+                    throw new IllegalArgumentException("겹치는 라인이 존재하면 안됩니다.");
+                });
     }
 
     static LadderLine of(int pointCount) {
--- src/test/java/ladder/model/LadderLineTest.java
+++ src/test/java/ladder/model/LadderLineTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
+import static java.lang.Boolean.*;
+import static java.lang.Boolean.FALSE;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 @DisplayName("사다리 라인 테스트")
@@ -21,5 +24,14 @@ public class LadderLineTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new LadderLine(new ArrayList<>()))
                 .withMessage("포인트 목록이 비었습니다.");
+    }
+
+    @DisplayName("겹치는 라인이 존재하면 예외가 발생한다.")
+    @Test
+    void overlappingLineExceptionTest() {
+        // given, when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LadderLine(Arrays.asList(FALSE, TRUE, TRUE, FALSE)))
+                .withMessage("겹치는 라인이 존재하면 안됩니다.");
     }
 }
