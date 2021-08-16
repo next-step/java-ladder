482f9fe6f7bb8a8f0e7ca2c572fa0f8ea862bac0
rkdals213 <rkdals213@naver.com>
2021-08-16 15:30:14 +0900

    feat: Ladder객체에도 데이터 검증 로직 추가

--- src/main/java/step3/domain/ladder/Ladder.java
+++ src/main/java/step3/domain/ladder/Ladder.java
@@ -10,14 +10,56 @@ public class Ladder {
 
     private final Participants participants;
 
+    private static final int MAX_NAME_LENGTH = 5;
+
+    private static final int MIN_HEIGHT = 1;
+
+    private static final int MIN_PARTICIPANTS = 1;
+
     public Ladder(List<LadderLine> ladderLines, List<String> names) {
         this.ladderLines = ladderLines;
         this.participants = Participants.of(names);
     }
 
     public static Ladder create(List<String> names, int height) {
+        validateNames(names);
+        validateHeight(height);
         List<LadderLine> lines = createLines(height, names.size());
         return new Ladder(lines, names);
+    }
+
+    private static void validateNames(List<String> names) {
+        for (String name : names) {
+            validateNamesLength(name);
+        }
+
+        if (lessThanMinParticipants(names.size())) {
+            throw new RuntimeException("참여자는 최소 1명 이상이여야 합니다.");
+        }
+    }
+
+    private static void validateNamesLength(String name) {
+        if (moreThanMaxNameLength(name.length())) {
+            throw new RuntimeException("이름의 글자는 최대 5자리를 넘을 수 없습니다.");
+        }
+    }
+
+    private static boolean moreThanMaxNameLength(int size) {
+        return size > MAX_NAME_LENGTH;
+    }
+
+    private static boolean lessThanMinParticipants(int size) {
+        return size < MIN_PARTICIPANTS;
+    }
+
+    private static void validateHeight(int height) {
+        if (heightIsNotAvailable(height)) {
+            throw new RuntimeException("사다리의 높이는 양수여야 합니다.");
+        }
+    }
+
+    private static boolean heightIsNotAvailable(int height) {
+        return height < MIN_HEIGHT;
     }
 
     private static List<LadderLine> createLines(int height, int size) {
