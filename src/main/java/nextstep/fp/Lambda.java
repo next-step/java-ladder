ef314b13860052f6bc928f1c8bb9739901465b98
권기준 <gabunot@gmail.com>
2021-08-17 01:56:29 +0900

    자바8 스트림, 람다, Optional step1 커밋

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -18,39 +18,19 @@ public class Lambda {
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> System.out.println("Hello from thread")).start();
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+//        return numbers.stream().mapToInt(number -> number).sum();
+        return numbers.stream().reduce(0, Integer::sum);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return numbers.stream().filter(number -> number % 2 == 0).reduce(0, Integer::sum);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return numbers.stream().filter(number -> number > 3).reduce(0, Integer::sum);
     }
 }
