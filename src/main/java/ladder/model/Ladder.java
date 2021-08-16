8e7a02a2d6c00940a228d1b71a752e82937c8804
YooInKeun <keun0390@naver.com>
2021-08-16 21:01:18 +0900

    feat(chore): 사다리 최소 높이, 유효성 검사 기능 추가

--- src/main/java/ladder/model/Ladder.java
+++ src/main/java/ladder/model/Ladder.java
@@ -5,17 +5,25 @@ import java.util.List;
 
 public class Ladder {
     private static final int MIN_PLAYER_COUNT = 2;
+    private static final int MIN_LADDER_HEIGHT = 1;
 
     private final List<Boolean> points;
 
-    public Ladder(int playerCount, int maxLadderHeight) {
+    Ladder(int playerCount, int ladderHeight) {
         validateMinPlayerCount(playerCount);
+        validateMinLadderHeight(ladderHeight);
         this.points = new ArrayList<>();
     }
 
     private void validateMinPlayerCount(int playerCount) {
         if (playerCount < MIN_PLAYER_COUNT) {
             throw new IllegalArgumentException(String.format("참가자 수는 최소 %d명 이어야 합니다.", MIN_PLAYER_COUNT));
+        }
+    }
+
+    private void validateMinLadderHeight(int ladderHeight) {
+        if (ladderHeight < MIN_LADDER_HEIGHT) {
+            throw new IllegalArgumentException(String.format("사다리 높이는 최소 %d 이어야 합니다.", MIN_LADDER_HEIGHT));
         }
     }
 }
--- src/test/java/ladder/model/LadderTest.java
+++ src/test/java/ladder/model/LadderTest.java
@@ -16,4 +16,13 @@ public class LadderTest {
                 .isThrownBy(() -> new Ladder(1, 5))
                 .withMessage("참가자 수는 최소 2명 이어야 합니다.");
     }
+
+    @DisplayName("사다리의 높이가 1 미만이면 예외가 발생한다.")
+    @Test
+    void ladderHeightLessThanMinExceptionTest() {
+        // given, when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Ladder(5, 0))
+                .withMessage("사다리 높이는 최소 1 이어야 합니다.");
+    }
 }
