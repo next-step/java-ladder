5d35ce2514a43770ff4ed8d911e690071379d133
PIH6585 <pih6585@naver.com>
2021-08-16 16:31:13 +0900

    feat : 에러 메세지 추가

--- src/main/java/ladder/message/ErrorMessage.java
+++ src/main/java/ladder/message/ErrorMessage.java
@@ -1,8 +1,9 @@
 package ladder.message;
 
 public class ErrorMessage {
-	public static final String EMPTY_ERROR_MESSAGE = "이름이 빈값 입니다.";
+	public static final String EMPTY_ERROR_MESSAGE = "빈값을 입력 할 수 없습니다.";
 	public static final String LIMIT_LENGTH_MESSAGE = "이름은 최대 5글자 입니다.";
 	public static final String NUMBER_TYPE_ERROR_MESSAGE = "숫자만 입력 가능합니다.";
 	public static final String LIMIT_HEIGHT_ERROR_MESSAGE = "높이는 0이하가 될 수 없습니다.";
+	public static final String NOT_EQUALS_LENGTH_ERROR_MESSAGE = "참여자와 결과값의 숫자가 일치하지 않습니다.";
 }
