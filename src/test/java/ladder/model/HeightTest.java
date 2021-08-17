7a4f09632554f2a602eccd15f70b5f60bb4a67c2
PIH6585 <pih6585@naver.com>
2021-08-17 10:38:04 +0900

    refactor : 띄어쓰기 수정

--- src/test/java/ladder/model/HeightTest.java
+++ src/test/java/ladder/model/HeightTest.java
@@ -10,7 +10,7 @@ class HeightTest {
 
 	@Test
 	@DisplayName("사다리의 높이를 생성한다.")
-	public void createLadderHeight(){
+	public void createLadderHeight() {
 		Height height = new Height(5);
 
 		assertThat(height).isEqualTo(new Height(5));
@@ -19,7 +19,7 @@ class HeightTest {
 
 	@Test
 	@DisplayName("사다리의 높이가 0이하이면 예외가 발생된다.")
-	public void checkLimitHeight(){
+	public void checkLimitHeight() {
 		assertThrows(IllegalArgumentException.class, () ->
 			new Height(0)
 		);
