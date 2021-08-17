490741ec8e32da60ebe2f9da1c6cbb2662477c70
kdh85 <modudamba@naver.com>
2021-08-17 22:03:31 +0900

    refactor : 사다리게임 step2 Line객체 생성 책임 관련 리팩토링 객체생성 책임을 객체 스스로가 관리하기 위해 객체생성 메소드를 서비스인 LineGenerator에서 Line 클래스로 이관함. LineGenerator는 삭제 처리함.

--- src/main/java/ladderGame/step2/model/Line.java
+++ src/main/java/ladderGame/step2/model/Line.java
@@ -1,20 +1,47 @@
 package ladderGame.step2.model;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import ladderGame.step2.service.LineGenerator;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class Line {
+
+  private static final int INT_ONE = 1;
+
+  private static Random random = new Random();
 
   private List<Boolean> points;
 
   public Line(final int countOfPerson) {
-    this.points = Collections.unmodifiableList(LineGenerator.createLine(countOfPerson));
+    this.points = Collections.unmodifiableList(createLine(countOfPerson));
   }
 
   public List<Boolean> getPoints() {
     return points;
+  }
+
+  public static List<Boolean> createLine(final int countOfPerson) {
+
+    List<Boolean> lines = new ArrayList<>();
+
+    lines.add(false);
+
+    IntStream.range(INT_ONE, countOfPerson)
+        .forEach(person -> lines.add(isHaveNode(lines.get(person - INT_ONE))));
+
+    return lines;
+  }
+
+  private static Boolean isHaveNode(final Boolean afterNode) {
+
+    if (afterNode) {
+      return false;
+    }
+    return random.nextBoolean();
+
   }
 
   @Override
