ab73d9e68f13b62a8cdae1491cfd558ca1721daf
kdh85 <modudamba@naver.com>
2021-08-17 23:27:17 +0900

    test : 사다리게임 step2 PlayersTest 테스트코드 리펙토링 기존에 스트링 메세지를 객체가 사용하는 메세지로 변경함.

--- src/test/java/ladderGame/step2/model/PlayersTest.java
+++ src/test/java/ladderGame/step2/model/PlayersTest.java
@@ -1,6 +1,7 @@
 package ladderGame.step2.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,5 +13,15 @@ class PlayersTest {
   void createPlayersTest() {
     Players players = new Players(Players.createUser("user1,user2"));
     assertThat(players.count()).isEqualTo(2);
+  }
+
+  @DisplayName("생성된 사용자수가 0을 초과하는지 검증하는 테스트.")
+  @Test
+  void validationPlayersCount() {
+    assertThatThrownBy(
+        ()->new Players(Players.createUser(",,,"))
+    )
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage(Players.MSG_ERROR_LIMIT_PLAYERS);
   }
 }
