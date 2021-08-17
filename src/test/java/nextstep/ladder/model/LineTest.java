ab164916667968ccf4a486e2e274c8ef4b9575e6
sangw0804 <sangw0804@likelion.org>
2021-08-17 23:21:32 +0900

    refactor(LineTest): 테스트 리팩토링

--- src/test/java/nextstep/ladder/model/LineTest.java
+++ src/test/java/nextstep/ladder/model/LineTest.java
@@ -6,47 +6,55 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LineTest {
+    private static final int HEIGHT_1 = 1;
+
+    private static final Leg legWidth0 = new Leg(0, HEIGHT_1);
+    private static final Leg legWidth1 = new Leg(1, HEIGHT_1);
+    private static final Leg legWidth2 = new Leg(2, HEIGHT_1);
+    private static final Leg legWidth3 = new Leg(3, HEIGHT_1);
+
     @Test
     @DisplayName("leg 의 같은 height 라도 인접한 leg 가 아니면 등록 가능")
     public void register() {
-        Leg leg0 = new Leg(0, 2);
-        Leg leg1 = new Leg(1, 2);
-        Line existLine = new Line(1);
-        existLine.register(leg0, leg1);
-        Leg leg2 = new Leg(2, 2);
-        Leg leg3 = new Leg(3, 2);
-        Line newLine = new Line(1);
-        newLine.register(leg3, leg2);
+        Line existLine = new Line(HEIGHT_1);
+        existLine.register(legWidth0, legWidth1);
+
+        Line newLine = new Line(HEIGHT_1);
+        newLine.register(legWidth3, legWidth2);
     }
 
     @Test
-    @DisplayName("leg 의 같은 height 에 다른 Line 이 있으면 안된다")
+    @DisplayName("leg 의 같은 height 에 다른 Line 이 있으면 등록 무시.")
     public void registerSameHeight() {
-        Leg leg0 = new Leg(0, 2);
-        Leg leg1 = new Leg(1, 2);
-        Line existLine = new Line(1);
-        existLine.register(leg0, leg1);
-        Leg leg2 = new Leg(2, 2);
+        Line existLine = new Line(HEIGHT_1);
+        existLine.register(legWidth0, legWidth1);
 
-        Line newLine = new Line(1);
-        newLine.register(leg1, leg2);
+        Line newLine = new Line(HEIGHT_1);
+        newLine.register(legWidth1, legWidth2);
 
         assertThat(newLine).hasFieldOrPropertyWithValue("leftLeg", null);
         assertThat(newLine).hasFieldOrPropertyWithValue("rightLeg", null);
     }
 
     @Test
-    @DisplayName("같은 위치에 두개의 line 을 등록할 수 없다")
+    @DisplayName("같은 위치에 두개의 line 은 등록 무시.")
     public void registerSameLine() {
-        Leg leg0 = new Leg(0, 2);
-        Leg leg1 = new Leg(1, 2);
-        Line existLine = new Line(1);
-        existLine.register(leg0, leg1);
+        Line existLine = new Line(HEIGHT_1);
+        existLine.register(legWidth0, legWidth1);
 
-        Line newLine = new Line(1);
-        newLine.register(leg0, leg1);
+        Line newLine = new Line(HEIGHT_1);
+        newLine.register(legWidth0, legWidth1);
 
         assertThat(newLine).hasFieldOrPropertyWithValue("leftLeg", null);
         assertThat(newLine).hasFieldOrPropertyWithValue("rightLeg", null);
+    }
+
+    @Test
+    @DisplayName("height 가 같은지 판단.")
+    public void heightIs() {
+        Line line = new Line(HEIGHT_1);
+
+        assertThat(line.heightIs(new CoordinateValue(HEIGHT_1))).isTrue();
+        assertThat(line.heightIs(new CoordinateValue(2))).isFalse();
     }
 }
