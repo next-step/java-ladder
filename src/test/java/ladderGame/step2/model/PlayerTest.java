43e02b6b941ddd70cadac680df08434d890273ad
kdh85 <modudamba@naver.com>
2021-08-17 23:26:32 +0900

    test : 사다리게임 step2 PlayerTest 테스트코드 추가. 객체생성에 관련된 테스트코드를 케이스별로 추가함.

--- src/test/java/ladderGame/step2/model/PlayerTest.java
+++ src/test/java/ladderGame/step2/model/PlayerTest.java
@@ -13,4 +13,18 @@ class PlayerTest {
     assertThat(new Player("user")).isEqualTo(new Player("user"));
   }
 
+  @DisplayName("사용자 수 만큼의 이름을 받아 플래이어를 생성.")
+  @Test
+  void createPlayers() {
+
+    assertThat(Players.createUser("user1,user2").size()).isEqualTo(2);
+    assertThat(Players.createUser("user1,user2")).contains(new Player("user1"));
+    assertThat(Players.createUser("user1,user2")).contains(new Player("user2"));
+  }
+
+  @DisplayName("중복되는 이름을 자동으로 제거하고 생성.")
+  @Test
+  void distinctDuplicateName() {
+    assertThat(Players.createUser("user3,user3").size()).isEqualTo(1);
+  }
 }
