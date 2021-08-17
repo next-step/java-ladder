b4a11b937221214359ef1b608da3cece18560f85
kdh85 <modudamba@naver.com>
2021-08-17 23:42:54 +0900

    test : 사다리게임 step2 LinesTest 테스트코드 리펙토링 기존에 스트링 메세지를 객체가 사용하는 메세지로 변경함.

--- src/test/java/ladderGame/step2/model/LinesTest.java
+++ src/test/java/ladderGame/step2/model/LinesTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class LinesTest {
+
   @DisplayName("사다리 최소 높이값 검증 테스트.")
   @ParameterizedTest
   @ValueSource(ints = {-1,0})
@@ -16,6 +17,6 @@ class LinesTest {
         ()->new Lines(height, 1)
     )
         .isInstanceOf(IllegalArgumentException.class)
-        .hasMessage("높이값은 최소 1부터 가능합니다.");
+        .hasMessage(Lines.MSG_ERROR_LIMIT_HEIGHT);
   }
 }
