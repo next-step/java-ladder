a5f2c1fb8ebd4dd7f862ed5f27ad27dc8634356a
changsubkwak <paradise1732@gmail.com>
2021-08-18 08:22:04 +0900

    Step1 - 1단계 - 자바8 스트림, 람다, Optional (#1031)
    * fix: '람다 실습 1 - 익명 클래스를 람다로 전환' 완료
    
    * fix: '람다 실습 2 - 람다를 활용해 중복 제거' 완료
    
    * fix: '람다 실습 2 - 람다를 활용해 중복 제거' 완료
    
    * fix: 'map, reduce, filter 실습 1, 2' 완료
    
    * fix: '요구사항 1 - Optional을 활용해 조건에 따른 반환' 완료
    
    * fix: '요구사항 2 - Optional에서 값을 반환' 완료
    
    * fix: '요구사항 3 - Optional에서 exception 처리' 완료
    
    * fix: sumAll함수의 함수 인터페이스를 사용한 수정
    
    * fix: 소문자로 변환하는 기능 추가
    
    * refactor: 조건문을 필터 두 개로 나누어 사용하였음

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,11 +16,9 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream()
+            .filter(word -> word.length() > 12)
+            .count();
     }
 
     public static void printLongestWordTop100() throws IOException {
@@ -28,6 +27,13 @@ public class StreamStudy {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         // TODO 이 부분에 구현한다.
+        words.stream()
+            .filter(s -> s.length() > 12)
+            .distinct()
+            .sorted((s1, s2) -> s2.length() - s1.length())
+            .limit(100)
+            .map(String::toLowerCase)
+            .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +45,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+            .filter(number -> number > 3)
+            .map(number -> number * 2)
+            .reduce(0, Integer::sum);
     }
 }
