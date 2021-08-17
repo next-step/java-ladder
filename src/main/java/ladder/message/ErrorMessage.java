318ca2cc2e608591513d5f2a444d8f753c3d8402
PIH6585 <pih6585@naver.com>
2021-08-17 10:37:52 +0900

    feat : 에러메세지 추가

--- src/main/java/ladder/message/ErrorMessage.java
+++ src/main/java/ladder/message/ErrorMessage.java
@@ -6,4 +6,6 @@ public class ErrorMessage {
 	public static final String NUMBER_TYPE_ERROR_MESSAGE = "숫자만 입력 가능합니다.";
 	public static final String LIMIT_HEIGHT_ERROR_MESSAGE = "높이는 0이하가 될 수 없습니다.";
 	public static final String NOT_EQUALS_LENGTH_ERROR_MESSAGE = "참여자와 결과값의 숫자가 일치하지 않습니다.";
+	public static final String NOT_FIND_PLAYER_ERROR_MESSAGE = "해당 플레이어는 존재하지 않습니다.";
+	public static final String NOT_MATCH_MOVE_ERROR_MESSAGE = "올바른 이동값이 아닙니다.";
 }
