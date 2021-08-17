11703e829307c9b7b95271a13553bd769166aaa6
ChangSubKwak <paradise1732@gmail.com>
2021-08-17 21:48:23 +0900

    fix: '요구사항 3 - Optional에서 exception 처리' 완료

--- src/main/java/nextstep/optional/Expression.java
+++ src/main/java/nextstep/optional/Expression.java
@@ -1,5 +1,8 @@
 package nextstep.optional;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 enum Expression {
     PLUS("+"), MINUS("-"), TIMES("*"), DIVIDE("/");
 
@@ -13,13 +16,21 @@ enum Expression {
         return expression.equals(e.expression);
     }
 
-    static Expression of(String expression) {
+    static Expression ofOld(String expression) {
         for (Expression v : values()) {
             if (matchExpression(v, expression)) {
                 return v;
             }
         }
-
         throw new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression));
+    }
+
+    static Expression of(String expression) {
+        String statement = String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression);
+
+        return Arrays.stream(Expression.values())
+            .filter(e -> matchExpression(e, expression))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException(statement));
     }
 }
