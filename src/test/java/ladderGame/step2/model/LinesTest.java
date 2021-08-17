0c9a65218badff0dcd9954c46c6dc63fb4a20c07
kdh85 <modudamba@naver.com>
2021-08-17 22:58:54 +0900

    test : 사다리게임 step2 LinesTest 높이값 검증코드 추가 입력되는 최소 높이값이 1이상인지를 검증하는 테스트코드 추가.

--- src/test/java/ladderGame/step2/model/LinesTest.java
+++ src/test/java/ladderGame/step2/model/LinesTest.java
@@ -1,19 +1,21 @@
 package ladderGame.step2.model;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class LinesTest {
-  @DisplayName("플래이어수와 사다리높이를 입력해서 사다리 생성.")
+  @DisplayName("사다리 최소 높이값 검증 테스트.")
   @ParameterizedTest
-  @ValueSource(ints = {1,2,3,4})
-  void createLadderLayerTest(int height) {
-    Lines lines = new Lines(height, 1);
-
-    assertThat(lines.toList().get(0)).isEqualTo(new Line(1));
-    assertThat(lines.toList().size()).isEqualTo(height);
+  @ValueSource(ints = {-1,0})
+  void limitHeightTest(int height) {
+    
+    assertThatThrownBy(
+        ()->new Lines(height, 1)
+    )
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("높이값은 최소 1부터 가능합니다.");
   }
 }
