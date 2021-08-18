0311f314423c82d15ec69056c564e6ec5237ec9b
YooInKeun <keun0390@naver.com>
2021-08-18 18:23:15 +0900

    refactor: PlayerName 유효성 검사 메소드 합치기

--- src/main/java/ladder/model/PlayerName.java
+++ src/main/java/ladder/model/PlayerName.java
@@ -6,18 +6,15 @@ public class PlayerName {
     private final String name;
 
     PlayerName(String name) {
-        validateNotEmpty(name);
-        validateMaxLength(name);
+        validate(name);
         this.name = name;
     }
 
-    private void validateNotEmpty(String name) {
+    private void validate(String name) {
         if (name == null || name.trim().isEmpty()) {
             throw new IllegalArgumentException("참가자 이름이 비어있습니다.");
         }
-    }
 
-    private void validateMaxLength(String name) {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException(String.format("참가자 이름은 %d자를 넘을 수 없습니다.", MAX_NAME_LENGTH));
         }
