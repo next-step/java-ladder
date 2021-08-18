5e7dfa07730442e808bdc26a832ef8e51e75609d
rladowl92 <rladowl92@naver.com>
2021-08-19 01:04:37 +0900

    test: 1명일 경우 실패 테스트

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LineTest {
     @DisplayName("사람 수-1만큼 라인이 생성되어야함.")
@@ -12,6 +13,12 @@ class LineTest {
         Line line = new Line(5);
         assertThat(line.size()).isEqualTo(4);
         System.out.println(line.toString());
+    }
+
+    @DisplayName("1명은 플레이 불가능")
+    @Test
+    void error() {
+        assertThatThrownBy(() -> new Line(1)).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
