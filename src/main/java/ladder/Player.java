5ba80114cd0a531c50fe8510861b4cbbebe87111
rladowl92 <rladowl92@naver.com>
2021-08-18 09:36:25 +0900

    feat: 사람 이름 vo 생성

---
+++ src/main/java/ladder/Player.java
@@ -0,0 +1,27 @@
+package ladder;
+
+import java.util.Objects;
+
+public class Player {
+    private String value;
+
+    public Player(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("사람 이름은 최대 5글자까지 입력 가능합니다.");
+        }
+        this.value = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(value, player.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
