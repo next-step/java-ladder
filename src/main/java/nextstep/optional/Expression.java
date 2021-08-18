82650069b30d3a3cf424a28b96a2c9c7f7f76e06
beadoer1 <beadoer1@gmail.com>
2021-08-18 16:46:06 +0900

    refactor(Optional) : Optional 실습 3 Expression.of() stream 을 이용한 구현

--- src/main/java/nextstep/optional/Expression.java
+++ src/main/java/nextstep/optional/Expression.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Arrays;
+
 enum Expression {
     PLUS("+"), MINUS("-"), TIMES("*"), DIVIDE("/");
 
@@ -13,13 +15,28 @@ enum Expression {
         return expression.equals(e.expression);
     }
 
-    static Expression of(String expression) {
-        for (Expression v : values()) {
-            if (matchExpression(v, expression)) {
-                return v;
-            }
-        }
+//    static Expression of(String expression) {
+//        for (Expression v : values()) {
+//            if (matchExpression(v, expression)) {
+//                return v;
+//            }
+//        }
+//
+//        throw new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression));
+//    }
 
-        throw new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression));
+    /**
+     * nextstep.optional.ExpressionTest 의 테스트가 통과하도록 Expression 의 of 메소드를 구현한다.
+     * 단, of 메소드를 구현할 때 자바 8의 stream 을 기반으로 구현한다.
+     */
+
+    static Expression of(String expression) {
+        return Arrays.stream(values())
+                .filter(v -> matchExpression(v, expression))
+                .findFirst()
+                .orElseThrow(
+                        () ->  new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression))
+                );
     }
+
 }
