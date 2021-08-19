b07ef075ebb4e3508ec19f46cd56b40a3ddb5ada
kdh85 <modudamba@naver.com>
2021-08-19 21:13:21 +0900

    feat  : 사다리게임(step3)- Point클래스 집합인 Line클래스 생성 Point 클래스의 집합인 불변 콜랙션 Line 클래스 구현. 입력되는 사용자수 검증.

---
+++ src/main/java/ladderGame/step3/model/Line.java
@@ -0,0 +1,65 @@
+package ladderGame.step3.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+import ladderGame.step3.util.RandomMove;
+
+public class Line {
+
+  public static final String MSG_ERROR_LIMIT_COUNT = "최소 1명이상의 플래이어를 입력해주세요.";
+
+  private static final int START_INDEX = 1;
+
+  private final List<Point> points;
+
+  public Line(final List<Point> points) {
+    this.points = Collections.unmodifiableList(points);
+  }
+
+  public static Line createLine(final int count) {
+
+    validationCount(count);
+
+    List<Point> newPoints = new ArrayList<>();
+
+    Point point = Point.first(RandomMove.createRandomMoveValue());
+
+    newPoints.add(point);
+
+    int lastCount = count - START_INDEX;
+
+    IntStream.range(START_INDEX, lastCount)
+        .mapToObj(i -> point.next(i, RandomMove.createRandomMoveValue()))
+        .forEach(newPoints::add);
+
+    newPoints.add(point.last(lastCount, RandomMove.createRandomMoveValue()));
+
+    return new Line(newPoints);
+  }
+
+  private static void validationCount(final int count) {
+    if (count < START_INDEX) {
+      throw new IllegalArgumentException(MSG_ERROR_LIMIT_COUNT);
+    }
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    final Line line = (Line) o;
+    return Objects.equals(points, line.points);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(points);
+  }
+}
