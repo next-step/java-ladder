bc5d9de07a336e797b4936335a27bc11309a12e4
kdh85 <modudamba@naver.com>
2021-08-19 19:17:45 +0900

    feat  : 사다리게임(step3)- 사다리 각 위치에 대한 정보를 담는 Point 클래스 생성 이전위치에 대한 판별값과 생성하는 시점의 위치값으로 사다리 정보를 생성. 각 위치값 기준으로 이동시 발생하는 이동거리를 반환받아서 관리.

---
+++ src/main/java/ladderGame/step3/model/Point.java
@@ -0,0 +1,69 @@
+package ladderGame.step3.model;
+
+import java.util.Objects;
+
+public class Point {
+
+  public static final String MSG_ERROR_LIMIT_MIN_VALUE = "시작값은 0보자 작을 수 없습니다.";
+
+  public static final String MSG_ERROR_INVALID_MOVE = "할 수 없는 움직임 입니다.";
+
+  private static final int LIMIT_INDEX_START = 0;
+
+  private final int currentPoint;
+
+  private final Location location;
+
+  public Point(final int currentPoint) {
+    this(currentPoint, new Location(false, false));
+  }
+
+  public Point(final int point, final Location location) {
+    validationPoint(point);
+    this.currentPoint = point;
+    this.location = location;
+  }
+
+  private void validationPoint(final int point) {
+    if (point < LIMIT_INDEX_START) {
+      throw new IllegalArgumentException(MSG_ERROR_LIMIT_MIN_VALUE);
+    }
+  }
+
+  public static Point first(final boolean randomMoveValue) {
+    return new Point(LIMIT_INDEX_START, Location.first(randomMoveValue));
+  }
+
+  public Point next(final int count, final boolean randomMoveValue) {
+    return new Point(count, this.location.next(randomMoveValue));
+  }
+
+  public Point last(final int count, final boolean randomMoveValue) {
+    return new Point(count, this.location.next(randomMoveValue).last());
+  }
+
+  public int move() {
+    if (this.currentPoint == LIMIT_INDEX_START &&
+        this.location.movement().equals(Direction.BACK)) {
+      throw new IllegalArgumentException(MSG_ERROR_INVALID_MOVE);
+    }
+    return this.currentPoint + this.location.point(location.movement());
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    final Point point = (Point) o;
+    return currentPoint == point.currentPoint && Objects.equals(location, point.location);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(currentPoint, location);
+  }
+}
