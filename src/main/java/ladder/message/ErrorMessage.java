bd3f9bc309cc139ce5bf3332b65b29bc5f69dade
PIH6585 <pih6585@naver.com>
2021-08-20 15:54:57 +0900

    refactor : 예외메세지 수정 및 추가

--- src/main/java/ladder/message/ErrorMessage.java
+++ src/main/java/ladder/message/ErrorMessage.java
@@ -2,11 +2,13 @@ package ladder.message;
 
 public class ErrorMessage {
 	public static final String EMPTY_ERROR_MESSAGE = "빈값을 입력 할 수 없습니다.";
-	public static final String LIMIT_LENGTH_MESSAGE = "이름은 최대 5글자 입니다.";
+	public static final String LIMIT_LENGTH_MESSAGE = "최대 5글자 까지 허용 됩니다.";
 	public static final String NUMBER_TYPE_ERROR_MESSAGE = "숫자만 입력 가능합니다.";
 	public static final String LIMIT_HEIGHT_ERROR_MESSAGE = "높이는 0이하가 될 수 없습니다.";
 	public static final String NOT_EQUALS_LENGTH_ERROR_MESSAGE = "참여자와 결과값의 숫자가 일치하지 않습니다.";
 	public static final String NOT_FIND_PLAYER_ERROR_MESSAGE = "해당 플레이어는 존재하지 않습니다.";
 	public static final String NOT_MATCH_MOVE_ERROR_MESSAGE = "올바른 이동값이 아닙니다.";
 	public static final String DIRECTION_STATUS_ERROR_MESSAGE = "양방향으로 사다리가 진행 될 수 없습니다.";
+	public static final String INSTANCE_CREATE_ERROR_MESSAGE = "인스턴스 생성이 불가능 합니다.";
+	public static final String NULL_ERROR_MESSAGE = "NULL 을 허용 할 수 없습니다.";
 }
