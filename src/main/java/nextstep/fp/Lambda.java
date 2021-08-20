9fcecd481e0b4373cc7bb4914203098b6442da9d
iiaii <jhun9409@naver.com>
2021-08-20 23:37:36 +0900

    [step1] refactor: 람다 표현으로 수정 및 Conditional 인터페이스 추가

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -13,44 +13,30 @@ public class Lambda {
 
     public static void printAllLambda(List<Integer> numbers) {
         System.out.println("printAllLambda");
-
         numbers.forEach(System.out::println);
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> System.out.println("Hello from thread"))
+                .start();
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAll(numbers, number -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, number -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, number -> number > 3);
     }
+
+    private static int sumAll(List<Integer> numbers, Conditional condition) {
+        return numbers.stream()
+                .filter(condition::test)
+                .reduce(0, Integer::sum);
+    }
+
 }
