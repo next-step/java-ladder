7fee6f3516ef18b5fcdd0323097dbad4302abf17
hjjangDev <ju0398@hanmail.net>
2021-08-20 13:44:17 +0900

    feat : ladderException 추가

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -1,15 +1,18 @@
 package ladder.domain;
 
 import ladder.exception.InvalidInputException;
+import ladder.exception.LadderException;
 import ladder.strategy.PointStrategy;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.IntStream;
 
 public class Line {
-    private static final String INVALID_HEIGHT_MESSAGE = "2이상의 높이를 입력해주세요";
+    private static final String INVALID_HEIGHT_MESSAGE = "2 이상의 높이를 입력해주세요";
+    private static final String INVALID_TRY_COUNT_MESSAGE = "사다리 생성 시도 횟수를 초과했습니다. 다시 시도해주세요.";
 
     private List<Boolean> points;
 
@@ -30,12 +33,15 @@ public class Line {
     }
 
 
-    public static Line of(int height, Line line) {
+    public static Line of(int height, Line prevLine) {
         List<Boolean> points = new ArrayList<>();
+        int tryCount = 0;
         while (!valid(points)) {
             points.clear();
+            checkTryCount(tryCount);
             IntStream.range(0, height)
-                     .forEach(point -> points.add(PointStrategy.generate(line.getPoints().get(point))));
+                     .forEach(point -> points.add(PointStrategy.generate(prevLine.getPoints().get(point))));
+            tryCount++;
         }
 
         return new Line(points);
@@ -48,9 +54,26 @@ public class Line {
         return true;
     }
 
+    private static void checkTryCount(int tryCount) {
+        if (tryCount > 20) {
+            throw new LadderException(INVALID_TRY_COUNT_MESSAGE);
+        }
+    }
+
     public List<Boolean> getPoints() {
         return Collections.unmodifiableList(points);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Line line = (Line) o;
+        return Objects.equals(points, line.points);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(points);
+    }
 }
---
+++ src/main/java/ladder/exception/LadderException.java
@@ -0,0 +1,7 @@
+package ladder.exception;
+
+public class LadderException extends RuntimeException {
+    public LadderException(String s) {
+        super(s);
+    }
+}
