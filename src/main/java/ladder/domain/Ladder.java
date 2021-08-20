7efa4f2dfa55994c6a2460316385c92865ba0dd6
ChangSubKwak <paradise1732@gmail.com>
2021-08-20 22:02:59 +0900

    feat: 사다리의 높이 유효성 체크 추가

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -8,6 +8,7 @@ public class Ladder {
     private final List<Line> ladder;
 
     private Ladder(int countOfPerson, int height) {
+        validate(height);
         ladder = IntStream.range(0, height)
             .mapToObj(index -> Line.from(countOfPerson))
             .collect(Collectors.toList());
@@ -15,6 +16,12 @@ public class Ladder {
 
     public static Ladder from(int countOfPerson, int height) {
         return new Ladder(countOfPerson, height);
+    }
+
+    private void validate(int height) {
+        if (height < 2) {
+            throw new IllegalArgumentException("사다리의 높이는 1이상이어야 합니다.");
+        }
     }
 
     public List<Line> toList() {
