979e9bf9d6c9b1d6f18a9083015baa93df97fa35
kdh85 <modudamba@naver.com>
2021-08-20 15:25:06 +0900

    feat  : 사다리게임(step3)- LadderApplication 상품명 입,출력 추가 입력하는 상품명을 반환하여 출력하도록 구현.

--- src/main/java/ladderGame/step3/LadderApplication.java
+++ src/main/java/ladderGame/step3/LadderApplication.java
@@ -2,6 +2,7 @@ package ladderGame.step3;
 
 import ladderGame.step3.model.Ladder;
 import ladderGame.step3.model.Players;
+import ladderGame.step3.model.Prizes;
 import ladderGame.step3.view.InputView;
 import ladderGame.step3.view.ResultView;
 
@@ -9,12 +10,15 @@ public class LadderApplication {
 
   public static void main(String[] args) {
     String usersName = InputView.inputUserNames();
+    String goods = InputView.inputGoods();
     int ladderHeight = InputView.inputLadderHeight();
 
     Players players = new Players(Players.of(usersName));
+    Prizes prizes = new Prizes(Prizes.createPrizes(goods, players.count()));
     Ladder ladder = new Ladder(Ladder.of(ladderHeight, players.count()));
 
     ResultView.printUsersName(players.playersName());
     ResultView.printLadder(ladder);
+    ResultView.printPrizes(prizes);
   }
 }
