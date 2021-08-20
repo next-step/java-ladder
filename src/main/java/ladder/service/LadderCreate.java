a76298d3045f01bb59091a1956b7f7f021c89083
PIH6585 <pih6585@naver.com>
2021-08-20 15:55:08 +0900

    refactor : 전략패턴 삭제에 따른 파라미터 수정

--- src/main/java/ladder/service/LadderCreate.java
+++ src/main/java/ladder/service/LadderCreate.java
@@ -5,10 +5,8 @@ import java.util.stream.IntStream;
 
 import ladder.model.Height;
 import ladder.model.Line;
-import ladder.model.LineSketch;
 import ladder.model.Lines;
 import ladder.model.Players;
-import ladder.strategy.RandomStrategy;
 
 public class LadderCreate {
 
@@ -16,7 +14,7 @@ public class LadderCreate {
 
 	public static Lines createLadder(Players players, Height height) {
 		return new Lines(IntStream.range(START_INCLUSIVE, height.getHeight())
-			.mapToObj(i -> Line.createLine(players.findPlayerCount(),new LineSketch()))
+			.mapToObj(i -> Line.createLine(players.findPlayerCount()))
 			.collect(Collectors.toList()));
 	}
 
