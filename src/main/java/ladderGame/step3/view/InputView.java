6ebe1d45b27314589e4381e079d2d02c94ce1e69
kdh85 <modudamba@naver.com>
2021-08-20 15:22:08 +0900

    feat  : 사다리게임(step3)- InputView클래스 상품등록 메소드 추가 상품명을 쉼표를 포함여 입력받아 문자열로 반환하는 메소드 구현.

--- src/main/java/ladderGame/step3/view/InputView.java
+++ src/main/java/ladderGame/step3/view/InputView.java
@@ -7,6 +7,7 @@ public class InputView {
   private static final String MSG_INSERT_PLAYER_NAMES = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
 
   private static final String MSG_INSERT_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
+  private static final String MSG_INSERT_GOODS = "실행 결과를 입력하세요. (결과는 (,)로 구분하세요)";
 
   private static Scanner scanner = new Scanner(System.in);
 
@@ -18,5 +19,10 @@ public class InputView {
   public static int inputLadderHeight() {
     System.out.println(MSG_INSERT_HEIGHT);
     return scanner.nextInt();
+  }
+
+  public static String inputGoods() {
+    System.out.println(MSG_INSERT_GOODS);
+    return scanner.nextLine();
   }
 }
