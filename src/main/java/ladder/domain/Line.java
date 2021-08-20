9204a2ef02720a9f7e92a62eb0b221a7c1ce92fb
ChangSubKwak <paradise1732@gmail.com>
2021-08-20 22:01:41 +0900

    fix: Point클래스 추가에 따른 수정

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -1,75 +1,49 @@
 package ladder.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Line {
-    private List<Boolean> points;
+    private final int START_INDEX_MIDDLE_LINE = 1;
+    private final List<Point> points;
 
     private Line(int countOfPerson) {
-        Random random = new Random();
-        points = IntStream.range(0, countOfPerson)
-            .mapToObj(point -> random.nextBoolean())
-            .collect(Collectors.toList());
-        commonInit();
-    }
-
-    private Line(List<Boolean> points) {
-        this.points = points;
-        commonInit();
-    }
-
-    private void commonInit() {
-        points.set(points.size() - 1, false);
-        checkCorrectLadderAndFix();
-    }
-
-    private boolean isTrueEqualToPrevious(int currentIndex) {
-        if (currentIndex < 1) {
-            return false;
-        }
-
-        if (points.get(currentIndex - 1) == points.get(currentIndex)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private void checkCorrectLadderAndFix() {
-        if (checkNoneTrueAndChange(points.contains(true))) {
-            return;
-        }
-        checkContinuousTrueAndChange();
-    }
-
-    private void checkContinuousTrueAndChange() {
-        Random random = new Random();
-        IntStream.range(0, points.size() - 1)
-            .filter(this::isTrueEqualToPrevious)
-            .forEach(index -> points.set(index - random.nextInt(2), false));
-    }
-
-    private boolean checkNoneTrueAndChange(boolean containsTrue) {
-        if (!containsTrue) {
-            Random random = new Random();
-            points.set(random.nextInt(points.size() - 1), true);
-            return true;
-        }
-        return false;
+        validate(countOfPerson);
+        List<Point> points = new ArrayList<>();
+        constructLine(countOfPerson, points);
+        this.points = Collections.unmodifiableList(points);
     }
 
     public static Line from(int countOfPerson) {
         return new Line(countOfPerson);
     }
 
-    public static Line from(List<Boolean> points) {
-        return new Line(points);
+    private void validate(final int countOfPerson) {
+        if (countOfPerson < 2) {
+            throw new IllegalArgumentException("사람 수는 최소 2명 이상이어야 합니다.");
+        }
     }
 
-    public List<Boolean> toList() {
+    private void constructLine(final int countOfPerson, final List<Point> points) {
+        Random random = new Random();
+        points.add(Point.of(false, random.nextBoolean()));
+        IntStream.range(START_INDEX_MIDDLE_LINE, countOfPerson - 1)
+            .forEach(i -> points.add(Point.of(isExistLeft(random, points.get(i - 1)), random.nextBoolean())));
+        points.add(Point.of(random.nextBoolean(), false));
+    }
+
+    private boolean isExistLeft(final Random random, final Point point) {
+        boolean isExistLeft = random.nextBoolean();
+        if (point.existRight()) {
+            isExistLeft = false;
+        }
+        return isExistLeft;
+    }
+
+    public List<Point> toList() {
         return points;
     }
 }
