245785140201a3cf9e8bb83c3fc76e9a9011a533
beadoer1 <beadoer1@gmail.com>
2021-08-20 23:55:41 +0900

    feat(LadderGame) : LaadderGame 생성자 구현

---
+++ src/main/java/laddergame/domain/LadderGame.java
@@ -0,0 +1,22 @@
+package laddergame.domain;
+
+import java.util.List;
+
+public class LadderGame {
+
+    private final People people;
+    private final Ladder ladder;
+
+    public LadderGame(String peopleString, int heightOfLadder) {
+        this.people = new People(peopleString);
+        this.ladder = new Ladder(people.size(), heightOfLadder);
+    }
+
+    public List<Person> getPeople() {
+        return people.getPeople();
+    }
+
+    public List<Line> getLadder() {
+        return ladder.getLadder();
+    }
+}
