1b7b4cebef707d73ec99bbd70fc3e6744a8f0aec
beadoer1 <beadoer1@gmail.com>
2021-08-21 08:41:37 +0900

    feat(Person) : Person 예외 별도 분리 처리

--- src/main/java/laddergame/LadderGameMain.java
+++ src/main/java/laddergame/LadderGameMain.java
@@ -1,18 +1,23 @@
 package laddergame;
 
 import laddergame.domain.LadderGame;
+import laddergame.exception.CustomException;
 import laddergame.view.InputView;
 import laddergame.view.ResultView;
 
 public class LadderGameMain {
 
     public static void main(String[] args){
-        String peopleString = InputView.inputPeople();
+        try {
+            String peopleString = InputView.inputPeople();
 
-        int heightOfLadder = InputView.inputHeightOfLadder();
+            int heightOfLadder = InputView.inputHeightOfLadder();
 
-        LadderGame ladderGame = new LadderGame(peopleString, heightOfLadder);
+            LadderGame ladderGame = new LadderGame(peopleString, heightOfLadder);
 
-        ResultView.showResult(ladderGame);
+            ResultView.showResult(ladderGame);
+        } catch (CustomException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/laddergame/domain/Person.java
+++ src/main/java/laddergame/domain/Person.java
@@ -7,16 +7,33 @@ import java.util.Optional;
 
 public class Person {
     public static final int MAX_LENGTH_OF_NAME = 5;
+    public static final String LENGTH_OF_NAME_EXCEPTION_MESSAGE = "참여자 이름 양식 오류. 최대 " + MAX_LENGTH_OF_NAME + "자";
+    public static final String EMPTY_STRING_EXCEPTION_MESSAGE = "참여자 이름 양식 오류. 빈 값 미허용";
 
     private final String name;
 
     public Person(String name) {
         this.name = Optional.ofNullable(name)
                 .map(String::trim)
-                .filter((n) -> (0 < n.length() && n.length() <= MAX_LENGTH_OF_NAME))
+                .map(Person::validateLengthOfName)
+                .map(Person::validateEmptyString)
                 .orElseThrow(
-                        () -> new CustomException("이름 양식에 맞지 않습니다. 빈 값 미허용, 최대 " + MAX_LENGTH_OF_NAME + "자")
+                        () -> new CustomException(EMPTY_STRING_EXCEPTION_MESSAGE)
                 );
+    }
+
+    private static String validateLengthOfName(String name) {
+        if (name.length() > MAX_LENGTH_OF_NAME) {
+            throw new CustomException(LENGTH_OF_NAME_EXCEPTION_MESSAGE);
+        }
+        return name;
+    }
+
+    private static String validateEmptyString(String name) {
+        if (name.length() == 0) {
+            throw new CustomException(EMPTY_STRING_EXCEPTION_MESSAGE);
+        }
+        return name;
     }
 
     public String getName() {
