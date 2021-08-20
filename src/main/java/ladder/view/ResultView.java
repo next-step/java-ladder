f3e2c4a5c097bc24de2f62f6cf776fad6e5fb1e8
ChangSubKwak <paradise1732@gmail.com>
2021-08-20 22:06:26 +0900

    fix: Point클래스 추가에 따라 스트림 구현 부분 대폭 수정

--- src/main/java/ladder/view/ResultView.java
+++ src/main/java/ladder/view/ResultView.java
@@ -1,61 +1,53 @@
 package ladder.view;
 
+import ladder.domain.Line;
+import ladder.domain.Point;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import ladder.domain.Line;
-
 public class ResultView {
     private static final String OUTPUT_EXECUTION_RESULT = "실행결과";
-    private static final String OUTPUT_LADDER_FRONT_MARGIN = " ";
-    private static final String OUTPUT_LADDER_WIDTH_STRING = "-";
-    private static final String OUTPUT_LADDER_HEIGHT_STRING = "|";
+    private static final String OUTPUT_LADDER_LINE_UNIT_SPACE = " ";
+    private static final String OUTPUT_LADDER_LINE_UNIT_DASH = "-";
+    private static final String OUTPUT_LADDER_LINE_UNIT_COLUMN = "|";
+    private static final String DEFAULT_NAME_OUTPUT_FORMAT = "%6s";
     private static final int DEFAULT_LADDER_WIDTH = 5;
 
-    private void outputLadderWidth(boolean isPrint, boolean isLast) {
-        StringBuilder stringBuilder = new StringBuilder(OUTPUT_LADDER_HEIGHT_STRING);
-        String lineComponent = " ";
-        if (isPrint && !isLast) {
-            lineComponent = OUTPUT_LADDER_WIDTH_STRING;
-        }
-
-        String finalLineComponent = lineComponent;
-        stringBuilder.append(IntStream.range(0, DEFAULT_LADDER_WIDTH)
-            .mapToObj(e -> finalLineComponent)
-            .collect(Collectors.joining(""))
-        );
-        System.out.print(stringBuilder);
-    }
-
-    private void outputFrontMargin() {
-        IntStream.range(0, DEFAULT_LADDER_WIDTH)
-            .forEach(i -> System.out.print(OUTPUT_LADDER_FRONT_MARGIN));
-    }
-
-    private void outputLadderLine(Line line) {
-        outputFrontMargin();
-
-        int lengthOfLine = line.toList().size();
-        IntStream.range(0, lengthOfLine)
-                .forEach(i -> outputLadderWidth(line.toList().get(i), i == lengthOfLine - 1));
-
-        System.out.print(System.lineSeparator());
-    }
-
-
-    public void outputLadder(List<Line> lines, List<String> names) {
-        System.out.println("\n" + OUTPUT_EXECUTION_RESULT + "\n");
-
-        outputNames(names);
-        IntStream.range(0, lines.size())
-            .forEach(i -> outputLadderLine(lines.get(i)));
+    private void outputName(String name) {
+        System.out.printf(DEFAULT_NAME_OUTPUT_FORMAT, name);
     }
 
     private void outputNames(List<String> names) {
-        names.stream()
-                .forEach(name -> System.out.printf("%6s", name));
-
+        names.forEach(this::outputName);
         System.out.print(System.lineSeparator());
+    }
+
+    private void outputLadderWidth(Point point) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String lineUnit = OUTPUT_LADDER_LINE_UNIT_SPACE;
+        if (point.existLeft()) {
+            lineUnit = OUTPUT_LADDER_LINE_UNIT_DASH;
+        }
+
+        final String finalLineUnit = lineUnit;
+        stringBuilder.append(IntStream.range(0, DEFAULT_LADDER_WIDTH)
+            .mapToObj(e -> finalLineUnit)
+            .collect(Collectors.joining(""))
+        );
+        stringBuilder.append(OUTPUT_LADDER_LINE_UNIT_COLUMN);
+        System.out.print(stringBuilder);
+    }
+
+    private void outputLadderLine(Line line) {
+        line.toList().forEach(this::outputLadderWidth);
+        System.out.print(System.lineSeparator());
+    }
+
+    public void outputLadder(List<Line> lines, List<String> names) {
+        System.out.println("\n" + OUTPUT_EXECUTION_RESULT + "\n");
+        outputNames(names);
+        lines.forEach(this::outputLadderLine);
     }
 }
