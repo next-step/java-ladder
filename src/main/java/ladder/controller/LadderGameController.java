8a4dc477dc857eb2527e4f174bfad8337cc385ea
ChangSubKwak <paradise1732@gmail.com>
2021-08-20 09:01:43 +0900

    feat: 입력과 출력을 제어하는 클래스(LadderGameController)의 구현

---
+++ src/main/java/ladder/controller/LadderGameController.java
@@ -0,0 +1,22 @@
+package ladder.controller;
+
+import java.util.List;
+
+import ladder.domain.Ladder;
+import ladder.view.InputView;
+import ladder.view.ResultView;
+
+public class LadderGameController {
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
+
+        List<String> names = inputView.inputParticipantsName();
+        int height = inputView.inputMaximumHeight();
+
+        Ladder ladder = Ladder.from(names.size(), height);
+        resultView.outputLadder(ladder.toList(), names);
+
+        inputView.scannerClose();
+    }
+}
