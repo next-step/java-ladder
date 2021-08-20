7f48a85119430fc2d2890bc30dcaf440f61e8ad4
kimdonggyu-dev <donggyu.dev@gmail.com>
2021-08-20 13:34:36 +0900

    refactor: RandomPointGenerateStrategy Random 멤버 변수 static 추가

--- src/main/java/nextstep/ladders/domain/RandomPointGenerateStrategy.java
+++ src/main/java/nextstep/ladders/domain/RandomPointGenerateStrategy.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class RandomPointGenerateStrategy implements PointGenerateStrategy {
 
-    private final Random random = new Random();
+    private static final Random random = new Random();
 
     @Override
     public boolean next(final boolean previous) {
