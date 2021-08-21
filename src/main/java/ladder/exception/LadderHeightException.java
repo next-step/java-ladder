08398400fd1377845d47d2439ff43f86c21ad471
ChangSubKwak <paradise1732@gmail.com>
2021-08-22 05:31:38 +0900

    feat: 사다리높이, 이름 길이, 사람 인원 등에 대한 예외 클래스의 작성

---
+++ src/main/java/ladder/exception/LadderHeightException.java
@@ -0,0 +1,7 @@
+package ladder.exception;
+
+public class LadderHeightException extends RuntimeException {
+    public LadderHeightException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ladder/exception/NameLengthException.java
@@ -0,0 +1,7 @@
+package ladder.exception;
+
+public class NameLengthException extends RuntimeException {
+    public NameLengthException(String message) {
+        super(message);
+    }
+}
