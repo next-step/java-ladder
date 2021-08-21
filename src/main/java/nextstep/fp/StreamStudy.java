2819e1fcda7c9072e4eeb3cf68aaee8504b52a26
LAPTOP-I4NE0UIJ\yongs <smilesigma@gmail.com>
2021-08-22 08:29:55 +0900

    feat: "길이가 12이상인 단어 필터 -> 길이순대로 정렬 -> 100개 한정 -> 소문자로 출력 " 하는 함수 구현.

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,7 +24,14 @@ public class StreamStudy {
             .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        Comparator<String> comparingByLength = Comparator.comparingInt(String::length);
+
+        words.stream()
+            .filter(w -> w.length() > 12)
+            .sorted(comparingByLength.reversed())
+            .distinct()
+            .limit(100)
+            .forEach(w -> System.out.println(w.toLowerCase()));
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
