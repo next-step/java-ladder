dd1f66acf12917f5e82c9c1d21c93c8ce711a239
ChangSubKwak <paradise1732@gmail.com>
2021-08-22 05:40:02 +0900

    fix: 상수 추가 및 스트림 사용을 Stream.generate를 사용하도록 변경

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -1,17 +1,25 @@
 package ladder.domain;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import ladder.exception.LadderHeightException;
 
 public class Ladder {
+    private static final int LADDER_MIN_HEIGHT = 2;
+    private static final String LADDER_MIN_HEIGHT_EXCEPTION_COMMENT = "사다리의 높이는 %d이상이어야 합니다.";
+
     private final List<Line> ladder;
 
     private Ladder(int countOfPerson, int height) {
         validate(height);
-        ladder = IntStream.range(0, height)
-            .mapToObj(index -> Line.from(countOfPerson))
-            .collect(Collectors.toList());
+        ladder = Collections.unmodifiableList(
+            Stream.generate(() -> Line.from(countOfPerson))
+                .limit(height)
+                .collect(Collectors.toList())
+        );
     }
 
     public static Ladder from(int countOfPerson, int height) {
@@ -19,8 +27,8 @@ public class Ladder {
     }
 
     private void validate(int height) {
-        if (height < 2) {
-            throw new IllegalArgumentException("사다리의 높이는 1이상이어야 합니다.");
+        if (height < LADDER_MIN_HEIGHT) {
+            throw new LadderHeightException(String.format(LADDER_MIN_HEIGHT_EXCEPTION_COMMENT, LADDER_MIN_HEIGHT));
         }
     }
 
