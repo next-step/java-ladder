b10d948827b1c684603309d97d1ede1457d549de
harris <harrisleesh@gmail.com>
2021-08-21 10:21:46 +0900

    test : Create Line count and points test

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -2,8 +2,14 @@ package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LineTest {
 
@@ -14,6 +20,20 @@ class LineTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new Line(personCount, () -> true))
                 .withMessage("사다리 게임에 참여하는 사람 수가 너무 작습니다. 사람 수 : " + personCount);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8})
+    @DisplayName("항상 true인 확률로 라인생성 했을 때 라인은 사람 수 / 2 만큼 생성된다. 0, 2, 4 순으로 True 이다.")
+    void trueConstructor(int personCount) {
+        int lineCountExpect = personCount / 2;
+        Line line = new Line(personCount, () -> true);
+        long lineCountActual = line.getPoints().stream()
+                .filter(point -> point)
+                .count();
+        assertEquals(lineCountExpect, lineCountActual);
+        IntStream.range(0, personCount / 2).forEach(idx ->
+                assertTrue(line.getPoints().get(idx * 2)));
     }
 
 }
