33cc457ac580f64a3bdf0f252baf2a10ebe86925
ChangSubKwak <paradise1732@gmail.com>
2021-08-22 05:35:36 +0900

    fix: 커스텀 예외 클래스 추가에 따른 수정 및 @DisplayName 추가

--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -1,14 +1,17 @@
 package ladder;
 
-import ladder.domain.Ladder;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import ladder.domain.Ladder;
+import ladder.exception.LadderHeightException;
 
 class LadderTest {
+    @DisplayName("참여할_사람의_인원_수와_사다리_높이가_주어지면_사다리_높이만큼의_Line객체를_가진_Ladder가_생성된다")
     @Test
-    void 참여할_사람의_인원_수와_사다리_높이가_주어지면_사다리_높이만큼의_Line객체를_가진_Ladder가_생성된다() {
+    void newLadderTest() {
         int countOfPerson = 4;
         int height = 5;
         Ladder ladder = Ladder.from(countOfPerson, height);
@@ -17,10 +20,11 @@ class LadderTest {
         assertThat(ladder.toList().size()).isEqualTo(height);
     }
 
+    @DisplayName("사다리_높이가_0이하이면_예외가_발생한다")
     @Test
-    void 사다리_높이가_0이하이면_예외가_발생한다() {
+    void exceptionTest() {
         int countOfPerson = 4;
         int height = 0;
-        assertThatThrownBy(() -> Ladder.from(countOfPerson, height)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Ladder.from(countOfPerson, height)).isInstanceOf(LadderHeightException.class);
     }
 }
