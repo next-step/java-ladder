cd1c35022dbad6d74710638b035430b62dbc7785
Sangmin Lee <dltkdals2202@gmail.com>
2021-08-21 17:43:49 +0900

    Step1 리뷰 요청 드립니다! (#1052)
    * refactor : 익명 클래스를 람다로 전환하도록 수정
    
    * refactor : 람다를 활용한 중복제거
    
    * refactor : filter를 이용하여 문자 카운트 하는 기능 수정
    
    * Revert "refactor : filter를 이용하여 문자 카운트 하는 기능 수정"
    
    This reverts commit fc142dd4
    
    * feat : map, reduce, filter 실습 구현
    
    * feature : map, reduce, filter 실습2 구현
    
    * feat : Optional 요구사항 1 구현
    
    * Revert "feat : Optional 요구사항 1 구현"
    
    This reverts commit ccee2b7e49e7e2aa8e1798b0b7ed6888b9d7b042.
    
    * feat : Optional 요구사항 1 구현
    
    * feat : Optional 요구사항 2 구현
    
    * feat : Optional 요구사항 3 구현

--- src/main/java/nextstep/optional/Users.java
+++ src/main/java/nextstep/optional/Users.java
@@ -2,22 +2,29 @@ package nextstep.optional;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Users {
     static final User DEFAULT_USER = new User("codesquad", 100);
 
     List<User> users = Arrays.asList(
+            null,
             new User("crong", 35),
             new User("pobi", 30),
             new User("jk", 40),
+            new User(null, 40),
+            new User(null, null),
+            new User("kk", null),
             new User("honux", 45));
 
     User getUser(String name) {
-        for (User user : users) {
-            if (user.matchName(name)) {
-                return user;
-            }
-        }
-        return DEFAULT_USER;
+
+        return users.stream()
+                .filter(user -> Optional.ofNullable(user).isPresent())
+                .filter(user -> Optional.ofNullable(user.getName()).isPresent())
+                .filter(user -> user.matchName(name))
+                .findAny()
+                .orElse(DEFAULT_USER);
+
     }
 }
