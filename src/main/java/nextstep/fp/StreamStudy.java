8bda91e79ac587dc79b1edeaae7c40c8ebc206d5
최정헌 <jhun9409@gmail.com>
2021-08-21 11:00:08 +0900

    [step1] 자바8 스트림, 람다, Optional (#1049)
    * [step1] refactor: 람다 표현으로 수정 및 Conditional 인터페이스 추가
    
    * [step1] refactor: 람다 스트림 적용하여 수정
    
    * [step1] refactor: Optional 적용

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -8,6 +8,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.Comparator.comparing;
+
 public class StreamStudy {
 
     public static long countWords() throws IOException {
@@ -15,30 +17,47 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream()
+                .filter(StreamStudy::isLongerThanTwelve)
+                .count();
     }
 
     public static void printLongestWordTop100() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+        int maxSize = 100;
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+                .distinct()
+                .filter(StreamStudy::isLongerThanTwelve)
+                .sorted(comparing(String::length).reversed())
+                .limit(maxSize)
+                .map(String::toLowerCase)
+                .forEach(System.out::println);
+    }
+
+    private static boolean isLongerThanTwelve(final String word) {
+        return word.length() > 12;
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
+        return doubleNumbers(numbers, number -> true);
+    }
+
+    private static List<Integer> doubleNumbers(List<Integer> numbers, Conditional conditional) {
+        return numbers.stream()
+                .filter(conditional::test)
+                .map(x -> 2 * x)
+                .collect(Collectors.toList());
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers.stream()
+                .reduce(0, Integer::sum);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return sumAll(doubleNumbers(numbers, number -> number > 3));
     }
 }
