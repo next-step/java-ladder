15613bb527b0d7dd003e676f21fe041f97f9fa6b
채윤병 <ybchae0801@naver.com>
2021-08-22 00:02:26 +0900

    feat: map, reduce, filter 실습 1
    - List에 담긴 모든 숫자 중 3보다 큰 숫자를 2배 한 후 모든 값의 합을 구한다.

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -39,6 +39,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(number -> number > 3)
+                .mapToInt(number -> number * 2)
+                .reduce(0, Integer::sum);
     }
 }
