27cecbfa8a07e41e0979fe97cc63ebc0a45c185c
harris <harrisleesh@gmail.com>
2021-08-22 15:33:04 +0900

    refactor : Remove no use test

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -1,16 +1,13 @@
 package ladder.domain;
 
-import ladder.view.ResultView;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 class LadderTest {
     private List<Name> names = new ArrayList<>();
+
     @BeforeEach
     void setUp() {
         names.add(new Name("harr"));
@@ -18,14 +15,5 @@ class LadderTest {
         names.add(new Name("hurr"));
         names.add(new Name("hirr"));
         names.add(new Name("herr"));
-    }
-
-    @Test
-    @DisplayName("사다리 생성 View 확인")
-    void constructor(){
-        Random random = new Random();
-        Ladder ladder = new Ladder(5, 5, () -> random.nextBoolean());
-        ResultView resultView = new ResultView();
-        resultView.printLadder(names, ladder);
     }
 }
