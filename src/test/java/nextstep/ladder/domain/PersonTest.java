5ad60e0e98578019e0ea4b87b50c228cb802a787
dongle <dongle@r2v.io>
2021-08-22 22:28:51 +0900

    test: person 이름에대한 테스트 코드추가

--- src/test/java/nextstep/ladder/domain/PersonTest.java
+++ src/test/java/nextstep/ladder/domain/PersonTest.java
@@ -2,6 +2,8 @@ package nextstep.ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -16,9 +18,17 @@ class PersonTest {
         assertThat(person.getName()).isEqualTo("Phobi");
     }
 
+    @ParameterizedTest(name = "5글자 미만이면 5글자로 만든다")
+    @ValueSource(strings = {"phob", "pho", "ph"})
+    void addNameWithBlank(String name) {
+        Person givenPerson = Person.of(name);
+
+        assertThat(givenPerson.getName().length()).isEqualTo(5);
+    }
+
     @DisplayName("사람이 이름은 최대 5글자 초과이면 IllegalArgument 발생")
     @Test
-     void personWithIllegalArguments() {
-         assertThatIllegalArgumentException().isThrownBy(() -> Person.of("Phobi2"));
-     }
+    void personWithIllegalArguments() {
+        assertThatIllegalArgumentException().isThrownBy(() -> Person.of("Phobi2"));
+    }
 }
