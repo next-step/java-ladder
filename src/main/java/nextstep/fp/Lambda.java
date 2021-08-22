e6d4a7dd7f5482ad4c5eca8bbb06a67ac895c294
YUNBYEONG <ybchae0801@gmail.com>
2021-08-22 11:24:50 +0900

    1단계 - 자바8 스트림, 람다, Optional 리뷰 부탁드립니다~ (#1061)
    * refactor: 람다 실습 1 - 익명 클래스를 람다로 전환
    
    * refactor: 람다 실습 2 - 람다를 활용해 중복 제거
    
    * feat: map, reduce, filter 실습 1
    
    - List에 담긴 모든 숫자 중 3보다 큰 숫자를 2배 한 후 모든 값의 합을 구한다.
    
    * feat: map, reduce, filter 실습 2
    
    - 단어의 길이가 12자를 초과하는 단어를 추출한다.
    - 12자가 넘는 단어 중 길이가 긴 순서로 100개의 단어를 추출한다.
    - 단어 중복을 허용하지 않는다. 즉, 서로 다른 단어 100개를 추출해야 한다.
    - 추출한 100개의 단어를 출력한다. 모든 단어는 소문자로 출력해야 한다.
    
    * feat: Optional 을 활용해 조건에 따른 반환
    
    * refactor: Optional 에서 값을 반환
    
    * refactor: Optional 에서 exception 처리

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -26,31 +26,11 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+    public static int sumAll(List<Integer> numbers, Conditional conditional) {
+        return numbers.stream()
+                .filter(conditional::test)
+                .mapToInt(num -> num)
+                .sum();
     }
 
-    public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
-    }
-
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
-    }
 }
