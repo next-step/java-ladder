0eecad0d3daaa425bee3a7ce88fa3535c8694cac
harris <harrisleesh@gmail.com>
2021-08-22 15:40:48 +0900

    refactor : Remove domain dependency from view

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -1,20 +1,13 @@
 package ladder.view;
 
-import ladder.domain.Name;
-
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class InputView {
     public static final Scanner scanner = new Scanner(System.in);
 
-    public List<Name> requestName() {
+    public String requestName() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        return Arrays.stream(scanner.nextLine().split(","))
-                .map(Name::new)
-                .collect(Collectors.toList());
+        return scanner.nextLine();
     }
 
     public int requestHeight() {
