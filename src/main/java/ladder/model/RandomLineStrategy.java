8dfbb17f980b381a20daff6920f7bb0d4cde783a
mewookim <k011215@gmail.com>
2021-08-22 15:22:13 +0900

    refactor : random method modified

--- src/main/java/ladder/model/RandomLineStrategy.java
+++ src/main/java/ladder/model/RandomLineStrategy.java
@@ -5,9 +5,10 @@ import java.util.Random;
 
 public class RandomLineStrategy implements LadderLineStrategy {
 
+    private static final Random rand = new Random();
+
     @Override
     public boolean boolLine() {
-        Random rand = new Random();
-        return rand.nextInt(2) == 1;
+        return rand.nextBoolean();
     }
 }
