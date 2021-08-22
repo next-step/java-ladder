06b62be1f80364e47c01a1c9869aedbde6c5184f
GeonHee Park <66653324+gunny6026@users.noreply.github.com>
2021-08-22 21:15:48 +0900

    Step1 - 자바8 스트림, 람다, Optional (#1063)
    * refactor : 익명 클래스를 람다로 전환
    
    * refactor : 람다를 활용해 중복 제거
    
    * refactor : 변경되는 부분 인터페이스로 추출
    
    * feat : 스트림(stream) map, filter , reduce 사용
    
    * feat : Optional을 활용하여 조건에 따른 값 반환
    
    * refactor : stream, optional 사용
    
    * refactor : Arrays.stream , Optional Exception 처리

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
@@ -26,31 +27,11 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
-    }
-
-    public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
-    }
-
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+    public static int sumAll(List<Integer> numbers, Conditional c) {
+        return numbers
+                .stream()
+                .filter(c::test)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 }
--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,11 +16,10 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words
+                .stream()
+                .filter(w -> w.length() > 12)
+                .count();
     }
 
     public static void printLongestWordTop100() throws IOException {
@@ -28,6 +28,14 @@ public class StreamStudy {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(s -> s.length() > 12)
+                .sorted()
+                .sorted(Comparator.comparingInt(String::length).reversed())
+                .limit(100)
+                .distinct()
+                .map(s -> s.toLowerCase())
+                .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -35,10 +43,17 @@ public class StreamStudy {
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers
+                .stream()
+                .reduce(0, (x, y) -> x + y);
+
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers
+                .stream()
+                .filter(num -> num > 3)
+                .mapToInt(num -> num * 2)
+                .sum();
     }
 }
