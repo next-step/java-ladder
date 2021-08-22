737eff24524a4c8b1ca613c0f6a03ca189066fa4
LAPTOP-I4NE0UIJ\yongs <smilesigma@gmail.com>
2021-08-22 10:42:52 +0900

    refact: getUsers메서드 optional을 사용해 처리.

--- src/main/java/nextstep/optional/Users.java
+++ src/main/java/nextstep/optional/Users.java
@@ -2,22 +2,26 @@ package nextstep.optional;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Users {
+
     static final User DEFAULT_USER = new User("codesquad", 100);
 
     List<User> users = Arrays.asList(
-            new User("crong", 35),
-            new User("pobi", 30),
-            new User("jk", 40),
-            new User("honux", 45));
+        new User("crong", 35),
+        new User("pobi", 30),
+        new User("jk", 40),
+        new User("honux", 45));
 
     User getUser(String name) {
-        for (User user : users) {
-            if (user.matchName(name)) {
-                return user;
-            }
-        }
-        return DEFAULT_USER;
+
+        return Optional.ofNullable(users)
+            .map(us -> us.stream()
+                .filter(u -> u.matchName(name))
+                .findFirst()
+                .orElse(DEFAULT_USER))
+            .get();
+
     }
 }
