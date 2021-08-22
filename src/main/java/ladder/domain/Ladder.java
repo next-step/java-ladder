d746a99dd82ef966f84893830e1f474495b76edf
harris <harrisleesh@gmail.com>
2021-08-22 15:39:37 +0900

    refactor : Change getter name to lines using unmodifiableList

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -1,18 +1,19 @@
 package ladder.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class Ladder {
-    private List<Line> ladder = new ArrayList<>();
+    private List<Line> lines = new ArrayList<>();
 
-    public Ladder(int height, int personCount, Conditional conditional) {
+    public Ladder(int height, int personCount, LineGenerateStrategy lineGenerateStrategy) {
         IntStream.range(0, height)
-                .forEach(idx -> ladder.add(new Line(personCount, conditional)));
+                .forEach(idx -> lines.add(new Line(personCount, lineGenerateStrategy)));
     }
 
-    public List<Line> getLadder() {
-        return ladder;
+    public List<Line> lines() {
+        return Collections.unmodifiableList(lines);
     }
 }
