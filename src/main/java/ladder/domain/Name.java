b0f412c2e88088a91d678289961c2f88e09b1aae
harris <harrisleesh@gmail.com>
2021-08-22 15:38:57 +0900

    refactor : extract validate method

--- src/main/java/ladder/domain/Name.java
+++ src/main/java/ladder/domain/Name.java
@@ -9,9 +9,7 @@ public class Name {
     private String name;
 
     public Name(String name) {
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("이름은 최대 5자까지 입력할 수 있습니다. name : " + name);
-        }
+        validate(name);
         this.name = name;
     }
 
