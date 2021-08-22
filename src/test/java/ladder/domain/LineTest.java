c65bd5fde3f11b1475082a80808bff5bf1ce95f2
harris <harrisleesh@gmail.com>
2021-08-22 15:32:36 +0900

    test : Create test validates no successive Line points

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -2,13 +2,11 @@ package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Random;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LineTest {
@@ -22,18 +20,13 @@ class LineTest {
                 .withMessage("사다리 게임에 참여하는 사람 수가 너무 작습니다. 사람 수 : " + personCount);
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {4, 5, 6, 7, 8})
-    @DisplayName("항상 true인 확률로 라인생성 했을 때 라인은 사람 수 / 2 만큼 생성된다. 0, 2, 4 순으로 True 이다.")
-    void trueConstructor(int personCount) {
-        int lineCountExpect = personCount / 2;
-        Line line = new Line(personCount, () -> true);
-        long lineCountActual = line.getPoints().stream()
-                .filter(point -> point)
-                .count();
-        assertEquals(lineCountExpect, lineCountActual);
-        IntStream.range(0, personCount / 2).forEach(idx ->
-                assertTrue(line.getPoints().get(idx * 2)));
+    @Test
+    @DisplayName("라인 생성 시 라인이 연속으로 겹치지 않는다.")
+    void constructor() {
+        int personCount = 5;
+        Line line = new Line(personCount, () -> new Random().nextBoolean());
+        assertTrue(IntStream.range(1, personCount)
+                .allMatch(idx -> !(line.lineExists(idx - 1) && line.lineExists(idx))));
     }
 
 }
