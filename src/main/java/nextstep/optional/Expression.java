24de510d315cec97efaa0a6dd85431da9defbabf
LAPTOP-I4NE0UIJ\yongs <smilesigma@gmail.com>
2021-08-22 11:10:52 +0900

    refact: Optional에서 Exception 처리.

--- src/main/java/nextstep/optional/Expression.java
+++ src/main/java/nextstep/optional/Expression.java
@@ -1,5 +1,8 @@
 package nextstep.optional;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 enum Expression {
     PLUS("+"), MINUS("-"), TIMES("*"), DIVIDE("/");
 
@@ -14,12 +17,13 @@ enum Expression {
     }
 
     static Expression of(String expression) {
-        for (Expression v : values()) {
-            if (matchExpression(v, expression)) {
-                return v;
-            }
-        }
 
-        throw new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression));
+        return Optional.ofNullable(values())
+            .map(vs -> Arrays.stream(vs)
+                .filter(v -> matchExpression(v, expression))
+                .findFirst())
+            .get()
+            .orElseThrow(() -> new IllegalArgumentException(
+                String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression)));
     }
 }
