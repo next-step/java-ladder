0acff4bf08c89300bf39ae907e52c19049ce4f23
harris <harrisleesh@gmail.com>
2021-08-24 00:11:30 +0900

    refactor : Remove useless codes

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -1,25 +1,13 @@
 package ladder.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LadderTest {
-    private List<Name> names = new ArrayList<>();
-
-    @BeforeEach
-    void setUp() {
-        names.add(new Name("harr"));
-        names.add(new Name("horr"));
-        names.add(new Name("hurr"));
-        names.add(new Name("hirr"));
-        names.add(new Name("herr"));
-    }
 
     @Test
     @DisplayName("사다리를 생성시 파라미터인 Height, personCount를 기반으로 lines의 사이즈, points의 사이즈를 검증한다.")
