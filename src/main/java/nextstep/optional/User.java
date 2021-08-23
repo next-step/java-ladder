580e3f227fbc9ee642dd4285f93bb74076633799
jaeyoungjang2 <lion85488@gmail.com>
2021-08-23 21:30:05 +0900

    feat: 옵셔널 실습1

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,7 +35,10 @@ public class User {
     }
 
     public static boolean ageIsInRange2(User user) {
-        return false;
+        Optional<User> user1 = Optional.ofNullable(user);
+        return user1.filter(user2 -> user2.getAge() > 30)
+                .filter(user2 -> user2.getAge() < 45)
+                .isPresent();
     }
 
 
