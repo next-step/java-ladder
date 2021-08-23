459833b5dc4d5ab68525496d47c9bbbc1cc5f51d
kdh85 <modudamba@naver.com>
2021-08-23 23:52:07 +0900

    test  : 사다리게임(step4)- LadderTest 테스트 코드 리팩토링. 사다리 생성시 높이값과 사용자값을 주입해서 생성시 높이값 만큼 사다리 객체가 생성되는지 테스트 코드를 추가.

--- src/test/java/ladderGame/step3/model/LadderTest.java
+++ src/test/java/ladderGame/step3/model/LadderTest.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LadderTest {
 
@@ -16,7 +18,7 @@ class LadderTest {
     int height = 0;
     int playerCount = 1;
 
-    assertThatThrownBy(() -> new Ladder(Ladder.of(height,playerCount)))
+    assertThatThrownBy(() -> new Ladder(Ladder.of(height, playerCount)))
         .isInstanceOf(IllegalArgumentException.class);
   }
 
@@ -25,11 +27,20 @@ class LadderTest {
   void createLadder() {
 
     List<Point> points = new ArrayList<>();
-    points.add(new Point(0,new Location(false,false)));
+    points.add(new Point(0, new Location(false, false)));
 
     List<Line> lines = new ArrayList<>();
     lines.add(new Line(points));
 
     assertThat(new Ladder(lines)).isEqualTo(new Ladder(lines));
+  }
+
+  @DisplayName("생성되는 사다리 높이값에 대한 검증.")
+  @ParameterizedTest
+  @CsvSource(value = {"1,2,1","5,4,5"})
+  void createCount(int height, int players, int count) {
+    List<Line> of = Ladder.of(height, players);
+
+    assertThat(of.size()).isEqualTo(count);
   }
 }
