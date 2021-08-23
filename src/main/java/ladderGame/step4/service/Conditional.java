c33f2c15063f3c80aab10ff3507171a199684a03
kdh85 <modudamba@naver.com>
2021-08-23 16:07:37 +0900

    refactor  : 사다리게임(step4)- 사다리 사용자 검색관련 Conditional 인터페이스 변수 변경 리펙토링 기존의 문자열을 직접 주입하는 구조에서 결과값 객체를 이용하도록 변경되었기 때문에 인터페이스를 리팩토링함.

--- src/main/java/ladderGame/step4/service/Conditional.java
+++ src/main/java/ladderGame/step4/service/Conditional.java
@@ -1,5 +1,7 @@
 package ladderGame.step4.service;
 
+import ladderGame.step4.model.MatchResult;
+
 public interface Conditional {
-  boolean isHasCondition(String name);
+  boolean isHasCondition(MatchResult matchResult);
 }
