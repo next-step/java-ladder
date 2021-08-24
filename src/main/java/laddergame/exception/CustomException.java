4daa2000b1149cacd966ffa5da00f301c8e5b19d
beadoer1 <beadoer1@gmail.com>
2021-08-24 14:58:51 +0900

    refactor : 미사용 생성자 삭제

--- src/main/java/laddergame/exception/CustomException.java
+++ src/main/java/laddergame/exception/CustomException.java
@@ -1,23 +1,7 @@
 package laddergame.exception;
 
 public class CustomException extends RuntimeException {
-    public CustomException() {
-        super();
-    }
-
     public CustomException(String message) {
         super(message);
-    }
-
-    public CustomException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public CustomException(Throwable cause) {
-        super(cause);
-    }
-
-    protected CustomException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
