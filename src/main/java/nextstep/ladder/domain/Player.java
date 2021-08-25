58896de392aa46a2b68b5f1cf283f6f6dbf780c8
ckdgus08 <ckdgus0808@naver.com>
2021-08-26 00:22:31 +0900

    feat: valid player name

--- src/main/java/nextstep/ladder/domain/Player.java
+++ src/main/java/nextstep/ladder/domain/Player.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 public class Player {
 
     public static final int MAX_LENGTH = 5;
+    public static final int MIN_LENGTH = 1;
     private final String name;
 
     private Player(String name) {
@@ -12,14 +13,17 @@ public class Player {
         this.name = name;
     }
 
+    public static Player from(String name) {
+        return new Player(name);
+    }
+
     private void validPlayerNameLength(String name) {
+        if (name == null || name.length() < MIN_LENGTH) {
+            throw new IllegalArgumentException("Player 이름은 공백 혹은 NULL이 될 수 없습니다.");
+        }
         if (name.length() > MAX_LENGTH) {
             throw new IllegalArgumentException("Player 이름은 " + MAX_LENGTH + "글자가 넘으면 안됩니다. 현재 : " + name.length() + "글자");
         }
-    }
-
-    public static Player from(String name) {
-        return new Player(name);
     }
 
     @Override
