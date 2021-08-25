3a23dcda9952f07134eed6ff2c9dcdfdf5dcf1ae
kdh85 <modudamba@naver.com>
2021-08-25 10:45:01 +0900

    refactor  : 사다리게임(step4)- LadderService  리팩토링 도메인에 대한 의존성을 제거하고 이로인해 불필요해진 값 검증 메소드를 제거함.

--- src/main/java/ladderGame/step4/service/LadderService.java
+++ src/main/java/ladderGame/step4/service/LadderService.java
@@ -9,41 +9,19 @@ import ladderGame.step4.model.Prizes;
 
 public class LadderService {
 
-  private Players players;
-
-  private Ladder ladder;
-
-  public List<String> createPlayers(final String playerNames) {
-    players = new Players(Players.from(playerNames));
-    return players.playersName();
+  public Players createPlayers(final String playerNames) {
+    return new Players(Players.from(playerNames));
   }
 
-  public Ladder createLadder(final int ladderHeight) {
-    validationPlayers();
-    ladder =  new Ladder(Ladder.of(ladderHeight, players.count()));
-    return ladder;
+  public Ladder createLadder(final int ladderHeight, final int playerCount) {
+    return new Ladder(Ladder.of(ladderHeight, playerCount));
   }
 
-  private void validationPlayers() {
-    if(players == null){
-      throw new IllegalArgumentException("사용자를 먼저 등록해주세요.");
-    }
+  public Prizes createPrizes(final String goods, final int playerCount) {
+    return new Prizes(Prizes.of(goods, playerCount));
   }
 
-  public Prizes createPrizes(final String goods) {
-    validationPlayers();
-    return new Prizes(Prizes.of(goods, players.count()));
-  }
-
-  public List<MatchResult> getMatchResults(final String findName) {
-    validationPlayers();
-    validationLadder();
+  public List<MatchResult> getMatchResults(final Ladder ladder, final Players players, final String findName) {
     return MatchResults.createMatchResult(ladder, players).searchPrizeWithCondition(findName);
-  }
-
-  private void validationLadder() {
-    if(ladder == null){
-      throw new IllegalArgumentException("사다리를 먼저 생성해주세요.");
-    }
   }
 }
