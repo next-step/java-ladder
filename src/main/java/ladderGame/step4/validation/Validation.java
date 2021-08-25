4b8920bbd3230fbacb9079bfe221bc1d6a8b3400
kdh85 <modudamba@naver.com>
2021-08-25 09:17:45 +0900

    refactor  : 사다리게임(step4)- Validation 메소드 리팩토링 메소드의 정렬 순서를 화면에서 호출하는 순으로 재조정하고, 기존에 미흡했던 입력값 검증을 보완하는 리팩토링을 수행함.

--- src/main/java/ladderGame/step4/validation/Validation.java
+++ src/main/java/ladderGame/step4/validation/Validation.java
@@ -2,10 +2,33 @@ package ladderGame.step4.validation;
 
 public class Validation {
 
-  public static final String SPLIT_MARK = ",";
-  public static final int LIMIT_HEIGHT = 1;
-  public static final String MSG_ERROR_LIMIT_HEIGHT = "사다리높이는 1이상부터 가능합니다.";
-  public static final String MSG_ERROR_INCORRECT_COUNT = "사용자 수와 결과의 수가 일치하지 않습니다.";
+  private static final String SPLIT_MARK = ",";
+  private static final int LIMIT_HEIGHT = 1;
+  private static final String MSG_ERROR_LIMIT_HEIGHT = "사다리높이는 1이상부터 가능합니다.";
+  private static final String MSG_ERROR_INCORRECT_COUNT = "사용자 수와 결과의 수가 일치하지 않습니다.";
+  private static final String MSG_ERROR_NO_PLAYERS = "사용자를 2명 이상 입력하세요.";
+  private static final int LIMIT_PLAYER_COUNT = 2;
+
+  public static void validationNames(final String usersName) {
+    if (usersName.split(SPLIT_MARK).length < LIMIT_PLAYER_COUNT) {
+      throw new IllegalArgumentException(MSG_ERROR_NO_PLAYERS);
+    }
+  }
+
+  public static void validationNamesAndGoodsCount(final String usersName, final String goods) {
+
+    validationPrizes(goods);
+
+    if (usersName.split(SPLIT_MARK).length != goods.split(SPLIT_MARK).length) {
+      throw new IllegalArgumentException(MSG_ERROR_INCORRECT_COUNT);
+    }
+  }
+
+  private static void validationPrizes(final String goods) {
+    if (goods.split(SPLIT_MARK).length < LIMIT_PLAYER_COUNT) {
+      throw new IllegalArgumentException("상품을 2개 이상 입력하세요.");
+    }
+  }
 
   public static void validationLimitHeight(final int ladderHeight) {
     if (ladderHeight < LIMIT_HEIGHT) {
@@ -13,9 +36,4 @@ public class Validation {
     }
   }
 
-  public static void validationNamesAndGoodsCount(final String usersName, final String goods) {
-    if (usersName.split(SPLIT_MARK).length != goods.split(SPLIT_MARK).length) {
-      throw new IllegalArgumentException(MSG_ERROR_INCORRECT_COUNT);
-    }
-  }
 }
