71523b8bebad569128998983407d41762d26face
gunny6026 <gunny6026@naver.com>
2021-08-25 13:16:43 +0900

    feat : 참여자 수에 맞는 사다리 라인 생성

---
+++ src/main/java/nextstep/ladder/step2/domain/Ladder.java
@@ -0,0 +1,7 @@
+package nextstep.ladder.step2.domain;
+
+import java.util.List;
+
+public class Ladder {
+    private List<Line> lines;
+}
--- src/main/java/nextstep/ladder/step2/domain/Line.java
+++ src/main/java/nextstep/ladder/step2/domain/Line.java
@@ -1,8 +1,28 @@
 package nextstep.ladder.step2.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Line {
-    private List<Boolean> lines;
+    private static final Random random = new Random();
+    private List<Boolean> points;
 
+    public Line() {
+        this.points = new ArrayList<>();
+        points.add(false);
+    }
+
+    public void createLine(int playerCount) {
+        for (int i = 1; i < playerCount; i++) {
+            points.add(checkPreviousPoint(points.get(i-1)));
+        }
+    }
+
+    public boolean checkPreviousPoint(boolean previousPoint) {
+        if (previousPoint) {
+            return false;
+        }
+        return random.nextBoolean();
+    }
 }
