3b64375e5db373c120cdf083d53a94c09722e370
kdh85 <modudamba@naver.com>
2021-08-25 10:45:50 +0900

    test  : 사다리게임(step4)- LadderServiceTest  리팩토링 도메인에 대한 의존성을 제거로 인한 메소드 변경관련 코드 리팩토링을 수행. 불필요해진 값 검증 테스트코드를 제거함.

--- src/test/java/ladderGame/step4/service/LadderServiceTest.java
+++ src/test/java/ladderGame/step4/service/LadderServiceTest.java
@@ -1,11 +1,11 @@
 package ladderGame.step4.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import ladderGame.step4.model.Ladder;
 import ladderGame.step4.model.MatchResult;
+import ladderGame.step4.model.Players;
 import ladderGame.step4.model.Prizes;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,16 +18,8 @@ class LadderServiceTest {
   @Test
   void createPlayers() {
     LadderService ladderService = new LadderService();
-    List<String> players = ladderService.createPlayers("user1,user2");
-    assertThat(players).containsExactly("user1","user2");
-  }
-
-  @DisplayName("사다리 객체 생성 이전 사용자 생성 검증 테스트.")
-  @Test
-  void validationLadderCreate() {
-    LadderService ladderService = new LadderService();
-    assertThatThrownBy(() -> ladderService.createLadder(1))
-        .isInstanceOf(IllegalArgumentException.class);
+    List<String> players = ladderService.createPlayers("user1,user2").playersName();
+    assertThat(players).containsExactly("user1", "user2");
   }
 
   @DisplayName("사다리 객체 생성 테스트.")
@@ -35,19 +27,10 @@ class LadderServiceTest {
   void createLadder() {
     LadderService ladderService = new LadderService();
 
-    ladderService.createPlayers("user1,user2");
-    Ladder ladder = ladderService.createLadder(1);
+    List<String> players = ladderService.createPlayers("user1,user2").playersName();
+    Ladder ladder = ladderService.createLadder(1, players.size());
 
     assertThat(ladder.ladderValues().size()).isEqualTo(1);
-  }
-
-
-  @DisplayName("상품결과 생성 이전 사용자 생성 검증 테스트.")
-  @Test
-  void validationPrizesCreate() {
-    LadderService ladderService = new LadderService();
-    assertThatThrownBy(() -> ladderService.createPrizes("g1,g2,g3"))
-        .isInstanceOf(IllegalArgumentException.class);
   }
 
   @DisplayName("상품결과 객체 생성 테스트.")
@@ -55,31 +38,22 @@ class LadderServiceTest {
   void creatPrizes() {
     LadderService ladderService = new LadderService();
     ladderService.createPlayers("user1,user2");
-    Prizes prizes = ladderService.createPrizes("good1,good2");
+    Prizes prizes = ladderService.createPrizes("good1,good2", 2);
 
     assertThat(prizes.prizeNames().size()).isEqualTo(2);
-    assertThat(prizes.prizeNames()).containsExactly("good1","good2");
-  }
-
-  @DisplayName("결과값 생성 이전 사용자,사다리,상품 객체 검증 테스트.")
-  @Test
-  void validationMatchResultCreate() {
-    LadderService ladderService = new LadderService();
-    assertThatThrownBy(() -> ladderService.getMatchResults("user1"))
-        .isInstanceOf(IllegalArgumentException.class);
+    assertThat(prizes.prizeNames()).containsExactly("good1", "good2");
   }
 
   @DisplayName("결과값 반환 검증.")
   @ParameterizedTest
-  @CsvSource(value = {"user1,1","user2,1","all,2"})
+  @CsvSource(value = {"user1,1", "user2,1", "all,2"})
   void createMatchResult(String name, int count) {
     LadderService ladderService = new LadderService();
 
-    ladderService.createPlayers("user1,user2");
-    ladderService.createLadder(1);
-    ladderService.createPrizes("good1,good2");
+    Players players = ladderService.createPlayers("user1,user2");
+    Ladder ladder = ladderService.createLadder(1, players.playersName().size());
 
-    List<MatchResult> user1 = ladderService.getMatchResults(name);
+    List<MatchResult> user1 = ladderService.getMatchResults(ladder, players, name);
 
     assertThat(user1.size()).isEqualTo(count);
   }
