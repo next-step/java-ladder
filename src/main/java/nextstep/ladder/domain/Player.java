7664bc317739b7541d32067b03182027e10af633
ckdgus08 <ckdgus0808@naver.com>
2021-08-26 00:16:54 +0900

    feat: Player 생성

--- src/main/java/nextstep/ladder/domain/Player.java
+++ src/main/java/nextstep/ladder/domain/Player.java
@@ -1,4 +1,37 @@
 package nextstep.ladder.domain;
 
+import java.util.Objects;
+
 public class Player {
+
+    public static final int MAX_LENGTH = 5;
+    private final String name;
+
+    private Player(String name) {
+        validPlayerNameLength(name);
+        this.name = name;
+    }
+
+    private void validPlayerNameLength(String name) {
+        if (name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("Player 이름은 " + MAX_LENGTH + "글자가 넘으면 안됩니다. 현재 : " + name.length() + "글자");
+        }
+    }
+
+    public static Player from(String name) {
+        return new Player(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Player)) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
--- src/test/java/nextstep/ladder/domain/PlayerTest.java
+++ src/test/java/nextstep/ladder/domain/PlayerTest.java
@@ -1,4 +1,15 @@
 package nextstep.ladder.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class PlayerTest {
+
+    @DisplayName("string을 매개변수로 player를 생성한다.")
+    @Test
+    void create() {
+        String name = "pobi";
+        Assertions.assertThat(Player.from(name)).isEqualTo(Player.from(name));
+    }
 }
