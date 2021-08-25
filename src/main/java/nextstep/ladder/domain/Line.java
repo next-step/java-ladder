de2086d7d798e156c4b34039ca577baaca238199
ckdgus08 <ckdgus0808@naver.com>
2021-08-25 23:31:57 +0900

    feat: valid Line 가로선 갯수 2 미만일때, 연속해서 2개이상 위치할때 에러발생

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -15,6 +15,9 @@ public class Line {
     }
 
     private Line(List<Boolean> points) {
+        validPointsCount(points);
+        validContinuous(points);
+
         this.points = points;
     }
 
@@ -22,6 +25,22 @@ public class Line {
         return new Line(
                 Arrays.stream(existPoint)
                         .collect(Collectors.toList()));
+    }
+
+    private void validContinuous(List<Boolean> points) {
+        boolean temp = points.get(0);
+        for (int i = 1; i < points.size(); i++) {
+            if (temp && points.get(i)) {
+                throw new IllegalArgumentException("가로 선은 연속해서 2개 이상 위치할 수 없습니다.");
+            }
+            temp = points.get(i);
+        }
+    }
+
+    private void validPointsCount(List<Boolean> points) {
+        if (points.size() < 2) {
+            throw new IllegalArgumentException("포인트 갯수는 최소 2개 이상 이어야합니다." + "현재 : " + points.size());
+        }
     }
 
     @Override
--- src/test/java/nextstep/ladder/domain/LineTest.java
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LineTest {
 
@@ -13,4 +14,21 @@ public class LineTest {
         assertThat(Line.from(true, false, true)).isEqualTo(Line.from(true, false, true));
     }
 
+    @DisplayName("가로라인 선이 2개 미만일때 에러가 발생한다.")
+    @Test
+    void validPointCount() {
+        assertThatThrownBy(() -> Line.from(true))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Line.from(false))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("|-----|-----| 와 같이 가로라인 선이 2개 연속으로 오면 에러가 발생한다.")
+    @Test
+    void validContinuous() {
+        assertThatThrownBy(() -> Line.from(true, true, false))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Line.from(false, true, true))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
