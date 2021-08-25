edec86f6c5ef8c593c87946b133194cc0d98b2b3
0n1dev <all_mind@naver.com>
2021-08-25 14:42:42 +0900

    feat : InputView 클래스 구현

---
+++ src/main/java/nextstep/ladder/LadderMain.java
@@ -0,0 +1,21 @@
+package nextstep.ladder;
+
+import nextstep.ladder.model.Ladder;
+import nextstep.ladder.model.Members;
+import nextstep.ladder.strategy.RandomLineStrategy;
+import nextstep.ladder.view.InputView;
+import nextstep.ladder.view.OutputView;
+
+public class LadderMain {
+
+	public static void main(String[] args) {
+		String names = InputView.inputName();
+		int height = InputView.inputHeight();
+
+		Members members = Members.of(names);
+
+		Ladder ladder = Ladder.of(new RandomLineStrategy(), height, members.size());
+
+		OutputView.runResult(members, ladder);
+	}
+}
