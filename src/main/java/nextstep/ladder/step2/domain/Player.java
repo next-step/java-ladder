68df91a8b545cbf5e08b42835f0bf9cbf87873ee
gunny6026 <gunny6026@naver.com>
2021-08-25 14:48:03 +0900

    test : Player 생성, 예외 테스트

--- src/main/java/nextstep/ladder/step2/domain/Player.java
+++ src/main/java/nextstep/ladder/step2/domain/Player.java
@@ -1,10 +1,12 @@
 package nextstep.ladder.step2.domain;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Player {
     private static final String DELIMITER = ",";
+    private static final int PLAYER_MIN_COUNT = 2;
     private List<Name> players;
 
     public Player(String inputPlayers) {
@@ -16,9 +18,19 @@ public class Player {
     }
 
     public Player(String[] players) {
-        this.players = new ArrayList<>();
-        for (String player : players) {
-            this.players.add(new Name(player));
+        if (players.length < PLAYER_MIN_COUNT) {
+            throw new IllegalArgumentException("참가자 수가 부족합니다. 게임을 진행하려면 최소 " + PLAYER_MIN_COUNT + "명 이상 필요합니다.");
         }
+        this.players = Arrays.stream(players)
+                .map(Name::new)
+                .collect(Collectors.toList());
+    }
+
+    public int getPlayerCount() {
+        return players.size();
+    }
+
+    public List<Name> getPlayers() {
+        return players;
     }
 }
