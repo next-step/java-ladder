7ec2212ea81695388003c751f986edffa474d3a1
shineoov <shineoov88@gmail.com>
2021-08-26 01:17:40 +0900

    test: Ladder 실행 검증 테스트 추가

--- src/test/java/ladder/domain/ladder/LadderTest.java
+++ src/test/java/ladder/domain/ladder/LadderTest.java
@@ -4,8 +4,12 @@ import ladder.exception.LadderMinimumHeightException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -37,4 +41,28 @@ class LadderTest {
                 .isInstanceOf(LadderMinimumHeightException.class);
     }
 
+    static Stream<Arguments> provideLadder() {
+        DirectionStrategy directionStrategy = () -> true;
+        return Stream.of(
+                Arguments.of(new Ladder(directionStrategy, 3, 3), 0, 1),
+                Arguments.of(new Ladder(directionStrategy, 3, 3), 1, 0),
+                Arguments.of(new Ladder(directionStrategy, 3, 3), 2, 2),
+                Arguments.of(new Ladder(directionStrategy, 2, 4), 0, 0),
+                Arguments.of(new Ladder(directionStrategy, 2, 4), 1, 1),
+                Arguments.of(new Ladder(directionStrategy, 2, 4), 2, 2),
+                Arguments.of(new Ladder(directionStrategy, 2, 4), 3, 3)
+        );
+    }
+
+    @ParameterizedTest(name = "startPoint: {1}, endPoint: {2}")
+    @MethodSource("provideLadder")
+    @DisplayName("사다리 실행")
+    void ladderRun(Ladder ladder, int startPoint, int endPoint) {
+        // given
+        // when
+        int actual = ladder.run(startPoint);
+
+        // then
+        assertThat(actual).isEqualTo(endPoint);
+    }
 }
