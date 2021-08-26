df5938e71bb2948444fafe46967e01c7182c9554
ChangSubKwak <paradise1732@gmail.com>
2021-08-27 03:01:31 +0900

    test: 결과를 담는 클래스(Result)의 테스트 케이스 구현

---
+++ src/test/java/ladder/ResultTest.java
@@ -0,0 +1,25 @@
+package ladder;
+
+import ladder.domain.Result;
+import ladder.exception.NullValueException;
+import ladder.exception.ResultLengthException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ResultTest {
+    @DisplayName("실행결과 하나를 입력 받으면 실행결과 객체 Result를 생성한다")
+    @Test
+    void newResultTest() {
+        assertThat(Result.from("꽝")).isInstanceOf(Result.class);
+    }
+
+    @DisplayName("실행결과 데이터가 널이거나 길이가 0이면 예외를 던진다")
+    @Test
+    void throwExceptionTest() {
+        assertThatThrownBy(() -> Result.from(null)).isInstanceOf(NullValueException.class);
+        assertThatThrownBy(() -> Result.from("")).isInstanceOf(ResultLengthException.class);
+    }
+}
