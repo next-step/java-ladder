86b33d7905c922d3e8f32c684f7423b0962f8aa0
hiro <kiryuinh32@gmail.com>
2021-08-27 01:44:16 +0900

    feat: User 를 List 로 관리하고, 생성하는 Users 생성

---
+++ src/main/java/labber/domain/Users.java
@@ -0,0 +1,22 @@
+package labber.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Users {
+    private final List<User> users;
+
+    public Users(String[] usersName) {
+        List<User> users = new ArrayList<>();
+        for (String name : usersName) {
+            User user = new User(name);
+            users.add(user);
+        }
+        this.users = users;
+    }
+
+    public List<User> getUsers() {
+        return Collections.unmodifiableList(users);
+    }
+}
