9d6e082adc8aec07d64c9dcd8bc1f619b75cc330
ChangSubKwak <paradise1732@gmail.com>
2021-08-27 02:48:26 +0900

    feat: 사다리 높이 예외 클래스 구현

--- src/main/java/ladder/exception/LadderHeightException.java
+++ src/main/java/ladder/exception/LadderHeightException.java
@@ -1,7 +1,9 @@
 package ladder.exception;
 
 public class LadderHeightException extends RuntimeException {
-    public LadderHeightException(String message) {
-        super(message);
+    private static final String LADDER_MIN_HEIGHT_EXCEPTION_COMMENT = "사다리의 높이는 %d이상이어야 합니다.";
+
+    public LadderHeightException(int height) {
+        super(String.format(LADDER_MIN_HEIGHT_EXCEPTION_COMMENT, height));
     }
 }
