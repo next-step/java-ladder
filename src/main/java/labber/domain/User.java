7b6958c33b3a10da1a35d96755ae607acb419cfb
hiro <kiryuinh32@gmail.com>
2021-08-27 01:58:50 +0900

    refactor: User 의 예외를 처리하는 숫자를 상수로 추출

--- src/main/java/labber/domain/User.java
+++ src/main/java/labber/domain/User.java
@@ -4,6 +4,8 @@ import labber.exception.UserNameLengthException;
 
 public class User {
     private final String name;
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final int MIN_NAME_LENGTH = 1;
 
     public User(String name) {
         validateName(name);
@@ -11,7 +13,7 @@ public class User {
     }
 
     private void validateName(String name) {
-        if (name.length() > 5 || name.length() < 1) {
+        if (name.length() > MAX_NAME_LENGTH || name.length() < MIN_NAME_LENGTH) {
             throw new UserNameLengthException();
         }
     }
