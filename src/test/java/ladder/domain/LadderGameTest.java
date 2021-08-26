63964e027692b34f271b3c2393d2e852cf096b68
shineoov <shineoov88@gmail.com>
2021-08-26 22:16:19 +0900

    test: 사다리게임 테스트 재작성

--- src/test/java/ladder/domain/LadderGameTest.java
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -2,13 +2,18 @@ package ladder.domain;
 
 import ladder.domain.ladder.DirectionStrategy;
 import ladder.domain.ladder.Ladder;
+import ladder.domain.result.Result;
 import ladder.exception.LadderGameDifferentSizeException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -17,29 +22,46 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @DisplayName("사다리게임 테스트")
 class LadderGameTest {
 
-    LadderGame ladderGame;
-    DirectionStrategy directionStrategy;
+    static Stream<Arguments> provideLadderGame() {
+        LadderGame ladderGame = new LadderGame(
+                Arrays.asList("user1", "user2", "user3"),
+                Arrays.asList("result1", "result2", "result3")
+        );
 
-    @BeforeEach
-    void setUp() {
-        List<String> users = Arrays.asList("red", "blue", "green");
-        List<String> results = Arrays.asList("꽝", "1000", "20000");
-        ladderGame = new LadderGame(users, results);
-        directionStrategy = () -> true;
+        return Stream.of(
+                Arguments.of(ladderGame, 1, "user1", new Result("result2")),
+                Arguments.of(ladderGame, 2, "user1", new Result("result1")),
+                Arguments.of(ladderGame, 5, "user3", new Result("result3"))
+        );
     }
 
-    @Test
-    @DisplayName("사다리 생성 후 사다리 높이 확인")
-    void ladderGameHeightCheck() {
+    @ParameterizedTest(name = "{2} => {3}")
+    @MethodSource("provideLadderGame")
+    @DisplayName("사다리 게임 실행 후 결과 값 확인")
+    void ladderGameExecuteResultCheck(LadderGame ladderGame, int ladderHeight, String user, Result result) {
         // given
-        int givenHeightCount = 3;
+        Ladder ladder = ladderGame.generateLadder(ladderHeight, () -> true);
+        LadderGameResult ladderGameResult = ladderGame.execute(ladder);
 
         // when
-        Ladder ladder = ladderGame.generateLadder(givenHeightCount, directionStrategy);
-        int ladderHeight = ladder.getLines().size();
+        Result actual = ladderGameResult.getLadderGameResult(user);
 
         // then
-        assertThat(ladderHeight).isEqualTo(givenHeightCount);
+        assertThat(actual).isEqualTo(result);
+    }
+
+    @ParameterizedTest(name = "사다리 높이 = {1}")
+    @MethodSource("provideLadderGame")
+    @DisplayName("사다리게임에서 사다리 생성 후 사다리 높이 확인")
+    void generateLadderHeightCheck(LadderGame ladderGame, int ladderHeight) {
+        // given
+        Ladder ladder = ladderGame.generateLadder(ladderHeight, () -> true);
+
+        // when
+        int actual = ladder.getLines().size();
+
+        // then
+        assertThat(actual).isEqualTo(ladderHeight);
     }
 
     @Test
