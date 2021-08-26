514d6ae85efebfe4dafc28c08734066b7a3074ae
seondongpyo <seondongpyo@gmail.com>
2021-08-27 00:48:35 +0900

    test(Players): 참가자들의 이름을 리스트로 반환하는 테스트 케이스 추가

--- src/test/java/nextstep/step4/domain/PlayersTest.java
+++ src/test/java/nextstep/step4/domain/PlayersTest.java
@@ -7,18 +7,24 @@ import org.junit.jupiter.api.Test;
 
 class PlayersTest {
 
+	private final Players players = Players.from("pobi,crong,honux,jk");
+
 	@DisplayName("입력 받은 이름들로 Players 객체를 생성한다.")
 	@Test
 	void create() {
-		Players players = Players.from("pobi,crong,honux,jk");
 		assertThat(players).isEqualTo(Players.from("pobi,crong,honux,jk"));
 	}
 
 	@DisplayName("입력 받은 이름들은 콤마(,)를 기준으로 나눈다.")
 	@Test
 	void split() {
-		Players players = Players.from("pobi,crong,honux,jk");
 		assertThat(players.size()).isEqualTo(4);
+	}
+
+	@DisplayName("참가자들의 이름을 리스트로 반환한다.")
+	@Test
+	void names() {
+		assertThat(players.names()).containsExactly("pobi", "crong", "honux", "jk");
 	}
 
 
