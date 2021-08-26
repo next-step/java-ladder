ad693146e5e81b71af3f986c9f10af045233d921
seondongpyo <seondongpyo@gmail.com>
2021-08-27 00:49:11 +0900

    test(Prizes): 사다리 게임의 모든 결과 값들을 리스트로 반환하는 테스트 케이스 추가

--- src/test/java/nextstep/step4/domain/PrizesTest.java
+++ src/test/java/nextstep/step4/domain/PrizesTest.java
@@ -7,21 +7,27 @@ import org.junit.jupiter.api.Test;
 
 class PrizesTest {
 
+	private final Prizes prizes = Prizes.from("꽝,1000,5000,10000");
+
 	@DisplayName("사다리 게임 결과 값들을 포장하는 일급 콜렉션 Prizes 객체를 생성한다.")
 	@Test
 	void create() {
-		Prizes prizes = Prizes.from("꽝,1000,5000,10000");
 		assertThat(prizes).isEqualTo(Prizes.from("꽝,1000,5000,10000"));
 	}
 
 	@DisplayName("최종 사다리 지점에 해당하는 Prize를 가져온다.")
 	@Test
 	void of() {
-		Prizes prizes = Prizes.from("꽝,1000,5000,10000");
 		assertThat(prizes.of(0)).isEqualTo(new Prize("꽝"));
 		assertThat(prizes.of(1)).isEqualTo(new Prize("1000"));
 		assertThat(prizes.of(2)).isEqualTo(new Prize("5000"));
 		assertThat(prizes.of(3)).isEqualTo(new Prize("10000"));
+	}
+
+	@DisplayName("사다리 게임의 모든 결과 값들을 리스트로 반환한다.")
+	@Test
+	void values() {
+		assertThat(prizes.values()).containsExactly("꽝", "1000", "5000", "10000");
 	}
 
 }
