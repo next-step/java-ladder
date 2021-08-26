9775169aa606f31ed91b38ab931d5f16df01e4de
hiro <kiryuinh32@gmail.com>
2021-08-27 01:47:48 +0900

    feat: 사다리의 Line 클래스 생성

---
+++ src/main/java/labber/domain/Line.java
@@ -0,0 +1,42 @@
+package labber.domain;
+
+import labber.exception.LineCountException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class Line {
+    private static final Random RANDOM = new Random();
+
+    private final List<Boolean> points;
+
+    public Line (int countOfPerson) {
+        validateCount(countOfPerson);
+
+        List<Boolean> points = new ArrayList<>();
+
+        points.add(RANDOM.nextBoolean());
+
+        for (int i = 0; i < countOfPerson - 1; i++) {
+            boolean point = RANDOM.nextBoolean();
+            if (points.get(i) == true && point == true) {
+                points.add(!point);
+                continue;
+            }
+            points.add(point);
+        }
+        this.points = points;
+    }
+
+    private void validateCount(int countOfPerson) {
+        if (countOfPerson < 1) {
+            throw new LineCountException();
+        }
+    }
+
+    public List<Boolean> getPoints() {
+        return Collections.unmodifiableList(points);
+    }
+}
