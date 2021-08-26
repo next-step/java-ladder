e23ae2521745077cbf71b87f5d7d1d91e9c4fdbb
ChangSubKwak <paradise1732@gmail.com>
2021-08-27 03:17:56 +0900

    feat: 실행결과 입력, 결과를 보고 싶은 사람 입력 기능 추가

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String INPUT_PARTICIPANTS_NAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String INPUT_EXECUTION_RESULT = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String INPUT_MAXIMUM_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
+    private static final String INPUT_WANTED_RESULT_OF_NAME = "결과를 보고 싶은 사람은?";
+
 
     private String inputStringValue() {
         return scanner.nextLine();
@@ -22,9 +25,22 @@ public class InputView {
         return inputStringValue();
     }
 
+    public String inputExecutionResult() {
+        System.out.print(System.lineSeparator());
+        System.out.println(INPUT_EXECUTION_RESULT);
+        return inputStringValue();
+    }
+
     public int inputMaximumHeight() {
-        System.out.println("\n" + INPUT_MAXIMUM_HEIGHT);
+        System.out.print(System.lineSeparator());
+        System.out.println(INPUT_MAXIMUM_HEIGHT);
         return inputIntValue();
+    }
+
+    public String inputWantedResultOfName() {
+        System.out.print(System.lineSeparator());
+        System.out.println(INPUT_WANTED_RESULT_OF_NAME);
+        return inputStringValue();
     }
 
     public void scannerClose() {
