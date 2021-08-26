1e5748140479a11eb23452c25b91415751fd1337
ChangSubKwak <paradise1732@gmail.com>
2021-08-27 03:14:47 +0900

    test: Ladder클래스의 resultIndexOf 함수 테스트 케이스 추가

--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -1,12 +1,19 @@
 package ladder;
 
-import static org.assertj.core.api.Assertions.*;
-
+import ladder.domain.Index;
+import ladder.domain.Ladder;
+import ladder.domain.Line;
+import ladder.domain.Point;
+import ladder.exception.LadderHeightException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import ladder.domain.Ladder;
-import ladder.exception.LadderHeightException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LadderTest {
     @DisplayName("참여할_사람의_인원_수와_사다리_높이가_주어지면_사다리_높이만큼의_Line객체를_가진_Ladder가_생성된다")
@@ -26,5 +33,21 @@ class LadderTest {
         int countOfPerson = 4;
         int height = 0;
         assertThatThrownBy(() -> Ladder.from(countOfPerson, height)).isInstanceOf(LadderHeightException.class);
+    }
+
+    @DisplayName("사다리와 시작인덱스가 주어진다면, 결과인덱스값을 알 수 있다")
+    @Test
+    void resultIndexOfTest() {
+        // 기본 사다리 생성 |-----|
+        List<Point> points = new ArrayList<>();
+        points.add(Point.of(false, true));
+        points.add(Point.of(true, false));
+        points.add(Point.of(false, true));
+        points.add(Point.of(true, false));
+
+        List<Line> lines = Arrays.asList(Line.from(points));
+        Ladder ladder = Ladder.from(lines);
+        assertThat(ladder.resultIndexOf(Index.from(0))).isEqualTo(1);
+        assertThat(ladder.resultIndexOf(Index.from(1))).isEqualTo(0);
     }
 }
