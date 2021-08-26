c884e7347d230aae77e96c60d1e94f8f8fe8bade
seondongpyo <seondongpyo@gmail.com>
2021-08-27 00:42:55 +0900

    feat(LadderMain): 사다리 게임을 실행하는 main 메서드 추가

---
+++ src/main/java/nextstep/step4/LadderMain.java
@@ -0,0 +1,28 @@
+package nextstep.step4;
+
+import nextstep.step4.domain.Height;
+import nextstep.step4.domain.Ladder;
+import nextstep.step4.domain.LadderFactory;
+import nextstep.step4.domain.Players;
+import nextstep.step4.domain.Prizes;
+import nextstep.step4.domain.Result;
+import nextstep.step4.view.InputView;
+import nextstep.step4.view.ResultView;
+
+public class LadderMain {
+
+	public static void main(String[] args) {
+		Players players = Players.from(InputView.playerNames());
+		Prizes prizes = Prizes.from(InputView.prizes());
+		Height height = new Height(InputView.ladderHeight());
+		Ladder ladder = LadderFactory.from(players, height, prizes);
+		Result result = ladder.play();
+
+		ResultView.playerNames(players);
+		ResultView.ladderStructure(ladder);
+		ResultView.prizes(prizes);
+
+		String playerName = InputView.targetPlayerName();
+		ResultView.resultOfPlayer(result, playerName);
+	}
+}
