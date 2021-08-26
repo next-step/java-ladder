1842886bc9aac76c4e088646b89887daebc6d9b7
kdh85 <modudamba@naver.com>
2021-08-26 12:27:42 +0900

    test  : 사다리게임(step4)- LadderServiceTest 메소드 리팩토링 기존에 각 도메인별 생성 메소드에 대한 테스트코드를 dto로 반환하는 생성메소드로 변경됨으로 인해 리팩토링함.

--- src/test/java/ladderGame/step4/service/LadderServiceTest.java
+++ src/test/java/ladderGame/step4/service/LadderServiceTest.java
@@ -1,60 +1,29 @@
 package ladderGame.step4.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-import java.util.List;
-import ladderGame.step4.model.Ladder;
-import ladderGame.step4.model.MatchResult;
-import ladderGame.step4.model.Players;
-import ladderGame.step4.model.Prizes;
+import ladderGame.step4.dto.CreatorDto;
+import ladderGame.step4.dto.ModelDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 class LadderServiceTest {
 
-  @DisplayName("사용자 객체 생성 테스트.")
+  @DisplayName("입력되는 사용자명, 사다리높이값, 경품내용을 주입받아 사용자, 사다리, 경품을 생성하여 dto로 반환되는지 확인한다.")
   @Test
-  void createPlayers() {
+  void create() {
+    String playerNames = "user1,user2";
+    String goods = "prz1,prz2";
+    int ladderHeight = 2;
+
     LadderService ladderService = new LadderService();
-    List<String> players = ladderService.createPlayers("user1,user2").playersName();
-    assertThat(players).containsExactly("user1", "user2");
-  }
+    ModelDto modelDto = ladderService.create(new CreatorDto(playerNames, goods, ladderHeight));
 
-  @DisplayName("사다리 객체 생성 테스트.")
-  @Test
-  void createLadder() {
-    LadderService ladderService = new LadderService();
-
-    List<String> players = ladderService.createPlayers("user1,user2").playersName();
-    Ladder ladder = ladderService.createLadder(1, players.size());
-
-    assertThat(ladder.ladderValues().size()).isEqualTo(1);
-  }
-
-  @DisplayName("상품결과 객체 생성 테스트.")
-  @Test
-  void creatPrizes() {
-    LadderService ladderService = new LadderService();
-    ladderService.createPlayers("user1,user2");
-    Prizes prizes = ladderService.createPrizes("good1,good2", 2);
-
-    assertThat(prizes.prizeNames().size()).isEqualTo(2);
-    assertThat(prizes.prizeNames()).containsExactly("good1", "good2");
-  }
-
-  @DisplayName("결과값 반환 검증.")
-  @ParameterizedTest
-  @CsvSource(value = {"user1,1", "user2,1", "all,2"})
-  void createMatchResult(String name, int count) {
-    LadderService ladderService = new LadderService();
-
-    Players players = ladderService.createPlayers("user1,user2");
-    Ladder ladder = ladderService.createLadder(1, players.playersName().size());
-
-    List<MatchResult> user1 = ladderService.getMatchResults(ladder, players, name);
-
-    assertThat(user1.size()).isEqualTo(count);
+    assertAll(
+        () -> assertThat(modelDto.getPlayers().playersName()).contains("user1,user2"),
+        () -> assertThat(modelDto.getLadder().ladderValues().size()).isEqualTo(2),
+        () -> assertThat(modelDto.getPrizes().prizeNames()).contains("prz1,prz2")
+    );
   }
 }
