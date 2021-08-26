995b4567a155641d0a9a20e5c3b3ac4329c1799c
hiro <kiryuinh32@gmail.com>
2021-08-27 02:05:13 +0900

    refactor: ResultView 의 문자열 상수로 추출

--- src/main/java/labber/view/ResultView.java
+++ src/main/java/labber/view/ResultView.java
@@ -8,7 +8,10 @@ import labber.domain.Users;
 public class ResultView {
     private static final StringBuilder STRING_BUILDER = new StringBuilder();
     private static final String EXECUTION_RESULT = "실행 결과";
-
+    private static final String LADDER_LENGTH = "|";
+    private static final String NO_LADDER = "     ";
+    private static final String LADDER = "-----";
+    private static final int INIT_NUMBER = 0;
 
     public static void printResult(Users users, Ladder ladder) {
         System.out.println(EXECUTION_RESULT);
@@ -19,18 +22,18 @@ public class ResultView {
         System.out.println();
 
         for (Line line : ladder.getLines()) {
-            STRING_BUILDER.append("|");
+            STRING_BUILDER.append(LADDER_LENGTH);
             for (Boolean points : line.getPoints()) {
-                if (points == false) {
-                    STRING_BUILDER.append("     ");
-                    STRING_BUILDER.append("|");
+                if (!points) {
+                    STRING_BUILDER.append(NO_LADDER);
+                    STRING_BUILDER.append(LADDER_LENGTH);
                     continue;
                 }
-                STRING_BUILDER.append("-----");
-                STRING_BUILDER.append("|");
+                STRING_BUILDER.append(LADDER);
+                STRING_BUILDER.append(LADDER_LENGTH);
             }
             System.out.println(STRING_BUILDER);
-            STRING_BUILDER.setLength(0);
+            STRING_BUILDER.setLength(INIT_NUMBER);
         }
     }
 }
