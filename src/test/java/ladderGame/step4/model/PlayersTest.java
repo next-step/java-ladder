e2193e162ee3a771e972b617a360e9f51abf3ff2
kdh85 <modudamba@naver.com>
2021-08-26 09:52:00 +0900

    test  : 사다리게임(step4)- PlayersTest, MatchResultsTest 테스트코드 리팩토링 Players 객체를 정적메소드로만 객체생성이 되도록 수정에 따라 테스트코드를 리팩토링함.

--- src/test/java/ladderGame/step4/model/PlayersTest.java
+++ src/test/java/ladderGame/step4/model/PlayersTest.java
@@ -3,7 +3,6 @@ package ladderGame.step4.model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,23 +11,23 @@ class PlayersTest {
   @DisplayName("사용자명을 쉼표단위로 여러개 입력하여 사용자를 생성.")
   @Test
   void createPlayersTest() {
-    List<PlayerName> of = Players.from("user1,user2");
-    assertThat(of.size()).isEqualTo(2);
+    Players players = Players.from("user1,user2");
 
-    assertThat(new Players(of)).isEqualTo(new Players(of));
+    assertThat(players.playersName().size()).isEqualTo(2);
+    assertThat(players).isEqualTo(Players.from("user1,user2"));
   }
 
   @DisplayName("입력값 기준으로 사용자 명수 검증 테스트.")
   @Test
   void playersCountTest() {
-    assertThatThrownBy(() -> new Players(Players.from(",,,")))
+    assertThatThrownBy(() -> Players.from(",,,"))
         .isInstanceOf(IllegalArgumentException.class);
   }
 
   @DisplayName("입력된 사용자명의 문자열 리스트를 반환.")
   @Test
   void writePlayersName() {
-    Players players = new Players(Players.from("user1,user2"));
+    Players players = Players.from("user1,user2");
 
     assertThat(players.playersName().size()).isEqualTo(2);
     assertThat(players.playersName()).containsExactly("user1","user2");
@@ -37,7 +36,7 @@ class PlayersTest {
   @DisplayName("이름이 같은 객체의 인덱스 반환.")
   @Test
   void findNameIndex() {
-    Players players = new Players(Players.from("user1,user2"));
+    Players players = Players.from("user1,user2");
 
     assertThat(players.findIndexByName("user2")).isEqualTo(1);
   }
