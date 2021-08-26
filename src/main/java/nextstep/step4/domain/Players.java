6d221de7fbaad7ca7c2555ccac0a5a1591352c7c
seondongpyo <seondongpyo@gmail.com>
2021-08-26 22:36:01 +0900

    feat: 참가자 리스트를 멤버로 갖는 일급 콜렉션 Players 클래스 추가

---
+++ src/main/java/nextstep/step4/domain/Players.java
@@ -0,0 +1,46 @@
+package nextstep.step4.domain;
+
+import static java.util.stream.Collectors.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class Players {
+
+	private static final String DELIMITER = ",";
+
+	private final List<Player> players;
+
+	public Players(List<Player> players) {
+		this.players = new ArrayList<>(players);
+	}
+
+	public static Players from(String names) {
+		List<Player> players = Arrays.stream(names.split(DELIMITER))
+								.map(Player::new)
+								.collect(collectingAndThen(toList(), Collections::unmodifiableList));
+		return new Players(players);
+	}
+
+	public int size() {
+		return players.size();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Players players1 = (Players)o;
+		return Objects.equals(players, players1.players);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(players);
+	}
+}
---
+++ src/test/java/nextstep/step4/domain/PlayersTest.java
@@ -0,0 +1,25 @@
+package nextstep.step4.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PlayersTest {
+
+	@DisplayName("입력 받은 이름들로 Players 객체를 생성한다.")
+	@Test
+	void create() {
+		Players players = Players.from("pobi,crong,honux,jk");
+		assertThat(players).isEqualTo(Players.from("pobi,crong,honux,jk"));
+	}
+
+	@DisplayName("입력 받은 이름들은 콤마(,)를 기준으로 나눈다.")
+	@Test
+	void split() {
+		Players players = Players.from("pobi,crong,honux,jk");
+		assertThat(players.size()).isEqualTo(4);
+	}
+
+
+}
