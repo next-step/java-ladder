f38a23429977c104c65ed09381be4626202f9eab
newchk610@gmail.com <gprms@1208>
2021-08-26 10:36:18 +0900

    feat: Add Line 객체 및 테스트 생성

--- src/main/java/nextstep/ladder/Line.java
+++ src/main/java/nextstep/ladder/Line.java
@@ -1,2 +1,27 @@
-package nextstep.ladder;public class Line {
+package nextstep.ladder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Line {
+    private List<Boolean> points = new ArrayList<>();
+
+    private Line(int size, RandomPoint random) {
+        for (int i = 0; i < size; i++) {
+            points.add(random.makeBoolean());
+        }
+    }
+
+    public static Line of(int size, RandomPoint random) {
+        return new Line(size, random);
+    }
+
+
+    public int getSize() {
+        return points.size();
+    }
+
+    public List<Boolean> getList() {
+        return new ArrayList<Boolean>(points);
+    }
 }
--- src/test/java/nextstep/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -1,4 +1,29 @@
-import static org.junit.jupiter.api.Assertions.*;
+package nextstep.ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class LineTest {
-  
+
+    @Test
+    @DisplayName("라인 생성 길이체크")
+    void create1() {
+        RandomPoint random = () -> true;
+        Line line = Line.of(5, random);
+
+        assertThat(line.getSize()).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("라인 생성 리스트 확인")
+    void create2() {
+        RandomPoint random = () -> true;
+        Line line = Line.of(2, random);
+
+        assertThat(line.getList()).containsExactly(true,true);
+    }
+
+
 }
