7cb0fc34e4de975c8894fdc46f130f4f4aff6e01
kdh85 <modudamba@naver.com>
2021-08-26 10:26:26 +0900

    refactor  : 사다리게임(step4)- LadderService 메소드 리팩토링 기존에 new 객체를 생성해서 반환하던 방식에서 정적메소드가 생성해서 반환한 것을 넘겨주는 방식으로 변경함.

--- src/main/java/ladderGame/step4/service/LadderService.java
+++ src/main/java/ladderGame/step4/service/LadderService.java
@@ -10,15 +10,15 @@ import ladderGame.step4.model.Prizes;
 public class LadderService {
 
   public Players createPlayers(final String playerNames) {
-    return new Players(Players.from(playerNames));
+    return Players.from(playerNames);
   }
 
   public Ladder createLadder(final int ladderHeight, final int playerCount) {
-    return new Ladder(Ladder.of(ladderHeight, playerCount));
+    return Ladder.of(ladderHeight, playerCount);
   }
 
   public Prizes createPrizes(final String goods, final int playerCount) {
-    return new Prizes(Prizes.of(goods, playerCount));
+    return Prizes.of(goods, playerCount);
   }
 
   public List<MatchResult> getMatchResults(final Ladder ladder, final Players players, final String findName) {
