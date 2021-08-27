a218bbab2ddd10aa98db9971b93f399f56181d04
gunny6026 <gunny6026@naver.com>
2021-08-27 10:48:18 +0900

    refactor : 배열에서 컬렉션으로 전환

--- src/main/java/nextstep/ladder/domain/Players.java
+++ src/main/java/nextstep/ladder/domain/Players.java
@@ -10,28 +10,28 @@ public class Players {
     private static final int PLAYERS_MIN_COUNT = 2;
     private List<Name> players;
 
-    public Players(String inputPlayers) {
-        this(toArray(inputPlayers));
+    public Players(String players) {
+        this(toList(players));
     }
 
-    public static String[] toArray(String inputPlayers) {
-        return inputPlayers.split(DELIMITER);
-    }
-
-    public Players(String[] players) {
-        if (players.length < PLAYERS_MIN_COUNT) {
-            throw new IllegalArgumentException("참가자 수가 부족합니다. 게임을 진행하려면 최소 " + PLAYERS_MIN_COUNT + "명 이상 필요합니다.");
-        }
-        this.players = Arrays.stream(players)
+    private static List<Name> toList(String players) {
+        return Arrays.stream(players.split(DELIMITER))
                 .map(Name::new)
                 .collect(Collectors.toList());
+    }
+
+    public Players(List<Name> players) {
+        if (players.size() < PLAYERS_MIN_COUNT) {
+            throw new IllegalArgumentException("참가자 수가 부족합니다. 게임을 진행하려면 최소 " + PLAYERS_MIN_COUNT + "명 이상 필요합니다.");
+        }
+        this.players = players;
     }
 
     public int size() {
         return players.size();
     }
 
-    public Stream<Name> stream(){
+    public Stream<Name> stream() {
         return players.stream();
     }
 }
