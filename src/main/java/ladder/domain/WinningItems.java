1266947d399825c9c03f7eecc5d713ed04d8bf4c
hjjangDev <ju0398@hanmail.net>
2021-08-27 22:42:44 +0900

    refactor : WinningItems 유효성 검사 추가

--- src/main/java/ladder/domain/WinningItems.java
+++ src/main/java/ladder/domain/WinningItems.java
@@ -1,14 +1,25 @@
 package ladder.domain;
 
+import ladder.exception.InvalidInputException;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
 public class WinningItems {
+    private static final String INVALID_WINNING_ITEMS_MESSAGE = "실행 결과의 갯수가 올바르지 않습니다.";
+
     private List<String> winningItems;
 
-    public WinningItems(List<String> winningItems) {
+    public WinningItems(List<String> names, List<String> winningItems) {
+        if (names.size() != winningItems.size()) {
+            throw new InvalidInputException(INVALID_WINNING_ITEMS_MESSAGE);
+        }
         this.winningItems = winningItems;
+    }
+
+    public String get(int index) {
+        return winningItems.get(index);
     }
 
     public List<String> getWinningItems() {
