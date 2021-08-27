1da81ce501e1086fff6cf70b6c6b64ba58b88317
hiro <kiryuinh32@gmail.com>
2021-08-27 23:31:29 +0900

    feat: InputView.askLadderHeight 예외 상황시, 안내 출력문 추가 (리뷰 반영)

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
 
     private static final String ASK_USER_NAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String ASK_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
+    private static final String NO_NUMBER_INPUT = "숫자를 입력해야 합니다.";
 
     public static String[] askUserName() {
         System.out.println(ASK_USER_NAME);
@@ -20,6 +21,7 @@ public class InputView {
         try {
             return Integer.parseInt(SCANNER.nextLine());
         } catch (RuntimeException e) {
+            System.out.println(NO_NUMBER_INPUT);
             return askLadderHeight();
         }
     }
