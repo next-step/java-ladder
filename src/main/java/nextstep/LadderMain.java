7e3d9b8aa8a89333d1ebd5b5e459963ac871e881
boss.back <boss.back@kakaocorp.com>
2021-08-27 16:04:04 +0900

    docs: 요구사항 정리

---
+++ src/main/java/nextstep/LadderMain.java
@@ -0,0 +1,9 @@
+package nextstep;
+
+import nextstep.ladder.controller.LadderController;
+
+public class LadderMain {
+    public static void main(String[] args) {
+        LadderController.startLadder();
+    }
+}
---
+++ src/main/java/nextstep/ladder/controller/LadderController.java
@@ -0,0 +1,8 @@
+package nextstep.ladder.controller;
+
+public class LadderController {
+
+    public static void startLadder(){
+
+    }
+}
