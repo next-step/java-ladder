a91f63a07b085ad231fc0b9e3fe92b34816c12cd
gunny6026 <gunny6026@naver.com>
2021-08-27 10:38:46 +0900

    refactor : 약어 대신 구체적인 이름 사용

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -7,15 +7,15 @@ import java.util.Scanner;
 public class InputView {
     private static final String INPUT_PLAYERS_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String INPUT_LADDER_COUNT_MESSAGE = "최대 사다리 높이는 몇 개 인가요?";
-    private static final Scanner sc = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static String inputPlayers() {
         System.out.println(INPUT_PLAYERS_MESSAGE);
-        return Validation.isEmptyAndNull(sc.nextLine());
+        return Validation.isEmptyAndNull(scanner.nextLine());
     }
 
     public static int inputLadderCount() {
         System.out.println(INPUT_LADDER_COUNT_MESSAGE);
-        return sc.nextInt();
+        return scanner.nextInt();
     }
 }
