46cf47851e50896ab6915f7cb7af8104a995505f
LAPTOP-I4NE0UIJ\yongs <smilesigma@gmail.com>
2021-08-27 18:26:59 +0900

    refact: 여때껏 해왔던 부분 전체 삭제.

--- src/main/java/nextstep/ladder/exception/HorizontalLineException.java
+++
@@ -1,8 +0,0 @@
-package nextstep.ladder.exception;
-
-public class HorizontalLineException extends IllegalArgumentException {
-
-    public HorizontalLineException(String s) {
-        super(s);
-    }
-}
