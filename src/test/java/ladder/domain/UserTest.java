b033bcc39d5e7191fe7e9775e095491e22b8c433
hiro <kiryuinh32@gmail.com>
2021-08-28 00:27:01 +0900

    test: User 생성 테스트 추가 (리뷰 반영)

--- src/test/java/ladder/domain/UserTest.java
+++ src/test/java/ladder/domain/UserTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class UserTest {
@@ -14,6 +15,14 @@ class UserTest {
     @ValueSource(strings = {"overFive", ""})
     void UserNameException(String name) {
         assertThatThrownBy(() -> new User(name)).isInstanceOf(UserNameLengthException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("User 생성 테스트")
+    @ValueSource(strings = {"hi", "hi1", "hi2", "hi3", "hi4"})
+    void createUserTest(String name) {
+        User user = new User(name);
+        assertThat(user.getName()).isEqualTo(name);
     }
 
 }
