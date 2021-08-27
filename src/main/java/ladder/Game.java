e8317d4a87a959ca2b755477c36cdb01dc601e0e
hjjangDev <ju0398@hanmail.net>
2021-08-27 22:39:54 +0900

    refactor :  Result 클래스 삭제 및 Location클래스 추가

--- src/main/java/ladder/Game.java
+++ src/main/java/ladder/Game.java
@@ -1,38 +1,24 @@
 package ladder;
 
 import ladder.domain.Ladder;
-import ladder.domain.Result;
 import ladder.domain.Users;
 import ladder.domain.WinningItems;
-import ladder.exception.InvalidInputException;
 import ladder.view.InputView;
 import ladder.view.OutputView;
 
 import java.util.List;
 
 public class Game {
-    private static final String INVALID_WINNING_ITEMS_MESSAGE = "실행 결과의 갯수가 올바르지 않습니다.";
 
     public static void main(String[] args) {
         List<String> names = InputView.inputNamesOfUsers();
         List<String> items = InputView.inputWinningItems();
-        validateItems(names, items);
 
         Users users = Users.valueOf(names);
-        WinningItems winningItems = new WinningItems(items);
+        WinningItems winningItems = new WinningItems(names, items);
         Ladder ladder = Ladder.create(InputView.inputHeightOfLadder(), names.size());
 
         OutputView.printLadderGameInfo(users, ladder, winningItems);
-
-        Result result = new Result(users, ladder, winningItems);
-        result.calculateLadderResult();
-
-        OutputView.printResult(InputView.inputResultPersonName(), result.getResult());
-    }
-
-    private static void validateItems(List<String> names, List<String> items) {
-        if (names.size() != items.size()) {
-            throw new InvalidInputException(INVALID_WINNING_ITEMS_MESSAGE);
-        }
+        OutputView.printResult(InputView.inputResultPersonName(), ladder.calculateLadderResult(users, winningItems));
     }
 }
--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -1,16 +1,15 @@
 package ladder.view;
 
-import ladder.domain.*;
+import ladder.domain.Ladder;
+import ladder.domain.Name;
+import ladder.domain.Users;
+import ladder.domain.WinningItems;
 
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class OutputView {
     private static final String LADDER_TITLE = "\n사다리 결과\n";
-    private static final String RESULT_TITLE = "\n실행 결과\n";
-    private static final String LINE_TRUE = "-----";
-    private static final String LINE_FALSE = "     ";
-    private static final String LINE_DELIMITER = "|";
+    private static final String RESULT_TITLE = "\n실행 결과";
 
     public static void printLadderGameInfo(Users users, Ladder ladder, WinningItems winningItems) {
         OutputView.printNames(users);
@@ -20,15 +19,12 @@ public class OutputView {
 
     private static void printNames(Users users) {
         System.out.println(LADDER_TITLE);
-        users.getNames().forEach(name -> System.out.printf("%6s", name.getName()));
+        users.getNames().forEach(name -> System.out.printf("%6s", name));
         System.out.println();
     }
 
     private static void printLadder(Ladder ladder) {
-        ladder.getLines()
-              .stream()
-              .map(OutputView::generateLine)
-              .forEach(System.out::println);
+        System.out.println(ladder.getLadderString());
     }
 
     private static void printItems(WinningItems winningItems) {
@@ -37,20 +33,12 @@ public class OutputView {
         System.out.println();
     }
 
-    private static String generateLine(Line line) {
-        return line.getPoints()
-                   .stream()
-                   .map(flag -> flag ? LINE_TRUE : LINE_FALSE)
-                   .collect(Collectors.joining(LINE_DELIMITER)) + "|";
-
-    }
-
-    public static void printResult(String name, Map<String, String> result) {
+    public static void printResult(String name, Map<Name, String> result) {
         System.out.println(RESULT_TITLE);
         if ("all".equals(name)) {
             result.entrySet().stream().forEach(e -> System.out.println(e.getKey() + " : " + e.getValue()));
             return;
         }
-        System.out.println(result.get(name));
+        System.out.println(result.get(Name.valueOf(name)));
     }
 }
