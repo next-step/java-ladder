70ea76f7b56dbb7e84fe6d24240c14ef5473f3dd
ChangSubKwak <paradise1732@gmail.com>
2021-08-29 02:38:27 +0900

    refactor: Line을 LadderLine으로 변경함에 따른 수정

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -1,31 +1,31 @@
 package ladder.domain;
 
-import ladder.exception.LadderHeightException;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import ladder.exception.LadderHeightException;
+
 public class Ladder {
     private static final int LADDER_MIN_HEIGHT = 2;
 
-    private final List<Line> ladder;
+    private final List<LadderLine> ladder;
 
-    private Ladder(List<Line> ladder) {
+    private Ladder(List<LadderLine> ladder) {
         this.ladder = ladder;
     }
 
     private Ladder(int countOfPerson, int height) {
         validate(height);
         ladder = Collections.unmodifiableList(
-            Stream.generate(() -> Line.from(countOfPerson))
+            Stream.generate(() -> LadderLine.init(countOfPerson))
                 .limit(height)
                 .collect(Collectors.toList())
         );
     }
 
-    public static Ladder from(List<Line> ladder) {
+    public static Ladder from(List<LadderLine> ladder) {
         return new Ladder(ladder);
     }
 
@@ -40,11 +40,11 @@ public class Ladder {
     }
 
     public int resultIndexOf(Index index) {
-        ladder.stream().forEach(index::moveOf);
+        ladder.forEach(index::moveOf);
         return index.val();
     }
 
-    public List<Line> toList() {
+    public List<LadderLine> toList() {
         return ladder;
     }
 }
--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -1,19 +1,20 @@
 package ladder;
 
-import ladder.domain.Index;
-import ladder.domain.Ladder;
-import ladder.domain.Line;
-import ladder.domain.Point;
-import ladder.exception.LadderHeightException;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ladder.domain.Index;
+import ladder.domain.Ladder;
+import ladder.domain.LadderLine;
+import ladder.domain.Point;
+import ladder.exception.LadderHeightException;
 
 class LadderTest {
     @DisplayName("참여할_사람의_인원_수와_사다리_높이가_주어지면_사다리_높이만큼의_Line객체를_가진_Ladder가_생성된다")
@@ -40,14 +41,26 @@ class LadderTest {
     void resultIndexOfTest() {
         // 기본 사다리 생성 |-----|
         List<Point> points = new ArrayList<>();
-        points.add(Point.of(false, true));
-        points.add(Point.of(true, false));
-        points.add(Point.of(false, true));
-        points.add(Point.of(true, false));
+        points.add(Point.first(true));
+        points.add(points.get(0).next(false));
+        points.add(points.get(1).next(true));
+        points.add(points.get(2).last());
 
-        List<Line> lines = Arrays.asList(Line.from(points));
-        Ladder ladder = Ladder.from(lines);
+        List<LadderLine> ladderLines = Collections.singletonList(LadderLine.from(points));
+        Ladder ladder = Ladder.from(ladderLines);
         assertThat(ladder.resultIndexOf(Index.from(0))).isEqualTo(1);
         assertThat(ladder.resultIndexOf(Index.from(1))).isEqualTo(0);
+    }
+
+    @Test
+    public void init() {
+        int sizeOfPerson = 5;
+        System.out.println(LadderLine.init(sizeOfPerson));
+    }
+
+    @Test
+    public void move() {
+        LadderLine line = LadderLine.init(2);
+        System.out.println("ladder result : " + line.move(0));
     }
 }
