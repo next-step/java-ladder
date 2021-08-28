55d5cf12fd4e4cabde567377106af879e11b00da
jaeyoungjang2 <lion85488@gmail.com>
2021-08-28 10:52:56 +0900

    feat: 높이 1이하 예외처리

--- src/main/java/step2/LadderApp.java
+++ src/main/java/step2/LadderApp.java
@@ -1,12 +1,14 @@
 package step2;
 
+import step2.domain.Ladder;
+import step2.domain.Name;
 import step2.view.InputView;
 
 public class LadderApp {
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        String nameOfParticipant = inputView.requireNameOfParticipant();
-        int heightOfLadder = inputView.requireHeightOfLadder();
+        Name nameOfParticipant = new Name(inputView.requireNameOfParticipant());
+        Ladder ladder = new Ladder(inputView.requireHeightOfLadder());
         
     }
 }
