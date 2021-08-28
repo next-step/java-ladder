1f1e657f0fd0c0386dc96348d1603f98398bcb78
harris <harrisleesh@gmail.com>
2021-08-28 21:14:36 +0900

    refactor : Change the responsibility of split input to InputView

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class InputView {
     public static final Scanner scanner = new Scanner(System.in);
+    public static final String VALID_DELIMITER = ",";
 
-    public String requestName() {
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        return scanner.nextLine();
+    public String[] requestName() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(" + VALID_DELIMITER + ")로 구분하세요)");
+        return scanner.nextLine().split(VALID_DELIMITER);
     }
 
     public int requestHeight() {
