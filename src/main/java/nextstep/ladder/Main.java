7834b95ada67af03cc4bb9336b81d4d44b0c5c07
jayyhkwon <jayyhkwon@gmail.com>
2021-08-28 15:39:07 +0900

    feat step2 Main 클래스 추가

---
+++ src/main/java/nextstep/ladder/Main.java
@@ -0,0 +1,11 @@
+package nextstep.ladder;
+
+import nextstep.ladder.controller.Game;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+    }
+}
