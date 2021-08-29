c2a923104c957ff33e2f5f2b737b644be0c4fb8d
gashe-soo <gashe7@gmail.com>
2021-08-29 20:28:17 +0900

    feat:사람 이름 최소 1글자 제한

--- src/main/java/ladder/model/Name.java
+++ src/main/java/ladder/model/Name.java
@@ -3,6 +3,7 @@ package ladder.model;
 public class Name {
 
     private static final int MAX_LENGTH = 5;
+    private static final int MIN_LENGTH = 1;
 
     private String name;
 
@@ -11,8 +12,12 @@ public class Name {
         this.name = name;
     }
 
-    private void validateName(String name){
-        if(name.length() > MAX_LENGTH){
+    private void validateName(String name) {
+        if (name == null || name.length() < MIN_LENGTH) {
+            throw new IllegalArgumentException("이름은 최소 1글자입니다.");
+        }
+
+        if (name.length() > MAX_LENGTH) {
             throw new IllegalArgumentException("이름은 최대 5글자입니다.");
         }
     }
