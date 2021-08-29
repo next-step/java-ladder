3dd319eafbf1c862dc67a9d4f1c4bf93679d35ae
조원진 <>
2021-08-29 19:38:14 +0900

    refactor(RandomDirectionMakingStrategy) : 인터페이스 참조 변경

--- src/main/java/ladder/domain/RandomDirectionMakingStrategy.java
+++ src/main/java/ladder/domain/RandomDirectionMakingStrategy.java
@@ -1,17 +1,18 @@
 package ladder.domain;
 
 import java.security.SecureRandom;
+import java.util.Random;
 
 public class RandomDirectionMakingStrategy implements DirectionMakingStrategy {
     private static final int TENTH = 10;
 
     private static final int DECISION_PERCENTAGE = 5;
 
-    private final SecureRandom secureRandom = new SecureRandom();
+    private final Random random = new SecureRandom();
 
     @Override
     public Direction generate() {
-        if (secureRandom.nextInt(TENTH) < DECISION_PERCENTAGE) {
+        if (random.nextInt(TENTH) < DECISION_PERCENTAGE) {
             return Direction.RIGHT;
         }
         return Direction.NONE;
