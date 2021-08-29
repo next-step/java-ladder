25262787e631dc8e62fb0129bebc48704109d74d
iiaii <jhun9409@naver.com>
2021-08-29 23:48:53 +0900

    [step3] test, feat: 현재위치에서 이동하는 moveFrom 구현 및 테스트

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -9,6 +9,8 @@ import java.util.stream.IntStream;
 public class Line {
 
     private static final String LESS_THAN_ZERO = "playerCount 값은 0보다 작을 수 없습니다 -> %d";
+    private static final String INVALID_POINT = "사다리 지점의 바 생성 방향이 부적절합니다";
+    private static final String INVALID_POSITION = "해당 position 에는 Point 가 존재하지 않습니다 -> %d";
     private static final int ZERO = 0;
     private static final Random RANDOM = new Random();
 
@@ -19,6 +21,14 @@ public class Line {
         this.points = createPoints(playerCount);
     }
 
+    public Line(final List<Point> points) {
+        List<Point> sortedPoints = points.stream()
+                .sorted()
+                .collect(Collectors.toList());
+        validatePoints(sortedPoints);
+        this.points = sortedPoints;
+    }
+
     public List<Boolean> getLineBarStatus() {
         return points.stream()
                 .map(Point::isRightDirection)
@@ -26,12 +36,58 @@ public class Line {
                 .collect(Collectors.toList());
     }
 
+    public int moveFrom(int position) {
+        requiteValidPosition(position);
+        return points.get(position)
+                .move();
+    }
+
     private List<Point> createPoints(final int playerCount) {
         AtomicBoolean prevBarInstalled = new AtomicBoolean(false);
         return IntStream.range(0, playerCount)
-                .mapToObj(i -> new Point(i, Direction.decide(prevBarInstalled.get(), RANDOM.nextBoolean())))
+                .mapToObj(i -> new Point(i, Direction.decide(prevBarInstalled.get(), isCurrentInstall(playerCount, i))))
                 .peek(point -> prevBarInstalled.set(point.isRightDirection()))
                 .collect(Collectors.toList());
+    }
+
+    private boolean isCurrentInstall(final int playerCount, final int index) {
+        return playerCount - 1 != index
+                && RANDOM.nextBoolean();
+    }
+
+    private void requiteValidPosition(final int position) {
+        if (points.size() - 1 < position) {
+            throw new IllegalArgumentException(String.format(INVALID_POSITION, position));
+        }
+    }
+
+    private void validatePoints(final List<Point> points) {
+        boolean isValidPoints = IntStream.range(0, points.size() - 1)
+                .filter(i -> !isValidPointDirection(points.get(i), points.get(i + 1)))
+                .findAny()
+                .isPresent();
+        if (isValidPoints) {
+            throw new IllegalArgumentException(INVALID_POINT);
+        }
+
+    }
+
+    private boolean isValidPointDirection(final Point current, final Point next) {
+        return isCurrentRightNextLeft(current, next)
+                || isCurrentLeftNextNotLeft(current, next)
+                || isCurrentNoneNextNotLeft(current, next);
+    }
+
+    private boolean isCurrentNoneNextNotLeft(final Point current, final Point next) {
+        return current.isEqualDirection(Direction.NONE) && !next.isEqualDirection(Direction.LEFT);
+    }
+
+    private boolean isCurrentLeftNextNotLeft(final Point current, final Point next) {
+        return current.isEqualDirection(Direction.LEFT) && !next.isEqualDirection(Direction.LEFT);
+    }
+
+    private boolean isCurrentRightNextLeft(final Point current, final Point next) {
+        return current.isRightDirection() && next.isEqualDirection(Direction.LEFT);
     }
 
     private void validatePlayerCount(final int playerCount) {
