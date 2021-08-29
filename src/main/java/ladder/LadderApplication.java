a95521ae8266196fe50f92bf0ae3e6e27549d307
hiro <kiryuinh32@gmail.com>
2021-08-30 05:59:39 +0900

    feat: 출력과 재처리를 위한 main 메소드 구현

--- src/main/java/ladder/LadderApplication.java
+++ src/main/java/ladder/LadderApplication.java
@@ -1,10 +1,9 @@
 package ladder;
 
-import ladder.domain.Ladder;
-import ladder.domain.Users;
+import ladder.domain.*;
+import ladder.exception.NotFoundUserException;
 
-import static ladder.view.InputView.askLadderHeight;
-import static ladder.view.InputView.askUserName;
+import static ladder.view.InputView.*;
 import static ladder.view.ResultView.*;
 
 public class LadderApplication {
@@ -12,11 +11,48 @@ public class LadderApplication {
         String[] usersName = askUserName();
         Users users = new Users(usersName);
 
+        int winningItemsCount = usersName.length;
+        WinningItems winningItems = getWinningItems(winningItemsCount);
+
         int ladderHeight = askLadderHeight();
         Ladder ladder = new Ladder(ladderHeight, usersName.length);
 
         printUserList(users);
         printLadder(ladder);
+        printWinningItems(winningItems);
+
+        MatchResult matchResult = new MatchResult(users, ladder, winningItems);
+
+        while (true) {
+            getWhoResult(matchResult, users);
+        }
+
+    }
+
+    private static WinningItems getWinningItems(int count) {
+        String[] winningItems = askWinningItems();
+        if (winningItems.length != count) {
+            System.out.println("참여자 숫자 만큼 당첨 항목을 입력하세요\n");
+            return getWinningItems(count);
+        }
+        return new WinningItems(winningItems);
+    }
+
+    private static void getWhoResult(MatchResult matchResult, Users users) {
+        String name = askWhoResult();
+        if (name.equals("all")) {
+            printAllResult(matchResult);
+            return;
+        }
+
+        try {
+            User user = users.getUserToName(name);
+            printResult(matchResult, user);
+        } catch (NotFoundUserException e) {
+            System.out.println("사람 이름 잘못 입력했어요\n");
+            getWhoResult(matchResult, users);
+        }
+
     }
 
 }
