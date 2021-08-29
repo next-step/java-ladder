b348c4b484a813f17ce446565c98c9573e388b3a
gashe-soo <gashe7@gmail.com>
2021-08-30 00:00:28 +0900

    feat:사다리 이동 기능

--- src/main/java/ladder/model/Line.java
+++ src/main/java/ladder/model/Line.java
@@ -5,20 +5,36 @@ import java.util.List;
 public class Line {
 
     private static final String LINE_OVERLAPPED_MSG = "라인은 겹치면 안됩니다.";
+    private List<Boolean> steps;
 
-    public static void of(List<Boolean> steps) {
+    private Line(List<Boolean> steps) {
+        this.steps = steps;
+    }
+
+    public static Line of(List<Boolean> steps) {
         validateStep(steps);
+        return new Line(steps);
     }
 
     private static void validateStep(List<Boolean> steps) {
         for (int i = 0; i < steps.size() - 1; i++) {
-            isOverLapped(steps.get(i), steps.get(i+1));
+            isOverLapped(steps.get(i), steps.get(i + 1));
         }
     }
 
     private static void isOverLapped(Boolean firstStep, Boolean secondStep) {
-        if(firstStep && secondStep){
+        if (firstStep && secondStep) {
             throw new IllegalArgumentException(LINE_OVERLAPPED_MSG);
         }
+    }
+
+    public int move(int now) {
+        if (steps.get(now)) {
+            return now - 1;
+        }
+        if (steps.get(now + 1)) {
+            return now + 1;
+        }
+        return now;
     }
 }
--- src/test/java/ladder/model/LineTest.java
+++ src/test/java/ladder/model/LineTest.java
@@ -4,17 +4,54 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class LineTest {
 
     @Test
-    void 라인은_겹치면_오류발생(){
+    void 라인은_겹치면_오류발생() {
         assertThatIllegalArgumentException()
                 .isThrownBy(
-                        () -> Line.of(Arrays.asList(true,true,false,false))
+                        () -> Line.of(Arrays.asList(true, true, false, false))
                 ).withMessageContaining("라인은 겹치면 안됩니다.");
+    }
+
+    @Test
+    void 왼쪽_디딤대가_있을경우_디딤대를_타고_이동() {
+        // given
+        Line line = Line.of(Arrays.asList(false, true, false, true));
+
+        // when
+        int nowPosition = line.move(1);
+
+        // then
+        assertThat(nowPosition).isEqualTo(0);
+    }
+
+    @Test
+    void 오른쪽_디딤대가_있을경우_디딤대를_타고_이동() {
+        // given
+        Line line = Line.of(Arrays.asList(false, true, false, true));
+
+        // when
+        int nowPosition = line.move(2);
+
+        // then
+        assertThat(nowPosition).isEqualTo(3);
+    }
+
+    @Test
+    void 디딤대가_양쪽에_없을경우_아래칸으로_이동() {
+        // given
+        Line line = Line.of(Arrays.asList(false, false, false, true));
+
+        // when
+        int nowPosition = line.move(1);
+
+        // then
+        assertThat(nowPosition).isEqualTo(1);
     }
 
 }
