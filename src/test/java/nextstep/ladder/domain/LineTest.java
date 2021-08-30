6adabbd56ef673688d050a0d4d0fc821b73b81d8
SONG22 <ansongi527@gmail.com>
2021-08-30 14:03:51 +0900

    test(step2): 라인생성 test code

--- src/test/java/nextstep/ladder/domain/LineTest.java
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -3,18 +3,24 @@ package nextstep.ladder.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class LineTest {
 
-    @DisplayName("라인의 좌표 값에 선이 없으면 생성")
+    @DisplayName("라인에 올바른(true 이후엔 반드시 false) boolean 리스트가 오면 생성된다.")
     @Test
-    void create(){
-
+    void create() {
+        Line line = new Line(Arrays.asList(false, true, false));
+        assertThat(line).isEqualTo(new Line(Arrays.asList(false, true, false)));
     }
 
-    @DisplayName("라인의 좌표 값에 선이 있으면 생성실패")
+    @DisplayName("라인에 올바르지 않은 boolean 리스트가 오면 생성 실패")
     @Test
-    void create_fail(){
-
-
+    void create_fail() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Line(Arrays.asList(true, true, false)));
     }
 }
