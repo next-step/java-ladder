4fe5f3e0cdf61972db88bc919baf249ddfcf15ad
iiaii <jhun9409@naver.com>
2021-08-30 22:22:04 +0900

    [step3] test, feat: 플레이어 이름이 all 인 경우 생성되지 않도록 구현 및 테스트

--- src/main/java/nextstep/ladder/domain/Player.java
+++ src/main/java/nextstep/ladder/domain/Player.java
@@ -1,10 +1,14 @@
 package nextstep.ladder.domain;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 public class Player {
 
+    private static final String UNALLOWABLE_NAME = "%s 이라는 이름의 플레이어는 생성 될 수 없습니다";
     private static final String VALID_LENGTH = "입력값은 공백을 제외한 %s~%s글자 여야 합니다 -> %s";
+    private static final List<String> BANNED_NAMES = Collections.singletonList("all");
     private static final int MIN_LENGTH = 1;
     public static final int MAX_LENGTH = 5;
 
@@ -12,11 +16,19 @@ public class Player {
 
     public Player(final String name) {
         requireValidLength(name);
+        validateName(name);
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    private void validateName(final String name) {
+        if (BANNED_NAMES.stream()
+                .anyMatch(bannedName -> bannedName.equals(name))) {
+            throw new IllegalArgumentException(String.format(UNALLOWABLE_NAME, name));
+        }
     }
 
     private void requireValidLength(final String name) {
--- src/test/java/nextstep/ladder/domain/PlayerTest.java
+++ src/test/java/nextstep/ladder/domain/PlayerTest.java
@@ -1,6 +1,8 @@
 package nextstep.ladder.domain;
 
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -8,6 +10,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class PlayerTest {
+
+    @Test
+    @DisplayName("플레이어 이름이 all 인 경우")
+    public void notAllowedName() {
+        // given
+        String name = "all";
+        String message = String.format("%s 이라는 이름의 플레이어는 생성 될 수 없습니다", name);
+
+        // when
+        ThrowingCallable throwingCallable = () -> new Player(name);
+
+        // then
+        assertThatThrownBy(throwingCallable)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(message);
+    }
 
     @ParameterizedTest(name = "플레이어 생성 - 1~5글자가 아닌 경우 | {arguments}")
     @ValueSource(strings = {"abcdef", ""})
