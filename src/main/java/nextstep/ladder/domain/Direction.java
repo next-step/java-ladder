e63ecb53cadc3aca13f5c605555d219030318c0c
gunny6026 <gunny6026@naver.com>
2021-08-31 15:06:09 +0900

    fix : 사다리의 이동을 책임지는 Direction 객체

---
+++ src/main/java/nextstep/ladder/domain/Direction.java
@@ -0,0 +1,14 @@
+package nextstep.ladder.domain;
+
+public class Direction {
+    private int direction;
+
+    public Direction(int direction) {
+        this.direction = direction;
+    }
+
+    public int move(int point) {
+        direction += point;
+       return direction;
+    }
+}
