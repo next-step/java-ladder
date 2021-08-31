f4d04805821326aecd2443804e16797a92065904
SONG22 <ansongi527@gmail.com>
2021-08-31 20:54:25 +0900

    feat(step3): 결과 객체 validation 구현

--- src/main/java/nextstep/ladder/domain/LadderResults.java
+++ src/main/java/nextstep/ladder/domain/LadderResults.java
@@ -6,8 +6,10 @@ public class LadderResults {
     private List<Player> players;
     private List<String> results;
 
-
     public LadderResults(List<Player> players, List<String> results) {
+        if (players.size() != results.size()) {
+            throw new IllegalArgumentException("결과 수와 참여자 수는 같아야 합니다.");
+        }
         this.players = players;
         this.results = results;
     }
