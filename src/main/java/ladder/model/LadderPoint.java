ee14a421603a16be8993ab270eeb96afc961eda8
YooInKeun <keun0390@naver.com>
2021-08-31 20:43:39 +0900

    feat(chore): 사다리 포인트 이동 후의 인덱스 구하는 기능 추가

--- src/main/java/ladder/model/LadderPoint.java
+++ src/main/java/ladder/model/LadderPoint.java
@@ -2,6 +2,7 @@ package ladder.model;
 
 public class LadderPoint {
     private static final int MIN_INDEX = 0;
+    private static final int INDEX_GAP_BETWEEN_CURRENT_AND_NEXT_POINT = 1;
 
     private final int index;
     private final LadderDirection direction;
@@ -16,5 +17,17 @@ public class LadderPoint {
         if (index < MIN_INDEX) {
             throw new IllegalArgumentException(String.format("포인트 인덱스는 %d이상 이어야 합니다.", MIN_INDEX));
         }
+    }
+
+    int move() {
+        if (direction.isRight()) {
+            return index + INDEX_GAP_BETWEEN_CURRENT_AND_NEXT_POINT;
+        }
+
+        if (direction.isLeft()) {
+            return index - INDEX_GAP_BETWEEN_CURRENT_AND_NEXT_POINT;
+        }
+
+        return index;
     }
 }
--- src/test/java/ladder/model/LadderPointTest.java
+++ src/test/java/ladder/model/LadderPointTest.java
@@ -2,8 +2,11 @@ package ladder.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("사다리 포인트 테스트")
 public class LadderPointTest {
@@ -15,5 +18,17 @@ public class LadderPointTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new LadderPoint(-1, new LadderDirection(false, false)))
                 .withMessage("포인트 인덱스는 0이상 이어야 합니다.");
+    }
+
+    @DisplayName("사다리에서 움직일 때 값이 true인 사다리 방향에 따라 포인트 인덱스가 증가하거나 감소하거나 그대로다.")
+    @CsvSource(value = {"false:false:1", "false:true:2", "true:false:0"}, delimiter = ':')
+    @ParameterizedTest
+    void moveLadderPointTest(boolean left, boolean right, int movedIndex) {
+        // given, when
+        LadderDirection direction = new LadderDirection(left, right);
+        LadderPoint ladderPoint = new LadderPoint(1, direction);
+
+        // then
+        assertEquals(ladderPoint.move(), movedIndex);
     }
 }
