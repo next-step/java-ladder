6b08b465d319ee42d5fc559412d1b380d9ffce87
채윤병 <ybchae0801@naver.com>
2021-08-31 23:21:48 +0900

    feat: 사다리 높이 예외 처리 로직 추가

---
+++ src/main/java/nextstep/laddergame/domain/LadderHeights.java
@@ -0,0 +1,28 @@
+package nextstep.laddergame.domain;
+
+public class LadderHeights {
+
+    private static final int MINIMUM_HEIGHTS = 1;
+    private static final String INVALID_HEIGHTS_EXCEPTION_MESSAGE_FORMAT = "사다리의 높이가 유효하지 않습니다. heights: %s";
+
+    private final int heights;
+
+    public LadderHeights(int heights) {
+        validateMinimumHeights(heights);
+        this.heights = heights;
+    }
+
+    private void validateMinimumHeights(int heights) {
+        if (heights < MINIMUM_HEIGHTS) {
+            throw new IllegalArgumentException(String.format(INVALID_HEIGHTS_EXCEPTION_MESSAGE_FORMAT, heights));
+        }
+    }
+
+    public static LadderHeights of(int heights) {
+        return new LadderHeights(heights);
+    }
+
+    public int getHeights() {
+        return heights;
+    }
+}
