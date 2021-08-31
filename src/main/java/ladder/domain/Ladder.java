e0387b190a6b11847b0ce4e280d5827fa5b9131a
harris <harrisleesh@gmail.com>
2021-09-01 00:23:41 +0900

    feat : Create resultOf, resultAll method in ladder

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -6,14 +6,31 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class Ladder {
+    private int width;
     private List<Line> lines = new ArrayList<>();
 
-    public Ladder(int height, int personCount, LineGenerateStrategy lineGenerateStrategy) {
+    public Ladder(int height, int width, LineGenerateStrategy lineGenerateStrategy) {
+        this.width = width;
         IntStream.range(0, height)
-                .forEach(idx -> lines.add(new Line(personCount, lineGenerateStrategy)));
+                .forEach(idx -> lines.add(new Line(width, lineGenerateStrategy)));
     }
 
     public List<Line> lines() {
         return Collections.unmodifiableList(lines);
+    }
+
+    public int resultOf(int startIndex) {
+        int endIndex = startIndex;
+        for (Line line : lines) {
+            endIndex = line.move(endIndex);
+        }
+        return endIndex;
+    }
+
+    public List<Integer> resultAll() {
+        List<Integer> resultAll = new ArrayList<>();
+        IntStream.range(0, width)
+                .forEach(index -> resultAll.add(resultOf(index)));
+        return resultAll;
     }
 }
