72f24163849dd9eedcb94c30ac885e8e971d4bb2
YooInKeun <keun0390@naver.com>
2021-08-31 20:06:46 +0900

    feat(chore): 다음 사다리 방향 생성하는 기능 추가

--- src/main/java/ladder/model/LadderDirection.java
+++ src/main/java/ladder/model/LadderDirection.java
@@ -1,6 +1,10 @@
 package ladder.model;
 
+import java.util.Random;
+
 public class LadderDirection {
+    private static final Random random = new Random();
+
     private final boolean left;
     private final boolean right;
 
@@ -10,8 +14,19 @@ public class LadderDirection {
         this.right = right;
     }
 
+    LadderDirection next() {
+        if (right) {
+            return next(false);
+        }
+        return next(generatePoint());
+    }
+
     static LadderDirection first(boolean right) {
         return new LadderDirection(false, right);
+    }
+
+    private LadderDirection next(boolean nextRight) {
+        return new LadderDirection(this.right, nextRight);
     }
 
     static LadderDirection last(boolean left) {
@@ -22,6 +37,10 @@ public class LadderDirection {
         if (left && right) {
             throw new IllegalArgumentException("사다리 방향이 양쪽으로 있을 수 없습니다.");
         }
+    }
+
+    private boolean generatePoint() {
+        return random.nextBoolean();
     }
 
     boolean isLeft() {
--- src/test/java/ladder/model/LadderDirectionTest.java
+++ src/test/java/ladder/model/LadderDirectionTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 @DisplayName("사다리 방향 테스트")
 public class LadderDirectionTest {
 
-    @DisplayName("사다리 방향이 양쪽으로 있으면 예외가 발생한다.")
+    @DisplayName("사다리 방향이 양쪽으로 true면 예외가 발생한다.")
     @Test
     void bothDirectionsExceptionTest() {
         // given, when, then
@@ -18,7 +18,7 @@ public class LadderDirectionTest {
                 .withMessage("사다리 방향이 양쪽으로 있을 수 없습니다.");
     }
 
-    @DisplayName("첫번째 사다리의 방향에서 왼쪽 방향은 false 이어야 한다.")
+    @DisplayName("첫번째 사다리의 방향에서 왼쪽은 false 이어야 한다.")
     @Test
     void leftOfFirstLadderDirectionTest() {
         // given, when
@@ -28,7 +28,7 @@ public class LadderDirectionTest {
         assertSame(firstDirection.isLeft(), false);
     }
 
-    @DisplayName("마지막 사다리의 방향에서 오른쪽 방향은 false 이어야 한다.")
+    @DisplayName("마지막 사다리의 방향에서 오른쪽은 false 이어야 한다.")
     @Test
     void rightOfLastLadderDirectionTest() {
         // given, when
@@ -36,5 +36,16 @@ public class LadderDirectionTest {
 
         // then
         assertSame(lastDirection.isRight(), false);
+    }
+
+    @DisplayName("오른쪽이 true인 사다리 방향에서 다음 사다리 방향을 구하면, 다음 사다리 방향의 오른쪽은 false 이어야 한다.")
+    @Test
+    void rightOfNextLadderDirectionTest() {
+        // given, when
+        LadderDirection ladderDirection = new LadderDirection(false, true);
+        LadderDirection nextDirection = ladderDirection.next();
+
+        // then
+        assertSame(nextDirection.isRight(), false);
     }
 }
