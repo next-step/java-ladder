2960afb7a9cd6d84d1c28155ff1ed6b42eb0ff60
harris <harrisleesh@gmail.com>
2021-09-01 00:23:03 +0900

    feat : Create move method in Line entity

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -6,25 +6,22 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class Line {
-    public static final boolean FALSE_BECAUSE_OF_LEFT_TRUE = false;
-    public static final int MIN_PERSON_COUNT = 2;
+    private static final boolean FALSE_BECAUSE_OF_LEFT_TRUE = false;
+    private static final boolean OUT_OF_BOUND = false;
+    private static final int MIN_WIDTH = 2;
     private List<Boolean> points = new ArrayList<>();
 
-    public Line(int personCount, LineGenerateStrategy lineGenerateStrategy) {
-        validate(personCount);
-        makeLine(personCount - 1, lineGenerateStrategy);
-    }
-
-    private void validate(int personCount) {
-        if (personCount < MIN_PERSON_COUNT) {
-            throw new IllegalArgumentException("사다리 게임에 참여하는 사람 수가 너무 작습니다. 사람 수 : " + personCount);
-        }
-    }
-
-    private void makeLine(int ladderWidth, LineGenerateStrategy lineGenerateStrategy) {
+    public Line(int width, LineGenerateStrategy lineGenerateStrategy) {
+        validate(width);
         points.add(lineGenerateStrategy.generatable());
-        IntStream.range(1, ladderWidth)
+        IntStream.range(1, width - 1)
                 .forEach(position -> points.add(generatable(position, lineGenerateStrategy)));
+    }
+
+    private void validate(int width) {
+        if (width < MIN_WIDTH) {
+            throw new IllegalArgumentException("사다리 게임에 참여하는 사람 수가 너무 작습니다. 사람 수 : " + width);
+        }
     }
 
     private boolean generatable(int position, LineGenerateStrategy lineGenerateStrategy) {
@@ -34,11 +31,24 @@ public class Line {
         return lineGenerateStrategy.generatable();
     }
 
-    public boolean lineExists(int position) {
+    public int move(int endIndex) {
+        if (lineExists(endIndex - 1)) {
+            return endIndex - 1;
+        } else if (lineExists(endIndex)) {
+            return endIndex + 1;
+        }
+        return endIndex;
+    }
+
+    private boolean lineExists(int position) {
+        if (position < 0 || position >= points.size()) {
+            return OUT_OF_BOUND;
+        }
         return points.get(position);
     }
 
     public List<Boolean> points() {
         return Collections.unmodifiableList(points);
     }
+
 }
