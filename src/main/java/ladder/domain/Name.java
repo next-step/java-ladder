986c86a8d10dc27a6d0ca6fa2c2f824710b820d1
harris <harrisleesh@gmail.com>
2021-09-01 00:24:53 +0900

    feat : Create Names 1st Collection name List

--- src/main/java/ladder/domain/Name.java
+++ src/main/java/ladder/domain/Name.java
@@ -1,8 +1,6 @@
 package ladder.domain;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Objects;
 
 public class Name {
     private static final String VALID_NAME_CHARACTER = "0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣";
@@ -22,27 +20,29 @@ public class Name {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("이름은 최대 5자까지 입력할 수 있습니다. name : " + name);
         }
-    }
-
-    public static List<Name> of(String[] names) {
-        Arrays.stream(names).forEach(name -> validateName(name));
-        return Arrays.stream(names)
-                .map(Name::new)
-                .collect(Collectors.toList());
-    }
-
-    private static void validateName(String name) {
         if (!name.matches(validNameFormatRegex())) {
             throw new IllegalArgumentException("이름 리스트에 유효하지 않은 특수문자가 포함되어 있습니다.");
         }
-
     }
 
     private static String validNameFormatRegex() {
         return REGEX_PREFIX + VALID_NAME_CHARACTER + REGEX_SEPERATOR + REGEX_SUFFIX;
     }
 
-    public String getName() {
+    public String name() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
