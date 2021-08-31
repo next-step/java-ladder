584be7db24d7358991346a7c0f29161a904b0f1a
hiro <kiryuinh32@gmail.com>
2021-08-31 23:06:08 +0900

    refactor: NotFoundUserException 에서 잘못 입력된 인자를 받을수 있도록 수정 (리뷰반영)

--- src/main/java/ladder/exception/NotFoundUserException.java
+++ src/main/java/ladder/exception/NotFoundUserException.java
@@ -1,9 +1,9 @@
 package ladder.exception;
 
 public class NotFoundUserException extends RuntimeException {
-    private static final String NOT_FOUND_USER = "해당하는 유저를 찾을수 없습니다.";
+    private static final String NOT_FOUND_USER = "해당하는 유저를 찾을수 없습니다. ";
 
-    public NotFoundUserException() {
-        super(NOT_FOUND_USER);
+    public NotFoundUserException(String user) {
+        super(NOT_FOUND_USER + user);
     }
 }
