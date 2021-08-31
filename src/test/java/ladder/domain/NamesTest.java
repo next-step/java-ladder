47a46cdaabc34c94def4fcabe566028ccb455dca
harris <harrisleesh@gmail.com>
2021-09-01 00:35:32 +0900

    test : Create exception test for Names entity

---
+++ src/test/java/ladder/domain/NamesTest.java
@@ -0,0 +1,19 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NamesTest {
+
+    @Test
+    @DisplayName("Names 생성 시 사람의 수가 2보다 작으면 IllegalArgumentException이 발생한다.")
+    void minPerson(){
+        String[] nameString = {"harri"};
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Names(nameString))
+                .withMessage("사다리 게임에 참여하는 사람 수가 너무 작습니다. 사람 수 : " + 1);
+    }
+}
