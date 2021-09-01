b1642e144b0199f4aa88575c148e28c6a93d9cbc
yook0328 <yook0328@gmail.com>
2021-09-01 22:53:25 +0900

    refactor: 현재 불필요한 객체 삭제.

--- src/test/java/nextstep/ladder/LadderGameTest.java
+++ src/test/java/nextstep/ladder/LadderGameTest.java
@@ -1,13 +1,16 @@
 package nextstep.ladder;
 
-import nextstep.ladder.model.Ladder;
 import nextstep.ladder.model.Person;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -31,22 +34,59 @@ public class LadderGameTest {
         assertThatIllegalArgumentException().isThrownBy(() -> LadderGame.makePeople(input));
     }
 
-    @DisplayName("사다리 생성 테스트")
-    @ParameterizedTest
-    @CsvSource({"5,3", "3,2", "1,2", "3,5"})
-    public void makeLadderTest(int numberOfPeople, int height) {
-        //when
-        Ladder ladder = LadderGame.makeLadder(numberOfPeople, height);
-
-        //then
-        assertThat(ladder.getHeight()).isEqualTo(height);
-        assertThat(ladder.getLines().size()).isEqualTo(numberOfPeople);
-    }
-
     @DisplayName("잘못된 입력값에 의한 사다리 테스트")
     @ParameterizedTest
     @CsvSource({"-5,3", "3,-2", "0,2", "3,0", "0,0"})
     public void wrongInputMakeLadderTest(int numberOfPeople, int height) {
-        assertThatIllegalArgumentException().isThrownBy(() -> LadderGame.makeLadder(numberOfPeople, height));
+        assertThatIllegalArgumentException().isThrownBy(() -> LadderGame.makeLines(numberOfPeople, height));
+    }
+
+
+    @Test
+    public void test() {
+        Map<Foo, Integer> map = new HashMap<>();
+
+        Foo b = new Foo("b");
+        map.put(new Foo("a"), 1);
+        map.put(b, 2);
+        System.out.println(map.size());  // (1)
+        map.put(new Foo("b"), 3);
+        System.out.println(map.size());  //(2)
+
+        b.setTest("c");
+        Foo c = new Foo("c");
+        System.out.println(map.get(c));  //(3)
+    }
+
+    class Foo {
+        String test;
+
+        public Foo(String test) {
+            this.test = test;
+        }
+
+        public void setTest(String test) {
+            this.test = test;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Foo test1 = (Foo) o;
+            return Objects.equals(test, test1.test);
+        }
+
+        @Override
+        public String toString() {
+            return "Foo{" +
+                    "test='" + test + '\'' +
+                    '}';
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(test);
+        }
     }
 }
