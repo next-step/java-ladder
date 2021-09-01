c1571528ca9be8af6519a2e153673b8c0b6e1eea
iiaii <jhun9409@naver.com>
2021-09-01 22:48:06 +0900

    [step4] test, feat: Line 제거 및 LadderLine 구현 및 테스트

---
+++ src/main/java/nextstep/ladder/domain/LadderLine.java
@@ -0,0 +1,55 @@
+package nextstep.ladder.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static nextstep.ladder.domain.LadderPointGenerator.generatePoint;
+
+public class LadderLine {
+
+    private final List<Point> points;
+
+    public LadderLine(List<Point> points) {
+        this.points = points;
+    }
+
+    public int move(int position) {
+        return points.get(position).move();
+    }
+
+    public static LadderLine init(int sizeOfPerson) {
+        List<Point> points = new ArrayList<>();
+        Point point = initFirst(points);
+        point = initBody(sizeOfPerson, points, point);
+        initLast(points, point);
+        return new LadderLine(points);
+    }
+
+    public List<Boolean> getLineBarStatus() {
+        return points.stream()
+                .map(Point::isRightDirection)
+                .limit(points.size() - 1)
+                .collect(Collectors.toList());
+    }
+
+    private static Point initBody(int sizeOfPerson, List<Point> points, Point point) {
+        for (int i = 1; i < sizeOfPerson - 1; i++) {
+            point = point.next();
+            points.add(point);
+        }
+        return point;
+    }
+
+    private static void initLast(List<Point> points, Point point) {
+        point = point.last();
+        points.add(point);
+    }
+
+    private static Point initFirst(List<Point> points) {
+        Point point = Point.first(generatePoint());
+        points.add(point);
+        return point;
+    }
+
+}
