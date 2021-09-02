be151c367860b1f386aee81133583a2dd39ae96e
beadoer1 <beadoer1@gmail.com>
2021-09-02 16:44:58 +0900

    feat(Point) : fail, fail 입력에 대한 에러 발생 test fail 생성

--- src/test/java/ladderrefactoring/domain/ladder/PointTest.java
+++ src/test/java/ladderrefactoring/domain/ladder/PointTest.java
@@ -1,10 +1,12 @@
 package ladderrefactoring.domain.ladder;
+import ladderrefactoring.exception.CustomException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PointTest {
 
@@ -15,6 +17,14 @@ class PointTest {
         Point point = new Point(2, false, false);
         // then
         assertThat(point).isEqualTo(new Point(2, false, false));
+    }
+
+    @Test
+    @DisplayName("Point 생성 실패 : true, true")
+    void create_fail() {
+        // when, then
+        assertThatThrownBy(() -> new Point(2, true, true))
+                .isInstanceOf(CustomException.class);
     }
 
     @ParameterizedTest(name = "Point 이동 [{index}] {0} {1} {2} -> {3}")
