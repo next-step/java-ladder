59c8a8a27f9189f4ac92f438b04fd21355592916
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-03 03:23:53 +0900

    feat: splitByComma 유틸 메소드 추가

--- src/main/java/ladder/util/StringUtil.java
+++ src/main/java/ladder/util/StringUtil.java
@@ -6,6 +6,11 @@ public class StringUtil {
 
     private static final int ZERO = 0;
 
+    private static final String COMMA = ",";
+
+    private static final String BLANK = "\\s";
+    private static final String EMPTY = "";
+
     private StringUtil() {
     }
 
@@ -19,6 +24,10 @@ public class StringUtil {
             }
         }
         return true;
+    }
+
+    public static String[] splitByComma(String input) {
+        return input.replaceAll(BLANK, EMPTY).split(COMMA);
     }
 
 }
