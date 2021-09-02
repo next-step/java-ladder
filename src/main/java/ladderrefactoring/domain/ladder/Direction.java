311e7a31144bcd39a2282e977f15e319fe24a79f
beadoer1 <beadoer1@gmail.com>
2021-09-02 18:12:42 +0900

    feat(Direction) : true, true 생성 실패 validation 구현

--- src/main/java/ladderrefactoring/domain/ladder/Direction.java
+++ src/main/java/ladderrefactoring/domain/ladder/Direction.java
@@ -1,14 +1,26 @@
 package ladderrefactoring.domain.ladder;
 
+import ladderrefactoring.exception.CustomException;
+
 import java.util.Objects;
 
 public class Direction {
+
+    public static final String EXIST_TRUE_REPETITION = "연속된 사다리가 존재 합니다.";
+
     private final boolean left;
     private final boolean right;
 
     public Direction(boolean left, boolean right) {
+        checkTrueRepetition(left, right);
         this.left = left;
         this.right = right;
+    }
+
+    private void checkTrueRepetition(boolean beforeValue, boolean currentValue) {
+        if (beforeValue && currentValue) {
+            throw new CustomException(EXIST_TRUE_REPETITION);
+        }
     }
 
     @Override
