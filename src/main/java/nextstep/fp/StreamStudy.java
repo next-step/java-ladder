b0e321d667f8d4a506ae484dec06c35b25fe55c5
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-02 17:51:40 +0900

    feat: printLongestWordTop100() 메소드 구현

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -32,7 +32,11 @@ public class StreamStudy {
             .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+            .filter(word -> word.length()>12)
+            .map(String::toLowerCase)
+            .distinct()
+            .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
