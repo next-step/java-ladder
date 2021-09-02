19c766de2700d7b3cbff24cee6756684693d798b
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-03 02:47:21 +0900

    feat: 사다리 생성 시 높이 1보다 작을 경우 Exception 기능 구현 및 테스트

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -2,10 +2,22 @@ package ladder.domain;
 
 public class Ladder {
 
+    private static final int MINIMUM_LADDER_HEIGHT = 1;
+
+    private static final String INVALID_LADDER_HEIGHT_ERROR_MESSAGE = "사다리 높이는 0보다 큰 값만 들어와야 한다.";
+
     private final int height;
 
     public Ladder(int height) {
+        checkLadderHeight(height);
+
         this.height = height;
+    }
+
+    private static void checkLadderHeight(int height) {
+        if (height < MINIMUM_LADDER_HEIGHT) {
+            throw new IllegalArgumentException(INVALID_LADDER_HEIGHT_ERROR_MESSAGE);
+        }
     }
 
 }
--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -1,9 +1,12 @@
 package ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LadderTest {
 
@@ -19,6 +22,17 @@ class LadderTest {
 
         // then
         assertThat(ladder).isNotNull();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    @DisplayName("사다리 생성 시 높이가 1보다 작은 값이 들어오면 Exception이 반환되어야 한다.")
+    void ladderFailByLowHeightTest(int input) {
+
+        // when & then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> new Ladder(input))
+            .withMessageMatching("사다리 높이는 0보다 큰 값만 들어와야 한다.");
     }
 
 }
