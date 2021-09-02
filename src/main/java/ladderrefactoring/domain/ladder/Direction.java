64d64d380d252201926d7a27a18dc0db9d7cff3a
beadoer1 <beadoer1@gmail.com>
2021-09-02 18:11:27 +0900

    feat(Direction) : equals(), hashCode() 구현

--- src/main/java/ladderrefactoring/domain/ladder/Direction.java
+++ src/main/java/ladderrefactoring/domain/ladder/Direction.java
@@ -1,5 +1,7 @@
 package ladderrefactoring.domain.ladder;
 
+import java.util.Objects;
+
 public class Direction {
     private final boolean left;
     private final boolean right;
@@ -7,5 +9,18 @@ public class Direction {
     public Direction(boolean left, boolean right) {
         this.left = left;
         this.right = right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Direction)) return false;
+        Direction direction = (Direction) o;
+        return left == direction.left && right == direction.right;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
     }
 }
