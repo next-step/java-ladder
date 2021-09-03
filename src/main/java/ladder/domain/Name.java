ca3df445fc4a602eac47225b56330ca2e0feba28
harris <harrisleesh@gmail.com>
2021-09-03 23:01:06 +0900

    refactor : Change method to constant

--- src/main/java/ladder/domain/Name.java
+++ src/main/java/ladder/domain/Name.java
@@ -3,10 +3,7 @@ package ladder.domain;
 import java.util.Objects;
 
 public class Name {
-    private static final String VALID_NAME_CHARACTER = "0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣";
-    public static final String REGEX_SEPERATOR = "|";
-    public static final String REGEX_PREFIX = "[";
-    public static final String REGEX_SUFFIX = "]*";
+    private static final String VALID_NAME_CHARACTER = "[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣|]*";
     public static final int MAX_NAME_LENGTH = 5;
 
     private String name;
@@ -20,13 +17,9 @@ public class Name {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("이름은 최대 5자까지 입력할 수 있습니다. name : " + name);
         }
-        if (!name.matches(validNameFormatRegex())) {
+        if (!name.matches(VALID_NAME_CHARACTER)) {
             throw new IllegalArgumentException("이름 리스트에 유효하지 않은 특수문자가 포함되어 있습니다.");
         }
-    }
-
-    private static String validNameFormatRegex() {
-        return REGEX_PREFIX + VALID_NAME_CHARACTER + REGEX_SEPERATOR + REGEX_SUFFIX;
     }
 
     public String name() {
