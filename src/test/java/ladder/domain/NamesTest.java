30e298a1b7075b61e9ffc93168c17116ce361db5
harris <harrisleesh@gmail.com>
2021-09-03 23:59:29 +0900

    test : Create test find wrongName

--- src/test/java/ladder/domain/NamesTest.java
+++ src/test/java/ladder/domain/NamesTest.java
@@ -9,10 +9,20 @@ class NamesTest {
 
     @Test
     @DisplayName("Names 생성 시 사람의 수가 2보다 작으면 IllegalArgumentException이 발생한다.")
-    void minPerson(){
+    void minPerson() {
         String[] nameString = {"harri"};
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new Names(nameString))
                 .withMessage("사다리 게임에 참여하는 사람 수가 너무 작습니다. 사람 수 : " + 1);
+    }
+
+    @Test
+    @DisplayName("잘못된 이름을 검색하면 IllegalArgumentException이 발생한다.")
+    void wrongName() {
+        String[] names = {"harri", "harro"};
+        Names namesObject = new Names(names);
+        String name = "harra";
+        assertThatIllegalArgumentException().isThrownBy(() -> namesObject.indexOf(name))
+                .withMessage("잘못된 이름입니다. name : " + name);
     }
 }
