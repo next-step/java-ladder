895ba7347450ed2712b6632ca27103c337f1b2e4
testminseok <opso_@naver.com>
2021-09-04 14:41:32 +0900

    refactor add Constant

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -7,10 +7,14 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
+    private static final int MAX_SIZE = 100;
+    private static final int MIN_VALUE = 3;
+    private static final int MIN_LENGTH = 12;
+    private static final int INIT_TOTAL_VALUE = 0;
+    private static final int MULTIPLIED_VALUE = 2;
 
     public static long countWords() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
@@ -18,7 +22,7 @@ public class StreamStudy {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         return words.stream()
-                .filter(w -> w.length() > 12)
+                .filter(w -> w.length() > MIN_LENGTH)
                 .count();
     }
 
@@ -28,28 +32,28 @@ public class StreamStudy {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         words.stream()
-                .filter(w -> w.length() > 12)
+                .filter(w -> w.length() > MIN_LENGTH)
                 .distinct()
                 .sorted(Comparator.comparing(String::length).reversed())
-                .limit(100)
+                .limit(MAX_SIZE)
                 .forEach(s -> System.out.println(s.toLowerCase()));
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
         return numbers.stream()
-                .map(x -> 2 * x)
+                .map(x -> MULTIPLIED_VALUE * x)
                 .collect(Collectors.toList());
     }
 
     public static long sumAll(List<Integer> numbers) {
         return numbers.stream()
-                .reduce(0, (x, y) -> x + y);
+                .reduce(INIT_TOTAL_VALUE, (x, y) -> x + y);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
         return numbers.stream()
-                .filter(num -> num > 3)
-                .map(num -> num * 2)
-                .reduce(0, Integer::sum);
+                .filter(num -> num > MIN_VALUE)
+                .map(num -> num * MULTIPLIED_VALUE)
+                .reduce(INIT_TOTAL_VALUE, Integer::sum);
     }
 }
