26cf22297eafee06e277e2eb9406a04f2aa20803
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-04 23:16:41 +0900

    feat: Point VO 구현 및 테스트

--- src/main/java/ladder/domain/ladder/Point.java
+++ src/main/java/ladder/domain/ladder/Point.java
@@ -1,26 +1,65 @@
 package ladder.domain.ladder;
 
-import static java.util.Optional.ofNullable;
-
-import java.util.Optional;
+import java.util.Objects;
+import ladder.strategy.LineGenerateStrategy;
 
 public class Point {
 
+    private static final boolean DISCONNECT_POINT = false;
+    private static final int START_POINT_INDEX = 0;
+    private static final int VALUE_TO_NEXT_INDEX = 1;
+
     private final int position;
 
-    private final Point linkPoint;
+    private final boolean connected;
 
-    public Point(int position) {
+    public Point(int position, boolean connected) {
         this.position = position;
-        this.linkPoint = null;
+        this.connected = connected;
     }
 
     public int position() {
         return position;
     }
 
-    public Optional<Point> linkPoint() {
-        return ofNullable(linkPoint);
+    public boolean connected() {
+        return connected;
     }
 
+    public static Point createFirst(LineGenerateStrategy lineGenerateStrategy) {
+        return new Point(START_POINT_INDEX, lineGenerateStrategy.generateLine());
+    }
+
+    public static Point createLast(Point point) {
+        return new Point(point.position + 1, DISCONNECT_POINT);
+    }
+
+    public static Point createNextByBeforePoint(Point before, LineGenerateStrategy lineGenerateStrategy) {
+        return new Point(before.getNextPosition(), createNextConnect(before, lineGenerateStrategy));
+    }
+
+    private static boolean createNextConnect(Point before, LineGenerateStrategy lineGenerateStrategy) {
+        return !before.connected && lineGenerateStrategy.generateLine();
+    }
+
+    private int getNextPosition() {
+        return position + VALUE_TO_NEXT_INDEX;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Point point = (Point) o;
+        return position == point.position && connected == point.connected;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, connected);
+    }
 }
--- src/test/java/ladder/domain/ladder/PointTest.java
+++ src/test/java/ladder/domain/ladder/PointTest.java
@@ -1,25 +1,106 @@
 package ladder.domain.ladder;
 
+import static ladder.domain.ladder.Point.createFirst;
+import static ladder.domain.ladder.Point.createLast;
+import static ladder.domain.ladder.Point.createNextByBeforePoint;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Random;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class PointTest {
 
+    @Nested
+    @DisplayName("첫번째 Point 객체를 만들 수 있다.")
+    class createFirstTest {
+
+        @Test
+        @DisplayName("[connected = true]")
+        void trueConnected() {
+
+            // given
+            Point expected = new Point(0, true);
+
+            // when
+            Point result = createFirst(() -> true);
+
+            // then
+            assertThat(result).isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("[connected = false]")
+        void falseConnected() {
+
+            // given
+            Point expected = new Point(0, false);
+
+            // when
+            Point result = createFirst(() -> false);
+
+            // then
+            assertThat(result).isEqualTo(expected);
+        }
+
+    }
+
     @Test
-    @DisplayName("Point 객체는 본인의 위치와 연결된 point참조값을 가진다.")
-    void pointCreateTest() {
+    @DisplayName("마지막 Point 객체를 만들 수 있다.")
+    void createLaseTest() {
 
         // given
-        int position = 2;
+        int index = 6;
+        Point nowPoint = new Point(index, true);
+        Point expected = new Point(index + 1, false);
 
         // when
-        Point point = new Point(2);
+        Point result = createLast(nowPoint);
 
         // then
-        assertThat(point.position()).isEqualTo(position);
-        assertThat(point.linkPoint().orElse(null)).isNull();
+        assertThat(result).isEqualTo(expected);
     }
+
+    @Nested
+    @DisplayName("다음 Point 객체를 만들 수 있다.")
+    class createNextPointTest {
+
+        @Test
+        @DisplayName("[before connected = true]")
+        void trueConnected() {
+
+            // given
+            int index = 6;
+            Point nowPoint = new Point(index, true);
+            Random random = new Random();
+            Point expected = new Point(index + 1, false);
+
+            // when
+            Point result = createNextByBeforePoint(nowPoint, () -> random.nextBoolean());
+
+            // then
+            assertThat(result).isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("[before connected = false]")
+        void falseConnected() {
+
+            // given
+            int index = 6;
+            Point nowPoint = new Point(index, false);
+            Random random = new Random();
+            Point expected = new Point(index + 1, true);
+
+            // when
+            Point result = createNextByBeforePoint(nowPoint, () -> true);
+
+            // then
+            assertThat(result).isEqualTo(expected);
+        }
+
+    }
+
 
 }
