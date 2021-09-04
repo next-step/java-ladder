3691d3c8c19a63b01f7b6df7b7ecb2eb54e7b00d
yook0328 <yook0328@gmail.com>
2021-09-04 16:06:54 +0900

    test: Point 다음 값에 대한 TC 작성

--- src/test/java/nextstep/ladder/model/PointTest.java
+++ src/test/java/nextstep/ladder/model/PointTest.java
@@ -3,6 +3,7 @@ package nextstep.ladder.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class PointTest {
@@ -28,5 +29,33 @@ public class PointTest {
         assertThatIllegalArgumentException().isThrownBy(() -> point.use(goal));
     }
 
+    @DisplayName("다음 이동값 테스트")
+    @Test
+    public void moveHorizontalTest() {
+        //given
+        int pos = 1;
+        int goal = 2;
+        Point point = new Point();
+        point.use(goal);
 
+        //when
+        int next = point.moveHorizontal(pos);
+
+        //then
+        assertThat(goal).isEqualTo(next);
+    }
+
+    @DisplayName("수평 이동 안할 시 테스트")
+    @Test
+    public void notMoveHorizontalTest() {
+        //given
+        int pos = 1;
+        Point point = new Point();
+
+        //when
+        int next = point.moveHorizontal(pos);
+
+        //then
+        assertThat(pos).isEqualTo(next);
+    }
 }
