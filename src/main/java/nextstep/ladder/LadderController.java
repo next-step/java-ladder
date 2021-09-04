f33c7e6d316c3f469ba14456059a95fee8bf1045
yook0328 <yook0328@gmail.com>
2021-09-04 16:00:24 +0900

    feat: 게임 구현부 추가, reward 부분 추가 refactor: init 하는 부분들 함수로 추출

--- src/main/java/nextstep/ladder/LadderController.java
+++ src/main/java/nextstep/ladder/LadderController.java
@@ -1,24 +1,77 @@
 package nextstep.ladder;
 
-import nextstep.ladder.model.Line;
+import nextstep.ladder.model.Lines;
 import nextstep.ladder.model.Person;
+import nextstep.ladder.model.Result;
+import nextstep.ladder.model.Reward;
 import nextstep.ladder.view.LadderGameDrawer;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LadderController {
+    private static final String LAST_COMMAND = "all";
+
     public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
 
-        LadderGameDrawer.drawPeopleInputText();
-        List<Person> people = LadderGame.makePeople(scanner.nextLine());
+        List<Person> people = initPeoplePhase(scanner);
+        List<Reward> rewards = initRewardsPhase(scanner);
+        validateRewardsAndPeople(people, rewards);
 
+        Lines lines = initLinesPhase(scanner, people.size());
+        LadderGameDrawer.drawLadderResult(people, lines, rewards);
+
+        while (true) {
+            LadderGameDrawer.drawWantedResultInputText();
+            String command = scanner.nextLine().trim();
+
+            LadderGameDrawer.drawResultText();
+            if (LAST_COMMAND.equals(command)) {
+                List<Result> results = IntStream.range(0, people.size())
+                        .mapToObj((index) -> new Result(people.get(index), rewards.get(lines.doResult(index))))
+                        .collect(Collectors.toList());
+                LadderGameDrawer.drawResults(results);
+                break;
+            }
+
+            int startPosition = findPeoplePosition(command, people);
+            LadderGameDrawer.drawResult(new Result(people.get(startPosition), rewards.get(lines.doResult(startPosition))));
+        }
+    }
+
+    private static List<Person> initPeoplePhase(Scanner scanner) {
+        LadderGameDrawer.drawPeopleInputText();
+        return LadderGameMakeUtil.makePeople(scanner.nextLine());
+    }
+
+    private static List<Reward> initRewardsPhase(Scanner scanner) {
+        LadderGameDrawer.drawRewardInputText();
+        return LadderGameMakeUtil.makeRewards(scanner.nextLine());
+    }
+
+    private static Lines initLinesPhase(Scanner scanner, int width) {
         LadderGameDrawer.drawHeightInputText();
         int height = Integer.valueOf(scanner.nextLine());
 
-        List<Line> lines = LadderGame.makeLines(people.size(), height);
+        return LadderGameMakeUtil.makeLines(width, height);
+    }
 
-        LadderGameDrawer.drawResult(people, lines);
+    private static void validateRewardsAndPeople(List<Person> people, List<Reward> rewards) {
+        if (rewards.size() == people.size()) {
+            return;
+        }
+
+        throw new IllegalArgumentException("참가자의 명수와 보상의 갯수가 일치하지 않습니다.");
+    }
+
+    private static int findPeoplePosition(String name, List<Person> people) {
+        if (!people.contains(name)) {
+            throw new IllegalArgumentException("this name does not contains in people ");
+        }
+
+        return people.indexOf(new Person(name));
     }
 }
