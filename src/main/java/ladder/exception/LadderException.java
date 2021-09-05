908f7d6b2a2e5167c1e9d01b810446e5338291bc
hiro <kiryuinh32@gmail.com>
2021-09-06 00:32:16 +0900

    refactor: exception 생성시 인자를 받도록 수정

--- src/main/java/ladder/exception/LadderException.java
+++
@@ -1,9 +0,0 @@
-package ladder.exception;
-
-public class LadderException extends RuntimeException {
-    private static final String HEIGHT_EXCEPTION = "사다리 최소 높이 부족";
-
-    public LadderException() {
-        super(HEIGHT_EXCEPTION);
-    }
-}
--- src/main/java/ladder/exception/UserNameLengthException.java
+++ src/main/java/ladder/exception/UserNameLengthException.java
@@ -1,9 +1,9 @@
 package ladder.exception;
 
 public class UserNameLengthException extends RuntimeException {
-    private static String USER_NAME_LENGTH_EXCEPTION = "사용자 이름은 5글자 이하로 생성 가능";
+    private static String USER_NAME_LENGTH_EXCEPTION = "사용자 이름은 5글자 이하로 생성 가능 입력한 이름 : %s";
 
-    public UserNameLengthException() {
-        super(USER_NAME_LENGTH_EXCEPTION);
+    public UserNameLengthException(String name) {
+        super(String.format(USER_NAME_LENGTH_EXCEPTION, name));
     }
 }
