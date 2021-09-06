2026f0bed8d2c2d1cba3edb6ae944443b6b9a41c
yook0328 <yook0328@gmail.com>
2021-09-06 23:49:23 +0900

    test: 방향 테스트 구현.

---
+++ src/test/java/nextstep/ladder/model/DirectionTest.java
@@ -0,0 +1,41 @@
+package nextstep.ladder.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class DirectionTest {
+    @DisplayName("우측 방향 생성 테스트")
+    @Test
+    public void rightDirectionTest() {
+        //when
+        Direction direction = Direction.right();
+
+        //then
+        assertThat(direction.isRight()).isEqualTo(true);
+        assertThat(direction.isLeft()).isEqualTo(false);
+    }
+
+    @DisplayName("좌측 방향 생성 테스트")
+    @Test
+    public void leftDirectionTest() {
+        //when
+        Direction direction = Direction.left();
+
+        //then
+        assertThat(direction.isRight()).isEqualTo(false);
+        assertThat(direction.isLeft()).isEqualTo(true);
+    }
+
+    @DisplayName("무방향 생성 테스트")
+    @Test
+    public void nonDirectionTest() {
+        //when
+        Direction direction = Direction.straight();
+
+        //then
+        assertThat(direction.isRight()).isEqualTo(false);
+        assertThat(direction.isLeft()).isEqualTo(false);
+    }
+}
