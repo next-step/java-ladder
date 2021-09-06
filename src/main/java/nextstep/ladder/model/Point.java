8df61692ef330d3838cb2f370c14d30855358a2e
yook0328 <yook0328@gmail.com>
2021-09-06 23:49:52 +0900

    refactor: Point 객체가 Direction을 사용하도록 수정.

--- src/main/java/nextstep/ladder/model/Point.java
+++ src/main/java/nextstep/ladder/model/Point.java
@@ -1,34 +1,23 @@
 package nextstep.ladder.model;
 
 public class Point {
-    private static final int MINIMUM_USED_NUMBER = 0;
-    private int goal = -1;
+    private int index;
+    private Direction direction;
 
-    public boolean isUsed() {
-        return goal >= MINIMUM_USED_NUMBER;
+    public Point(int index, Direction direction) {
+        this.index = index;
+        this.direction = direction;
     }
 
-    public void use(int goal) {
-        if (isUsed()) {
-            throw new IllegalArgumentException("이미 사용된 point 입니다. ");
+    public int move() {
+        if (direction.isLeft()) {
+            return index - 1;
         }
 
-        if (goal < MINIMUM_USED_NUMBER) {
-            throw new IllegalArgumentException("음수 값을 목표로 받을 수 없습니다. goal : " + goal);
+        if (direction.isRight()) {
+            return index + 1;
         }
 
-        this.goal = goal;
-    }
-
-    public int getGoal() {
-        return goal;
-    }
-
-    public int moveHorizontal(int x) {
-        if (!isUsed()) {
-            return x;
-        }
-
-        return goal;
+        return index;
     }
 }
