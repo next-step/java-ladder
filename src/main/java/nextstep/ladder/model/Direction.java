1e9a950cddd4858006c5f83129fb96ecb3e987aa
yook0328 <yook0328@gmail.com>
2021-09-06 23:49:06 +0900

    feat: 방향 객체 구현.

---
+++ src/main/java/nextstep/ladder/model/Direction.java
@@ -0,0 +1,59 @@
+package nextstep.ladder.model;
+
+import java.util.Objects;
+
+public class Direction {
+    private final boolean left;
+    private final boolean right;
+
+    private Direction(boolean left, boolean right) {
+        if (left && right) {
+            throw new IllegalStateException();
+        }
+
+        this.left = left;
+        this.right = right;
+    }
+
+    public boolean isRight() {
+        return this.right;
+    }
+
+    public boolean isLeft() {
+        return this.left;
+    }
+
+    public static Direction left() {
+        return new Direction(true, false);
+    }
+
+    public static Direction right() {
+        return new Direction(false, true);
+    }
+
+    public static Direction straight() {
+        return new Direction(false, false);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Direction pair = (Direction) o;
+        return left == pair.left &&
+                right == pair.right;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
+    }
+
+    @Override
+    public String toString() {
+        return "Direction{" +
+                "left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
