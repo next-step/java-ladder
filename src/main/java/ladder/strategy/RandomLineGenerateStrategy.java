ae10ba28f02c723845990bcc1b2100ff3e822dea
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-06 12:20:21 +0900

    refactor: Random 객체 하나만 생성하도록 적용

--- src/main/java/ladder/strategy/RandomLineGenerateStrategy.java
+++ src/main/java/ladder/strategy/RandomLineGenerateStrategy.java
@@ -9,16 +9,20 @@ public class RandomLineGenerateStrategy implements LineGenerateStrategy {
 
     private static final class LazyHolder {
         private static final RandomLineGenerateStrategy instance = new RandomLineGenerateStrategy();
+        private static final Random random = new Random();
     }
 
     public static RandomLineGenerateStrategy getInstance() {
         return LazyHolder.instance;
     }
 
+    public static Random getRandom() {
+        return LazyHolder.random;
+    }
+
     @Override
     public boolean generateLine() {
-        Random random = new Random();
-        return random.nextBoolean();
+        return getRandom().nextBoolean();
     }
 
 }
