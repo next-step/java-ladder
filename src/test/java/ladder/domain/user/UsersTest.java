5d40dd79552edf7fcbea1ab87b5b483a3ee52a5a
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-06 12:21:07 +0900

    refactor

--- src/test/java/ladder/domain/user/UsersTest.java
+++ src/test/java/ladder/domain/user/UsersTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import java.util.Arrays;
 import java.util.List;
-import ladder.domain.ladder.LadderHeight;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
