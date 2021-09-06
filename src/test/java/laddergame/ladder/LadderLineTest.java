3899cc2bc0f19181a2bc6d8b5f283cea0b661b60
JoyDaheeCha <daheecha0612@gmail.com>
2021-09-06 21:51:30 +0900

    refactor: LadderLines 테스트 코드 내 DisplayName 변경

--- src/test/java/laddergame/ladder/LadderLineTest.java
+++ src/test/java/laddergame/ladder/LadderLineTest.java
@@ -10,7 +10,7 @@ class LadderLineTest {
 
     private static final int USER_COUNT = 4;
 
-    @DisplayName("연결가능여부가 항상 false일 경우, 라인수는 0개")
+    @DisplayName("연결 가능 여부가 항상 false: 연결된 곳은 0개")
     @Test
     void connectable_false() {
         assertThat(
@@ -22,7 +22,7 @@ class LadderLineTest {
     }
 
 
-    @DisplayName("연결 가능 여부가 항상 true일 경우 라인수는 1개 이상")
+    @DisplayName("연결 가능 여부가 항상 true: 연결된 곳은 1개 이상")
     @Test
     void connectable_true() {
         assertThat(
@@ -33,7 +33,7 @@ class LadderLineTest {
         ).isNotZero();
     }
 
-    @DisplayName("사람수-1 = 포인트 수") // TODO : 포인트 수 리네임
+    @DisplayName("연결가능여부 수 : 참가자수보다 1 적다")
     @Test
     void pointsCount_equals_countOfPearson_minus_1() {
         assertThat(new LadderLine(USER_COUNT, () -> Connection.CONNECTED).connections().size())
