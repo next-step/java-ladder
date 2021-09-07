358f0d10ebb67ed34c699ede21de26bd08ad3a5c
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-07 16:19:47 +0900

    feat: ladder results 생성시 유저 수 불일치 exception기능 구현

--- src/main/java/ladder/domain/ladder/LadderResults.java
+++ src/main/java/ladder/domain/ladder/LadderResults.java
@@ -9,15 +9,25 @@ import java.util.stream.Collectors;
 
 public class LadderResults {
 
+    private static final String INVALID_RESULTS_COUNT_ERROR_MESSAGE = "사람 수와 동일한 수의 결과가 입력되어야 한다.";
+
     private final List<String> results;
 
     public LadderResults(List<String> results) {
         this.results = results;
     }
 
-    public static LadderResults of(String input) {
+    public static LadderResults from(String input, int userCount) {
+        checkResultsCount(input, userCount);
+
         return new LadderResults(Arrays.stream(splitByComma(input))
             .collect(Collectors.toList()));
+    }
+
+    private static void checkResultsCount(String input, int userCount) {
+        if (splitByComma(input).length != userCount) {
+            throw new IllegalArgumentException(INVALID_RESULTS_COUNT_ERROR_MESSAGE);
+        }
     }
 
     @Override
@@ -36,4 +46,5 @@ public class LadderResults {
     public int hashCode() {
         return Objects.hash(results);
     }
+
 }
--- src/test/java/ladder/domain/ladder/LadderResultsTest.java
+++ src/test/java/ladder/domain/ladder/LadderResultsTest.java
@@ -1,6 +1,8 @@
 package ladder.domain.ladder;
 
+import static ladder.domain.ladder.Point.createNextByBeforePoint;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
@@ -15,13 +17,28 @@ class LadderResultsTest {
 
         // given
         String input = "꽝,5000,꽝,3000";
+        int userCount = 4;
         LadderResults expected = new LadderResults(Arrays.asList("꽝", "5000", "꽝", "3000"));
 
         // when
-        LadderResults results =  LadderResults.of(input);
+        LadderResults results =  LadderResults.from(input, 4);
 
         // then
         assertThat(results).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("실행 결과가 사람수와 일치하지 않으면 exception이 발생되어야 한다.")
+    void ladderResultSaveFailTest() {
+
+        // given
+        String input = "꽝,5000,꽝,3000";
+        int userCount = 5;
+
+        // when & then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> LadderResults.from(input, userCount))
+            .withMessageMatching("사람 수와 동일한 수의 결과가 입력되어야 한다.");
     }
 
 }
