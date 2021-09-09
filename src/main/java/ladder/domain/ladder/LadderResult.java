01492cc01d7e4c6465b2ad0e8f2d39859fe89282
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-09 17:17:40 +0900

    feat: 없는 유저에 대한 결과 반환시 Exception 기능 구현

--- src/main/java/ladder/domain/ladder/LadderResult.java
+++ src/main/java/ladder/domain/ladder/LadderResult.java
@@ -5,6 +5,8 @@ import ladder.domain.user.User;
 
 public class LadderResult {
 
+    private static final String INVALID_CONTAINS_USER_ERROR_MESSAGE = "입력된 사람의 결과만 확인할 수 있다.";
+
     private final Map<User, String> results;
 
     public LadderResult(Map<User, String> results) {
@@ -12,7 +14,15 @@ public class LadderResult {
     }
 
     public String getResultByUser(User user) {
+        checkContainsUser(user);
+
         return results.get(user);
+    }
+
+    private void checkContainsUser(User user) {
+        if (!results.containsKey(user)) {
+            throw new IllegalArgumentException(INVALID_CONTAINS_USER_ERROR_MESSAGE);
+        }
     }
 
 }
--- src/test/java/ladder/domain/ladder/LadderResultTest.java
+++ src/test/java/ladder/domain/ladder/LadderResultTest.java
@@ -1,32 +1,57 @@
 package ladder.domain.ladder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import java.util.HashMap;
 import java.util.Map;
 import ladder.domain.user.User;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LadderResultTest {
+
+    private static Map<User, String> results;
+
+    private static User pobi;
+    private static User honux;
+
+    @BeforeAll
+    static void beforeAll() {
+        results = new HashMap<>();
+        User pobi = new User("pobi");
+        User honux = new User("honux");
+        results.put(pobi, "꽝");
+        results.put(honux, "5000");
+    }
 
     @Test
     @DisplayName("현재 사람에 대한 결과를 반환할 수 있다.")
     void getNowUserResultTest() {
 
         // given
-        Map<User, String> results = new HashMap<>();
-        User user1 = new User("pobi");
-        User user2 = new User("honux");
-        results.put(user1, "꽝");
-        results.put(user2, "5000");
         LadderResult ladderResult = new LadderResult(results);
 
         // when
-        String result = ladderResult.getResultByUser(user1);
+        String result = ladderResult.getResultByUser(pobi);
 
         // then
         assertThat(result).isEqualTo("꽝");
+    }
+
+    @Test
+    @DisplayName("없는 사람의 결과를 요청받으면 Exception이 발생되어야 한다.")
+    void failByNoneUserTest() {
+
+        // given
+        User user = new User("test");
+        LadderResult ladderResult = new LadderResult(results);
+
+        // when & then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> ladderResult.getResultByUser(user))
+            .withMessageMatching("입력된 사람의 결과만 확인할 수 있다.");
     }
 
 }
