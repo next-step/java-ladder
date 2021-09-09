c826cce5f6a29f4a10dab5a1425e5edb01a77b90
조원진 <>
2021-09-09 23:11:22 +0900

    feat(Ladders) : 플레이어와 플레이 결과를 매개변수로 받아 사다리 게임을 실행하는 로직 추가

--- src/test/java/ladder/domain/LaddersTest.java
+++ src/test/java/ladder/domain/LaddersTest.java
@@ -1,28 +1,65 @@
 package ladder.domain;
 
+import ladder.util.StringUtil;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class LaddersTest {
+
+    private Ladders ladders;
+
+    @BeforeEach
+    void setup() {
+        LadderWidth ladderWidth = new LadderWidth(4);
+        LadderHeight ladderHeight = new LadderHeight(3);
+        ladders = Ladders.initLadders(ladderWidth, ladderHeight, () -> Direction.RIGHT);
+    }
+
     @DisplayName("initLadders 높이 확인")
     @Test
     void initLadders_ReturnInputHeight() {
-        LadderWidth ladderWidth = new LadderWidth(4);
-        LadderHeight ladderHeight = new LadderHeight(3);
-        Ladders ladders = Ladders.initLadders(ladderWidth, ladderHeight, () -> Direction.RIGHT);
-
         assertThat(ladders.getLadderHeight()).isEqualTo(new LadderHeight(3));
     }
 
     @DisplayName("initLadders 가로 너비 확인")
     @Test
     void initLadders_ReturnInputWidth() {
-        LadderWidth ladderWidth = new LadderWidth(4);
-        LadderHeight ladderHeight = new LadderHeight(3);
-        Ladders ladders = Ladders.initLadders(ladderWidth, ladderHeight, () -> Direction.RIGHT);
-
         assertThat(ladders.getLadderWidth()).isEqualTo(new LadderWidth(4));
+    }
+
+    @DisplayName("플레이어 수와 사다리 넓이 길이가 다를 경우 IllegalArgumentException 발생")
+    @Test
+    void play_ThrowsIllegalArgumentException_IfPlayersSizeAndLadderWidthAreDifferent() {
+        Players players = new Players(StringUtil.split("risa, jisu, rose"));
+        PlayResults playResults = new PlayResults(StringUtil.split("꽝, 5000원, 10000원"));
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> ladders.play(players, playResults)
+        );
+    }
+
+    @DisplayName("플레이어 수와 플레이결과 수가 다를 경우 IllegalArgumentException 발생")
+    @Test
+    void play_ThrowsIllegalArgumentException_IfPlayersSizeAndPlayResultsSizeAreDifferent() {
+        Players players = new Players(StringUtil.split("risa, jisu, rose, zeny"));
+        PlayResults playResults = new PlayResults(StringUtil.split("꽝, 5000원, 10000원"));
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> ladders.play(players, playResults)
+        );
+    }
+
+    @DisplayName("사다리게임 플레이")
+    @Test
+    void play() {
+        Players players = new Players(StringUtil.split("risa,jisu,rose,zeny"));
+        PlayResults playResults = new PlayResults(StringUtil.split("꽝,5000원,꽝,10000원"));
+        ladders.play(players, playResults);
+        Player expected = new Player("risa");
+        expected.report(new PlayResult("5000원"));
+
+        assertThat(players.find(new Name("risa"))).isEqualTo(expected);
     }
 }
