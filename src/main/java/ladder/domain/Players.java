9fedf833bb1f51307ddc9978d9c267863c1d6f14
조원진 <>
2021-09-09 23:10:19 +0900

    feat(Players) : Position, Name을 매개변수로 받아 Player를 찾는 find 기능 추가

--- src/main/java/ladder/domain/Players.java
+++ src/main/java/ladder/domain/Players.java
@@ -5,16 +5,29 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Players {
+    private static final int MIN_COUNT = 2;
+
     private final List<Player> players;
 
     public Players(final String[] players) {
-        this(Stream.of(players)
-                .map(Player::new)
-                .collect(Collectors.toList()));
+        this(toPlayers(players));
     }
 
-    private Players(List<Player> players) {
+    private static List<Player> toPlayers(final String[] players) {
+        return Stream.of(players)
+                .map(Player::new)
+                .collect(Collectors.toList());
+    }
+
+    private Players(final List<Player> players) {
+        validateSize(players);
         this.players = players;
+    }
+
+    private void validateSize(final List<Player> players) {
+        if (players.size() < MIN_COUNT) {
+            throw new IllegalArgumentException("최소 인원은 " + MIN_COUNT + " 명 이상입니다.");
+        }
     }
 
     public int size() {
@@ -23,5 +36,19 @@ public class Players {
 
     public List<Player> getPlayers() {
         return players;
+    }
+
+    public Player find(final Position position) {
+        if (position.getValue() >= players.size()) {
+            throw new IllegalArgumentException("Position 값이 플레이어 수보다 큽니다.");
+        }
+        return players.get(position.getValue());
+    }
+
+    public Player find(final Name name) {
+        return players.stream()
+                .filter(player -> player.isSame(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("찾으려는 플레이어가 없습니다. 이름을 확인해주세요."));
     }
 }
