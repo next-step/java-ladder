d110fddaabdb3d06326e292428fa7a83264527a1
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-09 12:54:09 +0900

    refactor

--- src/test/java/ladder/domain/ladder/PointTest.java
+++ src/test/java/ladder/domain/ladder/PointTest.java
@@ -67,7 +67,7 @@ class PointTest {
         }
 
         @Test
-        @DisplayName("[connected = false]")
+        @DisplayName("[prevconnected = false]")
         void falseConnected() {
 
             // given
