3da9c66dce3fd16872dc8bc8af3e00f84e798457
devchang202 <devchang202@gmail.com>
2021-09-10 00:18:28 +0900

    refactor(InputView) : InputView에서는 날 것의 문자열이나 숫자를 입력 받고 이 데이터들을 domain으로 넘겨 처리하는 것으로 변경

--- src/main/java/ladder/LadderGame.java
+++ src/main/java/ladder/LadderGame.java
@@ -2,15 +2,17 @@ package ladder;
 
 import ladder.domain.AutoLineGenerator;
 import ladder.domain.Ladder;
-import ladder.domain.LadderLineGenerator;
 import ladder.domain.Participants;
 import ladder.view.InputView;
 import ladder.view.ResultView;
 
 public class LadderGame {
     public static void main(String[] args) {
-        Participants participants = InputView.inputParticipants();
-        Ladder ladder = InputView.inputLadder(participants.getCountOfParticipants(), AutoLineGenerator.create());
+        String[] participantsNames = InputView.inputParticipants();
+        Participants participants = new Participants(participantsNames);
+
+        int ladderHeight = InputView.inputLadderHeight();
+        Ladder ladder = Ladder.create(participants.getCountOfParticipants(), ladderHeight, AutoLineGenerator.create());
 
         ResultView.printResult(participants.getPaticipants(), ladder.getFloors());
     }
--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -9,14 +9,14 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private static final String GUIDE_INPUT_PARTICIPANTS = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String GUIDE_INPUT_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
     private static final String NAME_DELIMITER = ",";
     private static final Scanner SCANNER = new Scanner(System.in);
-    private static final String GUIDE_INPUT_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
 
-    public static Participants inputParticipants() {
+    public static String[] inputParticipants() {
         System.out.println(GUIDE_INPUT_PARTICIPANTS);
 
-        return new Participants(readNames());
+        return readNames();
     }
 
     private static String[] readNames() {
@@ -25,13 +25,7 @@ public class InputView {
                 .toArray(String[]::new);
     }
 
-    public static Ladder inputLadder(int countOfParticipants, LadderLineGenerator ladderLineGenerator) {
-        int ladderHeight = inputLadderHeight();
-
-        return Ladder.create(countOfParticipants, ladderHeight, ladderLineGenerator);
-    }
-
-    private static int inputLadderHeight() {
+    public static int inputLadderHeight() {
         System.out.println(GUIDE_INPUT_LADDER_HEIGHT);
 
         return getInt();
