36d479ed1c837eadf74c28e5b6771ecd2f0be9ac
jaeyoungjang2 <lion85488@gmail.com>
2021-09-10 17:46:09 +0900

    feat: Directoion 클래스 생성 Direction 초기화 메서드 구현

--- src/main/java/step4/Direction.java
+++ src/main/java/step4/Direction.java
@@ -1,2 +1,35 @@
-package step4;public class Direction {
+package step4;
+
+import java.util.Objects;
+
+public class Direction {
+
+    private final boolean before;
+    private final boolean current;
+
+    public Direction(boolean before, boolean current) {
+        this.before = before;
+        this.current = current;
+    }
+
+    public static Direction of(boolean before, boolean current) {
+        return new Direction(before, current);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Direction direction = (Direction) o;
+        return before == direction.before && current == direction.current;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(before, current);
+    }
 }
