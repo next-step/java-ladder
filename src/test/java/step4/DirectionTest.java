f7c473f60230f22015a2f892b7aaee5c50bcc376
jaeyoungjang2 <lion85488@gmail.com>
2021-09-10 17:48:51 +0900

    test: 양쪽 모두 가지가 존재하는 사다리 예외처리 테스트

--- src/test/java/step4/DirectionTest.java
+++ src/test/java/step4/DirectionTest.java
@@ -1,12 +1,20 @@
 package step4;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class DirectionTest {
     @Test
     public void init() {
-        Assertions.assertThat(Direction.of(true, false)).isEqualTo(Direction.of(true, false));
+        assertThat(Direction.of(true, false)).isEqualTo(Direction.of(true, false));
     }
 
+    @Test
+    public void init_invalid() {
+        assertThatThrownBy(() -> {
+            Direction.of(true, true);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 }
