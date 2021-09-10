496dc2a3ae6ac3c920b04c28b0528267b6a468e7
YuHyeon <klyhyeon@globalorder.co.kr>
2021-09-11 03:29:49 +0900

    feat:요구사항3 구현완료

--- src/main/java/nextstep/optional/Expression.java
+++ src/main/java/nextstep/optional/Expression.java
@@ -16,10 +16,11 @@ enum Expression {
     }
 
     static Expression of(String expression) {
-        Arrays.stream(values())
+        return Arrays.stream(values())
                 .filter(v -> matchExpression(v, expression))
-                .findFirst().orElse(new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다", expression)));
-
-        throw new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression));
+                .findFirst()
+                .orElseThrow(() ->
+                        new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression))
+                );
     }
 }
