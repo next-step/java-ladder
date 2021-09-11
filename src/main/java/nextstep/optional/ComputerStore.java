417c4e48e98dc26e6d65fdbc81af06b96794f943
YuHyeon <klyhyeon@globalorder.co.kr>
2021-09-12 02:02:00 +0900

    refactor: getVersion Optional 리팩토링

--- src/main/java/nextstep/optional/ComputerStore.java
+++ src/main/java/nextstep/optional/ComputerStore.java
@@ -3,21 +3,17 @@ package nextstep.optional;
 import nextstep.optional.Computer.Soundcard;
 import nextstep.optional.Computer.USB;
 
+import java.util.Optional;
+
 public class ComputerStore {
     public static final String UNKNOWN_VERSION = "UNKNOWN";
 
     public static String getVersion(Computer computer) {
-        String version = UNKNOWN_VERSION;
-        if (computer != null) {
-            Soundcard soundcard = computer.getSoundcard();
-            if (soundcard != null) {
-                USB usb = soundcard.getUsb();
-                if (usb != null) {
-                    version = usb.getVersion();
-                }
-            }
-        }
-        return version;
+        return Optional.of(computer)
+                .map(Computer::getSoundcard)
+                .map(Soundcard::getUsb)
+                .map(USB::getVersion)
+                .orElse(UNKNOWN_VERSION);
     }
 
     public static String getVersionOptional(Computer computer) {
