29af8de1a7927476c3251212d0e68f5c8b01a0d9
minyul <ggomjae@gmail.com>
2021-09-12 15:05:42 +0900

    feat : Line 클래스를 추가하다

---
+++ src/main/java/domain/Line.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Line {
+	private List<Boolean> points = new ArrayList<>();
+
+	public Line (int countOfPerson) {
+
+	}
+}
