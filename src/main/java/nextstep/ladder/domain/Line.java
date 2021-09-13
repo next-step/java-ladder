c4d27d57325f3123d9c8219a250e4f030d42efc5
namhyun.sung <namhyun.sung@yanolja.com>
2021-09-13 23:11:40 +0900

    feat: line 입력 validate 추가

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -1,13 +1,19 @@
 package nextstep.ladder.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Line {
-    private List<Boolean> points = new ArrayList<>();
+    private List<Boolean> points;
 
     public Line(List<Boolean> points) {
+        validNotEmptyPoints(points);
         this.points = points;
+    }
+
+    private void validNotEmptyPoints(List<Boolean> points) {
+        if (points == null || points.isEmpty()) {
+            throw new IllegalArgumentException("포인트 목록이 비었습니다.");
+        }
     }
 
     public List<Boolean> getPoints() {
