1dac881f6cf61e23dd7e74656172093fd26acf19
namhyun.sung <namhyun.sung@yanolja.com>
2021-09-13 23:12:44 +0900

    fix: 사다리 생성 및 validate 코드 작성

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -3,10 +3,21 @@ package nextstep.ladder.domain;
 import java.util.List;
 
 public class Ladder {
-    private List<Line> lines;
+    private static final int MIN_LADDER_HEIGHT = 1;
 
-    public Ladder(List<Line> lines) {
-        this.lines = lines;
+    private final List<Line> lines;
+
+    public Ladder(int playerCount, int ladderHeight) {
+        validLadderHeight(ladderHeight);
+        LadderGenerator ladderLinesGenerator = LadderGenerateFactory.findGenerator();
+        lines = ladderLinesGenerator.generate(ladderHeight, playerCount);
+
+    }
+
+    private void validLadderHeight(int ladderHeight) {
+        if (ladderHeight < MIN_LADDER_HEIGHT) {
+            throw new IllegalArgumentException(String.format("사다리 높이는 최소 %d 이어야 합니다.", MIN_LADDER_HEIGHT));
+        }
     }
 
     public List<Line> getLines() {
