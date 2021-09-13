b4e9040e4694723f748ab81a5d3f60720b9b3c0a
namhyun.sung <namhyun.sung@yanolja.com>
2021-09-13 23:12:27 +0900

    test: 사다리 생성 및 validate 테스트 추가

---
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -0,0 +1,30 @@
+package nextstep.ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LadderTest {
+
+    @Test
+    @DisplayName("사다리를 생성한다.")
+    void create_ladder_test() {
+        int ladderHeight = 5;
+        int playerCount = 4;
+        Ladder ladder = new Ladder(playerCount, ladderHeight);
+
+        assertThat(ladder.getLines().size()).isEqualTo(ladderHeight);
+    }
+
+    @Test
+    @DisplayName("사다리 높이가 1보다 작을 경우 예외가 발생한다.")
+    void create_ladder_exception_test() {
+        assertThatThrownBy(() ->
+                new Ladder(3, 0)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+}
