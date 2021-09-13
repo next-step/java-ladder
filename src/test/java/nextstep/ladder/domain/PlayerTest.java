004e12ad4c35d13c00df5716e5b5af1f628b9d60
namhyun.sung <namhyun.sung@yanolja.com>
2021-09-13 23:10:24 +0900

    test : 플레이어 이름 등록 테스트 추가

--- src/test/java/nextstep/ladder/domain/PlayerTest.java
+++ src/test/java/nextstep/ladder/domain/PlayerTest.java
@@ -1,13 +1,25 @@
-package nextstep.ladder;
+package nextstep.ladder.domain;
 
 import nextstep.ladder.domain.Player;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayerTest {
+
+    @Test
+    @DisplayName("플레이어 이름을 등록한다.")
+    void player_add_test() {
+        String name = "next";
+
+        Player player = new Player(name);
+
+        assertThat(player.getName()).isEqualTo(name);
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"asdfgh", "사다리게임중"})
