b00871d5f5ef9454a9a6ca0b66c2bb2373e7dc64
namhyun.sung <namhyun.sung@yanolja.com>
2021-09-13 23:46:23 +0900

    fix: 여러쓰레드에서 사용헀을 때 처리방법이 떠오르지 않아서 다른 방법으로 변경

--- src/main/java/nextstep/ladder/domain/AutoLadderGenerator.java
+++ src/main/java/nextstep/ladder/domain/AutoLadderGenerator.java
@@ -1,29 +1,43 @@
 package nextstep.ladder.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class AutoLadderGenerator implements LadderGenerator {
     private static int FIRST_INDEX = 0;
-    private static boolean postLine = false;
+    private static final int LAST_POINT_COUNT = 1;
     private static final Random random = new Random();
 
     public List<Line> generate(int height, int width) {
         return IntStream.range(FIRST_INDEX, height)
-                .mapToObj(n -> new Line(
-                                Stream
-                                        .generate(this::determine)
-                                        .limit(width - 1)
-                                        .collect(Collectors.toList())
-                        )
+                .mapToObj(n ->
+                        new Line(generateRandomPointValues(width - 1))
                 ).collect(Collectors.toList());
     }
 
-    private boolean determine() {
-        postLine = postLine ? false : random.nextBoolean();
-        return postLine;
+    private static List<Boolean> generateRandomPointValues(int pointValueCount) {
+        Boolean firstPoint = random.nextBoolean();
+        List<Boolean> points = new ArrayList<>(Collections.singletonList(firstPoint));
+
+        int pointValueCountExcludingLastPointValue = pointValueCount - LAST_POINT_COUNT;
+        IntStream.range(FIRST_INDEX, pointValueCountExcludingLastPointValue)
+                .forEach(index -> {
+                    Boolean previousPoint = points.get(index);
+                    points.add(generateRandomPointValue(previousPoint));
+                });
+
+        return points;
     }
+
+    private static Boolean generateRandomPointValue(Boolean previousPoint) {
+        if (previousPoint == true) {
+            return false;
+        }
+        return random.nextBoolean();
+    }
+
 }
