036b05fa4bd0af9adef396b82c8923497d0623b7
namhyun.sung <namhyun.sung@yanolja.com>
2021-09-13 23:11:03 +0900

    refactor: validate 메소드 명 변경

--- src/main/java/nextstep/ladder/domain/Player.java
+++ src/main/java/nextstep/ladder/domain/Player.java
@@ -4,11 +4,11 @@ public class Player {
     private String name;
 
     public Player(String name) {
-        valid(name);
+        validNameLength(name);
         this.name = name;
     }
 
-    private void valid(String name) {
+    private void validNameLength(String name) {
         if(name.length() > 5) {
             throw new IllegalArgumentException("이름은 5글자르 넘을 수 없습니다.");
         }
