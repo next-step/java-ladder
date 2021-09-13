ffd7f680b7ecb99464cdbd1295c32749ae42fb3d
longbeom <wowow1859@naver.com>
2021-09-13 19:39:55 +0900

    chore : 예외 메시지 상수 선언

--- src/main/java/nextstep/ladder/domain/Name.java
+++ src/main/java/nextstep/ladder/domain/Name.java
@@ -4,6 +4,9 @@ import nextstep.ladder.exception.NameLengthException;
 
 public class Name {
 	private static final int MAX_LENGTH = 5;
+	private static final String NAME_MAX_LENGTH_EXCEPTION_MESSAGE = "이름은 최대 5글자 입니다. (이름: %s)";
+	private static final String NAME_MIN_LENGTH_EXCEPTION_MESSAGE = "이름은 최소 1글자 입니다. (이름: %s)";
+
 	private final String name;
 
 	public Name(String name) {
@@ -13,10 +16,10 @@ public class Name {
 
 	private void checkLength(String name) {
 		if (name.length() > MAX_LENGTH) {
-			throw new NameLengthException(String.format("이름은 최대 5글자 입니다. (이름: %s)", name));
+			throw new NameLengthException(String.format(NAME_MAX_LENGTH_EXCEPTION_MESSAGE, name));
 		}
 		if (name.isEmpty()) {
-			throw new NameLengthException(String.format("이름은 최소 1글자 이상이어야 합니다. (이름: %s)", name));
+			throw new NameLengthException(String.format(NAME_MIN_LENGTH_EXCEPTION_MESSAGE, name));
 		}
 	}
 }
