19eed70396ab2cc0f3cf1e25b85d0ef6b71c57c0
longbeom <wowow1859@naver.com>
2021-09-13 19:43:26 +0900

    feat : 사다리 높이 입력

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 
 public class InputView {
 	private static final String INPUT_NAMES = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+	private static final String INPUT_LADDER_HEIGHT = "최대 사다리의 높이는 몇 개z인가요?";
 	private static final Scanner SCANNER = new Scanner(System.in);
 
 	public String inputNames() {
 		System.out.println(INPUT_NAMES);
 		return SCANNER.nextLine();
+	}
+
+	public int inputLadderHeight() {
+		System.out.println(INPUT_LADDER_HEIGHT);
+		return SCANNER.nextInt();
 	}
 }
