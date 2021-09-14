5c6fe1bae861637f21befed815a6ed20422964a4
namhyun.sung <namhyun.sung@yanolja.com>
2021-09-15 00:00:42 +0900

    feat: 결과 값 저장 및 예외처리 구현

---
+++ src/main/java/nextstep/ladder/domain/LadderResults.java
@@ -0,0 +1,32 @@
+package nextstep.ladder.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LadderResults {
+
+    private List<LadderResult> ladderResults;
+
+    public LadderResults(String resultsString) {
+        this.ladderResults = init(resultsString);
+    }
+
+    private List<LadderResult> init(String resultsString) {
+        validResult(resultsString);
+        String[] resultsArray = resultsString.split(",");
+        return Arrays.stream(resultsArray)
+                .map(player -> new LadderResult(player))
+                .collect(Collectors.toList());
+    }
+
+    private void validResult(String resultsString) {
+        if(resultsString == null) {
+            throw new IllegalArgumentException("입력한 결과가 비어있습니다.");
+        }
+    }
+
+    public int countOfLadderResults() {
+        return ladderResults.size();
+    }
+}
