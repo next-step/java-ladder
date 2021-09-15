04090c0bab4afa2c1736f10c945a429b446a6870
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-15 17:01:24 +0900

    refactor: 메소드 분리

--- src/main/java/step4/domain/ladder/LadderLine.java
+++ src/main/java/step4/domain/ladder/LadderLine.java
@@ -20,15 +20,29 @@ public class LadderLine {
 
     public static LadderLine createLine(Users users, DirectionGenerateStrategy strategy) {
         List<Point> points = new ArrayList<>();
+        Point point = createFirst(strategy, points);
+        point = createBody(users, strategy, points, point);
+        createLast(points, point);
+        return new LadderLine(points);
+    }
+
+    private static Point createFirst(DirectionGenerateStrategy strategy, List<Point> points) {
         Point point = Point.first(strategy);
         points.add(point);
+        return point;
+    }
+
+    private static Point createBody(Users users, DirectionGenerateStrategy strategy, List<Point> points, Point point) {
         for (int i=1; i < users.usersCount() - 1; i++){
             point = point.next(strategy);
             points.add(point);
         }
+        return point;
+    }
+
+    private static void createLast(List<Point> points, Point point) {
         point = point.last();
         points.add(point);
-        return new LadderLine(points);
     }
 
     @Override
