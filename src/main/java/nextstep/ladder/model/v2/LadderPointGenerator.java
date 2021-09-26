e03fc93d3aeda1b5637563588c5160d2d4807363
sangw0804 <sangw0804@likelion.org>
2021-09-26 20:16:21 +0900

    feat: LateralLadder 구현

--- src/main/java/nextstep/ladder/model/v2/LadderPointGenerator.java
+++ src/main/java/nextstep/ladder/model/v2/LadderPointGenerator.java
@@ -1,11 +1,5 @@
 package nextstep.ladder.model.v2;
 
-import java.util.Random;
-
-public class LadderPointGenerator {
-    private static final Random random = new Random();
-
-    public static boolean generatePoint() {
-        return random.nextBoolean();
-    }
+public interface LadderPointGenerator {
+    boolean generatePoint();
 }
