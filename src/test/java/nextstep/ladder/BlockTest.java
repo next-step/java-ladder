b99ba191b5dc8998a2f783df472adf105f0b9c45
HueWoon Kim <kho0307@gmail.com>
2021-09-28 01:17:11 +0900

    test: 블록 생성, 이동 테스트 및 라인 생성 및 알맞은 좌표 테스트 추가
    - 블록 생성 시나리오 테스트, 블록 이동 테스트 보강 및 추가
    - 올바른 라인 생성 확인 테스트, 라인 실행 테스트 추가

--- src/test/java/nextstep/ladder/BlockTest.java
+++ src/test/java/nextstep/ladder/BlockTest.java
@@ -13,12 +13,80 @@ import nextstep.ladder.domain.Position;
 public class BlockTest {
 
 	@Test
-	@DisplayName("블록 생성")
-	void test() {
+	@DisplayName("오른쪽 방향성 블록 시작")
+	void right() {
 		Block first = new Block(Direction.RIGHT, new Position(1));
 
 		Block second = first.makeNext(Direction.LEFT);
 
 		assertThat(second.direction()).isEqualTo(Direction.LEFT);
+	}
+
+	@Test
+	@DisplayName("정지 방향성 블록 시작")
+	void stop() {
+		Block first = new Block(Direction.NOTHING, new Position(1));
+
+		Block second = first.makeNext(Direction.NOTHING);
+
+		assertThat(second.direction()).isEqualTo(Direction.NOTHING);
+
+		Block third = second.makeNext(Direction.RIGHT);
+
+		assertThat(third.direction()).isEqualTo(Direction.RIGHT);
+	}
+
+	@Test
+	@DisplayName("왼쪽 방향성 블록 시작")
+	void left() {
+		Block first = new Block(Direction.LEFT, new Position(2));
+
+		Block second = first.makeNext(Direction.RIGHT);
+
+		assertThat(second.direction()).isEqualTo(Direction.RIGHT);
+
+		Block third = second.makeNext(Direction.LEFT);
+
+		assertThat(third.direction()).isEqualTo(Direction.LEFT);
+
+		Block forth = third.makeNext(Direction.NOTHING);
+
+		assertThat(forth.direction()).isEqualTo(Direction.NOTHING);
+	}
+
+	@Test
+	@DisplayName("오른쪽 이동")
+	void rightMove() {
+		Block first = new Block(Direction.RIGHT, new Position(1));
+
+		Participant participant = new Participant("test", new Position(1));
+
+		first.enter(participant);
+
+		assertThat(participant.position()).isEqualTo(new Position(2));
+	}
+
+	@Test
+	@DisplayName("왼쪽 이동")
+	void leftMove() {
+		Block first = new Block(Direction.LEFT, new Position(1));
+
+		Participant participant = new Participant("test", new Position(2));
+
+		first.enter(participant);
+
+		assertThat(participant.position()).isEqualTo(new Position(1));
+	}
+
+	@Test
+	@DisplayName("정지")
+	void notMove() {
+		Block first = new Block(Direction.NOTHING, new Position(1));
+
+		Participant participant = new Participant("test", new Position(1));
+
+		first.enter(participant);
+
+		assertThat(participant.position()).isEqualTo(new Position(1));
 	}
 }
--- src/test/java/nextstep/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -5,7 +5,11 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import nextstep.ladder.domain.Block;
+import nextstep.ladder.domain.Direction;
 import nextstep.ladder.domain.Line;
+import nextstep.ladder.domain.Participant;
+import nextstep.ladder.domain.Position;
 
 public class LineTest {
 
@@ -19,5 +23,35 @@ public class LineTest {
 		assertThatThrownBy(() -> {
 			new Line(1);
 		}).isExactlyInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	@DisplayName("라인에서 좌표에 해당하는 블록에 적용 받음")
+	void test2() {
+		Line line = new Line(4);
+		int testPosition = 2;
+		Participant participant = new Participant("test", new Position(testPosition));
+		line.play(participant);
+
+		Participant participant2 = new Participant("test2", new Position(testPosition));
+		line.play(participant2);
+
+		assertThat(participant.position()).isEqualTo(participant2.position());
+	}
+
+	@Test
+	@DisplayName("오른쪽일때 왼쪽 블랙 생성, 왼쪽 or 정지일때 오른쪽 or 정지 생성")
+	void test3() {
+		Line line = new Line(5);
+		Direction prev = Direction.NOTHING;
+		for (Block block : line.blocks()) {
+			if(prev == Direction.RIGHT) {
+				assertThat(block.direction()).isEqualTo(Direction.LEFT);
+				prev = block.direction();
+				continue;
+			}
+			assertThat(block.direction()).isIn(Direction.RIGHT, Direction.NOTHING);
+			prev = block.direction();
+		}
 	}
 }
