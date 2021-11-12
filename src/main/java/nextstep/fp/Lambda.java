19c85d3b401d349a529d63e614fa86bfd0f532d6
KimMinjeong <mor2222@naver.com>
2021-11-12 17:08:42 +0900

    1단계 - 자바8 스트림, 람다, Optional (#1197)
    * feat: lambda 실습.
    
    * feat: stream 실습.
    
    * feat: optional exception 처리 실습.

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
@@ -18,36 +19,25 @@ public class Lambda {
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> System.out.println("Hello from thread")).start();
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAllByCondition(numbers, (number) -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAllByCondition(numbers, (number) -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
+        return sumAllByCondition(numbers, (number) -> number % 3 == 0);
+    }
+
+    public static int sumAllByCondition(List<Integer> numbers, Function<Integer, Boolean> condition) {
         int total = 0;
         for (int number : numbers) {
-            if (number > 3) {
+            if (condition.apply(number)) {
                 total += number;
             }
         }
