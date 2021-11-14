aad1efe2fbd2f8fac80c6a61fe3babd7a89aa265
csh0034 <csh0034@gmail.com>
2021-11-14 13:09:11 +0900

    feat: stream

--- src/test/java/nextstep/fp/StreamStudyTest.java
+++ src/test/java/nextstep/fp/StreamStudyTest.java
@@ -1,48 +1,48 @@
 package nextstep.fp;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-public class StreamStudyTest {
-    private List<Integer> numbers;
+class StreamStudyTest {
+	private List<Integer> numbers;
 
-    @BeforeEach
-    public void setup() {
-        numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-    }
+	@BeforeEach
+	void setup() {
+		numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+	}
 
-    @Test
-    public void countWords() throws Exception {
-        long result = StreamStudy.countWords();
-        System.out.println("result : " + result);
-    }
+	@Test
+	void countWords() throws Exception {
+		long result = StreamStudy.countWords();
+		System.out.println("result : " + result);
+	}
 
-    @Test
-    public void printLongestWordTop100() throws Exception {
-        StreamStudy.printLongestWordTop100();
-    }
+	@Test
+	void printLongestWordTop100() throws Exception {
+		StreamStudy.printLongestWordTop100();
+	}
 
-    @Test
-    public void map() throws Exception {
-        List<Integer> doubleNumbers = StreamStudy.doubleNumbers(numbers);
-        doubleNumbers.forEach(System.out::println);
-    }
+	@Test
+	public void map() {
+		List<Integer> doubleNumbers = StreamStudy.doubleNumbers(numbers);
+		doubleNumbers.forEach(System.out::println);
+	}
 
-    @Test
-    public void sumAll() throws Exception {
-        long sum = StreamStudy.sumAll(numbers);
-        assertThat(sum).isEqualTo(21);
-    }
+	@Test
+	void sumAll() {
+		long sum = StreamStudy.sumAll(numbers);
+		assertThat(sum).isEqualTo(21);
+	}
 
-    @Test
-    public void sumOverThreeAndDouble() throws Exception {
-        numbers = Arrays.asList(3, 1, 6, 2, 4, 8);
-        long sum = StreamStudy.sumOverThreeAndDouble(numbers);
-        assertThat(sum).isEqualTo(36);
-    }
+	@Test
+	void sumOverThreeAndDouble() {
+		numbers = Arrays.asList(3, 1, 6, 2, 4, 8);
+		long sum = StreamStudy.sumOverThreeAndDouble(numbers);
+		assertThat(sum).isEqualTo(36);
+	}
 }
