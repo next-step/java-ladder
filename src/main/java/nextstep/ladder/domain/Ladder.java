399f703616f92a5547998e24559c0bec7d901177
csh0034 <csh0034@gmail.com>
2021-11-17 03:06:17 +0900

    feat: Ladder 검증 추가

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -6,12 +6,42 @@ import java.util.List;
 import java.util.Objects;
 
 import nextstep.ladder.generator.PointGenerator;
+import nextstep.ladder.util.CollectionUtils;
 
 public class Ladder {
+	private static final String EMPTY_MESSAGE = "Point 리스트가 비어있습니다.";
+	private static final String OVERLAP_MESSAGE = "사다리 라인이 겹쳐지면 안됩니다.";
+
 	private final List<Point> points;
 
 	private Ladder(List<Point> points) {
+		validate(points);
 		this.points = new ArrayList<>(points);
+	}
+
+	private void validate(List<Point> points) {
+		validateSize(points);
+		validateOverlap(points);
+	}
+
+	private void validateSize(List<Point> points) {
+		if (CollectionUtils.isEmpty(points)) {
+			throw new IllegalArgumentException(EMPTY_MESSAGE);
+		}
+	}
+
+	private void validateOverlap(List<Point> points) {
+		for (int i = 0; i < points.size() - 1; i++) {
+			comparePoint(points, i);
+		}
+	}
+
+	private void comparePoint(List<Point> points, int idx) {
+		Point current = points.get(idx);
+		Point next = points.get(idx + 1);
+		if (current.hasLine() && next.hasLine()) {
+			throw new IllegalArgumentException(OVERLAP_MESSAGE);
+		}
 	}
 
 	public static Ladder create(List<Point> points) {
--- src/test/java/nextstep/ladder/domain/LadderTest.java
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -1,9 +1,19 @@
 package nextstep.ladder.domain;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import nextstep.ladder.generator.FixedPointGenerator;
 import nextstep.ladder.generator.PointGenerator;
@@ -22,5 +32,45 @@ class LadderTest {
 
 		// then
 		assertThat(ladder.values().size()).isEqualTo(width * height);
+	}
+
+	@DisplayName("point list 가 비어있을 경우 예외 발생")
+	@Test
+	void validateSize() {
+		// given
+		List<Point> emptyPoints = Collections.emptyList();
+
+		// when then
+		assertAll(
+			() -> assertThatIllegalArgumentException()
+				.isThrownBy(() -> Ladder.create(null)),
+			() -> assertThatIllegalArgumentException()
+				.isThrownBy(() -> Ladder.create(emptyPoints))
+		);
+	}
+
+	@DisplayName("사다리의 라인이 겹쳐져 있으면 예외 발생")
+	@ParameterizedTest(name = "{index}. points : {0}")
+	@MethodSource("providePoints")
+	void validateOverlap(List<Point> points) {
+		// when then
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> Ladder.create(points));
+	}
+
+	private static Stream<Arguments> providePoints() {
+		return Stream.of(
+			arguments(createPoints(false, false, true, true)),
+			arguments(createPoints(false, true, true, false)),
+			arguments(createPoints(true, true, false, false))
+		);
+	}
+
+	private static List<Point> createPoints(boolean... hasLines) {
+		List<Point> points = new ArrayList<>();
+		for (boolean hasLine : hasLines) {
+			points.add(Point.create(hasLine));
+		}
+		return points;
 	}
 }
