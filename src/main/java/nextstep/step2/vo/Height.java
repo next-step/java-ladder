271b11c6187ab3abe38b485b35fc8f1689fe9243
marty.ni <marty.ni@kakaocorp.com>
2021-11-17 23:01:18 +0900

    Feat : Height 구현

--- src/main/java/nextstep/step2/vo/Height.java
+++ src/main/java/nextstep/step2/vo/Height.java
@@ -1,2 +1,28 @@
-package nextstep.step2.vo;public class Height {
+package nextstep.step2.vo;
+
+public class Height {
+
+    private static final int MIN_HEIGHT = 1;
+    private static final String NULL_OR_LESS_HEIGHT_MESSAGE = "높이 값은 1 이상 이어야 합니다.";
+
+    private final int value;
+
+    private Height(int value) {
+        this.value = value;
+    }
+
+    public static Height createWithString(String input) {
+        if(input == null || input.isEmpty()) {
+            throw new IllegalArgumentException(NULL_OR_LESS_HEIGHT_MESSAGE);
+        }
+        return create(Integer.parseInt(input));
+    }
+
+    public static Height create(int value) {
+        if(value < MIN_HEIGHT) {
+            throw new IllegalArgumentException(NULL_OR_LESS_HEIGHT_MESSAGE);
+        }
+
+        return new Height(value);
+    }
 }
--- src/test/java/nextstep/step2/vo/HeightTest.java
+++ src/test/java/nextstep/step2/vo/HeightTest.java
@@ -1,4 +1,27 @@
+package nextstep.step2.vo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.*;
+
 class HeightTest {
-  
+
+    @DisplayName("value 의 크기가 1 보다 작을 경우 illegal exception")
+    @Test
+    void lessThanOneTest() {
+        assertThatIllegalArgumentException().isThrownBy(() -> Height.create(0));
+    }
+
+    @DisplayName("Null or Empty String input illegal exception")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void nullOrEmptyTest(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> Height.createWithString(input));
+    }
 }
