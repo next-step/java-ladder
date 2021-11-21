846f3d43aa90a882b118f210682a557c6b0bf025
csh0034 <csh0034@gmail.com>
2021-11-21 22:18:22 +0900

    chore: 오타 수정

--- src/main/java/nextstep/ladder/dto/LadderResultDto.java
+++ src/main/java/nextstep/ladder/dto/LadderResultDto.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import nextstep.ladder.view.ResultView;
 
 public class LadderResultDto {
-	private static final String LAST_LINE_PEED = "\\s$";
+	private static final String LAST_LINE_FEED = "\\s$";
 	private static final String EMPTY = "";
 
 	private final Map<String, String> resultMap;
@@ -24,7 +24,7 @@ public class LadderResultDto {
 			builder.append(value);
 			builder.append(System.lineSeparator());
 		});
-		return builder.toString().replaceAll(LAST_LINE_PEED, EMPTY);
+		return builder.toString().replaceAll(LAST_LINE_FEED, EMPTY);
 	}
 
 	public String getResult(String key) {
