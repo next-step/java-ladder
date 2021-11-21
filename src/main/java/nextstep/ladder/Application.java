8ff983122f994abd0cc768801885396e159d5ed1
csh0034 <csh0034@gmail.com>
2021-11-22 00:39:07 +0900

    chore: Application import 변경, RandomLadderGenerator 파일명 변경

--- src/main/java/nextstep/ladder/Application.java
+++ src/main/java/nextstep/ladder/Application.java
@@ -1,13 +1,13 @@
 package nextstep.ladder;
 
 import nextstep.ladder.controller.LadderController;
-import nextstep.ladder.generator.RandomGenerator;
-import nextstep.ladder.generator.RandomLadderGenerator;
 import nextstep.ladder.converter.ResultConverter;
+import nextstep.ladder.generator.RandomGenerator;
+import nextstep.ladder.generator.RandomBooleanGenerator;
 
 public class Application {
 	public static void main(String[] args) {
-		RandomGenerator generator = new RandomLadderGenerator();
+		RandomGenerator generator = new RandomBooleanGenerator();
 		ResultConverter resultConverter = new ResultConverter();
 		LadderController ladderController = new LadderController(generator, resultConverter);
 		ladderController.start();
--- src/main/java/nextstep/ladder/generator/RandomBooleanGenerator.java
+++ src/main/java/nextstep/ladder/generator/RandomBooleanGenerator.java
@@ -2,7 +2,7 @@ package nextstep.ladder.generator;
 
 import java.util.Random;
 
-public class RandomLadderGenerator implements RandomGenerator {
+public class RandomBooleanGenerator implements RandomGenerator {
 	private static final Random RANDOM = new Random();
 
 	@Override
