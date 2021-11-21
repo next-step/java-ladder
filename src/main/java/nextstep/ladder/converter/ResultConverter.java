bbe027ee0fd1c5a0a96c1a645ac652e27b939051
csh0034 <csh0034@gmail.com>
2021-11-22 00:50:12 +0900

    refactor: ResultConverter 이동부분 메서드 분리

--- src/main/java/nextstep/ladder/converter/ResultConverter.java
+++ src/main/java/nextstep/ladder/converter/ResultConverter.java
@@ -2,10 +2,12 @@ package nextstep.ladder.converter;
 
 import static java.util.stream.Collectors.*;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import nextstep.ladder.domain.ExecutionResult;
 import nextstep.ladder.domain.Ladder;
+import nextstep.ladder.domain.Line;
 import nextstep.ladder.domain.Participant;
 import nextstep.ladder.domain.Participants;
 import nextstep.ladder.dto.LadderResultDto;
@@ -14,9 +16,14 @@ public class ResultConverter {
 	public LadderResultDto convert(Ladder ladder, Participants participants, ExecutionResult executionResult) {
 		return participants.getValues().stream()
 			.collect(collectingAndThen(toMap(Participant::toString, participant -> {
-				AtomicInteger index = new AtomicInteger(participants.getIndex(participant));
-				ladder.getLines().forEach(line -> index.set(line.move(index.get())));
+				AtomicInteger index = getMovedLastIndex(ladder.getLines(), participants.getIndex(participant));
 				return executionResult.getResult(index.get());
 			}), LadderResultDto::new));
+	}
+
+	private AtomicInteger getMovedLastIndex(List<Line> lines, int startIndex) {
+		AtomicInteger index = new AtomicInteger(startIndex);
+		lines.forEach(line -> index.set(line.move(index.get())));
+		return index;
 	}
 }
