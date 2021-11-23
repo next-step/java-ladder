88c736f3edab88d26c57e7b8af8106b41b6d6a48
marty.ni <marty.ni@kakaocorp.com>
2021-11-24 00:03:43 +0900

    Refactor : Lines Test

---
+++ src/test/java/nextstep/step2/vo/LinesTest.java
@@ -0,0 +1,21 @@
+package nextstep.step2.vo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class LinesTest {
+
+    @DisplayName("Null or Empty input illegal exception")
+    @Test
+    void nullOrEmptyTest() {
+        assertThatIllegalArgumentException().isThrownBy(() -> Lines.create(null));
+        assertThatIllegalArgumentException().isThrownBy(() -> Lines.create(Collections.emptyList()));
+    }
+
+}
