c6d0e14ac267d9055736597bd124aeab279934f1
csh0034 <csh0034@gmail.com>
2021-11-23 21:14:44 +0900

    chore: import 정리

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 import nextstep.ladder.generator.Generator;
