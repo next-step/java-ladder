62a3ed0a0c63d9a4ff8efb02c0506ea0625f476d
marty.ni <marty.ni@kakaocorp.com>
2021-11-24 00:13:30 +0900

    Refactor : Lines Test 추가

--- src/test/java/nextstep/step2/vo/LinesTest.java
+++ src/test/java/nextstep/step2/vo/LinesTest.java
@@ -1,10 +1,13 @@
 package nextstep.step2.vo;
 
+import nextstep.step2.domain.Line;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class LinesTest {
@@ -15,5 +18,13 @@ class LinesTest {
         assertThatIllegalArgumentException().isThrownBy(() -> Lines.create(null));
         assertThatIllegalArgumentException().isThrownBy(() -> Lines.create(Collections.emptyList()));
     }
+
+    @DisplayName("정상 생성 테스트")
+    @Test
+    void createTEst() {
+        assertThat(Lines.create(Arrays.asList(Line.createWithEndLine(2,()->true))))
+                .isEqualTo(Lines.create(Arrays.asList(Line.createWithEndLine(2,()->true))));
+    }
+
 
 }
