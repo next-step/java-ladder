3d04b72e9aedff061c16f894b2969d1019fd35db
marty.ni <marty.ni@kakaocorp.com>
2021-11-24 00:00:15 +0900

    Refactor : Name test 추가

--- src/test/java/nextstep/step2/vo/NameTest.java
+++ src/test/java/nextstep/step2/vo/NameTest.java
@@ -1,10 +1,12 @@
 package nextstep.step2.vo;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class NameTest {
@@ -21,6 +23,18 @@ class NameTest {
     @ValueSource(strings = {"abcdef"})
     void rangeTest(String input) {
         assertThatIllegalArgumentException().isThrownBy(() -> Name.create(input));
+    }
+
+    @DisplayName("정상 생성 테스트")
+    @Test
+    void createTest() {
+        assertThat(Name.create("miz")).isEqualTo(Name.create("miz"));
+    }
+
+    @DisplayName("length()는 이름의 길이를 반환한다.")
+    @Test
+    void lengthTest() {
+        assertThat(Name.create("miz").length()).isEqualTo(3);
     }
 
 }
