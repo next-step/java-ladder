0eb48f89597aa2495c991b0dc2d19cf445e9dd00
ydh6226 <ydh6226@naver.com>
2021-11-24 23:00:25 +0900

    feat: Positive 생성

---
+++ src/main/java/nextstep/ladder/domain/Positive.java
@@ -0,0 +1,35 @@
+package nextstep.ladder.domain;
+
+import java.util.Objects;
+
+public class Positive {
+
+    public static final String NOT_POSITIVE_ERROR_MESSAGE = "양수가 아닙니다.";
+    private static final int MIN_VALUE = 1;
+
+    private final int value;
+
+    public Positive(int value) {
+        checkPositive(value);
+        this.value = value;
+    }
+
+    private void checkPositive(int value) {
+        if (value < MIN_VALUE) {
+            throw new IllegalArgumentException(NOT_POSITIVE_ERROR_MESSAGE);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Positive positive = (Positive) o;
+        return value == positive.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
