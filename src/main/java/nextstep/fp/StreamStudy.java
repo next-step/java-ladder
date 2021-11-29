9ea5362a95eb66aab300476991f5a31ed45a2a98
ChangmoKang <dkssudckdah@gmail.com>
2021-11-29 18:51:22 +0900

    feat: map, reduce, filter 실습 2

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
@@ -28,6 +29,17 @@ public class StreamStudy {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         // TODO 이 부분에 구현한다.
+        final List<String> newWords = words.stream()
+                .filter(word -> word.length() > 12) // 단어의 길이가 12자를 초과하는 단어를 추출한다.
+                .distinct() // 단어 중복을 허용하지 않는다
+                .map(word -> word.toLowerCase()) // 모든 단어는 소문자로 출력해야 한다.
+                .sorted((a, b) -> b.length() - a.length()) // 길이가 긴 순서로 단어를 추출한다.
+                .limit(100) // 100개의 단어를 추출한다.
+                .collect(Collectors.toList());
+
+        for (String newWord : newWords) {
+            System.out.println("newWord = " + newWord);
+        }
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
