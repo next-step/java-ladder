4e24b5879f1864a57ceedf0d81c6c206edfe5661
ChangmoKang <dkssudckdah@gmail.com>
2021-11-29 20:17:29 +0900

    fix: Comparator.comparing 사용

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
@@ -33,7 +34,7 @@ public class StreamStudy {
                 .filter(word -> word.length() > 12) // 단어의 길이가 12자를 초과하는 단어를 추출한다.
                 .distinct() // 단어 중복을 허용하지 않는다
                 .map(String::toLowerCase) // 모든 단어는 소문자로 출력해야 한다.
-                .sorted((a, b) -> b.length() - a.length()) // 길이가 긴 순서로 단어를 추출한다.
+                .sorted(Comparator.comparing(String::length).reversed()) // 길이가 긴 순서로 단어를 추출한다.
                 .limit(100) // 100개의 단어를 추출한다.
                 .collect(Collectors.toList());
 
