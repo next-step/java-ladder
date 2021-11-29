24ad6a7d8cd10060e7d4aa23c9521a9ebfc4a886
kangyonghyun <hmoon826@gmail.com>
2021-11-29 14:58:33 +0900

    (refactor) stream - 스트림 메서드 실습

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -4,8 +4,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
@@ -27,7 +26,12 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(s -> s.length() > 12)
+                .sorted(Comparator.comparing(String::length).reversed())
+                .distinct()
+                .limit(100)
+                .forEach(s -> System.out.println(s.toLowerCase()));
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +43,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(n -> n > 3)
+                .mapToLong(n -> 2L * n)
+                .reduce(0, Long::sum);
     }
 }
