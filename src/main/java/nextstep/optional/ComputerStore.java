4f5a33c7481b6fe0f7bb443e1e4fc006a478cb47
joy <joy.y@kakaocorp.com>
2021-12-01 04:51:36 +0900

    1단계 - 자바8 스트림, 람다, Optional

--- src/main/java/nextstep/optional/ComputerStore.java
+++ src/main/java/nextstep/optional/ComputerStore.java
@@ -3,6 +3,8 @@ package nextstep.optional;
 import nextstep.optional.Computer.Soundcard;
 import nextstep.optional.Computer.USB;
 
+import java.util.Optional;
+
 public class ComputerStore {
     public static final String UNKNOWN_VERSION = "UNKNOWN";
 
@@ -21,6 +23,12 @@ public class ComputerStore {
     }
 
     public static String getVersionOptional(Computer computer) {
-        return null;
+        return Optional.ofNullable(computer)
+                .filter(c -> Optional.ofNullable(c.getSoundcard()).isPresent())
+                .map(Computer::getSoundcard)
+                .filter(s -> Optional.ofNullable(s.getUsb()).isPresent())
+                .map(Soundcard::getUsb)
+                .map(USB::getVersion)
+                .orElse(UNKNOWN_VERSION);
     }
 }
