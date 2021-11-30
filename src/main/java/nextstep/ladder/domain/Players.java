66f43a02e56887ecb570c524f388d258912b6be9
hiro <kiryuinh32@gmail.com>
2021-11-30 23:56:36 +0900

    feat: List<Player>를 가진 Players 구현

---
+++ src/main/java/nextstep/ladder/domain/Players.java
@@ -0,0 +1,31 @@
+package nextstep.ladder.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Players {
+    private static final int MIN_PLAYER_COUNT = 1;
+
+    private final List<Player> players;
+
+    private Players(List<Player> players) {
+        validatePlayersCount(players);
+        this.players = players;
+    }
+
+    private void validatePlayersCount(List<Player> players) {
+        if (Objects.isNull(players) || players.size() < MIN_PLAYER_COUNT) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static Players from(String[] names) {
+        List<Player> players = Arrays.stream(names)
+                .map(Player::from)
+                .collect(Collectors.toList());
+
+        return new Players(players);
+    }
+}
