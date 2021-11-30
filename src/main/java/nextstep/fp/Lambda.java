e58b659f4f944a64033c208da5c84ad0fa46b2f8
Jeonghee Lee <jjoylee@users.noreply.github.com>
2021-11-30 09:49:41 +0900

    [1단계] 자바8 스트림, 람다, Optional (#1219)
    * feat : 람다 실습 1 - 익명 클래스를 람다로 전환
    
    * feat : 람다 실습 2 - 람다를 활용해 중복 제거
    
    * feat : map, reduce, filter 실습 1
    
    * feat : map, reduce, filter 실습 2
    
    * feat : 요구사항 1 - Optional을 활용해 조건에 따른 반환
    
    * feat : 요구사항 2 - Optional에서 값을 반환
    
    * feat : 요구사항 3 - Optional에서 exception 처리

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
         System.out.println("printAllOld");
-
         for (int number : numbers) {
             System.out.println(number);
         }
@@ -13,7 +12,6 @@ public class Lambda {
 
     public static void printAllLambda(List<Integer> numbers) {
         System.out.println("printAllLambda");
-
         numbers.forEach(System.out::println);
     }
 
@@ -26,31 +24,10 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
-    }
-
-    public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
-    }
-
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+    public static int sumAll(List<Integer> numbers, Conditional c) {
+        return numbers.stream()
+                .filter(c::test)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 }
