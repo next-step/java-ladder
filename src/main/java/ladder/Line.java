3048c5f6033af6eab30e4860371ce35a23ee7260
ChangmoKang <dkssudckdah@gmail.com>
2021-12-01 15:35:24 +0900

    refactor: method 기능 분리

--- src/main/java/ladder/Line.java
+++ src/main/java/ladder/Line.java
@@ -8,14 +8,14 @@ import java.util.stream.Collectors;
 
 /**
  * Line 의 List<Boolean> field 의 의미는 아래와 같습니다.
- *      [false, false, false] 인 경우
- *     "      |      |      |"
- *
- *      [false, true, false] 인 경우
- *     "      |-----|      |"
- *
- *      [false, true, false, true] 인 경우
- *     "      |-----|      |-----|"
+ * [false, false, false] 인 경우
+ * "      |      |      |"
+ * <p>
+ * [false, true, false] 인 경우
+ * "      |-----|      |"
+ * <p>
+ * [false, true, false, true] 인 경우
+ * "      |-----|      |-----|"
  */
 public class Line {
     private final List<Boolean> value;
@@ -31,17 +31,26 @@ public class Line {
 
     private void validate(List<Boolean> value) {
         // 첫번째 다리가 true 인 경우 Exception throw
+        firstElementValidate(value);
+
+        // 선이 이어지는 경우에 Exception throw
+        nearbyElementsValidate(value);
+    }
+
+    private void firstElementValidate(List<Boolean> value) {
         if (value.get(0)) {
             throw new IllegalArgumentException();
         }
+    }
 
-        // 선이 이어지는 경우에 Exception throw
-        value.stream().reduce(false, (left, right) -> {
-            if (left && right) {
-                throw new IllegalArgumentException();
-            }
-            return right;
-        });
+    private void nearbyElementsValidate(List<Boolean> value) {
+        value.stream()
+                .reduce((left, right) -> {
+                    if (left && right) {
+                        throw new IllegalArgumentException();
+                    }
+                    return right;
+                });
     }
 
     @Override
