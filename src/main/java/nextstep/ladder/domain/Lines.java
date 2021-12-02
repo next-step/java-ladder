c4e80fdd6fa438745efac346d0780574dbcca595
hiro <kiryuinh32@gmail.com>
2021-12-03 01:35:09 +0900

    feat: Line을 갖는 일급컬렉션 Lines 구현

---
+++ src/main/java/nextstep/ladder/domain/Lines.java
@@ -0,0 +1,29 @@
+package nextstep.ladder.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Lines {
+    private static final int MIN_LINES_SIZE = 1;
+
+    private final List<Line> lines;
+
+    private Lines(List<Line> lines) {
+        validateLines(lines);
+        this.lines = lines;
+    }
+
+    private static void validateLines(List<Line> lines) {
+        if (Objects.isNull(lines) || lines.size() < MIN_LINES_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static Lines from(List<Line> lines) {
+        return new Lines(lines);
+    }
+
+    public List<Line> getLines() {
+        return lines;
+    }
+}
