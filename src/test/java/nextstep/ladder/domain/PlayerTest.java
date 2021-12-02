c4f490e2292a5b23c3412457b82f0029a91c695a
hiro <kiryuinh32@gmail.com>
2021-12-03 01:32:35 +0900

    test: Player 생성, 예외 테스트

---
+++ src/test/java/nextstep/ladder/domain/PlayerTest.java
@@ -0,0 +1,34 @@
+package nextstep.ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PlayerTest {
+
+    @ParameterizedTest
+    @DisplayName("1~5자의 이름으로 Playr를 생성한다")
+    @ValueSource(strings = {"a", "ab", "abc", "abcd", "abcde"})
+    void createPlayer(String name) {
+        assertThat(Player.from(name)).isInstanceOf(Player.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Player의 이름이 5자를 초과하면 예외가 발생한다")
+    @ValueSource(strings = {"abcdef", "abcdefg"})
+    void nameLengthException(String name) {
+        assertThatThrownBy(() -> Player.from(name)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Player의 이름이 없으면 예외가 발생한다")
+    @NullAndEmptySource
+    void nameEmptyException(String name) {
+        assertThatThrownBy(() -> Player.from(name)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
