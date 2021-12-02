b285fe997435829407258334631bea3d1a5331ba
doorisopen <doorwasopen@gmail.com>
2021-12-03 00:33:09 +0900

    feat: step3 결과를 보고 싶은 사람 입력

--- src/main/java/step2/view/InputView.java
+++ src/main/java/step2/view/InputView.java
@@ -8,6 +8,7 @@ public class InputView {
     private static final String INPUT_PARTICIPANT_NAME_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String INPUT_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     private static final String INPUT_LADDER_REWARD_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private static final String INPUT_REWARD_RESULT_NAME_MESSAGE = "결과를 보고 싶은 사람은?";
 
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -25,6 +26,11 @@ public class InputView {
 
     public static String inputLadderReward() {
         System.out.println(INPUT_LADDER_REWARD_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static String inputRewardResultName() {
+        System.out.println(INPUT_REWARD_RESULT_NAME_MESSAGE);
         return scanner.nextLine();
     }
 }
