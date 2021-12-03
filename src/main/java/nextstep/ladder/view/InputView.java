9f18e5c313633cf8d75cd45309684df4bb25a2f0
hiro <kiryuinh32@gmail.com>
2021-12-03 11:21:32 +0900

    style: InputView 필드 변수 네이밍 상수에 맞도록 수정

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
-    private static final String askPlayersName = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
-    private static final String askLadderHeight = "최대 사다리 높이는 몇개 인가요?";
+    private static final String ASK_PLAYERS_NAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String ASK_LADDER_HEIGHT = "최대 사다리 높이는 몇개 인가요?";
 
     private InputView() {
     }
 
     public static String askPlayerssName() {
-        System.out.println(askPlayersName);
+        System.out.println(ASK_PLAYERS_NAME);
         return SCANNER.nextLine();
     }
 
     public static int askLadderHeight() {
-        System.out.println(askLadderHeight);
+        System.out.println(ASK_LADDER_HEIGHT);
         String height = SCANNER.nextLine();
         try {
             return Integer.parseInt(height);
