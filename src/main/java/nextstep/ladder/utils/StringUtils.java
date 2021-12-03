1492ad1714e4800bfd9e2c9cb95ddc19c7da43b6
hiro <kiryuinh32@gmail.com>
2021-12-03 11:18:05 +0900

    feat: StringUtils 클래스 생성자 호출시 예외 추가

--- src/main/java/nextstep/ladder/utils/StringUtils.java
+++ src/main/java/nextstep/ladder/utils/StringUtils.java
@@ -4,6 +4,7 @@ public class StringUtils {
     private static final String DELIMITER = ",";
 
     private StringUtils() {
+        throw new AssertionError();
     }
 
     public static String[] split(String s) {
