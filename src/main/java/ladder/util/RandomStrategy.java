b08c61a4c3cacf98dd75bc63f67501a6be468298
ChangmoKang <dkssudckdah@gmail.com>
2021-12-04 18:32:43 +0900

    fix: Random 객체 정적 변수로 변경

--- src/main/java/ladder/util/RandomStrategy.java
+++ src/main/java/ladder/util/RandomStrategy.java
@@ -4,9 +4,11 @@ import java.util.Random;
 import java.util.function.Predicate;
 
 public class RandomStrategy implements Predicate<Boolean> {
+    private static final Random random = new Random();
+
     @Override
     public boolean test(Boolean value) {
-        if (value || new Random().nextInt() % 2 == 0) {
+        if (value || random.nextInt() % 2 == 0) {
             return false;
         }
 
