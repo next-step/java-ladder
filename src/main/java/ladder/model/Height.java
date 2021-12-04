df17511df4195917ea7c7d10cb20abdcc3ace87e
jjoy <ekdnlt1995@gmail.com>
2021-12-04 16:47:03 +0900

    feat : 사다리 높이 개발

--- src/main/java/ladder/model/Height.java
+++ src/main/java/ladder/model/Height.java
@@ -1,5 +1,7 @@
 package ladder.model;
 
+import java.util.Objects;
+
 public class Height {
 
     private static final int MIN_HEIGHT = 0;
@@ -10,9 +12,26 @@ public class Height {
         this.height = height;
     }
 
+    public int getHeight() {
+        return height;
+    }
+
     private void checkValidation(int height) {
         if(height < MIN_HEIGHT) {
-            throw new IllegalArgumentException("최대 사다리 높이는 0 이상이여야 합니다.");
+            throw new IllegalArgumentException("최대 사다리 높이는 " + MIN_HEIGHT + " 이상이여야 합니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Height height1 = (Height) o;
+        return height == height1.height;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(height);
     }
 }
