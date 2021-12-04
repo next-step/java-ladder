ccae56f54f0e6a874a083ee2b01a3ffded5f2ea2
user <kth4878@naver.com>
2021-12-04 10:14:54 +0900

    [Feat] 사다리 높이 클래스 추가

---
+++ src/main/java/ladder/domain/Height.java
@@ -0,0 +1,14 @@
+package ladder.domain;
+
+public class Height {
+    private final int height;
+
+    public Height(int height) {
+        this.height = height;
+    }
+
+    public int value() {
+        return height;
+    }
+
+}
