e5e0d75f19fb89325b909c2c82efb29d163f8f06
jjoy <ekdnlt1995@gmail.com>
2021-12-04 17:28:04 +0900

    feat : 참가자 이름 객체 개발

--- src/main/java/ladder/model/Name.java
+++ src/main/java/ladder/model/Name.java
@@ -1,8 +1,11 @@
 package ladder.model;
 
+import ladder.common.CommonValue;
+
+import java.util.Objects;
+
 public class Name {
 
-    private final static int MAX_LENGTH = 5;
     private final String name;
 
     public Name(String name) {
@@ -10,12 +13,29 @@ public class Name {
         this.name = name;
     }
 
+    public String get() {
+        return this.name;
+    }
+
     private void checkValidation(String name) {
-        if(name == null || name.isEmpty()) {
+        if(name == null || name.trim().isEmpty()) {
             throw new IllegalArgumentException("이름이 null 이거나 비어있습니다.");
         }
-        if(name.length() > MAX_LENGTH) {
-            throw new IllegalArgumentException("이름 길이는 " + MAX_LENGTH + "를 넘으면 안됩니다.");
+        if(name.length() > CommonValue.NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException("이름 길이는 " + CommonValue.NAME_MAX_LENGTH + "를 넘으면 안됩니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
---
+++ src/test/java/ladder/model/NameTest.java
@@ -0,0 +1,40 @@
+package ladder.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class NameTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = { "123456", "1234567", "12345678"})
+    @DisplayName("이름 길이가 5를 넘으면 예외 발생 테스트")
+    void nameLengthOverFiveException(String value) {
+        nameExceptionThrown(value, "이름 길이는 5를 넘으면 안됩니다.");
+    }
+
+    @NullAndEmptySource
+    @ParameterizedTest
+    @DisplayName("이름이 null 이거나 비어있으면 예외 발생 테스트")
+    void nullEmptyNameException(String value) {
+        nameExceptionThrown(value, "이름이 null 이거나 비어있습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "1", "12", "123", "1234", "12345"})
+    @DisplayName("이름 길이가 5 이하면 정상적으로 생성 테스트")
+    void nameObjectCreateTest(String value) {
+        Name name = new Name(value);
+        assertThat(name).isEqualTo(new Name(value));
+    }
+
+    private void nameExceptionThrown(String value, String s) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Name(value))
+                .withMessage(s);
+    }
+}
