b8612d8cfb79765836704c71333b44a420963d0c
jjoy <ekdnlt1995@gmail.com>
2021-12-05 21:44:53 +0900

    refactor : 변수와 생성자 사이 공백 라인추가

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
+
     private InputView() {}
 
     public static String getStringValue(String message) {
