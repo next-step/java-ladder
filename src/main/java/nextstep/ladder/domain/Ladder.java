d6b6947e299fa563313d3a86dc1c1e9fa23e907a
DongHyo <kdhyo98@gmail.com>
2021-12-06 00:55:20 +0900

    radder 객체 생성

---
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -0,0 +1,28 @@
+package nextstep.ladder.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Ladder {
+
+    private final List<Line> ladder;
+
+    private Ladder(List<Line> ladder) {
+        this.ladder = ladder;
+    }
+
+    public List<Line> getLadder() {
+        return Collections.unmodifiableList(ladder);
+    }
+
+    public static Ladder of(int width, Height height) {
+        List<Line> lines = IntStream.range(0, height.getHeight())
+            .mapToObj((i) -> Line.of(width, new RandomLineStrategy()))
+            .collect(Collectors.toCollection(ArrayList::new));
+        return new Ladder(lines);
+    }
+
+}
