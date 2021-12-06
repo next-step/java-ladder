7e1018844c152fe1c13e872499e2c4aca59fe31f
dante(이산하) <dante.93@kakaocorp.com>
2021-12-06 23:04:24 +0900

    refactor (add some test code)

--- src/main/java/nextstep/laddergame/model/Name.java
+++ src/main/java/nextstep/laddergame/model/Name.java
@@ -1,5 +1,7 @@
 package nextstep.laddergame.model;
 
+import java.util.Objects;
+
 import nextstep.laddergame.exception.InvalidNameException;
 
 public class Name {
@@ -22,5 +24,22 @@ public class Name {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
