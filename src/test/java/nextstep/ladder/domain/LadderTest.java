a9a91dd86c11ce75efa6fbc71b9f50502f4c6b8f
giyeon95 <giyeon15@gmail.com>
2021-12-06 21:04:28 +0900

    test: `getLinesByReflection` 불필요 메소드 제거

--- src/test/java/nextstep/ladder/domain/LadderTest.java
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -2,10 +2,7 @@ package nextstep.ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.lang.reflect.Field;
 import java.util.List;
-import nextstep.ladder.domain.Ladder;
-import nextstep.ladder.domain.Line;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,33 +10,25 @@ public class LadderTest {
 
     @Test
     @DisplayName("Ladder 생성시, 전달한 갯수만큼 lines 생성되어야 한다.")
-    void createHeightSizeTest() throws NoSuchFieldException, IllegalAccessException {
+    void createHeightSizeTest() {
         Ladder ladder = Ladder.create(PositiveNumber.create(5), 10);
 
-        List<Line> linesByReflection = getLinesByReflection(ladder);
+        List<Line> lines = ladder.getLines();
 
-        assertThat(linesByReflection).hasSize(5);
-        assertThat(linesByReflection.get(0).getPoints()).hasSize(9);
+        assertThat(lines).hasSize(5);
+        assertThat(lines.get(0).getPoints()).hasSize(9);
         System.out.println(ladder);
     }
 
     @Test
     @DisplayName("Ladder 생성시, 생성되는 각 Line의 갯수는, 참가자의 수 -1 이다.")
-    void createLineInPointSizeTest() throws NoSuchFieldException, IllegalAccessException {
+    void createLineInPointSizeTest() {
 
         int participantsCount = 10;
         Ladder ladder = Ladder.create(PositiveNumber.create(5), participantsCount);
 
-        List<Line> linesByReflection = getLinesByReflection(ladder);
+        List<Line> lines = ladder.getLines();
 
-        assertThat(linesByReflection.get(0).getPoints()).hasSize(participantsCount - 1);
-    }
-
-    @SuppressWarnings("unchecked")
-    private List<Line> getLinesByReflection(Ladder ladder)
-        throws NoSuchFieldException, IllegalAccessException {
-        Field linesField = Ladder.class.getDeclaredField("lines");
-        linesField.setAccessible(true);
-        return (List<Line>) linesField.get(ladder);
+        assertThat(lines.get(0).getPoints()).hasSize(participantsCount - 1);
     }
 }
