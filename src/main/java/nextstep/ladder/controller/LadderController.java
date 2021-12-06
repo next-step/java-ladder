9d00cfe2856ec82cea44c2d527e95b5a89e7d69a
minjoon_lee <minjoon1995@naver.com>
2021-12-06 21:37:46 +0900

    feature : controller 생성

--- src/main/java/nextstep/ladder/controller/LadderController.java
+++ src/main/java/nextstep/ladder/controller/LadderController.java
@@ -1,11 +1,17 @@
 package nextstep.ladder.controller;
 
 import nextstep.ladder.domain.Lines;
+import nextstep.ladder.domain.Participants;
+import nextstep.ladder.view.Input;
 import nextstep.ladder.view.OutPut;
 
 public class LadderController {
     public static void main(String[] args) {
-        Lines lines = Lines.of(5,5);
+        int countOfLine = Input.InputCountOfLine();
+        String participantsString = Input.InputParticipants();
+        Participants participants = Participants.of(participantsString);
+        Lines lines = Lines.of(participants.getParticipantsSize(),countOfLine);
+        OutPut.viewParticipants(participants);
         OutPut.viewLadder(lines);
     }
 }
