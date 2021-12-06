e0a83b7501baebcb73cb4cf349928fc363f9f6f5
dante(이산하) <dante.93@kakaocorp.com>
2021-12-06 22:46:47 +0900

    refactor (use static factory method)

--- src/main/java/nextstep/laddergame/model/Participant.java
+++ src/main/java/nextstep/laddergame/model/Participant.java
@@ -4,12 +4,12 @@ public class Participant {
 
     private final Name name;
 
-    public Participant(String name) {
-        this(new Name(name));
-    }
-
     public Participant(Name name) {
         this.name = name;
+    }
+
+    public static Participant from(String name) {
+        return new Participant(new Name(name));
     }
 
     public Name name() {
--- src/main/java/nextstep/laddergame/view/InputView.java
+++ src/main/java/nextstep/laddergame/view/InputView.java
@@ -18,7 +18,7 @@ public final class InputView {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         List<String> participants = Arrays.asList(ScannerUtils.nextLine().split(DELIMITER));
         return participants.stream()
-                           .map(Participant::new)
+                           .map(Participant::from)
                            .collect(Collectors.toList());
     }
 
