838aaf832918f34e00645f20a9008ea3f2b6b02c
ChangmoKang <dkssudckdah@gmail.com>
2021-12-06 17:20:16 +0900

    feat: Ladder result message 추가

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -3,7 +3,9 @@ package ladder.domain;
 import ladder.util.GameUtil;
 import ladder.util.RandomStrategy;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -26,6 +28,25 @@ public class Ladder {
 
     private void validate(List<Line> value) {
         GameUtil.requireNonNullOrSizeGreaterThanZero(value);
+    }
+
+    public LadderResult result() {
+        Map<Integer, Integer> result = new HashMap<>();
+
+        for (int currPos = 0; currPos < this.value.get(0).size(); currPos++) {
+            final int eachResult = resultByPosition(currPos);
+            result.put(currPos, eachResult);
+        }
+
+        return new LadderResult(result);
+    }
+
+    private int resultByPosition(int initPosition) {
+        int target = initPosition;
+        for (final Line line : this.value) {
+            target = line.nextPosition(target);
+        }
+        return target;
     }
 
     @Override
--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -1,11 +1,14 @@
 package ladder.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -45,6 +48,35 @@ public class LadderTest {
     static Stream<Arguments> invalid_createSource() {
         return Stream.of(
                 Arguments.of(Arrays.asList())
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("resultSource")
+    @DisplayName("Ladder 는 LadderResult 를 반환할 수 있다.")
+    void result(List<Line> input) {
+        // given
+        Map<Integer, Integer> value = new HashMap<>();
+        value.put(3, 1);
+        value.put(2, 0);
+        value.put(1, 2);
+        value.put(0, 3);
+
+        // when
+        Ladder ladder = new Ladder(input);
+
+        // then
+        assertThat(ladder.result()).isEqualTo(new LadderResult(value));
+    }
+
+    static Stream<Arguments> resultSource() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(
+                        new Line(Arrays.asList(false, false, true, false)),
+                        new Line(Arrays.asList(false, true, false, true)),
+                        new Line(Arrays.asList(false, false, true, false)),
+                        new Line(Arrays.asList(false, false, false, true))
+                ))
         );
     }
 }
