24d094ff93e971da638083cd4914065a3cb59270
soo <yunsoo0510@daum.net>
2021-12-06 11:19:49 +0900

    feat : Rail 구현

---
+++ src/main/java/ladder/Rail.java
@@ -0,0 +1,23 @@
+package ladder;
+
+public class Rail implements LadderPart {
+
+    private static final String RAIL_VALUE = "|";
+    private static final Rail rail = new Rail(RAIL_VALUE);
+
+    private final String value;
+
+    private Rail(String value) {
+        this.value = value;
+    }
+
+    public static Rail rail() {
+        return rail;
+    }
+
+    @Override
+    public String value() {
+        return value;
+    }
+
+}
---
+++ src/test/java/ladder/RailTest.java
@@ -0,0 +1,22 @@
+package ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RailTest {
+
+    @DisplayName("Rail은 LadderPart의 구현체")
+    @Test
+    void create() {
+        assertThat(Rail.rail()).isInstanceOf(LadderPart.class);
+    }
+
+    @DisplayName("Rail의 value 확인")
+    @Test
+    void value() {
+        assertThat(Rail.rail().value()).isEqualTo("|");
+    }
+
+}
