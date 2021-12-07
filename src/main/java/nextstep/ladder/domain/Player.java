ba5a4855e571bf02c01de251e9c7a7ac85f81116
hiro <kiryuinh32@gmail.com>
2021-12-07 22:52:38 +0900

    feat: Player#equals 재정의

--- src/main/java/nextstep/ladder/domain/Player.java
+++ src/main/java/nextstep/ladder/domain/Player.java
@@ -27,7 +27,20 @@ public class Player {
         }
     }
 
-    public String getName() {
+    public String name() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
