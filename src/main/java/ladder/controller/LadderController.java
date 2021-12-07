3ef1b5ec3e3da4b97d5b5254f68c7f94e0185781
kangyonghyun <hmoon826@gmail.com>
2021-12-07 15:04:27 +0900

    (feat) 사다리객체 생성 추가

---
+++ src/main/java/ladder/controller/LadderController.java
@@ -0,0 +1,38 @@
+package ladder.controller;
+
+import ladder.domain.*;
+import ladder.domain.PlayerName;
+import ladder.domain.Position;
+
+import java.util.Arrays;
+
+public class LadderController {
+
+    public static void main(String[] args) {
+
+//        List<String> names = InputView.printInputNames();
+//        Players players = new Players(names);
+//        int height = InputView.printInputLadderHeight();
+//        ResultView.printResult(Arrays.asList("psdd", "honux", "crong", "jd", "sss", "ss"), 5);
+
+//        Player player1 = new Player(new PlayerName("pobi"));
+//        Player player2 = new Player(new PlayerName("honux"));
+//        Player player3 = new Player(new PlayerName("crong"));
+//        Player player4 = new Player(new PlayerName("jk"));
+//        Players players = new Players(Arrays.asList(player1, player2, player3, player4));
+//        Point point1 = new Point(new Position(0), new Direction(false, false) false, true, direction);
+//        Point point2 = new Point(new Position(1), new Direction(false, false)false, false, direction);
+//        Point point3 = new Point(new Position(2), new Direction(false, false)false, true, direction);
+//        Point point4 = new Point(new Position(3), new Direction(false, false)false, false, direction);
+//        //라인변경
+//        Point point5 = new Point(new Position(4), new Direction(false, false)false, false, direction);
+//        Point point6 = new Point(new Position(5), false, true, direction);
+//        Point point7 = new Point(new Position(6), false, false, direction);
+//        Point point8 = new Point(new Position(7), true, false, direction);
+//        Line line = new Line(Arrays.asList(point1, point2, point3, point4, point5, point6, point7, point8));
+//        Ladder ladder = new Ladder(line);
+//        ResultView.printLadder(ladder);
+
+    }
+
+}
