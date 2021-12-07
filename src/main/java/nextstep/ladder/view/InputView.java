61a6eaca1b1d930773fe37fcc085ba7d146d15b0
hiro <kiryuinh32@gmail.com>
2021-12-07 23:12:48 +0900

    view: InputView 구현

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String ASK_PLAYERS_NAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String ASK_RESULTS = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)\n";
     private static final String ASK_LADDER_HEIGHT = "최대 사다리 높이는 몇개 인가요?";
+    private static final String ENTER_NUMBER = "숫자를 입력 하세요";
+    private static final String ASK_FIND_PLAYER = "결과를 보고 싶은 사람은?";
 
     private InputView() {
     }
@@ -21,8 +24,18 @@ public class InputView {
         try {
             return Integer.parseInt(height);
         } catch (NumberFormatException e) {
-            System.out.println("숫자를 입력 하세요");
+            System.out.println(ENTER_NUMBER);
             return askLadderHeight();
         }
+    }
+
+    public static String askResults() {
+        System.out.print(ASK_RESULTS);
+        return SCANNER.nextLine();
+    }
+
+    public static String askResultOfPlayer() {
+        System.out.println(ASK_FIND_PLAYER);
+        return SCANNER.nextLine();
     }
 }
