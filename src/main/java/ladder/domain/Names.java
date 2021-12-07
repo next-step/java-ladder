d9392d4048e7d83a46b4d1d72a52530a3c5e4ea1
soosue <yunsoo0510@daum.net>
2021-12-07 13:33:21 +0900

    feat : Names 구현

--- src/main/java/ladder/domain/Names.java
+++ src/main/java/ladder/domain/Names.java
@@ -1,5 +1,7 @@
 package ladder.domain;
 
+import ladder.util.Assert;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -7,41 +9,21 @@ import java.util.stream.Collectors;
 
 public class Names {
 
+    private static final String NAMES_DELIMITER = ",";
+    private static final String NULL_MESSAGE = "입력이 null일 수 없습니다.";
+
     private final List<Name> names;
 
     public Names(String names) {
-        this(ValidNames.toNameList(names));
-    }
+        Assert.notNull(names, NULL_MESSAGE);
 
-    public Names(List<Name> names) {
-        //null이면 안됨. size 0도 안됨
-        this.names = names;
+        this.names = Arrays.stream(names.split(NAMES_DELIMITER))
+                .map(Name::new)
+                .collect(Collectors.toList());
     }
 
     public List<Name> getNames() {
         return Collections.unmodifiableList(names);
-    }
-
-    private static class ValidNames {
-
-        private static final String NAMES_DELIMITER = ",";
-        private static final String NULL_MESSAGE = "입력이 null일 수 없습니다.";
-
-        private final String value;
-
-        public ValidNames(String names) {
-            if (names == null) {
-                throw new IllegalArgumentException(NULL_MESSAGE);
-            }
-            value = names;
-        }
-
-        public static List<Name> toNameList(String names) {
-            ValidNames validNames = new ValidNames(names);
-            return Arrays.stream(validNames.value.split(NAMES_DELIMITER))
-                    .map(Name::new)
-                    .collect(Collectors.toList());
-        }
     }
 
 }
--- src/test/java/ladder/domain/NamesTest.java
+++ src/test/java/ladder/domain/NamesTest.java
@@ -1,11 +1,14 @@
 package ladder.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NamesTest {
 
+    @DisplayName(",로 구분하여 Names 생성")
     @Test
     void create() {
         Names names = new Names("pobi, honux, crong, jk");
@@ -13,6 +16,12 @@ public class NamesTest {
         assertThat(names.getNames()).hasSize(4);
     }
 
-
+    @DisplayName("생성 파라미터는 null일 수 없음")
+    @Test
+    void create_null() {
+        assertThatThrownBy(() -> new Names(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("입력이 null일 수 없습니다.");
+    }
 
 }
