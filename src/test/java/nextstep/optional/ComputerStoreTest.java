5ddb2aeb15e2e6c741032001a877997c7772256a
las9897 <las9897@gmail.com>
2021-12-07 09:37:48 +0900

    refactor: getVersion Optional 적용

--- src/test/java/nextstep/optional/ComputerStoreTest.java
+++ src/test/java/nextstep/optional/ComputerStoreTest.java
@@ -7,30 +7,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ComputerStoreTest {
-    @Test
-    public void getVersion() {
-        String version = "pobi's usb";
-        Soundcard soundcard = new Soundcard(new USB(version));
-        Computer computer = new Computer(soundcard);
-        assertThat(ComputerStore.getVersion(computer)).isEqualTo(version);
-    }
-
-    @Test
-    public void getVersionWhenComputerIsNull() throws Exception {
-        assertThat(ComputerStore.getVersion(null)).isEqualTo(ComputerStore.UNKNOWN_VERSION);
-    }
-
-    @Test
-    public void getVersionWhenSoundcardIsNull() throws Exception {
-        Computer computer = new Computer(null);
-        assertThat(ComputerStore.getVersion(computer)).isEqualTo(ComputerStore.UNKNOWN_VERSION);
-    }
-
-    @Test
-    public void getVersionWhenUSBIsNull() throws Exception {
-        Computer computer = new Computer(new Soundcard(null));
-        assertThat(ComputerStore.getVersion(computer)).isEqualTo(ComputerStore.UNKNOWN_VERSION);
-    }
 
     @Test
     public void getVersionOptional() {
