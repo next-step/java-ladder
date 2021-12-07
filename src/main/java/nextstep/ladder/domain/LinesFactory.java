5499ac930f760bb3a6a55765c5b5b44584df12c5
hiro <kiryuinh32@gmail.com>
2021-12-07 23:05:00 +0900

    feat: LinesFactory 구현
    rowCount, colCount, Linegenerator, BoolListGenerator를 통해 Lines 생성

---
+++ src/main/java/nextstep/ladder/domain/LinesFactory.java
@@ -0,0 +1,15 @@
+package nextstep.ladder.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LinesFactory {
+    public static Lines of(int rowCount, int colCount, LineGenerator lineGenerator, BooleanListGenerator listGenerator) {
+        List<Line> lines = IntStream.range(0, colCount)
+                .mapToObj(i -> lineGenerator.generate(listGenerator.generate(rowCount)))
+                .collect(Collectors.toList());
+
+        return Lines.from(lines);
+    }
+}
