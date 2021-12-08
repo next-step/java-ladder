ceac362879f669c94c997000cd2b37f02afc934f
user <kth4878@naver.com>
2021-12-08 17:13:11 +0900

    [Feat] 참가자별 당첨 클래스 추가

---
+++ src/main/java/ladder/domain/Winning.java
@@ -0,0 +1,20 @@
+package ladder.domain;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class Winning {
+    private final Map<Name, Prize> winning;
+
+    public Winning(Map<Name, Prize> winning) {
+        this.winning = winning;
+    }
+
+    public Prize prize(Name key) {
+        return winning.get(key);
+    }
+
+    public Map<Name, Prize> playerByPrize() {
+        return Collections.unmodifiableMap(winning);
+    }
+}
