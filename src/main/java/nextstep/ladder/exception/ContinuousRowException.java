538bf93626cccbe435ebb123d81e841c9e130411
hiro <kiryuinh32@gmail.com>
2021-12-08 21:32:16 +0900

    delete: ContinuousRowException 사용하지 않는 클래스 삭제

--- src/main/java/nextstep/ladder/exception/ContinuousRowException.java
+++
@@ -1,9 +0,0 @@
-package nextstep.ladder.exception;
-
-public class ContinuousRowException extends RuntimeException {
-    private static final String MESSAGE = "사다리 행이 연속으로 존재합니다.";
-
-    public ContinuousRowException() {
-        super(MESSAGE);
-    }
-}
