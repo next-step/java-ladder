57f436721389f0e91d35afd8d27d189068e651b2
minjoon_lee <minjoon1995@naver.com>
2021-12-09 21:42:03 +0900

    refactoring : string 으로 만들 수 있는 line 추가 및 point 추가

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -1,22 +1,52 @@
 package nextstep.ladder.domain;
 
-import nextstep.ladder.strategy.LadderStrategy;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Line {
-    private final List<Boolean> points;
+    private final List<Point> points;
 
-    private Line(List<Boolean> points) {
+    private Line(List<Point> points) {
         this.points = points;
     }
 
-    public static Line of(int countOfParticipants, LadderStrategy ladderStrategy) {
-        return new Line(ladderStrategy.makeLadder(countOfParticipants));
+    public static Line ofRandom(int countOfParticipants) {
+        List<Point> points = new ArrayList<>();
+        for (int index = 0; index < countOfParticipants - 1; index++) {
+            addPoint(points, index);
+        }
+        return new Line(points);
     }
 
-    public List<Boolean> getPoints() {
-        return points;
+    public static Line ofString(String input) {
+        checkCanBeBoolean(input);
+        List<Point> points = Arrays.stream(input.split(","))
+                .map(point -> Point.of(new Boolean(point)))
+                .collect(Collectors.toList());
+        return new Line(points);
     }
 
+    private static void addPoint(List<Point> points, int index) {
+        if (index == 0) {
+            points.add(Point.ofRandom());
+        } else if (points.get(index - 1) == Point.of(true)) {
+            points.add(Point.of(false));
+        } else if (points.get(index - 1) == Point.of(false)) {
+            points.add(Point.ofRandom());
+        }
+    }
+
+    private static void checkCanBeBoolean(String input) {
+        Optional<String> inputError = Arrays.stream(input.split(","))
+                .filter(point -> !point.equals("false") && !point.equals("true"))
+                .findFirst();
+        if (inputError.isPresent()) {
+            throw new IllegalArgumentException("Boolean 으로 변환될 수 없습니다.");
+        }
+    }
+
+    public List<Point> getPoints() {
+        return Collections.unmodifiableList(points);
+    }
 }
