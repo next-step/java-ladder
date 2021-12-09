e1df477f38440b3c49404a4a24b313cb9ad22d30
LeeJeongGi <apple4rhk@naver.com>
2021-12-09 16:07:46 +0900

    refactor : CsvSource 활용하여 생성 테스트하기.

--- src/test/java/nextstep/ladder/model/ParticipantsTest.java
+++ src/test/java/nextstep/ladder/model/ParticipantsTest.java
@@ -1,17 +1,19 @@
 package nextstep.ladder.model;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ParticipantsTest {
 
-    @Test
+    @ParameterizedTest
     @DisplayName("참석자 이름 입력했을 때 생성하는 검증 로직")
-    void 참가자_생성() {
-        Participants participants = Participants.from("lee,kim,oh");
+    @CsvSource(value = {"lee,kim,oh:3", "lee,kim:2"}, delimiter = ':')
+    void createTest(String inputData, int sizeResult) {
+        Participants participants = Participants.from(inputData);
 
-        assertThat(participants).isEqualTo(Participants.from("lee,kim,oh"));
+        assertThat(participants.size()).isEqualTo(sizeResult);
     }
 }
