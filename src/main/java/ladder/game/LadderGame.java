dafaf982d4941df25a2641b01854910c884928f0
jjoy <ekdnlt1995@gmail.com>
2021-12-10 03:35:44 +0900

    feat : 게임 수정

--- src/main/java/ladder/game/LadderGame.java
+++ src/main/java/ladder/game/LadderGame.java
@@ -1,26 +1,35 @@
 package ladder.game;
 
-import ladder.model.Height;
-import ladder.model.Line;
+import ladder.model.ladder.Height;
+import ladder.model.ladder.Ladder;
 import ladder.model.player.Players;
-
-import java.util.*;
+import ladder.model.result.Result;
+import ladder.model.result.Results;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class LadderGame {
 
+    private final Ladder ladder;
     private final Players players;
-    private final Height height;
+    private final Results results;
 
-    public LadderGame(Players players, Height height) {
+    public LadderGame(Height height, Players players, Results results) {
+        this.ladder = Ladder.create(height.get(), players.count());
         this.players = players;
-        this.height = height;
+        this.results = results;
     }
 
-    public List<Line> play() {
-        List<Line> lineList = new ArrayList<Line>();
-        for (int cnt = 0; cnt < height.get(); cnt++) {
-            lineList.add(new Line(players.size()));
+    public Ladder getLadder() {
+        return this.ladder;
+    }
+
+    public Map<String,String> play() {
+        Map<String,String> gameResult = new LinkedHashMap<String,String>();
+        for(int startIndex = 0 ; startIndex < players.count() ; startIndex++) {
+            Result result = results.get(ladder.move(startIndex));
+            gameResult.put(players.name(startIndex), result.get());
         }
-        return lineList;
+        return gameResult;
     }
 }
--- src/main/java/ladder/game/LadderGameApp.java
+++ src/main/java/ladder/game/LadderGameApp.java
@@ -1,22 +1,22 @@
 package ladder.game;
 
-import ladder.model.*;
+import ladder.model.ladder.Height;
+import ladder.model.result.Results;
 import ladder.model.stringas.StringAsHeight;
 import ladder.model.player.Players;
 import ladder.view.InputView;
 import ladder.view.ResultView;
-
-import java.util.*;
 
 public class LadderGameApp {
 
     public static void main(String[] args) {
         try {
             Players players = new Players(InputView.getStringValue("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)"));
+            Results results = new Results(InputView.getStringValue("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)"), players.count());
             Height height = new StringAsHeight(InputView.getStringValue("최대 사다리 높이는 몇 개인가요?")).get();
-            LadderGame game = new LadderGame(players, height);
-            List<Line> lineList = game.play();
-            ResultView.printResult(players, lineList);
+            LadderGame game = new LadderGame(height, players, results);
+            ResultView.printLadderResult(players, game.getLadder(), results);
+            ResultView.printResult(game.play(), InputView.getStringValue("결과를 보고 싶은 사람은?"));
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
