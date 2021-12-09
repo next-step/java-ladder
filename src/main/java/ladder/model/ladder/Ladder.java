7f1cefc9f7503467a7566a26d39cee9ed8ff8280
jjoy <ekdnlt1995@gmail.com>
2021-12-10 03:35:26 +0900

    feat : line으로 만드는 사다리 객체 생성

---
+++ src/main/java/ladder/model/ladder/Ladder.java
@@ -0,0 +1,34 @@
+package ladder.model.ladder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ladder {
+
+    private final List<Line> lines;
+
+    private Ladder(List<Line> lines) {
+        this.lines = lines;
+    }
+
+    public static Ladder create(int height, int countOfPlayer) {
+        List<Line> lines = new ArrayList<Line>();
+        for (int cnt = 0; cnt < height; cnt++) {
+            lines.add(new Line(countOfPlayer));
+        }
+        return new Ladder(lines);
+    }
+
+    public int move(int startIndex) {
+        int currentIndex = startIndex;
+        for(Line line : lines) {
+            currentIndex = line.move(currentIndex);
+        }
+        return currentIndex;
+    }
+
+    public List<Line> lines() {
+        return this.lines;
+    }
+
+}
