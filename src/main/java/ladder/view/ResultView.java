0481eb5b569ff1249b08bbc15576829bc9fb6f88
jjoy <ekdnlt1995@gmail.com>
2021-12-10 03:36:11 +0900

    feat : 결과 출력

--- src/main/java/ladder/view/ResultView.java
+++ src/main/java/ladder/view/ResultView.java
@@ -1,20 +1,25 @@
 package ladder.view;
 
-import ladder.model.Line;
+import ladder.model.ladder.Ladder;
+import ladder.model.ladder.Line;
 import ladder.model.point.Point;
 import ladder.model.player.Player;
 import ladder.model.player.Players;
+import ladder.model.result.Result;
+import ladder.model.result.Results;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class ResultView {
 
-    public static void printResult(Players players, List<Line> lines) {
-        System.out.println("실행결과");
+    public static void printLadderResult(Players players, Ladder ladder, Results results) {
+        System.out.println("사다리 결과");
         System.out.println();
         printPlayerName(players.get());
-        printLines(lines);
+        printLadder(ladder);
+        printResults(results.get());
     }
 
     private static void printPlayerName(List<Player> players) {
@@ -22,12 +27,12 @@ public class ResultView {
         System.out.println();
     }
 
-    private static void printLines(List<Line> lines) {
-        lines.forEach(ResultView::printLine);
+    private static void printLadder(Ladder ladder) {
+        ladder.lines().forEach(ResultView::printLine);
     }
 
     private static void printLine(Line line) {
-        line.getPoints()
+        line.points()
                 .get()
                 .forEach(ResultView::printPoint);
         System.out.println();
@@ -35,9 +40,28 @@ public class ResultView {
 
     private static void printPoint(Point point) {
         String value = " ";
-        if(point.getIsLine()) {
+        if(point.isLeft())  {
             value = "-";
         }
         System.out.print(String.join("", Collections.nCopies(5, value)) + "|");
+    }
+
+    private static void printResults(List<Result> results) {
+        results.forEach(result -> System.out.printf("%-5s ", result.get()));
+        System.out.println();
+    }
+
+    public static void printResult(Map<String, String> resultMap, String name) {
+        System.out.println("실행 결과");
+        if(name.equals("all")) {
+            printAll(resultMap);
+        }
+        if(resultMap.containsKey(name)) {
+            System.out.println(resultMap.get(name));
+        }
+    }
+
+    private static void printAll(Map<String, String> gameResult) {
+        gameResult.forEach((name,result) -> System.out.println(name + " : " + result));
     }
 }
