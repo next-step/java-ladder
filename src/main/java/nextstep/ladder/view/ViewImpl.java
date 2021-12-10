7e04faf3266e644a5ea99741b7d901934bc77455
supniverse <kmsup2@gmail.com>
2021-12-10 16:11:04 +0900

    FEATURE: 리뷰 반영 (이름 표기 구현 수정)

--- src/main/java/nextstep/ladder/view/ViewImpl.java
+++ src/main/java/nextstep/ladder/view/ViewImpl.java
@@ -8,12 +8,16 @@ import nextstep.ladder.domain.entity.Names;
 import nextstep.ladder.domain.entity.Point;
 import nextstep.ladder.domain.entity.Symbol;
 
+import java.util.stream.Collectors;
+
 import static java.util.stream.Collectors.joining;
 
 public class ViewImpl implements View {
 
   private static final String RESULT_MESSAGE = "실행결과";
-  private static final int ONE = 1;
+  private static final int FIRST_INDEX = 1;
+  private static final int LADDER_SIZE = 5;
+  private static final int MAX_BLANK_SIZE = 6;
 
   private final InputView inputView;
   private final ResultView resultView;
@@ -37,36 +41,38 @@ public class ViewImpl implements View {
   public void printResult(Names names, Ladder ladder) {
     resultView.printMessage(RESULT_MESSAGE);
     resultView.printMessage(printNames(names));
-    resultView.printMessage(printLadder(names, ladder));
+    resultView.printMessage(printLadder(ladder));
   }
 
   private String printNames(Names names) {
     return names.stream()
-            .map(Name::tellName)
-            .collect(joining(Symbol.BLANK.getValue()));
+                .map(Name::tellName)
+                .map(name -> name.concat(Symbol.printSymbolAsSize(Symbol.BLANK, MAX_BLANK_SIZE - name.length())))
+                .collect(Collectors.joining());
   }
 
-  private String printLadder(Names names, Ladder ladder) {
+  private String printLadder(Ladder ladder) {
     return ladder.stream()
-                 .map(line -> printLine(line, names.maxSize()))
+                 .map(this::printLine)
                  .collect(joining(System.lineSeparator()));
   }
 
-  private String printLine(Line line, int size) {
-    String collectPoint = line.stream()
-                              .skip(ONE)
-                              .map(point -> printPoint(point, size))
-                              .collect(joining(Symbol.VERTICAL_LINE.getValue()));
-
-    return Symbol.VERTICAL_LINE.getValue().concat(collectPoint).concat(Symbol.VERTICAL_LINE.getValue());
+  private String printLine(Line line) {
+    return line.stream().skip(FIRST_INDEX)
+                        .map(this::printPoint)
+                        .collect(
+                                  joining(
+                                          Symbol.VERTICAL_LINE.getValue(),
+                                          Symbol.VERTICAL_LINE.getValue(),
+                                          Symbol.VERTICAL_LINE.getValue()));
   }
 
-  private String printPoint(Point point, int size) {
+  private String printPoint(Point point) {
     if (point.hasWay()) {
-      return Symbol.printSymbolAsSize(Symbol.DASH, size);
+      return Symbol.printSymbolAsSize(Symbol.DASH, LADDER_SIZE);
     }
 
-    return Symbol.printSymbolAsSize(Symbol.BLANK, size);
+    return Symbol.printSymbolAsSize(Symbol.BLANK, LADDER_SIZE);
   }
 
 }
