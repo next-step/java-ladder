5e351bad0e82958f6381fbe778f6fd8b0381e20b
jeje <las9897@gmail.com>
2021-12-10 20:17:55 +0900

    refactor: validateNameLength Optional 적용

--- src/test/java/nextstep/ladder/model/NameTest.java
+++ src/test/java/nextstep/ladder/model/NameTest.java
@@ -2,6 +2,8 @@ package nextstep.ladder.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
@@ -12,4 +14,13 @@ class NameTest {
     void nameMaxLengthOverTest() {
         assertThatIllegalArgumentException().isThrownBy(() -> new Name("abcdef"));
     }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("사람 이름이 null 이거나 빈 값일 경우 IllegalArgumentException 발생 테스트")
+    void nameIsNull(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Name(input));
+    }
+
+
 }
