778be2abe712006f5b2dd232a20ff6846bc0d477
orgojy <giveawesome@gmail.com>
2021-12-12 19:37:31 +0900

    feat: 함수형 인터페이스를 이용하여 행동 파라미터 람다를 활용한 숫자 총합 메서드 변경

--- src/test/java/nextstep/fp/LambdaTest.java
+++ src/test/java/nextstep/fp/LambdaTest.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -8,7 +9,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class LambdaTest {
+class LambdaTest {
     private List<Integer> numbers;
 
     @BeforeEach
@@ -31,21 +32,24 @@ public class LambdaTest {
         Lambda.runThread();
     }
 
+    @DisplayName("리스트의 모든 수의 합 구하기")
     @Test
-    public void sumAll() throws Exception {
-        int sum = Lambda.sumAll(numbers);
+    void sumAll() throws Exception {
+        int sum = Lambda.sumAll(numbers, n -> true);
         assertThat(sum).isEqualTo(21);
     }
 
+    @DisplayName("리스트의 모든 수 중에 짝수 합 구하기")
     @Test
-    public void sumAllEven() throws Exception {
-        int sum = Lambda.sumAllEven(numbers);
+    void sumAllEven() throws Exception {
+        int sum = Lambda.sumAll(numbers, n -> n % 2 == 0);
         assertThat(sum).isEqualTo(12);
     }
 
+    @DisplayName("리스트의 모든 수 중에 3을 넘어선 수 합 구하기")
     @Test
-    public void sumAllOverThree() throws Exception {
-        int sum = Lambda.sumAllOverThree(numbers);
+    void sumAllOverThree() throws Exception {
+        int sum = Lambda.sumAll(numbers, n -> n > 3);
         assertThat(sum).isEqualTo(15);
     }
 }
