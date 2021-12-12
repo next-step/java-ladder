8b798b9c2ea59544266cd076a9437c93c0012642
minjoon_lee <minjoon1995@naver.com>
2021-12-12 20:50:04 +0900

    feature : line에서 첫번째 포인트와 마지막 포인트 빌더 패턴 추가

--- src/main/java/nextstep/ladder/domain/Point.java
+++ src/main/java/nextstep/ladder/domain/Point.java
@@ -1,31 +1,49 @@
 package nextstep.ladder.domain;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 public class Point {
-    private static final Random RANDOM = new Random();
-    private static final int RANDOM_END = 10;
-    private static final int RANDOM_BOUNDARY = 5;
+    public static final Random RANDOM = new Random();
+    public static final int RANDOM_END = 10;
+
+    public static final int RANDOM_BOUNDARY = 5;
+
     private static final Map<String, Point> POINT_FOR_CASH = new HashMap<>();
 
     static {
-        POINT_FOR_CASH.put("true", new Point(true));
-        POINT_FOR_CASH.put("false", new Point(false));
+        POINT_FOR_CASH.put("left", new Point(Arrays.asList(true, false)));
+        POINT_FOR_CASH.put("right", new Point(Arrays.asList(false, true)));
+        POINT_FOR_CASH.put("none", new Point(Arrays.asList(false, false)));
     }
 
-    private final boolean point;
+    private final List<Boolean> point;
 
-    private Point(boolean point) {
+    private Point(List<Boolean> point) {
         this.point = point;
     }
 
-    public static Point of(Boolean point) {
-        return POINT_FOR_CASH.get(point.toString());
+    public static Point of(String point) {
+        return POINT_FOR_CASH.get(point);
     }
 
-    public static Point ofRandom() {
-        return Point.of(RANDOM.nextInt(RANDOM_END) > RANDOM_BOUNDARY);
+
+    public static Point ofRandomFirstPoint() {
+        int randomNumber = RANDOM.nextInt(RANDOM_END);
+        if (randomNumber > RANDOM_BOUNDARY) {
+            return Point.of("right");
+        } else if (randomNumber <= RANDOM_BOUNDARY) {
+            return Point.of("none");
+        }
+        return null;
+    }
+
+    public static Point ofRandomLastPoint(Point previousPoint) {
+        if (previousPoint == Point.of("right")) {
+            return Point.of("left");
+        }
+        if (previousPoint != Point.of("right")) {
+            return Point.of("none");
+        }
+        return null;
     }
 }
