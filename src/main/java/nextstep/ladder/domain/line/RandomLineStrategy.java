0c5c90dc563e0373089401f5a512514fbd058917
sid <sid.seo@kakaocorp.com>
2021-12-12 18:02:05 +0900

    chore: apply feedback
    refactoring RandomLineStrategy
     - 임시구현 리팩토링
     - 상수 변수 이름 대문자로

--- src/main/java/nextstep/ladder/domain/line/RandomLineStrategy.java
+++ src/main/java/nextstep/ladder/domain/line/RandomLineStrategy.java
@@ -1,26 +1,26 @@
 package nextstep.ladder.domain.line;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import nextstep.ladder.domain.LineCount;
 
 public class RandomLineStrategy implements LineGenerateStrategy {
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
+
+    private final AtomicBoolean previousPoint = new AtomicBoolean(RANDOM.nextBoolean());
 
     @Override
     public List<Boolean> generate(LineCount lineCount) {
-        List<Boolean> points = new ArrayList<>(lineCount.toInt());
-        points.add(random.nextBoolean());
-        for(int i = 1; i < lineCount.toInt(); i++) {
-            if (points.get(i - 1)) {
-                points.add(false);
-            } else {
-                points.add(random.nextBoolean());
-            }
-        }
+        return Stream.generate(this::generatePoint)
+                .limit(lineCount.toInt())
+                .collect(Collectors.toList());
+    }
 
-        return points;
+    public boolean generatePoint() {
+        return previousPoint.getAndSet(!previousPoint.get() && RANDOM.nextBoolean());
     }
 }
