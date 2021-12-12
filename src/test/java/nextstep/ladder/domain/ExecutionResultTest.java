640ec9315d87cb48f5e32a764bf3e62b10380f1c
DongHyo <kdhyo98@gmail.com>
2021-12-12 16:25:35 +0900

    refactor : displayName 및 Exception 변경

--- src/test/java/nextstep/ladder/domain/ExecutionResultTest.java
+++ src/test/java/nextstep/ladder/domain/ExecutionResultTest.java
@@ -3,6 +3,9 @@ package nextstep.ladder.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+import nextstep.ladder.exception.ExecutionResultNullPointerException;
+import nextstep.ladder.exception.ExecutionResultSizeException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
@@ -11,21 +14,24 @@ public class ExecutionResultTest {
 
     @ParameterizedTest
     @CsvSource(value = {"꽝,5000,꽝,3000:4", "꽝,5000:2", "꽝,5000,꽝,3000,꽝:5"}, delimiter = ':')
-    void 생성성공(String input, int size) {
+    @DisplayName("생성 성공")
+    void create(String input, int size) {
         assertThat(ExecutionResult.of(input, size)).isInstanceOf(ExecutionResult.class);
     }
 
     @ParameterizedTest
     @NullAndEmptySource
+    @DisplayName("null 혹은 Empty 일 경우 예외처리")
     void nullOrEmptyException(String input) {
-        assertThatExceptionOfType(NullPointerException.class)
+        assertThatExceptionOfType(ExecutionResultNullPointerException.class)
             .isThrownBy(() -> ExecutionResult.of(input, 1));
     }
 
     @ParameterizedTest
     @CsvSource(value = {"꽝,5000,꽝,3000:3", "꽝,5000:3", "꽝,5000,꽝,3000,꽝:6"}, delimiter = ':')
-    void name사이즈와불일치(String input, int size) {
-        assertThatExceptionOfType(IllegalArgumentException.class)
+    @DisplayName("players 사이즈랑 일치하지 않을 경우 예외처리")
+    void executionResultSizeException(String input, int size) {
+        assertThatExceptionOfType(ExecutionResultSizeException.class)
             .isThrownBy(() -> ExecutionResult.of(input, size));
     }
 
