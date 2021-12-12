bd81d323407ca541878dd3b5a71973598dab9144
JaeHan-Kim <hgal8877@gmail.com>
2021-12-12 13:43:04 +0900

    feat : 개별 참가자의 이름을 저장하는 객체 추가

---
+++ src/main/java/nextstep/ladder/domain/Name.java
@@ -0,0 +1,37 @@
+package nextstep.ladder.domain;
+
+import java.util.Objects;
+
+public class Name {
+    private final int MAX_NAME_LENGTH = 5;
+
+    private final String value;
+
+    public Name(String value) {
+        validValue(value);
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    private void validValue(String value) {
+        if (value == null || value.equals("") || value.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("문자열의 값이 유효하지 않습니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name = (Name) o;
+        return Objects.equals(value, name.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
