e38c08ae24e4fcb8676119fdb700d41cc753f220
DongHyo <kdhyo98@gmail.com>
2021-12-14 02:25:51 +0900

    feat : toString 오버라이딩

--- src/main/java/nextstep/ladder/domain/Name.java
+++ src/main/java/nextstep/ladder/domain/Name.java
@@ -34,6 +34,11 @@ public class Name {
     }
 
     @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) {
             return true;
