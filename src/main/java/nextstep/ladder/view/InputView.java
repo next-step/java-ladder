dc5be63c12d9cdb18a3ade019bb63e9db40753c4
DongHyo <kdhyo98@gmail.com>
2021-12-14 00:34:07 +0900

    refactor : while 내부의 if문 제거

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -22,4 +22,8 @@ public class InputView {
         return ScannerUtils.nextLine();
     }
 
+    public static String inputPlayerResult() {
+        System.out.println("결과를 보고 싶은 사람은?");
+        return ScannerUtils.nextLine();
+    }
 }
--- src/main/java/nextstep/ladder/view/PlayersResultView.java
+++ src/main/java/nextstep/ladder/view/PlayersResultView.java
@@ -4,30 +4,25 @@ import java.util.Map;
 import nextstep.ladder.domain.Item;
 import nextstep.ladder.domain.Name;
 import nextstep.ladder.domain.StringFactory;
-import nextstep.ladder.util.ScannerUtils;
 
 public class PlayersResultView {
 
     private static final Name ALL = StringFactory.name("all");
     private static final String DELIMITER = " : ";
     private static final String NULL_MESSAGE = "존재하지 않는 참가자 명 입니다.";
+    private static final String RESULT_MESSAGE = "실행 결과";
 
     private PlayersResultView() {
     }
 
     public static void gameResult(Map<Name, Item> playerResult) {
-        while (true) {
-            System.out.println("결과를 보고 싶은 사람은?");
-            Name name = StringFactory.name(ScannerUtils.nextLine());
-
-            System.out.println("\n실행 결과");
-            if (ALL.equals(name)) {
-                viewAll(playerResult);
-                break;
-            }
-
+        Name name = StringFactory.name(InputView.inputPlayerResult());
+        while (!ALL.equals(name)) {
+            System.out.println(RESULT_MESSAGE);
             view(playerResult, name);
+            name = StringFactory.name(InputView.inputPlayerResult());
         }
+        viewAll(playerResult);
     }
 
     private static void view(Map<Name, Item> playerResult, Name name) {
@@ -36,11 +31,11 @@ public class PlayersResultView {
             System.out.println(NULL_MESSAGE);
             return;
         }
-
         System.out.println(result);
     }
 
     private static void viewAll(Map<Name, Item> playerResult) {
+        System.out.println(RESULT_MESSAGE);
         for (Name player : playerResult.keySet()) {
             System.out.println(join(player, playerResult.get(player)));
         }
@@ -49,4 +44,5 @@ public class PlayersResultView {
     private static String join(Name player, Item item) {
         return String.join(DELIMITER, player.toString(), item.toString());
     }
+
 }
