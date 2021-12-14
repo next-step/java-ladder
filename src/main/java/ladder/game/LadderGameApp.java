84d9d399df9877077ca634d9bcdf0a8f3a3a2f10
jjoy <ekdnlt1995@gmail.com>
2021-12-14 23:22:09 +0900

    refactor : 수정완료

--- src/main/java/ladder/game/LadderGameApp.java
+++ src/main/java/ladder/game/LadderGameApp.java
@@ -1,6 +1,7 @@
 package ladder.game;
 
 import ladder.model.ladder.Height;
+import ladder.model.ladder.Ladder;
 import ladder.model.result.Results;
 import ladder.model.stringas.StringAsHeight;
 import ladder.model.player.Players;
@@ -14,9 +15,10 @@ public class LadderGameApp {
             Players players = new Players(InputView.getStringValue("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)"));
             Results results = new Results(InputView.getStringValue("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)"), players.count());
             Height height = new StringAsHeight(InputView.getStringValue("최대 사다리 높이는 몇 개인가요?")).get();
-            LadderGame game = new LadderGame(height, players, results);
-            ResultView.printLadderResult(players, game.getLadder(), results);
-            ResultView.printResult(game.play(), InputView.getStringValue("결과를 보고 싶은 사람은?"));
+            Ladder ladder = Ladder.create(height.get(), players.count());
+            ResultView.printLadderResult(players, ladder, results);
+            LadderGame game = new LadderGame(ladder, players);
+            ResultView.printResult(game.play(InputView.getStringValue("결과를 보고 싶은 사람은?")), results);
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
