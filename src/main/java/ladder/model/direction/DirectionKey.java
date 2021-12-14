12db145e0ff090ea9e54e03f7277113f18c4c2be
jjoy <ekdnlt1995@gmail.com>
2021-12-14 23:12:13 +0900

    refactor : static 제거

--- src/main/java/ladder/model/direction/DirectionKey.java
+++ src/main/java/ladder/model/direction/DirectionKey.java
@@ -2,16 +2,14 @@ package ladder.model.direction;
 
 public class DirectionKey {
 
-    private DirectionKey() {}
-
-    public static int get(boolean left, boolean right){
+    public int get(boolean left, boolean right){
         if(left == false && right == false) {
             return 1;
         }
-        if(left == false && right == true) {
+        if(left == false && right) {
             return 2;
         }
-        if(left == true && right == false) {
+        if(left && right == false) {
             return 3;
         }
         throw new IllegalArgumentException("선과 선은 나란히 그릴 수 없습니다.");
