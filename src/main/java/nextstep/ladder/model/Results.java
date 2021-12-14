4389a40dc87cc546f5416b1e63a6848cf8c4a0e8
jeje <las9897@gmail.com>
2021-12-15 01:32:40 +0900

    feat: Results convertTo, validation 로직 추가

--- src/main/java/nextstep/ladder/model/Results.java
+++ src/main/java/nextstep/ladder/model/Results.java
@@ -1,11 +1,31 @@
 package nextstep.ladder.model;
 
+import nextstep.ladder.common.Name;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Results {
+
+    private static final String MESSAGE_NOT_EQUALS_SIZE = "플레이어와 결과의 수가 일치하지 않습니다.";
+
     private final List<Result> results;
 
-    public Results(List<Result> results) {
-        this.results = results;
+    public Results(String[] splitResults, int width) {
+        this.results = convertTo(validationSize(splitResults, width));
+    }
+
+    private List<Result> convertTo(String[] splitResults) {
+        return Arrays.stream(splitResults)
+                .map(Result::new)
+                .collect(Collectors.toList());
+    }
+
+    private String[] validationSize(String[] splitResults, int width) {
+        if (splitResults.length != width) {
+            throw new IllegalArgumentException(MESSAGE_NOT_EQUALS_SIZE);
+        }
+        return splitResults;
     }
 }
