a2adff5d3cb9b56c17df0fd6cb7d612b9fd9bd6f
las9897 <las9897@gmail.com>
2021-12-15 10:21:20 +0900

    refactor: Line 유효성 검사 추가

--- src/main/java/nextstep/ladder/model/Line.java
+++ src/main/java/nextstep/ladder/model/Line.java
@@ -1,17 +1,26 @@
 package nextstep.ladder.model;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Line {
 
     public static final String BLANK_LINE = "     ";
     public static final String EXIST_LINE = "-----";
     private static final String LADDER_COLUMN = "|";
+    private static final int MIN_WIDTH = 2;
+    private static final String MESSAGE_MIN_WIDTH = "너비가 최소 2는 되어야합니다.";
 
     private final List<Boolean> points;
 
     public Line(List<Boolean> points) {
-        this.points = points;
+        this.points = validationWidth(points);
+    }
+
+    private List<Boolean> validationWidth(List<Boolean> points) {
+        return Optional.ofNullable(points)
+                .filter(p -> p.size() >= MIN_WIDTH)
+                .orElseThrow(() -> new IllegalArgumentException(MESSAGE_MIN_WIDTH));
     }
 
     private String isLine(boolean point) {
