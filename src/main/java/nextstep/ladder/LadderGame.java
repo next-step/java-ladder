703f3b07831b56f4cfb99b79cf9ef8b71b549f88
las9897 <las9897@gmail.com>
2021-12-15 09:27:45 +0900

    styles: 불필요한 공백 제거

--- src/main/java/nextstep/ladder/LadderGame.java
+++ src/main/java/nextstep/ladder/LadderGame.java
@@ -10,8 +10,6 @@ public class LadderGame {
     }
 
     public static void main(String[] args) {
-
-
         String inputNames = InputView.nextLine("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         int height = InputView.nextInt("최대 사다리 높이는 몇 개인가요?");
 
