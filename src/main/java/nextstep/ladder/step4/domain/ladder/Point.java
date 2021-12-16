c3a64fee16358745c8a0d0dfdaf3a03369edf35a
dongHyo <kdhyo98@gmail.com>
2021-12-17 02:56:38 +0900

    refactor : Point, Move 객체 생성

---
+++ src/main/java/nextstep/ladder/step4/domain/ladder/Point.java
@@ -0,0 +1,17 @@
+package nextstep.ladder.step4.domain.ladder;
+
+public class Point {
+
+    private final int index;
+    private final Direction direction;
+
+    public Point(int index, Direction direction) {
+        this.index = index;
+        this.direction = direction;
+    }
+
+    public int move() {
+        return Move.of(direction).move(this.index);
+    }
+
+}
