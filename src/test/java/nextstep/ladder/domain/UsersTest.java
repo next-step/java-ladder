e3499c6df7a94457cf8a54820233b5c8d33e38c3
Han <hankim@tapasmedia.co>
2021-12-16 21:17:20 +0900

    test : refactor variable

--- src/test/java/nextstep/ladder/domain/UsersTest.java
+++ src/test/java/nextstep/ladder/domain/UsersTest.java
@@ -3,17 +3,17 @@ package nextstep.ladder.domain;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author han
  */
 class UsersTest {
 
+    static final String NAMES = "pobi,honux,crong,jk";
+
     @Test
     void createByString() {
-        String input = "pobi,honux,crong,jk";
-        Users users = Users.createByString(input);
+        Users users = Users.createByString(NAMES);
         System.out.println(users);
         assertThat(users.getUsers().size()).isEqualTo(4);
     }
