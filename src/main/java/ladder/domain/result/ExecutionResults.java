d37ec0c41569ecc708f101b36f72697b77ecd19a
kangyonghyun <hmoon826@gmail.com>
2021-12-18 01:38:03 +0900

    (refactor) 결과값 출력 로직 이동

--- src/main/java/ladder/domain/result/ExecutionResults.java
+++ src/main/java/ladder/domain/result/ExecutionResults.java
@@ -1,26 +1,37 @@
 package ladder.domain.result;
 
+import ladder.domain.ladder.Ladder;
+import ladder.domain.user.LadderPlayers;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
 public class ExecutionResults {
 
     public static final String ERROR_EMPTY_MSG = "리스트가 비어있습니다..";
+
     private final List<String> results;
 
     public ExecutionResults(List<String> results) {
-        if (results == null || results.size() == 0) {
+        if (Objects.isNull(results) || results.isEmpty()) {
             throw new IllegalArgumentException(ERROR_EMPTY_MSG);
         }
         this.results = results;
     }
 
-    public List<String> getResults() {
-        return results;
+    public ExecutionResults executeGame(LadderPlayers players, Ladder ladder) {
+        List<String> gameResult = new ArrayList<>();
+        for (int index = 0; index < players.size(); index++) {
+            int findIndex = ladder.move(index);
+            gameResult.add(this.results.get(findIndex));
+        }
+        return new ExecutionResults(gameResult);
     }
 
-    public String getResults(int index) {
-        return results.get(index);
+    public List<String> getResults() {
+        return Collections.unmodifiableList(this.results);
     }
 
     @Override
@@ -31,8 +42,8 @@ public class ExecutionResults {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        ExecutionResults result = (ExecutionResults) o;
-        return Objects.equals(results, result.results);
+        ExecutionResults that = (ExecutionResults) o;
+        return Objects.equals(results, that.results);
     }
 
     @Override
