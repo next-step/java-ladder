abf40351897923433ee35582dd029b154fc871a9
dongHyo <kdhyo98@gmail.com>
2021-12-19 04:03:14 +0900

    refactor : 둘 다 true일 경우 강제로 false 변경 로직 추가로 인한 테스트케이스 삭제

--- src/test/java/nextstep/ladder/step4/domain/ladder/DirectionTest.java
+++ src/test/java/nextstep/ladder/step4/domain/ladder/DirectionTest.java
@@ -1,12 +1,9 @@
 package nextstep.ladder.step4.domain.ladder;
 
 import static java.lang.Boolean.FALSE;
-import static java.lang.Boolean.TRUE;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -25,13 +22,6 @@ public class DirectionTest {
     @DisplayName("last 테스트")
     void last(boolean left) {
         assertThat(Direction.first(left).last()).isEqualTo(Direction.first(left).next(FALSE));
-    }
-
-    @Test
-    @DisplayName("true 두 번 예외처리")
-    void trueNextTrueException() {
-        assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> Direction.first(TRUE).next(TRUE));
     }
 
 }
