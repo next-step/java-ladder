5ad762d27774beb7e9538cfc80c5ab106f76391b
monkeyDugi <lbd4946@gmail.com>
2021-12-18 18:53:30 +0900

    feat : 사다리 라인 생성

---
+++ src/main/java/ladder/domain/HorizontalLine.java
@@ -0,0 +1,30 @@
+package ladder.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class HorizontalLine {
+
+    private final List<Point> points;
+
+    public HorizontalLine(List<Point> points) {
+        this.points = points;
+    }
+
+    public int move() {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HorizontalLine that = (HorizontalLine) o;
+        return Objects.equals(points, that.points);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(points);
+    }
+}
---
+++ src/test/java/ladder/domain/LineGeneratorTest.java
@@ -0,0 +1,31 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LineGeneratorTest {
+
+    @Test
+    void 라인_생성() {
+        // given
+        int numberOfParticipant = 4;
+        LineGenerator lineGenerator = new LineGenerator();
+
+        // when
+        HorizontalLine line = lineGenerator.create(numberOfParticipant, () -> true);
+
+        // then
+        List<Point> points = new ArrayList<>();
+        Point first = Point.first(() -> true);
+        points.add(first);
+        points.add(first.next(() -> true));
+        points.add(first.next(() -> true).next(() -> true));
+        points.add(first.next(() -> true).next(() -> true).last());
+
+        assertThat(line).isEqualTo(new HorizontalLine(points));
+    }
+}
