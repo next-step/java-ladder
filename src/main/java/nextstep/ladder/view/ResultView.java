99ddd2bf0c6c3a135dc337d011c8197d2d0ed372
JaeHan-Kim <hgal8877@gmail.com>
2021-12-19 22:03:52 +0900

    feat : 게임 결과 출력 기능 추가

--- src/main/java/nextstep/ladder/view/ResultView.java
+++ src/main/java/nextstep/ladder/view/ResultView.java
@@ -1,12 +1,13 @@
 package nextstep.ladder.view;
 
-import nextstep.ladder.domain.Ladder;
-import nextstep.ladder.domain.Names;
-import nextstep.ladder.domain.Point;
+import nextstep.ladder.domain.*;
 
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ResultView {
+    private final static String LADDER_RUN_MESSAGE = "사다리 결과\n";
     private final static String GAME_RUN_MESSAGE = "실행결과\n";
     private final static String NAME_FORMAT = "%6s";
     private final static String LADDER_BLANK = "     ";
@@ -14,9 +15,16 @@ public class ResultView {
     private final static String LADDER_LINE = "|";
 
     public void gameResult(Names names, Ladder ladder) {
-        System.out.println(GAME_RUN_MESSAGE);
+        System.out.println(LADDER_RUN_MESSAGE);
         System.out.println(printEntryName(names));
         printLaddersResult(ladder);
+    }
+
+    public void printItems(Items items) {
+        System.out.println( items.getItems()
+                .stream()
+                .map(item -> String.format(NAME_FORMAT, item.getValue()))
+                .collect(Collectors.joining()));
     }
 
     private String printEntryName(Names names) {
@@ -28,7 +36,8 @@ public class ResultView {
 
     private void printLaddersResult(Ladder ladder) {
         ladder.getLadder().forEach(line -> {
-                    line.getLine().forEach(this::printPoint);
+                    IntStream.range(0, line.getLine().size() - 1)
+                            .forEach(index -> printPoint(line.getLine().get(index)));
                     System.out.println();
                 });
     }
@@ -39,5 +48,16 @@ public class ResultView {
             return;
         }
         System.out.printf("%s%s", LADDER_BLANK, LADDER_LINE);
+    }
+
+    public void printPlayerResult(Items items, int index) {
+        System.out.print(GAME_RUN_MESSAGE);
+        System.out.println(items.getItems().get(index).getValue());
+    }
+
+    public void printGameAllPlayerResult(GameResult gameResult, Items items) {
+        System.out.print(GAME_RUN_MESSAGE);
+        Map<Name, Integer> result = gameResult.getGameResult();
+        result.forEach((name, index) -> System.out.printf("%s : %s\n", name.getValue(), items.getItems().get(index).getValue()));
     }
 }
