c815d70192696c52090ef0fd20b4718f43951083
강호진 <blackpearl1210@gmail.com>
2021-12-19 17:22:17 +0900

    step3 입력, 출력, 실행 파트 수정

--- src/main/java/nextstep/ladder/LadderApplication.java
+++ src/main/java/nextstep/ladder/LadderApplication.java
@@ -1,13 +1,28 @@
 package nextstep.ladder;
 
+import nextstep.ladder.domain.*;
+import nextstep.ladder.strategy.PointStrategy;
+import nextstep.ladder.strategy.RandomPointStrategy;
+
+import static nextstep.ladder.view.InputView.*;
+import static nextstep.ladder.view.InputView.requestLadderLayer;
+import static nextstep.ladder.view.ResultView.*;
+
 public class LadderApplication {
     public static void main(String[] args) {
-//        LadderGame ladderGame = new LadderGame(InputView.requestMemberNames(), InputView.requestLadderLayer());
-//
-//        PointStrategy pointStrategy = new RandomPointStrategy();
-//
-//        ResultView.responseInitMessage();
-//        ResultView.responseLadderMembers(ladderGame.getMembers());
-//        ResultView.responseLadder(ladderGame.create(pointStrategy));
+        Members members = Members.from(requestMemberNames());
+        Rewards rewards = Rewards.of(requestRewards(), members);
+        Layer layer = new Layer(requestLadderLayer());
+
+        PointStrategy pointStrategy = new RandomPointStrategy();
+        LadderGame ladderGame = new LadderGame(members, layer);
+        Ladder ladder = ladderGame.create(pointStrategy);
+
+        responseInitMessage();
+        responseLadderMembers(members);
+        responseLadder(ladder);
+        responseRewards(rewards);
+
+        responseResults(requestResultMember(), ladderGame.start(ladder, rewards));
     }
 }
--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -3,18 +3,33 @@ package nextstep.ladder.view;
 import java.util.Scanner;
 
 public class InputView {
-//    private static final String REQUEST_MEMBER_NAME_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
-//    private static final String REQUEST_MAX_LADDER_LAYER = "최대 사다리 높이는 몇 개인가요?";
-//    private static final Scanner scanner = new Scanner(System.in);
-//
-//    public static String requestMemberNames() {
-//        System.out.println(REQUEST_MEMBER_NAME_MESSAGE);
-//        return scanner.next();
-//    }
-//
-//    public static int requestLadderLayer() {
-//        System.out.println();
-//        System.out.println(REQUEST_MAX_LADDER_LAYER);
-//        return scanner.nextInt();
-//    }
+    private static final String REQUEST_MEMBER_NAME_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String REQUEST_REWARDS_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요";
+    private static final String REQUEST_MAX_LADDER_LAYER = "최대 사다리 높이는 몇 개인가요?";
+    private static final String REQUEST_REWARDS_MEMBER_MESSAGE = "결과를 보고 싶은 사람은?";
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String requestMemberNames() {
+        System.out.println(REQUEST_MEMBER_NAME_MESSAGE);
+        return scanner.next();
+    }
+
+    public static String requestRewards() {
+        System.out.println();
+        System.out.println(REQUEST_REWARDS_MESSAGE);
+        return scanner.next();
+    }
+
+    public static int requestLadderLayer() {
+        System.out.println();
+        System.out.println(REQUEST_MAX_LADDER_LAYER);
+        return scanner.nextInt();
+    }
+
+    public static String requestResultMember() {
+        System.out.println();
+        System.out.println();
+        System.out.println(REQUEST_REWARDS_MEMBER_MESSAGE);
+        return scanner.next();
+    }
 }
