0399d5252c3648fa774fbd7c05841abe0151fa2b
JaeHan-Kim <hgal8877@gmail.com>
2021-12-19 20:45:25 +0900

    feat : 게임의 상품을 저장하는 값 객체 및 일급 컬렉션 추가

---
+++ src/main/java/nextstep/ladder/domain/Item.java
@@ -0,0 +1,13 @@
+package nextstep.ladder.domain;
+
+public class Item {
+    final String value;
+
+    public Item(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
