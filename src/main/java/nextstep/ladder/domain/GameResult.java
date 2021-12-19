5ffdcfba1c167c08ab7b452e901a4e2a1805aad3
JaeHan-Kim <hgal8877@gmail.com>
2021-12-19 22:00:16 +0900

    feat : 게임결과 도메인 추가

---
+++ src/main/java/nextstep/ladder/domain/GameResult.java
@@ -0,0 +1,30 @@
+package nextstep.ladder.domain;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+public class GameResult {
+    private final static int ADD_INDEX = 1;
+
+    private final Map<Name, Integer> result;
+
+    public GameResult(Ladder ladder, Names names) {
+        this.result =  new HashMap<>();
+        IntStream.range(0, names.getNames().size())
+                .forEach(index -> {
+                    Name name = names.getNames().get(index);
+                    int endPoint = ladder.discovery(index + ADD_INDEX);
+                    result.put(name, endPoint);
+                });
+    }
+
+    public Map<Name, Integer> getGameResult() {
+        return Collections.unmodifiableMap(result);
+    }
+
+    public Integer getResultIndex(Name name) {
+        return result.get(name);
+    }
+}
