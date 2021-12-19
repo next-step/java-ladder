f4cc47784485fad5e9c0e1ef31c5782f1ab1ff69
JaeHan-Kim <hgal8877@gmail.com>
2021-12-19 22:03:14 +0900

    feat : 사다리 게임 결과 입력 기능 추가

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class InputView {
     private final static String LADDER_GAME_ENTRY_MEMBER_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private final static String LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    private final static String INPUT_ITEMS_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private final static String INPUT_ENTRY_MEMBER_RESULT_ITEM = "결과를 보고 싶은 사람은?";
     private final static Scanner scanner = new Scanner(System.in);
 
     public InputView() {
@@ -17,6 +19,16 @@ public class InputView {
 
     public int inputLadderHeight() {
         System.out.println(LADDER_HEIGHT_MESSAGE);
-        return scanner.nextInt();
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public String inputItems() {
+        System.out.println(INPUT_ITEMS_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public String inputResultName() {
+        System.out.println(INPUT_ENTRY_MEMBER_RESULT_ITEM);
+        return scanner.nextLine();
     }
 }
