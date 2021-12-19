683bd9afe3534c4b55666e46878659fb1fef6d5f
JaeHan-Kim <hgal8877@gmail.com>
2021-12-19 20:43:18 +0900

    test : 포인트의 좌우 위치를 통해 다음 위치를 구하는 테스트 추가

--- src/test/java/nextstep/ladder/domain/PointTest.java
+++ src/test/java/nextstep/ladder/domain/PointTest.java
@@ -2,6 +2,8 @@ package nextstep.ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,5 +16,14 @@ class PointTest {
         Point point = new Point(false);
 
         assertThat(point).isNotNull();
+    }
+    @DisplayName("점의 좌우 이동으로 다음 위치를 구하는 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"false:true:1", "true:false:-1", "false:false:0"}, delimiter = ':')
+    void checkDirectionTest(boolean left, boolean current, int nextLocation) {
+        Point leftPoint = new Point(left);
+        Point currentPoint = new Point(current);
+
+        assertThat(currentPoint.checkDirection(leftPoint)).isEqualTo(nextLocation);
     }
 }
