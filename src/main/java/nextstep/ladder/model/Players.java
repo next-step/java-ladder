be0e46c52d5c736ce4158b398f71cbaf3606c70a
jeje <las9897@gmail.com>
2021-12-22 13:30:56 +0900

    feat: 사다리타기 게임 결과 Report 생성

--- src/main/java/nextstep/ladder/model/Players.java
+++ src/main/java/nextstep/ladder/model/Players.java
@@ -7,7 +7,7 @@ import java.util.stream.IntStream;
 public class Players {
 
     private static final String MESSAGE_CANT_PLAY_THE_GAME_ALONE = "혼자서는 사다리게임을 할 수 없습니다.";
-    private static final String EXCEPTION_NOT_FOUND_PLAYER = "Player를 찾을 수 없습니다.";
+    public static final String EXCEPTION_NOT_FOUND_PLAYER = "Player를 찾을 수 없습니다.";
     private static final int PLAYERS_MIN_SIZE = 1;
 
     private final List<Player> players;
---
+++ src/test/java/nextstep/ladder/model/ReportTest.java
@@ -0,0 +1,45 @@
+package nextstep.ladder.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ReportTest {
+
+    Report report;
+    Players players;
+    Results results;
+
+    @BeforeEach
+    void setUp() {
+        String[] splitNames = "pobi,honux,crong,jk".split(",");
+        String[] splitResults = "꽝,5000,꽝,꽝".split(",");
+
+        players = new Players(splitNames);
+        results = new Results(splitResults, splitNames.length);
+        report = new Report(players, results);
+    }
+
+    @Test
+    @DisplayName("Report 생성")
+    void constructor() {
+        Index zeroIndex = new Index();
+        Player player = players.findPlayerByIndex(zeroIndex);
+        Result result = results.findResultByIndex(zeroIndex);
+
+        assertThat(report.getPlayerResults().get(player)).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"pobi,0,꽝", "honux,1,5000", "crong,2,꽝", "jk,3,꽝"})
+    @DisplayName("이름으로 Result 찾기")
+    void findResultByPlayer(String playerName, int index, String resultName) {
+        Result result = report.findResultByPlayerName(playerName);
+
+        assertThat(result).isEqualTo(new Result(resultName, new Index(index)));
+    }
+}
