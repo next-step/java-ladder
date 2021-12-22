0c7790e405a32cb8e1d97cb1d515ebf1400146c0
kangyonghyun <hmoon826@gmail.com>
2021-12-22 15:48:28 +0900

    (refactor) 테스트 given절은 setUp으로 이동

--- src/test/java/ladder/strategy/DefaultPlayResultTest.java
+++ src/test/java/ladder/strategy/DefaultPlayResultTest.java
@@ -1,13 +1,15 @@
 package ladder.strategy;
 
+import ladder.domain.dto.LadderInfoDto;
 import ladder.domain.ladder.Ladder;
 import ladder.domain.ladder.LadderHeight;
-import ladder.domain.result.ExecutionResults;
 import ladder.domain.user.LadderPlayers;
+import ladder.domain.user.PlayerName;
 import ladder.generator.DefaultLadderGenerator;
 import ladder.generator.DefaultLineGenerator;
 import ladder.generator.LadderGenerator;
 import ladder.generator.LineGenerator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -21,30 +23,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class DefaultPlayResultTest {
 
+    LadderInfoDto infoDto;
+    Ladder ladder;
+
+    @BeforeEach
+    void setUp() {
+        infoDto = new LadderInfoDto(Arrays.asList("pobi", "honux", "crong"), Arrays.asList("꽝", "5000", "10000"), 3);
+        LadderPlayers ladderPlayers = infoDto.getLadderPlayers();
+        LadderHeight height = infoDto.getHeight();
+        LineGenerator lineGenerator = new DefaultLineGenerator(() -> true);
+        LadderGenerator generator = new DefaultLadderGenerator(lineGenerator);
+        ladder = generator.generate(ladderPlayers.size(), height);
+    }
+
     @ParameterizedTest
     @MethodSource("provideIndexValue")
     @DisplayName("사다리 게힘 실행 시, 모든 결과항목 반환 - 결과에 맞는 항목들 Map 으로 반환")
-    void createAllResults(String name, String expectedItem) {
-        LadderPlayers players = new LadderPlayers(Arrays.asList("pobi", "honux", "crong"));
-        LineGenerator lineGenerator = new DefaultLineGenerator(() -> true);
-        LadderHeight height = new LadderHeight(3);
-        LadderGenerator generator = new DefaultLadderGenerator(lineGenerator, height);
-        Ladder ladder = generator.generate(players.size());
-        ExecutionResults items = new ExecutionResults(Arrays.asList("꽝", "5000", "10000"));
-
-        PlayResult result = new DefaultPlayResult(ladder);
-        Map<String, String> results = result.play(players, items);
+    void createAllResults(PlayerName name, String expectedItem) {
+        PlayResult result = new DefaultPlayResult();
+        Map<PlayerName, String> results = result.play(ladder, infoDto);
 
         assertThat(results).size().isEqualTo(3);
-        assertThat(results.get(name)).isEqualTo(expectedItem);
         assertThat(results.get(name)).isEqualTo(expectedItem);
     }
 
     private static Stream<Arguments> provideIndexValue() {
         return Stream.of(
-                Arguments.of("pobi", "5000"),
-                Arguments.of("honux", "꽝"),
-                Arguments.of("crong", "10000")
+                Arguments.of(new PlayerName("pobi"), "5000"),
+                Arguments.of(new PlayerName("honux"), "꽝"),
+                Arguments.of(new PlayerName("crong"), "10000")
         );
     }
 
