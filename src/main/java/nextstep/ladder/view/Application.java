000f61316265cf5b73e7d580af07bab4d3fa1414
orgojy <giveawesome@gmail.com>
2021-12-22 14:21:03 +0900

    feat: 사다리 게임 실행결과 출력 기능 `OutputView` 추가

--- src/main/java/nextstep/ladder/view/Application.java
+++ src/main/java/nextstep/ladder/view/Application.java
@@ -4,5 +4,6 @@ public class Application {
     public static void main(String[] args) {
         String participantNames = InputView.readParticipantNames();
         int heightOfLadder = InputView.readHeightOfLadder();
+        OutputView.printParticipantNames();
     }
 }
---
+++ src/main/java/nextstep/ladder/view/OutputView.java
@@ -0,0 +1,18 @@
+package nextstep.ladder.view;
+
+public final class OutputView {
+
+    private OutputView() {
+    }
+
+    public static void printParticipantNames() {
+        System.out.println("\n실행결과\n" +
+                "\n" +
+                "pobi  honux crong   jk\n" +
+                "    |-----|     |-----|\n" +
+                "    |     |-----|     |\n" +
+                "    |-----|     |     |\n" +
+                "    |     |-----|     |\n" +
+                "    |-----|     |-----|");
+    }
+}
