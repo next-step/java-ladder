b4c46d6d40b3f3b69be05ac1c5f389b523d72841
orgojy <giveawesome@gmail.com>
2021-12-22 22:28:58 +0900

    feat: 사다리 생성 검증 기능 추가

--- src/main/java/nextstep/ladder/model/Ladder.java
+++ src/main/java/nextstep/ladder/model/Ladder.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public final class Ladder {
+    public static final int MIN_USER_SIZE = 1;
+    public static final int MIN_HEIGHT = 1;
 
     private final List<LadderLine> ladders;
 
@@ -15,11 +17,21 @@ public final class Ladder {
     }
 
     public static Ladder initate(int userSize, int height) {
+        validate(userSize, height);
         return new Ladder(ladderLines(userSize, height));
     }
 
     public List<LadderLine> getLadders() {
         return Collections.unmodifiableList(ladders);
+    }
+
+    private static void validate(int userSize, int height) {
+        if (userSize < MIN_USER_SIZE) {
+            throw new IllegalArgumentException(String.format("전달된 사용자의 수(%d)는 %d이상이 필요합니다.", userSize, MIN_USER_SIZE));
+        }
+        if (height < MIN_HEIGHT) {
+            throw new IllegalArgumentException(String.format("전달된 사다리의 높이(%d)는 %d이상이 필요합니다.", height, MIN_HEIGHT));
+        }
     }
 
     private static List<LadderLine> ladderLines(int userSize, int height) {
