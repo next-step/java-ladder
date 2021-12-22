4f839fe2fe2cbc94ca37b2eeb6600a45fa8224e7
gyuyeol <gyuyeol.lee@live.com>
2021-12-22 20:08:09 +0900

    feat(Player): Players 구현

---
+++ src/test/java/rick/domain/PlayersTest.java
@@ -0,0 +1,19 @@
+package rick.domain;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+class PlayersTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("null, empty 리스트")
+    void nullAndEmptyName(final List<String> names) {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new Players(names));
+    }
+}
