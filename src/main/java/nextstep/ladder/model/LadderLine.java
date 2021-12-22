7a4c73bd6731a23e6884f41aa94a81fa76b445ee
orgojy <giveawesome@gmail.com>
2021-12-23 01:43:05 +0900

    feat: 사다리 연결 라인 생성 기능 추가

--- src/main/java/nextstep/ladder/model/LadderLine.java
+++ src/main/java/nextstep/ladder/model/LadderLine.java
@@ -1,7 +1,43 @@
 package nextstep.ladder.model;
 
-public class LadderLine {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public final class LadderLine {
+    private static final RandomGenerator RANDOM_GENERATOR = new RandomGenerator();
+
+    private final List<LadderPoint> points;
+
+    private LadderLine(List<LadderPoint> points) {
+        this.points = new ArrayList<>(points);
+    }
+
     public static LadderLine init(int userSize) {
-        return new LadderLine();
+        List<LadderPoint> points = new ArrayList<>(userSize);
+        LadderPoint firstPoint = firstPoint();
+        points.add(firstPoint);
+        points.add(lastPoint(middlePoint(userSize, points, firstPoint)));
+        return new LadderLine(points);
+    }
+
+    private static LadderPoint firstPoint() {
+        return LadderPoint.first(RANDOM_GENERATOR.trueOrFalse());
+    }
+
+    private static LadderPoint middlePoint(int userSize, List<LadderPoint> points, LadderPoint point) {
+        for (int i = 1; i < userSize - 1; i++) {
+            point = point.next();
+            points.add(point);
+        }
+        return point;
+    }
+
+    private static LadderPoint lastPoint(LadderPoint point) {
+        return point.last();
+    }
+
+    public List<LadderPoint> getPoints() {
+        return Collections.unmodifiableList(points);
     }
 }
