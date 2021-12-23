d2499a31c64d7155622b01c95a0764ba4cb545a8
sojeongw <sojeongw.official@gmail.com>
2021-12-24 01:33:32 +0900

    feat: 인원 수 만큼 가로 라인에 칸을 만든다

---
+++ src/main/java/nextstep/ladder/Line.java
@@ -0,0 +1,41 @@
+package nextstep.ladder;
+
+import java.util.Objects;
+
+public class Line {
+    public static final String PARTICIPANTS_LIMIT_MESSAGE = "참가자는 2명 이상이어야 합니다.";
+    public static final int MIN_PARTICIPANTS = 2;
+    private final Points points;
+
+    private Line(Points points) {
+        this.points = points;
+    }
+
+    public static Line from(int numberOfParticipants) {
+        if (numberOfParticipants < MIN_PARTICIPANTS) {
+            throw new IllegalArgumentException(PARTICIPANTS_LIMIT_MESSAGE);
+        }
+
+        return new Line(Points.of(numberOfParticipants, new RandomStrategy()));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Line line = (Line) o;
+        return Objects.equals(points, line.points);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(points);
+    }
+
+    @Override
+    public String toString() {
+        return "Line{" +
+                "points=" + points +
+                '}';
+    }
+}
---
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -0,0 +1,24 @@
+package nextstep.ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class LineTest {
+
+    @Test
+    @DisplayName("인원 수만큼 가로 라인을 그려서 사다리를 만든다")
+    void shouldCreate() {
+        Line line = Line.from(2);
+        assertThat(line).isEqualTo(Line.from(2));
+    }
+
+    @Test
+    @DisplayName("참가자가 없거나 1명이면 IllegalArgumentException을 던진다")
+    void shouldThrowWhenInvalidNumberOfParticipants() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Line.from(1));
+    }
+}
