51bea3a0a19c56eed323ffeb9e1edf5a0f1d404f
sojeongw <sojeongw.official@gmail.com>
2021-12-24 03:50:05 +0900

    fix: 불필요한 값이 추가되는 버그를 수정한다

--- src/main/java/nextstep/ladder/domain/ladder/strategy/RandomStrategy.java
+++ src/main/java/nextstep/ladder/domain/ladder/strategy/RandomStrategy.java
@@ -5,21 +5,21 @@ import java.util.List;
 import java.util.Random;
 
 public class RandomStrategy implements Strategy {
-    private final List<Boolean> points = new ArrayList<>();
     private final Random random = new Random();
 
     @Override
     public List<Boolean> points(int numberOfParticipants) {
+        List<Boolean> points = new ArrayList<>();
         points.add(random.nextBoolean());
 
-        for (int i = 1; i < numberOfParticipants; i++) {
-            points.add(newPoint(i));
+        for (int i = 1; i < numberOfParticipants - 1; i++) {
+            points.add(newPoint(points, i));
         }
 
         return points;
     }
 
-    private boolean newPoint(int i) {
+    private boolean newPoint(List<Boolean> points, int i) {
         Boolean previousValue = points.get(i - 1);
 
         if (previousValue) {
