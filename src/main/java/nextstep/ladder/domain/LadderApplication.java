81d3f2cce6be34ee9a4084f243555b938e79192e
sojeongw <sojeongw.official@gmail.com>
2021-12-24 03:54:16 +0900

    feat: 결과를 출력한다

--- src/main/java/nextstep/ladder/domain/LadderApplication.java
+++ src/main/java/nextstep/ladder/domain/LadderApplication.java
@@ -1,16 +1,19 @@
 package nextstep.ladder.domain;
 
-import nextstep.ladder.InputView;
 import nextstep.ladder.domain.ladder.Ladder;
 import nextstep.ladder.domain.ladder.strategy.RandomStrategy;
 import nextstep.ladder.domain.participant.Participants;
+import nextstep.ladder.domain.view.InputView;
+import nextstep.ladder.domain.view.ResultView;
 
 public class LadderApplication {
     public static void main(String[] args) {
         Participants participants = InputView.participants();
         int heightOfLadder = InputView.heightOfLadder();
-        Condition condition = Condition.of(participants, heightOfLadder);
 
-        Ladder.from(condition, new RandomStrategy());
+        Condition condition = Condition.of(participants, heightOfLadder);
+        Ladder ladder = Ladder.from(condition, new RandomStrategy());
+
+        ResultView.result(participants, ladder);
     }
 }
