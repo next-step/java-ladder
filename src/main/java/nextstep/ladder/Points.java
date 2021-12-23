bf6e233f7040f13011ab989d9b0d64394baa7fcf
sojeongw <sojeongw.official@gmail.com>
2021-12-24 01:32:58 +0900

    feat: 발판이 가로로 겹치지 않도록 랜덤하게 생성한다

---
+++ src/main/java/nextstep/ladder/Points.java
@@ -0,0 +1,41 @@
+package nextstep.ladder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Points {
+    private final List<Boolean> values = new ArrayList<>();
+
+    private Points(List<Boolean> points) {
+        this.values.addAll(points);
+    }
+
+    public static Points from(List<Boolean> points) {
+        return new Points(points);
+    }
+
+    public static Points of(int numberOfParticipants, Strategy strategy) {
+        return new Points(strategy.points(numberOfParticipants));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Points points = (Points) o;
+        return Objects.equals(values, points.values);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(values);
+    }
+
+    @Override
+    public String toString() {
+        return "Points{" +
+                "values=" + values +
+                '}';
+    }
+}
---
+++ src/test/java/nextstep/ladder/PointsTest.java
@@ -0,0 +1,21 @@
+package nextstep.ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PointsTest {
+
+    @Test
+    @DisplayName("인원 수만큼 해당 지점에 대한 사다리 계단 존재 유무를 생성한다")
+    void shouldCreate() {
+        List<Boolean> values = Arrays.asList(true, false, true);
+        Points points = Points.of(3, numberOfParticipants -> values);
+
+        assertThat(points).isEqualTo(Points.from(values));
+    }
+}
