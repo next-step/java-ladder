6a1be22fd851116fcd5d4fc1c6bdca6ced5d64a4
hyenny <qlwb9191@naver.com>
2021-12-24 13:25:05 +0900

    feat: Point 테스트 케이스 정리

---
+++ src/test/java/ladderinterface/PointTest.java
@@ -0,0 +1,32 @@
+package ladderinterface;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PointTest {
+
+    @DisplayName("첫번째 점의 위치는 0이고 방향을 가짐")
+    @Test
+    void first() {
+
+    }
+
+    @DisplayName("왼쪽으로 이동시 위치가 -1")
+    @Test
+    void move_left() {
+
+    }
+
+    @DisplayName("오른쪽으로 이동시 위치가 +1")
+    @Test
+    void move_right() {
+
+    }
+
+    @DisplayName("아래로 이동시 현재 위치 반환")
+    @Test
+    void move_down() {
+
+    }
+
+}
