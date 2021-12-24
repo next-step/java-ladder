efec4bb8a9966010dea649ba2a6079cae7f94799
hyenny <qlwb9191@naver.com>
2021-12-24 13:19:47 +0900

    feat: Direction 구현

--- src/main/java/ladderinterface/concrete/Direction.java
+++ src/main/java/ladderinterface/concrete/Direction.java
@@ -1,10 +1,15 @@
 package ladderinterface.concrete;
 
+import java.util.Objects;
+
 public class Direction {
     private final boolean prev;
     private final boolean current;
 
     private Direction(boolean prev, boolean current) {
+        if (prev && current) {
+            throw new IllegalArgumentException("라인이 겹치지 않아야 합니다.");
+        }
         this.prev = prev;
         this.current = current;
     }
@@ -17,4 +22,36 @@ public class Direction {
         return new Direction(false, current);
     }
 
+    public Direction next(boolean current) {
+        return new Direction(this.current, current);
+    }
+
+    public Direction last() {
+        return new Direction(this.current, false);
+    }
+
+    public boolean isLeft() {
+        return prev && !current;
+    }
+
+    public boolean isRight() {
+        return !prev && current;
+    }
+
+    public boolean isDown() {
+        return !prev && !current;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Direction direction = (Direction) o;
+        return prev == direction.prev && current == direction.current;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(prev, current);
+    }
 }
--- src/test/java/ladderinterface/DirectionTest.java
+++ src/test/java/ladderinterface/DirectionTest.java
@@ -5,13 +5,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class DirectionTest {
 
     @DisplayName("라인이 겹치면 예외 : 이전 점과 현재 점이 true, true")
     @Test
     void 라인겹침예외() {
-
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Direction.first(true).next(true))
+                .withMessage("라인이 겹치지 않아야 합니다.");
     }
 
     @DisplayName("첫번째 점은 이전 점이 무조건 false")
@@ -24,25 +27,29 @@ public class DirectionTest {
     @DisplayName("마지막 점은 현재 점이 무조건 false")
     @Test
     void last() {
-
+        Direction direction = Direction.first(true).last();
+        assertThat(direction).isEqualTo(Direction.of(true, false));
     }
 
     @DisplayName("왼쪽 방향으로 가려면 이전 점이 true, 현재 점이 false")
     @Test
     void left() {
-
+        Direction direction = Direction.first(true).next(false);
+        assertThat(direction.isLeft()).isTrue();
     }
 
     @DisplayName("오른쪽 방향으로 가려면 이전 점이 false, 현재 점이 true")
     @Test
     void right() {
-
+        Direction direction = Direction.first(false).next(true);
+        assertThat(direction.isRight()).isTrue();
     }
 
     @DisplayName("아래 방향으로 가려면 이전 점이 false, 현재 점이 false")
     @Test
     void down() {
-
+        Direction direction = Direction.first(false).next(false);
+        assertThat(direction.isDown()).isTrue();
     }
 
 }
