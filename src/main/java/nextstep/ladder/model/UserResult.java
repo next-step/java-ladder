8c949833334265ceea9eb8a2702c630bc9419573
orgojy <giveawesome@gmail.com>
2021-12-24 21:45:34 +0900

    feat: 개별 실행 결과 `UserResult` 생성 검증 기능 추가

--- src/main/java/nextstep/ladder/model/UserResult.java
+++ src/main/java/nextstep/ladder/model/UserResult.java
@@ -1,11 +1,14 @@
 package nextstep.ladder.model;
 
+import java.util.Objects;
+
 public final class UserResult {
 
     private final User user;
     private final LadderResult result;
 
     public UserResult(User user, LadderResult result) {
+        validate(user, result);
         this.user = user;
         this.result = result;
     }
@@ -16,5 +19,14 @@ public final class UserResult {
 
     public LadderResult getResult() {
         return result;
+    }
+
+    private void validate(User user, LadderResult result) {
+        if (Objects.isNull(user)) {
+            throw new IllegalArgumentException("사용자 결과에 필요한 사용자 정보가 null입니다.");
+        }
+        if (Objects.isNull(result)) {
+            throw new IllegalArgumentException("사용자의 사다리 타기 결과가 null입니다.");
+        }
     }
 }
--- src/test/java/nextstep/ladder/model/UserResultTest.java
+++ src/test/java/nextstep/ladder/model/UserResultTest.java
@@ -3,6 +3,7 @@ package nextstep.ladder.model;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class UserResultTest {
     public static final UserResult USER_RESULT_WITH_BANG = new UserResult(UserTest.CRONG, LadderResultTest.BANG);
@@ -12,6 +13,22 @@ public class UserResultTest {
     void createTest() {
         // then
         assertThat(USER_RESULT_WITH_BANG).isNotNull();
+    }
+
+    @Test
+    void checkExceptionWithNullUserTest() {
+        // when & then
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> new UserResult(null, LadderResultTest.BANG)
+        );
+    }
+
+    @Test
+    void checkExceptionWithNullLadderResultTest() {
+        // when & then
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> new UserResult(UserTest.RYAN, null)
+        );
     }
 
     @Test
