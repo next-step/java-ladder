227e290e0a9340e7eab00f1a5b3e1e705bd83c43
jeje <las9897@gmail.com>
2021-12-24 19:44:16 +0900

    fix: 랜덤성 테스트 제거

--- src/test/java/nextstep/ladder/LadderGameTest.java
+++ src/test/java/nextstep/ladder/LadderGameTest.java
@@ -16,26 +16,6 @@ class LadderGameTest {
 
     private static final String DELIMITER = ",";
 
-    @Test
-    @DisplayName("랜덤으로 잘 그려지는지 테스트")
-    void ladderDrawTest() {
-        String inputNames = "pobi,honux,crong,jk,adsfa,qfadg,qszcq,dafda,11231";
-        String[] splitNames = StringUtils.validationNotNullAndEmpty(inputNames).split(DELIMITER);
-        Players players = new Players(splitNames);
-        int width = splitNames.length;
-
-        String inputResults = "꽝,5000,꽝,꽝,꽝,꽝,꽝,꽝,10000";
-        String[] splitResults = StringUtils.validationNotNullAndEmpty(inputResults).split(DELIMITER);
-        Results results = new Results(splitResults, width);
-
-        int height = 10;
-        Lines lines = LinesFactory.of(width, height);
-
-        Ladder ladder = new Ladder(players, lines, results);
-
-        OutputView.print(ladder);
-    }
-
     @ParameterizedTest
     @CsvSource(value = {"0,pobi", "1,honux", "2,crong", "3,jk"})
     @DisplayName("통합 테스트")
