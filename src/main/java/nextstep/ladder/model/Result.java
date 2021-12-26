6b246c5fffe30941b9f97e9705c63432e7960911
jeje <las9897@gmail.com>
2021-12-26 22:44:06 +0900

    styles: 안쓰는 생성자 삭제

--- src/main/java/nextstep/ladder/model/Result.java
+++ src/main/java/nextstep/ladder/model/Result.java
@@ -6,11 +6,6 @@ public class Result extends Name {
 
     private final Index index;
 
-    public Result(String name) {
-        super(name);
-        this.index = new Index();
-    }
-
     public Result(String name, Index index) {
         super(name);
         this.index = index;
