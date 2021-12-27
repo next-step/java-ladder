06ab7c2a3f91cf8599678fc0b212658daf84b979
Han <hankim@tapasmedia.co>
2021-12-27 17:34:09 +0900

    feat : toPlay()

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -1,5 +1,10 @@
 package nextstep.ladder.domain;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author han
  */
@@ -8,7 +13,7 @@ public class Ladder {
     private final Lines lines;
     private final Results results;
 
-    private Ladder(Users users, Lines lines, Results results) {
+    public Ladder(Users users, Lines lines, Results results) {
         this.users = users;
         this.lines = lines;
         this.results = results;
@@ -16,6 +21,13 @@ public class Ladder {
 
     public static Ladder from(Users users, Height height, Results results) {
         return new Ladder(users, Lines.of(users, height), results);
+    }
+
+    public Results getResultsBy(User user) {
+        if (user.isAll()) {
+            return results;
+        }
+        return Results.of(results.getResultBy(users.getIndexBy(user)));
     }
 
     public Users getUsers() {
@@ -28,5 +40,42 @@ public class Ladder {
 
     public Results getResults() {
         return results;
+    }
+
+    public List<Position> toPlay() {
+        Line line = this.lines.getLines().get(0);
+        List<Position> init = init(line);
+
+        for (int i = 1; i < this.lines.getLines().size(); i++) {
+            Line line1 = this.lines.getLines().get(i);
+            init = next(init, line1);
+        }
+
+        return init;
+    }
+
+    private List<Position> init(Line line) {
+        List<Position> init = new ArrayList<>();
+
+        for (int i = 0; i < line.getPoints().size(); i++) {
+            Point point = line.getPoints().get(i);
+            Position position = new Position(i, point, users.getUsers().get(i));
+            init.add(position.move());
+        }
+        List<Position> collect = init.stream().sorted(Comparator.comparing(Position::getIndex)).collect(Collectors.toList());
+        return collect;
+    }
+
+    private List<Position> next(List<Position> positions, Line line) {
+        List<Position> init = new ArrayList<>();
+
+        for (int i = 0; i < line.getPoints().size(); i++) {
+            Point point = line.getPoints().get(i);
+            Position position1 = positions.get(i);
+            Position position = new Position(position1.getIndex(), point, position1.getUser());
+            init.add(position.move());
+        }
+        List<Position> collect = init.stream().sorted(Comparator.comparing(Position::getIndex)).collect(Collectors.toList());
+        return collect;
     }
 }
--- src/test/java/nextstep/ladder/domain/LadderTest.java
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -1,7 +1,16 @@
 package nextstep.ladder.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 /**
@@ -15,5 +24,28 @@ class LadderTest {
         Height height = new Height(5);
         Results results = Results.createByString(ResultsTest.RESULTS);
         assertDoesNotThrow(() -> Ladder.from(users, height, results));
+    }
+
+    @ParameterizedTest
+    @DisplayName("test")
+    @MethodSource("provideLines")
+    void toPlay(Lines lines) {
+        Users users = Users.createByString(UsersTest.NAMES);
+        Results results = Results.createByString(ResultsTest.RESULTS);
+
+        Ladder ladder = new Ladder(users, lines, results);
+        List<Position> positions = ladder.toPlay();
+        assertThat(positions.get(0).getUser().getName()).isEqualTo("jk");
+        assertThat(positions.get(1).getUser().getName()).isEqualTo("honux");
+        assertThat(positions.get(2).getUser().getName()).isEqualTo("crong");
+        assertThat(positions.get(3).getUser().getName()).isEqualTo("pobi");
+    }
+
+    private static Stream<Arguments> provideLines() {
+        return Stream.of(
+            Arguments.of(new Lines(Arrays.asList(
+                new Line(Arrays.asList(new Point(false, true), new Point(true, false), new Point(false, true), new Point(true, false))),
+                new Line(Arrays.asList(new Point(false, false), new Point(false, true), new Point(true, false), new Point(false, false))),
+                (new Line(Arrays.asList(new Point(false, true), new Point(true, false), new Point(false, true), new Point(true, false))))))));
     }
 }
