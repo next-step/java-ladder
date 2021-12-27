d96ab2191ed64ea856e4cfe510ad223f3047c42a
soosue <yunsoo0510@daum.net>
2021-12-27 14:28:04 +0900

    refactor : PlayerCount(원시값포장) 테스트 코드 작성 및 구현

---
+++ src/test/java/ladder/domain/PlayerCountTest.java
@@ -0,0 +1,20 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PlayerCountTest {
+    @Test
+    void 포지션_2이상의_수로_생성가능하다_동등성가짐() {
+        assertThat(new PlayerCount(2)).isEqualTo(new PlayerCount(2));
+    }
+
+    @Test
+    void 포지션_생성_1이하의수를_가질수없다() {
+        assertThatThrownBy(() -> new PlayerCount(1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(PlayerCount.INVALID_VALUE_MESSAGE);
+    }
+}
