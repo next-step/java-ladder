6bf5d8c161f52e755d70409664885042e89d9485
soosue <yunsoo0510@daum.net>
2021-12-27 15:25:01 +0900

    refactor : Player 테스트 코드 작성 및 구현

---
+++ src/main/java/ladderstep4/ladder/domain/Player.java
@@ -0,0 +1,33 @@
+package ladderstep4.ladder.domain;
+
+import java.util.Objects;
+
+public class Player {
+    public static final String INVALID_NAME_MESSAGE = "이름은 1~5글자입니다.";
+
+    private static final int MAX_LENGTH = 5;
+
+    private final String name;
+
+    public Player(String name) {
+        if (Objects.isNull(name) || name.trim().isEmpty() || name.trim().length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(INVALID_NAME_MESSAGE);
+        }
+        this.name = name.trim();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Player player = (Player) o;
+
+        return name != null ? name.equals(player.name) : player.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+}
