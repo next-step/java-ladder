83a2f24c0c9964427ab674b2a27359b002bc9d44
DahamLeee <dlek1011@gmail.com>
2022-04-27 11:20:09 +0900

    feat [CREATE] UserName Class

---
+++ src/main/java/nextstep/ladder/UserName.java
@@ -0,0 +1,25 @@
+package nextstep.ladder;
+
+import java.util.Objects;
+
+public class UserName {
+
+    private String userName;
+
+    public UserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserName userName1 = (UserName) o;
+        return Objects.equals(userName, userName1.userName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userName);
+    }
+}
---
+++ src/test/java/nextstep/ladder/UserNameTest.java
@@ -0,0 +1,14 @@
+package nextstep.ladder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class UserNameTest {
+
+    @Test
+    void create() {
+        UserName userName = new UserName("wang");
+        assertThat(userName).isEqualTo(new UserName("wang"));
+    }
+}
