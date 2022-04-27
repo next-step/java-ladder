fc44c51475801d21009e683e3d846993fbafb095
cooper <pbg0205@naver.com>
2022-04-28 01:32:36 +0900

    feat: Ladder 생성 로직 추가

---
+++ src/main/java/ladder/model/Ladder.java
@@ -0,0 +1,22 @@
+package ladder.model;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Ladder {
+
+    private final List<Line> lines;
+
+    private Ladder(List<Line> lines) {
+        this.lines = Collections.unmodifiableList(lines);
+    }
+
+    public static Ladder of(int width, int height, GenerationStrategy generationStrategy) {
+        return IntStream.range(0, height)
+                .mapToObj(index -> Line.of(width, index, generationStrategy))
+                .collect(Collectors.collectingAndThen(Collectors.toList(), Ladder::new));
+    }
+
+}
