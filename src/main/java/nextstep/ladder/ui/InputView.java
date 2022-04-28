888e8daddbfe57853bd11bf1e3bc9f73c3a3130e
imcool2551 <imcool2551@gmail.com>
2022-04-29 00:01:12 +0900

    refactor+bugfix: InputView
    상수를 대문자로 표현했습니다.
    split 메서드가 공백을 제거하지 않도록 했습니다.

--- src/main/java/nextstep/ladder/ui/InputView.java
+++ src/main/java/nextstep/ladder/ui/InputView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     private InputView() {
         throw new UnsupportedOperationException();
@@ -13,11 +13,11 @@ public class InputView {
 
     public static List<String> promptNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        return List.of(scanner.nextLine().split(","));
+        return List.of(SCANNER.nextLine().split(",", -1));
     }
 
     public static int promptHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        return Integer.parseInt(scanner.nextLine());
+        return Integer.parseInt(SCANNER.nextLine());
     }
 }
