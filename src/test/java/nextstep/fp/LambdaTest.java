07eedeefd6abeea8e096c58bb731b7827c4cdc08
yonghee <54215394+devyonghee@users.noreply.github.com>
2022-04-29 16:22:53 +0900

    1단계 - 자바8 스트림, 람다, Optional (#1332)
    * refactor(CarTest) : 람다 변환
    
    * refactor(Lambda) : 중복 합산 로직 제거
    
    * feat(StreamStudy) : 스트림 실습
    
    * feat(User) : ageIsInRange2 구현
    
    * refactor(Users) : getUser(String name) 메소드를 stream, Optional 로 변경
    
    * refactor(Expression) : stream, Optional 사용으로 변경

--- src/test/java/nextstep/fp/LambdaTest.java
+++ src/test/java/nextstep/fp/LambdaTest.java
@@ -8,43 +8,44 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class LambdaTest {
+class LambdaTest {
+
     private List<Integer> numbers;
 
     @BeforeEach
-    public void setup() {
+    void setup() {
         numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
     }
 
     @Test
-    public void printAllOld() throws Exception {
+    void printAllOld() {
         Lambda.printAllOld(numbers);
     }
 
     @Test
-    public void printAllLambda() throws Exception {
+    void printAllLambda() {
         Lambda.printAllLambda(numbers);
     }
 
     @Test
-    public void runThread() throws Exception {
+    void runThread() {
         Lambda.runThread();
     }
 
     @Test
-    public void sumAll() throws Exception {
+    void sumAll() {
         int sum = Lambda.sumAll(numbers);
         assertThat(sum).isEqualTo(21);
     }
 
     @Test
-    public void sumAllEven() throws Exception {
+    void sumAllEven() {
         int sum = Lambda.sumAllEven(numbers);
         assertThat(sum).isEqualTo(12);
     }
 
     @Test
-    public void sumAllOverThree() throws Exception {
+    void sumAllOverThree() {
         int sum = Lambda.sumAllOverThree(numbers);
         assertThat(sum).isEqualTo(15);
     }
