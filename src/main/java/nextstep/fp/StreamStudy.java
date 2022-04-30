f8d82d5c6d554d2f640302f4f64a163c8354c8fc
younghun <asqwklop12@naver.com>
2022-04-30 12:18:45 +0900

    feat List에 담긴 모든 숫자 중 3보다 큰 숫자를 2배 한 후 모든 값의 합을 구한다.

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,8 +23,13 @@ public class StreamStudy {
     String contents = Files.readString(Paths
         .get("src/main/resources/fp/war-and-peace.txt"));
     List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
-
-    // TODO 이 부분에 구현한다.
+    words
+        .stream()
+        .filter(word -> 12 < word.length())
+        .sorted(Comparator.comparing(String::length).reversed())
+        .distinct()
+        .limit(100)
+        .forEach(System.out::println);
   }
 
   public static List<Integer> doubleNumbers(List<Integer> numbers) {
