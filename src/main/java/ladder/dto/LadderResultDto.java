70d8de5556de743f35755751f43d375d022b6cf5
cooper <pbg0205@naver.com>
2022-04-30 17:43:15 +0900

    feat: 사다리게임 결과를 위한 ResultConverter 작성 및 DTO 생성

---
+++ src/main/java/ladder/dto/LadderResultDto.java
@@ -0,0 +1,24 @@
+package ladder.dto;
+
+import ladder.model.ExecutionResult;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class LadderResultDto {
+
+    private final Map<String, ExecutionResult> results;
+
+    private LadderResultDto(Map<String, ExecutionResult> results) {
+        this.results = Collections.unmodifiableMap(results);
+    }
+
+    public static LadderResultDto create(Map<String, ExecutionResult> results) {
+        return new LadderResultDto(results);
+    }
+
+    public ExecutionResult showLadderResult(String participantName) {
+        return results.get(participantName);
+    }
+
+}
--- src/main/java/ladder/model/ExecutionResults.java
+++ src/main/java/ladder/model/ExecutionResults.java
@@ -19,4 +19,7 @@ public class ExecutionResults {
         return executionResults;
     }
 
+    public ExecutionResult getResult(int resultIndex) {
+        return executionResults.get(resultIndex);
+    }
 }
