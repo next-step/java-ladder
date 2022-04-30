40c32ab10e810f164c873b93c8d8b4bdd3965d93
younghun <asqwklop12@naver.com>
2022-04-30 12:08:12 +0900

    feat List에 담긴 모든 숫자 중 3보다 큰 숫자를 2배 한 후 모든 값의 합을 구한다.

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -10,35 +10,35 @@ import java.util.stream.Collectors;
 
 public class StreamStudy {
 
-    public static long countWords() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+  public static long countWords() throws IOException {
+    String contents = Files.readString(Paths
+        .get("src/main/resources/fp/war-and-peace.txt"));
+    List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+    return words.stream()
+        .filter(word -> word.length() > 12).count();
+  }
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
-    }
+  public static void printLongestWordTop100() throws IOException {
+    String contents = Files.readString(Paths
+        .get("src/main/resources/fp/war-and-peace.txt"));
+    List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-    public static void printLongestWordTop100() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+    // TODO 이 부분에 구현한다.
+  }
 
-        // TODO 이 부분에 구현한다.
-    }
+  public static List<Integer> doubleNumbers(List<Integer> numbers) {
+    return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
+  }
 
-    public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
-    }
+  public static long sumAll(List<Integer> numbers) {
+    return numbers.stream().reduce(0, (x, y) -> x + y);
+  }
 
-    public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
-    }
-
-    public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
-    }
+  public static long sumOverThreeAndDouble(List<Integer> numbers) {
+    return numbers
+        .stream()
+        .filter(number -> 3 < number)
+        .map(number -> 2 * number)
+        .reduce(0, Integer::sum);
+  }
 }
