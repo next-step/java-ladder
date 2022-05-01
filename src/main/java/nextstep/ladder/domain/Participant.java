46432ce60e1c1dc04e8e45aee02b4b5e7ff43064
imcool2551 <imcool2551@gmail.com>
2022-05-01 22:41:55 +0900

    feat: Participant
    동등성 비교를 추가했습니다.

--- src/main/java/nextstep/ladder/domain/Participant.java
+++ src/main/java/nextstep/ladder/domain/Participant.java
@@ -1,5 +1,7 @@
 package nextstep.ladder.domain;
 
+import java.util.Objects;
+
 public class Participant {
 
     static final int MIN_LENGTH = 1;
@@ -14,14 +16,27 @@ public class Participant {
 
     private void validate(String name) {
         if (name == null || name.isBlank()) {
-            throw new ParticipantNameException();
+            throw new InvalidParticipantNameException();
         }
         if (name.length() < MIN_LENGTH || name.length() > MAX_LENGTH) {
-            throw new ParticipantNameException(name);
+            throw new InvalidParticipantNameException(name);
         }
     }
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Participant that = (Participant) o;
+        return Objects.equals(getName(), that.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
     }
 }
