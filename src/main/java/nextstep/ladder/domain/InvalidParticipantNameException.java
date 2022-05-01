1a9ec773af1909862fab7754e47169d53ea3f567
imcool2551 <imcool2551@gmail.com>
2022-05-01 22:41:27 +0900

    refactor: ParticipantNameException
    이름을 InvalidParticipantNameException으로 변경하고 에러 메시지를 상수로 만들었습니다.

---
+++ src/main/java/nextstep/ladder/domain/InvalidParticipantNameException.java
@@ -0,0 +1,20 @@
+package nextstep.ladder.domain;
+
+import static nextstep.ladder.domain.Participant.MAX_LENGTH;
+import static nextstep.ladder.domain.Participant.MIN_LENGTH;
+
+public class InvalidParticipantNameException extends IllegalArgumentException {
+
+    private static final String DEFAULT_ERROR_MESSAGE = "invalid participant name";
+    private static final String ERROR_MESSAGE = "name must be length of " + MIN_LENGTH + " to " +
+            MAX_LENGTH + ", provided: ";
+
+    public InvalidParticipantNameException() {
+        super(DEFAULT_ERROR_MESSAGE);
+    }
+
+    public InvalidParticipantNameException(String name) {
+        super(ERROR_MESSAGE + name);
+    }
+
+}
