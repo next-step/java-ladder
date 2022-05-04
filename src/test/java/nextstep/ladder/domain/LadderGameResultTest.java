ccb82dabff8a5d7978b90554e921c4495d0fdcfb
imcool2551 <imcool2551@gmail.com>
2022-05-04 18:19:27 +0900

    refactor: LadderGameResultTest
    @BeforeEach 를 사용하여 중복된 테스트 픽스쳐를 제거했습니다.

--- src/test/java/nextstep/ladder/domain/LadderGameResultTest.java
+++ src/test/java/nextstep/ladder/domain/LadderGameResultTest.java
@@ -1,5 +1,6 @@
 package nextstep.ladder.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,27 +11,25 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LadderGameResultTest {
 
+    LadderGameResult result;
 
-    @Test
-    void findPrizeByName() {
-        LadderGameResult result = new LadderGameResult();
+    @BeforeEach
+    void setUp() {
+        result = new LadderGameResult();
 
         result.put(new Participant("a"), new Prize("0"));
         result.put(new Participant("b"), new Prize("1"));
         result.put(new Participant("c"), new Prize("2"));
+    }
 
+    @Test
+    void findPrizeByName() {
         assertThat(result.findPrizeByName("a")).isEqualTo("0");
     }
 
     @DisplayName("게임 결과가 없는 참여자 이름으로 조회하면 예외")
     @Test
     void GivenNonExistingName_findPrizeByName_ThrowsNoSuchElementException() {
-        LadderGameResult result = new LadderGameResult();
-
-        result.put(new Participant("a"), new Prize("0"));
-        result.put(new Participant("b"), new Prize("1"));
-        result.put(new Participant("c"), new Prize("2"));
-
         assertThatThrownBy(() -> result.findPrizeByName("d"))
                 .isInstanceOf(NoSuchElementException.class);
     }
