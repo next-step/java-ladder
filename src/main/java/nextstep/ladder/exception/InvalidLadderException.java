300ad0d56902147f379cf1b0851f9673963cbf06
imcool2551 <imcool2551@gmail.com>
2022-05-04 18:16:03 +0900

    refactor: Exceptions
    에러 메시지 생성시 String.format 함수를 사용했습니다.

--- src/main/java/nextstep/ladder/exception/InvalidLadderException.java
+++ src/main/java/nextstep/ladder/exception/InvalidLadderException.java
@@ -2,11 +2,13 @@ package nextstep.ladder.exception;
 
 public class InvalidLadderException extends IllegalArgumentException {
 
+    private static final String MESSAGE = "width and height must be over 0. width: %d, height: %d";
+
     public InvalidLadderException(int width, int height) {
         super(buildErrorMessage(width, height));
     }
 
     private static String buildErrorMessage(int width, int height) {
-        return "width and height must be over 0. width: " + width + ", height: " + height;
+        return String.format(MESSAGE, width, height);
     }
 }
