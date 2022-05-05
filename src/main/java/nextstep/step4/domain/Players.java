8f56bbe9bb11714d13a2cb7fa73cbdde54af7f4d
DahamLeee <dlek1011@gmail.com>
2022-05-05 13:13:15 +0900

    feat: Create Players [First Class Collection]

---
+++ src/main/java/nextstep/step4/domain/Players.java
@@ -0,0 +1,29 @@
+package nextstep.step4.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Collections.*;
+
+public class Players {
+
+    private final List<PlayerName> players;
+
+    private Players(List<PlayerName> players) {
+        this.players = players;
+    }
+
+    public static Players from(List<String> players) {
+        return new Players(players.stream()
+                .map(PlayerName::from)
+                .collect(Collectors.toList()));
+    }
+
+    public int getCountOfPlayer() {
+        return players.size();
+    }
+
+    public List<PlayerName> getPlayers() {
+        return unmodifiableList(players);
+    }
+}
