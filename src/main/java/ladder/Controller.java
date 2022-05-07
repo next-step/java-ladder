15359d0d7e80f9ba19a062b8a891aa6f5beb0960
hjeom <nara1030@naver.com>
2022-05-07 17:41:35 +0900

    style: 사용않는 부분 삭제

--- src/main/java/ladder/Controller.java
+++ src/main/java/ladder/Controller.java
@@ -7,10 +7,7 @@ import ladder.exception.InvalidNumberOfParticipants;
 import ladder.view.InputView;
 import ladder.view.ResultView;
 
-import java.util.Scanner;
-
 public class Controller {
-    private static final Scanner SCANNER = new Scanner(System.in);
     private static final String SEPARATOR_OF_NAMES = ",";
     private static final String ERROR_MESSAGE_OF_HEIGHT_OF_LADDER = "ERROR] 사다리 높이는 숫자를 입력해야 합니다.";
 
