b0c56fbf3364e41b81650e25c5f2b72c86496f69
DahamLeee <dlek1011@gmail.com>
2022-05-07 11:16:08 +0900

    refactor: add ladderGameResult Map

--- src/main/java/nextstep/step4/domain/LadderGame.java
+++ src/main/java/nextstep/step4/domain/LadderGame.java
@@ -2,8 +2,14 @@ package nextstep.step4.domain;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+
+import static java.util.Collections.*;
+import static java.util.stream.Collectors.*;
 
 public class LadderGame {
+
+    private final Map<PlayerName, String> ladderGameResults;
 
     private final Players players;
     private final Ladder ladder;
@@ -13,43 +19,41 @@ public class LadderGame {
         this.players = players;
         this.ladder = ladder;
         this.ladderResult = ladderResult;
+        ladderGameResults = findAllResult();
     }
 
     public static LadderGame of(Players players, Ladder ladder, LadderResult ladderResult) {
         return new LadderGame(players, ladder, ladderResult);
     }
 
+    private Map<PlayerName, String> findAllResult() {
+        Map<PlayerName, String> ladderGameResults = new HashMap<>();
+        for (int playerColumn = 0; playerColumn < players.getCountOfPlayer(); playerColumn++) {
+            addLadderResult(ladderGameResults, playerColumn);
+        }
+        return ladderGameResults;
+    }
+
+    private void addLadderResult(Map<PlayerName, String> ladderGameResults, int playerColumn) {
+        ladderGameResults.put(players.findPlayerByColumn(playerColumn), ladderResult.findLadderResultByEndColumn(ladder.findEndColumByStartColumn(playerColumn)));
+    }
+
     public Map<PlayerName, String> findResultsByPlayerName(String playerName) {
         if (isAll(playerName)) {
-            return findAllResult();
+            return unmodifiableMap(ladderGameResults);
         }
-        return findOneLadderResultByPlayerName(playerName);
+
+        return findOneResult(playerName);
     }
 
     private boolean isAll(String playerName) {
         return playerName.equals("all");
     }
 
-    private Map<PlayerName, String> findAllResult() {
-        Map<PlayerName, String> results = new HashMap<>();
-
-        for (int playerColumn = 0; playerColumn < players.getCountOfPlayer(); playerColumn++) {
-            addLadderResult(results, playerColumn);
-        }
-
-        return results;
-    }
-
-    private Map<PlayerName, String> findOneLadderResultByPlayerName(String playerName) {
-        Map<PlayerName, String> result = new HashMap<>();
-
-        int playerColumn = players.findColumnByPlayerName(playerName);
-        addLadderResult(result, playerColumn);
-
-        return result;
-    }
-
-    private void addLadderResult(Map<PlayerName, String> result, int playerColumn) {
-        result.put(players.findPlayerByColumn(playerColumn), ladderResult.findLadderResultByEndColumn(ladder.findEndColumByStartColumn(playerColumn)));
+    private Map<PlayerName, String> findOneResult(String playerName) {
+        return ladderGameResults.entrySet()
+                .stream()
+                .filter(result -> result.getKey().equals(PlayerName.from(playerName)))
+                .collect(toMap(Entry::getKey, Entry::getValue));
     }
 }
