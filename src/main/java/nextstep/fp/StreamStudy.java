598dfee8e66ef61ea93c68365a38c46dc53a40fb
hihisososo <hihisososo@naver.com>
2022-05-07 12:11:38 +0900

    refactor: StreamStudy stream 개선

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -7,7 +7,6 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
@@ -17,13 +16,9 @@ public class StreamStudy {
         .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
     List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-    long count = 0;
-    for (String w : words) {
-      if (w.length() > 12) {
-        count++;
-      }
-    }
-    return count;
+    return words.stream()
+        .filter(word -> word.length() > 12)
+        .count();
   }
 
   public static void printLongestWordTop100() throws IOException {
@@ -31,11 +26,10 @@ public class StreamStudy {
         .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
     List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-    // TODO 이 부분에 구현한다.
     words.stream()
         .filter(word -> word.length() > 12)
-        .sorted(Comparator.comparing(String::length).reversed())
         .distinct()
+        .sorted(Comparator.comparing(String::length).reversed())
         .limit(100)
         .forEach(word -> System.out.println(word.toLowerCase()));
   }
@@ -45,13 +39,13 @@ public class StreamStudy {
   }
 
   public static long sumAll(List<Integer> numbers) {
-    return numbers.stream().reduce(0, (x, y) -> x + y);
+    return numbers.stream().reduce(0, Integer::sum);
   }
 
   public static long sumOverThreeAndDouble(List<Integer> numbers) {
     return numbers.stream()
         .filter(number -> number > 3)
         .map(number -> number * 2)
-        .reduce(0, (number1, number2) -> number1 + number2);
+        .reduce(0, Integer::sum);
   }
 }
