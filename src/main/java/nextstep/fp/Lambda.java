18a90dbd880059163735a6eb27a0e4a8d389da2c
syeeuns <syeeuns@gmail.com>
2022-05-07 14:39:59 +0900

    Feat: Lambda 요구사항 구현

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
@@ -27,27 +28,21 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAll(numbers, (Integer number) -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, (Integer number) -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
+        return sumAll(numbers, (Integer number) -> number > 3);
+    }
+
+    private static int sumAll(List<Integer> numbers, Conditional c) {
         int total = 0;
         for (int number : numbers) {
-            if (number > 3) {
+            if (c.test(number)) {
                 total += number;
             }
         }
