eab1bff57198d7e4dee64cf8302c7272dad8ebbd
itdar <thesockerr@gmail.com>
2022-05-09 23:27:02 +0900

    refactor: 이전 단계 import 정리

--- src/main/java/nextstep/optional/Users.java
+++ src/main/java/nextstep/optional/Users.java
@@ -2,7 +2,6 @@ package nextstep.optional;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 public class Users {
     static final User DEFAULT_USER = new User("codesquad", 100);
