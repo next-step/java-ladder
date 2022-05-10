0fdd53b74e0c70a72f08fab78581845a3909e259
pangnem <sprnd645@gmail.com>
2022-05-11 04:03:53 +0900

    feat: Optional

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -7,6 +9,41 @@ public class User {
     public User(String name, Integer age) {
         this.name = name;
         this.age = age;
+    }
+
+    public static boolean ageIsInRange1(User user) {
+        return user != null && ageNotNull(user)
+                && (ageOver29(user)
+                && ageUnder46(user));
+    }
+
+    public static boolean ageIsInRange2(User user) {
+        return Optional.ofNullable(user)
+                .filter(User::ageNotNull)
+                .map(User::getAge)
+                .filter(User::ageOver29)
+                .filter(User::ageUnder46)
+                .isPresent();
+    }
+
+    private static boolean ageUnder46(Integer age) {
+        return age <= 45;
+    }
+
+    private static boolean ageOver29(Integer age) {
+        return age >= 30;
+    }
+
+    private static boolean ageUnder46(User user) {
+        return ageUnder46(user.getAge());
+    }
+
+    private static boolean ageOver29(User user) {
+        return user.getAge() >= 30;
+    }
+
+    private static boolean ageNotNull(User user) {
+        return user.getAge() != null;
     }
 
     public String getName() {
@@ -19,21 +56,6 @@ public class User {
 
     public boolean matchName(String name) {
         return this.name.equals(name);
-    }
-
-    public static boolean ageIsInRange1(User user) {
-        boolean isInRange = false;
-
-        if (user != null && user.getAge() != null
-                && (user.getAge() >= 30
-                && user.getAge() <= 45)) {
-            isInRange = true;
-        }
-        return isInRange;
-    }
-
-    public static boolean ageIsInRange2(User user) {
-        return false;
     }
 
     @Override
@@ -66,4 +88,5 @@ public class User {
             return false;
         return true;
     }
+
 }
