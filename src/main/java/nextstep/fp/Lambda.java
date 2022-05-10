cf5cc3bec5ecb2e93ce70d42d5b545db7a6120b3
Chunghwan Kim <49121847+ch0213@users.noreply.github.com>
2022-05-10 09:58:11 +0900

    [사다리타기 - FP, OOP] 1단계 - 자바8 스트림, 람다, Optional (#1354)
    * refactor: 람다를 활용해 중복 제거
    
    * refactor: 스트림을 활용해 리팩터링
    
    * refactor: 익명함수를 람다로 교체
    
    * test: save 기능 리팩터링에 따라 테스트 코드 변경
    
    * feat: sum 조건에 맞는지 체크하는 기능을 정의한 인터페이스
    
    * refactor: Optional을 통해 기존 기능 리팩터링
    
    * refactor: 스트림과 람다를 사용하여 기존 기능 변경
    
    * refactor: Optional과 Lamda를 이용해 getUser 리팩터링
    
    * fix: 더하기 조건을 외부에서 주입하지 않고 내부에서 결정하도록 수정

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -18,39 +18,26 @@ public class Lambda {
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> System.out.println("Hello from thread")).start();
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .filter(number -> number % 2 == 0)
+                .sum();
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .filter(number -> number > 3)
+                .sum();
     }
 }
---
+++ src/main/java/nextstep/fp/SumCondition.java
@@ -0,0 +1,5 @@
+package nextstep.fp;
+
+public interface SumCondition {
+    boolean match(int number);
+}
