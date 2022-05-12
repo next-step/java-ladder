35783ba4d0587de292930ba25d3054444ce35f39
zieunx <wldms_1217@naver.com>
2022-05-12 22:49:59 +0900

    fix(step1/StreamStudy): 다양한 stream method 실습 - map, reduce, filter 실습 1

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -11,15 +11,13 @@ import java.util.stream.Collectors;
 public class StreamStudy {
 
     public static long countWords() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+        String contents = Files.readString(Paths
+                .get("src/main/resources/fp/war-and-peace.txt"));
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream()
+            .filter(w -> w.length() > 12)
+            .count();
     }
 
     public static void printLongestWordTop100() throws IOException {
@@ -39,6 +37,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+            .filter(n -> n > 3)
+            .map(n -> n * 2)
+            .reduce(0, (x, y) -> x + y);
     }
 }
