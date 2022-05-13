173e717a190b50477556d627f65d6ce735617331
ssamzag <ssamzag@gmail.com>
2022-05-13 23:12:29 +0900

    chore: 미사용 import 문 제거

--- src/main/java/nextstep/ladder/domain/LineMaker.java
+++ src/main/java/nextstep/ladder/domain/LineMaker.java
@@ -1,7 +1,5 @@
 package nextstep.ladder.domain;
 
-import nextstep.ladder.domain.Line;
-
 public interface LineMaker {
     Line makeLine();
 }
--- src/test/java/nextstep/ladder/domain/UserTest.java
+++ src/test/java/nextstep/ladder/domain/UserTest.java
@@ -3,7 +3,6 @@ package nextstep.ladder.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class UserTest {
