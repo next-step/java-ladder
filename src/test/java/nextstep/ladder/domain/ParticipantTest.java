7b64e3e226932487b64b1a5d407133f455967be3
younlll <youn.lee96@gmail.com>
2022-05-13 22:45:04 +0900

    test: 참가자 클래스 테스트 작성

---
+++ src/test/java/nextstep/ladder/domain/ParticipantTest.java
@@ -0,0 +1,35 @@
+package nextstep.ladder.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import nextstep.ladder.domain.Participant;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ParticipantTest {
+    
+    @Test
+    @DisplayName("참가자 객체를 생성합니다.")
+    void createParticipantObject() {
+      // given
+      String name = "pobi";
+
+      // when
+      Participant participant = new Participant(name);
+      
+      // then
+      assertThat(participant.toString()).isEqualTo(name);
+    }
+    
+    @Test
+    @DisplayName("참가자 이름의 길이가 5자를 초과하는 경우 예외처리를 합니다.")
+    void exceptionParticipantNameLengthOverStandard() {
+      // given
+      String name = "pobipobi";
+
+      // when & then
+      assertThatThrownBy(() -> new Participant(name))
+              .isInstanceOf(IllegalArgumentException.class);
+    }
+}
