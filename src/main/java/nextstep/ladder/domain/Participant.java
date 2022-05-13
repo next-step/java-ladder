01a7f177a16e9aff2f8c87a12050da8dcd95cd67
younlll <youn.lee96@gmail.com>
2022-05-13 23:15:15 +0900

    feat: 참가자 이름 null 또는 empty 검증 추가

--- src/main/java/nextstep/ladder/domain/Participant.java
+++ src/main/java/nextstep/ladder/domain/Participant.java
@@ -4,17 +4,29 @@ public class Participant {
 
     private static final int BASE_LENGTH_PARTICIPANT_NAME = 5;
     private static final String BASE_LENGTH_OVER_ERROR_MESSAGE = "참가자 이름의 길이가 기준 길이 5를 초과하였습니다.(입력된 참가자명: %s)";
+    private static final String PARTICIPANT_NULL_OR_EMPTY_ERROR_MESSAGE = "참가자 이름이 null 또는 empty입니다.";
 
     private final String name;
 
     public Participant(String name) {
-        validateValueLength(name);
+        validate(name);
         this.name = name;
+    }
+
+    private void validate(String name) {
+        validateNullAndEmpty(name);
+        validateValueLength(name);
+    }
+
+    private void validateNullAndEmpty(String name) {
+        if(name == null || name.isEmpty()) {
+            throw new IllegalArgumentException(String.format(BASE_LENGTH_OVER_ERROR_MESSAGE, name));
+        }
     }
 
     private void validateValueLength(String name) {
         if(name.length() > BASE_LENGTH_PARTICIPANT_NAME) {
-            throw new IllegalArgumentException(String.format(BASE_LENGTH_OVER_ERROR_MESSAGE, name));
+            throw new IllegalArgumentException(PARTICIPANT_NULL_OR_EMPTY_ERROR_MESSAGE);
         }
     }
 
