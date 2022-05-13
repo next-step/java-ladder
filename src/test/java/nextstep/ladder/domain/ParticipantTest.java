1addd96562ba1ae5c7046027f4a7be04f243edc0
younlll <youn.lee96@gmail.com>
2022-05-13 23:10:54 +0900

    test: 참가자 이름이 null이거나 empty인 경우 예외처리 테스트

--- src/test/java/nextstep/ladder/domain/ParticipantTest.java
+++ src/test/java/nextstep/ladder/domain/ParticipantTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 public class ParticipantTest {
 
@@ -30,5 +32,13 @@ public class ParticipantTest {
         // when & then
         assertThatThrownBy(() -> new Participant(name))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest(name = "참가자의 이름이 null이거나 empty인 경우 예외처리를 합니다.")
+    @NullAndEmptySource
+    void exceptionParticipantIsNullOrEmpty(String name) {
+      // when & then
+      assertThatThrownBy(() -> new Participant(name))
+              .isInstanceOf(IllegalArgumentException.class);
     }
 }
