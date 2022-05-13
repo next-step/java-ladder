4b403ffe0efe1a9aa2e22d63d0b043109b251926
younlll <youn.lee96@gmail.com>
2022-05-14 07:57:07 +0900

    feat: 도메인 참가자의 일급 컬렉션

---
+++ src/main/java/nextstep/ladder/domain/Participants.java
@@ -0,0 +1,34 @@
+package nextstep.ladder.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Participants {
+
+    private static final String PARTICIPANTS_NULL_OR_EMPTY_ERROR_MESSAGE = "입력된 참가자들이 없습니다.";
+
+    private final List<Participant> values;
+
+    public Participants(String[] names) {
+        validateNullAndEmpty(names);
+        values = convertToList(names);
+    }
+
+    private void validateNullAndEmpty(String[] names) {
+        if(names == null || names.length == 0) {
+            throw new IllegalArgumentException(PARTICIPANTS_NULL_OR_EMPTY_ERROR_MESSAGE);
+        }
+    }
+
+    private List<Participant> convertToList(String[] names) {
+        return Arrays.stream(names)
+                .map(Participant::new)
+                .collect(Collectors.toList());
+    }
+
+
+    public List<Participant> getValues() {
+        return values;
+    }
+}
