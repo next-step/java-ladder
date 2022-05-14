d55365387731ca01db8a3f070c7dd8a855d44799
wu2ee <gulb1602@gmail.com>
2022-05-14 18:07:46 +0900

    chore : 쓰지 않는 import 문 제거

--- src/test/java/ladder/domain/ParticipantTest.java
+++ src/test/java/ladder/domain/ParticipantTest.java
@@ -1,11 +1,8 @@
 package ladder.domain;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ParticipantTest {
