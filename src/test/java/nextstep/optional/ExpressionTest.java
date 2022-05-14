bfac6b6f9de5a3c19f675ee12f448804bf4b5584
pangnem <sprnd645@gmail.com>
2022-05-14 15:58:55 +0900

    test(step1): Expression.of 케이스 추가

--- src/test/java/nextstep/optional/ExpressionTest.java
+++ src/test/java/nextstep/optional/ExpressionTest.java
@@ -10,13 +10,14 @@ public class ExpressionTest {
     @Test
     public void of() {
         assertThat(Expression.PLUS == Expression.of("+")).isTrue();
+        assertThat(Expression.MINUS == Expression.of("-")).isTrue();
+        assertThat(Expression.TIMES == Expression.of("*")).isTrue();
+        assertThat(Expression.DIVIDE == Expression.of("/")).isTrue();
     }
 
     @Test
     public void notValidExpression() {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> {
-                    Expression.of("&");
-                });
+                .isThrownBy(() -> Expression.of("&"));
     }
 }
