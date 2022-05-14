f8390645308d341646591f1a93af212f34f24f33
asqwklop12 <asqwklop12@gmail.com>
2022-05-15 03:51:56 +0900

    chore 더 이상 ParicipantName에서 ToString을 사용할 수 없습니다

--- src/main/java/nextstep/ladder/model/ParticipantName.java
+++ src/main/java/nextstep/ladder/model/ParticipantName.java
@@ -5,7 +5,6 @@ import java.util.Objects;
 public final class ParticipantName {
 
     private static final String EXCEPTION_MESSAGE = "참가자의 이름의 길이는 1자리에서 5자리입니다.";
-    private static final String BLANK_VALUE = " ";
     private static final int MIN_NAME_LENGTH = 1;
     private static final int MAX_NAME_LENGTH = 5;
 
@@ -22,6 +21,7 @@ public final class ParticipantName {
             throw new IndexOutOfBoundsException(EXCEPTION_MESSAGE);
         }
     }
+
     public long length() {
         return value.length();
     }
@@ -39,9 +39,4 @@ public final class ParticipantName {
         return Objects.hash(value);
     }
 
-    @Override
-    public String toString() {
-        int repeatValue = MAX_NAME_LENGTH - value.length();
-        return BLANK_VALUE.repeat(repeatValue).concat(value);
-    }
 }
