2a4846e2403368f39c941e39a09b953c6fcaac0b
younlll <youn.lee96@gmail.com>
2022-05-14 09:32:33 +0900

    feat: 사다리 위치정보

---
+++ src/main/java/nextstep/ladder/domain/Position.java
@@ -0,0 +1,14 @@
+package nextstep.ladder.domain;
+
+public class Position {
+
+    private final boolean value;
+
+    public Position(boolean value) {
+        this.value = value;
+    }
+
+    public boolean getValue() {
+        return value;
+    }
+}
