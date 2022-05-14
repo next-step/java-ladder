37b128791debd7926dc5cd58e7a16f432a4c1500
asqwklop12 <asqwklop12@gmail.com>
2022-05-14 22:46:01 +0900

    사다리를 수동으로 생성합니다.

--- src/main/java/nextstep/ladder/model/Line.java
+++ src/main/java/nextstep/ladder/model/Line.java
@@ -1,11 +1,71 @@
 package nextstep.ladder.model;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public final class Line {
-    private List<String> parts;
 
-    public Line(List<String> parts) {
-        this.parts = parts;
+    private static final String CONTINUITY_CREATE_MESSAGE = "연속으로 바를 생성할 수 없습니다.";
+    private static final String LAST_CREATE_MESSAGE = "마지막 바는 생성할 수 없습니다.";
+    private static final String PARTICIPANT_COUNT_EXCEPTION_MESSAGE = "참가자의 수는 최소 2명이여야 합니다.";
+    private static final int ONE = 1;
+    private static final int TWO = 2;
+
+    private final List<Bar> bars;
+
+    public Line(List<Boolean> bars) {
+        validate(bars);
+        this.bars = convert(bars);
+    }
+
+    private static List<Bar> convert(List<Boolean> bars) {
+        return bars.stream()
+                .map(Bar::new)
+                .collect(Collectors.toList());
+    }
+
+    public int barsCount() {
+        return bars.size();
+    }
+
+    public void validate(List<Boolean> bars) {
+        int size = bars.size();
+        barCountValidate(size);
+        for (int i = ONE; i < size; i++) {
+            continuityCreateValidate(bars.get(i - ONE), bars.get(i));
+        }
+        lastBarCreateValidate(bars.get(size - ONE));
+    }
+
+    private void barCountValidate(int size) {
+        if (size < TWO) {
+            throw new IllegalArgumentException(PARTICIPANT_COUNT_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void lastBarCreateValidate(boolean currentBarStatus) {
+        if (currentBarStatus) {
+            throw new IllegalArgumentException(LAST_CREATE_MESSAGE);
+        }
+    }
+
+    private void continuityCreateValidate(boolean beforeBarStatus, boolean currentBarStatus) {
+        if (beforeBarStatus && currentBarStatus) {
+            throw new IllegalArgumentException(CONTINUITY_CREATE_MESSAGE);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Line line = (Line) o;
+        return Objects.equals(bars, line.bars);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bars);
     }
 }
--- src/test/java/nextstep/ladder/model/LineTest.java
+++ src/test/java/nextstep/ladder/model/LineTest.java
@@ -1,12 +1,66 @@
 package nextstep.ladder.model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 class LineTest {
 
     @Test
+    @DisplayName("사다리의 바의 갯수는 참가지의 갯수와 같습니다.")
     void create() {
+        Line line = new Line(
+                List.of(
+                        true,
+                        false,
+                        true,
+                        false
+                )
+        );
+        assertThat(line.barsCount()).isEqualTo(4);
+    }
 
+    @Test
+    @DisplayName("사다리의 바는 연속으로 생성이 될 수 없습니다.")
+    void notContinuityTrueBar() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Line line = new Line(
+                    List.of(
+                            true,
+                            true,
+                            false
+                    )
+            );
+        });
+    }
+
+    @Test
+    @DisplayName("마지막 참가자의 바는 생성이 되지 않습니다.")
+    void lastCrateBar() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Line line = new Line(
+                    List.of(
+                            true,
+                            true
+                    )
+            );
+        });
+    }
+
+    @Test
+    @DisplayName("참가자는 최소 2명이상이어야 합니다.")
+    void createTwoBars() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Line line = new Line(
+                    List.of(
+                            false
+                    )
+            );
+        });
     }
 }
