61ee18d610309575efefee221075125be38a158f
wu2ee <gulb1602@gmail.com>
2022-05-14 18:17:25 +0900

    feat : 참가자 그룹 객체 생성

---
+++ src/main/java/ladder/domain/ParticipantGroup.java
@@ -0,0 +1,35 @@
+package ladder.domain;
+
+import java.util.List;
+
+public class ParticipantGroup {
+    private static final int MIN_PARTICIPANTS = 2;
+
+    private final List<Participant> participants;
+
+    public ParticipantGroup(List<Participant> participants) {
+        validateParticipants(participants);
+        this.participants = participants;
+    }
+
+    public int participantsCount() {
+        return participants.size();
+    }
+
+    public int maxParticipantNameLength() {
+        return participants.stream().mapToInt(Participant::nameLength).max().getAsInt();
+    }
+
+    private void validateParticipants(List<Participant> participants) {
+        if (participants == null) {
+            throw new IllegalArgumentException("참가자들은 null 일 수 없습니다.");
+        }
+        validateParticipantsCount(participants.size());
+    }
+
+    private void validateParticipantsCount(int participantsCount) {
+        if (participantsCount < MIN_PARTICIPANTS) {
+            throw new IllegalArgumentException(String.format("사다리 게임 참가자는 2명 미만일 수 없습니다. 현재 참가자 수 : %d", participantsCount));
+        }
+    }
+}
---
+++ src/test/java/ladder/domain/ParticipantGroupTest.java
@@ -0,0 +1,43 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ParticipantGroupTest {
+    @ParameterizedTest
+    @NullSource
+    void ParticipantGroup_참가자들이_null_인_경우(List<Participant> participants) {
+        assertThatThrownBy(() -> new ParticipantGroup(participants)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void participantsCount_참가자_카운팅() {
+        assertThat(new ParticipantGroup(List.of(
+                new Participant(new Name("wu2ee")),
+                new Participant(new Name("pobi")),
+                new Participant(new Name("honux")))).participantsCount()
+        ).isEqualTo(3);
+    }
+
+    @Test
+    void ParticipantsGroup_참가자가_2명_미만인_경우() {
+        assertThatThrownBy(() -> new ParticipantGroup(List.of(
+                new Participant(new Name("wu2ee"))
+        ))).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void maxParticipantNameLength_참가자_이름중_최대_길이() {
+        assertThat(new ParticipantGroup(List.of(
+                new Participant(new Name("k")),
+                new Participant(new Name("kbstv")),
+                new Participant(new Name("kb")))).maxParticipantNameLength()
+        ).isEqualTo(5);
+    }
+}
