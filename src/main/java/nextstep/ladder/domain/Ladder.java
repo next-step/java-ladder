9be7ef74eae954e841d339248d3b9bcd770b6d10
guswns1659 <guswns1659@gmail.com>
2022-05-16 06:51:13 +0900

    step2 feat: Stream 이용하며 Ladder 초기화

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -2,20 +2,21 @@ package nextstep.ladder.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Ladder {
     private final List<Line> lines;
 
-    public static Ladder of(int row, int height) {
-        List<Line> tempLines = new ArrayList<>();
-        for (int count = 0; count < row; row++) {
-            tempLines.add(new Line(height));
-        }
-        return new Ladder(tempLines);
+    public static Ladder of(int countOfLines, int height) {
+        return new Ladder(
+                IntStream.range(0, countOfLines)
+                        .mapToObj(i -> Line.of(height))
+                        .collect(Collectors.toList())
+        );
     }
 
     private Ladder(List<Line> lines) {
         this.lines = lines;
     }
-
 }
