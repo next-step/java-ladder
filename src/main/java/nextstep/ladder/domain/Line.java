264067b03eb6d2a0e218556a2e245f83a27911b3
pangnem <sprnd645@gmail.com>
2022-05-16 23:31:39 +0900

    refactor: toPoints 메서드 Points 클래스로 이동

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -1,48 +1,23 @@
 package nextstep.ladder.domain;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.IntStream;
 
 public class Line {
     private static final String STICK = "|";
-    private static final Random RANDOM = new Random();
     private static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
     private final Points points;
 
     public Line(Members members) {
-        this(toPoints(members));
+        this(new Points(members));
     }
 
     Line(List<Boolean> points) {
-        this.points = new Points(points);
+        this(new Points(points));
     }
 
-    private static List<Boolean> toPoints(Members members) {
-        List<Boolean> points = new ArrayList<>();
-        IntStream.rangeClosed(0, members.size())
-                .forEach(i -> setPoints(points, i));
-
-        return points;
-    }
-
-    private static void setPoints(List<Boolean> points, int i) {
-        if (previousHasEdge(points, i)) {
-            points.add(false);
-            return;
-        }
-
-        points.add(RANDOM.nextBoolean());
-    }
-
-    private static boolean previousHasEdge(List<Boolean> points, int i) {
-        if (points.isEmpty()) {
-            return false;
-        }
-
-        return points.get(i - 1);
+    Line(Points points) {
+        this.points = points;
     }
 
     public String getPoint() {
--- src/main/java/nextstep/ladder/domain/Points.java
+++ src/main/java/nextstep/ladder/domain/Points.java
@@ -1,9 +1,13 @@
 package nextstep.ladder.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Points {
+    private static final Random RANDOM = new Random();
     private static final String STICK = "|";
     private static final String EDGE = "-----";
     private static final String EMPTY = "     ";
@@ -12,6 +16,35 @@ public class Points {
 
     public Points(List<Boolean> points) {
         this.points = points;
+    }
+
+    public Points(Members members) {
+        this(toPoints(members));
+    }
+
+    private static List<Boolean> toPoints(Members members) {
+        List<Boolean> points = new ArrayList<>();
+        IntStream.rangeClosed(0, members.size())
+                .forEach(i -> setPoints(points, i));
+
+        return points;
+    }
+
+    private static void setPoints(List<Boolean> points, int i) {
+        if (previousHasEdge(points, i)) {
+            points.add(false);
+            return;
+        }
+
+        points.add(RANDOM.nextBoolean());
+    }
+
+    private static boolean previousHasEdge(List<Boolean> points, int i) {
+        if (points.isEmpty()) {
+            return false;
+        }
+
+        return points.get(i - 1);
     }
 
     public String getPoint() {
