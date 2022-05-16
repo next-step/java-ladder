f20982ed602a4fda7aad203c2d5acfbd14ee6bb5
chkim <cndghks0213@gmail.com>
2022-05-16 22:18:04 +0900

    feat: 사다리 클래스 구현

---
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -0,0 +1,29 @@
+package nextstep.ladder.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Ladder {
+
+    private final List<LadderLine> ladderLines;
+
+    private Ladder(List<LadderLine> ladderLines) {
+        this.ladderLines = ladderLines;
+    }
+
+    public static Ladder from(int height, int sizeOfPerson, ConnectStrategy connectStrategy) {
+        List<LadderLine> ladderLines = new ArrayList<>();
+        for (int i = 1; i <= height; i++) {
+            ladderLines.add(LadderLine.from(sizeOfPerson, connectStrategy));
+        }
+        return new Ladder(ladderLines);
+    }
+
+    @Override
+    public String toString() {
+        return ladderLines.stream()
+                .map(LadderLine::toString)
+                .collect(Collectors.joining("\n"));
+    }
+}
---
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -0,0 +1,14 @@
+package nextstep.ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LadderTest {
+
+    @DisplayName("사다리의 높이, 참여 인원 수, 연결 전략을 입력하면 사다리가 생성된다.")
+    @Test
+    void createLadderTest() {
+        Ladder ladder = Ladder.from(5, 5, new RandomConnectStrategy());
+        System.out.println(ladder);
+    }
+}
