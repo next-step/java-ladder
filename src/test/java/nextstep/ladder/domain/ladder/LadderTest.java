3bb641424d77aaa92d64b86f6cb80303df6a2565
chkim <cndghks0213@gmail.com>
2022-05-17 00:05:24 +0900

    test: 사다리의 높이가 1보다 작은 경우 테스트 추가

--- src/test/java/nextstep/ladder/domain/ladder/LadderTest.java
+++ src/test/java/nextstep/ladder/domain/ladder/LadderTest.java
@@ -1,8 +1,11 @@
 package nextstep.ladder.domain.ladder;
 
 import nextstep.ladder.util.RandomConnectStrategy;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LadderTest {
 
@@ -11,5 +14,13 @@ public class LadderTest {
     void createLadderTest() {
         Ladder ladder = Ladder.from(5, 5, new RandomConnectStrategy());
         System.out.println(ladder);
+    }
+
+    @DisplayName("사다리의 높이가 1보다 작으면 예외가 발생한다..")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    void createLadderTestFail(int height) {
+        Assertions.assertThatThrownBy(() -> Ladder.from(height, 5, new RandomConnectStrategy()))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
