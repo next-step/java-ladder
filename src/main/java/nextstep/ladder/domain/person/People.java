eddf0ad7a9eb077873b5a223972e78e08170f103
chkim <cndghks0213@gmail.com>
2022-05-16 23:25:24 +0900

    feat: 여러 명의 참여자를 나타내는 People 클래스 구현

--- src/main/java/nextstep/ladder/domain/person/People.java
+++ src/main/java/nextstep/ladder/domain/person/People.java
@@ -1,4 +1,35 @@
 package nextstep.ladder.domain.person;
 
+import nextstep.ladder.util.StringSpliter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class People {
+    private final List<Person> people;
+
+    public People(List<Person> people) {
+        validatePeopleCount(people);
+        this.people = people;
+    }
+
+    private void validatePeopleCount(List<Person> people) {
+        if (people.size() < 2) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static People of(String names) {
+        List<Person> people = StringSpliter.split(names)
+                .stream().map(Person::new)
+                .collect(Collectors.toList());
+        return new People(people);
+    }
+
+    @Override
+    public String toString() {
+        return people.stream()
+                .map(Person::toString)
+                .collect(Collectors.joining());
+    }
 }
