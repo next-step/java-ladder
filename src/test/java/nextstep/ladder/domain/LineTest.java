9a5ca77642d122d8dc08b05ce2e615865be227a6
guswns1659 <guswns1659@gmail.com>
2022-05-17 07:12:35 +0900

    step2 feat: Line init 테스트 추가

--- src/test/java/nextstep/ladder/domain/LineTest.java
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -1,6 +1,8 @@
 package nextstep.ladder.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 
@@ -8,8 +10,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
 
+    @ParameterizedTest
+    @ValueSource(ints = {0,1,2,3})
+    void init(int index) {
+        assertThat(Line.init(4).point(index)).isFalse();
+    }
+
     @Test
-    void init() {
+    void compareGiven_create() {
         Line compare = new Line(Arrays.asList(true,false,false,false));
         Line original = Line.init(4);
         original.create(compare);
