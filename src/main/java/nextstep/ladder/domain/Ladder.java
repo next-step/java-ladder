4050d5d00b79814e681c031763467babb12e18fa
ssamzag <ssamzag@gmail.com>
2022-05-17 01:32:42 +0900

    refactor(Ladder.java): 생성자의 유효성 검사를 가장 처음하도록 변경

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -4,21 +4,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class Ladder {
+    private static final String EMPTY_ERROR_MESSAGE = "Ladder 객체가 비어있습니다.";
+    private static final String DIFFERENT_LINE_COUNT_ERROR_MESSAEG = "크기가 다른 라인객체가 존재합니다.";
     private final List<Line> lines;
 
     public Ladder(List<Line> lines) {
+        validate(lines);
         this.lines = lines;
-        validate();
     }
 
-    private void validate() {
+    private void validate(List<Line> lines) {
         if (lines == null || lines.size() == 0) {
-            throw new IllegalArgumentException("Ladder 객체가 비어있습니다.");
+            throw new IllegalArgumentException(EMPTY_ERROR_MESSAGE);
         }
 
         for (Line line : lines) {
             if (!line.isSamePointSize(lines.get(0))) {
-                throw new IllegalArgumentException("크기가 다른 라인객체가 존재합니다.");
+                throw new IllegalArgumentException(DIFFERENT_LINE_COUNT_ERROR_MESSAEG);
             }
         }
     }
