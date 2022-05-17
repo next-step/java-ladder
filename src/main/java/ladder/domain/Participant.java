b8f6436b37e5e286d77a5a26a94e30578b260ee4
bky4564 <bky4564@hunet.co.kr>
2022-05-17 13:09:07 +0900

    feat: 참가자 객체 생성 및 test 추가

---
+++ src/main/java/ladder/domain/Participant.java
@@ -0,0 +1,29 @@
+package ladder.domain;
+
+import ladder.exception.InvalidParticipantNameException;
+
+public class Participant {
+
+    private static final int MAX_PARTICIPANT_NAME_LENGTH = 5;
+    private static final int MIN_PARTICIPANT_NAME_LENGTH = 1;
+    private final String name;
+
+    public Participant(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (hasRightLength(name)) {
+            throw new InvalidParticipantNameException();
+        }
+    }
+
+    private boolean hasRightLength(String name) {
+        return name.length() > MAX_PARTICIPANT_NAME_LENGTH || name.length() < MIN_PARTICIPANT_NAME_LENGTH;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
