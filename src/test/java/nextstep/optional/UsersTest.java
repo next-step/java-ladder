09a85ceaad87cd52acf81e81b365608657dc9402
kang-jisu <rkdwltn0425@gmail.com>
2022-05-18 15:28:49 +0900

    feat: Optional을 사용해 Users.getUser 구현
    - users 리스트를 스트림, filter를 이용해 매칭되는 이름이 있는지 찾고 Optional로 값이 없다면 Default를 설정

--- src/test/java/nextstep/optional/UsersTest.java
+++ src/test/java/nextstep/optional/UsersTest.java
@@ -1,12 +1,15 @@
 package nextstep.optional;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("Optional 실습 - Users 클래스 테스트")
 public class UsersTest {
 
     @Test
+    @DisplayName("Users 리스트에 존재하는 유저를 받아온다.")
     void getUser() {
         Users users = new Users();
         assertThat(users.getUser("crong")).isEqualTo(new User("crong", 35));
@@ -14,6 +17,7 @@ public class UsersTest {
 
 
     @Test
+    @DisplayName("Users 리스트에 존재하지 않으면 DEFAULT_USERS가 반환된다.")
     void getDefaultUser() {
         Users users = new Users();
         assertThat(users.getUser("codesquard")).isEqualTo(Users.DEFAULT_USER);
