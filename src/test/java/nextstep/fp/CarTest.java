25e5704552fbc7be737eebe759bb71ea54698caf
kang-jisu <rkdwltn0425@gmail.com>
2022-05-18 12:45:47 +0900

    feat: 람다 실습 2 - 람다를 활용해 중복 제거
    - 더할 수를 판별하는 Condition.test 인터페이스를 추출
    - 조건을 익명클래스(람다)로 전달
    - 조건이 true일 때만 수를 더해 반환

--- src/test/java/nextstep/fp/CarTest.java
+++ src/test/java/nextstep/fp/CarTest.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DisplayName("Car 클래스 테스트")
+@DisplayName("람다 실습 - Car 클래스 테스트")
 public class CarTest {
     @Test
     @DisplayName("movable하면 1칸 전진한다.")
     void 이동() {
         Car car = new Car("pobi", 0);
-        Car actual = car.move(()->true);
+        Car actual = car.move(() -> true);
         assertThat(actual).isEqualTo(new Car("pobi", 1));
     }
 
@@ -19,7 +19,7 @@ public class CarTest {
     @DisplayName("movable하지 않으면 정지한다.")
     void 정지() {
         Car car = new Car("pobi", 0);
-        Car actual = car.move(()->false);
+        Car actual = car.move(() -> false);
         assertThat(actual).isEqualTo(new Car("pobi", 0));
     }
 }
--- src/test/java/nextstep/fp/LambdaTest.java
+++ src/test/java/nextstep/fp/LambdaTest.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -8,6 +9,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("람다 실습 - Lambda 클래스 테스트")
 public class LambdaTest {
     private List<Integer> numbers;
 
@@ -17,33 +19,39 @@ public class LambdaTest {
     }
 
     @Test
+    @DisplayName("람다 없던 시절 출력")
     public void printAllOld() throws Exception {
         Lambda.printAllOld(numbers);
     }
 
     @Test
+    @DisplayName("람다를 이용한 출력")
     public void printAllLambda() throws Exception {
         Lambda.printAllLambda(numbers);
     }
 
     @Test
+    @DisplayName("쓰레드 실행")
     public void runThread() throws Exception {
         Lambda.runThread();
     }
 
     @Test
+    @DisplayName("리스트의 모든 숫자를 더한다.")
     public void sumAll() throws Exception {
         int sum = Lambda.sumAll(numbers);
         assertThat(sum).isEqualTo(21);
     }
 
     @Test
+    @DisplayName("리스트의 숫자 중 짝수만 더한다.")
     public void sumAllEven() throws Exception {
         int sum = Lambda.sumAllEven(numbers);
         assertThat(sum).isEqualTo(12);
     }
 
     @Test
+    @DisplayName("리스트의 숫자 중 3보다 큰 수만 더한다.")
     public void sumAllOverThree() throws Exception {
         int sum = Lambda.sumAllOverThree(numbers);
         assertThat(sum).isEqualTo(15);
