5e769a4ee2addff610343ae9caa8feaa32a6e640
Seongheon Sim <river.water.sea@gmail.com>
2022-05-19 01:46:56 +0900

    fix: Person 클래스 verify() 메소드 Optional 적용
    1. checkNameLength() -> verify(), 메소드 명 형용사로 수정
    2. verify() 메소드에서 Optional 을 이용하여 name 데이터의 길이를 검증할 수 있도록 수정

--- src/main/java/nextstep/ladder/model/Person.java
+++ src/main/java/nextstep/ladder/model/Person.java
@@ -1,5 +1,7 @@
 package nextstep.ladder.model;
 
+import java.util.Optional;
+
 public class Person {
 
     private static final String OUT_OF_LENGTH_MESSAGE = "이름의 길이를 초과했습니다. 이름은 최대 5자까지 허용합니다.";
@@ -7,14 +9,17 @@ public class Person {
     private final String name;
 
     private Person(String name) {
-        checkNameLength(name);
-        this.name = name;
+        this.name = verify(name);
     }
 
-    private static void checkNameLength(String name) {
-        if (name.isBlank() || name.length() < 1 || name.length() > 5) {
-            throw new IndexOutOfBoundsException(OUT_OF_LENGTH_MESSAGE);
-        }
+    private static String verify(String name) {
+        Optional.of(name)
+                .map(String::length)
+                .filter(length -> 0 < length)
+                .filter(length -> length < 6)
+                .orElseThrow(() -> new IndexOutOfBoundsException(OUT_OF_LENGTH_MESSAGE));
+
+        return name;
     }
 
     public static Person is(String name) {
