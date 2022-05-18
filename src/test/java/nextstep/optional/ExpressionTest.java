b21483a7e35237ae72861fa8c78419c95d35089c
kang-jisu <rkdwltn0425@gmail.com>
2022-05-18 16:00:30 +0900

    feat: Optional을 사용해 Express구현
    - users 리스트를 스트림, filter를 이용해 매칭되는 이름이 있는지 찾고 Optional로 값이 없다면 Default를 설정

--- src/test/java/nextstep/optional/ExpressionTest.java
+++ src/test/java/nextstep/optional/ExpressionTest.java
@@ -1,18 +1,21 @@
 package nextstep.optional;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-
+@DisplayName("Optional 실습 - Expression 클래스 테스트")
 public class ExpressionTest {
     @Test
+    @DisplayName("문자와 매칭되는 Expression 얻어오는 테스트")
     void of() {
         assertThat(Expression.PLUS == Expression.of("+")).isTrue();
     }
 
     @Test
+    @DisplayName("문자와 매칭되는 Expression이 없을 경우 예외가 발생한다.")
     void notValidExpression() {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> {
