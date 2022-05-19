d6fa3475f3729f2d0b2075443dc542e6736e134f
sanghyeon <sanghyeon@finda.co.kr>
2022-05-20 00:27:22 +0900

    refactor: 리뷰반영 - Players 초기화

--- src/main/java/step2/domain/player/Players.java
+++ src/main/java/step2/domain/player/Players.java
@@ -1,6 +1,5 @@
 package step2.domain.player;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
@@ -13,18 +12,16 @@ public class Players {
 
 	private static final int MIN_COUNT = 2;
 
-	private final Set<Player> values = new HashSet<>();
+	private final Set<Player> values;
 
 	public Players(List<String> input) {
 		Validator.notNull(input, ErrorTarget.NAME_INPUT);
 		Validator.duplicate(input, String.format("입력 값에 중복이 존재합니다. 입력 : %s", input));
 		Validator.min(MIN_COUNT, input.size(), String.format("플레이어의 최소 숫자는 %d 입니다. 입력 : %d", MIN_COUNT, input.size()));
 
-		Set<Player> players = input.stream()
+		this.values = input.stream()
 			.map(Player::new)
 			.collect(Collectors.toSet());
-
-		values.addAll(players);
 	}
 
 	public int numberOfPlayer() {
