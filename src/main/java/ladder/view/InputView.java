a7e657f600f2ea947c99202485a62676d480e612
wu2ee <gulb1602@gmail.com>
2022-05-20 01:39:39 +0900

    feat & refactor : 사다리 게임 실행 Controller 및 view 구현

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -9,7 +9,9 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String COMMA_DELIMITER = ",";
     private static final String PARTICIPANT_GUIDE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
-    private static final String LADDER_HEIGHT_GUIDE = "최대 사다리 높이는 몇 개인가요?";
+    private static final String EXECUTE_RESULT_GUIDE = "\n실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private static final String LADDER_HEIGHT_GUIDE = "\n최대 사다리 높이는 몇 개인가요?";
+    private static final String VIEW_COMMAND_GUIDE = "\n결과를 보고 싶은 사람은?";
 
     private InputView() {
     }
@@ -19,8 +21,18 @@ public class InputView {
         return SplitStringUtils.split(scanner.nextLine(), COMMA_DELIMITER);
     }
 
+    public static List<String> scanScores() {
+        System.out.println(EXECUTE_RESULT_GUIDE);
+        return SplitStringUtils.split(scanner.nextLine(), COMMA_DELIMITER);
+    }
+
     public static int scanLadderHeight() {
         System.out.println(LADDER_HEIGHT_GUIDE);
-        return scanner.nextInt();
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static String scanResultViewCommand() {
+        System.out.println(VIEW_COMMAND_GUIDE);
+        return scanner.nextLine();
     }
 }
--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -1,6 +1,7 @@
 package ladder.view;
 
 import ladder.domain.Ladder;
+import ladder.domain.LadderResult;
 import ladder.domain.Point;
 
 import java.util.List;
@@ -9,12 +10,25 @@ public class OutputView {
     private static final String BLANK = " ";
     private static final String UNIT_HORIZONTAL_LINE = "-";
     private static final String UNIT_VERTICAL_LINE = "|";
+    private static final String VIEW_LADDER_GUIDE = "\n사다리 결과\n";
+    private static final String VIEW_RESULT_GUIDE = "\n실행 결과";
+    private static final String ALL_VIEW_RESULT_FORMAT = "%s : %s";
+    private static final String RESCAN_COMMAND_GUIDE = "\n해당 사람은 참가자가 아닙니다. 다시 입력 해주세요.";
 
     private OutputView() {
     }
 
+    public static void printResultGuide() {
+        System.out.println(VIEW_LADDER_GUIDE);
+    }
+
     public static void printParticipants(List<String> participantNames, int maxConnectionLineLength) {
         participantNames.forEach(name -> System.out.print(name + BLANK.repeat(maxConnectionLineLength - name.length())));
+        System.out.println();
+    }
+
+    public static void printScores(List<String> scores, int maxConnectionLineLength) {
+        scores.forEach(score -> System.out.print(score + BLANK.repeat(maxConnectionLineLength - score.length())));
         System.out.println();
     }
 
@@ -38,5 +52,19 @@ public class OutputView {
 
     public static void printLadder(Ladder ladder, int maxConnectionLineLength) {
         ladder.lines().forEach(line -> printLine(line.points(), maxConnectionLineLength));
+    }
+
+    public static void printResultView(String command, LadderResult ladderResult) {
+        System.out.println(VIEW_RESULT_GUIDE);
+        System.out.println(ladderResult.getScore(command));
+    }
+
+    public static void printAllResultView(LadderResult ladderResult) {
+        System.out.println(VIEW_RESULT_GUIDE);
+        ladderResult.ladderResults().forEach((participant, score) -> System.out.println(String.format(ALL_VIEW_RESULT_FORMAT, participant.name(), score.score())));
+    }
+
+    public static void printReScanCommand() {
+        System.out.println(RESCAN_COMMAND_GUIDE);
     }
 }
