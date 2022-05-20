98760216221a4341adf1d6edfe352630bd9003d2
sanghyeon <sanghyeon@finda.co.kr>
2022-05-21 02:43:30 +0900

    feature: Direction - 방향에 따라서 다음 라인 인덱스 결정

--- src/main/java/step2/domain/ladder/Direction.java
+++ src/main/java/step2/domain/ladder/Direction.java
@@ -1,7 +1,20 @@
 package step2.domain.ladder;
 
+import java.util.function.IntUnaryOperator;
+
 public enum Direction {
-	LEFT,
-	STRAIGHT,
-	RIGHT
+
+	LEFT(input -> input - 1),
+	STRAIGHT(input -> input),
+	RIGHT(input -> input + 1);
+
+	private final IntUnaryOperator nextLineIndexCalculator;
+
+	Direction(IntUnaryOperator nextLineIndexCalculator) {
+		this.nextLineIndexCalculator = nextLineIndexCalculator;
+	}
+
+	public int nextIndex(int playerIndex) {
+		return this.nextLineIndexCalculator.applyAsInt(playerIndex);
+	}
 }
