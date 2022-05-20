90a5a1bcc6326dd64e8376ccda4074feecee555b
wlroh <wlroh.paul@gmail.com>
2022-05-21 01:35:41 +0900

    refactor: 메소드 분리
    들여쓰기 단계가 두 단계로 되어있는 부분이 있어 메소드 분리함.

--- src/main/java/ladder/strategy/RandomGeneration.java
+++ src/main/java/ladder/strategy/RandomGeneration.java
@@ -14,13 +14,17 @@ public class RandomGeneration implements GenerationStrategy {
     public List<Point> generatePoints(int count) {
         List<Point> points = new ArrayList<>();
         for (int i = 0; i < count; i++) {
-            if (isLastPointConnected(points)) {
-                points.add(Point.DISCONNECTED);
-                continue;
-            }
-            points.add(Point.valueOf(random.nextBoolean()));
+            generateRandom(points);
         }
         return points;
+    }
+
+    private void generateRandom(List<Point> points) {
+        if (isLastPointConnected(points)) {
+            points.add(Point.DISCONNECTED);
+            return;
+        }
+        points.add(Point.valueOf(random.nextBoolean()));
     }
 
     private boolean isLastPointConnected(List<Point> points) {
