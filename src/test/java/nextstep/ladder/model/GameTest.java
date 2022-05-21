c3fdce84d434de0624fcc923cd2d9e224f309d2e
Seongheon Sim <river.water.sea@gmail.com>
2022-05-22 04:52:10 +0900

    fix: GameTest 수정
    1. @MethodSource 적용

--- src/test/java/nextstep/ladder/model/GameTest.java
+++ src/test/java/nextstep/ladder/model/GameTest.java
@@ -5,20 +5,33 @@ import nextstep.ladder.model.ladder.Line;
 import nextstep.ladder.model.ladder.Lines;
 import nextstep.ladder.model.ladder.Point;
 import nextstep.ladder.model.player.People;
-import nextstep.ladder.view.ResultView;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameTest {
 
-    @ParameterizedTest(name = "참여자 중 한명의 결과 찾기 - {0}")
-    @CsvSource(delimiter = '|', value = {"a|30", "b|20", "c|10"})
-    void find(String name, String score) {
+    @ParameterizedTest(name = "참여자 중 한명의 결과 찾기 - {1} = {2}")
+    @MethodSource("findResult")
+    void find(Game game, String name, String score) {
+        assertThat(game.result(name).score()).isEqualTo(score);
+    }
 
+    private static Stream<Arguments> findResult() {
+        Game game = createGame();
+        return Stream.of(
+                Arguments.of(game, "a", "30"),
+                Arguments.of(game, "b", "20"),
+                Arguments.of(game, "c", "10")
+        );
+    }
+
+    private static Game createGame() {
         List<String> names = List.of("a", "b", "c");
         People people = People.of(names);
 
@@ -30,9 +43,6 @@ public class GameTest {
         List<String> scores = List.of("10", "20", "30");
         EndPoints endPoints = EndPoints.of(scores);
 
-        Game game = Game.ready(people, lines, endPoints);
-        ResultView.printLadder(people, game.ladder());
-
-        assertThat(game.result(name).score()).isEqualTo(score);
+        return Game.ready(people, lines, endPoints);
     }
 }
