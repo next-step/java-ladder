8e28122a483f6f1363d247394ac6f0fdb7970397
younlll <youn.lee96@gmail.com>
2022-05-22 02:27:14 +0900

    test: 사다리를 탄 결과 테스트

--- src/test/java/nextstep/ladder/domain/LadderTest.java
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -5,11 +5,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import nextstep.ladder.dto.LadderStandardDto;
+import nextstep.ladder.generator.FixedProductionGenerator;
 import nextstep.ladder.generator.PositionGenerator;
 import nextstep.ladder.generator.RandomProductionGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 public class LadderTest {
@@ -33,5 +35,19 @@ public class LadderTest {
         // when & then
         assertThatThrownBy(() -> new Ladder(lines))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest(name = "{0}번째 참가자가 사다리를 탄 결과는 {1}이다")
+    @CsvSource({"0,1", "1,0"})
+    void checkedLadderRideResultPosition(int position, int expected) {
+        // given
+        LadderStandardDto ladderStandardDto = new LadderStandardDto(new Height(3), 2);
+        Ladder ladder = new Ladder(new FixedProductionGenerator(), ladderStandardDto);
+
+        // when
+        int result = ladder.ride(position);
+
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 }
