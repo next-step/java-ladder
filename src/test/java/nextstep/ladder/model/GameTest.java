ef851c5b5119dd892485fb9f040125388134b0fc
Seongheon Sim <river.water.sea@gmail.com>
2022-05-23 23:27:51 +0900

    fix: Point.noLine(), hasLine() 메소드 삭제

--- src/test/java/nextstep/ladder/model/GameTest.java
+++ src/test/java/nextstep/ladder/model/GameTest.java
@@ -5,7 +5,6 @@ import nextstep.ladder.model.ladder.Line;
 import nextstep.ladder.model.ladder.Lines;
 import nextstep.ladder.model.ladder.Point;
 import nextstep.ladder.model.player.People;
-import nextstep.ladder.view.ResultView;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -20,8 +19,6 @@ public class GameTest {
     @ParameterizedTest(name = "참여자 중 한명의 결과 찾기 - {1} = {2}")
     @MethodSource("findResult")
     void find(Game game, String name, String score) {
-        ResultView.printLadder(game.people(), game.ladder());
-        ResultView.printAllResult(game);
         assertThat(game.result(name).score()).isEqualTo(score);
     }
 
@@ -34,16 +31,21 @@ public class GameTest {
         );
     }
 
+    private static Point noLine() {
+        return Point.of(false);
+    }
+
+    private static Point hasLine() {
+        return Point.of(true);
+    }
+
     private static Game createGame() {
         List<String> names = List.of("a", "b", "c");
         People people = People.of(names);
 
-        Point falsePoint = Point.noLine();
-        Point truePoint = Point.hasLine();
-
-        Line first = Line.create(truePoint, falsePoint, falsePoint);
-        Line second = Line.create(falsePoint, truePoint, falsePoint);
-        Line third = Line.create(truePoint, falsePoint, falsePoint);
+        Line first = Line.create(hasLine(), noLine(), noLine());
+        Line second = Line.create(noLine(), hasLine(), noLine());
+        Line third = Line.create(hasLine(), noLine(), noLine());
         Lines lines = Lines.of(List.of(first, second, third));
 
         List<String> scores = List.of("10", "20", "30");
