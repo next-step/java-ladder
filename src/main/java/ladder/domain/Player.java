25c42ec91e7a45b77b7eeb4eeb9549b7e9dee140
devgony <devgony@gmail.com>
2022-05-23 18:49:12 +0900

    feat: class Name with max 5, add test cases

--- src/main/java/ladder/domain/Player.java
+++ src/main/java/ladder/domain/Player.java
@@ -2,18 +2,35 @@ package ladder.domain;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Player {
     private final String name;
 
     public Player(String name) {
-        this.name= name;
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("name should be less than 5, but: " + name.length());
+        }
+        this.name = name;
     }
 
     public static List<Player> parse(String line) {
         return Arrays.stream(line.split("\\s*,\\s*"))
                 .map(Player::new)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
---
+++ src/test/java/ladder/domain/PlayerTest.java
@@ -0,0 +1,20 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PlayerTest {
+    @Test
+    void nameOver5GetsError() {
+        assertThatThrownBy(() -> new Player("123456")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void parseToPlayers() {
+        assertThat(Player.parse("a,b")).isEqualTo(Arrays.asList(new Player("a"), new Player("b")));
+    }
+}
