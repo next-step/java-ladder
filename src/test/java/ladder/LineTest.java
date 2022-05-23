872b6dc3a02e60e7ff10498f018f9203e069a680
rumor1993 <rumor1993@naver.com>
2022-05-23 22:19:15 +0900

    refactor: 테스트 싫패 -> 수정 -> 성공

--- src/test/java/ladder/LineTest.java
+++ src/test/java/ladder/LineTest.java
@@ -12,6 +12,6 @@ public class LineTest {
     @Test
     void 해당_라인에_선이_존재해야한다() {
         Line line = new Line(Arrays.asList(new Point(false), new Point(false), new Point(false), new Point(false), new Point(false)));
-        Assertions.assertThat(line.hasLine()).isTrue();
+        Assertions.assertThat(line.hasLine()).isFalse();
     }
 }
