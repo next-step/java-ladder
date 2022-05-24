8af040dbd52df75c4803e1664c44d3d3d0db9410
guswns1659 <guswns1659@gmail.com>
2022-05-25 06:44:47 +0900

    step3 feat: 좌표가 사다리에서 움직이는 기능 추가

--- src/main/java/nextstep/ladder/exception/LadderExceptionCode.java
+++ src/main/java/nextstep/ladder/exception/LadderExceptionCode.java
@@ -1,11 +1,18 @@
 package nextstep.ladder.exception;
 
 public enum LadderExceptionCode {
+    // player
     INVALID_NAME_FORMAT("이름은 null이거나 빈 값은 안됩니다."),
     INVAILD_NAME_LENGTH("이름은 0~5글자 사이어야 합니다."),
+
+    // ladder
     INVAILD_LADDER_HEIGHT("사다리 높이는 1이상 이어야 합니다."),
     FAIL_LADDER_INITIALIZATION("사다리는 라인이 1이상 이어야 합니다."),
-    INVALID_HEIGHT("Height는 null이어선 안됩니다.");
+    INVALID_HEIGHT("Height는 null이어선 안됩니다."),
+    INVALID_LADDER_FORMAT("사다리 모양이 정상적이지 않습니다."),
+
+    // coordinate
+    INVALID_COORDINATE_FORMAT("좌표의 x, y값은 0이상 이어야 합니다.");
     
     private final String message;
 
---
+++ src/test/java/nextstep/ladder/domain/CoordinateTest.java
@@ -0,0 +1,66 @@
+package nextstep.ladder.domain;
+
+import nextstep.ladder.exception.LadderException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CoordinateTest {
+
+    private List<Line> validLines;
+    private List<Line> invalidLines;
+
+    @BeforeEach
+    void setUp() {
+        validLines = List.of(
+                Line.of(Arrays.asList(TRUE, TRUE, FALSE)),
+                Line.of(Arrays.asList(FALSE, FALSE, TRUE))
+        );
+
+        invalidLines = List.of(
+                Line.of(Arrays.asList(TRUE, TRUE, FALSE)),
+                Line.of(Arrays.asList(TRUE, FALSE, TRUE))
+        );
+    }
+
+    @Test
+    void coordinate_createdFail() {
+        assertThatThrownBy(() -> Coordinate.of(-1, -1))
+                .isInstanceOf(LadderException.class);
+    }
+
+    @Test
+    void coordinate_createdSuccess() {
+        assertThat(Coordinate.of(1,1)).isEqualTo(Coordinate.of(1,1));
+    }
+
+    @DisplayName("좌표 한 개가 이동 성공")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "0:0:1:1",
+            "2:2:1:3"
+    }, delimiter = ':')
+    void coordinateGiven_moveSuccess(int x, int y, int x1, int y1) {
+        Coordinate coordinate = new Coordinate(x, y);
+        coordinate.move(validLines);
+        assertThat(coordinate).isEqualTo(new Coordinate(x1,y1));
+    }
+
+    @DisplayName("바가 같은 라인에 2개가 있어 좌표 한 개가 이동 실패")
+    @Test
+    void coordinateGiven_moveFail() {
+        Coordinate coordinate = new Coordinate(1, 0);
+        assertThatThrownBy(() -> coordinate.move(invalidLines))
+                .isInstanceOf(LadderException.class);
+    }
+}
