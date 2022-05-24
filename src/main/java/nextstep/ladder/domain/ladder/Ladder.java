2af133f6845696b9105fd4ca4d007d862a2d2a16
chkim <cndghks0213@gmail.com>
2022-05-25 00:05:56 +0900

    feat: 결과 이름 목록을 바탕으로 결과를 생성하는 기능, 사다리 이동 기능 구현

--- src/main/java/nextstep/ladder/domain/ladder/Ladder.java
+++ src/main/java/nextstep/ladder/domain/ladder/Ladder.java
@@ -1,5 +1,10 @@
 package nextstep.ladder.domain.ladder;
 
+import nextstep.ladder.domain.Position;
+import nextstep.ladder.domain.person.People;
+import nextstep.ladder.domain.person.Person;
+import nextstep.ladder.domain.result.Result;
+import nextstep.ladder.domain.result.Results;
 import nextstep.ladder.util.ConnectStrategy;
 
 import java.util.ArrayList;
@@ -7,19 +12,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Ladder {
-
     private static final int MIN_HEIGHT = 1;
+
     private final List<LadderLine> ladderLines;
 
     private Ladder(List<LadderLine> ladderLines) {
         this.ladderLines = ladderLines;
     }
 
-    public static Ladder from(int height, int sizeOfPerson, ConnectStrategy connectStrategy) {
+    public static Ladder from(int height, People people, ConnectStrategy connectStrategy) {
         validateHeight(height);
         List<LadderLine> ladderLines = new ArrayList<>();
         for (int i = 1; i <= height; i++) {
-            ladderLines.add(LadderLine.from(sizeOfPerson, connectStrategy));
+            ladderLines.add(LadderLine.of(people.size(), connectStrategy));
         }
         return new Ladder(ladderLines);
     }
@@ -30,10 +35,39 @@ public class Ladder {
         }
     }
 
+    public Results results(String names) {
+        Results results = Results.from(names);
+        if (lineLength() != results.size()) {
+            throw new IllegalArgumentException("사다리의 가로 길이와 결과의 길이가 같아야 합니다.");
+        }
+        return results;
+    }
+
+    public List<Result> results(People people, Results results) {
+        return people.people().stream()
+                .map(this::move)
+                .map(results::result)
+                .collect(Collectors.toList());
+    }
+
+    public Position move(Person person) {
+        Position position = person.position();
+        for (int height = 0; height < ladderLines.size(); height++) {
+            position = ladderLines.get(height).move(position);
+        }
+        return position;
+    }
+
+    private int lineLength() {
+        return ladderLines.get(0).size();
+    }
+
     @Override
     public String toString() {
         return ladderLines.stream()
                 .map(LadderLine::toString)
-                .collect(Collectors.joining("\n"));
+                .collect(Collectors.joining("\n")) + "\n";
     }
+
+
 }
--- src/test/java/nextstep/ladder/domain/ladder/LadderTest.java
+++ src/test/java/nextstep/ladder/domain/ladder/LadderTest.java
@@ -1,26 +1,46 @@
 package nextstep.ladder.domain.ladder;
 
+import nextstep.ladder.domain.person.People;
+import nextstep.ladder.domain.result.Results;
 import nextstep.ladder.util.RandomConnectStrategy;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class LadderTest {
 
-    @DisplayName("사다리의 높이, 참여 인원 수, 연결 전략을 입력하면 사다리가 생성된다.")
+    @DisplayName("사다리의 높이, 참여 인원, 연결 전략을 입력하면 사다리가 생성된다.")
     @Test
     void createLadderTest() {
-        Ladder ladder = Ladder.from(5, 5, new RandomConnectStrategy());
+        Ladder ladder = Ladder.from(5, People.from("pobi,honux,crong,jk,abc"), new RandomConnectStrategy());
         System.out.println(ladder);
     }
 
-    @DisplayName("사다리의 높이가 1보다 작으면 예외가 발생한다..")
+    @DisplayName("사다리의 높이가 1보다 작으면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(ints = {-1, 0})
     void createLadderTestFail(int height) {
-        Assertions.assertThatThrownBy(() -> Ladder.from(height, 5, new RandomConnectStrategy()))
+        assertThatThrownBy(() -> Ladder.from(height, People.from("pobi,honux,crong,jk,abc"), new RandomConnectStrategy()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("사다리에 결과 문자열을 넘겨 결과 목록을 생성한다.")
+    @Test
+    void resultsTest() {
+        People people = People.from("pobi,honux,crong,jk,abc");
+        Ladder ladder = Ladder.from(5, people, new RandomConnectStrategy());
+        ladder.results("꽝,5000,꽝,3000,20000");
+    }
+
+    @DisplayName("사다리에 결과 문자열을 넘겨 결과 목록을 생성한다. 사다리의 한 행의 길이와 결과의 길이가 다르면 예외가 발생한다.")
+    @Test
+    void resultsTestFail() {
+        Ladder ladder = Ladder.from(5, People.from("pobi,honux,crong,jk,abc"), new RandomConnectStrategy());
+        assertThatThrownBy(() -> ladder.results("꽝,5000,꽝,3000,20000,꽝"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
