feebe644d3661843e74f17cd6080c8741a8c2d45
chkim <cndghks0213@gmail.com>
2022-05-25 00:05:15 +0900

    feat: 사다리의 각 행에서 이동, 위치로 지점 반환 기능 추가

--- src/test/java/nextstep/ladder/domain/ladder/LadderLineTest.java
+++ src/test/java/nextstep/ladder/domain/ladder/LadderLineTest.java
@@ -1,15 +1,33 @@
 package nextstep.ladder.domain.ladder;
 
+import nextstep.ladder.domain.Position;
 import nextstep.ladder.util.RandomConnectStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LadderLineTest {
 
     @DisplayName("사다리의 한 행을 생성한다.")
     @Test
     void createLadderLineTest() {
-        LadderLine ladderLine = LadderLine.from(5, new RandomConnectStrategy());
+        LadderLine ladderLine = LadderLine.of(5, new RandomConnectStrategy());
         System.out.println(ladderLine);
+    }
+
+    @DisplayName("사다리의 한 행의 특점 지점을 반환한다.")
+    @Test
+    void pointTest() {
+        LadderLine ladderLine = LadderLine.of(5, new RandomConnectStrategy());
+        assertThat(ladderLine.point(Position.from(3)))
+                .isEqualTo(Point.of(Position.from(3), Direction.of(true, false)));
+    }
+
+    @DisplayName("사다리의 한 행의 크기를 반환한다.")
+    @Test
+    void sizeTest() {
+        LadderLine ladderLine = LadderLine.of(5, new RandomConnectStrategy());
+        assertThat(ladderLine.size()).isEqualTo(5);
     }
 }
