d8cf09d75d91438fa6479cdc7e536229b276129a
Roman <roman@channel.io>
2022-05-25 04:56:10 +0900

    Refactor :: Controller, View 리팩토링

--- src/main/java/ladder/LadderController.java
+++ src/main/java/ladder/LadderController.java
@@ -14,12 +14,50 @@ public class LadderController {
   }
 
   public void proceed() {
-    Players players = inputView.inputPlayerName();
+    Players players = inputView.inputPlayers();
+    Prizes prizes = inputView.inputResults(players);
     Height height = inputView.inputHeight();
 
     Ladder ladder = Ladder.from(players, height);
-    outputView.printResult();
-    outputView.printPlayerNames(players);
-    outputView.printLine(ladder);
+
+    outputView.printLadder(players, ladder, prizes);
+    LadderResults ladderResults = execute(ladder, players, prizes);
+    selectResult(ladderResults, players);
+  }
+
+  private LadderResults execute(Ladder ladder, Players players, Prizes prizes) {
+    LadderResults ladderResults = LadderResults.init(players);
+    for (Name player : players.playerNames()) {
+      LineIndex index = LineIndex.init(players, player);
+      ladderResults.put(player, ladder.findResult(prizes, index));
+    }
+    return ladderResults;
+  }
+
+  private void selectResult(LadderResults ladderResults, Players players) {
+    boolean keepGoing;
+    do {
+      keepGoing = printResult(ladderResults, players);
+    } while (keepGoing);
+  }
+
+  private boolean printResult(LadderResults ladderResults, Players players) {
+    Name player = inputView.inputName();
+    if (Name.EXIT.equals(player)) {
+      return false;
+    }
+
+    if (Name.ALL.equals(player)) {
+      outputView.printResultAll(ladderResults);
+      return true;
+    }
+
+    if (!players.has(player)) {
+      outputView.printWrongName();
+      return true;
+    }
+
+    outputView.printResult(ladderResults.prizeOf(player));
+    return true;
   }
 }
--- src/main/java/view/LadderOutputView.java
+++ src/main/java/view/LadderOutputView.java
@@ -1,36 +1,52 @@
 package view;
 
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.StringJoiner;
+import java.util.stream.Collectors;
 import ladder.Ladder;
 import ladder.Line;
+import ladder.Name;
 import ladder.Players;
+import ladder.Prizes;
+import ladder.LadderResults;
 
 public class LadderOutputView {
 
-  private static final String RESULT = "실행결과";
+  private static final String MESSAGE_FOR_WRONG_PLAYER_NAME = "잘못된 플레이어 이름입니다.";
+  private static final String RESULT_INFO = "실행결과";
+  private static final String LADDER_RESULT = "사다리 결과\n";
   private static final String LINE = "|";
   private static final String POINT = "-----";
   private static final String NOT_POINT = "     ";
   private static final String NEW_LINE = "\n";
   private static final String BLANK = " ";
+  private static final String PLAYER_AND_PRIZE_DELIMITER = " : ";
   private static final int NAME_DISTANCE = 6;
 
-  public void printResult() {
-    print(RESULT);
+
+  public void printLadderInfo() {
+    print(LADDER_RESULT);
   }
 
-  public void printLine(Ladder ladder) {
+  public void printLadder(Players players, Ladder ladder, Prizes prizes) {
+    printLadderInfo();
+    printPlayerNames(players);
     List<Line> lines = ladder.lines();
     StringJoiner stringJoiner = new StringJoiner(NEW_LINE);
     for (Line line : lines) {
       stringJoiner.add(lineToString(line));
     }
     print(stringJoiner.toString());
+    printPrizes(prizes);
   }
 
   public void printPlayerNames(Players players) {
-    List<String> names = players.playerNames();
+    List<String> names = players.playerNames()
+        .stream()
+        .map(Name::toString)
+        .collect(Collectors.toUnmodifiableList());
     StringBuilder stringBuilder = new StringBuilder();
     for (String name : names) {
       stringBuilder.append(name)
@@ -38,6 +54,34 @@ public class LadderOutputView {
     }
     print(stringBuilder.toString());
   }
+
+  public void printWrongName() {
+    print(MESSAGE_FOR_WRONG_PLAYER_NAME);
+  }
+
+  public void printPrizes(Prizes prizes) {
+    List<String> prizeList = prizes.prizes();
+    StringBuilder stringBuilder = new StringBuilder();
+    for (String prize : prizeList) {
+      stringBuilder.append(prize)
+          .append(BLANK.repeat(NAME_DISTANCE - prize.length()));
+    }
+    print(stringBuilder.toString());
+  }
+
+  public void printResultAll(LadderResults ladderResults) {
+    Set<Entry<Name, Name>> results = ladderResults.allPlayersAndResults();
+    StringJoiner stringJoiner = new StringJoiner(NEW_LINE);
+    for (Entry<Name, Name> result : results) {
+      stringJoiner.add(result.getKey() + PLAYER_AND_PRIZE_DELIMITER +  result.getValue());
+    }
+    print(stringJoiner.toString());
+  }
+
+  public void printResult(Name prize) {
+    print(RESULT_INFO + NEW_LINE + prize.toString());
+  }
+
 
   private String lineToString(Line line) {
     List<Boolean> points = line.points();
