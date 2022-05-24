8c500c46a9670f1ec73981538c031553f84636d4
chkim <cndghks0213@gmail.com>
2022-05-25 00:07:46 +0900

    feat: 사다리 결과와 각 참가자별 실행 결과를 출력하는 기능 추가

--- src/main/java/nextstep/ladder/LadderGame.java
+++ src/main/java/nextstep/ladder/LadderGame.java
@@ -1,27 +1,31 @@
 package nextstep.ladder;
 
+import nextstep.ladder.domain.Position;
 import nextstep.ladder.domain.ladder.Ladder;
 import nextstep.ladder.domain.person.People;
+import nextstep.ladder.domain.person.Person;
+import nextstep.ladder.domain.result.Results;
 import nextstep.ladder.util.RandomConnectStrategy;
-import nextstep.ladder.util.StringSpliter;
 import nextstep.ladder.view.InputView;
 import nextstep.ladder.view.ResultView;
 
 public class LadderGame {
     private static final String UNEXPECTED_ERROR_MESSAGE = "[ERROR] 예상치못한 에러가 발생했습니다. 게임을 재시작합니다.";
     private static final String LADDER_GAME_ERROR_MESSAGE = "\n[INFO] 게임을 재시작합니다.";
+    private static final String ALL = "all";
 
     public void start() {
         try {
             String names = InputView.inputPeopleName();
+            String resultNames = InputView.inputResults();
             String ladderHeight = InputView.inputLadderHeight();
 
-            People people = People.of(names);
-            Ladder ladder = Ladder.from(Integer.parseInt(ladderHeight), StringSpliter.length(names), new RandomConnectStrategy());
+            People people = People.from(names);
+            Ladder ladder = Ladder.from(Integer.parseInt(ladderHeight), people, new RandomConnectStrategy());
+            Results results = ladder.results(resultNames);
 
-            ResultView.printResultMessage();
-            ResultView.printPeople(people);
-            ResultView.printLadder(ladder);
+            ResultView.printResults(people, ladder, results);
+            findPersonResult(people, ladder, results);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage() + LADDER_GAME_ERROR_MESSAGE);
             start();
@@ -29,6 +33,31 @@ public class LadderGame {
             System.out.println(UNEXPECTED_ERROR_MESSAGE);
             start();
         }
+    }
+
+    public void findPersonResult(People people, Ladder ladder, Results results) {
+        while (true) {
+            if (matchPersonAndResult(people, ladder, results)) {
+                break;
+            }
+        }
+    }
+
+    private boolean matchPersonAndResult(People people, Ladder ladder, Results results) {
+        String findName = InputView.InputFindPersonName();
+        if (findName.equals(ALL)) {
+            ResultView.printPersonAndResult(people.people(), ladder.results(people, results));
+            return true;
+        }
+
+        try {
+            Person person = people.person(findName);
+            Position resultPosition = ladder.move(person);
+            ResultView.printMatchResult(results.result(resultPosition));
+        } catch (Exception e) {
+            findPersonResult(people, ladder, results);
+        }
+        return false;
     }
 
     public static void main(String[] args) {
