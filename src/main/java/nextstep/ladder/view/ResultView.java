1cc1ba4925042e997344d7229395f8e04809dfc1
chkim <cndghks0213@gmail.com>
2022-05-25 00:07:08 +0900

    feat: 사다리 타기 결과, 실행 결과 출력 기능 구현

--- src/main/java/nextstep/ladder/view/ResultView.java
+++ src/main/java/nextstep/ladder/view/ResultView.java
@@ -2,19 +2,30 @@ package nextstep.ladder.view;
 
 import nextstep.ladder.domain.ladder.Ladder;
 import nextstep.ladder.domain.person.People;
+import nextstep.ladder.domain.person.Person;
+import nextstep.ladder.domain.result.Result;
+import nextstep.ladder.domain.result.Results;
+
+import java.util.List;
 
 public class ResultView {
-    private static final String RESULT_MESSAGE = "실행결과";
+    private static final String RESULT_MESSAGE = "\n실행결과";
 
-    public static void printResultMessage() {
+    public static void printResults(People people, Ladder ladder, Results results) {
         System.out.println(RESULT_MESSAGE);
+        StringBuilder stringBuilder = new StringBuilder();
+        System.out.println(stringBuilder.append(people).append(ladder).append(results));
     }
 
-    public static void printPeople(People people) {
-        System.out.println(people);
+    public static void printMatchResult(Result result) {
+        System.out.println(RESULT_MESSAGE);
+        System.out.println(result + "\n");
     }
 
-    public static void printLadder(Ladder ladder) {
-        System.out.println(ladder);
+    public static void printPersonAndResult(List<Person> people, List<Result> results) {
+        System.out.println(RESULT_MESSAGE);
+        for (int index = 0; index < people.size(); index++) {
+            System.out.println(people.get(index) + " : " + results.get(index));
+        }
     }
 }
