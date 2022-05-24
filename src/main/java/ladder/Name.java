39d0be27451ff0633ce88b4fdd5f6810b461c475
Roman <roman@channel.io>
2022-05-25 04:54:51 +0900

    Refactor & Test :: Name 리팩토링 및 테스트 수정

--- src/main/java/ladder/Name.java
+++ src/main/java/ladder/Name.java
@@ -1,10 +1,17 @@
 package ladder;
 
+import java.util.Objects;
+
 public class Name {
 
+  private static final String EXIT_TRIGGER = "exit";
+  private static final String ALL_TRIGGER = "all";
   private static final String MESSAGE_FOR_BLANK = "참가자 이름이 비어있으면 안됩니다.";
   private static final String MESSAGE_FOR_OVER_LENGTH = "참가자 이름은 %s글자 이내여야 합니다.";
   private static final int MAX_NAME_LENGTH = 5;
+
+  public static final Name ALL = new Name(ALL_TRIGGER);
+  public static final Name EXIT = new Name(EXIT_TRIGGER);
 
   private final String name;
 
@@ -18,12 +25,41 @@ public class Name {
     return this.name;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Name other = (Name) o;
+    return Objects.equals(name, other.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
+
   private void validateName(String name) {
     if (name == null || name.isBlank()) {
       throw new IllegalArgumentException(MESSAGE_FOR_BLANK);
     }
+
     if (name.length() > MAX_NAME_LENGTH) {
       throw new IllegalArgumentException(String.format(MESSAGE_FOR_OVER_LENGTH, MAX_NAME_LENGTH));
     }
+  }
+
+  public static Name of(String name) {
+    if (ALL_TRIGGER.equals(name)) {
+      return ALL;
+    }
+
+    if (EXIT_TRIGGER.equals(name)) {
+      return EXIT;
+    }
+    return new Name(name);
   }
 }
--- src/test/java/ladder/NameTest.java
+++ src/test/java/ladder/NameTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -22,6 +24,23 @@ class NameTest {
     assertDoesNotThrow(() -> new Name(validName));
   }
 
+  @DisplayName("all으로 캐싱 Name 생성 성공")
+  @Test
+  void Name_ALL_생성_성공() {
+      assertThat(Name.ALL).isEqualTo(new Name("all"));
+  }
+
+  @DisplayName("exit으로 캐싱 Name 생성 성공")
+  @Test
+  void Name_EXIT_생성_성공() {
+    assertThat(Name.EXIT).isEqualTo(new Name("exit"));
+  }
+
+  @Test
+  void equals_성공() {
+    assertThat(new Name("test")).isEqualTo(new Name("test"));
+  }
+
   @ParameterizedTest(name = "{0}으로 Name 생성 실패")
   @ValueSource(strings = {"", " ", "123456"})
   void Name_생성_실패(String invalidName) {
@@ -33,5 +52,15 @@ class NameTest {
   void toString_성공(String rawName) {
     Name name = new Name(rawName);
     assertThat(name.toString()).isEqualTo(rawName);
+  }
+
+  @Test
+  void NAME_ALL_생성_성공() {
+    assertThat(Name.of("all")).isEqualTo(Name.ALL);
+  }
+
+  @Test
+  void NAME_EXIT_생성_성공() {
+    assertThat(Name.of("exit")).isEqualTo(Name.EXIT);
   }
 }
