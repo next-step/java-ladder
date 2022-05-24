79280cdb3cb0419437cef516f9c55e4889f03263
chkim <cndghks0213@gmail.com>
2022-05-25 00:02:10 +0900

    feat: Name equals, hashCode 재정의

--- src/main/java/nextstep/ladder/domain/person/Name.java
+++ src/main/java/nextstep/ladder/domain/person/Name.java
@@ -1,5 +1,7 @@
 package nextstep.ladder.domain.person;
 
+import java.util.Objects;
+
 public class Name {
     private static final int MAX_LENGTH = 5;
     private static final int MIN_LENGTH = 1;
@@ -15,6 +17,19 @@ public class Name {
         if (name.length() > MAX_LENGTH || name.length() < MIN_LENGTH) {
             throw new IllegalArgumentException("[ERROR] 참여자의 이름은 1자 이상 5자 미만으로 설정해주세요.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 
     @Override
