19a98ada58a0aea3ac03e3714ca21895272205e2
chkim <cndghks0213@gmail.com>
2022-05-25 00:06:27 +0900

    feat: 결과 목록, 결과를 보고 싶은 사람 이름을 입력받는 기능 구현

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String INPUT_RESULTS_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String INPUT_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    private static final String INPUT_FIND_PERSON_NAME = "결과를 보고 싶은 사람은?";
 
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -13,8 +15,18 @@ public class InputView {
         return scanner.nextLine();
     }
 
+    public static String inputResults() {
+        System.out.println(INPUT_RESULTS_MESSAGE);
+        return scanner.nextLine();
+    }
+
     public static String inputLadderHeight() {
         System.out.println(INPUT_LADDER_HEIGHT_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static String InputFindPersonName() {
+        System.out.println(INPUT_FIND_PERSON_NAME);
         return scanner.nextLine();
     }
 }
