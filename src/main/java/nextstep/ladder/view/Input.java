c8f0c330758537f6f08110fc190cf1fbc5b538aa
eun jung <62321968+dmswjd4117@users.noreply.github.com>
2022-05-25 13:57:37 +0900

    refactor: Name -> Player 로 이름 변경

--- src/main/java/nextstep/ladder/view/Input.java
+++ src/main/java/nextstep/ladder/view/Input.java
@@ -1,6 +1,6 @@
 package nextstep.ladder.view;
 
-import nextstep.ladder.domain.Name;
+import nextstep.ladder.domain.Player;
 
 import java.util.List;
 import java.util.Scanner;
@@ -8,16 +8,16 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Input {
-    private final static String PARTICIPATION_USERNAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
-    private final static String MAX_LADDER_LENGTH = "최대 사다리 높이는 몇 개인가요?";
-    private final static String NAME_DELIMITER = ",";
-    private static Scanner scanner = new Scanner(System.in);
+    private static final String PARTICIPATION_USERNAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String MAX_LADDER_LENGTH = "최대 사다리 높이는 몇 개인가요?";
+    private static final String NAME_DELIMITER = ",";
+    private static final Scanner scanner = new Scanner(System.in);
 
-    public static List<Name> readUserNameList(){
+    public static List<Player> readUserNameList(){
         System.out.println(PARTICIPATION_USERNAME);
         String input = scanner.nextLine();
         return Stream.of(input.split(NAME_DELIMITER))
-                .map(Name::new)
+                .map(Player::new)
                 .collect(Collectors.toList());
     }
 
--- src/test/java/nextstep/ladder/domain/PlayerTest.java
+++ src/test/java/nextstep/ladder/domain/PlayerTest.java
@@ -2,15 +2,24 @@ package nextstep.ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class NameTest {
+class PlayerTest {
 
     @Test
     @DisplayName("이름이 5글자를 넘어가면 예외가 발생한다.")
     void 이름_생성_예외(){
-        assertThatThrownBy(()->new Name("123456"))
+        assertThatThrownBy(()->new Player("123456"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    @DisplayName("Player 객체 생성 테스트")
+    void Player_생성() {
+        assertThat(new Player("test"))
+                .isNotNull()
+                .isInstanceOf(Player.class);
+    }
 }
