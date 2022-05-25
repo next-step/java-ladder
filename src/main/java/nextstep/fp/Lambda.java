9574eee0a6528032b7beffd52525fca63c24099e
KimYongKwon <dydrnjs518@gmail.com>
2022-05-25 22:49:04 +0900

    refactor - step1 람다 수정

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -26,28 +26,19 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers, Conditional c) {
-        int total = 0;
-
-        for (int number : numbers) {
-            if (c.isPossibleSum(number)) {
-                total += number;
-            }
-        }
-        return total;
+    public static int sumAll(List<Integer> numbers) {
+        return sum(numbers, number -> true);
     }
 
-    public static int sumAllEven(List<Integer> numbers, Conditional c) {
-        int total = 0;
-        for (int number : numbers) {
-            if (c.isPossibleSum((number))) {
-                total += number;
-            }
-        }
-        return total;
+    public static int sumAllEven(List<Integer> numbers) {
+        return sum(numbers, number -> (number % 2 == 0));
     }
 
-    public static int sumAllOverThree(List<Integer> numbers, Conditional c) {
+    public static int sumAllOverThree(List<Integer> numbers) {
+        return sum(numbers, number -> (number > 3));
+    }
+
+    public static int sum(List<Integer> numbers, Conditional c) {
         int total = 0;
         for (int number : numbers) {
             if (c.isPossibleSum(number)) {
