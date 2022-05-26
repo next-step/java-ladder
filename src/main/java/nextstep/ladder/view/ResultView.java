b7282472529bb0295407e69bf061cf4faab70b31
chkim <cndghks0213@gmail.com>
2022-05-27 04:54:54 +0900

    refactor: 참가자와 사다리 결과를 출력하는 로직을 ResultView로 이동

--- src/main/java/nextstep/ladder/view/ResultView.java
+++ src/main/java/nextstep/ladder/view/ResultView.java
@@ -1,5 +1,6 @@
 package nextstep.ladder.view;
 
+import nextstep.ladder.domain.Position;
 import nextstep.ladder.domain.ladder.Ladder;
 import nextstep.ladder.domain.person.People;
 import nextstep.ladder.domain.person.Person;
@@ -10,11 +11,39 @@ import java.util.List;
 
 public class ResultView {
     private static final String RESULT_MESSAGE = "\n실행결과";
+    private static final String ALL = "all";
 
     public static void printResults(People people, Ladder ladder, Results results) {
         System.out.println(RESULT_MESSAGE);
         StringBuilder stringBuilder = new StringBuilder();
         System.out.println(stringBuilder.append(people).append(ladder).append(results));
+
+        printPersonAndMatchedResult(people, ladder, results);
+    }
+
+    private static void printPersonAndMatchedResult(People people, Ladder ladder, Results results) {
+        while (true) {
+            if (matchPersonAndResult(people, ladder, results)) {
+                break;
+            }
+        }
+    }
+
+    private static boolean matchPersonAndResult(People people, Ladder ladder, Results results) {
+        String findName = InputView.InputFindPersonName();
+        if (findName.equals(ALL)) {
+            ResultView.printPersonAndResult(people.people(), ladder.results(people, results));
+            return true;
+        }
+
+        try {
+            Person person = people.person(findName);
+            Position resultPosition = ladder.move(person);
+            ResultView.printMatchResult(results.result(resultPosition));
+        } catch (Exception e) {
+            printPersonAndMatchedResult(people, ladder, results);
+        }
+        return false;
     }
 
     public static void printMatchResult(Result result) {
@@ -28,4 +57,5 @@ public class ResultView {
             System.out.println(people.get(index) + " : " + results.get(index));
         }
     }
+
 }
