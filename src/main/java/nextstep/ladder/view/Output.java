ad851f0e58c12d9445c05852c87bd35da766c2cd
dmswjd4117 <62321968+dmswjd4117@users.noreply.github.com>
2022-05-27 07:43:03 +0900

    refactor: Output.printAllGameResults 스트림 사용하기

--- src/main/java/nextstep/ladder/view/Output.java
+++ src/main/java/nextstep/ladder/view/Output.java
@@ -11,6 +11,7 @@ public class Output {
     private static final String NOT_EMPTY_POINT = "-----";
     private static final String EMPTY_POINT = "     ";
     private static final String VERTICAL_LINE = "|";
+    private static final String DELIMITER_BETWEEN_NAME_AND_RESULT = " : ";
     private static final int VERTICAL_LINE_WIDTH = 1;
     private static final int POINT_WIDTH = 5;
 
@@ -74,13 +75,14 @@ public class Output {
 
     public static void printAllGameResults(Map<Player, LadderResult> gameResults) {
         System.out.println(GAME_RESULT_MESSAGE);
-//        gameResults.entrySet().stream();
-        for(Map.Entry<Player, LadderResult> entry: gameResults.entrySet()){
-            Player player = entry.getKey();
-            LadderResult ladderResult = entry.getValue();
-            System.out.println(String.join(" : ", player.getName(), ladderResult.getValue()));
-        }
+        gameResults.entrySet()
+                .forEach(Output::printNameAndResult);
+    }
 
+    private static void printNameAndResult(Map.Entry<Player, LadderResult> entry){
+        Player player = entry.getKey();
+        LadderResult ladderResult = entry.getValue();
+        System.out.println(String.join(DELIMITER_BETWEEN_NAME_AND_RESULT, player.getName(), ladderResult.getValue()));
     }
 
     public static void printGameResultByPlayerName(LadderResult result) {
