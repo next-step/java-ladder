2cac674ddb2482988e248319d3a46dea65e77d47
dmswjd4117 <62321968+dmswjd4117@users.noreply.github.com>
2022-05-28 14:18:48 +0900

    feat: Line points 생성, 움직임 구현

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -3,29 +3,22 @@ package nextstep.ladder.domain;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.IntStream;
 
 import static java.util.Collections.unmodifiableList;
 
 public class Line {
 
     private static final String CANNOT_NULL_FOR_POINTS = "Line의 points는 null일 수 없습니다.";
-    private static final String INVALID_LINE_POINT_CNT = "0개 이상의 point개수를 가진 라인만 생성할 수 있습니다.";
-    private static final int EDGE_CNT = 2;
-    private static final int EDGE_WIDTH = 1;
-    private static final int MIN_POINT_CNT = 0;
-    private static final int DIFFERENCE_BETWEEN_POINT = 1;
-    private static final int DIFFERENCE_BETWEEN_POSITION = 1;
+    private static final int MIN_POINT_CNT = 1;
 
+    private final List<Point> points;
 
-    private final List<Boolean> points;
-
-    public Line(List<Boolean> points) {
+    public Line(List<Point> points) {
         validate(points);
         this.points = unmodifiableList(points);
     }
 
-    private void validate(List<Boolean> points){
+    private void validate(List<Point> points){
         if(points == null){
             throw new IllegalArgumentException(CANNOT_NULL_FOR_POINTS);
         }
@@ -33,41 +26,43 @@ public class Line {
 
     public static Line of(int cnt, PointCreationRule pointCreationRule){
         if(cnt < MIN_POINT_CNT){
-            throw new IllegalArgumentException(INVALID_LINE_POINT_CNT);
+            throw new IllegalArgumentException(MIN_POINT_CNT+"개 이상의 point개수를 가진 라인만 생성할 수 있습니다.");
         }
-        List<Boolean> points = new ArrayList<>();
-        for(int i=0; i<cnt+EDGE_CNT; i++){
-            points.add(false);
-        }
-        IntStream.rangeClosed(EDGE_WIDTH, cnt)
-                .filter((num)->pointCreationRule.canCreate())
-                .filter(index-> isValid(points, index))
-                .forEach(index -> createPoint(points, index));
+        List<Point> points = new ArrayList<>();
+
+        Point firstPoint = initFirst(points, pointCreationRule);
+        Point lastBeforePoint = initBody(cnt-2, points, firstPoint, pointCreationRule);
+        initLast(points, lastBeforePoint);
+
         return new Line(points);
     }
 
-    private static boolean isValid(List<Boolean> line, int index){
-        return !line.get(index- DIFFERENCE_BETWEEN_POINT) && !line.get(index+ DIFFERENCE_BETWEEN_POINT);
+    private static Point initBody(int count, List<Point> points, Point point, PointCreationRule pointCreationRule) {
+        for(int i=0; i<count; i++){
+            point = point.next(pointCreationRule.canCreate());
+            points.add(point);
+        }
+        return point;
     }
 
-    private static void createPoint(List<Boolean> line, int index){
-        line.set(index, true);
+    private static Point initFirst(List<Point> points, PointCreationRule pointCreationRule){
+        Point point = Point.first(pointCreationRule.canCreate());
+        points.add(point);
+        return point;
     }
 
-    public List<Boolean> getPoints(){
+    private static void initLast(List<Point> points, Point lastBeforePoint){
+        Point point = lastBeforePoint.last();
+        points.add(point);
+    }
+
+    public List<Point> getPoints(){
         return points;
     }
 
     public int moveFrom(int position) {
-        boolean leftPoint = points.get(position);
-        boolean rightPoint = points.get(position+ DIFFERENCE_BETWEEN_POINT);
-        if(leftPoint){
-            return position-DIFFERENCE_BETWEEN_POSITION;
-        }
-        if(rightPoint){
-            return position+DIFFERENCE_BETWEEN_POSITION;
-        }
-        return position;
+        Point point = getPoints().get(position);
+        return point.move();
     }
 
     @Override
