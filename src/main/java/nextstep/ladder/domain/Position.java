37bf9dd552038fdf4ab192a571f9a2c3de454b4d
younlll <youn.lee96@gmail.com>
2022-05-30 22:27:43 +0900

    feat: 위치에 대한 기능 구현

---
+++ src/main/java/nextstep/ladder/domain/Position.java
@@ -0,0 +1,55 @@
+package nextstep.ladder.domain;
+
+import nextstep.ladder.generator.PositionGenerator;
+
+public class Position {
+    private static final int INCREASE_VALUE = 1;
+    private static final int INITIAL_POSITION = 0;
+
+    private final int value;
+    private final Direction direction;
+
+    public Position(int index, Direction direction) {
+        this.value = index;
+        this.direction = direction;
+    }
+
+    public int ride() {
+        if(direction.isRight()) {
+            return value + INCREASE_VALUE;
+        }
+
+        if(direction.isLeft()) {
+            return value - INCREASE_VALUE;
+        }
+
+        return this.value;
+    }
+
+    public Position next(PositionGenerator positionGenerator) {
+        return new Position(value + INCREASE_VALUE, direction.next(positionGenerator));
+    }
+
+    public Position next(boolean right) {
+        return new Position(value + INCREASE_VALUE, direction.last());
+    }
+
+    public static Position first(boolean right) {
+        return new Position(INITIAL_POSITION, Direction.first(right));
+    }
+
+    public boolean isLeft() {
+        return direction.isLeft();
+    }
+
+    public boolean isRight() {
+        return direction.isRight();
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "value=" + value +
+                '}';
+    }
+}
