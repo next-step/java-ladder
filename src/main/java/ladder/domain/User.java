7888db2776cb8ab664fa30d354e903b70e847f0c
rumor1993 <rumor1993@naver.com>
2022-05-31 18:30:52 +0900

    feat: 유저 객체에 위치 객체 추가

--- src/main/java/ladder/domain/User.java
+++ src/main/java/ladder/domain/User.java
@@ -1,12 +1,30 @@
 package ladder.domain;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class User {
     private Name name;
+    private Position position;
 
     public User(String name) {
         this.name = new Name(name);
+    }
+
+    public User(String name, int index) {
+        this(name);
+        this.position = new Position(index, 0);
+    }
+
+    public int right(List<Line> lines) {
+        Optional.ofNullable(lines.get(position.getRightPosition()));
+        return 0;
+    }
+
+    public int left(List<Line> lines) {
+        Optional.ofNullable(lines.get(position.getRightPosition()));
+        return 0;
     }
 
     @Override
@@ -26,4 +44,5 @@ public class User {
     public String toString() {
         return name.toString();
     }
+
 }
