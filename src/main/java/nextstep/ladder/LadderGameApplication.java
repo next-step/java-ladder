ad3179eb190042835e4c24d8918b8862488ebfdb
jeongjaeeom <jeongjae.eom@gmail.com>
2022-05-31 14:46:23 +0900

    feat($compile): 사디리 포인트 이동(왼쪽, 오른쪽, 멈춤) 기능 구현.
    사디리 포인트 이동(왼쪽, 오른쪽, 멈춤) 기능 구현.
    - 현재 포인트를 가지는 Point객체 추가
    - 현재 이동 방향을 알 수 있는 Direction Enum 객체 추가.

--- src/main/java/nextstep/ladder/LadderGameApplication.java
+++ src/main/java/nextstep/ladder/LadderGameApplication.java
@@ -1,17 +1,8 @@
 package nextstep.ladder;
 
-import nextstep.ladder.domain.Height;
-import nextstep.ladder.domain.Lines;
-import nextstep.ladder.domain.Players;
-import nextstep.ladder.view.InputView;
-import nextstep.ladder.view.ResultView;
-
 public class LadderGameApplication {
 
   public static void main(String[] args) {
-    Players players = new Players(Players.create(InputView.playerNames()));
-    Height height = new Height(InputView.ladderHeight());
-    Lines lines = Lines.of(height.height(), players.count());
-    ResultView.print(players, lines);
+
   }
 }
