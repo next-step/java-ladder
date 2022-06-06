6937294b9c4acdd13306f34c01b1fb442e109db4
Roman <roman@channel.io>
2022-06-06 15:25:47 +0900

    BugFix :: moveLeft() 의 잘못된 로직 수정

--- src/main/java/ladder/LineIndex.java
+++ src/main/java/ladder/LineIndex.java
@@ -8,6 +8,7 @@ public class LineIndex {
   private static final String MESSAGE_FOR_INVALID_INDEX = "현재 위치는 0과 참가자수 사이여야 합니다.";
   private static final int NEXT = 1;
   private static final int PREV = 1;
+
   private final int playerNumber;
   private final int index;
 
@@ -18,15 +19,28 @@ public class LineIndex {
   }
 
   public LineIndex moveRight() {
-    return new LineIndex(playerNumber, NEXT + index);
+    return new LineIndex(playerNumber, index + NEXT);
   }
 
   public LineIndex moveLeft() {
-    return new LineIndex(playerNumber, PREV + index);
+    return new LineIndex(playerNumber, index - PREV);
   }
 
   public int value() {
     return index;
+  }
+
+  private void validateIndex(int playerNumber, int index) {
+    if (index < 0 || playerNumber <= index) {
+      throw new IllegalArgumentException(MESSAGE_FOR_INVALID_INDEX);
+    }
+  }
+
+  public static LineIndex init(Players players, Name name) {
+    if (!players.has(name)) {
+      throw new IllegalArgumentException(MESSAGE_FOR_INVALID_NAME);
+    }
+    return new LineIndex(players.playerNumber(), players.startIndexOf(name));
   }
 
   @Override
@@ -44,18 +58,5 @@ public class LineIndex {
   @Override
   public int hashCode() {
     return Objects.hash(playerNumber, index);
-  }
-
-  private void validateIndex(int playerNumber, int index) {
-    if (index < 0 || playerNumber <= index) {
-      throw new IllegalArgumentException(MESSAGE_FOR_INVALID_INDEX);
-    }
-  }
-
-  public static LineIndex init(Players players, Name name) {
-    if (!players.has(name)) {
-      throw new IllegalArgumentException(MESSAGE_FOR_INVALID_NAME);
-    }
-    return new LineIndex(players.playerNumber(), players.startIndexOf(name));
   }
 }
