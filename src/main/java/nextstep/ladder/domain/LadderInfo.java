3e1892962a87901f39bd295018898b3881308ef8
jess <official1126@naver.com>
2022-06-07 22:55:16 +0900

    refactor class definition convention

--- src/main/java/nextstep/ladder/domain/LadderInfo.java
+++ src/main/java/nextstep/ladder/domain/LadderInfo.java
@@ -4,16 +4,26 @@ import nextstep.ladder.exceptions.OutOfLadderSizeException;
 
 public class LadderInfo {
 
+    private final Lines lines;
+
     private final int height;
 
     private final int numberOfParticipants;
-
-    private final Lines lines;
 
     public LadderInfo(int height, int numberOfParticipants) {
         this.height = height;
         this.numberOfParticipants = numberOfParticipants;
         this.lines = new Lines(height, numberOfParticipants);
+    }
+
+    public boolean lines(int row, int column) {
+        if (row < 0
+                || row >= height
+                || column < 0
+                || column >= numberOfParticipants - 1) {
+            throw new OutOfLadderSizeException("사다리 범위를 넘는 값 조회입니다.");
+        }
+        return this.lines.value().get(row).points().get(column).linked();
     }
 
     public int height() {
@@ -22,12 +32,5 @@ public class LadderInfo {
 
     public int numberOfParticipants() {
         return numberOfParticipants;
-    }
-
-    public boolean lines(int row, int column) {
-        if (row < 0 || row >= height || column < 0 || column >= numberOfParticipants - 1) {
-            throw new OutOfLadderSizeException("사다리 범위를 넘는 값 조회입니다.");
-        }
-        return this.lines.value().get(row).points().get(column).linked();
     }
 }
