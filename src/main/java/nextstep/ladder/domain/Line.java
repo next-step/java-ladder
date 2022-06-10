19f901d23571818a0351c989d79cd3d934d96a63
jess <official1126@naver.com>
2022-06-10 19:39:57 +0900

    refactor violated indentation rule

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -5,23 +5,27 @@ import java.util.List;
 
 public class Line {
 
-    private static final RandomLineStrategy randomLineStrategy = new RandomLineStrategy();
+    private static final RandomLineStrategy RANDOM_LINE_STRATEGY = new RandomLineStrategy();
 
     private final List<Point> points;
 
     public Line(int numberOfParticipants) {
         this.points = new ArrayList<>();
 
-        Point point = new Point();
-        this.points.add(point.next(randomLineStrategy));
-
-        for (int i = 1; i < numberOfParticipants - 1; i++) {
-            if (this.points.get(i-1).linked()) {
-                this.points.add(new Point());
-                continue;
-            }
-            this.points.add(point.next(randomLineStrategy));
+        for (int i = 0; i < numberOfParticipants - 1; i++) {
+            this.points.add(nextPoint(i));
         }
+    }
+
+    private Point nextPoint(int index) {
+        if (index == 0) {
+            return new Point().next(RANDOM_LINE_STRATEGY);
+        }
+        if (this.points.get(index - 1).linked()) {
+            return new Point();
+        }
+
+        return new Point().next(RANDOM_LINE_STRATEGY);
     }
 
     public List<Point> points() {
