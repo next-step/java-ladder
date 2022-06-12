9563bbcdb74b80f25173531f33c68f3d94ff98fb
rumor1993 <rumor1993@naver.com>
2022-06-12 23:02:37 +0900

    refactor: Direction init_invalid 테스트 케이스 추가

--- src/test/java/ladder/domain/DirectionTest.java
+++ src/test/java/ladder/domain/DirectionTest.java
@@ -11,4 +11,10 @@ class DirectionTest {
     void init() {
         assertThat(Direction.of(true, false)).isEqualTo(Direction.of(true, false));
     }
+
+
+    @Test
+    public void init_invalid() {
+        assertThatThrownBy(() -> Direction.of(true, true)).isInstanceOf(IllegalStateException.class);
+    }
 }
