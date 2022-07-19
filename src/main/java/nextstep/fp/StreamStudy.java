fc6aa360b2739f6a61e689f37c64f493ed984fd3
jaden94 <46964910+jadenkim94@users.noreply.github.com>
2022-07-19 21:18:07 +0900

    Step1 (#1453)
    * refactor/ use lamda instead anonymous class
    
    * refactor/ use lamda for remove duplication
    
    * refactor/ implement sumOverThreeAndDouble(), printLongestWordTop100() using stream
    
    * refactor/ use optional

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,7 +5,9 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
@@ -15,19 +17,29 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream()
+                .filter(w -> w.length() > 12)
+                .count();
     }
 
-    public static void printLongestWordTop100() throws IOException {
+    /*
+    단어의 길이가 12자를 초과하는 단어를 추출한다.
+    12자가 넘는 단어 중 길이가 긴 순서로 100개의 단어를 추출한다.
+    단어 중복을 허용하지 않는다. 즉, 서로 다른 단어 100개를 추출해야 한다.
+    추출한 100개의 단어를 출력한다. 모든 단어는 소문자로 출력해야 한다.
+     */
+    public static List<String> printLongestWordTop100() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        return words.stream()
+            .filter( word -> word.length() > 12)
+            .distinct()
+            .sorted((o1, o2) -> o2.length() - o1.length())
+            .limit(100)
+            .map(word -> word.toLowerCase(Locale.ROOT))
+            .collect(Collectors.toList());
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -38,7 +50,10 @@ public class StreamStudy {
         return numbers.stream().reduce(0, (x, y) -> x + y);
     }
 
+    // List에 담긴 모든 숫자 중 3보다 큰 숫자를 2배 한 후 모든 값의 합
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return 2 * numbers.stream()
+            .filter(num -> num > 3)
+            .reduce(0, (x, y) -> x + y);
     }
 }
