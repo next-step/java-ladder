7b0c26de28709d1c91cd270a549982ae8b3f3fa7
JunH <tjdtls690@gmail.com>
2022-09-28 01:28:54 +0900

    :sparkles: feat : 가로라인의 맨 끝은 무조건 라인이 없어야 하는 테스트 완료

--- src/test/java/ladder/step2/domain/LineTest.java
+++ src/test/java/ladder/step2/domain/LineTest.java
@@ -2,9 +2,7 @@ package ladder.step2.domain;
 
 import ladder.step2.domain.dto.LineDTO;
 import ladder.step2.domain.strategy.RandomLineCreateStrategy;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.*;
 
 import java.util.List;
 import java.util.stream.IntStream;
@@ -12,18 +10,26 @@ import java.util.stream.IntStream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
+    private List<Boolean> partLines;
     
-    @Nested
-    @DisplayName("올바른 가로라인 생성 확인")
-    class CorrectCreatePartLine {
-        @DisplayName("가로라인이 겹치지 않는지 확인")
-        @RepeatedTest(value = 100, name = "{displayName}")
-        void correct_create_part_line() {
-            Line line = new Line(5, new RandomLineCreateStrategy());
-            LineDTO lineDTO = line.getLineInformation();
-            List<Boolean> partLines = lineDTO.getPartLines();
-            IntStream.range(0, partLines.size() - 1)
-                    .forEach(index -> assertThat(partLines.get(index) && partLines.get(index + 1)).isFalse());
-        }
+    @BeforeEach
+    void setUp() {
+        Line line = new Line(5, new RandomLineCreateStrategy());
+        LineDTO lineDTO = line.getLineInformation();
+        partLines = lineDTO.getPartLines();
     }
+    
+    @Test
+    @DisplayName("가로라인이 겹치지 않는지 확인")
+    void correct_create_part_line() {
+        IntStream.range(0, partLines.size() - 1)
+                .forEach(index -> assertThat(partLines.get(index) && partLines.get(index + 1)).isFalse());
+    }
+
+    @Test
+    @DisplayName("가로라인의 끝 부분은 부분라인이 없는지 확인")
+    void last_part_line() {
+        assertThat(partLines.get(partLines.size() - 1)).isFalse();
+    }
+    
 }
