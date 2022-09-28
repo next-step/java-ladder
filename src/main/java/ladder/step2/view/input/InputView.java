86a6e6cc9e36df8f618dd4fa64ad7cb6bbc2ca1c
JunH <tjdtls690@gmail.com>
2022-09-29 03:21:04 +0900

    :sparkles: feat : 쉼표를 구분자로 받는 기능 구현

--- src/main/java/ladder/step2/view/input/InputView.java
+++ src/main/java/ladder/step2/view/input/InputView.java
@@ -4,13 +4,34 @@ import ladder.step2.domain.Player;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class InputView {
+    private static final String INPUT_EXCEPTION_MESSAGE = "올바른 입력 형식이 아닙니다. 다시 입력해주세요.";
+    private static final String PLAYER_NAMES_INPUT_FORM = "[a-zA-Z]{1,5}(,[a-zA-Z]{1,5})*";
+    private static final String SPACE = " ";
+    private static final String EMPTY = "";
+    private static final String DELIMITER = ",";
+    
     public static List<Player> inputPlayerNames(String playerNames) {
-        playerNames = playerNames.replace(" ", "");
-        return Arrays.stream(playerNames.split(","))
+        playerNames = deleteSpace(playerNames);
+        checkInputForm(playerNames);
+        return Arrays.stream(playerNames.split(DELIMITER))
                 .map(Player::new)
                 .collect(Collectors.toList());
+    }
+    
+    private static String deleteSpace(String playerNames) {
+        playerNames = playerNames.replace(SPACE, EMPTY);
+        return playerNames;
+    }
+    
+    private static void checkInputForm(String playerNames) {
+        Matcher matcher = Pattern.compile(PLAYER_NAMES_INPUT_FORM).matcher(playerNames);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(INPUT_EXCEPTION_MESSAGE);
+        }
     }
 }
--- src/test/java/ladder/step2/view/input/InputViewTest.java
+++ src/test/java/ladder/step2/view/input/InputViewTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class InputViewTest {
     
@@ -16,5 +16,13 @@ class InputViewTest {
     void inputPlayerNames() {
         List<Player> players = InputView.inputPlayerNames("pobi, jun, honux, jk");
         assertThat(players).isEqualTo(Arrays.asList(new Player("pobi"), new Player("jun"), new Player("honux"), new Player("jk")));
+    }
+    
+    @Test
+    @DisplayName("구분자가 쉼표가 아니면 예외 던지기")
+    void delimiterException() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputView.inputPlayerNames("pobi, jun, honux. jk"))
+                .withMessage("올바른 입력 형식이 아닙니다. 다시 입력해주세요.");
     }
 }
