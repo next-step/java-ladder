91fa5a141f472b95fb2026f78f5a0558c2e8bbc1
JunH <tjdtls690@gmail.com>
2022-09-29 14:28:50 +0900

    :memo: docs : 기능 구현 목록 수정

--- src/main/java/ladder/step2/view/input/InputView.java
+++ src/main/java/ladder/step2/view/input/InputView.java
@@ -4,28 +4,38 @@ import ladder.step2.domain.Player;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
     private static final String INPUT_EXCEPTION_MESSAGE = "올바른 입력 형식이 아닙니다. 다시 입력해주세요.";
     private static final String PLAYER_NAMES_INPUT_FORM = "[a-zA-Z]{1,5}(,[a-zA-Z]{1,5})*";
     private static final String SPACE = " ";
     private static final String EMPTY = "";
     private static final String DELIMITER = ",";
     
+    public static List<Player> inputPlayerNames() {
+        try {
+            return inputPlayerNames(SCANNER.nextLine());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputPlayerNames();
+        }
+    }
+    
     public static List<Player> inputPlayerNames(String playerNames) {
         playerNames = deleteSpace(playerNames);
         checkInputForm(playerNames);
+        return getPlayers(playerNames);
+    }
+    
+    private static List<Player> getPlayers(String playerNames) {
         return Arrays.stream(playerNames.split(DELIMITER))
                 .map(Player::new)
                 .collect(Collectors.toList());
-    }
-    
-    private static String deleteSpace(String playerNames) {
-        playerNames = playerNames.replace(SPACE, EMPTY);
-        return playerNames;
     }
     
     private static void checkInputForm(String playerNames) {
@@ -33,5 +43,13 @@ public class InputView {
         if (!matcher.matches()) {
             throw new IllegalArgumentException(INPUT_EXCEPTION_MESSAGE);
         }
+    }
+    
+    private static String deleteSpace(String playerNames) {
+        return playerNames.replace(SPACE, EMPTY);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(inputPlayerNames());
     }
 }
