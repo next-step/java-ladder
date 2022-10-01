83917381dbf81adf588644b00b9ab58468898eb5
JunH <tjdtls690@gmail.com>
2022-10-01 14:46:55 +0900

    :art: refactor : 플레이어 이름 길이 점검 코드 추가

--- src/main/java/ladder/step2/domain/PlayerName.java
+++ src/main/java/ladder/step2/domain/PlayerName.java
@@ -5,14 +5,20 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class PlayerName {
+    private static final String PLAYER_NAME_LENGTH_EXCEPTION_MESSAGE = "플레이어 이름은 5자를 초과할 수 없습니다.";
     private static final String PLAYER_NAME_PRINT_FORM = "%%%ds%%%ds";
     private static final String EMPTY = "";
+    private static final int PLAYER_NAME_MAX_LENGTH = 5;
     private static final int TOTAL_NAME_LENGTH = 9;
     private static final int TWO = 2;
     
     private final String playerName;
     
     public PlayerName(String playerName) {
+        if (playerName.length() > PLAYER_NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException(PLAYER_NAME_LENGTH_EXCEPTION_MESSAGE);
+        }
+        
         this.playerName = playerName;
     }
     
--- src/test/java/ladder/step2/domain/PlayerNameTest.java
+++ src/test/java/ladder/step2/domain/PlayerNameTest.java
@@ -3,7 +3,7 @@ package ladder.step2.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class PlayerNameTest {
     public static final PlayerName JUN = new PlayerName("jun");
@@ -15,5 +15,12 @@ class PlayerNameTest {
     @DisplayName("플레이어 이름 생성")
     void create() {
         assertThat(JUN).isNotNull();
+    }
+    
+    @Test
+    @DisplayName("플레이어 이름 5자 초과시 예뢰")
+    void player_name_length_exception() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new PlayerName("honuxs"))
+                .withMessage("플레이어 이름은 5자를 초과할 수 없습니다.");
     }
 }
