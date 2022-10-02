8059eb993b0c83eece8ffe9fcc053d269a6ea7f5
devcs96 <katd6@naver.com>
2022-10-02 23:59:17 +0900

    :recycle: refactor(service) : 람다를 활용해 중복제거

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -3,6 +3,9 @@ package nextstep.fp;
 import java.util.List;
 
 public class Lambda {
+
+    private static final int DEFAULT_RESULT = 0;
+
     public static void printAllOld(List<Integer> numbers) {
         System.out.println("printAllOld");
 
@@ -26,31 +29,7 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
-    }
-
-    public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
-    }
-
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+    public static int sumByCondition(List<Integer> numbers, SumCondition condition) {
+        return numbers.stream().filter(condition::filter).reduce(Integer::sum).orElse(DEFAULT_RESULT);
     }
 }
---
+++ src/main/java/nextstep/fp/SumCondition.java
@@ -0,0 +1,7 @@
+package nextstep.fp;
+
+@FunctionalInterface
+public interface SumCondition {
+
+    boolean filter(Integer num);
+}
