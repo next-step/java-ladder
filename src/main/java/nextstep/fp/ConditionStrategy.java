9d359517cae4f124c7d133d04f2b566c18ddbe7f
codeleesh(이상호) <38518639+codeleesh@users.noreply.github.com>
2022-10-03 21:41:39 +0900

    Step1 : 1단계 - 자바8 스트림, 람다, Optional (#1459)
    * refactor(test): 익명 클래스를 람다로 전환
    
    * refactor(test): 익명 클래스를 람다로 전환
    
    * refactor: 람다를 활용해 중복 제거
    
    * refactor(test): 람다를 활용해 중복 제거
    
    * refactor(test): 람다를 활용해 중복 제거
    
    * refactor: 람다를 활용해 중복 제거
    
    * refactor: Stream 실습1, 실습2
    
    * refactor(test): Stream 실습1, 실습2
    
    * refactor: Stream 실습2
    
    * feat: 람다를 활용해 중복 제거
    
    * refactor: Optional을 활용해 조건에 따른 반환
    
    * refactor: Optional에서 값을 반환
    
    * refactor: 사용하지 않는 임포트문 제거
    
    * refactor: Optional에서 exception 처리
    
    * docs: 람다 및 Stream, Optional 실습 테스트 작성

---
+++ src/main/java/nextstep/fp/ConditionStrategy.java
@@ -0,0 +1,5 @@
+package nextstep.fp;
+
+public interface ConditionStrategy {
+    boolean condition(Integer number);
+}
--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -1,7 +1,6 @@
 package nextstep.fp;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -10,35 +9,50 @@ import java.util.stream.Collectors;
 
 public class StreamStudy {
 
+    public static final String FILE_FP_WAR_AND_PEACE_TXT = "src/main/resources/fp/war-and-peace.txt";
+    public static final int WORD_LIMIT_SIZE = 12;
+    public static final int WORD_LIMIT_PRINT = 100;
+
+    private StreamStudy() {}
+
     public static long countWords() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+        String contents = Files.readString(Paths
+                .get(FILE_FP_WAR_AND_PEACE_TXT));
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream()
+                .filter(word -> word.length() > WORD_LIMIT_SIZE)
+                .count();
     }
 
     public static void printLongestWordTop100() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+        String contents = Files.readString(Paths
+                .get(FILE_FP_WAR_AND_PEACE_TXT));
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(word -> word.length() > WORD_LIMIT_SIZE)
+                .sorted()
+                .distinct()
+                .limit(WORD_LIMIT_PRINT)
+                .forEach(word -> System.out.println(word.toLowerCase()));
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
+        return numbers.stream()
+                .map(x -> 2 * x)
+                .collect(Collectors.toList());
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers.stream()
+                .reduce(0, Integer::sum);
     }
 
-    public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+    public static long sumOverThreeAndDouble(final List<Integer> numbers) {
+        return numbers.stream()
+                .filter(number -> number > 3)
+                .map(number -> 2 * number)
+                .reduce(0, Integer::sum);
     }
 }
