3f5541889ea2fbc18f7d9bed3f04ae1d1ad66a94
devcs96 <katd6@naver.com>
2022-10-08 03:39:31 +0900

    :recycle: refactor(ui) : 출력 메소드 리팩토링

--- src/main/java/ladder/ui/OutputView.java
+++ src/main/java/ladder/ui/OutputView.java
@@ -19,35 +19,47 @@ public class OutputView {
 
     private static void printName(Users users) {
         for(int i = 0; i < users.size() ; i++){
-            String username = getNameOfUser(users.getUsers().get(i));
-            if ( i == 0 ){
-                System.out.print(username);
-                continue;
-            }
-            System.out.print(addDelimiter(INTERVAL - username.length(), LADDER_WITH_NO_SPACE) + username);
+            printSingleName(users, i);
         }
+    }
+
+    private static void printSingleName(Users users, int index) {
+        String username = getNameOfUser(users.getUsers().get(index));
+        if ( index == 0 ){
+            System.out.print(username);
+            return;
+        }
+        System.out.print(addDelimiter(INTERVAL - username.length(), LADDER_WITH_NO_SPACE) + username);
     }
 
     private static void printLine(Users users, int length) {
         for (int i = 0; i  < length; i ++){
-            for (int j = 0; j < users.size() ; j++){
-                StringBuilder stringBuilder = new StringBuilder();
-                User user = users.getUsers().get(j);
-                String username = getNameOfUser(user);
-                LineUnit unit = user.getVerticalLine().getLineUnits().get(i);
-                if (j == 0 ){
-                    stringBuilder.append(addDelimiter(username.length(),LADDER_WITH_NO_SPACE) + VERTICAL_UNIT);
-                }else{
-                    if (unit.hasPrevious()) {
-                        stringBuilder.append(addDelimiter(INTERVAL-1, HORIZONTAL_UNIT) + VERTICAL_UNIT);
-                    }else{
-                        stringBuilder.append(addDelimiter(INTERVAL-1,LADDER_WITH_NO_SPACE) + VERTICAL_UNIT);
-                    }
-                }
-                System.out.print(stringBuilder);
-            }
-            System.out.println("");
+            printSingleLine(users, i);
+            System.out.println();
         }
+    }
+
+    private static void printSingleLine(Users users, int index) {
+        for (int j = 0; j < users.size() ; j++){
+            StringBuilder stringBuilder = new StringBuilder();
+            User user = users.getUsers().get(j);
+            String username = getNameOfUser(user);
+            LineUnit unit = user.getVerticalLine().getLineUnits().get(index);
+            appendSingleLine(j, stringBuilder, username, unit);
+            System.out.print(stringBuilder);
+        }
+    }
+
+    private static void appendSingleLine(int index, StringBuilder stringBuilder, String username, LineUnit unit) {
+        if (index == 0 ){
+            stringBuilder.append(addDelimiter(username.length(),LADDER_WITH_NO_SPACE) + VERTICAL_UNIT);
+            return;
+        }
+        if (unit.hasPrevious()) {
+            stringBuilder.append(addDelimiter(INTERVAL - 1, HORIZONTAL_UNIT) + VERTICAL_UNIT);
+            return;
+        }
+        stringBuilder.append(addDelimiter(INTERVAL-1,LADDER_WITH_NO_SPACE) + VERTICAL_UNIT);
     }
 
     private static String addDelimiter(int length ,String delimiter){
