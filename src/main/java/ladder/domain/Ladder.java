10ed6f4b36d074ec1b4266e273cb0ad61ef16133
Wordbe <seonghojin3@gmail.com>
2022-10-09 02:51:16 +0900

    feat: Ladder 생성

---
+++ src/main/java/ladder/domain/Ladder.java
@@ -0,0 +1,40 @@
+package ladder.domain;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Ladder {
+    private final List<Line> lines;
+
+    public Ladder(Integer personCount, Integer height, LineFactory lineFactory) {
+        this.lines = IntStream.range(0, height)
+                .mapToObj(i -> lineFactory.create(personCount - 1))
+                .collect(toList());
+    }
+
+    public Ladder(List<Line> lines) {
+        this.lines = lines;
+    }
+
+    public List<Line> lines() {
+        return lines;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Ladder ladder = (Ladder) o;
+
+        return Objects.equals(lines, ladder.lines);
+    }
+
+    @Override
+    public int hashCode() {
+        return lines != null ? lines.hashCode() : 0;
+    }
+}
