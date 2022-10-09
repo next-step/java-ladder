7bd9a82cb7f1a8ec895e29ddefd0444eb7c25503
Wordbe <seonghojin3@gmail.com>
2022-10-09 22:45:29 +0900

    fix: 테스트 height=2 상황으로 변경

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -11,8 +11,8 @@ class LadderTest {
     @Test
     void create() {
         Line line = new Line(List.of(true, false, true));
-        Ladder ladder = new Ladder(4, 1, personCount -> line);
+        Ladder ladder = new Ladder(4, 2, personCount -> line);
 
-        assertThat(ladder).isEqualTo(new Ladder(List.of(line)));
+        assertThat(ladder).isEqualTo(new Ladder(List.of(line, line)));
     }
 }
