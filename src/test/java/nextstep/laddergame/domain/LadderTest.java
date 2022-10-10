4e24d2f567d7ddc179fbea4f314dd6af1da0b36a
OhSeungCheol <sc.oh131@gmail.com>
2022-10-10 22:48:27 +0900

    [M2] test: Bridge 개수 테스트 케이스 추가
    참고: https://github.com/next-step/java-ladder/pull/1468

--- src/test/java/nextstep/laddergame/domain/LadderTest.java
+++ src/test/java/nextstep/laddergame/domain/LadderTest.java
@@ -9,10 +9,16 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LadderTest {
-    private static List<String> gamer = Arrays.asList("pobi", "honux", "crong", "jk");
+    private static List<Gamer> gamers = Arrays.asList(
+            new Gamer("pobi")
+            , new Gamer("honux")
+            , new Gamer("crong")
+            , new Gamer("jk")
+    );
     private static int width = 4;
     private static int height = 5;
-    private static Ladder ladder = new Ladder(gamer, height, DrawBridgeStrategy.DEFAULT_STRATEGY);
+    private static Ladder ladder = new Ladder(gamers, height, DrawBridgeStrategy.DEFAULT_STRATEGY);
+    private static int MAX_BRIDGE_COUNT = gamers.size() / 2 * height;
 
     @Test
     public void ladder_size_test() {
@@ -25,5 +31,46 @@ class LadderTest {
     @Test
     public void print_ladder() {
         ResultView.printLadder(ladder);
+    }
+
+    @Test
+    public void ladder_bridge_count_test1() {
+        Ladder ladder = new Ladder(gamers, height, DrawBridgeStrategy.DRAW_ALL);
+        assertThat(getBridgeCount(ladder)).isEqualTo(MAX_BRIDGE_COUNT);
+    }
+
+    @Test
+    public void ladder_bridge_count_test2() {
+        Ladder ladder = new Ladder(gamers, height, DrawBridgeStrategy.DRAW_ANY);
+        assertThat(getBridgeCount(ladder)).isEqualTo(0);
+    }
+
+    private int getBridgeCount(Ladder ladder) {
+        int bridgeCount = 0;
+        for (Line line : ladder.getBoard()) {
+            bridgeCount += getBridgeCount(line);
+        }
+        return bridgeCount;
+    }
+
+    private int getBridgeCount(Line line) {
+        int bridgeCount = 0;
+        for (LadderPiece ladderPiece : line.getLadderPieces()) {
+            bridgeCount += getBridgeCount(ladderPiece);
+        }
+
+        // Bridge 하나는 양쪽 Piece 에 의해 각각 한번씩 총 두번 카운트되기 때문에 /2 를 해줌.
+        return bridgeCount / 2;
+    }
+
+    private int getBridgeCount(LadderPiece ladderPiece) {
+        int bridgeCount = 0;
+        if (ladderPiece.isRightBridge()) {
+            bridgeCount++;
+        }
+        if (ladderPiece.isLeftBridge()) {
+            bridgeCount++;
+        }
+        return bridgeCount;
     }
 }
