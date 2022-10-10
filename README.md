51a4d83e4dcef46084432d1147829fce3c2f8bd3
hiblue02 <38008976+hiblue02@users.noreply.github.com>
2022-10-10 14:38:58 +0900

    Step01 자바8 스트림, 람다, Optional (#1460)
    * feat(ReadMe): 요구사항 추가
    
    * refactor(CarTest): MoveStrategy 익명 클래스를 람다로 전환
    - public 삭제
    
    * feat(Lambda): 함수형 인터페이스 SumCondition 생성
    - 합산 조건을 담당하는 함수형 인터페이스 SumCondition 추가
    - SumCondition을 주입받아 계산하는 sumByCondition 생성
    - sumAll, sumAllEven, sumAllOverThree : 합산조건을 익명클래스로 만들어서 sumByCondition에 주입하는 것으로 수정
    
    * feat(StreamStudy): sumOverThreeAndDouble, printLongestWordTop100 기능 구현
    
    * feat(User): ageIsInRange2 기능 구현
    
    * refactor(Users): getUser: stream, optional로 구현
    
    * refactor(Expression): of를 Stream, orElseThrow로 구현
    
    * feat(ReadMe): 요구사항 구현 체크
    
    * refactor(SumCondition): @FunctionalInterface 추가
    
    * refactor(MoveStrategy): @FunctionalInterface 추가
    
    * style(StreamStudy): 체이닝 줄바꿈
    
    * refactor(SumCondition): @FunctionalInterface 선언
    
    * refactor(ComputerStore): getVersionOptional 구현

--- README.md
+++ README.md
@@ -7,3 +7,33 @@
 
 ## 온라인 코드 리뷰 과정
 * [텍스트와 이미지로 살펴보는 온라인 코드 리뷰 과정](https://github.com/nextstep-step/nextstep-docs/tree/master/codereview)
+
+# 람다, 스트림, Optional
+## 기능 및 프로그래밍 요구사항
+* [x] CarTest의 익명 클래스를 람다로 전환한다.
+* [x] Lambda의 sumAll, sumAllEven, sumAllOverThree의 중복을 람다를 사용해 제거한다.
+  * List의 담긴 값 중 3보다 큰 수만을 더해야 한다.
+  * [x] 변경되는 부분과, 변경되지 않는 부분의 코드를 분리한다. 
+  * [x] 변경되는 부분을 인터페이스로 추출하고, 구현체를 람다를 사용해 익명 클래스로 구현한다. 
+* [x] StreamStudyTest에서 map, reduce, filter를 실습한다.
+  * [x] countWords를 filter로 구현한다.
+  * [x] sumOverThreeAndDouble() 메서드를 구현한다.
+    * List에 담긴 모든 숫자 중 3보다 큰 숫자를 2배한 후 총합을 구한다.
+  * [x] printLongestWordTop100() 메서드를 구현한다.
+    * [x] 단어의 길이가 12자를 초과하는 단어를 추출한다.
+    * [x] 12자가 넘는 단어 중 길이가 긴 순서로 100개의 단어를 추출한다.
+    * [x] 단어 중복을 허용하지 않는다. 즉, 서로 다른 단어 100개를 추출해야 한다.
+    * [x] 추출한 100개의 단어를 출력한다. 모든 단어는 소문자로 출력해야 한다.
+* [x] Optional 
+  * [x] Optional을 사용해 ageIsInRange2()를 구현한다.
+    * User의 ageIsInRange1() 메소드는 30살 이상, 45살 이하에 해당하는 User가 존재하는 경우 true를 반환하는 메소드이다.
+    * 메소드 인자로 받은 User를 Optional로 생성하면 stream의 map, filter와 같은 메소드를 사용하는 것이 가능하다.
+    * Optional.ofNullable(user)을 활용해 User을 Optional로 생성하는 것이 가능하다.
+    * Optional의 map(), filter() 메소드등을 활용해 필요한 데이터를 추출
+    * Optional의 isPresent() 메소드 활용
+  * [x] Users의 getUser() 메소드를 자바 8의 stream과 Optional을 활용해 구현한다.
+    *  Optional의 orElse() 메소드 활용해 구현한다.
+  * [x] Expression의 of 메소드를 Stream을 사용해 구현한다. 
+    * Optional의 orElseThrow() 메소드 활용해 구현한다.
+    * 일치하는 값 하나를 추출할 때 findFirst() 메소드를 활용 가능하다.
+  * [x] UserTest의 테스트가 모두 pass해야 한다.
--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -3,6 +3,10 @@ package nextstep.fp;
 import java.util.List;
 
 public class Lambda {
+
+    private Lambda() {
+    }
+
     public static void printAllOld(List<Integer> numbers) {
         System.out.println("printAllOld");
 
@@ -18,39 +22,26 @@ public class Lambda {
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> System.out.println("Hello from thread")).start();
     }
 
+
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumByCondition(numbers, num -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumByCondition(numbers, num -> num % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumByCondition(numbers, num -> num > 3);
     }
+
+    private static int sumByCondition(List<Integer> numbers, SumCondition condition) {
+        return numbers.stream()
+                .filter(condition::condition)
+                .reduce(0, Integer::sum);
+    }
+
 }
