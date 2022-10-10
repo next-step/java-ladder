d4465c9d82f79258cdcffc0f9c4d34493eef6a36
OhSeungCheol <sc.oh131@gmail.com>
2022-10-10 22:27:28 +0900

    [M2] refactor: 매직 리터럴 상수화
    참고: https://github.com/next-step/java-ladder/pull/1468

--- src/main/java/nextstep/laddergame/view/ResultView.java
+++ src/main/java/nextstep/laddergame/view/ResultView.java
@@ -6,10 +6,16 @@ import nextstep.laddergame.domain.LadderPiece;
 import nextstep.laddergame.domain.Line;
 
 public class ResultView {
+    private static final String RESULT_STRING = "\n실행결과\n";
+    private static final String NAME_SEPARATOR = " ";
+    private static final String LADDER_PIECE = "|";
+    private static final String NO_BRIDGE_STRING = "     ";
+    private static final String BRIDGE_STRING = "-----";
+
     public static void printLadder(Ladder ladder) {
-        System.out.println("\n실행결과\n");
+        System.out.println(RESULT_STRING);
         for (Gamer gamer : ladder.getGamers()) {
-            System.out.print(gamer.getName() + " ");
+            System.out.print(gamer.getName() + NAME_SEPARATOR);
         }
         System.out.println();
         for (Line line : ladder.getBoard()) {
@@ -20,16 +26,16 @@ public class ResultView {
 
     private static void printLine(Line line) {
         for (LadderPiece ladderPiece : line.getLadderPieces()) {
-            System.out.print("|");
+            System.out.print(LADDER_PIECE);
             printBridgeString(ladderPiece);
         }
     }
 
     private static void printBridgeString(LadderPiece ladderPiece) {
         if (ladderPiece.isRightBridge()) {
-            System.out.print("-----");
+            System.out.print(BRIDGE_STRING);
             return;
         }
-        System.out.print("     ");
+        System.out.print(NO_BRIDGE_STRING);
     }
 }
