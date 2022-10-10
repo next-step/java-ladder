5e9e8a1ccd602339a14295d2949efeb161d4437f
nooose <hd15807@gmail.com>
2022-10-10 18:01:08 +0900

    feat: 이름 도메인 기능 구현

---
+++ src/main/java/nextstep/ladder/domain/Name.java
@@ -0,0 +1,57 @@
+package nextstep.ladder.domain;
+
+import java.util.Objects;
+
+public class Name {
+    private static final int DEFAULT_NAME_LENGTH = 5;
+    private static final String LENGTH_EXCEPTION_MESSAGE = "5글자 이하의 이름만 가능합니다.";
+    private static final String NULL_BLANK_EXCEPTION_MESSAGE = "Null 또는 공백 문자열은 이름이 될 수 없습니다.";
+
+    private final String value;
+
+    public static Name of(String value) {
+        return new Name(value);
+    }
+
+    public Name(String value) {
+        validate(value);
+        this.value = value.strip();
+    }
+
+    private void validate(String value) {
+        if (isNullOrBlank(value)) {
+            throw new IllegalArgumentException(NULL_BLANK_EXCEPTION_MESSAGE);
+        }
+
+        if (isOverLength(value)) {
+            throw new IllegalArgumentException(LENGTH_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private boolean isNullOrBlank(String value) {
+        return Objects.isNull(value) || value.isBlank();
+    }
+
+    private boolean isOverLength(String value) {
+        return value.strip().length() > DEFAULT_NAME_LENGTH;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Name name = (Name) o;
+        return value.equals(name.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ src/main/java/nextstep/ladder/domain/Names.java
@@ -0,0 +1,30 @@
+package nextstep.ladder.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Names {
+    public static final String DEFAULT_DELIMITER = ",";
+
+    private final List<Name> values;
+
+    public static Names of(String names) {
+        return new Names(names);
+    }
+
+    public Names(String names) {
+        values = Stream.of(names.split(DEFAULT_DELIMITER))
+                .map(Name::of)
+                .collect(Collectors.toList());
+    }
+
+    public boolean contains(Name name) {
+        return values.contains(name);
+    }
+
+    public List<Name> values() {
+        return Collections.unmodifiableList(values);
+    }
+}
