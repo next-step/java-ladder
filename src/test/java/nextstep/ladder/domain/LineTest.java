4dd50218bf02d3c54e8cc845c4bdc204bcda3d87
michol.lee <lovethefeel@gmail.com>
2022-10-10 23:54:30 +0900

    feat(test): 사다리 한 라인 테스트

---
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -0,0 +1,25 @@
+package nextstep.ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LineTest {
+
+    @DisplayName("사다리 한 라인을 확인한다.")
+    @Test
+    void create() {
+
+        final Point point = new Point(0, Movement.first(true));
+        final List<Point> points = List.of(point
+                , point.next(false)
+                , point.next(true)
+        );
+        final Line line = new Line(points);
+
+        assertThat(line).isEqualTo(new Line(points));
+    }
+}
