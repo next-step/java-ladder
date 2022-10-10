1f0009997e2b58aca51f7eb00144d9af2cc808d2
OhSeungCheol <sc.oh131@gmail.com>
2022-10-10 22:02:48 +0900

    [M2] fix: 테스트 코드 컴파일 에러 수정
    참고: https://github.com/next-step/java-ladder/pull/1468

--- src/test/java/nextstep/laddergame/domain/LadderTest.java
+++ src/test/java/nextstep/laddergame/domain/LadderTest.java
@@ -3,14 +3,16 @@ package nextstep.laddergame.domain;
 import nextstep.laddergame.view.ResultView;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LadderTest {
+    private static List<String> gamer = Arrays.asList("pobi", "honux", "crong", "jk");
     private static int width = 4;
     private static int height = 5;
-    private static Ladder ladder = new Ladder(width, height, DrawBridgeStrategy.DEFAULT_STRATEGY);
+    private static Ladder ladder = new Ladder(gamer, height, DrawBridgeStrategy.DEFAULT_STRATEGY);
 
     @Test
     public void ladder_size_test() {
