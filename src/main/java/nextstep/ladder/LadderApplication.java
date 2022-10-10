fd9dc032c6621354bb23e2c58b7cb84f113ea033
michol.lee <lovethefeel@gmail.com>
2022-10-10 23:51:37 +0900

    feat: 사다리 메인 실행

---
+++ src/main/java/nextstep/ladder/LadderApplication.java
@@ -0,0 +1,24 @@
+package nextstep.ladder;
+
+import nextstep.ladder.domain.Ladder;
+import nextstep.ladder.domain.LadderCreator;
+import nextstep.ladder.domain.ParticipationName;
+import nextstep.ladder.domain.Result;
+import nextstep.ladder.dto.ParticipationNameDto;
+import nextstep.ladder.view.InputView;
+import nextstep.ladder.view.ResultView;
+
+import java.util.List;
+
+public class LadderApplication {
+
+    public static void main(String[] args) {
+
+        final List<ParticipationName> participationNames = ParticipationName.from(InputView.inputName());
+        final LadderCreator ladderCreator = LadderCreator.of(participationNames.size(), InputView.inputLadderHeight());
+        final Ladder ladder = ladderCreator.start();
+        ResultView.resultTitlePrint();
+        ResultView.resultNamePrint(ParticipationNameDto.from(participationNames));
+        ResultView.resultLadderPrint(Result.show(ladder));
+    }
+}
