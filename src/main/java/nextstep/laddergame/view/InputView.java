072425173de7430363d633be71d227f3fc3594d1
OhSeungCheol <sc.oh131@gmail.com>
2022-10-11 11:41:28 +0900

    [M2] refactor: 매직 리터럴 상수화(2)
    참고: https://github.com/next-step/java-ladder/pull/1468

--- src/main/java/nextstep/laddergame/view/InputView.java
+++ src/main/java/nextstep/laddergame/view/InputView.java
@@ -10,19 +10,22 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private static final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]\\d*(\\.\\d+)?$");
+    private static final String INPUT_GAMER_NAMS_STRING = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)\"";
+    private static final String NAME_SEPARATE = ",";
+    private static final String INPUT_LADDER_HEIGHT_STRING = "\n최대 사다리 높이는 몇 개인가요?";
 
     public static List<Gamer> inputGamerNames() {
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        System.out.println(INPUT_GAMER_NAMS_STRING);
         Scanner scanner = new Scanner(System.in);
         String gamerNamesString = scanner.nextLine();
         return Arrays
-                .stream(gamerNamesString.split(","))
+                .stream(gamerNamesString.split(NAME_SEPARATE))
                 .map(Gamer::new)
                 .collect(Collectors.toList());
     }
 
     public static Integer inputLadderHeight() {
-        System.out.println("\n최대 사다리 높이는 몇 개인가요?");
+        System.out.println(INPUT_LADDER_HEIGHT_STRING);
         Scanner scanner = new Scanner(System.in);
         String ladderHeightString = scanner.nextLine();
         checkValidLadderHeight(ladderHeightString);
