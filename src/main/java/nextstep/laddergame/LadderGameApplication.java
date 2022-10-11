1f1936bf64b0bf4c10def7d00059d260b22174bc
OhSeungCheol <sc.oh131@gmail.com>
2022-10-11 19:20:05 +0900

    [M3] refactor: 보상 일급 컬렉션 추가
    참고: https://edu.nextstep.camp/s/DVVUSLFe/ls/LonguG1G

--- src/main/java/nextstep/laddergame/LadderGameApplication.java
+++ src/main/java/nextstep/laddergame/LadderGameApplication.java
@@ -1,9 +1,6 @@
 package nextstep.laddergame;
 
-import nextstep.laddergame.domain.DrawBridgeStrategy;
-import nextstep.laddergame.domain.Game;
-import nextstep.laddergame.domain.Gamer;
-import nextstep.laddergame.domain.Height;
+import nextstep.laddergame.domain.*;
 import nextstep.laddergame.view.InputView;
 import nextstep.laddergame.view.ResultView;
 
@@ -13,13 +10,15 @@ public class LadderGameApplication {
     public static void main(String[] args) {
         // Parameter Sample
         // pobi,honux,crong,jk
+        // 꽝,5000,꽝,3000
         // 5
 
         List<Gamer> gamers = InputView.inputGamerNames();
+        RewardWrapper rewardWrapper = InputView.inputRewards();
         Height ladderHeight = InputView.inputLadderHeight();
 
         Game game = new Game(gamers, ladderHeight, DrawBridgeStrategy.DEFAULT_STRATEGY);
 
-        ResultView.printLadder(game);
+        ResultView.printResult(game, rewardWrapper);
     }
 }
