dd7838542993d249f45963552ef5631ad7c3910b
jinukix <jinukix@styleshare.kr>
2022-10-11 13:21:33 +0900

    feat: Bridge 도메인 작성

---
+++ src/main/java/step1/domain/Bridge.java
@@ -0,0 +1,53 @@
+package step1.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import step1.utils.RandomGenerator;
+
+public class Bridge {
+
+    private final boolean isOpen;
+
+    public Bridge(boolean isOpen) {
+        this.isOpen = isOpen;
+    }
+
+    public static Bridge fromBeforeLineBridge(Bridge beforeLineBridge) {
+        if (beforeLineBridge.isOpen()) {
+            return new Bridge(false);
+        }
+
+        return new Bridge(RandomGenerator.nextBoolean());
+    }
+
+    public static List<Bridge> createAllClosedBridges(int count) {
+        return IntStream.range(0, count)
+            .mapToObj(i -> new Bridge(false))
+            .collect(Collectors.toList());
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Bridge)) {
+            return false;
+        }
+
+        Bridge bridge = (Bridge) o;
+
+        return isOpen == bridge.isOpen;
+    }
+
+    @Override
+    public int hashCode() {
+        return (isOpen ? 1 : 0);
+    }
+}
