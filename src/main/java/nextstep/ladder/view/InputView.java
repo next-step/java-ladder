f2cb2490bf350fa9bc4aac93c0dd4b16dea7adb0
Jaepil Yu <jaepilyu1324@gmail.com>
2022-10-12 20:26:47 +0900

    feat: , 기준으로 이름 입력

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -1,4 +1,17 @@
 package nextstep.ladder.view;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class InputView {
+    private static final String PEOPLE_NAMES_INPUT_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private final Scanner scanner = new Scanner(System.in);
+
+    public List<String> getPeopleNames() {
+        System.out.println(PEOPLE_NAMES_INPUT_MESSAGE);
+        String[] peopleNames = scanner.nextLine().split(",");
+        System.out.println();
+        return Arrays.asList(peopleNames);
+    }
 }
