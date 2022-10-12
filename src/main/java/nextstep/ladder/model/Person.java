40c53bb03fe0f5f36465ec3d79aa4af9819dd369
Jaepil Yu <jaepilyu1324@gmail.com>
2022-10-12 23:33:42 +0900

    feat: 이름 관련 예외 처리 보강

--- src/main/java/nextstep/ladder/model/Person.java
+++ src/main/java/nextstep/ladder/model/Person.java
@@ -1,6 +1,9 @@
 package nextstep.ladder.model;
 
 public class Person {
+    private static final String EMPTY_NAME_EXCEPTION_MESSAGE = "공백뿐인 이름은 사용할 수 없습니다.";
+    private static final String EXCEEDED_NAME_LENGTH_EXCEPTION_MESSAGE = "이름의 길이는 5자를 초과할 수 없습니다.";
+
     private final String name;
 
     public Person(String name) {
@@ -10,11 +13,11 @@ public class Person {
 
     private void validate(String name) {
         if (name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(EMPTY_NAME_EXCEPTION_MESSAGE);
         }
 
         if (name.length() > 5) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(EXCEEDED_NAME_LENGTH_EXCEPTION_MESSAGE);
         }
     }
 
--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -3,15 +3,30 @@ package nextstep.ladder.view;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Supplier;
 
 public class InputView {
     private static final String PEOPLE_NAMES_INPUT_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String INVALID_INPUT_MESSAGE = "잘못된 형식의 입력을 수행하셨습니다.";
+
     private final Scanner scanner = new Scanner(System.in);
 
     public List<String> getPeopleNames() {
+        return wrapMethod(this::doGetPeopleNames);
+    }
+
+    private List<String> doGetPeopleNames() {
         System.out.println(PEOPLE_NAMES_INPUT_MESSAGE);
         String[] peopleNames = scanner.nextLine().split(",");
         System.out.println();
         return Arrays.asList(peopleNames);
+    }
+
+    private<T> T wrapMethod(Supplier<T> method) {
+        try {
+            return method.get();
+        } catch (Exception e) {
+            throw new RuntimeException(INVALID_INPUT_MESSAGE);
+        }
     }
 }
