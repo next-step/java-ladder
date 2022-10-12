32a1d81c9c7d2c21760370e5f45dea1c50449a51
jinukix <jinukix@styleshare.kr>
2022-10-12 16:40:27 +0900

    refactor: foreach 문 개선

--- src/main/java/step1/domain/Lines.java
+++ src/main/java/step1/domain/Lines.java
@@ -43,27 +43,30 @@ public class Lines {
     }
 
     private void validateHeightSame(List<Line> lines, int height) {
-        lines.stream()
-            .filter(line -> !line.isEqualsHeight(height))
-            .forEach(exists -> {
-                throw new IllegalArgumentException("Line의 모든 높이는 같아야합니다.");
-            });
+        boolean isNotSame = lines.stream()
+            .anyMatch(line -> !line.isEqualsHeight(height));
+
+        if (isNotSame) {
+            throw new IllegalArgumentException("Line의 모든 높이는 같아야합니다.");
+        }
     }
 
     private void validateFirstLineIsAllFalse(List<Line> lines) {
-        lines.get(0)
+        boolean allClosed = lines.get(0)
             .getBridges().stream()
-            .filter(Bridge::isOpen)
-            .forEach(exists -> {
-                throw new IllegalArgumentException("첫 번째 Line의 모든 Bridge는 False여야합니다.");
-            });
+            .anyMatch(Bridge::isOpen);
+
+        if (allClosed) {
+            throw new IllegalArgumentException("첫 번째 Line의 모든 Bridge는 False여야합니다.");
+        }
     }
 
     private void validateBridgesDuplicated(List<Line> lines) {
-        IntStream.range(1, lines.size())
-            .filter(idx -> lines.get(idx).isDuplicatedLine(lines.get(idx - 1)))
-            .forEach(exists -> {
-                throw new IllegalArgumentException("가로 라인에 인접한 Bridge는 같이 열려있을 수 없습니다.");
-            });
+        boolean isDuplicated = IntStream.range(1, lines.size())
+            .anyMatch(idx -> lines.get(idx).isDuplicatedLine(lines.get(idx - 1)));
+
+        if (isDuplicated) {
+            throw new IllegalArgumentException("가로 라인에 인접한 Bridge는 같이 열려있을 수 없습니다.");
+        }
     }
 }
