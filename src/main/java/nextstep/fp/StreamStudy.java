7bcd98f1063a1a4603b99f48858668e6809ba3ba
robin.son <robin.son@kakaocorp.com>
2022-10-13 20:33:09 +0900

    feature : 스트림(stream) map, filter, reduce

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,8 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,11 +17,7 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream().filter(word -> word.length() > 12).count();
     }
 
     public static void printLongestWordTop100() throws IOException {
@@ -27,7 +25,15 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        List<String> distincWordsOverTwelveOrderByLengthDesc = words
+                .stream()
+                .filter(word -> word.length() > 12)
+                .distinct()
+                .sorted(Comparator.comparingInt(String::length))
+                .collect(Collectors.toList());
+
+        distincWordsOverTwelveOrderByLengthDesc.subList(0, 100)
+                .forEach(word -> System.out.println(word.toLowerCase()));
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +45,6 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream().filter(number -> number > 3).map(number -> number*2).mapToInt(Integer::intValue).sum();
     }
 }
