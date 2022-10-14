790ac1daa9e9b1f0af43cf613192f6fdf06b096b
owen.qq <owen.qq@kakaocorp.com>
2022-10-14 13:10:17 +0900

    feat: 람다 실습2 중복제거 구현

---
+++ src/main/java/nextstep/fp/ConditionalSum.java
@@ -0,0 +1,5 @@
+package nextstep.fp;
+
+public interface ConditionalSum {
+    boolean test(Integer number);
+}
--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -27,30 +27,18 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAll(numbers, number -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, number -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, number -> number > 3);
+    }
+
+    private static int sumAll(List<Integer> numbers, ConditionalSum conditionalSum) {
+        return numbers.stream().filter(conditionalSum::test).reduce(Integer::sum).orElseGet(() -> 0);
     }
 }
