844d7793cc8474b4df2e65d5acf3c5d7b7fc9a2b
michol.lee <lovethefeel@gmail.com>
2022-10-16 23:35:38 +0900

    refactor: 코드리뷰 반영으로 메인 로직 수정

--- src/main/java/nextstep/ladder/LadderApplication.java
+++ src/main/java/nextstep/ladder/LadderApplication.java
@@ -2,21 +2,19 @@ package nextstep.ladder;
 
 import nextstep.ladder.domain.Ladder;
 import nextstep.ladder.domain.LadderCreator;
-import nextstep.ladder.domain.ParticipationName;
+import nextstep.ladder.domain.ParticipationNames;
 import nextstep.ladder.domain.Result;
 import nextstep.ladder.dto.ParticipationNameDto;
 import nextstep.ladder.view.InputView;
 import nextstep.ladder.view.ResultView;
 
-import java.util.List;
-
 public class LadderApplication {
 
     public static void main(String[] args) {
 
-        final List<ParticipationName> participationNames = ParticipationName.from(InputView.inputName());
+        final ParticipationNames participationNames = new ParticipationNames(InputView.inputName());
         final LadderCreator ladderCreator = LadderCreator.of(participationNames.size(), InputView.inputLadderHeight());
-        final Ladder ladder = ladderCreator.start();
+        final Ladder ladder = ladderCreator.create();
         ResultView.resultPrint(ParticipationNameDto.from(participationNames), Result.show(ladder));
     }
 }
