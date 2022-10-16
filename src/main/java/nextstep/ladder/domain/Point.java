b79d3c0e897f7a3b55135dea294adfdd18c158e0
michol.lee <lovethefeel@gmail.com>
2022-10-16 23:33:28 +0900

    refactor: 코드리뷰 반영 - Point와 Movement 합침

--- src/main/java/nextstep/ladder/domain/Point.java
+++ src/main/java/nextstep/ladder/domain/Point.java
@@ -5,25 +5,30 @@ import java.util.Objects;
 public class Point {
 
     private final int index;
-    private final Movement movement;
+    private final Direction direction;
 
-    public Point(final int index, final Movement movement) {
+    public Point(final int index, final Direction direction) {
 
         this.index = index;
-        this.movement = movement;
+        this.direction = direction;
     }
 
-    public static Point first(final Movement first) {
+    public static Point first(final boolean right) {
 
-        return new Point(0, first);
+        return new Point(0, Direction.of(false, right));
+    }
+
+    public boolean duplicate() {
+
+        return this.direction.duplicate();
     }
 
     public int move() {
 
-        if (this.movement.move() == Direction.RIGHT) {
+        if (this.direction == Direction.RIGHT) {
             return this.index + 1;
         }
-        if (this.movement.move() == Direction.LEFT) {
+        if (this.direction == Direction.LEFT) {
             return this.index - 1;
         }
         return this.index;
@@ -32,7 +37,7 @@ public class Point {
     public Point next(final boolean generate) {
 
         final int index = this.index + 1;
-        return new Point(index, this.movement.next(generate));
+        return new Point(index, this.direction.next(generate));
     }
 
     public int getIndex() {
@@ -40,19 +45,14 @@ public class Point {
         return this.index;
     }
 
-    public Movement getMovement() {
+    public Direction getDirection() {
 
-        return this.movement;
+        return this.direction;
     }
 
     public boolean untilExcludeStart(final int size, final int countOfPerson) {
 
         return size < countOfPerson - 1;
-    }
-
-    public boolean duplicate() {
-
-        return this.movement.duplicate();
     }
 
     @Override
@@ -61,12 +61,12 @@ public class Point {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Point point = (Point) o;
-        return index == point.index && Objects.equals(movement, point.movement);
+        return index == point.index && direction == point.direction;
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(index, movement);
+        return Objects.hash(index, direction);
     }
 }
