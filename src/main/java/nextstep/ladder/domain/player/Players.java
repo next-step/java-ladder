1442307a958197f50e10dac53c0b5016d88bbcb7
nooose <hd15807@gmail.com>
2022-10-16 14:05:06 +0900

    refactor: 플레이어들의 중복을 허용하지 않도록 변경
    #1483 피드백 반영
    중복을 제거하는 것이 아닌 예외를 발생시킨다.

--- src/main/java/nextstep/ladder/domain/player/Players.java
+++ src/main/java/nextstep/ladder/domain/player/Players.java
@@ -1,14 +1,36 @@
 package nextstep.ladder.domain.player;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Players {
+    private static final String DUPLICATED_NAME_EXCEPTION_MESSAGE = "중복된 이름의 플레이어는 허용하지 않습니다.";
+    private static final String PLAYER_SIZE_EXCEPTION_MESSAGE = "1명 이상의 플레이어가 있어야 합니다.";
     private final List<Player> values;
 
-    public static Players create(String names, String delimiter) {
-        return create(new Names(names, delimiter));
+    public Players(List<Player> values) {
+        checkSize(values);
+        checkDuplicatePlayer(values);
+
+        this.values = values;
+    }
+
+    private void checkSize(List<Player> values) {
+        if (values.size() == 0) {
+            throw new IllegalArgumentException(PLAYER_SIZE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void checkDuplicatePlayer(List<Player> values) {
+        if (new HashSet<>(values).size() != values.size()) {
+            throw new IllegalArgumentException(DUPLICATED_NAME_EXCEPTION_MESSAGE);
+        }
+    }
+
+    public static Players create(String ... names) {
+        return create(new Names(names));
     }
 
     public static Players create(Names names) {
@@ -18,12 +40,6 @@ public class Players {
                 .collect(Collectors.toList());
 
         return new Players(players);
-    }
-
-    public Players(List<Player> values) {
-        this.values = values.stream()
-                .distinct()
-                .collect(Collectors.toList());
     }
 
     public int count() {
