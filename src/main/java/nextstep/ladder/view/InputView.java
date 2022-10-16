1c9072e165a24a1c36ba887cdefd8e51e2c5a95f
michol.lee <lovethefeel@gmail.com>
2022-10-16 23:31:16 +0900

    refactor: 이름 및 높이 입력의 띠른 익셉션 처리

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -1,22 +1,47 @@
 package nextstep.ladder.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
+
+    private static final String REGEX_COMMA = ",";
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
     private InputView() {}
 
-    public static String inputName() {
+    public static List<String> inputName() {
 
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        return SCANNER.nextLine();
+        String name = SCANNER.nextLine();
+        validate(name);
+        return split(name);
     }
 
-    public static String inputLadderHeight() {
+    private static void validate(final String name) {
+
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("참가자 이름은 필수값입니다.");
+        }
+    }
+
+    private static List<String> split(final String name) {
+
+        return Arrays.stream(name.split(REGEX_COMMA))
+                .map(String::valueOf)
+                .collect(Collectors.toList());
+    }
+
+    public static int inputLadderHeight() {
 
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        return SCANNER.nextLine();
+        try {
+            return Integer.parseInt(SCANNER.nextLine());
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException("숫자만 입력해야 합니다.");
+        }
     }
 }
