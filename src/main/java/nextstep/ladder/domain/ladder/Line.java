0dc90676f7e7cd1846e51f9078588e9978dc8994
nooose <hd15807@gmail.com>
2022-10-16 14:24:20 +0900

    fix: 불완전하게 사다리가 생성되는 상황 수정
    #1483 피드백 반영
    그 외 생성자를 메서드 보다 위에 배치하도록 변경했고,
    상수명을 수정해 의미를 더 정확하게 표현했다.

--- src/main/java/nextstep/ladder/domain/ladder/Line.java
+++ src/main/java/nextstep/ladder/domain/ladder/Line.java
@@ -6,51 +6,10 @@ import java.util.List;
 
 public class Line {
     private static final String POINT_OVERLAP_MESSAGE = "점이 연속해 이어져 있습니다.";
-    private static final String POINTS_SIZE_EXCEPTION_MESSAGE = "선은 점이 1개 이상이어야 합니다.";
-    private static final int MIN_SIZE = 1;
+    private static final int MIN_POINTS_SIZE = 1;
+    private static final String POINTS_SIZE_EXCEPTION_MESSAGE = "점이 " + MIN_POINTS_SIZE + "개 이상이어야 합니다.";
 
     private final List<Point> points;
-
-    public static Line create(int countOfPoint, EnablePointStrategy strategy) {
-        List<Point> points = initPoints(strategy);
-
-        if (isOnePoint(countOfPoint)) {
-            return new Line(points);
-        }
-
-        if (isTwoPoint(countOfPoint)) {
-            points.add(Point.lastOf(points.get(0), strategy));
-            return new Line(points);
-        }
-
-        setPoints(points, strategy, countOfPoint);
-        return new Line(points);
-    }
-
-    private static void setPoints(List<Point> points, EnablePointStrategy strategy, int countOfPoint) {
-        for (int i = 1; i < countOfPoint - 1; i++) {
-            Point prevPoint = points.get(i - 1);
-            Point nextPoint = Point.nextOf(prevPoint, strategy);
-            points.add(nextPoint);
-        }
-
-        Point lastPoint = Point.lastOf(points.get(points.size() - 1), strategy);
-        points.add(lastPoint);
-    }
-
-    private static List<Point> initPoints(EnablePointStrategy strategy) {
-        Point firstPoint = Point.first(strategy);
-        return new ArrayList<>(List.of(firstPoint));
-    }
-
-    private static boolean isOnePoint(int countOfPoint) {
-        return countOfPoint == 1;
-    }
-
-    private static boolean isTwoPoint(int countOfPoint) {
-        return countOfPoint == 2;
-    }
-
 
     public Line(List<Point> points) {
         validate(points);
@@ -68,7 +27,7 @@ public class Line {
     }
 
     private boolean isNotValidSize(List<Point> points) {
-        return points.size() < MIN_SIZE;
+        return points.size() < MIN_POINTS_SIZE;
     }
 
     private boolean isNotValidPoints(List<Point> points) {
@@ -83,6 +42,53 @@ public class Line {
         }
 
         return false;
+    }
+
+    public static Line create(int countOfPoint, EnablePointStrategy strategy) {
+        if (isZeroPoint(countOfPoint)) {
+            return new Line(List.of());
+        }
+
+        if (isOnePoint(countOfPoint)) {
+            return new Line(List.of(new Point(0, false, false)));
+        }
+
+        List<Point> points = initPoints(strategy);
+        if (isTwoPoint(countOfPoint)) {
+            points.add(Point.lastOf(points.get(0), strategy));
+            return new Line(points);
+        }
+
+        setPoints(points, strategy, countOfPoint);
+        return new Line(points);
+    }
+
+    private static boolean isZeroPoint(int countOfPoint) {
+        return countOfPoint == 0;
+    }
+
+    private static boolean isOnePoint(int countOfPoint) {
+        return countOfPoint == 1;
+    }
+
+    private static boolean isTwoPoint(int countOfPoint) {
+        return countOfPoint == 2;
+    }
+
+    private static void setPoints(List<Point> points, EnablePointStrategy strategy, int countOfPoint) {
+        for (int i = 1; i < countOfPoint - 1; i++) {
+            Point prevPoint = points.get(i - 1);
+            Point nextPoint = Point.nextOf(prevPoint, strategy);
+            points.add(nextPoint);
+        }
+
+        Point lastPoint = Point.lastOf(points.get(points.size() - 1), strategy);
+        points.add(lastPoint);
+    }
+
+    private static List<Point> initPoints(EnablePointStrategy strategy) {
+        Point firstPoint = Point.first(strategy);
+        return new ArrayList<>(List.of(firstPoint));
     }
 
     public List<Point> points() {
