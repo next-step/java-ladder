dbd479c4881b4b2dd397a9ab2245b80320ca1011
michol.lee <lovethefeel@gmail.com>
2022-10-16 23:07:05 +0900

    refactor: 방향을 나타내는 클래스 책임 부여

--- src/main/java/nextstep/ladder/domain/Direction.java
+++ src/main/java/nextstep/ladder/domain/Direction.java
@@ -1,5 +1,59 @@
 package nextstep.ladder.domain;
 
+import java.util.Arrays;
+
 public enum Direction {
-    RIGHT, DOWN, LEFT
+    RIGHT(false, true),
+    DOWN(false, false),
+    LEFT(true, false);
+
+    private final boolean left;
+    private final boolean right;
+
+    Direction(final boolean left, final boolean right) {
+
+        this.left = left;
+        this.right = right;
+    }
+
+    public static Direction first(final boolean right) {
+
+        return Direction.of(false, right);
+    }
+
+    public static Direction of(final boolean left, final boolean right) {
+
+        return Arrays.stream(Direction.values())
+                .filter(direction -> direction.left == left && direction.right == right)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("연속된 사다리는 그릴 수 없습니다."));
+    }
+
+    Direction direct(boolean right) {
+
+        if (this.right && right) {
+            return Direction.of(this.right, false);
+        }
+        return Direction.of(this.right, right);
+    }
+
+    public Direction next(final boolean right) {
+
+        return direct(right);
+    }
+
+    public boolean duplicate() {
+
+        return this.left && this.right;
+    }
+
+    public boolean isLeft() {
+
+        return left;
+    }
+
+    public boolean isRight() {
+
+        return right;
+    }
 }
