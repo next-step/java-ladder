f1018e16c9f905617004ec6c5d94bb143c8dd475
seungwoo.song <seungwoo.song@nhnpayco.com>
2022-10-17 23:04:14 +0900

    refactor(InputView.java): name -> person 변환로직 추가

--- src/main/java/ladder/InputDTO.java
+++ src/main/java/ladder/InputDTO.java
@@ -1,15 +1,27 @@
 package ladder;
 
+import ladder.domain.Person;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by seungwoo.song on 2022-10-17
  */
 public class InputDTO {
 
-	private String names;
+	private List<Person> persons;
 	private int height;
 
 	public InputDTO(String names, int height) {
-		this.names = names;
+		this.persons = toPersons(names);
 		this.height = height;
+	}
+
+	private static List<Person> toPersons(String names) {
+		return Arrays.stream(names.split(","))
+			.map(Person::new)
+			.collect(Collectors.toList());
 	}
 }
