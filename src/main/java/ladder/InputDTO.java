1a1a2efaf6505c1cbc8bd12fff73bf66069add83
seungwoo.song <seungwoo.song@nhnpayco.com>
2022-10-17 23:01:59 +0900

    feat(InputView.java): view 추가

---
+++ src/main/java/ladder/InputDTO.java
@@ -0,0 +1,15 @@
+package ladder;
+
+/**
+ * Created by seungwoo.song on 2022-10-17
+ */
+public class InputDTO {
+
+	private String names;
+	private int height;
+
+	public InputDTO(String names, int height) {
+		this.names = names;
+		this.height = height;
+	}
+}
