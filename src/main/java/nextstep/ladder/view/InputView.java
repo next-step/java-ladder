89d24396108f7fdecb502e5a667e54329b688470
michol.lee <lovethefeel@gmail.com>
2022-10-18 23:56:21 +0900

    feat: 결과 검색 입력

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -7,18 +7,26 @@ import java.util.stream.Collectors;
 
 public class InputView {
 
-    private static final String REGEX_COMMA = ",";
+    private static final String DELIMITER = ",";
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
     private InputView() {}
 
-    public static List<String> inputName() {
+    public static List<String> inputParticipationNames() {
 
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        String name = SCANNER.nextLine();
+        final String name = SCANNER.nextLine();
         validate(name);
         return split(name);
+    }
+
+    public static List<String> inputRunningResult() {
+
+        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        final String result = SCANNER.nextLine();
+        validate(result);
+        return split(result);
     }
 
     private static void validate(final String name) {
@@ -30,7 +38,7 @@ public class InputView {
 
     private static List<String> split(final String name) {
 
-        return Arrays.stream(name.split(REGEX_COMMA))
+        return Arrays.stream(name.split(DELIMITER))
                 .map(String::valueOf)
                 .collect(Collectors.toList());
     }
@@ -39,9 +47,18 @@ public class InputView {
 
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         try {
-            return Integer.parseInt(SCANNER.nextLine());
+            return Integer.parseInt(SCANNER.next());
         } catch (NumberFormatException nfe) {
             throw new IllegalArgumentException("숫자만 입력해야 합니다.");
         }
     }
+
+    public static String inputResultName() {
+
+        System.out.println("결과를 보고 싶은 사람은?");
+        final String name = SCANNER.next();
+        validate(name);
+        return name;
+    }
+
 }
