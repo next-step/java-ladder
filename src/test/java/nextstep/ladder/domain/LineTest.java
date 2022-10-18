69daccd45f3563d55e4f3c353a5f75191a491b1a
michol.lee <lovethefeel@gmail.com>
2022-10-19 00:16:57 +0900

    refactor(test): 사다리 한 라인 생성 테스트

--- src/test/java/nextstep/ladder/domain/LineTest.java
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -1,25 +1,54 @@
 package nextstep.ladder.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LineTest {
 
-    @DisplayName("사다리 한 라인을 확인한다.")
-    @Test
-    void create() {
+    private Line line;
 
-        final Point point = Point.first(true);
-        final List<Point> points = List.of(point
-                , point.next(false)
-                , point.next(true)
+    @BeforeEach
+    void setUp() {
+
+        final Point first = Point.first(true);
+        final List<Point> points = List.of(first
+                , first.next(false)
+                , first.next(false).next(true)
+                , first.next(false).next(true).next(false)
+        );
+        line = new Line(points);
+    }
+
+    /**
+     *     0     1     2     3
+     *     |-----|     |-----|
+     *   꽝    5000  꽝    3000
+     */
+    @DisplayName("사다리 한 라인을 생성 후 이동결과를 확인한다.")
+    @Test
+    void verify_line() {
+
+        final Point first = Point.first(true);
+        final List<Point> points = List.of(first
+                , first.next(false)
+                , first.next(false).next(true)
+                , first.next(false).next(true).next(false)
         );
         final Line line = new Line(points);
+        final Map<Integer, Integer> convert = line.convert();
 
-        assertThat(line).isEqualTo(new Line(points));
+        assertAll(
+                () -> assertThat(convert.get(0)).isEqualTo(1),
+                () -> assertThat(convert.get(1)).isEqualTo(0),
+                () -> assertThat(convert.get(2)).isEqualTo(3),
+                () -> assertThat(convert.get(3)).isEqualTo(2)
+        );
     }
 }
