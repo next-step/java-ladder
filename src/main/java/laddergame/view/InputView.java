16a2bd11fbe82f584d4ad4ba88f00578934bb469
hiblue02 <hiblue02@gmail.com>
2022-10-18 23:08:07 +0900

    feat(InputView): 결과를 보고 싶은 사람 이름을 입력받는 기능 추가

--- src/main/java/laddergame/view/InputView.java
+++ src/main/java/laddergame/view/InputView.java
@@ -28,4 +28,9 @@ public class InputView {
         }
     }
 
+    public static String askPersonNameWantToKnowResult(){
+        System.out.println("결과를 보고 싶은 사람은?");
+        return SCANNER.nextLine();
+    }
+
 }
