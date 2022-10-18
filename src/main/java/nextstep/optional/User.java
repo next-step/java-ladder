bb8399514ddd2fbdaa71084f49eab43b173cbbe7
annelguplus <annelguplus@gmail.com>
2022-10-19 03:00:29 +0900

    feat: 1단계 - 자바8, 스트림, 람다, Optional

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,7 +35,11 @@ public class User {
     }
 
     public static boolean ageIsInRange2(User user) {
-        return false;
+        return Optional.ofNullable(user)
+                .filter((x) -> x.getAge() != null)
+                .filter((x) -> x.getAge() >= 30)
+                .filter((x) -> x.getAge() <= 42)
+                .isPresent();
     }
 
     @Override
