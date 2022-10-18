d50611151a1062ed744ad84f3f2e6e7c42fe5f10
jbpark-in-seoul <jbparkinseoul@gmail.com>
2022-10-18 17:40:24 +0900

    feat(step2): 입력 도메인 - (new) 사다리의 높이 입력

--- src/main/java/nextstep/ladder/Input.java
+++ src/main/java/nextstep/ladder/Input.java
@@ -1,13 +1,15 @@
 package nextstep.ladder;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Input {
+    private final static Scanner SCANNER = new Scanner(System.in);
     private final static String ENTER_NAME_OF_PERSON = "참여할 사람 이름을 입력하세요. (이름은 쉼표로 구분해야 하며, 최대 다섯 자를 넘을 수 없습니다.)";
+    private final static String ENTER_HEIGHT_OF_LADDER = "사다리의 최대 높이는 몇인가요?";
     private final static String INVALID_NAME_FORMAT = "입력된 사람의 이름 포맷이 올바르지 않습니다.";
     private final static String NOT_ENTERED_VALUE = "값이 입력되지 않았습니다.";
     private final static String SEPARATOR_COMMA = ",";
-    private final static Scanner SCANNER = new Scanner(System.in);
 
     public static String inputPerson() {
         System.out.println(ENTER_NAME_OF_PERSON);
@@ -18,10 +20,19 @@ public class Input {
         throw new IllegalArgumentException(INVALID_NAME_FORMAT);
     }
 
+    public static int inputHeightOfLadder() {
+        System.out.println(ENTER_HEIGHT_OF_LADDER);
+        return SCANNER.nextInt();
+    }
+
     private static boolean containsSeparator(String input) {
-        if (input == null || input.equals("")) {
+        if (isNullOrSpace(input)) {
             throw new IllegalArgumentException(NOT_ENTERED_VALUE);
         }
         return input.contains(SEPARATOR_COMMA);
+    }
+
+    private static boolean isNullOrSpace(String text) {
+        return text == null || text.equals("");
     }
 }
