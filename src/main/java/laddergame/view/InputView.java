a5a2e18ccda01b50ad29fb6075ec48240297524d
hiblue02 <hiblue02@gmail.com>
2022-10-20 00:54:46 +0900

    fix(InputView): 결과를 보고싶은 사람 입력 시 nextLine 추가

--- src/main/java/laddergame/view/InputView.java
+++ src/main/java/laddergame/view/InputView.java
@@ -28,12 +28,13 @@ public class InputView {
         }
     }
 
-    public static String askPersonNameWantToKnowResult(){
+    public static String askPersonNameWantToKnowResult() {
+        SCANNER.nextLine();
         System.out.println("결과를 보고 싶은 사람은?");
         return SCANNER.nextLine();
     }
 
-    public static String[] askRewards(){
+    public static String[] askRewards() {
         System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
         String nextLine = SCANNER.nextLine();
         return nextLine.split(",");
---
+++ src/test/java/laddergame/view/InputViewTest.java
@@ -0,0 +1,12 @@
+package laddergame.view;
+
+import org.junit.jupiter.api.Test;
+
+class InputViewTest {
+
+    @Test
+    void test() {
+        String s = InputView.askPersonNameWantToKnowResult();
+    }
+
+}
