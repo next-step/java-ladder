ed31ff4b9cc35c10503f5b73d3aaaa83400a0bb1
Lee Anne <melllamodahye@gmail.com>
2022-10-19 19:37:03 +0900

    feat: 1단계 - 자바8, 스트림, 람다, Optional (#1504)
    Co-authored-by: annelguplus <annelguplus@gmail.com>

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,5 +1,7 @@
 package nextstep.fp;
 
+import nextstep.optional.Condition;
+
 import java.util.List;
 
 public class Lambda {
@@ -26,31 +28,15 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+    public static int sumAll(List<Integer> numbers, Condition condition) {
+        return numbers.stream().filter(condition::isMet).mapToInt(Integer::intValue).sum();
     }
 
-    public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+    public static int sumAllEven(List<Integer> numbers, Condition condition) {
+        return numbers.stream().filter(condition::isMet).mapToInt(Integer::intValue).sum();
     }
 
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+    public static int sumAllOverThree(List<Integer> numbers, Condition condition) {
+        return numbers.stream().filter(condition::isMet).mapToInt(Integer::intValue).sum();
     }
 }
---
+++ src/main/java/nextstep/optional/Condition.java
@@ -0,0 +1,5 @@
+package nextstep.optional;
+
+public interface Condition {
+    boolean isMet(int number);
+}
