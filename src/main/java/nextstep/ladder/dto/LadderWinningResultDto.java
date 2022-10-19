d28620fb9d20207242d387e23494c55ba659462d
michol.lee <lovethefeel@gmail.com>
2022-10-19 23:43:13 +0900

    refactor: 결과 출력을 위한 변환 로직 추가

--- src/main/java/nextstep/ladder/dto/LadderWinningResultDto.java
+++ src/main/java/nextstep/ladder/dto/LadderWinningResultDto.java
@@ -1,7 +1,6 @@
 package nextstep.ladder.dto;
 
-import nextstep.ladder.domain.LadderWinningResult;
-import nextstep.ladder.domain.ParticipationNames;
+import nextstep.ladder.domain.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -16,10 +15,52 @@ public class LadderWinningResultDto {
         this.ladderResultDto = ladderResultDto;
     }
 
-    public static LadderWinningResultDto of(final LadderWinningResult ladderWinningResult
-            , final ParticipationNames participationNames, final List<String> resultNames) {
+    public static LadderWinningResultDto of(final Ladder ladder, final List<String> participationNames
+            , final List<String> resultNames) {
 
-        return new LadderWinningResultDto(collect(ladderWinningResult, participationNames, resultNames));
+        return new LadderWinningResultDto(collect(convert(ladder), participationNames, resultNames));
+    }
+
+    private static Map<String, String> collect(final Map<Integer, Integer> ladderResult
+            , final List<String> participationNames, final List<String> resultNames) {
+
+        final Map<String, String> ladderResultDto = new HashMap<>();
+        for (Map.Entry<Integer, Integer> entry : ladderResult.entrySet()) {
+            ladderResultDto.put(participationNames.get(entry.getKey()), resultNames.get(entry.getValue()));
+        }
+        return ladderResultDto;
+    }
+
+    private static Map<Integer, Integer> convert(final Ladder ladder) {
+
+        final Map<Integer, Integer> ladderResult = new HashMap<>();
+        final List<Line> lines = ladder.getLines();
+        for (int i = 0; i < lines.size(); i++) {
+            verify(ladderResult, lines.get(i), i);
+        }
+        return ladderResult;
+    }
+
+    private static void verify(final Map<Integer, Integer> ladderResult, final Line line, final int row) {
+
+        for (int column = 0; column < line.getPoints().size(); column++) {
+            final Point point = findPoint(ladderResult, line, row, column);
+            ladderResult.put(column, point.move());
+        }
+    }
+
+    private static Point findPoint(final Map<Integer, Integer> ladderResult, final Line line
+            , final int row, final int column) {
+
+        if (row == 0) {
+            return getPoint(line, column);
+        }
+        return getPoint(line, ladderResult.get(column));
+    }
+
+    private static Point getPoint(final Line line, int point) {
+
+        return line.getPoints().get(point);
     }
 
     public Map<String, String> getLadderResultDto() {
@@ -27,14 +68,8 @@ public class LadderWinningResultDto {
         return ladderResultDto;
     }
 
-    public static Map<String, String> collect(final LadderWinningResult ladderWinningResult
-            , final ParticipationNames participationNames, final List<String> resultNames) {
+    public String findByName(final String key) {
 
-        final Map<String, String> ladderResultDto = new HashMap<>();
-        final Map<Integer, Integer> ladderResult = ladderWinningResult.getLadderResult();
-        for (Map.Entry<Integer, Integer> entry : ladderResult.entrySet()) {
-            ladderResultDto.put(participationNames.getName().get(entry.getKey()), resultNames.get(entry.getValue()));
-        }
-        return ladderResultDto;
+        return this.ladderResultDto.get(key);
     }
 }
