5698e624c1957774766e152634d2c4ba191cf5df
모진혁 <gkgk246@naver.com>
2022-10-20 23:48:41 +0900

    test : Line클래스에서의 move메소드 테스트 추가

--- src/test/java/nextstep/ladder/Model/LineTest.java
+++ src/test/java/nextstep/ladder/Model/LineTest.java
@@ -10,6 +10,9 @@ import static org.assertj.core.api.Assertions.*;
 
 class LineTest {
 
+    private static final int START_LOCATION = 1;
+    private static final int EXPECT_LOCATION = 0;
+
     @Test
     @DisplayName("Line을 생성한 이후에 true만을 삽입하여 원하는 결과가 나오는지에 대한 테스트")
     public void connectBridgeTrueTest() {
@@ -36,6 +39,17 @@ class LineTest {
         expectLine.add(new Bridge(false));
         expectLine.add(new Bridge(false));
         assertThat(line.getLine()).isEqualTo(expectLine);
+    }
+
+    @Test
+    @DisplayName("Line을 생성한 이후에 move가 정상적으로 작동 되는지에 대한 테스트")
+    public void lineMoveTest(){
+        Line line = new Line(new ArrayList<>());
+        line.checkDecidingConnectBridge(() ->false);
+        line.checkDecidingConnectBridge(() ->true);
+        line.checkDecidingConnectBridge(() ->false);
+        assertThat(line.move(START_LOCATION)).isEqualTo(EXPECT_LOCATION);
+
     }
 
 }
