5c4ec156656a1c19946063d0e592431c082cfe9e
jhwlim <jhwlim25@gmail.com>
2022-10-21 01:40:46 +0900

    test : 사다리 게임 결과 테스트 추가

---
+++ src/test/java/laddergame/domain/LadderGameTest.java
@@ -0,0 +1,43 @@
+package laddergame.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LadderGameTest {
+
+    private final List<ParticipantName> participantNames = List.of(
+            new ParticipantName("a"),
+            new ParticipantName("ab"),
+            new ParticipantName("abc")
+    );
+
+    private final List<LadderGameReward> rewards = List.of(
+            new LadderGameReward("꽝"),
+            new LadderGameReward("꽝2"),
+            new LadderGameReward("당첨")
+    );
+
+    private final Ladder ladder = new Ladder(List.of(
+            new LadderLine(List.of(false, true)),
+            new LadderLine(List.of(true, false)),
+            new LadderLine(List.of(true, false))
+    ));
+
+    @DisplayName("사다리 게임 최종 결과를 반환해야 한다.")
+    @Test
+    void calculateRewardByParticipantName() {
+        LadderGame ladderGame = new LadderGame(participantNames, rewards, ladder);
+        assertThat(ladderGame.calculateRewardByParticipantName())
+                .isEqualTo(Map.of(
+                        new ParticipantName("a"), new LadderGameReward("꽝"),
+                        new ParticipantName("ab"), new LadderGameReward("당첨"),
+                        new ParticipantName("abc"), new LadderGameReward("꽝2")
+                ));
+    }
+
+}
