be10328e17942986097bec591d9f1f644056b8a6
jungwoon727 <jungwoon727@gmail.com>
2022-10-21 15:56:54 +0900

    feat: 람다, 스트림 실습 완료

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,15 +5,16 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
 
     public static long countWords() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+        String contents = Files.readString(Paths
+                .get("src/main/resources/fp/war-and-peace.txt"));
+        String[] words = contents.split("[\\P{L}]+");
 
         long count = 0;
         for (String w : words) {
@@ -23,11 +24,21 @@ public class StreamStudy {
     }
 
     public static void printLongestWordTop100() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+        findLongestWordTop100().forEach(System.out::println);
+    }
+
+    public static List<String> findLongestWordTop100() throws IOException {
+        String contents = Files.readString(Paths
+                .get("src/main/resources/fp/war-and-peace.txt"));
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        return words.stream()
+                .filter(word -> word.length() > 12)
+                .map(String::toLowerCase)
+                .distinct()
+                .limit(100)
+                .sorted((word1, word2) -> word2.length() - word1.length())
+                .collect(Collectors.toList());
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -35,10 +46,13 @@ public class StreamStudy {
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers.stream().reduce(0, Integer::sum);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(number -> number > 3)
+                .map(number -> number * 2)
+                .reduce(0, Integer::sum);
     }
 }
--- src/test/java/nextstep/fp/StreamStudyTest.java
+++ src/test/java/nextstep/fp/StreamStudyTest.java
@@ -1,19 +1,27 @@
 package nextstep.fp;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StreamStudyTest {
     private List<Integer> numbers;
+    private List<String> words;
 
     @BeforeEach
-    public void setup() {
+    public void setup() throws IOException {
         numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        words = StreamStudy.findLongestWordTop100();
     }
 
     @Test
@@ -22,25 +30,73 @@ public class StreamStudyTest {
         System.out.println("result : " + result);
     }
 
-    @Test
-    public void printLongestWordTop100() throws Exception {
-        StreamStudy.printLongestWordTop100();
+    @DisplayName("12자가 넘는 단어 중 길이가 긴 순서로 100개의 단어를 출력한다. 단 중복이 없어야하며, 모두 소문자로 출력해야한다.")
+    @Nested
+    class LongestWordTop100 {
+
+        @DisplayName("출력")
+        @Test
+        public void printLongestWordTop100() throws IOException {
+            StreamStudy.printLongestWordTop100();
+        }
+
+        @DisplayName("길이가 긴 순서대로 정렬되어 있는지")
+        @Test
+        public void printLongestWordTop100_sort() {
+            IntStream.range(0, words.size() - 1)
+                    .forEach(index -> assertThat(findWordLength(words, index))
+                            .isGreaterThanOrEqualTo(findWordLength(words, index + 1)));
+        }
+
+        private int findWordLength(List<String> words, int index) {
+            return words.get(index).length();
+        }
+
+        @DisplayName("모든 단어가 12자가 넘는지 확인")
+        @Test
+        public void printLongestWordTop100_max_word_length() {
+            words.forEach(word -> assertThat(word.length()).isGreaterThan(12));
+        }
+
+        @DisplayName("모든 단어가 소문자인지 확인")
+        @Test
+        public void printLongestWordTop100_lower_case() {
+            words.forEach(word -> assertThat(word).isLowerCase());
+        }
+
+        @DisplayName("중복된 단어는 없는지 확인")
+        @Test
+        public void printLongestWordTop100_duplicate() {
+            int expected = words.size();
+
+            int actual = new HashSet<>(words).size();
+
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @DisplayName("단어 개수가 100가인지 확인")
+        @Test
+        public void printLongestWordTop100_list_size() {
+            int actual = words.size();
+
+            assertThat(actual).isEqualTo(100);
+        }
     }
 
     @Test
-    public void map() throws Exception {
+    public void map() {
         List<Integer> doubleNumbers = StreamStudy.doubleNumbers(numbers);
         doubleNumbers.forEach(System.out::println);
     }
 
     @Test
-    public void sumAll() throws Exception {
+    public void sumAll() {
         long sum = StreamStudy.sumAll(numbers);
         assertThat(sum).isEqualTo(21);
     }
 
     @Test
-    public void sumOverThreeAndDouble() throws Exception {
+    public void sumOverThreeAndDouble() {
         numbers = Arrays.asList(3, 1, 6, 2, 4, 8);
         long sum = StreamStudy.sumOverThreeAndDouble(numbers);
         assertThat(sum).isEqualTo(36);
