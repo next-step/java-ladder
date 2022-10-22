b5af7a579aa803eb68881ef3c86b03e1dbceb1c6
jbpark-in-seoul <jbparkinseoul@gmail.com>
2022-10-22 19:02:13 +0900

    refactor(step2): LadderParser - 메서드명 변경

--- src/main/java/nextstep/ladder/domain/LadderParser.java
+++ src/main/java/nextstep/ladder/domain/LadderParser.java
@@ -1,14 +1,26 @@
 package nextstep.ladder.domain;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LadderParser {
 
-    public static List<Person> listPerson(String[] names) {
-        return Arrays.stream(names)
-                .map(Person::new)
+    private LadderParser() {
+        throw new AssertionError();
+    }
+
+    public static List<Person> people(String[] names) {
+        return IntStream.range(0, names.length)
+                .mapToObj(sequence -> new Person(names[sequence]))
                 .collect(Collectors.toUnmodifiableList());
+    }
+
+    public static String appendWhiteSpaceInside(String text, int length) {
+        return String.format(new StringBuilder()
+                .append("%")
+                .append(length)
+                .append("s")
+                .toString(), text);
     }
 }
--- src/test/java/nextstep/ladder/domain/LadderParserTest.java
+++ src/test/java/nextstep/ladder/domain/LadderParserTest.java
@@ -12,7 +12,7 @@ public class LadderParserTest {
     @Test
     @DisplayName("입력 받은 사람의 이름의 타입을 파싱한다.(String[] -> List<Person>)")
     void stringArrayToListPerson() {
-        assertThat(LadderParser.listPerson(new String[]{"정우성", "이정재"}))
+        assertThat(LadderParser.people(new String[]{"정우성", "이정재"}))
                 .isInstanceOf(List.class);
     }
 }
