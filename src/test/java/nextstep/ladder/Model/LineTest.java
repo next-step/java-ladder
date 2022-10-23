5f00d64ab7b85688416117ca08b307d601ece064
모진혁 <gkgk246@naver.com>
2022-10-23 19:17:39 +0900

    test : Line Move메소드 왼쪽 오른쪽 둘다 테스트하도록 수정

--- src/test/java/nextstep/ladder/Model/LineTest.java
+++ src/test/java/nextstep/ladder/Model/LineTest.java
@@ -11,7 +11,8 @@ import static org.assertj.core.api.Assertions.*;
 class LineTest {
 
     private static final int START_LOCATION = 1;
-    private static final int EXPECT_LOCATION = 0;
+    private static final int LEFT_EXPECT_LOCATION = 0;
+    private static final int RIGHT_EXPECT_LOCATION = 2;
 
     @Test
     @DisplayName("Line을 생성한 이후에 true만을 삽입하여 원하는 결과가 나오는지에 대한 테스트")
@@ -42,13 +43,23 @@ class LineTest {
     }
 
     @Test
-    @DisplayName("Line을 생성한 이후에 move가 정상적으로 작동 되는지에 대한 테스트")
-    public void lineMoveTest(){
+    @DisplayName("Line을 생성한 이후에 move메소드가 왼쪽으로 정상적으로 작동 되는지에 대한 테스트")
+    public void lineLeftMoveTest(){
         Line line = new Line(new ArrayList<>());
         line.checkDecidingConnectBridge(() ->false);
         line.checkDecidingConnectBridge(() ->true);
         line.checkDecidingConnectBridge(() ->false);
-        assertThat(line.move(START_LOCATION)).isEqualTo(EXPECT_LOCATION);
+        assertThat(line.move(START_LOCATION)).isEqualTo(LEFT_EXPECT_LOCATION);
+    }
+
+    @Test
+    @DisplayName("Line을 생성한 이후에 move메소드가 오른쪽으로 정상적으로 작동 되는지에 대한 테스트")
+    public void lineRightMoveTest(){
+        Line line = new Line(new ArrayList<>());
+        line.checkDecidingConnectBridge(() ->false);
+        line.checkDecidingConnectBridge(() ->false);
+        line.checkDecidingConnectBridge(() ->true);
+        assertThat(line.move(START_LOCATION)).isEqualTo(RIGHT_EXPECT_LOCATION);
     }
 
 }
