130928f1b640d39ed89fb2b704529037a99a5259
yunhwan <pyh8618@gmail.com>
2022-10-23 12:12:12 +0900

    refactor : Line생성시 가로라인 상태를 바로 세팅하도록 변경

--- src/main/java/nextstep/laddar/Line.java
+++ src/main/java/nextstep/laddar/Line.java
@@ -1,59 +1,47 @@
 package nextstep.laddar;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Line {
-    private final List<Boolean> positions;
+    private final List<Boolean> positions = new ArrayList<>();
+    private final BooleanGenerator booleanGenerator;
 
-    public Line(int countOfPerson) {
-        this.positions = IntStream.range(0, countOfPerson)
-                .mapToObj((i) -> false)
-                .collect(Collectors.toList());
+    public Line(int countOfPerson, BooleanGenerator booleanGenerator) {
+        this.booleanGenerator = booleanGenerator;
+        for (int i = 0; i < countOfPerson; i++) {
+            this.positions.add(false);
+        }
+        for (int position = 0; position < this.positions.size(); position++) {
+            positions.set(position, putHorizontalLine(position));
+        }
     }
 
-    public void putHorizontalLine(int position, BooleanGenerator booleanGenerator) {
-        Optional.of(position)
-                .filter(pos -> !hasHorizontalLine(pos))
-                .filter(pos -> !hasNearHorizontalLine(pos))
-                .ifPresent(pos -> this.positions.set(pos, booleanGenerator.generate()));
+    public boolean putHorizontalLine(int position) {
+        if (!hasNearHorizontalLine(position)) {
+            return booleanGenerator.generate();
+        }
+        return false;
     }
 
     public boolean hasNearHorizontalLine(int position) {
-        int rangeNumber = Optional.of(position)
-                .filter(this::isRangePosition)
-                .orElseThrow(() -> new IllegalArgumentException("해당 위치에는 다리를 놓을 수 없습니다."));
-
-        boolean hasLeftLine = Optional.of(rangeNumber)
-                .filter(number -> number > 0 && positions.get(number - 1).equals(true))
-                .isPresent();
-        boolean hasRightLine = Optional.of(rangeNumber)
-                .filter(number -> number < positions.size() - 1 && positions.get(number + 1).equals(true))
-                .isPresent();
-
-        return hasLeftLine || hasRightLine;
+        validateRangePosition(position);
+        return hasLeftLine(position) || hasRightRine(position);
     }
 
-    boolean hasHorizontalLine(int position) {
-        return Optional.of(
-                        Optional.of(position)
-                                .filter(this::isRangePosition)
-                                .orElseThrow(() -> new IllegalArgumentException("해당 위치에는 다리를 놓을 수 없습니다."))
-                )
-                .map(positions::get)
-                .filter(hasLine -> hasLine.equals(true))
-                .isPresent();
+    private boolean hasRightRine(int position) {
+        return position < positions.size() - 1 && positions.get(position + 1).equals(true);
     }
 
-    private boolean isRangePosition(Integer pos) {
-        return pos < positions.size() && pos >= 0;
+    private boolean hasLeftLine(int position) {
+        return position > 0 && positions.get(position - 1).equals(true);
     }
 
-    public void makeHorizontalLines(BooleanGenerator booleanGenerator) {
-        IntStream.range(0, positions.size())
-                .forEach((idx) -> putHorizontalLine(idx, booleanGenerator));
+    private void validateRangePosition(int pos) {
+        if (pos >= positions.size() || pos < 0) {
+            throw new IllegalArgumentException("해당 위치에는 다리를 놓을 수 없습니다.");
+        }
     }
 
     public List<Boolean> getPositions() {
