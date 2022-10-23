263d696474abc797f1f85bc1ef6c3243c43c14dc
yunhwan <pyh8618@gmail.com>
2022-10-23 12:14:19 +0900

    refactor : User 불필요한 Optional 삭제

--- src/main/java/nextstep/laddar/User.java
+++ src/main/java/nextstep/laddar/User.java
@@ -3,21 +3,18 @@ package nextstep.laddar;
 import java.util.Optional;
 
 public class User {
-    private static final int NAME_MAX_LENGTH = 5;
+    public static final int NAME_MAX_LENGTH = 5;
 
     private final String name;
 
     public User(String name) {
-        this.name = Optional.ofNullable(name)
-                .filter(username -> NAME_MAX_LENGTH >= username.length())
-                .orElseThrow(() -> new IllegalArgumentException("이름은 5자를 넘을 수 없습니다."));
+        if (NAME_MAX_LENGTH < name.length()) {
+            throw new IllegalArgumentException("이름은 5자를 넘을 수 없습니다.");
+        }
+        this.name = name;
     }
 
     public String getName() {
         return name;
-    }
-
-    public static int getMaxNameLength() {
-        return NAME_MAX_LENGTH;
     }
 }
