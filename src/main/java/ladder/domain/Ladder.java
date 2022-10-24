966e0aaed2d0b6357a3cef8359ada974f2025801
Yunsik-Choi <choiys0311@gmail.com>
2022-10-25 01:42:25 +0900

    feat : Ladder create

---
+++ src/main/java/ladder/domain/Ladder.java
@@ -0,0 +1,14 @@
+package ladder.domain;
+
+public enum Ladder {
+
+    LEFT(true, false), RIGHT(false, true), NONE(false, false);
+
+    private final boolean left;
+    private final boolean right;
+
+    Ladder(boolean left, boolean right) {
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ src/test/java/ladder/domain/LadderTest.java
@@ -0,0 +1,18 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class LadderTest {
+
+    @Test
+    void create() {
+        Assertions.assertAll(
+                () -> assertThat(Ladder.LEFT).isEqualTo(Ladder.LEFT),
+                () -> assertThat(Ladder.LEFT).isNotEqualTo(Ladder.RIGHT),
+                () -> assertThat(Ladder.LEFT).isNotEqualTo(Ladder.NONE)
+        );
+    }
+}
