d38ff92bf61c6c98faf3c2a2aa26120786825bcc
오은경 <194765@shinsegae.com>
2022-10-24 17:16:37 +0900

    feat : Optional 실습 2 - Optional에서 값을 반환

--- src/main/java/nextstep/optional/Users.java
+++ src/main/java/nextstep/optional/Users.java
@@ -1,7 +1,9 @@
 package nextstep.optional;
 
+import javax.swing.text.html.Option;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Users {
     static final User DEFAULT_USER = new User("codesquad", 100);
@@ -12,12 +14,19 @@ public class Users {
             new User("jk", 40),
             new User("honux", 45));
 
-    User getUser(String name) {
+    User getUser2(String name) {
         for (User user : users) {
             if (user.matchName(name)) {
                 return user;
             }
         }
         return DEFAULT_USER;
+    }
+
+    User getUser(String name) {
+        return users.stream()
+                .filter(user -> user.matchName(name))
+                .findAny()
+                .orElse(DEFAULT_USER);
     }
 }
