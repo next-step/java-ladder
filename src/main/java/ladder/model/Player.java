a0b222d032c0753321cfdcb392db11ff066c985c
bella.n <bella.n@kakaocorp.com>
2022-10-24 11:32:24 +0900

    refactor: 이름 최대 길이 상수화

--- src/main/java/ladder/model/Player.java
+++ src/main/java/ladder/model/Player.java
@@ -6,11 +6,13 @@ import java.util.Optional;
 
 public class Player {
 
+    private static final int MAX_NAME_LENGTH = 5;
+
     private String name;
 
     public Player(String name) {
         this.name = Optional.of(name)
-                .filter(value -> value.length() <= 5)
+                .filter(value -> value.length() <= MAX_NAME_LENGTH)
                 .orElseThrow(() -> new WrongParameterException("[입력 오류] 플레이어의 이름은 최대 5자 입니다."));
     }
 
