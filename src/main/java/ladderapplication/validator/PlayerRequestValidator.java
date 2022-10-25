1ff12a8735ff14423843411c1d631f88e833c0cf
robin.son <robin.son@kakaocorp.com>
2022-10-25 21:02:02 +0900

    test : 입력받은 이름은 5글자가 넘지않는지 확인하는 벨리데이터의 테스트 추가

---
+++ src/main/java/ladderapplication/validator/PlayerRequestValidator.java
@@ -0,0 +1,10 @@
+package ladderapplication.validator;
+
+import ladderapplication.models.requests.PlayerRequest;
+
+public class PlayerRequestValidator {
+
+    public static void validation(PlayerRequest playerRequest) {
+    }
+
+}
