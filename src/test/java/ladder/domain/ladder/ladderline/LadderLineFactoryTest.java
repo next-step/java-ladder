a1c17bed858616c3e52907d7376653868d218cd0
Yunsik-Choi <choiys0311@gmail.com>
2022-10-26 01:55:21 +0900

    ⛑️ test : LadderLineFactoryTest setup

--- src/test/java/ladder/domain/ladder/ladderline/LadderLineFactoryTest.java
+++ src/test/java/ladder/domain/ladder/ladderline/LadderLineFactoryTest.java
@@ -4,20 +4,27 @@ import ladder.domain.ladder.Ladder;
 import ladder.domain.ladder.LadderWidth;
 import ladder.domain.ladder.strategy.LadderNotContinuousConnectStrategy;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 class LadderLineFactoryTest {
 
-    @Test
-    void create() {
-        LadderLineFactory ladderLineFactory = new LadderLineFactory(new LadderNotContinuousConnectStrategy()){
+    private LadderLineFactory ladderLineFactory;
+
+    @BeforeEach
+    void setUp() {
+        this.ladderLineFactory = new LadderLineFactory(new LadderNotContinuousConnectStrategy()){
             @Override
             protected Ladder randomConnectableLadder(Ladder beforeLadder) {
                 return Ladder.NONE;
             }
         };
+    }
+
+    @Test
+    void create() {
         LadderLine actual = ladderLineFactory.randomLadderLine(new LadderWidth(3));
 
         Assertions.assertThat(actual).isEqualTo(new LadderLine(List.of(Ladder.NONE,Ladder.NONE,Ladder.NONE)));
