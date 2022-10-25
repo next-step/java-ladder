1bac8a535f667dade7f7f1c6d09ef9c781129142
o3ozzvb <ou1112@naver.com>
2022-10-25 22:45:32 +0900

    refactor(NamesTest) : Test 구문 수정

--- src/test/java/ladder/domain/NamesTest.java
+++ src/test/java/ladder/domain/NamesTest.java
@@ -1,14 +1,7 @@
 package ladder.domain;
 
-import ladder.domain.Name;
-import ladder.domain.Names;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,9 +12,8 @@ public class NamesTest {
     public void 사람이름_쉼표로_구분(String candidates) {
         Names names = Names.createNames(candidates);
         assertThat(names.getNames().size()).isEqualTo(4);
-        assertThat(names.getNames().contains(new Name("pobi"))).isTrue();
-        assertThat(names.getNames().contains(new Name("honux"))).isTrue();
-        assertThat(names.getNames().contains(new Name("crong"))).isTrue();
-        assertThat(names.getNames().contains(new Name("jk"))).isTrue();
+        assertThat(names.getNames()).containsExactly(
+                new Name("pobi"), new Name("honux"), new Name("crong"), new Name("jk")
+        );
     }
 }
