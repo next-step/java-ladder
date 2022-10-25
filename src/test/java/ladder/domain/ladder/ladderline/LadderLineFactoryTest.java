9ace3fea58278775d286103cfc137c596ac44fcf
Yunsik-Choi <choiys0311@gmail.com>
2022-10-26 02:02:27 +0900

    ⛑️ test : LadderLineFactoryTest rename create 테스트를 random_ladder_line 으로 수정

--- src/test/java/ladder/domain/ladder/ladderline/LadderLineFactoryTest.java
+++ src/test/java/ladder/domain/ladder/ladderline/LadderLineFactoryTest.java
@@ -3,11 +3,12 @@ package ladder.domain.ladder.ladderline;
 import ladder.domain.ladder.Ladder;
 import ladder.domain.ladder.LadderWidth;
 import ladder.domain.ladder.strategy.LadderNotContinuousConnectStrategy;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 class LadderLineFactoryTest {
 
@@ -24,9 +25,9 @@ class LadderLineFactoryTest {
     }
 
     @Test
-    void create() {
+    void random_ladderline() {
         LadderLine actual = ladderLineFactory.randomLadderLine(new LadderWidth(3));
 
-        Assertions.assertThat(actual).isEqualTo(new LadderLine(List.of(Ladder.NONE,Ladder.NONE,Ladder.NONE)));
+        assertThat(actual).isEqualTo(new LadderLine(List.of(Ladder.NONE,Ladder.NONE,Ladder.NONE)));
     }
 }
