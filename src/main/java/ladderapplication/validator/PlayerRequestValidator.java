23a65d22a81b8f9f31d8b1ddbe3c8af0f22ccfc1
robin.son <robin.son@kakaocorp.com>
2022-10-25 21:03:59 +0900

    feature : 입력받은 사람의 이름은 5글자를 넘지 않는다.

--- src/main/java/ladderapplication/validator/PlayerRequestValidator.java
+++ src/main/java/ladderapplication/validator/PlayerRequestValidator.java
@@ -4,7 +4,12 @@ import ladderapplication.models.requests.PlayerRequest;
 
 public class PlayerRequestValidator {
 
+    private static final String WRONG_NAME = "플레이어 이름음 5글자 이하 여야 합니다.";
+
     public static void validation(PlayerRequest playerRequest) {
+        if (playerRequest.getName().length() > 5) {
+            throw new IllegalArgumentException(WRONG_NAME);
+        }
     }
 
 }
