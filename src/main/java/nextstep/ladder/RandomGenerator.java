3231c174641d98a9f99da27b59c9d849a3fb06e4
yunhwan <pyh8618@gmail.com>
2022-10-26 02:56:45 +0900

    refactor : RAMDOM 객체 상수화

--- src/main/java/nextstep/ladder/RandomGenerator.java
+++ src/main/java/nextstep/ladder/RandomGenerator.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class RandomGenerator implements BooleanGenerator {
 
-    private final Random RANDOM = new Random();
+    private static final Random RANDOM = new Random();
 
     @Override
     public boolean generate() {
