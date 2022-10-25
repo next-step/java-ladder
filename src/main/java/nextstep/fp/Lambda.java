fb41df395d64c942bd447dc6cd3410c4f42f0623
Yunsik-Choi <choiys0311@gmail.com>
2022-10-25 09:55:15 +0900

    refactor : Lambda sumAll 메소드명 변경

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -26,19 +26,19 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
-        return sumAll(numbers, number -> true);
+    public static int sumAllByConditional(List<Integer> numbers) {
+        return sumAllByConditional(numbers, number -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        return sumAll(numbers, number -> number % 2 == 0);
+        return sumAllByConditional(numbers, number -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        return sumAll(numbers, number -> number > 3);
+        return sumAllByConditional(numbers, number -> number > 3);
     }
 
-    public static int sumAll(List<Integer> numbers, Conditional c) {
+    public static int sumAllByConditional(List<Integer> numbers, Conditional c) {
         int total = 0;
         for (int number : numbers) {
             if (c.test(number)) {
