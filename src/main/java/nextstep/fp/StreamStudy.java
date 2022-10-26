da62252ec3c32a98337b49520be4153e0d374f24
Ji-Yeong Kim <kingkk31@naver.com>
2022-10-26 19:46:13 +0900

    1단계 - 자바8 스트림, 람다, Optional (#1532)
    * refactor: MoveStrategy 구현 부분 람다로 변경 (람다 실습 1)
    
    * refactor: 조건별 덧셈 중복 제거 및 조건 구현 (람다 실습 2)
    
    * feat: sumOverThreeAndDouble 메소드 구현 (스트림 실습 1)
    
    * feat: printLongestWordTop100 메소드 구현 (스트림 실습 2)
    
    * feat: ageIsInRange2 메소드 구현 (Optional 요구사항 1)
    
    * refactor: getUser 메소드 Stream, Optional 도입 (Optional 요구사항 2)
    
    * refactor: Expression of 메소드 Stream, Optional 도입 (Optional 요구사항 3)

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -10,6 +10,9 @@ import java.util.stream.Collectors;
 
 public class StreamStudy {
 
+    private static final int WORD_LENGTH = 12;
+    private static final int WORD_LIMIT = 100;
+
     public static long countWords() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
@@ -27,7 +30,13 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(str -> str.length() > WORD_LENGTH)
+                .distinct()
+                .map(String::toLowerCase)
+                .sorted((left, right) -> right.length() - left.length())
+                .limit(WORD_LIMIT + 1)
+                .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +48,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(number -> number > 3)
+                .map(number -> number * 2)
+                .reduce(0, Integer::sum);
     }
 }
