ebb6d5ed5d7adde6718a5902c1963ca4d06d25c5
hiblue02 <hiblue02@gmail.com>
2022-10-28 02:09:56 +0900

    feat(Direction): 방향을 관리하는 객체 추가

--- src/main/java/laddergame/Direction.java
+++ src/main/java/laddergame/Direction.java
@@ -2,26 +2,54 @@ package laddergame;
 
 import java.util.Random;
 
+import static java.lang.Boolean.FALSE;
+
 public class Direction {
+
     private static final Random RANDOM = new Random();
 
     private final boolean left;
+    private final boolean right;
 
-    private Direction(boolean left) {
+    private Direction(boolean left, boolean right) {
+        if (left && right) {
+            throw new IllegalStateException();
+        }
+
         this.left = left;
+        this.right = right;
+
     }
 
-    public static Direction random(){
-        return new Direction(RANDOM.nextBoolean());
+    public boolean isRight() {
+        return this.right;
     }
 
-    public boolean isLeft(){
-        return left;
+    public boolean isLeft() {
+        return this.left;
     }
 
-    public boolean isRight(){
-        return !left;
+    public Direction next(boolean nextRight) {
+        return of(this.right, nextRight);
     }
 
+    public Direction next() {
+        if (this.right) {
+            return next(FALSE);
+        }
+        return next(RANDOM.nextBoolean());
+    }
+
+    public static Direction of(boolean first, boolean second) {
+        return new Direction(first, second);
+    }
+
+    public static Direction first() {
+        return of(FALSE, RANDOM.nextBoolean());
+    }
+
+    public Direction last() {
+        return of(this.right, FALSE);
+    }
 
 }
---
+++ src/test/java/laddergame/DirectionTest.java
@@ -0,0 +1,63 @@
+package laddergame;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class DirectionTest {
+    @Test
+    @DisplayName("첫번째 방향은 언제나 왼쪽으로는 갈 수 없음")
+    void first() {
+        //given
+        Direction first = Direction.first();
+        //then
+        assertThat(first.isLeft()).isFalse();
+    }
+
+    @Test
+    @DisplayName("마지막 방향은 언제나 오른쪽으로는 갈 수 없음")
+    void last() {
+        //given
+        Direction last = Direction.first().last();
+        //then
+        assertThat(last.isRight()).isFalse();
+    }
+
+    @Test
+    @DisplayName("양 방향이 들어오면 오류")
+    void left_and_right_all_same_time() {
+        assertThatThrownBy(() -> Direction.of(true, true)).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("방향이 아예 없으면 정상")
+    void left_and_right_none_same_time() {
+        assertThatNoException().isThrownBy(() -> Direction.of(false, false));
+    }
+
+    @Test
+    @DisplayName("다음 방향을 만들때 이전 방향을 가져가는지 확인")
+    void next() {
+        //given
+        Direction first = Direction.first();
+        Direction next = first.next();
+        //then
+        assertThat(first.isRight()).isEqualTo(next.isLeft());
+    }
+
+    @Test
+    @DisplayName("방향을 연달아서 안 만드는지 획인")
+    void next_연달아() {
+        //given
+        Direction direction = Direction.of(false, true);
+        Direction next = direction.next();
+        //then
+        assertAll(
+                () -> assertThat(next.isRight()).isFalse(),
+                () -> assertThat(next.isLeft()).isTrue()
+        );
+
+    }
+}
