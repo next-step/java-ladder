2157251e0409c662fc363699ca2e94d3855cb652
hiblue02 <hiblue02@gmail.com>
2022-10-28 02:15:58 +0900

    refactor(RewardsTest): 사람 명수와 보상 개수 비교 테스트 삭제

--- src/test/java/laddergame/domain/RewardsTest.java
+++ src/test/java/laddergame/domain/RewardsTest.java
@@ -7,25 +7,6 @@ import static org.assertj.core.api.Assertions.*;
 
 class RewardsTest {
 
-    @Test
-    @DisplayName("사람 명수와 보상 개수가 같으면 정상")
-    void number_person_equal_number_rewards() {
-        //given
-        People people = new People("aaa,bbb,ccc,ddd".split(","));
-        Rewards rewards = new Rewards("꽝,5000,꽝,3000".split(","));
-        //then
-        assertThatNoException().isThrownBy(() -> rewards.validate(people));
-    }
-
-    @Test
-    @DisplayName("사람 명수와 보상 개수가 다르면 오류")
-    void number_person_not_equal_number_rewards() {
-        //given
-        People people = new People("aaa,bbb,ccc".split(","));
-        Rewards rewards = new Rewards("꽝,5000,꽝,3000,꽝".split(","));
-        //then
-        assertThatIllegalArgumentException().isThrownBy(() -> rewards.validate(people));
-    }
 
     @Test
     @DisplayName("인덱스 조회")
