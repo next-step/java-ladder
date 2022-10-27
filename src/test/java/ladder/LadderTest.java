b7b59f4850afe4aec0a1648ef122e5ee83d28423
gumsu <chavi9@naver.com>
2022-10-27 14:54:18 +0900

    test: 사다리에 겹치는 라인이 있는지 테스트 추가

--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -2,6 +2,8 @@ package ladder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 
 class LadderTest {
@@ -10,5 +12,15 @@ class LadderTest {
     void create() {
         Ladder ladder = Ladder.of(4,5);
         assertThat(ladder.getLadderLines().size()).isEqualTo(5);
+    }
+
+    @Test
+    void 사다리_모양_검증() {
+        Ladder ladder = Ladder.of(4, 5);
+        List<Row> rows = ladder.getLadderLines();
+
+        assertThat(IntStream.range(1, rows.size())
+            .allMatch(index -> rows.get(index) != rows.get(index - 1)))
+            .isTrue();
     }
 }
