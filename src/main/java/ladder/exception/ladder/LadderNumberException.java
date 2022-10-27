d5ae599e31aac4ced1ec6b2be30e831400bb210a
Yunsik-Choi <choiys0311@gmail.com>
2022-10-27 16:34:14 +0900

    ♻️ refactor : 사용허지 않는 Exception 제거

--- src/main/java/ladder/exception/ladder/LadderNumberException.java
+++
@@ -1,9 +0,0 @@
-package ladder.exception.ladder;
-
-import ladder.exception.LadderException;
-
-public class LadderNumberException extends LadderException {
-    public LadderNumberException(int type) {
-        super("사다리 타입 " + type + "이 존재하지 않습니다.");
-    }
-}
