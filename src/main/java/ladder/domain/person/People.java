cbf1654bcf780179d9403b44a8cfaaa613f4c7e5
Yunsik-Choi <choiys0311@gmail.com>
2022-10-27 15:59:08 +0900

    ⚡feat : People 최소값 검증로직 추가

--- src/main/java/ladder/domain/person/People.java
+++ src/main/java/ladder/domain/person/People.java
@@ -1,5 +1,7 @@
 package ladder.domain.person;
 
+import ladder.exception.person.PeopleSizeException;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,8 +9,12 @@ import java.util.stream.Collectors;
 public class People {
 
     private final List<Person> people;
+    private static final int PEOPLE_MIN = 2;
 
-    public People(String[] strings) {
+    public People(String... strings) {
+        if (strings.length < PEOPLE_MIN) {
+            throw new PeopleSizeException();
+        }
         this.people = Arrays.stream(strings)
                 .map(Person::new)
                 .collect(Collectors.toList());
