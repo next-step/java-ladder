19d7a8cfae9d5af30de78870f334ee6d667f4681
Yunsik-Choi <choiys0311@gmail.com>
2022-10-28 14:36:16 +0900

    ⚡feat : LadderGameCreateOutputView inputPlayResult

--- src/main/java/ladder/view/ViewConst.java
+++ src/main/java/ladder/view/ViewConst.java
@@ -4,5 +4,6 @@ public class ViewConst {
 
     public static final String PERSON_NAME_DELIMITER = " ";
     public static String PERSON_NAME_SEPARATOR = ",";
+    public static String INPUT_RESULT_SEPARATOR = ",";
     public static final int PERSON_NAME_LENGTH_MAX = 5;
 }
--- src/main/java/ladder/view/output/LadderGameCreateOutputView.java
+++ src/main/java/ladder/view/output/LadderGameCreateOutputView.java
@@ -8,11 +8,15 @@ import static ladder.view.ViewConst.*;
 public class LadderGameCreateOutputView {
 
     public static void inputPeople() {
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(" + PERSON_NAME_SEPARATOR + ")로 구분하세요)");
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표("+ PERSON_NAME_SEPARATOR+")로 구분하세요)");
     }
 
     public static void inputPeopleException() {
         System.out.println("사람 이름 입력이 잘못됐습니다 다시 이력해주세요.");
+    }
+
+    public static void inputPlayResult() {
+        System.out.println("실행 결과를 입력하세요. (결과는 쉼표("+ INPUT_RESULT_SEPARATOR+")로 구분하세요)");
     }
 
     public static void inputLadderHeight() {
