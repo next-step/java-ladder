ade05fa4a5840e2c8c4cdb75a86e3a1d4b5e8cde
Yunsik-Choi <choiys0311@gmail.com>
2022-10-28 14:58:11 +0900

    ⚡feat : LadderGameCreateOutputView outputResults

--- src/main/java/ladder/view/ViewConst.java
+++ src/main/java/ladder/view/ViewConst.java
@@ -3,6 +3,7 @@ package ladder.view;
 public class ViewConst {
 
     public static final String PERSON_NAME_DELIMITER = " ";
+    public static final String RESULT_DELIMITER = " ";
     public static String PERSON_NAME_SEPARATOR = ",";
     public static String INPUT_RESULT_SEPARATOR = ",";
     public static final int PERSON_NAME_LENGTH_MAX = 5;
