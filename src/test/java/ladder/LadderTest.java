5dac0f663ae8460daf219d157168acd90e069317
gumsu <chavi9@naver.com>
2022-10-29 00:31:50 +0900

    refactor: row 연속 라인 생성 여부 테스트 이동

--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -2,8 +2,6 @@ package ladder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
-import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 
 class LadderTest {
@@ -12,15 +10,5 @@ class LadderTest {
     void create() {
         Ladder ladder = Ladder.of(4,5);
         assertThat(ladder.getLadderLines().size()).isEqualTo(5);
-    }
-
-    @Test
-    void 사다리_모양_검증() {
-        Ladder ladder = Ladder.of(4, 5);
-        List<Row> rows = ladder.getLadderLines();
-
-        assertThat(IntStream.range(1, rows.size())
-            .allMatch(index -> rows.get(index) != rows.get(index - 1)))
-            .isTrue();
     }
 }
---
+++ src/test/java/ladder/RowGeneratorTest.java
@@ -0,0 +1,19 @@
+package ladder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.Test;
+
+class RowGeneratorTest {
+
+    @Test
+    void 라인_연속_생성_여부_검증() {
+        RowGenerator rowGenerator = new RowGenerator();
+        Row row = rowGenerator.create(4);
+
+        assertThat(IntStream.range(1, row.getPoints().size())
+            .noneMatch(index -> row.getPoints().get(index) && row.getPoints().get(index - 1)))
+            .isTrue();
+    }
+}
