1d8420a6a89bfb7f9351e59be047f4ad199c5658
Yunsik-Choi <choiys0311@gmail.com>
2022-10-28 18:54:06 +0900

    ♻️ refactor : LadderCreateResultOutputDto

--- src/main/java/ladder/dto/LadderCreateResultOutputDto.java
+++ src/main/java/ladder/dto/LadderCreateResultOutputDto.java
@@ -3,24 +3,19 @@ package ladder.dto;
 import ladder.domain.ladder.ladderline.LadderLines;
 import ladder.domain.person.People;
 import ladder.domain.result.Results;
-import ladder.util.LadderOutputConverter;
 
 public class LadderCreateResultOutputDto {
 
-    private final String people;
-    private final String ladderLines;
-    private final String results;
+    private final String resultOutput;
 
     public LadderCreateResultOutputDto(People people, LadderLines ladderLines, Results results) {
-        this.people = LadderOutputConverter.peopleName(people);
-        this.ladderLines = LadderOutputConverter.ladderLinesOutput(ladderLines);
-        this.results = LadderOutputConverter.resultOutput(results);
+        this.resultOutput = people + "\n" +
+                ladderLines + "\n" +
+                results + "\n";
     }
 
     @Override
     public String toString() {
-        return people + "\n" +
-                ladderLines + "\n" +
-                results + "\n";
+        return resultOutput;
     }
 }
