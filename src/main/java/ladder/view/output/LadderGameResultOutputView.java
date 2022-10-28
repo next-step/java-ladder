fb04f4e8a9f34352de94064df9a287b0dd5ff940
Yunsik-Choi <choiys0311@gmail.com>
2022-10-28 10:08:18 +0900

    ⚡feat : LadderGameResultOutputView resultGuide

--- src/main/java/ladder/view/output/LadderGameResultOutputView.java
+++ src/main/java/ladder/view/output/LadderGameResultOutputView.java
@@ -5,4 +5,8 @@ public class LadderGameResultOutputView {
     public void inputResultPersonName() {
         System.out.println("결과를 보고 싶은 사람은?");
     }
+
+    public void resultGuide() {
+        System.out.println("실행 결과");
+    }
 }
