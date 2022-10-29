0f7880b72e318d5f07d2aeb00b0bb4c8aee7c7e4
mjin <myeongjin1220@gmail.com>
2022-10-29 12:27:29 +0900

    [사다리타기] 1단계 - 자바8 스트림, 람다, Optional (#1542)
    * docs: README.md에 요구사항 명세
    
    * feat: 람다 실습 1 - Functional Interface Lambda
    
    * feat: 람다 실습 2 - Functional Interface Lambda
    
    * feat: 스트림 실습 1 구현
    
    * feat: 스트림 실습 2 구현
    
    * feat: Optional 요구사항 1 - Optional을 활용해 조건에 따른 반환
    
    * feat: Optional 요구사항 2 - Optional에서 값을 반환
    
    * feat: Optional 요구사항 3 - Optional에서 exception 처리

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -27,18 +27,38 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+             .filter(word -> word.length() > 12)
+             .distinct()
+             .sorted((first, second) -> {
+                 if (first.length() == second.length()) {
+                     return 0;
+                 }
+
+                 return first.length() < second.length() ? 1 : -1;
+             })
+             .collect(Collectors.toList())
+             .subList(0, 100)
+             .stream()
+             .map(String::toLowerCase)
+             .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
+        return numbers.stream()
+                      .map(x -> 2 * x)
+                      .collect(Collectors.toList());
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers.stream()
+                      .reduce(0, (x, y) -> x + y);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                      .filter(number -> number > 3)
+                      .map(number -> number * 2)
+                      .reduce(0, Integer::sum);
     }
 }
