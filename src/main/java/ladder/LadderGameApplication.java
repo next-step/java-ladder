53418860457727b40fbf75fd9a83a02c6217ccc0
Rein(임인호) <rein.withus@kakaocorp.com>
2022-10-29 19:44:08 +0900

    fix step2. UserNames 제거

--- src/main/java/ladder/LadderGameApplication.java
+++ src/main/java/ladder/LadderGameApplication.java
@@ -1,20 +1,21 @@
 package ladder;
 
+import java.util.List;
+
 import ladder.domain.Height;
 import ladder.domain.LadderGame;
-import ladder.domain.UserNames;
 import ladder.domain.Users;
 import ladder.view.ConsoleView;
 import ladder.view.UserInput;
 
 public class LadderGameApplication {
     private static final int MAX_NAME_SIZE = 5;
-    
+
     public static void main(String[] args) {
         UserInput input = new UserInput();
-        UserNames names = new UserNames(input.getUserNames(MAX_NAME_SIZE));
-        LadderGame game = new LadderGame(new Users(names), new Height(input.getHeight()));
+        List<String> names = input.getUserNames(MAX_NAME_SIZE);
+        LadderGame game = new LadderGame(Users.getInstance(names), new Height(input.getHeight()));
 
-        new ConsoleView(names.getAll(), game.execute().lines(), MAX_NAME_SIZE).show();
+        new ConsoleView(names, game.execute().lines(), MAX_NAME_SIZE).show();
     }
 }
--- src/main/java/ladder/domain/Users.java
+++ src/main/java/ladder/domain/Users.java
@@ -1,14 +1,23 @@
 package ladder.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Users {
     private final List<User> users;
 
-    public Users(UserNames names) {
-        this.users = names.toUsers();
+    public Users(List<User> users) {
+        this.users = users;
     }
 
+    public static Users getInstance(List<String> names) {
+        List<User> users = new ArrayList<>();
+        for (String name : names) {
+            users.add(new User(name));
+        }
+        return new Users(users);   
+    }
+    
     public int count() {
         return users.size();
     }
