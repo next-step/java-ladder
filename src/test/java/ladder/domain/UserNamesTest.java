d0f76f075434a001a4da04a9fe3d9588023732d0
Rein(임인호) <rein.withus@kakaocorp.com>
2022-10-29 19:39:34 +0900

    fix step2. User 의 equals 제거

--- src/test/java/ladder/domain/UserNamesTest.java
+++ src/test/java/ladder/domain/UserNamesTest.java
@@ -3,6 +3,7 @@ package ladder.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,7 @@ class UserNamesTest {
     @DisplayName("사용자 목록을 리턴한다.")
     @Test
     void toUsers() {
-        assertThat(new UserNames(List.of("pobi", "honix")).toUsers()).hasSameElementsAs(List.of(new User("pobi"), new User("honix")));
+        assertThat(getNames(new UserNames(List.of("pobi", "honix")).toUsers())).containsExactly("pobi", "honix");
     }
 
     @DisplayName("전체 사용자의 이름 목록을 리턴한다.")
@@ -19,5 +20,9 @@ class UserNamesTest {
     void getAll() {
         List<String> names = List.of("pobi", "honix");
         assertThat(new UserNames(names).getAll()).hasSameElementsAs(names);
+    }
+    
+    private List<String> getNames(List<User> users) {
+        return users.stream().map(user -> user.getName()).collect(Collectors.toList());
     }
 }
