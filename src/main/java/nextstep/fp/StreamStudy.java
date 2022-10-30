3db09a31d0e70d6379350ef154a5bb35d76419ba
jimbaemon <jimbae@kakao.com>
2022-10-30 23:32:16 +0900

    실습 2 printLongestWordTop100() 메서드를 구현

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,7 +5,9 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
@@ -27,7 +29,15 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        List<String> collect = words.stream()
+            .distinct()
+            .filter(word -> word.length() > 12)
+            .map(word -> word.toLowerCase(Locale.ROOT))
+            .sorted(Comparator.comparingInt(String::length))
+            .limit(100)
+            .collect(Collectors.toList());
+
+        System.out.println(collect);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
