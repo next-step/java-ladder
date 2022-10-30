b8bf300852f88ce1f72839f7df42efd714d9cc58
o3ozzvb <ou1112@naver.com>
2022-10-30 21:44:50 +0900

    feat : 사다리 타기 구현 (결과 인덱스 구하기)

--- src/main/java/ladder/domain/Point.java
+++ src/main/java/ladder/domain/Point.java
@@ -1,19 +1,23 @@
 package ladder.domain;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class Point {
 
     private final boolean exist;
 
+    public static Point of(boolean value) {
+        return new Point(value);
+    }
+
     public Point() {
-        this(new Random().nextBoolean());
+        this.exist = new Random().nextBoolean();
     }
 
     public Point(boolean exist) {
         this.exist = exist;
     }
-
     public boolean isExist() {
         return exist;
     }
@@ -23,5 +27,18 @@ public class Point {
             return new Point(false);
         }
         return new Point();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return exist == point.exist;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(exist);
     }
 }
--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -2,6 +2,9 @@ package ladder.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,7 +12,33 @@ class LadderTest {
 
     @Test
     public void Ladder_생성() {
-        Ladder ladder = new Ladder(4, 5);
+        Ladder ladder = Ladder.of(4, 5);
         assertThat(ladder.getLines().size()).isEqualTo(5);
+    }
+
+    @Test
+    public void 사다리_결과_인덱스() {
+        List<Point> points = new ArrayList<>();
+        points.add(new Point(true));
+        points.add(new Point(false));
+        points.add(new Point(true));
+
+        List<Point> points2 = new ArrayList<>();
+        points2.add(new Point(false));
+        points2.add(new Point(true));
+        points2.add(new Point(false));
+
+        List<Line> lines = new ArrayList<>();
+        lines.add(new Line(points));
+        lines.add(new Line(points2));
+        lines.add(new Line(points));
+        lines.add(new Line(points2));
+        lines.add(new Line(points));
+
+        Ladder ladder = new Ladder(lines);
+        assertThat(ladder.getLadderEndIdx(0)).isEqualTo(2);
+        assertThat(ladder.getLadderEndIdx(1)).isEqualTo(3);
+        assertThat(ladder.getLadderEndIdx(2)).isEqualTo(0);
+        assertThat(ladder.getLadderEndIdx(3)).isEqualTo(1);
     }
 }
