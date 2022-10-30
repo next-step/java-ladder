2cbcc132856a424b8bf91a335a64b2a630cb5543
Sungjun Hwang <opr99@naver.com>
2022-10-30 16:40:05 +0900

    1단계 - 자바8 스트림, 람다, Optional (#1544)
    * feat: 익명 클래스 람다로 전환
    
    * feat: 람다를 활용해 중복 제거
    
    * feat: 다양한 stream method 실습
    
    * feat: Optional 실습

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -27,7 +27,13 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(w -> w.length() > 12)
+                .sorted((o1, o2) -> o2.length() - o1.length())
+                .limit(100)
+                .distinct()
+                .map(String::toLowerCase)
+                .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +45,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(n -> n > 3)
+                .map(n -> n * 2)
+                .reduce(0, Integer::sum);
     }
 }
