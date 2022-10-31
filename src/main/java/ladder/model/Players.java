33cb99e4fb61e56c501cb934b9e512fb1bc0f197
bella.n <bella.n@kakaocorp.com>
2022-10-31 11:08:53 +0900

    refactor: 정적 팩토리 메서드 생성

--- src/main/java/ladder/model/Players.java
+++ src/main/java/ladder/model/Players.java
@@ -8,10 +8,16 @@ public class Players {
 
     private List<Player> players;
 
-    public Players(String[] playersName) {
-        this.players = Arrays.stream(playersName)
+    public Players(List<Player> players) {
+        this.players = players;
+    }
+
+    public static Players of(String[] playersName) {
+        List<Player> players = Arrays.stream(playersName)
                 .map(Player::new)
                 .collect(Collectors.toList());
+
+        return new Players(players);
     }
 
     public List<Player> getPlayers() {
