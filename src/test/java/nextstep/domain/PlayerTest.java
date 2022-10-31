56aeb88fc43bd07229ef1c960288b077c49f761a
caoyu-dev <choyumin01@gmail.com>
2022-11-01 03:24:41 +0900

    [feat] PlayerTest 생성

--- src/test/java/nextstep/domain/PlayerTest.java
+++ src/test/java/nextstep/domain/PlayerTest.java
@@ -1,4 +1,14 @@
-import static org.junit.jupiter.api.Assertions.*;
+package nextstep.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class PlayerTest {
-  
+
+    @Test
+    void create_More_Than_Five_Letters_Name() {
+        assertThatThrownBy(() -> new Player("abcdef"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
