bbe5442d67e71b6a43fd12e54531eb24eca5f351
caoyu-dev <choyumin01@gmail.com>
2022-11-01 03:27:56 +0900

    [feat] main 클래스 추가 및 view 추가

--- src/main/java/nextstep/LadderApplication.java
+++ src/main/java/nextstep/LadderApplication.java
@@ -1,2 +1,18 @@
-package nextstep;public class LadderApplication {
+package nextstep;
+
+import nextstep.domain.Ladder;
+import nextstep.domain.Players;
+import nextstep.strategy.RandomLineStrategy;
+import nextstep.view.Input;
+import nextstep.view.Output;
+
+public class LadderApplication {
+    public static void main(String[] args) {
+        Players playerNames = Input.getUserName();
+        int height = Input.getHeight();
+
+        Ladder ladder = new Ladder(height, playerNames.getPlayers().size(), new RandomLineStrategy());
+        Output.printName(playerNames.getPlayers());
+        Output.printLadder(ladder);
+    }
 }
--- src/main/java/nextstep/view/Output.java
+++ src/main/java/nextstep/view/Output.java
@@ -1,2 +1,45 @@
-package nextstep.view;public class Output {
+package nextstep.view;
+
+import nextstep.domain.Ladder;
+import nextstep.domain.Line;
+import nextstep.domain.Player;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Output {
+    public static void printName(List<Player> players) {
+        StringBuilder sb = new StringBuilder();
+
+        String result = players.stream()
+                .map(Player::getName)
+                .collect(Collectors.joining("  "));
+
+        sb.append(String.format("\n실행결과\n %s", result));
+        System.out.println(sb);
+    }
+
+    public static void printLadder(Ladder ladder) {
+        StringBuilder sb = new StringBuilder();
+
+        ladder.getLines().stream()
+                .forEachOrdered(line -> getLine(sb, line));
+
+        System.out.println(sb);
+    }
+
+    private static void getLine(StringBuilder sb, Line line) {
+        sb.append("    ");
+        sb.append("|");
+        line.getPoints().stream()
+                .forEachOrdered(idx -> sb.append(print(idx)).append("|"));
+        sb.append("\n");
+    }
+
+    private static String print(Boolean idx) {
+        if (idx == true) {
+            return "-----";
+        }
+        return "     ";
+    }
 }
