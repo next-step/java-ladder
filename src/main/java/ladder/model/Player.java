7538b3e6e83b72c0837780efd16ba48d357e9752
bella.n <bella.n@kakaocorp.com>
2022-10-31 11:09:08 +0900

    refactor: optional 제거

--- src/main/java/ladder/model/Player.java
+++ src/main/java/ladder/model/Player.java
@@ -2,8 +2,6 @@ package ladder.model;
 
 import ladder.exception.WrongParameterException;
 
-import java.util.Optional;
-
 public class Player {
 
     private static final int MAX_NAME_LENGTH = 5;
@@ -11,9 +9,11 @@ public class Player {
     private String name;
 
     public Player(String name) {
-        this.name = Optional.of(name)
-                .filter(value -> value.length() <= MAX_NAME_LENGTH)
-                .orElseThrow(() -> new WrongParameterException("[입력 오류] 플레이어의 이름은 최대 5자 입니다."));
+        this.name = name;
+
+        if (this.name.length() > MAX_NAME_LENGTH) {
+            throw new WrongParameterException("[입력 오류] 플레이어의 이름은 최대 5자 입니다.");
+        }
     }
 
     public String getName() {
