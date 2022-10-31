dfc05baa525dd92803da775140a4c14765d80fa1
caoyu-dev <choyumin01@gmail.com>
2022-11-01 03:26:55 +0900

    [feat] Line 및 LineTest 생성

--- src/main/java/nextstep/domain/Line.java
+++ src/main/java/nextstep/domain/Line.java
@@ -1,2 +1,54 @@
-package nextstep.domain;public class Line {
+package nextstep.domain;
+
+import nextstep.strategy.LineStrategy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Line {
+    private List<Boolean> points = new ArrayList<>();
+    private LineStrategy lineStrategy;
+
+    public Line(int countPerson, LineStrategy lineStrategy) {
+        if (countPerson < 2) {
+            throw new IllegalArgumentException("2명 이상 필요하다.");
+        }
+        this.lineStrategy = lineStrategy;
+
+        points = of(countPerson, lineStrategy);
+    }
+
+    public List<Boolean> getPoints() {
+        return Collections.unmodifiableList(points);
+    }
+
+    public List<Boolean> of(int count, LineStrategy lineStrategy) {
+        for (int i = 0; i < count - 1; i++) {
+            points.add(false);
+        }
+
+        for (int point = 0; point < points.size(); point++) {
+            points.set(point, setStrategy(point, lineStrategy));
+        }
+
+        return points;
+    }
+
+    private Boolean setStrategy(int point, LineStrategy lineStrategy) {
+        if (hasNotStrategy(point) == false) {
+            return lineStrategy.generate();
+        }
+        return false;
+    }
+
+    private boolean hasNotStrategy(int point) {
+        if (point < 0 || point > points.size() + 1) {
+            throw new IllegalArgumentException("해당 point 에 line 을 놓을 수 없다.");
+        }
+
+        // 양사이드 true 체크
+        return point > 0 && points.get(point - 1).equals(true)
+                || point < points.size() - 1&& points.get(point + 1).equals(true);
+    }
 }
--- src/test/java/nextstep/domain/LineTest.java
+++ src/test/java/nextstep/domain/LineTest.java
@@ -1,4 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
+package nextstep.domain;
+
+import nextstep.strategy.RandomLineStrategy;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class LineTest {
-  
+
+    @Test
+    void create_line() {
+        Line line = new Line(5, new RandomLineStrategy());
+        assertThat(line.getPoints().size()).isEqualTo(4);
+    }
+
+    @Test
+    void create_line_with_true_strategy() {
+        Line line = new Line(5, () -> true);
+        List<Boolean> points = line.getPoints();
+        assertThat(points).isEqualTo(List.of(true, false, true, false));
+    }
 }
