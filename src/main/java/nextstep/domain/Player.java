d32ecccdc2f897ef08428bdbdf6cbb9905e5d20b
caoyu-dev <choyumin01@gmail.com>
2022-11-01 03:23:57 +0900

    [feat] Player 및 Players 생성

--- src/main/java/nextstep/domain/Player.java
+++ src/main/java/nextstep/domain/Player.java
@@ -1,2 +1,20 @@
-package nextstep.domain;public class Player {
+package nextstep.domain;
+
+public class Player {
+    public final String name;
+
+    public Player(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 4자 이하로 짧게 써야 한다.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
 }
