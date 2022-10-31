cbc84a6a489f35d8fa1449c28f9d04428c8e6d58
suchan <aud248@naver.com>
2022-10-31 16:19:41 +0900

    test: 한 라인의 point 하나 테스트

--- src/test/java/nextstep/step2/domain/RandomLineGeneratorTest.java
+++ src/test/java/nextstep/step2/domain/RandomLineGeneratorTest.java
@@ -5,14 +5,32 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 class RandomLineGeneratorTest {
 
     @DisplayName("마지막 위치의 Point 연결은 무조건 false 반환")
     @ParameterizedTest()
     @ValueSource(ints = {3, 5, 9, 13})
-    void test(int position){
+    void last_point_always_false(int position){
         RandomLineGenerator randomLineGenerator = RandomLineGenerator.from(position);
         boolean randomBoolean = randomLineGenerator.createRandomBoolean(position);
         Assertions.assertThat(randomBoolean).isFalse();
+    }
+
+    @DisplayName("한 라인의 point 는 무조건 하나")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 5, 9, 13})
+    void one_line_point_always_one(int position) {
+        RandomLineGenerator randomLineGenerator = RandomLineGenerator.from(position);
+        List<Boolean> points = IntStream.rangeClosed(1, position)
+                .mapToObj(i -> randomLineGenerator.createRandomBoolean(i))
+                .collect(Collectors.toList());
+        int countOfPoint = (int) points.stream()
+                .filter(point -> point == true)
+                .count();
+        Assertions.assertThat(countOfPoint).isEqualTo(1);
     }
 }
