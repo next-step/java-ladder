4f9132f13349ff56cb394b61d5725951cdf93189
Cheeze <cheeze.ball@kakaocorp.com>
2022-10-31 20:42:18 +0900

    refact() : test케이스 추가

--- src/test/java/nextstep/ladder/UserTest.java
+++ src/test/java/nextstep/ladder/UserTest.java
@@ -2,6 +2,7 @@ package nextstep.ladder;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,8 +10,17 @@ import nextstep.ladder.domain.User;
 
 public class UserTest {
 
+    @DisplayName("이름 길이가 5자 이상이면 예외를 발생시키는 테스트")
     @ParameterizedTest
-    @ValueSource(strings = { "abcde, ddddd" })
+    @ValueSource(strings = { "abcde", "ddddd" })
+    void user_name_length_validate_test(String name) {
+        assertThatThrownBy(() -> new User(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이름은 5자 미만이어야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "a", "ddddd", "테스트" })
     void user_name_length_test(String name) {
         assertThatThrownBy(() -> new User(name))
                 .isInstanceOf(IllegalArgumentException.class)
