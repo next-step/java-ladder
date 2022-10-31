58d97f290a5b25eef78968cc1edecfa097365cec
caoyu-dev <choyumin01@gmail.com>
2022-11-01 03:26:04 +0900

    [feat] Players 생성

--- src/main/java/nextstep/domain/Players.java
+++ src/main/java/nextstep/domain/Players.java
@@ -1,2 +1,18 @@
-package nextstep.domain;public class Players {
+package nextstep.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Players {
+    private final List<Player> players;
+
+    public Players(List<String> players) {
+        this.players = players.stream()
+                .map(Player::new)
+                .collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
 }
