2b17e629d2b1863a38b9603fb0427fb2cda86999
seungwoo.song <seungwoo.song@nhnpayco.com>
2022-11-01 20:42:07 +0900

    docs: 문서수정 feat(Direction.java): 이동방향을 결정하는 기능 추가

---
+++ src/main/java/ladder/domain/Direction.java
@@ -0,0 +1,45 @@
+package ladder.domain;
+
+import javax.print.attribute.standard.ReferenceUriSchemesSupported;
+import java.util.stream.Stream;
+
+/**
+ * Created by seungwoo.song on 2022-11-01
+ */
+public enum Direction {
+
+	LEFT(true, false),
+	RIGHT(false, true),
+	NONE(false, false);
+
+	private final boolean before;
+	private final boolean current;
+
+	Direction(boolean before, boolean current) {
+		this.before = before;
+		this.current = current;
+	}
+
+	public static Direction of(boolean before, boolean current) {
+		if (before && current) {
+			throw new IllegalArgumentException("left right 모두 true 일 수 없습니다");
+		}
+
+		return Stream.of(values())
+			.filter(e -> e.before == before && e.current == current)
+			.findAny()
+			.get();
+	}
+
+	public static Direction first(boolean current) {
+		return of(false, current);
+	}
+
+	public Direction last() {
+		return of(current, false);
+	}
+
+	public Direction next(boolean next) {
+		return Direction.of(current, next);
+	}
+}
