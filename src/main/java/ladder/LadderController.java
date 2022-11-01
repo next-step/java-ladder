f8d702b0979c85a4437454738a967a2fbaa8bb99
Yunsik-Choi <choiys0311@gmail.com>
2022-11-01 22:40:28 +0900

    ♻️ refactor : LadderController 입력 예외처리 로직 제거

--- src/main/java/ladder/LadderController.java
+++ src/main/java/ladder/LadderController.java
@@ -1,6 +1,5 @@
 package ladder;
 
-import ladder.domain.LadderTextInput;
 import ladder.domain.person.Person;
 import ladder.domain.person.SearchPeopleNames;
 import ladder.domain.Result;
@@ -16,10 +15,7 @@ import ladder.util.LadderOutputConverter;
 import ladder.view.InputView;
 import ladder.view.OutputView;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-
 
 public class LadderController {
 
@@ -32,57 +28,18 @@ public class LadderController {
     }
 
     public void gameStart() {
-        People people = inputPeople();
+        People people = new People(InputView.personNameInput());
 
-        Rewards rewards = inputPlayResult();
+        Rewards rewards = new Rewards(InputView.splitResult());
 
-        LadderLines ladderLines = ladderGameCreateService.createLadderLine(new LadderWidth(people.number()), inputHeight());
+        LadderLines ladderLines = ladderGameCreateService.createLadderLine(new LadderWidth(people.number()), new LadderHeight(InputView.ladderHeight()));
 
         OutputView.ladderCreateResult(people, ladderLines, rewards);
 
-        List<Person> resultPersonList = ladderGameResultService.resultPersonList(people, inputResultPersonName());
+        List<Person> resultPersonList = ladderGameResultService.resultPersonList(people, new SearchPeopleNames(InputView.inputResultPersonName()));
 
         Result result = ladderGameResultService.ladderGameResult(new LadderGameResultDto(rewards, ladderLines), resultPersonList);
 
         OutputView.gameResult(LadderOutputConverter.resultOutput(result));
-    }
-
-
-    private People inputPeople() {
-        try {
-            return new People(InputView.personNameInput());
-        } catch (Exception e) {
-            System.out.println("사람 이름 입력이 잘못됐습니다 다시 이력해주세요.");
-        }
-        return inputPeople();
-    }
-
-    private Rewards inputPlayResult() {
-        try {
-            return new Rewards(InputView.splitResult());
-        } catch (Exception e) {
-            System.out.println("사다리 게임 실행 결과 입력이 잘못됐습니다 다시 입력해주세요.");
-        }
-        return inputPlayResult();
-    }
-
-    private static LadderHeight inputHeight() {
-        try {
-            return new LadderHeight(InputView.ladderHeight());
-        } catch (Exception e) {
-            System.out.println("사다리 높이 입력이 잘못됐습니다 다시 입력해주세요.");
-        }
-        return inputHeight();
-    }
-
-    private static SearchPeopleNames inputResultPersonName() {
-        try {
-            return new SearchPeopleNames(Arrays.stream(InputView.inputResultPersonName())
-                    .map(LadderTextInput::new)
-                    .collect(Collectors.toList()));
-        } catch (Exception e) {
-            System.out.println("결과를 보고 싶은 사람 이름 입력이 잘못됐습니다 다시 입력해주세요");
-        }
-        return inputResultPersonName();
     }
 }
