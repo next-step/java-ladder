25e3d31973cdfcab96aa5b1d807fe62a78a405ca
ChaeHyeon <86863306+cohys7@users.noreply.github.com>
2022-11-01 21:27:08 +0900

    1단계 - 자바8 스트림, 람다, Optional (#1554)
    * 람다 실습 1: 익명 클래스를 람다로 전환
    
    * 람다 실습 2: 람다를 활용해 중복 제거
    
    * 스트림 실습: 실습 전 리팩토링
    
    - filter 적용
    - Files.readString 사용
    - method interface로 수정
    
    * 스트림 실습 1: sumOverThreeAndDouble 구현
    
    - 모든 숫자 중 3보다 큰 숫자를 추출하여
    - 2배 하고
    - 전체 합 구하기
    
    * 스트림 실습 2: printLongestWordTop100 구현
    
    - 단어의 길이가 12자를 초과하는
    - 서로 다른 단어 중
    - 길이가 긴 순서로 100개를 추출해서
    - 소문자로 출력
    
    * optional 실습 1: ageIsInRange2 구현
    
    - 30살 이상, 45살 이하에 해당하는 User가 존재하는 경우 true를 반환
    - optional, filter 활용
    
    * optional 실습 2: getUser 수정
    
    - optional과 stream 사용
    
    * optional 실습 3: of 메서드 수정
    
    - optional과 stream 사용

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,40 +5,57 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
 
     public static long countWords() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+        String contents = Files.readString(Paths.get("src/main/resources/fp/war-and-peace.txt"));
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream()
+                .filter(word -> word.length() > 12)
+                .count();
     }
 
     public static void printLongestWordTop100() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+        String contents = Files.readString(Paths.get("src/main/resources/fp/war-and-peace.txt"));
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(word -> word.length() > 12)
+                .distinct()
+                .sorted(Comparator.comparing(String::length).reversed())
+                .limit(100)
+                .map(String::toLowerCase)
+                .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
+        return numbers.stream()
+                .map(x -> 2 * x)
+                .collect(Collectors.toList());
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers.stream()
+                .reduce(0, Integer::sum);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return sumAll(doubleNumbers(filterOverThree(numbers)));
+    }
+
+    private static List<Integer> filterOverThree(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(StreamStudy::isOverThree)
+                .collect(Collectors.toList());
+    }
+
+    private static boolean isOverThree(Integer number) {
+        return number > 3;
     }
 }
