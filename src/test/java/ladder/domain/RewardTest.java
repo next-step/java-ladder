f6b382f880b445f44a01f63906fbf6cc92fcb34b
Yunsik-Choi <choiys0311@gmail.com>
2022-11-02 15:06:25 +0900

    ⚡feat : Reward 추가

---
+++ src/test/java/ladder/domain/RewardTest.java
@@ -0,0 +1,15 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class RewardTest {
+
+    @Test
+    void create() {
+        Reward reward = new Reward("꽝");
+
+        assertThat(reward).isEqualTo(new Reward("꽝"));
+    }
+}
