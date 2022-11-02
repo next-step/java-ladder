c0d835b1496fd458266d84a3df53b9fb684ddec7
caoyu-dev <choyumin01@gmail.com>
2022-11-03 01:18:52 +0900

    [feat] 일급 객체 `Name` 분리

--- src/main/java/nextstep/domain/Player.java
+++ src/main/java/nextstep/domain/Player.java
@@ -1,21 +1,14 @@
 package nextstep.domain;
 
 public class Player {
-    private static final int MINIMUM_NAME_LENGTH = 5;
-    public final String name;
+
+    public final Name name;
 
     public Player(String name) {
-        validate(name);
-        this.name = name;
-    }
-
-    private void validate(String name) {
-        if (name.length() > MINIMUM_NAME_LENGTH) {
-            throw new IllegalArgumentException("이름은 최소 5자 이하로 짧게 쓰여져야 한다.");
-        }
+        this.name = new Name(name);
     }
 
     public String getName() {
-        return name;
+        return name.getName();
     }
 }
