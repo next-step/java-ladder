2606d4d64c387e4cd1c3bc0c889ec0414f709220
nhs219 <heesun_z@naver.com>
2022-11-02 10:33:07 +0900

    refactor : 복수형이라 people로 변경 및 private 선언

--- src/main/java/nextstep/ladder/Person.java
+++ src/main/java/nextstep/ladder/Person.java
@@ -6,27 +6,27 @@ import java.util.Objects;
 
 public class Person {
 
-    List<String> person = new ArrayList<>();
+    private List<String> people = new ArrayList<>();
 
     public Person(String[] people) {
         for (String person : people) {
             checkDuplicate(person);
-            this.person.add(person);
+            this.people.add(person);
         }
     }
 
     private void checkDuplicate(String person) {
-        if (this.person.contains(person)) {
+        if (this.people.contains(person)) {
             throw new IllegalArgumentException("이름은 중복으로 입력 불가합니다.");
         }
     }
 
     public int countPerson() {
-        return person.size();
+        return people.size();
     }
 
     public List<String> getPerson() {
-        return person;
+        return people;
     }
 
     @Override
@@ -34,11 +34,11 @@ public class Person {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Person person1 = (Person) o;
-        return Objects.equals(person, person1.person);
+        return Objects.equals(people, person1.people);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(person);
+        return Objects.hash(people);
     }
 }
