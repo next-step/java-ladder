feb33738aeed974e823d603c771fd1272c0cb5c0
heoseungyeon <dia0312@naver.com>
2022-11-04 06:48:42 +0900

    chore: printLongestWordTop100 불필요한 줄 제거

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -27,7 +27,7 @@ public class StreamStudy {
         String contents = new String(Files.readAllBytes(Paths
             .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
-        // TODO 이 부분에 구현한다.
+
         words.stream()
             .filter(word -> word.length() > 12)
             .sorted()
@@ -35,7 +35,6 @@ public class StreamStudy {
             .limit(100)
             .map(word -> word.toLowerCase())
             .forEach(System.out::println);
-
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
