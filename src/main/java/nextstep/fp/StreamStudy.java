1ec7db3decc787d846581134c214d6c130a8233e
allstarkenny <kyunghyun96@naver.com>
2022-11-03 11:23:19 +0900

    feat: add stream step

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
@@ -26,7 +27,7 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream().filter(w -> w.length() > 12).sorted().distinct().map(w -> w.toLowerCase()).limit(100).forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -38,6 +39,12 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+
+        return numbers
+                .stream()
+                .filter(x -> x > 3)
+                .map(x -> 2 * x)
+                .mapToInt(i -> i)
+                .sum();
     }
 }
