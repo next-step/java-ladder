48cd172cca426782aeee40c681554f84bd954ce2
celine.kk <celine.kk@kakaocorp.com>
2022-11-03 13:05:14 +0900

    feat. ladderCounts 변수 추가

--- src/main/java/step3/domain/Ladders.java
+++ src/main/java/step3/domain/Ladders.java
@@ -6,12 +6,14 @@ public class Ladders {
     private static final String HEIGHT_EXCEPTION = "사다리의 높이는 1이상이어야 합니다.";
 
     private final int height;
+    private final int ladderCounts;
     private final List<Lines> levels;
 
     public Ladders(int height, int userCounts, LineConditional conditional) {
         validateHeight(height);
         this.height = height;
-        this.levels = createLadderLevels(userCounts, conditional);
+        this.ladderCounts = userCounts - 1;
+        this.levels = createLadderLevels(conditional);
     }
 
     private void validateHeight(int height) {
@@ -20,17 +22,17 @@ public class Ladders {
         }
     }
 
-    private List<Lines> createLadderLevels(int userCounts, LineConditional conditional) {
+    private List<Lines> createLadderLevels(LineConditional conditional) {
         List<Lines> lines = new ArrayList<>();
         for (int i = 0; i < this.height; i++) {
-            lines.add(new Lines(createLevelLines(userCounts, conditional)));
+            lines.add(new Lines(createLevelLines(conditional)));
         }
         return lines;
     }
 
-    private List<Boolean> createLevelLines(int userCounts, LineConditional conditional) {
+    private List<Boolean> createLevelLines(LineConditional conditional) {
         List<Boolean> lines = new ArrayList<>();
-        for (int i = 0; i < userCounts - 1; i++) {
+        for (int i = 0; i < this.ladderCounts; i++) {
             lines.add(canHaveLine(lines, i, conditional));
         }
         return lines;
