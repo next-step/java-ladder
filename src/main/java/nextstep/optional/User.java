ec41c80df023a40f64acf49aac9abd70ec3c3622
Jiwoon Hong <jacksonhong0111@gmail.com>
2022-11-03 09:37:00 +0900

    Step1 리뷰 요청 (#1575)
    * [feat] lambda practice 1
    
    * [feat] lambda practice 2
    
    * [feat] stream practice
    
    * [feat] optional practice 1
    
    * [feat] optional practice 2, 3

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,8 @@
 package nextstep.optional;
 
+import java.util.Objects;
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,7 +36,13 @@ public class User {
     }
 
     public static boolean ageIsInRange2(User user) {
-        return false;
+        Optional<User> optionalUser = Optional.ofNullable(user);
+        return optionalUser
+                .filter(user1 -> Objects.nonNull(user1.age))
+                .map(user1 -> user1.age)
+                .filter(age -> age >= 30)
+                .filter(age -> age <= 45)
+                .isPresent();
     }
 
     @Override
