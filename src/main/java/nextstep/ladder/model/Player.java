df26b5a169ba66a34654c30526ee4391128908d3
jimbaemon <jimbae@kakao.com>
2022-11-04 21:04:15 +0900

    feat: 참여자의 이름은 5자를 넘지 않는다.

---
+++ src/main/java/nextstep/ladder/model/Player.java
@@ -0,0 +1,16 @@
+package nextstep.ladder.model;
+
+public class Player {
+	private final String name;
+
+	public Player(final String name) {
+		validate(name);
+		this.name = name;
+	}
+
+	private void validate(final String name) {
+		if (name.length() > 5) {
+			throw new IllegalArgumentException("사용자의 이름은 5자를 넘을 수 없습니다");
+		}
+	}
+}
---
+++ src/test/java/nextstep/ladder/PlayerTest.java
@@ -0,0 +1,18 @@
+package nextstep.ladder;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import nextstep.ladder.model.Player;
+
+class PlayerTest {
+
+	@Test
+	void 사용자의_이름은_5자를_넘길수_없다() {
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> new Player("abcdef"))
+			.withMessage("사용자의 이름은 5자를 넘을 수 없습니다");
+	}
+
+}
