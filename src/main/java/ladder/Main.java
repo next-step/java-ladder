a3485b97c9e37addb30a7cad227ea8de69a3ceab
bella.n <bella.n@kakaocorp.com>
2022-11-05 22:07:15 +0900

    feat: 사다리 실행 결과, 리워즈 결과 show 기능 추가

--- src/main/java/ladder/Main.java
+++ src/main/java/ladder/Main.java
@@ -1,21 +1,30 @@
 package ladder;
 
-import ladder.model.Ladder;
+import ladder.model.LadderGame;
+import ladder.model.LadderRewards;
 import ladder.model.Players;
 
-import static ladder.view.InputView.scanLadderHeight;
-import static ladder.view.InputView.scanPlayersName;
+import static ladder.view.InputView.*;
 import static ladder.view.OutputView.*;
 
 public class Main {
-
     public static void main(String[] args) {
         Players players = Players.of(scanPlayersName());
 
-        Ladder ladder = new Ladder(players.getPlayersCount(), scanLadderHeight());
+        LadderGame ladderGame = new LadderGame(scanLadderResult(), players.getPlayersCount(), scanLadderHeight());
 
         showResultMessage();
         showPlayersName(players);
-        showLadder(ladder);
+        showLadder(ladderGame);
+        showReward(ladderGame.getLadderRewards());
+
+        String player = scanPlayerResult();
+
+        LadderRewards result = ladderGame.getResult();
+
+        players.findPlayerNumber(player).ifPresentOrElse(
+                number -> showResultByPlayer(result.get(number), players.getPlayer(number))
+                , () -> showResultAll(result, players)
+        );
     }
 }
