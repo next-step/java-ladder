769a9ce06eeaf16df00a65faf9e7cdac1a89e4d8
jimbaemon <jimbae@kakao.com>
2022-11-05 21:16:18 +0900

    feat: 사용자가 입력한 만큼 사다리의 층을 만든다.

---
+++ src/main/java/nextstep/ladder/model/Ladder.java
@@ -0,0 +1,21 @@
+package nextstep.ladder.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ladder {
+    private final List<Lines> lines;
+
+    public Ladder(final int numberOfFloors, final int numberOfPlayers) {
+        List<Lines> result = new ArrayList<>();
+        for (int i = 0; i < numberOfFloors; i++) {
+            result.add(new Lines(numberOfPlayers));
+        }
+        this.lines = result;
+    }
+
+
+    public int numberOfFloors() {
+        return lines.size();
+    }
+}
