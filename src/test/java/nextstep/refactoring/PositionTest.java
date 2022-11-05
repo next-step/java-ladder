d6433717ff7f2b70903c6bbf5fadc00f92c42577
yunhwan <pyh8618@gmail.com>
2022-11-06 01:25:50 +0900

    feat(Position) : 현재 위치와 Spot(다리상태)를 가지고 있는 객체
    - 현재 위치에서 다음 위치를 반환하는 메서드인 move()를 가짐

---
+++ src/test/java/nextstep/refactoring/PositionTest.java
@@ -0,0 +1,39 @@
+package nextstep.refactoring;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PositionTest {
+    private Position first;
+
+    @BeforeEach
+    void setUp() {
+        first = Position.first(true);
+    }
+
+    @Test
+    public void first() {
+        Position result = new Position(0, Spot.first(true));
+        assertThat(first).isEqualTo(result);
+    }
+
+    @Test
+    public void second() {
+        Position second = first.next(false);
+        Position result = new Position(1, Spot.first(false));
+        assertThat(second).isEqualTo(result);
+    }
+
+    @Test
+    public void last() {
+        Position last = first.last();
+
+        Spot spotFirst = Spot.first(true);
+        Spot spotLast = spotFirst.last();
+
+        Position positionLast = new Position(1, spotLast);
+        assertThat(last).isEqualTo(positionLast);
+    }
+}
