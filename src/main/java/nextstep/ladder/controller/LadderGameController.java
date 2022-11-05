49aa28c69ed0c238794d862512785ccf16b4285d
jimbaemon <jimbae@kakao.com>
2022-11-05 22:22:29 +0900

    feat: 연결.

---
+++ src/main/java/nextstep/ladder/controller/LadderGameController.java
@@ -0,0 +1,25 @@
+package nextstep.ladder.controller;
+
+import nextstep.ladder.model.Ladder;
+import nextstep.ladder.model.Lines;
+import nextstep.ladder.model.Player;
+import nextstep.ladder.model.Players;
+import nextstep.ladder.view.Input;
+import nextstep.ladder.view.Output;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class LadderGameController {
+
+    public static void main(String[] args) {
+        String[] strings = Input.inputPlayers();
+        Players players = Arrays.stream(strings)
+                .map(Player::new)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), Players::new));
+
+        Ladder ladder = new Ladder(Input.inputLadderHeights(), players.number());
+
+        Output.outputResult(ladder, players);
+    }
+}
