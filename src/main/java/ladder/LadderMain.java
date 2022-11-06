f738f3036b268cb933430262c2f8b14c763a931d
mjin1220 <myeongjin1220@gmail.com>
2022-11-06 16:39:19 +0900

    feat: InputView는 원시값을 넘기는 책임만 가지도록 변경

--- src/main/java/ladder/LadderMain.java
+++ src/main/java/ladder/LadderMain.java
@@ -2,16 +2,22 @@ package ladder;
 
 import ladder.domain.Ladder;
 import ladder.domain.LadderHeight;
+import ladder.domain.Participant;
 import ladder.domain.Participants;
 import ladder.strategy.BridgeLinesRandomStrategy;
 import ladder.view.InputView;
 import ladder.view.ResultView;
 
+import java.util.stream.Collectors;
+
 public class LadderMain {
 
     public static void main(String[] args) {
-        final Participants participants = InputView.inputParticipants();
-        final LadderHeight ladderHeight = InputView.inputLadderHeight();
+        final Participants participants = new Participants(InputView.inputParticipantNames()
+                                                                    .stream()
+                                                                    .map(Participant::new)
+                                                                    .collect(Collectors.toList()));
+        final LadderHeight ladderHeight = new LadderHeight(InputView.inputLadderHeight());
         ResultView.print(new Ladder(participants, ladderHeight, new BridgeLinesRandomStrategy()));
     }
 }
