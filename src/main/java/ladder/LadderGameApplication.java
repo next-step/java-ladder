f442ee71f23c3aa124c58013e27c16348ff19a53
Rein(임인호) <rein.withus@kakaocorp.com>
2022-11-08 00:41:42 +0900

    fix. step3 LadderGameApplication 생성자 제거

--- src/main/java/ladder/LadderGameApplication.java
+++ src/main/java/ladder/LadderGameApplication.java
@@ -19,63 +19,47 @@ public class LadderGameApplication {
     private static final int RESULT_SEARCH_COUNT = 2;
     private static final String ALL_USER_NAME = "all";
     private static final ExecutionResultConsoleView executionView = new ExecutionResultConsoleView();
-    
-    private final Users users;
-    private final Ladder ladder;
-    
-    public LadderGameApplication(final Users users, final Ladder ladder) {
-        this.users = users;
-        this.ladder = ladder;
-    }
 
     public static void main(String[] args) {
         UserInput input = new UserInput();
         Users users = Users.from(input.getUserNames());
         int height = input.getHeight();
         Results results = new Results(users.count(), input.getResult());
-
-        LadderGameApplication game = new LadderGameApplication(users, Ladder.of(users, height, results));
-        show(game, results);
-        showExecutionResult(game, input);
+        Ladder ladder = Ladder.of(users, height, results);
+        
+        showUser(users);
+        showLadder(users, ladder);
+        showResult(users, results);
+        showExecutionResult(input, ladder);
     }
     
-    private void show(Results results) {
-        showUser();
-        showLadder();
-        showResult(results);
+    private static void showUser(final Users users) {
+        new UserConsoleView(new UserDisplayer(users, displaySize(users))).show();
     }
 
-    private void showUser() {
-        new UserConsoleView(new UserDisplayer(users, displaySize())).show();
+    private static void showLadder(final Users users,  final Ladder ladder) {
+        new LineConsoleView(new LineDisplayer(ladder.lines(), displaySize(users))).show();
     }
 
-    private void showLadder() {
-        new LineConsoleView(new LineDisplayer(ladder.lines(), displaySize())).show();
+    private static void showResult(final Users users, final Results results) {
+        new ResultConsoleView(new ResultDisplayer(results.getAll(), displaySize(users))).show();
     }
 
-    private void showResult(final Results results) {
-        new ResultConsoleView(new ResultDisplayer(results.getAll(), displaySize())).show();
-    }
-
-    private int displaySize() {
+    private static int displaySize(final Users users) {
         return users.getMaxNameSize() + users.names().size();
     }
 
-    private void showExecutionResult(final String userName) {
+    private static void showExecutionResult(final UserInput input, final Ladder ladder) {
+        IntStream.range(0, RESULT_SEARCH_COUNT).forEach(count -> {
+            showExecutionResult(input.getUserName(), ladder);
+        });
+    }
+    
+    private static void showExecutionResult(final String userName, final Ladder ladder) {
         if (ALL_USER_NAME.equals(userName)) {
             executionView.show(ladder.getResult());
             return;
         }
         executionView.show(ladder.getResultBy(new User(userName)));
-    }
-    
-    private static void show(LadderGameApplication game, Results results) {
-        game.show(results);
-    }
-    
-    private static void showExecutionResult(final LadderGameApplication game, final UserInput input) {
-        IntStream.range(0, RESULT_SEARCH_COUNT).forEach(count -> {
-            game.showExecutionResult(input.getUserName());
-        });
     }
 }
