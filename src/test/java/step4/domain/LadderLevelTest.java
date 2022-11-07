fac2d41f45114af3621bd443c499582863fcec52
celine.kk <celine.kk@kakaocorp.com>
2022-11-07 16:43:26 +0900

    feat. 리뷰 반영 : 테스트 이름 수정, 테스트케이스 성공케이스로 수정

--- src/test/java/step4/domain/LadderLevelTest.java
+++ src/test/java/step4/domain/LadderLevelTest.java
@@ -2,18 +2,18 @@ package step4.domain;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import step4.domain.LadderLevel;
-import step4.domain.Line;
+import step4.view.ResultView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class LinesTest {
+class LadderLevelTest {
     @ParameterizedTest
     @CsvSource(value = {"0,1", "1,0", "2,3", "3,2"})
     void 방향을_맞게_안내해주는지(int startIndex, int expectedArrivalIndex) {
-        LadderLevel lines = new LadderLevel(List.of(new Line(true), new Line(true), new Line(true)));
+        LadderLevel lines = new LadderLevel(List.of(new Line(true), new Line(false), new Line(true)));
         assertThat(lines.getDirection(startIndex)).isEqualTo(expectedArrivalIndex);
     }
 }
