4f877fdbee52941b71aba1d8ee25414862e2587e
wugatama <dnr7410@naver.com>
2022-11-09 00:33:28 +0900

    refactor: 분리기호 상수화

--- src/main/java/ladder/Players.java
+++ src/main/java/ladder/Players.java
@@ -5,10 +5,11 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Players {
+    private static final String SEPARATOR = ",";
     private final List<Player> players;
 
     public Players(String input) {
-        List<String> playersOfString = Stream.of(input.split(",")).collect(Collectors.toList());
+        List<String> playersOfString = Stream.of(input.split(SEPARATOR)).collect(Collectors.toList());
         this.players = playersOfString.stream().map(Player::new).collect(Collectors.toUnmodifiableList());
         if (playersOfString.size() < 2) {
             throw new IllegalArgumentException("2명이상 입력해주세요");
