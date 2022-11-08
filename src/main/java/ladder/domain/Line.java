f74b6c73763108d8a95a880611eaeab6e249898e
jwoh <jwoh@wemakeprice.com>
2022-11-08 10:14:32 +0900

    refect : setter제거, 상수 변수명 변경

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -7,21 +7,21 @@ import java.util.stream.IntStream;
 
 public class Line {
 
-    private static final Random random = new Random();
+     private static final Random RANDOM = new Random();
 
     private final List<Boolean> points = new ArrayList<>();
 
     public Line(int countOfUser) {
         IntStream.range(0, countOfUser-1)
-            .forEach(index -> points.add(setPoint(index)));
+            .forEach(index -> points.add(addPoint(index)));
     }
 
     public List<Boolean> getPoints() {
         return points;
     }
 
-    private boolean setPoint(int index) {
-        boolean point = random.nextBoolean();
+    private boolean addPoint(int index) {
+        boolean point = RANDOM.nextBoolean();
 
         if (index > 0) {
             point = validPoint(point, points.get(index - 1));
