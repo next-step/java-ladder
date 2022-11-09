b6706748703dea9831b4be56371b345aea657351
karon-ma <karon.ma@kakaocorp.com>
2022-11-09 16:50:11 +0900

    feat: Add ladder height

--- src/main/java/ladder/model/LadderFactory.java
+++ src/main/java/ladder/model/LadderFactory.java
@@ -7,14 +7,8 @@ import java.util.stream.IntStream;
 public enum LadderFactory {
     INSTANCE;
 
-    private static final int MIN_HEIGHT = 2;
-
-    public Ladder generateLadder(int countOfUser, int height) {
-        if (height < MIN_HEIGHT) {
-            throw new IllegalArgumentException(String.format("사다리 높이의 최소값은 %d입니다. 입력높이: %d", MIN_HEIGHT, height));
-        }
-
-        List<Line> lines = IntStream.range(0, height)
+    public Ladder generateLadder(int countOfUser, LadderHeight height) {
+        List<Line> lines = IntStream.range(0, height.getValue())
                 .boxed()
                 .map(i -> LineFactory.INSTANCE.generateRandomLine(countOfUser))
                 .collect(Collectors.toList());
--- src/test/java/ladder/model/LadderFactoryTest.java
+++ src/test/java/ladder/model/LadderFactoryTest.java
@@ -8,14 +8,8 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 public class LadderFactoryTest {
-    @ParameterizedTest
-    @ValueSource(ints = {0, 1})
-    void testFailWhenNotEnoughHeight(int height) {
-        assertThatIllegalArgumentException().isThrownBy(() -> LadderFactory.INSTANCE.generateLadder(3, height));
-    }
-
     @Test
     void testSuccessWhenGenerateLadder() {
-        assertThatNoException().isThrownBy(() -> LadderFactory.INSTANCE.generateLadder(3,2));
+        assertThatNoException().isThrownBy(() -> LadderFactory.INSTANCE.generateLadder(3,new LadderHeight("2")));
     }
 }
