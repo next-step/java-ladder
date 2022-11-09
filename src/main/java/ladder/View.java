5f0e5bc4fb2916e7d36e83e8d465a319e23c2e29
wugatama <dnr7410@naver.com>
2022-11-10 02:22:14 +0900

    refactor: 사다리 메소드 분리

--- src/main/java/ladder/View.java
+++ src/main/java/ladder/View.java
@@ -7,6 +7,7 @@ public class View {
     private final static String COLUMN = "|";
     private final static String ROW = "-";
     private final static String BLANK = " ";
+    private final static Random random = new Random();
 
     public void laddersView(Players players, int ladders) {
         List<Player> participants = players.getPlayers();
@@ -15,57 +16,82 @@ public class View {
 
         for (int i = -1; i < ladders; i++) {
             if (i == -1) {
-                for (Player participant : participants) {
-                    System.out.print(participant.getName() + "\t");
-                }
-                System.out.println();
+                printPlayersName(participants);
                 continue;
             }
-            for (int k = 0; k < participants.get(0).getName().length(); k++) {
-                System.out.print(BLANK);
-            }
-            System.out.print(COLUMN);
-            int randoms = new Random().nextInt(9) + 1;
-            if (i == 0) {
-                if (randoms > 4) {
-                    for (int k = 0; k < 4; k++) {
-                        System.out.print(ROW);
-                    }
-                    check[i][0] = true;
-                    check[i + 1][0] = true;
-                } else {
-                    for (int k = 0; k < 4; k++) {
-                        System.out.print(BLANK);
-                    }
-                }
-            }
-            if (i >= 1) {
-                if (!check[i - 1][0]) {
-                    for (int k = 0; k < 4; k++) {
-                        System.out.print(ROW);
-                    }
-                    check[i][0] = true;
-                } else {
-                    for (int k = 0; k < 4; k++) {
-                        System.out.print(BLANK);
-                    }
-                }
-            }
-            System.out.print(COLUMN);
-            for (int j = 1; j < participants.size() - 1; j++) {
-                if (!check[i][j - 1]) {
-                    for (int k = 0; k < 4; k++) {
-                        System.out.print(ROW);
-                    }
-                    check[i][j] = true;
-                } else {
-                    for (int k = 0; k < 4; k++) {
-                        System.out.print(BLANK);
-                    }
-                }
-                System.out.print(COLUMN);
-            }
+            makeTabByFirstPlayerName(participants.get(0).getName());
+            int randoms = makeRandom();
+            makeFirstSpace(check, i, randoms);
+            makeSpaces(participants, check, i);
             System.out.println();
         }
+    }
+
+    private static void makeSpaces(List<Player> participants, boolean[][] check, int i) {
+        for (int j = 1; j < participants.size() - 1; j++) {
+            checkFrontSpace(check, i, j);
+            System.out.print(COLUMN);
+        }
+    }
+
+    private static void checkFrontSpace(boolean[][] check, int i, int j) {
+        if (!check[i][j - 1]) {
+            makeRow();
+            check[i][j] = true;
+        } else {
+            makeBlank();
+        }
+
+    }
+
+    private static void makeRow() {
+        for (int k = 0; k < 4; k++) {
+            System.out.print(ROW);
+        }
+    }
+
+    private static void makeFirstSpace(boolean[][] check, int i, int randoms) {
+        if (i == 0) {
+            if (randoms > 4) {
+                makeRow();
+                check[i][0] = true;
+                check[i + 1][0] = true;
+            } else {
+                makeBlank();
+            }
+        }
+        if (i >= 1) {
+            if (!check[i - 1][0]) {
+                makeRow();
+                check[i][0] = true;
+            } else {
+                makeBlank();
+            }
+        }
+        System.out.print(COLUMN);
+    }
+
+    private static void makeBlank() {
+        for (int k = 0; k < 4; k++) {
+            System.out.print(BLANK);
+        }
+    }
+
+    private static int makeRandom() {
+        return random.nextInt(9) + 1;
+    }
+
+    private static void makeTabByFirstPlayerName(String player) {
+        for (int k = 0; k < player.length(); k++) {
+            System.out.print(BLANK);
+        }
+        System.out.print(COLUMN);
+    }
+
+    private static void printPlayersName(List<Player> participants) {
+        for (Player participant : participants) {
+            System.out.print(participant.getName() + "\t");
+        }
+        System.out.println();
     }
 }
