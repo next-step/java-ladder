0bf4e0cd7adb4c09919675cf05c341d2c8364971
wugatama <dnr7410@naver.com>
2022-11-09 22:56:48 +0900

    refactor: test 수정

--- src/test/java/ladder/PlayersTest.java
+++ src/test/java/ladder/PlayersTest.java
@@ -2,6 +2,9 @@ package ladder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 public class PlayersTest {
@@ -9,7 +12,8 @@ public class PlayersTest {
     @Test
     void create() {
         String input = "jin,jolly,wuga";
-        Players players = new Players(input);
+        List<String> inputPlayers = Stream.of(input.split(",")).collect(Collectors.toList());
+        Players players = new Players(inputPlayers);
 
         assertThat(players.getPlayers().size()).isEqualTo(3);
     }
