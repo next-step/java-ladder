ef1dd1146bbbfc2be414b946ee6ed02e57870112
heoseungyeon <dia0312@naver.com>
2022-11-10 18:32:43 +0900

    feat: 연속 Point 생성 방지 기능 구현

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -5,15 +5,31 @@ import java.util.List;
 
 public class Line {
     private final List<Boolean> points = new ArrayList<>();
+    private final LineGenerationStrategy lineGenerationStrategy;
 
     private Line(LineGenerationStrategy lineGenerationStrategy, int countOfPerson) {
+        this.lineGenerationStrategy = lineGenerationStrategy;
         for (int i = 0; i < countOfPerson - 1; i++) {
-            points.add(lineGenerationStrategy.generate());
+            points.add(generatePoint(i));
         }
     }
 
     public static Line of(LineGenerationStrategy lineGenerationStrategy, int countOfPerson) {
         return new Line(lineGenerationStrategy, countOfPerson);
+    }
+
+    private Boolean generatePoint(int i) {
+        if (generatable(i)) {
+            return lineGenerationStrategy.generate();
+        }
+        return false;
+    }
+
+    private boolean generatable(int index) {
+        if (index == 0) {
+            return true;
+        }
+        return !points.get(index - 1);
     }
 
     public List<Boolean> getPoints() {
