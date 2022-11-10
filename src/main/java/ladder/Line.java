b632894868fd003aafa00c699ae5fb4da9132808
min16 <minji.na@cre.ma>
2022-11-11 00:13:34 +0900

    FEATURE -  한 라인에서 연속으로 연결 안되도록 포인트 생성

--- src/main/java/ladder/Line.java
+++ src/main/java/ladder/Line.java
@@ -3,14 +3,29 @@ package ladder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Random;
 
 public class Line {
+    private static final Random random = new Random();
+
     private List<Point> points = new ArrayList<>();
 
+
     public Line(int countOfPerson) {
+        boolean lastConnected = false;
         for (int i = 0; i < countOfPerson; i ++) {
-            points.add(new Point(true));
+            boolean nextConnected = !lastConnected && randomBoolean();
+            lastConnected = nextConnected;
+            points.add(new Point(nextConnected));
         }
+    }
+
+    public List<Point> getPoints() {
+        return points;
+    }
+
+    private boolean randomBoolean() {
+        return random.nextBoolean();
     }
 
     @Override
--- src/test/java/ladder/LineTest.java
+++ src/test/java/ladder/LineTest.java
@@ -2,11 +2,13 @@ package ladder;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineTest {
     @Test
-    public void 생성() {
-        assertThat(new Line(2)).isEqualTo(new Line(2));
+    public void 연속으로_연결하면_안됨() {
+        assertThat(new Line(2).getPoints()).isNotEqualTo(Arrays.asList(new Point(true), new Point(true)));
     }
 }
