b31bdaad77a0fcb161bf7107b5349337b9548ece
caoyu-dev <choyumin01@gmail.com>
2022-11-11 00:29:18 +0900

    [refactor] 사다리 실행 결과 출력되도록 수정

--- src/main/java/nextstep/LadderApplication.java
+++ src/main/java/nextstep/LadderApplication.java
@@ -2,21 +2,22 @@ package nextstep;
 
 import nextstep.domain.Ladder;
 import nextstep.domain.Players;
+import nextstep.domain.Result;
 import nextstep.strategy.RandomLineStrategy;
 import nextstep.view.Input;
 import nextstep.view.Output;
 
-import java.util.List;
-
 public class LadderApplication {
     public static void main(String[] args) {
         Players playerNames = Input.getUserName();
-        List<String> result = Input.getResult();
+        Result result = new Result(Input.getResult());
         int height = Input.getHeight();
 
         Ladder ladder = new Ladder(height, playerNames.getPlayersSize(), RandomLineStrategy.getInstance());
         Output.printName(playerNames.getPlayers());
         Output.printLadder(ladder);
         Output.printResult(result);
+
+        Input.printPlayerResult(playerNames, result, ladder);
     }
 }
