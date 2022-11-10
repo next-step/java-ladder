7d3d7ee79816fa5c700681de88e73f0a9bf68e08
heoseungyeon <dia0312@naver.com>
2022-11-10 15:01:21 +0900

    feat: 사다리 높이 입력 기능 구현

--- src/main/java/nextstep/ladder/LadderGameApp.java
+++ src/main/java/nextstep/ladder/LadderGameApp.java
@@ -2,6 +2,7 @@ package nextstep.ladder;
 
 import nextstep.ladder.domain.Names;
 import nextstep.ladder.view.InputView;
+import nextstep.ladder.view.dto.AskHeightOfLadderDto;
 import nextstep.ladder.view.dto.AskParticipantNamesDto;
 
 public class LadderGameApp {
@@ -10,5 +11,8 @@ public class LadderGameApp {
         final InputView inputView = new InputView();
         AskParticipantNamesDto askParticipantNamesDto = inputView.askParticipantNames();
         Names names = askParticipantNamesDto.getNames();
+
+        AskHeightOfLadderDto askHeightOfLadderDto = inputView.askHeightOfLadder();
+        int heightOfLadder = askHeightOfLadderDto.getHeight();
     }
 }
--- src/main/java/nextstep/ladder/exception/ErrorMessage.java
+++ src/main/java/nextstep/ladder/exception/ErrorMessage.java
@@ -1,8 +1,8 @@
 package nextstep.ladder.exception;
 
 public enum ErrorMessage {
-    INVALID_LENGTH_OF_NAME("이름은 최대 {0}자까지 가능합니다.");
-
+    INVALID_LENGTH_OF_NAME("이름은 최대 {0}자까지 가능합니다."),
+    INVALID_HEIGHT_OF_LADDER("사다리의 높이는 양수이어야 합니다");
     String message;
 
     ErrorMessage(String message) {
