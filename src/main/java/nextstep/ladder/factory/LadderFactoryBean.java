ffd9a510c6d2c0e2b114dd717482ebc6ed01cbc8
michol.lee <lovethefeel@gmail.com>
2022-11-13 19:47:43 +0900

    feat: factory bean 생성

---
+++ src/main/java/nextstep/ladder/factory/LadderFactoryBean.java
@@ -0,0 +1,15 @@
+package nextstep.ladder.factory;
+
+import nextstep.ladder.codeleesh.domain.CodeLadderCreator;
+import nextstep.ladder.codeleesh.domain.CodeLineCreator;
+import nextstep.ladder.engine.LadderCreator;
+import nextstep.ladder.engine.LineCreator;
+
+public class LadderFactoryBean {
+
+    public static LadderCreator createLadderFactory() {
+
+        final LineCreator lineCreator = new CodeLineCreator();
+        return new CodeLadderCreator(lineCreator);
+    }
+}
