f76fee373865eeb5ef19a69e6f02e85ed1251d64
bella.n <bella.n@kakaocorp.com>
2022-11-14 11:44:04 +0900

    refactor: Point creator 메소드 생성

--- src/test/java/ladder/model/PointTest.java
+++ src/test/java/ladder/model/PointTest.java
@@ -8,35 +8,33 @@ class PointTest {
 
     @Test
     public void 사다리_마지막_다리_검증() {
-        Point point = new Point(1, new Direction(false, true));
+        Direction result = createPoint(false, true).last().getDirection();
 
         Direction expected = new Direction(true, false);
-
-        Direction result = point.last().getDirection();
 
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void 사다리_연결_불가능_검증() {
-        Point point = new Point(1, new Direction(false, true));
+        Direction result = createPoint(false, true).next(false).getDirection();
 
         Direction expected = new Direction(true, false);
-
-        Direction result = point.next(false).getDirection();
 
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void 사다리_연결_가능_검증() {
-        Point point = new Point(1, new Direction(false, false));
+        Direction result = createPoint(false, false).next(true).getDirection();
 
         Direction expected = new Direction(false, true);
 
-        Direction result = point.next(true).getDirection();
-
         assertThat(result).isEqualTo(expected);
+    }
+
+    private Point createPoint(boolean left, boolean right) {
+        return new Point(1, new Direction(left, right));
     }
 
 }
