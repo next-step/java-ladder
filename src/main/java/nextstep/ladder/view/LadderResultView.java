e0498538e011b3f0e64af764cbe6a801540e66ea
hasunzo <gktjsdl470@gmail.com>
2022-11-15 21:57:55 +0900

    refactor : ui 출력 로직 개선

--- src/main/java/nextstep/ladder/view/LadderResultView.java
+++ src/main/java/nextstep/ladder/view/LadderResultView.java
@@ -7,6 +7,10 @@ import java.util.List;
 
 public class LadderResultView {
 
+    private static final String BLANK = "    ";
+    private static final String LINE = "-----";
+    private static final String DIVISION_LINE = "|";
+
     public void printLadder(List<User> users, List<Line> lines) {
         printUsers(users);
         printLines(lines);
@@ -14,22 +18,23 @@ public class LadderResultView {
 
     private void printLines(List<Line> lines) {
         for (Line line : lines) {
-            StringBuilder sb = new StringBuilder("   ");
-            sb.append("|");
+            StringBuilder sb = new StringBuilder(BLANK);
             createLine(line, sb);
+            sb.append(DIVISION_LINE);
             System.out.println(sb);
         }
     }
 
     private void createLine(Line line, StringBuilder sb) {
         for (Boolean point : line.getPoints()) {
-            if (point) {
-                sb.append("-----");
-            } else {
-                sb.append("     ");
-            }
-            sb.append("|");
+            sb.append(DIVISION_LINE);
+            sb.append(printLineByPoint(point));
         }
+    }
+
+    private String printLineByPoint(boolean point) {
+        if (point) return LINE;
+        return BLANK;
     }
 
     private void printUsers(List<User> users) {
