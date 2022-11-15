9b21c3f4a07b5f5d053109d2b22a54cf04ce7d36
hasunzo <gktjsdl470@gmail.com>
2022-11-15 22:33:16 +0900

    refactor : Users 일급 컬렉션 생성 로직 개선
    - inputNames를 받아 User List를 생성해주는 클래스 추가

--- src/main/java/nextstep/ladder/service/LadderService.java
+++ src/main/java/nextstep/ladder/service/LadderService.java
@@ -4,16 +4,13 @@ import nextstep.ladder.domain.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class LadderService {
-
-    private static final String REGEX = ",";
 
     private final Ladder ladder;
 
     public LadderService(String inputNames, int height) {
-        Users users = new Users(createUsers(inputNames));
+        Users users = new Users(StringUserNamesToUsersGenerator.toUsers(inputNames));
         Lines lines = new Lines(createLines(height, users.getUserSize()));
         this.ladder = new Ladder(users, lines);
     }
@@ -24,16 +21,6 @@ public class LadderService {
             lines.add(new Line(countOfPerson, new RandomLineGenerator()));
         }
         return lines;
-    }
-
-    private List<User> createUsers(String inputNames) {
-        return splitCommaName(inputNames).stream()
-                .map(User::new)
-                .collect(Collectors.toList());
-    }
-
-    private List<String> splitCommaName(String inputNames) {
-        return List.of(inputNames.split(REGEX));
     }
 
     public Ladder getLadder() {
