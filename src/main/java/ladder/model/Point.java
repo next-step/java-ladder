1c6ef79abdca51c00dedfcf95e83814e4466ed9d
김남희 <40882598+snt2525@users.noreply.github.com>
2022-11-16 16:13:30 +0900

    Step4  - 사다리(리팩토링) (#1630)
    * refactor: 메인 생성자 선언 순서 변경
    
    * refactor: 생성자 대신 of 메소드로 수정
    
    * refactor: Point creator 메소드 생성
    
    Co-authored-by: bella.n <bella.n@kakaocorp.com>

--- src/main/java/ladder/model/Point.java
+++ src/main/java/ladder/model/Point.java
@@ -5,17 +5,17 @@ public class Point {
     private final int index;
     private final Direction direction;
 
-    public Point(int index, Direction direction) {
-        this.index = index;
-        this.direction = direction;
-    }
-
     public Point next(boolean rightRandom) {
         if (direction.isConnectableNext()) {
             return new Point(index + 1, direction.next(rightRandom));
         }
 
         return new Point(index + 1, direction.next());
+    }
+
+    public Point(int index, Direction direction) {
+        this.index = index;
+        this.direction = direction;
     }
 
     public Point last() {
