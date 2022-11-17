6c01add8bc8b5ae444b453e81ec7994405245db1
sangwon lee <rollo@korda.im>
2022-11-17 15:55:12 +0900

    feat(3단계 - 사다리(게임 실행)) : InputView

--- src/main/java/ladder/controller/LadderController.java
+++ src/main/java/ladder/controller/LadderController.java
@@ -8,7 +8,8 @@ import java.util.List;
 
 public class LadderController {
     public static void main(String[] args) {
-        List<String> nameList = InputView.InputNameList();
+        List<String> nameList = InputView.inputNameList();
+        List<String> resultList = InputView.inputResultList();
         Integer height = InputView.InputHeight();
         Ladder ladder = new Ladder(nameList, height);
         ResultView.printLadder(ladder);
--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String INPUT_NAME_LIST_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    public static final String INPUT_RESULT_LIST_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String INPUT_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     private static final String VALIDATE_INTEGER = "양수만 입력할 수 있습니다.";
 
@@ -18,10 +19,16 @@ public class InputView {
         throw new AssertionError();
     }
 
-    public static List<String> InputNameList() {
+    public static List<String> inputNameList() {
         System.out.println(INPUT_NAME_LIST_MESSAGE);
         String inputNameList = SCANNER.nextLine();
-        return makeNameList(inputNameList);
+        return makeList(inputNameList);
+    }
+
+    public static List<String> inputResultList() {
+        System.out.println(INPUT_RESULT_LIST_MESSAGE);
+        String inputNameList = SCANNER.nextLine();
+        return makeList(inputNameList);
     }
 
     public static Integer InputHeight() {
@@ -30,7 +37,7 @@ public class InputView {
         return convertStringToInt(inputHeight);
     }
 
-    private static List<String> makeNameList(String inputNameList) {
+    private static List<String> makeList(String inputNameList) {
         String trimmedInputNumberList = inputNameList.replaceAll(" ", "");
         return Arrays.stream(trimmedInputNumberList.split(","))
                 .collect(Collectors.toList());
