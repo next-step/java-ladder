4b7bb384bb7739831a97fb59665576d2990a239a
saerang <lsg10119@gamail.com>
2023-04-27 00:06:12 +0900

    style: 코드 포맷팅

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -13,7 +13,7 @@ public class StreamStudy {
 
     public static long countWords() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
-                                                                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         long count = 0;
@@ -25,7 +25,7 @@ public class StreamStudy {
 
     public static void printLongestWordTop100() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
-                                                                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         // TODO 이 부분에 구현한다.
