c0c119b73553ffba47b984e02ac7506a600415ad
oneny <gjwodnjs1996@gmail.com>
2023-04-30 16:53:41 +0900

    feat: User 구현

---
+++ src/test/java/ladder/UserTest.java
@@ -0,0 +1,23 @@
+package ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UserTest {
+  @Test
+  @DisplayName("5글자 이하 유저이름 반환 테스트")
+  public void create_유저이름() {
+    assertThat(new User("oneny").name()).isEqualTo("oneny");
+  }
+
+  @Test
+  @DisplayName("유저이름 5글자 초과하는 경우 IllegalArgumentException throw 테스트")
+  public void create_5글자_초과_유저이름() {
+    assertThatThrownBy(() -> new User("threeny"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("이름은 최대 5글자까지 부여할 수 있습니다. 현재 이름 : threeny");
+  }
+}
