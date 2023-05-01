c557894ae62200704fb1195cdac12657bb96a423
saerang <lsg10119@gamail.com>
2023-05-01 22:42:20 +0900

    feat: Member 구현

---
+++ src/main/java/nextstep/ladder/Member.java
@@ -0,0 +1,29 @@
+package nextstep.ladder;
+
+import java.util.Objects;
+
+public class Member {
+    public static final int MAX_NAME_LENGTH = 5;
+    private final String name;
+
+    public Member(String name) {
+        if (name == null || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(String.format("이름은 %d 글자를 초과할 수 없습니다.", MAX_NAME_LENGTH));
+        }
+
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(name, member.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
---
+++ src/test/java/nextstep/ladder/MemberTest.java
@@ -0,0 +1,24 @@
+package nextstep.ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MemberTest {
+
+    @Test
+    @DisplayName("Member 생성")
+    void test01() {
+        Member member = new Member("rang");
+
+        assertThat(member).isEqualTo(new Member("rang"));
+    }
+
+    @Test
+    @DisplayName("이름이 5글자 초과하면 에러 발생")
+    void test02() {
+        assertThatThrownBy(() -> new Member("saerang")).isInstanceOf(IllegalArgumentException.class);
+    }
+}
