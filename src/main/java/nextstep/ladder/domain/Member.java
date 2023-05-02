a279dc6ff7dd039282732e51ed08e31462d856f6
saerang <lsg10119@gamail.com>
2023-05-02 23:59:33 +0900

    refactor: 이름 출력을 위해 member 의 name 메서드 구현

--- src/main/java/nextstep/ladder/domain/Member.java
+++ src/main/java/nextstep/ladder/domain/Member.java
@@ -14,6 +14,10 @@ public class Member {
         this.name = name;
     }
 
+    public String name() {
+        return this.name;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
