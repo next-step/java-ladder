c476a1c77721c452bc513ad6dc573a916857e061
saerang <lsg10119@gamail.com>
2023-05-02 23:15:55 +0900

    feat: Line 구현

---
+++ src/main/java/nextstep/ladder/Line.java
@@ -0,0 +1,36 @@
+package nextstep.ladder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class Line {
+    private final List<Boolean> points = new ArrayList<>();
+
+    public Line(int countOfMember) {
+        IntStream.range(0, countOfMember - 1).forEach(i -> this.addPoint(i, new Random().nextBoolean()));
+    }
+
+    public Line(Boolean... booleans) {
+        IntStream.range(0, booleans.length).forEachOrdered(i -> this.addPoint(i, booleans[i]));
+    }
+
+    private void addPoint(int index, boolean isPoint) {
+        if (this.canNotHavePoint(index)) {
+            points.add(false);
+            return;
+        }
+
+        points.add(isPoint);
+    }
+
+    private boolean canNotHavePoint(int index) {
+        return index == 0 || points.get(index - 1);
+    }
+
+
+    public List<Boolean> points() {
+        return this.points;
+    }
+}
---
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -0,0 +1,33 @@
+package nextstep.ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LineTest {
+
+    @Test
+    @DisplayName("라인의 개수는 (사람 수 - 1) 이다")
+    void test01() {
+        Line line = new Line(5);
+
+        assertThat(line.points()).hasSize(4);
+    }
+
+    @Test
+    @DisplayName("0번째 Point 는 항상 false 이다.")
+    void test02() {
+        Line line = new Line(true, false, false);
+
+        assertThat(line.points()).containsExactly(false, false, false);
+    }
+
+    @Test
+    @DisplayName("이전 Point 가 true 면 다음 Point 는 false 이다.")
+    void test03() {
+        Line line = new Line(false, true, true);
+
+        assertThat(line.points()).containsExactly(false, true, false);
+    }
+}
