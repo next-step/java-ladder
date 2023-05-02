cac72b7d9b7473164ebbb1d40aaf5b32dc77fc32
saerang <lsg10119@gamail.com>
2023-05-02 23:28:41 +0900

    feat: Ladder 구현

---
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -0,0 +1,20 @@
+package nextstep.ladder.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Ladder {
+    private final List<Line> lines;
+
+    public Ladder(int height, int countOfNumber) {
+        lines = IntStream.range(0, height)
+                         .mapToObj(h -> new Line(countOfNumber))
+                         .collect(Collectors.toList());
+    }
+
+    public List<Line> lines() {
+        return Collections.unmodifiableList(this.lines);
+    }
+}
---
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -0,0 +1,17 @@
+package nextstep.ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LadderTest {
+
+    @Test
+    @DisplayName("Ladder 높이와 사람 수를 가지고 생성한다.")
+    void test01() {
+        Ladder ladder = new Ladder(4, 5);
+
+        assertThat(ladder.lines()).hasSize(4);
+    }
+}
