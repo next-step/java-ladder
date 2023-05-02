eaf5352e5a0f98a755af823c491f8b183788827c
right1203 <right1203@gmail.com>
2023-05-02 21:59:40 +0900

    refactor: 피드백 반영

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -16,8 +16,9 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = words.stream().filter((word) -> word.length() > 12).count();
-        return count;
+        return words.stream()
+                .filter((word) -> word.length() > 12)
+                .count();
     }
 
     public static void printLongestWordTop100() throws IOException {
