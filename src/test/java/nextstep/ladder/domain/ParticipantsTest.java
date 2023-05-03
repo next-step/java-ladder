fc74fe54a1ec4238ed9f7b8427678aa8db0efc79
In-wook Hwang <inwook9@gmail.com>
2023-05-04 01:17:25 +0900

    feat: Participants 기능추가

---
+++ src/test/java/nextstep/ladder/domain/ParticipantsTest.java
@@ -0,0 +1,44 @@
+package nextstep.ladder.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ParticipantsTest {
+
+    private List<String> expected;
+    private String input;
+
+    @BeforeEach
+    void setUp() {
+        expected = Arrays.asList("pobi", "honux", "crong", "jk");
+        input = String.join(",", expected);
+    }
+
+    @Test
+    @DisplayName("참여자는 사용자 이름을 반환한다.")
+    void test01() {
+        Participants participants = Participants.of(input);
+        List<String> actual = participants.getUserNames()
+                .stream()
+                .map(UserName::getName)
+                .collect(Collectors.toUnmodifiableList());
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("참여자는 사용자 수를 반환한다.")
+    void test02() {
+        Participants participants = Participants.of(input);
+
+        assertThat(participants.count()).isEqualTo(expected.size());
+    }
+
+}
