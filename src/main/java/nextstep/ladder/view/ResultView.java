644297b158f3b3764d3d206d09372216ba74f3e2
In-wook Hwang <inwook9@gmail.com>
2023-05-04 03:12:13 +0900

    feat: ResultView 기능추가

---
+++ src/main/java/nextstep/ladder/view/ResultView.java
@@ -0,0 +1,55 @@
+package nextstep.ladder.view;
+
+import nextstep.ladder.domain.*;
+
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public final class ResultView {
+
+    private static final String FIRST_COLUMN = "    |";
+    private static final String NEW_LINE = "\n";
+    private static final String CONNECTED_ROW = "-----|";
+    private static final String UNCONNECTED_ROW = "     |";
+
+    private ResultView() {
+        throw new IllegalCallerException("잘못된 객체생성 입니다.");
+    }
+
+    public static void draw(Participants participants, Ladder ladder) {
+        System.out.println("\n실행결과\n");
+
+        String drewUserNames = drawUserNames(participants);
+        System.out.println(drewUserNames);
+
+        String drewLines = drawLines(ladder.getLineColumns());
+        System.out.println(drewLines);
+    }
+
+    private static String drawLines(LineColumns lineColumns) {
+        return lineColumns.getColumns()
+                .stream()
+                .map(column -> FIRST_COLUMN + drawRows(column))
+                .collect(Collectors.joining(NEW_LINE));
+    }
+
+    private static String drawUserNames(Participants participants) {
+        return participants.getUserNames()
+                .stream()
+                .map(UserName::getName)
+                .map(name -> String.format("%5s", name))
+                .collect(Collectors.joining(" "));
+    }
+
+    private static String drawRows(LineRows column) {
+        return column.getConnectables()
+                .stream()
+                .map(drawRow())
+                .collect(Collectors.joining());
+    }
+
+    private static Function<Connectable, String> drawRow() {
+        return connectable -> connectable.isConnected() ? CONNECTED_ROW : UNCONNECTED_ROW;
+    }
+
+}
