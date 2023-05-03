0d32e4945a208d67e373d9aa96eb96461469fe26
In-wook Hwang <inwook9@gmail.com>
2023-05-04 02:47:09 +0900

    feat: InputView 기능추가

---
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -0,0 +1,23 @@
+package nextstep.ladder.view;
+
+import java.util.Scanner;
+
+public final class InputView {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private InputView() {
+        throw new IllegalCallerException("잘못된 객체생성 입니다.");
+    }
+
+    public static String showUserNamesConsole() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        return SCANNER.nextLine();
+    }
+
+    public static int showLadderHeightConsole() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        return Integer.parseInt(SCANNER.nextLine());
+    }
+
+}
