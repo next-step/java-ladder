6002f8a415d169a463d81aabf22b01e1a5fb4547
kyeongminlee <kyeongmin.jake.lee@gmail.com>
2023-05-05 14:58:44 +0900

    feature - 사다리 높이 입력 기능

--- src/main/java/nextstep/ladder/Main.java
+++ src/main/java/nextstep/ladder/Main.java
@@ -1,10 +1,12 @@
 package nextstep.ladder;
 
+import nextstep.ladder.domain.Heights;
 import nextstep.ladder.domain.Participants;
 import nextstep.ladder.view.InputView;
 
 public class Main {
     public static void main(String[] args) {
         Participants participants = InputView.inputNames();
+        Heights heights = InputView.inputHeights();
     }
 }
--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -1,5 +1,6 @@
 package nextstep.ladder.view;
 
+import nextstep.ladder.domain.Heights;
 import nextstep.ladder.domain.Participants;
 
 import java.util.List;
@@ -12,5 +13,11 @@ public class InputView {
     public static Participants inputNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         return Participants.from(List.of(SCANNER.nextLine().split(DELIMITER)));
+    }
+
+    public static Heights inputHeights() {
+        System.out.println();
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        return new Heights(SCANNER.nextInt());
     }
 }
