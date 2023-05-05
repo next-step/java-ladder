b2c70e7b86d2b7d43e56ef839dd4fef8d97335b8
kyeongminlee <kyeongmin.jake.lee@gmail.com>
2023-05-05 12:53:52 +0900

    feature - 참가자 생성 기능

---
+++ src/main/java/nextstep/ladder/domain/Participants.java
@@ -0,0 +1,22 @@
+package nextstep.ladder.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Participants {
+
+    private final List<Name> participants;
+
+    public Participants(List<Name> participants) {
+        this.participants = participants;
+    }
+
+    public static Participants from(List<String> names) {
+        List<Name> participants = names.stream()
+                .map(name -> new Name(name.strip()))
+                .collect(Collectors.toList());
+
+        return new Participants(participants);
+    }
+
+}
---
+++ src/test/java/nextstep/ladder/domain/ParticipantsTest.java
@@ -0,0 +1,27 @@
+package nextstep.ladder.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ParticipantsTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi,honux,crong,jk", "pobi, honux, crong, jk"})
+    @DisplayName("참가자 객체 생성 테스트")
+    void createParticipantsTest(String inputNames) {
+        String[] splitNames = inputNames.split(",");
+        List<String> names = List.of(splitNames);
+        Participants participants = Participants.from(names);
+
+        assertThat(participants)
+                .isInstanceOf(Participants.class);
+    }
+
+}
