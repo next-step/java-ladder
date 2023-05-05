f5450806617be97deef77c30650f51acffece7ec
kyeongminlee <kyeongmin.jake.lee@gmail.com>
2023-05-05 15:40:56 +0900

    feature - 개별 라인 생성 기능

---
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -0,0 +1,14 @@
+package nextstep.ladder.domain;
+
+public class Line {
+
+    private final boolean line;
+
+    public Line(boolean line) {
+        this.line = line;
+    }
+
+    public boolean hasLine() {
+        return line;
+    }
+}
