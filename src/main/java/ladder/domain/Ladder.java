64f892a0f32afd2bb6e072e8d55483f17f8128a3
dong <dong.22@kakaoenterprise.com>
2023-05-07 21:05:17 +0900

    feat : savepoint1

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -2,12 +2,28 @@ package ladder.domain;
 
 import ladder.control.Preferences;
 
-public class Ladder {
-    Lines lines;
-    Pillars pillars;
+import java.util.List;
+import java.util.Set;
 
-    public Ladder(int height, int width) {
-        pillars = new Pillars(height,width);
-        lines = new Lines(height,width,Preferences.lineCount(width));
+public class Ladder {
+    private final int column;
+    private final int row;
+    private final Lines lines;
+    private final Pillars pillars;
+
+    public Ladder(int column, int row) {
+        this.column = column;
+        this.row = row;
+        this.pillars = new Pillars(column, row);
+        this.lines = new Lines(column, row, Preferences.lineCount(row));
+    }
+
+    public List<String> rendering() {
+        Set<Line> lines1 = lines.allLines();
+        for(int i = 0; i< column; i++) {
+            for(int j = 0; j< row; j++ ) {
+
+            }
+        }
     }
 }
