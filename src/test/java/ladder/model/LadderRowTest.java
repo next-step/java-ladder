5d8461c7f27471730510a179e756dfdeb8643b68
Baek Jiyeon <100.paperkite@gmail.com>
2023-05-07 19:44:05 +0900

    feat(test): 사다리 가로줄 테스트 추가

--- src/test/java/ladder/model/LadderRowTest.java
+++ src/test/java/ladder/model/LadderRowTest.java
@@ -1,11 +1,11 @@
 package ladder.model;
 
+import ladder.strategy.RandomStrategy;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.*;
 
 class LadderRowTest {
     @Test
@@ -26,5 +26,28 @@ class LadderRowTest {
         //when, then
         assertThatCode(() -> new LadderRow(overlapped))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    public void 설정한_width_만큼_생성되어야_한다() throws Exception {
+        //given
+        int width = 5;
+        LadderRow row = LadderRow.create(width, new RandomStrategy());
+
+        //when, then
+        assertThat(row.size()).isEqualTo(width);
+    }
+
+    @Test
+    public void 사다리_칸_존재여부를_확인할_수_있다() throws Exception {
+        //given
+        LadderRow row = new LadderRow(List.of(false, false, true, false));
+
+        //when, then
+        assertThat(row.exists(0)).isFalse();
+        assertThat(row.exists(1)).isFalse();
+        assertThat(row.exists(2)).isTrue();
+        assertThat(row.exists(3)).isFalse();
+
     }
 }
