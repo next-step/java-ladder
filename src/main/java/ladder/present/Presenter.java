5884775af41fd5fadfb80504da435e8a75be9441
dong <dong.22@kakaoenterprise.com>
2023-05-07 10:02:07 +0900

    test : ui layer impl

--- src/main/java/ladder/present/Presenter.java
+++ src/main/java/ladder/present/Presenter.java
@@ -1,15 +1,32 @@
 package ladder.present;
 
 import ladder.domain.Ladder;
+import ladder.domain.User;
 import ladder.domain.Users;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Presenter {
+    private final Scanner scanner;
+
+    public Presenter() {
+        this.scanner = new Scanner(System.in);
+    }
+
     public Users users() {
-        throw new RuntimeException();
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        String s = scanner.nextLine();
+        String[] split = s.split(",");
+        List<User> collect = Arrays.stream(split).map(User::new).collect(Collectors.toList());
+        return new Users(collect);
     }
 
     public int ladderHeight() {
-        throw new RuntimeException();
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        return Integer.parseInt(scanner.nextLine());
     }
 
     public void renderingLadder(Users users, Ladder ladder) {
