2fc8a31904d816b4b57464cd886754450be577a2
dong <dong.22@kakaoenterprise.com>
2023-05-07 14:43:15 +0900

    test : test code add

--- src/test/java/ladder/domain/UsersTest.java
+++ src/test/java/ladder/domain/UsersTest.java
@@ -1,7 +1,27 @@
 package ladder.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UsersTest {
 
+
+    @DisplayName("관리하는 user 수를 리턴한다")
+    @Test
+    public void count() {
+        //given
+        User kilby = new User("kilby");
+        User david = new User("Dawon-Kahng");
+        User feynman = new User("feynman");
+        Users users = new Users(List.of(kilby, david, feynman));
+        //when
+        int count = users.count();
+        //then
+        assertThat(count).as("").isEqualTo(3);
+    }
 }
