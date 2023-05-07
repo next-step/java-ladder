b2c9a0357fec2641d4a3a10d645d3830f93b7a77
didrlgus <rlgusdid@naver.com>
2023-05-07 19:41:31 +0900

    refactor: 2단계 - 코드리뷰 반영
    - import 구문에 * 사용하는 부분 수정

--- src/test/java/ladder/domain/HeightTest.java
+++ src/test/java/ladder/domain/HeightTest.java
@@ -1,6 +1,7 @@
 package ladder.domain;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
