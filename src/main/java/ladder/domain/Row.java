febff36b3cc76653ef8bcc9387c48fc7730b8c57
dong <dong.22@kakaoenterprise.com>
2023-05-08 22:52:47 +0900

    feat : equals and HashCode impl

--- src/main/java/ladder/domain/Row.java
+++ src/main/java/ladder/domain/Row.java
@@ -14,4 +14,17 @@ public class Row {
     public boolean isSame(Row otherRow) {
         return this.value == otherRow.value;
     }
+
+    @Override
+    public int hashCode() {
+        return this.value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Row otherRow = (Row) o;
+        return this.hashCode() == otherRow.hashCode();
+    }
 }
