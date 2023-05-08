b4cf64eb519a6546b9578e4fa156165a12dc9417
dong <dong.22@kakaoenterprise.com>
2023-05-08 22:15:47 +0900

    feat : 캐싱 도입

--- src/main/java/ladder/domain/Column.java
+++ src/main/java/ladder/domain/Column.java
@@ -1,12 +1,27 @@
 package ladder.domain;
 
+import ladder.control.Preferences;
+import ladder.exception.OutOfColumnRangeException;
+
 import java.util.Objects;
 
 public class Column {
     private final int value;
-
-    public Column(int value) {
+    private static final Column[] COLUMNS_CACHE = new Column[Preferences.maxColumnPolicy()];
+    static {
+        for(int i=0 ; i<=Preferences.maxColumnPolicy() ; i++) {
+            COLUMNS_CACHE[i] = new Column(i);
+        }
+    }
+    private Column(int value) {
         this.value = value;
+    }
+
+    public static Column of(int value) {
+        if(value>Preferences.maxColumnPolicy()) {
+            throw new OutOfColumnRangeException();
+        }
+        return COLUMNS_CACHE[value];
     }
 
     public int getValue() {
