a8387eb34c5f88368a0064dcb4177b84251ae838
dong <dong.22@kakaoenterprise.com>
2023-05-08 21:41:56 +0900

    refactor : 바뀐거 수정

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -13,15 +13,18 @@ public class Ladder {
     public Ladder(int column, int row) {
         this.column = column;
         this.row = row;
-        this.lines = Lines.of(column, row, Preferences.createLineCount(column,row));
+        this.lines = Lines.of(column, row, Preferences.createLineCount(column, row));
     }
 
     public List<String> rendering() {
         List<String> strings = new ArrayList<>();
-        StringBuilder lineBuilder;
-        for (int columnIndex = 0; columnIndex < column; columnIndex++) {
+
+        for (int rowIndex = 0; rowIndex < row; rowIndex++) {
+            StringBuilder lineBuilder;
             lineBuilder = new StringBuilder();
-            for (int rowIndex = 0; rowIndex < row; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < column; columnIndex++) {
+
+
                 lineBuilder.append("|");
                 if (lines.existLine(columnIndex, rowIndex)) {
                     lineBuilder.append("------");
