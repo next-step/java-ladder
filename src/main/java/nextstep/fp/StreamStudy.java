0d47b8ec85c4768bf0c2e9694764148cb8de1f7c
ethan.v <ethan.v@kakaocorp.com>
2023-05-09 01:55:59 +0900

    [step1] -스트림,람다, Optional

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,21 +5,21 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
+    public static final int WORD_COUNT = 12;
+    public static final int TOP_N = 100;
+
 
     public static long countWords() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream().filter(s -> s.length() > WORD_COUNT).count();
     }
 
     public static void printLongestWordTop100() throws IOException {
@@ -28,6 +28,14 @@ public class StreamStudy {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(s -> s.length() > WORD_COUNT)
+                .sorted(Comparator.comparingInt(String::length).reversed())
+                .distinct()
+                .limit(TOP_N)
+                .map(String::toLowerCase)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +47,6 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream().filter(integer -> integer > 3).map(x -> x * 2).mapToInt(Integer::intValue).sum();
     }
 }
