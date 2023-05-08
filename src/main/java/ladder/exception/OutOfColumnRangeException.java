5d124f5079e9106bd9728df01bd77c6251598b80
dong <dong.22@kakaoenterprise.com>
2023-05-08 22:15:14 +0900

    feat : 커스텀익셉션 추가

---
+++ src/main/java/ladder/exception/OutOfColumnRangeException.java
@@ -0,0 +1,7 @@
+package ladder.exception;
+
+public class OutOfColumnRangeException extends IllegalArgumentException {
+    public OutOfColumnRangeException() {
+        super("사용 가능한 Column 의 범위에서 초과하였습니다");
+    }
+}
