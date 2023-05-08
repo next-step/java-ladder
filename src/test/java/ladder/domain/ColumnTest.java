a6e29a261c0906048684d5d10b9635c43093726f
dong <dong.22@kakaoenterprise.com>
2023-05-09 00:43:33 +0900

    test : 테스트코드 추가

--- src/test/java/ladder/domain/ColumnTest.java
+++ src/test/java/ladder/domain/ColumnTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class ColumnTest {
@@ -18,5 +19,16 @@ class ColumnTest {
                 () -> assertThat(Column.of(1)).as("숫자가 같으면 동일하다").isEqualTo(Column.of(1)),
                 () -> assertThat(Column.of(2)).as("숫자가 다르면 동일하지 않다").isNotEqualTo(Column.of(4))
         );
+    }
+    @DisplayName("범위를 초과한 value 입력시 예외가 발생한다")
+    @Test
+    public void outOfColumnRangeException() {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> {
+
+        }).isInstanceOf(Object.class)
+                .hasMessageContaining("추가");
     }
 }
