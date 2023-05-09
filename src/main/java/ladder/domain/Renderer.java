457f1ef6e20b76c286665c63c0942436bb89ba4b
dong <dong.22@kakaoenterprise.com>
2023-05-09 12:41:42 +0900

    refactor : 표현부분과 표현을 생성하는 로직을 분리

--- src/main/java/ladder/domain/Renderer.java
+++ src/main/java/ladder/domain/Renderer.java
@@ -3,6 +3,7 @@ package ladder.domain;
 import ladder.control.Preferences;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -10,30 +11,48 @@ public class Renderer {
     private final int column;
     private final int row;
     private final Lines lines;
+    private final Users users;
 
-    public Renderer(int column, int row) {
+    public Renderer(int column, int row, Lines lines, Users users) {
         this.column = column;
         this.row = row;
-        this.lines = Lines.of(column, row, Preferences.createLineCount(column, row));
+        this.lines = lines;
+        this.users = users;
     }
 
-    public List<String> rendering() {
+//    public Renderer(int column, int row, Users users) {
+//        this.column = column;
+//        this.row = row;
+//        this.lines = Lines.of(column, row, Preferences.createLineCount(column, row));
+//        this.names = users.names();
+//    }
+
+    private List<String> renderingSceneLadderArea() {
         return IntStream.range(0, row)
-                .mapToObj(rowIndex -> renderingRow(rowIndex, column))
+                .mapToObj(rowIndex -> renderingHorizontal(rowIndex, column))
                 .collect(Collectors.toList());
     }
 
-    private String renderingRow(int rowIndex, int columnRange) {
+    private String renderingHorizontal(int rowIndex, int columnRange) {
         return IntStream.range(0, columnRange)
-                .mapToObj(columnIndex -> pixelRendering(columnIndex, rowIndex))
+                .mapToObj(columnIndex -> renderingPixel(columnIndex, rowIndex))
                 .collect(Collectors.joining());
     }
 
-    private String pixelRendering(int columnIndex, int rowIndex) {
+    private String renderingPixel(int columnIndex, int rowIndex) {
         return Preferences.pillarShape().concat(lineShape(columnIndex, rowIndex));
     }
 
     private String lineShape(int columnIndex, int rowIndex) {
         return lines.existLine(columnIndex, rowIndex) ? Preferences.allocatedLineShape() : Preferences.emptyLineShape();
+    }
+
+    public Scene renderingSceneWithUser() {
+        return new Scene(renderingSceneLadderArea(),renderingSceneUserArea());
+    }
+
+    private String renderingSceneUserArea() {
+        return users.names().stream()
+                .collect(Collectors.joining(Preferences.userDelimiter()));
     }
 }
---
+++ src/main/java/ladder/domain/Scene.java
@@ -0,0 +1,21 @@
+package ladder.domain;
+
+import java.util.List;
+
+public class Scene {
+    private final List<String> ladderArea;
+    private final String userArea;
+
+    public Scene(List<String> ladderArea, String userArea) {
+        this.ladderArea = ladderArea;
+        this.userArea = userArea;
+    }
+
+    public List<String> getLadderArea() {
+        return ladderArea;
+    }
+
+    public String userArea() {
+        return userArea;
+    }
+}
