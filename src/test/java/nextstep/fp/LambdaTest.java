5f61d239946cae26ad56758a55a0291032d41528
soochan <tncksdl05@gmail.com>
2023-05-10 13:14:00 +0900

    test : 람다식을 적극 활용하여, Interface 구현체 구성

--- src/test/java/nextstep/fp/LambdaTest.java
+++ src/test/java/nextstep/fp/LambdaTest.java
@@ -33,19 +33,109 @@ public class LambdaTest {
 
     @Test
     public void sumAll() throws Exception {
-        int sum = Lambda.sumAll(numbers);
+        Conditional all = new Conditional() {
+            @Override
+            public boolean test(Integer number) {
+                return true;
+            }
+        };
+
+        int sum = Lambda.sumAll(numbers,all);
         assertThat(sum).isEqualTo(21);
     }
 
     @Test
+    public void sumAll_람다활용_인라인() {
+        int sum = Lambda.sumAll(numbers, (number) -> true );
+        assertThat(sum).isEqualTo(21);
+    }
+
+    @Test
+    public void sumAll_람다활용_변수() {
+        Conditional all = (number) -> true;
+        int sum = Lambda.sumAll(numbers, all);
+        assertThat(sum).isEqualTo(21);
+    }
+
+    @Test
+    public void sumAll_람다활용_메서드() {
+        int sum = Lambda.sumAll(numbers, isTrue());
+        assertThat(sum).isEqualTo(21);
+    }
+
+    private Conditional isTrue() {
+        return (number) -> true;
+    }
+
+    @Test
     public void sumAllEven() throws Exception {
-        int sum = Lambda.sumAllEven(numbers);
+        Conditional allEven = new Conditional() {
+            @Override
+            public boolean test(Integer number) {
+                return number % 2 == 0;
+            }
+        };
+
+        int sum = Lambda.sumAllEven(numbers, allEven);
         assertThat(sum).isEqualTo(12);
     }
 
     @Test
+    public void sumAllEven_람다활용_인라인() {
+        int sum = Lambda.sumAllEven(numbers, (number) -> number % 2 == 0);
+        assertThat(sum).isEqualTo(12);
+    }
+
+    @Test
+    public void sumAllEven_람다활용_변수() {
+        Conditional allEven = (number) -> number % 2 == 0;
+        int sum = Lambda.sumAllEven(numbers, allEven);
+        assertThat(sum).isEqualTo(12);
+    }
+
+    @Test
+    public void sumAllEven_람다활용_메서드() {
+        int sum = Lambda.sumAllEven(numbers, isEven());
+        assertThat(sum).isEqualTo(12);
+    }
+
+    private Conditional isEven() {
+        return (number) -> number % 2 == 0;
+    }
+
+    @Test
     public void sumAllOverThree() throws Exception {
-        int sum = Lambda.sumAllOverThree(numbers);
+        Conditional overThree = new Conditional() {
+            @Override
+            public boolean test(Integer number) {
+                return number > 3;
+            }
+        };
+
+        int sum = Lambda.sumAllOverThree(numbers, overThree);
         assertThat(sum).isEqualTo(15);
+    }
+
+    @Test
+    public void sumAllOverThree_람다활용_인라인() {
+        int sum = Lambda.sumAllOverThree(numbers, (number) -> number > 3);
+        assertThat(sum).isEqualTo(15);
+    }
+
+    @Test
+    public void sumAllOverThree_람다활용_변수() {
+        Conditional overThree = (number) -> number > 3;
+        int sum = Lambda.sumAllOverThree(numbers, overThree);
+        assertThat(sum).isEqualTo(15);
+    }
+
+    @Test
+    public void sumAllOverThree_람다활용_메서드() {
+        int sum = Lambda.sumAllOverThree(numbers, isOverThree());
+        assertThat(sum).isEqualTo(15);
+    }
+
+    private Conditional isOverThree() {
+        return (number) -> number > 3;
     }
 }
