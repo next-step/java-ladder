128294c3bd34c6823a45acb9cee6d6affe4c9e6f
Geunwoo Lee <87115015+player-geun@users.noreply.github.com>
2023-05-10 14:17:05 +0900

    Step1 완료 (#1701)
    * refactor : 익명 클래스를 람다로 전환
    
    * refactor : 람다를 활용해 중복 제거
    
    * refactor : map, reduce, filter를 활용해 구현
    
    * refactor : printLongestWordTop100() 메서드를 구현
    
    * refactor : Optional을 활용해 ageIsInRange2() 메소드 구현
    
    * refactor : getUser() 를 stream과 Optional을 활용해 구현
    
    * refactor : stream을 활용하여 of 메소드를 구현

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,7 +28,15 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        List<String> collect = words.stream()
+                .filter(e -> e.length() > 12)
+                .sorted(Comparator.comparing(String::length))
+                .distinct()
+                .limit(100)
+                .map(String::toLowerCase)
+                .collect(Collectors.toList());
+
+        System.out.println(collect);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +48,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(e -> e > 3)
+                .map(e -> e * 2)
+                .reduce(0, Integer::sum);
     }
 }
