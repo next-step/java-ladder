6c7831b19f68125f9689cab09c3885dbf42429d1
dkswnkk <jjoy9810@gmail.com>
2023-05-11 01:14:56 +0900

    feat: 사다리 선 일급 컬렉션 처리

---
+++ src/main/java/nextstep/ladder/domain/Lines.java
@@ -0,0 +1,15 @@
+package nextstep.ladder.domain;
+
+import java.util.List;
+
+public class Lines {
+    private final List<Line> lines;
+
+    public Lines(List<Line> lines) {
+        this.lines = lines;
+    }
+
+    public List<Line> getLines() {
+        return lines;
+    }
+}
