65a18f54eb9a4e9189a2cf452907e1c0bb46d5e3
KimJuHyun <rlawngus0910@naver.com>
2023-05-11 21:14:04 +0900

    feat : 참여자 이름, 사다리 높이 입력 기능

---
+++ src/main/java/nextstep/LadderGame.java
@@ -0,0 +1,11 @@
+package nextstep;
+
+import nextstep.view.InputView;
+
+public class LadderGame {
+
+    public static void main(String[] args) {
+        String participants = InputView.participants();
+        int height = InputView.height();
+    }
+}
