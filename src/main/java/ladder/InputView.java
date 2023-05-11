14f342c73169596815735366eadfdbfbbbc34fd6
Gangbean <xbox2537@gmail.com>
2023-05-12 05:41:55 +0900

    (feat) InputView implementation

---
+++ src/main/java/ladder/InputView.java
@@ -0,0 +1,23 @@
+package ladder;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    public static void askNames() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+    }
+
+    public static String nextString() {
+        return new Scanner(System.in).nextLine();
+    }
+
+    public static void askHeight() {
+        System.out.println("\n최대 사다리 높이는 몇 개 인가요?");
+    }
+
+    public static int nextInt() {
+        return new Scanner(System.in).nextInt();
+    }
+
+}
