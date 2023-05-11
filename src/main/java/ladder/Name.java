a56c7fa51dd5341ec8f3a2ed807f8e2a12b6759b
Gangbean <xbox2537@gmail.com>
2023-05-11 15:24:59 +0900

    (feat) Name implementation

---
+++ src/main/java/ladder/Name.java
@@ -0,0 +1,45 @@
+package ladder;
+
+import java.util.Objects;
+
+public class Name {
+
+    private final String name;
+
+    private Name(String name) {
+        this.name = valid(name);
+    }
+
+    private String valid(String name) {
+        if (isNotValid(name)) {
+            throw new IllegalArgumentException("이름은 빈값일 수 없습니다.");
+        }
+        return name;
+    }
+
+    private boolean isNotValid(String name) {
+        return name == null || name.isBlank();
+    }
+
+    public static Name of(String name) {
+        return new Name(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+}
---
+++ src/test/java/ladder/NameTest.java
@@ -0,0 +1,32 @@
+package ladder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class NameTest {
+
+    @Test
+    void 이름_널() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> Name.of(null))
+            .withMessageMatching("이름은 빈값일 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void 이름_빈값(String input) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> Name.of(input))
+            .withMessageMatching("이름은 빈값일 수 없습니다.");
+    }
+
+    @Test
+    void 이름생성() {
+        assertThat(Name.of("abced")).isEqualTo(Name.of("abced"));
+    }
+
+}
