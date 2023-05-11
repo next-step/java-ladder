8bc759019f09dd7c9875be78bb2b506fcca17a9f
soochan <tncksdl05@gmail.com>
2023-05-11 16:09:40 +0900

    refactor : 사다리 출력 기능 리펙토링

--- src/main/java/ladder/view/output/OutputLaddersView.java
+++ src/main/java/ladder/view/output/OutputLaddersView.java
@@ -9,14 +9,26 @@ public class OutputLaddersView {
   private static final String START_EMPTY_SPACE = "     ";
   private static final String LAST = "|";
 
-  public static void printLadder(Ladders ladders) {
-    IntStream.range(0, ladders.height()).forEach(rowNumber -> {
-      printStartEmptySpace();
-      ladders.getRow(rowNumber).stream()
-          .map(OutputLaddersView::ladder)
-          .forEach(System.out::print);
-      printLastLine();
+  public static void printLadderOfRow(Ladders ladders) {
+    IntStream.range(0, height(ladders)).forEach(rowNumber -> {
+      printLaddersOfRow(ladders, rowNumber);
     });
+  }
+
+  private static int height(Ladders ladders) {
+    return ladders.height();
+  }
+
+  private static void printLaddersOfRow(Ladders ladders, int rowNumber) {
+    printStartEmptySpaceOfRow();
+    printLadderOfRow(ladders, rowNumber);
+    printLastLineOfRow();
+  }
+
+  private static void printLadderOfRow(Ladders ladders, int rowNumber) {
+    ladders.getRow(rowNumber).stream()
+        .map(OutputLaddersView::ladder)
+        .forEach(System.out::print);
   }
 
   private static String ladder(Boolean connection) {
@@ -26,11 +38,11 @@ public class OutputLaddersView {
     return NOT_LADDER;
   }
 
-  private static void printLastLine() {
+  private static void printLastLineOfRow() {
     System.out.println(LAST);
   }
 
-  private static void printStartEmptySpace() {
+  private static void printStartEmptySpaceOfRow() {
     System.out.print(START_EMPTY_SPACE);
   }
 
