b46462f2fd03a6b84362a1f5e874b9cc35fadd92
Gangbean <xbox2537@gmail.com>
2023-05-12 05:38:13 +0900

    (feat) Names implementation

---
+++ src/main/java/ladder/Names.java
@@ -0,0 +1,63 @@
+package ladder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Names {
+
+    public static final String DELIMITER = ",";
+
+    private final List<String> names;
+
+    private Names(List<String> names) {
+        this.names = names;
+    }
+
+    public static Names of(String names) {
+        return of(Arrays.stream(splitString(names))
+            .map(String::trim)
+            .collect(Collectors.toList()));
+    }
+
+    private static String[] splitString(String names) {
+        return names.split(DELIMITER);
+    }
+
+    public static Names of(List<String> names) {
+        return new Names(names);
+    }
+
+    public List<String> names() {
+        return List.copyOf(names);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Names names1 = (Names) o;
+        return Objects.equals(names, names1.names);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(names);
+    }
+
+    public int count() {
+        return names.size();
+    }
+
+    public int lengthSum() {
+        return names.stream()
+            .map(x -> x.length())
+            .reduce(0, (a,b) -> a + b);
+    }
+
+}
---
+++ src/test/java/ladder/NamesTest.java
@@ -0,0 +1,34 @@
+package ladder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class NamesTest {
+
+    @Test
+    void 이름_길이_합() {
+        assertThat(Names.of("a, b, c").lengthSum()).isEqualTo(3);
+    }
+
+    @Test
+    void 이름_개수() {
+        assertThat(Names.of("a,b,c").count()).isEqualTo(3);
+    }
+
+    @Test
+    void 이름목록생성_구분자() {
+        assertThat(Names.of("a,b,c").names()).containsExactly("a", "b", "c");
+    }
+
+    @Test
+    void 이름목록생성() {
+        assertAll(
+            () -> assertThat(Names.of(List.of("a", "b", "c")).names()).containsExactly("a", "b", "c"),
+            () -> assertThat(Names.of(List.of("a", "b", "c"))).isEqualTo(Names.of(List.of("a","b","c")))
+        );
+    }
+
+}
