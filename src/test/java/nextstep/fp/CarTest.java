aa903f415cb5cc911914dc79bd6f575403297c18
soochan <91618389+soochangoforit@users.noreply.github.com>
2023-05-11 18:43:36 +0900

    [Step1] 스트림, 람다, Optional 학습 테스트 리뷰 요청 (#1705)
    * refactor : 람다를 최대한 다양하게 활용
    
    * refactor : 변경 가능성 있는 부분을 Interface로 추출
    
    * test : 람다식을 적극 활용하여, Interface 구현체 구성
    
    * refactor : List에 담긴 모든 숫자 중 3보다 큰 숫자를 2배 한 후 모든 값의 합을 구한다. 지금까지 학습한 map, reduce, filter를 활용해 구현해야 한다.
    
    * feat : map, reduce, filter 실습 2 구현
    
    * feat : 요구사항 1 - Optional을 활용해 조건에 따른 반환 구현
    
    * feat : 요구사항 2 - Optional에서 값을 반환 구현
    
    * feat : 요구사항 3 - Optional에서 exception 처리 구현

--- src/test/java/nextstep/fp/CarTest.java
+++ src/test/java/nextstep/fp/CarTest.java
@@ -18,6 +18,38 @@ public class CarTest {
     }
 
     @Test
+    void 이동_람다활용_인라인() {
+        Car car = new Car("pobi", 0);
+
+        Car actual = car.move(() -> true);
+
+        assertThat(actual).isEqualTo(new Car("pobi", 1));
+    }
+
+    @Test
+    void 이동_람다활용_변수할당() {
+        Car car = new Car("pobi", 0);
+
+        MoveStrategy moveStrategy = () -> true;
+        Car actual = car.move(moveStrategy);
+
+        assertThat(actual).isEqualTo(new Car("pobi", 1));
+    }
+
+    @Test
+    void 이동_람다활용_메서드할당() {
+        Car car = new Car("pobi", 0);
+
+        Car actual = car.move(alwaysMove());
+
+        assertThat(actual).isEqualTo(new Car("pobi", 1));
+    }
+
+    private MoveStrategy alwaysMove() {
+        return () -> true;
+    }
+
+    @Test
     public void 정지() {
         Car car = new Car("pobi", 0);
         Car actual = car.move(new MoveStrategy() {
@@ -27,5 +59,37 @@ public class CarTest {
             }
         });
         assertThat(actual).isEqualTo(new Car("pobi", 0));
+    }
+
+    @Test
+    void 정지_람다활용_인라인() {
+        Car car = new Car("pobi", 0);
+
+        Car actual = car.move(() -> false);
+
+        assertThat(actual).isEqualTo(new Car("pobi", 0));
+    }
+
+    @Test
+    void 정지_람다활용_변수할당() {
+        Car car = new Car("pobi", 0);
+
+        MoveStrategy moveStrategy = () -> false;
+        Car actual = car.move(moveStrategy);
+
+        assertThat(actual).isEqualTo(new Car("pobi", 0));
+    }
+
+    @Test
+    void 정지_람다활용_메서드할당() {
+        Car car = new Car("pobi", 0);
+
+        Car actual = car.move(neverMove());
+
+        assertThat(actual).isEqualTo(new Car("pobi", 0));
+    }
+
+    private MoveStrategy neverMove() {
+        return () -> false;
     }
 }
