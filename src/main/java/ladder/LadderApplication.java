8d780b8e656bdf1c43f67940064b086aa762057c
Gangbean <xbox2537@gmail.com>
2023-05-12 05:42:57 +0900

    (feat) LadderApplication implementation

---
+++ src/main/java/ladder/LadderApplication.java
@@ -0,0 +1,31 @@
+package ladder;
+
+public class LadderApplication {
+
+    public static void main(String[] args) {
+        while(!isEnd()){}
+    }
+
+    private static boolean isEnd() {
+        try {
+            InputView.askNames();
+            String input = InputView.nextString();
+            Names names = Names.of(input);
+
+            InputView.askHeight();
+            int height = InputView.nextInt();
+            Ladder ladder = Ladder.of(height, names.count() - 1);
+
+            int width = names.lengthSum() / names.count();
+
+            OutputView.printResult();
+            OutputView.printNames(names, width);
+            OutputView.printLadder(ladder, width * 2);
+            return true;
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+}
