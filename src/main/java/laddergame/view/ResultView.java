748f3ffdde39680846511b395785567be8ff143c
cohi <tkfka156@gmail.com>
2023-05-13 00:54:44 +0900

    :recycle: refactor: 하드코딩 상수화 및 메서드 분리

--- src/main/java/laddergame/view/ResultView.java
+++ src/main/java/laddergame/view/ResultView.java
@@ -6,6 +6,11 @@ import laddergame.domain.Line;
 
 public class ResultView {
 
+    private static final String WIDTH_LINE = "------";
+    private static final String BLANK = "      ";
+    private static final String HEIGHT_LINE = "|";
+    private static final String NEW_LINE = "\n";
+
     private final LadderGame ladderGame;
 
     public ResultView(final LadderGame ladderGame) {
@@ -17,25 +22,32 @@ public class ResultView {
         System.out.println();
 
         StringBuilder sb = new StringBuilder();
+        printNames(sb);
+        printLadder(sb);
 
-        ladderGame.getNames().forEach(n -> sb.append(n).append("      "));
-        sb.append("\n");
-
-        List<Line> linePerDepth = ladderGame.getLinePerDepth();
-        linePerDepth.forEach(line -> {
-            sb.append("|");
-            line.getPoints().forEach(e -> printLine(sb, e));
-            sb.append("\n");
-        });
         System.out.println(sb);
     }
 
-    private void printLine(final StringBuilder sb, final Boolean point) {
-        if (point) {
-            sb.append("------").append("|");
+    private void printNames(final StringBuilder sb) {
+        ladderGame.getNames().forEach(name -> sb.append(name).append(BLANK));
+        sb.append(NEW_LINE);
+    }
+
+    private void printLadder(final StringBuilder sb) {
+        List<Line> linePerDepth = ladderGame.getLinePerDepth();
+        linePerDepth.forEach(line -> {
+            sb.append(HEIGHT_LINE);
+            line.getPoints().forEach(isTrue -> printLine(sb, isTrue));
+            sb.append(NEW_LINE);
+        });
+    }
+
+    private void printLine(final StringBuilder sb, final Boolean isTrue) {
+        if (isTrue) {
+            sb.append(WIDTH_LINE).append(HEIGHT_LINE);
             return;
         }
-        sb.append("      ").append("|");
+        sb.append(BLANK).append(HEIGHT_LINE);
     }
 }
 
