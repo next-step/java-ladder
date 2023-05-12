ffba3df61327e64df0860b4067ab4080378932f8
didrlgus <rlgusdid@naver.com>
2023-05-12 09:57:27 +0900

    refactor: 3단계 - 리팩토링
    - 메서드 바디 생략된 부분 수정

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -31,10 +31,12 @@ public class Ladder {
 
 	@Override
 	public boolean equals(Object o) {
-		if (this == o)
+		if (this == o) {
 			return true;
-		if (o == null || getClass() != o.getClass())
+		}
+		if (o == null || getClass() != o.getClass()) {
 			return false;
+		}
 		Ladder ladder = (Ladder)o;
 		return Objects.equals(lines, ladder.lines);
 	}
