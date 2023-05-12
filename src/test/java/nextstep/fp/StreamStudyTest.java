181c1723bc631cb6c8c9863447f9825c6dc3ad33
patrickYoo <yhy1045@naver.com>
2023-05-12 12:48:53 +0900

    feat: map, reduce, filter 실습 1 완료

--- src/test/java/nextstep/fp/StreamStudyTest.java
+++ src/test/java/nextstep/fp/StreamStudyTest.java
@@ -40,9 +40,8 @@ public class StreamStudyTest {
     }
 
     @Test
-    public void sumOverThreeAndDouble() throws Exception {
-        numbers = Arrays.asList(3, 1, 6, 2, 4, 8);
-        long sum = StreamStudy.sumOverThreeAndDouble(numbers);
-        assertThat(sum).isEqualTo(36);
+    public void sumOverThreeAndDouble() {
+        assertThat(StreamStudy.sumOverThreeAndDouble(Arrays.asList(3, 1, 6, 2, 4, 8)))
+                .isEqualTo(36);
     }
 }
