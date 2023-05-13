f18a16cfa8d8873cc42094313435eea3f4546b0b
hoyeoon <hoyeon.backend@gmail.com>
2023-05-14 01:05:11 +0900

    refactor: 메서드 분리, 매직넘버 상수화

--- src/main/java/nextstep/ladder/Line.java
+++ src/main/java/nextstep/ladder/Line.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class Line {
 
+    private static final int BEGIN_INDEX = 0;
+    private static final boolean EXIST_POINT = true;
+    private static final boolean NON_EXIST_POINT = false;
     private static final double HALF = 0.5;
 
     private List<Boolean> points = new ArrayList<>();
@@ -14,26 +17,34 @@ public class Line {
     }
 
     public void makeLine(int countOfPerson) {
-        for (int i = 0; i < countOfPerson; i++) {
-            makePoint(i);
+        for (int i = BEGIN_INDEX; i < countOfPerson; i++) {
+            addPoint(i);
         }
     }
 
-    private void makePoint(int idx) {
-        if (idx == 0) {
-            points.add(false);
+    private void addPoint(int idx) {
+        if (BEGIN_INDEX == idx) {
+            points.add(NON_EXIST_POINT);
             return;
         }
 
         if (isMakeable(idx)) {
-            points.add(true);
+            points.add(EXIST_POINT);
             return;
         }
-        points.add(false);
+        points.add(NON_EXIST_POINT);
     }
 
     private boolean isMakeable(int idx) {
-        return points.get(idx - 1) == false && HALF < Math.random();
+        return isPrevPointNonExist(idx) && existCriteria();
+    }
+
+    private boolean isPrevPointNonExist(int idx) {
+        return NON_EXIST_POINT == points.get(idx - 1);
+    }
+
+    private boolean existCriteria() {
+        return HALF < Math.random();
     }
 
     public List<Boolean> points() {
