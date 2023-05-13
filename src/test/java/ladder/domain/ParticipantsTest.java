e1ab90911e6224f7751ca2fc096f3f056365b2e2
soochan <tncksdl05@gmail.com>
2023-05-13 09:39:50 +0900

    test : 테스트 코드에서 List of 사용하도록 변경 (코드 리뷰)

--- src/test/java/ladder/domain/ParticipantsTest.java
+++ src/test/java/ladder/domain/ParticipantsTest.java
@@ -2,7 +2,6 @@ package ladder.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
@@ -10,7 +9,7 @@ import org.junit.jupiter.api.Test;
 class ParticipantsTest {
   @Test
   void 참가자_생성_테스트() {
-    List<Name> names = Arrays.asList(new Name("Alice"), new Name("Bob"), new Name("Carol"));
+    List<Name> names = List.of(new Name("Alice"), new Name("Bob"), new Name("Carol"));
     Participants participants = new Participants(names);
 
     List<Participant> expectedParticipants = names.stream()
