e2f4ec75cadb4593eb23784912f9ecb8a9f717fe
hoyeoon <hoyeon.backend@gmail.com>
2023-05-14 01:46:19 +0900

    test: 글자수 예외처리

--- src/test/java/nextstep/ladder/domain/PersonTest.java
+++ src/test/java/nextstep/ladder/domain/PersonTest.java
@@ -2,6 +2,7 @@ package nextstep.ladder.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class PersonTest {
@@ -11,4 +12,12 @@ public class PersonTest {
         Person pobi = new Person("pobi");
         assertThat(pobi.name()).isEqualTo(new Person("pobi").name());
     }
+
+    @Test
+    void 글자수_5이상시_예외() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Person("hoyeon"))
+                .withMessageContaining("글자수는 최대 5글자까지 부여할 수 있습니다.");
+    }
+
 }
