9e89fe154d79e0e4a34495a1284af713667a364c
patrickYoo <yhy1045@naver.com>
2023-05-14 00:40:39 +0900

    feat: 참가자 데이터를 저장할 수 있는 객체 추가.

--- src/test/java/ladder/ParticipantInputTest.java
+++ src/test/java/ladder/ParticipantInputTest.java
@@ -1,7 +1,10 @@
 package ladder;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
@@ -13,6 +16,11 @@ public class ParticipantInputTest {
         assertThatThrownBy(() -> new ParticipantInput(source))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("참가자 이름은 필수 입력값입니다.");
+    }
 
+    @Test
+    @DisplayName("참가자, 또는 비어있지 않은 문자열 입력시 정상 객체가 생성된다")
+    void objectCreateTest() {
+        assertDoesNotThrow(() -> new ParticipantInput("test,name"));
     }
 }
---
+++ src/test/java/ladder/ParticipantTest.java
@@ -0,0 +1,34 @@
+package ladder;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+public class ParticipantTest {
+
+    @Test
+    @DisplayName("참가자 이름이 유입되었을 경우 정상객체를 생성한다.")
+    void objCreateTest() {
+        assertDoesNotThrow(() -> new Participant("test"));
+    }
+
+    @ParameterizedTest(name = "null or 빈 문자열 유입시 에러를 발생한다.")
+    @NullAndEmptySource
+    void nullAndEmptyTest(String source) {
+        assertThatThrownBy(() -> new Participant(source))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("참가자명이 입력되지 않았습니다.");
+    }
+
+    @Test
+    @DisplayName("참여자 이름이 5글자를 초과했을 경우 익셉션을 발생시킨다.")
+    void tooLongName() {
+        assertThatThrownBy(() -> new Participant("abcdef"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("참가자 이름은 5자를 초과할 수 없습니다.");
+    }
+}
