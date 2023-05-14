f6a3bc11c259242ea9c27d134ce5f5c9b608b3bd
dong <dong.22@kakaoenterprise.com>
2023-05-14 20:31:28 +0900

    test : 테스트코드 추가

--- src/test/java/ladder/domain/LinesTest.java
+++ src/test/java/ladder/domain/LinesTest.java
@@ -26,6 +26,37 @@ public class LinesTest {
         );
     }
 
+    @DisplayName("존재여부 확인")
+    @Test
+    public void exisit() {
+        //given
+        Line line11 = new Line(1, 1);
+        Line line22 = new Line(2, 2);
+        Lines lines = new Lines(Set.of(line11, line22));
+
+        //when
+        Line newLine11 = new Line(Column.of(1), Row.of(1));
+        Line newLine22 = new Line(Column.of(2), Row.of(2));
+        Line line33 = new Line(3, 3);
+        //then
+        assertAll("존재 여부를 확인",
+                () -> assertThat(lines.existLine(line11))
+                        .as("값이 같고, 인스턴스도 같은 Line 이 존재할때 True 를 반환한다")
+                        .isTrue(),
+                () -> assertThat(lines.existLine(line22))
+                        .as("값이 같고, 인스턴스도 같은 Line 이 존재할때 True 를 반환한다")
+                        .isTrue(),
+                () -> assertThat(lines.existLine(newLine11))
+                        .as("값은 같지만, 인스턴스가 다른 Line 이 존재할때 True 를 반환한다")
+                        .isTrue(),
+                () -> assertThat(lines.existLine(newLine22))
+                        .as("값은 같지만, 인스턴스가 다른 Line 이 존재할때 True 를 반환한다")
+                        .isTrue(),
+                () -> assertThat(lines.existLine(line33))
+                        .as("값이 다른 인스턴스는 False 를 반환한다")
+                        .isFalse()
+        );
+    }
 
     @DisplayName("사다리 생성가능 여부를 판단하는 메서드를 검증한다")
     @Test
