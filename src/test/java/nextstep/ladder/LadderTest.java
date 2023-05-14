eff3cdb3cb3fd2d9aa8032e2b49dd53b9c75d441
dkswnkk <jjoy9810@gmail.com>
2023-05-14 21:36:40 +0900

    test: 테스트 코드 작성

--- src/test/java/nextstep/ladder/LadderTest.java
+++ src/test/java/nextstep/ladder/LadderTest.java
@@ -1,10 +1,18 @@
 package nextstep.ladder;
 
-import nextstep.ladder.domain.ParticipantName;
+import nextstep.ladder.domain.LadderResult;
+import nextstep.ladder.domain.LadderResults;
+import nextstep.ladder.domain.Lines;
+import nextstep.ladder.domain.Participant;
+import nextstep.ladder.domain.Participants;
+import nextstep.ladder.domain.strategy.RandomLadderPointGenerateStrategy;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -16,16 +24,7 @@ public class LadderTest {
     void validateParticipantNameLength_test() {
         String name = "NewJeans";
 
-        assertThatThrownBy(() -> new ParticipantName(name))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("참가자 이름이 영문이 아닌 문자가 포함되어 있다면 예외를 던진다.")
-    void validateParticipantAlphabetName_test() {
-        String name = "2NE1";
-
-        assertThatThrownBy(() -> new ParticipantName(name))
+        assertThatThrownBy(() -> new Participant(name))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -33,8 +32,67 @@ public class LadderTest {
     @NullAndEmptySource
     @DisplayName("참가자의 이름이 null이거나 빈 문자일 경우 예외를 던진다.")
     void validateParticipantNameNullAndEmpty_test(String name) {
-        assertThatThrownBy(() -> new ParticipantName(name))
+        assertThatThrownBy(() -> new Participant(name))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("사다리 결과의 입력이 null이거나 빈 문자일 경우 예외를 던진다.")
+    void validateLadderResultNullAndEmpty_test(String result) {
+        assertThatThrownBy(() -> new LadderResult(result))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("참가자 수와 결과 수가 일치하지 않으면 예외를 던진다.")
+    void validateLadderResults_test() {
+        Participants participants = new Participants(List.of(new Participant("a")));
+        String inputLadderResults = "꽝, 1000";
+
+        assertThatThrownBy(() -> new LadderResults(participants, inputLadderResults))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("존재 하지 않는 참가자의 결과를 요구하면 예외를 던진다.")
+    void ladderResultByParticipantThrow_test() {
+        Participants participants = new Participants(List.of(new Participant("a")));
+        String inputLadderResults = "꽝";
+        LadderResults ladderResults = new LadderResults(participants, inputLadderResults);
+
+        assertThatThrownBy(() -> ladderResults.getLadderResultByParticipant(new Participant("b")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("라인이 겹치게 생성되었으면 예외를 던진다.")
+    void shouldNotOverlapLines_test() {
+        int height = 5;
+        int numberOfParticipants = 4;
+
+        assertThatThrownBy(() -> new Lines(numberOfParticipants, height, size -> List.of(true, true, true, true)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("사다리는 높이만큼 라인이 생성되어야 한다.")
+    void tests() {
+        int height = 5;
+        int numberOfParticipants = 4;
+        Lines lines = new Lines(numberOfParticipants, height, new RandomLadderPointGenerateStrategy());
+
+        Assertions.assertThat(height).isEqualTo(lines.getLines().size());
+    }
+
+    @Test
+    @DisplayName("사다리의 라인은 (참가자 수 -1) 만큼 생성되어야 한다.")
+    void testSI() {
+        int height = 5;
+        int numberOfParticipants = 4;
+        Lines lines = new Lines(numberOfParticipants, height, new RandomLadderPointGenerateStrategy());
+
+        lines.getLines().forEach(line -> Assertions.assertThat(numberOfParticipants - 1).isEqualTo(line.getPoints().size()));
     }
 
 }
