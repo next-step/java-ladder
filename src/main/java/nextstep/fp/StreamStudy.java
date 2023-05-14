ffb50494c35141878aff8b024883aff37d3af4d5
Cho <201132036@office.skhu.ac.kr>
2023-05-14 13:33:23 +0900

    [step1] feat : 스트림, 람다, Optional 실습 (#1690)
    Co-authored-by: whdms705 <whdms705@nate.com>

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,10 +5,13 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
+    public static final int MIN_WORD_LENGTH = 12;
+    public static final int MAX_WORD_COUNT = 100;
 
     public static long countWords() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
@@ -27,7 +30,17 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        List<String> longWords =
+                words
+                .stream()
+                .filter(word -> word.length() > MIN_WORD_LENGTH)
+                .distinct()
+                .sorted(Comparator.comparingInt(String::length).reversed())
+                .limit(MAX_WORD_COUNT)
+                .map(String::toLowerCase)
+                .collect(Collectors.toList());
+
+        longWords.forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +52,10 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers
+                .stream()
+                .filter(number -> number > 3)
+                .mapToLong(number -> number * 2)
+                .sum();
     }
 }
