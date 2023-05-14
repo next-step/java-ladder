34de9735493eb098dcfbb00cd75161e9e2d4d75b
dkswnkk <jjoy9810@gmail.com>
2023-05-14 21:34:51 +0900

    feat: 겹치는 선 검증 로직 추가

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -1,16 +1,28 @@
 package nextstep.ladder.domain;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Line {
     private final List<Boolean> points;
 
     public Line(int countOfParticipant, GenerateLadderPointStrategy generateLadderPointStrategy) {
         this.points = generateLadderPointStrategy.generate(countOfParticipant);
+
+        validateOverlap();
     }
 
     public List<Boolean> getPoints() {
         return points;
+    }
+
+    private void validateOverlap() {
+        IntStream.range(0, points.size() - 1)
+                .filter(i -> points.get(i) && points.get(i + 1))
+                .findAny()
+                .ifPresent(i -> {
+                    throw new IllegalArgumentException("겹치게 생성된 라인이 존재합니다.");
+                });
     }
 
 }
