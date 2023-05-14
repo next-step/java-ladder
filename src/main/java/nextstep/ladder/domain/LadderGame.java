7703773239029a5cf57ebe52fc5a6bcebf447828
dkswnkk <jjoy9810@gmail.com>
2023-05-14 18:06:45 +0900

    refactor: 사다리 생성 책임자 변경

--- src/main/java/nextstep/ladder/domain/LadderGame.java
+++ src/main/java/nextstep/ladder/domain/LadderGame.java
@@ -1,24 +1,12 @@
 package nextstep.ladder.domain;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 public class LadderGame {
     private final ParticipantNames participantNames;
     private final Lines lines;
 
     public LadderGame(ParticipantNames participantNames, Height height, GenerateLadderPointStrategy generateLadderPointStrategy) {
         this.participantNames = participantNames;
-        this.lines = setLines(participantNames.size(), height, generateLadderPointStrategy);
-    }
-
-    private static Lines setLines(int countOfParticipant, Height height, GenerateLadderPointStrategy generateLadderPointStrategy) {
-        List<Line> lines = IntStream.range(0, height.getHeight())
-                .mapToObj(i -> new Line(countOfParticipant, generateLadderPointStrategy))
-                .collect(Collectors.toList());
-
-        return new Lines(lines);
+        this.lines = new Lines(participantNames.size(), height, generateLadderPointStrategy);
     }
 
     public ParticipantNames getParticipantNames() {
--- src/main/java/nextstep/ladder/domain/Lines.java
+++ src/main/java/nextstep/ladder/domain/Lines.java
@@ -1,15 +1,25 @@
 package nextstep.ladder.domain;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Lines {
     private final List<Line> lines;
 
-    public Lines(List<Line> lines) {
-        this.lines = lines;
+    public Lines(int countOfParticipant, Height height, GenerateLadderPointStrategy generateLadderPointStrategy) {
+        this.lines = setLines(countOfParticipant, height, generateLadderPointStrategy);
+    }
+
+    private List<Line> setLines(int countOfParticipant, Height height, GenerateLadderPointStrategy generateLadderPointStrategy) {
+        return IntStream.range(0, height.getHeight())
+                .mapToObj(i -> new Line(countOfParticipant, generateLadderPointStrategy))
+                .collect(Collectors.toList());
     }
 
     public List<Line> getLines() {
-        return lines;
+        return Collections.unmodifiableList(lines);
     }
+
 }
