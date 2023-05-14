0536fa6ede4dfc598717872de4b81c6a78be5d80
dkswnkk <jjoy9810@gmail.com>
2023-05-14 21:36:14 +0900

    feat: LottoController 로작 추가

--- src/main/java/nextstep/ladder/controller/LadderController.java
+++ src/main/java/nextstep/ladder/controller/LadderController.java
@@ -1,7 +1,8 @@
 package nextstep.ladder.controller;
 
 import nextstep.ladder.domain.Height;
-import nextstep.ladder.domain.LadderGame;
+import nextstep.ladder.domain.LadderResults;
+import nextstep.ladder.domain.Lines;
 import nextstep.ladder.domain.Participants;
 import nextstep.ladder.domain.strategy.RandomLadderPointGenerateStrategy;
 import nextstep.ladder.view.InputView;
@@ -11,11 +12,16 @@ public class LadderController {
 
     public static void main(String[] args) {
         Participants participants = InputView.inputParticipantsNames();
+        String inputLadderResults = InputView.inputLadderResults();
         Height height = InputView.inputMaxLadderHeight();
 
-        LadderGame ladderGame = new LadderGame(participants, height, new RandomLadderPointGenerateStrategy());
+        Lines lines = new Lines(participants.size(), height.getHeight(), new RandomLadderPointGenerateStrategy());
+        LadderResults ladderResults = new LadderResults(participants, inputLadderResults);
 
-        OutputView.printParticipantNames(ladderGame.getParticipantNames());
-        OutputView.printLadders(ladderGame.getLines());
+        OutputView.printParticipantNames(participants);
+        OutputView.printLadders(lines);
+        OutputView.printLadderResults(ladderResults);
+
+        OutputView.printResultByParticipant(InputView.inputParticipantNameForResult(), ladderResults);
     }
 }
