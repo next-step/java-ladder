c2698f37e0675b7317e294bcf326e042a0bf2649
dkswnkk <jjoy9810@gmail.com>
2023-05-14 18:07:48 +0900

    refactor: 요구사항 아닌 로직 제거

--- src/main/java/nextstep/ladder/domain/ParticipantName.java
+++ src/main/java/nextstep/ladder/domain/ParticipantName.java
@@ -2,7 +2,6 @@ package nextstep.ladder.domain;
 
 public class ParticipantName {
     private static final int MAX_NAME_LENGTH = 5;
-    private static final String ALPHABET = "^[a-zA-Z]*$";
     private final String name;
 
     public ParticipantName(String name) {
@@ -21,9 +20,6 @@ public class ParticipantName {
         }
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException(String.format("이름은 최대 %d 길이까지 부여할 수 있습니다.", MAX_NAME_LENGTH));
-        }
-        if (!name.matches(ALPHABET)) {
-            throw new IllegalArgumentException("이름은 알파벳만 부여할 수 있습니다.");
         }
     }
 
