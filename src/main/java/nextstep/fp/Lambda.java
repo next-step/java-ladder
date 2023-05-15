e823e504f43efdf18d972a6ae97c8a9bf130248f
lily.yu <lily.yu@kakaocorp.com>
2023-05-15 18:02:59 +0900

    feat: stream 실습

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -26,31 +26,10 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
-    }
-
-    public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
-    }
-
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+    public static int sumAllBy(List<Integer> numbers, Conditional conditional) { //number -> number > 3
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .filter(conditional::filter)
+                .sum();
     }
 }
