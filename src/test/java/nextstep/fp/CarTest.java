c4238a0484853ea69a97171467ea179152a63fb2
황재원 <50973479+jaewon0913@users.noreply.github.com>
2023-05-15 17:44:16 +0900

    Step1 - 스트림, 람다, Optional (#1730)
    * Feat(Ladder) : Step1 스트림, 람다, Optional
    
    - 기능 요구 사항 작성
    
    * Feat(Ladder) : Step1 스트림, 람다, Optional
    
    - 익명 클래스를 람다로 전환
    
    * Feat(Ladder) : Step1 스트림, 람다, Optional
    
    - 람다를 활용해 중복 제거
    
    * Feat(Ladder) : Step1 스트림, 람다, Optional
    
    - map, reduce, filter 실습 1
    
    * Feat(Ladder) : Step1 스트림, 람다, Optional
    
    - map, reduce, filter 실습 2
    
    * Feat(Ladder) : Step1 스트림, 람다, Optional
    
    - Optional을 활용해 조건에 따른 반환
    
    * Feat(Ladder) : Step1 스트림, 람다, Optional
    
    - Optional에서 값을 반환
    
    * Feat(Ladder) : Step1 스트림, 람다, Optional
    
    - Optional에서 exception 처리

--- src/test/java/nextstep/fp/CarTest.java
+++ src/test/java/nextstep/fp/CarTest.java
@@ -8,24 +8,16 @@ public class CarTest {
     @Test
     public void 이동() {
         Car car = new Car("pobi", 0);
-        Car actual = car.move(new MoveStrategy() {
-            @Override
-            public boolean isMovable() {
-                return true;
-            }
-        });
+        Car actual = car.move(() -> ((MoveStrategy) () -> true).isMovable());
+
         assertThat(actual).isEqualTo(new Car("pobi", 1));
     }
 
     @Test
     public void 정지() {
         Car car = new Car("pobi", 0);
-        Car actual = car.move(new MoveStrategy() {
-            @Override
-            public boolean isMovable() {
-                return false;
-            }
-        });
+        Car actual = car.move(() -> ((MoveStrategy) () -> false).isMovable());
+
         assertThat(actual).isEqualTo(new Car("pobi", 0));
     }
 }
---
+++ step1.md
@@ -0,0 +1,8 @@
+# STEP1 : 1단계 - 스트림, 람다, Optional
+
+- [ ] test : 람다 실습 1 - 익명 클래스를 람다로 전환 연습 
+- [ ] refactor : 람다 실습 2 - 람다를 활용해 중복 제거
+- [ ] refactor : 다양한 stream method 실습
+- [ ] test : Optional을 활용해 조건에 따른 반환
+- [ ] refactor : Optional에서 값을 반환
+- [ ] refactor : Optional에서 exception 처리
