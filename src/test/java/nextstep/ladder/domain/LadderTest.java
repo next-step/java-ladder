14fe3a55be312b2f2a201efe5c128fae76031a43
juno-junho <ssmm0205@naver.com>
2023-05-15 16:27:46 +0900

    test(domain): 테스트 이름 및 구조 리펙토링

--- src/test/java/nextstep/ladder/domain/LadderTest.java
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -1,24 +1,22 @@
 package nextstep.ladder.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class LadderTest {
 
+    @DisplayName("참여자와 사다리 높이가 주어졌을 때 사다리 높이 만큼의 Line 리스트를 생성한다.")
     @Test
-    void 높이와_참여자가_주어졌을때_높이와_참여자에_따른_크기의_리스트를_생성한다() {
+    void when_ParticipantsAndHeightOfLadderIsGiven_Expect_HeightSizeOfLineList() {
         List<String> participants = List.of("pobi", "honux", "crong", "jk");
         int ladderHeight = 5;
 
         List<Line> ladder = Ladder.of(participants, ladderHeight);
 
-        assertAll(
-                () -> assertThat(ladder).hasSize(ladderHeight),
-                () -> assertThat(ladder.get(0).size()).isEqualTo(participants.size() - 1)
-        );
+        assertThat(ladder).hasSize(ladderHeight);
     }
 }
--- src/test/java/nextstep/ladder/domain/LineTest.java
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -4,18 +4,17 @@ import nextstep.ladder.domain.strategy.BridgeStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 
 class LineTest {
 
-    @DisplayName("중복된 다리가 있을 경우 예외를 발생한다.")
+    @DisplayName("중복된 다리가 있을 경우 중복된 다리를 삭제한다.")
     @Test
-    void when_hasDuplicateBridge_Expect_throwException() {
-        int width = 5;
-
+    void when_hasDuplicateBridge_Expect_EraseBridge() {
+        int width = 10;
         BridgeStrategy strategy = () -> true;
 
-        assertThatThrownBy(() -> new Line(width, strategy))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatNoException()
+                .isThrownBy(() -> new Line(width, strategy));
     }
 }
