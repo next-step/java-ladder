271399f0af892ee882bce3ef2b1ec61bc3f2f463
juno-junho <ssmm0205@naver.com>
2023-05-15 16:35:02 +0900

    test(domain): 사다리 높이와 넓이 validate 메서드 추가

--- src/test/java/nextstep/ladder/domain/LadderTest.java
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LadderTest {
 
@@ -18,5 +19,26 @@ public class LadderTest {
         List<Line> ladder = Ladder.of(participants, ladderHeight);
 
         assertThat(ladder).hasSize(ladderHeight);
+    }
+
+    @DisplayName("참여자가 1명 이하면 예외를 발생한다.")
+    @Test
+    void when_ParticipantsAreUnderTwo_Expects_ThrowException() {
+        List<String> participants = List.of("pobi");
+        int ladderHeight = 5;
+
+
+        assertThatThrownBy(() -> Ladder.of(participants, ladderHeight))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("높이가 1 미만이면 예외를 발생한다.")
+    @Test
+    void when_LadderHeightIsLessThanOne_Expects_ThrowException() {
+        List<String> participants = List.of("pobi", "crong", "honux", "jk");
+        int ladderHeight = 0;
+
+        assertThatThrownBy(() -> Ladder.of(participants, ladderHeight))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
