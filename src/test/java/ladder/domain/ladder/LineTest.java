5cc8bb29b1fadbcf7c71f47d6f5d94c4b208df75
soochan <tncksdl05@gmail.com>
2023-05-16 08:55:32 +0900

    test : line 테스트 코드 수정

--- src/test/java/ladder/domain/ladder/LineTest.java
+++ src/test/java/ladder/domain/ladder/LineTest.java
@@ -3,19 +3,15 @@ package ladder.domain.ladder;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
-import ladder.domain.ladder.Line;
-import ladder.domain.strategy.NextPointGenerationStrategy;
+import ladder.domain.strategy.RandomPointGenerationStrategy;
 import org.junit.jupiter.api.Test;
 
 class LineTest {
 
 
   @Test
-  void 랜덤한_boolean이_나오는_함수에서_항상_true가_나오더라도_연속하지_않은_사다리가_나오는것을_검증() {
-    int numberOfParticipants = 10;
-    NextPointGenerationStrategy generationStrategy = () -> true;
-
-    Line line = Line.createLine(numberOfParticipants, generationStrategy);
+  void 하나의_row에_true가_연속해서_나오지_않음_검증() {
+    Line line = Line.createLine(10, new RandomPointGenerationStrategy());
 
     List<Boolean> points = line.getPoints();
     boolean previousConnection = points.get(0);
@@ -29,28 +25,28 @@ class LineTest {
 
   @Test
   void 오른쪽이_연결된_경우_boolean_true_검증() {
-    Line line = Line.createLine(3, () -> true);
+    Line line = new Line(true, false, true);
 
     assertThat(line.isRightConnected(0)).isTrue();
   }
 
   @Test
   void 사다리_마지막_인덱스는_오른쪽이_연결되지_않음_검증() {
-    Line line = Line.createLine(3, () -> true);
+    Line line = new Line(true, false, true);
 
-    assertThat(line.isRightConnected(2)).isFalse();
+    assertThat(line.isRightConnected(3)).isFalse();
   }
 
   @Test
   void 왼쪽이_연결된_경우_boolean_true_검증() {
-    Line line = Line.createLine(3, () -> true);
+    Line line = new Line(true, false, true);
 
     assertThat(line.isLeftConnected(1)).isTrue();
   }
 
   @Test
   void 사다리_첫_인덱스는_왼쪽이_연결되지_않음_검증() {
-    Line line = Line.createLine(3, () -> true);
+    Line line = new Line(true, false, true);
 
     assertThat(line.isLeftConnected(0)).isFalse();
   }
