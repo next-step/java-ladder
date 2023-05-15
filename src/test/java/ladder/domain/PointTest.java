cc390a1a09a156c62fd84eee8c300b9097cc3c9d
right1203 <right1203@gmail.com>
2023-05-15 14:00:02 +0900

    test: move 테스트

--- src/test/java/ladder/domain/PointTest.java
+++ src/test/java/ladder/domain/PointTest.java
@@ -2,6 +2,8 @@ package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -47,5 +49,20 @@ class PointTest {
         Point first = Point.first(true);
         Point last = first.last();
         assertThat(last).isEqualTo(new Point(true, false));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"true, false, -1", "false, true, 1", "false, false, 0"}, delimiter = ',')
+    @DisplayName("move로 이동 확인")
+    void move(boolean left, boolean right, int expected) {
+        // given
+        Point first = Point.first(left);
+        Point next = first.next(() -> right);
+        Point last = next.last();
+
+        // when
+        int actual = next.move();
+
+        assertThat(actual).isEqualTo(expected);
     }
 }
