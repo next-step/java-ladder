03c57ae48e952dae69291a1cf93014f455a45c4c
right1203 <right1203@gmail.com>
2023-05-15 13:07:28 +0900

    feat: Controller 개발

--- src/main/java/ladder/controller/LadderController.java
+++ src/main/java/ladder/controller/LadderController.java
@@ -1,13 +1,12 @@
 package ladder.controller;
 
-import ladder.domain.Height;
-import ladder.domain.Ladder;
-import ladder.domain.RandomDrawStrategy;
-import ladder.domain.Users;
+import ladder.domain.*;
 import ladder.view.InputView;
 import ladder.view.OutputView;
 
 public class LadderController {
+    private static final String ALL_MESSAGE = "all";
+    private static final String EXIT_MESSAGE = "exit";
     private final InputView inputView;
     private final OutputView outputView;
 
@@ -18,7 +17,29 @@ public class LadderController {
 
     public void startGame() {
         Ladder ladder = createLadder();
-        outputView.printLadder(ladder);
+        Result result = new Result(inputView.readResult());
+        outputView.printLadder(ladder, result);
+        printResultUntilExit(ladder, result);
+    }
+
+    private void printResultUntilExit(Ladder ladder, Result result) {
+        boolean endFlag = false;
+        while (!endFlag) {
+            endFlag = printResultOrEnd(ladder, result);
+        }
+    }
+
+    private boolean printResultOrEnd(Ladder ladder, Result result) {
+        String userName = inputView.readUserName();
+        if (userName.equals(ALL_MESSAGE)) {
+            outputView.printAllResult(ladder, result);
+            return false;
+        }
+        if (userName.equals(EXIT_MESSAGE)) {
+            return true;
+        }
+        outputView.printUserResult(new User(userName), ladder, result);
+        return false;
     }
 
     private Ladder createLadder() {
