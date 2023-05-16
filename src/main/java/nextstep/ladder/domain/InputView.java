9ab92ca896c06494fbb1253afada423b1d5d6fbc
seongjo24 <kangsj24@naver.com>
2023-05-17 02:00:47 +0900

    추가 요구사항 입출력 추가

--- src/main/java/nextstep/ladder/domain/InputView.java
+++ src/main/java/nextstep/ladder/domain/InputView.java
@@ -1,6 +1,7 @@
 package nextstep.ladder.domain;
 
 import nextstep.ladder.dto.LadderInput;
+import nextstep.ladder.dto.ResultInput;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,11 +12,28 @@ public class InputView {
     public LadderInput initRadderInformation() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        List<String> users = inputToUsers(scanner.next());
+        List<String> users = StringToList(scanner.next());
         nameLengthValidate(users);
+        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        List<String> results = StringToList(scanner.next());
+        validateResultsSize(users, results);
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         int height = Optional.ofNullable(scanner.nextInt()).orElseThrow(() -> new IllegalArgumentException("최대 사다리 높이는 몇 개인가요?"));
-        return new LadderInput(users, height);
+        return new LadderInput(users, height, results);
+    }
+
+    public void validateResultsSize(List<String> users, List<String> results) {
+        if (users.size() != results.size()) {
+            throw new IllegalArgumentException("사람수와 결과가 맞지 않습니다.");
+        }
+    }
+
+    public ResultInput initResultInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("");
+        System.out.println("결과를 보고 싶은 사람은?");
+        String name = scanner.next();
+        return new ResultInput(name);
     }
 
     public void nameLengthValidate(List<String> participants) {
@@ -25,7 +43,7 @@ public class InputView {
                 .orElseThrow(() -> new IllegalArgumentException("이름은 최대 5글자 최소1글자 입니다."));
     }
 
-    public List<String> inputToUsers(String input) {
+    public List<String> StringToList(String input) {
         return Arrays.asList(input.split(","));
     }
 }
--- src/main/java/nextstep/ladder/domain/ResultView.java
+++ src/main/java/nextstep/ladder/domain/ResultView.java
@@ -5,16 +5,44 @@ public class ResultView {
     private static final String COLUMN = "|";
     private static final String LINE = "_____";
     private static final String EMPTY_LINE = "     ";
+    private Ladder ladder;
 
-    public void printLadder(Ladder ladder) {
+
+    public ResultView(Ladder ladder) {
+        this.ladder = ladder;
+    }
+
+    public ResultView() {
+    }
+
+    public void printLadder() {
         for (User user : ladder.getUsers().getUsers()) {
             System.out.print(user.getName() + EMPTY_LINE);
         }
         System.out.println("");
-        for (Row row : ladder.getPoints().getPoints()) {
+        for (Row row : ladder.getRows().getPoints()) {
             printRow(row);
             System.out.println("");
         }
+        for (String result : ladder.getResults().getResults()) {
+            System.out.print(result + EMPTY_LINE);
+        }
+    }
+
+    public void printResult(String name, boolean isAll) {
+        System.out.println("실행결과");
+        System.out.println(getResult(name, isAll));
+    }
+
+    public String getResult(String name, boolean isAll) {
+        String result = "";
+        if (isAll) {
+            for (User user : ladder.getUsers().getUsers()) {
+                result += user.getName() + " : " + user.getResult() + "\n";
+            }
+            return result;
+        }
+        return name + " : " + ladder.getUsers().getResultByName(name);
     }
 
     public void printRow(Row row) {
