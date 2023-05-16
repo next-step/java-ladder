022dc307c1b58b46e120d0251dcdf8a9b7257a8a
eohyemin <eohyemin@gmail.com>
2023-05-16 17:42:35 +0900

    refactor(Point): 좌,우 값 Direction으로 변경

--- src/main/java/ladder/model/Point.java
+++ src/main/java/ladder/model/Point.java
@@ -4,46 +4,38 @@ import java.util.Objects;
 
 public class Point {
     private final Position position;
-    private final boolean left;
-    private final boolean right;
+    private final Direction direction;
 
-    public Point(Position position, boolean left, boolean right) {
-        validatePoint(left, right);
+    public Point(Position position, Direction direction) {
         this.position = position;
-        this.left = left;
-        this.right = right;
+        this.direction = direction;
     }
 
-    private void validatePoint(boolean left, boolean right) {
-        if (left && right) {
-            throw new IllegalArgumentException("사다리 한 위치에 가로 라인이 겹칠 수 없습니다.");
-        }
+    public Point(int position, Direction direction) {
+        this(new Position(position), direction);
     }
 
     public Point(int position, boolean left, boolean right) {
-        this(new Position(position), left, right);
+        this(new Position(position), new Direction(left, right));
     }
 
     public static Point createFirst(boolean right) {
-        return new Point(0, false, right);
+        return new Point(0, Direction.createFirst(right));
     }
 
     public Point createNext(boolean right) {
-        if (this.right) {
-            return new Point(this.position.right(), true, false);
-        }
-        return new Point(this.position.right(), false, right);
+        return new Point(this.position.right(), this.direction.createNext(right));
     }
 
     public Point createLast() {
-        return new Point(this.position.right(), this.right, false);
+        return new Point(this.position.right(), this.direction.createLast());
     }
 
     public int move() {
-        if (left) {
+        if (direction.hasLeft()) {
             return position.left();
         }
-        if (right) {
+        if (direction.hasRight()) {
             return position.right();
         }
         return position.getPosition();
@@ -53,23 +45,16 @@ public class Point {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         Point point = (Point) o;
-
-        if (left != point.left) return false;
-        if (right != point.right) return false;
-        return Objects.equals(position, point.position);
+        return Objects.equals(position, point.position) && Objects.equals(direction, point.direction);
     }
 
     @Override
     public int hashCode() {
-        int result = position != null ? position.hashCode() : 0;
-        result = 31 * result + (left ? 1 : 0);
-        result = 31 * result + (right ? 1 : 0);
-        return result;
+        return Objects.hash(position, direction);
     }
 
     public boolean getLeft() {
-        return this.left;
+        return this.direction.hasLeft();
     }
 }
