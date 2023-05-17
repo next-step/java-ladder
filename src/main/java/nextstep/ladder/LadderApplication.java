6d4c229309cfc1cf99034f360386ab4e4632ea87
ryan <wsdf25867@gmail.com>
2023-05-18 00:56:21 +0900

    fix : Application, ResultView 수정

--- src/main/java/nextstep/ladder/LadderApplication.java
+++ src/main/java/nextstep/ladder/LadderApplication.java
@@ -1,20 +1,30 @@
 package nextstep.ladder;
 
+import nextstep.ladder.domain.ExecutionResult;
+import nextstep.ladder.domain.Gamers;
 import nextstep.ladder.domain.Ladder;
-import nextstep.ladder.domain.Names;
 import nextstep.ladder.domain.RandomConnector;
 import nextstep.ladder.view.InputView;
 import nextstep.ladder.view.ResultView;
 
+import java.util.List;
+
 public class LadderApplication {
     public static void main(String[] args) {
-        String names = InputView.inputNames();
-        Names gamers = Names.of(names);
+        List<String> names = InputView.inputNames();
+        Gamers gamers = Gamers.from(names);
+        List<String> executionResult = InputView.inputExecutionResult();
 
         int height = InputView.inputLadderHeight();
         Ladder ladder = Ladder.ofHeightAndCountOfPerson(height, gamers.size(), new RandomConnector());
 
-        ResultView.printNames(gamers.toDto());
+        ResultView.printGamers(gamers.toDto());
         ResultView.printLadder(ladder.toDto());
+        ResultView.printResult(executionResult);
+
+        Gamers climbedGamers = gamers.climb(ladder);
+        ExecutionResult result = ExecutionResult.of(climbedGamers, executionResult);
+        String target = InputView.inputResultTarget();
+        ResultView.printResultTarget(result.toDto(), target);
     }
 }
