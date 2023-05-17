7f3e6c7988da16c9ce03b3d514fb941b78370e98
player-geun <player_geun@naver.com>
2023-05-17 10:32:26 +0900

    feat : Username의 일급 객체인 Usernames 을 생성

---
+++ src/main/java/nextstep/domain/Usernames.java
@@ -0,0 +1,27 @@
+package nextstep.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Usernames {
+
+    private final List<Username> usernames;
+
+    public Usernames(List<String> usernames) {
+        this.usernames = toUsernames(usernames);
+    }
+
+    private List<Username> toUsernames(List<String> usernames) {
+        return usernames.stream()
+                .map(Username::new)
+                .collect(Collectors.toList());
+    }
+
+    public int size() {
+        return usernames.size();
+    }
+
+    public List<Username> getUsernames() {
+        return usernames;
+    }
+}
