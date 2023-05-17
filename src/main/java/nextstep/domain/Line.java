4c7b34b1652d21de444c513e355977d8d5a489d2
player-geun <player_geun@naver.com>
2023-05-17 15:38:13 +0900

    refactor : 사다리 타기할 방향을 구하는 기능 추가 구현

--- src/main/java/nextstep/domain/Line.java
+++ src/main/java/nextstep/domain/Line.java
@@ -4,10 +4,30 @@ import java.util.List;
 
 public class Line {
 
+    public static final int DOWN = 0;
+    public static final int LEFT = -1;
+    public static final int RIGHT = 1;
+
     private final List<Boolean> points;
 
     public Line(LineCreateStrategy strategy, int peopleCount) {
         this.points = strategy.create(peopleCount - 1);
+    }
+
+    public Line(List<Boolean> points) {
+        this.points = points;
+    }
+
+    public int getDirection(int start) {
+        if (start < points.size() && points.get(start)) {
+            return RIGHT;
+        }
+
+        if (start - 1 >= 0 && points.get(start - 1)) {
+            return LEFT;
+        }
+
+        return DOWN;
     }
 
     public List<Boolean> getPoints() {
