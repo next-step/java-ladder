e6fb4495bfeab59de84bc0052565c7f2577e575c
player-geun <player_geun@naver.com>
2023-05-17 15:39:11 +0900

    feat : 추가된 요구사항에 맞게 controller 변경

--- src/main/java/nextstep/controller/LadderGameController.java
+++ src/main/java/nextstep/controller/LadderGameController.java
@@ -1,18 +1,38 @@
 package nextstep.controller;
 
-import nextstep.domain.Lines;
-import nextstep.domain.Usernames;
+import nextstep.domain.*;
 import nextstep.view.InputView;
 import nextstep.view.OutputView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class LadderGameController {
 
     public static void main(String[] args) {
         Usernames usernames = new Usernames(InputView.inputUserNames());
+        InputResults inputResults
+                = new InputResults(InputView.inputLadderResult(), usernames.size());
         int ladderHeight = InputView.inputLadderHeightMax();
 
         Lines lines = new Lines(usernames.size(), ladderHeight);
         OutputView.outputUserNames(usernames.getUsernames());
         OutputView.outputLadder(lines);
+        OutputView.outputLadderResult(inputResults.getResults());
+
+        Ladder ladder = new Ladder(lines, usernames.size());
+        List<Integer> ladderResults = getLadderResult(ladder, usernames);
+        OutputView.outputExecutionResult(ladderResults, inputResults, usernames);
+        ladderResults = getLadderResult(ladder, usernames);
+        OutputView.outputExecutionResult(ladderResults, inputResults, usernames);
+    }
+
+    private static List<Integer> getLadderResult(Ladder ladder, Usernames usernames) {
+        String input = InputView.inputUsernameForResult();
+
+        if (input.equals("all")) {
+            return ladder.getResults();
+        }
+        return List.of(ladder.getResult(new Username(input), usernames));
     }
 }
