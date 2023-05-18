de777cf2bcd37bd1eb812cd994664fd9974e1c26
dong <dong.22@kakaoenterprise.com>
2023-05-19 01:22:49 +0900

    test : 불필요한 테스트이지만 주석 용도로 추가

---
+++ src/test/java/ladder/domain/SceneTest.java
@@ -0,0 +1,16 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SceneTest {
+    @Disabled
+    @DisplayName("데이터 전달용 객체이므로 로직이 존재하지 않아 테스트를 작성하지 않았습니다")
+    @Test
+    public void placeHolder() {
+        //given
+        //when
+        //then
+    }
+}
