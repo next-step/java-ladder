e9828b80830035219326e0c9e78038aa905bc46f
hoa0217 <gjwjdghk123@naver.com>
2023-05-18 12:56:56 +0900

    feature: Ladder 출력 시 사람 이름도 함께 출력한다.

--- src/main/java/nextstep/laddergame/ApplicationGame.java
+++ src/main/java/nextstep/laddergame/ApplicationGame.java
@@ -1,14 +1,22 @@
 package nextstep.laddergame;
 
-import nextstep.laddergame.domain.Height;
+import nextstep.laddergame.domain.Ladder;
+import nextstep.laddergame.domain.LadderInfo;
 import nextstep.laddergame.domain.Members;
+import nextstep.laddergame.domain.strategy.LineStrategy;
+import nextstep.laddergame.domain.strategy.LineStrategyNotTwoWay;
+import nextstep.laddergame.domain.strategy.decorator.LineStrategyRandom;
 import nextstep.laddergame.ui.InputView;
+import nextstep.laddergame.ui.OutputView;
 
 public class ApplicationGame {
 
   public static void main(String[] args) {
     Members members = InputView.inputMembers();
-    Height height = InputView.inputLadderHeight();
+    LadderInfo ladderInfo = InputView.inputLadderHeight(members.getSize());
+    LineStrategy lineStrategy = new LineStrategyRandom(new LineStrategyNotTwoWay());
+    Ladder ladder = ladderInfo.generateLadder(lineStrategy);
 
+    OutputView.printResult(members, ladder);
   }
 }
---
+++ src/main/java/nextstep/laddergame/ui/OutputView.java
@@ -0,0 +1,42 @@
+package nextstep.laddergame.ui;
+
+import nextstep.laddergame.domain.Ladder;
+import nextstep.laddergame.domain.Member;
+import nextstep.laddergame.domain.Members;
+import nextstep.laddergame.domain.Point;
+import nextstep.laddergame.domain.Row;
+
+public class OutputView {
+
+  private OutputView() {
+  }
+
+  public static void printResult(Members members, Ladder ladder) {
+    printMembers(members);
+    printLadder(ladder);
+  }
+
+  private static void printLadder(Ladder ladder) {
+    for (Row row : ladder.getRows()) {
+      for (Point point : row.getPoints()) {
+        if (point.checkLeftWay()) {
+          System.out.print("-".repeat(5));
+        } else {
+          System.out.print(" ".repeat(5));
+        }
+        System.out.print("|");
+      }
+      System.out.println();
+    }
+  }
+
+  private static void printMembers(Members members) {
+    for (Member member : members.getMembers()) {
+      String name = member.getName();
+      String blank = " ".repeat(6 - name.length());
+      System.out.print(blank + name);
+    }
+    System.out.println();
+  }
+
+}
