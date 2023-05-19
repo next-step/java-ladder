145a0da1d0fc4744d7ee284a1130ffa9108d6516
cohi <tkfka156@gmail.com>
2023-05-19 22:29:37 +0900

    :sparkles: feat: 사다리 생성 컨테이너 구현

--- src/main/java/refactoring/LadderGameRunner.java
+++ src/main/java/refactoring/LadderGameRunner.java
@@ -3,6 +3,7 @@ package refactoring;
 import java.util.Scanner;
 import refactoring.domain.Ladder;
 import refactoring.domain.LadderFatory;
+import refactoring.domain.LineFactory;
 import refactoring.domain.Match;
 import refactoring.domain.MatchResults;
 import refactoring.domain.MyLadderFactory;
@@ -12,33 +13,52 @@ import refactoring.utils.CommaSplit;
 import refactoring.view.InputView;
 import refactoring.domain.Players;
 import refactoring.view.LadderView;
+import refactoring.view.ResultView;
 
 public class LadderGameRunner {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         InputView inputView = new InputView();
+        var players = getPlayers(scanner, inputView);
+        var depth = getDepth(scanner, inputView);
+
+        var ladderFactory = LadderFactoryBean.createLadderFactory();
+        var ladder = ladderFactory.create(depth, players.size());
+        var results = getResults(scanner, inputView);
+
+        var ladderView = new LadderView(players, ladder);
+        ladderView.printLadder(results);
+
+        var match = ladder.match(results, new MatchResults());
+        printResult(scanner, players, match);
+    }
+
+    private static void printResult(final Scanner scanner, final Players players, final Match match) {
+        ResultView resultView = new ResultView(match, players);
+        resultView.printTargetResult();
+        String next = scanner.next();
+        resultView.printResult(next);
+    }
+
+    private static Results getResults(final Scanner scanner, final InputView inputView) {
+        inputView.printExecutionResult();
+        String reword = scanner.next();
+        String[] rewords = CommaSplit.split(reword);
+        return Results.of(rewords);
+    }
+
+    private static int getDepth(final Scanner scanner, final InputView inputView) {
+        inputView.printDepthComment();
+        int depth = scanner.nextInt();
+        return depth;
+    }
+
+    private static Players getPlayers(final Scanner scanner, final InputView inputView) {
         inputView.printNameComment();
         String names = scanner.nextLine();
         String[] split = CommaSplit.split(names);
-        Players players = Players.of(split);
-
-        inputView.printDepthComment();
-        int depth = scanner.nextInt();
-        LadderFatory ladderFactory = new MyLadderFactory(new MyLineFactory());
-        Ladder ladder = ladderFactory.create(depth, players.size());
-
-        inputView.printExecutionResult();
-        String reword = scanner.nextLine();
-
-        String[] rewords = CommaSplit.split(reword);
-        Results results  = Results.of(rewords);
-        LadderView ladderView = new LadderView(players, ladder);
-        ladderView.printLadder(results);
-
-
-
-
+        return Players.of(split);
     }
 
 }
