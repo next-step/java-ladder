26147449654e657aaaf10625af76aeaf38a2008e
juno-junho <ssmm0205@naver.com>
2023-05-19 20:36:19 +0900

    feat(utils): private 생성자를 통한 인스턴스화 방지

--- src/main/java/nextstep/ladder/utils/Util.java
+++ src/main/java/nextstep/ladder/utils/Util.java
@@ -6,6 +6,10 @@ import java.util.stream.Collectors;
 
 public class Util {
 
+    private Util() {
+        throw new AssertionError("Util 클래스의 인스턴스를 생성할 수 없습니다!");
+    }
+
     public static List<String> convertArrayToList(String[] splitResults) {
         return Arrays.stream(splitResults).collect(Collectors.toList());
     }
