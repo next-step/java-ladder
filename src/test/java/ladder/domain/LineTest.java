2fc84bc7f131cdf93f4dcbd2f474cf0ad7e105d8
dong <dong.22@kakaoenterprise.com>
2023-05-19 15:48:48 +0900

    test : change var name

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -11,29 +11,29 @@ public class LineTest {
     @Test
     public void equals() {
         //given
-        Line lineOne = new Line(1, 1);
-        Line lineTwo = new Line(2, 2);
-        Line lineTwice = new Line(2, 2);
+        Line line11 = new Line(1, 1);
+        Line line22 = new Line(2, 2);
+        Line line21 = new Line(2, 2);
         //when
         //then
         assertAll("동등성을 검증한다",
-                () -> assertThat(lineTwo).as("").isEqualTo(lineTwice),
-                () -> assertThat(lineOne).as("").isNotEqualTo(lineTwo)
+                () -> assertThat(line22).as("").isEqualTo(line21),
+                () -> assertThat(line11).as("").isNotEqualTo(line22)
         );
     }
 
-    @DisplayName("")
+    @DisplayName("동일한 column, row 를 갖는 Line 이면 인스턴스가 다르더라도 동일한 객체로 취급하는 로직을 검증")
     @Test
     public void isSame() {
         //given
-        Line lineOne = new Line(1, 1);
+        Line comparativeLine11 = new Line(1, 1);
         //when
         //then
         assertAll("동등성을 검증한다",
-                () -> assertThat(lineOne.isSame(1, 1)).as("").isTrue(),
-                () -> assertThat(lineOne.isSame(2, 1)).as("").isFalse(),
-                () -> assertThat(lineOne.isSame(1, 2)).as("").isFalse(),
-                () -> assertThat(lineOne.isSame(2, 2)).as("").isFalse()
+                () -> assertThat(comparativeLine11.isSame(1, 1)).as("").isTrue(),
+                () -> assertThat(comparativeLine11.isSame(2, 1)).as("").isFalse(),
+                () -> assertThat(comparativeLine11.isSame(1, 2)).as("").isFalse(),
+                () -> assertThat(comparativeLine11.isSame(2, 2)).as("").isFalse()
         );
     }
 }
