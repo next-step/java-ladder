66115223d8e1a3949b83adcffcee022ebfac4f45
lehdqlsl <lehdqlsl@naver.com>
2023-05-20 00:01:39 +0900

    Step1 (#1795)
    * refactor: 람다, 스트림 적용
    
    * refactor: 중복코드 제거
    
    * refactor: stream
    
    * refactor: optional

--- src/test/java/nextstep/optional/UsersTest.java
+++ src/test/java/nextstep/optional/UsersTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UsersTest {
-
+    /*
+    nextstep.optional.Users의 getUser() 메소드를 자바 8의 stream과 Optional을 활용해 구현한다.
+    자바 8의 stream과 Optional을 사용하도록 리팩토링한 후 UsersTest의 단위 테스트가 통과해야 한다.
+     */
     @Test
     public void getUser() {
         Users users = new Users();
