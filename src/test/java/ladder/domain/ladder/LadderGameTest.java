558d88e0ebca5e5e8ec387db4bc30f9cf857ee60
SightStudio <sdm3285@gmail.com>
2023-05-21 00:02:37 +0900

    refactor 사다리게임에서 유저, 보상 분리에 따른 테스트코드 분리 및 미사용 import 제거

--- src/test/java/ladder/domain/ladder/LadderGameTest.java
+++ src/test/java/ladder/domain/ladder/LadderGameTest.java
@@ -1,12 +1,14 @@
 package ladder.domain.ladder;
 
 import config.BaseTest;
-import exception.ExceptionCode;
 import java.util.Arrays;
 import java.util.List;
-import ladder.domain.ladder.ladder.Ladder;
+import ladder.domain.ladder.player.LadderGamePlayerInfo;
 import ladder.domain.ladder.reword.LadderGameReword;
+import ladder.domain.ladder.reword.LadderGameRewordInfo;
 import ladder.domain.ladder.setting.LadderGameSetting;
+import ladder.domain.player.DefaultPlayerGenerator;
+import ladder.domain.player.PlayerGenerator;
 import ladder.domain.result.LadderGameResult;
 import ladder.testDouble.NextStepExampleFixedLadderGenerator;
 import org.assertj.core.api.Assertions;
@@ -18,79 +20,40 @@ public class LadderGameTest extends BaseTest {
 
   int 사다리_높이;
   LadderGameSetting gameSetting;
-
-  List<String> 기본_게임_보상_목록;
+  PlayerGenerator playerGenerator;
+  LadderGameRewordInfo 게임_보상_기본;
 
   @BeforeEach
   void test() {
     사다리_높이 = 5;
+    게임_보상_기본 = new LadderGameRewordInfo(List.of("꽝", "5000", "꽝", "3000"));
     gameSetting = LadderGameSetting.withDefaultSetting();
-    기본_게임_보상_목록 = List.of("꽝", "5000", "꽝", "3000");
+    playerGenerator = new DefaultPlayerGenerator();
   }
 
   @Test
-  @DisplayName("LadderGame | 플레이어목록에 동일한 이름이 있는 경우, 게임을 시작 할 수 없다.")
-  void 사다리게임의_높이만큼_라인이_생성된다() {
-    // given
-    List<String> 플레이어_이름_목록 = Arrays.asList("s1", "s2", "seol");
-    LadderGame ladderGame = new LadderGame(플레이어_이름_목록, 기본_게임_보상_목록, 사다리_높이, gameSetting);
-
-    // when
-    Ladder ladder = ladderGame.getLadder();
-
-    // then
-    Assertions.assertThat(ladder.getLadderHeight())
-        .isEqualTo(사다리_높이);
-  }
-
-  @Test
-  @DisplayName("LadderGame | 플레이어목록에 동일한 이름이 있는 경우, 게임을 시작 할 수 없다.")
-  void 사다리게임에_참가한_플레이어_이름이_고유해야한다() {
-    // given
-    List<String> 플레이어_이름_목록 = Arrays.asList("sight", "sight", "seol");
-
-    // when & then
-    super.assertThatThrowsLadderGameException(
-        () -> new LadderGame(플레이어_이름_목록, 기본_게임_보상_목록, 사다리_높이, gameSetting),
-        ExceptionCode.DUPLICATE_PLAYER_IN_GAME
-    );
-  }
-
-  @Test
-  @DisplayName("LadderGame | 사다리게임에 참가한 플레이어는 최소 두명 이상이어야한다")
-  void 사다리게임에_참가한_플레이어는_최소_두명_이상이어야한다() {
-    // given
-    List<String> 플레이어_이름_목록 = Arrays.asList("sight");
-
-    // when & then
-    super.assertThatThrowsLadderGameException(
-        () -> new LadderGame(플레이어_이름_목록, 기본_게임_보상_목록, 사다리_높이, gameSetting),
-        ExceptionCode.MIN_PLAYER_COUNT_REQUIRED
-    );
-  }
-
-  @Test
-  @DisplayName("LadderGame.play | 사다리 게임이 끝나면 모든 플레이어의 사다리 높이는 설정한 사다리 높이 + 1을 가진다 (결과까지)")
+  @DisplayName("LadderGame.play() | 사다리 게임 종료 후 모든 플레이어의 사다리 높이는 설정한 사다리 높이와 동일하다")
   void 게임_종료_후_사다리_높이와_플레이어의_높이는_동일() {
     // given
     List<String> 플레이어_이름_목록 = Arrays.asList("a","b", "c", "d");
+    LadderGamePlayerInfo 플레이어_정보 = playerGenerator.generatePlayerList(플레이어_이름_목록);
 
     gameSetting = LadderGameSetting.builder()
         .ladderGenerator(new NextStepExampleFixedLadderGenerator())
         .build();
 
-    LadderGame ladderGame = new LadderGame(플레이어_이름_목록, 기본_게임_보상_목록, 사다리_높이, gameSetting);
+    LadderGame ladderGame = new LadderGame(플레이어_이름_목록.size(), 사다리_높이, gameSetting);
 
     // when
-    ladderGame.play();
+    ladderGame.play(플레이어_정보, 게임_보상_기본);
 
     // then
-    Assertions.assertThat(ladderGame.getPlayerInfo().getPlayers())
+    Assertions.assertThat(플레이어_정보.getPlayers())
         .allMatch(player -> player.getCurrentHeight() == 사다리_높이);
   }
 
   @Test
-  @DisplayName("LadderGame.play | 넥스트 스텝 예시) 사다리 게임을 넥스트 스텝의 예제와 동일하게 진행하고, 결과를 비교한다.")
+  @DisplayName("LadderGame.play() | 넥스트 스텝 예시) 사다리 게임을 넥스트 스텝의 예제와 동일하게 진행하고, 결과를 비교한다.")
   void 사다리_게임_넥스트스텝_예시() {
     /*
       pobi : 꽝
@@ -101,15 +64,16 @@ public class LadderGameTest extends BaseTest {
 
     // given
     List<String> 플레이어_이름_목록 = Arrays.asList("pobi","honux", "crong", "jk");
+    LadderGamePlayerInfo 플레이어_정보 = playerGenerator.generatePlayerList(플레이어_이름_목록);
 
     gameSetting = LadderGameSetting.builder()
         .ladderGenerator(new NextStepExampleFixedLadderGenerator())
         .build();
 
-    LadderGame ladderGame = new LadderGame(플레이어_이름_목록, 기본_게임_보상_목록, 사다리_높이, gameSetting);
+    LadderGame ladderGame = new LadderGame(플레이어_이름_목록.size(), 사다리_높이, gameSetting);
 
     // when
-    LadderGameResult result = ladderGame.play();
+    LadderGameResult result = ladderGame.play(플레이어_정보, 게임_보상_기본);
 
     // then
     var mapAssert = Assertions.assertThat(result.getPlayerResultMap());
--- src/test/java/ladder/domain/ladder/LadderGeneratorTest.java
+++ src/test/java/ladder/domain/ladder/LadderGeneratorTest.java
@@ -2,8 +2,12 @@ package ladder.domain.ladder;
 
 import config.BaseTest;
 import exception.ExceptionCode;
+import java.util.Arrays;
+import java.util.List;
 import ladder.domain.ladder.ladder.DefaultLadderGenerator;
+import ladder.domain.ladder.ladder.Ladder;
 import ladder.domain.ladder.ladder.LadderGenerator;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,13 +25,28 @@ public class LadderGeneratorTest extends BaseTest {
   @DisplayName("LadderGenerator | 사다리게임의 사다리 높이는 최소 1 이상이어야한다.")
   void 사다리게임의_사다리_높이는_최소_1_이상이어야한다() {
     // given
-    int playerCount = 10;
-    int ladderHeight = 0;
+    List<String> 플레이어_이름_목록 = Arrays.asList("s1");
+    int 사다리_높이 = 0;
 
     // when & then
     super.assertThatThrowsLadderGameException(
-        () -> sut.generateLadderLines(playerCount, ladderHeight),
+        () -> sut.generateLadderLines(플레이어_이름_목록.size(), 사다리_높이),
         ExceptionCode.MIN_LADDER_HEIGHT_REQUIRED
     );
+  }
+
+  @Test
+  @DisplayName("LadderGenerator | 사다리게임의 높이만큼 라인이 생성된다")
+  void 사다리게임의_높이만큼_라인이_생성된다() {
+    // given
+    List<String> 플레이어_이름_목록 = Arrays.asList("s1", "s2", "seol");
+    int 사다리_높이 = 5;
+
+    // when
+    Ladder ladder = sut.generateLadderLines(플레이어_이름_목록.size(), 사다리_높이);
+
+    // then
+    Assertions.assertThat(ladder.getLadderHeight())
+        .isEqualTo(사다리_높이);
   }
 }
