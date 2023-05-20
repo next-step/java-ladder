2af10a621351e97243b772ef60ac1728a284a97c
dong <dong.22@kakaoenterprise.com>
2023-05-20 17:54:53 +0900

    refactor : username 은 5자리까지 허용된다

--- src/test/java/ladder/domain/UserTest.java
+++ src/test/java/ladder/domain/UserTest.java
@@ -1,5 +1,23 @@
 package ladder.domain;
 
-public class UserTest {
+import ladder.control.Preferences;
+import ladder.exception.OutOfRoWRangeException;
+import ladder.exception.OutOfUserNameLengthException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UserTest {
+    @DisplayName("userName 의 길이가 5를 초과하는 경우 예외를 던진다")
+    @Test
+    public void userNameLength() {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            new User("여섯글자이름");
+        }).isInstanceOf(OutOfUserNameLengthException.class)
+                .hasMessageContaining("유저 이름의 길이 제한을 초과하였습니다");
+    }
 }
