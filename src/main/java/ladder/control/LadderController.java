a411eeb80c87cebc4dbc59b4642e0b1a21b54341
dong <dong.22@kakaoenterprise.com>
2023-05-20 10:49:48 +0900

    refactor : 적절한 개행을 통해서 문맥을 나누고 가독성을 높이도록 수정

--- src/main/java/ladder/control/LadderController.java
+++ src/main/java/ladder/control/LadderController.java
@@ -2,6 +2,7 @@ package ladder.control;
 
 import ladder.domain.Lines;
 import ladder.domain.Renderer;
+import ladder.domain.Scene;
 import ladder.domain.Users;
 import ladder.present.Presenter;
 
@@ -9,9 +10,13 @@ public class LadderController {
     public static void main(String[] args) {
         Presenter presenter = new Presenter();
         Users users = presenter.users();
+
         int row = presenter.ladderHeight();
         int column = users.count();
+
         Lines lines = Lines.of(column, row);
-        presenter.renderingLadder(new Renderer(column, row, lines, users).renderingSceneWithUser());
+        Scene scene = new Renderer(column, row, lines, users)
+                .renderingSceneWithUser();
+        presenter.renderingLadder(scene);
     }
 }
