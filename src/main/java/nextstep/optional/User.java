b715bd5ebf5d9343764751d7efb6be91798815a2
choesoyeong <cszero0421@naver.com>
2023-05-20 12:35:00 +0900

    step1 (#1789)
    Co-authored-by: ayla-c <ayla@MacBook-Pro-235.local>

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,7 +35,10 @@ public class User {
     }
 
     public static boolean ageIsInRange2(User user) {
-        return false;
+        Optional<User> oUser = Optional.ofNullable(user);
+        return oUser.map(x -> x.getAge())
+                .filter(x -> x>=30 && x<=45)
+                .isPresent();
     }
 
     @Override
