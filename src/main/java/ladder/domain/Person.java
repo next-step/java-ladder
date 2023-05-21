8fd9e3980086aa25acc0c8401ed27d463865c5e9
jerry shin <shiney798@gmail.com>
2023-05-21 23:43:21 +0900

    feat : Person 클래스 및 테스트 코드 작성

---
+++ src/main/java/ladder/domain/Person.java
@@ -0,0 +1,32 @@
+package ladder.domain;
+
+public class Person {
+    public static final int MAX_NAME_LENGTH = 5;
+    private final String name;
+
+    public Person(String name) {
+        validateName(name);
+
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("이름이 Null 또는 빈값이 입력되었습니다.");
+        }
+
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름이 최대허용길이(" + MAX_NAME_LENGTH + ")를 초과하였습니다");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this.name.equals(obj);
+    }
+}
---
+++ src/test/java/ladder/domain/PersonTest.java
@@ -0,0 +1,34 @@
+package ladder.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class PersonTest {
+
+
+    @ParameterizedTest(name = "{displayName} [{index}] {arguments}")
+    @NullAndEmptySource
+    void Null및빈값검증(String name) {
+        assertThatThrownBy(() -> {
+            Person person = new Person(name);
+        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("이름이 Null 또는 빈값이 입력되었습니다.");
+    }
+
+    @Test
+    void 이름최대길이검증() {
+        assertThatThrownBy(() -> {
+            Person person = new Person("ethann");
+        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("이름이 최대허용길이(5)를 초과하였습니다");
+    }
+
+    @ParameterizedTest(name = "{displayName} [{index}] {arguments}")
+    @ValueSource(strings = {"j", "jerry"})
+    void 정상생성검증(String name) {
+        Assertions.assertThat(new Person(name)).isEqualTo(name);
+    }
+}
