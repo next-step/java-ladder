7b313e57c34f0eecd4cb7fbb9f55a96e740147b3
SeokkAng <kaytjrl@gmail.com>
2023-05-21 11:11:50 +0900

    [step4] 사다리(리팩터링) - in -> out 방식 (#1793)
    Co-authored-by: ethan.v <ethan.v@kakaocorp.com>

--- src/test/java/step3/domain/LineTest.java
+++ src/test/java/step3/domain/LineTest.java
@@ -7,10 +7,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
     @Test
-    @DisplayName("사다리 라인을 생성하고 검증한다. fakeMoveStrategy 는 모두 false로 설정한다.")
+    @DisplayName("사다리 라인을 생성하고 검증한다. fakeMoveStrategy 가 false일 때 => false, true, false로 구성되어야 한다.")
     void createLine() {
         Line line = Line.createLine(3, new FakeMoveStrategy());
         assertThat(line.size()).isEqualTo(3);
-        assertThat(line.getCurrent()).containsExactly(false, false, false);
+        assertThat(line.getCurrent()).containsExactly(false, true, false);
     }
 }
