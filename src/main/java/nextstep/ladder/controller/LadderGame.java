2bf7ea4454feb223f4a7f2e42b7f153e8d2f8c2c
juno-junho <ssmm0205@naver.com>
2023-05-22 04:46:56 +0900

    feat(controller): controller 수정

--- src/main/java/nextstep/ladder/controller/LadderGame.java
+++ src/main/java/nextstep/ladder/controller/LadderGame.java
@@ -1,16 +1,19 @@
 package nextstep.ladder.controller;
 
-import nextstep.ladder.domain.Height;
-import nextstep.ladder.domain.Ladder;
-import nextstep.ladder.domain.Participants;
-import nextstep.ladder.domain.Result;
+import nextstep.ladder.domain.*;
 import nextstep.ladder.domain.strategy.BridgeStrategy;
+import nextstep.ladder.utils.Util;
+
+import java.util.List;
+import java.util.Map;
 
 import static nextstep.ladder.view.InputView.*;
 import static nextstep.ladder.view.OutputView.printLadder;
+import static nextstep.ladder.view.OutputView.printResult;
 
 public class LadderGame {
 
+    public static final String DELIMITER = ",";
     private final BridgeStrategy strategy;
 
     public LadderGame(BridgeStrategy strategy) {
@@ -18,18 +21,33 @@ public class LadderGame {
     }
 
     public void run() {
-        Participants participants = new Participants(getParticipants());
-        Result result = Result.of(participants, getResults());
-
+        Participants participants = new Participants(getParticipantNames());
+        LadderResult result = LadderResult.of(participants, getResultList());
         Height height = new Height(getLadderHeight());
-
         Ladder ladder = Ladder.of(participants,height,strategy);
 
         printLadder(participants, result, ladder);
 
-//        while (true){
-//            String gameResult = result.gameResult(participants, getParticipantName(), ladder);
-//            printResult(gameResult);
-//        }
+        Map<String, String> gameResult = ladder.generateResult(participants, result);
+        loopResult(participants, gameResult);
+    }
+
+    private void loopResult(Participants participants, Map<String, String> gameResult) {
+        while (true){
+            String input = getNameOrCodeForResult();
+            if (input.equals(Code.EXIT.getCode())) {
+                break;
+            }
+            participants.validateResultInput(input);
+            printResult(input, gameResult);
+        }
+    }
+
+    private List<String> getResultList() {
+        return Util.separateToList(getResults(), DELIMITER);
+    }
+
+    private List<String> getParticipantNames() {
+        return Util.separateToList(getParticipants(), DELIMITER);
     }
 }
