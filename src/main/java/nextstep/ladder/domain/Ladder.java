5d42de69d75710be13f14ce34d0df14bacf2df47
juno-junho <ssmm0205@naver.com>
2023-05-22 04:45:50 +0900

    refactor(domain): 결과 계산 하는 로직 ladder 클래스로 이동

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -2,14 +2,14 @@ package nextstep.ladder.domain;
 
 import nextstep.ladder.domain.strategy.BridgeStrategy;
 
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Ladder implements Iterable<Line>{
 
     private final List<Line> ladder;
+    private final Map<String, String> gameResult;
 
     public static Ladder of(Participants participants, Height height, BridgeStrategy strategy) {
         return new Ladder(participants, height, strategy);
@@ -17,6 +17,7 @@ public class Ladder implements Iterable<Line>{
 
     private Ladder(Participants participants, Height height, BridgeStrategy strategy) {
         this.ladder = create(participants,height, strategy);
+        this.gameResult = new LinkedHashMap<>();
     }
 
     public List<Line> create(Participants participants, Height height, BridgeStrategy strategy) {
@@ -32,5 +33,41 @@ public class Ladder implements Iterable<Line>{
     @Override
     public Iterator<Line> iterator() {
         return ladder.iterator();
+    }
+
+    public Map<String, String> generateResult(Participants participants, LadderResult result) {
+        participants.forEach(name -> {
+            int index = calculateIndexOfResult(participants, name);
+            gameResult.put(name, result.get(index));
+        });
+        return gameResult;
+    }
+    private int calculateIndexOfResult(Participants participants, String name) {
+        int index = participants.indexOf(name);
+        int maxIndex = participants.maxIndex();
+
+        for (Line line : ladder) {
+            index = calculateIndex(index, maxIndex, line);
+        }
+        return index;
+    }
+
+    private int calculateIndex(int index, int maxIndex, Line line) {
+        if (checkIndexLine(index, maxIndex, line)) {
+            index++;
+            return index;
+        }
+        if (checkPreviousIndexLine(index, line)) {
+            index--;
+        }
+        return index;
+    }
+
+    private static boolean checkIndexLine(int index, int maxIndex, Line line) {
+        return index != maxIndex && line.get(index);
+    }
+
+    private static boolean checkPreviousIndexLine(int index, Line line) {
+        return index - 1 >= 0 && line.get(index - 1);
     }
 }
