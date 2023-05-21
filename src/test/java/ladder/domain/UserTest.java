6b7813362d6975350abfe10c7c396693b0fa9d1c
KangHo-dev <tiger452003@gmail.com>
2023-05-22 00:12:42 +0900

    feat : 참여자 (User) 도메인 추가

---
+++ src/test/java/ladder/domain/UserTest.java
@@ -0,0 +1,20 @@
+package ladder.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class UserTest {
+    @DisplayName("참여자 이름이 5글자 초과일 경우")
+    @ParameterizedTest
+    @ValueSource(strings = {"abcdef","abcdefg"})
+    void 참여자_생성_이름_5글자_초과(String userName) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new User(userName))
+                .withMessageMatching("참여자 이름은 최대 5글자 입니다.");
+    }
+
+}
