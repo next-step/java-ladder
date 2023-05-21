2b72a1c5575e845c694de18c37e70e1f3d9f9be3
juno-junho <ssmm0205@naver.com>
2023-05-22 04:44:46 +0900

    refactor(util): 불필요한 메서드 삭제

--- src/main/java/nextstep/ladder/utils/Util.java
+++ src/main/java/nextstep/ladder/utils/Util.java
@@ -10,10 +10,6 @@ public class Util {
         throw new AssertionError("Util 클래스의 인스턴스를 생성할 수 없습니다!");
     }
 
-    public static List<String> convertArrayToList(String[] splitResults) {
-        return Arrays.stream(splitResults).collect(Collectors.toList());
-    }
-
     public static List<String> separateToList(String participants, String delimiter) {
         return Arrays.stream(participants.split(delimiter))
                 .collect(Collectors.toList());
