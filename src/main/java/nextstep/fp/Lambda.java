fedaf77269816e20bbc7dccb3f06b04d909b8401
jerry-shin <shiney798@gmail.com>
2023-05-21 21:39:07 +0900

    Step1 : 스트림, 람다, Optional (#1802)
    * docs : 람다 실습내용 Readme 추가
    
    * refactor : 람다 실습1 수행 적용
    
    * refactor : Money 클래스 추가
    
    * refactor : 람다 실습2 수행
    
    * refactor : 람다 실습2 구조 리팩터링
    
    * docs : 스트림 실습 요구사항 추가
    
    * feat : 스트림 실습1 요구사항 구현
    
    * feat : 스트림 실습2 요구사항 구현
    
    * docs : Optional 요구사항 추가
    
    * feat : Optional 요구사항1 구현
    
    * feat : Optional 요구사항2 구현
    
    * feat : Optional 요구사항3 구현

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -27,30 +27,18 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sum(numbers, n -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sum(numbers, n -> n % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sum(numbers, n -> n > 3);
+    }
+
+    private static int sum(List<Integer> numbers, Conditional c) {
+        return numbers.stream().filter(c::test).reduce(0, Integer::sum);
     }
 }
