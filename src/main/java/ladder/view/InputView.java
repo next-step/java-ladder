77e768fa980fa076efc27b30c53b94d25f6ebb98
KangHo-dev <tiger452003@gmail.com>
2023-05-23 05:46:42 +0900

    view : 입출력클래스 및 어플리케이션 추가

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -13,12 +13,19 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String USERS_DELIMITER = ",";
 
-    public static Users inputUsers() {
+    public static List<User> inputUsers() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         String[] usersName = scanner.nextLine().split(USERS_DELIMITER);
 
-        return Users.create(Arrays.stream(usersName)
+        return Arrays.stream(usersName)
                 .map(User::create)
-                .collect(Collectors.toList()));
+                .collect(Collectors.toList());
+    }
+
+    public static int inputHeight() {
+        System.out.println("최대 사다리 높이는 몇 개 인가요?");
+        int height = scanner.nextInt();
+        scanner.nextLine();
+        return height;
     }
 }
