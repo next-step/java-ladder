0734682c422eba40c0413b3c32f1410125b6f20f
KangHo-dev <tiger452003@gmail.com>
2023-05-24 01:51:24 +0900

    refactor : 줄여쓰지 않기 (array to list)

--- src/test/java/ladder/domain/UsersTest.java
+++ src/test/java/ladder/domain/UsersTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,10 +16,15 @@ class UsersTest {
     @DisplayName("Users 객체 생성 테스트")
     @Test
     void Users_객체생성() {
-        String[] usersArr = {"user1", "user2", "user3"};
-        Users users = Users.create(usersArr);
+        String[] userNames = {"user1", "user2", "user3"};
 
-        Assertions.assertThat(users.size()).isEqualTo(usersArr.length);
+        List<String> userNameList = Arrays.stream(userNames)
+                .limit(userNames.length)
+                .collect(Collectors.toList());
+
+        Users users = Users.create(userNameList);
+
+        Assertions.assertThat(users.size()).isEqualTo(userNameList.size());
     }
 
 }
