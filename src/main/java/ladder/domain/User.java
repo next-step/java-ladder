30e8e83a7d796217529ca955e7ffc3f21d11492b
KangHo-dev <tiger452003@gmail.com>
2023-05-24 01:21:12 +0900

    refactor : equals, hashCode 삭제 (이름이 같다고 같은 객체는 아니다.)

--- src/main/java/ladder/domain/User.java
+++ src/main/java/ladder/domain/User.java
@@ -25,16 +25,4 @@ public class User {
         }
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(name, user.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
 }
