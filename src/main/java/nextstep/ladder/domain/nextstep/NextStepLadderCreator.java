a3bacba45465e73dee7c24b3277623b1810f514b
juno-junho <ssmm0205@naver.com>
2023-05-24 22:03:15 +0900

    refactor(domain): strategy 추가

--- src/main/java/nextstep/ladder/domain/nextstep/NextStepLadderCreator.java
+++ src/main/java/nextstep/ladder/domain/nextstep/NextStepLadderCreator.java
@@ -4,10 +4,12 @@ import nextstep.ladder.domain.Height;
 import nextstep.ladder.domain.Ladder;
 import nextstep.ladder.domain.Participants;
 import nextstep.ladder.domain.engine.LadderCreator;
+import nextstep.ladder.domain.strategy.BridgeStrategy;
 
 public class NextStepLadderCreator implements LadderCreator {
+
     @Override
-    public Ladder create(Participants participants, Height height) {
+    public Ladder create(Participants participants, Height height, BridgeStrategy strategy) {
         return null;
     }
 }
--- src/main/java/nextstep/ladder/domain/nextstep/NextStepLineCreator.java
+++ src/main/java/nextstep/ladder/domain/nextstep/NextStepLineCreator.java
@@ -2,10 +2,11 @@ package nextstep.ladder.domain.nextstep;
 
 import nextstep.ladder.domain.Line;
 import nextstep.ladder.domain.engine.LineCreator;
+import nextstep.ladder.domain.strategy.BridgeStrategy;
 
 public class NextStepLineCreator implements LineCreator {
     @Override
-    public Line create(int width) {
+    public Line create(int width, BridgeStrategy strategy) {
         return null;
     }
 }
