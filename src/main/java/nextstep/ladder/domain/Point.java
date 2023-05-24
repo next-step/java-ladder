8822e7c6c048bc9b76dd08f8f276801e3214f5ed
juno-junho <ssmm0205@naver.com>
2023-05-24 23:48:13 +0900

    feat(domain): Point에 정적 팩토리 메서드 추가

--- src/main/java/nextstep/ladder/domain/Point.java
+++ src/main/java/nextstep/ladder/domain/Point.java
@@ -1,12 +1,18 @@
 package nextstep.ladder.domain;
 
+import nextstep.ladder.domain.strategy.BridgeStrategy;
+
 public class Point {
 
     private final int index;
 
     private final Direction direction;
 
-    Point(int index, Direction direction) {
+    public static Point first(BridgeStrategy strategy) {
+        return new Point(0, Direction.first(strategy.makeBridge()));
+    }
+
+    public Point(int index, Direction direction) {
         this.index = index;
         this.direction = direction;
     }
@@ -14,4 +20,9 @@ public class Point {
     public int move() {
         return index + direction.move();
     }
+
+
+/*    public void setFirst() {
+        this.direction.
+    }*/
 }
