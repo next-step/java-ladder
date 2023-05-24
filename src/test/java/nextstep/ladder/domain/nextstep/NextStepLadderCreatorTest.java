41f05b5d2d53b3dcfef51f649ee064869324638f
juno-junho <ssmm0205@naver.com>
2023-05-25 02:30:14 +0900

    test(nextstep): 테스트 작성 x

---
+++ src/test/java/nextstep/ladder/domain/nextstep/NextStepLadderCreatorTest.java
@@ -0,0 +1,7 @@
+package nextstep.ladder.domain.nextstep;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NextStepLadderCreatorTest {
+
+}
---
+++ src/test/java/nextstep/ladder/domain/nextstep/NextStepLineTest.java
@@ -0,0 +1,15 @@
+package nextstep.ladder.domain.nextstep;
+
+import java.util.List;
+
+class NextStepLineTest {
+
+    private List<Point> points;
+
+   /* @BeforeEach
+    void setUp() {
+        new Point(0, Direction.of(true));
+        this.points = List.of();
+    }
+*/
+}
