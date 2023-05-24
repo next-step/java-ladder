6b669230982dc5657cd08a33dab54022bfdc23aa
KangHo-dev <tiger452003@gmail.com>
2023-05-25 00:53:03 +0900

    feat : GameResult 객체 생성 (실행 결과를 담는 객체)

---
+++ src/main/java/ladder/domain/GameResult.java
@@ -0,0 +1,35 @@
+package ladder.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class GameResult {
+    private List<String> gameResults;
+
+    private GameResult(List<String> gameResults) {
+        validateGameResult(gameResults);
+        this.gameResults = gameResults;
+    }
+
+    private void validateGameResult(List<String> gameResults) {
+        if (isEmpty(gameResults)) {
+            throw new IllegalArgumentException("GameResult is Empty");
+        }
+    }
+
+    private boolean isEmpty(List<String> gameResults) {
+        return gameResults == null || gameResults.isEmpty();
+    }
+
+    public static GameResult create(List<String> gameResults) {
+        return new GameResult(gameResults);
+    }
+
+    public List<String> gameResult() {
+        return Collections.unmodifiableList(gameResults);
+    }
+
+    public int size() {
+        return this.gameResults.size();
+    }
+}
