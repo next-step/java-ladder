498caba586a07899981de6aa218d73e6b2c16aca
juno-junho <ssmm0205@naver.com>
2023-05-24 18:57:53 +0900

    test(domain): 경우에 따른 Point 테스트 작성

--- src/test/java/nextstep/ladder/domain/PointTest.java
+++ src/test/java/nextstep/ladder/domain/PointTest.java
@@ -1,10 +1,9 @@
 package nextstep.ladder.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.awt.*;
+import static org.assertj.core.api.Assertions.*;
 
 class PointTest {
 
@@ -12,7 +11,28 @@ class PointTest {
     @DisplayName("index가 1일때, 이전값이 true이고 현재 값이 false이면 0를 반환한다.")
     @Test
     void name() {
-        Point point = new Point(0, true, false);
+        Point point = new Point(1, true, false);
         assertThat(point.move()).isEqualTo(0);
+    }
+
+    @DisplayName("index가 1일때, 이전값이 false 현재 값이 true이면 2를 반환한다.")
+    @Test
+    void name1() {
+        Point point = new Point(1, false, true);
+        assertThat(point.move()).isEqualTo(0);
+    }
+
+    @DisplayName("index가 1일때, 이전값이 false이고 현재 값이 false이면 1를 반환한다.")
+    @Test
+    void name2() {
+        Point point = new Point(1, false, false);
+        assertThat(point.move()).isEqualTo(1);
+    }
+
+    @DisplayName("index가 1일때, 이전값이 true이고 현재 값이 true이면 예외를 발생한다.")
+    @Test
+    void name3() {
+        assertThatThrownBy(() -> new Point(1, true, true))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
