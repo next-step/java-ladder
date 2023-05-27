b973de6419de9ba09c9b3883d6793b1ad5b3cc82
highjune <highjune37@gmail.com>
2023-05-27 16:24:35 +0900

    test(domain): LadderRow의isLeftEnd, isRightEnd 테스트 완성

--- src/test/java/ladder/domain/ladder/LadderRowTest.java
+++ src/test/java/ladder/domain/ladder/LadderRowTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LadderRowTest {
@@ -33,9 +34,23 @@ public class LadderRowTest {
     @DisplayName("해당하는 곳의 라인이 있는지 판단하는 함수의 값을 확인한다.")
     void hasLine_IfThereTrue_True() {
         LadderRow ladderRow = new LadderRow(Arrays.asList(false, true, false));
-        Assertions.assertThat(ladderRow.hasLine(0)).isFalse();
-        Assertions.assertThat(ladderRow.hasLine(1)).isTrue();
-        Assertions.assertThat(ladderRow.hasLine(2)).isFalse();
+        assertThat(ladderRow.hasLine(0)).isFalse();
+        assertThat(ladderRow.hasLine(1)).isTrue();
+        assertThat(ladderRow.hasLine(2)).isFalse();
+    }
+
+    @Test
+    @DisplayName("왼쪽의 끝인지 판단하는 함수의 값을 확인한다.")
+    void isLeftEnd_IfEnd_True() {
+        LadderRow ladderRow = new LadderRow(Arrays.asList(false, true, false));
+        assertThat(ladderRow.isLeftEnd(0)).isTrue();
+    }
+
+    @Test
+    @DisplayName("오른쪽의 끝인지 판단하는 함수의 값을 확인한다.")
+    void isRightEnd_IfEnd_True() {
+        LadderRow ladderRow = new LadderRow(Arrays.asList(false, true, false));
+        assertThat(ladderRow.isRightEnd(3)).isTrue();
     }
 
 
