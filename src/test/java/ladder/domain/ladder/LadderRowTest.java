b6e79a2dab77dac5ac579f2226d83cbf1aa25c44
highjune <highjune37@gmail.com>
2023-05-27 16:28:49 +0900

    test(domain): LadderRow의 isNotEnd 함수 테스트 & 공통 변수 빼내기

--- src/test/java/ladder/domain/ladder/LadderRowTest.java
+++ src/test/java/ladder/domain/ladder/LadderRowTest.java
@@ -1,7 +1,7 @@
 package ladder.domain.ladder;
 
 import ladder.strategy.LineStrategy;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,12 +9,18 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LadderRowTest {
 
     private LineStrategy lineStrategy = () -> true;
+    private LadderRow ladderRow;
+
+    @BeforeEach
+    void setUp() {
+        ladderRow = new LadderRow(Arrays.asList(false, true, false));
+    }
+
 
     @Test
     @DisplayName("주어진 너비(width) 만큼의 line을 생성한다.")
@@ -33,7 +39,6 @@ public class LadderRowTest {
     @Test
     @DisplayName("해당하는 곳의 라인이 있는지 판단하는 함수의 값을 확인한다.")
     void hasLine_IfThereTrue_True() {
-        LadderRow ladderRow = new LadderRow(Arrays.asList(false, true, false));
         assertThat(ladderRow.hasLine(0)).isFalse();
         assertThat(ladderRow.hasLine(1)).isTrue();
         assertThat(ladderRow.hasLine(2)).isFalse();
@@ -42,16 +47,20 @@ public class LadderRowTest {
     @Test
     @DisplayName("왼쪽의 끝인지 판단하는 함수의 값을 확인한다.")
     void isLeftEnd_IfEnd_True() {
-        LadderRow ladderRow = new LadderRow(Arrays.asList(false, true, false));
         assertThat(ladderRow.isLeftEnd(0)).isTrue();
     }
 
     @Test
     @DisplayName("오른쪽의 끝인지 판단하는 함수의 값을 확인한다.")
     void isRightEnd_IfEnd_True() {
-        LadderRow ladderRow = new LadderRow(Arrays.asList(false, true, false));
         assertThat(ladderRow.isRightEnd(3)).isTrue();
     }
 
+    @ParameterizedTest
+    @DisplayName("끝이 아닌지 판단하는 함수의 값을 확인한다.")
+    @ValueSource(ints = {0, 1, 2, 3})
+    void isNotEnd_IfNotEnd_Ture(int input) {
+        assertThat(ladderRow.isNotEnd(input)).isTrue();
+    }
 
 }
