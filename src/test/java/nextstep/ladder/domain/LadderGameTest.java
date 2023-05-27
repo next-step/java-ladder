9b9d118ffd2359016712df3ff95de5e831db13da
“gisungPark” <gisung2215@naver.com>
2023-05-27 23:34:50 +0900

    feat: <step3> 테스트 코드 수정

--- src/test/java/nextstep/ladder/domain/LadderGameTest.java
+++ src/test/java/nextstep/ladder/domain/LadderGameTest.java
@@ -1,31 +1,20 @@
 package nextstep.ladder.domain;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 
 class LadderGameTest {
-    UserNames userNames;
 
-    @BeforeEach
-    void beforeEach() {
-        this.userNames = UserNames.of(new String[]{"one", "two", "three"});
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {2, 4, 6})
-    public void 사다리게임_생성_테스트(int ladderHeight) throws Exception {
-        LadderGame ladderGame = LadderGame.create(new LadderHeight(ladderHeight), userNames);
-        assertThat(ladderGame.toList().size()).as("사다리 높이 확인").isEqualTo(ladderHeight);
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {0, -1, -12})
-    public void 사다리게임_생성_예외_테스트(int ladderHeight) throws Exception {
-        assertThatIllegalArgumentException().isThrownBy(() -> LadderGame.create(new LadderHeight(ladderHeight), userNames));
+    @Test
+    public void 사다리게임_생성_테스트() throws Exception {
+        LadderHeight ladderHeight1 = new LadderHeight(3);
+        UserNames userNames = UserNames.of(Arrays.asList(UserNameTest.POBI, UserNameTest.CRONG));
+        assertThat(LadderGame.create(ladderHeight1, userNames))
+                .isNotNull()
+                .isInstanceOf(LadderGame.class);
     }
 }
--- src/test/java/nextstep/ladder/domain/UserNameTest.java
+++ src/test/java/nextstep/ladder/domain/UserNameTest.java
@@ -11,22 +11,34 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 class UserNameTest {
 
+    public static final UserName POBI = UserName.of("pobi");
+    public static final UserName HONUX = UserName.of("honux");
+    public static final UserName CRONG = UserName.of("crong");
+
     @ParameterizedTest
     @ValueSource(strings = {"pobi", "honux", "crong", "jk"})
     public void 이름_생성_테스트(String name) throws Exception {
-        assertThat(UserName.of(name)).isNotNull();
+        assertThat(UserName.of(name)).isNotNull()
+                .isInstanceOf(UserName.class);
     }
 
     @ParameterizedTest
     @NullSource
+    public void 이름_생성_null_테스트(String name) throws Exception {
+        assertThatIllegalArgumentException().isThrownBy(() -> UserName.of(name));
+    }
+
+    @ParameterizedTest
     @EmptySource
-    public void 이름_예외_테스트(String name) throws Exception {
+    public void 이름_생성_공백_테스트(String name) throws Exception {
         assertThatIllegalArgumentException().isThrownBy(() -> UserName.of(name));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"pobi_name", "honux_name", "crong_name", "jk_name"})
     public void 이름_최대길이_테스트(String name) throws Exception {
-        assertThatIllegalArgumentException().isThrownBy(() -> UserName.of(name));
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            UserName.of(name);
+        });
     }
 }
