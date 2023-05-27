17fae89b690f46daf1a4cc37e0f39c9768e4070b
wotjd4305 <wotjd4305@naver.com>
2023-05-27 19:48:56 +0900

    [step3] chore. 불필요 import 제거

--- src/test/java/nextstep/ladder/ResultsTest.java
+++ src/test/java/nextstep/ladder/ResultsTest.java
@@ -1,11 +1,9 @@
 package nextstep.ladder;
 
-import nextstep.ladder.drawPolicy.AllDraw;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ResultsTest {
 
