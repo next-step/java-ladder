6edb9400139a2a4b4a905e222d32681dd00adc0e
jaden.1230 <jaden.1230@kakaocorp.com>
2023-05-28 12:47:56 +0900

    refactor: apply point

--- src/main/java/ladder/Model/Move.java
+++ src/main/java/ladder/Model/Move.java
@@ -14,45 +14,23 @@ public class Move {
 
         oldIndexes.forEach(integer -> {
             int result = integer;
-            if (moveLeft(ladderLine, integer)) {
-                newIndexes.add(result - 1);
-                return;
-            }
 
-            if (moveRight(ladderLine, integer)) {
-                newIndexes.add(result + 1);
-                return;
-            }
+            boolean hasLeft = isExistHorizonLines(ladderLine.horizonLines(), integer - 1);
+            boolean hasRight = isExistHorizonLines(ladderLine.horizonLines(), integer);
+
+            result += new Point(hasLeft, hasRight).move().of();
+
             newIndexes.add(result);
         });
 
         return newIndexes;
     }
 
-    private boolean moveLeft(LadderLine ladderLine, int index) {
-        if (index == MIN_WIDTH) {
-            return false;
-        }
-
-        return (index - 1 >= MIN_WIDTH && isExistHorizonLines(ladderLine.horizonLines(), index - 1));
-    }
-
-    private boolean moveRight(LadderLine ladderLine, int index) {
-        int width = ladderLine.size();
-        if (index == width) {
-            return false;
-        }
-
-        return (index + 1 < width && isExistHorizonLines(ladderLine.horizonLines(), index));
-    }
-
     private boolean isExistHorizonLines(List<Boolean> line, int index) {
-        if (index >= line.size()) {
+        if (index < MIN_WIDTH || index >= line.size()) {
             return false;
         }
 
         return line.get(index);
     }
-
-
 }
