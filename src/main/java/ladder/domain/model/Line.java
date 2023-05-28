23fc771d55d572b547ac9b49fe0cd868622446d7
박태영 <>
2023-05-28 19:55:12 +0900

    * refactor :  Random 요소를 분리

--- src/main/java/ladder/domain/model/Line.java
+++ src/main/java/ladder/domain/model/Line.java
@@ -1,35 +1,42 @@
 package ladder.domain.model;
 
 import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class Line {
+    private static final Random RANDOM = new Random();
+    private static final int RANGE_START = 0;
+    private static final int ZERO = 0;
     private Points points = new Points();
+
+
     public void calculatorLine(int countOfPerson) {
-        points.calculatorLine(countOfPerson);
+        int rangeEnd = countOfPerson - 1;
+        IntStream.range(RANGE_START, rangeEnd)
+                .forEach(count -> {
+                    if (count == ZERO || !points.getPoints().get(count - 1)) {
+                        points.connectLine(() -> RANDOM.nextBoolean());
+                        return;
+                    }
+                    points.connectLine(() -> false);
+                });
     }
 
-    public void movePlayer(Player player) {
-        int point = player.getPoint();
-
-        if (isPointPullRight(point)) {
-            player.moveLeft(points.getPoints());
-            return;
-        }
-
-        if (isPointPullLeft(point)) {
-            player.moveRight(points.getPoints());
-            return;
-        }
-        player.move(points.getPoints());
-    }
-
-
-    boolean isPointPullRight(int result) {
+    public boolean isPointFullRight(int result) {
         return result >= points.getPoints().size();
     }
 
-    boolean isPointPullLeft(int result) {
+    public boolean isPointFullLeft(int result) {
         return result <= 0;
+    }
+
+    public boolean isRightConnect(int point) {
+        return getLine().get(point);
+    }
+
+    public boolean isLeftConnect(int point) {
+        return getLine().get(point - 1);
     }
 
     public List<Boolean> getLine() {
