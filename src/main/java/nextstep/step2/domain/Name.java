5e035d24cbe5d0facc49124cb389fce9c442d296
juhyun <zzang9ha@naver.com>
2023-05-29 20:56:12 +0900

    refactor: Name 동등 비교 추가

--- src/main/java/nextstep/step2/domain/Name.java
+++ src/main/java/nextstep/step2/domain/Name.java
@@ -1,5 +1,7 @@
 package nextstep.step2.domain;
 
+import java.util.Objects;
+
 public class Name {
 
     private static final int MAXIMUM_NAME_SIZE = 5;
@@ -20,5 +22,19 @@ public class Name {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+
+        return Objects.equals(getName(), name1.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
     }
 }
