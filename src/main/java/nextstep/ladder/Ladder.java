2d9aeba631ad5929232129703dec5c1558796040
wotjd4305 <wotjd4305@naver.com>
2023-05-29 23:49:51 +0900

    [step4] feat. Ladder 생성시 예외추가

--- src/main/java/nextstep/ladder/Ladder.java
+++ src/main/java/nextstep/ladder/Ladder.java
@@ -6,7 +6,7 @@ import nextstep.ladder.drawPolicy.DrawablePolicy;
 
 public class Ladder {
 
-    private List<LadderRow> ladderRows;
+    private final List<LadderRow> ladderRows;
 
     public Ladder(List<LadderRow> ladderRows) {
         this.ladderRows = ladderRows;
@@ -14,6 +14,10 @@ public class Ladder {
 
     public Ladder(Persons persons, Height height, DrawablePolicy drawablePolicy){
         ladderRows = new ArrayList<>();
+
+        if (persons.personListSize() <= 0) {
+            throw new IllegalArgumentException("사다리 게임에 참여하는 사람은 0또는 음수가 될 수 없습니다");
+        }
 
         for (int i = 0; i < height.getHeight(); i++) {
             LadderRow ladderRow = new LadderRow(persons.personListSize(), drawablePolicy);
