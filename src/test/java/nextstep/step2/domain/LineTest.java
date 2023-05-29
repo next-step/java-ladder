1c8052fb1aba69b3980c0800ee1cadf29e3b8731
juhyun <zzang9ha@naver.com>
2023-05-29 21:06:18 +0900

    test: Line 이동 테스트 추가

--- src/test/java/nextstep/step2/domain/LineTest.java
+++ src/test/java/nextstep/step2/domain/LineTest.java
@@ -1,7 +1,9 @@
 package nextstep.step2.domain;
 
-import nextstep.step2.generator.RandomBooleanGenerator;
+import nextstep.step2.generator.BooleanGenerator;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -10,20 +12,61 @@ class LineTest {
 
     @Test
     void 참여인원보다_한개_적은_Line_객체를_생성한다() {
-        final var actual = createMockLine();
+        final var actual = createMockLine(() -> false);
 
-        assertThat(actual.points()).containsExactly(false, false, false);
+        assertThat(actual.points()).hasSize(3);
     }
 
     @Test
-    void 참여이원이_존재하지_않으면_예외를_던진다() {
+    void 연속된_라인이_존재하면_예외를_던진다() {
+        final var points = List.of(
+                new Point(true), new Point(true), new Point(true)
+        );
+
         assertThrows(
                 IllegalArgumentException.class,
-                () -> new Line(0, new RandomBooleanGenerator())
+                () -> new Line(points)
         );
     }
 
-    private Line createMockLine() {
-        return new Line(4, () -> false);
+    @Test
+    void 사다리_라인_오른쪽_이동_테스트() {
+        final var points = createPoints(true, false, false);
+        final var line = new Line(points);
+        final var current = 0;
+
+        final var actual = line.move(current);
+
+        assertThat(actual).isEqualTo(current + 1);
+    }
+
+    @Test
+    void 사다리_라인_왼쪽_이동_테스트() {
+        final var points = createPoints(true, false, false);
+        final var line = new Line(points);
+        final var current = 1;
+
+        final var actual = line.move(current);
+
+        assertThat(actual).isEqualTo(current - 1);
+    }
+
+    @Test
+    void 사다리_라인_제자리_테스트() {
+        final var points = createPoints(false, false, false);
+        final var line = new Line(points);
+        final var current = 1;
+
+        final var actual = line.move(current);
+
+        assertThat(actual).isEqualTo(current);
+    }
+
+    static List<Point> createPoints(boolean point1, boolean point2, boolean point3) {
+        return List.of(new Point(point1), new Point(point2), new Point(point3));
+    }
+
+    private Line createMockLine(BooleanGenerator generator) {
+        return new Line(4, generator);
     }
 }
