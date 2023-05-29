8252f350874b6972d049a84042f04ff0914ea7be
juhyun <zzang9ha@naver.com>
2023-05-29 20:56:35 +0900

    test: Participants 로직 테스트 추가

--- src/test/java/nextstep/step2/domain/ParticipantsTest.java
+++ src/test/java/nextstep/step2/domain/ParticipantsTest.java
@@ -3,7 +3,9 @@ package nextstep.step2.domain;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ParticipantsTest {
@@ -14,5 +16,31 @@ class ParticipantsTest {
                 IllegalArgumentException.class,
                 () -> new Participants(new ArrayList<>())
         );
+    }
+
+    @Test
+    void 참여자의_수를_반환한다() {
+        final var participants = createParticipants();
+
+        int actual = participants.count();
+
+        assertThat(actual).isEqualTo(3);
+    }
+
+    @Test
+    void 참여자의_위치를_반환한다() {
+        final var participants = createParticipants();
+
+        final var actual = participants.position("b");
+
+        assertThat(actual).isEqualTo(1);
+    }
+
+    private Participants createParticipants() {
+        final var participants = List.of(
+                new Participant("a"), new Participant("b"), new Participant("c")
+        );
+
+        return new Participants(participants);
     }
 }
