17e3d2e685a3969fdd00e38b8f722b2ccf53f6c4
hoyeoon <hoyeon.backend@gmail.com>
2023-05-30 00:11:10 +0900

    feat: 사다리 2차원 배열 만들기

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -2,9 +2,12 @@ package nextstep.ladder.domain;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Ladder {
+
+    private static final int BEGIN_INDEX = 0;
 
     private final int verticalLineCount;
     private final Lines lines;
@@ -27,4 +30,35 @@ public class Ladder {
     public Lines lines() {
         return lines;
     }
+
+    public String[][] result() {
+        List<Line> lines = this.lines.value();
+
+        int width = verticalLineCount * 2 - 1;
+        int height = this.lines.value().size();
+
+        String[][] ladder = new String[height][width];
+
+        IntStream.range(BEGIN_INDEX, height)
+                .forEach(i -> IntStream.range(BEGIN_INDEX, width)
+                        .forEach(j -> {
+                            if (isVerticalLine(j)) {
+                                ladder[i][j] = "v";
+                                return;
+                            }
+                            if (isHorizontalLine(lines, i, j)) {
+                                ladder[i][j] = "h";
+                            }
+                        }));
+        return ladder;
+    }
+
+    private boolean isVerticalLine(int j) {
+        return j % 2 == 0;
+    }
+
+    private Boolean isHorizontalLine(List<Line> lines, int i, int j) {
+        return lines.get(i).value().get((j - 1) / 2);
+    }
+
 }
--- src/test/java/nextstep/ladder/domain/LadderTest.java
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -2,6 +2,8 @@ package nextstep.ladder.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LadderTest {
@@ -14,5 +16,25 @@ public class LadderTest {
         Ladder ladder = new Ladder(verticalLineCount, height);
         assertThat(ladder.lines().value().size()).isEqualTo(height);
 
+    }
+
+    @Test
+    void 사다리_2차원배열_만들기() {
+        //given
+        int verticalLineCount = 4;
+        Lines lines = new Lines(Arrays.asList(
+                new Line(Arrays.asList(true, false, true)),
+                new Line(Arrays.asList(false, true, false)),
+                new Line(Arrays.asList(true, false, false)),
+                new Line(Arrays.asList(false, true, false)),
+                new Line(Arrays.asList(true, false, true))
+        ));
+
+        //when
+        Ladder ladder = new Ladder(verticalLineCount, lines);
+        String[][] result = ladder.result();
+
+        //then
+        System.out.println(Arrays.deepToString(result));
     }
 }
