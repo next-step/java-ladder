42f3c435a206010d2088c670b13e65b3f959794d
hoyeoon <hoyeon.backend@gmail.com>
2023-05-30 04:19:05 +0900

    refactor: 테스트 수정

--- src/test/java/nextstep/ladder/domain/LineTest.java
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -18,11 +18,11 @@ public class LineTest {
 
     @Test
     void create() {
-        assertThat(line.value().size()).isEqualTo(4);
+        assertThat(line.value().size()).isEqualTo(3);
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3})
+    @ValueSource(ints = {1, 2})
     void 라인이_겹치지_않는지_확인(int idx) {
         Boolean prevPoint = line.value().get(idx - 1);
         Boolean currPoint = line.value().get(idx);
