2c54bcf55d7288b2c1679aa31eb39b9e29708527
dhkimneer <kimds989@naver.com>
2023-05-31 04:50:32 +0900

    tests: Add FailedMakePointsStrategy.java for LineTest

---
+++ src/test/java/ladder/domain/test/FailedMakePointsStrategy.java
@@ -0,0 +1,20 @@
+package ladder.domain.test;
+
+import ladder.domain.MakePointsStrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/**
+ * 연속된 가로줄이 있게 만드는 Strategy
+ */
+public class FailedMakePointsStrategy implements MakePointsStrategy {
+
+    @Override
+    public List<Boolean> makePoints(int countOfPerson) {
+        List<Boolean> points = new ArrayList<>();
+        IntStream.range(0, countOfPerson - 1).forEach(index -> points.add(true));
+        return points;
+    }
+}
