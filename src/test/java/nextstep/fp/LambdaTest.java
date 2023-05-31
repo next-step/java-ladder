18fbd1764f27041162d72092d830b5cae3730464
joooahn <33419257+joooahn@users.noreply.github.com>
2023-05-31 13:31:04 +0900

    feat: step1 구현 (#1854)
    Co-authored-by: ian.ja <ian.ja@kakaocorp.com>

--- src/test/java/nextstep/fp/LambdaTest.java
+++ src/test/java/nextstep/fp/LambdaTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -33,19 +34,19 @@ public class LambdaTest {
 
     @Test
     public void sumAll() throws Exception {
-        int sum = Lambda.sumAll(numbers);
+        int sum = Lambda.sumByCondition(numbers, integer -> true);
         assertThat(sum).isEqualTo(21);
     }
 
     @Test
     public void sumAllEven() throws Exception {
-        int sum = Lambda.sumAllEven(numbers);
+        int sum = Lambda.sumByCondition(numbers, integer -> integer % 2 == 0);
         assertThat(sum).isEqualTo(12);
     }
 
     @Test
     public void sumAllOverThree() throws Exception {
-        int sum = Lambda.sumAllOverThree(numbers);
+        int sum = Lambda.sumByCondition(numbers, integer -> integer > 3);
         assertThat(sum).isEqualTo(15);
     }
 }
