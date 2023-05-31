79561be5380855cce5f24429d21454caa564ba5a
dhkimneer <kimds989@naver.com>
2023-06-01 01:56:59 +0900

    tests: Update ParticipantNameTest.java

--- src/test/java/ladder/domain/ParticipantNameTest.java
+++ src/test/java/ladder/domain/ParticipantNameTest.java
@@ -1,8 +1,9 @@
 package ladder.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class ParticipantNameTest {
 
@@ -13,8 +14,21 @@ public class ParticipantNameTest {
         String participantName = "Arirang";
 
         // then
-        Assertions.assertThatThrownBy(() -> new ParticipantName(participantName))
+        assertThatThrownBy(() -> new ParticipantName(participantName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("사다리 게임에 참여하는 사람의 이름은 최대 5글자입니다.");
+    }
+
+    @Test
+    @DisplayName("[요구사항 2] 사다리 게임 참여자의 이름이 제한 조건을 만족할 경우 올바르게 ParticipantName이 생성된다.")
+    void 요구사항_2() {
+        // given
+        String participantName = "Ari";
+
+        // when
+        ParticipantName participantNameDomain = new ParticipantName(participantName);
+
+        // then
+        assertThat(participantNameDomain.getParticipantNameAsString()).isEqualTo(participantName);
     }
 }
