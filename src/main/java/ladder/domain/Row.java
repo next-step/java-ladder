2b48c397f19d234ebbc489f90bff032a130e30b9
dong <dong.22@kakaoenterprise.com>
2023-05-31 13:31:20 +0900

    refactor : Map 자료구조를 사용하도록 개선

--- src/main/java/ladder/domain/Row.java
+++ src/main/java/ladder/domain/Row.java
@@ -3,17 +3,17 @@ package ladder.domain;
 import ladder.exception.OutOfRoWRangeException;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Row {
     private static final int ROW_CACHE_MAX = 50;
 
-    private static final List<Row> ROW_CACHE =
+    private static final Map<Integer,Row> ROW_CACHE =
             IntStream.rangeClosed(0, ROW_CACHE_MAX)
                     .boxed()
-                    .map(Row::new)
-                    .collect(Collectors.toList());
+                    .collect(Collectors.toMap(i-> i,Row::new));
     private final int value;
 
     private Row(int value) {
@@ -24,10 +24,7 @@ public class Row {
         if (value < 0) {
             throw new OutOfRoWRangeException();
         }
-        if (value <= ROW_CACHE_MAX) {
-            return ROW_CACHE.get(value);
-        }
-        return new Row(value);
+        return ROW_CACHE.computeIfAbsent(value, Row::new);
     }
 
     public boolean isSame(Row otherRow) {
