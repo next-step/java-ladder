ef6a93dcd6ef5bfd39045b45fcebb7b955dc9d54
yes <yes.chicken@kakaocorp.com>
2023-06-01 13:55:53 +0900

    refactor: 어느 방향으로 이동하는지 판단하는 로직을 별도 클래스로 분리

--- src/main/java/nextstep/ladder/domain/LineStrategy.java
+++ src/main/java/nextstep/ladder/domain/LineStrategy.java
@@ -2,6 +2,7 @@ package nextstep.ladder.domain;
 
 import java.util.List;
 
+@FunctionalInterface
 public interface LineStrategy {
     List<Boolean> generate(int n);
 }
--- src/test/java/nextstep/ladder/domain/LineTest.java
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 
 class LineTest {
     @Test
@@ -14,6 +15,13 @@ class LineTest {
     public void invalidCountOfPerson() {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new Line(1, new RandomLineStrategy()));
+    }
+
+    @Test
+    @DisplayName("라인은 겹치도록 생성될 수 없다")
+    public void invalidLine() {
+        assertThatIllegalStateException()
+                .isThrownBy(() -> new Line(3, new AllLineStrategy()));
     }
 
     @Test
