1c9847fe54b5529be59392939531c6b4f6f5fc58
dong <dong.22@kakaoenterprise.com>
2023-06-10 09:00:41 +0900

    style : 코드 스타일링

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Set;
-import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
--- src/test/java/ladder/present/RendererTest.java
+++ src/test/java/ladder/present/RendererTest.java
@@ -16,7 +16,7 @@ import java.util.Set;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class RendererTest {
 
