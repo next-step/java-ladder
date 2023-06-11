74603855e35a71bfc603f1b7b56a2ceba2c99da8
dong <dong.22@kakaoenterprise.com>
2023-06-12 00:42:42 +0900

    refactor : 예외처리에서 완성되지 않은 부분 수정

--- src/main/java/ladder/domain/User.java
+++ src/main/java/ladder/domain/User.java
@@ -41,8 +41,10 @@ public class User {
 
   @Override
   public String toString() {
-    return "User{" +
-        "name='" + name + '\'' +
-        '}';
+    return "User{"
+        + "name='"
+        + name
+        + '\''
+        + '}';
   }
 }
--- src/main/java/ladder/exception/OutOfLadderHeightException.java
+++
@@ -1,8 +0,0 @@
-package ladder.exception;
-
-public class OutOfLadderHeightException extends IllegalArgumentException {
-
-  public OutOfLadderHeightException() {
-    super("입력한 사다리의 높이가 너무 높습니다");
-  }
-}
