651b36a1d31d3d24266f357f6e504cc08436a7d9
SeokyungKim <kskyung0624@gmail.com>
2023-06-30 14:43:49 +0900

    refactor : LadderGame이 가지던 기능을 Main으로 옮기고, LadderFactory의 LadderBarStatusDecider 인스턴스를 주입받도록 변경한다

--- src/main/java/nextstep/ladder/Main.java
+++ src/main/java/nextstep/ladder/Main.java
@@ -1,8 +1,23 @@
 package nextstep.ladder;
 
 
+import nextstep.ladder.domain.Ladder;
+import nextstep.ladder.domain.LadderFactory;
+import nextstep.ladder.domain.PlayerNames;
+import nextstep.ladder.domain.RandomLadderBarStatusDecider;
+import nextstep.ladder.presentation.InputView;
+import nextstep.ladder.presentation.ResultView;
+
 public class Main {
+  private static final InputView inputView = new InputView();
+  private static final ResultView resultView = new ResultView();
+
   public static void main(String[] args) {
-    new LadderGame().play();
+    PlayerNames playerNames = inputView.getPlayerNames();
+    int ladderLength = inputView.getLadderLength();
+
+    Ladder ladder = LadderFactory.createLadder(ladderLength, playerNames.size(), new RandomLadderBarStatusDecider());
+
+    resultView.printResult(playerNames, ladder);
   }
 }
