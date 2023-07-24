eb38c708370f327a77c61b704e9585ec0408d3ba
rkd5656meta <rkd8307@naver.com>
2023-07-25 01:35:05 +0900

    test: indexOf 기능 테스트
    - name 기반 위치를 리턴 해주는 indexOf 기능 테스트
    - 없는 이름 검색시 IllegalArgumentException 처리 테스트

--- src/test/java/nextstep/ladder/domain/PlayersTest.java
+++ src/test/java/nextstep/ladder/domain/PlayersTest.java
@@ -1,6 +1,8 @@
 package nextstep.ladder.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -22,6 +24,25 @@ public class PlayersTest {
 
         assertThat(result).isEqualTo("   kbc  kbc2");
     }
+
+    @ParameterizedTest
+    @CsvSource(delimiter = ':', value = {"kbc:0", "kbc3:2", "kbc4:3"})
+    void Player_위치_반환_테스트(String name, int expected){
+        Players players = new Players(List.of(new Player("kbc"), new Player("kbc2"), new Player("kbc3"), new Player("kbc4")));
+
+        int index = players.indexOf(name);
+
+        assertThat(index).isEqualTo(expected);
+    }
+
+    @Test
+    void 없는_Player_찾기_유효성_체크(){
+        Players players = new Players(List.of(new Player("kbc"), new Player("kbc2")));
+
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> players.indexOf("kbc3"));
+    }
+
 
 
 }
