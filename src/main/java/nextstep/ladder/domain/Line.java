2a4e9cf2f24515f9def99400e6093b28a5f8df31
rkd5656meta <rkd8307@naver.com>
2023-07-25 01:27:41 +0900

    refactor: move 기능 추가 및 Collectors.joining 사용
    - point 위치 기반으로 움직인 후의 point 위치 반환 해주는 move 메소드 추가
    - toString 사용 시 Collectors.joining 사용으로 변환

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -12,6 +12,68 @@ public class Line {
 
     @Override
     public String toString() {
-        return String.join("", points.stream().map(Point::toString).collect(Collectors.toList()));
+        return points.stream().map(Point::toString).collect(Collectors.joining(""));
     }
+
+    public int move(int pointIndex) {
+        pointsRangeCheck(pointIndex);
+
+        if (isStartIndex(pointIndex)) {
+            return moveToRightPoint(pointIndex);
+        }
+
+        if (isEndIndex(pointIndex)){
+            return moveToLeftPoint(pointIndex);
+        }
+
+        return moveToBothPoint(pointIndex);
+    }
+
+    private void pointsRangeCheck(int pointIndex) {
+        int maxIndex = points.size()-1;
+
+        if (pointIndex < 0 || maxIndex < pointIndex){
+            throw new IllegalArgumentException("point index는 0~"+maxIndex+" 입니다.");
+        }
+    }
+
+    private boolean isStartIndex(int pointIndex){
+        return pointIndex == 0;
+    }
+
+    private boolean isEndIndex(int pointIndex){
+        return points.size()-1 == pointIndex;
+    }
+
+    private int moveToBothPoint(int pointIndex){
+        int leftPointIndex = moveToLeftPoint(pointIndex);
+
+        if (leftPointIndex == pointIndex){
+            return moveToRightPoint(pointIndex);
+        }
+
+        return leftPointIndex;
+    }
+
+    private int moveToLeftPoint(int pointIndex){
+        if (isMovable(pointIndex)){
+            return pointIndex-1;
+        }
+
+        return pointIndex;
+    }
+
+    private int moveToRightPoint(int pointIndex){
+        if (isMovable(pointIndex + 1)){
+            return pointIndex+1;
+        }
+
+        return pointIndex;
+    }
+
+    private Boolean isMovable(int pointIndex) {
+        return points.get(pointIndex).isMovable();
+    }
+
+
 }
