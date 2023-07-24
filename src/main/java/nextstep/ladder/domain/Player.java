3b961327ee3d5d947c11d2d25399900528f8055b
rkd5656meta <rkd8307@naver.com>
2023-07-25 01:56:30 +0900

    refactor: equals, hashcode 추가 및 메소드 순서 변경

--- src/main/java/nextstep/ladder/domain/Player.java
+++ src/main/java/nextstep/ladder/domain/Player.java
@@ -19,15 +19,6 @@ public class Player {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        return nameForPrint();
-    }
-
-    private String nameForPrint() {
-        return BLANK.repeat(NAME_PRINT_LENGTH - name.length()) + name;
-    }
-
     private void nullCheck(String name) {
         if (Objects.isNull(name) || name.isBlank()) {
             throw new IllegalArgumentException("참여자 이름을 입력해 주세요.");
@@ -38,5 +29,29 @@ public class Player {
         if (NAME_MAX_LENGTH < name.length()) {
             throw new IllegalArgumentException("참여자 이름은 1~5자로 설정 하세요.");
         }
+    }
+
+    @Override
+    public String toString() {
+        return nameForPrint();
+    }
+
+    private String nameForPrint() {
+        return BLANK.repeat(NAME_PRINT_LENGTH - name.length()) + name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Player player = (Player) o;
+
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
     }
 }
