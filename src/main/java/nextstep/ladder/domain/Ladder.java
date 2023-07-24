d3cbebb8af03d6e12924b04a65849b51b7750533
rkd5656meta <rkd8307@naver.com>
2023-07-25 01:56:49 +0900

    refactor: toString 순서 변경 및 Players 객체 final로 변경

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class Ladder {
 
-    public final Players players;
+    private final Players players;
     private final PointFactory pointFactory = new DefaultPointFactory();
 
     public final List<Line> ladder;
@@ -17,11 +17,6 @@ public class Ladder {
         this.players = players;
 
         this.ladder = createLine(ladderHeight);
-    }
-
-    @Override
-    public String toString() {
-        return players.toString() + "\n" + linesToString();
     }
 
     private String linesToString() {
@@ -44,5 +39,10 @@ public class Ladder {
         if (ladderHeight <= 0) {
             throw new IllegalArgumentException("높이는 1 이상으로 설정 하세요");
         }
+    }
+
+    @Override
+    public String toString() {
+        return players.toString() + "\n" + linesToString();
     }
 }
