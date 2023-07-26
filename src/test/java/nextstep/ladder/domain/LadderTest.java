dfd042d57fe0cb6708fc24e0daa3f7fd2a7b6c75
rkd5656meta <rkd8307@naver.com>
2023-07-26 18:17:02 +0900

    test: movePlayer, movePlayerAll 테스트 추가

--- src/test/java/nextstep/ladder/domain/LadderTest.java
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -1,18 +1,54 @@
 package nextstep.ladder.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LadderTest {
 
+    private Ladder ladder;
+    private WinningItems winningItems;
+
+    @BeforeEach
+    void init() {
+        Players players = new Players("kbc", "kbc1", "kbc2");
+        List<Line> lines = List.of(
+                new Line(players.numberOfPlayers(), c -> List.of(new Point(false), new Point(false), new Point(true))),
+                new Line(players.numberOfPlayers(), c -> List.of(new Point(false), new Point(false), new Point(true))),
+                new Line(players.numberOfPlayers(), c -> List.of(new Point(false), new Point(true), new Point(false))),
+                new Line(players.numberOfPlayers(), c -> List.of(new Point(false), new Point(false), new Point(true)))
+        );
+        ladder = new Ladder(lines, players);
+        winningItems = new WinningItems(players.numberOfPlayers(), "꽝", "1000", "2000");
+    }
+
     @Test
-    void 높이_유효성_테스트(){
-        Players players = new Players(List.of(new Player("kbc"), new Player("kbc2")));
+    void 높이_유효성_테스트() {
+        Players players = new Players(List.of(Player.of("kbc"), Player.of("kbc2")));
 
         assertThatIllegalArgumentException().isThrownBy(
                 () -> new Ladder(0, players));
+    }
+
+    @Test
+    void movePlayer_테스트() {
+        PlayerWinnings playerWinnings = ladder.movePlayer("kbc", winningItems);
+
+        String result = playerWinnings.toString();
+
+        assertThat(result).isEqualTo("kbc: 2000");
+    }
+
+    @Test
+    void movePlayerAll_테스트() {
+        List<PlayerWinnings> playerWinnings = ladder.movePlayerAll(winningItems);
+
+        String result = playerWinnings.toString();
+
+        assertThat(result).isEqualTo("[kbc: 2000, kbc1: 꽝, kbc2: 1000]");
     }
 }
