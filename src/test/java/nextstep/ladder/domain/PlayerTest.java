123d601357a2417e49fb2413faef986a122b601a
rkd5656meta <rkd8307@naver.com>
2023-07-26 18:50:15 +0900

    test: 이름 출력 테스트 삭제 및 Player of롤 객체 생성 변경

--- src/test/java/nextstep/ladder/domain/PlayerTest.java
+++ src/test/java/nextstep/ladder/domain/PlayerTest.java
@@ -1,10 +1,7 @@
 package nextstep.ladder.domain;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -14,24 +11,15 @@ public class PlayerTest {
     void 이름_유효성_체크() {
         assertAll(
                 () -> assertThatIllegalArgumentException().isThrownBy(
-                        () -> new Player(null)
+                        () -> Player.of(null)
                 ), () -> assertThatIllegalArgumentException().isThrownBy(
-                        () -> new Player("           ")
+                        () -> Player.of("           ")
                 ), () -> assertThatIllegalArgumentException().isThrownBy(
-                        () -> new Player("")
+                        () -> Player.of("")
                 ), () -> assertThatIllegalArgumentException().isThrownBy(
-                        () -> new Player("fiveover")
+                        () -> Player.of("fiveover")
                 )
         );
     }
-
-    @ParameterizedTest
-    @CsvSource(delimiter = ':', value = {"kbc:.    kbc", "kang:.   kang", "kbc12:.  kbc12"})
-    void 이름_출력_테스트(String name, String expected) {
-        String playerName = new Player(name).toString();
-
-        assertThat(playerName).isEqualTo(expected.replace(".", ""));
-    }
-
 
 }
