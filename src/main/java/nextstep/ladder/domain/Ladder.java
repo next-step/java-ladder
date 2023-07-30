17167c0d60160b1f17f61d95f5faffcfcb74f673
rkd5656meta <rkd8307@naver.com>
2023-07-31 04:04:03 +0900

    refactor: 사용하지 않는 코드 정리 및 코드 컨벤션 정리

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -1,6 +1,5 @@
 package nextstep.ladder.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -40,22 +39,6 @@ public class Ladder {
         }
 
         return index;
-    }
-
-    private List<Line> createLine(int ladderHeight) {
-        List<Line> lines = new ArrayList<>();
-
-        for (int i = 0; i < ladderHeight; i++) {
-            lines.add(new Line(this.numberOfPlayers, pointFactory));
-        }
-
-        return lines;
-    }
-
-    private void checkLadderHeight(int ladderHeight) {
-        if (ladderHeight <= 0) {
-            throw new IllegalArgumentException("높이는 1 이상으로 설정 하세요");
-        }
     }
 
     @Override
