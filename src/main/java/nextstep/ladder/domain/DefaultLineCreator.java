063d040e0e81ede54ed0c871072e085ace903858
rkd5656meta <rkd8307@naver.com>
2023-07-31 04:04:30 +0900

    refactor: 코드 컨벤션

--- src/main/java/nextstep/ladder/domain/DefaultLineCreator.java
+++ src/main/java/nextstep/ladder/domain/DefaultLineCreator.java
@@ -3,7 +3,7 @@ package nextstep.ladder.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DefaultLineCreator implements LineCreator{
+public class DefaultLineCreator implements LineCreator {
 
     private final int ladderHeight;
 
