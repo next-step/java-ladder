6858853e125ee5a362b98888f08b58f99a1afc4a
rkd5656meta <rkd8307@naver.com>
2023-07-31 02:06:42 +0900

    refactor:  players 분리를 위한 리팩토링 진행
    - MoveResult를 return 하는 movePlayer 추가

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -9,25 +9,37 @@ public class Ladder {
 
     private final List<Line> ladder;
     private final Players players;
+    private final int numberOfPlayers;
+
     private final PointFactory pointFactory = new DefaultPointFactory();
+
+    public Ladder(int ladderHeight, int numberOfPlayers) {
+        checkLadderHeight(ladderHeight);
+
+        this.players = null;
+        this.ladder = createLine(ladderHeight);
+        this.numberOfPlayers = numberOfPlayers;
+    }
 
     public Ladder(int ladderHeight, Players players) {
         checkLadderHeight(ladderHeight);
 
         this.players = players;
         this.ladder = createLine(ladderHeight);
+        this.numberOfPlayers = 0;
     }
 
     public Ladder(List<Line> ladder, Players players) {
         this.players = players;
         this.ladder = ladder;
+        this.numberOfPlayers = 0;
     }
 
     public PlayerWinnings movePlayer(String name, WinningItems winningItems) {
         return createPlayerWinnings(players.indexOf(name), winningItems);
     }
 
-    public List<PlayerWinnings> movePlayerAll(WinningItems winningItems) {
+    public List<PlayerWinnings> movePlayer(WinningItems winningItems) {
         List<PlayerWinnings> playerWinnings = new ArrayList<>();
         IntStream.range(0, players.numberOfPlayers())
                 .forEach(i -> playerWinnings.add(createPlayerWinnings(i, winningItems)));
@@ -37,6 +49,24 @@ public class Ladder {
 
     private PlayerWinnings createPlayerWinnings(int playerIndex, WinningItems winningItems) {
         return new PlayerWinnings(Player.of(players.getPlayerNameByIndex(playerIndex)), winningItems.getWinningItem(move(playerIndex)));
+    }
+
+    private MoveResult movePlayer(int playerIndex) {
+        MoveResult moveResult = new MoveResult();
+
+        moveResult.put(playerIndex, move(playerIndex));
+
+        return new MoveResult();
+    }
+
+    private MoveResult movePlayer() {
+        MoveResult moveResult = new MoveResult();
+
+        for (int playerIndex = 0; playerIndex < numberOfPlayers; playerIndex++) {
+            moveResult.put(playerIndex, move(playerIndex));
+        }
+
+        return new MoveResult();
     }
 
     private int move(int index) {
