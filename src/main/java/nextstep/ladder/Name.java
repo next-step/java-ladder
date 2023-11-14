b15ca0029ec92c1cae82167a01b0ff5bf30064cc
g0rani <pjw0405@naver.com>
2023-11-14 20:06:05 +0900

    refactor: 이름에 대한 공백 체크 구현
    - null 과 공백에 대한 예외 처리

--- src/main/java/nextstep/ladder/Name.java
+++ src/main/java/nextstep/ladder/Name.java
@@ -5,8 +5,19 @@ public class Name {
     private final String name;
 
     public Name(String name) {
+        validateBlank(name);
         validateNameOverThenFive(name);
         this.name = name;
+    }
+
+    private void validateBlank(String name) {
+        if (isBlank(name)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isBlank(String name) {
+        return name == null || name.isBlank();
     }
 
     private static void validateNameOverThenFive(String name) {
