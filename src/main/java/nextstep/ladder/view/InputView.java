9a96d5972427e67f446411d28eb1d382634185bc
g0rani <pjw0405@naver.com>
2023-11-15 17:42:02 +0900

    refactor: InputView 상수 접근 제어자 수정

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -9,7 +9,7 @@ public class InputView {
 
     private static final String INPUT_NAMES = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String COMMA = ",";
-    public static final String INPUT_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
+    private static final String INPUT_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
 
     private final Scanner scanner;
 
