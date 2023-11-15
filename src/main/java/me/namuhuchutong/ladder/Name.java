e16378ddb5ca00d4675fbb783d0d6ae135457ec5
namuhuchutong <namuhuchutong@gmail.com>
2023-11-15 22:00:14 +0900

    feat: 사용자 이름 최대 5자 제한 기능 구현

---
+++ src/main/java/me/namuhuchutong/ladder/Name.java
@@ -0,0 +1,21 @@
+package me.namuhuchutong.ladder;
+
+public class Name {
+
+    private final String name;
+
+    public Name(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름의 크기는 5자를 넘을 수 없습니다.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+}
