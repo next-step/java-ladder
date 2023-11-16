cddd46e8e9d06b4b64fcc95d4dc2a8a662f04799
hyeonjinnoh <hyeonjin.noh@sixshop.com>
2023-11-17 00:00:31 +0900

    feat: 사용자로부터 최대 사다리 높이를 입력받을 수 있다

--- src/main/java/ladder/controller/LadderController.java
+++ src/main/java/ladder/controller/LadderController.java
@@ -9,6 +9,7 @@ public class LadderController {
         InputView inputView = new InputView();
 
         Names names = new Names(inputView.names());
+        int ladderHeight = inputView.ladderHeight();
     }
 
 }
--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -6,12 +6,18 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String ASK_NAMES = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String ASK_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
     private static final String COMMA = ",";
     private static final Scanner scanner = new Scanner(System.in);
 
     public List<String> names() {
         System.out.println(ASK_NAMES);
         return List.of(scanner.nextLine().split(COMMA));
+    }
+
+    public int ladderHeight() {
+        System.out.println(ASK_LADDER_HEIGHT);
+        return scanner.nextInt();
     }
 
 }
