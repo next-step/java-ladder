417257409d67930e12cbf02661661f90acdcb7f3
hyeonjinnoh <hyeonjin.noh@sixshop.com>
2023-11-16 23:47:09 +0900

    feat: 사람 이름은 쉼표(,)를 기준으로 구분할 수 있다

--- src/main/java/ladder/controller/LadderController.java
+++ src/main/java/ladder/controller/LadderController.java
@@ -2,13 +2,14 @@ package ladder.controller;
 
 import ladder.view.InputView;
 
+import java.util.List;
+
 public class LadderController {
 
     public static void main(String[] args) {
         InputView inputView = new InputView();
 
-        String names = inputView.names();
-        System.out.println(names);
+        List<String> names = inputView.names();
     }
 
 }
--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -1,15 +1,17 @@
 package ladder.view;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
     private static final String ASK_NAMES = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String COMMA = ",";
     private static final Scanner scanner = new Scanner(System.in);
 
-    public String names() {
+    public List<String> names() {
         System.out.println(ASK_NAMES);
-        return scanner.nextLine();
+        return List.of(scanner.nextLine().split(COMMA));
     }
 
 }
