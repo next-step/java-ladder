bc364f66bccd22817399e6efb06253af8f0ca5ea
HyunggiPark <phg9587@naver.com>
2023-11-19 19:30:53 +0900

    feat: 사람 이름은 최대 5글자 까지만 허용한다.

--- src/main/java/nextstep/ladder/input/UserNames.java
+++ src/main/java/nextstep/ladder/input/UserNames.java
@@ -2,6 +2,7 @@ package nextstep.ladder.input;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserNames {
     private final List<String> names;
@@ -9,7 +10,17 @@ public class UserNames {
     public UserNames(final String userNamesText) {
         final String[] splitTexts = userNamesText.split(",");
 
-        this.names = Arrays.asList(splitTexts);
+        this.names = Arrays.stream(splitTexts)
+                .filter(name -> name.length() <= 5)
+                .collect(Collectors.toUnmodifiableList());
+
+        if (!isSameSize(splitTexts.length)) {
+            throw new IllegalArgumentException("이름은 5글자 이하만 가능합니다.");
+        }
+    }
+
+    private boolean isSameSize(final int size) {
+        return this.names.size() == size;
     }
 
     public List<String> getNames() {
--- src/test/java/nextstep/ladder/input/UserNamesTest.java
+++ src/test/java/nextstep/ladder/input/UserNamesTest.java
@@ -7,10 +7,11 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class UserNamesTest {
     @ParameterizedTest
-    @ValueSource(strings = {"pobi,honux,crong,jk", "A,B,C", "12-34,1-2-3,123"})
+    @ValueSource(strings = {"pobi,honux,crong,jk", "A,B,C", "1,2,3,45-67,8-9-0"})
     @DisplayName("사람 이름을 ','로 구분해서 넣으면, 사람 이름을 저장한 UserNames 객체가 생성된다")
     void testUserNamesConstructor(String text) {
         //given
@@ -22,4 +23,13 @@ class UserNamesTest {
         assertThat(userNames.getNames()).containsExactlyElementsOf(Arrays.asList(text.split(",")));
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"pobibb,honux,crong,jk", "A,B,C,DEFGHI", "12-34-1,2,3,4"})
+    @DisplayName("사람 이름 중 5글자를 넘는 이름이 있으면, IllegalArgumentException이 발생한다")
+    void throwIllegalArgumentExceptionIfNameIsLongerThan5(String text) {
+        //given, when, then
+        assertThatThrownBy(() -> new UserNames(text))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 5글자 이하만 가능합니다.");
+    }
 }
