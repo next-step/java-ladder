4b5b29e9250788dfda6f1c83dfc425539979ddea
KimGwangHoon <khkimykk@naver.com>
2023-11-20 01:38:04 +0900

    test: 사다리 가로칸 테스트

---
+++ src/test/java/nextstep/ladder/domian/LineTest.java
@@ -0,0 +1,29 @@
+package nextstep.ladder.domian;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class LineTest {
+
+    private Line line;
+
+    void createLine(List<Boolean> points) {
+        line = new Line(points);
+    }
+
+    @DisplayName("사다리 가로칸을 생성한다")
+    @Test
+    void 가로_칸을_생성한다() {
+        List<Boolean> points = new ArrayList<>();
+        points.add(false);
+        points.add(true);
+        points.add(false);
+        createLine(points);
+        assertThat(line.points).usingRecursiveComparison().isEqualTo(new Line(points));
+    }
+}
