e6452a848e9f9f294838e2074c3d511b50dd3147
엄태권 <akfls221@gmail.com>
2023-11-19 17:10:23 +0900

    feat : Ladder 테스트 및 클래스 추가

---
+++ src/test/java/ladder/domain/LadderTest.java
@@ -0,0 +1,33 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class LadderTest {
+
+    @Test
+    @DisplayName("사다리의 높이를 5로 지정할 경우 사다리의 Line은 5개가 생성된다.")
+    void createLadder() {
+        int totalLineCount = 5;
+        int joinMemberCount = 4;
+
+        Ladder ladder = new Ladder(totalLineCount, joinMemberCount, () -> true);
+
+        assertThat(ladder.getLines()).hasSize(5);
+    }
+
+    @ParameterizedTest(name = "사다리의 높이를 {0} 으로 생성시 오류가 발생한다.")
+    @ValueSource(ints = {0, -1})
+    void createLadder_0_음수(int totalLineCount) {
+        int joinMemberCount = 4;
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Ladder(totalLineCount, joinMemberCount, () -> true))
+                .withMessage("최대 사다리 높이는 0개 이하로 생성할 수 없습니다.");
+    }
+}
