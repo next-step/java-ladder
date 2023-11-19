ba2cd89f529d4259b1b6de6872472f9d8e730876
jinhyeon <70142711+qwe5507@users.noreply.github.com>
2023-11-19 11:05:19 +0900

    Step1 - 스트림, 람다, Optional (#1889)
    * 람다 실습 1 - 익명 클래스를 람다로 전환
    
    * 람다 실습 2 - 람다를 활용해 중복 제거
    
    * map, reduce, filter 실습
    
    * Optional 실습

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -27,30 +27,20 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAll(numbers, (n) -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, (n) -> n % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, (n) -> n > 3);
+    }
+
+    private static int sumAll(List<Integer> numbers, Conditional conditional) {
+        return numbers.stream()
+                .filter(conditional::isCalculable)
+                .reduce(0, Integer::sum);
     }
 }
