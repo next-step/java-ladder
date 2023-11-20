31fb5c78f4a9445568f0dd94928f0821f71d8a90
sukhyunmoon <sukhyunmoon@sk.com>
2023-11-20 17:32:02 +0900

    refactor: 코드 정리

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -5,14 +5,10 @@ import java.util.List;
 
 public class Ladder {
 
-    private final List<Row> rows;
+    private final List<Row> rows = new ArrayList<>();;
 
-    public Ladder() {
-        this.rows = new ArrayList<>();
-    }
-
-    public List<Row> make(int wallCount, int Height){
-        for(int i = 0; i < Height; i++){
+    public List<Row> make(int wallCount, int height){
+        for(int i = 0; i < height; i++){
             this.rows.add(new Row(wallCount));
         }
         return this.rows;
