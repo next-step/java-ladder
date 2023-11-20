11a6a4e945e499c08f4bc8dc5ee9a4ad5fa9fd1a
ahlight <107451991+jonghunbaek@users.noreply.github.com>
2023-11-21 01:32:37 +0900

    사다리 미션 - 1단계 리뷰요청합니다! (#1899)
    * refactor : 람다 실습 1 구현 완료
    
    * refactor : 람다 실습 2 구현 완료
    
    * refactor : stream실습 1,2 구현 완료
    
    * refactor : optional실습 구현 완료

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
@@ -27,30 +28,21 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return calculate(number -> true, numbers);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return calculate(number -> number % 2 == 0, numbers);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return calculate(number -> number > 3, numbers);
+    }
+
+    private static int calculate(Predicate<Integer> predicate, List<Integer> numbers) {
+        return numbers.stream()
+            .mapToInt(number -> number)
+            .filter(predicate::test)
+            .sum();
     }
 }
