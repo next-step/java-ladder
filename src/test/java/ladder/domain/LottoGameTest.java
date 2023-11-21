3c9e40f564b3a95ee5112f3956a9963ddfa448a6
sukhyunmoon <sukhyunmoon@sk.com>
2023-11-21 18:41:41 +0900

    test: LottoGame test

--- src/test/java/ladder/domain/LottoGameTest.java
+++ src/test/java/ladder/domain/LottoGameTest.java
@@ -1,2 +1,24 @@
-package ladder.domain;public class LottoGameTest {
+package ladder.domain;
+
+import ladder.LadderGame;
+import ladder.domain.util.RandomBooleanGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoGameTest {
+
+    @Test
+    @DisplayName("로또 게임 결과의 집합은 입력된 로또게임 결과의 항목과 모두 일치해야한다.")
+    void createLottoGameResult(){
+        Names names = new Names(List.of(new Name("ryan"), new Name("jason"), new Name("brown")));
+        List<String> prizes = List.of("꽝", "꽝", "3등");
+        Ladder ladder = new Ladder(names.names().size()-1, 6, new RandomBooleanGenerator()).makeLadder();
+        Map<Name, String> result = LadderGame.playing(names, prizes, ladder);
+        assertThat(prizes).containsAll(result.values());
+    }
 }
