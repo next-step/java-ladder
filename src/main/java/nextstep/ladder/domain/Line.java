8d504cc678088488d598280aab06a9e6f0888d36
KimGwangHoon <khkimykk@naver.com>
2023-11-21 23:25:40 +0900

    style: 최소 너비 상수, 생성자 사용 순서에 맞춰 메서드 위치 변경

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 public class Line {
 
+    private static final int MIN_WIDTH = 2;
+
     private final List<Boolean> points = new ArrayList<>();
 
     public Line(int width, RuleStrategy ruleStrategy) {
@@ -21,17 +23,17 @@ public class Line {
         }
     }
 
+    private void validateWidth(int width) {
+        if (width < MIN_WIDTH) {
+            throw new LineSizeException();
+        }
+    }
+
     private boolean beforePointCheck(boolean beforePoint, RuleStrategy ruleStrategy) {
         if (beforePoint) {
             return false;
         }
         return ruleStrategy.build();
-    }
-
-    private void validateWidth(int width) {
-        if (width < 2) {
-            throw new LineSizeException();
-        }
     }
 
     public List<Boolean> points() {
