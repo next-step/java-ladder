bafa10098b8768ce461e1363e37d11ddbbb9f26a
gilbertlim <9ilbert.app@gmail.com>
2023-11-21 22:38:50 +0900

    feat: 최대 사다리 높이 입력 구현

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -9,7 +9,14 @@ import static ladder.view.inputParser.parse;
 public class InputView {
 
     public static PersonNames inputNames() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         Scanner scanner = new Scanner(System.in);
         return parse(scanner.nextLine());
+    }
+
+    public static int inputLadderMaxHeight() {
+        System.out.println("\n최대 사다리 높이는 몇 개인가요?");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
     }
 }
