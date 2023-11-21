d5170e1f882c6ee913444d8874255b56d9bc4578
hyeonjinnoh <hyeonjin.noh@sixshop.com>
2023-11-21 20:35:03 +0900

    test: 행에 대해 경계값과 이동가능여부 테스트케이스 추가

--- src/test/java/ladder/RowTest.java
+++ src/test/java/ladder/RowTest.java
@@ -4,9 +4,12 @@ import ladder.domain.Row;
 import ladder.exception.LineDuplicateException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class RowTest {
@@ -19,6 +22,19 @@ public class RowTest {
                 new Row(Arrays.asList(false, true, true));
             })
             .withMessageMatching("가로 라인은 겹칠 수 없습니다");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 3})
+    @DisplayName("행의 경계값을 벗어나는지 확인할 수 있다")
+    public void row_boundary(int index) {
+        assertThat(new Row(Arrays.asList(true, false, false)).isBoundary(index)).isTrue();
+    }
+
+    @Test
+    @DisplayName("행에서 이동가능한 위치를 확인할 수 있다")
+    public void row_available() {
+        assertThat(new Row(Arrays.asList(true, false, false)).available(0)).isTrue();
     }
 
 }
