316b71ff1c562a60d9bb9bf67bfce0f5443c702a
taegoon <hantaegune@gmail.com>
2023-11-22 23:37:46 +0900

    refactor: 람다 실습 1 - 익명 클래스를 람다로 전환 람다 실습 2 - 람다를 활용해 중복 제거 feture: map, reduce, filter 실습 1,2 요구사항 1 - Optional을 활용해 조건에 따른 반환 요구사항 2 - Optional에서 값을 반환 요구사항 3 - Optional에서 exception 처리

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,6 +29,14 @@ public class StreamStudy {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(word -> word.length() > 12)                         // 12자를 초과하는 단어 필터링
+                .distinct()                                                 // 중복 단어 제거
+                .sorted(Comparator.comparingInt(String::length).reversed()) // 단어 길이에 따라 내림차순 정렬
+                .limit(100)                                                 // 상위 100개 단어만 선택
+                .map(String::toLowerCase)                                   // 모든 단어를 소문자로 변환
+                .forEach(System.out::println);                              // 각 단어 출력
+
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +48,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(number -> number > 3)
+                .map(number -> number * 2)
+                .reduce(0, Integer::sum);
     }
 }
