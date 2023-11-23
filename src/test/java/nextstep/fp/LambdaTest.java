e5615ea23e57dfb1d668308961427f2d671f383b
sunggyupaik <melon9751@naver.com>
2023-11-23 19:41:55 +0900

    [feat] 랃다를 활용해 중복을 제거하라
    람다를 활용해 덧셈 중복을 제거하는 람다를 구현합니다

--- src/test/java/nextstep/fp/LambdaTest.java
+++ src/test/java/nextstep/fp/LambdaTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LambdaTest {
     private List<Integer> numbers;
+    private final static int MIN_NUMBER = 4;
+    private final static Conditional conditional = (number) -> number >= MIN_NUMBER;
 
     @BeforeEach
     public void setup() {
@@ -38,14 +41,32 @@ public class LambdaTest {
     }
 
     @Test
+    public void sumAllWithConditional() throws Exception {
+        int sum = Lambda.sumAll(numbers, number -> true);
+        assertThat(sum).isEqualTo(21);
+    }
+
+    @Test
     public void sumAllEven() throws Exception {
         int sum = Lambda.sumAllEven(numbers);
         assertThat(sum).isEqualTo(12);
     }
 
     @Test
+    public void sumAllEvenWithConditional() throws Exception {
+        int sum = Lambda.sumAllEven(numbers, number -> number % 2 == 0);
+        assertThat(sum).isEqualTo(12);
+    }
+
+    @Test
     public void sumAllOverThree() throws Exception {
         int sum = Lambda.sumAllOverThree(numbers);
+        assertThat(sum).isEqualTo(15);
+    }
+
+    @Test
+    public void sumAllOverThreeWithConditional() throws Exception {
+        int sum = Lambda.sumAllOverThree(numbers, number -> number > 3);
         assertThat(sum).isEqualTo(15);
     }
 }
