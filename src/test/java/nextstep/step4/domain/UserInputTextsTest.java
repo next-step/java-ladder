489a6d7b259c913ad22fe83ac3aa3f38feae28e4
HyunggiPark <phg9587@naver.com>
2023-11-24 02:07:36 +0900

    refact: 테스트 클래스 명 변경

--- src/test/java/nextstep/step4/domain/UserInputTextsTest.java
+++ src/test/java/nextstep/step4/domain/UserInputTextsTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class UserDataTextsTest {
+class UserInputTextsTest {
     @Test
     @DisplayName("참여할 사람들의 이름을 입력으로 UserInputTexts 객체를 생성할 수 있다.")
     void testUserInputTextsConstructor() {
@@ -21,7 +21,7 @@ class UserDataTextsTest {
     }
 
     @Test
-    @DisplayName("참여할 사람들의 이름 중 5글자가 넘는 이름이 있으면, IllegalArgumentException이 발생한다.")
+    @DisplayName("참여할 사람들의 이름 중 " + UserInputTexts.MAX_TEXT_LENGTH + "글자가 넘는 이름이 있으면, IllegalArgumentException이 발생한다.")
     void testUserInputTextsConstructorWithExceptionIfNameLengthIsLongerThan5() {
         //given
         final String userNamesText = "pobi,honux,crong,jk,pooobie";
@@ -29,6 +29,6 @@ class UserDataTextsTest {
         //when, then
         assertThatThrownBy(() -> new UserInputTexts(userNamesText))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("이름 및 실행결과는 5글자까지만 허용합니다.");
+                .hasMessage("이름 및 실행결과는 " + UserInputTexts.MAX_TEXT_LENGTH + "글자까지만 허용합니다.");
     }
 }
