3fbb8ad7d68b5176bd04a5d9a26ee6c9a007b707
jeonghuntech <jeonghun.tech@gmail.com>
2023-11-24 23:55:19 +0900

    test: 사다리 생성 테스트 추가

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -5,13 +5,17 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class LadderTest {
 
     @Test
     @DisplayName("입력 받은 수 만큼 사다리를 생성한다. 최소 높이는 2개 이상이어야 한다.")
     public void create(){
-        Assertions.assertThatThrownBy(() -> {
-            Ladder.of(1,10, new RandomPathStrategy());
-        }).isInstanceOf(IllegalArgumentException.class);
+        Ladder ladder = Ladder.of(2, 2,  size -> List.of(true, false));
+        Assertions.assertThat(ladder).isEqualTo(Ladder.of(2, 2,  size -> List.of(true, false)));
+        Assertions.assertThat(ladder).isNotEqualTo(Ladder.of(3, 3,  size -> List.of(true, false, true)));
+
+        Assertions.assertThatThrownBy(() -> Ladder.of(1,10, new RandomPathStrategy())).isInstanceOf(IllegalArgumentException.class);
     }
 }
