190ce98e9b6e2e84bfe551cb4d9f19dd951d5cec
hhj <abc@def.com>
2023-11-24 17:39:22 +0900

    refactor : test 코드 원복

--- src/test/java/nextstep/optional/UsersTest.java
+++ src/test/java/nextstep/optional/UsersTest.java
@@ -9,13 +9,13 @@ public class UsersTest {
     @Test
     public void getUser() {
         Users users = new Users();
-        assertThat(users.getUser("crong").get()).isEqualTo(new User("crong", 35));
+        assertThat(users.getUser("crong")).isEqualTo(new User("crong", 35));
     }
 
 
     @Test
     public void getDefaultUser() {
         Users users = new Users();
-        assertThat(users.getUser("codesquard").get()).isEqualTo(Users.DEFAULT_USER);
+        assertThat(users.getUser("codesquard")).isEqualTo(Users.DEFAULT_USER);
     }
 }
