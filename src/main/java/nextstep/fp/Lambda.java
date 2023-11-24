c340a8870264e09b30ad504c9e4554a33e5e1921
Jin-Mook <jink0608@naver.com>
2023-11-24 15:37:44 +0900

    [step1] feature 람다 실습 2 - 람다를 활용해 중복 제거

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -26,31 +26,21 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+    public static int sumAll(List<Integer> numbers, SumCondition sumCondition) {
+        return numbers.stream()
+                .filter(sumCondition::isSatisfiedCondition)
+                .reduce(0, Integer::sum);
     }
 
-    public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+    public static int sumAllEven(List<Integer> numbers, SumCondition sumCondition) {
+        return numbers.stream()
+                .filter(sumCondition::isSatisfiedCondition)
+                .reduce(0, Integer::sum);
     }
 
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+    public static int sumAllOverThree(List<Integer> numbers, SumCondition sumCondition) {
+        return numbers.stream()
+                .filter(sumCondition::isSatisfiedCondition)
+                .reduce(0, Integer::sum);
     }
 }
