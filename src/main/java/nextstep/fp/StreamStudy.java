94502f65cad991ad5c5d8c74ab3a4f2ac6842007
jhest <jhest@sk.com>
2023-11-24 14:00:10 +0900

    [step01][stream] practice #2

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -27,7 +27,14 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        System.out.println(
+                words.stream()
+                .filter(w -> w.length() > 12)
+                .sorted()
+                .distinct()
+                .map(String::toLowerCase)
+                .collect(Collectors.toList())
+                .subList(0, 100));
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
