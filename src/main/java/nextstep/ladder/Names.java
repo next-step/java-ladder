f49fde90781fdce28a79ffa5dc0cb6a628d7db3a
이승준 <gaedageul55+git@gmail.com>
2023-11-24 09:12:44 +0900

    이름 생성기 구현

---
+++ src/main/java/nextstep/ladder/Names.java
@@ -0,0 +1,23 @@
+package nextstep.ladder;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 여러개의 이름을 생성하는 유틸용 클래스.
+ * @see Name
+ */
+public class Names {
+    private Names() {
+    }
+
+    public static List<Name> of(String names) {
+        return splitNames(names).stream()
+                .map(Name::new)
+                .collect(Collectors.toList());
+    }
+
+    private static List<String> splitNames(String names) {
+        return List.of(names.split(","));
+    }
+}
