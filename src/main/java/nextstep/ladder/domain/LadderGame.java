58616e48106294478dbbf2900895426b8e85501c
jonghunbaek <qor6028@naver.com>
2023-11-24 17:16:37 +0900

    remove : 불필요한 equals and hashcode 오버라이딩 삭제 및 테스트 삭제

--- src/main/java/nextstep/ladder/domain/LadderGame.java
+++ src/main/java/nextstep/ladder/domain/LadderGame.java
@@ -3,21 +3,18 @@ package nextstep.ladder.domain;
 import nextstep.ladder.controller.dto.GameInfo;
 import nextstep.ladder.domain.wrapper.Players;
 
-import java.util.Objects;
-
 public class LadderGame {
 
     private final Players players;
     private final Ladder ladder;
 
-    public LadderGame(GameInfo gameInfo) {
-        this.players = gameInfo.players();
-        this.ladder = gameInfo.ladder();
-    }
-
     public LadderGame(Players players, Ladder ladder) {
         this.players = players;
         this.ladder = ladder;
+    }
+
+    public LadderGame(GameInfo gameInfo) {
+        this(gameInfo.players(), gameInfo.ladder());
     }
 
     public String playersName() {
@@ -26,18 +23,5 @@ public class LadderGame {
 
     public String ladder() {
         return ladder.toString();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        LadderGame that = (LadderGame) o;
-        return Objects.equals(players, that.players) && Objects.equals(ladder, that.ladder);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(players, ladder);
     }
 }
--- src/test/java/nextstep/ladder/domain/LadderGameTest.java
+++ src/test/java/nextstep/ladder/domain/LadderGameTest.java
@@ -1,38 +1,5 @@
 package nextstep.ladder.domain;
 
-import nextstep.ladder.domain.wrapper.Players;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import static org.assertj.core.api.Assertions.*;
-
 public class LadderGameTest {
 
-    @DisplayName("인자로 GameInfo를 전달받아 사다리 게임을 생성한다.")
-    @Test
-    void createLottoGame() {
-        // given
-        String[] names = {"홍길동", "전창길", "코난"};
-        Ladder ladder = new Ladder(List.of(
-            new Bridges(List.of(true, false, true)),
-            new Bridges(List.of(true, false, true)),
-            new Bridges(List.of(true, false, false)),
-            new Bridges(List.of(false, false, false)),
-            new Bridges(List.of(false, true, false))
-            ));
-        Players players = new Players(IntStream.range(0, names.length)
-            .mapToObj(i -> Player.PlayerBuilder.builder()
-                .name(names[i])
-                .widthPosition(i)
-                .heightPosition(1)
-                .build())
-            .collect(Collectors.toUnmodifiableList()));
-
-        // when & then
-        assertThat(new LadderGame(players, ladder)).isEqualTo(new LadderGame(players, ladder));
-    }
 }
