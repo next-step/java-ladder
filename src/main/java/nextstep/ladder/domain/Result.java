98cd3a5d2948722264369cfd4b80f354dc80ae66
KimGwangHoon <khkimykk@naver.com>
2023-11-24 21:46:14 +0900

    feat: 결과 생성 구현

---
+++ src/main/java/nextstep/ladder/domain/Result.java
@@ -0,0 +1,14 @@
+package nextstep.ladder.domain;
+
+public class Result {
+
+    private final String value;
+
+    public Result(String result) {
+        this.value = result;
+    }
+
+    public String value() {
+        return value;
+    }
+}
--- src/test/java/nextstep/ladder/domian/ResultTest.java
+++ src/test/java/nextstep/ladder/domian/ResultTest.java
@@ -1,5 +1,6 @@
 package nextstep.ladder.domian;
 
+import nextstep.ladder.domain.Result;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
