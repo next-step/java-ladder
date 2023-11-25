efafb29af84024053ecda6e99bcf20c4ee1c9d58
Jinhong Kim <deepred.kim@gmail.com>
2023-11-25 21:58:40 +0900

    요구사항 3 - Optional에서 exception 처리

--- src/main/java/nextstep/optional/ComputerStore.java
+++ src/main/java/nextstep/optional/ComputerStore.java
@@ -1,5 +1,6 @@
 package nextstep.optional;
 
+import java.util.Optional;
 import nextstep.optional.Computer.Soundcard;
 import nextstep.optional.Computer.USB;
 
@@ -21,6 +22,11 @@ public class ComputerStore {
     }
 
     public static String getVersionOptional(Computer computer) {
-        return null;
+        Optional<Computer> optComputer = Optional.ofNullable(computer);
+        return optComputer
+            .map(Computer::getSoundcard)
+            .map(Soundcard::getUsb)
+            .map(USB::getVersion)
+            .orElse(UNKNOWN_VERSION);
     }
 }
