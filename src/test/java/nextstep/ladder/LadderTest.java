5eb6687571d69cfc81e0c627caccfa1cf4460bf5
solchan98 <dev.sol0427@gmail.com>
2023-11-25 16:05:58 +0900

    feat: 사다리 생성 조건, 최소 높이에 대한 실패 테스트 케이스 작성

--- src/test/java/nextstep/ladder/LadderTest.java
+++ src/test/java/nextstep/ladder/LadderTest.java
@@ -1,6 +1,8 @@
 package nextstep.ladder;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -11,5 +13,12 @@ class LadderTest {
         assertThatThrownBy(() -> new Ladder(0, 1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("참여자는 최소 1명 이상이어야 합니다.");
+    }
+
+    @Test
+    void 사다리는_최소_0이상의_높이를_가져야_한다() {
+        assertThatThrownBy(() -> new Ladder(1, -1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("높이는 최소 0이상이어야 합니다.");
     }
 }
