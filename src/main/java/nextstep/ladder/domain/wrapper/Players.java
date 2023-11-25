aee0aa0ca86cc1d7df565d12b78fc34ce6e93d94
jonghunbaek <qor6028@naver.com>
2023-11-25 15:22:49 +0900

    feat : 플레이어 번호, 이동할 위치 값을 전달 받아 플레이어를 이동한다.

--- src/main/java/nextstep/ladder/domain/wrapper/Players.java
+++ src/main/java/nextstep/ladder/domain/wrapper/Players.java
@@ -1,5 +1,6 @@
 package nextstep.ladder.domain.wrapper;
 
+import nextstep.ladder.domain.Coordinate;
 import nextstep.ladder.domain.Player;
 
 import java.util.List;
@@ -20,12 +21,25 @@ public class Players {
             .collect(Collectors.joining());
     }
 
+    // TODO : 마무리해야함
+    public Player findPlayerBy(Coordinate coordinate) {
+        return players.stream()
+            .filter(player -> player.isMatching(coordinate))
+            .findAny()
+            .orElseThrow(() -> new IllegalArgumentException("해당 좌표 값에 존재하는 Player가 없습니다."));
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Players players1 = (Players) o;
         return Objects.equals(players, players1.players);
+    }
+
+    public void move(int playerNo, int x, int y) {
+        players.get(playerNo)
+            .move(x, y);
     }
 
     @Override
--- src/test/java/nextstep/ladder/domain/wrapper/PlayersTest.java
+++ src/test/java/nextstep/ladder/domain/wrapper/PlayersTest.java
@@ -1,5 +1,6 @@
 package nextstep.ladder.domain.wrapper;
 
+import nextstep.ladder.domain.Coordinate;
 import nextstep.ladder.domain.Player;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,15 +18,34 @@ public class PlayersTest {
         // given
         String[] names = {"홍길동", "전창길", "코난"};
 
+        Coordinate.init(new Width(names.length), new Height(5));
         Players players = new Players(IntStream.range(0, names.length)
-            .mapToObj(i -> Player.PlayerBuilder.builder()
-                .name(names[i])
-                .widthPosition(i)
-                .heightPosition(1)
-                .build())
+            .mapToObj(i -> new Player(names[i], Coordinate.of(i, 0)))
             .collect(Collectors.toList()));
 
         // when & then
         assertThat(players.names()).isEqualTo("홍길동   전창길   코난    ");
+    }
+
+    @DisplayName("인자로 Player번호와 Ladder를 전달 받아 해당 번호의 Player를 이동한다.")
+    @Test
+    void move() {
+        // given
+        String[] names = {"홍길동", "전창길", "코난", "브라이언"};
+
+        Coordinate.init(new Width(names.length), new Height(5));
+        Players players = new Players(IntStream.range(0, names.length)
+            .mapToObj(i -> new Player(names[i], Coordinate.of(i, 0)))
+            .collect(Collectors.toList()));
+
+        int playerNo = 1;
+        int x = 0;
+        int y = 1;
+
+        // when
+        players.move(playerNo, x, y);
+
+        // then
+        assertThat(players.findPlayerBy(Coordinate.of(0, 1)).name()).isEqualTo("전창길");
     }
 }
