9fb97d5dfb4bd3553d406c8ce7a3af6ddff7342b
solchan98 <dev.sol0427@gmail.com>
2023-11-25 16:07:46 +0900

    feat: 사다리 생성 조건, 최소 높이 검증 로직 구현

--- src/main/java/nextstep/ladder/Ladder.java
+++ src/main/java/nextstep/ladder/Ladder.java
@@ -9,11 +9,17 @@ public class Ladder {
 
     private static final int MIN_COUNT_OF_PERSON = 1;
 
+    private static final int MIN_HEIGHT_OF_HEIGHT = 0;
+
     private final List<Line> lines;
 
     public Ladder(int countOfPerson, int maxHeightOfLadder) {
         if (countOfPerson < MIN_COUNT_OF_PERSON) {
             throw new IllegalArgumentException("참여자는 최소 " + MIN_COUNT_OF_PERSON + "명 이상이어야 합니다.");
+        }
+
+        if (maxHeightOfLadder < MIN_HEIGHT_OF_HEIGHT) {
+            throw new IllegalArgumentException("높이는 최소 " + MIN_HEIGHT_OF_HEIGHT + "이상이어야 합니다.");
         }
 
         this.lines = IntStream.range(0, maxHeightOfLadder)
--- src/test/java/nextstep/ladder/LadderTest.java
+++ src/test/java/nextstep/ladder/LadderTest.java
@@ -1,8 +1,6 @@
 package nextstep.ladder;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
