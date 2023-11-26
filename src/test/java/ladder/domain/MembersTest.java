19b10c8bb35ff500bef02d4f9a745dd25e88d513
yoohojeong <yh132@naver.com>
2023-11-27 02:27:03 +0900

    test : MembersTest, NameTest 수정

--- src/test/java/ladder/domain/MembersTest.java
+++ src/test/java/ladder/domain/MembersTest.java
@@ -8,7 +8,7 @@ class MembersTest {
 
     @Test
     void 게임_참여자_수_확인() {
-        Members members = new Members("aaaa, bbb, cc");
+        Members members = new Members("aaaa", "bbb", "cc");
         assertThat(members.countOfMembers()).isEqualTo(3);
     }
 }
--- src/test/java/ladder/domain/NameTest.java
+++ src/test/java/ladder/domain/NameTest.java
@@ -9,6 +9,6 @@ class NameTest {
     @Test
     void 다섯글자_초과_이름_입력_시_에러() {
         assertThatThrownBy(() -> new Name("aaaaaa")).isInstanceOf(IllegalArgumentException.class)
-            .hasMessage("5자 이하만 입력가능합니다.");
+            .hasMessage("이름은 5자 이하만 입력가능합니다.");
     }
 }
