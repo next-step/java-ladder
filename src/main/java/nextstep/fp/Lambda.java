2a997f99d460e257fb6cdaee577eb76a675120e0
Lee JE <58355531+Jane096@users.noreply.github.com>
2023-11-26 16:26:57 +0900

    Step1: 람다, Optional (#1931)
    * fix: CarTest MoveStrategy 람다식으로 변경
    
    * feat: 람다실습 1, 람다실습2 완료
    
    * feat: Stream 실습
    
    * feat: Optional_1 요구사항
    
    * feat: Optional_2 요구사항
    
    * feat: Optional_3 요구사항
    
    * 람다로 리팩토링
    
    * `@FuctionalInterface` 어노테이션 선언
    
    * printLongestWordTop100 메서드 스트림 수정
    
    * ageIsInRange1 map() filter() 사용해서 수정
    
    ---------
    
    Co-authored-by: Jieun Lee <jes7077@gmail.com>

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -18,39 +18,24 @@ public class Lambda {
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> System.out.println("Hello from thread")).start();
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAllByCondition(numbers, number -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAllByCondition(numbers, number -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAllByCondition(numbers, number -> number > 3);
+    }
+
+    public static int sumAllByCondition(List<Integer> numbers, Conditional condition) {
+        return numbers.stream()
+                .filter(condition::test)
+                .reduce(0, Integer::sum);
     }
 }
