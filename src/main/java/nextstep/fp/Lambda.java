6ac77cface81e5f71047a40a5560a781f7c25d56
jane096 <jes7077@naver.com>
2023-11-26 13:45:10 +0900

    람다로 리팩토링

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -34,13 +34,8 @@ public class Lambda {
     }
 
     public static int sumAllByCondition(List<Integer> numbers, Conditional condition) {
-        int total = 0;
-        for (int number : numbers) {
-            if (condition.test(number)) {
-                total += number;
-            }
-        }
-
-        return total;
+        return numbers.stream()
+                .filter(condition::test)
+                .reduce(0, Integer::sum);
     }
 }
