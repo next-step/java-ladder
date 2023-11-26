fe5fc3609b73ebd5ca9f0326eddb0f8648141108
namuhuchutong <namuhuchutong@gmail.com>
2023-11-26 19:48:41 +0900

    refactor: Row, Rows 테스트 케이스 수정

--- src/test/java/me/namuhuchutong/ladder/RowTest.java
+++ src/test/java/me/namuhuchutong/ladder/RowTest.java
@@ -1,7 +1,8 @@
 package me.namuhuchutong.ladder;
 
-import me.namuhuchutong.ladder.domain.implement.Row;
+import me.namuhuchutong.ladder.domain.engine.LineCreator;
 import me.namuhuchutong.ladder.domain.engine.ScaffoldFactory;
+import me.namuhuchutong.ladder.domain.implement.RowCreator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,9 +14,10 @@ class RowTest {
     @DisplayName("|--|--| 모양으로 연속된 사다리는 허용하지 않는다.")
     @Test
     void scaffold_should_not_be_continuous() {
+        LineCreator lineCreator = new RowCreator();
         ScaffoldFactory testFactory = () -> HYPHEN;
 
-        assertThatThrownBy(() -> Row.from(5, testFactory))
+        assertThatThrownBy(() -> lineCreator.createLine(5, testFactory))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
