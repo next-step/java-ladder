1de453d75552a486c3abeda2c377c49c5892057e
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-27 19:19:56 +0900

    feat : Participant 참가자 이름을 검증하는 기능 추가

---
+++ src/test/java/nextstep/ladder/domain/ParticipantTest.java
@@ -0,0 +1,23 @@
+package nextstep.ladder.domain;
+
+import static nextstep.ladder.domain.Participant.PARTICIPANT_NAME_LENGTH_EXCEPTION;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ParticipantTest {
+
+    @ParameterizedTest
+    @DisplayName("이름이 1자 이상 5자 이하가 아니면 예외를 던진다.")
+    @ValueSource(strings = {"", " ", "succaa"})
+    void create_participant_exception(String given) {
+        // when // then
+        assertThatThrownBy(() -> new Participant(given))
+                .isExactlyInstanceOf(IllegalStateException.class)
+                .hasMessage(PARTICIPANT_NAME_LENGTH_EXCEPTION);
+    }
+
+
+}
