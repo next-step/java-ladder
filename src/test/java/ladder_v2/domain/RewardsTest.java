fdc5c0dca66f82161a58dc519fe8ce2e7c7a09d3
sukhyunmoon <sukhyunmoon@sk.com>
2023-11-27 17:01:03 +0900

    feat:보상은 2개 이상이어야 한다.

--- src/test/java/ladder_v2/domain/RewardsTest.java
+++ src/test/java/ladder_v2/domain/RewardsTest.java
@@ -1,2 +1,20 @@
-package ladder_v2.domain;public class RewardsTest {
+package ladder_v2.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class RewardsTest {
+
+    @Test
+    @DisplayName("보상은 2개 이상이어야 한다.")
+    void rewardsInput(){
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Rewards(List.of("10000"));
+        });
+    }
+
 }
