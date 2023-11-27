10545bff18502031adef5ed62b6e9af208538049
simbokyung <synk98@gmail.com>
2023-11-27 20:27:55 +0900

    Feat :UI 연결 및 readme 요구사항 작성

--- src/main/java/ladder/LadderController.java
+++ src/main/java/ladder/LadderController.java
@@ -1,13 +1,12 @@
 package ladder;
 
-import ladder.domain.Height;
-import ladder.domain.LadderLines;
-import ladder.domain.People;
+import ladder.domain.*;
 import ladder.domain.strategy.CoordinateGenerator;
 import ladder.domain.strategy.RandomCoordinateGenerator;
+import ladder.ui.ResultView;
 
-import static ladder.ui.InputView.inputHeight;
-import static ladder.ui.InputView.inputNames;
+import static ladder.ui.InputView.*;
+import static ladder.ui.ResultView.*;
 import static ladder.ui.ResultView.printLadder;
 import static ladder.ui.ResultView.printTitle;
 
@@ -17,12 +16,18 @@ public class LadderController {
 
     public static void main(String[] args) {
         People people = new People(inputNames());
+        Gifts gifts = new Gifts(inputGifts(),people);
         Height height = new Height(inputHeight());
 
         LadderLines ladder = LadderLines.of(people, height, COORDINATE_GENERATION);
 
         printTitle(people);
         printLadder(ladder);
+        printEndLadder(gifts);
+
+       GameResult gameResult = GameResult.valueOf(people,ladder,gifts);
+
+       printResult(gameResult);
 
     }
 }
--- src/main/java/ladder/ui/ResultView.java
+++ src/main/java/ladder/ui/ResultView.java
@@ -1,9 +1,6 @@
 package ladder.ui;
 
-import ladder.domain.Coordinate;
-import ladder.domain.LadderLines;
-import ladder.domain.LadderLine;
-import ladder.domain.People;
+import ladder.domain.*;
 
 public class ResultView {
     private ResultView() {
@@ -18,6 +15,40 @@ public class ResultView {
 
     public static void printLadder(LadderLines ladder) {
         ladder.draw().forEach(ResultView::drawLine);
+    }
+
+    public static void printEndLadder(Gifts gifts) {
+        gifts.values().forEach(item -> System.out.printf(String.format("%5s ", item)));
+        System.out.println();
+    }
+
+    public static void printResult(GameResult gameResult) {
+        while (true) {
+            String text = InputView.inputPerson();
+
+            if (checkEnd(text)) {
+                System.out.printf("%n종료되었습니다.%n");
+                break;
+            }
+
+            System.out.printf("%n실행 결과%n");
+            if (checkAll(text)) {
+                gameResult.values().forEach((name, gift) -> System.out.printf("%s : %s%n", name, gift));
+                continue;
+            }
+
+            System.out.printf("%s : %s%n", text, gameResult.checkResult(new Name(text)));
+
+        }
+
+    }
+
+    private static boolean checkAll(String text) {
+        return text.equals("all");
+    }
+
+    private static boolean checkEnd(String text) {
+        return text.equals(",");
     }
 
     private static void drawLine(LadderLine ladderLine) {
