d381c33a1aa387a60da700f0e2434c194a2aab29
jonghunbaek <qor6028@naver.com>
2023-11-28 00:52:21 +0900

    rename : 상수, 변수명 변경

--- src/main/java/nextstep/ladder/domain/Bridges.java
+++ src/main/java/nextstep/ladder/domain/Bridges.java
@@ -12,7 +12,8 @@ public class Bridges {
 
     public static final String TRUE_BRIDGE = "-----";
     public static final String FALSE_BRIDGE = "     ";
-    private static final int[] DIRECTIONS = {0 , -1};
+    private static final int LEFT = -1;
+    private static final int RIGHT = 0;
     public static final int DEPTH_ADJUSTMENT = 1;
 
     private final List<Boolean> bridges;
@@ -30,28 +31,28 @@ public class Bridges {
             .anyMatch(i -> bridges.get(i) && bridges.get(i + 1));
     }
 
-    public int move(int currentPosition) {
-        return Arrays.stream(DIRECTIONS)
-            .filter(direction -> isMovable(currentPosition + direction))
-            .map(direction -> movableSide(direction, currentPosition + direction))
+    public int move(int current) {
+        return Arrays.stream(new int[] {LEFT, RIGHT})
+            .filter(direction -> isMovable(current + direction))
+            .map(direction -> movableSide(direction, current + direction))
             .findAny()
-            .orElse(currentPosition);
+            .orElse(current);
     }
 
     private int movableSide(int direction, int next) {
-        if (direction == DIRECTIONS[0]) {
+        if (direction == RIGHT) {
             return next + DEPTH_ADJUSTMENT;
         }
 
         return next;
     }
 
-    public boolean isMovable(int nextCoordinate) {
-        if (isOutOfRange(nextCoordinate)) {
+    public boolean isMovable(int next) {
+        if (isOutOfRange(next)) {
             return false;
         }
 
-        return bridges.get(nextCoordinate);
+        return bridges.get(next);
     }
 
     private boolean isOutOfRange(int nextCoordinate) {
--- src/test/java/nextstep/ladder/domain/LadderTest.java
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -1,8 +1,5 @@
 package nextstep.ladder.domain;
 
-import nextstep.ladder.domain.wrapper.Height;
-import nextstep.ladder.domain.wrapper.Width;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
