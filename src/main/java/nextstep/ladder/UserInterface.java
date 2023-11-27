c3214bfe199ca68b76a6ad000c0e5039567b01a6
pilming44 <pilming44@naver.com>
2023-11-27 22:04:01 +0900

    feat: view 영역 추가

---
+++ src/main/java/nextstep/ladder/UserInterface.java
@@ -0,0 +1,10 @@
+package nextstep.ladder;
+
+import nextstep.ladder.view.InputView;
+
+public class UserInterface {
+    public static void main(String[] args) {
+        String participantNames = InputView.participantNames();
+        String ladderLevel = InputView.ladderLevel();
+    }
+}
