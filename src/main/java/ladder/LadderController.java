18faab95c89c15adec71e307448104456d9ac127
simbokyung <synk98@gmail.com>
2023-11-28 17:21:49 +0900

    Refactor : UI 피드백 반영

--- src/main/java/ladder/LadderController.java
+++ src/main/java/ladder/LadderController.java
@@ -3,12 +3,10 @@ package ladder;
 import ladder.domain.*;
 import ladder.domain.strategy.CoordinateGenerator;
 import ladder.domain.strategy.RandomCoordinateGenerator;
-import ladder.ui.ResultView;
 
 import static ladder.ui.InputView.*;
-import static ladder.ui.ResultView.*;
+import static ladder.ui.ResultView.printGame;
 import static ladder.ui.ResultView.printLadder;
-import static ladder.ui.ResultView.printTitle;
 
 public class LadderController {
 
@@ -16,18 +14,16 @@ public class LadderController {
 
     public static void main(String[] args) {
         People people = new People(inputNames());
-        Gifts gifts = new Gifts(inputGifts(),people);
+        Gifts gifts = new Gifts(inputGifts(), people);
         Height height = new Height(inputHeight());
 
         LadderLines ladder = LadderLines.of(people, height, COORDINATE_GENERATION);
 
-        printTitle(people);
-        printLadder(ladder);
-        printEndLadder(gifts);
+        printLadder(people, ladder, gifts);
 
-       GameResult gameResult = GameResult.valueOf(people,ladder,gifts);
+        GameResult gameResult = GameResult.valueOf(people, ladder, gifts);
 
-       printResult(gameResult, people);
+        printGame(gameResult, people);
 
     }
 }
--- src/main/java/ladder/ui/ResultView.java
+++ src/main/java/ladder/ui/ResultView.java
@@ -6,70 +6,34 @@ public class ResultView {
     private ResultView() {
     }
 
-    public static void printTitle(People people) {
+    public static void printLadder(People people, LadderLines ladder, Gifts gifts) {
+        printTitle(people);
+        ladder.draw().forEach(ResultView::drawLine);
+        printEndLadder(gifts);
+
+    }
+
+    public static void printGame(GameResult gameResult, People people) {
+        while (true) {
+            String text = InputView.inputPerson();
+
+            if (isEnd(text)) {
+                break;
+            }
+
+            System.out.printf("%n실행 결과%n");
+            printGameResult(gameResult, text);
+
+        }
+
+    }
+
+    private static void printTitle(People people) {
         System.out.printf("실행결과%n%n");
 
         people.values().forEach(item -> System.out.printf(String.format("%5s ", item)));
         System.out.println();
     }
-
-    public static void printLadder(LadderLines ladder) {
-        ladder.draw().forEach(ResultView::drawLine);
-    }
-
-    public static void printEndLadder(Gifts gifts) {
-        gifts.values().forEach(item -> System.out.printf(String.format("%5s ", item)));
-        System.out.println();
-    }
-
-    public static void printResult(GameResult gameResult, People people) {
-        while (true) {
-            String text = InputView.inputPerson();
-
-            if (checkEnd(text)) break;
-
-            if (checkContains(people, text)) continue;
-
-            System.out.printf("%n실행 결과%n");
-            if (checkAll(text)) {
-                printAll(gameResult);
-                continue;
-            }
-
-            printSolo(gameResult, text);
-
-        }
-
-    }
-
-    private static void printSolo(GameResult gameResult, String text) {
-        System.out.printf("%s : %s%n", text, gameResult.checkResult(new Name(text)));
-    }
-
-    private static void printAll(GameResult gameResult) {
-        gameResult.values().forEach((name, gift) -> System.out.printf("%s : %s%n", name, gift));
-    }
-
-    private static boolean checkContains(People people, String text) {
-        if (!checkAll(text) &&!people.contains(new Name(text))) {
-            System.out.println("잘못입력하셨습니다.");
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean checkEnd(String text) {
-        if (text.equals(",")) {
-            System.out.printf("%n종료되었습니다.%n");
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean checkAll(String text) {
-        return text.equals("all");
-    }
-
 
     private static void drawLine(LadderLine ladderLine) {
         ladderLine.draw().forEach(item -> System.out.print(drawCoordinate(item)));
@@ -77,10 +41,55 @@ public class ResultView {
     }
 
     private static String drawCoordinate(Coordinate coordinate) {
-        if (coordinate.exist()) {
+        if (coordinate.isLeft()) {
             return "-----|";
         }
         return "     |";
     }
+
+    private static void printEndLadder(Gifts gifts) {
+        gifts.values().forEach(item -> System.out.printf(String.format("%5s ", item)));
+        System.out.println();
+    }
+
+    private static boolean isEnd(String text) {
+        if (text.equals(",")) {
+            System.out.printf("%n종료되었습니다.%n");
+            return true;
+        }
+        return false;
+    }
+
+    private static void printGameResult(GameResult gameResult, String text) {
+        if (isAll(text)) {
+            printAll(gameResult);
+            return;
+        }
+        printSolo(gameResult, text);
+    }
+
+    private static boolean isAll(String text) {
+        return text.equals("all");
+    }
+
+    private static void printAll(GameResult gameResult) {
+        gameResult.values().forEach((name, gift) -> System.out.printf("%s : %s%n", name, gift));
+    }
+
+    private static void printSolo(GameResult gameResult, String text) {
+        Gift gift = findGift(gameResult, text);
+
+        if (gift == null) {
+            System.out.println("잘못입력하셨습니다.");
+            return;
+        }
+
+        System.out.printf("%s : %s%n", text, gift);
+    }
+
+    private static Gift findGift(GameResult gameResult, String text) {
+        return gameResult.checkResult(new Name(text));
+    }
+
 
 }
