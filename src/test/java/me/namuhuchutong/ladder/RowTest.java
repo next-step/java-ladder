7b65cd08f45f63967e3d98cfc90b7fdf145df6e2
namuhuchutong <namuhuchutong@gmail.com>
2023-11-28 17:36:58 +0900

    refactor: 기존 테스트 케이스 수정

--- src/test/java/me/namuhuchutong/ladder/RowTest.java
+++ src/test/java/me/namuhuchutong/ladder/RowTest.java
@@ -1,10 +1,14 @@
 package me.namuhuchutong.ladder;
 
 import me.namuhuchutong.ladder.domain.engine.LineCreator;
-import me.namuhuchutong.ladder.domain.implement.RowCreator;
+import me.namuhuchutong.ladder.domain.implement.Row;
+import me.namuhuchutong.ladder.domain.implement.wrapper.LadderExpression;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static me.namuhuchutong.ladder.domain.implement.wrapper.LadderExpression.*;
 import static org.assertj.core.api.Assertions.*;
 
 class RowTest {
@@ -12,7 +16,8 @@ class RowTest {
     @DisplayName("|--|--| 모양으로 연속된 사다리는 허용하지 않는다.")
     @Test
     void scaffold_should_not_be_continuous() {
-        LineCreator lineCreator = new RowCreator(() -> true);
+        List<LadderExpression> givenLadder = List.of(HYPHEN, VERTICAL_BAR, HYPHEN);
+        LineCreator lineCreator = participants -> new Row(givenLadder);
 
         assertThatThrownBy(() -> lineCreator.createLine(5))
                 .isInstanceOf(IllegalArgumentException.class);
