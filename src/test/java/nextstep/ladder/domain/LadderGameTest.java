2f0773a1b694a912d40af5ce3a1dfd058237bc9b
jonghunbaek <qor6028@naver.com>
2023-11-29 21:33:24 +0900

    refactor : bridges -> points 변경으로 테스트 코드 수정

--- src/test/java/nextstep/ladder/domain/LadderGameTest.java
+++ src/test/java/nextstep/ladder/domain/LadderGameTest.java
@@ -1,11 +1,15 @@
 package nextstep.ladder.domain;
 
 import nextstep.ladder.controller.dto.GameResult;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static nextstep.ladder.domain.Direction.*;
+import static nextstep.ladder.domain.Direction.CENTER;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LadderGameTest {
@@ -16,11 +20,30 @@ class LadderGameTest {
         // given
         Players players = new Players(List.of("홍길동", "전창길", "코난", "브라이언"));
         Ladder ladder = new Ladder(List.of(
-            new Bridges(List.of(true, false, true)),
-            new Bridges(List.of(true, false, true)),
-            new Bridges(List.of(true, false, false)),
-            new Bridges(List.of(false, false, false)),
-            new Bridges(List.of(false, true, false))
+            new Points(List.of(
+                new Point(0, RIGHT),
+                new Point(1, LEFT),
+                new Point(2, RIGHT),
+                new Point(3, LEFT)
+            )),
+            new Points(List.of(
+                new Point(0, CENTER),
+                new Point(1, RIGHT),
+                new Point(2, LEFT),
+                new Point(3, CENTER)
+            )),
+            new Points(List.of(
+                new Point(0, RIGHT),
+                new Point(1, LEFT),
+                new Point(2, RIGHT),
+                new Point(3, LEFT)
+            )),
+            new Points(List.of(
+                new Point(0, CENTER),
+                new Point(1, RIGHT),
+                new Point(2, LEFT),
+                new Point(3, CENTER)
+            ))
         ));
         WinningPrize winningPrize = new WinningPrize(List.of("1000", "5000", "2000", "3000"));
         LadderGame ladderGame = new LadderGame(players, ladder, winningPrize);
@@ -30,10 +53,10 @@ class LadderGameTest {
 
         // then
         assertAll(
-            () -> assertEquals("2000", result.prize("홍길동")),
-            () -> assertEquals("1000", result.prize("전창길")),
+            () -> assertEquals("3000", result.prize("홍길동")),
+            () -> assertEquals("2000", result.prize("전창길")),
             () -> assertEquals("5000", result.prize("코난")),
-            () -> assertEquals("3000", result.prize("브라이언"))
+            () -> assertEquals("1000", result.prize("브라이언"))
         );
     }
 }
