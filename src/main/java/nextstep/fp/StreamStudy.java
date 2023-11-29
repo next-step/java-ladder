793c1dd4590c250ccbdb378816448721bbb99e52
Hyesooo <hyesookim95@gmail.com>
2023-11-29 22:48:37 +0900

    feat: 스트림 연습

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -15,11 +15,7 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream().filter(w -> w.length() > 12).count();
     }
 
     public static void printLongestWordTop100() throws IOException {
@@ -27,7 +23,14 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        System.out.println(
+                words.stream().filter(w -> w.length() > 12)
+                        .sorted((x, y) -> x.length() - y.length())
+                        .distinct()
+                        .limit(100)
+                        .map(String::toLowerCase)
+                        .collect(Collectors.joining(", "))
+        );
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +42,8 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream().filter(number -> number > 3)
+                .map(number -> number * 2)
+                .reduce(0, Integer::sum);
     }
 }
