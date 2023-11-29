9d6dc2cc111e000c60e7a8d68d11bc91131da988
jonghunbaek <qor6028@naver.com>
2023-11-29 21:45:08 +0900

    refactor : toString 리팩토링

--- src/main/java/nextstep/ladder/domain/Point.java
+++ src/main/java/nextstep/ladder/domain/Point.java
@@ -1,9 +1,15 @@
 package nextstep.ladder.domain;
 
+import static nextstep.ladder.domain.Direction.CENTER;
+import static nextstep.ladder.domain.Direction.LEFT;
+
 public class Point {
 
-    private int point;
-    private Direction direction;
+    public static final String TRUE_BRIDGE = "-----";
+    public static final String FALSE_BRIDGE = "     ";
+
+    private final int point;
+    private final Direction direction;
 
     public Point(int point, Direction direction) {
         this.point = point;
@@ -14,7 +20,12 @@ public class Point {
         return direction.nextPoint(point);
     }
 
-    public Direction direction() {
-        return this.direction;
+    @Override
+    public String toString() {
+        if (LEFT.equals(direction) || CENTER.equals(direction)) {
+            return FALSE_BRIDGE;
+        }
+
+        return TRUE_BRIDGE;
     }
 }
--- src/main/java/nextstep/ladder/domain/Points.java
+++ src/main/java/nextstep/ladder/domain/Points.java
@@ -4,14 +4,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static nextstep.ladder.domain.Direction.*;
 import static nextstep.ladder.domain.Ladder.LADDER_POLE;
 
 public class Points {
-    public static final String TRUE_BRIDGE = "-----";
-    public static final String FALSE_BRIDGE = "     ";
 
-    private List<Point> points;
+    private final List<Point> points;
 
     public Points(List<Point> points) {
         this.points = points;
@@ -25,15 +22,7 @@ public class Points {
     public String toString() {
         return IntStream.range(0, points.size() - 1)
             .boxed()
-            .map(i -> drawLadder(points.get(i).direction()))
+            .map(i -> points.get(i).toString())
             .collect(Collectors.joining(LADDER_POLE));
-    }
-
-    private String drawLadder(Direction direction) {
-        if (LEFT.equals(direction) || CENTER.equals(direction)) {
-            return FALSE_BRIDGE;
-        }
-
-        return TRUE_BRIDGE;
     }
 }
