a36f89b5a5f7a59b18c846c341a5033bc5d4d5f6
chunghyunleeme <chunghyun.dev@gmail.com>
2023-11-30 00:16:51 +0900

    refactor: printLongestWordTop100

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,7 +24,12 @@ public class StreamStudy {
             .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+            .sorted((w1, w2) ->
+                Integer.compare(w2.length(), w1.length())
+            ).collect(Collectors.toList())
+            .subList(0, 100)
+            .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
