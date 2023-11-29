05cd1cabd288f3791704f55461644a7e64fe9c80
jeounpar <cdex6531@ajou.ac.kr>
2023-11-29 12:19:31 +0900

    feat : 사다리 타기가 정상적으로 동작하기 위해 라인이 겹치지 않도록 한다

--- src/main/java/nextstep/ladder/model/Line.java
+++ src/main/java/nextstep/ladder/model/Line.java
@@ -2,12 +2,41 @@ package nextstep.ladder.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class Line {
 
     private final List<Boolean> points;
 
-    public Line(int countOfPerson) {
+    public Line(int countOfPerson, Random random) {
         this.points = new ArrayList<>();
+        this.points.add(random.nextBoolean());
+        IntStream.range(0, countOfPerson - 2)
+            .forEach(index -> initPoint(index, random.nextBoolean()));
+        validateLine(countOfPerson);
+    }
+
+    private void validateLine(int countOfPerson) {
+        IntStream.range(0, countOfPerson - 2)
+            .filter(index -> points.get(index) && points.get(index + 1))
+            .forEach(point -> {
+                throw new IllegalArgumentException("사다리 라인이 겹칩니다");
+            });
+    }
+
+    private void initPoint(int index, boolean nextBoolean) {
+        if (this.points.get(index) && nextBoolean || this.points.get(index) && !nextBoolean
+            || !this.points.get(index) && !nextBoolean) {
+            this.points.add(false);
+            return;
+        }
+        if (!this.points.get(index) && nextBoolean) {
+            this.points.add(true);
+        }
+    }
+
+    public List<Boolean> getPoints() {
+        return this.points;
     }
 }
--- src/main/java/nextstep/ladder/view/OutputView.java
+++ src/main/java/nextstep/ladder/view/OutputView.java
@@ -2,4 +2,7 @@ package nextstep.ladder.view;
 
 public class OutputView {
 
+    public OutputView() {
+        System.out.println("실행결과\n");
+    }
 }
