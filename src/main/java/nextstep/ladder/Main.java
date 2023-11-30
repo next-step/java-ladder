619a674c92d93071480d98264ed5a259b3236cca
choomi1217 <whdudal1217@naver.com>
2023-11-30 16:11:35 +0900

    feat: 실행

---
+++ src/main/java/nextstep/ladder/Main.java
@@ -0,0 +1,27 @@
+package nextstep.ladder;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String string = "pobi,honux,crong,jk";
+        Players players = Players.from(Arrays.stream(string.split(",")).collect(Collectors.toList()));
+
+        Ladder ladder = Ladder.of(5, players.list().size());
+        ladder.lines().forEach(line -> {
+            line.line().forEach(bool->{
+                if(bool){
+                    System.out.print("-----");
+                }else {
+                    System.out.print("     ");
+                }
+                System.out.print("|");
+            });
+            System.out.print("\n");
+        });
+    }
+}
