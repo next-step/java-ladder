b58572e95433b070c4831c1123ec49330d51f60a
choomi1217 <whdudal1217@naver.com>
2023-11-30 23:50:14 +0900

    refactor: `main` 에서 `LadderController` 실행하도록 변경

--- src/main/java/nextstep/ladder/Main.java
+++ src/main/java/nextstep/ladder/Main.java
@@ -1,17 +1,10 @@
 package nextstep.ladder;
 
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import nextstep.ladder.controller.LadderController;
 
 public class Main {
 
     public static void main(String[] args) {
-        String string = "pobi,honux,crong,jk";
-        Players players = Players.from(Arrays.stream(string.split(",")).collect(Collectors.toList()));
-
-        Ladder ladder = Ladder.of(5, players.list().size());
-        ladder.lines().forEach(Line::draw);
+        LadderController.of(System.in, System.out).start();
     }
 }
