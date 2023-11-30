2cd4aece692b0ec63b6b157c4353a62af8bed998
choomi1217 <whdudal1217@naver.com>
2023-11-30 16:11:08 +0900

    refactor: 랜덤 값 생성

--- src/main/java/nextstep/ladder/Line.java
+++ src/main/java/nextstep/ladder/Line.java
@@ -6,6 +6,7 @@ import java.util.stream.IntStream;
 
 public class Line {
 
+    private static final RandomLineGenerator randomLineGenerator = RandomLineGenerator.getInstance();
     private final List<Boolean> line;
 
     private Line(List<Boolean> line) {
@@ -15,10 +16,15 @@ public class Line {
     public static Line from(int count) {
         return new Line(
             IntStream.range(0, count)
-                .mapToObj(i -> Boolean.FALSE)
+                .mapToObj(i -> randomLineGenerator.randomLine())
                 .collect(Collectors.toList())
         );
     }
+
+    public List<Boolean> line() {
+        return line;
+    }
+
 
     public int lineSize() {
         return line.size();
