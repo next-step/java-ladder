d4a45b50512853eca0fb1367d2d810d8f3e9f1ec
choomi1217 <whdudal1217@naver.com>
2023-11-30 23:49:37 +0900

    feat: `InputView` 생성

---
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -0,0 +1,25 @@
+package nextstep.ladder.view;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class InputView {
+
+    private final Scanner scanner;
+
+    public InputView(InputStream in) {
+        this.scanner = new Scanner(in);
+    }
+
+    public static InputView from(InputStream in) {
+        return new InputView(in);
+    }
+
+    public String initPlayers() {
+        return scanner.nextLine();
+    }
+
+    public int initLadderHeight() {
+        return scanner.nextInt();
+    }
+}
