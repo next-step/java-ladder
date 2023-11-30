e3b6781f483c857d31c3eed93fdd2bcf8513bfa3
hoho282 <jiny0229@naver.com>
2023-11-30 13:01:11 +0900

    feat : 1단계 연습 완료

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -3,6 +3,7 @@ package nextstep.fp;
 import java.util.List;
 
 public class Lambda {
+
     public static void printAllOld(List<Integer> numbers) {
         System.out.println("printAllOld");
 
@@ -18,39 +19,34 @@ public class Lambda {
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
+        new Thread(() -> System.out.println("Hello from thread")).start();
+    }
+
+    // 일반화된 sumAll 메서드
+    public static int conditionalSum(List<Integer> numbers, Conditional condition) {
+        int total = 0;
+        for (int number : numbers) {
+            if (condition.test(number)) {
+                total += number;
             }
-        }).start();
+        }
+        return total;
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return conditionalSum(numbers, x -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return conditionalSum(numbers, x -> x % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return conditionalSum(numbers, x -> x > 3);
+    }
+
+    public interface Conditional {
+
+        boolean test(Integer number);
     }
 }
