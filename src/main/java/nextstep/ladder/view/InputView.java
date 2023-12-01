d2664d326e45363a47dbe6de0e6d6fdcbbc2fb7b
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-12-01 15:01:30 +0900

    refactor : InputView 값의 상수화

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 public class InputView {
 
     private static final String INPUT_PARTICIPANTS_NAME_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    public static final String INPUT_MAX_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
 
     private static final String INPUT_PATTERN_EXCEPTION = "참가자 이름들의_구분자를_콤마로_입력해주세요";
     private static final String INPUT_LADDER_HEIGHT_EXCEPTION = "사다리 높이는 숫자로 입력해주세요.";
@@ -29,7 +30,7 @@ public class InputView {
     }
 
     public int height() {
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        System.out.println(INPUT_MAX_HEIGHT_MESSAGE);
         try {
             return Integer.parseInt(scanner.nextLine());
         } catch (NumberFormatException e) {
