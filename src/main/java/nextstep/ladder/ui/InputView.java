90ab5e40f29d241556ef7c457112cc414c85a490
pawoo0211 <pawoo0211@naver.com>
2023-12-03 16:32:58 +0900

    [step3] refactor: inputView 불필요한 코드 제거

--- src/main/java/nextstep/ladder/ui/InputView.java
+++ src/main/java/nextstep/ladder/ui/InputView.java
@@ -9,27 +9,19 @@ public class InputView {
     public String[] inputParticipant() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         String participants = scanner.nextLine();
-        return splitBy(participants);
+        return participants.split(",");
     }
 
     public String[] inputResults() {
         System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
         String results = scanner.nextLine();
-        return splitBy(results);
-    }
-
-    private String[] splitBy(String participants) {
-        return participants.split(",");
+        return results.split(",");
     }
 
     public int inputHighCount() {
-        printEmpty();
+        System.out.println("");
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         return scanner.nextInt();
-    }
-
-    private void printEmpty() {
-        System.out.println("");
     }
 
     public String inputWantShow() {
