70a7d0f092286b1b6b79e993193bcaa68229b43d
pawoo0211 <pawoo0211@naver.com>
2023-12-03 16:27:07 +0900

    [step3] feature: 참가자 도메인 구현

--- src/main/java/nextstep/ladder/domain/line/Line.java
+++ src/main/java/nextstep/ladder/domain/line/Line.java
@@ -1,15 +1,71 @@
 package nextstep.ladder.domain.line;
 
+import nextstep.ladder.domain.participant.Participant;
+import nextstep.ladder.domain.participant.Participants;
+
 public class Line {
+    private final int rowLineNumber;
     private RowLinePositions rowLinePosition;
 
+    // Refactoring method
     public Line(String[] participants) {
         int participantNumber = participants.length;
-        int rowLineNumber = participantNumber - 1;
+        rowLineNumber = participantNumber - 1;
         rowLinePosition = RowLinePositions.create(rowLineNumber);
+    }
+
+    // New method
+    // rowLineNumber == participants.length - 1
+    public Line(int rowLineNumber) {
+        this.rowLineNumber = rowLineNumber;
+        rowLinePosition = RowLinePositions.create(rowLineNumber);
+    }
+
+    public Line(int rowLineNumber, RowLinePositions rowLinePositions) {
+        this.rowLineNumber = rowLineNumber;
+        this.rowLinePosition = rowLinePositions;
     }
 
     public boolean isTruePosition(int index) {
         return rowLinePosition.isTrue(index);
+    }
+
+    public void movableParticipants(Participants participants) {
+        participants.getParticipantList()
+                .stream()
+                .forEachOrdered(participant -> movableParticipant(participant));
+    }
+
+    public void movableParticipant(Participant participant) {
+        if (participant.getCurrentIndex() == rowLineNumber) {
+            movableLeft(participant);
+            return;
+        }
+        if (movableLeft(participant)) {
+            return;
+        }
+        movableRight(participant);
+    }
+
+    private boolean movableLeft(Participant participant) {
+        int index = participant.getCurrentIndex();
+        if (index != 0 && isTruePosition(index -1)) {
+            participant.moveLeft();
+            return true;
+        }
+        return false;
+    }
+
+    private boolean movableRight(Participant participant) {
+        int index = participant.getCurrentIndex();
+        /**
+         * Fix
+         * - rowLineNumber가 3일 때, index != rowLineNumber로 설정할 경우 index + 1에서 예외 발생
+         */
+        if (index != rowLineNumber && isTruePosition(index)) {
+            participant.moveRight();
+            return true;
+        }
+        return false;
     }
 }
