7e9755fa6dd526885babe50e82fd8bfb882ce22b
geunnseung <106180361+geunnseung@users.noreply.github.com>
2023-12-06 22:37:45 +0900

    Step1 (#2001)
    * :safety_vest: CarTest
    - 익명 클래스를 람다로 전환
    
    * :safety_vest: CarTest
    - 람다를 활용해 중복 제거
    
    * :sparkles: stream mehtod 실습
    
    * :sparkles: optional 실습

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
@@ -26,8 +27,13 @@ public class StreamStudy {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
-
-        // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(w -> w.length() > 12)
+                .sorted()
+                .map(String::toLowerCase)
+                .distinct()
+                .limit(100)
+                .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +45,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(n -> n > 3)
+                .map(n -> n * 2)
+                .reduce(0, (x, y) -> x + y);
     }
 }
