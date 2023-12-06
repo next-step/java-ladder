2fb39da826ec6ebed7bfcfe01f7c820440080400
geunnseung <bsg1017@naver.com>
2023-12-06 16:46:23 +0900

    :safety_vest: CarTest - 람다를 활용해 중복 제거

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.concurrent.locks.Condition;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
@@ -27,27 +28,24 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+
+        return sum(numbers, num -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+
+        return sum(numbers, num -> num % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
+
+        return sum(numbers, num -> num > 3);
+    }
+
+    public static int sum(List<Integer> numbers, Conditional c) {
         int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
+        for (Integer number : numbers) {
+            if (c.test(number)) {
                 total += number;
             }
         }
