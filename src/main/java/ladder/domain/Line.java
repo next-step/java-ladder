ea02fa58aeda5ae14f10fb74ad736a03bf17465d
hhj <abc@def.com>
2023-12-07 12:15:39 +0900

    refactor : random 기능 외부에서 주입 받게 개선

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -2,25 +2,13 @@ package ladder.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.util.Objects;
 
 public class Line {
 
     private List<Boolean> points = new ArrayList<>();
 
-    private static final Random random = new Random();
-
     private static final String CONSECUTIVE_TRUES_CANNOT_BE_ENTERED = "Line을 생성할 때 true가 연속으로 들어올 수 없습니다.";
-
-    public Line(int countOfPerson) {
-        // 라인의 좌표 값에 선이 있는지 유무를 판단하는 로직 추가
-        List<Boolean> line = makeRandomLine(countOfPerson);
-        validation(line);
-        this.points = line;
-    }
 
     public Line(List<Boolean> list) {
         validation(list);
@@ -33,39 +21,37 @@ public class Line {
         }
     }
 
-    private static void continuousLineValidation(List<Boolean> list, int i) {
+    private void continuousLineValidation(List<Boolean> list, int i) {
         if(list.get(i) == true){
             List<Boolean> consecutiveElements = new ArrayList<>();
             consecutiveElements.add(list.get(i - 1));
             consecutiveElements.add(list.get(i));
 
-            boolean areConsecutiveEqual = consecutiveElements.stream()
-                    .distinct()
-                    .count() == 1;
-
-            inCaseOfConsecutive(areConsecutiveEqual);
+            inCaseOfConsecutive(consecutiveElements);
         }
     }
 
-    private static void inCaseOfConsecutive(boolean areConsecutiveEqual) {
-        if (areConsecutiveEqual) {
-            throw new RuntimeException(CONSECUTIVE_TRUES_CANNOT_BE_ENTERED);
+    private static void inCaseOfConsecutive(List<Boolean> areConsecutiveEqual) {
+        if (areConsecutiveEqual.get(0) == areConsecutiveEqual.get(1)) {
+            throw new IllegalArgumentException(CONSECUTIVE_TRUES_CANNOT_BE_ENTERED);
         }
     }
 
-    private List<Boolean> makeRandomLine(int countOfPerson){
-        List<Boolean> booleanList = Stream.iterate(random.nextBoolean(), bool -> {
-            if (bool) {
-                return false;
-            } else {
-                return random.nextBoolean();
-            }
-        }).limit(countOfPerson-1).collect(Collectors.toList());
-        return booleanList;
+    public List<Boolean> stream() {
+        return this.points;
     }
 
-    public Stream<Boolean> stream() {
-        return this.points.stream();
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Line that = (Line) o;
+        return Objects.equals(points, that.points);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(points);
     }
 
 }
