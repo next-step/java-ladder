50a33e0fdc5dee30a3a6398ee8017d8725912fe9
sunggyupaik <melon9751@naver.com>
2023-12-07 21:06:31 +0900

    [test] 사다리 한 줄 생성 유효성을 추가하라
    Brace는 리스트로 만드는 경우 연결되지 안않을 수 있습니다.
    Point의 오른쪽은 다음 Point의 왼쪽과 같은 반환 값을 가져야 합니다.
    모든 선이 정상적으로 연결되어 있는지 유효성을 검사합니다

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -2,6 +2,7 @@ package nextstep.ladder.domain;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Line implements Iterable<Brace> {
     private final List<Brace> braces;
@@ -13,6 +14,7 @@ public class Line implements Iterable<Brace> {
 
     private void validate(List<Brace> braces) {
         checkBracesSizeIsValid(braces);
+        checkBracesNotConnected(braces);
     }
 
     private static void checkBracesSizeIsValid(List<Brace> braces) {
@@ -21,12 +23,25 @@ public class Line implements Iterable<Brace> {
         }
     }
 
+    private void checkBracesNotConnected(List<Brace> braces) {
+        IntStream.range(0, braces.size())
+                .filter(index -> notConnected(braces, index))
+                .findFirst()
+                .ifPresent(index -> {
+                    throw new IllegalArgumentException("사다리가 정상적으로 이어져 있지 않습니다.");
+                });
+    }
+
+    private static boolean notConnected(List<Brace> braces, int index) {
+        return braces.get(index).isRight() && !braces.get(index + 1).isLeft();
+    }
+
     public int move(int index) {
-        if(isLeft(index)) {
+        if (isLeft(index)) {
             return index - 1;
         }
 
-        if(isRight(index)) {
+        if (isRight(index)) {
             return index  + 1;
         }
 
--- src/test/java/nextstep/ladder/domain/LineTest.java
+++ src/test/java/nextstep/ladder/domain/LineTest.java
@@ -10,12 +10,30 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LineTest {
+    private static final Point POINT_LEFT = new Point(true, false);
+    private static final Point POINT_RIGHT = new Point(false, true);
+    private static final Point POINT_NONE = new Point(false, false);
+
+    private static final Brace BRACE_LEFT = new Brace(POINT_LEFT);
+    private static final Brace BRACE_RIGHT = new Brace(POINT_RIGHT);
+    private static final Brace BRACE_NONE = new Brace(POINT_NONE);
+
     @ParameterizedTest
     @NullAndEmptySource
-    @DisplayName("Line은 은 아무런 Point가 주어지지 않으면 예외를 던진다.")
+    @DisplayName("Line은 아무런 Point가 주어지지 않으면 예외를 던진다.")
     void newObject_NullAndEmpty_throwsException(List<Brace> nullAndEmpty) {
         assertThatThrownBy(
                 () -> new Line(nullAndEmpty)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("Line은 가로 선이 연결되어 있지 않으면 예외를 던진다.")
+    void newObject_notConnected_throwsException() {
+        List<Brace> braces = List.of(BRACE_RIGHT, BRACE_RIGHT);
+
+        assertThatThrownBy(
+                () -> new Line(braces)
         ).isInstanceOf(IllegalArgumentException.class);
     }
 }
