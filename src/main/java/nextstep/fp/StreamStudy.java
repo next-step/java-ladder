64dc9ea34045b49d243cf67d5b139949a3f106c1
h3yon <46602874+h3yon@users.noreply.github.com>
2023-12-08 15:55:38 +0900

    feature: 🚀 1단계 - 스트림, 람다, Optional (#2007)

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -24,10 +24,16 @@ public class StreamStudy {
 
     public static void printLongestWordTop100() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+                                                                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+             .filter(word -> word.length() > 12)
+             .map(String::toLowerCase)
+             .distinct()
+             .sorted()
+             .limit(100)
+             .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -35,10 +41,13 @@ public class StreamStudy {
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers.stream().reduce(0, Integer::sum);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return sumAll(numbers.stream()
+                             .filter(number -> number > 3)
+                             .map(number -> number * 2)
+                             .collect(Collectors.toList()));
     }
 }
