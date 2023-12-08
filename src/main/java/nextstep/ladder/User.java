9d0bcab9a7116d2987afc069d50867cde3f5aa67
이창섭 <ventulus95@gmail.com>
2023-12-09 00:39:41 +0900

    refact: 상수처리

--- src/main/java/nextstep/ladder/User.java
+++ src/main/java/nextstep/ladder/User.java
@@ -5,7 +5,6 @@ public class User {
     public static final int MAX_NAME_LENGTH = 5;
     private String name;
 
-
     public User(String name) {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("이름은 5자를 초과할 수 없습니다.");
