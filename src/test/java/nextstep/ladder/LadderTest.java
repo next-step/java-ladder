b6f9fd55947d919cae71165a6520ddf3143b64d3
solchan98 <dev.sol0427@gmail.com>
2023-12-08 21:43:21 +0900

    feat: 사다리 게임 결과 확인 실패 작성

--- src/test/java/nextstep/ladder/LadderTest.java
+++ src/test/java/nextstep/ladder/LadderTest.java
@@ -1,8 +1,15 @@
 package nextstep.ladder;
 
 import nextstep.ladder.domain.Ladder;
+import nextstep.ladder.domain.Line;
+import nextstep.ladder.domain.Point;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LadderTest {
@@ -16,5 +23,18 @@ class LadderTest {
         assertThatThrownBy(() -> Ladder.of(VALID_MIM_COUNT_OF_PERSON, INVALID_HEIGHT_OF_LADDER))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("최소 높이는 최소 1이상이어야 합니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"pobi:꽝", "honux:5000"}, delimiter = ':')
+    void 사다리는_이름을_받아_결과를_반환할_수_있다(String playerName, String result) {
+        Ladder ladder = Ladder.from(
+                List.of(
+                        Line.from(List.of(new Point(false), new Point(false))),
+                        Line.from(List.of(new Point(false), new Point(false)))
+                )
+        );
+
+        assertThat(ladder.play(playerName)).isEqualsTo(result);
     }
 }
