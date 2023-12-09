dc1aabf0256edc018d98d4eee339b17fd3fe4093
choomi1217 <whdudal1217@naver.com>
2023-12-09 17:57:59 +0900

    refactor: `Step`에 현재, 이전 스텝을 꺼내가는 로직 추가 및 `last` 메소드 추가

--- src/main/java/nextstep/ladder/model/Step.java
+++ src/main/java/nextstep/ladder/model/Step.java
@@ -12,7 +12,7 @@ public class Step {
         this.previousStep = previousStep;
     }
 
-    public static Step of(Boolean currentStep, Boolean previousStep){
+    public static Step of(Boolean currentStep, Boolean previousStep) {
         return new Step(currentStep, previousStep);
     }
 
@@ -20,19 +20,20 @@ public class Step {
         return of(currentStep, false);
     }
 
-    public static Step emptyStep() {
-        return new Step(false, false);
-    }
-
-    public Step overlapStepReplace() {
-        if (currentStep.equals(previousStep) && previousStep.equals(true)) {
-            return new Step(false, false);
-        }
-        return new Step(currentStep, previousStep);
+    public static Step last(Boolean previousStep) {
+        return of(false, previousStep);
     }
 
     public boolean canMove() {
         return this.currentStep;
+    }
+
+    public Boolean currentStep() {
+        return currentStep;
+    }
+
+    public Boolean previousStep() {
+        return previousStep;
     }
 
     @Override
@@ -52,7 +53,4 @@ public class Step {
         return Objects.hash(currentStep);
     }
 
-    public Boolean currentStep() {
-        return currentStep;
-    }
 }
