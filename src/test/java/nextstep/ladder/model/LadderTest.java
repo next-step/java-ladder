936d41634de89ab5982d613869d1be62d4a86770
choomi1217 <whdudal1217@naver.com>
2023-12-09 17:58:14 +0900

    refactor: 명확한 표현을 위해 `Line`에서 `RowLine`으로 변경

--- src/test/java/nextstep/ladder/model/LadderTest.java
+++ src/test/java/nextstep/ladder/model/LadderTest.java
@@ -1,9 +1,9 @@
-package nextstep.ladder;
+package nextstep.ladder.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import nextstep.ladder.model.Ladder;
-import nextstep.ladder.model.LineFactory;
+import nextstep.ladder.model.RowLineFactory;
 import nextstep.ladder.model.RandomLineGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,28 +11,34 @@ import org.junit.jupiter.api.Test;
 
 public class LadderTest {
 
-    private LineFactory lineFactory;
+    private RowLineFactory rowLineFactory;
 
     @BeforeEach
     void setting(){
-        lineFactory = LineFactory.from(RandomLineGenerator.getInstance(), 3);
+        rowLineFactory = RowLineFactory.from(RandomLineGenerator.getInstance(), 3);
     }
 
 
     @DisplayName("최대 높이만큼 라인을 만듭니다.")
     @Test
     void createLines() {
-        Ladder ladder = Ladder.of(5, lineFactory);
+        Ladder ladder = Ladder.of(5, rowLineFactory);
         assertThat(ladder.ladderSize()).isEqualTo(5);
     }
 
     @DisplayName("첫번째 사다리는 항상 거짓입니다.")
     @Test
     void alwaysFalseThatFirstLadder(){
-        Ladder ladder = Ladder.of(5, lineFactory);
+        Ladder ladder = Ladder.of(5, rowLineFactory);
         ladder.lines().forEach(line -> {
             assertThat(line.line().findFirst().get().canMove()).isFalse();
         });
+    }
+
+    @DisplayName("")
+    @Test
+    void a(){
+
     }
 
 }
