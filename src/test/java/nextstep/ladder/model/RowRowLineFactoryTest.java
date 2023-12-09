10b0ce660219a6a0959eed6097860fae29610683
choomi1217 <whdudal1217@naver.com>
2023-12-09 17:57:16 +0900

    refactor: 명확한 표현을 위해 `Line`에서 `RowLine`으로 변경

--- src/test/java/nextstep/ladder/model/RowRowLineFactoryTest.java
+++ src/test/java/nextstep/ladder/model/RowRowLineFactoryTest.java
@@ -1,20 +1,20 @@
-package nextstep.ladder;
+package nextstep.ladder.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import nextstep.ladder.model.Line;
-import nextstep.ladder.model.LineFactory;
+import nextstep.ladder.model.RowLine;
+import nextstep.ladder.model.RowLineFactory;
 import nextstep.ladder.model.RandomLineGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class LineFactoryTest {
+public class RowRowLineFactoryTest {
 
     @DisplayName("참여자 수를 받아서 라인 하나를 만든다.")
     @Test
     void createOneLine(){
-        Line line = LineFactory.from(RandomLineGenerator.getInstance(), 4).line();
-        assertThat(line.lineSize()).isEqualTo(4);
+        RowLine rowLine = RowLineFactory.from(RandomLineGenerator.getInstance(), 4).line();
+        assertThat(rowLine.lineSize()).isEqualTo(4);
     }
 
 }
