7b463749c3427c3d53df2aaf92fba69eb7a3699e
choomi1217 <whdudal1217@naver.com>
2023-12-09 17:53:21 +0900

    feat: `ScoreCalculator` 생성

---
+++ src/main/java/nextstep/ladder/model/ScoreCalculator.java
@@ -0,0 +1,44 @@
+package nextstep.ladder.model;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ScoreCalculator {
+
+    private final Players players;
+    private final Ladder ladder;
+
+    ScoreCalculator(Players players, Ladder ladder) {
+        this.players = players;
+        this.ladder = ladder;
+    }
+
+    public static ScoreCalculator of(Players players, Ladder ladder) {
+        return new ScoreCalculator(players, ladder);
+    }
+
+    public PlayersGameResult playersGameResult() {
+        List<Score> scoreList = players.list().stream()
+            .map(player -> ScoreCalculator.of(this.players, this.ladder).calculateScore(player))
+            .collect(Collectors.toList());
+
+        return PlayersGameResult.of(scoreList);
+    }
+
+    private Score calculateScore(Player player) {
+        Score score = Score.of(player, players.list().indexOf(player));
+        return ladder.lines()
+            .stream()
+            .reduce(score, (currentScore, rowLine) -> {
+                Step currentStep = rowLine.step(currentScore.intScore());
+                if (currentStep.currentStep()) {
+                    return currentScore.addSCore();
+                }
+                if (currentStep.previousStep()) {
+                    return currentScore.minusScore();
+                }
+                return currentScore;
+            }, (score1, score2) -> score2);
+    }
+
+}
