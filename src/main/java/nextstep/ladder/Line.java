8ccb0841948409fc3fafd1822ac4d4dffb5e8698
이창섭 <ventulus95@gmail.com>
2023-12-10 21:42:12 +0900

    fix: 간단한 이슈 수정

--- src/main/java/nextstep/ladder/Line.java
+++ src/main/java/nextstep/ladder/Line.java
@@ -1,7 +1,5 @@
 package nextstep.ladder;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -9,12 +7,13 @@ import java.util.stream.Stream;
 
 public class Line {
 
+    public static final int MAX_PERSON_SIZE = 1;
     private List<Boolean> points;
     private Random random = new Random();
 
     public Line (int countOfPerson) {
-        if (countOfPerson <= 1 ) {
-             throw new IllegalArgumentException("라인은 1명이상인 경우만 생성됩니다");
+        if (countOfPerson <= MAX_PERSON_SIZE) {
+             throw new IllegalArgumentException("라인은 1명 이상인 경우만 생성 됩니다");
         }
         this.points = Stream.iterate(random.nextBoolean(), this::hasLine)
                 .limit(countOfPerson-1)
