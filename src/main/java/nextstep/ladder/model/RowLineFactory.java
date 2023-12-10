d621ed175ada023e2abd725660d02c17a5e4b458
choomi1217 <whdudal1217@naver.com>
2023-12-10 16:34:49 +0900

    feat: `StepFactory`를 이용해 if문 하나 제거

--- src/main/java/nextstep/ladder/model/RowLineFactory.java
+++ src/main/java/nextstep/ladder/model/RowLineFactory.java
@@ -2,14 +2,17 @@ package nextstep.ladder.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class RowLineFactory {
 
     private final RandomLineGenerator randomLineGenerator;
+    private final StepFactory stepFactory;
     private final int count;
 
     private RowLineFactory(RandomLineGenerator randomLineGenerator, int count) {
         this.randomLineGenerator = randomLineGenerator;
+        this.stepFactory = new StepFactory();
         this.count = count;
     }
 
@@ -19,22 +22,23 @@ public class RowLineFactory {
 
     public RowLine line() {
         List<Step> line = new ArrayList<>();
-        line.add(Step.first(randomLineGenerator.randomStep()));
+        line.add(stepFactory.firstStep(randomLineGenerator));
 
-        for (int i = 1; i < count; i++) {
-            Boolean previousStep = line.get(i - 1).currentStep();
-            if (i == count - 1) {
-                line.add(Step.last(previousStep));
+        IntStream.range(1, count).forEach(i -> {
+            Boolean previousStep = line.get(i - 1).previousStep();
+            if (isItLastLine(i)) {
+                line.add(stepFactory.lastStep(previousStep));
             } else {
-                if (previousStep) {
-                    line.add(Step.of(false, previousStep));
-                } else {
-                    line.add(Step.of(randomLineGenerator.randomStep(), previousStep));
-                }
+                line.add(stepFactory.createStep(previousStep, randomLineGenerator));
             }
-        }
+        });
+
         return new RowLine(line);
     }
+
+    private boolean isItLastLine(int i) {
+        return i == count - 1;
+    }
+
+
 }
-
-
