4ba3dc8d3f6f1a7f34096365dad6f5f31bd7fb6c
choomi1217 <whdudal1217@naver.com>
2023-12-10 16:45:41 +0900

    test: `canFindPlayerWithPlayerName`, `canFindIndexWithPlayerName` 테스트 생성

--- src/test/java/nextstep/ladder/model/PlayersTest.java
+++ src/test/java/nextstep/ladder/model/PlayersTest.java
@@ -23,4 +23,23 @@ public class PlayersTest {
         assertThat(Players.from(strings).list()).containsExactlyInAnyOrder(Player.from("pobi"),
             Player.from("honux"));
     }
+
+    @DisplayName("player 이름을 이용해 Player를 찾을 수 있습니다.")
+    @Test
+    void canFindPlayerWithPlayerName(){
+        String poby = "poby";
+        Players players = Players.from(List.of(poby));
+        Player playerPoby = players.findPlayer(poby);
+        assertThat(playerPoby.name()).isEqualTo(poby);
+    }
+
+    @DisplayName("player 이름을 이용해 Player의 순서를 찾을 수 있습니다.")
+    @Test
+    void canFindIndexWithPlayerName(){
+        String poby = "poby";
+        Players players = Players.from(List.of(poby));
+        int index = players.orderByPlayerName(poby);
+        assertThat(index).isEqualTo(0);
+    }
+
 }
