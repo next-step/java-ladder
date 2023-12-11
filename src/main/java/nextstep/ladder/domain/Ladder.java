75435c081512bf012baf3e9a809f428083b59d9d
이창섭 <ventulus95@gmail.com>
2023-12-11 23:12:54 +0900

    refact: 랜덤의 의존성 주입 형태로 수정

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -1,13 +1,16 @@
 package nextstep.ladder.domain;
 
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Ladder {
 
     public static final int MAX_HEIGHT = 1;
+    public static final Random random = new Random();
     private List<Line> lines;
+
 
     public Ladder(int rows, int columns) {
         this.lines = makeLines(rows, columns);
@@ -21,7 +24,7 @@ public class Ladder {
         if (columns < MAX_HEIGHT) {
             throw new IllegalArgumentException(String.format("사다리는 높이가 %d 이상이여야 합니다", MAX_HEIGHT));
         }
-        return Stream.generate(() -> new Line(rows))
+        return Stream.generate(() -> new Line(rows, random))
                 .limit(columns)
                 .collect(Collectors.toList());
     }
--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -9,13 +9,15 @@ public class Line {
 
     public static final int MAX_PERSON_SIZE = 1;
     private final List<Boolean> points;
-    private Random random = new Random();
+    private final Random random;
 
     public Line(List<Boolean> points) {
         this.points = points;
+        this.random = new Random();
     }
 
-    public Line (int countOfPerson) {
+    public Line (int countOfPerson, Random random) {
+        this.random = random;
         if (countOfPerson <= MAX_PERSON_SIZE) {
              throw new IllegalArgumentException(String.format("라인은 %d명 이상인 경우만 생성 됩니다", MAX_PERSON_SIZE));
         }
