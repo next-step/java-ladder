24bba53a41d91c0c90df5d6f75bc26042558731b
kimbro97 <rlagudwog@gmail.com>
2023-12-12 00:11:09 +0900

    refactor: NPE예방 작업

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -2,6 +2,7 @@ package nextstep.ladder.domain;
 
 import nextstep.ladder.domain.strategy.GenerateLadderPoint;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Line {
@@ -9,6 +10,9 @@ public class Line {
 
     public Line(int namesSize, GenerateLadderPoint generateLadderPoint) {
         this.points = generateLadderPoint.generate(namesSize);
+        if (this.points == null) {
+            this.points = new ArrayList<>();
+        }
     }
 
     public List<Boolean> getPoints() {
