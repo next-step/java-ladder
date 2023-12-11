393d8b09d176815bb5e8dab9f10eae199e726739
이창섭 <ventulus95@gmail.com>
2023-12-11 22:26:45 +0900

    add: 사다리 그리는 테스트

--- src/test/java/nextstep/ladder/OutputViewTest.java
+++ src/test/java/nextstep/ladder/OutputViewTest.java
@@ -13,6 +13,6 @@ class OutputViewTest {
     void 사다리의_형태는_그림으로_출력해야한다() {
         OutputView outputView = new OutputView();
         Ladder ladder = new Ladder(List.of(new Line(List.of(true, false, true)), new Line(List.of(true, false, false))));
-        assertThat(outputView.ListStringLadder(ladder)).contains("     |-----|     |-----|", "     |-----|     |     |");
+        assertThat(outputView.stringLadder(ladder)).contains("     |-----|     |-----|", "     |-----|     |     |");
     }
 }
