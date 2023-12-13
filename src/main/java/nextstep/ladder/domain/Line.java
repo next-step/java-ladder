c2afd6e9c8d019ef7f911509efd3bcc1619fe3dc
hoon25 <cchoon95@hanmail.net>
2023-12-13 23:49:49 +0900

    refactor(line): for문 람다로 변경 및 메서드 분리

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -20,11 +20,14 @@ public class Line {
         this.points.addAll(points);
     }
 
-    private static void validateExistSequentialLine(List<Boolean> points) {
-        for (int i = 1; i < points.size(); i++) {
-            if (points.get(i - 1) && points.get(i)) {
-                throw new IllegalArgumentException("라인은 연속으로 겹치면 안됩니다.");
-            }
+    private void validateExistSequentialLine(List<Boolean> points) {
+        IntStream.range(1, points.size())
+                .forEach((i) -> validateSequential(points, i));
+    }
+
+    private void validateSequential(List<Boolean> points, int idx) {
+        if (points.get(idx - 1) && points.get(idx)) {
+            throw new IllegalArgumentException("라인은 연속으로 겹치면 안됩니다.");
         }
     }
 
