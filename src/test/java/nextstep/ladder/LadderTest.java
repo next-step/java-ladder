8c2e5d9451c599b665114489489649df3ea9d761
solchan98 <dev.sol0427@gmail.com>
2023-12-13 17:29:36 +0900

    refactor: 가독성 향상을 위한 LadderTest 수정

--- src/test/java/nextstep/ladder/LadderTest.java
+++ src/test/java/nextstep/ladder/LadderTest.java
@@ -22,31 +22,30 @@ class LadderTest {
 
     @Test
     void 사다리의_플레이어_수와_결과_수는_동일해야_한다() {
-        assertThatThrownBy(() -> Ladder.of(
-                        List.of("pobi", "honux"),
-                        new Lines(List.of(
-                                Line.from(List.of(new Point(false), new Point(false))),
-                                Line.from(List.of(new Point(false), new Point(false)))
-                        ))
-                        , List.of("꽝")
-                )
-        )
+        List<String> playerNames = List.of("pobi", "honux");
+        List<String> result = List.of("꽝");
+        Line line = lineOf(false, false);
+        Lines lines = new Lines(List.of(line, line));
+
+        assertThatThrownBy(() -> Ladder.of(playerNames, lines, result))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("플레이어와 결과의 수는 동일해야 합니다.");
     }
 
     @Test
     void 사다리는_게임_실행_후_결과를_반환할_수_있다() {
-        Ladder ladder = new Ladder(
-                List.of("pobi", "honux"),
-                new Lines(List.of(
-                        Line.from(List.of(new Point(false), new Point(false))),
-                        Line.from(List.of(new Point(false), new Point(false)))
-                )),
-                List.of("꽝", "5000")
-        );
+        List<String> playerNames = List.of("pobi", "honux");
+        List<String> result = List.of("꽝", "5000");
+        Line line = lineOf(false, false);
+        Lines lines = new Lines(List.of(line, line));
+
+        Ladder ladder = new Ladder(playerNames, lines, result);
 
         assertThat(ladder.play()).isNotNull();
+    }
+
+    private Line lineOf(boolean first, boolean second) {
+        return Line.from(List.of(new Point(first), new Point(second)));
     }
 
     @Test
