ea8733d813adb523680ea3cb33535200210be128
이효인 <hyoin@trycatch.kr>
2023-12-13 09:24:37 +0900

    refactor: service 제거

--- src/main/java/ladder/controller/LadderController.java
+++ src/main/java/ladder/controller/LadderController.java
@@ -2,17 +2,15 @@ package ladder.controller;
 
 import ladder.domain.Lines;
 import ladder.domain.Participants;
-import ladder.service.LadderService;
 import ladder.view.InputView;
 import ladder.view.ResultView;
 
 public class LadderController {
 
     public static void run() {
-        Participants participants = LadderService.createParticipants(InputView.inputParticipants());
+        Participants participants = Participants.from(InputView.inputParticipants());
         int ladderHeight = InputView.inputLadderHeight();
-
-        Lines lines = LadderService.createLines(participants.size(), ladderHeight);
+        Lines lines = new Lines(participants.size(), ladderHeight);
         ResultView.showResult(participants, lines);
     }
 }
