3c539778870f356dc15e88d3bfd93e18032dbec8
ParkSeolDev <parkseol.dev@gmail.com>
2024-01-07 02:27:07 +0900

    optional 실습

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -4,40 +4,40 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Lambda {
-    public static void printAllOld(List<Integer> numbers) {
-        System.out.println("printAllOld");
+	public static void printAllOld(List<Integer> numbers) {
+		System.out.println("printAllOld");
 
-        for (int number : numbers) {
-            System.out.println(number);
-        }
-    }
+		for (int number : numbers) {
+			System.out.println(number);
+		}
+	}
 
-    public static void printAllLambda(List<Integer> numbers) {
-        System.out.println("printAllLambda");
+	public static void printAllLambda(List<Integer> numbers) {
+		System.out.println("printAllLambda");
 
-        numbers.forEach(System.out::println);
-    }
+		numbers.forEach(System.out::println);
+	}
 
-    public static void runThread() {
-        new Thread(() -> System.out.println("Hello from thread")).start();
-    }
+	public static void runThread() {
+		new Thread(() -> System.out.println("Hello from thread")).start();
+	}
 
-    public static int sumAll(List<Integer> numbers) {
-        return sum(numbers, number -> true);
-    }
+	public static int sumAll(List<Integer> numbers) {
+		return sum(numbers, number -> true);
+	}
 
-    public static int sumAllEven(List<Integer> numbers) {
-        return sum(numbers, number -> number % 2 == 0);
-    }
+	public static int sumAllEven(List<Integer> numbers) {
+		return sum(numbers, number -> number % 2 == 0);
+	}
 
-    public static int sumAllOverThree(List<Integer> numbers) {
-        return sum(numbers, number -> number > 3);
-    }
+	public static int sumAllOverThree(List<Integer> numbers) {
+		return sum(numbers, number -> number > 3);
+	}
 
-    private static int sum(List<Integer> numbers, Predicate<Integer> condition) {
-        return numbers.stream()
-            .filter(condition)
-            .mapToInt(Integer::intValue)
-            .sum();
-    }
+	private static int sum(List<Integer> numbers, Predicate<Integer> condition) {
+		return numbers.stream()
+			.filter(condition)
+			.mapToInt(Integer::intValue)
+			.sum();
+	}
 }
--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -11,40 +11,40 @@ import java.util.stream.Collectors;
 
 public class StreamStudy {
 
-    public static long countWords() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+	public static long countWords() throws IOException {
+		String contents = new String(Files.readAllBytes(Paths
+			.get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        return words.stream().filter(w -> w.length() > 12).count();
-    }
+		return words.stream().filter(w -> w.length() > 12).count();
+	}
 
-    public static void printLongestWordTop100() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+	public static void printLongestWordTop100() throws IOException {
+		String contents = new String(Files.readAllBytes(Paths
+			.get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        words.stream()
-            .filter(word -> word.length() > 12)
-            .distinct()
-            .sorted(Comparator.comparing(String::length).reversed())
-            .limit(100)
-            .map(String::toLowerCase)
-            .forEach(System.out::println);
-    }
+		words.stream()
+			.filter(word -> word.length() > 12)
+			.distinct()
+			.sorted(Comparator.comparing(String::length).reversed())
+			.limit(100)
+			.map(String::toLowerCase)
+			.forEach(System.out::println);
+	}
 
-    public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
-    }
+	public static List<Integer> doubleNumbers(List<Integer> numbers) {
+		return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
+	}
 
-    public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
-    }
+	public static long sumAll(List<Integer> numbers) {
+		return numbers.stream().reduce(0, (x, y) -> x + y);
+	}
 
-    public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return numbers.stream()
-            .filter(n -> n > 3)
-            .map(x -> 2 * x)
-            .reduce(0, (a, b) -> a + b);
-    }
+	public static long sumOverThreeAndDouble(List<Integer> numbers) {
+		return numbers.stream()
+			.filter(number -> number > 3)
+			.map(x -> 2 * x)
+			.reduce(0, (a, b) -> a + b);
+	}
 }
