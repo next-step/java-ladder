bbd80db2e5ff95ba711138bba5be00a378bd9b02
ParkSeolDev <108073530+ParkSeolDev@users.noreply.github.com>
2024-01-07 11:59:50 +0900

    Step1 스트림, 람다, Optional (#2038)
    * 익명 클래스를 람다로 전환
    
    * 람다를 활용해 중복 제거
    
    * stream method 실습
    
    * optional 실습

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,56 +1,43 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Lambda {
-    public static void printAllOld(List<Integer> numbers) {
-        System.out.println("printAllOld");
+	public static void printAllOld(List<Integer> numbers) {
+		System.out.println("printAllOld");
 
-        for (int number : numbers) {
-            System.out.println(number);
-        }
-    }
+		for (int number : numbers) {
+			System.out.println(number);
+		}
+	}
 
-    public static void printAllLambda(List<Integer> numbers) {
-        System.out.println("printAllLambda");
+	public static void printAllLambda(List<Integer> numbers) {
+		System.out.println("printAllLambda");
 
-        numbers.forEach(System.out::println);
-    }
+		numbers.forEach(System.out::println);
+	}
 
-    public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
-    }
+	public static void runThread() {
+		new Thread(() -> System.out.println("Hello from thread")).start();
+	}
 
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
-    }
+	public static int sumAll(List<Integer> numbers) {
+		return sum(numbers, number -> true);
+	}
 
-    public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
-    }
+	public static int sumAllEven(List<Integer> numbers) {
+		return sum(numbers, number -> number % 2 == 0);
+	}
 
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
-    }
+	public static int sumAllOverThree(List<Integer> numbers) {
+		return sum(numbers, number -> number > 3);
+	}
+
+	private static int sum(List<Integer> numbers, Predicate<Integer> condition) {
+		return numbers.stream()
+			.filter(condition)
+			.mapToInt(Integer::intValue)
+			.sum();
+	}
 }
