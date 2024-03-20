ac7d9f3c42691d2c9d616df69a26abb2597cf95e
deepredk <deepred.kim@gmail.com>
2024-03-20 09:22:03 +0900

    람다 실습 2 - 람다를 활용해 중복 제거

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,8 +1,10 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Lambda {
+
     public static void printAllOld(List<Integer> numbers) {
         System.out.println("printAllOld");
 
@@ -18,39 +20,25 @@ public class Lambda {
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> System.out.println("Hello from thread")).start();
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAllConditionally(numbers, i -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAllConditionally(numbers, i -> i % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAllConditionally(numbers, i -> i > 3);
+    }
+
+    private static int sumAllConditionally(List<Integer> numbers, Conditional conditional) {
+        return numbers.stream()
+            .filter(conditional::test)
+            .mapToInt(Integer::valueOf)
+            .sum();
     }
 }
