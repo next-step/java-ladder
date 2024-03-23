6e672f126747e8140b0f0af517cdfd3564cf675b
leejinwoo <leejinwoo1126@gmail.com>
2024-03-23 11:36:54 +0900

    feat: 라인 기능 구현, 테스트

---
+++ src/main/java/nextstep/ladder/model/Line.java
@@ -0,0 +1,84 @@
+package nextstep.ladder.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class Line {
+    private static final int MIN_COUNT = 2;
+    private static final Random RANDOM = new Random();
+
+    private final List<Boolean> points;
+
+    public static Line from(int countOfPerson) {
+        return new Line(create(countOfPerson));
+    }
+
+    public static Line from(List<Boolean> points) {
+        return new Line(points);
+    }
+
+    private static List<Boolean> create(int countOfPerson) {
+        if (countOfPerson < MIN_COUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        List<Boolean> result = new ArrayList<>();
+        result.add(canConnect());
+        for (int i = 1; i < countOfPerson - 1; i++) {
+            result.add(isBuildable(result.get(i - 1)));
+        }
+
+        return result;
+    }
+
+    private static boolean isBuildable(boolean prev) {
+        return !prev && canConnect();
+    }
+
+    private static boolean canConnect() {
+        return RANDOM.nextBoolean();
+    }
+
+    private Line(List<Boolean> points) {
+        assertNullOrEmpty(points);
+        assertInvalidLine(points);
+        this.points = points;
+    }
+
+    private void assertNullOrEmpty(List<Boolean> points) {
+        if (points == null || points.isEmpty()) {
+            throw new IllegalArgumentException("라인 정보가 비어있습니다");
+        }
+    }
+
+    private void assertInvalidLine(List<Boolean> points) {
+        IntStream.rangeClosed(0, points.size() - 2)
+                .forEach(idx -> checkContinuousLine(points.get(idx), points.get(idx + 1)));
+    }
+
+    private void checkContinuousLine(boolean prev, boolean next) {
+        if (prev && next) {
+            throw new IllegalArgumentException("라인이 겹치지 않아야 합니다");
+        }
+    }
+
+    public List<Boolean> getPoints() {
+        return points;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        Line line = (Line) other;
+        return Objects.equals(points, line.points);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(points);
+    }
+}
---
+++ src/test/java/nextstep/ladder/model/LineTest.java
@@ -0,0 +1,41 @@
+package nextstep.ladder.model;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LineTest {
+
+    @Test
+    void create() {
+        Line line = Line.from(Arrays.asList(true, false, true));
+        assertThat(line).isEqualTo(Line.from(Arrays.asList(true, false, true)));
+    }
+
+    @Test
+    void 인원수가_2명_미만인경우_예외던진다() {
+        assertThatThrownBy(() -> Line.from(1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 라인은_연속적으로_연결할수없다() {
+        assertThatThrownBy(() -> Line.from(Arrays.asList(false, true, true)))
+                .isInstanceOf(IllegalArgumentException.class);
+        
+        assertThatThrownBy(() -> Line.from(Arrays.asList(true, true, false)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @RepeatedTest(10)
+    void 사다리_라인이_연속으로_연결되는_경우는_없다() {
+        assertThatNoException()
+                .isThrownBy(() -> Line.from(3));
+    }
+
+}
