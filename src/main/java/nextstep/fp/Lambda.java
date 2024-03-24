55ecaae2ec8b4c47a863d198ecef19dea1738780
firewoody237 <firewoody237@gmail.com>
2024-03-25 00:54:51 +0900

    refactor: Lambda 요구사항에 맞게 변경

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -26,13 +26,21 @@ public class Lambda {
         }).start();
     }
 
-    public static int sum(List<Integer> numbers, SumCondition sumCondition) {
-        int total = 0;
-        for (int number : numbers) {
-            if (sumCondition.isMatchCondition(number)) {
-                total += number;
-            }
-        }
-        return total;
+    public static int sumAll(List<Integer> numbers) {
+        return sum(numbers, number -> true);
+    }
+
+    public static int sumAllEven(List<Integer> numbers) {
+        return sum(numbers, number -> number % 2 == 0);
+    }
+
+    public static int sumAllOverThree(List<Integer> numbers) {
+        return sum(numbers, number -> number > 3);
+    }
+
+    private static int sum(List<Integer> numbers, SumCondition condition) {
+        return numbers.stream()
+                .filter(condition::isMatchCondition)
+                .reduce(0, Integer::sum);
     }
 }
