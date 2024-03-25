b24d191c22975c3aa12d61686afd6281c97972cb
hellonayeon <hellonykwon@gmail.com>
2024-03-25 22:36:51 +0900

    Refactor: printLongestWordTop100 메서드 람다 표현식으로 수정

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
@@ -22,7 +25,21 @@ public class StreamStudy {
         String contents = Files.readString(Paths.get("src/main/resources/fp/war-and-peace.txt"));
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        AtomicInteger index = new AtomicInteger(0);
+        StringBuilder stringBuilder = new StringBuilder();
+        words.stream()
+                .filter(word -> word.length() > 12)
+                .sorted(Comparator.comparing(String::length).reversed())
+                .distinct()
+                .limit(100)
+                .forEach(getStringConsumer(index, stringBuilder));
+        System.out.println(stringBuilder);
+    }
+
+    private static Consumer<String> getStringConsumer(AtomicInteger index, StringBuilder stringBuilder) {
+        return word -> stringBuilder
+                .append(String.format("%03d. %s", index.incrementAndGet(), word))
+                .append(System.lineSeparator());
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
