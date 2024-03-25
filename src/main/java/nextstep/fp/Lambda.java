b95490556413c159ce298098121037d60a24bfa4
hellonayeon <hellonykwon@gmail.com>
2024-03-25 21:46:46 +0900

    Fix: 람다를 활용하여 sumAll 메서드 중복 제거

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -27,11 +27,9 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
     public static int sumAllEven(List<Integer> numbers) {
