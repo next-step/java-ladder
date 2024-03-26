ae592d1f7327a23aa880afb66ced879d8bdbc562
nimoh <spakers38@gmail.com>
2024-03-26 21:04:34 +0900

    feat: [Step 2] 최대 사다리 높이 입력받아 반환 (숫자 아닌 경우 예외처리 후 재시도)

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,12 +11,16 @@ import java.util.stream.Collectors;
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     public static final String PLAYER_NAME_INPUT_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    public static final String MAX_LADDER_HEIGHT_INPUT_MESSAGE = "최대 사다리 높이는 몇 개 인가요?";
+    public static final String LADDER_HEIGHT_RANGE_OVER = "1 이상의 정수만 입력할 수 있습니다.";
+    public static final String TRY_INPUT_AGAIN = " 다시 입력해주세요.";
+    public static final String NUMBER_FORMAT_EXCEPTION = "숫자만 입력할 수 있습니다.";
 
     public Players inputPlayers() {
         return inputPlayers(PLAYER_NAME_INPUT_MESSAGE);
     }
 
-    public Players inputPlayers(String message) {
+    private Players inputPlayers(String message) {
         System.out.println(message);
         List<String> nameList = StringUtil.splitStringToList(SCANNER.nextLine(), ",");
         try {
@@ -26,8 +30,31 @@ public class InputView {
 
             return Players.from(playerList);
         } catch (IllegalArgumentException e) {
-            return inputPlayers(e.getMessage());
+            return inputPlayers(e.getMessage() + TRY_INPUT_AGAIN);
         }
+    }
+
+    public int inputLadderHeight() {
+        return inputLadderHeight(MAX_LADDER_HEIGHT_INPUT_MESSAGE);
+    }
+
+    private int inputLadderHeight(String message) {
+        System.out.println(message);
+        try {
+            return scanLadderHeight();
+        } catch (NumberFormatException e) {
+            return inputLadderHeight(NUMBER_FORMAT_EXCEPTION + TRY_INPUT_AGAIN);
+        } catch (IllegalArgumentException e) {
+            return inputLadderHeight(e.getMessage() + TRY_INPUT_AGAIN);
+        }
+    }
+
+    private static int scanLadderHeight() {
+        int inputInt = Integer.parseInt(SCANNER.nextLine());
+        if (inputInt < 1) {
+            throw new IllegalArgumentException(LADDER_HEIGHT_RANGE_OVER + TRY_INPUT_AGAIN);
+        }
+        return inputInt;
     }
 
 }
