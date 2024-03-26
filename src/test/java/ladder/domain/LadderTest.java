660a30f045c29e81755807a0fd20207f242c8370
deepredk <deepred.kim@gmail.com>
2024-03-26 14:34:46 +0900

    test; Ladder.result()에 대한 테스트코드 작성

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -1,5 +1,9 @@
 package ladder.domain;
 
+import static ladder.domain.Link.LEFT_LINK;
+import static ladder.domain.Link.NO_LINK;
+import static ladder.domain.Link.RIGHT_LINK;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -24,5 +28,30 @@ class LadderTest {
         assertThatThrownBy(() -> Ladder.from(Collections.emptyList()))
             .isInstanceOf(IllegalArgumentException.class);
 
+    }
+
+    @Test
+    @DisplayName("인덱스 별 사다리 결과를 반환한다")
+    void result() {
+        Ladder ladder = makeLadder();
+        LadderResult result = ladder.result();
+
+        assertThat(result.resultIndexOf(0)).isEqualTo(2);
+        assertThat(result.resultIndexOf(1)).isEqualTo(0);
+        assertThat(result.resultIndexOf(2)).isEqualTo(3);
+        assertThat(result.resultIndexOf(3)).isEqualTo(1);
+    }
+
+    private static Ladder makeLadder() {
+        /*
+            |-----|     |-----|
+            |     |-----|     |
+         */
+        return Ladder.from(
+            List.of(
+                Line.from(List.of(RIGHT_LINK, LEFT_LINK, RIGHT_LINK, LEFT_LINK)),
+                Line.from(List.of(NO_LINK, RIGHT_LINK, LEFT_LINK, NO_LINK))
+            )
+        );
     }
 }
