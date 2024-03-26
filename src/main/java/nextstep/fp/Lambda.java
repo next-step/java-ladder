0d98553b6eb7e235f288f10b8544969de1d3e2a9
hellonayeon <hellonykwon@gmail.com>
2024-03-26 20:46:24 +0900

    Revert "Refactor: Predicate<Integer> 를 파라미터로 넘겨주어 sum 메서드 단일화"

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -26,31 +26,24 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers, Conditional conditional) {
+    public static int sumAll(List<Integer> numbers) {
         return numbers.stream()
-                .filter(conditional::test)
                 .mapToInt(Integer::intValue)
                 .sum();
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return numbers.stream()
+                .filter(number -> number > 3)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
 }
