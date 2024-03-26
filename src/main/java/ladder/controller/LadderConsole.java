6622ab9600d58e49f2d69ef7e2813827678fddbe
deepredk <deepred.kim@gmail.com>
2024-03-26 14:09:50 +0900

    refactor: LadderGame 삭제
    - 별 다른 책임을 가지고 있지 않아 분리가 불필요하다고 판단함

--- src/main/java/ladder/controller/LadderConsole.java
+++ src/main/java/ladder/controller/LadderConsole.java
@@ -3,8 +3,8 @@ package ladder.controller;
 import java.util.List;
 import ladder.domain.Ladder;
 import ladder.domain.LadderFactory;
-import ladder.domain.LadderGame;
 import ladder.domain.LadderGameResult;
+import ladder.domain.LadderJudge;
 import ladder.domain.Participant;
 import ladder.view.InputView;
 import ladder.view.OutputView;
@@ -29,8 +29,8 @@ public class LadderConsole {
         Ladder ladder = LadderFactory.generate(participants.size(), height);
         outputView.printLadder(ladder, participants, results);
 
-        LadderGame ladderGame = new LadderGame(ladder, participants, results);
-        LadderGameResult gameResult = ladderGame.result();
+        LadderJudge ladderJudge = new LadderJudge(participants, results);
+        LadderGameResult gameResult = ladderJudge.judge(ladder.result());
         checkGameResult(participants, gameResult);
     }
 
