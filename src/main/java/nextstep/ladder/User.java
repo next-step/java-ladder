82315adbfbc9d1f801202c622caf5a5f14d43f99
hellonayeon <hellonykwon@gmail.com>
2024-03-27 21:22:05 +0900

    Feature: User name 글자수 검증 메서드 추가

--- src/main/java/nextstep/ladder/User.java
+++ src/main/java/nextstep/ladder/User.java
@@ -1,11 +1,25 @@
 package nextstep.ladder;
 
+import nextstep.ladder.exception.ExceedUserNameLengthException;
+
 public class User {
 
+    private static final int MAX_NAME_LENGTH = 5;
     private final String name;
 
     public User(String name) {
+        validateNameLength(name);
         this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (exceedNameLength(name)) {
+            throw new ExceedUserNameLengthException(name);
+        }
+    }
+
+    private boolean exceedNameLength(String name) {
+        return name.length() > MAX_NAME_LENGTH;
     }
 
     public String getName() {
---
+++ src/main/java/nextstep/ladder/exception/ExceedUserNameLengthException.java
@@ -0,0 +1,10 @@
+package nextstep.ladder.exception;
+
+import java.text.MessageFormat;
+
+public class ExceedUserNameLengthException extends IllegalArgumentException {
+
+    public ExceedUserNameLengthException(String name) {
+        super(MessageFormat.format("사람의 이름은 5글자를 초과할 수 없습니다. (입력된 이름: {0}, 입력된 글자수: {1})", name, name.length()));
+    }
+}
