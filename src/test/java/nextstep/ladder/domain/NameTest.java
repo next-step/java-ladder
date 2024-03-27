1d7bfbf8415e6869ca1885493c793ecef67ebec2
leeedohyun <ldh19738@gmail.com>
2024-03-27 14:01:13 +0900

    test: 이름이 빈 문자열이거나 null인 경우 예외 테스트

--- src/test/java/nextstep/ladder/domain/NameTest.java
+++ src/test/java/nextstep/ladder/domain/NameTest.java
@@ -2,12 +2,16 @@ package nextstep.ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class NameTest {
 
-    @Test
-    void 참여하는_사람의_이름_예외를_테스트한다() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new Name("unicorn"));
+    @ParameterizedTest
+    @ValueSource(strings = {"unicorn", "ladder"})
+    @NullAndEmptySource
+    void 참여하는_사람의_이름_예외를_테스트한다(final String name) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Name(name));
     }
 }
