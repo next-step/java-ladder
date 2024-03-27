ed3911c9ef5b9811b38b7977474f67ab2e278a20
deepredk <deepred.kim@gmail.com>
2024-03-27 13:20:07 +0900

    refactor: IntStream => Stream.generate() 사용하도록 개선

--- src/main/java/ladder/domain/LadderFactory.java
+++ src/main/java/ladder/domain/LadderFactory.java
@@ -2,13 +2,13 @@ package ladder.domain;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class LadderFactory {
 
     public static Ladder generate(int participantCount, int height) {
-        List<Line> lines = IntStream.range(0, height)
-            .mapToObj(i -> LineFactory.generate(participantCount))
+        List<Line> lines = Stream.generate(() -> LineFactory.generate(participantCount))
+            .limit(height)
             .collect(Collectors.toList());
         return Ladder.from(lines);
     }
