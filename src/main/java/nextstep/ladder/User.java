ef96df4ddf33cca8810d8f91ef4d5ed7e903f28d
hellonayeon <hellonykwon@gmail.com>
2024-03-27 21:14:09 +0900

    Feature: User 클래스 추가

---
+++ src/main/java/nextstep/ladder/User.java
@@ -0,0 +1,14 @@
+package nextstep.ladder;
+
+public class User {
+
+    private final String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
