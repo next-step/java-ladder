4b54682dd52853af8fa5146f77349d8cdcf739d8
Yongha Kwon <yongha.kwon12@gmail.com>
2024-03-28 08:00:18 +0900

    [Step1] 스트림, 람다, Optional (#2063)
    * refact: 익명 클래스 람다로 전환
    
    * refact: 람다를 활용한 중복 제거
    
    * refact: mapToInt 대신 reduce 사용하여 합 구하도록 수정
    
    * feat: 3보다 큰 수 2배 한 후, 모든 값 합 구하는 기능 추가
    
    * feat: 12글자 초과하는 단어 중 길이가 긴 순서대로 서로 다른 단어 100개 소문자로 출력
    
    * feat: 요구사항 1 - Optional을 활용해 조건에 따른 반환
    
    * feat: 요구사항 2 - Optional에서 값을 반환
    
    * feat: 요구사항 3 - Optional에서 exception 처리

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,6 +29,12 @@ public class StreamStudy {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         // TODO 이 부분에 구현한다.
+        words.stream().filter(word -> word.length() > 12)
+                .map(String::toLowerCase)
+                .distinct()
+                .sorted((a,b) -> Integer.compare(b.length() , a.length()))
+                .limit(100)
+                .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -35,10 +42,10 @@ public class StreamStudy {
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers.stream().reduce(0, Integer::sum);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream().filter(number -> number > 3).map(number -> number * 2).reduce(0, Integer::sum);
     }
 }
--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,7 +35,15 @@ public class User {
     }
 
     public static boolean ageIsInRange2(User user) {
-        return false;
+        Optional<User> validUser = Optional.ofNullable(user);
+
+        if (validUser.isEmpty()) {
+            return false;
+        }
+
+        return validUser
+                .filter(inputUser -> inputUser.age != null && inputUser.age >= 30 && inputUser.age <= 45)
+                .isPresent();
     }
 
     @Override
