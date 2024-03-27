e2dfe01c108920f39377f82bfa96192e85ddd7df
hellonayeon <hellonykwon@gmail.com>
2024-03-28 00:32:51 +0900

    Feature: Main 클래스 작성

---
+++ src/main/java/nextstep/ladder/Main.java
@@ -0,0 +1,21 @@
+package nextstep.ladder;
+
+import nextstep.ladder.domain.Ladder;
+import nextstep.ladder.domain.LadderGame;
+import nextstep.ladder.domain.Participants;
+import nextstep.ladder.ui.InputView;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> names = InputView.readParticipantNames();
+        int height = InputView.readLadderMaxHeight();
+
+        Participants participants = Participants.of(names);
+        Ladder ladder = Ladder.of(participants.numberOf(), height);
+        LadderGame ladderGame = new LadderGame(participants, ladder);
+    }
+
+}
