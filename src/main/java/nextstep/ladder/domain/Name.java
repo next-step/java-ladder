b85668b5f894d107ea93258e687a472f1ad36a64
leeedohyun <ldh19738@gmail.com>
2024-03-27 14:14:57 +0900

    feat: 쉼표로 구분된 참여자 이름을 일급 컬렉션으로 포장

--- src/main/java/nextstep/ladder/domain/Name.java
+++ src/main/java/nextstep/ladder/domain/Name.java
@@ -1,5 +1,7 @@
 package nextstep.ladder.domain;
 
+import java.util.Objects;
+
 public class Name {
 
     private static final int MAX_NAME_LENGTH = 5;
@@ -22,5 +24,22 @@ public class Name {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("이름의 길이는 5자를 초과할 수 없습니다.");
         }
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
