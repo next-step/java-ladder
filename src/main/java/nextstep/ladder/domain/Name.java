faf69a566ea686b05bf4faeec23238194f4cf2e9
leeedohyun <ldh19738@gmail.com>
2024-03-27 14:01:39 +0900

    feat: 이름이 빈 문자열이거나 null인 경우 예외 처리

--- src/main/java/nextstep/ladder/domain/Name.java
+++ src/main/java/nextstep/ladder/domain/Name.java
@@ -7,12 +7,19 @@ public class Name {
     private final String name;
 
     public Name(final String name) {
+        validateEmpty(name);
         validateNameLength(name);
         this.name = name;
     }
 
     private void validateNameLength(final String name) {
         if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름의 길이는 5자를 초과할 수 없습니다.");
+        }
+    }
+
+    private void validateEmpty(final String name) {
+        if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("이름의 길이는 5자를 초과할 수 없습니다.");
         }
     }
