39a1b67d22d586e565bcbfd2c08d6ae3447b4832
hellonayeon <hellonykwon@gmail.com>
2024-03-27 21:21:44 +0900

    Test: 사람 생성 시 5글자를 초과하는 경우의 단위 테스트 작성

--- src/test/java/nextstep/ladder/UserTest.java
+++ src/test/java/nextstep/ladder/UserTest.java
@@ -1,5 +1,6 @@
 package nextstep.ladder;
 
+import nextstep.ladder.exception.ExceedUserNameLengthException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +12,14 @@ public class UserTest {
     @DisplayName("[성공] 5글자 이하의 이름을 가지는 사람을 생성한다.")
     void 사람_생성() {
         assertThat((new User("pobi")).getName()).hasSizeLessThan(5);
+    }
+
+    @Test
+    @DisplayName("[실패] 5글자 초과의 이름을 가지는 사람을 생성하려는 경우 IllegalUserNameLengthException 예외가 발생한다.")
+    void 사람_생성_예외() {
+        assertThatExceptionOfType(ExceedUserNameLengthException.class)
+                .isThrownBy(() -> {
+                    new User("nayeon");
+                }).withMessageContaining("사람의 이름은 5글자를 초과할 수 없습니다.");
     }
 }
