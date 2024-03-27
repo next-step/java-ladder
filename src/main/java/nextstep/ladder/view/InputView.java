772c4709c21074413ef478342304a528b825b268
leeedohyun <ldh19738@gmail.com>
2024-03-27 13:46:33 +0900

    feat: 사다리 높이 입력 기능 구현

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -11,4 +11,9 @@ public class InputView {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         return SCANNER.nextLine().split(NAME_SPLITTER);
     }
+
+    public int inputLadderHeight() {
+        System.out.println("\n최대 사다리 높이는 몇 개인가요?");
+        return SCANNER.nextInt();
+    }
 }
