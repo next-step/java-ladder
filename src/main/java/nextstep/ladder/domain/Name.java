6e8e8f813fe805497699623033badf03bb99d275
leeedohyun <ldh19738@gmail.com>
2024-03-27 13:52:48 +0900

    feat: 이름이 5자를 초과한 경우 예외 처리

---
+++ src/main/java/nextstep/ladder/domain/Name.java
@@ -0,0 +1,19 @@
+package nextstep.ladder.domain;
+
+public class Name {
+
+    private static final int MAX_NAME_LENGTH = 5;
+
+    private final String name;
+
+    public Name(final String name) {
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(final String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름의 길이는 5자를 초과할 수 없습니다.");
+        }
+    }
+}
