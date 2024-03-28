e3fa3494cd30c44e6f111ac326878a9dc81f82e7
이승호 <seung7642@openbase.co.kr>
2024-03-28 13:01:51 +0900

    feat: Line 객체는 특정 높이에 bridge 를 추가할 수 있다.

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,6 +1,7 @@
 package domain;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Ladder {
--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -1,12 +1,24 @@
 package domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Line {
 
     private final Name name;
     private final int height;
+    private Map<Integer, Boolean> bridges = new HashMap<>();
 
     public Line(Name name, int height) {
         this.name = name;
         this.height = height;
+    }
+
+    public void addBridge(int height) {
+        bridges.put(height, true);
+    }
+
+    public boolean hasBridge(int height) {
+        return bridges.getOrDefault(height, false);
     }
 }
