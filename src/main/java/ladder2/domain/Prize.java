32f05aed84aa472853d9633a89d8a95fe66e94f6
deepredk <deepred.kim@gmail.com>
2024-03-28 13:21:06 +0900

    feat: 당첨 상품(Prize) 구현

--- src/main/java/ladder2/domain/Prize.java
+++ src/main/java/ladder2/domain/Prize.java
@@ -2,4 +2,19 @@ package ladder2.domain;
 
 public class Prize {
 
+    private final int index;
+    private final String name;
+
+    public Prize(int index, String name) {
+        this.index = index;
+        this.name = name;
+    }
+
+    public int index() {
+        return index;
+    }
+
+    public String name() {
+        return name;
+    }
 }
