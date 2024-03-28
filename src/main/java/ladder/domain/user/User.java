52ce8921b38d2f4185396ea724a1aba69b5cf8ac
Sangmin <sm9171@nate.com>
2024-03-28 16:29:05 +0900

    refactor : if 컨밴션, 매직넘버 상수화

--- src/main/java/ladder/domain/user/User.java
+++ src/main/java/ladder/domain/user/User.java
@@ -1,6 +1,7 @@
 package ladder.domain.user;
 
 public class User {
+	private static final int NAME_LENGTH_MAX_NUMBER = 5;
 	private final String name;
 
 	public User(String name) {
@@ -8,10 +9,10 @@ public class User {
 	}
 
 	public static User createUser(String name) {
-		if(name == null || name.isEmpty()){
+		if (name == null || name.isEmpty()) {
 			throw new IllegalArgumentException("이름은 null이나 빈 문자열이 될 수 없습니다.");
 		}
-		if(name.length() > 5){
+		if (name.length() > NAME_LENGTH_MAX_NUMBER) {
 			throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
 		}
 		return new User(name);
