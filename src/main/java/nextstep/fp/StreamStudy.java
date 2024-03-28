c103b68f8e6836ba7fd705fcdcbcf26b65d4245c
홍유석(YU SEOK HONG) <jericoh@naver.com>
2024-03-28 10:42:48 +0900

    사다리타기 - FP, OOP: 1단계 - 스트림, 람다, Optional (#2065)
    * test: 람다 실습1 - 익명 클래스를 람다로 전환
    
    * test: 람다 실습 2 - 람다를 활용해 중복 제거
    - 테스트 구현
    
    * feat: 람다 실습 2 - 람다를 활용해 중복 제거
    - 람다 이용하여 중복 제거하도록 로직 구현
    
    * feat: map, reduce, filter 실습 1, 2 구현
    
    * feat: Optional
    - (요구사항 1 - Optional을 활용해 조건에 따른 반환) 구현
    
    * refactor: Optional
    - (요구사항 2 - Optional에서 값을 반환) 구현
    
    * refactor: Optional
    - (요구사항 3 - Optional에서 exception 처리) 구현
    
    * refactor: 코드 포맷팅
    
    * refactor: 어노테이션 추가
    
    * refactor: map 사용하도록 수정

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -27,7 +27,13 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(word -> word.length() > 12)
+                .sorted((o1, o2) -> o2.length() - o1.length())
+                .distinct()
+                .limit(100)
+                .map(String::toLowerCase)
+                .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -35,10 +41,13 @@ public class StreamStudy {
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers.stream().reduce(0, Integer::sum);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(number -> number > 3)
+                .map(number -> number * 2)
+                .reduce(0, Integer::sum);
     }
 }
