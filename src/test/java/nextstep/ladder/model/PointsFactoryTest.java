340eef583541478579bdcc8807037d3d56e11e87
leejinwoo <leejinwoo1126@gmail.com>
2024-03-28 12:50:59 +0900

    test: PointsFactory 예외 케이스 추가

--- src/test/java/nextstep/ladder/model/PointsFactoryTest.java
+++ src/test/java/nextstep/ladder/model/PointsFactoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class PointsFactoryTest {
 
@@ -15,5 +16,11 @@ class PointsFactoryTest {
         List<Point> points = PointsFactory.createBy(countOfPerson);
 
         assertThat(points).hasSize(4);
+    }
+
+    @Test
+    void 최소인원수_미만인경우_예외를_던진다() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> PointsFactory.createBy(1));
     }
 }
