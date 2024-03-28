9da73cc128ccbd57923f33734628867b2b06258e
deepredk <deepred.kim@gmail.com>
2024-03-28 12:24:26 +0900

    feat: 사다리 구현 - 여러 사다리 행을 알고있다

---
+++ src/main/java/ladder2/domain/Ladder.java
@@ -0,0 +1,12 @@
+package ladder2.domain;
+
+import java.util.List;
+
+public class Ladder {
+
+    private final List<LadderRow> rows;
+
+    public Ladder(List<LadderRow> rows) {
+        this.rows = rows;
+    }
+}
