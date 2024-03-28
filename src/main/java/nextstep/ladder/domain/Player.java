e579f15c43ce350889479f0200f6685237a672a1
kwanik-kor <rhksdlr134@naver.com>
2024-03-28 21:08:53 +0900

    feat: Player 생성자 추가 및 테스트 코드 추가

---
+++ src/main/java/nextstep/ladder/domain/Player.java
@@ -0,0 +1,20 @@
+package nextstep.ladder.domain;
+
+public class Player {
+    private static final int MAX_NAME_LENGTH = 5;
+
+    private final String name;
+
+    public Player(String name) {
+        assertNameNotBlankAndLengthUnderFive(name);
+
+        this.name = name;
+    }
+
+    private void assertNameNotBlankAndLengthUnderFive(String name) {
+        if (name == null || name.isBlank() || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 1글자에서 5글자까지 입력 가능합니다.");
+        }
+    }
+
+}
