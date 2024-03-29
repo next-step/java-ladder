1aed61552b4fbf36b292e92df2ffbb5d6d38852b
leeedohyun <ldh19738@gmail.com>
2024-03-29 19:29:34 +0900

    feat: Boolean 값 포장

---
+++ src/main/java/nextstep/ladder/domain/Bridge.java
@@ -0,0 +1,20 @@
+package nextstep.ladder.domain;
+
+public enum Bridge {
+
+    BRIDGE(true),
+    NON_BRIDGE(false);
+
+    private final boolean isBridge;
+
+    Bridge(final boolean isBridge) {
+        this.isBridge = isBridge;
+    }
+
+    public static Bridge of(final boolean now) {
+        if (BRIDGE.isBridge == now) {
+            return BRIDGE;
+        }
+        return NON_BRIDGE;
+    }
+}
