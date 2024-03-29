73f0fca26bb1e8612230281c93292cb1c2d9a19e
hong yuseok <jericoh@naver.com>
2024-03-29 17:35:57 +0900

    feat: Validator 구현

---
+++ src/main/java/laddergame/util/Validator.java
@@ -0,0 +1,21 @@
+package laddergame.util;
+
+import java.util.regex.Pattern;
+
+import static java.util.Objects.nonNull;
+
+public class Validator {
+    private static final Pattern PATTERN_FOR_POSITIVE_INTEGER = Pattern.compile("^([1-9][0-9]*)$");
+
+    public static boolean isNonBlank(String stringInput) {
+        return nonNull(stringInput) && !stringInput.isBlank();
+    }
+
+    public static boolean isValidStringLength(String stringInput, int maxStringLength) {
+        return isNonBlank(stringInput) && stringInput.length() <= maxStringLength;
+    }
+
+    public static boolean isPositiveInteger(String numberString) {
+        return isNonBlank(numberString) && PATTERN_FOR_POSITIVE_INTEGER.matcher(numberString).find();
+    }
+}
