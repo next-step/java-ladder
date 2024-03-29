c1ed858efe2dd5b80475498a8391429f788473a8
hong yuseok <jericoh@naver.com>
2024-03-29 20:26:22 +0900

    test: Validator.isGreaterThanStandard()에 대한 테스트 구현 및 다른 테스트 코드 리팩토링

--- src/test/java/laddergame/util/ValidatorTest.java
+++ src/test/java/laddergame/util/ValidatorTest.java
@@ -12,22 +12,29 @@ class ValidatorTest {
     @ParameterizedTest
     @NullAndEmptySource
     @ValueSource(strings = {"    "})
-    @DisplayName("isNonBlank(): stringInput이 NULL 또는 빈문자열인 경우 false를 반환한다.")
+    @DisplayName("isNonBlank(): stringInput이 NULL 또는 빈문자열인 경우 false를 그렇지 않은 경우 true를 반환한다.")
     void testIsNonBlank(String stringInput) {
         assertThat(Validator.isNonBlank(stringInput)).isEqualTo(false);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"NULL:1:false", "'':1:false", "'  ':1:false", "abc:1:false", "abc:3:true", "abc:4:true"}, delimiter = ':', nullValues = "NULL")
-    @DisplayName("isValidStringLength(): ")
+    @DisplayName("isValidStringLength(): stringInput이 NULL 또는 빈문자열 또는 stringInput의 길이가 maxStringLength 보다 큰 경우 false를 그렇지 않은 경우 true를 반환한다.")
     void testIsValidStringLength(String stringInput, int maxStringLength, boolean expected) {
         assertThat(Validator.isValidStringLength(stringInput, maxStringLength)).isEqualTo(expected);
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1:true", "10:true", "14000:true", "abc:false", "1.1:false", "NULL:false", "'':false", "'   ':false", "-1:false", "0:false", "0123:false"}, delimiter = ':', nullValues = "NULL")
-    @DisplayName("isPositiveInteger(): 양의 정수인 경우 true를 그렇지 않은 경우 false를 반환한다.")
+    @CsvSource(value = {"abc:false", "1.1:false", "NULL:false", "'':false", "'   ':false", "-1:false", "0:false", "0123:false", "1:true", "10:true", "14000:true"}, delimiter = ':', nullValues = "NULL")
+    @DisplayName("isPositiveInteger(): numberString이 양의 정수인 경우 true를 그렇지 않은 경우 false를 반환한다.")
     void testIsPositiveInteger(String numberString, boolean expected) {
         assertThat(Validator.isPositiveInteger(numberString)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:2:false", "5:2:true"}, delimiter = ':')
+    @DisplayName("isGreaterThanStandard(): number가 standard 보다 큰 경우 true를 그렇자 않은 경우 false를 반환한다.")
+    void testIsGreaterThanStandard(int number, int standard, boolean expected) {
+        assertThat(Validator.isGreaterThanStandard(number, standard)).isEqualTo(expected);
     }
 }
