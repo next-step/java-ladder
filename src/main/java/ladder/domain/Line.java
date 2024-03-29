daf7a46c10cb59b5f4999ba300ab6a5b72185093
jaeyounglee <ljyda214@gmail.com>
2024-03-30 02:34:56 +0900

    FEAT : Line 객체 생성

---
+++ src/main/java/ladder/domain/Line.java
@@ -0,0 +1,45 @@
+package ladder.domain;
+
+import ladder.Lambda;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Line {
+
+    private List<Point> points;
+
+    public Line(int countOfPerson) {
+        this(generatePoints(countOfPerson));
+  }
+
+    private static List<Point> generatePoints(int countOfPerson) {
+        Random random = new Random();
+        return Stream.iterate(new Point(random.nextBoolean()),
+                        previous -> Lambda.decidePointExist(
+                                previous,
+                                new Point(random.nextBoolean()),
+                                Point::isEqualToTrue)
+                )
+                .limit(countOfPerson - 1L)
+                .collect(Collectors.toList());
+    }
+
+    public Line(Boolean... points)  {
+        this(Arrays.stream(points).map(Point::new).collect(Collectors.toList()));
+
+    }
+
+    public Line(List<Point> points) {
+        IntStream.range(0, points.size()-1)
+                .forEach(num ->  Lambda.validatePoint(points.get(num), points.get(num + 1), Point::isEqualToTrue));
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return points.stream().map(Point::toString).collect(Collectors.joining()) + "|";
+    }
+}
