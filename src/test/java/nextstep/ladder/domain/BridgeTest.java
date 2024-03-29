e372a836c8bd4185794e836e6547a028ba13af5f
leeedohyun <ldh19738@gmail.com>
2024-03-29 19:29:09 +0900

    test: boolean 값으로 다리 생성 여부 테스트

---
+++ src/test/java/nextstep/ladder/domain/BridgeTest.java
@@ -0,0 +1,19 @@
+package nextstep.ladder.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class BridgeTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"true,BRIDGE", "false,NON_BRIDGE"})
+    @DisplayName("랜덤으로 선택된 boolean 값으로 다리 생성 여부를 판단한다")
+    void createBridge(final boolean isBridge, final Bridge expected) {
+        final Bridge bridge = Bridge.of(isBridge);
+
+        assertThat(bridge).isEqualTo(expected);
+    }
+}
