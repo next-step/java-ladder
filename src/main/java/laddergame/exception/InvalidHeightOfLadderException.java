c500d0f47b01df9f89984af954cc95d9dda83877
hong yuseok <jericoh@naver.com>
2024-03-30 00:28:36 +0900

    feat: 사다리 높이 입력 기능 구현

--- src/main/java/laddergame/exception/InvalidHeightOfLadderException.java
+++ src/main/java/laddergame/exception/InvalidHeightOfLadderException.java
@@ -4,6 +4,10 @@ public class InvalidHeightOfLadderException extends IllegalArgumentException {
     private static final String WRONG_LADDER_HEIGHT_MESSAGE = "(%s): 잘못된 사다리 높이입니다.";
 
     public InvalidHeightOfLadderException(int wrongInput) {
+        super(String.valueOf(wrongInput));
+    }
+
+    public InvalidHeightOfLadderException(String wrongInput) {
         super(String.format(WRONG_LADDER_HEIGHT_MESSAGE, wrongInput));
     }
 }
--- src/main/java/laddergame/view/InputView.java
+++ src/main/java/laddergame/view/InputView.java
@@ -1,6 +1,9 @@
 package laddergame.view;
 
+import laddergame.domain.HeightOfLadder;
 import laddergame.domain.Player;
+import laddergame.exception.InvalidHeightOfLadderException;
+import laddergame.util.Validator;
 
 import java.util.Arrays;
 import java.util.List;
@@ -9,7 +12,9 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private static final String PLAYER_INPUT_GUIDE_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String HEIGHT_OF_LADDER_INPUT_GUIDE_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     private static final String COMMA_DELIMITER = ",";
+    private static final String NEXT_LINE = System.lineSeparator();
     private static final Scanner SCANNER = new Scanner(System.in);
 
     private InputView() {
@@ -26,4 +31,22 @@ public class InputView {
                 .collect(Collectors.toList());
     }
 
+    public static HeightOfLadder enteredHeightOfLadder() {
+        String heightOfLadderGuideMessage = new StringBuilder()
+                .append(NEXT_LINE)
+                .append(HEIGHT_OF_LADDER_INPUT_GUIDE_MESSAGE)
+                .toString();
+        System.out.println(heightOfLadderGuideMessage);
+
+        String heightInput = SCANNER.nextLine();
+        validateHeightInput(heightInput);
+
+        return HeightOfLadder.valueOf(Integer.parseInt(heightInput));
+    }
+
+    private static void validateHeightInput(String heightInput) {
+        if(!Validator.isPositiveInteger(heightInput)) {
+            throw new InvalidHeightOfLadderException(heightInput);
+        }
+    }
 }
