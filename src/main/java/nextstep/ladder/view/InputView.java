b7ed28a15f1dc75a66032c104cb007535d5602db
leeedohyun <ldh19738@gmail.com>
2024-03-29 23:02:39 +0900

    feat: 실행 결과 입력 구현

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -9,11 +9,20 @@ public class InputView {
 
     public String[] inputNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        return SCANNER.nextLine().split(NAME_SPLITTER);
+        return split(SCANNER.nextLine());
     }
 
     public int inputLadderHeight() {
         System.out.println("\n최대 사다리 높이는 몇 개인가요?");
         return SCANNER.nextInt();
+    }
+
+    public String[] inputResult() {
+        System.out.println("\n실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        return split(SCANNER.next());
+    }
+
+    private String[] split(final String input) {
+        return input.split(NAME_SPLITTER);
     }
 }
