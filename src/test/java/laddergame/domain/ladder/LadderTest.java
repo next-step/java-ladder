5b3924e654a08539c0c14142d1c96462d7bae6e0
hong yuseok <jericoh@naver.com>
2024-03-30 04:35:08 +0900

    test: Ladder에 대한 테스트 구현

---
+++ src/test/java/laddergame/domain/ladder/LadderTest.java
@@ -0,0 +1,25 @@
+package laddergame.domain.ladder;
+
+import laddergame.domain.HeightOfLadder;
+import laddergame.domain.ladder.strategy.LinkStrategy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LadderTest {
+    @Test
+    @DisplayName("생성된 Ladder 인스턴스의 lines의 크기는 heightOfLadder.height과 동일하다.")
+    void testInstanceCreation() {
+        HeightOfLadder heightOfLadder = HeightOfLadder.valueOf(5);
+        int numberOfPlayers = 4;
+        LinkStrategy linkStrategyStub = () -> true;
+
+        List<Line> lines = Ladder.newLadder(heightOfLadder, numberOfPlayers, linkStrategyStub)
+                .lines();
+
+        assertThat(lines.size()).isEqualTo(heightOfLadder.height());
+    }
+}
