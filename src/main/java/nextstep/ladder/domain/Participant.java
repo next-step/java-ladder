d2532f071ad9a15f3b3bd0d024f123ef3fd3c9f7
hellonayeon <hellonykwon@gmail.com>
2024-03-29 16:13:08 +0900

    Refactor: (Feedback) 상수값을 참조하여 ParticipantTest 단위 테스트 작성 (사람 이름 글자수 제한 테스트)

--- src/main/java/nextstep/ladder/domain/Participant.java
+++ src/main/java/nextstep/ladder/domain/Participant.java
@@ -4,7 +4,7 @@ import nextstep.ladder.exception.ParticipantNameLengthExceedException;
 
 public class Participant {
 
-    private static final int MAX_NAME_LENGTH = 5;
+    public static final int MAX_NAME_LENGTH = 5;
     private final String name;
 
     public Participant(String name) {
--- src/test/java/nextstep/ladder/ParticipantTest.java
+++ src/test/java/nextstep/ladder/ParticipantTest.java
@@ -5,22 +5,24 @@ import nextstep.ladder.exception.ParticipantNameLengthExceedException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.text.MessageFormat;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class ParticipantTest {
 
     @Test
-    @DisplayName("[성공] 5글자 이하의 이름을 가지는 사람을 생성한다.")
+    @DisplayName("[성공] 사람을 생성한다.")
     void 사람_생성() {
-        assertThat((new Participant("pobi")).getName()).hasSizeLessThan(5);
+        assertThat((new Participant("pobi")).getName()).hasSizeLessThan(Participant.MAX_NAME_LENGTH);
     }
 
     @Test
-    @DisplayName("[실패] 5글자 초과의 이름을 가지는 사람을 생성하려는 경우 IllegalUserNameLengthException 예외가 발생한다.")
+    @DisplayName("[실패] 이름 글자수가 초과된 사람을 생성하려는 경우 IllegalUserNameLengthException 예외가 발생한다.")
     void 사람_생성_예외() {
         assertThatExceptionOfType(ParticipantNameLengthExceedException.class)
                 .isThrownBy(() -> {
                     new Participant("nayeon");
-                }).withMessageContaining("사람의 이름은 5글자를 초과할 수 없습니다.");
+                }).withMessageContaining(MessageFormat.format("사람의 이름은 {0}글자를 초과할 수 없습니다.", Participant.MAX_NAME_LENGTH));
     }
 }
