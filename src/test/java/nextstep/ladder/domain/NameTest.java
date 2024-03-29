9b10f646ed2899271fb02f57aff246c193ff9793
leeedohyun <ldh19738@gmail.com>
2024-03-29 19:23:19 +0900

    test: 사용자 이름 생성 테스트

--- src/test/java/nextstep/ladder/domain/NameTest.java
+++ src/test/java/nextstep/ladder/domain/NameTest.java
@@ -1,12 +1,19 @@
 package nextstep.ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class NameTest {
+
+    @Test
+    void 사용자의_이름을_생성한다() {
+        assertThatNoException().isThrownBy(() -> new Name("pobi"));
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"unicorn", "ladder"})
