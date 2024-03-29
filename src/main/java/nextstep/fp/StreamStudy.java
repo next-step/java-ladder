0e6e9c3d270188a10d52d0e25cf04947b0a43f5e
Yancy <yeeji77@naver.com>
2024-03-29 13:36:01 +0900

    [Step1] 스트림, 람다, Optional (#2068)
    * refactor: 익명 클래스를 람다로 전환
    
    * refactor: 람다를 활용해 중복 제거
    
    * refactor: 다양한 stream method 실습
    
    * refactor: Optional 실습
    
    ---------
    
    Co-authored-by: yeeji-kwon <yeji.kwon@myrealtrip.com>

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -23,11 +23,17 @@ public class StreamStudy {
     }
 
     public static void printLongestWordTop100() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+        String contents = Files.readString(Paths
+                .get("src/main/resources/fp/war-and-peace.txt"));
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+            .filter(it -> it.length() > 12)
+            .distinct()
+            .sorted(((o1, o2) -> o2.length() - o1.length()))
+            .limit(100)
+            .map(String::toLowerCase)
+            .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +45,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+            .filter(it -> it > 3)
+            .map(it -> it * 2)
+            .reduce(0, Integer::sum);
     }
 }
