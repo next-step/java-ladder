85dec7d37296bcf12b56e39b4d15bb9be7e32238
hong yuseok <jericoh@naver.com>
2024-03-30 13:54:00 +0900

    feat: Players.numberOfPlayers() 구현

--- src/main/java/laddergame/domain/Players.java
+++ src/main/java/laddergame/domain/Players.java
@@ -2,12 +2,24 @@ package laddergame.domain;
 
 import laddergame.exception.InvalidPlayersException;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Players {
     public static final int PLAYERS_MIN_NUMBER = 2;
 
     private final List<Player> players;
+
+    private Players(String... players) {
+        this(Arrays.stream(players)
+                .map(Player::valueOf)
+                .collect(Collectors.toList()));
+    }
+
+    private Players(Player... players) {
+        this(List.of(players));
+    }
 
     private Players(List<Player> players) {
         validatePlayers(players);
@@ -20,11 +32,23 @@ public class Players {
         }
     }
 
+    public static Players newPlayers(String... players) {
+        return new Players(players);
+    }
+
+    public static Players newPlayers(Player... players) {
+        return new Players(players);
+    }
+
     public static Players valueOf(List<Player> players) {
         return new Players(players);
     }
 
     public Player findPlayerByIndex(int index) {
         return players.get(index);
+    }
+
+    public int numberOfPlayers() {
+        return players.size();
     }
 }
