c98eb91f39d38dd186aae443dabfe5c3b588eef6
pado0 <bgshhd95@gmail.com>
2024-03-30 22:05:39 +0900

    feat:[practice] 람다로 중복 제거하기 - 공통부분을 람다(익명클래스 구현체)로 대체하기

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -26,27 +26,19 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers, Conditional c) {
-        int total = 0;
-        for (int number : numbers) {
-            if(c.test(number)){
-                total += number;
-            }
-        }
-        return total;
+    public static int sumAll(List<Integer> numbers) {
+        return sumAllByCondition(numbers, (number -> true));
     }
 
-    public static int sumAllEven(List<Integer> numbers, Conditional c) {
-        int total = 0;
-        for (int number : numbers) {
-            if (c.test(number)) {
-                total += number;
-            }
-        }
-        return total;
+    public static int sumAllEven(List<Integer> numbers) {
+        return sumAllByCondition(numbers, number -> number % 2 == 0 );
     }
 
-    public static int sumAllOverThree(List<Integer> numbers, Conditional c) {
+    public static int sumAllOverThree(List<Integer> numbers) {
+        return sumAllByCondition(numbers, number -> number > 3);
+    }
+
+    public static int sumAllByCondition(List<Integer> numbers, Conditional c) {
         int total = 0;
         for (int number : numbers) {
             if (c.test(number)) {
