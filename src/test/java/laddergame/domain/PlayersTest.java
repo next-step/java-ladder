8093ff93726f32bdef0e8a8b2007ef1ef35fc6cd
hong yuseok <jericoh@naver.com>
2024-03-30 13:53:29 +0900

    test: Players.numberOfPlayers() 테스트 구현

--- src/test/java/laddergame/domain/PlayersTest.java
+++ src/test/java/laddergame/domain/PlayersTest.java
@@ -19,13 +19,20 @@ class PlayersTest {
     }
 
     @Test
-    @DisplayName("findPlayerByIndex(): players 중 전달된 index 순서에 해당하는 player를 반환한다.")
+    @DisplayName("findPlayerByIndex(): Players.players 중 전달된 index 순서에 해당하는 player를 반환한다.")
     void testFindPlayerByIndex() {
         Player firstPlayer = Player.valueOf("a");
         Player secondPlayer = Player.valueOf("b");
-        Players players = Players.valueOf(List.of(firstPlayer, secondPlayer));
+        Players players = Players.newPlayers(firstPlayer, secondPlayer);
 
         assertThat(players.findPlayerByIndex(0)).isEqualTo(firstPlayer);
         assertThat(players.findPlayerByIndex(1)).isEqualTo(secondPlayer);
+    }
+
+    @Test
+    @DisplayName("numberOfPlayers(): Players.players의 크기를 반환한다.")
+    void testNumberOfPlayers() {
+        Players players = Players.newPlayers("a", "b", "c");
+        assertThat(players.numberOfPlayers()).isEqualTo(3);
     }
 }
