92f120a02403ead823a81b526ce6cc2fafda962b
namhyeop <skaguq@gmail.com>
2024-04-01 02:25:27 +0900

    feat : User 구현

---
+++ src/main/java/nextstep/ladder/domain/user/User.java
@@ -0,0 +1,20 @@
+package nextstep.ladder.domain.user;
+
+import nextstep.ladder.error.exception.UserNameEmptyException;
+
+public class User {
+
+    private String userName;
+
+    public User(String userName) {
+        if (userName == null || userName.isEmpty() || userName.isBlank()){
+            throw new UserNameEmptyException(userName);
+        }
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%6s", userName);
+    }
+}
