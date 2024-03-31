dbd76ebbadb219836865902f0ecb1926c28713eb
namhyeop <skaguq@gmail.com>
2024-04-01 02:26:20 +0900

    feat : Line 구현

---
+++ src/main/java/nextstep/ladder/domain/line/Line.java
@@ -0,0 +1,36 @@
+package nextstep.ladder.domain.line;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Line {
+
+    private List<Boolean> points = new ArrayList<>();
+
+    private final Random random = new Random();
+
+    public Line(int countOfPerson) {
+        boolean before = false;
+        for (int i = 0; i < countOfPerson - 1; i++) {
+            before = addLineValue(before);
+        }
+    }
+
+    private boolean addLineValue(boolean before) {
+        if (before == false) {
+            boolean currentState = random.nextBoolean();
+            points.add(currentState);
+            return currentState;
+        }
+        return points.add(false);
+    }
+
+    public List<Boolean> getPoints() {
+        return points;
+    }
+
+    public int size() {
+        return points.size();
+    }
+}
