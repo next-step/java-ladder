c647112f4ad3259deb968b260f0ff2b4972db7b2
kwanik-kor <rhksdlr134@naver.com>
2024-03-31 11:02:03 +0900

    feat: 참가자 유효성검증 로직 추가

--- src/main/java/nextstep/ladder/domain/player/Players.java
+++ src/main/java/nextstep/ladder/domain/player/Players.java
@@ -15,9 +15,29 @@ public class Players {
     }
 
     public Players(List<Player> players) {
-        if (players != null && !players.isEmpty()) {
-            this.players.addAll(players);
+        assertPlayersNotEmpty(players);
+        assertPlayerNamesNotDuplicated(players);
+
+        this.players.addAll(players);
+    }
+
+    private void assertPlayersNotEmpty(List<Player> players) {
+        if (players == null || players.isEmpty()) {
+            throw new IllegalArgumentException("참가자 이름을 입력해주세요.");
         }
+    }
+
+    private void assertPlayerNamesNotDuplicated(List<Player> players) {
+        if (players.size() != distinctNameSize(players)) {
+            throw new IllegalArgumentException("참가자 이름은 중복될 수 없습니다.");
+        }
+    }
+
+    private int distinctNameSize(List<Player> players) {
+        return (int) players.stream()
+                .map(Player::name)
+                .distinct()
+                .count();
     }
 
     public Count count() {
--- src/test/java/nextstep/ladder/domain/player/PlayersTest.java
+++ src/test/java/nextstep/ladder/domain/player/PlayersTest.java
@@ -1,22 +1,45 @@
 package nextstep.ladder.domain.player;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.*;
 
 public class PlayersTest {
 
-    @DisplayName("Players는 참여할 사람 이름 배열로 생성할 수 있다.")
-    @Test
-    void constructor() {
-        final String[] names = new String[]{"pobi", "honux", "crong", "jk"};
-        assertThatCode(() -> new Players(names))
-                .doesNotThrowAnyException();
+    @DisplayName("Players는")
+    @Nested
+    class Describe_constructor {
+
+        @DisplayName("참여할 사람 이름 배열로 생성할 수 있다.")
+        @Test
+        void constructor() {
+            final String[] names = new String[]{"pobi", "honux", "crong", "jk"};
+            assertThatCode(() -> new Players(names))
+                    .doesNotThrowAnyException();
+        }
+
+        @DisplayName("참가할 사람을 반드시 필요로 한다.")
+        @Test
+        void requires_player_names() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> new Players(Collections.emptyList()))
+                    .withMessage("참가자 이름을 입력해주세요.");
+        }
+
+        @DisplayName("중복된 참가자 이름을 허용하지 않는다.")
+        @Test
+        void requires_not_duplicated_names() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> new Players(new String[]{"pobi", "pobi"}))
+                    .withMessage("참가자 이름은 중복될 수 없습니다.");
+        }
+
     }
+
 
     @DisplayName("count 메서드는 참가자 수를 반환한다.")
     @Test
@@ -29,8 +52,8 @@ public class PlayersTest {
     @DisplayName("maxNameLength는 참가자 이름 중 가장 긴 이름의 길이를 반환한다.")
     @Test
     void maxNameLength() {
-        assertThat(new Players(Collections.emptyList()).maxNameLength())
-                .isEqualTo(new Count(0));
+        assertThat(new Players(new String[]{"pobi", "honux", "a", "ab"}).maxNameLength())
+                .isEqualTo(new Count(5));
 
     }
 
