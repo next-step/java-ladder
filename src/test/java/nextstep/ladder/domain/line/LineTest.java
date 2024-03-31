93a03686b41512b0cd2bdd3454d85708a18447e2
namhyeop <skaguq@gmail.com>
2024-04-01 02:49:46 +0900

    feat : Line 테스트 코드 수정

--- src/test/java/nextstep/ladder/domain/line/LineTest.java
+++ src/test/java/nextstep/ladder/domain/line/LineTest.java
@@ -7,10 +7,10 @@ import org.junit.jupiter.api.Test;
 class LineTest {
 
     @Test
-    public void 한_라인은_사람의_수만큼_생성되어야_한다 (){
+    public void 한_라인은_사람의_수보다_하나_작게_생성되어야_한다() {
         int countOfPerson = 6;
         Line line = new Line(countOfPerson);
 
-        assertThat(line.size()).isEqualTo(countOfPerson);
+        assertThat(line.size()).isEqualTo(countOfPerson - 1);
     }
 }
