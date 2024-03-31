0cbd9e87d23ca4743004505b7bc40eeeebd1621b
hellonayeon <hellonykwon@gmail.com>
2024-04-01 00:17:39 +0900

    Test: LadderGame 실행 단위 테스트 작성

--- src/test/java/nextstep/ladder/LadderGameTest.java
+++ src/test/java/nextstep/ladder/LadderGameTest.java
@@ -2,28 +2,37 @@ package nextstep.ladder;
 
 import nextstep.ladder.domain.LadderGame;
 import nextstep.ladder.domain.Line;
-import org.assertj.core.api.Assertions;
+import nextstep.ladder.domain.ParticipantPosition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LadderGameTest {
 
-    @Test
-    @DisplayName("[성공] 사다리 게임을 생성한다.")
-    void 사다리_게임_생성() {
-        LadderGame ladder = LadderGame.of(List.of("pobi", "honux", "crong", "ny"),
-                List.of(
+    public static final LadderGame LADDER_GAME = LadderGame.of(List.of("pobi", "honux", "crong", "ny"),
+            List.of(
                     Line.of(true, false, true),
                     Line.of(false, true, false),
                     Line.of(true, false, false),
                     Line.of(false, true, false),
                     Line.of(true, false, true)
-                )
-        );
+            )
+    );
 
-        Assertions.assertThat(ladder.getParticipantsSize()).isEqualTo(4);
-        Assertions.assertThat(ladder.getLadderHeight()).isEqualTo(5);
+    @Test
+    @DisplayName("[성공] 사다리 게임을 생성한다.")
+    void 사다리_게임_생성() {
+        assertThat(LADDER_GAME.getParticipantsSize()).isEqualTo(4);
+        assertThat(LADDER_GAME.getLadderHeight()).isEqualTo(5);
     }
+
+    @Test
+    @DisplayName("[성공] 사다리 게임을 실행한다.")
+    void 사다리_게임_실행() {
+        assertThat(LADDER_GAME.execute("pobi")).containsOnly(ParticipantPosition.of("pobi", 0));
+    }
+
 }
