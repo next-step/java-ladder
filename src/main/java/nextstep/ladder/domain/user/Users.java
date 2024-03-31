dbc085f467f3259b0e81d124d3244835ce81c4ae
namhyeop <skaguq@gmail.com>
2024-04-01 02:25:37 +0900

    feat : Users 구현

---
+++ src/main/java/nextstep/ladder/domain/user/Users.java
@@ -0,0 +1,42 @@
+package nextstep.ladder.domain.user;
+
+import java.util.ArrayList;
+import java.util.List;
+import nextstep.ladder.error.exception.UserSizeEmptyException;
+
+public class Users {
+
+    private List<User> users;
+
+    public Users(String... userNames){
+        this(toUsers(userNames));
+    }
+
+    private static List<User> toUsers(String... userNames) {
+        List<User> users = new ArrayList<>();
+        for (String userName : userNames){
+            users.add(new User(userName));
+        }
+        return users;
+    }
+
+    public Users(List<User> users) {
+        if(users.size() == 0){
+            throw new UserSizeEmptyException(users);
+        }
+        this.users = users;
+    }
+
+    public int size(){
+        return users.size();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (User user : users) {
+            sb.append(user.toString());
+        }
+        return sb.toString();
+    }
+}
