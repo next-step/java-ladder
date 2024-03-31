a0d7a17a2562e586166f40920aa0fa0c349a0920
namhyeop <skaguq@gmail.com>
2024-04-01 02:49:23 +0900

    feat : Lines 예외 발생 테스트 코드 추가

--- src/test/java/nextstep/ladder/domain/line/LinesTest.java
+++ src/test/java/nextstep/ladder/domain/line/LinesTest.java
@@ -1,7 +1,12 @@
 package nextstep.ladder.domain.line;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
+import nextstep.ladder.domain.user.Users;
+import nextstep.ladder.error.exception.LadderHeightSizeException;
+import nextstep.ladder.error.exception.UserSizeEmptyException;
 import org.junit.jupiter.api.Test;
 
 class LinesTest {
@@ -13,5 +18,12 @@ class LinesTest {
         Lines lines = new Lines(height, countOfPerson);
 
         assertThat(lines.size()).isEqualTo(5);
+    }
+
+    @Test
+    public void 사다리_높이가_1보다_작을_경우_예외가_발생해야_한다 (){
+        assertThatThrownBy(() -> new Lines(0, 3 ))
+            .isInstanceOf(LadderHeightSizeException.class)
+            .hasMessage("사다리의 높이는 0보다 커야 합니다 입력값: 0");
     }
 }
