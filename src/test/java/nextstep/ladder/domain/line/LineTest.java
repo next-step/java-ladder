f44c1909cf5328b44579cf5f78e864d352d9b423
namhyeop <skaguq@gmail.com>
2024-04-01 02:25:59 +0900

    feat : Line 테스트 코드 구현

---
+++ src/test/java/nextstep/ladder/domain/line/LineTest.java
@@ -0,0 +1,16 @@
+package nextstep.ladder.domain.line;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class LineTest {
+
+    @Test
+    public void 한_라인은_사람의_수만큼_생성되어야_한다 (){
+        int countOfPerson = 6;
+        Line line = new Line(countOfPerson);
+
+        assertThat(line.size()).isEqualTo(countOfPerson);
+    }
+}
