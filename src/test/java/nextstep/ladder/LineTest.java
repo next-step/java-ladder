aeaa1eb187b064201ac3e2962c87296b1a55b435
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-04-02 01:59:09 +0900

    Style: 테스트코드를 읽기 쉽게 포매팅하라

--- src/test/java/nextstep/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -11,17 +11,17 @@ public class LineTest {
     @DisplayName("0명이거나 음수를 입력했을 때, IllegalArgumentException을 반환한다 ")
     @Test
     void zeroOrNegative() {
-        assertThatThrownBy(() -> {
-            new Line(0, true);
-        }).isInstanceOf(IllegalArgumentException.class)
+        assertThatThrownBy(() -> new Line(0, true))
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("참가자는 1명 이상이어야 합니다.");
     }
 
     @DisplayName("Line 객체를 초기화했을 때, 앞 부분이 가로선이 생겼다면 그 다음 부분은 공백이 된다")
     @Test
     void horizonRule() {
-        assertThat(new Line(3, true).getPoints().get(4) //true
-                .equals(new Line(3, false).getPoints().get(5))).isFalse(); //false
+        assertThat(new Line(3, true).getPoints().get(4)
+                .equals(new Line(3, false).getPoints().get(5)))
+                .isFalse();
 
     }
 
