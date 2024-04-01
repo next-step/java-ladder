c81bd7fb77ca3cad35ab8d0d7e8a9d9045caba88
kylekim2123 <kyle2123@naver.com>
2024-04-01 21:36:16 +0900

    [STEP 2] feat: 플레이어 객체 equals, hashCode 재정의 및 이름 제한 길이 예외 로직 추가

--- src/main/java/ladder/Player.java
+++ src/main/java/ladder/Player.java
@@ -1,11 +1,17 @@
 package ladder;
 
+import java.text.MessageFormat;
+import java.util.Objects;
+
 public class Player {
+
+    static final int MAXIMUM_NAME_LENGTH = 5;
 
     private final String name;
 
     public Player(final String name) {
         validateNameIsNotNullOrBlank(name);
+        validateNameNotExceedMaxLength(name);
 
         this.name = name;
     }
@@ -14,5 +20,32 @@ public class Player {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("플레이어의 이름은 null이거나 비어있을 수 없습니다. 이름: " + name);
         }
+    }
+
+    private void validateNameNotExceedMaxLength(String name) {
+        if (name.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException(MessageFormat.format(
+                    "플레이어의 이름은 {0}자를 초과할 수 없습니다. 이름 : {1}", MAXIMUM_NAME_LENGTH, name));
+        }
+    }
+
+    @Override
+    public boolean equals(final Object otherPlayer) {
+        if (this == otherPlayer) {
+            return true;
+        }
+
+        if (otherPlayer == null || getClass() != otherPlayer.getClass()) {
+            return false;
+        }
+
+        final Player player = (Player)otherPlayer;
+
+        return Objects.equals(this.name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.name);
     }
 }
