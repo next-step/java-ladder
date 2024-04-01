ba7480008020457ce50c040f0d63cecc1f23e749
jaeyounglee <ljyda214@gmail.com>
2024-04-02 01:20:09 +0900

    FEAT : 해당 Point 에서 움직일 수 있는 여부 판단하는 메소드 구현

--- src/main/java/ladder/domain/Point.java
+++ src/main/java/ladder/domain/Point.java
@@ -22,6 +22,10 @@ public class Point {
         return isExist;
     }
 
+    public boolean moveHorizontal() {
+        return this.isExist;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
