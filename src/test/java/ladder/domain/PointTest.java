1eb61af7010c7a2ae01a1bfb8be5c966a072400e
jaeyounglee <ljyda214@gmail.com>
2024-04-02 01:29:11 +0900

    STYLE : import 수정

--- src/test/java/ladder/domain/PointTest.java
+++ src/test/java/ladder/domain/PointTest.java
@@ -1,9 +1,9 @@
 package ladder.domain;
 
+
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class PointTest {
 
