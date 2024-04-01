e95242d3181ff82c3fa6107f508858d5025c84bb
kylekim2123 <kyle2123@naver.com>
2024-04-01 19:56:35 +0900

    [STEP 1] refactor: 람다 실습 1 - 익명 클래스를 람다로 전환

--- src/test/java/nextstep/fp/CarTest.java
+++ src/test/java/nextstep/fp/CarTest.java
@@ -1,31 +1,24 @@
 package nextstep.fp;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CarTest {
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
     @Test
-    public void 이동() {
-        Car car = new Car("pobi", 0);
-        Car actual = car.move(new MoveStrategy() {
-            @Override
-            public boolean isMovable() {
-                return true;
-            }
-        });
+    void 이동() {
+        final Car car = new Car("pobi", 0);
+        final Car actual = car.move(() -> true);
+
         assertThat(actual).isEqualTo(new Car("pobi", 1));
     }
 
     @Test
-    public void 정지() {
-        Car car = new Car("pobi", 0);
-        Car actual = car.move(new MoveStrategy() {
-            @Override
-            public boolean isMovable() {
-                return false;
-            }
-        });
+    void 정지() {
+        final Car car = new Car("pobi", 0);
+        final Car actual = car.move(() -> false);
+
         assertThat(actual).isEqualTo(new Car("pobi", 0));
     }
 }
