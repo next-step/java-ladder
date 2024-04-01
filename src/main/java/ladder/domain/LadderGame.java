651d024fa731b9b379dbac6f2fa43a71a390eb80
jaeyounglee <ljyda214@gmail.com>
2024-04-02 01:36:35 +0900

    FEAT : 타입 이름 변경 반영

--- src/main/java/ladder/domain/LadderGame.java
+++ src/main/java/ladder/domain/LadderGame.java
@@ -1,33 +1,34 @@
 package ladder.domain;
 
+import java.util.List;
+
 public class LadderGame {
 
     private final Ladder ladder;
-    private final Participant participant;
+    private final Participants participants;
 
     public LadderGame(int height, String inputText) {
-        this(new Ladder(height, getLengthOfLadder(inputText)), new Participant(inputText) );
+        this(new Ladder(height, getLengthOfLadder(inputText)), new Participants(inputText) );
     }
 
     private static int getLengthOfLadder(String inputText) {
         return inputText.split(",").length;
     }
 
-    public LadderGame(Ladder ladder, Participant participant) {
+    public LadderGame(Ladder ladder, Participants participants) {
         this.ladder = ladder;
-        this.participant = participant;
+        this.participants = participants;
+    }
+
+    public List<Gamer> doStart() {
+        return this.participants.startGame(ladder);
     }
 
     public Ladder getLadder() {
         return ladder;
     }
 
-    public Participant getParticipant() {
-        return participant;
-    }
-
-    @Override
-    public String toString() {
-        return participant.toString() + "\n" + ladder.toString();
+    public Participants getParticipant() {
+        return participants;
     }
 }
