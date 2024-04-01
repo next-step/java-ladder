d08dc43519e684f5e30172d68b260fcd8ae4c8c5
danalee <eleeje97@gmail.com>
2024-04-01 23:29:24 +0900

    fix: step1 리뷰 반영

--- src/main/java/nextstep/optional/Expression.java
+++ src/main/java/nextstep/optional/Expression.java
@@ -17,7 +17,7 @@ enum Expression {
 
     static Expression of(String expression) {
         return Stream.of(values())
-                .filter(v -> matchExpression(v, expression))
+                .filter(value -> matchExpression(value, expression))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression)));
     }
