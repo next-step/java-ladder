dc9a7be08678ac454b2a21842b45dec695e8fb96
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-04-02 01:41:33 +0900

    Test: 0명이거나 음수를 입력했을 때, IllegalArgumentException을 반환한다

--- src/main/java/nextstep/ladder/Line.java
+++ src/main/java/nextstep/ladder/Line.java
@@ -8,6 +8,10 @@ public class Line {
     private List<Boolean> points = new ArrayList<>();
 
     public Line(int countOfPerson, boolean hasLadder) { //참가자수
+        if (countOfPerson < 1) {
+            throw new IllegalArgumentException("참가자는 1명 이상이어야 합니다.");
+        }
+
         for (int i = 0; i < countOfPerson - 1; i++) {
             //hasLadder = random.nextBoolean();
             for (int j = 0; j < 5; j++) {
--- src/test/java/nextstep/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -4,8 +4,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LineTest {
+
+    @DisplayName("0명이거나 음수를 입력했을 때, IllegalArgumentException을 반환한다 ")
+    @Test
+    void zeroOrNegative() {
+        assertThatThrownBy(() -> {
+            new Line(0, true);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("참가자는 1명 이상이어야 합니다.");
+    }
 
     @DisplayName("Line 객체를 초기화했을 때, 앞 부분이 가로선이 생겼다면 그 다음 부분은 공백이 된다")
     @Test
