e6074130f0c7610df49be9b81973eb818b52f5cb
이승호 <seung7642@gmail.com>
2024-04-01 21:12:52 +0900

    refactor: 객체 생성 한 번만 하기.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,9 +10,10 @@ import java.util.stream.Collectors;
 
 public class InputView {
 
+    public static final Scanner scanner = new Scanner(System.in);
+
     public static Players promptForPlayer() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         System.out.println();
         return new Players(Arrays.stream(input.split(","))
@@ -22,7 +23,6 @@ public class InputView {
 
     public static Height promptForHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        Scanner scanner = new Scanner(System.in);
         int height = scanner.nextInt();
         System.out.println();
         return new Height(height);
