96ab21141884237e621a88f8d4d07aee0be9c7f8
hong yuseok <jericoh@naver.com>
2024-04-03 01:50:02 +0900

    feat: ResultOfLadder 구현

---
+++ src/main/java/laddergame/domain/ResultOfLadder.java
@@ -0,0 +1,19 @@
+package laddergame.domain;
+
+import java.util.List;
+
+public class ResultOfLadder {
+    private final List<WinningContent> results;
+
+    private ResultOfLadder(List<WinningContent> results) {
+        this.results = results;
+    }
+
+    public static ResultOfLadder valueOf(List<WinningContent> results) {
+        return new ResultOfLadder(results);
+    }
+
+    public WinningContent findWinningContentByIndex(int index) {
+        return results.get(index);
+    }
+}
