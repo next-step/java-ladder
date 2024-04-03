47a5710d0c0d883e652a39c17c189a65d5e3ddf4
hellonayeon <hellonykwon@gmail.com>
2024-04-03 22:12:05 +0900

    Test: 왼쪽, 오른쪽 모두 이동 가능한 포인트를 생성하는 경우 실패 단위 테스트 작성

--- src/test/java/nextstep/ladder/PointTest.java
+++ src/test/java/nextstep/ladder/PointTest.java
@@ -2,10 +2,12 @@ package nextstep.ladder;
 
 import nextstep.ladder.domain.Direction;
 import nextstep.ladder.domain.Point;
+import nextstep.ladder.exception.ConsecutivePointException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class PointTest {
 
@@ -37,4 +39,11 @@ public class PointTest {
         assertThat(point.move()).isEqualTo(Direction.LEFT);
     }
 
+    @Test
+    @DisplayName("[실패] 왼쪽, 오른쪽 모두 이동 가능한 포인트 생성 시 ConsecutivePointException 예외가 발생한다.")
+    void 연속_포인트() {
+        assertThatExceptionOfType(ConsecutivePointException.class)
+                .isThrownBy(() -> Point.middle(true, true))
+                .withMessageContaining("왼쪽과 오른쪽으로 모두 이동 가능한 포인트는 생성할 수 없습니다.");
+    }
 }
