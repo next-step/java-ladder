5282e301791c35b31be88bc142a7197c6ebe14f9
hellonayeon <hellonykwon@gmail.com>
2024-04-04 00:10:34 +0900

    Revert "Delete: LadderTest, LineTest"
    This reverts commit ac91ca0ddb2ff8b1d6c1c37a9d2636e124896646.

---
+++ src/test/java/nextstep/ladder/LadderTest.java
@@ -0,0 +1,26 @@
+package nextstep.ladder;
+
+import nextstep.ladder.domain.Ladder;
+import nextstep.ladder.domain.Line;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class LadderTest {
+
+    @Test
+    @DisplayName("[성공] 사다리를 생성한다.")
+    void 사다리_생성() {
+        Ladder ladder = Ladder.of(List.of(
+                Line.of(true, false, true),
+                Line.of(false, true, false),
+                Line.of(true, false, false),
+                Line.of(false, true, false),
+                Line.of(true, false, true)
+        ));
+        Assertions.assertThat(ladder.getHeight()).isEqualTo(5);
+    }
+
+}
