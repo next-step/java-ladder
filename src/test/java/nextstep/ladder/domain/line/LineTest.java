e496d6068274c9b4a22e7b96c3d8ab072a276e61
namhyeop <skaguq@gmail.com>
2024-04-03 22:33:07 +0900

    feat : 테스트 코드 접근제어자 제거

--- src/test/java/nextstep/ladder/domain/line/LineTest.java
+++ src/test/java/nextstep/ladder/domain/line/LineTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class LineTest {
 
     @Test
-    public void 한_라인은_사람의_수보다_하나_작게_생성되어야_한다() {
+    void 한_라인은_사람의_수보다_하나_작게_생성되어야_한다() {
         int countOfPerson = 6;
         Line line = new Line(countOfPerson);
 
--- src/test/java/nextstep/ladder/domain/line/LinesTest.java
+++ src/test/java/nextstep/ladder/domain/line/LinesTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 class LinesTest {
 
     @Test
-    public void 높이와_사람의_수가_전달될때_높이와_사람의_수만큼_라인들이_생겨야_한다() {
+    void 높이와_사람의_수가_전달될때_높이와_사람의_수만큼_라인들이_생겨야_한다() {
         int height = 5;
         int countOfPerson = 4;
         Lines lines = new Lines(height, countOfPerson);
@@ -18,7 +18,7 @@ class LinesTest {
     }
 
     @Test
-    public void 사다리_높이가_1보다_작을_경우_예외가_발생해야_한다() {
+    void 사다리_높이가_1보다_작을_경우_예외가_발생해야_한다() {
         assertThatThrownBy(() -> new Lines(0, 3))
             .isInstanceOf(LadderHeightSizeException.class)
             .hasMessage("사다리의 높이는 0보다 커야 합니다 입력값: 0");
