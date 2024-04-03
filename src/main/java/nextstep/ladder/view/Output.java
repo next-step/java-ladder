6e22f500cabc21861775ddf0f892ad2e15657f85
namhyeop <skaguq@gmail.com>
2024-04-04 01:30:15 +0900

    refactor : Output indent 리팩토링

--- src/main/java/nextstep/ladder/view/Output.java
+++ src/main/java/nextstep/ladder/view/Output.java
@@ -7,24 +7,39 @@ import nextstep.ladder.domain.user.Users;
 
 public class Output {
 
+    private static final String HORIZONTAL_LINE = "-----|";
+    private static final String EMPTY_SPACE = "     |";
+    private static final String VERTICAL_LINE = "|";
+    private static final String RESULT_HEADER = "실행결과";
+
     private Output() {
     }
 
     public static void printResult(Users users, Lines lines) {
-        System.out.println("실행결과");
+        System.out.println(RESULT_HEADER);
+        printUserNames(users);
+        printLadderLines(lines);
+    }
+
+    private static void printUserNames(Users users) {
         users.getUsers()
             .forEach(user -> System.out.print(String.format("%6s", user.getUserName())));
         System.out.println();
+    }
+
+    private static void printLadderLines(Lines lines) {
         for (Line line : lines.getLines()) {
-            System.out.print(String.format("%6s", "|"));
-            for (LadderConstructionStatus status : line.getPoints()) {
-                if (!status.isLadderConstructionStatusFalse()) {
-                    System.out.print(String.format("%6s", "-----|"));
-                } else {
-                    System.out.print(String.format("%6s", "     |"));
-                }
-            }
+            System.out.print(String.format("%6s", VERTICAL_LINE));
+            printLineStatus(line);
             System.out.println();
+        }
+    }
+
+    private static void printLineStatus(Line line) {
+        for (LadderConstructionStatus status : line.getPoints()) {
+            System.out.print(String.format("%6s", status.isLadderConstructionStatusFalse()
+                ? EMPTY_SPACE
+                : HORIZONTAL_LINE));
         }
     }
 }
