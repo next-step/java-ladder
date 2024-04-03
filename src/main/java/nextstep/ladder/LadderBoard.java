86d5e82d317be43fdd8d61eb360985e1fe74333c
kimsunghyun <tjdgus2319@gmail.com>
2024-04-03 21:53:30 +0900

    feat: 라인을 List로 가지고있는 LadderBoard 기능 구현

--- src/main/java/nextstep/ladder/LadderBoard.java
+++ src/main/java/nextstep/ladder/LadderBoard.java
@@ -1,17 +1,24 @@
 package nextstep.ladder;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class LadderBoard {
-    private List<Line> ladderBoard;
+    private List<Line> ladderBoard = new ArrayList<>();
 
     public LadderBoard(List<Line> ladderBoard) {
         this.ladderBoard = ladderBoard;
     }
 
+    public LadderBoard(int countOfHeight, int countOfPerson) {
+        for (int i = 0; i < countOfHeight; i++) {
+            ladderBoard.add(new Line(countOfPerson));
+        }
+    }
+
     public List<Line> getLadderBoard() {
-        return ladderBoard;
+        return this.ladderBoard;
     }
 
 }
