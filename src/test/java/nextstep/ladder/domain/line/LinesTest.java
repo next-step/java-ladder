470b9cea1d0c251d4031f8eef5916ae41d1b9dbd
namhyeop <skaguq@gmail.com>
2024-04-03 22:32:03 +0900

    feat : 불필요한 import문 제거

--- src/test/java/nextstep/ladder/domain/line/LinesTest.java
+++ src/test/java/nextstep/ladder/domain/line/LinesTest.java
@@ -3,16 +3,13 @@ package nextstep.ladder.domain.line;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.List;
-import nextstep.ladder.domain.user.Users;
 import nextstep.ladder.error.exception.LadderHeightSizeException;
-import nextstep.ladder.error.exception.UserSizeEmptyException;
 import org.junit.jupiter.api.Test;
 
 class LinesTest {
 
     @Test
-    public void 높이와_사람의_수가_전달될때_높이와_사람의_수만큼_라인들이_생겨야_한다 (){
+    public void 높이와_사람의_수가_전달될때_높이와_사람의_수만큼_라인들이_생겨야_한다() {
         int height = 5;
         int countOfPerson = 4;
         Lines lines = new Lines(height, countOfPerson);
@@ -21,8 +18,8 @@ class LinesTest {
     }
 
     @Test
-    public void 사다리_높이가_1보다_작을_경우_예외가_발생해야_한다 (){
-        assertThatThrownBy(() -> new Lines(0, 3 ))
+    public void 사다리_높이가_1보다_작을_경우_예외가_발생해야_한다() {
+        assertThatThrownBy(() -> new Lines(0, 3))
             .isInstanceOf(LadderHeightSizeException.class)
             .hasMessage("사다리의 높이는 0보다 커야 합니다 입력값: 0");
     }
