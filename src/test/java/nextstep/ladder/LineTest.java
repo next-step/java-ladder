77bf5b8a949ae5897cbbaf411290b5baaedc2c0d
kimsunghyun <tjdgus2319@gmail.com>
2024-04-03 21:20:33 +0900

    test: 라인에 전략패턴 적용 테스트

--- src/test/java/nextstep/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LineTest {
+
     @Test
     @DisplayName("라인이 겹칠 때 생성 불가 테스트")
     void lineOverLabTest() {
@@ -23,6 +25,22 @@ public class LineTest {
     void lineConstructTest() {
         Line line = new Line(List.of(false,true,false));
         assertThat(line.getPoints().get(1)).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("라인 그리기 생성 성공 테스트")
+    void lineSuccessTest() {
+        Line line = new Line(new ArrayList<>());
+        line.drawLine(() -> true);
+        assertThat(line.getPoints().get(0)).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("라인 그리기 실패 성공 테스트")
+    void lineFailTest() {
+        Line line = new Line(new ArrayList<>());
+        line.drawLine(() -> false);
+        assertThat(line.getPoints().get(0)).isEqualTo(false);
     }
 
 }
