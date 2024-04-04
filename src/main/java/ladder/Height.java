7d7317d3eb465f9e8f10c7ba9c13734e2ba73a7a
kylekim2123 <kyle2123@naver.com>
2024-04-04 19:23:10 +0900

    [STEP 2] test: PlayersCount, Height 정상 생성 테스트 코드 추가

--- src/main/java/ladder/Height.java
+++ src/main/java/ladder/Height.java
@@ -12,6 +12,10 @@ public class Height {
         this.value = value;
     }
 
+    public int value() {
+        return this.value;
+    }
+
     private void validateHeightIsNotLessThanMinimum(final int height) {
         if (height < MINIMUM_HEIGHT) {
             throw new IllegalArgumentException("사다리는 최소 높이보다 작을 수 없습니다. 높이: " + height);
--- src/test/java/ladder/HeightTest.java
+++ src/test/java/ladder/HeightTest.java
@@ -1,12 +1,22 @@
 package ladder;
 
 import static ladder.Height.MINIMUM_HEIGHT;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class HeightTest {
+
+    @Test
+    @DisplayName("사다리 높이를 생성한다.")
+    void Height() {
+        final Height height = new Height(MINIMUM_HEIGHT);
+
+        assertThat(height.value())
+                .isEqualTo(MINIMUM_HEIGHT);
+    }
 
     @Test
     @DisplayName("사다리 높이가 최소 높이 미만인 경우 예외를 던진다.")
