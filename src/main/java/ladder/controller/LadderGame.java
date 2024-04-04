22232525d92a28503947298da028d517eb1fcedb
kylekim2123 <kyle2123@naver.com>
2024-04-04 21:02:04 +0900

    [STEP 2] feat: Ladder Application과 컨트롤러 추가

---
+++ src/main/java/ladder/controller/LadderGame.java
@@ -0,0 +1,36 @@
+package ladder.controller;
+
+import java.util.List;
+
+import ladder.domain.ladder.Ladder;
+import ladder.domain.line.ConnectionGenerator;
+import ladder.domain.player.Players;
+import ladder.view.LadderInputView;
+import ladder.view.LadderOutputView;
+
+public class LadderGame {
+
+    private final LadderInputView inputView;
+    private final LadderOutputView outputView;
+    private final ConnectionGenerator connectionGenerator;
+
+    public LadderGame(
+            final LadderInputView inputView,
+            final LadderOutputView outputView,
+            final ConnectionGenerator connectionGenerator
+    ) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.connectionGenerator = connectionGenerator;
+    }
+
+    public void run() {
+        final List<String> playerNames = inputView.readPlayerNames();
+        final Players players = Players.from(playerNames);
+
+        int height = inputView.readLadderHeight();
+        final Ladder ladder = Ladder.from(players.playersCount(), height, connectionGenerator);
+
+        outputView.printResult(players, ladder);
+    }
+}
