5fe05f47561e57097f10036b87e9585d20a4aaf5
kylekim2123 <kyle2123@naver.com>
2024-04-04 19:04:23 +0900

    [STEP 2] feat: 플레이어 수가 최소 인원보다 적은 경우의 예외 로직 추가

--- src/main/java/ladder/PlayersCount.java
+++ src/main/java/ladder/PlayersCount.java
@@ -7,6 +7,14 @@ public class PlayersCount {
     private final int value;
 
     public PlayersCount(final int value) {
+        validatePlayersCountIsNotLessThanMinimum(value);
+
         this.value = value;
+    }
+
+    private void validatePlayersCountIsNotLessThanMinimum(final int playersCount) {
+        if (playersCount < MINIMUM_PLAYERS_COUNT) {
+            throw new IllegalArgumentException("플레이어 수는 최소 인원 이상이어야 합니다. 플레이어 수: " + playersCount);
+        }
     }
 }
