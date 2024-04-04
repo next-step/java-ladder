95a57c1cc2fda448913bf88c46958336520f032e
hellonayeon <hellonykwon@gmail.com>
2024-04-04 21:12:03 +0900

    Refactor: (Feedback) Line 팩토리 메서드 제거, 기본 생성자 public 으로 수정

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -7,11 +7,7 @@ public class Line {
 
     private final List<Point> points;
 
-    public static Line of(List<Point> points) {
-        return new Line(points);
-    }
-
-    private Line(List<Point> points) {
+    public Line(List<Point> points) {
         this.points = points;
     }
 
