ed3f42652c9d8e670666109b653804dee37db6dd
kylekim2123 <kyle2123@naver.com>
2024-04-04 19:38:02 +0900

    [STEP 2] feat: Ladder 정상 생성 로직 추가

---
+++ src/main/java/ladder/Ladder.java
@@ -0,0 +1,45 @@
+package ladder;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Ladder {
+
+    private final List<Line> lines;
+
+    Ladder(final List<Line> lines) {
+        this.lines = lines;
+    }
+
+    public static Ladder from(
+            final PlayersCount playersCount,
+            final Height height,
+            final ConnectionGenerator connectionGenerator
+    ) {
+        final List<Line> lines = Stream.generate(() -> Line.from(playersCount, connectionGenerator))
+                .limit(height.value())
+                .collect(Collectors.toUnmodifiableList());
+
+        return new Ladder(lines);
+    }
+
+    @Override
+    public boolean equals(final Object otherLadder) {
+        if (this == otherLadder) {
+            return true;
+        }
+
+        if (otherLadder == null || getClass() != otherLadder.getClass()) {
+            return false;
+        }
+
+        return Objects.equals(this.lines, ((Ladder)otherLadder).lines);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.lines);
+    }
+}
