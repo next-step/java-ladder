b71d73640733a0633b03018607ee143f26e89015
kylekim2123 <kyle2123@naver.com>
2024-04-04 19:11:12 +0900

    [STEP 2] refactor: Line 내에 equals, hashCode를 재정의하고 테스트 코드 변경

--- src/main/java/ladder/Line.java
+++ src/main/java/ladder/Line.java
@@ -4,13 +4,14 @@ import static java.lang.Boolean.FALSE;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.IntStream;
 
 public class Line {
 
     private final List<Boolean> connections;
 
-    private Line(final List<Boolean> connections) {
+    Line(final List<Boolean> connections) {
         this.connections = connections;
     }
 
@@ -30,5 +31,23 @@ public class Line {
                 .forEach(connections::add);
 
         return connections;
+    }
+
+    @Override
+    public boolean equals(final Object otherLine) {
+        if (this == otherLine) {
+            return true;
+        }
+
+        if (otherLine == null || getClass() != otherLine.getClass()) {
+            return false;
+        }
+
+        return Objects.equals(this.connections, ((Line)otherLine).connections);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.connections);
     }
 }
--- src/test/java/ladder/LineTest.java
+++ src/test/java/ladder/LineTest.java
@@ -2,6 +2,8 @@ package ladder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +12,7 @@ class LineTest {
     @Test
     @DisplayName("플레이어 수를 기준으로 사다리의 라인을 생성한다.")
     void Line_PlayersCount() {
-        assertThat(Line.from(new PlayersCount(5), new RandomConnectionGenerator()))
-                .isNotNull();
+        assertThat(Line.from(new PlayersCount(5), () -> true))
+                .isEqualTo(new Line(List.of(true, false, true, false, true)));
     }
 }
