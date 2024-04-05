3cd4b10a6bd5d165ed7c0476fbfe04c781678028
namhyeop <skaguq@gmail.com>
2024-04-06 03:47:19 +0900

    feat : 사용자 존재하지 않을 경우 예외 발생을 위한 예외 타입 추가

---
+++ src/main/java/nextstep/ladder/error/exception/NotExistUserException.java
@@ -0,0 +1,10 @@
+package nextstep.ladder.error.exception;
+
+import java.text.MessageFormat;
+
+public class NotExistUserException extends Throwable {
+
+    public NotExistUserException(Object parameter) {
+        super(MessageFormat.format("{0}. 입력값: {1}", "존재하지 않는 사용자입니다", parameter));
+    }
+}
