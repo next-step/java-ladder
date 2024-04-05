49ba25af5c8f65389edc590772133a95d98e9fe7
이승호 <seung7642@gmail.com>
2024-04-05 21:14:33 +0900

    feat: 특정 보상의 위치를 구한다.

---
+++ src/main/java/domain/Rewards.java
@@ -0,0 +1,19 @@
+package domain;
+
+import java.util.List;
+
+public class Rewards {
+
+    private final List<Reward> rewards;
+
+    public Rewards(List<Reward> rewards) {
+        this.rewards = rewards;
+    }
+
+    public Position position(Reward reward) {
+        if (!rewards.contains(reward)) {
+            throw new IllegalArgumentException("없는 보상입니다.");
+        }
+        return new Position(rewards.indexOf(reward), 0);
+    }
+}
