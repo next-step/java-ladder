1ea3285e118908f4ed6936d1060d0e03de0dd3a2
이승호 <seung7642@openbase.co.kr>
2024-04-05 18:31:26 +0900

    test: 특정 플레이어에 대한 결과를 확인한다.

---
+++ src/main/java/domain/Reward.java
@@ -0,0 +1,29 @@
+package domain;
+
+import java.util.Objects;
+
+public class Reward {
+
+    private final String reward;
+
+    public Reward(String reward) {
+        this.reward = reward;
+    }
+
+    public String getReward() {
+        return reward;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Reward reward1 = (Reward) o;
+        return Objects.equals(reward, reward1.reward);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(reward);
+    }
+}
