1bc7a0ba6eea69f4eceee47a66491a333aa302e8
jaeyounglee <ljyda214@gmail.com>
2024-04-07 02:06:40 +0900

    FEAT : 클래스 이름 변경

--- src/main/java/ladder/domain/Ladder/factory/LineFactory.java
+++ src/main/java/ladder/domain/Ladder/factory/LineFactory.java
@@ -1,34 +1,20 @@
 package ladder.domain.Ladder.factory;
 
-import ladder.utils.LambdaUtils;
-import ladder.domain.Ladder.Point;
+import ladder.domain.Ladder.Line;
+import ladder.domain.Ladder.PointPredicate;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LineFactory {
 
-    private static final Random random = ThreadLocalRandom.current();
-
     private LineFactory() {
     }
 
-    public static List<Point> generatePoints(int countOfPerson) {
-        return Stream.iterate(new Point(random.nextBoolean()),
-                        previous -> LambdaUtils.decidePointExist(
-                                previous,
-                                new Point(random.nextBoolean()),
-                                Point::isEqualToTrue)
-                )
-                .limit(countOfPerson - 1L)
+    public static List<Line> generateLines(int height, int countOfPerson, PointPredicate pointPredicate) {
+        return Stream.generate(() -> new Line(countOfPerson, pointPredicate))
+                .limit(height)
                 .collect(Collectors.toList());
-    }
-
-    public static List<Point> generatePoints(Boolean[] points) {
-        return Arrays.stream(points).map(Point::new).collect(Collectors.toList());
     }
 }
