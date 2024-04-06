88a9142d47d163967047759addceb1d05ed1d039
jaeyounglee <ljyda214@gmail.com>
2024-04-06 22:41:49 +0900

    FEAT : String split 관련 기능을 담당하는 Utils class 생성

---
+++ src/main/java/ladder/utils/SplitUtils.java
@@ -0,0 +1,11 @@
+package ladder.utils;
+
+public class SplitUtils {
+
+    private SplitUtils() {
+    }
+
+    public static String[] split(String text) {
+        return text.split(",");
+    }
+}
