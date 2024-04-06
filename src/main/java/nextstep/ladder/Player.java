6929a1fe72d641e7ef6d71b0fc236a0547eb8f46
sang5c <skah321@gmail.com>
2024-04-06 19:32:20 +0900

    feat: 참가자는 이름을 갖는다

--- src/main/java/nextstep/ladder/Player.java
+++ src/main/java/nextstep/ladder/Player.java
@@ -4,7 +4,18 @@ public class Player {
     private final String name;
 
     public Player(String name) {
+        validate(name);
         this.name = name;
+    }
+
+    private static void validate(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("이름은 비어있을 수 없습니다.");
+        }
+
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
+        }
     }
 
     public String getName() {
---
+++ src/test/java/nextstep/ladder/PlayerTest.java
@@ -0,0 +1,25 @@
+package nextstep.ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PlayerTest {
+
+    @DisplayName("이름은 5글자 이하만 가능하다")
+    @Test
+    void tooLongName() {
+        assertThatThrownBy(() -> new Player("123456")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("이름은 비어있을 수 없다")
+    @ParameterizedTest(name = "name = {0}")
+    @NullAndEmptySource
+    void emptyName(String name) {
+        assertThatThrownBy(() -> new Player(name)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
