3010e65f2eb4747fdd1ddb1b52005cd4d451e5a1
astrid-dm <himax_01@naver.com>
2024-04-06 13:54:18 +0900

    feat : 참가자 목록에 특정 참가자가 있는지 조회하는 로직 추가

--- src/main/java/ladder/model/Participants.java
+++ src/main/java/ladder/model/Participants.java
@@ -3,8 +3,12 @@ package ladder.model;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Participants {
+    public static final String ALL_PARTICIPANTS = "all";
+    public static final String NO_MATCHING_PARTICIPANT = "해당 참가자는 존재하지 않습니다.";
+
     private List<Participant> participants;
 
     private Participants(List<Participant> participants) {
@@ -12,8 +16,8 @@ public class Participants {
     }
 
     public static Participants of(List<String> participants) {
-        List<Participant> pluralParticipants = participants.stream()
-                .map(Participant::of)
+        List<Participant> pluralParticipants = IntStream.range(0, participants.size())
+                .mapToObj(index -> Participant.of(participants.get(index), index))
                 .collect(Collectors.toList());
 
         return new Participants(pluralParticipants);
@@ -26,4 +30,21 @@ public class Participants {
     public List<Participant> getParticipants() {
         return Collections.unmodifiableList(participants);
     }
+
+    public List<Participant> findParticipants(String name) {
+        if (name.equals(ALL_PARTICIPANTS)) {
+            return participants;
+        }
+
+        List<Participant> result = List.of(findParticipant(name));
+        return result;
+    }
+
+    private Participant findParticipant(String name) {
+        return participants.stream()
+                .filter(participant -> participant.isEqualParticipant(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(NO_MATCHING_PARTICIPANT));
+    }
+
 }
