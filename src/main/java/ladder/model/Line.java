7a7aca95f840534a08d17875570ca9debf4177c9
astrid-dm <himax_01@naver.com>
2024-04-06 13:49:19 +0900

    feat : 특정 인덱스의에서 사다리 타기 실행 시, 어디로 움직일지 반환하는 로직 구현

--- src/main/java/ladder/model/Line.java
+++ src/main/java/ladder/model/Line.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class Line {
+    public static final int MOVE_LEFT = -1;
+    public static final int MOVE_RIGHT = 1;
     public static final String INVALID_LINE = "사다리의 연달은 라인이 겹칠 수는 없습니다.";
     private final List<Boolean> points;
 
@@ -30,5 +32,25 @@ public class Line {
 
     public boolean isConnectedLadder(int index) {
         return points.get(index);
+    }
+
+    protected int moveByDistance(int index) {
+        if (isConnectedLadder(index)) {
+            return MOVE_RIGHT;
+        }
+
+        if (isNotTheFirstIndex(index) && isConnectedLadder(index - 1)) {
+            return MOVE_LEFT;
+        }
+
+        return 0;
+    }
+
+    private boolean isNotTheFirstIndex(int index) {
+        if (index == 0) {
+            return false;
+        }
+
+        return true;
     }
 }
