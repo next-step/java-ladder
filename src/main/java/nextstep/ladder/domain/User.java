bc75f270fe8f1ded47a58227938554854643d00c
JJewon <eldertree777@gmail.com>
2024-04-08 01:24:29 +0900

    [feat] 이름은 최대 5글자이며, 초과시 예외 발생

--- src/main/java/nextstep/ladder/domain/User.java
+++ src/main/java/nextstep/ladder/domain/User.java
@@ -5,10 +5,17 @@ public class User {
     private static final String USER_INVAILD_MESSEAGE = "잘못된 유저명 입니다";
 
     public User(String userName) {
+        checkValidName(userName);
+        this.userName = userName;
+    }
+
+    private static void checkValidName(String userName) {
         if (userName == null || userName.isEmpty() || userName.isBlank()) {
             throw new IllegalArgumentException(USER_INVAILD_MESSEAGE);
         }
 
-        this.userName = userName;
+        if (userName.length() > 5) {
+            throw new IllegalArgumentException(USER_INVAILD_MESSEAGE);
+        }
     }
 }
--- src/test/java/nextstep/ladder/UserTest.java
+++ src/test/java/nextstep/ladder/UserTest.java
@@ -3,14 +3,24 @@ package nextstep.ladder;
 
 import nextstep.ladder.domain.User;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 public class UserTest {
+    private String testName = "test";
+    private String exceedLengthName = "a".repeat(100);
     @ParameterizedTest
     @NullAndEmptySource
     void userEmptyTest(String name) {
         Assertions.assertThatThrownBy(() -> new User(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 유저명 입니다");
+    }
+
+    @Test
+    void exceedLengthTest() {
+        Assertions.assertThatThrownBy(() -> new User(exceedLengthName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("잘못된 유저명 입니다");
     }
