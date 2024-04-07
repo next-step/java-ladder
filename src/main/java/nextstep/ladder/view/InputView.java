b32f850d1a32e35f0ef36a547df047ec00028b3d
ksh <tjdgus2319@gmail.com>
2024-04-07 19:27:22 +0900

    refactor: Scaaner 객체 재활용

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -6,8 +6,9 @@ import java.util.Scanner;
 
 public class InputView {
 	private final static String NAME_DELIMITER = ",";
+	private static Scanner scanner = new Scanner(System.in);
+
 	public static List<String> InputNames() {
-		Scanner scanner = new Scanner(System.in);
 		System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
 		String names = scanner.nextLine();
 		List<String> stringNames = getStringNames(names);
@@ -20,7 +21,6 @@ public class InputView {
 	}
 
 	public static int InputLadderHeight() {
-		Scanner scanner = new Scanner(System.in);
 		System.out.println("최대 사다리 높이는 몇 개인가요?");
 		return scanner.nextInt();
 	}
