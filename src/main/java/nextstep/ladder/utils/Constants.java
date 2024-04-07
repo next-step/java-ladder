c393625a4e75a239b9d108c8a2cea41f467b3ec6
namhyeop <skaguq@gmail.com>
2024-04-08 00:28:35 +0900

    refactor : 불필요한 상수 제거

--- src/main/java/nextstep/ladder/utils/Constants.java
+++ src/main/java/nextstep/ladder/utils/Constants.java
@@ -3,7 +3,6 @@ package nextstep.ladder.utils;
 public class Constants {
 
     public static final int USER_NAME_LIMIT_SIZE = 5;
-    public static final int NOT_EXIST_USER_CODE = -99999999;
 
     private Constants() {
     }
