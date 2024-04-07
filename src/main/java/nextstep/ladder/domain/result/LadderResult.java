4f1aed9044663007d26e7e1c15cf357be95bbd67
namhyeop <skaguq@gmail.com>
2024-04-07 15:59:21 +0900

    feat : ladderResult를 Result 객체로 리팩토링

--- src/main/java/nextstep/ladder/domain/result/LadderResult.java
+++ src/main/java/nextstep/ladder/domain/result/LadderResult.java
@@ -1,12 +1,12 @@
 package nextstep.ladder.domain.result;
 
 
-import static nextstep.ladder.utils.Constants.NOT_EXIST_USER_CODE;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.IntStream;
 import nextstep.ladder.domain.line.Lines;
 import nextstep.ladder.domain.user.User;
 import nextstep.ladder.domain.user.Users;
@@ -15,29 +15,26 @@ import nextstep.ladder.error.exception.NotExistUserException;
 
 public class LadderResult {
 
-    private final Map<User, Integer> ladderResult = new HashMap<>();
-    private List<String> drewList;
+    private final Map<User, Result> ladderResult = new HashMap<>();
+    private List<String> draws;
 
-    public LadderResult(List<String> drewList, Users users) {
-        if (drewList.size() != users.size()) {
-            throw new LadderUsersMismatchException(drewList.size(), users.size());
+    public LadderResult(List<String> draws, Users users) {
+        if (draws.size() != users.size()) {
+            throw new LadderUsersMismatchException(draws.size(), users.size());
         }
 
-        this.drewList = drewList;
+        this.draws = draws;
     }
 
     public void calculateLadderResult(Users users, Lines lines) {
-        users.addLadderResult(ladderResult, lines);
+        IntStream.range(0, users.size())
+            .forEach(userIndex -> ladderResult.put(users.findUser(userIndex),
+                new Result(draws.get(lines.findUserResultIndex(userIndex)))));
     }
 
-    public String userDrawResult(User user) throws NotExistUserException {
-        int result = ladderResult.getOrDefault(user, NOT_EXIST_USER_CODE);
-
-        if (NOT_EXIST_USER_CODE == result) {
-            throw new NotExistUserException(user.getUserName());
-        }
-
-        return drewList.get(result);
+    public Result findUserDrawResult(User user) throws NotExistUserException {
+        return Optional.ofNullable(ladderResult.get(user))
+            .orElseThrow(() -> new NotExistUserException(user.getUserName()));
     }
 
     public Set<User> keySet() {
