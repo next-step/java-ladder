19bb6e02aa7881f89ae2538d279394faf950fae7
JJewon <eldertree777@gmail.com>
2024-04-08 02:26:47 +0900

    [feat] Line 사다리 폭은 1보다 작은 경우 예외 발생

--- src/test/java/nextstep/ladder/domain/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/domain/ladder/LineTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class LineTest {
 
     @Test
@@ -12,5 +14,13 @@ public class LineTest {
         Assertions.assertThatThrownBy(() -> new Line(true, true))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("연속된 연결을 될 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("사다리 폭은 1보다 작은 경우 예외 발생")
+    void lineInvalid02Test() {
+        Assertions.assertThatThrownBy(() -> new Line(List.of()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("빈 List<Point>를 통해 Line을 생성할 수 없습니다.");
     }
 }
