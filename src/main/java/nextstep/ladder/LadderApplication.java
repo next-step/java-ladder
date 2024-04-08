0a30861d14aa5fd88d8e07f86043b2d4c40e2a5c
JJewon <eldertree777@gmail.com>
2024-04-09 03:39:25 +0900

    [style] rename main -> LadderApplication

--- src/main/java/nextstep/ladder/LadderApplication.java
+++ src/main/java/nextstep/ladder/LadderApplication.java
@@ -4,7 +4,7 @@ import nextstep.ladder.controller.LadderController;
 import nextstep.ladder.domain.generator.RandomLineGenerator;
 import nextstep.ladder.service.LadderService;
 
-public class main {
+public class LadderApplication {
     public static void main(String[] args) {
         LadderController ladderController = new LadderController(new LadderService(new RandomLineGenerator()));
         ladderController.play();
