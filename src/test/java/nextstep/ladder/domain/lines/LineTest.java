148226f4ffdae95764be304995e00da080633530
namhyeop <skaguq@gmail.com>
2024-04-08 21:48:47 +0900

    refactor : LineTest Interface 적용

--- src/test/java/nextstep/ladder/domain/lines/LineTest.java
+++ src/test/java/nextstep/ladder/domain/lines/LineTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import nextstep.ladder.domain.ladderConnectOrder.LadderConnectOrder;
 import nextstep.ladder.domain.lines.line.Line;
 import nextstep.ladder.domain.lines.line.impl.LineImpl;
+import nextstep.ladder.domain.lines.point.impl.PointImpl;
 import nextstep.ladder.error.exception.LineNoConsecutiveTrues;
 import nextstep.ladder.domain.ladderConnectOrder.impl.RandomLadderConnectOrder;
 import org.junit.jupiter.api.Test;
@@ -27,39 +28,39 @@ class LineTest {
     @Test
     void Line에_두_개의_사다리가_연결된_경우_예외가_발생한다() {
         assertThatThrownBy(
-            () -> new LineImpl(List.of(new Point(true), new Point(true))))
+            () -> new LineImpl(List.of(new PointImpl(true), new PointImpl(true))))
             .isInstanceOf(LineNoConsecutiveTrues.class)
             .hasMessage("Line은 연속해서 사다리를 연결할 수 없습니다");
     }
 
     @Test
     void Line은_UserIndex를_오른쪽으로_이동할지_결정한다() {
-        Line firstLineImpl = new LineImpl(List.of(new Point(true), new Point(false))); //  |-----|     |
-        Line secondLineImpl = new LineImpl(List.of(new Point(false), new Point(true))); // |     |-----|
+        Line firstLine = new LineImpl(List.of(new PointImpl(true), new PointImpl(false))); //  |-----|     |
+        Line secondLine = new LineImpl(List.of(new PointImpl(false), new PointImpl(true))); // |     |-----|
 
-        assertThat(firstLineImpl.canMoveRight(0)).isTrue();
-        assertThat(secondLineImpl.canMoveRight(0)).isFalse();
+        assertThat(firstLine.canMoveRight(0)).isTrue();
+        assertThat(secondLine.canMoveRight(0)).isFalse();
     }
 
     @Test
     void Line의_가장_오른쪽에_사용자가_위치한_경우_오른쪽으로_이동할_수_없다() {
-        Line line = new LineImpl(List.of(new Point(true), new Point(false))); //  |-----|     |
+        Line line = new LineImpl(List.of(new PointImpl(true), new PointImpl(false))); //  |-----|     |
 
         assertThat(line.canMoveRight(2)).isFalse();
     }
 
     @Test
     void Line은_UserIndex를_왼쪽으로_이동할지_결정한다() {
-        Line firstLineImpl = new LineImpl(List.of(new Point(true), new Point(false))); //  |-----|     |
-        Line secondLineImpl = new LineImpl(List.of(new Point(false), new Point(true))); // |     |-----|
+        Line firstLine = new LineImpl(List.of(new PointImpl(true), new PointImpl(false))); //  |-----|     |
+        Line secondLine = new LineImpl(List.of(new PointImpl(false), new PointImpl(true))); // |     |-----|
 
-        assertThat(firstLineImpl.canMoveLeft(1)).isTrue();
-        assertThat(secondLineImpl.canMoveLeft(1)).isFalse();
+        assertThat(firstLine.canMoveLeft(1)).isTrue();
+        assertThat(secondLine.canMoveLeft(1)).isFalse();
     }
 
     @Test
     void Line의_가장_왼쪽에_사용자가_위치한_경우_왼쪽으로_이동할_수_없다() {
-        Line line = new LineImpl(List.of(new Point(true), new Point(false))); //  |-----|     |
+        Line line = new LineImpl(List.of(new PointImpl(true), new PointImpl(false))); //  |-----|     |
 
         assertThat(line.canMoveLeft(0)).isFalse();
     }
