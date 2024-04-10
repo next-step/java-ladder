c7518e8253df351c08e5093c65131d1f62290eea
kbzz17 <108657069+kbzz17@users.noreply.github.com>
2024-04-10 17:49:45 +0900

    🚀 1단계 - 스트림, 람다, Optional 미션완료 (#2129)
    * [test] CarTest 람다 변환
    
    * [test] 람드를 활용한 중복제거
    
    * [test] 다양한 stream Method 실습
    
    * [feat] 요구사항1 Optional을 활용해 조건에 따른 반환
    
    * [feat] 요구사항2 Optional에서 값을 반환
    
    * [feat] 요구사항3 Optional에서 Exception 처리
    
    * [feat] 요구사항4 ComputerStore 통과시키기

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,22 +13,29 @@ public class StreamStudy {
 
     public static long countWords() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+            .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream()
+            .filter(w -> w.length() > 12)
+            .count();
     }
 
     public static void printLongestWordTop100() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+            .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         // TODO 이 부분에 구현한다.
+        words.stream()
+            .filter(s -> s.length() > 12)
+            .map(String::toLowerCase)
+            .distinct()
+            .sorted(Comparator.comparingInt(String::length))
+            .collect(Collectors.toList())
+            .stream()
+            .limit(100)
+            .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +47,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+            .filter(num -> num > 3)
+            .map(num -> num * 2)
+            .reduce(0, Integer::sum);
     }
 }
