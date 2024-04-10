ce4a4cb7ac50e5c613a022e184bd70129bf47e19
Jioo Jung <64303211+jioome@users.noreply.github.com>
2024-04-10 20:53:31 +0900

    feat: 이름 입력 기능

---
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -0,0 +1,13 @@
+package nextstep.ladder.view;
+
+import java.util.Scanner;
+
+public class InputView {
+	private static final String DELIMITER = ",";
+	Scanner scanner = new Scanner(System.in);
+
+	public String[] inputNames() {
+		System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+		return scanner.nextLine().split(DELIMITER);
+	}
+}
