37d71ed8ba549ade9e400c2f8c62f57cabcbc4ea
jaeyounglee <ljyda214@gmail.com>
2024-04-13 01:15:26 +0900

    REFACTOR : PointFactory 클래스 리팩토링

--- src/main/java/ladder/domain/Ladder/factory/PointFactory.java
+++ src/main/java/ladder/domain/Ladder/factory/PointFactory.java
@@ -2,9 +2,11 @@ package ladder.domain.Ladder.factory;
 
 import ladder.domain.Ladder.Point;
 import ladder.domain.Ladder.PointPredicate;
-import ladder.utils.LambdaUtils;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -14,20 +16,29 @@ public class PointFactory {
     private PointFactory() {
     }
 
-    public static List<Point> generatePoints(int countOfPerson, PointPredicate predicate) {
-        return Stream.iterate(new Point(predicate),
-                        previous -> LambdaUtils.decidePointExist(
-                                previous,
-                                new Point(predicate),
-                                Point::isEqualToTrue)
-                )
+    public static List<Point> generatePoints2(int countOfPerson, PointPredicate predicate) {
+        return create(getPredicatedValues(countOfPerson, predicate));
+    }
+
+    public static List<Point> generatePoints2(Boolean[] exists) {
+        return create(Arrays.asList(exists));
+    }
+
+    private static List<Boolean> getPredicatedValues(int countOfPerson, PointPredicate predicate) {
+        return Stream.generate(predicate::exists)
                 .limit(countOfPerson - 1L)
                 .collect(Collectors.toList());
     }
 
-    public static List<Point> generatePoints(Boolean[] points) {
-        return Arrays.stream(points)
-                .map(Point::new)
-                .collect(Collectors.toList());
+    private static List<Point> create(List<Boolean> exists) {
+        Deque<Point> points = new ArrayDeque<>();
+        points.push(Point.first(exists.get(0)));
+
+        for (int i = 1; i < exists.size(); i++) {
+            points.addLast(points.getLast().next(exists.get(i)));
+        }
+
+        points.push(points.getLast().last());
+        return new ArrayList<>(points);
     }
 }
