a2d507036f891d42e293af363019cb7b1282c227
jaeyounglee <ljyda214@gmail.com>
2024-04-13 01:18:18 +0900

    CHG : 사용하지 않는 메소드 삭제

--- src/main/java/ladder/utils/LambdaUtils.java
+++ src/main/java/ladder/utils/LambdaUtils.java
@@ -1,24 +1,10 @@
 package ladder.utils;
 
-import ladder.domain.Ladder.Point;
 import ladder.error.ErrorMessage;
 
 import java.util.function.BiPredicate;
 
 public class LambdaUtils {
-
-    public static Point decidePointExist(Point point1, Point point2, BiPredicate<Point, Point> predicate) {
-        if (predicate.test(point1, point2)) {
-            return new Point(false);
-        }
-        return point2;
-    }
-
-    public static void validatePointRule(Point p1, Point p2, BiPredicate<Point, Point> predicate) {
-        if (predicate.test(p1,p2)) {
-            throw new IllegalArgumentException(ErrorMessage.ERR_INVALID_POINT.print());
-        }
-    }
 
     public static void validateLength(String name, int number, BiPredicate<String , Integer> predicate) {
         if (predicate.test(name, number)) {
