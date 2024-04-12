ea1d1e0e7b383094776f4ef4ddcc2a5707af8e0a
jaeyounglee <ljyda214@gmail.com>
2024-04-13 01:23:42 +0900

    FEAT : 생성자 추가

--- src/main/java/ladder/utils/LambdaUtils.java
+++ src/main/java/ladder/utils/LambdaUtils.java
@@ -6,6 +6,9 @@ import java.util.function.BiPredicate;
 
 public class LambdaUtils {
 
+    private LambdaUtils() {
+    }
+
     public static void validateLength(String name, int number, BiPredicate<String , Integer> predicate) {
         if (predicate.test(name, number)) {
             throw new IllegalArgumentException(ErrorMessage.ERR_OUT_OF_LENGTH.print());
