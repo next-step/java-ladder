c71455855289657d40aa0069f5f8b9af589f001b
kwanik-kor <rhksdlr134@naver.com>
2024-04-13 17:44:19 +0900

    feat: Rows 제거
    - Ladder가 List<Row>에 대한 일급컬렉션 그 자체의 역할을 하고 있다고 판단

--- src/main/java/nextstep/ladder/domain/ladder/Ladder.java
+++ src/main/java/nextstep/ladder/domain/ladder/Ladder.java
@@ -2,20 +2,38 @@ package nextstep.ladder.domain.ladder;
 
 import nextstep.ladder.domain.player.Count;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Ladder {
-    private final Rows rows;
+    private final List<Row> values = new ArrayList<>();
 
-    public Ladder(Height height, Count playerCount, RungGenerateStrategy strategy) {
-        this(new Rows(height, playerCount, strategy));
+    public Ladder(Height height, Count playersCount, RungGenerateStrategy strategy) {
+        this(Stream.generate(() -> new Row(playersCount, strategy))
+                .limit(height.value())
+                .collect(Collectors.toList()));
     }
 
-    public Ladder(Rows rows) {
-        this.rows = rows;
+    public Ladder(List<Row> values) {
+        assertRowsNotEmpty(values);
+
+        this.values.addAll(values);
     }
 
-    public void forEachRows(Consumer<Row> action) {
-        rows.forEach(action);
+    private void assertRowsNotEmpty(List<Row> rows) {
+        if (rows == null || rows.isEmpty()) {
+            throw new IllegalArgumentException("사다리 행은 비어있을 수 없습니다.");
+        }
+    }
+
+    public Height height() {
+        return new Height(this.values.size());
+    }
+
+    public void forEach(Consumer<Row> action) {
+        values.forEach(action);
     }
 }
--- src/test/java/nextstep/ladder/domain/ladder/LadderTest.java
+++ src/test/java/nextstep/ladder/domain/ladder/LadderTest.java
@@ -9,19 +9,19 @@ import java.util.Collections;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class RowsTest {
+public class LadderTest {
 
-    @DisplayName("Rows는 높이 만큼의 행을 가진다.")
+    @DisplayName("사다리는 높이 만큼의 행을 가진다.")
     @Test
     void constructor() {
-        assertThat(new Rows(new Height(5), new Count(3), () -> true).height())
+        assertThat(new Ladder(new Height(5), new Count(3), () -> true).height())
                 .isEqualTo(new Height(5));
     }
 
-    @DisplayName("Rows는 반드시 Row를 한 개 이상 가져야 한다.")
+    @DisplayName("사다리는 반드시 Row를 한 개 이상 가져야 한다.")
     @Test
     void empty_constructor() {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Rows(Collections.emptyList()));
+                .isThrownBy(() -> new Ladder(Collections.emptyList()));
     }
 }
