4bfa11a348270da8725165832e12ca53a26baf2c
pado0 <bgshhd95@gmail.com>
2024-04-13 22:31:31 +0900

    refact:[step2] magic number wrapping

--- src/main/java/ladder/domain/Floor.java
+++ src/main/java/ladder/domain/Floor.java
@@ -1,10 +1,11 @@
 package ladder.domain;
 
 public class Floor {
+    public static final int MIN_HEIGHT = 2;
     private int height;
 
     public Floor(int height) {
-        if (height <= 1) {
+        if (height < MIN_HEIGHT) {
             throw new IllegalArgumentException("사다리의 높이는 2보다 커야합니다.");
         }
         this.height = height;
--- src/main/java/ladder/domain/Player.java
+++ src/main/java/ladder/domain/Player.java
@@ -3,10 +3,11 @@ package ladder.domain;
 import java.util.Optional;
 
 public class Player {
+    public static final int MAX_NAME_LENGTH = 5;
     private String name;
 
     public Player(String name) {
-        if(name.length() > 5){
+        if(name.length() > MAX_NAME_LENGTH){
             throw new IllegalArgumentException("이름이 5자를 초과하였습니다.");
         }
         if(name.isBlank()){
