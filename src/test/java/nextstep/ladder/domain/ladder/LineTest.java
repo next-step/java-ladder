e94f53749413d049f6cc0862029f36af1afa5676
JJewon <eldertree777@gmail.com>
2024-04-15 01:34:44 +0900

    [refactor] assertThatThrownBy -> assertThatIllegalArgumentException

--- src/test/java/nextstep/ladder/domain/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/domain/ladder/LineTest.java
@@ -14,9 +14,7 @@ public class LineTest {
     @Test
     @DisplayName("n-1 번째 이어진 경우 n번째는 이어질 수 없음.")
     void lineInvalid01Test() {
-        Assertions.assertThatThrownBy(() -> new Line(true, true))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("연속된 연결을 될 수 없습니다.");
+        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> new Line(true, true));
     }
 
     @Test
