d4caa1de6e5c87e1bf481395c2d1c6719e921d5a
JJewon <eldertree777@gmail.com>
2024-04-15 01:26:01 +0900

    [refactor] 매직넘버 상수치환

--- src/main/java/nextstep/ladder/domain/user/User.java
+++ src/main/java/nextstep/ladder/domain/user/User.java
@@ -3,6 +3,7 @@ package nextstep.ladder.domain.user;
 public class User {
     private final String userName;
     private static final String USER_INVAILD_MESSEAGE = "잘못된 유저명 입니다";
+    private static final int MAX_NAME_LENGTH = 5;
 
     public User(String userName) {
         checkValidName(userName);
@@ -14,7 +15,7 @@ public class User {
             throw new IllegalArgumentException(USER_INVAILD_MESSEAGE);
         }
 
-        if (userName.length() > 5) {
+        if (userName.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException(USER_INVAILD_MESSEAGE);
         }
     }
