f3a5921f635f87faa477ab5d282d4c807513f79b
naayoungkwon <kny8092@gmail.com>
2024-04-14 17:32:41 +0900

    test: People, Point, Line 생성자 Test 추가

---
+++ src/test/java/nextstep/ladder/domain/PointTest.java
@@ -0,0 +1,24 @@
+package nextstep.ladder.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PointTest {
+
+  @Test
+  @DisplayName("Bridge가 없는 Point 인스턴스를 생성한다")
+  void createTrueInstance() {
+    Point point = Point.of(Boolean.TRUE);
+    assertThat(point.isNotBridge()).isFalse();
+  }
+
+  @Test
+  @DisplayName("Bridge가 없는 Point 인스턴스를 생성한다")
+  void createFalseInstance() {
+    Point point = Point.of(Boolean.FALSE);
+    assertThat(point.isNotBridge()).isTrue();
+  }
+
+}
