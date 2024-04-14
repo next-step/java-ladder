0a8de5ea8bded03046a881592dfc0add9c7f6aaf
이승호 <seung7642@gmail.com>
2024-04-14 22:09:09 +0900

    feat: 다리 (Bridge) 를 생성하고, 이동한다.

--- src/main/java/domain/Bridge.java
+++ src/main/java/domain/Bridge.java
@@ -3,6 +3,8 @@ package domain;
 public class Bridge {
 
     private final boolean value;
+    private boolean left;
+    private boolean current;
 
     public Bridge() {
         this(false);
@@ -10,6 +12,26 @@ public class Bridge {
 
     public Bridge(boolean value) {
         this.value = value;
+    }
+
+    private Bridge(boolean left, boolean current) {
+        this.value = current;
+        this.left = left;
+        this.current = current;
+    }
+
+    public static Bridge first(boolean current) {
+        return new Bridge(false, current);
+    }
+
+    public Direction move() {
+        if (left) {
+            return Direction.LEFT;
+        }
+        if (current) {
+            return Direction.RIGHT;
+        }
+        return Direction.PASS;
     }
 
     public boolean has() {
---
+++ src/main/java/domain/Direction.java
@@ -0,0 +1,8 @@
+package domain;
+
+public enum Direction {
+
+    LEFT,
+    PASS,
+    RIGHT;
+}
