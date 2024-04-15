c0699b43d8c03601c968704f3e5761a9def48814
Jioo Jung <64303211+jioome@users.noreply.github.com>
2024-04-15 23:29:46 +0900

    feat: 실행 결과 입력

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -8,11 +8,20 @@ public class InputView {
 
 	public String[] inputNames() {
 		System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-		return scanner.nextLine().split(DELIMITER);
+		return split(scanner.nextLine());
+	}
+
+	public String[] inputResult() {
+		System.out.println("\n실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+		return split(scanner.nextLine());
 	}
 
 	public int inputLadderHeight(){
 		System.out.println("\n최대 사다리 높이는 몇 개인가요?");
 		return scanner.nextInt();
+	}
+
+	public String[] split(final String input) {
+		return input.split(DELIMITER);
 	}
 }
