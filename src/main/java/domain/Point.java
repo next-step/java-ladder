b7656435be4541d05ca94ee024a346a858fa0b1d
nimoh <spakers38@gmail.com>
2024-04-16 20:29:39 +0900

    refactor: [Step 4] Point에 first, last, of 정적 팩토리 메서드 추가

--- src/main/java/domain/Point.java
+++ src/main/java/domain/Point.java
@@ -1,8 +1,22 @@
 package domain;
 
 public class Point {
+    private boolean isFirst;
+    private boolean isLast;
     private final boolean left;
     private final boolean current;
+
+    public static Point first(boolean current) {
+        return new Point(true, false, false, current);
+    }
+
+    public static Point last(boolean left) {
+        return new Point(false, true, left, false);
+    }
+
+    public static Point of(boolean left, boolean current) {
+        return new Point(false, false, left, current);
+    }
 
     public Point(boolean current) {
         this(false, current);
@@ -15,8 +29,26 @@ public class Point {
         this.current = current;
     }
 
+    private Point(boolean isFirst, boolean isLast, boolean left, boolean current) {
+        if (left && current) {
+            throw new IllegalArgumentException();
+        }
+        this.isFirst = isFirst;
+        this.isLast = isLast;
+        this.left = left;
+        this.current = current;
+    }
+
     public boolean isExist() {
         return current;
+    }
+
+    public boolean isFirst() {
+        return isFirst;
+    }
+
+    public boolean isLast() {
+        return isLast;
     }
 
     public PointDirection move() {
--- src/test/java/domain/PointTest.java
+++ src/test/java/domain/PointTest.java
@@ -11,8 +11,32 @@ class PointTest {
     @Test
     @DisplayName("left와 current가 true면 예외 발생")
     void invalid() {
-        assertThatThrownBy(() -> new Point(true, true))
+        assertThatThrownBy(() -> Point.of(true, true))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("first()로 생성 시 isFirst true")
+    void first() {
+        Point first = Point.first(true);
+        assertThat(first.isFirst()).isTrue();
+        assertThat(first.isLast()).isFalse();
+    }
+
+    @Test
+    @DisplayName("last()로 생성 시 isLast true")
+    void last() {
+        Point last = Point.last(true);
+        assertThat(last.isLast()).isTrue();
+        assertThat(last.isFirst()).isFalse();
+    }
+
+    @Test
+    @DisplayName("of()로 생성 시 isLast와 isFirst는 false")
+    void of() {
+        Point of = Point.of(true, false);
+        assertThat(of.isLast()).isFalse();
+        assertThat(of.isFirst()).isFalse();
     }
 
     @Nested
@@ -35,6 +59,13 @@ class PointTest {
         @Test
         @DisplayName("left와 current가 false면 아래로 이동")
         void down() {
+            Point point = new Point(false, false);
+            assertThat(point.move()).isEqualTo(PointDirection.DOWN);
+        }
+
+        @Test
+        @DisplayName("left와 current가 false면 아래로 이동")
+        void next() {
             Point point = new Point(false, false);
             assertThat(point.move()).isEqualTo(PointDirection.DOWN);
         }
