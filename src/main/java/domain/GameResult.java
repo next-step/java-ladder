902add8d47a69221294b78af05e77e7900e268ce
이승호 <seung7642@gmail.com>
2024-04-16 09:08:29 +0900

    refactor: 사용되지 않는 코드 제거

--- src/main/java/domain/GameResult.java
+++ src/main/java/domain/GameResult.java
@@ -1,12 +1,7 @@
 package domain;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+public class GameResult {
 
-public class GameResult implements Iterable<Map.Entry<Player, Reward>> {
-
-    private final Map<Player, Reward> result = new HashMap<>();
     private final Player player;
     private final Reward reward;
 
@@ -15,20 +10,11 @@ public class GameResult implements Iterable<Map.Entry<Player, Reward>> {
         this.reward = reward;
     }
 
-    public void add(Player player, Reward reward) {
-        result.put(player, reward);
-    }
-
     public Player getPlayer() {
         return player;
     }
 
     public Reward getReward() {
         return reward;
-    }
-
-    @Override
-    public Iterator<Map.Entry<Player, Reward>> iterator() {
-        return result.entrySet().iterator();
     }
 }
