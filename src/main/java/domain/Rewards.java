3e4f265965f6a895f80a1a2496a794d47de3599d
이승호 <seung7642@gmail.com>
2024-04-17 23:57:26 +0900

    refactor: 불필요한 코드 삭제

--- src/main/java/domain/Rewards.java
+++ src/main/java/domain/Rewards.java
@@ -1,6 +1,5 @@
 package domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Rewards {
@@ -8,7 +7,7 @@ public class Rewards {
     private final List<Reward> rewards;
 
     public Rewards(List<Reward> rewards) {
-        this.rewards = new ArrayList<>(rewards);
+        this.rewards = rewards;
     }
 
     public Reward findByOrder(int order) {
