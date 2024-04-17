8b00b4b0ede4c1b3accff2de9ea4a4a1d8ce9f6e
NaayoungKwon <kny8092@gmail.com>
2024-04-18 00:42:58 +0900

    refactor: domain 내 클래스 패키지 분리

--- src/test/java/nextstep/ladder/domain/PeopleTest.java
+++ src/test/java/nextstep/ladder/domain/PeopleTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+
+import nextstep.ladder.domain.person.People;
+import nextstep.ladder.domain.person.Person;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
