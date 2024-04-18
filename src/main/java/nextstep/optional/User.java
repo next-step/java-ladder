562dc489d5806ae8eb3469d22785bc6d47244d4e
Yushin Kim <kkad45@naver.com>
2024-04-18 14:03:54 +0900

    Step1 기능을 구현했습니다.  (#2154)
    * step1 람다 실습 구현
    
    * step1 람다 중복 코드 제거
    
    * step1 reduce, map, filter 실습1 구현
    
    * step1 reduce, map, filter 실습2 구현
    
    * step1 optional 실습1 구현
    
    * step1 optional 실습2 구현
    
    * step1 optional 실습3 구현

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -24,16 +26,20 @@ public class User {
     public static boolean ageIsInRange1(User user) {
         boolean isInRange = false;
 
-        if (user != null && user.getAge() != null
-                && (user.getAge() >= 30
-                && user.getAge() <= 45)) {
+        if (user != null && ageIsInRange(user.getAge())) {
             isInRange = true;
         }
         return isInRange;
     }
 
     public static boolean ageIsInRange2(User user) {
-        return false;
+        return Optional.ofNullable(user)
+                .map(userInOpt -> ageIsInRange(userInOpt.getAge()))
+                .orElse(false);
+    }
+
+    public static boolean ageIsInRange(Integer age) {
+        return age != null && age >= 30 && age <= 45;
     }
 
     @Override
--- src/test/java/nextstep/optional/ExpressionTest.java
+++ src/test/java/nextstep/optional/ExpressionTest.java
@@ -19,4 +19,5 @@ public class ExpressionTest {
                     Expression.of("&");
                 });
     }
+
 }
