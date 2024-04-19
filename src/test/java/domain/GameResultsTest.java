7978b535cc2f5e45d7606bd822e09cfb7456e72b
이승호 <seung7642@gmail.com>
2024-04-19 20:32:33 +0900

    test: 특정 플레이어의 결과를 가져온다.

--- src/test/java/domain/GameResultsTest.java
+++ src/test/java/domain/GameResultsTest.java
@@ -7,10 +7,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameResultsTest {
 
+    private static final GameResults GRS1 = GameResults.createResults(LadderTest.L1, PlayersTest.PS1, RewardsTest.RS1);
+
     @DisplayName("전체 결과를 생성한다.")
     @Test
     void test01() {
+        assertThat(GRS1).isNotNull();
+    }
+
+    @DisplayName("특정 플레이어의 결과를 가져온다.")
+    @Test
+    void test02() {
         GameResults results = GameResults.createResults(LadderTest.L1, PlayersTest.PS1, RewardsTest.RS1);
-        assertThat(results).isNotNull();
+        GRS1.resultByPlayer(PlayerTest.P1).isEqualTo(RewardTest.R2);
     }
 }
