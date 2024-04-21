49cef02150b667620a18eee0fe793400c893bdc8
sunghyun <tjdgus2319@gmail.com>
2024-04-21 17:45:41 +0900

    feat: 실행 결과 만드는 메소드 구현

--- src/main/java/nextstep/ladder/domain/GameUsers.java
+++ src/main/java/nextstep/ladder/domain/GameUsers.java
@@ -1,36 +1,46 @@
 package nextstep.ladder.domain;
 
 import nextstep.ladder.GameUser;
+import nextstep.ladder.LadderGame;
 import nextstep.ladder.Position;
+import org.w3c.dom.ls.LSOutput;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class GameUsers {
-	private List<GameUser> gameUsers;
+    private List<GameUser> gameUsers = new ArrayList<>();
 
-	public GameUsers(List<GameUser> gameUsers) {
-		this.gameUsers = gameUsers;
-	}
+    public GameUsers(List<GameUser> gameUsers) {
+        this.gameUsers = gameUsers;
+    }
 
-	public GameUsers(UserNames userNames) {
-		IntStream.range(0, userNames.getNames().size())
-				.mapToObj(i -> new GameUser(userNames.getNames().get(i), new Position(i)))
-				.forEach(gameUsers::add);
+    public GameUsers(UserNames userNames) {
+        IntStream.range(0, userNames.getNames().size())
+                .mapToObj(i -> new GameUser(userNames.getNames().get(i), new Position(i)))
+                .forEach(gameUsers::add);
+    }
 
-	}
+    public void moveAllUsersOnLadderBoard(LadderBoard ladderBoard) {
+        for (Line line : ladderBoard.getLadderBoard()) {
+            for (GameUser gameUser : gameUsers) {
+                gameUser.move(line);
+            }
+        }
+    }
 
-	public void moveAllUsersOnLadderBoard(LadderBoard ladderBoard) {
-		for (Line line : ladderBoard.getLadderBoard()) {
-			for (GameUser gameUser : gameUsers) {
-				gameUser.move(line);
-			}
-		}
-	}
+    public List<Integer> getResultPosition() {
+        return gameUsers.stream().map(gameUser -> gameUser.getPosition()).collect(Collectors.toList());
+    }
 
-	public List<Integer> getResultPosition() {
-		return gameUsers.stream().map(gameUser -> gameUser.getPosition()).collect(Collectors.toList());
-	}
+    public LadderGameResult makeResult(List<String> executionResult) {
+        Map<String, String> result = new LinkedHashMap<>();
+
+        IntStream.range(0, gameUsers.size())
+                .forEach(i -> result.put(gameUsers.get(i).getUserName(), executionResult.get(gameUsers.get(i).getPosition())));
+
+        return new LadderGameResult(result);
+    }
 
 }
--- src/test/java/nextstep/ladder/GameUsersTest.java
+++ src/test/java/nextstep/ladder/GameUsersTest.java
@@ -1,18 +1,34 @@
 package nextstep.ladder;
 
-import nextstep.ladder.domain.GameUsers;
-import nextstep.ladder.domain.LadderBoard;
-import nextstep.ladder.domain.Line;
-import nextstep.ladder.domain.Point;
+import nextstep.ladder.domain.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameUsersTest {
+
+	@Test
+	@DisplayName("사다리 게임 결과 정상 동작 테스트")
+	void ladderGameResultTest() {
+		List<GameUser> gameUserList = List.of(
+				new GameUser("pobi", new Position(0)),
+				new GameUser("xyz", new Position(1)),
+				new GameUser("yz1", new Position(2)),
+				new GameUser("zzz", new Position(3))
+		);
+		List<String> executionResult = List.of("꽝","5000","꽝","3000");
+		GameUsers gameUsers = new GameUsers(gameUserList);
+		LadderGameResult result = gameUsers.makeResult(executionResult);
+		assertThat(result.getSingleResult("pobi")).isEqualTo("꽝");
+		assertThat(result.getSingleResult("xyz")).isEqualTo("5000");
+		assertThat(result.getSingleResult("yz1")).isEqualTo("꽝");
+		assertThat(result.getSingleResult("zzz")).isEqualTo("3000");
+	}
 
 	@Test
 	@DisplayName("사다리 게임 로직 정상 동작 테스트")
