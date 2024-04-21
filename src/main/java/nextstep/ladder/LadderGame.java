b4e88da193c291992c1b0d6d5f3d8f16c1f9e249
sunghyun <tjdgus2319@gmail.com>
2024-04-21 17:46:25 +0900

    feat: 실행결과 기능 추가한 게임 로직

--- src/main/java/nextstep/ladder/LadderGame.java
+++ src/main/java/nextstep/ladder/LadderGame.java
@@ -2,26 +2,46 @@ package nextstep.ladder;
 
 import nextstep.ladder.domain.GameUsers;
 import nextstep.ladder.domain.LadderBoard;
+import nextstep.ladder.domain.LadderGameResult;
 import nextstep.ladder.domain.UserNames;
 import nextstep.ladder.view.InputView;
 import nextstep.ladder.view.ResultView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LadderGame {
-	public static void start() {
-		UserNames userNames = new UserNames(InputView.InputNames());
-		List<String> executionResult = InputView.InputExecutionResult();
-		int ladderHeight = InputView.InputLadderHeight();
+    private static final String QUIT_RESULT_VIEW = "q";
+    private static final String ALL_RESULT_VIEW = "all";
 
-		GameUsers gameUsers = new GameUsers(userNames);
+    public static void start() {
+        UserNames userNames = new UserNames(InputView.InputNames());
+        List<String> executionResult = InputView.InputExecutionResult();
+        int ladderHeight = InputView.InputLadderHeight();
 
-		LadderBoard ladderBoard = new LadderBoard(ladderHeight, userNames.size());
-		gameUsers.moveAllUsersOnLadderBoard(ladderBoard);
-		List<Integer> resultPosition = gameUsers.getResultPosition();
+        GameUsers gameUsers = new GameUsers(userNames);
 
-		ResultView.printNames(userNames);
-		ResultView.printLadderBoard(ladderBoard);
-	}
+        LadderBoard ladderBoard = new LadderBoard(ladderHeight, userNames.size());
+        gameUsers.moveAllUsersOnLadderBoard(ladderBoard);
+
+        LadderGameResult result = gameUsers.makeResult(executionResult);
+
+        ResultView.printNames(userNames);
+        ResultView.printLadderBoard(ladderBoard);
+        ResultView.printLadderGameResult(executionResult);
+
+        while (true) {
+            String interestedPerson = InputView.inputInterestedPerson();
+            if (interestedPerson.equals(QUIT_RESULT_VIEW)) {
+                return;
+            }
+            if (interestedPerson.equals(ALL_RESULT_VIEW)) {
+                ResultView.printResultPerson(result);
+            } else {
+                ResultView.printResultSinglePerson(result.getSingleResult(interestedPerson));
+            }
+        }
+    }
 
 }
