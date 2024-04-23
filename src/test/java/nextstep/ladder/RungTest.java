1c03dc6892d7a52504d9064a744975c2e770ceb4
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-04-23 14:10:50 +0900

    Refactor: refactor by parameterizedTest

--- src/test/java/nextstep/ladder/RungTest.java
+++ src/test/java/nextstep/ladder/RungTest.java
@@ -2,19 +2,15 @@ package nextstep.ladder;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class RungTest {
-    @Test
-    void rightMove() {
-        Rung rung = new Rung(0, Direction.of(false, true));
+    @ParameterizedTest
+    @CsvSource({"0, false, 0", "0, true, 1"})
+    void rightMove(int position, boolean right, int expected) {
+        Rung rung = new Rung(position, Direction.first(right));
 
-        Assertions.assertThat(rung.move()).isEqualTo(1);
-    }
-
-    @Test
-    void passMove() {
-        Rung rung = new Rung(0, Direction.of(false, false));
-
-        Assertions.assertThat(rung.move()).isEqualTo(0);
+        Assertions.assertThat(rung.move()).isEqualTo(expected);
     }
 }
