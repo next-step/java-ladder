483bbbcfa0fa60fb8322a9bc8a998bc7a371a4b7
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-04-26 18:20:01 +0900

    Test: GameLadderTest#game()

--- src/test/java/nextstep/ladder/domain/rdd/GameLadderTest.java
+++ src/test/java/nextstep/ladder/domain/rdd/GameLadderTest.java
@@ -2,7 +2,7 @@ package nextstep.ladder.domain.rdd;
 
 import nextstep.ladder.domain.Rung;
 import nextstep.ladder.interfaces.Ladder;
-import nextstep.ladder.interfaces.Line;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -14,10 +14,14 @@ public class GameLadderTest {
         Rung second = first.next(false);
         Rung last = second.last();
 
-        List<Rung> rungs = List.of(first, second, last);
+        GameLine firstLine = new GameLine(List.of(first, second, last));
+        GameLine secondLine = new GameLine(List.of(first, second, last));
+        GameLine LastLine = new GameLine(List.of(first, second, last));
 
-        Line line = new GameLine(rungs);
-        Ladder ladder = new GameLadder(1, List.of(line));
-        ladder.game();
+        Ladder ladder = new GameLadder(3, List.of(firstLine, secondLine, LastLine));
+
+        Assertions.assertThat(ladder.game().findResult(0)).isEqualTo(1);
+        Assertions.assertThat(ladder.game().findResult(1)).isEqualTo(0);
+        Assertions.assertThat(ladder.game().findResult(2)).isEqualTo(2);
     }
 }
