9784b7cd4ca25612ee1b2925a7d8c89b69f84d6a
유지웅 <poujis0@gmail.com>
2024-05-03 01:14:30 +0900

    [refactor] (InOut) Point 클래스 리팩토링

--- src/test/java/nextstep/ladder/PointTest.java
+++ src/test/java/nextstep/ladder/PointTest.java
@@ -1,29 +1,48 @@
 package nextstep.ladder;
 
+import static nextstep.ladder.domain.ladder.Direction.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import nextstep.ladder.domain.ladder.Point;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PointTest {
 
     @Test
-    @DisplayName("연결 여부를 반환하는 메소드 테스트")
-    void IS_CONNECTED_TEST() {
-        Point point = Point.of(true);
-        assertThat(point.isConnected()).isEqualTo(true);
-
-        point = Point.of(false);
-        assertThat(point.isConnected()).isEqualTo(false);
+    @DisplayName("왼쪽이 이미 연결되어 있다면 그 다음엔 연결하지 않는다.")
+    void LEFT_ALREADY_CONNECTED_THEN_NOT_CONNECTED() {
+        Point point = Point.of(true, false);
+        assertThat(Point.first(() -> true).next(() -> true)).isEqualTo(point);
     }
 
     @Test
-    @DisplayName("연결 여부를 나타내는 Point 객체를 생성하는 테스트")
-    void GENERATE_POINT_TEST() {
-        assertThat(Point.generatePoint(() -> true).isConnected()).isEqualTo(true);
-        assertThat(Point.generatePoint(() -> false).isConnected()).isEqualTo(false);
+    @DisplayName("왼쪽 오른쪽 모두 연결되어 있을 수 없다.")
+    void VALID_POINT() {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> Point.of(true, true));
+    }
+
+    @Test
+    @DisplayName("왼쪽과 연결되어 있으면 왼쪽으로 이동한다.")
+    void MOVE_LEFT() {
+        Point point = Point.first(() -> true).next(() -> false);
+        assertThat(point.move()).isEqualTo(LEFT);
+    }
+
+    @Test
+    @DisplayName("오른쪽과 연결되어 있으면 오른쪽으로 이동한다.")
+    void MOVE_RIGHT() {
+        Point point = Point.first(() -> false).next(() -> true);
+        assertThat(point.move()).isEqualTo(RIGHT);
+    }
+
+    @Test
+    @DisplayName("연결되어 있지 않으면 그대로 지나간다.")
+    void MOVE_PASS() {
+        Point point = Point.first(() -> false);
+        assertThat(point.move()).isEqualTo(PASS);
     }
 
 }
