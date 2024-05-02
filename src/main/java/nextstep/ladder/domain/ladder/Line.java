e78a4444acbbe506af161369db804a5f4c3db4df
유지웅 <poujis0@gmail.com>
2024-05-03 01:15:51 +0900

    [refactor] (InOut) Line 클래스 리팩토링

--- src/main/java/nextstep/ladder/domain/ladder/Line.java
+++ src/main/java/nextstep/ladder/domain/ladder/Line.java
@@ -1,78 +1,58 @@
 package nextstep.ladder.domain.ladder;
 
-import static nextstep.ladder.domain.ladder.Point.generatePoint;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import nextstep.ladder.domain.generator.RandomPointGenerator;
 
 public class Line {
 
-    private static final int NEXT_INDEX = 1;
-    private final List<Point> points;
-
-    private Line(List<Point> points) {
-        if (validAvailableLine(points)) {
-            throw new IllegalArgumentException("라인이 겹칩니다.");
-        }
-        this.points = points;
-    }
-
-    public static Line of(List<Point> points) {
-        return new Line(points);
-    }
+    private final List<Cross> crosses;
 
     public static Line of(Boolean... points) {
-        return new Line(
-            Arrays.stream(points)
-                .map(Point::of)
-                .collect(Collectors.toList())
-        );
+        List<Cross> crosses = new ArrayList<>();
+        Point point = Point.first(() -> points[0]);
+        crosses.add(new Cross(0, point));
+
+        for (int i = 1; i < points.length; i++) {
+            point = point.next(points[i]);
+            crosses.add(new Cross(i, point));
+        }
+
+        crosses.add(new Cross(points.length, point.last()));
+        return new Line(crosses);
+    }
+
+    private Line(List<Cross> crosses) {
+        this.crosses = crosses;
+    }
+
+    public List<Cross> getCrosses() {
+        return crosses;
     }
 
     public List<Point> getPoints() {
-        return points;
+        return crosses.stream().map((Cross::getPoint)).collect(Collectors.toList());
     }
 
-    public static Line generateLine(int userCount) {
-        List<Point> points = new ArrayList<>();
-
-        IntStream.range(0, userCount - 1)
-            .forEach(idx -> addAvailablePoint(points, idx));
-
-        return Line.of(points);
+    public int move(int position) {
+        return crosses.get(position).move();
     }
 
-    public int move(int index) {
-        if (index != 0 && points.get(index - 1).isConnected()) {
-            return -1;
-        }
-        if (index != points.size() && points.get(index).isConnected()) {
-            return 1;
-        }
-        return 0;
-    }
+    public static Line generate(int userCount) {
+        List<Cross> crosses = new ArrayList<>();
 
-    private boolean validAvailableLine(List<Point> points) {
-        return IntStream.range(0, points.size() - 1)
-            .anyMatch(idx -> points.get(idx).isConnected() && points.get(idx + 1).isConnected());
-    }
-
-    private static void addAvailablePoint(List<Point> points, int idx) {
-        if (idx == 0) {
-            points.add(generatePoint(new RandomPointGenerator()));
-            return;
+        Cross cross = Cross.first();
+        crosses.add(cross);
+        for (int i = 1; i < userCount; i++) {
+            if (i == userCount - 1) {
+                cross = cross.last();
+            } else {
+                cross = cross.next();
+            }
+            crosses.add(cross);
         }
-
-        if (!points.get(idx - 1).isConnected()) {
-            points.add(generatePoint(new RandomPointGenerator()));
-            return;
-        }
-
-        points.add(Point.of(false));
+        return new Line(crosses);
     }
 
 }
