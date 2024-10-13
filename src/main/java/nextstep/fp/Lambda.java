694086269f72d34117c4f9b23b5232ecfd3ac6ef
skdev0619 <skdev0619@naver.com>
2024-10-13 17:31:09 +0900

    람다 실습 2 - 람다를 활용해 중복 제거

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.IntPredicate;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
@@ -26,31 +27,28 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
+    public static int sumAll(List<Integer> numbers, IntPredicate condition) {
         int total = 0;
         for (int number : numbers) {
-            total += number;
+            total = sumByCondition(number, total, condition);
         }
         return total;
     }
 
-    public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+    public static int sumAllEven(List<Integer> numbers, IntPredicate condition) {
+        //if (number % 2 == 0)
+        return sumAll(numbers, condition);
     }
 
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
+    public static int sumAllOverThree(List<Integer> numbers, IntPredicate condition) {
+        //if (number > 3)
+        return sumAll(numbers, condition);
+    }
+
+    private static int sumByCondition(int number, int totalSum, IntPredicate condition) {
+        if(condition.test(number)){
+            totalSum += number;
         }
-        return total;
+        return totalSum;
     }
 }
