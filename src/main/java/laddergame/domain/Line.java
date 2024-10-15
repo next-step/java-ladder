c5ab49de365874b5c473c0e37bd066bb29ed9f20
skdev0619 <skdev0619@naver.com>
2024-10-15 13:40:42 +0900

    라인의 가로는 옆 라인과 겹칠 수 없다

--- src/main/java/laddergame/domain/Line.java
+++ src/main/java/laddergame/domain/Line.java
@@ -2,21 +2,47 @@ package laddergame.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Line {
-    private List<Boolean> points = new ArrayList<>();
+    private List<Boolean> points;
 
     public Line(int playerCount) {
         initialize(playerCount - 1);
     }
 
+    public Line(List<Boolean> points) {
+        this.points = points;
+        validate();
+    }
+
     private void initialize(int width) {
+        this.points = new ArrayList<>();
         for (int i = 0; i < width; i++) {
             points.add(Boolean.FALSE);
         }
     }
 
+    private void validate() {
+        boolean hasOverlap = IntStream
+                    .range(0, points.size())
+                    .anyMatch(this::hasBothHorizontalLine);
+
+        if(hasOverlap){
+            throw new IllegalArgumentException("라인의 가로는 옆 라인과 겹칠 수 없습니다");
+        }
+    }
+
+    private boolean hasBothHorizontalLine(int index) {
+        return points.get(index) && points.get(index + 1);
+    }
+
+
     public int size(){
         return points.size();
+    }
+
+    public List<Boolean> getPoints() {
+        return points;
     }
 }
--- src/test/java/laddergame/domain/LineTest.java
+++ src/test/java/laddergame/domain/LineTest.java
@@ -3,7 +3,10 @@ package laddergame.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class LineTest {
 
@@ -15,5 +18,15 @@ class LineTest {
         Line line = new Line(playerCount);
 
         assertThat(line.size()).isEqualTo(2);
+    }
+
+    @DisplayName("라인의 가로는 옆 라인과 겹칠 수 없다")
+    @Test
+    void validateLine(){
+        List<Boolean> points = List.of(true, true, false);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Line(points))
+                .withMessage("라인의 가로는 옆 라인과 겹칠 수 없습니다");
     }
 }
