534309144bb9a7cf49472a7bcd30837d6db20fda
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-15 20:49:11 +0900

    refactor: map, reduce, filter 실습 1, 2

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
+
+    private final static int WORD_MAX = 100;
+    private final static int WORD_MINIMUM_LENGTH = 12;
 
     public static long countWords() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
@@ -22,12 +24,24 @@ public class StreamStudy {
         return count;
     }
 
-    public static void printLongestWordTop100() throws IOException {
+    public static List<String> printLongestWordTop100() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        Set<String> wordsSet = words.stream()
+                .filter(word -> word.length() > WORD_MINIMUM_LENGTH)
+                .map(String::toLowerCase).sorted()
+                .distinct()
+                .limit(WORD_MAX)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        return words.stream()
+                .filter(word -> word.length() > WORD_MINIMUM_LENGTH)
+                .map(String::toLowerCase).sorted()
+                .distinct()
+                .limit(WORD_MAX)
+                .collect(Collectors.toList());
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -35,10 +49,13 @@ public class StreamStudy {
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers.stream().reduce(0, Integer::sum);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(num -> num > 3)
+                .map(num -> num * 2)
+                .reduce(0, Integer::sum);
     }
 }
