4863cc02b74b1b321799d4b1c53a8cd544f5d9d1
Hoseop.Kim <kimho314@gmail.com>
2024-10-17 19:56:26 +0900

    [feat] :  getVersionOptional 구현

--- src/main/java/nextstep/optional/ComputerStore.java
+++ src/main/java/nextstep/optional/ComputerStore.java
@@ -3,6 +3,9 @@ package nextstep.optional;
 import nextstep.optional.Computer.Soundcard;
 import nextstep.optional.Computer.USB;
 
+import java.util.Objects;
+import java.util.Optional;
+
 public class ComputerStore {
     public static final String UNKNOWN_VERSION = "UNKNOWN";
 
@@ -21,6 +24,10 @@ public class ComputerStore {
     }
 
     public static String getVersionOptional(Computer computer) {
-        return null;
+        return Optional.ofNullable(computer)
+                .map(Computer::getSoundcard)
+                .map(Soundcard::getUsb)
+                .map(USB::getVersion)
+                .orElse(UNKNOWN_VERSION);
     }
 }
