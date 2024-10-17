4a8287434c7ed4d61fd93a53d2292f67f355bfec
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-10-17 13:23:51 +0900

    feat: players로 참가자 검증 추가
    players로 참가자 검증 추가

---
+++ src/main/java/ladder/Players.java
@@ -0,0 +1,41 @@
+package ladder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Players {
+
+    private final List<Player> players;
+
+    public Players(String... players) {
+        this(Arrays.stream(players).map(Player::new).collect(Collectors.toList()));
+    }
+
+    public Players(List<Player> players) {
+        validSize(players);
+        this.players = players;
+    }
+
+    private void validSize(List<Player> players) {
+        if (players.size() < 2) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Players))
+            return false;
+        Players that = (Players)o;
+        return Objects.equals(players, that.players);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(players);
+    }
+}
