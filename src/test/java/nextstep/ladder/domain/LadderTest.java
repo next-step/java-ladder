a2178bc4c04eb660631ae3dbe3eab6470291a00f
Byungil Sohn <skylord418@gmail.com>
2024-10-18 20:32:30 +0900

    fix : fix compile error in LadderTest

--- src/test/java/nextstep/ladder/domain/LadderTest.java
+++ src/test/java/nextstep/ladder/domain/LadderTest.java
@@ -10,6 +10,6 @@ public class LadderTest {
     void create() {
         Ladder ladder = Ladder.of(5, 4);
 
-        assertThat(ladder.ladderCount()).isEqualTo(5);
+        assertThat(ladder.count()).isEqualTo(5);
     }
 }
