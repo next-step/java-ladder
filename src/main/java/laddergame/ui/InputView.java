2a791ca746a0ec66d238e75ac6cbbe9902130d4c
skdev0619 <skdev0619@naver.com>
2024-10-19 02:40:51 +0900

    사다리 실행 결과를 보여줄 플레이어 1명의 이름 또는 플레이어 전원(all)을 입력한다

--- src/main/java/laddergame/ui/InputView.java
+++ src/main/java/laddergame/ui/InputView.java
@@ -14,6 +14,8 @@ public class InputView {
     private static final String PLAYER_INPUT_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String GAME_RESULT_INPUT_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String MAX_LADDER_HEIGHT_INPUT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    private static final String RESULT_PLAYER_INPUT_MESSAGE = "결과를 보고 싶은 사람은?";
+
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public List<Player> getPlayerFromUser(){
@@ -67,10 +69,21 @@ public class InputView {
         return gameResults;
     }
 
-
-
     public int getMaxLadderHeightFromUser(){
         System.out.println(MAX_LADDER_HEIGHT_INPUT_MESSAGE);
-        return SCANNER.nextInt();
+        int maxheight = SCANNER.nextInt();
+        SCANNER.nextLine(); //nextInt() 사용으로 인한 개행 문자 제거
+        return maxheight;
+    }
+
+    public String getResultPlayerFromUser(){
+        System.out.println();
+        System.out.println(RESULT_PLAYER_INPUT_MESSAGE);
+        String playerName = SCANNER.nextLine();
+
+        if(playerName.isBlank()){
+            throw new IllegalArgumentException("결과를 보고 싶은 사람의 이름을 입력하세요");
+        }
+        return playerName;
     }
 }
