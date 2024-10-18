66de459ed9be16ff47581e6564d4f6a1c85e1492
skdev0619 <skdev0619@naver.com>
2024-10-18 17:44:40 +0900

    플레이어는 사다리의 시작 위치 정보를 가지고 있다

--- src/main/java/laddergame/domain/Player.java
+++ src/main/java/laddergame/domain/Player.java
@@ -4,12 +4,18 @@ public class Player {
     private static final int NAME_LIMIT = 5;
 
     private final String name;
+    private Position position;
 
-    public Player(String name) {
+    public Player(String name, int position) {
+        this(name, new Position(position));
+    }
+
+    public Player(String name, Position position) {
         if(name.length() > NAME_LIMIT){
             throw new IllegalArgumentException(String.format("플레이어의 이름을 %d자 이하로 입력해주세요.", NAME_LIMIT));
         }
         this.name = name;
+        this.position = position;
     }
 
     public String getName() {
--- src/main/java/laddergame/ui/InputView.java
+++ src/main/java/laddergame/ui/InputView.java
@@ -2,33 +2,42 @@ package laddergame.ui;
 
 import laddergame.domain.Player;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.toList;
 
 public class InputView {
-    private  static final String PLAYER_NAME_SEPERATOR = ",";
+    private  static final String INPUT_DELIMITER = ",";
     private static final String PLAYER_INPUT_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String MAX_LADDER_HEIGHT_INPUT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public List<Player> getPlayerFromUser(){
+        String playerInput = getPlayerInput();
+        String[] playerNames = split(playerInput);
+        return getPlayers(playerNames);
+    }
+
+    private static String getPlayerInput() {
         System.out.println(PLAYER_INPUT_MESSAGE);
         String players = SCANNER.nextLine();
 
         if(players.isBlank()){
             throw new IllegalArgumentException("사다리 게임에 참여할 사람 이름을 입력해주세요");
         }
+        return players;
+    }
 
-        return Arrays.stream(splitNames(players))
-                .map(Player::new)
+    private List<Player> getPlayers(String[] playerNames) {
+        return IntStream.range(0, playerNames.length)
+                .mapToObj(index -> new Player(playerNames[index], index))
                 .collect(toList());
     }
 
-    private String[] splitNames(String playerNames){
-        return playerNames.replace(" ", "").split(PLAYER_NAME_SEPERATOR);
+    private String[] split(String joiningTest){
+        return joiningTest.replace(" ", "").split(INPUT_DELIMITER);
     }
 
     public int getMaxLadderHeightFromUser(){
