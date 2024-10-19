2e9e81154b79b2bfc5021942ec80a38d61074e77
이윤선 <yoonseon3@gmail.com>
2024-10-20 05:08:25 +0900

    chore: DisplayName 성공 or 실패 구분 추가

--- src/test/java/nextstep/ladder/domain/LadderRowTest.java
+++ src/test/java/nextstep/ladder/domain/LadderRowTest.java
@@ -7,14 +7,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LadderRowTest {
     @Test
-    @DisplayName("LadderRow 객체를 생성할 때 주어진 넓이 만큼 사다리를 넓이를 생성한다.")
+    @DisplayName("성공 - LadderRow 객체를 생성할 때 주어진 넓이 만큼 사다리를 넓이를 생성한다.")
     void initTest() {
         LadderRow ladderRow = new LadderRow(new PositiveNumber(5), () -> true);
         assertThat(ladderRow.getRow()).hasSize(5);
     }
 
     @Test
-    @DisplayName("getBar 메서드가 이전의 사다리 bar가 비어있으면 새로운 bar를 생성시킨다.")
+    @DisplayName("성공 - getBar 메서드가 이전의 사다리 bar가 비어있으면 새로운 bar를 생성시킨다.")
     void generateLineTest() {
         LadderRow ladderRow = new LadderRow(new PositiveNumber(5), () -> true);
         assertThat(ladderRow.getRow().get(0)).isEqualTo("     ");
@@ -25,7 +25,7 @@ class LadderRowTest {
     }
 
     @Test
-    @DisplayName("getBar 메서드가 첫 번째 Bar를 항상 비우고 생성한다.")
+    @DisplayName("성공 - getBar 메서드가 첫 번째 Bar를 항상 비우고 생성한다.")
     void shouldCreateBarWithFirstBarEmpty() {
         LadderRow ladderRow1 = new LadderRow(new PositiveNumber(5), () -> true);
         LadderRow ladderRow2 = new LadderRow(new PositiveNumber(5), () -> false);
