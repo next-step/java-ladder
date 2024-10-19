71d79cdf9c9630ce71dc1540eb43e97d6b85ba7f
이윤선 <yoonseon3@gmail.com>
2024-10-20 02:03:00 +0900

    test: getUsers 메서드가 반환된 List를 수정했을 때 예외가 발생한다.

--- src/test/java/nextstep/ladder/domain/UsersTest.java
+++ src/test/java/nextstep/ladder/domain/UsersTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class UsersTest {
     @Test
@@ -19,5 +20,16 @@ class UsersTest {
                 .hasSize(3)
                 .extracting(User::getName)
                 .containsExactly("홍길동", "전우치", "고길동");
+    }
+
+    @Test
+    @DisplayName("실패 - getUsers 메서드가 반환된 List를 수정했을 때 예외가 발생한다.")
+    void throwExceptionWhenModifyingReturnedList() {
+        List<User> initUsers = List.of(new User("홍길동"),
+                new User("전우치"),
+                new User("고길동"));
+        List<User> findUsers = new Users(initUsers).getUsers();
+        assertThatThrownBy(() -> findUsers.add(new User("둘리")))
+                .isInstanceOf(UnsupportedOperationException.class);
     }
 }
