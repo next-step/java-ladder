754fe4ffe4b849f414fff507032dbd4c019ae1b3
이윤선 <yoonseon3@gmail.com>
2024-10-20 05:06:05 +0900

    test: getBar 메서드가 첫 번째 Bar를 항상 비우고 생성한다.

--- src/test/java/nextstep/ladder/domain/LadderRowTest.java
+++ src/test/java/nextstep/ladder/domain/LadderRowTest.java
@@ -23,4 +23,13 @@ class LadderRowTest {
         assertThat(ladderRow.getRow().get(3)).isEqualTo("-----");
         assertThat(ladderRow.getRow().get(4)).isEqualTo("     ");
     }
+
+    @Test
+    @DisplayName("getBar 메서드가 첫 번째 Bar를 항상 비우고 생성한다.")
+    void shouldCreateBarWithFirstBarEmpty() {
+        LadderRow ladderRow1 = new LadderRow(new PositiveNumber(5), () -> true);
+        LadderRow ladderRow2 = new LadderRow(new PositiveNumber(5), () -> false);
+        assertThat(ladderRow1.getRow().get(0)).isEqualTo("     ");
+        assertThat(ladderRow2.getRow().get(0)).isEqualTo("     ");
+    }
 }
