6cec7079b28d81e8744b21d905cfefb1f4594f06
joonkb <57327375+joonkb@users.noreply.github.com>
2024-10-19 17:24:42 +0900

    🚀 1단계 - 스트림, 람다, Optional 구현 및 리뷰요청 (#2183)
    * [feat] 람다식을 활용한 중복제거
    
    * [feat] sumOverThreeAndDouble 기능 구현 및 테스트
    
    * [feat] printLongestWordTop100 구현 및 테스트
    
    * [feat] 요구사항 1 구현 및 테스트
    
    * [feat] 요구사항 2-3 구현 및 테스트
    
    * [feat] getVersionOptional 구현 및 테스트

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,8 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.stream.Collectors;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
@@ -26,31 +28,18 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+    public static int sumAll(List<Integer> numbers, Conditional c) {
+        return numbers.stream()
+                .filter(c::test)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, num -> num % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, num -> num > 3);
     }
 }
--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,10 +5,13 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
+
+    public static final int WORD_MAX_LENGTH = 12;
 
     public static long countWords() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
@@ -27,7 +30,13 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(w -> w.length() > WORD_MAX_LENGTH)
+                .map(String::toLowerCase)
+                .distinct()
+                .limit(100)
+                .sorted(Comparator.comparingInt(String::length))
+                .forEachOrdered(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +48,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(x -> x > 3)
+                .mapToLong(x -> x * 2)
+                .reduce(0, (x, y) -> x + y);
     }
 }
