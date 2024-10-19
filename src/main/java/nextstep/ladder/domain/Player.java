d646e8f5983468c966e9c72b2570d9f93b56c182
Byungil Sohn <skylord418@gmail.com>
2024-10-19 22:14:43 +0900

    style : update indenting and add brackets

--- src/main/java/nextstep/ladder/domain/Player.java
+++ src/main/java/nextstep/ladder/domain/Player.java
@@ -21,8 +21,12 @@ public class Player {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Player player = (Player) o;
         return Objects.equals(name, player.name);
     }
