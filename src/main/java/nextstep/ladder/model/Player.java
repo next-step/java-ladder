5624fbfa56adff6b1d5b22f9817905a1e0e5076e
Hoseop.Kim <kimho314@gmail.com>
2024-10-20 18:49:17 +0900

    [feat] : refactor Player class

--- src/main/java/nextstep/ladder/model/Player.java
+++ src/main/java/nextstep/ladder/model/Player.java
@@ -1,11 +1,12 @@
 package nextstep.ladder.model;
 
 public class Player {
+    private static final int MAX_NAME_LENGTH = 5;
     private String name;
 
     public Player(String name) {
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("name is too long");
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("name must not be longer than 5");
         }
         this.name = name;
     }
