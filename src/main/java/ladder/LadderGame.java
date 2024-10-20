0755712dfba41cefe7b28fb68075bbbab4f7d613
green <green@mobidoc.us>
2024-10-20 15:08:55 +0900

    [step2]feat: LadderGame 라인 생성 전략 제거

--- src/main/java/ladder/LadderGame.java
+++ src/main/java/ladder/LadderGame.java
@@ -8,12 +8,10 @@ import java.util.List;
 public class LadderGame {
     private final InputView inputView;
     private final ResultView resultView;
-    private final LineGenerateStrategy lineGenerateStrategy;
 
     public LadderGame(InputView inputView, ResultView resultView) {
         this.inputView = inputView;
         this.resultView = resultView;
-        this.lineGenerateStrategy = new RandomLineGenerator();
     }
 
     public void run() {
