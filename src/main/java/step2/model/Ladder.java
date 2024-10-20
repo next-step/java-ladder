b2511869aa4311ed990b4f59084203e23a9af56a
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-20 20:34:17 +0900

    feat: 참여인원 이름길이 확인 메소드

--- src/main/java/step2/model/Ladder.java
+++ src/main/java/step2/model/Ladder.java
@@ -10,9 +10,11 @@ public class Ladder {
 
     private final static String MINIMUM_PERSON_COUNT_MESSAGE = "참여 인원은 2명이상이여야 합니다.";
     private final static String MINIMUM_LADDER_COUNT_MESSAGE = "사다리의 높이는 2이상이여여만 합니다.";
+    private final static String MAXIMUM_PERSON_NAME_LENGTH_EXCEPTION = "참여 인원의 이름은 5자를 초과하면 안됩니다.";
 
     private final static int MINIMUM_PERSON_COUNT = 2;
     private final static int MINIMUM_LADDER_COUNT = 2;
+    private final static int MAXIMUM_PERSON_NAME_LENGTH = 5;
 
     private String[] person;
     private final List<Line> line = new ArrayList<>();
@@ -21,6 +23,11 @@ public class Ladder {
         String[] personArr = StringUtil.divideNum(personStr);
         confirmPersonCount(personArr);
         confirmLadderCount(ladderSize);
+
+        for(String person : personArr) {
+            confirmPersonNameLength(person);
+        }
+
         this.person = personArr;
         for (int i = 0; i < ladderSize; i++) {
             line.add(Line.createLine(personArr, new LadderGenerator()));
@@ -39,6 +46,13 @@ public class Ladder {
     private void confirmPersonCount(String[] personArr) {
         if (personArr.length < MINIMUM_PERSON_COUNT) {
             throw new IllegalArgumentException(MINIMUM_PERSON_COUNT_MESSAGE);
+        }
+    }
+
+    //참여하는 인원 이름길이를 체크한다.
+    private void confirmPersonNameLength(String person) {
+        if (person.length() > MAXIMUM_PERSON_NAME_LENGTH) {
+            throw new IllegalArgumentException(MAXIMUM_PERSON_NAME_LENGTH_EXCEPTION);
         }
     }
 
