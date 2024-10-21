9a0de78a8fb862523493552af718165ded15c672
heechul <khc41@lgensol.com>
2024-10-21 14:28:49 +0900

    feat: 사다리 게임 진행 및 결과 반환 로직 생성

--- src/main/java/ladder/service/LadderGame.java
+++ src/main/java/ladder/service/LadderGame.java
@@ -1,9 +1,12 @@
 package ladder.service;
 
 import ladder.domain.CreateStrategy;
+import ladder.domain.LadderResult;
 import ladder.domain.Line;
 import ladder.domain.Member;
+import ladder.domain.Reword;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -26,4 +29,27 @@ public class LadderGame {
                 .mapToObj(num -> new Line(members.size(), createStrategy))
                 .collect(Collectors.toList());
     }
+
+    public LadderResult playLadders(List<Line> ladders, List<Member> members, List<Reword> rewords) {
+        List<Integer> point = getResultPoints(ladders, members.size());
+        return new LadderResult(getResultMap(members, rewords, point));
+    }
+
+    private static List<Integer> getResultPoints(List<Line> ladders, int memberCount) {
+        List<Integer> point = IntStream.range(0, memberCount)
+                .boxed()
+                .collect(Collectors.toList());
+        for (Line ladder : ladders) {
+            point = ladder.moveResult(point);
+        }
+        return point;
+    }
+
+    private static LinkedHashMap<Member, Reword> getResultMap(List<Member> members, List<Reword> rewords, List<Integer> point) {
+        LinkedHashMap<Member, Reword> map = new LinkedHashMap<>();
+        IntStream.range(0, members.size())
+                .forEach(index -> map.put(members.get(index), rewords.get(point.get(index))));
+        return map;
+    }
+
 }
--- src/test/java/ladder/domain/LadderGameTest.java
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -11,6 +11,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LadderGameTest {
 
     @Test
+    @DisplayName("사다리 게임을 진행한다.")
+    void 사다리게임_진행() {
+        LadderGame game = LadderGame.getInstance();
+        List<Member> members = List.of(new Member("asd"), new Member("hee"));
+        List<Reword> rewords = List.of(new Reword("5000"), new Reword("4000"));
+        List<Line> ladders = game.createLadders(members, 3, () -> true);
+        LadderResult results = game.playLadders(ladders, members, rewords);
+        assertThat(results.getSize()).isEqualTo(2);
+        assertThat(results.getReword(new Member("asd"))).isEqualTo(new Reword("4000"));
+        assertThat(results.getReword(new Member("hee"))).isEqualTo(new Reword("5000"));
+    }
+
+    @Test
     @DisplayName("참여자와 높이를 입력하면 그에 맞는 사다리를 생성한다.")
     void 사다리_생성() {
         List<Line> ladders = LadderGame.getInstance().createLadders(List.of(new Member("asd"), new Member("hee")), 5, () -> true);
