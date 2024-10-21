6c9fbb2f78a2c452fafebb21816258be5c536e0a
heechul <khc41@lgensol.com>
2024-10-21 09:00:04 +0900

    refactor: Member 정상 케이스 테스트 작성

--- src/test/java/ladder/domain/MemberTest.java
+++ src/test/java/ladder/domain/MemberTest.java
@@ -1,15 +1,23 @@
 package ladder.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class MemberTest {
 
     @Test
+    @DisplayName("생성자로 이름을 생성한다.")
+    void 이름_생성() {
+        assertThat(new Member("asda").getName()).isEqualTo("asda");
+    }
+
+    @Test
     @DisplayName("이름이 5자 초과일 경우 예외를 발생시킨다.")
     void 이름이5자초과() {
-        Assertions.assertThatThrownBy(() -> new Member("asdasd"))
+        assertThatThrownBy(() -> new Member("asdasd"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
