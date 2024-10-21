7642a456237a8e2883ca80b13ff05ab97abeb485
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-21 18:55:00 +0900

    test: 테스트코드

--- src/test/java/step2/model/LadderTest.java
+++ src/test/java/step2/model/LadderTest.java
@@ -2,6 +2,9 @@ package step2.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step2.ganerator.RandomGenerator;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -9,6 +12,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 /*
 - 참여할 사람 이름을 입력받는다.
 - 최대 사다리의 높이를 입력받는다.
+- 사다리를 생성한다.
 - 최대 사다리의 높이를 입력을 시 사다리는 높이는 2이상이여야한다.
 */
 public class LadderTest {
@@ -16,9 +20,10 @@ public class LadderTest {
     @DisplayName("참여할 사람 이름을 입력받는다.")
     @Test
     void inputPersonTest() {
-        Ladder ladder = new Ladder();
+        RandomGenerator randomGenerator = () -> true;
+        Ladder ladder = new Ladder(randomGenerator);
         String person = "pobi,honux,crong,jk";
-        ladder.inputPerson(person, 5);
+        ladder.createLadder(person, 5);
         assertThat(ladder.getPerson().getNames())
                 .extracting("name")
                 .containsExactly("pobi", "honux", "crong", "jk");
@@ -27,18 +32,38 @@ public class LadderTest {
     @DisplayName("최대 사다리의 높이를 입력받는다.")
     @Test
     void inputMaxLadderTest() {
-        Ladder ladder = new Ladder();
+        RandomGenerator randomGenerator = () -> true;
+        Ladder ladder = new Ladder(randomGenerator);
         String person = "pobi,honux,crong,jk";
-        ladder.inputPerson(person, 5);
+        ladder.createLadder(person, 5);
         assertThat(ladder.getLine().size()).isEqualTo(5);
+    }
+
+    @DisplayName("사다리를 생성한다.")
+    @Test
+    void createLadderTest() {
+        RandomGenerator randomGenerator = () -> true;
+        Ladder ladder = new Ladder(randomGenerator);
+        String person = "pobi,honux,crong,jk";
+        ladder.createLadder(person, 5);
+        assertThat(ladder.getLine())
+                .extracting(Line::getPoints)
+                .containsExactly(
+                        List.of(false, true, false, true),
+                        List.of(false, true, false, true),
+                        List.of(false, true, false, true),
+                        List.of(false, true, false, true),
+                        List.of(false, true, false, true)
+                );
     }
 
     @DisplayName("최대 사다리의 높이를 입력을 시 사다리의 높이는 2이상이여야한다.")
     @Test
     void inputMaxLadderMustOverTwoTest() {
-        Ladder ladder = new Ladder();
+        RandomGenerator randomGenerator = () -> true;
+        Ladder ladder = new Ladder(randomGenerator);
         String person = "pobi,honux,crong,jk";
-        assertThatThrownBy(() -> ladder.inputPerson(person, 1))
+        assertThatThrownBy(() -> ladder.createLadder(person, 1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("사다리의 높이는 2이상이여여만 합니다.");
     }
--- src/test/java/step2/model/LineTest.java
+++ src/test/java/step2/model/LineTest.java
@@ -9,17 +9,20 @@ import step2.util.StringUtil;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /*
-- 사다리 한줄 생성할 시 첫번째 계단은 무조건 false
+- 사다리 한줄 생성할 시 첫번째 계단은 무조건 false 이고 각 라인은 겹치지않는다.
 - 인원수대로 라인을 생성한다.
 */
 public class LineTest {
 
-    @DisplayName("사다리 한줄 생성할 시 첫번째 계단은 무조건 false")
+    @DisplayName("사다리 한줄 생성할 시 첫번째 계단은 무조건 false 이고 이고 각 라인은 겹치지않는다.")
     @Test
     void createLineTest() {
         String[] personArr = StringUtil.divideNum("pobi,honux,crong,jk");
+
         RandomGenerator randomGenerator = () -> true;
+
         Line line = Line.createLine(Person.createPerson(personArr), randomGenerator);
+
         assertThat(line.getPoints()).containsExactly(false, true, false, true);
     }
 
