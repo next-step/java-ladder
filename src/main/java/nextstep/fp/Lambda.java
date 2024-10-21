7b06e927428a6cc1fbdff4c707f4fa11d59a15eb
hwan2-99 <93575221+hwan2-99@users.noreply.github.com>
2024-10-21 17:57:47 +0900

    Step1: 스트림, 람다, Optional 미션 제출합니다! (#2197)
    * test: 자동차 전진 테스트 조건 람다식으로 수정
    
    * refactor: 덧셈계산기 조건 중복되는 부분 람다식으로 표현
    
    * refactor: 덧셈계산기 조건 중복되는 부분 람다식으로 표현
    
    * feat: map, reduce, filter 실습 1 구현
    
    * refactor: 요구사항 1 - Optional을 활용해 조건에 따른 반환
    
    * refactor: 요구사항 2 - Optional에서 값을 반환
    
    * refactor: 요구사항 3 - Optional에서 exception 처리

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.IntPredicate;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
@@ -27,30 +28,21 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumByCondition(numbers, number -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumByCondition(numbers, n -> n % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumByCondition(numbers, number -> number > 3);
+    }
+
+    private static int sumByCondition(List<Integer> numbers, IntPredicate condition) {
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .filter(condition)
+                .sum();
     }
 }
