f59d056b1c9c8dd27eaccf9b5b9464df6173a4f4
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-10-21 14:41:56 +0900

    feat: 다음 위치 계산하는 로직 추가
    다음 위치 계산하는 로직 추가

--- src/main/java/ladder/Line.java
+++ src/main/java/ladder/Line.java
@@ -21,6 +21,42 @@ public class Line {
         this.points = lineGenerator.run(ladderSize);
     }
 
+    public int findNextPoint(int startPoint) {
+        if (isFirst(startPoint)) {
+            return checkHorizon(0);
+        }
+        if (isLast(startPoint)) {
+            return -checkHorizon(startPoint - 1);
+        }
+        if (findHorizon(startPoint - 1)) {
+            return -checkHorizon(startPoint - 1);
+        }
+        if (findHorizon(startPoint)) {
+            return checkHorizon(startPoint);
+        }
+
+        return startPoint;
+    }
+
+    private boolean findHorizon(int point) {
+        return points.get(point);
+    }
+
+    private int checkHorizon(int prevPoint) {
+        if (points.get(prevPoint)) {
+            return 1;
+        }
+        return 0;
+    }
+
+    private boolean isFirst(int startPoint) {
+        return startPoint == 0;
+    }
+
+    private boolean isLast(int startPoint) {
+        return startPoint == points.size();
+    }
+
     public List<Boolean> getPoints() {
         return points;
     }
