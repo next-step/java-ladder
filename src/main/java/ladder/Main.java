ab051e2c8dc0979b51708690e5c9da57f18b6dec
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-10-21 15:18:40 +0900

    feat: 결과 출력 실행부분 추가
    결과 출력 실행부분 추가

--- src/main/java/ladder/Main.java
+++ src/main/java/ladder/Main.java
@@ -4,14 +4,27 @@ import ladder.view.InputView;
 import ladder.view.OutputView;
 
 public class Main {
+
+    public static final String CHECK_ALL = "all";
+
     public static void main(String[] args) {
         Players players = InputView.inputPlayers();
-        LadderResult ladderResult = InputView.inputResult();
-        ladderResult.validSize(players.size());
+        InputResult inputResult = InputView.inputResult();
+        inputResult.validSize(players.size());
         int height = InputView.inputHeight();
 
         Ladder ladder = new Ladder(height, players.size());
+        LadderResult ladderResult = ladder.run(players, inputResult);
 
-        OutputView.outputLadderGameResult(players, ladder, ladderResult);
+        OutputView.outputLadderGame(players, ladder, inputResult);
+
+        String personName = InputView.wantPerson();
+        while (!personName.equals(CHECK_ALL)) {
+            System.out.println();
+            OutputView.outputOneResult(personName, ladderResult);
+            personName = InputView.wantPerson();
+        }
+        System.out.println();
+        OutputView.outputAllResult(ladderResult);
     }
 }
