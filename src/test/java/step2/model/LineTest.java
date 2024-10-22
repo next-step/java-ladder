ca0141aa13a3ee8c9a7b96aab9a789d766d4fed6
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-22 12:48:03 +0900

    test: 테스트 코드

--- src/test/java/step2/model/LineTest.java
+++ src/test/java/step2/model/LineTest.java
@@ -2,26 +2,24 @@ package step2.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import step2.ganerator.LadderGenerator;
 import step2.ganerator.RandomGenerator;
-import step2.util.StringUtil;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /*
 - 사다리 한줄 생성할 시 첫번째 계단은 무조건 false 이고 각 라인은 겹치지않는다.
 - 인원수대로 라인을 생성한다.
+- 좌우로 살피고 true인 라인으로 옮긴다.
 */
 public class LineTest {
 
     @DisplayName("사다리 한줄 생성할 시 첫번째 계단은 무조건 false 이고 이고 각 라인은 겹치지않는다.")
     @Test
     void createLineTest() {
-        String[] personArr = StringUtil.divideNum("pobi,honux,crong,jk");
-
-        RandomGenerator randomGenerator = () -> true;
-
-        Line line = Line.createLine(Person.createPerson(personArr), randomGenerator);
+        Line line = Line.createLine(Person.createPerson("pobi,honux,crong,jk"), () -> true);
 
         assertThat(line.getPoints()).containsExactly(false, true, false, true);
     }
@@ -29,8 +27,15 @@ public class LineTest {
     @DisplayName("인원수대로 라인을 생성한다.")
     @Test
     void createLinePersonTest() {
-        String[] personArr = StringUtil.divideNum("pobi,honux,crong,jk");
-        Line line = Line.createLine(Person.createPerson(personArr), new LadderGenerator());
+        Line line = Line.createLine(Person.createPerson("pobi,honux,crong,jk"), new LadderGenerator());
         assertThat(line.getPoints().size()).isEqualTo(4);
+    }
+
+    @DisplayName("좌우로 살피고 true인 라인으로 옮긴다.")
+    @ParameterizedTest
+    @CsvSource(value = {"0:1", "1:0"}, delimiter = ':')
+    void getLineForwardTest(int currentIndex, int endIndex ) {
+        Line line = Line.createLine(Person.createPerson("pobi,honux,crong,jk"), () -> true);
+        assertThat(line.getLineForward(currentIndex)).isEqualTo(endIndex);
     }
 }
--- src/test/java/step2/model/PersonTest.java
+++ src/test/java/step2/model/PersonTest.java
@@ -2,7 +2,6 @@ package step2.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import step2.util.StringUtil;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -10,13 +9,15 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 /*
 - 참여할 사람들을 생성한다.
 - 참여할 사람 이름을 입력받을 시 최소 2명이상이여야한다.
+- 이름을 입력시 몇번째 사다리인지 반환한다.
+- 이름을 입력시 존재하지 않는 사람이면 예외를 발생시킨다.
 */
 public class PersonTest {
 
     @DisplayName("참여할 사람들을 생성한다.")
     @Test
     void createPersonTest() {
-        Person person = Person.createPerson(StringUtil.divideNum("pobi,honux,crong,jk"));
+        Person person = Person.createPerson("pobi,honux,crong,jk");
         assertThat(person.getNames())
                 .extracting("name")
                 .containsExactly("pobi", "honux", "crong", "jk");
@@ -25,9 +26,27 @@ public class PersonTest {
     @DisplayName("참여할 사람 이름을 입력받을 시 최소 2명이상이여야한다.")
     @Test
     void createPersonMustOverTwoTest() {
-        assertThatThrownBy(() -> Person.createPerson(StringUtil.divideNum("pobi")))
+        assertThatThrownBy(() -> Person.createPerson("pobi"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("참여 인원은 2명이상이여야 합니다.");
+    }
+
+    @DisplayName("이름을 입력시 몇번째 사다리인지 반환한다.")
+    @Test
+    void getPersonIndexTest() {
+        Person person = Person.createPerson("pobi,honux,crong,jk");
+
+        assertThat(person.getPersonIndex("honux")).isEqualTo(new int[]{1});
+    }
+
+    @DisplayName("이름을 입력시 존재하지 않는 사람이면 예외를 발생시킨다.")
+    @Test
+    void getPersonIndexNotExistThrowExceptionTest() {
+        Person person = Person.createPerson("pobi,honux,crong,jk");
+
+        assertThatThrownBy(() -> person.getPersonIndex("test"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름이 일치하는 사람이 없습니다.");
     }
 
 }
