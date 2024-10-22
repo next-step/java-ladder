fd57e09e78e2e3e6f91033c6cdc62e5f7bcad829
hwan2-99 <thh1233123@gmail.com>
2024-10-22 21:23:48 +0900

    feat: 사다리 최대높이 입력기능 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,11 @@
+import ladder.domain.Ladder;
 import ladder.domain.Users;
+import ladder.strategy.RandomGenerateStrategy;
 import ladder.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
         final Users users = new Users(InputView.inputUserNames());
+        final Ladder ladder = new Ladder(InputView.maxLength(), users, new RandomGenerateStrategy());
     }
 }
--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -9,6 +9,8 @@ import ladder.domain.UserName;
 public class InputView {
     private static final String REGEX = ",|, ";
     private static final String INPUT_USERS_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String NUMBER_ERROR = "숫자만 입력가능합니다.";
+
     private static final Scanner SCANNER = new Scanner(System.in);
 
     private static String inputValue() {
@@ -20,5 +22,19 @@ public class InputView {
         return Arrays.stream(inputValue().split(REGEX))
                 .map(UserName::new)
                 .collect(Collectors.toList());
+    }
+
+    public static int maxLength() {
+        String inputValue = inputValue();
+        validateNumber(inputValue);
+        return Integer.parseInt(inputValue);
+    }
+
+    private static void validateNumber(String inputValue) {
+        try {
+            Integer.parseInt(inputValue);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMBER_ERROR);
+        }
     }
 }
