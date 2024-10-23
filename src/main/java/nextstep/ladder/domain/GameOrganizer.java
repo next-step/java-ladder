f2d7a7df40fe093821e16f5ae574d0a7c47a2a87
Byungil Sohn <skylord418@gmail.com>
2024-10-24 02:38:49 +0900

    style : update intent

--- src/main/java/nextstep/ladder/domain/GameOrganizer.java
+++ src/main/java/nextstep/ladder/domain/GameOrganizer.java
@@ -32,10 +32,12 @@ public class GameOrganizer {
     }
 
     private static List<String> toList(String strings) {
-        return Stream.of(Optional.ofNullable(strings)
-                        .filter(string -> !string.isBlank())
-                        .orElseThrow(() -> new IllegalArgumentException("문자열에 공백 또는 null은 허용되지 않습니다."))
-                        .split(DELIMITER))
+        return Stream.of(
+                        Optional.ofNullable(strings)
+                                .filter(string -> !string.isBlank())
+                                .orElseThrow(() ->
+                                        new IllegalArgumentException("문자열에 공백 또는 null은 허용되지 않습니다."))
+                                .split(DELIMITER))
                 .map(String::trim)
                 .collect(Collectors.toList());
     }
