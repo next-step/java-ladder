7386c28b06dd3668758e078f2b88502a807ec06e
seulpi <74290204+seulpi@users.noreply.github.com>
2024-10-24 09:39:29 +0900

    refactor: 람다, Optional 적용 (#2207)

--- src/test/java/nextstep/optional/UserTest.java
+++ src/test/java/nextstep/optional/UserTest.java
@@ -7,6 +7,14 @@ import static nextstep.optional.User.ageIsInRange2;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserTest {
+
+    //nextstep.optional.User의 ageIsInRange1() 메소드는 30살 이상, 45살 이하에 해당하는 User가 존재하는 경우 true를 반환하는 메소드이다.
+    //
+    //같은 기능을 Optional을 활용해 ageIsInRange2() 메소드에 구현한다. 메소드 인자로 받은 User를 Optional로 생성하면 stream의 map, filter와 같은 메소드를 사용하는 것이 가능하다.
+    //
+    //nextstep.optional.UserTest의 테스트가 모두 pass해야 한다.
+
+
     @Test
     public void whenFiltersWithoutOptional_thenCorrect() {
         assertThat(ageIsInRange1(new User("crong", 35))).isTrue();
