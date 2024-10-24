c312d0fafde07ff00a5e997625afbaadd4a0b6a6
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-24 18:55:11 +0900

    test: Point 생성 테스트코드 추가

--- src/test/java/step2/model/PointTest.java
+++ src/test/java/step2/model/PointTest.java
@@ -6,10 +6,33 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /*
+- 첫 사다리 좌우 여부를 생성한다.
+- 다음 사다리 좌우 여부를 생성한다.
 - 왼쪽으로 이동한다.
 - 오른쪽으로 이동한다.
 */
 public class PointTest {
+
+    @DisplayName("첫 사다리 좌우 여부를 생성한다.")
+    @Test
+    void firstTest() {
+        Point point = Point.first(true);
+        assertThat(point).extracting("left", "right")
+                .containsExactly(
+                        false, true
+                );
+    }
+
+    @DisplayName("다음 사다리 좌우 여부를 생성한다.")
+    @Test
+    void nextTest() {
+        Point point = Point.first(true);
+        Point nextPoint = point.next(false);
+        assertThat(nextPoint).extracting("left", "right")
+                .containsExactly(
+                        true, false
+                );
+    }
 
     @DisplayName("왼쪽으로 이동한다.")
     @Test
