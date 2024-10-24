836e8459962764f89a884c82a903b8ebd889fa6a
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-10-24 13:22:09 +0900

    feat: 참가자 객체 추가
    참가자 객체 추가

---
+++ src/main/java/ladder2/Player.java
@@ -0,0 +1,13 @@
+package ladder2;
+
+public class Player {
+
+    private final String name;
+
+    public Player(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+    }
+}
