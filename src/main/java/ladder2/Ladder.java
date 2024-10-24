9fb2e873f256a810e159aee71b2e91cb0165debb
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-10-24 15:22:32 +0900

    feat: 사다리 실행 부분 구현 추가
    사다리 실행 부분 구현 추가

--- src/main/java/ladder2/Ladder.java
+++ src/main/java/ladder2/Ladder.java
@@ -1,6 +1,9 @@
 package ladder2;
 
+import java.util.HashMap;
 import java.util.List;
+
+import ladder.InputResult;
 
 public class Ladder {
 
@@ -8,5 +11,27 @@ public class Ladder {
 
     public Ladder(List<Line> lines) {
         this.lines = lines;
+    }
+
+    public LadderResult run(Players players, InputResult inputResult) {
+        HashMap<String, String> ladderResult = new HashMap<>();
+
+        for (int startPoint = 0; startPoint < players.size(); startPoint++) {
+            int endPoint = move(startPoint);
+            ladderResult.put(players.get(startPoint), inputResult.get(endPoint));
+        }
+        return new LadderResult(ladderResult);
+    }
+
+    public int move(int position) {
+        int current = position;
+        for (Line line : lines) {
+            current = line.move(current);
+        }
+        return current;
+    }
+
+    public List<Line> getLines() {
+        return lines;
     }
 }
