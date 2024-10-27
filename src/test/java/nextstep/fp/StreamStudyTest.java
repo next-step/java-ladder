ca3b8e46ec5c5c3edb8b2e53bcec1f1bfe731343
yun <90806941+yunji1201@users.noreply.github.com>
2024-10-27 12:01:26 +0900

    1단계 - 스트림, 람다, Optional (#2226)
    * docs : 요구사항 정리
    
    * refactor : 익명 클래스를 람다로 전환 - CarTest
    
    * refactor : 람다를 활용해 중복 제거 - sumAll, sumAllEven, sumAllOverThree method
    
    * feat : map, reduce, filter 실습 1
    
    * refactor : stream 연산단계 줄이기
    
    * feat : map, reduce, filter 실습 2
    
    * feat : Optional을 활용해 조건에 따른 반환
    
    * feat : Optional에서 값을 반환
    
    * feat : Optional에서 exception 처리

--- src/test/java/nextstep/fp/StreamStudyTest.java
+++ src/test/java/nextstep/fp/StreamStudyTest.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -23,6 +24,7 @@ public class StreamStudyTest {
     }
 
     @Test
+    @DisplayName("단어 길이가 12자를 초과하는 걸 순서대로 100개 중복없이 소문자로 추출하는 테스트")
     public void printLongestWordTop100() throws Exception {
         StreamStudy.printLongestWordTop100();
     }
@@ -40,6 +42,7 @@ public class StreamStudyTest {
     }
 
     @Test
+    @DisplayName("List에 담긴 모든 숫자 중 3보다 큰 숫자를 2배 한 후 모든 값의 합을 구하는 테스트")
     public void sumOverThreeAndDouble() throws Exception {
         numbers = Arrays.asList(3, 1, 6, 2, 4, 8);
         long sum = StreamStudy.sumOverThreeAndDouble(numbers);
