159a67428b4c9ea4725b5f6c29761a823bd21ed5
Hoseop.Kim <kimho314@gmail.com>
2024-10-27 20:42:08 +0900

    [chore] : refactor RandomPointGenerator

--- src/main/java/nextstep/ladder/v2/model/RandomPointGenerator.java
+++ src/main/java/nextstep/ladder/v2/model/RandomPointGenerator.java
@@ -3,14 +3,11 @@ package nextstep.ladder.v2.model;
 import java.util.Random;
 
 public class RandomPointGenerator implements PointGenerator {
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
 
     @Override
     public boolean generate(boolean prev) {
-        // prev == true, cur == true -> false
-        // else -> random.nextBoolean()
-
-        boolean nextBoolean = random.nextBoolean();
+        boolean nextBoolean = RANDOM.nextBoolean();
         if (prev && nextBoolean) {
             return false;
         }
