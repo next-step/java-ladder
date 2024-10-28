e2ba93934c5a31e088efe021959a69c564ab36e4
owljoa <needshield@gmail.com>
2024-10-28 19:29:06 +0900

    feat: 참여자 추가 - 이름은 최대 5글자까지 허용

---
+++ src/main/java/nextstep/ladder/User.java
@@ -0,0 +1,15 @@
+package nextstep.ladder;
+
+public class User {
+    private static final int MAX_NAME_LENGTH = 5;
+
+    private final String name;
+
+    public User(String name) {
+        if (name == null || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("참여자의 이름은 최대 5글자입니다");
+        }
+
+        this.name = name;
+    }
+}
