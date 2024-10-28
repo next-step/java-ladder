8ac885ab5688f2a46b0ebbb15fb68d8091c1e8b5
JIHO CHOI <jh.choi2@okestro.com>
2024-10-28 13:24:54 +0900

    test: Line 클래스 move 메서드 테스트

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -3,6 +3,8 @@ package ladder.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
@@ -14,5 +16,29 @@ class LineTest {
         Line line = new Line(numberOfPlayers, new RandomLineGenerator());
 
         assertThat(line.points()).hasSize(numberOfPlayers - 1);
+    }
+
+    @DisplayName("왼쪽 사다리로 건너간다.")
+    @Test
+    void testMoveLeft() {
+        Line line = Line.of(List.of(true, false, false));
+
+        assertThat(line.move(1)).isEqualTo(0);
+    }
+
+    @DisplayName("오른쪽 사다리로 건너간다.")
+    @Test
+    void testMoveRight() {
+        Line line = Line.of(List.of(false, true, false));
+
+        assertThat(line.move(1)).isEqualTo(2);
+    }
+
+    @DisplayName("현재 사다리에 머문다.")
+    @Test
+    void testMoveStraight() {
+        Line line = Line.of(List.of(false, false, false));
+
+        assertThat(line.move(1)).isEqualTo(1);
     }
 }
