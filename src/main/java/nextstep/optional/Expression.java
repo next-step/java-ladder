cd482921b43bdf2b16144aa00660c4990cf19273
sinsu <sinsu19012@gmail.com>
2024-10-28 21:22:02 +0900

    refactor: Optional 실습

--- src/main/java/nextstep/optional/Expression.java
+++ src/main/java/nextstep/optional/Expression.java
@@ -1,9 +1,14 @@
 package nextstep.optional;
 
-enum Expression {
-    PLUS("+"), MINUS("-"), TIMES("*"), DIVIDE("/");
+import java.util.Arrays;
 
-    private String expression;
+enum Expression {
+    PLUS("+"),
+    MINUS("-"),
+    TIMES("*"),
+    DIVIDE("/");
+
+    private final String expression;
 
     Expression(String expression) {
         this.expression = expression;
@@ -14,12 +19,10 @@ enum Expression {
     }
 
     static Expression of(String expression) {
-        for (Expression v : values()) {
-            if (matchExpression(v, expression)) {
-                return v;
-            }
-        }
+        return Arrays.stream(values())
+                .filter(v -> matchExpression(v, expression))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression)));
 
-        throw new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression));
     }
 }
