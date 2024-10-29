f52d3be6bd00c2e96a6e7db5e311599e6d071504
owljoa <needshield@gmail.com>
2024-10-29 20:27:17 +0900

    feat: 가로선을 무작위로 생성 - 사다리의 가로선이 연속되면 안됨

---
+++ src/main/java/nextstep/ladder/HorizontalLine.java
@@ -0,0 +1,48 @@
+package nextstep.ladder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class HorizontalLine {
+    private final List<Boolean> hasLineOrNotList;
+
+    private HorizontalLine(List<Boolean> hasLineOrNotList) {
+        this.hasLineOrNotList = hasLineOrNotList;
+    }
+
+    public HorizontalLine(int userCount) {
+        this(generateLines(userCount));
+    }
+
+    private static List<Boolean> generateLines(int userCount) {
+        Random random = new Random();
+        List<Boolean> lines = new ArrayList<>();
+        Boolean isPreviousLineExist = null;
+        int maxBooleanCountForLine = userCount - 1;
+
+        while (lines.size() < maxBooleanCountForLine) {
+            boolean isLineExist = generateIsLineExist(random, isPreviousLineExist);
+            lines.add(isLineExist);
+            isPreviousLineExist = isLineExist;
+        }
+
+        return lines;
+    }
+
+    private static boolean generateIsLineExist(Random random, Boolean isPreviousLineExist) {
+        boolean isLineExist = random.nextBoolean();
+        while (isPreviousLineExist != null && isLineExist == isPreviousLineExist) {
+            isLineExist = random.nextBoolean();
+        }
+        return isLineExist;
+    }
+
+    public int size() {
+        return hasLineOrNotList.size();
+    }
+
+    public Boolean hasLineOnIndex(int index) {
+        return hasLineOrNotList.get(index);
+    }
+}
