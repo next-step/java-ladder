9b2641b7b2406dee55718436eab3b86c704acf02
sinyebin <dpqls0625@naver.com>
2024-10-29 22:45:03 +0900

    feat: 참가자 이름을 쉼표(,) 기준으로 구분

---
+++ src/main/java/nextstep/ladder/Players.java
@@ -0,0 +1,20 @@
+package nextstep.ladder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Players {
+    private final List<Player> players;
+
+    public Players(String names) {
+        this.players = Arrays.stream(names.split(","))
+                .map(String::trim)
+                .map(Player::new)
+                .collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+}
---
+++ src/test/java/nextstep/ladder/PlayersTest.java
@@ -0,0 +1,15 @@
+package nextstep.ladder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PlayersTest {
+    @DisplayName("참가자 이름을 쉼표(,) 기준으로 구분")
+    @Test
+    void 참가자_이름_구분() {
+        Players players = new Players("pobi,honux,crong,jk");
+        assertThat(players.getPlayers()).hasSize(4);
+    }
+}
