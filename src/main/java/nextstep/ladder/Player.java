2789c5e9be78900362ea51340fbbe7f3c2cbbdc0
sinyebin <dpqls0625@naver.com>
2024-10-29 22:40:06 +0900

    feat: 참가자 이름은 최대 5글자

---
+++ src/main/java/nextstep/ladder/Player.java
@@ -0,0 +1,18 @@
+package nextstep.ladder;
+
+public class Player {
+    private static final String NAME_LENGTH_OVER = "이름은 최대 5글자까지 가능합니다.";
+    private static final int NAME_MAX_LENGTH = 5;
+    private final String name;
+
+    public Player(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException(NAME_LENGTH_OVER);
+        }
+    }
+}
