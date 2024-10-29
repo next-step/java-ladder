ddaac5ba7f4a5668e7649c6d0bd5d6678e9065d9
hunko <rhwlgns4386@naver.com>
2024-10-29 18:02:30 +0900

    feat : map, reduce, filter 실습 2 기능 구현

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,12 +25,24 @@ public class StreamStudy {
         return count;
     }
 
+    /*
+     * 단어의 길이가 12자를 초과하는 단어를 추출한다.
+     * 12자가 넘는 단어 중 길이가 긴 순서로 100개의 단어를 추출한다.
+     * 단어 중복을 허용하지 않는다. 즉, 서로 다른 단어 100개를 추출해야 한다.
+     * 추출한 100개의 단어를 출력한다. 모든 단어는 소문자로 출력해야 한다.
+     * */
     public static void printLongestWordTop100() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(s->s.length()>12)
+                .map(String::toLowerCase)
+                .distinct()
+                .sorted(Comparator.comparingInt(String::length))
+                .limit(100)
+                .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
