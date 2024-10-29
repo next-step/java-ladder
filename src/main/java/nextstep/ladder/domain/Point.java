b09204d031a512a317acd42888fe2c434621a9d8
이윤선 <yoonseon3@gmail.com>
2024-10-29 22:55:39 +0900

    refactor: ZERO 상수명 변경

--- src/main/java/nextstep/ladder/domain/Point.java
+++ src/main/java/nextstep/ladder/domain/Point.java
@@ -3,7 +3,7 @@ package nextstep.ladder.domain;
 import java.util.Objects;
 
 public class Point {
-    private static final int ZERO = 0;
+    private static final int MIN_VALUE = 0;
 
     private final int value;
 
@@ -13,7 +13,7 @@ public class Point {
     }
 
     private static void validateValue(int value) {
-        if (value < ZERO) {
+        if (value < MIN_VALUE) {
             throw new IllegalArgumentException("value는 0 미만일 수 없습니다.");
         }
     }
