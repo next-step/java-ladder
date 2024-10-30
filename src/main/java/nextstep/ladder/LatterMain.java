11cbe5abb3678ce01d2f091513550a92bd9a62bd
hunko <rhwlgns4386@naver.com>
2024-10-30 09:52:30 +0900

    feat : 이름입력로직 구현

---
+++ src/main/java/nextstep/ladder/LatterMain.java
@@ -0,0 +1,11 @@
+package nextstep.ladder;
+
+import nextstep.ladder.controller.LatterController;
+
+public class LatterMain {
+    public static void main(String[] args) {
+
+        LatterController controller = new LatterController();
+        controller.playGame();
+    }
+}
---
+++ src/main/java/nextstep/ladder/controller/LatterController.java
@@ -0,0 +1,10 @@
+package nextstep.ladder.controller;
+
+import nextstep.ladder.view.InputView;
+
+public class LatterController {
+
+    public void playGame() {
+        String names = InputView.readNames();
+    }
+}
