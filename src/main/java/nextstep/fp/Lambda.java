65de060143cfe30321b090fb0599a8e04edd059e
SangHyun Kim <ksh9891@gmail.com>
2024-10-30 22:05:44 +0900

    refactor(lambda): 람다를 활용해 중복 제거

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
@@ -22,30 +23,26 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sum(numbers, (number) -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sum(numbers, (number) -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
+        return sum(numbers, (number) -> number > 3);
+    }
+
+    public static int sum(final List<Integer> numbers, final Predicate<Integer> predicate) {
         int total = 0;
+
         for (int number : numbers) {
-            if (number > 3) {
+            if (predicate.test(number)) {
                 total += number;
             }
         }
+
         return total;
     }
 }
