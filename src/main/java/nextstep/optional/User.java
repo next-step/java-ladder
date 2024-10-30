8a994bbb580994279eeb9c0bfb91319311b2034e
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2024-10-30 14:15:41 +0900

    refactor : Optional 관련 실습 메서드 리펙토링

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,8 @@
 package nextstep.optional;
 
+import java.util.Objects;
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,7 +36,13 @@ public class User {
     }
 
     public static boolean ageIsInRange2(User user) {
-        return false;
+        return Optional.ofNullable(user)
+                .filter(Objects::nonNull)
+                .map(User::getAge)
+                .filter(Objects::nonNull)
+                .filter(age -> age >= 30)
+                .filter(age -> age <= 45)
+                .isPresent();
     }
 
     @Override
