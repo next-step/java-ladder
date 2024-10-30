75253d2317516f793d74c9978b7a2b72917b7420
KimSangHyun <ksh9891@gmail.com>
2024-10-30 22:57:26 +0900

    1단계 - 스트림, 람다, Optional (#2244)
    * doc(lambda): 요구사항 정리
    
    * feat(lambda): 람다 실습 1 - 익명 클래스를 람다로 전환
    
    * refactor(lambda): 익명 클래스를 람다로 전환
    
    * refactor(lambda): 람다를 활용해 중복 제거
    
    * feat(stream): map, reduce, filter 실습1
    
    * feat(stream):map, reduce, filter 실습2
    
    * feat(optional): 요구사항 1 - Optional 을 활용해 조건에 따른 반환
    
    * refactor(optional): 불필요한 import 제거
    
    * feat(optional): 요구사항 2 - Optional 에서 값을 반환
    
    * feat(optional): 요구사항 3 - Optional에서 exception 처리

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
@@ -18,39 +19,30 @@ public class Lambda {
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> System.out.println("Hello from thread")).start();
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sum(numbers, (number) -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sum(numbers, (number) -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
+        return sum(numbers, (number) -> number > 3);
+    }
+
+    public static int sum(final List<Integer> numbers, final Predicate<Integer> predicate) {
         int total = 0;
+
         for (int number : numbers) {
-            if (number > 3) {
+            if (predicate.test(number)) {
                 total += number;
             }
         }
+
         return total;
     }
 }
