c995af40da5f5890c1948eccb8c32f536c34b487
Seokhee Park <seokhee_park@nexcubecorp.com>
2024-10-30 17:53:17 +0900

    feat: 참여자 이름이 5글자를 초과하는 경우 예외 발생

--- src/main/java/nextstep/ladder/domain/Participant.java
+++ src/main/java/nextstep/ladder/domain/Participant.java
@@ -1,6 +1,8 @@
 package nextstep.ladder.domain;
 
 public class Participant {
+    private static final int MAX_NAME_LENGTH = 5;
+
     private final String name;
 
     public Participant(String name) {
@@ -11,6 +13,9 @@ public class Participant {
     private void validate(String name) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("참가자 이름은 null이거나 빈 문자열일 수 없습니다.");
+        }
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("참가자 이름은 5자를 초과할 수 없습니다.");
         }
     }
 }
--- src/test/java/nextstep/ladder/domain/ParticipantTest.java
+++ src/test/java/nextstep/ladder/domain/ParticipantTest.java
@@ -1,6 +1,7 @@
 package nextstep.ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
@@ -16,4 +17,10 @@ class ParticipantTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("참가자 명이 5글자 초과인 경우 예외 발생")
+    @Test
+    void createParticipantWithLongName() {
+        assertThatThrownBy(() -> new Participant("123456"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
