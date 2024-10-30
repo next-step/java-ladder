d4e1ae141d30aa1f67a835f92654f782f004687f
박경범 <pkb1998@naver.com>
2024-10-30 12:01:32 +0900

    # refactor : Stream과 Optional 사용하여 리팩토링

--- src/main/java/nextstep/optional/Users.java
+++ src/main/java/nextstep/optional/Users.java
@@ -10,14 +10,13 @@ public class Users {
             new User("crong", 35),
             new User("pobi", 30),
             new User("jk", 40),
-            new User("honux", 45));
+            new User("honux", 45)
+    );
 
     User getUser(String name) {
-        for (User user : users) {
-            if (user.matchName(name)) {
-                return user;
-            }
-        }
-        return DEFAULT_USER;
+        return users.stream()
+                .filter(user -> user.matchName(name))
+                .findFirst()
+                .orElse(DEFAULT_USER);
     }
 }
