d68229616bd4f71cdcc0097785cf945bd645fb50
parkkyoungbeom <gyoungbeom98@google.comgit config --global --listgit config --global user.email gyoungbeom98@google.com>
2024-10-31 22:51:25 +0900

    # refactor : 사다리 출력관련 indent 줄임

--- src/main/java/ladder/view/ResultView.java
+++ src/main/java/ladder/view/ResultView.java
@@ -1,22 +1,19 @@
 package ladder.view;
 
-import ladder.service.Bridge;
-import ladder.service.Ladder;
+import ladder.domain.Bridge;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class ResultView {
 
-    public static void printLadderResult(String[] participantNames, Ladder ladder) {
+    public static void printLadderResult(String[] participantNames, List<Bridge> ladder) {
         System.out.println("실행결과\n");
 
         printParticipantNames(participantNames);
 
-        List<Bridge> ladders = ladder.getLadders();
-        printLadder(ladders);
+        printLadder(ladder);
     }
 
     private static void printParticipantNames(String[] participantNames) {
@@ -28,15 +25,17 @@ public class ResultView {
     private static void printLadder(List<Bridge> ladders) {
         ladders.forEach(bridge -> {
             List<Boolean> connections = bridge.getConnections();
-            String output = IntStream.range(0, connections.size())
-                    .mapToObj(index -> {
-                        if (index == connections.size() - 1) {
-                            return connections.get(index) ? "|-----|" : "|     |";
-                        }
-                        return connections.get(index) ? "|-----" : "|     ";
-                    })
-                    .collect(Collectors.joining());
-            System.out.println("    " + output);
+            StringBuilder output = new StringBuilder("    |");
+
+            connections.forEach(isConnected -> {
+                if (isConnected) {
+                    output.append("-----|");
+                    return;
+                }
+                output.append("     |");
+            });
+
+            System.out.println(output);
         });
     }
 
