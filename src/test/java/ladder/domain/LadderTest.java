f7d914c85b0ff777a2a9b34393582703cb60a081
parkkyoungbeom <gyoungbeom98@google.comgit config --global --listgit config --global user.email gyoungbeom98@google.com>
2024-11-03 03:54:51 +0900

    # test : 사다리 타기 관련 테스트 코드 구현

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -3,7 +3,9 @@ package ladder.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -16,12 +18,34 @@ public class LadderTest {
 
         ConnectionStrategy alwaysTrueStrategy = new FakeConnectionStrategy(true);
 
-        List<Bridge> steps = ladder.createSteps(5, 4, alwaysTrueStrategy);
+        List<Bridge> steps = ladder.createSteps(new LadderSetting(4, 5), alwaysTrueStrategy);
 
         assertThat(steps.size()).isEqualTo(5);
         steps.forEach(bridge -> {
             assertThat(bridge.getConnections().size()).isEqualTo(3);
         });
+    }
+
+    @Test
+    @DisplayName("참가자의 사다리 타기 결과를 찾는 테스트")
+    public void findOutcomeForParticipant() {
+        Ladder ladder = new Ladder();
+
+        ConnectionStrategy alwaysTrueStrategy = new FakeConnectionStrategy(true);
+
+        ladder.createSteps(new LadderSetting(4, 5), alwaysTrueStrategy);
+
+        String[] participantNames = {"aaaa", "bbbb", "cccc", "dddd"};
+        String[] outcomes = {"꽝", "5000", "꽝", "3000"};
+
+        Map<String, String> expectedOutcomes = new LinkedHashMap<>();
+        expectedOutcomes.put("aaaa", "5000");
+        expectedOutcomes.put("bbbb", "꽝");
+        expectedOutcomes.put("cccc", "3000");
+        expectedOutcomes.put("dddd", "꽝");
+
+        assertThat(ladder.findOutcomeForParticipant(participantNames, outcomes))
+                .isEqualTo(expectedOutcomes);
     }
 
 }
--- src/test/java/ladder/service/LadderGameTest.java
+++ src/test/java/ladder/service/LadderGameTest.java
@@ -3,10 +3,13 @@ package ladder.service;
 import ladder.domain.Bridge;
 import ladder.domain.ConnectionStrategy;
 import ladder.domain.FakeConnectionStrategy;
+import ladder.domain.LadderSetting;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -19,12 +22,53 @@ public class LadderGameTest {
 
         ConnectionStrategy alwaysTrueStrategy = new FakeConnectionStrategy(true);
 
-        List<Bridge> ladder = ladderGame.createLadder(5, 4, alwaysTrueStrategy);
+        List<Bridge> ladder = ladderGame.createLadder(new LadderSetting(4, 5), alwaysTrueStrategy);
 
         assertThat(ladder.size()).isEqualTo(5);
         ladder.forEach(bridge -> {
             assertThat(bridge.getConnections().size()).isEqualTo(3);
         });
+    }
+
+    @Test
+    @DisplayName("입력받은 참가자의 결과를 찾는 테스트")
+    public void getOutcomeForeParticipant() {
+        LadderGame ladderGame = new LadderGame();
+
+        ConnectionStrategy alwaysTrueStrategy = new FakeConnectionStrategy(true);
+
+        List<Bridge> ladder = ladderGame.createLadder(new LadderSetting(4, 5), alwaysTrueStrategy);
+
+        String[] participantNames = {"aaaa", "bbbb", "cccc", "dddd"};
+        String[] outcomes = {"꽝", "5000", "꽝", "3000"};
+
+        ladderGame.playGame(participantNames, outcomes);
+
+        assertThat(ladderGame.getOutcomeForParticipant("aaaa")).isEqualTo("5000");
+    }
+
+    @Test
+    @DisplayName("사다리 게임 테스트")
+    public void playGame() {
+        LadderGame ladderGame = new LadderGame();
+
+        ConnectionStrategy alwaysTrueStrategy = new FakeConnectionStrategy(true);
+
+        List<Bridge> ladder = ladderGame.createLadder(new LadderSetting(4, 5), alwaysTrueStrategy);
+
+        String[] participantNames = {"aaaa", "bbbb", "cccc", "dddd"};
+        String[] outcomes = {"꽝", "5000", "꽝", "3000"};
+
+        ladderGame.playGame(participantNames, outcomes);
+
+        Map<String, String> expectedOutcomes = new LinkedHashMap<>();
+        expectedOutcomes.put("aaaa", "5000");
+        expectedOutcomes.put("bbbb", "꽝");
+        expectedOutcomes.put("cccc", "3000");
+        expectedOutcomes.put("dddd", "꽝");
+
+
+        assertThat(ladderGame.getParticipantOutcomes()).isEqualTo(expectedOutcomes);
     }
 
 }
