02861dcf2b4eb62e08b291e825fed36e9b9b38bc
ming <devel.moon17@gmail.com>
2024-11-02 23:19:52 +0900

    feat: 2단계 - 사람 이름 목록 stream 활용하여 코드 수정

--- src/main/java/nextstep/ladder/Usernames.java
+++ src/main/java/nextstep/ladder/Usernames.java
@@ -2,13 +2,14 @@ package nextstep.ladder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Usernames {
     List<Username> usernames = new ArrayList<>();
 
     public Usernames(List<String> usernameStrs){
-        for(String usernameStr : usernameStrs){
-            this.usernames.add(new Username(usernameStr));
-        }
+        this.usernames = usernameStrs.stream()
+                .map((str) -> new Username(str))
+                .collect(Collectors.toList());
     }
 }
