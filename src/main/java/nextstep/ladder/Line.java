3c6e1de89c68c01fbc1631b09e9bb2cce46d9c81
ming <devel.moon17@gmail.com>
2024-11-02 23:18:10 +0900

    feat: 2단계 - 사다리 가로선 생성 (랜덤), 사람 수만큼 세로 선 생성

---
+++ src/main/java/nextstep/ladder/Line.java
@@ -0,0 +1,24 @@
+package nextstep.ladder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class Line {
+    private List<Boolean> points = new ArrayList<>();
+    private static final Random RANDOM = new Random();
+
+    public Line(int countOfPerson){
+        IntStream.range(0, countOfPerson - 1).forEach((i) -> {
+            points.add(isLinePresentAt(i) ? false : RANDOM.nextBoolean());
+        });
+    }
+
+    private boolean isLinePresentAt(int i) {
+        if(i > 0 && points.get(i-1).booleanValue() == true){
+            return true;
+        }
+        return false;
+    }
+}
