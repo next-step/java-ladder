31943bab39c1f16612a456fec44e2b65970925e0
ming <devel.moon17@gmail.com>
2024-11-03 00:15:20 +0900

    refactor: 2단계 - 참여자 이름 길이 매직넘버 제거

--- src/main/java/nextstep/ladder/Username.java
+++ src/main/java/nextstep/ladder/Username.java
@@ -1,10 +1,12 @@
 package nextstep.ladder;
 
 public class Username {
-    String username;
+    private String username;
+
+    private static final int MAX_USERNAME_LENGTH = 5;
 
     public Username(String username) {
-        if (username.length() > 5) {
+        if (username.length() > MAX_USERNAME_LENGTH) {
             throw new IllegalArgumentException("사람 이름은 최대 5글자까지 입력 가능합니다.");
         }
         this.username = username;
