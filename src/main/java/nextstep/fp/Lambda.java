ff2c25eebc02cc4db4be99a48faae5c997e12c94
mitsui3point <rladbsgh0331@gmail.com>
2024-11-04 06:35:40 +0900

    feat(fp.lambda): modify Lambda(중복제거) add Condtion

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,5 +1,7 @@
 package nextstep.fp;
 
+import nextstep.fp.lambda.Condition;
+
 import java.util.List;
 
 public class Lambda {
@@ -26,31 +28,25 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
+    private static int sumAll(List<Integer> numbers, Condition c) {
         int total = 0;
         for (int number : numbers) {
-            total += number;
+            if (c.test(number)) {
+                total += number;
+            }
         }
         return total;
+    }
+
+    public static int sumAll(List<Integer> numbers) {
+        return sumAll(numbers, (number) -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, (number) -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, (number) -> number > 3);
     }
 }
