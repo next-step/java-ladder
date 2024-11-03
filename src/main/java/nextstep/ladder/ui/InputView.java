9f79127d6546480e796db4d3b0e361e989fe5be8
StoneHee99 <kiit0901@gmail.com>
2024-11-03 16:55:52 +0900

    feat: 실행결과 입력 기능 추가

--- src/main/java/nextstep/ladder/ui/InputView.java
+++ src/main/java/nextstep/ladder/ui/InputView.java
@@ -9,7 +9,8 @@ public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String PARTICIPANTS_NAME_QUESTION = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
-
+    private static final String RESULT_QUESTION = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private static final String LADDER_HEIGHT_QUESTION = "최대 사다리 높이는 몇 개인가요?";
     public static List<String> inputParticipantsName() {
         System.out.println(PARTICIPANTS_NAME_QUESTION);
         return Arrays.stream(SCANNER.nextLine()
@@ -18,9 +19,17 @@ public class InputView {
                 .collect(Collectors.toList());
     }
 
+    public static List<String> inputResult() {
+        System.out.println(RESULT_QUESTION);
+        return Arrays.stream(SCANNER.nextLine()
+                .split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
     public static int inputHeight() {
         printBlankLine();
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        System.out.println(LADDER_HEIGHT_QUESTION);
         return SCANNER.nextInt();
     }
 
