7fa86cb62d39728e907fcd17ae30a0e137d8b29b
Seokhee Park <seokhee_park@nexcubecorp.com>
2024-11-04 09:37:20 +0900

    feat: 결과 컬렉션 객체 추가

---
+++ src/main/java/nextstep/ladder/domain/LadderResult.java
@@ -0,0 +1,21 @@
+package nextstep.ladder.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LadderResult {
+
+    private final List<String> results;
+
+    public LadderResult(List<String> results) {
+        this.results = results;
+    }
+
+    public String getResult(int position) {
+        return results.get(position);
+    }
+
+    public List<String> getResults() {
+        return Collections.unmodifiableList(results);
+    }
+}
