9adbedda1a18dba5f44d15af466211a38f13fd95
KyoungBeom <pkb1998@naver.com>
2024-11-05 21:27:10 +0900

    # style : 누락된 {} 추가

--- src/main/java/ladder/domain/Position.java
+++ src/main/java/ladder/domain/Position.java
@@ -23,8 +23,12 @@ public class Position {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Position position = (Position) o;
         return value == position.value;
     }
