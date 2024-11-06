3a8c9db203c1c3a800c6020f7667385e1516b8c1
minham cho <mhtg90@gmail.com>
2024-11-07 00:31:25 +0900

    test : 이전 Dot의 값을 갖도록 Dot객체 개선 (테스트 케이스)

--- src/test/java/ladder/domain/DotTest.java
+++ src/test/java/ladder/domain/DotTest.java
@@ -1,53 +1,50 @@
 package ladder.domain;
 
-import org.junit.jupiter.api.DisplayName;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DotTest {
     @Test
     void create() {
-        Dot dot2 = new Dot(false);
-        assertFalse(dot2.getValue());
+        Dot dot2 = new Dot(false,false);
+        assertThat(dot2.getValue()).isFalse();
     }
 
     @Test
     void getMoveStep_stop() {
-        Dot dot2 = new Dot(false);
+        Dot dot2 = new Dot(false,false);
         int step = dot2.getMoveStep();
         assertThat(step).isEqualTo(0);
     }
 
     @Test
     void getMoveStep_move() {
-        Dot dot2 = new Dot(true);
+        Dot dot2 = new Dot(false,true);
         int step = dot2.getMoveStep();
         assertThat(step).isEqualTo(1);
     }
 
     @Test
-    void getMoveStepWithPreDot_back() {
-        Dot dot2 = new Dot(false);
-        int step = dot2.getMoveStepWithPreDot(true);
+    void getMoveDirection_back() {
+        Dot dot2 = new Dot(true,false);
+        int step = dot2.getMoveStep();
         assertThat(step).isEqualTo(-1);
     }
 
     @Test
     void getMoveStepWithPreDot_move() {
-        Dot dot2 = new Dot(true);
-        int step = dot2.getMoveStepWithPreDot(false);
+        Dot dot2 = new Dot(false, true);
+        int step = dot2.getMoveStep();
         assertThat(step).isEqualTo(1);
     }
 
     @Test
     void getMoveStepWithPreDot_stop() {
-        Dot dot2 = new Dot(false);
-        int step = dot2.getMoveStepWithPreDot(false);
+        Dot dot2 = new Dot(false,false);
+        int step = dot2.getMoveStep();
         assertThat(step).isEqualTo(0);
     }
 }
