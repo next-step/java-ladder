cb9cb72363930ec9b436a843a7da8ce2b8dbbfa4
ming <devel.moon17@gmail.com>
2024-11-06 20:12:38 +0900

    feat: 3단계 - 실행결과 입력 추가

--- src/main/java/nextstep/ladder/InputView.java
+++ src/main/java/nextstep/ladder/InputView.java
@@ -11,6 +11,11 @@ public class InputView {
         return List.of(scanner.next().split(","));
     }
 
+    public static List<String> inputResults() {
+        System.out.println("실행결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        return List.of(scanner.next().split(","));
+    }
+
     public static int inputHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         return scanner.nextInt();
--- src/main/java/nextstep/ladder/LadderMain.java
+++ src/main/java/nextstep/ladder/LadderMain.java
@@ -1,8 +1,11 @@
 package nextstep.ladder;
 
+import java.util.List;
+
 public class LadderMain {
     public static void main(String[] args) {
         Usernames usernames = new Usernames(InputView.inputUserNames());
+        List<String> results = InputView.inputResults();
         int height = InputView.inputHeight();
         ResultView.printLadderResult(usernames, new Lines(height, usernames.size()));
     }
