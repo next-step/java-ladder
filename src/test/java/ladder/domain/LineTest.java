fbb5e1ceedf8f74e5676b9360a66fd079f1ad7d7
minham cho <mhtg90@gmail.com>
2024-11-06 11:15:12 +0900

    test : 라인 결과 index 추가 기능 테스트

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -1,6 +1,7 @@
 package ladder.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,6 +12,27 @@ public class LineTest {
     @BeforeEach
     void beforeEech() {
         line = new Line(5, true);
+    }
+
+    @Test
+    @DisplayName("자신 Dot이 True인 경우, index + 1")
+    void getLineIndex_ofFirstDot() {
+        int result = line.getLineIndex(0);
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("앞쪽 & 자신 Dot이 모두 False인 경우, index")
+    void getLineIndex_ofLastDot() {
+        int result = line.getLineIndex(4);
+        assertThat(result).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("앞쪽 Dot이 True인 경우, index-1")
+    void getLineIndex_ofSecondDot() {
+        int result = line.getLineIndex(1);
+        assertThat(result).isEqualTo(0);
     }
 
     @Test
