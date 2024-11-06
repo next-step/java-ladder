e37e77ccd345bf6c60d32a4ce660876e872d4b3b
minham cho <mhtg90@gmail.com>
2024-11-06 11:14:41 +0900

    feat : 라인 결과 index 기능 추가

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -1,4 +1,5 @@
 package ladder.domain;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,18 +15,53 @@ public class Line {
         List<Dot> dots = new ArrayList<>();
         dots.add(0, new Dot(firstDotValue));
 
-        for (int i = 1; i < count - 1 ; i++) {
-            Boolean preDotValue = dots.get(i-1).getValue();
+        for (int i = 1; i < count - 1; i++) {
+            Boolean preDotValue = dots.get(i - 1).getValue();
             dots.add(i, new Dot(!preDotValue));
         }
 
-        dots.add(count - 1, new Dot( false));
+        dots.add(count - 1, new Dot(false));
 
         return dots;
     }
+
     public List<Boolean> getDots() {
         return dots.stream()
                 .map(Dot::getValue)
                 .collect(Collectors.toList());
+    }
+
+    public int getLineIndex(int index) {
+        if (index == 0) {
+            return getFirstDotIndex(index);
+        }
+        if (index == dots.size() - 1) {
+            return getLastDotIndex(index);
+        }
+        return getMiddleDotIndex(index);
+    }
+
+    private int getMiddleDotIndex(int index) {
+        if (dots.get(index - 1).getValue()) {
+            return index - 1;
+        }
+        if (dots.get(index).getValue()) {
+            return index + 1;
+        }
+        return index;
+    }
+
+    private int getLastDotIndex(int index) {
+        if (dots.get(index - 1).getValue()) {
+            return index - 1;
+        }
+        return index;
+    }
+
+    private int getFirstDotIndex(int index) {
+        if (dots.get(index).getValue()) {
+            return index + 1;
+        }
+        return index;
     }
 }
