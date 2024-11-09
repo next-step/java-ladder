a515c7bbf150be8ffb32303aa0fbdfc6d06a7eb5
BananMoon <younji1115@gmail.com>
2024-11-09 20:52:21 +0900

    refactor: 소스코드의 수직 정렬 원칙을 지킨다. (Step2 피드백 반영)

--- src/main/java/nextstep/laddergame/LadderGameInterface.java
+++ src/main/java/nextstep/laddergame/LadderGameInterface.java
@@ -5,15 +5,15 @@ import nextstep.laddergame.view.InputView;
 import nextstep.laddergame.view.OutputView;
 
 public class LadderGameInterface {
-
-    public void start() {
-        OutputView.printGameResult(LadderGame.start(
-                        InputView.enterParticipantsName(),
-                        InputView.enterLadderMaxHeight()));
-    }
-
     public static void main(String[] args) {
         LadderGameInterface gameInterface = new LadderGameInterface();
         gameInterface.start();
     }
+
+    public void start() {
+        OutputView.printGameResult(LadderGame.start(
+                InputView.enterParticipantsName(),
+                InputView.enterLadderMaxHeight()));
+    }
+
 }
