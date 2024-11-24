65c146666a284d222519d32d938a20c2f8766e99
praivesi <praivesi@gmail.com>
2024-11-25 00:00:46 +0900

    feat: add Studying stream & optional

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,7 +35,12 @@ public class User {
     }
 
     public static boolean ageIsInRange2(User user) {
-        return false;
+        Optional<User> optionalUser = Optional.ofNullable(user);
+
+        if (optionalUser.isEmpty()) { return false; }
+        if (optionalUser.get().getAge() == null) { return false; }
+
+        return optionalUser.filter(u -> u.age >= 30 && u.age <= 45).isPresent();
     }
 
     @Override
