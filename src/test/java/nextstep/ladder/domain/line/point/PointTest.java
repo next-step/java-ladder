e6ad6876d3da5591e83cf82b515083015d68177a
Ryumina-edu <fbalsk8897@gmail.com>
2024-11-25 16:44:01 +0900

    feat: 이동을 위한 값을 관리하는 Point 객체 추가

---
+++ src/test/java/nextstep/ladder/domain/line/point/PointTest.java
@@ -0,0 +1,35 @@
+package nextstep.ladder.domain.line.point;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PointTest {
+
+    @Test
+    void create_성공() {
+        Point point = new Point(true, false);
+
+        assertThat(point).isNotNull();
+        assertThat(point.canMoveLeft()).isTrue();
+        assertThat(point.canMoveRight()).isFalse();
+    }
+
+    @Test
+    @DisplayName("하나의 포인트에서 양쪽으로 동시에 이동할 수 없다")
+    void create_실패() {
+        assertThatThrownBy(() -> {
+            Point point = new Point(true, true);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("첫 번째 포인트는 왼쪽으로 이동할 수 없고, 오른쪽으로만 이동 가능성이 있어야 한다")
+    void first() {
+        Point point = Point.first(true);
+
+        assertThat(point.canMoveLeft()).isFalse();
+    }
+}
