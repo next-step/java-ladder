4588d3ce9755f39e87fe7760358ad21b21a942b7
Ryumina-edu <fbalsk8897@gmail.com>
2024-11-29 14:20:20 +0900

    feat: 이동 위치를 관리하는 Position 객체 추가

---
+++ src/main/java/nextstep/ladder/domain/ladderGame/position/Position.java
@@ -0,0 +1,31 @@
+package nextstep.ladder.domain.ladderGame.position;
+
+import nextstep.ladder.domain.line.point.Point;
+
+public class Position {
+    private final int position;
+
+    public Position(int position) {
+        if (0 > position) {
+            throw new IllegalArgumentException("위치는 음수일 수 없습니다.");
+        }
+
+        this.position = position;
+    }
+
+    public Position move(Point point) {
+        if (point.canMoveLeft()) {
+            return new Position(position - 1);
+        }
+
+        if (point.canMoveRight()) {
+            return new Position(position + 1);
+        }
+
+        return new Position(position);
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
---
+++ src/test/java/nextstep/ladder/domain/ladderGame/position/PositionTest.java
@@ -0,0 +1,33 @@
+package nextstep.ladder.domain.ladderGame.position;
+
+import nextstep.ladder.domain.line.point.Point;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PositionTest {
+
+    @Test
+    void create_성공() {
+        Position position = new Position(0);
+
+        Assertions.assertThat(position).isNotNull();
+        Assertions.assertThat(position.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    void create_실패() {
+        Assertions.assertThatThrownBy(() -> {
+            Position position = new Position(-1);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void move() {
+        Position position = new Position(1);
+        Point point = new Point(false, true);
+
+        Position movedPosition = position.move(point);
+
+        Assertions.assertThat(movedPosition.getPosition()).isEqualTo(2);
+    }
+}
