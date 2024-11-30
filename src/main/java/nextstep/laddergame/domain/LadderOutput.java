b8611c5edd461ab7e717e3ad5fbdab03cad2973e
BananMoon <younji1115@gmail.com>
2024-11-30 17:25:12 +0900

    feat: LadderOutput - 사다리 output 데이터 객체를 구현한다.

---
+++ src/main/java/nextstep/laddergame/domain/LadderOutput.java
@@ -0,0 +1,13 @@
+package nextstep.laddergame.domain;
+
+public class LadderOutput {
+    private final String winningData;
+
+    public LadderOutput(String winningData) {
+        this.winningData = winningData;
+    }
+
+    public String getWinningData() {
+        return winningData;
+    }
+}
