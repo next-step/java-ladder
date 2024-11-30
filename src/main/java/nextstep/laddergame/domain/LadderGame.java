11aa9ea35f6422fa8906091642f1de78eaadbe41
BananMoon <younji1115@gmail.com>
2024-11-30 17:38:36 +0900

    feat: 요청받은 참가자의 결과 값을 구하여 반환한다.

--- src/main/java/nextstep/laddergame/domain/LadderGame.java
+++ src/main/java/nextstep/laddergame/domain/LadderGame.java
@@ -1,22 +1,43 @@
 package nextstep.laddergame.domain;
 
+import nextstep.laddergame.service.LadderGameResult;
 import nextstep.laddergame.service.LaddersFactory;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LadderGame {
     private final Participants participants;
     private final Integer maxHeight;
     private final Ladders ladders;
+    private final List<LadderOutput> ladderOutputs;
 
-    private LadderGame(List<String> participants, Integer maxHeight, Ladders ladders) {
+    public LadderGame(List<String> participants, List<String> ladderOutputs, Integer maxHeight, LaddersFactory laddersFactory, LadderLinesGenerator ladderLinesGenerator) {
+        this(participants, ladderOutputs, maxHeight, laddersFactory.createLadders(participants.size(), maxHeight, ladderLinesGenerator));
+    }
+
+    private LadderGame(List<String> participants, List<String> ladderOutputs, Integer maxHeight, Ladders ladders) {
         this.participants = new Participants(participants);
         this.maxHeight = maxHeight;
         this.ladders = ladders;
+        this.ladderOutputs = ladderOutputs.stream().map(LadderOutput::new).collect(Collectors.toList());
     }
 
-    public LadderGame(List<String> participants, Integer maxHeight, LaddersFactory laddersFactory, LadderLinesGenerator ladderLinesGenerator) {
-        this(participants, maxHeight, laddersFactory.createLadders(participants.size(), maxHeight, ladderLinesGenerator));
+    public LadderGameResult resolveGameResult(Participant participant) {
+        Position ladderFinalPosition = participant.getPosition();
+        int lineIndex = 0;
+        while (lineIndex < maxHeight) {
+            ladderFinalPosition = moveOrNot(ladderFinalPosition, lineIndex);
+            lineIndex++;
+        }
+        return new LadderGameResult(participant, ladderOutputs.get(ladderFinalPosition.value()));
+    }
+
+    private Position moveOrNot(Position ladderFinalPosition, int lineIndex) {
+        if (ladders.isMovableLadder(ladderFinalPosition, lineIndex)) {
+            return ladderFinalPosition.move(ladders.resolveMoveDirection(ladderFinalPosition, lineIndex));
+        }
+        return ladderFinalPosition;
     }
 
     public Participants getParticipants() {
@@ -33,5 +54,13 @@ public class LadderGame {
 
     public int getLaddersSize() {
         return ladders.size();
+    }
+
+    public Participant getParticipant(String participantName) {
+        return participants.getParticipantBy(participantName);
+    }
+
+    public List<LadderOutput> getLadderOutputs() {
+        return ladderOutputs;
     }
 }
