35a16baf0a256ae1ae56b66aff2393b2d0b12b78
Ryumina-edu <fbalsk8897@gmail.com>
2024-12-02 15:58:42 +0900

    feat: 개인별 결과 출력을 위한 findIndexByName 메서드 추가

--- src/main/java/nextstep/ladder/domain/player/Players.java
+++ src/main/java/nextstep/ladder/domain/player/Players.java
@@ -2,11 +2,17 @@ package nextstep.ladder.domain.player;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 public class Players {
     private final List<Player> players;
 
     public Players(List<Player> players) {
+        if (players.size() < 2) {
+            throw new IllegalArgumentException("게임 참가자는 2명 이상이어야 합니다.");
+        }
+
         this.players = players;
     }
 
@@ -17,4 +23,17 @@ public class Players {
     public List<Player> getPlayers() {
         return Collections.unmodifiableList(players);
     }
+
+    public int findIndexByName(String playerName) {
+        Optional<Player> findedPlayer = players.stream()
+                                               .filter(player -> player.equals(new Player(playerName)))
+                                               .findFirst();
+
+        if (findedPlayer.isEmpty()) {
+            throw new NoSuchElementException("[" + playerName + "] 존재하지 않는 플레이어입니다.");
+        }
+
+        return players.indexOf(findedPlayer.get());
+    }
+
 }
--- src/test/java/nextstep/ladder/domain/player/PlayersTest.java
+++ src/test/java/nextstep/ladder/domain/player/PlayersTest.java
@@ -1,10 +1,13 @@
 package nextstep.ladder.domain.player;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayersTest {
 
@@ -14,5 +17,24 @@ class PlayersTest {
 
         assertThat(players).isNotNull();
         assertThat(players.playerCount()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("존재하는 플레이어인 경우 해당 플레이어의 인덱스를 반환한다")
+    void findIndexByName_성공() {
+        Players players = new Players(List.of(PlayerTest.PLAYER1, PlayerTest.PLAYER2));
+
+        assertThat(players.findIndexByName("mina")).isEqualTo(0);
+        assertThat(players.findIndexByName("tori")).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 플레이어인 경우 NoSuchElementException이 발생한다")
+    void findIndexByName_실패() {
+        Players players = new Players(List.of(PlayerTest.PLAYER1, PlayerTest.PLAYER2));
+
+        assertThatThrownBy(() -> {
+            players.findIndexByName("bob");
+        }).isInstanceOf(NoSuchElementException.class);
     }
 }
