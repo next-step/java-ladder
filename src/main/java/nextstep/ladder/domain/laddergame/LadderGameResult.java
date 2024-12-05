a84055dac30f562c9db4fe3aac86ad75a3f244b9
Ryumina-edu <fbalsk8897@gmail.com>
2024-12-05 14:46:07 +0900

    refactor: PlayerPositions 객체 분리로 인한 LadderGameResult 수정

--- src/main/java/nextstep/ladder/domain/laddergame/LadderGameResult.java
+++ src/main/java/nextstep/ladder/domain/laddergame/LadderGameResult.java
@@ -1,6 +1,7 @@
 package nextstep.ladder.domain.laddergame;
 
 import nextstep.ladder.domain.ladder.LadderResult;
+import nextstep.ladder.domain.laddergame.position.PlayerPositions;
 import nextstep.ladder.domain.laddergame.position.Positions;
 import nextstep.ladder.domain.player.Player;
 import nextstep.ladder.domain.player.Players;
@@ -9,27 +10,27 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class LadderGameResult {
-    private final Players players;
-    private final List<Positions> positions;
+
+    private final PlayerPositions playerPositions;
     private final LadderResult ladderResult;
 
     public LadderGameResult(List<Player> players, List<Positions> positions, LadderResult ladderResult) {
-        this.players = new Players(players);
-        this.positions = positions;
+        this(new PlayerPositions(new Players(players), positions), ladderResult);
+    }
+
+    public LadderGameResult(PlayerPositions playerPositions, LadderResult ladderResult) {
+        this.playerPositions = playerPositions;
         this.ladderResult = ladderResult;
     }
 
     public String getResult(String playerName) {
-        Positions playerPositions = positions.get(players.findIndexByName(playerName));
-        int lastPosition = playerPositions.getLastPosition();
-
-        return ladderResult.getResultByIndex(lastPosition);
+        return ladderResult.getResultByIndex(playerPositions.getLastPosition(playerName));
     }
 
     public List<String> getResultAll() {
-        return players.getPlayers().stream()
-                      .map(player -> player.getName() + " : " + getResult(player.getName()))
-                      .collect(Collectors.toList());
+        return playerPositions.getPlayers().stream()
+                              .map(player -> player.getName() + " : " + getResult(player.getName()))
+                              .collect(Collectors.toList());
     }
 
 }
