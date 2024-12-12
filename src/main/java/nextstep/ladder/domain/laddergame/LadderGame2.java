81df4db993a7f1aff4994a85ddb552d099813e73
Ryumina-edu <fbalsk8897@gmail.com>
2024-12-12 15:50:58 +0900

    refactor: ResultPosition 객체를 반환하는 LadderGame2 추가

---
+++ src/main/java/nextstep/ladder/domain/laddergame/LadderGame2.java
@@ -0,0 +1,27 @@
+package nextstep.ladder.domain.laddergame;
+
+import nextstep.ladder.domain.ladder.Ladder;
+import nextstep.ladder.domain.laddergame.position.Positions;
+import nextstep.ladder.domain.laddergame.position.ResultPosition;
+import nextstep.ladder.domain.line.Line;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class LadderGame2 {
+
+    public ResultPosition play(Ladder ladder) {
+        ResultPosition resultPosition = new ResultPosition();
+
+        IntStream.range(0, ladder.getPlayers().size())
+                 .forEach(i -> {
+                     resultPosition.add(i, getLastPosition(ladder.getLines(), i));
+                 });
+
+        return resultPosition;
+    }
+
+    private int getLastPosition(List<Line> lines, int playerPosition) {
+        return new Positions().add(lines, playerPosition).getLastPosition();
+    }
+}
