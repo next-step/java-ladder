6d03212b15440d6e78748712c489e6df5636f7d2
Ryumina-edu <fbalsk8897@gmail.com>
2024-12-12 16:18:14 +0900

    refactor: 게임 결과를 매칭하기 위한 match 메서드 추가

--- src/main/java/nextstep/ladder/domain/laddergame/position/ResultPosition.java
+++ src/main/java/nextstep/ladder/domain/laddergame/position/ResultPosition.java
@@ -1,7 +1,12 @@
 package nextstep.ladder.domain.laddergame.position;
 
+import nextstep.ladder.domain.ladder.LadderResult;
+import nextstep.ladder.domain.laddergame.LadderGameResult2;
+import nextstep.ladder.domain.player.Players;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class ResultPosition {
     private final Map<Integer, Integer> positionMap;
@@ -20,6 +25,19 @@ public class ResultPosition {
 
     public int getFinishPosition(int startPosition) {
         return positionMap.get(startPosition);
+    }
+
+    public LadderGameResult2 match(Players players, LadderResult ladderResult) {
+        LadderGameResult2 ladderGameResult2 = new LadderGameResult2();
+
+        IntStream.range(0, players.getPlayers().size())
+                 .forEach(i -> {
+                     String name = players.getPlayers().get(i).getName();
+                     String result = ladderResult.getResultByIndex(getFinishPosition(i));
+                     ladderGameResult2.add(name, result);
+                 });
+
+        return ladderGameResult2;
     }
 
 }
--- src/test/java/nextstep/ladder/domain/laddergame/position/ResultPositionTest.java
+++ src/test/java/nextstep/ladder/domain/laddergame/position/ResultPositionTest.java
@@ -1,8 +1,14 @@
 package nextstep.ladder.domain.laddergame.position;
 
+import nextstep.ladder.domain.ladder.LadderResult;
+import nextstep.ladder.domain.laddergame.LadderGameResult2;
+import nextstep.ladder.domain.player.Player;
+import nextstep.ladder.domain.player.Players;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 class ResultPositionTest {
@@ -27,4 +33,25 @@ class ResultPositionTest {
 
         Assertions.assertThat(resultPosition.getFinishPosition(0)).isEqualTo(3);
     }
+
+    @Test
+    void match() {
+        Map<Integer, Integer> positionMap = new HashMap<>();
+        positionMap.put(0, 1);
+        positionMap.put(1, 0);
+
+        ResultPosition resultPosition = new ResultPosition(positionMap);
+
+        Player player1 = new Player("test1");
+        Player player2 = new Player("test2");
+        Players players = new Players(List.of(player1, player2));
+
+        LadderResult ladderResult = new LadderResult(List.of("꽝", "5000"));
+
+        LadderGameResult2 gameResult = resultPosition.match(players, ladderResult);
+
+        Assertions.assertThat(gameResult.getResult("test1")).isEqualTo("5000");
+        Assertions.assertThat(gameResult.getResult("test2")).isEqualTo("꽝");
+    }
+
 }
