73cec8327c6b8ca98fe4da7fd3ee31ca16499af8
kimsuyeondev <rhaehl@naver.com>
2024-12-19 00:30:43 +0900

    step4 - 리팩터링 - 오류 수정

--- src/main/java/factory/LinesFactoryBean.java
+++ src/main/java/factory/LinesFactoryBean.java
@@ -9,7 +9,7 @@ public class LinesFactoryBean {
     public static LinesCreator createNextStepLadderFactory(int size, int maxLadder, LineStrategy lineStrategy) {
         NextStepLines nextStepLines = new NextStepLines();
         nextStepLines.generateLine(size, maxLadder, lineStrategy);
-        return new NextStepLines();
+        return nextStepLines;
     }
 
 }
