f56bb41fb50ab7d505b88f3ecb98396fbbd21ffd
minji-go <kohminji10@gmail.com>
2025-04-04 00:42:36 +0900

    feat: 입출력 및 로직 제어

---
+++ src/main/java/ladder/view/OutputView.java
@@ -0,0 +1,29 @@
+package ladder.view;
+
+import java.util.List;
+
+public class OutputView {
+    public void printNames(List<String> names) {
+        System.out.println("실행결과");
+        System.out.println();
+
+        for(String name : names) {
+            System.out.print(String.format("%5s ", name));
+        }
+        System.out.println();
+    }
+
+    public void printLadders(List<List<Boolean>> ladders) {
+        for(List<Boolean> ladder : ladders) {
+            System.out.print("    ");
+            for(Boolean connected : ladder) {
+                if(connected) {
+                    System.out.print("|-----");
+                } else {
+                    System.out.print("|     ");
+                }
+            }
+            System.out.println("|");
+        }
+    }
+}
