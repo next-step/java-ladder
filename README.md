93b7e01354399bb8e694575c1a701b17b459404a
YEOJIN YU <90737926+lyeo72@users.noreply.github.com>
2025-04-04 17:42:46 +0900

    Step1 - 스트림, 람다, Optional (#2310)
    * feat: 구현 목록 기능명세 추가
    
    * feat: 익명 클래스를 람다로 전환
    
    * feat: 익명 클래스를 람다로 전환
    
    * docs: 기능 명세 추가
    
    * feat: stream 실습
    
    * feat: sumOverThreeAndDouble 구현
    
    * feat: printLongestWordTop100 구현
    
    * feat: Optional을 활용해 조건에 따른 반환 - ageIsInRange2 구현
    
    * refactor: Optional에서 값을 반환 - getUser 리팩토링
    
    * refactor: Optional에서 exception 처리 - of 메소드 구현

--- README.md
+++ README.md
@@ -7,3 +7,13 @@
 
 ## 온라인 코드 리뷰 과정
 * [텍스트와 이미지로 살펴보는 온라인 코드 리뷰 과정](https://github.com/nextstep-step/nextstep-docs/tree/master/codereview)
+
+### 구현 목록
+* 익명 클래스를 람다로 전환
+* 람다를 활용해 중복 제거
+* Stream 실습
+* List에 담긴 모든 숫자 중 3보다 큰 숫자를 2배 한 후 모든 값의 합을 구한다.
+* nextstep.fp.StreamStudy 클래스의 printLongestWordTop100() 메서드를 구현한다
+* Optional을 활용해 조건에 따른 반환
+* Optional에서 값을 반환
+* Optional에서 exception 처리
--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,29 +5,34 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
 
+    private static final String REGEX = "[\\P{L}]+";
+
     public static long countWords() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+        List<String> words = Arrays.asList(contents.split(REGEX));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream().filter(w -> w.length() > 12)
+                .count();
     }
 
-    public static void printLongestWordTop100() throws IOException {
+    public static List<String> printLongestWordTop100() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        return words.stream()
+                .filter(i -> i.length() > 12)
+                .sorted(Comparator.comparing(String::length).reversed())
+                .distinct()
+                .map(String::toLowerCase)
+                .limit(100).collect(Collectors.toList());
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -35,10 +40,13 @@ public class StreamStudy {
     }
 
     public static long sumAll(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (x, y) -> x + y);
+        return numbers.stream().reduce(0, Integer::sum);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(i -> i > 3)
+                .map(i -> i * 2)
+                .reduce(0, Integer::sum);
     }
 }
