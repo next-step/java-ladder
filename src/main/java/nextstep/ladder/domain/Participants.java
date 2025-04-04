f1a50ce975d9d3ce2e8cc987fc5f1ad0b6b52a0d
logan.t(김성태) <logan.t@kakaocorp.com>
2025-04-04 14:07:47 +0900

    refactor: 참여자 목록 객체로 수정

---
+++ src/main/java/nextstep/ladder/domain/Participants.java
@@ -0,0 +1,43 @@
+package nextstep.ladder.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Participants {
+    public static final String NULL_OR_EMPTY_ERROR_MESSAGE = "이름은 null 이거나 빈 문자열일 수 없습니다";
+    public static final String TOO_LENGTH_ERROR_MESSAGE = "이름은 1자 이상 5자 이하로 입력해야 합니다";
+    private final List<String> names;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Participants that = (Participants) o;
+        return Objects.equals(names, that.names);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(names);
+    }
+
+    public Participants(String[] names) {
+        this(List.of(names));
+    }
+
+    public Participants(List<String> names) {
+        this.valid(names);
+        this.names = names;
+    }
+
+    private void valid(List<String> names) {
+        names.forEach(name -> {
+            if (name.isBlank()) {
+                throw new IllegalArgumentException(NULL_OR_EMPTY_ERROR_MESSAGE);
+            }
+
+            if (name.length() > 5) {
+                throw new IllegalArgumentException(TOO_LENGTH_ERROR_MESSAGE + " - " + name);
+            }
+        });
+    }
+}
---
+++ src/test/java/nextstep/ladder/domain/ParticipantsTest.java
@@ -0,0 +1,34 @@
+package nextstep.ladder.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ParticipantsTest {
+    @Test
+    void create() {
+        assertThat(new Participants(new String[]{"p1"})).isEqualTo(new Participants(List.of("p1")));
+    }
+
+    @Test
+    void null_empty_name() {
+        assertThatThrownBy(() -> new Participants(new String[]{"", " "}))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Participants.NULL_OR_EMPTY_ERROR_MESSAGE);
+    }
+
+    @Test
+    void invalid_name() {
+        assertThatThrownBy(() -> new Participants(new String[]{"p123456"}))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Participants.TOO_LENGTH_ERROR_MESSAGE);
+    }
+
+    @Test
+    void valid_name() {
+        assertThat(new Participants(new String[]{"p1", "p2"})).isInstanceOf(Participants.class);
+    }
+}
