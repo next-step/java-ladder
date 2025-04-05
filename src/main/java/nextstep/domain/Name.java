c1c01c993d9bf0263d6b776f61e337139babe631
yang Jiyoung <0112jyoung@naver.com>
2025-04-05 16:35:51 +0900

    [feat] 참여자 이름 객체 설정

---
+++ src/main/java/nextstep/domain/Name.java
@@ -0,0 +1,22 @@
+package nextstep.domain;
+
+public class Name {
+    private static final int MAX_NAME_LENGHT = 5;
+    public final String name;
+
+    public Name(String name) {
+        checkValidName(name);
+        this.name = name;
+    }
+
+    private void checkValidName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("빈 이름을 등록할 수 없습니다.");
+        }
+        if (name.length() > MAX_NAME_LENGHT) {
+            throw new IllegalArgumentException("이름은 최대 5글자만 가능합니다.");
+        }
+    }
+
+
+}
