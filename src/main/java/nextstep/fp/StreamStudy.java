9d6512213c7485ebcb7167e5b6f74111b5add613
baki <hgs6424@naver.com>
2025-04-06 01:08:39 +0900

    feat: map, reduce, filter 실습 2

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -28,7 +28,12 @@ public class StreamStudy {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         // TODO 이 부분에 구현한다.
-        long count = words.stream().filter(w -> w.length() > 12).count();
+        words.stream()
+                .filter(word -> word.length() > 12)
+                .sorted((a, b) -> b.length() - a.length())
+                .distinct()
+                .limit(100)
+                .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
