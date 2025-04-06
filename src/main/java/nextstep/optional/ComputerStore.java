cf89782364dbdc822c27900d7ab7a8c56b649984
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-04-06 13:32:15 +0900

    [ladder] step1: feedback #3

--- src/main/java/nextstep/optional/ComputerStore.java
+++ src/main/java/nextstep/optional/ComputerStore.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Optional;
+
 import nextstep.optional.Computer.Soundcard;
 import nextstep.optional.Computer.USB;
 
@@ -21,6 +23,10 @@ public class ComputerStore {
     }
 
     public static String getVersionOptional(Computer computer) {
-        return null;
+        return Optional.ofNullable(computer)
+            .map(Computer::getSoundcard)
+            .map(Soundcard::getUsb)
+            .map(USB::getVersion)
+            .orElse(UNKNOWN_VERSION);
     }
 }
