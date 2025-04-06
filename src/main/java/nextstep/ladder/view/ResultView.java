2c32e174a7d5d3f36e4fdbe5ba4b4535e0802306
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-04-06 16:16:03 +0900

    [ladder] step2: Typo

--- src/main/java/nextstep/ladder/view/ResultView.java
+++ src/main/java/nextstep/ladder/view/ResultView.java
@@ -6,6 +6,7 @@ import nextstep.ladder.module.Line;
 public class ResultView {
 
     public void printBoard(Board board) {
+        System.out.println();
         System.out.println("실행 결과");
         System.out.println();
         printPeopleNames(board);
