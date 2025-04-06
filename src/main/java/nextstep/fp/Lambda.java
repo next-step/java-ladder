310cc7b30ad6ca0b986c3ce53a4b7cd2846ba914
홍예슬/서비스개발팀/팀원 <yeseul@estaid.ai>
2025-04-07 08:10:57 +0900

    feat(step1): 람다 실습2 람다를 활용해 중복 제거

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.IntPredicate;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
@@ -26,31 +27,22 @@ public class Lambda {
         }).start();
     }
 
+    public static int sumAllByCondition(List<Integer> numbers, Condition condition) {
+        return numbers.stream()
+                .filter(condition::test)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAllByCondition(numbers, number -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAllByCondition(numbers, number -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAllByCondition(numbers, number -> number > 3);
     }
 }
