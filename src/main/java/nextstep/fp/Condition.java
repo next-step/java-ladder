6e63eb085d0f0963bca2661cfd5dcf4c6d97715b
Yeseul <yeseul.dev@gmail.com>
2025-04-08 00:45:53 +0900

    사다리타기: 1단계 - 스트림, 람다, Optional (#2334)
    * feat(step1): 람다 실습1 익명 클래스를 람다로 전환
    
    * feat(step1): 람다 실습2 람다를 활용해 중복 제거
    
    * feat(step1): 다양한 Stream 메서드 실습
    
    * feat(step1): 다양한 Optional 메서드 실습
    
    * feat(step1): 메서드 이름 변경
    
    ---------
    
    Co-authored-by: 홍예슬/서비스개발팀/팀원 <yeseul@estaid.ai>

---
+++ src/main/java/nextstep/fp/Condition.java
@@ -0,0 +1,6 @@
+package nextstep.fp;
+
+@FunctionalInterface
+public interface Condition {
+    boolean doSomething(Integer number);
+}
--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -26,31 +26,22 @@ public class Lambda {
         }).start();
     }
 
+    public static int sumAllByCondition(List<Integer> numbers, Condition condition) {
+        return numbers.stream()
+                .filter(condition::doSomething)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAllByCondition(numbers, number -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAllByCondition(numbers, number -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAllByCondition(numbers, number -> number > 3);
     }
 }
