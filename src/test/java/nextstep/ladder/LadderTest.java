83715c9c928c5bb18202a4527233acd33af85618
baki <hgs6424@naver.com>
2025-04-07 22:59:47 +0900

    feat: add Ladder class and initial test for creation

---
+++ src/test/java/nextstep/ladder/LadderTest.java
@@ -0,0 +1,13 @@
+package nextstep.ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LadderTest {
+
+    @Test
+    @DisplayName("사다리 생성 테스트")
+    void createTest() {
+        var ladder = new Ladder();
+    }
+}
