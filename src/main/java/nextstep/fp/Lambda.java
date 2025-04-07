36eff86e3ea77066df5e53efbd0157a754cfd3d0
권오규 <2024961@vieworks.com>
2025-04-07 16:43:16 +0900

    1단계

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -3,51 +3,23 @@ package nextstep.fp;
 import java.util.List;
 
 public class Lambda {
-    public static void printAllOld(List<Integer> numbers) {
-        System.out.println("printAllOld");
-
-        for (int number : numbers) {
-            System.out.println(number);
-        }
-    }
-
-    public static void printAllLambda(List<Integer> numbers) {
-        System.out.println("printAllLambda");
-
-        numbers.forEach(System.out::println);
-    }
-
-    public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
-    }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAll(numbers, number -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, number -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
+        return sumAll(numbers, number -> number > 3);
+    }
+
+    public static int sumAll(List<Integer> numbers, Conditional c) {
         int total = 0;
         for (int number : numbers) {
-            if (number > 3) {
+            if (c.test(number)) {
                 total += number;
             }
         }
--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -28,6 +28,13 @@ public class StreamStudy {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         // TODO 이 부분에 구현한다.
+        words.stream()
+                .map(String::toLowerCase) // 모든 단어를 소문자로 변환
+                .filter(w -> w.length() > 12) // 12자 초과 필터링
+                .distinct() // 중복 제거
+                .sorted((w1, w2) -> Integer.compare(w2.length(), w1.length())) // 길이 기준 내림차순 정렬
+                .limit(100) // 상위 100개 추출
+                .forEach(System.out::println); // 출력
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
