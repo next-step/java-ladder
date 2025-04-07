1e6acd82b3f3acc0ad802aaa23ffc7d7c7997404
yang Jiyoung <0112jyoung@naver.com>
2025-04-07 21:03:41 +0900

    [test] 선 연속성 테스트

--- src/main/java/nextstep/domain/line/Line.java
+++ src/main/java/nextstep/domain/line/Line.java
@@ -4,10 +4,18 @@ import nextstep.generator.RandomNumberGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Line {
     private final List<Point> points;
+
+    public Line(List<Boolean> points) {
+        this.points = points.stream()
+                        .map(Point::new)
+                        .collect(Collectors.toList());
+        checkValidPoints();
+    }
 
     public Line(int playerCounts, RandomNumberGenerator randomNumberGenerator) {
         points = new ArrayList<>();
@@ -18,10 +26,10 @@ public class Line {
     }
 
     private void checkValidPoints() {
-        IntStream.range(0, this.points.size() - 1)
+        IntStream.range(0, this.points.size())
                 .filter(index -> this.points.get(index).isConnected() && this.points.get(index + 1).isConnected())
                 .findFirst()
-                .ifPresent(index -> new RuntimeException("연속으로 선이 이어지만 안됩니다."));
+                .ifPresent(index -> {throw new RuntimeException("연속으로 선이 이어지만 안됩니다.");});
     }
 
     private Point createLine(int index, RandomNumberGenerator randomNumberGenerator) {
