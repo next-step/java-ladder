03236e3715f1edbc3d0b995fee1ca5ecb72c30e9
jorippppong <duelee75@gmail.com>
2025-04-07 13:45:26 +0900

    feat: lambda 실습

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -26,31 +26,25 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
+    public static int sum(List<Integer> numbers, SumStrategy s) {
         int total = 0;
         for (int number : numbers) {
-            total += number;
+            if (s.isAddable(number)) {
+                total += number;
+            }
         }
         return total;
+    }
+
+    public static int sumAll(List<Integer> numbers) {
+        return sum(numbers, number -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sum(numbers, number -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sum(numbers, number -> number > 3);
     }
 }
