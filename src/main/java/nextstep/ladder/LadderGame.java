285e1711a622d4097ab439fc25d9eb73f73ae195
sophia.song(송지현)/kakao <sophia.song@kakaocorp.com>
2025-04-08 13:54:35 +0900

    [feat] LadderGame

--- src/main/java/nextstep/ladder/LadderGame.java
+++ src/main/java/nextstep/ladder/LadderGame.java
@@ -1,5 +1,18 @@
 package nextstep.ladder;
 
+import nextstep.ladder.view.InputView;
+import nextstep.ladder.view.ResultView;
+
 public class LadderGame {
+  public static void main(String[] args) {
+    Participants participants = InputView.readParticipants();
+    int height = InputView.readLadderHeight();
+
+    Ladder ladder = LadderGenerator.generate(participants.count(), height);
+
+    ResultView.printTitle();
+    ResultView.printParticipants(participants);
+    ResultView.printLadder(ladder);
+  }
 
 }
--- src/main/java/nextstep/ladder/view/ResultView.java
+++ src/main/java/nextstep/ladder/view/ResultView.java
@@ -1,32 +1,49 @@
 package nextstep.ladder.view;
 
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import nextstep.ladder.Ladder;
 import nextstep.ladder.Line;
 import nextstep.ladder.Participants;
 
 public class ResultView {
-  private static final int NAME_WIDTH = Participants.MAX_NAME_LENGTH + 1;
 
-  public void printTitle() {
+  public static void printTitle() {
     System.out.println("\n실행결과\n");
   }
 
-  public void printParticipants(Participants participants) {
-    participants.names().forEach(name -> System.out.printf("%-" + NAME_WIDTH + "s", name));
+  public static void printParticipants(Participants participants) {
+    participants.names().forEach(name -> System.out.printf("%" + (Participants.MAX_NAME_LENGTH + 1) + "s", name));
     System.out.println();
   }
 
-  public void printLadder(Ladder ladder) {
+  public static void printLadder(Ladder ladder) {
     for (Line line : ladder.lines()) {
       printLine(line);
     }
   }
 
-  private void printLine(Line line) {
-    String lineStr = line.points().stream()
-        .map(point -> "|" + (point ? "-----" : "     "))
+  private static void printLine(Line line) {
+    System.out.println(" ".repeat(Participants.MAX_NAME_LENGTH) + buildLine(line));
+  }
+
+  private static String buildLine(Line line) {
+    List<Boolean> points = line.points();
+
+    return IntStream.range(0, points.size() + 1)
+        .mapToObj(i -> {
+          String bar = "|";
+          if (i < points.size()) {
+            bar += segment(points.get(i));
+          }
+          return bar;
+        })
         .collect(Collectors.joining());
-    System.out.println(lineStr + "|");
+  }
+
+  private static String segment(boolean connected) {
+    String unit = connected ? "-" : " ";
+    return unit.repeat(Participants.MAX_NAME_LENGTH);
   }
 }
