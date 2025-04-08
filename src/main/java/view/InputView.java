ab3d97fe3916b600d03c1a48dc70887c45f55ec1
JiEung2 <181757@jnu.ac.kr>
2025-04-09 00:07:08 +0900

    fix input 메서드에서 split 및 형변환을 해서 반환하도록 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,19 +1,24 @@
 package view;
 
+import utils.Parser;
+
+import java.util.List;
 import java.util.Scanner;
+
+import static utils.Parser.*;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String PARTICIPANT_NAMES_INPUT_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String MAX_LADDER_HEIGHT_INPUT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
 
-    public static String inputParticipantNames() {
+    public static List<String> inputParticipantNames() {
         System.out.println(PARTICIPANT_NAMES_INPUT_MESSAGE);
-        return scanner.nextLine();
+        return split(scanner.nextLine());
     }
 
-    public static String inputMaxLadderHeight() {
+    public static int inputMaxLadderHeight() {
         System.out.println(MAX_LADDER_HEIGHT_INPUT_MESSAGE);
-        return scanner.nextLine();
+        return toInt(scanner.nextLine());
     }
 }
