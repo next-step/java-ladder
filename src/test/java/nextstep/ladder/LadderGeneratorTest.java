514821aff2e185282f54002094f55850b9ffdcd3
sophia.song(송지현)/kakao <sophia.song@kakaocorp.com>
2025-04-08 13:32:11 +0900

    [feat] LadderGeneratorTest

---
+++ src/test/java/nextstep/ladder/LadderGeneratorTest.java
@@ -0,0 +1,19 @@
+package nextstep.ladder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LadderGeneratorTest {
+
+  @Test
+  @DisplayName("입력받은 가로, 세로 높이 크기의 사다리가 생성된다.")
+  void generate_ladder() {
+    Ladder ladder = LadderGenerator.generate(5, 4);
+    assertThat(ladder.lines()).hasSize(4);
+    assertThat(ladder.lines().get(0).points()).hasSize(5);
+  }
+
+}
