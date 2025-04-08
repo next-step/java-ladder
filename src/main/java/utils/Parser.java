22787b8495ed589008535fc974ee82c69caee013
JiEung2 <181757@jnu.ac.kr>
2025-04-08 22:43:07 +0900

    feat 사람 이름을 쉼표를 기준으로 구분하는 기능 추가

---
+++ src/main/java/utils/Parser.java
@@ -0,0 +1,13 @@
+package utils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Parser {
+    public static List<String> split(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+}
