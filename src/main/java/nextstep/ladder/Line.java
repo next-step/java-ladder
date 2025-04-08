51b43ae440d95cc032359180580254ce80e19986
baki <hgs6424@naver.com>
2025-04-09 00:02:29 +0900

    feat: implement Line class constructor to initialize points based on player count

--- src/main/java/nextstep/ladder/Line.java
+++ src/main/java/nextstep/ladder/Line.java
@@ -1,5 +1,16 @@
 package nextstep.ladder;
 
-public class Line {
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+public class Line {
+    private final List<Boolean> points;
+
+    public Line(int playerCount) {
+        this.points = IntStream.range(1, playerCount)
+                .mapToObj(it -> new Random().nextBoolean())
+                .collect(Collectors.toUnmodifiableList());
+    }
 }
--- src/test/java/nextstep/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -8,6 +8,7 @@ public class LineTest {
     @Test
     @DisplayName("사다리 라인 생성 테스트")
     void createTest() {
-        var line = new Line();
+        var playerCount = 5;
+        var line = new Line(playerCount);
     }
 }
