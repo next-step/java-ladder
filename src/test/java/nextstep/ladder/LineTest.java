a46d191838dd4530134ec0e3321b5291125c6d46
baki <hgs6424@naver.com>
2025-04-08 23:47:10 +0900

    feat: add Line class and corresponding test for line creation

---
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -0,0 +1,13 @@
+package nextstep.ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LineTest {
+
+    @Test
+    @DisplayName("사다리 라인 생성 테스트")
+    void createTest() {
+        var line = new Line();
+    }
+}
