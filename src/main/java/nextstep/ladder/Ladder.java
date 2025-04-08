7552fb7c9d17553c5b1e17bf8d9fcdaf8338ef70
sophia.song(송지현)/kakao <sophia.song@kakaocorp.com>
2025-04-08 13:25:15 +0900

    [feat] Ladder

--- src/main/java/nextstep/ladder/Ladder.java
+++ src/main/java/nextstep/ladder/Ladder.java
@@ -1,5 +1,15 @@
 package nextstep.ladder;
 
-public class Ladder {
+import java.util.List;
 
+public class Ladder {
+  private final List<Line> lines;
+
+  public Ladder(List<Line> lines) {
+    this.lines = lines;
+  }
+
+  public List<Line> lines() {
+    return lines;
+  }
 }
