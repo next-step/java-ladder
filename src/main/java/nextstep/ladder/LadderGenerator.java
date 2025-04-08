345219cdab82aba2a8aaccc3aff43b68e1d6426a
sophia.song(송지현)/kakao <sophia.song@kakaocorp.com>
2025-04-08 13:33:28 +0900

    [feat] LadderGenerator

--- src/main/java/nextstep/ladder/LadderGenerator.java
+++ src/main/java/nextstep/ladder/LadderGenerator.java
@@ -1,5 +1,14 @@
 package nextstep.ladder;
 
-public class LadderGenerator {
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+public class LadderGenerator {
+  public static Ladder generate(int width, int height) {
+    List<Line> lines = IntStream.range(0, height)
+        .mapToObj(i -> new Line(width))
+        .collect(Collectors.toList());
+    return new Ladder(lines);
+  }
 }
--- src/test/java/nextstep/ladder/LadderGeneratorTest.java
+++ src/test/java/nextstep/ladder/LadderGeneratorTest.java
@@ -1,7 +1,6 @@
 package nextstep.ladder;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
