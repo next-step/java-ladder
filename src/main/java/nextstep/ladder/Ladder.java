6ff036708a454050b33f62de48234556bedb18a9
baki <hgs6424@naver.com>
2025-04-09 00:02:20 +0900

    feat: implement Ladder class with player initialization and height setting

--- src/main/java/nextstep/ladder/Ladder.java
+++ src/main/java/nextstep/ladder/Ladder.java
@@ -1,5 +1,14 @@
 package nextstep.ladder;
 
-public class Ladder {
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Ladder {
+    private final List<Player> players;
+    private final Height height;
+
+    public Ladder(List<String> players, int height) {
+        this.players = players.stream().map(Player::new).collect(Collectors.toList());
+        this.height = new Height(height);
+    }
 }
--- src/test/java/nextstep/ladder/LadderTest.java
+++ src/test/java/nextstep/ladder/LadderTest.java
@@ -3,11 +3,15 @@ package nextstep.ladder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class LadderTest {
 
     @Test
     @DisplayName("사다리 생성 테스트")
     void createTest() {
-        var ladder = new Ladder();
+        var players = List.of("baki", "pobi");
+        var height = 5;
+        new Ladder(players, height);
     }
 }
