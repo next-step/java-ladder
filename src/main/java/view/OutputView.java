e1a8a1d5fd00c0de605f525d97a73a77cd7437c9
JiEung2 <181757@jnu.ac.kr>
2025-04-09 00:13:27 +0900

    feat 사다리 출력 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,8 +1,15 @@
 package view;
 
+import domain.Line;
+
 import java.util.List;
 
 public class OutputView {
+
+    private static final String HORIZONTAL_LINE = "-----";
+    private static final String EMPTY_LINE = "     ";
+    private static final String VERTICAL_LINE = "|";
+
     public static void printResultTitle() {
         System.out.println("\n실행결과\n");
     }
@@ -12,5 +19,23 @@ public class OutputView {
             System.out.printf("%-6s", name);
         }
         System.out.println();
+    }
+
+    public static void printLadder(List<Line> lines) {
+        for (Line line : lines) {
+            printLine(line.getPoints());
+        }
+    }
+
+    private static void printLine(List<Boolean> points) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(EMPTY_LINE);
+
+        for (boolean point : points) {
+            sb.append(VERTICAL_LINE);
+            sb.append(point ? HORIZONTAL_LINE : EMPTY_LINE);
+        }
+        sb.append(VERTICAL_LINE);
+        System.out.println(sb);
     }
 }
