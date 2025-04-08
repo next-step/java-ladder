8b72ceeb6f7161019fbc1245bc66cffec4f14d4c
JiEung2 <181757@jnu.ac.kr>
2025-04-09 00:09:34 +0900

    feat 이름을 반환하는 기능 추가

--- src/main/java/domain/Participant.java
+++ src/main/java/domain/Participant.java
@@ -9,6 +9,10 @@ public class Participant {
         this.name = name;
     }
 
+    public String getName() {
+        return this.name;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
