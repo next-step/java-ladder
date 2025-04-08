76002fdf354b54ec5fab6ce6c9bfd3d833a0368d
zoolake <junhomoon224@gmail.com>
2025-04-08 12:32:16 +0900

    refactor : (코드리뷰) 정적 팩터리 메서드 존재로 생성자 접근제한자를 private 으로 낮춘다

--- src/main/java/ladder/domain/Players.java
+++ src/main/java/ladder/domain/Players.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 public class Players {
     private final List<Player> players;
 
-    public Players(List<Player> players) {
+    private Players(List<Player> players) {
         this.players = players;
     }
 
