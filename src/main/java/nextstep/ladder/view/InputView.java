034e0cd8ef8beaa84d1e2e03b77a4aa06f34e662
sophia.song(송지현)/kakao <sophia.song@kakaocorp.com>
2025-04-09 12:33:03 +0900

    [feat] InputView

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -1,8 +1,10 @@
 package nextstep.ladder.view;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import nextstep.ladder.Participants;
+import nextstep.ladder.Results;
 
 public class InputView {
   private static final Scanner scanner = new Scanner(System.in);
@@ -13,9 +15,21 @@ public class InputView {
     return Participants.of(Arrays.asList(raw));
   }
 
+  public static Results inputResults() {
+    System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+    String input = scanner.nextLine();
+    List<String> results = Arrays.asList(input.split(","));
+    return new Results(results);
+  }
+
   public static int readLadderHeight() {
     System.out.println("\n최대 사다리 높이는 몇 개인가요?");
     return scanner.nextInt();
+  }
+
+  public static String inputQuery() {
+    System.out.println("결과를 보고 싶은 사람은?");
+    return scanner.nextLine();
   }
 
   public static void closeScanner() {
