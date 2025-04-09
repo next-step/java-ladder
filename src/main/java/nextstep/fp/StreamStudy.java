35a111e492dc1ecb397ce7770efd1231e2424609
DemonBlade <trknight78@gmail.com>
2025-04-09 22:44:35 +0900

    feat:사다리타기 1단계-스트림,람다,Optional (#2344)
    * feat:사다리타기 1단계-스트림,람다,Optional
    
    * refactor:STEP1 PR 반영입니다.
    
    * refactor:메소드 레퍼런스 활용

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -4,8 +4,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
@@ -27,7 +26,13 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(word -> word.length() > 12)
+                .distinct()
+                .sorted(Comparator.comparingInt(String::length).reversed())
+                .limit(100)
+                .map(String::toLowerCase)
+                .forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +44,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(number -> number > 3)
+                .mapToLong(number -> number * 2)
+                .reduce(0, Long::sum); // vs sum()
     }
 }
