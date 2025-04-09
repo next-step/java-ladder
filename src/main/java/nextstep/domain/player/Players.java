e618a25d301d38d29239dd62fbd7aea9ae51f8d4
yang Jiyoung <0112jyoung@naver.com>
2025-04-10 00:04:11 +0900

    [feat] 참여자 숫자 제한 추가

--- src/main/java/nextstep/domain/player/Players.java
+++ src/main/java/nextstep/domain/player/Players.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Players {
+    private static final int MIN_PLAYER_COUNT = 2;
+
     private final List<Player> players;
 
     public Players(List<Player> players) {
+        checkValidPlayerCounts(players);
         this.players = players;
     }
 
@@ -18,5 +21,11 @@ public class Players {
         return players.stream()
                 .map(Player::getPlayerName)
                 .collect(Collectors.toList());
+    }
+
+    private void checkValidPlayerCounts(List<Player> players) {
+        if(players.size() < MIN_PLAYER_COUNT) {
+            throw new IllegalArgumentException("참여자는 2명이상이어야 합니다.");
+        }
     }
 }
--- src/test/java/nextstep/domain/player/PlayerTest.java
+++ src/test/java/nextstep/domain/player/PlayerTest.java
@@ -3,6 +3,9 @@ package nextstep.domain.player;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PlayerTest {
@@ -12,5 +15,13 @@ class PlayerTest {
     void init() {
         Player player = new Player("test");
         assertEquals(player.getPlayerName(), "test");
+    }
+
+    @Test
+    @DisplayName("참여자가 2명 미만인 경우 예외가 발생한다.")
+    void checkValidPlayersCount() {
+        Player player = new Player("test");
+        assertThatThrownBy(() -> new Players(List.of(player)))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
