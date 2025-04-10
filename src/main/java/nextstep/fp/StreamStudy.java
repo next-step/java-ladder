cbc788cdf63e4d28e2c8dfaf9b3d3dd998e73feb
luka.redwing(이재석)/kakao <luka.redwing@kakaocorp.com>
2025-04-10 21:05:27 +0900

    feat printLongestWordTop100 구현

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,7 +24,15 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        List<String> result = words.stream()
+                .filter(word -> word.length() > 12)
+                .sorted(Comparator.comparing(String::length, Comparator.reverseOrder()))
+                .distinct()
+                .limit(100)
+                .map(String::toLowerCase)
+                .collect(Collectors.toList());
+
+        result.forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
