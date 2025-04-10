e08cb5374f4dc55aa11d6883215bad556c73a66a
baki <hgs6424@naver.com>
2025-04-10 20:15:02 +0900

    feat: refactor Ladder class to use Lines for line management and simplify initialization

--- src/main/java/nextstep/ladder/Ladder.java
+++ src/main/java/nextstep/ladder/Ladder.java
@@ -6,12 +6,10 @@ import java.util.stream.IntStream;
 
 public class Ladder {
     private final Players players;
-    private final List<Line> lines;
+    private final Lines lines;
 
     public Ladder(List<String> players, int height) {
         this.players = new Players(players);
-        this.lines = IntStream.range(0, height)
-                .mapToObj(i -> new Line(players.size()))
-                .collect(Collectors.toList());
+        this.lines = new Lines(height, players.size());
     }
 }
