f40768d36e5721a1aafc172531f34e55b330bcd1
luka.redwing(이재석)/kakao <luka.redwing@kakaocorp.com>
2025-04-10 20:58:23 +0900

    feat sumOverThreeAndDouble 구현

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -15,11 +15,7 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream().filter(word -> word.length() > 12).count();
     }
 
     public static void printLongestWordTop100() throws IOException {
@@ -39,6 +35,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(n -> n > 3)
+                .map(n -> n * 2)
+                .reduce(0, Integer::sum);
     }
 }
