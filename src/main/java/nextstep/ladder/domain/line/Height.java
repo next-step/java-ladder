4b041005b526144c8def0c001e26b9da8f2ad694
gan-ta <gilmat@naver.com>
2025-04-10 23:05:17 +0900

    refactor: Height 클래스 로직 리펙토링

--- src/main/java/nextstep/ladder/domain/line/Height.java
+++ src/main/java/nextstep/ladder/domain/line/Height.java
@@ -1,10 +1,12 @@
 package nextstep.ladder.domain.line;
 
+import java.util.Objects;
+
 public class Height {
     private final int value;
 
     public Height(int height) {
-        if (height <= 0) {
+        if (height < 1) {
             throw new IllegalArgumentException("높이는 1 이상이여야 합니다.");
         }
         this.value = height;
@@ -12,5 +14,18 @@ public class Height {
 
     public int getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Height height = (Height) o;
+        return value == height.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
     }
 }
