dd018990ff4e35065991f9e2a9aa69793f2d6d0b
Bomin Kim <seot100@naver.com>
2025-04-11 07:46:33 +0900

    Step1 (#2361)
    * refactor: 익명 클래스를 람다로 전환
    
    * refactor: 람다를 활용해 중복 제거
    
    * refactor: 스트림으로 구현
    
    * refactor: Optional을 활용해 조건에 따른 반환
    
    * refactor: Optional에서 값을 반환
    
    * refactor: Optional에서 exception 처리

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,7 +28,13 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        System.out.println(words.stream()
+                .filter(x -> x.length() > 12)
+                .distinct()
+                .sorted(Comparator.comparingInt(String::length).reversed())
+                .limit(100)
+                .map(String::toLowerCase)
+        );
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +46,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .filter(x -> x > 3)
+                .map(x -> 2 * x)
+                .reduce(0, (x, y) -> x + y);
     }
 }
