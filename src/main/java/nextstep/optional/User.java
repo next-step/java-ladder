b3bedb437ff45423404b573fa87ca9f302f79044
luka.redwing(이재석)/kakao <luka.redwing@kakaocorp.com>
2025-04-10 21:16:09 +0900

    feat ageIsInRange2 구현

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,7 +35,12 @@ public class User {
     }
 
     public static boolean ageIsInRange2(User user) {
-        return false;
+        Optional<User> userOptional = Optional.ofNullable(user);
+        return userOptional
+                .filter(u -> u.getAge() != null)
+                .map(User::getAge)
+                .filter(age -> age >= 30)
+                .filter(age -> age <= 45).isPresent();
     }
 
     @Override
