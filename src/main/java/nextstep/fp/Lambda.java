ff9d60ad638502c7bc9fea41ea3baa8a714ef2e9
redwings <43954535+luku756@users.noreply.github.com>
2025-04-11 15:46:13 +0900

    [사다리 타기] Step1 (#2360)
    * refactor Cartest 람다 변환
    
    * refactor sumAll, sumAllEven, sumAllOverThree 공통 코드 수정
    
    * feat sumOverThreeAndDouble 구현
    
    * feat printLongestWordTop100 구현
    
    * feat ageIsInRange2 구현
    
    * refactor getUser 수정
    
    * refactor Expression of 수정
    
    ---------
    
    Co-authored-by: luka.redwing(이재석)/kakao <luka.redwing@kakaocorp.com>

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -27,27 +27,22 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return conditionalSum(numbers, number -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return conditionalSum(numbers, number -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
+        return conditionalSum(numbers, number -> number > 3);
+    }
+
+    private static int conditionalSum(List<Integer> numbers,
+            Conditional conditional){
         int total = 0;
         for (int number : numbers) {
-            if (number > 3) {
+            if (conditional.test(number)) {
                 total += number;
             }
         }
