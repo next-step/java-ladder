1335834e3fdc20176079855d3d6efab9821e6a23
gan-ta <gilmat@naver.com>
2025-04-13 22:42:35 +0900

    style: 불필요 코드 정리

--- src/main/java/nextstep/ladder/domain/Ladder.java
+++ src/main/java/nextstep/ladder/domain/Ladder.java
@@ -1,25 +1,14 @@
 package nextstep.ladder.domain;
 
 import nextstep.ladder.domain.edge.LadderConfig;
-import nextstep.ladder.domain.edge.LadderPrizes;
-import nextstep.ladder.domain.edge.LadderResult;
-import nextstep.ladder.domain.edge.LadderUsers;
 import nextstep.ladder.domain.line.Height;
 import nextstep.ladder.domain.line.LineFactory;
 import nextstep.ladder.domain.line.Lines;
 import nextstep.ladder.dto.LadderDto;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Ladder {
     private final Lines lines;
     private final LadderConfig ladderConfig;
-
-    public Ladder(LadderUsers users, LadderPrizes prizes, int height) {
-        this(new LadderConfig(users, prizes), new Height(height), new LineFactory());
-    }
 
     public Ladder(LadderConfig ladderConfig, Height height, LineFactory lineFactory) {
         this.ladderConfig = ladderConfig;
--- src/main/java/nextstep/ladder/domain/line/Lines.java
+++ src/main/java/nextstep/ladder/domain/line/Lines.java
@@ -1,7 +1,6 @@
 package nextstep.ladder.domain.line;
 
 import nextstep.ladder.domain.edge.LadderUsers;
-import nextstep.ladder.domain.edge.Position;
 
 import java.util.List;
 import java.util.stream.Collectors;
