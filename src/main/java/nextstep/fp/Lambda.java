c65282b3478ea88f26ac321014dc757f63a989b9
panya <47997136+kjy2844@users.noreply.github.com>
2025-04-13 21:21:58 +0900

    Step1 (#2373)
    * docs: readme 작성
    
    * feat: 람다 실습 1 - 익명 클래스를 람다로 전환
    
    * feat: 람다 실습 2 - 람다를 활용해 중복 제거
    
    * feat: map, reduce, filter 실습 1
    
    * feat: map, reduce, filter 실습 2
    
    * feat: 요구사항 1 - Optional을 활용해 조건에 따른 반환
    
    * feat: 요구사항 2 - Optional에서 값을 반환
    
    * feat: 요구사항 3 - Optional에서 exception 처리

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,6 +1,7 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
@@ -27,27 +28,21 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAll(numbers, number -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, number -> number % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
+        return sumAll(numbers, number -> number > 3);
+    }
+
+    private static int sumAll(List<Integer> numbers, Conditional condition) {
         int total = 0;
         for (int number : numbers) {
-            if (number > 3) {
+            if (condition.test(number)) {
                 total += number;
             }
         }
