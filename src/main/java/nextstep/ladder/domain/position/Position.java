d4a02fdbd002ace7cb7475acfac7dd8d90e52764
gan-ta <gilmat@naver.com>
2025-04-13 17:25:38 +0900

    feat: Position 클래스 추가

---
+++ src/main/java/nextstep/ladder/domain/position/Position.java
@@ -0,0 +1,49 @@
+package nextstep.ladder.domain.position;
+
+import nextstep.ladder.domain.line.Line;
+
+import java.util.Objects;
+
+public class Position {
+    private final int value;
+
+    public Position(int start) {
+        if (start < 0) {
+            throw new IllegalArgumentException("위치 값은 0 이하가 될 수 없습니다.");
+        }
+        this.value = start;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public boolean canMoveLeft(Line line) {
+        return value > 0 && line.get(value - 1).getValue();
+    }
+
+    public boolean canMoveRight(Line line) {
+        return value < line.size() - 1 && line.get(value).getValue();
+    }
+
+    public Position moveLeft() {
+        return new Position(value - 1);
+    }
+
+    public Position moveRight() {
+        return new Position(value + 1);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return value == position.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+}
---
+++ src/test/java/nextstep/ladder/domain/position/PositionTest.java
@@ -0,0 +1,80 @@
+package nextstep.ladder.domain.position;
+
+import nextstep.ladder.domain.line.Line;
+import nextstep.ladder.domain.line.Point;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PositionTest {
+
+    @DisplayName("Position 인스턴스 생성")
+    @Test
+    public void testConstructor() {
+        assertDoesNotThrow(() -> new Position(1));
+    }
+
+    @DisplayName("Position 인스턴스 생성 - 음수 값은 들어갈 수 없음")
+    @Test
+    public void testConstructor_throwException () {
+        assertThatThrownBy(() -> new Position(-1))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("위치 값은 0 이하가 될 수 없습니다.");
+    }
+
+    @DisplayName("왼쪽으로 움직일 수 있음")
+    @Test
+    public void testCanMoveLeft_True() {
+        Line line = new Line(List.of(new Point(true), new Point(false)));
+
+        assertThat(new Position(1).canMoveLeft(line)).isTrue();
+    }
+
+    @DisplayName("왼쪽으로 움직이지 못함")
+    @Test
+    public void testCanMoveLeft_False() {
+        Line line = new Line(List.of(new Point(true), new Point(false)));
+
+        assertAll(
+            () -> assertThat(new Position(0).canMoveLeft(line)).isFalse(),
+            () -> assertThat(new Position(2).canMoveLeft(line)).isFalse()
+        );
+    }
+
+    @DisplayName("오른쪽으로 움직일 수 있음")
+    @Test
+    public void testCanMoveRight_True() {
+        Line line = new Line(List.of(new Point(true), new Point(false)));
+
+        assertThat(new Position(0).canMoveRight(line)).isTrue();
+    }
+
+    @DisplayName("오른쪽으로 움직이지 못함")
+    @Test
+    public void testCanMoveRight_False() {
+        Line line = new Line(List.of(new Point(true), new Point(false)));
+
+        assertAll(
+            () -> assertThat(new Position(1).canMoveRight(line)).isFalse(),
+            () -> assertThat(new Position(2).canMoveRight(line)).isFalse()
+        );
+    }
+
+
+    @DisplayName("왼쪽으로 이동")
+    @Test
+    public void testMoveLeft() {
+        assertThat(new Position(1).moveLeft()).isEqualTo(new Position(0));
+    }
+
+    @DisplayName("오른쪽으로 이동")
+    @Test
+    public void testMoveRight() {
+        assertThat(new Position(1).moveRight()).isEqualTo(new Position(2));
+    }
+}
