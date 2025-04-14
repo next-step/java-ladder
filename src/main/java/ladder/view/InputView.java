611e590b6b367253f8657a7805c71b2bfb7720c4
jayden(김준기) <jayden.i@kakaocorp.com>
2025-04-15 00:55:43 +0900

    실행 결과를 입력 받고 출력한다.

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -3,8 +3,9 @@ package ladder.view;
 import java.util.Scanner;
 
 public class InputView {
-    public static final String INPUT_USER_NAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
-    public static final String INPUT_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
+    private static final String INPUT_USER_NAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String INPUT_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
+    private static final String INPUT_PLAY_RESULT = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
 
     private final Scanner scanner;
 
@@ -20,5 +21,10 @@ public class InputView {
     public int inputLadderHeight() {
         System.out.println(INPUT_LADDER_HEIGHT);
         return Integer.parseInt(scanner.nextLine());
+    }
+
+    public String inputPlayResult() {
+        System.out.println(INPUT_PLAY_RESULT);
+        return scanner.nextLine();
     }
 }
