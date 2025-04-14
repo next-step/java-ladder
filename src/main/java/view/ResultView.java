e0681c1e3798cd067ff68c87b7ebe8d3910a2716
panya <kjy2844@kaist.ac.kr>
2025-04-14 15:08:30 +0900

    feat: 참여자 이름 출력 (5자 기준 정렬)

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -10,13 +10,13 @@ public class ResultView {
     public static void printResult(List<Player> players, Ladder ladder) {
         System.out.println("실행 결과");
         for (Player player : players) {
-            System.out.print(String.format("%-5s", player.getName()));
+            System.out.print(String.format("%-5s ", player.getName()));
         }
         System.out.println();
 
         for (LadderRow ladderRow : ladder.getLadderRows()) {
-            System.out.print("    |");
-            ladderRow.getColumns().forEach(column -> System.out.print(column ? "----|" : "    |"));
+            System.out.print("|");
+            ladderRow.getColumns().forEach(column -> System.out.print(column ? "-----|" : "     |"));
             System.out.println();
         }
     }
