2c94741c182f9481f1ab155858975aff279cca6c
jayden(김준기) <jayden.i@kakaocorp.com>
2025-04-15 10:03:45 +0900

    main 메서드를 구현한다.

--- src/main/java/ladder/Main.java
+++ src/main/java/ladder/Main.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Scanner;
 
 import ladder.domain.Ladder;
+import ladder.domain.LadderSimulator;
+import ladder.domain.LineFactory;
 import ladder.domain.User;
 import ladder.view.InputView;
 
 import static java.util.stream.Collectors.toList;
-import static ladder.util.StringUtil.parseUserList;
+import static ladder.util.StringUtil.parse;
+import static ladder.view.OutputView.outputLadderResult;
 import static ladder.view.OutputView.printLadderResult;
 
 public class Main {
@@ -16,13 +19,18 @@ public class Main {
         InputView inputView = new InputView(new Scanner(System.in));
         String userInput = inputView.inputUserName();
 
-        List<User> users = parseUserList(userInput).stream()
+        List<User> users = parse(userInput).stream()
             .map(User::new)
             .collect(toList());
 
-        int ladderHeight = inputView.inputLadderHeight();
+        List<String> results = parse(inputView.inputPlayResult());
 
-        Ladder ladder = new Ladder(ladderHeight, users.size());
-        printLadderResult(ladder, users);
+        int height = inputView.inputLadderHeight();
+        LineFactory factory = new LineFactory(height, users.size());
+        Ladder ladder = factory.createLadder();
+        printLadderResult(ladder, users, results);
+
+        LadderSimulator simulator = new LadderSimulator(ladder, users, results);
+        outputLadderResult(simulator, inputView);
     }
 }
