01dd9d96e941a19c76003379f46cb2c65acf9904
kass.fresh <kass.fresh@kakaocorp.com>
2025-04-18 14:26:49 +0900

    feat: implement `Result`

---
+++ src/main/java/nextstep/ladder/domain/Result.java
@@ -0,0 +1,16 @@
+package nextstep.ladder.domain;
+
+public class Result {
+    private String result;
+
+    public Result(String result) {
+        if (result == null || result.isEmpty()){
+            throw new IllegalArgumentException("결과는 빈 값일 수 없습니다");
+        }
+        this.result = result;
+    }
+
+    public String getResult() {
+        return result;
+    }
+}
