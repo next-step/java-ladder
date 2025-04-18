b6076008817e7123bece62a00eb00a749ca85f88
kass.fresh <kass.fresh@kakaocorp.com>
2025-04-18 14:29:59 +0900

    feat: implement controller for `Results`

--- src/main/java/nextstep/ladder/controller/LadderGameController.java
+++ src/main/java/nextstep/ladder/controller/LadderGameController.java
@@ -2,6 +2,7 @@ package nextstep.ladder.controller;
 
 import nextstep.ladder.domain.Ladder;
 import nextstep.ladder.domain.Players;
+import nextstep.ladder.domain.Results;
 import nextstep.ladder.view.InputView;
 import nextstep.ladder.view.ResultView;
 
@@ -9,13 +10,17 @@ public class LadderGameController {
 
     public void run() {
         String inputNames = InputView.inputPlayerNames();
+        String inputResults = InputView.inputResults();
         int height = InputView.inputLadderHeight();
 
         Players players = new Players(inputNames);
         Ladder ladder = new Ladder(height, players.size());
+        Results results = new Results(inputResults);
 
+        ResultView.printLadderResultMessage();
         ResultView.printPlayers(players);
         ResultView.printLadder(ladder);
+        ResultView.printResults(results);
     }
 
     public static void main(String[] args) {
