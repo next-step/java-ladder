3e9729d7efd93d73ddd759e9053aec28faee7028
kass.fresh <kass.fresh@kakaocorp.com>
2025-04-18 14:11:25 +0900

    feat: implement inputView for `Results`

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -7,11 +7,22 @@ public class InputView {
 
     public static String inputPlayerNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        return scanner.nextLine();
+        String input = scanner.nextLine();
+        System.out.println();
+        return input;
+    }
+
+    public static String inputResults() {
+        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        String input = scanner.nextLine();
+        System.out.println();
+        return input;
     }
 
     public static int inputLadderHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        return Integer.parseInt(scanner.nextLine());
+        int input = Integer.parseInt(scanner.nextLine());
+        System.out.println();
+        return input;
     }
 }
