feab1d3b3ff765091f500ca5b68d4480d7d29e8c
kass.fresh <kass.fresh@kakaocorp.com>
2025-04-20 17:54:56 +0900

    feat: add equals() and hashcode() to `Result`

--- src/main/java/nextstep/ladder/domain/Result.java
+++ src/main/java/nextstep/ladder/domain/Result.java
@@ -1,5 +1,7 @@
 package nextstep.ladder.domain;
 
+import java.util.Objects;
+
 public class Result {
     private String result;
 
@@ -12,5 +14,18 @@ public class Result {
 
     public String getResult() {
         return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Result result1 = (Result) o;
+        return Objects.equals(result, result1.result);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(result);
     }
 }
