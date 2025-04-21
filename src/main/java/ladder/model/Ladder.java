6f76838e085971b661a29ebd94e6b332b57b4637
luka.redwing(이재석)/kakao <luka.redwing@kakaocorp.com>
2025-04-21 21:02:16 +0900

    refactor 코드 정리 (step 3 피드백)

--- src/main/java/ladder/model/Ladder.java
+++ src/main/java/ladder/model/Ladder.java
@@ -27,11 +27,23 @@ public class Ladder {
     }
 
     public int run(int startIndex) {
-        int w = startIndex;
+        int currentIndex = startIndex;
+
         for (LadderLine line : lines) {
-            w += line.canMoveLeft(w) ? -1 : line.canMoveRight(w) ? 1 : 0;
+            currentIndex = getNextIndex(line, currentIndex);
         }
-        return w;
+
+        return currentIndex;
+    }
+
+    private int getNextIndex(LadderLine line, int index) {
+        if (line.canMoveLeft(index)) {
+            return index - 1;
+        } else if (line.canMoveRight(index)) {
+            return index + 1;
+        } else {
+            return index;
+        }
     }
 
 }
