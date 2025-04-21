a81b23594d50454d9bbd95a4eeff97b0edf637f6
lyeo72 <lyeo7272@gmail.com>
2025-04-22 04:26:48 +0900

    feat: 결과까지 함께 출력하도록 수정

--- src/main/java/ladder/LadderGame.java
+++ src/main/java/ladder/LadderGame.java
@@ -6,8 +6,18 @@ import ladder.view.OutputView;
 public class LadderGame {
     public static void start() {
         Players players = new Players(InputView.readNames());
+        Results results = InputView.readResults(players);
         Ladder ladder = new Ladder(InputView.readHeight(), players.size(), new RandomPointStrategy());
-        LadderResults.from(players, ladder);
-        OutputView.printResult(players.players(), ladder);
+        LadderResults ladderResults = LadderResults.from(players, ladder, results);
+        OutputView.printResult(ladderResults, ladder);
+
+        while (true) {
+            String name = InputView.askPlayerName();
+            if ("all".equals(name)) {
+                OutputView.printAllResults(ladderResults);
+                break;
+            }
+            OutputView.printSingleResult(name, ladderResults);
+        }
     }
 }
