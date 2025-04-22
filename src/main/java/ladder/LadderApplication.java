9ccd9126c9a5d8153512ec14d595ff56f2e1526c
zoolake <57625026+zoolake@users.noreply.github.com>
2025-04-22 16:13:10 +0900

    🚀 4단계 - 사다리(리팩터링) (#2405)
    * refactor : (코드리뷰) BridgeGenerator 상수로 관리
    
    * refactor : 인터페이스로 타입 변경

--- src/main/java/ladder/LadderApplication.java
+++ src/main/java/ladder/LadderApplication.java
@@ -1,17 +1,21 @@
 package ladder;
 
 import ladder.domain.*;
+import ladder.generator.BridgeGenerator;
 import ladder.generator.RandomBridgeGenerator;
 import ladder.view.InputView;
 import ladder.view.OutputView;
 
 public class LadderApplication {
+
+    private static final BridgeGenerator BRIDGE_GENERATOR = new RandomBridgeGenerator();
+
     public static void main(String[] args) {
         Players players = Players.of(InputView.getPlayerNames());
         Prizes prizes = Prizes.of(InputView.getPrizeNames());
         Height height = new Height(InputView.getLadderHeight());
 
-        Ladder ladder = Ladder.generateLadder(players.count(), height.getHeight(), new RandomBridgeGenerator());
+        Ladder ladder = Ladder.generateLadder(players.count(), height.getHeight(), BRIDGE_GENERATOR);
         OutputView.printGeneratedLadderResult(players, prizes, ladder);
 
         LadderGame ladderGame = new LadderGame(players, prizes, ladder);
