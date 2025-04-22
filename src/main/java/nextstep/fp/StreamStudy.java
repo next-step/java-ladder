94217233fd1612b2ab120c9ad991ff44cf299f4f
choincnp <whdudwns7777@gmail.com>
2025-04-22 15:56:19 +0900

    refactor : implement stream methods

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,7 +28,14 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        words.stream()
+            .filter(word -> word.length() > 12)
+            .sorted(Comparator.comparingInt(String::length).reversed())
+            .distinct()
+            .limit(100)
+            .map(String::toLowerCase)
+            .forEach(System.out::println);
+
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +47,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+            .filter(i -> i > 3)
+            .map(i -> i * 2)
+            .reduce(0, Integer::sum);
     }
 }
