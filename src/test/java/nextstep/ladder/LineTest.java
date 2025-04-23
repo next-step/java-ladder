580b371ce61bac75f361d5758f4a686afef7a391
Jiyoung Won <jiyoung928@gmail.com>
2025-04-23 14:05:57 +0900

    feature : Line 클래스에 move 함수 구현 및 테스트 코드 추가

--- src/test/java/nextstep/ladder/LineTest.java
+++ src/test/java/nextstep/ladder/LineTest.java
@@ -1,6 +1,6 @@
 package nextstep.ladder;
 
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,15 +9,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LineTest {
 
+    Line line;
+    Line falseLine;
+
+    @BeforeEach
+    void setUp() {
+        // 4명의 참가자 (points 크기 3)
+        // 고정된 가로줄 패턴: [true, false, true]
+        line = new Line(4, () -> true);
+
+        // 모든 point가 false
+        falseLine = new Line(4, () -> false);
+    }
+
     @Test
     void newLine_trueStrategy() {
-        Line line = new Line(4, () -> true);
         List<Boolean> points = line.getPoints();
 
-        assertEquals(points.size(), 3); // 크기 검증
-
-        // 사다리가 겹치지 않도록 생성
-        // 패턴: [true, false, true]
+        assertEquals(points.size(), 3);
         assertTrue(points.get(0));
         assertFalse(points.get(1));
         assertTrue(points.get(2));
@@ -25,8 +34,8 @@ public class LineTest {
 
     @Test
     void newLine_falseStrategy() {
-        Line line = new Line(4, () -> false);
-        List<Boolean> points = line.getPoints();
+        falseLine = new Line(4, () -> false);
+        List<Boolean> points = falseLine.getPoints();
 
         assertEquals(points.size(), 3);
         assertAll("모든 포인트는 false",
@@ -36,4 +45,34 @@ public class LineTest {
         );
     }
 
+
+    @Test
+    void 오른쪽_이동() {
+        assertEquals(1, line.move(0));
+        assertEquals(3, line.move(2));
+
+    }
+
+    @Test
+    void 왼쪽_이동() {
+        assertEquals(0, line.move(1));
+        assertEquals(2, line.move(3));
+
+    }
+
+
+    @Test
+    void 이동_안함() {
+        assertEquals(1, falseLine.move(1));
+    }
+
+    @Test
+    void 이동_위치는_0보다큰수() {
+        assertThrows(IllegalArgumentException.class, () -> line.move(-1));
+    }
+
+    @Test
+    void 이동_위치는_참가자보다작은수() {
+        assertThrows(IllegalArgumentException.class, () -> line.move(4));
+    }
 }
