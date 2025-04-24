a7de36a621b64926eefc651cc269dac7e1d0dbf2
selene-p <hwp0223@gmail.com>
2025-04-24 19:46:32 +0900

    feat: 결과 입력받는 기능 구현

--- src/main/java/nextstep/ladder/view/InputView.java
+++ src/main/java/nextstep/ladder/view/InputView.java
@@ -22,4 +22,9 @@ public class InputView {
       throw new IllegalArgumentException("사다리 높이는 숫자여야 합니다.");
     }
   }
+
+  public String readResult() {
+    System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+    return scanner.nextLine();
+  }
 }
