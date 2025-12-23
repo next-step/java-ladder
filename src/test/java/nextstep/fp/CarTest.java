70fa29e913a27b8c4c2939d369547fe303301244
모재영 <mo@utransfer.com>
2025-12-23 14:08:14 +0900

    feat : 익명클래스 람다로 전환하여 테스트의 의도 명확히 부각

--- src/test/java/nextstep/fp/CarTest.java
+++ src/test/java/nextstep/fp/CarTest.java
@@ -8,24 +8,16 @@ public class CarTest {
     @Test
     public void 이동() {
         Car car = new Car("pobi", 0);
-        Car actual = car.move(new MoveStrategy() {
-            @Override
-            public boolean isMovable() {
-                return true;
-            }
-        });
-        assertThat(actual).isEqualTo(new Car("pobi", 1));
+        Car alwaysMovable = car.move(() -> true);
+
+        assertThat(alwaysMovable).isEqualTo(new Car("pobi", 1));
     }
 
     @Test
     public void 정지() {
         Car car = new Car("pobi", 0);
-        Car actual = car.move(new MoveStrategy() {
-            @Override
-            public boolean isMovable() {
-                return false;
-            }
-        });
-        assertThat(actual).isEqualTo(new Car("pobi", 0));
+        Car neverMovable = car.move(() -> false);
+
+        assertThat(neverMovable).isEqualTo(new Car("pobi", 0));
     }
 }
