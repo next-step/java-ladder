d03bf65f135fb498d837ad7e0b18fa43150706eb
ho-jun97 <hojun9707@naver.com>
2026-01-05 23:38:47 +0900

    refactor: stream method 실습

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,11 +16,7 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return words.stream().filter((word) -> word.length() > 12).count();
     }
 
     public static void printLongestWordTop100() throws IOException {
@@ -27,7 +24,21 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+        List<String> top100LongestWords = findTop100LongestWords(overTwelveWords(words));
+
+        top100LongestWords.forEach((word) -> System.out.println(word.toLowerCase()));
+    }
+
+    public static List<String> overTwelveWords(List<String> words) {
+        return words.stream().filter((word) -> word.length() > 12).collect(Collectors.toList());
+    }
+
+    public static List<String> findTop100LongestWords(List<String> words) {
+        return words.stream()
+                .distinct()
+                .sorted(Comparator.comparingInt(String::length).reversed())
+                .limit(100)
+                .collect(Collectors.toList());
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +50,9 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        return sumAll(doubleNumbers(overThreeNumbers(numbers)));
+    }
+    public static List<Integer> overThreeNumbers(List<Integer> numbers) {
+        return numbers.stream().filter(x -> x > 3).collect(Collectors.toList());
     }
 }
