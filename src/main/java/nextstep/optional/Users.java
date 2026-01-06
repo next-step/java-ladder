e3575d57e9bbd8d6faef1e7ad78a5d56351e64b0
ho-jun97 <79128248+ho-jun97@users.noreply.github.com>
2026-01-06 09:24:59 +0900

    Step1 - 스트림, 람다, Optional (#2435)
    * refactor: 람다 실습1
    
    * refactor: 람다 실습2
    
    * refactor: stream method 실습
    
    * refactor: Optional

--- src/main/java/nextstep/optional/Users.java
+++ src/main/java/nextstep/optional/Users.java
@@ -2,6 +2,7 @@ package nextstep.optional;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Users {
     static final User DEFAULT_USER = new User("codesquad", 100);
@@ -13,11 +14,9 @@ public class Users {
             new User("honux", 45));
 
     User getUser(String name) {
-        for (User user : users) {
-            if (user.matchName(name)) {
-                return user;
-            }
-        }
-        return DEFAULT_USER;
+        return users.stream()
+                .filter(u -> u.matchName(name))
+                .findFirst()
+                .orElse(DEFAULT_USER);
     }
 }
