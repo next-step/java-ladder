e990a5643b1606ecc8e13336b411b8b58638c2e3
qwer920414-ctrl <qwer920414@gmail.com>
2026-01-13 00:55:48 +0900

    feat: Optional 실습 3 - of2() -> of() 교체

--- src/main/java/nextstep/optional/Expression.java
+++ src/main/java/nextstep/optional/Expression.java
@@ -16,16 +16,6 @@ enum Expression {
     }
 
     static Expression of(String expression) {
-        for (Expression v : values()) {
-            if (matchExpression(v, expression)) {
-                return v;
-            }
-        }
-
-        throw new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression));
-    }
-
-    static Expression of2(String expression) {
         return Arrays.stream(values())
                 .filter(e -> matchExpression(e, expression))
                 .findFirst()
