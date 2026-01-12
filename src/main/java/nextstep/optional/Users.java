d1e5117f4091cdd434eda396b295eb634f586a91
qwer920414-ctrl <qwer920414@gmail.com>
2026-01-13 00:44:34 +0900

    feat: Optional 실습 2 - getUser2() -> getUser() 교체

--- src/main/java/nextstep/optional/Users.java
+++ src/main/java/nextstep/optional/Users.java
@@ -14,15 +14,6 @@ public class Users {
             new User("honux", 45));
 
     User getUser(String name) {
-        for (User user : users) {
-            if (user.matchName(name)) {
-                return user;
-            }
-        }
-        return DEFAULT_USER;
-    }
-
-    User getUser2(String name) {
         return users.stream()
                 .filter(user -> user.matchName(name))
                 .findFirst()
